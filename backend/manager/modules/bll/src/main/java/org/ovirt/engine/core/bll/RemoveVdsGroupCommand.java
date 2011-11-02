2ffef90d8e67f9e3293546b5b74c5d4f682a321c
Eli Mesika <emesika@redhat.com>
2011-11-02 12:06:12 +0200

    backend:NPE Error during CanDoActionFailure remove non-existing cluster (#750257)
    https://bugzilla.redhat.com/750257
    
    Adding a check to canDoAction, if cluster is not found in DB anymore,
    canDoAction will fail and generate a
    "Cannot Remove Cluster. Cluster id is not valid." message.
    
    Change-Id: I919c3f64ec94cda8bd0bd2c212b90eb7112a2d11
    Acked-by: FirstName LastName <email>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RemoveVdsGroupCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RemoveVdsGroupCommand.java
@@ -26,31 +26,39 @@ public class RemoveVdsGroupCommand<T extends VdsGroupParametersBase> extends Vds
     protected boolean canDoAction() {
         List<vm_pools> list = null;
         boolean returnValue = super.canDoAction();
-        getReturnValue().getCanDoActionMessages()
-                .add(VdcBllMessages.VAR__ACTION__REMOVE.toString());
-        if (getVdsGroup().getID().equals(VDSGroup.DEFAULT_VDS_GROUP_ID)) {
-            addCanDoActionMessage(VdcBllMessages.VDS_CANNOT_REMOVE_DEFAULT_VDS_GROUP);
+        getReturnValue().getCanDoActionMessages().add(
+                VdcBllMessages.VAR__ACTION__REMOVE.toString());
+        if (getVdsGroup() == null) {
+            addCanDoActionMessage(VdcBllMessages.VMT_CLUSTER_IS_NOT_VALID);
             returnValue = false;
-        }
+        } else {
+            if (getVdsGroup().getID().equals(VDSGroup.DEFAULT_VDS_GROUP_ID)) {
+                addCanDoActionMessage(VdcBllMessages.VDS_CANNOT_REMOVE_DEFAULT_VDS_GROUP);
+                returnValue = false;
+            }
 
-        if (DbFacade.getInstance().getVdsStaticDAO().getAllForVdsGroup(getVdsGroup().getID()).size() != 0) {
-            addCanDoActionMessage(VdcBllMessages.VDS_CANNOT_REMOVE_VDS_GROUP_VDS_DETECTED);
-            returnValue = false;
-        }
-        if (DbFacade.getInstance().getVmStaticDAO().getAllByVdsGroup(getVdsGroup().getID()).size() != 0) {
-            addCanDoActionMessage(VdcBllMessages.VM_CANNOT_REMOVE_VDS_GROUP_VMS_DETECTED);
-            returnValue = false;
-        }
-        if (DbFacade.getInstance().getVmTemplateDAO().getAllForVdsGroup(getVdsGroup().getID()).size() != 0) {
-            addCanDoActionMessage(VdcBllMessages.VMT_CANNOT_REMOVE_VDS_GROUP_VMTS_DETECTED);
-            returnValue = false;
-        }
-        if ((list = DbFacade.getInstance().getVmPoolDAO().getAll()).size() > 0) {
-            for (vm_pools pool : list) {
-                if (pool.getvds_group_id().equals(getVdsGroup().getID())) {
-                    addCanDoActionMessage(VdcBllMessages.VDS_GROUP_CANNOT_REMOVE_HAS_VM_POOLS);
-                    returnValue = false;
-                    break;
+            if (DbFacade.getInstance().getVdsStaticDAO()
+                    .getAllForVdsGroup(getVdsGroup().getID()).size() != 0) {
+                addCanDoActionMessage(VdcBllMessages.VDS_CANNOT_REMOVE_VDS_GROUP_VDS_DETECTED);
+                returnValue = false;
+            }
+            if (DbFacade.getInstance().getVmStaticDAO()
+                    .getAllByVdsGroup(getVdsGroup().getID()).size() != 0) {
+                addCanDoActionMessage(VdcBllMessages.VM_CANNOT_REMOVE_VDS_GROUP_VMS_DETECTED);
+                returnValue = false;
+            }
+            if (DbFacade.getInstance().getVmTemplateDAO()
+                    .getAllForVdsGroup(getVdsGroup().getID()).size() != 0) {
+                addCanDoActionMessage(VdcBllMessages.VMT_CANNOT_REMOVE_VDS_GROUP_VMTS_DETECTED);
+                returnValue = false;
+            }
+            if ((list = DbFacade.getInstance().getVmPoolDAO().getAll()).size() > 0) {
+                for (vm_pools pool : list) {
+                    if (pool.getvds_group_id().equals(getVdsGroup().getID())) {
+                        addCanDoActionMessage(VdcBllMessages.VDS_GROUP_CANNOT_REMOVE_HAS_VM_POOLS);
+                        returnValue = false;
+                        break;
+                    }
                 }
             }
         }
