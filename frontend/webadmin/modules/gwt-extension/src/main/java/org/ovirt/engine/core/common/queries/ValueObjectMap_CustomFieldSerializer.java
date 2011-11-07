07ff3f27e8f85d7283239534a7155e16ea7307d9
Juan Hernandez <juan.hernandez@redhat.com>
2011-11-07 14:18:12 +0100

    webadmin: Fix ValueObjectMap serielizer (#NA)
    Fix the custom serializer for ValueObjectMap so that it handles
    correctly arrays in the server to GUI direction, otherwise the
    power management options (which are carried in one of these maps)
    are always presented incorrectly.
    
    Change-Id: Ia2b22f5ac5e7285f07c5a02857285b64c8a79402

--- frontend/webadmin/modules/gwt-extension/src/main/java/org/ovirt/engine/core/common/queries/ValueObjectMap_CustomFieldSerializer.java
+++ frontend/webadmin/modules/gwt-extension/src/main/java/org/ovirt/engine/core/common/queries/ValueObjectMap_CustomFieldSerializer.java
@@ -5,23 +5,35 @@ import com.google.gwt.user.client.rpc.SerializationStreamReader;
 import com.google.gwt.user.client.rpc.SerializationStreamWriter;
 
 public class ValueObjectMap_CustomFieldSerializer {
-	public static void deserialize(SerializationStreamReader streamReader,
-			ValueObjectMap instance) throws SerializationException {
-		// Handled in instantiate
-	}
 
-	public static ValueObjectMap instantiate(
-			SerializationStreamReader streamReader)
-			throws SerializationException {
-		// occur first
-		ValueObjectMap r = new ValueObjectMap();
-		r.setValuePairs((ValueObjectPair[])streamReader.readObject());
+    public static ValueObjectMap instantiate(SerializationStreamReader reader) throws SerializationException {
+        return new ValueObjectMap();
+    }
 
-		return r;
-	}
+    public static void serialize(SerializationStreamWriter writer, ValueObjectMap instance) throws SerializationException {
+        // First write the number of pairs:
+        ValueObjectPair[] pairs = instance.getValuePairs();
+        writer.writeInt(pairs.length);
 
-	public static void serialize(SerializationStreamWriter streamWriter,
-			ValueObjectMap instance) throws SerializationException {
-		streamWriter.writeObject(instance.getValuePairs());
-	}
+        // Then, for each pair, write first the key and then the value:
+        for (ValueObjectPair pair : pairs) {
+            writer.writeObject(pair.getKey());
+            writer.writeObject(pair.getValue());
+        }
+    }
+
+    public static void deserialize(SerializationStreamReader reader, ValueObjectMap instance) throws SerializationException {
+        // First read the number of pairs:
+        int length = reader.readInt();
+
+        // Then, for each pair, read first the key and then the value:
+        ValueObjectPair[] pairs = new ValueObjectPair[length];
+        for (int i = 0; i < length; i++) {
+            pairs[i] = new ValueObjectPair();
+            pairs[i].setKey(reader.readObject());
+            pairs[i].setValue(reader.readObject());
+        }
+        instance.setValuePairs(pairs);
+    }
+
 }
