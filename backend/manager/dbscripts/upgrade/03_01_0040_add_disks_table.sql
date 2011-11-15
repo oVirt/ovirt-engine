dd40969c1a67cb1a2277ce2e58252cbea4445322
Mike Kolesnik <mkolesni@redhat.com>
2011-11-15 09:25:48 +0200

    backend: Add disks table
    This table will hold the disk objects, each for a certain VM.
    
    Change-Id: I3c766721deaa99d0aa1cf736eb938f97c3248e71

---
+++ backend/manager/dbscripts/upgrade/03_01_0040_add_disks_table.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION Upgrade_AddDisksTable()
+RETURNS void
+AS $function$
+BEGIN
+   IF EXISTS (SELECT * FROM pg_tables WHERE tablename ILIKE 'disks') THEN
+       RETURN;
+   END IF;
+
+   -- Add the disks table.
+   CREATE TABLE disks
+   (
+      disk_id UUID NOT NULL,
+      status VARCHAR(32) NOT NULL,
+      internal_drive_mapping INTEGER NOT NULL,
+      active_image_id UUID CONSTRAINT fk_disk_active_image REFERENCES images(image_guid) ON DELETE SET NULL,
+      disk_type VARCHAR(32) NOT NULL,
+      disk_interface VARCHAR(32) NOT NULL,
+      wipe_after_delete BOOLEAN NOT NULL DEFAULT false,
+      propagate_errors VARCHAR(32) NOT NULL DEFAULT 'Off',
+      CONSTRAINT pk_disks PRIMARY KEY(disk_id)
+   )
+   WITH OIDS;
+
+END; $function$
+LANGUAGE plpgsql;
+
+
+SELECT * FROM Upgrade_AddDisksTable();
+
+DROP FUNCTION Upgrade_AddDisksTable();
+
