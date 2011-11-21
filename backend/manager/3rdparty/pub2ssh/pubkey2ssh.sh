6463706f78c98f42f016b21e1c11ae07a3096237
Roy Golan <rgolan@redhat.com>
2011-11-21 17:41:00 +0200

    3rdparty: fix pukey2ssh.sh compile command
    use gcc instead of specific alias g++
    
    Change-Id: I64a0d9ffab5b91bd7be6817a66dc7a7e3d8c5902

--- backend/manager/3rdparty/pub2ssh/pubkey2ssh.sh
+++ backend/manager/3rdparty/pub2ssh/pubkey2ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-g++ -I/usr/include/openssl  -L/usr/lib64/  -lcrypto src/pubkey2ssh.c -o bin/pubkey2ssh
+gcc -I/usr/include/openssl  -L/usr/lib64/  -lcrypto src/pubkey2ssh.c -o bin/pubkey2ssh
 
 exit $?
