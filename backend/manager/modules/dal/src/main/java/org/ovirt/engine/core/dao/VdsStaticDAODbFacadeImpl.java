c010015877ead98833c2ad6d239097cdf2ff9006
Laszlo Hornyak <lhornyak@redhat.com>
2011-12-09 10:55:25 +0100

    engine-core: vdsstaticdao redundancies
    This patch removes some dependencies of the VdsStaticDAODbFacadeImpl
    class
    
    Change-Id: I5725caf23116c534fde087113add21e2ed9d9b2e

--- backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/VdsStaticDAODbFacadeImpl.java
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/VdsStaticDAODbFacadeImpl.java
@@ -12,10 +12,10 @@ import org.ovirt.engine.core.common.businessentities.VdsStatic;
 import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.utils.log.Log;
-import org.ovirt.engine.core.utils.log.LogFactory;
 import org.ovirt.engine.core.dal.dbbroker.DbFacadeUtils;
 import org.ovirt.engine.core.engineencryptutils.EncryptionUtils;
+import org.ovirt.engine.core.utils.log.Log;
+import org.ovirt.engine.core.utils.log.LogFactory;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
@@ -23,233 +23,64 @@ import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 /**
  * <code>VdsDAODbFacadeImpl</code> provides an implementation of {@link VdsDAO} that uses previously written code from
  * {@code DbFacade}.
- *
- *
  */
 public class VdsStaticDAODbFacadeImpl extends BaseDAODbFacade implements VdsStaticDAO {
 
-     @Override
+    @Override
     public VdsStatic get(Guid id) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("vds_id", id);
-
-        ParameterizedRowMapper<VdsStatic> mapper = new ParameterizedRowMapper<VdsStatic>() {
-            @Override
-            public VdsStatic mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                VdsStatic entity = new VdsStatic();
-                entity.sethost_name(rs.getString("host_name"));
-                entity.setManagmentIp(rs.getString("ip"));
-                entity.setUniqueID(rs.getString("vds_unique_id"));
-                entity.setport(rs.getInt("port"));
-                entity.setvds_group_id(Guid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setId(Guid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setserver_SSL_enabled(rs
-                        .getBoolean("server_SSL_enabled"));
-                entity.setvds_type(VDSType.forValue(rs.getInt("vds_type")));
-                entity.setvds_strength(rs.getInt("vds_strength"));
-                entity.setpm_type(rs.getString("pm_type"));
-                entity.setpm_user(rs.getString("pm_user"));
-                entity.setpm_password(decryptPassword(rs.getString("pm_password")));
-                entity.setpm_port((Integer) rs.getObject("pm_port"));
-                entity.setpm_options(rs.getString("pm_options"));
-                entity.setpm_enabled(rs.getBoolean("pm_enabled"));
-                entity.setOtpValidity(rs.getLong("otp_validity"));
-
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeRead("GetVdsStaticByVdsId", mapper, parameterSource);
+        return getCallsHandler().executeRead("GetVdsStaticByVdsId",
+                new VdsStaticRowMapper(),
+                getCustomMapSqlParameterSource()
+                        .addValue("vds_id", id));
     }
 
     @Override
     public VdsStatic get(String name) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("vds_name", name);
-
-        ParameterizedRowMapper<VdsStatic> mapper = new ParameterizedRowMapper<VdsStatic>() {
-            @Override
-            public VdsStatic mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                VdsStatic entity = new VdsStatic();
-                entity.sethost_name(rs.getString("host_name"));
-                entity.setManagmentIp(rs.getString("ip"));
-                entity.setUniqueID(rs.getString("vds_unique_id"));
-                entity.setport(rs.getInt("port"));
-                entity.setvds_group_id(Guid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setId(Guid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setserver_SSL_enabled(rs
-                        .getBoolean("server_SSL_enabled"));
-                entity.setvds_type(VDSType.forValue(rs.getInt("vds_type")));
-                entity.setvds_strength(rs.getInt("vds_strength"));
-                entity.setpm_type(rs.getString("pm_type"));
-                entity.setpm_user(rs.getString("pm_user"));
-                entity.setpm_password(decryptPassword(rs.getString("pm_password")));
-                entity.setpm_port((Integer) rs.getObject("pm_port"));
-                entity.setpm_options(rs.getString("pm_options"));
-                entity.setpm_enabled(rs.getBoolean("pm_enabled"));
-                entity.setOtpValidity(rs.getLong("otp_validity"));
-
-                return entity;
-            }
-        };
-
         return (VdsStatic) DbFacadeUtils.asSingleResult(getCallsHandler().executeReadList("GetVdsStaticByVdsName",
-                mapper,
-                parameterSource));
+                new VdsStaticRowMapper(),
+                getCustomMapSqlParameterSource()
+                        .addValue("vds_name", name)));
     }
 
     @Override
     public List<VdsStatic> getAllForHost(String host) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("host_name", host);
-
-        ParameterizedRowMapper<VdsStatic> mapper = new ParameterizedRowMapper<VdsStatic>() {
-            @Override
-            public VdsStatic mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                VdsStatic entity = new VdsStatic();
-                entity.sethost_name(rs.getString("host_name"));
-                entity.setManagmentIp(rs.getString("ip"));
-                entity.setUniqueID(rs.getString("vds_unique_id"));
-                entity.setport(rs.getInt("port"));
-                entity.setvds_group_id(Guid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setId(Guid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setserver_SSL_enabled(rs
-                        .getBoolean("server_SSL_enabled"));
-                entity.setvds_type(VDSType.forValue(rs.getInt("vds_type")));
-                entity.setvds_strength(rs.getInt("vds_strength"));
-                entity.setpm_type(rs.getString("pm_type"));
-                entity.setpm_user(rs.getString("pm_user"));
-                entity.setpm_password(decryptPassword(rs.getString("pm_password")));
-                entity.setpm_port((Integer) rs.getObject("pm_port"));
-                entity.setpm_options(rs.getString("pm_options"));
-                entity.setpm_enabled(rs.getBoolean("pm_enabled"));
-                entity.setOtpValidity(rs.getLong("otp_validity"));
-
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("GetVdsStaticByHostName", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetVdsStaticByHostName",
+                new VdsStaticRowMapper(),
+                getCustomMapSqlParameterSource()
+                        .addValue("host_name", host));
     }
 
-   @Override
+    @Override
     public List<VdsStatic> getAllWithIpAddress(String address) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("ip", address);
-
-        ParameterizedRowMapper<VdsStatic> mapper = new ParameterizedRowMapper<VdsStatic>() {
-            @Override
-            public VdsStatic mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                VdsStatic entity = new VdsStatic();
-                entity.sethost_name(rs.getString("host_name"));
-                entity.setManagmentIp(rs.getString("ip"));
-                entity.setUniqueID(rs.getString("vds_unique_id"));
-                entity.setport(rs.getInt("port"));
-                entity.setvds_group_id(Guid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setId(Guid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setserver_SSL_enabled(rs
-                        .getBoolean("server_SSL_enabled"));
-                entity.setvds_type(VDSType.forValue(rs.getInt("vds_type")));
-                entity.setvds_strength(rs.getInt("vds_strength"));
-                entity.setpm_type(rs.getString("pm_type"));
-                entity.setpm_user(rs.getString("pm_user"));
-                entity.setpm_password(decryptPassword(rs.getString("pm_password")));
-                entity.setpm_port((Integer) rs.getObject("pm_port"));
-                entity.setpm_options(rs.getString("pm_options"));
-                entity.setpm_enabled(rs.getBoolean("pm_enabled"));
-                entity.setOtpValidity(rs.getLong("otp_validity"));
-
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("GetVdsStaticByIp", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetVdsStaticByIp",
+                new VdsStaticRowMapper(),
+                getCustomMapSqlParameterSource()
+                        .addValue("ip", address));
     }
 
     @Override
     public List<VdsStatic> getAllForVdsGroup(Guid vdsGroup) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("vds_group_id", vdsGroup);
-
-        ParameterizedRowMapper<VdsStatic> mapper = new ParameterizedRowMapper<VdsStatic>() {
-            @Override
-            public VdsStatic mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                VdsStatic entity = new VdsStatic();
-                entity.sethost_name(rs.getString("host_name"));
-                entity.setManagmentIp(rs.getString("ip"));
-                entity.setUniqueID(rs.getString("vds_unique_id"));
-                entity.setport(rs.getInt("port"));
-                entity.setvds_group_id(Guid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setId(Guid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setserver_SSL_enabled(rs
-                        .getBoolean("server_SSL_enabled"));
-                entity.setvds_type(VDSType.forValue(rs.getInt("vds_type")));
-                entity.setvds_strength(rs.getInt("vds_strength"));
-                entity.setpm_type(rs.getString("pm_type"));
-                entity.setpm_user(rs.getString("pm_user"));
-                entity.setpm_password(decryptPassword(rs.getString("pm_password")));
-                entity.setpm_port((Integer) rs.getObject("pm_port"));
-                entity.setpm_options(rs.getString("pm_options"));
-                entity.setpm_enabled(rs.getBoolean("pm_enabled"));
-                entity.setOtpValidity(rs.getLong("otp_validity"));
-
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("GetVdsStaticByVdsGroupId", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetVdsStaticByVdsGroupId",
+                new VdsStaticRowMapper(),
+                getCustomMapSqlParameterSource()
+                        .addValue("vds_group_id", vdsGroup));
     }
 
     @Override
     public void save(VdsStatic vds) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("host_name", vds.gethost_name())
-                .addValue("ip", vds.getManagmentIp())
-                .addValue("vds_unique_id", vds.getUniqueID())
-                .addValue("port", vds.getport())
-                .addValue("vds_group_id", vds.getvds_group_id())
-                .addValue("vds_id", vds.getId())
-                .addValue("vds_name", vds.getvds_name())
-                .addValue("server_SSL_enabled", vds.getserver_SSL_enabled())
-                .addValue("vds_type", vds.getvds_type())
-                .addValue("vds_strength", vds.getvds_strength())
-                .addValue("pm_type", vds.getpm_type())
-                .addValue("pm_user", vds.getpm_user())
-                .addValue("pm_password", encryptPassword(vds.getpm_password()))
-                .addValue("pm_port", vds.getpm_port())
-                .addValue("pm_options", vds.getpm_options())
-                .addValue("pm_enabled", vds.getpm_enabled())
-                .addValue("otpValidity", vds.getOtpValidity())
-                .addValue("vds_spm_priority", vds.getVdsSpmPriority());
         Map<String, Object> dbResults = new SimpleJdbcCall(jdbcTemplate)
-                .withProcedureName("InsertVdsStatic").execute(parameterSource);
+                .withProcedureName("InsertVdsStatic").execute(getInsertOrUpdateParams(vds));
 
         vds.setId(new Guid(dbResults.get("vds_id").toString()));
     }
 
     @Override
     public void update(VdsStatic vds) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
+        getCallsHandler().executeModification("UpdateVdsStatic", getInsertOrUpdateParams(vds));
+    }
+
+    private MapSqlParameterSource getInsertOrUpdateParams(final VdsStatic vds) {
+        return getCustomMapSqlParameterSource()
                 .addValue("host_name", vds.gethost_name())
                 .addValue("ip", vds.getManagmentIp())
                 .addValue("vds_unique_id", vds.getUniqueID())
@@ -268,9 +99,7 @@ public class VdsStaticDAODbFacadeImpl extends BaseDAODbFacade implements VdsStat
                 .addValue("pm_enabled", vds.getpm_enabled())
                 .addValue("otp_validity", vds.getOtpValidity())
                 .addValue("vds_spm_priority", vds.getVdsSpmPriority());
-        getCallsHandler().executeModification("UpdateVdsStatic", parameterSource);
     }
-
 
     @Override
     public void remove(Guid id) {
@@ -289,37 +118,61 @@ public class VdsStaticDAODbFacadeImpl extends BaseDAODbFacade implements VdsStat
         if (StringHelper.isEmpty(password)) {
             return password;
         }
-        String result = password;
-
         String keyFile = Config.resolveKeyStorePath();
         String passwd = Config.<String> GetValue(ConfigValues.keystorePass, Config.DefaultConfigurationVersion);
         String alias = Config.<String> GetValue(ConfigValues.CertAlias, Config.DefaultConfigurationVersion);
         try {
-            result = EncryptionUtils.encrypt((String) result, keyFile, passwd, alias);
+            return EncryptionUtils.encrypt((String) password, keyFile, passwd, alias);
         } catch (Exception e) {
             throw new SecurityException(e);
         }
-        return result;
     }
 
     public static String decryptPassword(String password) {
         if (StringHelper.isEmpty(password)) {
             return password;
         }
-        String result = password;
-
         String keyFile = Config.resolveKeyStorePath();
         String passwd = Config.<String> GetValue(ConfigValues.keystorePass, Config.DefaultConfigurationVersion);
         String alias = Config.<String> GetValue(ConfigValues.CertAlias, Config.DefaultConfigurationVersion);
         try {
-            result = EncryptionUtils.decrypt((String) result, keyFile, passwd, alias);
+            return EncryptionUtils.decrypt((String) password, keyFile, passwd, alias);
         } catch (Exception e) {
             log.debugFormat("Failed to decrypt password, error message: {0}", e.getMessage());
-            result = password;
+            return password;
         }
-        return result;
     }
 
     private static Log log = LogFactory.getLog(VdsStaticDAODbFacadeImpl.class);
+
+    private final static class VdsStaticRowMapper implements ParameterizedRowMapper<VdsStatic> {
+        @Override
+        public VdsStatic mapRow(ResultSet rs, int rowNum)
+                throws SQLException {
+            VdsStatic entity = new VdsStatic();
+            entity.sethost_name(rs.getString("host_name"));
+            entity.setManagmentIp(rs.getString("ip"));
+            entity.setUniqueID(rs.getString("vds_unique_id"));
+            entity.setport(rs.getInt("port"));
+            entity.setvds_group_id(Guid.createGuidFromString(rs
+                    .getString("vds_group_id")));
+            entity.setId(Guid.createGuidFromString(rs
+                    .getString("vds_id")));
+            entity.setvds_name(rs.getString("vds_name"));
+            entity.setserver_SSL_enabled(rs
+                    .getBoolean("server_SSL_enabled"));
+            entity.setvds_type(VDSType.forValue(rs.getInt("vds_type")));
+            entity.setvds_strength(rs.getInt("vds_strength"));
+            entity.setpm_type(rs.getString("pm_type"));
+            entity.setpm_user(rs.getString("pm_user"));
+            entity.setpm_password(decryptPassword(rs.getString("pm_password")));
+            entity.setpm_port((Integer) rs.getObject("pm_port"));
+            entity.setpm_options(rs.getString("pm_options"));
+            entity.setpm_enabled(rs.getBoolean("pm_enabled"));
+            entity.setOtpValidity(rs.getLong("otp_validity"));
+
+            return entity;
+        }
+    }
 
 }
