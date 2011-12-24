132bef1efbb7377fe5bbb06a12bf430f8d756065
Moti Asayag <masayag@redhat.com>
2011-12-24 12:11:39 +0200

    backend: Minor code clean-up for VdsIdParametersBase
    Organize the VdsIdParametersBase class and remove C# related annotations.
    
    Change-Id: I0a8faa7fc6997d3a0d4f310deda77536849129e2

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/VdsIdParametersBase.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/VdsIdParametersBase.java
@@ -1,27 +1,21 @@
 package org.ovirt.engine.core.common.queries;
 
-import org.ovirt.engine.core.compat.*;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlType;
+import org.ovirt.engine.core.compat.Guid;
 
-@XmlAccessorType(XmlAccessType.NONE)
-@XmlType(name = "VdsIdParametersBase")
 public class VdsIdParametersBase extends VdcQueryParametersBase {
     private static final long serialVersionUID = -232268659060166995L;
 
-    public VdsIdParametersBase(Guid vdsId) {
-        _vdsId = vdsId;
-    }
-
-    @XmlElement(name = "VdsId")
-    private Guid _vdsId;
-
-    public Guid getVdsId() {
-        return _vdsId;
-    }
+    private Guid vdsId;
 
     public VdsIdParametersBase() {
     }
+
+    public VdsIdParametersBase(Guid vdsId) {
+        this.vdsId = vdsId;
+    }
+
+    public Guid getVdsId() {
+        return vdsId;
+    }
+
 }
