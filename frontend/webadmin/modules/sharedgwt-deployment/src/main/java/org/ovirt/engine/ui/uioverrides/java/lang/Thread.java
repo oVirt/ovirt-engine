62ac810d99342fd2fa471ca356b704b28893faf8
Asaf Shakarchi <asaf@redhat.com>
2012-01-01 12:21:43 +0200

    gwt: Removed sharedgwt-deployment module
    - Removed sharedgwt-deployment
    - Userportal depends on gwt-extensions now
    - Upgraded GWT version for UserPortal 2.2->2.3
    
    Change-Id: Ice988a9f113e5259840eada901821e105d661d4a

--- frontend/webadmin/modules/sharedgwt-deployment/src/main/java/org/ovirt/engine/ui/uioverrides/java/lang/Thread.java
+++
@@ -1,7 +0,0 @@
-package java.lang;
-
-public class Thread {
-    public static void sleep(long millis) throws InterruptedException {
-        throw new RuntimeException("Thread.sleep is not implemented in GWT!");
-    }
-}
