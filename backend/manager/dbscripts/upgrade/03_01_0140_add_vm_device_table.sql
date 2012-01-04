6043b0dacd0470fe70d60a2f040ef7fd98c060b9
Eli Mesika <emesika@redhat.com>
2012-01-04 11:19:52 +0200

    core: Adding vm_device table
    Adding the vm_device table to the database.
    The vm_device table stores both managed and unmanaged devices for each Vm or Template
    
    Change-Id: Ib76c08ada9dcbeb4fc08f4f6615a29fa20f09244

---
+++ backend/manager/dbscripts/upgrade/03_01_0140_add_vm_device_table.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION add_vm_device_03_01_0110()
+RETURNS void
+AS $function$
+BEGIN
+   IF NOT EXISTS (SELECT * FROM information_schema.tables WHERE table_name ILIKE 'vm_device') THEN
+       -- Add the vm_device table.
+       CREATE TABLE vm_device
+       (
+          device_id UUID NOT NULL,
+          vm_id UUID NOT NULL CONSTRAINT fk_vm_device_vm_static REFERENCES vm_static(vm_guid) ON DELETE CASCADE,
+          type varchar(30) NOT NULL,
+          device varchar(30) NOT NULL,
+          address varchar(255) NOT NULL,
+          boot_order int default 0,
+          spec_params text,
+          is_managed boolean NOT NULL default false,
+          is_plugged boolean NOT NULL default false,
+          is_shared boolean NOT NULL default false,
+          is_readonly boolean NOT NULL default false,
+          CONSTRAINT pk_vm_device  PRIMARY KEY(device_id,vm_id)
+       )
+       WITH OIDS;
+   END IF;
+
+END; $function$
+LANGUAGE plpgsql;
+
+SELECT * FROM add_vm_device_03_01_0110();
+DROP FUNCTION add_vm_device_03_01_0110();
+
