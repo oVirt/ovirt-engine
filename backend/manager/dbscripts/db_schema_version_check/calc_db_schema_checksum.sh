14012c756b7015aeabb309988d1f80590fbb3e73
Yair Zaslavsky <yzaslavs@redhat.com>
2012-01-04 15:20:26 +0200

    core: fixing calc_db_schema_checksum.sh
    Taking care of several issues + some styling to fix this script
    
    Acked-By:
    
    Change-Id: I35064f2bfc614b3b67fb9661cf1f28ffa349fa7f

--- backend/manager/dbscripts/db_schema_version_check/calc_db_schema_checksum.sh
+++ backend/manager/dbscripts/db_schema_version_check/calc_db_schema_checksum.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
-# selects all current checksums for all db scripts and performs md5sum on them (md5 on the list of md5 values)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DBSCRIPTS_DIR=$DIR/../../dbscripts
-service postgresql status>/dev/null 2>&1
+#This scripts calculate the value of the database schema checksum by performing an MD5 on all the MD5 values of
+#The installed scripts, as specified in the schema_version table
+die () {
+    printf >&2 "$@"
+    exit 1
+}
 
-source $DBSCRIPTS_DIR/dbfunctions.sh
-source $DBSCRIPTS_DIR/dbcustomfunctions.sh
+RETVAL=-1
+DBSCRIPTS_DIR=`dirname $(readlink -f "$0")`/..
+service postgresql status>/dev/null 2>&1  || die "postgresql is not running"
+
+source $DBSCRIPTS_DIR/dbfunctions.sh  || die "dbfunctions.sh script does not exist"
+source $DBSCRIPTS_DIR/dbcustomfunctions.sh || die "dbcustomfunctions.sh script does not exist"
+
 set_defaults
 while getopts d:u: option; do
-    case $option in
-	d) DATABASE=$OPTARG;;
-	u) USERNAME=$OPTARG;;
-    esac
+  case $option in
+    d) DATABASE=$OPTARG;;
+    u) USERNAME=$OPTARG;;
+  esac
 done
-if [ $?  -eq 0 ]; then
-  LOGFILE=target/calc_db_schema_checksum.log
-  CMD="select trim(checksum) from schema_version where state='INSTALLED' order by script"
-  execute_command "$CMD" ${DATABASE}> /tmp/md5_values_db
-  #following two lines make sure the format of the result from the db does not contain a space before each checksum, and a newline
-  #in order for it to match to the file that is obtained from the folder scanning. This is required as the selection returns for each row a space before
-  #the values and generates a new line
-  cat /tmp/md5_values_db | cut -d " " -f2 >/tmp/md5_values_db_2
-  sed -i '$d' /tmp/md5_values_db_2
-  RETVAL=`cat /tmp/md5_values_db_2 | md5sum | cut -d " " -f1`
-  rm -rf /tmp/md5_values_db*
-else
-  RETVAL=-1
-fi
+
+LOGFILE=/tmp/calc_db_schema_checksum.log
+CMD="select trim(checksum) from schema_version where state='INSTALLED' order by script"
+MD5_FILE=`mktemp`
+execute_command "$CMD" ${DATABASE}>"$MD5_FILE" || die "execution of SQL to get checksum from DB has failed"
+
+#following two lines make sure the format of the result from the db does not contain a space 
+#before each checksum, and a newline in order for it to match to the file that is obtained 
+#from the folder scanning. This is required as the selection returns for each row a space before
+#the values and generates a new line
+RETVAL=`sed '$d' "$MD5_FILE" | sed 's/ //g' | md5sum | sed 's/ .*//g'`
 echo $RETVAL
+exit 0
 
 
