99ecd475d2c8fbe8074be804f989a62b7a4f2d77
Ofer Schreiber <oschreib@redhat.com>
2012-01-08 12:00:02 +0200

    core: Fix SignLockFile config option
    Remove extra space preventing from
    03_01_0081_update_SignLockFile_config.sql to work.
    
    Change-Id: I30a7441fb77dbe62dc2030f9364ddb352c964920

--- backend/manager/dbscripts/upgrade/03_01_0081_update_SignLockFile_config.sql
+++ backend/manager/dbscripts/upgrade/03_01_0081_update_SignLockFile_config.sql
@@ -1,2 +1,2 @@
 -- update SignLockFile value to /var/lock/ovirt-engine/.openssl.exclusivelock
-select fn_db_update_config_value('SignLockFile ','/var/lock/ovirt-engine/.openssl.exclusivelock','general');
+select fn_db_update_config_value('SignLockFile','/var/lock/ovirt-engine/.openssl.exclusivelock','general');
