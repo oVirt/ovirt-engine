c6089eed3a1c9fc995dc4b9b5e975da6cb4edeec
Maor Lipchuk <mlipchuk@redhat.com>
2012-01-12 03:45:37 +0200

    ovirt-engine: Add quota DB tables
    The next patch adds quota tables.
    
    The quota tables which being added are the following:
    quota- Indicates the general meta data properties of the quota.
    quota_limitation- The limitation of each quota type (general and
    specifc).
    
    More specific details can be found in the following
    design:
    http://www.ovirt.org/wiki/Features/Design/Quota
    
    Quota feature description can be found here:
    http://www.ovirt.org/wiki/Features/Quota
    
    Acked By :Omer Frenkel  <ofrenkel@redhat.com>
    
    Change-Id: I6434e176a33b00be369c08586564d1ff048de5bc

---
+++ backend/manager/dbscripts/upgrade/03_01_0170_add_quota_tables.sql
@@ -0,0 +1,63 @@
+Create or replace FUNCTION fn_db_add_quota_tables_03_01_0170() returns void
+AS $procedure$
+begin
+-- Add quota table which reflects the meta data of the quota table.
+if (not exists (select 1 from INFORMATION_SCHEMA.TABLES where table_name='quota')) then
+  begin
+  CREATE TABLE quota
+  (
+     id UUID NOT NULL,
+     storage_pool_id UUID NOT NULL references storage_pool(id) on delete cascade,
+     quota_name VARCHAR(60) NOT NULL,
+     description VARCHAR(250),
+     _create_date TIMESTAMP WITH TIME ZONE default LOCALTIMESTAMP,
+     _update_date TIMESTAMP WITH TIME ZONE,
+     threshold_vds_group_percentage INTEGER default 80,
+     threshold_storage_percentage INTEGER default 80,
+     grace_vds_group_percentage INTEGER default 20,
+     grace_storage_percentage INTEGER default 20,
+     CONSTRAINT pk_quota PRIMARY KEY(id)
+  ) WITH OIDS;
+ end;
+ end if;
+
+-- Add quota_limitation table which reflects the limitations of the quota.
+if (not exists (select 1 from INFORMATION_SCHEMA.TABLES where table_name='quota_limitation')) then
+  begin
+    CREATE TABLE quota_limitation
+    (
+        id UUID NOT NULL,
+        quota_id UUID NOT NULL references quota(id) on delete cascade,
+        storage_id UUID NULL references storage_domain_static(id) on delete cascade,
+        vds_group_id UUID NULL references vds_groups(vds_group_id) on delete cascade,
+        virtual_cpu INTEGER,
+        mem_size_mb BIGINT,
+        storage_size_gb BIGINT,
+        CONSTRAINT pk_quota_limitation PRIMARY KEY(id)
+    ) WITH OIDS;
+  end;
+ end if;
+
+ DROP INDEX if exists IDX_quota_limitation_quota_id;
+ CREATE INDEX IDX_quota_limitation_quota_id ON quota_limitation(quota_id);
+
+-- Create partial index for not null values for fetching storage domains related to quota.
+ DROP INDEX if exists IDX_quota_limitation_storage_id;
+ CREATE INDEX IDX_quota_limitation_storage_id ON quota_limitation(storage_id)
+ WHERE storage_id IS NOT null;
+
+-- Create partial index for not null values for fetching vdsgroup related to quota.
+ DROP INDEX if exists IDX_quota_limitation_vds_group_id;
+ CREATE INDEX IDX_quota_limitation_vds_group_id ON quota_limitation(vds_group_id)
+ WHERE vds_group_id IS NOT null;
+
+-- Create partial index for not null values for fetching storagepool related to quota.
+ DROP INDEX if exists IDX_storage_pool_id;
+ CREATE INDEX IDX_storage_pool_id ON quota(storage_pool_id)
+ WHERE storage_pool_id IS NOT null;
+
+ END; $procedure$
+ LANGUAGE plpgsql;
+
+select fn_db_add_quota_tables_03_01_0170();
+drop function fn_db_add_quota_tables_03_01_0170();
