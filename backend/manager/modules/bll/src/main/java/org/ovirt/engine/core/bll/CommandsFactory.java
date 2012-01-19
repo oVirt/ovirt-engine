6b0c69d3d7ed7491a78238f4085c205326eefdaf
Michael Kublin <mkublin@redhat.com>
2012-01-19 12:33:49 +0200

    engine : Code clean up
    The following commit is performing a simple code clean up inside CommandsFactory
    
    Change-Id: I026764ba88e0651126829ec5a46de01d4786a50c

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandsFactory.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandsFactory.java
@@ -3,8 +3,8 @@ package org.ovirt.engine.core.bll;
 import java.lang.reflect.Constructor;
 import java.text.MessageFormat;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import org.ovirt.engine.core.common.action.VdcActionParametersBase;
 import org.ovirt.engine.core.common.action.VdcActionType;
@@ -13,13 +13,12 @@ import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.LogCompat;
 import org.ovirt.engine.core.compat.LogFactoryCompat;
-import org.ovirt.engine.core.compat.RefObject;
 import org.ovirt.engine.core.utils.ReflectionUtils;
 
 public final class CommandsFactory {
+    private static final String CLASS_NAME_FORMAT = "%1$s.%2$s%3$s";
     private static final String CommandSuffix = "Command";
     private static final String QueryPrefix = "Query";
-    private static final String CanDoCommandPrefix = "Can";
     private static final String CTOR_MISMATCH =
             "could not find matching constructor for Command class {0}";
     private static final String CTOR_NOT_FOUND_FOR_PARAMETERS =
@@ -28,53 +27,8 @@ public final class CommandsFactory {
     private static final String[] COMMAND_PACKAGES = new String[] { "org.ovirt.engine.core.bll",
             "org.ovirt.engine.core.bll.storage" };
 
-    private static Map<String, Class<CommandBase<? extends VdcActionParametersBase>>> commandsCache =
+    private static ConcurrentMap<String, Class<CommandBase<? extends VdcActionParametersBase>>> commandsCache =
             new ConcurrentHashMap<String, Class<CommandBase<? extends VdcActionParametersBase>>>(VdcActionType.values().length);
-
-    public static boolean canDoActionWithParameters(VdcActionType action,
-                                                    Object id,
-                                                    RefObject<java.util.ArrayList<String>> reasons,
-                                                    Object... additionalParameters) {
-        boolean returnValue = true;
-        reasons.argvalue = null;
-        try {
-
-            Class<?> actionType = getCommandClass(action.name(), CommandSuffix);
-
-            /**
-             * if action type not exist - operation valid
-             */
-            if (actionType != null) {
-                String canDoActionName = String.format("%1$s%2$s", CanDoCommandPrefix, action);
-                reasons.argvalue = new java.util.ArrayList<String>();
-                Object[] args;
-                if (additionalParameters != null) {
-                    args = new Object[2 + additionalParameters.length];
-                    for (int i = 0; i < additionalParameters.length; i++) {
-                        args[2 + i] = additionalParameters[i];
-                    }
-                } else {
-                    args = new Object[2];
-                }
-                args[0] = id;
-                args[1] = reasons.argvalue;
-                /**
-                 * Each command must implement static public function in order
-                 * to possibility to check operation validity
-                 */
-                java.lang.reflect.Method method = actionType.getMethod(canDoActionName);
-                /**
-                 * By default all operations valid
-                 */
-                if (method != null) {
-                    returnValue = (Boolean) method.invoke(null, args);
-                }
-            }
-        } catch (Exception e) {
-            log.error("Failed to check Action ", e);
-        }
-        return returnValue;
-    }
 
     @SuppressWarnings("unchecked")
     public static <P extends VdcActionParametersBase> CommandBase<P> CreateCommand(VdcActionType action, P parameters) {
@@ -85,7 +39,7 @@ public final class CommandsFactory {
             return (CommandBase<P>) constructor.newInstance(new Object[] { parameters });
         }
 
-        catch (java.lang.Exception e) {
+        catch (Exception e) {
             log.error(
                     "CommandsFactory [parameter: VdcActionParametersBase]: Failed to get type information using " +
                             "reflection for Action: " + action, e);
@@ -102,7 +56,6 @@ public final class CommandsFactory {
      *            the command id used by the compensation.
      * @return command instance or null if exception occurred.
      */
-    @SuppressWarnings("unchecked")
     public static CommandBase<?> CreateCommand(String className, Guid commandId) {
         Constructor<?> constructor = null;
         Boolean isAcessible = null;
@@ -114,7 +67,7 @@ public final class CommandsFactory {
                 constructor.setAccessible(true);
             }
             return (CommandBase<?>) constructor.newInstance(new Object[] { commandId });
-        } catch (java.lang.Exception e) {
+        } catch (Exception e) {
             log.error(
                     "CommandsFactory : Failed to get type information using " +
                             "reflection for Class : " + className + ", Command Id:" + commandId, e);
@@ -126,18 +79,12 @@ public final class CommandsFactory {
         }
     }
 
-    public static QueriesCommandBase CreateQueryCommand(VdcQueryType query, VdcQueryParametersBase parameters) {
-        java.lang.Class<?> type = null;
+    public static QueriesCommandBase<?> CreateQueryCommand(VdcQueryType query, VdcQueryParametersBase parameters) {
+        Class<?> type = null;
         try {
             type = getCommandClass(query.name(), QueryPrefix);
-            java.lang.Class<?>[] types = new java.lang.Class[1];
-            types[0] = parameters.getClass();
-
-            java.lang.reflect.Constructor<?> info = findCommandConstructor(type, types);
-            Object[] vdcParameters = new Object[1];
-            vdcParameters[0] = parameters;
-            Object tempVar = info.newInstance(vdcParameters);
-            return (QueriesCommandBase) ((tempVar instanceof QueriesCommandBase) ? tempVar : null);
+            Constructor<?> info = findCommandConstructor(type, parameters.getClass());
+            return (QueriesCommandBase<?>) info.newInstance(parameters);
         } catch (Exception e) {
             log.errorFormat("Command Factory: Failed to create command {0} using reflection\n. {1}", type, e);
             throw new RuntimeException(e);
@@ -146,20 +93,22 @@ public final class CommandsFactory {
 
     private static Class<CommandBase<? extends VdcActionParametersBase>> getCommandClass(String name, String suffix) {
         // try the cache first
-        if (commandsCache.get(name + suffix) != null)
-            return commandsCache.get(name + suffix);
-
+        String key = name + suffix;
+        Class<CommandBase<? extends VdcActionParametersBase>> clazz = commandsCache.get(key);
+        if (clazz != null) {
+            return clazz;
+        }
         for (String commandPackage : COMMAND_PACKAGES) {
-            String className = String.format("%1$s.%2$s%3$s", commandPackage, name, suffix);
+            String className = String.format(CLASS_NAME_FORMAT, commandPackage, name, suffix);
             Class<CommandBase<?>> type = loadClass(className);
             if (type != null) {
-                commandsCache.put(name + suffix, type); // update cache
+                commandsCache.putIfAbsent(key, type); // update cache
                 return type;
             }
         }
 
         // nothing found
-        log.warn("Unable to find class for action: " + name + suffix);
+        log.warn("Unable to find class for action: " + key);
         return null;
     }
 
