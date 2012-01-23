a1fd86626e34343b6c2418035795afa1a89a65b3
Mike Kolesnik <mkolesni@redhat.com>
2012-01-23 10:51:11 +0200

    core: Code cleanup CreateAllSnapshotsFromVmParameters
    1. Removed unnecessary JAXB annotations.
    2. Moved default ctor to top of class.
    
    Change-Id: I60bd36b5405c7122aa18fc676c267ce6c0939e50

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/CreateAllSnapshotsFromVmParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/CreateAllSnapshotsFromVmParameters.java
@@ -2,27 +2,22 @@ package org.ovirt.engine.core.common.action;
 
 import java.util.ArrayList;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
-
 import org.hibernate.validator.constraints.NotEmpty;
-
 import org.ovirt.engine.core.common.validation.group.CreateEntity;
 import org.ovirt.engine.core.compat.Guid;
 
-@XmlAccessorType(XmlAccessType.NONE)
-@XmlType(name = "CreateAllSnapshotsFromVmParameters")
 public class CreateAllSnapshotsFromVmParameters extends VmOperationParameterBase implements java.io.Serializable {
 
     private static final long serialVersionUID = 3456740034406494984L;
-    @XmlElement
+
     @NotEmpty(groups = { CreateEntity.class },
             message = "VALIDATION.DISK_IMAGE.DESCRIPTION.NOT_EMPTY")
     private String _description;
-    @XmlElement(name = "DisksListJport")
+
     public java.util.ArrayList<String> _disksList = new java.util.ArrayList<String>();
+
+    public CreateAllSnapshotsFromVmParameters() {
+    }
 
     public CreateAllSnapshotsFromVmParameters(Guid vmId, String description) {
         super(vmId);
@@ -39,8 +34,5 @@ public class CreateAllSnapshotsFromVmParameters extends VmOperationParameterBase
 
     public void setDisksList(java.util.ArrayList<String> value) {
         _disksList = value;
-    }
-
-    public CreateAllSnapshotsFromVmParameters() {
     }
 }
