a64f38a9cf432504af58afaa68e3f535001633cd
Moti Asayag <masayag@redhat.com>
2012-02-01 10:40:47 +0200

    core: Code cleanup for event_map
    Removed unused code and jaxb annotations from event_map and moved
    private members prior to constructor.
    
    Change-Id: I1bb118bdc1f792cec2af7fe54245003fff5af6d4

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/event_map.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/event_map.java
@@ -9,18 +9,7 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
 
-import org.ovirt.engine.core.common.AuditLogType;
-import org.ovirt.engine.core.common.utils.EnumUtils;
-import org.ovirt.engine.core.compat.LogCompat;
-import org.ovirt.engine.core.compat.LogFactoryCompat;
-
-@XmlAccessorType(XmlAccessType.NONE)
-@XmlType(name = "event_map")
 @Entity
 @Table(name = "event_map")
 public class event_map implements Serializable {
@@ -32,25 +21,15 @@ public class event_map implements Serializable {
             @AttributeOverride(name = "eventDownName", column = @Column(name = "event_down_name")) })
     private event_map_id id = new event_map_id();
 
-    public event_map() {
-    }
-
-    public event_map(String event_down_name, String event_up_name) {
-        this.id.eventDownName = event_down_name;
-        this.id.eventUpName = event_up_name;
-        try {
-            this.eventUpId = EnumUtils.valueOf(AuditLogType.class, event_down_name, true).getValue();
-            eventUpId = EnumUtils.valueOf(AuditLogType.class, event_up_name, true).getValue();
-        } catch (RuntimeException e) {
-            log.errorFormat("Could not find events {0},{1}\n{2}", event_down_name, event_up_name, e);
-        }
-
-    }
-
     @Transient
     private int eventDownId;
 
-    @XmlElement
+    @Transient
+    private int eventUpId;
+
+    public event_map() {
+    }
+
     public int getevent_down_id() {
         return this.eventDownId;
     }
@@ -59,10 +38,6 @@ public class event_map implements Serializable {
         this.eventDownId = value;
     }
 
-    @Transient
-    private int eventUpId;
-
-    @XmlElement
     public int getevent_up_id() {
         return this.eventUpId;
     }
@@ -71,7 +46,6 @@ public class event_map implements Serializable {
         this.eventUpId = value;
     }
 
-    @XmlElement
     public String getevent_up_name() {
         return this.id.eventUpName;
     }
@@ -80,7 +54,6 @@ public class event_map implements Serializable {
         this.id.eventUpName = value;
     }
 
-    @XmlElement
     public String getevent_down_name() {
         return this.id.eventDownName;
     }
@@ -88,7 +61,5 @@ public class event_map implements Serializable {
     public void setevent_down_name(String value) {
         this.id.eventDownName = value;
     }
-
-    private static LogCompat log = LogFactoryCompat.getLog(event_map.class);
 
 }
