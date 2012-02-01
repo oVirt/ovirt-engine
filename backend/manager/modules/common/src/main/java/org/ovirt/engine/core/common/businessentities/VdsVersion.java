b9a3ee76f92906a9c3fdd5b5fa327d530f7a41b2
Moti Asayag <masayag@redhat.com>
2012-02-01 10:55:16 +0200

    core: Code cleanup for VdsVersion
    Removed jaxb annotations, organized the class order and fixed some
    typos.
    
    Change-Id: I459659c2d53797577a59e9a9f98c748d465f848c

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VdsVersion.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VdsVersion.java
@@ -1,24 +1,24 @@
 package org.ovirt.engine.core.common.businessentities;
 
-import org.ovirt.engine.core.compat.*;
-
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 
-@XmlAccessorType(XmlAccessType.NONE)
-@XmlType(name = "VdsVersion")
+import org.ovirt.engine.core.compat.LogCompat;
+import org.ovirt.engine.core.compat.LogFactoryCompat;
+import org.ovirt.engine.core.compat.StringFormat;
+import org.ovirt.engine.core.compat.Version;
+
 public class VdsVersion implements Serializable {
     private static final long serialVersionUID = -3138828435468456070L;
+    private static final LogCompat log = LogFactoryCompat.getLog(VdsVersion.class);
     private String softwareVersion;
     private String softwareRevision;
     private String buildName;
     private String versionName;
     private Version mFullVersion;
 
-    @XmlElement(name = "SoftwareVersion")
+    public VdsVersion() {
+    }
+
     public String getSoftwareVersion() {
         return softwareVersion;
     }
@@ -27,7 +27,6 @@ public class VdsVersion implements Serializable {
         softwareVersion = value;
     }
 
-    @XmlElement(name = "SoftwareRevision")
     public String getSoftwareRevision() {
         return softwareRevision;
     }
@@ -36,7 +35,6 @@ public class VdsVersion implements Serializable {
         softwareRevision = value;
     }
 
-    @XmlElement(name = "BuildName")
     public String getBuildName() {
         return buildName;
     }
@@ -45,7 +43,6 @@ public class VdsVersion implements Serializable {
         buildName = value;
     }
 
-    @XmlElement(name = "VersionName")
     public String getVersionName() {
         return versionName;
     }
@@ -55,13 +52,20 @@ public class VdsVersion implements Serializable {
     }
 
     /**
-     * Version in .Net style: a.b.c.d when a: major version, b: minor version , c: major revision, d: minor revision
-     * assumption: Vds version in format: x.y where x = major version, y= minor version. there is no major revision
-     * recieved from vds - always 0
+     * Version in .Net style a.b.c.d when:
+     * <ul>
+     * <li>a: major version</li>
+     * <li>b: minor version</li>
+     * <li>c: major revision</li>
+     * <li>d: minor revision</li>
+     * <ul>
+     * <p>
+     * Assumption: VDS version in format: x.y where x = major version, y= minor version. There is no major revision
+     * received from VDS - always 0
      */
     public Version getFullVersion() {
         if (mFullVersion == null) {
-            // defencive code for prevent incorrect versioning
+            // defensive code for prevent incorrect versioning
             try {
                 if (getSoftwareVersion() != null && getSoftwareRevision() != null) {
                     String stringVersion;
@@ -75,8 +79,10 @@ public class VdsVersion implements Serializable {
 
                     mFullVersion = new Version(stringVersion);
                 }
-            } catch (java.lang.Exception e) {
-                log.warnFormat("Couldn't parse vds version: {0} , {1}", getSoftwareVersion(), getSoftwareRevision());
+            } catch (Exception e) {
+                log.info(StringFormat.format("Couldn't parse vds version: %s , %s",
+                        getSoftwareVersion(),
+                        getSoftwareRevision()));
             }
         }
 
@@ -106,8 +112,4 @@ public class VdsVersion implements Serializable {
         return getFullVersion() != null ? getFullVersion().hashCode() : -1;
     }
 
-    private static LogCompat log = LogFactoryCompat.getLog(VdsVersion.class);
-
-    public VdsVersion() {
-    }
 }
