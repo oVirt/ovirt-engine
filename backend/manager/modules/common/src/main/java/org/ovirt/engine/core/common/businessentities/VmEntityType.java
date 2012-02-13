f91eaf92e8e8770d662e8b78b00abfbd404809b6
Yair Zaslavsky <yzaslavs@redhat.com>
2012-02-13 19:58:29 +0200

    common: introducing VmEntityType
    This enum will help destinguish between entities if they
    are associated with VM or Template (for example - DiskImage)
    
    Change-Id: I6cecf3c99b21d0978135b83d7e5322b5dfca36e4
    Acked-By:

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmEntityType.java
@@ -0,0 +1,10 @@
+package org.ovirt.engine.core.common.businessentities;
+
+
+/**
+ * This enum is used for determining the entity type of the VM
+ */
+public enum VmEntityType {
+    VM,
+    TEMPLATE;
+}
