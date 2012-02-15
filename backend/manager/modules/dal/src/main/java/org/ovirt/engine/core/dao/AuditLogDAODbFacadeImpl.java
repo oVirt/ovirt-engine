fadc8910b7673feed4d6479dc9763e3ea7e88923
Moti Asayag <masayag@redhat.com>
2012-02-15 02:39:38 +0200

    core: Refactor AuditLogDAODbFacadeImpl
    The patch refactors AuditLogDAODbFacadeImpl by extracting repeated
    mapper creation with a single instance.
    
    It also refactors the audit-log to sql mapper into a method.
    
    Change-Id: Ib9e301b21d50ed8555b1b2841ac90f7d502fee89

--- backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/AuditLogDAODbFacadeImpl.java
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/AuditLogDAODbFacadeImpl.java
@@ -6,11 +6,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
-
 import org.ovirt.engine.core.common.AuditLogSeverity;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.businessentities.AuditLog;
@@ -20,230 +15,60 @@ import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.NGuid;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 import org.ovirt.engine.core.dal.dbbroker.DbFacadeUtils;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
 /**
- * ,code>AuditLogDAODbFacadeImpl</code> provides a concrete implementation of {@link AuditLogDAO}. It uses code
+ * <code>AuditLogDAODbFacadeImpl</code> provides a concrete implementation of {@link AuditLogDAO}. It uses code
  * refactored from {@link DbFacade}.
- *
- *
  */
 public class AuditLogDAODbFacadeImpl extends BaseDAODbFacade implements AuditLogDAO {
+
+    private static AuditLogRowMapper auditLogRowMapper = new AuditLogRowMapper();
 
     @Override
     public AuditLog get(long id) {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
                 .addValue("audit_log_id", id);
 
-        ParameterizedRowMapper<AuditLog> mapper = new ParameterizedRowMapper<AuditLog>() {
-            @Override
-            public AuditLog mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                AuditLog entity = new AuditLog();
-                entity.setaudit_log_id(rs.getLong("audit_log_id"));
-                entity.setlog_time(DbFacadeUtils.fromDate(rs
-                        .getTimestamp("log_time")));
-                entity.setlog_type(AuditLogType.forValue(rs.getInt("log_type")));
-                entity.setseverity(AuditLogSeverity.forValue(rs
-                        .getInt("severity")));
-                entity.setmessage(rs.getString("message"));
-                entity.setuser_id(NGuid.createGuidFromString(rs
-                        .getString("user_id")));
-                entity.setuser_name(rs.getString("user_name"));
-                entity.setvds_id(NGuid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setvm_id(NGuid.createGuidFromString(rs
-                        .getString("vm_id")));
-                entity.setvm_name(rs.getString("vm_name"));
-                entity.setvm_template_id(NGuid.createGuidFromString(rs
-                        .getString("vm_template_id")));
-                entity.setvm_template_name(rs.getString("vm_template_name"));
-                entity.setstorage_pool_id(NGuid.createGuidFromString(rs
-                        .getString("storage_pool_id")));
-                entity.setstorage_pool_name(rs.getString("storage_pool_name"));
-                entity.setstorage_domain_id(NGuid.createGuidFromString(rs
-                        .getString("storage_domain_id")));
-                entity.setstorage_domain_name(rs
-                        .getString("storage_domain_name"));
-                entity.setvds_group_id(NGuid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setvds_group_name(rs
-                        .getString("vds_group_name"));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeRead("GetAuditLogByAuditLogId", mapper, parameterSource);
+        return getCallsHandler().executeRead("GetAuditLogByAuditLogId", auditLogRowMapper, parameterSource);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public List<AuditLog> getAllAfterDate(Date cutoff) {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
                 .addValue("date", cutoff);
 
-        ParameterizedRowMapper<AuditLog> mapper = new ParameterizedRowMapper<AuditLog>() {
-            @Override
-            public AuditLog mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                AuditLog entity = new AuditLog();
-                entity.setaudit_log_id(rs.getLong("audit_log_id"));
-                entity.setlog_time(DbFacadeUtils.fromDate(rs
-                        .getTimestamp("log_time")));
-                entity.setlog_type(AuditLogType.forValue(rs.getInt("log_type")));
-                entity.setseverity(AuditLogSeverity.forValue(rs
-                        .getInt("severity")));
-                entity.setmessage(rs.getString("message"));
-                entity.setuser_id(NGuid.createGuidFromString(rs
-                        .getString("user_id")));
-                entity.setuser_name(rs.getString("user_name"));
-                entity.setvds_id(NGuid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setvm_id(NGuid.createGuidFromString(rs
-                        .getString("vm_id")));
-                entity.setvm_name(rs.getString("vm_name"));
-                entity.setvm_template_id(NGuid.createGuidFromString(rs
-                        .getString("vm_template_id")));
-                entity.setvm_template_name(rs.getString("vm_template_name"));
-                entity.setstorage_pool_id(NGuid.createGuidFromString(rs
-                        .getString("storage_pool_id")));
-                entity.setstorage_pool_name(rs.getString("storage_pool_name"));
-                entity.setstorage_domain_id(NGuid.createGuidFromString(rs
-                        .getString("storage_domain_id")));
-                entity.setstorage_domain_name(rs
-                        .getString("storage_domain_name"));
-                entity.setvds_group_id(NGuid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setvds_group_name(rs
-                        .getString("vds_group_name"));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("GetAuditLogLaterThenDate", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetAuditLogLaterThenDate", auditLogRowMapper, parameterSource);
 
     }
 
     @Override
     public List<AuditLog> getAllWithQuery(String query) {
-        ParameterizedRowMapper<AuditLog> mapper = new ParameterizedRowMapper<AuditLog>() {
-            @Override
-            public AuditLog mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                AuditLog entity = new AuditLog();
-                entity.setaudit_log_id(rs.getLong("audit_log_id"));
-                entity.setlog_time(DbFacadeUtils.fromDate(rs
-                        .getTimestamp("log_time")));
-                entity.setlog_type(AuditLogType.forValue(rs.getInt("log_type")));
-                entity.setseverity(AuditLogSeverity.forValue(rs
-                        .getInt("severity")));
-                entity.setmessage(rs.getString("message"));
-                entity.setuser_id(Guid.createGuidFromString(rs
-                        .getString("user_id")));
-                entity.setuser_name(rs.getString("user_name"));
-                entity.setvds_id(NGuid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setvm_id(Guid.createGuidFromString(rs.getString("vm_id")));
-                entity.setvm_name(rs.getString("vm_name"));
-                entity.setvm_template_id(Guid.createGuidFromString(rs
-                        .getString("vm_template_id")));
-                entity.setvm_template_name(rs.getString("vm_template_name"));
-                entity.setstorage_pool_id(Guid.createGuidFromString(rs
-                        .getString("storage_pool_id")));
-                entity.setstorage_pool_name(rs.getString("storage_pool_name"));
-                entity.setstorage_domain_id(Guid.createGuidFromString(rs
-                        .getString("storage_domain_id")));
-                entity.setstorage_domain_name(rs
-                        .getString("storage_domain_name"));
-                entity.setvds_group_id(NGuid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setvds_group_name(rs
-                        .getString("vds_group_name"));
-                return entity;
-            }
-        };
-
-        return new SimpleJdbcTemplate(jdbcTemplate).query(query, mapper);
+        return new SimpleJdbcTemplate(jdbcTemplate).query(query, auditLogRowMapper);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public List<AuditLog> getAll() {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource();
-
-        ParameterizedRowMapper<AuditLog> mapper = new ParameterizedRowMapper<AuditLog>() {
-            @Override
-            public AuditLog mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                AuditLog entity = new AuditLog();
-                entity.setaudit_log_id(rs.getLong("audit_log_id"));
-                entity.setlog_time(DbFacadeUtils.fromDate(rs
-                        .getTimestamp("log_time")));
-                entity.setlog_type(AuditLogType.forValue(rs.getInt("log_type")));
-                entity.setseverity(AuditLogSeverity.forValue(rs
-                        .getInt("severity")));
-                entity.setmessage(rs.getString("message"));
-                entity.setuser_id(NGuid.createGuidFromString(rs
-                        .getString("user_id")));
-                entity.setuser_name(rs.getString("user_name"));
-                entity.setvds_id(NGuid.createGuidFromString(rs
-                        .getString("vds_id")));
-                entity.setvds_name(rs.getString("vds_name"));
-                entity.setvm_id(NGuid.createGuidFromString(rs
-                        .getString("vm_id")));
-                entity.setvm_name(rs.getString("vm_name"));
-                entity.setvm_template_id(NGuid.createGuidFromString(rs
-                        .getString("vm_template_id")));
-                entity.setvm_template_name(rs.getString("vm_template_name"));
-                entity.setstorage_pool_id(NGuid.createGuidFromString(rs
-                        .getString("storage_pool_id")));
-                entity.setstorage_pool_name(rs.getString("storage_pool_name"));
-                entity.setstorage_domain_id(NGuid.createGuidFromString(rs
-                        .getString("storage_domain_id")));
-                entity.setstorage_domain_name(rs
-                        .getString("storage_domain_name"));
-                entity.setvds_group_id(NGuid.createGuidFromString(rs
-                        .getString("vds_group_id")));
-                entity.setvds_group_name(rs
-                        .getString("vds_group_name"));
-                return entity;
-            }
-        };
-        return getCallsHandler().executeReadList("GetAllFromAuditLog", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetAllFromAuditLog", auditLogRowMapper, parameterSource);
     }
 
     @Override
     public void save(AuditLog event) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("audit_log_id", event.getaudit_log_id())
-                .addValue("log_time", event.getlog_time())
-                .addValue("log_type", event.getlog_type())
-                .addValue("log_type_name", event.getlog_type_name())
-                .addValue("severity", event.getseverity())
-                .addValue("message", event.getmessage())
-                .addValue("user_id", event.getuser_id())
-                .addValue("user_name", event.getuser_name())
-                .addValue("vds_id", event.getvds_id())
-                .addValue("vds_name", event.getvds_name())
-                .addValue("vm_id", event.getvm_id())
-                .addValue("vm_name", event.getvm_name())
-                .addValue("vm_template_id", event.getvm_template_id())
-                .addValue("vm_template_name", event.getvm_template_name())
-                .addValue("storage_pool_id", event.getstorage_pool_id())
-                .addValue("storage_pool_name", event.getstorage_pool_name())
-                .addValue("storage_domain_id", event.getstorage_domain_id())
-                .addValue("storage_domain_name", event.getstorage_domain_name())
-                .addValue("vds_group_id", event.getvds_group_id())
-                .addValue("vds_group_name", event.getvds_group_name());
-
-        getCallsHandler().executeModification("InsertAuditLog", parameterSource);
+        getCallsHandler().executeModification("InsertAuditLog",
+                getSqlMapper(event).addValue("log_type_name", event.getlog_type_name()));
     }
 
     @Override
     public void update(AuditLog event) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
+        getCallsHandler().executeModification("UpdateAuditLog", getSqlMapper(event));
+    }
+
+    private MapSqlParameterSource getSqlMapper(AuditLog event) {
+        return getCustomMapSqlParameterSource()
                 .addValue("audit_log_id", event.getaudit_log_id())
                 .addValue("log_time", event.getlog_time())
                 .addValue("log_type", event.getlog_type())
@@ -263,8 +88,6 @@ public class AuditLogDAODbFacadeImpl extends BaseDAODbFacade implements AuditLog
                 .addValue("storage_domain_name", event.getstorage_domain_name())
                 .addValue("vds_group_id", event.getvds_group_id())
                 .addValue("vds_group_name", event.getvds_group_name());
-
-        getCallsHandler().executeModification("UpdateAuditLog", parameterSource);
     }
 
     @Override
@@ -313,5 +136,45 @@ public class AuditLogDAODbFacadeImpl extends BaseDAODbFacade implements AuditLog
 
         String resultKey = DbFacade.getInstance().getDbEngineDialect().getFunctionReturnKey();
         return dbResults.get(resultKey) != null ? ((Integer) dbResults.get(resultKey)).intValue() : 0;
+    }
+
+    private static class AuditLogRowMapper implements ParameterizedRowMapper<AuditLog> {
+
+        @Override
+        public AuditLog mapRow(ResultSet rs, int rowNum)
+                throws SQLException {
+            AuditLog entity = new AuditLog();
+            entity.setaudit_log_id(rs.getLong("audit_log_id"));
+            entity.setlog_time(DbFacadeUtils.fromDate(rs
+                    .getTimestamp("log_time")));
+            entity.setlog_type(AuditLogType.forValue(rs.getInt("log_type")));
+            entity.setseverity(AuditLogSeverity.forValue(rs
+                    .getInt("severity")));
+            entity.setmessage(rs.getString("message"));
+            entity.setuser_id(NGuid.createGuidFromString(rs
+                    .getString("user_id")));
+            entity.setuser_name(rs.getString("user_name"));
+            entity.setvds_id(NGuid.createGuidFromString(rs
+                    .getString("vds_id")));
+            entity.setvds_name(rs.getString("vds_name"));
+            entity.setvm_id(NGuid.createGuidFromString(rs
+                    .getString("vm_id")));
+            entity.setvm_name(rs.getString("vm_name"));
+            entity.setvm_template_id(NGuid.createGuidFromString(rs
+                    .getString("vm_template_id")));
+            entity.setvm_template_name(rs.getString("vm_template_name"));
+            entity.setstorage_pool_id(NGuid.createGuidFromString(rs
+                    .getString("storage_pool_id")));
+            entity.setstorage_pool_name(rs.getString("storage_pool_name"));
+            entity.setstorage_domain_id(NGuid.createGuidFromString(rs
+                    .getString("storage_domain_id")));
+            entity.setstorage_domain_name(rs
+                    .getString("storage_domain_name"));
+            entity.setvds_group_id(NGuid.createGuidFromString(rs
+                    .getString("vds_group_id")));
+            entity.setvds_group_name(rs
+                    .getString("vds_group_name"));
+            return entity;
+        }
     }
 }
