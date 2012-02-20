2b9c70c6d4d375ab09d55d41eeb7c5a2065f647b
Roy Golan <rgolan@redhat.com>
2012-02-20 12:43:48 +0200

    core: minor clean-up for InstallVdsParameters.java
    organise fields, constructors and then methods
    
    Change-Id: I2bdf3816a47fb66cde30db869d56cb8638a83635

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/InstallVdsParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/InstallVdsParameters.java
@@ -3,53 +3,51 @@ package org.ovirt.engine.core.common.action;
 import org.ovirt.engine.core.compat.Guid;
 
 public class InstallVdsParameters extends VdsActionParameters {
+
     private static final long serialVersionUID = 5066290843683399113L;
+
+    private String privateRootPassword;
+    private boolean privateIsReinstallOrUpgrade;
+    private String privateoVirtIsoFile;
+    private boolean overrideFirewall;
+
+    public InstallVdsParameters() {
+    }
 
     public InstallVdsParameters(Guid vdsId, String password) {
         super(vdsId);
         setRootPassword(password);
     }
 
-    private String privateRootPassword;
-
-    public String getRootPassword() {
-        return privateRootPassword;
-    }
-
-    private void setRootPassword(String value) {
-        privateRootPassword = value;
-    }
-
-    private boolean privateIsReinstallOrUpgrade;
-
     public boolean getIsReinstallOrUpgrade() {
         return privateIsReinstallOrUpgrade;
-    }
-
-    public void setIsReinstallOrUpgrade(boolean value) {
-        privateIsReinstallOrUpgrade = value;
-    }
-
-    private String privateoVirtIsoFile;
-
-    public String getoVirtIsoFile() {
-        return privateoVirtIsoFile;
-    }
-
-    public void setoVirtIsoFile(String value) {
-        privateoVirtIsoFile = value;
-    }
-
-    private boolean overrideFirewall;
-
-    public void setOverrideFirewall(boolean overrideFirewall) {
-        this.overrideFirewall = overrideFirewall;
     }
 
     public boolean getOverrideFirewall() {
         return overrideFirewall;
     }
 
-    public InstallVdsParameters() {
+    public String getoVirtIsoFile() {
+        return privateoVirtIsoFile;
+    }
+
+    public String getRootPassword() {
+        return privateRootPassword;
+    }
+
+    public void setIsReinstallOrUpgrade(boolean value) {
+        privateIsReinstallOrUpgrade = value;
+    }
+
+    public void setOverrideFirewall(boolean overrideFirewall) {
+        this.overrideFirewall = overrideFirewall;
+    }
+
+    public void setoVirtIsoFile(String value) {
+        privateoVirtIsoFile = value;
+    }
+
+    private void setRootPassword(String value) {
+        privateRootPassword = value;
     }
 }
