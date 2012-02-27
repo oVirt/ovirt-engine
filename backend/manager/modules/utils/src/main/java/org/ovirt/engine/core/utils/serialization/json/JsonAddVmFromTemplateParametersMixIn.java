51fd264ba23f7aca31c2cbaa443ecf71f009d7b1
Moti Asayag <masayag@redhat.com>
2012-02-27 19:16:02 +0200

    core: Fix AddVmFromTemplateParam desirialization error
    The patch adds another collection to be ignored by the serialization
    of the AddVmFromTemplateParameters.
    
    Change-Id: Ia546e3835abd348f4a33493b45830c7c9e65ed28

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/serialization/json/JsonAddVmFromTemplateParametersMixIn.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/serialization/json/JsonAddVmFromTemplateParametersMixIn.java
@@ -1,10 +1,13 @@
 package org.ovirt.engine.core.utils.serialization.json;
 
+import java.util.Map;
+
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.codehaus.jackson.annotate.JsonTypeInfo.As;
 import org.codehaus.jackson.annotate.JsonTypeInfo.Id;
 import org.ovirt.engine.core.common.action.AddVmFromTemplateParameters;
+import org.ovirt.engine.core.common.businessentities.DiskImageBase;
 import org.ovirt.engine.core.common.queries.ValueObjectMap;
 
 @SuppressWarnings("serial")
@@ -14,4 +17,8 @@ public abstract class JsonAddVmFromTemplateParametersMixIn extends AddVmFromTemp
     @JsonIgnore
     @Override
     public abstract ValueObjectMap getSerializedDiskInfoList();
+
+    @JsonIgnore
+    @Override
+    public abstract Map<String, DiskImageBase> getDiskInfoList();
 }
