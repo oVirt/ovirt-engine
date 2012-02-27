ee2357c3b8c5f4d482c190df1d65b94176a53ebb
Eli Mesika <emesika@redhat.com>
2012-02-28 01:07:11 +0200

    core: add helper methods to VmDeviceCommonUtils.
    Change-Id: Ibcc9d03601cf34becd0b5767cb6702e537860449

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/VmDeviceCommonUtils.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/VmDeviceCommonUtils.java
@@ -3,8 +3,10 @@ package org.ovirt.engine.core.common.utils;
 import java.util.List;
 
 import org.ovirt.engine.core.common.businessentities.BootSequence;
-import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VmDevice;
+import org.ovirt.engine.core.common.config.Config;
+import org.ovirt.engine.core.common.config.ConfigValues;
+import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.Version;
 
 public class VmDeviceCommonUtils {
@@ -12,6 +14,7 @@ public class VmDeviceCommonUtils {
     final static String NETWORK_CHAR = "N";
     final static String CDROM_CHAR = "D";
     final static String DRIVE_CHAR = "C";
+    public final static String CDROM_IMAGE_ID = "11111111-1111-1111-1111-111111111111";
 
     public static boolean isNetwork(VmDevice device) {
         return (device.getType().equals(VmDeviceType.getName(VmDeviceType.INTERFACE)));
@@ -61,8 +64,49 @@ public class VmDeviceCommonUtils {
         return ret;
     }
 
-    public static boolean isOldClusterVersion(VM vm) {
-        Version version = vm.getvds_group_compatibility_version();
+    public static boolean isOldClusterVersion(Version version) {
         return (!(version.getMajor() >= 3 && version.getMinor() >= 1));
     }
+
+    public static boolean isInWhiteList(String type, String device) {
+        String expr = getDeviceTypeSearchExpr(type, device);
+        String whiteList = Config.GetValue(ConfigValues.ManagedDevicesWhiteList);
+        return (whiteList.indexOf(expr) >= 0);
+    }
+
+    /**
+     * appends device id key/value to specParams
+     * @param deviceId
+     * @param specParams
+     * @return
+     */
+    public static String appendDeviceIdToSpecParams(Guid deviceId, String specParams) {
+        StringBuilder sb = new StringBuilder();
+        final String DEVICE = "deviceId=";
+        final String SEP = ",";
+        // device id is included in special parameters
+        // this is done in order to recognize the device when changes are passed from VDSM
+        if (specParams.indexOf(DEVICE) >= 0) {
+            return specParams;
+        } else {
+            if (specParams.length() > 0) {
+                sb.append(specParams);
+                sb.append(SEP);
+            }
+            sb.append(DEVICE);
+            sb.append(deviceId);
+        }
+        return sb.toString();
+    }
+
+    private static String getDeviceTypeSearchExpr(String type, String device) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("type=");
+        sb.append(type);
+        sb.append(" device=");
+        sb.append(device);
+        sb.append(" ");
+        return sb.toString();
+    }
+
 }
