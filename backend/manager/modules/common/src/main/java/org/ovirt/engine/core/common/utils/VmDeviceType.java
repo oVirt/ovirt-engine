b94604a6e73cf319b93ae50d1702f3c1aeefb734
Eli Mesika <emesika@redhat.com>
2012-02-28 00:41:46 +0200

    core: Extending VmDeviceType enum
    Adding types/devices to VmDeviceType enum.
    
    Change-Id: I8fdfad9de784b31fe1fd8315d77f3aa796804143

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/VmDeviceType.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/VmDeviceType.java
@@ -1,11 +1,15 @@
 package org.ovirt.engine.core.common.utils;
 
 public enum VmDeviceType {
+    FLOPPY,
     DISK,
     CDROM,
     INTERFACE,
+    BRIDGE,
     VIDEO,
-    BRIDGE;
+    QXL,
+    CIRRUS,
+    SOUND;
 
     public static String getName(VmDeviceType vmDEviceType) {
         return vmDEviceType.name().toLowerCase();
