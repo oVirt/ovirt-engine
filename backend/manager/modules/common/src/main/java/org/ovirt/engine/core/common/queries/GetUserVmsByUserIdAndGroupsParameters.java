cd80ceb3117c545f28479f6f1e00d1d754b0f011
Allon Mureinik <amureini@redhat.com>
2012-02-27 11:21:21 +0200

    core: Clean GetUserVmsByUserIdAndGroupsParameters
    Remove XML annotations which are no longer needed.
    
    Change-Id: Icf51463760b10eb04da2904b13ece362794102a2

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetUserVmsByUserIdAndGroupsParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetUserVmsByUserIdAndGroupsParameters.java
@@ -1,28 +1,22 @@
 package org.ovirt.engine.core.common.queries;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
-
 import org.ovirt.engine.core.compat.Guid;
 
-@XmlAccessorType(XmlAccessType.NONE)
-@XmlType(name = "GetUserVmsByUserIdAndGroupsParameters")
 public class GetUserVmsByUserIdAndGroupsParameters extends VdcQueryParametersBase {
 
     private static final long serialVersionUID = 98112763182708327L;
+
+    public GetUserVmsByUserIdAndGroupsParameters() {
+    }
 
     public GetUserVmsByUserIdAndGroupsParameters(Guid id) {
         _id = id;
     }
 
-    @XmlElement(name = "Id")
     private Guid _id = new Guid();
 
     private boolean includeDiskData;
 
-    @XmlElement(name = "IncludeDiskData", defaultValue = "false")
     public boolean getIncludeDiskData() {
         return includeDiskData;
     }
@@ -33,8 +27,5 @@ public class GetUserVmsByUserIdAndGroupsParameters extends VdcQueryParametersBas
 
     public Guid getId() {
         return _id;
-    }
-
-    public GetUserVmsByUserIdAndGroupsParameters() {
     }
 }
