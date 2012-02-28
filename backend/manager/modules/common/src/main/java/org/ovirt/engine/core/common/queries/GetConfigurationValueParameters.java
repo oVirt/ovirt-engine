d35601e7b513e01b5e6842e5abb8d36d0cc650a9
Allon Mureinik <amureini@redhat.com>
2012-02-28 14:55:29 +0200

    core: Cleanup GetConfigurationValueParameters
    Removed XML binding annotations that are no longer is use.
    
    Change-Id: I3017b81164bcf1704ed717fe4e7e7ddc4135ff95

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetConfigurationValueParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetConfigurationValueParameters.java
@@ -1,12 +1,6 @@
 package org.ovirt.engine.core.common.queries;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlType;
 
-@XmlAccessorType(XmlAccessType.NONE)
-@XmlType(name = "GetConfigurationValueParameters", namespace = "http://service.engine.ovirt.org")
 public class GetConfigurationValueParameters extends VdcQueryParametersBase {
     private static final long serialVersionUID = -5889171970595969719L;
 
@@ -14,14 +8,12 @@ public class GetConfigurationValueParameters extends VdcQueryParametersBase {
         _configValue = cVal;
     }
 
-    @XmlElement(name = "ConfigValue", required = true)
     private ConfigurationValues _configValue = ConfigurationValues.forValue(0);
 
     public ConfigurationValues getConfigValue() {
         return _configValue;
     }
 
-    @XmlElement(name = "Version")
     private String privateVersion;
 
     public String getVersion() {
