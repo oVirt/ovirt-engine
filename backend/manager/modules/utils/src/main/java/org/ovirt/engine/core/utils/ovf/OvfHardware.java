56e748d02f8077b6938d3cc06561eef1617f85a2
Eli Mesika <emesika@redhat.com>
2012-03-11 20:12:48 +0200

    core: adding CD and OTHER types to OvfHardware enum
    Extending the enum to represent CD and OTHER type.
    OTHER is used as a ResourceType for all un-managed devices.
    
    Change-Id: Ifad05280d89a49f18519394b2c831121b5692267

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfHardware.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfHardware.java
@@ -1,9 +1,11 @@
 package org.ovirt.engine.core.utils.ovf;
 
 public final class OvfHardware {
+    public static final String OTHER = "0";
     public static final String CPU = "3";
     public static final String Memory = "4";
     public static final String Network = "10";
+    public static final String CD = "15";
     public static final String DiskImage = "17";
     public static final String Monitor = "20";
     public static final String USB = "23";
