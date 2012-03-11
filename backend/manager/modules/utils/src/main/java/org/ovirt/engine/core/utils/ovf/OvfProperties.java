95fd2ac081a4aeaa56aac637a2513e33584fc95f
Eli Mesika <emesika@redhat.com>
2012-03-11 20:06:37 +0200

    core: Adding general OVF constants
    Adding constants used by OVF generation.
    
    Change-Id: Id6f26cf23b009481495acc857f036faff6c13ea0

---
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfProperties.java
@@ -0,0 +1,11 @@
+package org.ovirt.engine.core.utils.ovf;
+
+public class OvfProperties {
+
+    final static String VMD_DEVICE = "Device";
+    final static String VMD_TYPE = "Type";
+    final static String VMD_ADDRESS = "rasd:Address";
+    final static String VMD_BOOT_ORDER = "BootOrder";
+    final static String VMD_IS_PLUGGED = "IsPLugged";
+    final static String VMD_IS_READONLY = "IsReadOnly";
+}
