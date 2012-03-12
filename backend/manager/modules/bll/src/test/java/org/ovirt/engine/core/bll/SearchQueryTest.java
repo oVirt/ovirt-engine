db2ca9a8b2957cfaf740548e8344608fff249fea
Maor Lipchuk <mlipchuk@redhat.com>
2012-03-12 19:15:11 +0200

    engine: Add test for search query
    Adding a test for search query.
    The search for now tests the get all queries by checking if the view,
    key and the order by, are part of the generated sql.
    
    The tests which were added are testing storage pool, VM, VDS, disk image
    and cluster.
    
    Take in notice one test is ignored since searching for multiple
    Datacenters is not implemented in the search backend.
    So the patch which will fix that should also change the test from
    ignored.
    
    Change-Id: I07cc6795008b39281567643697e30b6710f6f457

---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/SearchQueryTest.java
@@ -0,0 +1,340 @@
+package org.ovirt.engine.core.bll;
+
+import static junit.framework.Assert.assertTrue;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.ovirt.engine.core.common.businessentities.DiskImage;
+import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.VDSGroup;
+import org.ovirt.engine.core.common.businessentities.VM;
+import org.ovirt.engine.core.common.businessentities.storage_pool;
+import org.ovirt.engine.core.common.config.Config;
+import org.ovirt.engine.core.common.config.ConfigValues;
+import org.ovirt.engine.core.common.interfaces.SearchType;
+import org.ovirt.engine.core.common.queries.SearchParameters;
+import org.ovirt.engine.core.dal.dbbroker.DbEngineDialect;
+import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.DiskImageDAO;
+import org.ovirt.engine.core.dao.StoragePoolDAO;
+import org.ovirt.engine.core.dao.VdsDAO;
+import org.ovirt.engine.core.dao.VdsGroupDAO;
+import org.ovirt.engine.core.dao.VmDAO;
+import org.ovirt.engine.core.searchbackend.SearchObjectAutoCompleter;
+import org.ovirt.engine.core.searchbackend.SearchObjects;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ DbFacade.class, Config.class })
+public class SearchQueryTest {
+
+    List<DiskImage> diskImageResultList = new ArrayList<DiskImage>();
+    List<VM> vmResultList = new ArrayList<VM>();
+    List<VDS> vdsResultList = new ArrayList<VDS>();
+    List<VDSGroup> vdsGroupResultList = new ArrayList<VDSGroup>();
+    List<storage_pool> storagePoolResultList = new ArrayList<storage_pool>();
+
+    public SearchQueryTest() {
+        MockitoAnnotations.initMocks(this);
+        mockStatic(Config.class);
+        mockStatic(DbFacade.class);
+
+    }
+
+    @Before
+    public void setup() throws Exception {
+        mockDAO();
+        mockConfig();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    private void mockDAO() {
+        final DiskImageDAO diskImageDAO = Mockito.mock(DiskImageDAO.class);
+        final VmDAO vmDAO = Mockito.mock(VmDAO.class);
+        final VdsDAO vdsDAO = Mockito.mock(VdsDAO.class);
+        final VdsGroupDAO vdsGroupDAO = Mockito.mock(VdsGroupDAO.class);
+        final StoragePoolDAO storagePoolDAO = Mockito.mock(StoragePoolDAO.class);
+        final DbEngineDialect dbEngineDialect = Mockito.mock(DbEngineDialect.class);
+        final DbFacade facadeMock = new DbFacade() {
+            @Override
+            public DiskImageDAO getDiskImageDAO() {
+                return diskImageDAO;
+            }
+
+            @Override
+            public VmDAO getVmDAO() {
+                return vmDAO;
+            }
+
+            @Override
+            public VdsDAO getVdsDAO() {
+                return vdsDAO;
+            }
+
+            @Override
+            public VdsGroupDAO getVdsGroupDAO() {
+                return vdsGroupDAO;
+            }
+
+            @Override
+            public StoragePoolDAO getStoragePoolDAO() {
+                return storagePoolDAO;
+            }
+
+            public DbEngineDialect getDbEngineDialect() {
+                return dbEngineDialect;
+            }
+        };
+
+        Mockito.when(DbFacade.getInstance()).thenReturn(facadeMock);
+        Mockito.when(dbEngineDialect.getPreSearchQueryCommand()).thenReturn("");
+
+        // mock DAOs
+        mockDiskImageDAO(diskImageDAO);
+        mockVMDAO(vmDAO);
+        mockVdsDAO(vdsDAO);
+        mockVdsGroupDAO(vdsGroupDAO);
+        mockStoragePoolDAO(storagePoolDAO);
+    }
+
+    /**
+     * Mock disk image DAO so that when getAllWithQuery will be called with the appropriate query string, a unique list will
+     * be returned. <BR/>
+     * This returned list will indicate, if the correct string has been passed as an argument to the getAllWithQuery
+     * API.
+     *
+     * @param diskImageDao
+     *            - The dao to be used
+     */
+    private void mockDiskImageDAO(final DiskImageDAO diskImageDAO) {
+        SearchObjectAutoCompleter search = new SearchObjectAutoCompleter(false);
+        Mockito.when(diskImageDAO.getAllWithQuery(Matchers.matches(getDiskImageRegexString(search))))
+                .thenReturn(diskImageResultList);
+    }
+
+    /**
+     * Mock vds group DAO so that when getAllWithQuery will be called with the appropriate query string, a unique list will
+     * be returned. <BR/>
+     * This returned list will indicate, if the correct string has been passed as an argument to the getAllWithQuery
+     * API.
+     *
+     * @param vdsGroupDAO
+     *            - The dao to be used
+     */
+    private void mockVdsGroupDAO(final VdsGroupDAO vdsGroupDAO) {
+        SearchObjectAutoCompleter search = new SearchObjectAutoCompleter(false);
+        Mockito.when(vdsGroupDAO.getAllWithQuery(Matchers.matches(getVdsGroupRegexString(search))))
+                .thenReturn(vdsGroupResultList);
+    }
+
+    /**
+     * Mock storage pool DAO so that when getAllWithQuery will be called with the appropriate query string, a unique list will
+     * be returned. <BR/>
+     * This returned list will indicate, if the correct string has been passed as an argument to the getAllWithQuery
+     * API.
+     *
+     * @param storagePoolDAO
+     *            - The dao to be used
+     */
+    private void mockStoragePoolDAO(final StoragePoolDAO storagePoolDAO) {
+        SearchObjectAutoCompleter search = new SearchObjectAutoCompleter(false);
+        Mockito.when(storagePoolDAO.getAllWithQuery(Matchers.matches(getStoragePoolRegexString(search))))
+                .thenReturn(storagePoolResultList);
+    }
+
+    /**
+     * Regex string which contains all of the disk image properties.
+     *
+     * @param search
+     */
+    private String getDiskImageRegexString(SearchObjectAutoCompleter search) {
+        return ".*" + search.getDefaultSort(SearchObjects.DISK_IMAGE_OBJ_NAME) + ".*"
+                + search.getRelatedTableNameWithOutTags(SearchObjects.DISK_IMAGE_OBJ_NAME) + ".* "
+                + search.getPrimeryKeyName(SearchObjects.DISK_IMAGE_OBJ_NAME) + ".*";
+    }
+
+    /**
+     * Mock Vds DAO so that when getAllWithQuery will be called with the appropriate query string, a unique list will
+     * be returned. <BR/>
+     * This returned list will indicate, if the correct string has been passed as an argument to the getAllWithQuery
+     * API.
+     *
+     * @param diskImageDao
+     *            - The dao to be used
+     */
+    private void mockVdsDAO(final VdsDAO vdsDAO) {
+        SearchObjectAutoCompleter search = new SearchObjectAutoCompleter(false);
+        Mockito.when(vdsDAO.getAllWithQuery(Matchers.matches(getVdsRegexString(search))))
+                .thenReturn(vdsResultList);
+    }
+
+    /**
+     * Mock VM DAO so that when getAllWithQuery will be called with the appropriate query string, a unique list will
+     * be returned. <BR/>
+     * This returned list will indicate, if the correct string has been passed as an argument to the getAllWithQuery
+     * API.
+     *
+     * @param vmDAO
+     *            - The dao to be used
+     */
+    private void mockVMDAO(final VmDAO vmDAO) {
+        SearchObjectAutoCompleter search = new SearchObjectAutoCompleter(false);
+        Mockito.when(vmDAO.getAllUsingQuery(Matchers.matches(getVMRegexString(search))))
+                .thenReturn(vmResultList);
+    }
+
+    /**
+     * Regex string which contains all of the VM properties.
+     *
+     * @param search
+     */
+    private String getVMRegexString(SearchObjectAutoCompleter search) {
+        return ".*" + search.getDefaultSort(SearchObjects.VM_OBJ_NAME) + ".*"
+                + search.getRelatedTableNameWithOutTags(SearchObjects.VM_OBJ_NAME) + ".* "
+                + search.getPrimeryKeyName(SearchObjects.VM_OBJ_NAME) + ".*";
+    }
+
+    /**
+     * Regex string which contains all of the VDS properties.
+     *
+     * @param search
+     */
+    private String getVdsRegexString(SearchObjectAutoCompleter search) {
+        return ".*" + search.getDefaultSort(SearchObjects.VDS_OBJ_NAME) + ".*"
+                + search.getRelatedTableNameWithOutTags(SearchObjects.VDS_OBJ_NAME) + ".* "
+                + search.getPrimeryKeyName(SearchObjects.VDS_OBJ_NAME) + ".*";
+    }
+
+    /**
+     * Regex string which contains all of the Vds group properties.
+     *
+     * @param search
+     */
+    private String getVdsGroupRegexString(SearchObjectAutoCompleter search) {
+        return ".*" + search.getDefaultSort(SearchObjects.VDC_CLUSTER_OBJ_NAME) + ".*"
+                + search.getRelatedTableNameWithOutTags(SearchObjects.VDC_CLUSTER_OBJ_NAME) + ".* "
+                + search.getPrimeryKeyName(SearchObjects.VDC_CLUSTER_OBJ_NAME) + ".*";
+    }
+
+    /**
+     * Regex string which contains all of the storage pool properties.
+     *
+     * @param search
+     */
+    private String getStoragePoolRegexString(SearchObjectAutoCompleter search) {
+        return ".*" + search.getDefaultSort(SearchObjects.VDC_STORAGE_POOL_OBJ_NAME) + ".*"
+                + search.getRelatedTableNameWithOutTags(SearchObjects.VDC_STORAGE_POOL_OBJ_NAME) + ".* "
+                + search.getPrimeryKeyName(SearchObjects.VDC_STORAGE_POOL_OBJ_NAME) + ".*";
+    }
+
+    /**
+     * Mock the configuration values used in the search logic.
+     */
+    private void mockConfig() {
+        when(Config.<Object> GetValue(ConfigValues.SearchResultsLimit)).thenReturn(100);
+        when(Config.<Object> GetValue(ConfigValues.DBSearchTemplate)).thenReturn("SELECT * FROM (SELECT *, ROW_NUMBER() OVER(%1$s) as RowNum FROM (%2$s)) as T1 ) as T2 %3$s");
+    }
+
+    @Test
+    public void testGetAllMultiDiskImageSearch() throws Exception {
+        SearchParameters searchParam = new SearchParameters("Disks:", SearchType.DiskImage);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(diskImageResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    @Test
+    public void testGetAllDiskImageSearch() throws Exception {
+        // The query Should be used is : "SELECT * FROM (SELECT *, ROW_NUMBER() OVER( ORDER BY disk_name ASC ) as RowNum
+        // FROM (SELECT * FROM vm_images_view WHERE ( image_guid IN (SELECT vm_images_view.image_guid FROM
+        // vm_images_view ))) as T1 ) as T2"
+        SearchParameters searchParam = new SearchParameters("Disk:", SearchType.DiskImage);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(diskImageResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    @Test
+    public void testGetAllVMSearch() throws Exception {
+        SearchParameters searchParam = new SearchParameters("VM:", SearchType.VM);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(vmResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    @Test
+    public void testGetAllMultiVmSearch() throws Exception {
+        SearchParameters searchParam = new SearchParameters("VMs:", SearchType.VM);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(vmResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    @Test
+    public void testGetAllVdsSearch() throws Exception {
+        SearchParameters searchParam = new SearchParameters("Host:", SearchType.VDS);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(vdsResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    @Test
+    public void testGetAllMultiVdsSearch() throws Exception {
+        SearchParameters searchParam = new SearchParameters("Hosts:", SearchType.VDS);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(vdsResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    @Test
+    public void testGetAllClusterSearch() throws Exception {
+        // The original query should be : SELECT * FROM (SELECT *, ROW_NUMBER() OVER( ORDER BY name ASC ) as RowNum FROM
+        // (SELECT * FROM vds_groups WHERE ( vds_group_id IN (SELECT vds_groups_storage_domain.vds_group_id FROM
+        // vds_groups_storage_domain ))) as T1 ) as T2
+        SearchParameters searchParam = new SearchParameters("Cluster:", SearchType.Cluster);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(vdsGroupResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    @Test
+    public void testGetAllMultiClusterSearch() throws Exception {
+        // The original query should be : SELECT * FROM (SELECT *, ROW_NUMBER() OVER( ORDER BY name ASC ) as RowNum FROM
+        // (SELECT * FROM vds_groups WHERE ( vds_group_id IN (SELECT vds_groups_storage_domain.vds_group_id FROM
+        // vds_groups_storage_domain ))) as T1 ) as T2
+        SearchParameters searchParam = new SearchParameters("Clusters:", SearchType.Cluster);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(vdsGroupResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    @Test
+    public void testGetAllStoragePoolSearch() throws Exception {
+        SearchParameters searchParam = new SearchParameters("Datacenter:", SearchType.StoragePool);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(storagePoolResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+
+    // TODO: Search using search text "Datacenters:" is not supported.
+    @Ignore
+    @Test
+    public void testGetAllMultiStoragePoolSearch() throws Exception {
+        SearchParameters searchParam = new SearchParameters("Datacenters:", SearchType.StoragePool);
+        SearchQuery<SearchParameters> searchQuery = spy(new SearchQuery<SearchParameters>(searchParam));
+        searchQuery.executeQueryCommand();
+        assertTrue(storagePoolResultList == searchQuery.getQueryReturnValue().getReturnValue());
+    }
+}
