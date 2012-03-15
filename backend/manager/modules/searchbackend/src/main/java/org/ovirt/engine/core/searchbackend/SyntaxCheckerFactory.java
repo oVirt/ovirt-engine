5d44470b3da378e8714afba31df81cb1bdcaad1d
Laszlo Hornyak <lhornyak@redhat.com>
2012-03-15 09:37:45 +0100

    engine-core: SyntaxCheckerFactory cleanup
    - removes the synchronization blocks, this may allow creating multiple
      instances in extreme cases, but the instances are immutable, they will
      do the same
    - stores the the syntax checkers in fields rather than in a map
    
    Change-Id: If2b632c3c13f4ba4b6f5373b942b29198ce6853d

--- backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/SyntaxCheckerFactory.java
+++ backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/SyntaxCheckerFactory.java
@@ -1,55 +1,35 @@
 package org.ovirt.engine.core.searchbackend;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 
 public class SyntaxCheckerFactory {
-    private static final String UISyntaxChecker = "UISyntaxChecker";
-    private static final String BackendSyntaxChecker = "BackendSyntaxChecker";
-    private static final String ADSyntaxChecker = "ADSyntaxChecker";
-    private static final Map<String, ISyntaxChecker> syntaxCheckers =
-            new HashMap<String, ISyntaxChecker>();
 
     private static String ConfigAuthenticationMethod;
+    private static final ISyntaxChecker uiSyntaxChecker = new SyntaxChecker(100, true);
+    private static ISyntaxChecker backendSyntaxChecker = null;
+    private static final ISyntaxChecker adSyntaxChecker = new ADSyntaxChecker();
 
     // This method is for used in client side by uicommon, which cannot access
     // server side config, so needs to get the auth method as a parameter.
 
     public static ISyntaxChecker CreateUISyntaxChecker(String AuthenticationMethod) {
         ConfigAuthenticationMethod = AuthenticationMethod;
-        synchronized (syntaxCheckers) {
-            if (!syntaxCheckers.containsKey(UISyntaxChecker)) {
-                SyntaxChecker checker = new SyntaxChecker(100, true);
-                syntaxCheckers.put(UISyntaxChecker, checker);
-            }
-            return syntaxCheckers.get(UISyntaxChecker);
-        }
+        return uiSyntaxChecker;
     }
 
     public static ISyntaxChecker CreateBackendSyntaxChecker(String AuthenticationMethod) {
         ConfigAuthenticationMethod = AuthenticationMethod;
-        synchronized (syntaxCheckers) {
-            if (!syntaxCheckers.containsKey(BackendSyntaxChecker)) {
-                SyntaxChecker checker = new SyntaxChecker(Config.<Integer> GetValue(ConfigValues.SearchResultsLimit),
-                        true);
-                syntaxCheckers.put(BackendSyntaxChecker, checker);
-            }
-            return syntaxCheckers.get(BackendSyntaxChecker);
+        if(backendSyntaxChecker == null) {
+            backendSyntaxChecker = new SyntaxChecker(Config.<Integer> GetValue(ConfigValues.SearchResultsLimit),
+                    true);
         }
+        return backendSyntaxChecker;
     }
 
     public static ISyntaxChecker CreateADSyntaxChecker(String AuthenticationMethod) {
         ConfigAuthenticationMethod = AuthenticationMethod;
-        synchronized (syntaxCheckers) {
-            if (!syntaxCheckers.containsKey(ADSyntaxChecker)) {
-                ADSyntaxChecker checker = new ADSyntaxChecker();
-                syntaxCheckers.put(ADSyntaxChecker, checker);
-            }
-            return syntaxCheckers.get(ADSyntaxChecker);
-        }
+        return adSyntaxChecker;
     }
 
     public static String getConfigAuthenticationMethod() {
