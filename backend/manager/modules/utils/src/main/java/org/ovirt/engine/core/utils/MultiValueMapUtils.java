ef59363748c36c7f25aa1f7c05d3ffada2094020
Allon Mureinik <amureini@redhat.com>
2012-03-15 17:30:37 +0200

    core: Further generalize MultiValueMapUtils
    Add support to MultiValueMapUtils for other collection types, instead of
    just assuming ArrayList is used.
    
    Change-Id: I1d03ec0716034fb94c67b827c0befe676dba351b

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/MultiValueMapUtils.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/MultiValueMapUtils.java
@@ -1,8 +1,12 @@
 package org.ovirt.engine.core.utils;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * A utility class for manipulating maps that have a value of a {@link List} type.
@@ -16,21 +20,71 @@ public class MultiValueMapUtils {
     /**
      * Adds an additional {@link #value} to the given {@link #key} in the {@link #map}.
      *
-     * If a list of values was already associated with this key, the new value is added to its end.
+     * If a collection of values was already associated with this key, the new value is added to its end.
      * It's assumed that the existing list implementation is mutable.
      *
-     * If there is no list associated with this value, a new list containing the value will be created.
+     * If there is no collection associated with this value, a new collection containing the value will be created.
      *
      * @param key The key of the map to which the value should be added.
      * @param value The value to add to the map
      * @param map The map to have the value added
+     * @param creator A factory to create the new (empty) collection for the values to be held
+     */
+    public static <K, V, C extends Collection<V>> void addToMap(K key,
+            V value,
+            Map<K, C> map,
+            CollectionCreator<V, C> creator) {
+        C existingValues = map.get(key);
+        if (existingValues == null) {
+            existingValues = creator.create();
+            map.put(key, existingValues);
+        }
+        existingValues.add(value);
+    }
+
+    /**
+     * Same as {@link #addToMap(Object, Object, Map, CollectionCreator)}, with the default {@link ArrayList} to hold the values.
      */
     public static <K, V> void addToMap(K key, V value, Map<K, List<V>> map) {
-        List<V> existingList = map.get(key);
-        if (existingList == null) {
-            existingList = new ArrayList<V>();
-            map.put(key, existingList);
+        addToMap(key, value, map, new ListCreator<V>());
+    }
+
+    public static interface CollectionCreator<V, C extends Collection<V>> {
+        C create();
+    }
+
+    public static class ListCreator<V> implements CollectionCreator<V, List<V>> {
+        @Override
+        public List<V> create() {
+            return new ArrayList<V>();
         }
-        existingList.add(value);
+    }
+
+    public static class ArrayListCreator<V> implements CollectionCreator<V, ArrayList<V>> {
+        @Override
+        public ArrayList<V> create() {
+            return new ArrayList<V>();
+        }
+    }
+
+    public static class LinkedListCreator<V> implements CollectionCreator<V, LinkedList<V>> {
+        @Override
+        public LinkedList<V> create() {
+            return new LinkedList<V>();
+        }
+    }
+
+    public static class SetCreator<V> implements CollectionCreator<V, Set<V>> {
+        @Override
+        public Set<V> create() {
+            return new HashSet<V>();
+        }
+    }
+
+    public static class HashSetCreator<V> implements CollectionCreator<V, HashSet<V>> {
+        @Override
+        public HashSet<V> create() {
+            return new HashSet<V>();
+        }
     }
 }
