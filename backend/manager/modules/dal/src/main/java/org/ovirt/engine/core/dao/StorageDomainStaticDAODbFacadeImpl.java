d292d95f864ddf9d52799dbb5cfa6cd5d83f4f6e
Laszlo Hornyak <lhornyak@redhat.com>
2012-03-20 17:37:21 +0100

    engine-core: cleanup StorageDomainStaticDAODbFacadeImpl
    Change-Id: Ic745f25b1524dcaaffd3e97b64e3d702ba6a97d5

--- backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/StorageDomainStaticDAODbFacadeImpl.java
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/StorageDomainStaticDAODbFacadeImpl.java
@@ -12,211 +12,55 @@ import org.ovirt.engine.core.compat.Guid;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 
-public class StorageDomainStaticDAODbFacadeImpl extends BaseDAODbFacade implements StorageDomainStaticDAO {
+public class StorageDomainStaticDAODbFacadeImpl extends DefaultGenericDaoDbFacade<storage_domain_static, Guid> implements StorageDomainStaticDAO {
 
-    @Override
-    public storage_domain_static get(Guid id) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("id", id);
-
-        ParameterizedRowMapper<storage_domain_static> mapper = new ParameterizedRowMapper<storage_domain_static>() {
-            @Override
-            public storage_domain_static mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                storage_domain_static entity = new storage_domain_static();
-                entity.setId(Guid.createGuidFromString(rs.getString("id")));
-                entity.setstorage(rs.getString("storage"));
-                entity.setstorage_name(rs.getString("storage_name"));
-                entity.setstorage_type(StorageType.forValue(rs
-                        .getInt("storage_type")));
-                entity.setstorage_domain_type(StorageDomainType.forValue(rs
-                        .getInt("storage_domain_type")));
-                entity.setStorageFormat(StorageFormatType.forValue(rs
-                        .getString("storage_domain_format_type")));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeRead("Getstorage_domain_staticByid", mapper, parameterSource);
+    public StorageDomainStaticDAODbFacadeImpl() {
+        super("storage_domain_static");
+        setProcedureNameForGet("Getstorage_domain_staticByid");
+        setProcedureNameForGetAll("GetAllFromstorage_domain_static");
     }
 
     @Override
     public storage_domain_static getByName(String name) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("name", name);
-
-        ParameterizedRowMapper<storage_domain_static> mapper = new ParameterizedRowMapper<storage_domain_static>() {
-            @Override
-            public storage_domain_static mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                storage_domain_static entity = new storage_domain_static();
-                entity.setId(Guid.createGuidFromString(rs.getString("id")));
-                entity.setstorage(rs.getString("storage"));
-                entity.setstorage_name(rs.getString("storage_name"));
-                entity.setstorage_type(StorageType.forValue(rs
-                        .getInt("storage_type")));
-                entity.setstorage_domain_type(StorageDomainType.forValue(rs
-                        .getInt("storage_domain_type")));
-                entity.setStorageFormat(StorageFormatType.forValue(rs
-                        .getString("storage_domain_format_type")));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeRead("Getstorage_domain_staticByName", mapper, parameterSource);
+        return getCallsHandler().executeRead("Getstorage_domain_staticByName",
+                StorageDomainStaticRowMapper.instance,
+                getCustomMapSqlParameterSource()
+                        .addValue("name", name));
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public List<storage_domain_static> getAllOfStorageType(
             StorageType type) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("storage_pool_type", type);
-
-        ParameterizedRowMapper<storage_domain_static> mapper = new ParameterizedRowMapper<storage_domain_static>() {
-            @Override
-            public storage_domain_static mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                storage_domain_static entity = new storage_domain_static();
-                entity.setId(Guid.createGuidFromString(rs.getString("id")));
-                entity.setstorage(rs.getString("storage"));
-                entity.setstorage_name(rs.getString("storage_name"));
-                entity.setstorage_type(StorageType.forValue(rs
-                        .getInt("storage_type")));
-                entity.setstorage_domain_type(StorageDomainType.forValue(rs
-                        .getInt("storage_domain_type")));
-                entity.setStorageFormat(StorageFormatType.forValue(rs
-                        .getString("storage_domain_format_type")));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("Getstorage_domain_staticBystorage_pool_type", mapper, parameterSource);
+        return getCallsHandler().executeReadList("Getstorage_domain_staticBystorage_pool_type",
+                StorageDomainStaticRowMapper.instance,
+                getCustomMapSqlParameterSource()
+                        .addValue("storage_pool_type", type));
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public List<storage_domain_static> getAllForStoragePoolOfStorageType(
             StorageType type, Guid pool) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("storage_type", type).addValue("storage_pool_id", pool);
-
-        ParameterizedRowMapper<storage_domain_static> mapper = new ParameterizedRowMapper<storage_domain_static>() {
-            @Override
-            public storage_domain_static mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                storage_domain_static entity = new storage_domain_static();
-                entity.setId(Guid.createGuidFromString(rs.getString("id")));
-                entity.setstorage(rs.getString("storage"));
-                entity.setstorage_name(rs.getString("storage_name"));
-                // entity.setstorage_pool_id(rs.getString("storage_pool_id"));
-                entity.setstorage_type(StorageType.forValue(rs
-                        .getInt("storage_type")));
-                entity.setstorage_pool_name(rs.getString("storage_pool_name"));
-                entity.setstorage_domain_type(StorageDomainType.forValue(rs
-                        .getInt("storage_domain_type")));
-                entity.setStorageFormat(StorageFormatType.forValue(rs
-                        .getString("storage_domain_format_type")));
-                // entity.setstorage_domain_shared_status(rs.getInt("storage_domain_shared_status"));
-                return entity;
-            }
-        };
-
         return getCallsHandler().executeReadList("Getstorage_domain_staticBystorage_type_and_storage_pool_id",
-                mapper,
-                parameterSource);
+                StorageDomainStaticRowMapper.instance,
+                getStoragePoolIdParameterSource(pool)
+                        .addValue("storage_type", type));
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public List<storage_domain_static> getAllForStoragePool(Guid id) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
+        return getCallsHandler().executeReadList("Getstorage_domain_staticBystorage_pool_id",
+                StorageDomainStaticRowMapper.instance,
+                getStoragePoolIdParameterSource(id));
+    }
+
+    private MapSqlParameterSource getStoragePoolIdParameterSource(Guid id) {
+        return getCustomMapSqlParameterSource()
                 .addValue("storage_pool_id", id);
-
-        ParameterizedRowMapper<storage_domain_static> mapper = new ParameterizedRowMapper<storage_domain_static>() {
-            @Override
-            public storage_domain_static mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                storage_domain_static entity = new storage_domain_static();
-                entity.setId(Guid.createGuidFromString(rs.getString("id")));
-                entity.setstorage(rs.getString("storage"));
-                entity.setstorage_name(rs.getString("storage_name"));
-                entity.setstorage_type(StorageType.forValue(rs
-                        .getInt("storage_type")));
-                entity.setstorage_domain_type(StorageDomainType.forValue(rs
-                        .getInt("storage_domain_type")));
-                entity.setStorageFormat(StorageFormatType.forValue(rs
-                        .getString("storage_domain_format_type")));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("Getstorage_domain_staticBystorage_pool_id", mapper, parameterSource);
     }
 
-    @Override
-    public void save(storage_domain_static domain) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("id", domain.getId())
-                .addValue("storage", domain.getstorage())
-                .addValue("storage_name", domain.getstorage_name())
-                .addValue("storage_type", domain.getstorage_type())
-                .addValue("storage_domain_type",
-                        domain.getstorage_domain_type())
-                .addValue("storage_domain_format_type", domain.getStorageFormat());
-
-        getCallsHandler().executeModification("Insertstorage_domain_static", parameterSource);
-    }
-
-    @Override
-    public void update(storage_domain_static domain) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("id", domain.getId())
-                .addValue("storage", domain.getstorage())
-                .addValue("storage_name", domain.getstorage_name())
-                .addValue("storage_type", domain.getstorage_type())
-                .addValue("storage_domain_type",
-                        domain.getstorage_domain_type())
-                .addValue("storage_domain_format_type", domain.getStorageFormat());
-
-        getCallsHandler().executeModification("Updatestorage_domain_static", parameterSource);
-    }
-
-    @Override
-    public void remove(Guid id) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("id", id);
-
-        getCallsHandler().executeModification("Deletestorage_domain_static", parameterSource);
-    }
-
-    @Override
-    public List<storage_domain_static> getAll() {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource();
-
-        ParameterizedRowMapper<storage_domain_static> mapper = new ParameterizedRowMapper<storage_domain_static>() {
-            @Override
-            public storage_domain_static mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                storage_domain_static entity = new storage_domain_static();
-                entity.setId(Guid.createGuidFromString(rs.getString("id")));
-                entity.setstorage(rs.getString("storage"));
-                entity.setstorage_name(rs.getString("storage_name"));
-                entity.setstorage_type(StorageType.forValue(rs.getInt("storage_type")));
-                entity.setstorage_domain_type(StorageDomainType.forValue(rs.getInt("storage_domain_type")));
-                entity.setStorageFormat(StorageFormatType.forValue(rs.getString("storage_domain_format_type")));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("GetAllFromstorage_domain_static", mapper, parameterSource);
-    }
-
-    @SuppressWarnings("unchecked")
     @Override
     public List<Guid> getAllIds(Guid pool, StorageDomainStatus status) {
-        MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
-                .addValue("storage_pool_id", pool)
+        MapSqlParameterSource parameterSource = getStoragePoolIdParameterSource(pool)
                 .addValue("status", status.getValue());
 
         ParameterizedRowMapper<Guid> mapper = new ParameterizedRowMapper<Guid>() {
@@ -228,4 +72,48 @@ public class StorageDomainStaticDAODbFacadeImpl extends BaseDAODbFacade implemen
 
         return getCallsHandler().executeReadList("GetStorageDomainIdsByStoragePoolIdAndStatus", mapper, parameterSource);
     }
+
+    @Override
+    protected MapSqlParameterSource createIdParameterMapper(Guid id) {
+        return getCustomMapSqlParameterSource()
+                .addValue("id", id);
+    }
+
+    @Override
+    protected MapSqlParameterSource createFullParametersMapper(storage_domain_static domain) {
+        return getCustomMapSqlParameterSource()
+                .addValue("id", domain.getId())
+                .addValue("storage", domain.getstorage())
+                .addValue("storage_name", domain.getstorage_name())
+                .addValue("storage_type", domain.getstorage_type())
+                .addValue("storage_domain_type",
+                        domain.getstorage_domain_type())
+                .addValue("storage_domain_format_type", domain.getStorageFormat());
+    }
+
+    @Override
+    protected ParameterizedRowMapper<storage_domain_static> createEntityRowMapper() {
+        return StorageDomainStaticRowMapper.instance;
+    }
+
+    private static final class StorageDomainStaticRowMapper implements ParameterizedRowMapper<storage_domain_static> {
+        public static final StorageDomainStaticRowMapper instance = new StorageDomainStaticRowMapper();
+
+        @Override
+        public storage_domain_static mapRow(ResultSet rs, int rowNum)
+                throws SQLException {
+            storage_domain_static entity = new storage_domain_static();
+            entity.setId(Guid.createGuidFromString(rs.getString("id")));
+            entity.setstorage(rs.getString("storage"));
+            entity.setstorage_name(rs.getString("storage_name"));
+            entity.setstorage_type(StorageType.forValue(rs
+                    .getInt("storage_type")));
+            entity.setstorage_domain_type(StorageDomainType.forValue(rs
+                    .getInt("storage_domain_type")));
+            entity.setStorageFormat(StorageFormatType.forValue(rs
+                    .getString("storage_domain_format_type")));
+            return entity;
+        }
+    }
+
 }
