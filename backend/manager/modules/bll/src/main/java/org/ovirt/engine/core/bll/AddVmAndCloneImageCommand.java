00410d4205e8ef025bb7bce01ad740facb0f09a1
Yair Zaslavsky <yzaslavs@redhat.com>
2012-03-22 11:06:52 +0200

    core: Fixing/refactoring buildAndCheckDestStorageDomains
    Fixing and refactoring buildAndChekcDestStorageDomains.
    1. At AddVmCommand - extracting a method for getting a collection of
    images that participate in the check
    2. At AddVmAndClone - overriding this method, returning the collection
    of the images to be cloned (and not the images of the template as in the
    base class).
    
    Change-Id: Iace8881f1d05f037a715e3a6679adaea0e2e4aee

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmAndCloneImageCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmAndCloneImageCommand.java
@@ -227,6 +227,10 @@ public abstract class AddVmAndCloneImageCommand<T extends VmManagementParameters
      */
     protected abstract Collection<DiskImage> getDiskImagesToBeCloned();
 
+    protected Collection<DiskImage> getImagesForMultipleStoragesDomainsCheck() {
+        return getDiskImagesToBeCloned();
+    }
+
     protected DiskImage getDiskImageToRemoveByParam(MoveOrCopyImageGroupParameters param) {
         Guid imageGroupId = param.getDestImageGroupId();
         Guid imageId = param.getDestinationImageId();
--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmCommand.java
@@ -158,7 +158,7 @@ public class AddVmCommand<T extends VmManagementParametersBase> extends VmManage
         }
         if (returnValue
                 && getVmTemplate().getDiskMap().size() > 0
-                && !LinqUtils.firstOrNull(getVmTemplate().getDiskMap().values(), new All<DiskImage>())
+                && !LinqUtils.firstOrNull(getImagesForMultipleStoragesDomainsCheck(), new All<DiskImage>())
                         .getId().equals(VmTemplateHandler.BlankVmTemplateId)) {
             if (!getStoragePoolId().equals(getVmTemplate().getstorage_pool_id().getValue())) {
                 reasons.add(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_POOL_NOT_MATCH.toString());
@@ -263,7 +263,7 @@ public class AddVmCommand<T extends VmManagementParametersBase> extends VmManage
         returnValue = buildAndCheckDestStorageDomains();
         if (returnValue) {
             storageToDisksMap =
-                    ImagesHandler.buildStorageToDiskMap(getVmTemplate().getDiskMap().values(),
+                    ImagesHandler.buildStorageToDiskMap(getImagesForMultipleStoragesDomainsCheck(),
                             imageToDestinationDomainMap);
             returnValue = CanDoAddVmCommand();
         }
@@ -306,7 +306,7 @@ public class AddVmCommand<T extends VmManagementParametersBase> extends VmManage
         } else {
             retValue = validateProvidedDestinations();
         }
-        if (retValue && getVmTemplate().getDiskMap().values().size() != imageToDestinationDomainMap.size()) {
+        if (retValue && getImagesForMultipleStoragesDomainsCheck().size() != imageToDestinationDomainMap.size()) {
             log.errorFormat("Can not found any default active domain for one of the disks of template with id : {0}",
                     getVmTemplate().getId());
             addCanDoActionMessage(VdcBllMessages.ACTION_TYPE_FAILED_MISSED_STORAGES_FOR_SOME_DISKS);
@@ -314,6 +314,10 @@ public class AddVmCommand<T extends VmManagementParametersBase> extends VmManage
         }
 
         return retValue;
+    }
+
+    protected Collection<DiskImage> getImagesForMultipleStoragesDomainsCheck() {
+        return getVmTemplate().getDiskMap().values();
     }
 
     private boolean validateProvidedDestinations() {
@@ -336,7 +340,7 @@ public class AddVmCommand<T extends VmManagementParametersBase> extends VmManage
         if (getParameters().getStorageDomainId() != null
                 && !Guid.Empty.equals(getParameters().getStorageDomainId())) {
             Guid storageId = getParameters().getStorageDomainId();
-            for (DiskImage image : getVmTemplate().getDiskMap().values()) {
+            for (DiskImage image : getImagesForMultipleStoragesDomainsCheck()) {
                 imageToDestinationDomainMap.put(image.getId(), storageId);
             }
             return validateProvidedDestinations();
@@ -353,7 +357,7 @@ public class AddVmCommand<T extends VmManagementParametersBase> extends VmManage
         getParameters().setQuotaId(QuotaHelper.getInstance().getQuotaIdToConsume(getParameters().getVmStaticData()
                 .getQuotaId(),
                 getStoragePool()));
-        for (DiskImage dit : getVmTemplate().getDiskMap().values()) {
+        for (DiskImage dit : getImagesForMultipleStoragesDomainsCheck()) {
             dit.setQuotaId(QuotaHelper.getInstance()
                     .getQuotaIdToConsume(getParameters().getVmStaticData().getQuotaId(),
                             getStoragePool()));
@@ -531,7 +535,7 @@ public class AddVmCommand<T extends VmManagementParametersBase> extends VmManage
                 throw new VdcBLLException(VdcBllErrors.IRS_IMAGE_STATUS_ILLEGAL);
             }
             VmHandler.LockVm(getVmId());
-            for (DiskImage dit : getVmTemplate().getDiskMap().values()) {
+            for (DiskImage dit : getImagesForMultipleStoragesDomainsCheck()) {
                 CreateSnapshotFromTemplateParameters tempVar = new CreateSnapshotFromTemplateParameters(
                         dit.getId(), getParameters().getVmStaticData().getId());
                 tempVar.setDestStorageDomainId(imageToDestinationDomainMap.get(dit.getId()));
