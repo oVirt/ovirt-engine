e63d001ea8d9b52264e88b6c011595c87e23012c
Gilad Chaplik <gchaplik@redhat.com>
2012-03-27 16:30:23 +0200

    webadmin: gluster- fix model provider bug
    the VM model provider was used instead of volume model provider.
    
    Change-Id: I16b87f6e316fef0c1cb6c3f123f4644b027b11ea

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/tab/MainTabVolumePresenter.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/tab/MainTabVolumePresenter.java
@@ -42,7 +42,7 @@ public class MainTabVolumePresenter extends AbstractMainTabWithDetailsPresenter<
     @TabInfo(container = MainTabPanelPresenter.class)
     static TabData getTabData(ClientGinjector ginjector) {
         return new ModelBoundTabData(ginjector.getApplicationConstants().volumeMainTabLabel(), 11,
-                ginjector.getMainTabVirtualMachineModelProvider());
+                ginjector.getMainTabVolumeModelProvider());
     }
 
     @Inject
