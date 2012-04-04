c7f9c40455964d0abb38bcfeccb7a0d8017439a8
Roy Golan <rgolan@redhat.com>
2012-04-04 11:06:13 +0300

    core: add network to serialization test
    Change-Id: I652971863865d5bf7554b335b611f9a9424b5596

--- backend/manager/modules/utils/src/test/java/org/ovirt/engine/core/utils/serialization/json/JsonObjectSerializerTest.java
+++ backend/manager/modules/utils/src/test/java/org/ovirt/engine/core/utils/serialization/json/JsonObjectSerializerTest.java
@@ -1,8 +1,11 @@
 package org.ovirt.engine.core.utils.serialization.json;
 
 import static org.junit.Assert.assertEquals;
+import junit.framework.Assert;
 
 import org.junit.Test;
+import org.ovirt.engine.core.common.businessentities.VdsNetworkInterface;
+import org.ovirt.engine.core.common.businessentities.network;
 
 
 /**
@@ -16,5 +19,19 @@ public class JsonObjectSerializerTest {
 
         assertEquals(serializablePojo.toJsonForm(true),
                 new JsonObjectSerializer().serialize(serializablePojo).replaceAll("\\s", ""));
+    }
+
+    @Test
+    public void serializeNetwork() {
+        network net = new network();
+        JsonObjectSerializer serialezer = new JsonObjectSerializer();
+        Assert.assertTrue(serialezer.serialize(net).length() > 0);
+    }
+
+    @Test
+    public void serializeVdsNetworkInterface() {
+        VdsNetworkInterface nic = new VdsNetworkInterface();
+        JsonObjectSerializer serialezer = new JsonObjectSerializer();
+        Assert.assertTrue(serialezer.serialize(nic).length() > 0);
     }
 }
