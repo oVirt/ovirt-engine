7fa49b5e3b497ba06cee71950d1bb2288d6b11c0
Mike Kolesnik <mkolesni@redhat.com>
2012-04-11 16:54:00 +0300

    core: Add new business entity: Disk
    In contrast to BaseDisk, this entity will also hold the storage specific
    implementation details, and is in fact a type of BaseDisk. The storage
    specific types will inherit from this type and extend it with storage
    specific data.
    
    Clients should update their code to use this class instead of DiskImage
    directly, to support other possible storage types.
    
    Change-Id: I8abd008b699c3b8e5e1222979708dacb686fce69

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/Disk.java
@@ -0,0 +1,83 @@
+package org.ovirt.engine.core.common.businessentities;
+
+import org.ovirt.engine.core.compat.Guid;
+
+/**
+ * The disk is contains data from the {@link BaseDisk} and the storage specific details for the disk, which are
+ * determined by {@link Disk#getDiskStorageType()}.<br>
+ * The disk may be attached to a VM or Template, which is indicated by {@link Disk#getVmEntityType()}. If it is null,
+ * then the disk is detached (floating).<br>
+ * <br>
+ * <b>Preferably, use this entity as the base reference wherever you don't need to hold a reference of a specific
+ * storage implementation.</b>
+ */
+public abstract class Disk extends BaseDisk {
+
+    private static final long serialVersionUID = 1380107681294904254L;
+
+    /**
+     * The VM Type is indicated by this field, or <code>null</code> if it is detached.
+     */
+    private VmEntityType vmEntityType;
+
+    public Disk() {
+    }
+
+    public Disk(Guid id,
+            int internalDriveMapping,
+            DiskInterface diskInterface,
+            boolean wipeAfterDelete,
+            PropagateErrors propagateErrors,
+            VmEntityType vmEntityType) {
+        super(id, internalDriveMapping, diskInterface, wipeAfterDelete, propagateErrors);
+        this.vmEntityType = vmEntityType;
+    }
+
+    /**
+     * @return The type of underlying storage implementation.
+     */
+    public abstract DiskStorageType getDiskStorageType();
+
+    public VmEntityType getVmEntityType() {
+        return vmEntityType;
+    }
+
+    public void setVmEntityType(VmEntityType vmEntityType) {
+        this.vmEntityType = vmEntityType;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((vmEntityType == null) ? 0 : vmEntityType.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (!(obj instanceof Disk)) {
+            return false;
+        }
+        Disk other = (Disk) obj;
+        if (vmEntityType != other.vmEntityType) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Enum of the disk's type, which defines which underlying storage details will be contained in the {@link Disk}
+     * object instance.
+     */
+    public enum DiskStorageType {
+        IMAGE,
+        LUN
+    }
+}
