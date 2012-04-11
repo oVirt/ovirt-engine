63e64764d83b2d218c53471c4a1738eed4995836
Allon Mureinik <amureini@redhat.com>
2012-04-11 16:27:51 +0300

    core: flag RefObject as @Deprecated
    It's a consensus in the community this class is an anti-pattern, and
    should not be used. This patch adds a @Deprecated annotation to reflect
    that.
    
    Change-Id: I217e34a21728aeb0e4c7d6657886707e3bca877f

--- backend/manager/modules/compat/src/main/java/org/ovirt/engine/core/compat/RefObject.java
+++ backend/manager/modules/compat/src/main/java/org/ovirt/engine/core/compat/RefObject.java
@@ -1,13 +1,14 @@
-//----------------------------------------------------------------------------------------
-//      Copyright (c) 2006 - 2009 Tangible Software Solutions Inc.
-//      This class can be used by anyone provided that the copyright notice remains intact.
+// ----------------------------------------------------------------------------------------
+// Copyright (c) 2006 - 2009 Tangible Software Solutions Inc.
+// This class can be used by anyone provided that the copyright notice remains intact.
 //
-//      This class is used to simulate the ability to pass arguments by reference in Java.
-//----------------------------------------------------------------------------------------
+// This class is used to simulate the ability to pass arguments by reference in Java.
+// ----------------------------------------------------------------------------------------
 package org.ovirt.engine.core.compat;
 
 import java.io.Serializable;
 
+@Deprecated
 public final class RefObject<T> implements Serializable {
     private static final long serialVersionUID = -6587265469544116899L;
 
