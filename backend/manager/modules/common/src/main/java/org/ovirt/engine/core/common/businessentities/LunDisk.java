68162b9fd7f48ce3ce16194edaf42e813d61c66c
Mike Kolesnik <mkolesni@redhat.com>
2012-04-12 09:54:09 +0300

    core: Added LunDisk business entity
    This business entity represents a Disk that is based directly on a LUN.
    
    Change-Id: I12d99785d2eee27123b645d2460a5cf65c4355f8

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/LunDisk.java
@@ -0,0 +1,73 @@
+package org.ovirt.engine.core.common.businessentities;
+
+import org.ovirt.engine.core.compat.Guid;
+
+/**
+ * A type of disk that is stored directly on a LUN ({@link LUNs}). This disk will contain the LUN details.
+ */
+public class LunDisk extends Disk {
+
+    private static final long serialVersionUID = -5177863078960026966L;
+
+    /**
+     * The LUN details.
+     */
+    private LUNs lun;
+
+    public LunDisk() {
+    }
+
+    public LunDisk(Guid id,
+            int internalDriveMapping,
+            DiskInterface diskInterface,
+            boolean wipeAfterDelete,
+            PropagateErrors propagateErrors,
+            VmEntityType vmEntityType,
+            LUNs lun) {
+        super(id, internalDriveMapping, diskInterface, wipeAfterDelete, propagateErrors, vmEntityType);
+        this.lun = lun;
+    }
+
+    @Override
+    public DiskStorageType getDiskStorageType() {
+        return DiskStorageType.LUN;
+    }
+
+    public LUNs getLun() {
+        return lun;
+    }
+
+    public void setLun(LUNs lun) {
+        this.lun = lun;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((lun == null) ? 0 : lun.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (!(obj instanceof LunDisk)) {
+            return false;
+        }
+        LunDisk other = (LunDisk) obj;
+        if (lun == null) {
+            if (other.lun != null) {
+                return false;
+            }
+        } else if (!lun.equals(other.lun)) {
+            return false;
+        }
+        return true;
+    }
+}
