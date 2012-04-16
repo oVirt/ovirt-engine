95069d5045269eb28b7f4337e0db176477b8af33
Allon Mureinik <amureini@redhat.com>
2012-04-16 18:40:39 +0300

    core: Test GetTemplateInterfacesByTemplateIdQuery
    Added a test case for GetTemplateInterfacesByTemplateIdQueryTest.
    
    Revision note:
    This patch originally also contained some added functionallity for the
    query which was eventaully merged in I3d63141b.
    This patch was left only with the test, which seemed to be a shame to
    abandon if it was already written.
    
    Change-Id: Ie10aa0c9f499ddeaa895729c0285932f362d4f58

---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetTemplateInterfacesByTemplateIdQueryTest.java
@@ -0,0 +1,40 @@
+package org.ovirt.engine.core.bll;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+import org.ovirt.engine.core.common.businessentities.VmNetworkInterface;
+import org.ovirt.engine.core.common.queries.GetVmTemplateParameters;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.VmNetworkInterfaceDAO;
+
+/**
+ * A test case for {@link GetTemplateInterfacesByTemplateIdQuery}.
+ * It does not test database implementation, but rather tests that the right delegations to the DAO occur.
+ */
+public class GetTemplateInterfacesByTemplateIdQueryTest extends AbstractUserQueryTest<GetVmTemplateParameters, GetTemplateInterfacesByTemplateIdQuery<GetVmTemplateParameters>> {
+    @Test
+    public void testExecuteQuery() {
+        Guid templateID = Guid.NewGuid();
+        List<VmNetworkInterface> expectedResult = Collections.singletonList(new VmNetworkInterface());
+
+        GetVmTemplateParameters paramsMock = getQueryParameters();
+        when(paramsMock.getId()).thenReturn(templateID);
+
+        VmNetworkInterfaceDAO vmNetworkInterfaceDAOMock = mock(VmNetworkInterfaceDAO.class);
+        when(vmNetworkInterfaceDAOMock.getAllForTemplate(templateID, getUser().getUserId(), paramsMock.isFiltered())).thenReturn(expectedResult);
+        when(getDbFacadeMockInstance().getVmNetworkInterfaceDAO()).thenReturn(vmNetworkInterfaceDAOMock);
+
+        getQuery().executeQueryCommand();
+
+        @SuppressWarnings("unchecked")
+        List<VmNetworkInterface> result = (List<VmNetworkInterface>) getQuery().getQueryReturnValue().getReturnValue();
+
+        assertEquals("Wrong interfaces returned", expectedResult, result);
+    }
+}
