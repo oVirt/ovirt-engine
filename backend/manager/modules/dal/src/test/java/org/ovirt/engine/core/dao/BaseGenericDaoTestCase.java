3d56afe14bab9308028abb55d81154761a4cfb0f
Mike Kolesnik <mkolesni@redhat.com>
2012-04-18 19:13:57 +0300

    core: Extract BaseReadDaoTestCase class
    This class is useful to write tests for ReadDao implementations, so
    extracting for future use.
    
    Change-Id: Iea31b8870b41254859e3eaec5e145d33a064fdde

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/BaseGenericDaoTestCase.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/BaseGenericDaoTestCase.java
@@ -1,73 +1,19 @@
 package org.ovirt.engine.core.dao;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.io.Serializable;
-import java.util.List;
 
 import org.junit.Test;
 import org.ovirt.engine.core.common.businessentities.BusinessEntity;
 
-public abstract class BaseGenericDaoTestCase<ID extends Serializable, T extends BusinessEntity<ID>, D extends GenericDao<T, ID>> extends BaseDAOTestCase {
-    protected D dao;
-
-    protected T existingEntity;
-
+public abstract class BaseGenericDaoTestCase<ID extends Serializable, T extends BusinessEntity<ID>,
+D extends GenericDao<T, ID>> extends BaseReadDaoTestCase<ID, T, D> {
     public BaseGenericDaoTestCase() {
         super();
     }
-
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
-        dao = prepareDao();
-
-        existingEntity = dao.get(getExistingEntityId());
-    }
-
-    abstract protected ID getExistingEntityId();
-
-    abstract protected D prepareDao();
-
-    /**
-     * Ensures that fetching a disk image with an invalid id fails.
-     */
-    @Test
-    public void testGetByIdWithInvalidId() {
-        T result = dao.get(generateNonExistingId());
-
-        assertNull(result);
-    }
-
-    abstract protected ID generateNonExistingId();
-
-    /**
-     * Ensures that retrieving a disk image by name works as expected.
-     */
-    @Test
-    public void testGet() {
-        T result = dao.get(existingEntity.getId());
-
-        assertNotNull(result);
-        assertEquals(existingEntity, result);
-    }
-
-    /**
-     * Ensures that retrieving all disk images works.
-     */
-    @Test
-    public void testGetAll() {
-        List<T> result = dao.getAll();
-
-        assertFalse(result.isEmpty());
-        assertEquals(getEneitiesTotalCount(), result.size());
-    }
-
-    abstract protected int getEneitiesTotalCount();
 
     /**
      * Ensures that saving a disk image works as expected.
---
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/BaseReadDaoTestCase.java
@@ -0,0 +1,73 @@
+package org.ovirt.engine.core.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.junit.Test;
+import org.ovirt.engine.core.common.businessentities.BusinessEntity;
+
+public abstract class BaseReadDaoTestCase<ID extends Serializable, T extends BusinessEntity<ID>,
+D extends ReadDao<T, ID>> extends BaseDAOTestCase {
+
+    protected D dao;
+    protected T existingEntity;
+
+    public BaseReadDaoTestCase() {
+        super();
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        dao = prepareDao();
+
+        existingEntity = dao.get(getExistingEntityId());
+    }
+
+    protected abstract ID getExistingEntityId();
+
+    protected abstract D prepareDao();
+
+    /**
+     * Ensures that fetching an entity with an invalid id fails.
+     */
+    @Test
+    public void testGetByIdWithInvalidId() {
+        T result = dao.get(generateNonExistingId());
+
+        assertNull(result);
+    }
+
+    protected abstract ID generateNonExistingId();
+
+    /**
+     * Ensures that retrieving an entity by id works as expected.
+     */
+    @Test
+    public void testGet() {
+        T result = dao.get(existingEntity.getId());
+
+        assertNotNull(result);
+        assertEquals(existingEntity, result);
+    }
+
+    /**
+     * Ensures that retrieving all entities works.
+     */
+    @Test
+    public void testGetAll() {
+        List<T> result = dao.getAll();
+
+        assertFalse(result.isEmpty());
+        assertEquals(getEneitiesTotalCount(), result.size());
+    }
+
+    protected abstract int getEneitiesTotalCount();
+
+}
