b218e0ae9b43cb27bb9b0bb5970722e83a45d119
Eli Mesika <emesika@redhat.com>
2012-04-18 19:13:07 +0300

    core: remove 03010250 version from schema_version
    Adding a custom.sql script under the upgarde/pre_upgrade directory
    The purpose of this file is to fix bugs
    
    Currently we have a bug that upgarde 03010250 was cherry-picked and installed after 03010130
    This caused all scripts between 03010140 and 03010240 not to run.
    This scripts removes the 03010250 from schema_version and updates 03010130 to be the curent version
    So, after that , all scripts from 03010140 and up will run
    Since 03010250 was a configuration change that is already handled in the new config.sql, no other handling is needed.
    
    Change-Id: If0e9b546ca10fb51f0b7e5a911baa0907281cae1

---
+++ backend/manager/dbscripts/upgrade/pre_upgrade/custom.sql
@@ -0,0 +1,13 @@
+/**********************************************************************************************
+   Currently we have a bug that upgarde 03010250 was cherry-picked and installed after 03010130
+   This caused all scripts between 03010140 and 03010240 not to run.
+   This scripts removes the 03010250 from schema_version and updates 03010130 to be the curent version
+   So, after that , all scripts from 03010140 and up will run
+   Since 03010250 was a configuration change that is already handled in the new config.sql, no other handling is needed.
+**********************************************************************************************/
+
+update schema_version set current = true where version = '03010130'
+and exists(select 1 from schema_version where version = '03010250' and current = true);
+
+delete from schema_version
+where version = '03010250' and current = true;
