faa34fb284f022b16788d42ea6d826f3857e4742
Michael Kublin <mkublin@redhat.com>
2012-04-18 20:56:11 +0300

    restapi : Fixing broken unit test
    The following unit tests were brokenn due change at import scenario.
    From now storage domain is not mandatory parameter at both scenarious
    
    Change-Id: I510ad6a18e55b72905818dcbc39e8a754e2193f6

--- backend/manager/modules/restapi/jaxrs/src/test/java/org/ovirt/engine/api/restapi/resource/BackendStorageDomainVmResourceTest.java
+++ backend/manager/modules/restapi/jaxrs/src/test/java/org/ovirt/engine/api/restapi/resource/BackendStorageDomainVmResourceTest.java
@@ -228,7 +228,7 @@ public class BackendStorageDomainVmResourceTest
             resource.doImport(new Action());
             fail("expected WebApplicationException on incomplete parameters");
         } catch (WebApplicationException wae) {
-            verifyIncompleteException(wae, "Action", "doImport", "cluster.id|name", "storageDomain.id|name");
+            verifyIncompleteException(wae, "Action", "doImport", "cluster.id|name");
         }
     }
 
