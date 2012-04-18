78b594230a6214de35cced635bf4c8629838aa98
Eli Mesika <emesika@redhat.com>
2012-04-18 18:15:42 +0300

    core: remove unused config generator script
    Remove old fill_config.sql generator script.
    
    Change-Id: I997fd77ee92d8bb03d16598b2957150444dd15e4

--- backend/manager/dbscripts/GenFieldsInVDCConfigSQL.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-javac FieldsInVDCConfigXmlToSQL.java
-java -cp /usr/share/java/postgresql-jdbc.jar:. FieldsInVDCConfigXmlToSQL ../dbscripts/FieldsInVDCConfig.xml >fill_config.sql
-rm -rf FieldsInVDCConfigXmlToSQL*class*
-
