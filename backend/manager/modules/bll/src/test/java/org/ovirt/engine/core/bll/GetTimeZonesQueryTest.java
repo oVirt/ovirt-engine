a4c028a2b8a4bba052034a20c7bd1d9281db9b8f
Allon Mureinik <amureini@redhat.com>
2012-04-24 16:12:50 +0300

    core: Extracted AbstractSysprepQueryTest
    Extracted a base class to handle common mocking for tests of queries
    based on Sysprep.
    
    Change-Id: I1be5516ceda522982d8923b3584d9d4bf8e8a61b

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetTimeZonesQueryTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetTimeZonesQueryTest.java
@@ -1,30 +1,17 @@
 package org.ovirt.engine.core.bll;
 
 import static org.junit.Assert.assertEquals;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.when;
 
 import java.util.Map;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.ovirt.engine.core.common.config.Config;
-import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 import org.ovirt.engine.core.vdsbroker.vdsbroker.SysprepHandler;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ Config.class })
-public class GetTimeZonesQueryTest extends AbstractUserQueryTest<VdcQueryParametersBase, GetTimeZonesQuery<VdcQueryParametersBase>> {
+public class GetTimeZonesQueryTest extends AbstractSysprepQueryTest<VdcQueryParametersBase, GetTimeZonesQuery<VdcQueryParametersBase>> {
 
     @Test
     public void testExecuteQuery() {
-        // Mock away the static initializer
-        mockStatic(Config.class);
-        when(Config.GetValue(ConfigValues.AdUserName)).thenReturn("");
-
         getQuery().executeQueryCommand();
 
         @SuppressWarnings("unchecked")
