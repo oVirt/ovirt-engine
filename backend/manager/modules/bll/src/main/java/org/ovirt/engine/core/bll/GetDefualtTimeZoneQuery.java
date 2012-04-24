00510a248e61081d1640f0b78745ca154f6493cb
Allon Mureinik <amureini@redhat.com>
2012-04-24 16:00:33 +0300

    core: Clean up imports on GetDefaultTimeZoneQuery
    Replaced * imports with FQCNs.
    
    Change-Id: I23ab2fe33408ef7dc05ef4048f7c1418d3e4ef3e

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetDefualtTimeZoneQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetDefualtTimeZoneQuery.java
@@ -1,9 +1,9 @@
 package org.ovirt.engine.core.bll;
 
-import org.ovirt.engine.core.compat.*;
 import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
-import org.ovirt.engine.core.common.queries.*;
+import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.compat.KeyValuePairCompat;
 import org.ovirt.engine.core.vdsbroker.vdsbroker.SysprepHandler;
 
 public class GetDefualtTimeZoneQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
