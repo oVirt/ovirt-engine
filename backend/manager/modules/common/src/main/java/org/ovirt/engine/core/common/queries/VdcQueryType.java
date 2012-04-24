1eeaba025abc8ba4aa7a1044acc7777b040516cc
Allon Mureinik <amureini@redhat.com>
2012-04-24 16:29:17 +0300

    core: Made GetCACertificateQuery a user query
    The query was flagged as user query so it could be used in the User
    Portal. No actual filtering is needed, since this it returns a static
    value that should be available to all the users in the system.
    
    Change-Id: Ia0f9bcc4c54af17f0157f43dd29f55e256dfa5ae

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/VdcQueryType.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/VdcQueryType.java
@@ -62,7 +62,7 @@ public enum VdcQueryType implements Serializable {
     GetVdsGroupsByStoragePoolId,
 
     // Certificate
-    GetCACertificate,
+    GetCACertificate(VdcQueryAuthType.User),
 
     // VM Templates queries
     IsVmTemlateWithSameNameExist,
