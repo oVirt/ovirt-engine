f5eece8cc5c03aacc661d97213b52993d25bcd36
Laszlo Hornyak <lhornyak@redhat.com>
2012-04-26 09:28:35 +0200

    core: [rmd] CreateComputerAccountCommand
    removed refobject
    
    Change-Id: I81e055ae7f798b63dd2c98722c24a0e4fc4c3d7f

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateComputerAccountCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateComputerAccountCommand.java
@@ -11,13 +11,12 @@ import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.PermissionSubject;
 import org.ovirt.engine.core.common.action.CreateComputerAccountParameters;
 import org.ovirt.engine.core.common.businessentities.VmStatic;
-import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.compat.RefObject;
 import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.core.dal.VdcBllMessages;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 // Wrong domain handling in IsComputerWithTheSameNameExists ad command
+@SuppressWarnings("serial")
 public class CreateComputerAccountCommand<T extends CreateComputerAccountParameters> extends CommandBase<T> {
     public CreateComputerAccountCommand(T parameters) {
         super(parameters);
@@ -41,25 +40,18 @@ public class CreateComputerAccountCommand<T extends CreateComputerAccountParamet
     @Override
     protected boolean canDoAction() {
         boolean returnValue = true;
-        String user = null;
-        String password = null;
-        String domain = null;
-        Guid vmId = getParameters().getVmId();
-        RefObject<String> tempRefObject = new RefObject<String>(user);
-        RefObject<String> tempRefObject2 = new RefObject<String>(password);
-        RefObject<String> tempRefObject3 = new RefObject<String>(domain);
-        GetActualCredentials(tempRefObject, tempRefObject2, tempRefObject3);
-        user = tempRefObject.argvalue;
-        password = tempRefObject2.argvalue;
-        domain = tempRefObject3.argvalue;
-        VmStatic vmStatic = DbFacade.getInstance().getVmStaticDAO().get(vmId);
+        final VmStatic vmStatic = DbFacade.getInstance().getVmStaticDAO().get(getParameters().getVmId());
         if (vmStatic != null) {
             setComputerName(vmStatic.getvm_name());
-            returnValue = ((Boolean) LdapFactory
-                    .getInstance(getParameters().getDomain())
-                    .RunAdAction(AdActionType.IsComputerWithTheSameNameExists,
-                            new LdapIsComputerWithSameNameExistsParameters(domain, user, password, getComputerName()))
-                    .getReturnValue()).booleanValue();
+            returnValue =
+                    ((Boolean) LdapFactory
+                            .getInstance(getParameters().getDomain())
+                            .RunAdAction(AdActionType.IsComputerWithTheSameNameExists,
+                                    new LdapIsComputerWithSameNameExistsParameters(getActualDomain(),
+                                            getActualUser(),
+                                            getActualPassword(),
+                                            getComputerName()))
+                            .getReturnValue()).booleanValue();
             if (returnValue) {
                 addCanDoActionMessage(VdcBllMessages.DIRECTORY_COMPUTER_WITH_THE_SAME_NAME_ALREADY_EXITS);
             }
@@ -74,44 +66,31 @@ public class CreateComputerAccountCommand<T extends CreateComputerAccountParamet
 
     @Override
     protected void executeCommand() {
-        String user = null;
-        String password = null;
-        String domain = null;
-        RefObject<String> tempRefObject = new RefObject<String>(user);
-        RefObject<String> tempRefObject2 = new RefObject<String>(password);
-        RefObject<String> tempRefObject3 = new RefObject<String>(domain);
-        GetActualCredentials(tempRefObject, tempRefObject2, tempRefObject3);
-        user = tempRefObject.argvalue;
-        password = tempRefObject2.argvalue;
-        domain = tempRefObject3.argvalue;
         setSucceeded(LdapFactory
                 .getInstance(getParameters().getDomain())
                 .RunAdAction(
                         AdActionType.CreateComputerAccount,
-                        new LdapCreateComputerAccountParameters(domain, user, password, getParameters().getPath(),
-                                getComputerName())).getSucceeded());
+                        new LdapCreateComputerAccountParameters(getActualDomain(),
+                                getActualUser(),
+                                getActualPassword(),
+                                getParameters().getPath(),
+                                getComputerName()))
+                .getSucceeded());
     }
 
-    /**
-     * Gets the actual credentials.
-     *
-     * @param user
-     *            The user.
-     * @param password
-     *            The password.
-     * @param domain
-     *            The domain.
-     */
-    private void GetActualCredentials(RefObject<String> user, RefObject<String> password, RefObject<String> domain) {
-        user.argvalue = StringHelper.isNullOrEmpty(getParameters().getUserName()) ? getCurrentUser().getUserName()
+    String getActualUser() {
+        return StringHelper.isNullOrEmpty(getParameters().getUserName()) ? getCurrentUser().getUserName()
                 : getParameters().getUserName();
+    }
 
-        password.argvalue = StringHelper.isNullOrEmpty(getParameters().getUserPassword()) ? getCurrentUser()
+    String getActualPassword() {
+        return StringHelper.isNullOrEmpty(getParameters().getUserPassword()) ? getCurrentUser()
                 .getPassword() : getParameters().getUserPassword();
+    }
 
-        domain.argvalue = StringHelper.isNullOrEmpty(getParameters().getDomain()) ? getCurrentUser()
+    String getActualDomain() {
+        return StringHelper.isNullOrEmpty(getParameters().getDomain()) ? getCurrentUser()
                 .getDomainControler() : getParameters().getDomain();
-
     }
 
     @Override
