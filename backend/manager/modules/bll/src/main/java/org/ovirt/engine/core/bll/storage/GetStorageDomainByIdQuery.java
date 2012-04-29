2a8602bda85b36c54e09ba38cc888439c1c16cf0
Allon Mureinik <amureini@redhat.com>
2012-04-29 17:47:15 +0300

    core: Clean GetStorageDomainByIdQuery
    Used the new getDbFacade() method.
    
    Change-Id: I6e00aa5948fc20ab35850f24954047439bda3324

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/GetStorageDomainByIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/GetStorageDomainByIdQuery.java
@@ -2,7 +2,6 @@ package org.ovirt.engine.core.bll.storage;
 
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.StorageDomainQueryParametersBase;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class GetStorageDomainByIdQuery<P extends StorageDomainQueryParametersBase> extends QueriesCommandBase<P> {
     public GetStorageDomainByIdQuery(P parameters) {
@@ -12,7 +11,7 @@ public class GetStorageDomainByIdQuery<P extends StorageDomainQueryParametersBas
     @Override
     protected void executeQueryCommand() {
         getQueryReturnValue().setReturnValue(
-                DbFacade.getInstance().getStorageDomainDAO().get(getParameters().getStorageDomainId(),
+                getDbFacade().getStorageDomainDAO().get(getParameters().getStorageDomainId(),
                         getUserID(),
                         getParameters().isFiltered()));
     }
