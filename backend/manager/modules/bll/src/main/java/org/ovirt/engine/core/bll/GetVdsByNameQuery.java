70e0d005b84984f1d32b1f7a06183c7ac984ef6d
Allon Mureinik <amureini@redhat.com>
2012-04-29 13:42:50 +0300

    core: Cleanup GetVdsByNameQuery
    Used the new getDbFacade() method.
    
    Change-Id: I42387cf241bd2008eae915a650dde2e6447a70bd

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVdsByNameQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVdsByNameQuery.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.queries.GetVdsByNameParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class GetVdsByNameQuery<P extends GetVdsByNameParameters> extends QueriesCommandBase<P> {
     public GetVdsByNameQuery(P parameters) {
@@ -13,7 +12,7 @@ public class GetVdsByNameQuery<P extends GetVdsByNameParameters> extends Queries
 
     @Override
     protected void executeQueryCommand() {
-        List<VDS> vds = DbFacade.getInstance().getVdsDAO().getAllWithName(getParameters().getName());
+        List<VDS> vds = getDbFacade().getVdsDAO().getAllWithName(getParameters().getName());
 
         if (vds.size() > 0) {
             getQueryReturnValue().setReturnValue(vds.get(0));
