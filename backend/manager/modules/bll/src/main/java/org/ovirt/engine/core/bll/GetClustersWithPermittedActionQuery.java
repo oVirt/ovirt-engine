2165df92cf3868035ab6f70f8726fe1c36f866ab
Allon Mureinik <amureini@redhat.com>
2012-04-29 17:17:40 +0300

    core: cleanup GetClustersWithPermittedActionQuery
    Used the new getDbFacade() method.
    
    Change-Id: Ia6914337ade87a489c8a6334b306134c43705898

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetClustersWithPermittedActionQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetClustersWithPermittedActionQuery.java
@@ -1,7 +1,6 @@
 package org.ovirt.engine.core.bll;
 
 import org.ovirt.engine.core.common.queries.GetEntitiesWithPermittedActionParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class GetClustersWithPermittedActionQuery<P extends GetEntitiesWithPermittedActionParameters>
         extends QueriesCommandBase<P> {
@@ -13,7 +12,7 @@ public class GetClustersWithPermittedActionQuery<P extends GetEntitiesWithPermit
     @Override
     protected void executeQueryCommand() {
         P params = getParameters();
-        setReturnValue(DbFacade.getInstance().getVdsGroupDAO().
-                getClustersWithPermittedAction(getUserID(), params.getActionGroup()));
+        setReturnValue(getDbFacade().getVdsGroupDAO().getClustersWithPermittedAction(getUserID(),
+                params.getActionGroup()));
     }
 }
