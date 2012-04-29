705f5bbfa0bec6ab41410bd7e0802c77a2fce0b2
Allon Mureinik <amureini@redhat.com>
2012-04-29 17:35:41 +0300

    core: Cleanup GetTemplatesRelatedToQuotaIdQuery
    Used the new getDbFacade() method.
    
    Change-Id: Id0835a0d4a819ae526fa101c45b3529d4795718b

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetTemplatesRelatedToQuotaIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetTemplatesRelatedToQuotaIdQuery.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.queries.GetEntitiesRelatedToQuotaIdParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class GetTemplatesRelatedToQuotaIdQuery<P extends GetEntitiesRelatedToQuotaIdParameters>
         extends QueriesCommandBase<P> {
@@ -14,8 +13,8 @@ public class GetTemplatesRelatedToQuotaIdQuery<P extends GetEntitiesRelatedToQuo
 
     @Override
     protected void executeQueryCommand() {
-        List<VmTemplate> vmTemplates = DbFacade.getInstance().getVmTemplateDAO().getAllTemplatesRelatedToQuotaId(
-                getParameters().getQuotaId());
+        List<VmTemplate> vmTemplates =
+                getDbFacade().getVmTemplateDAO().getAllTemplatesRelatedToQuotaId(getParameters().getQuotaId());
         for (VmTemplate vmTemplate : vmTemplates) {
             VmTemplateHandler.UpdateDisksFromDb(vmTemplate);
         }
