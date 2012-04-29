9a6a0c04b13d9dcde74765afb1216ece4052d62e
Allon Mureinik <amureini@redhat.com>
2012-04-29 13:49:35 +0300

    core: Cleanup GetQuotaStorageByQuotaIdQuery
    Used the new getDbFacade() method.
    
    Change-Id: Ie3abccf3cb9e6006da656346206f1e2e27134a76

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetQuotaStorageByQuotaIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetQuotaStorageByQuotaIdQuery.java
@@ -1,7 +1,6 @@
 package org.ovirt.engine.core.bll;
 
 import org.ovirt.engine.core.common.queries.GetQuotaStorageByQuotaIdQueryParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class GetQuotaStorageByQuotaIdQuery<P extends GetQuotaStorageByQuotaIdQueryParameters> extends QueriesCommandBase<P> {
     public GetQuotaStorageByQuotaIdQuery(P parameters) {
@@ -10,9 +9,7 @@ public class GetQuotaStorageByQuotaIdQuery<P extends GetQuotaStorageByQuotaIdQue
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(DbFacade.getInstance()
-                .getQuotaDAO()
-                .getQuotaStorageByQuotaGuidWithGeneralDefault(getParameters().getQuotaId()));
+        getQueryReturnValue().setReturnValue(
+                getDbFacade().getQuotaDAO().getQuotaStorageByQuotaGuidWithGeneralDefault(getParameters().getQuotaId()));
     }
-
 }
