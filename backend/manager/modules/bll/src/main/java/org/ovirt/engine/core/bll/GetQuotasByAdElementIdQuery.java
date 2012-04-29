b2c71e2b23ca6b1aaee038362604314c48e944ff
Allon Mureinik <amureini@redhat.com>
2012-04-29 17:28:29 +0300

    core: cleanup GetQuotasByAdElementIdQuery
    Used the new getDbFacade() method.
    
    Change-Id: Iabb1f52b7dc5b9e04aa7d9de15b815dfa51aed28

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetQuotasByAdElementIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetQuotasByAdElementIdQuery.java
@@ -1,7 +1,6 @@
 package org.ovirt.engine.core.bll;
 
 import org.ovirt.engine.core.common.queries.GetQuotasByAdElementIdQueryParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class GetQuotasByAdElementIdQuery<P extends GetQuotasByAdElementIdQueryParameters> extends QueriesCommandBase<P> {
     public GetQuotasByAdElementIdQuery(P parameters) {
@@ -10,7 +9,7 @@ public class GetQuotasByAdElementIdQuery<P extends GetQuotasByAdElementIdQueryPa
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(DbFacade.getInstance()
+        getQueryReturnValue().setReturnValue(getDbFacade()
                 .getQuotaDAO()
                 .getQuotaByAdElementId(getParameters().getAdElementId(), getParameters().getStoragePoolId()));
     }
