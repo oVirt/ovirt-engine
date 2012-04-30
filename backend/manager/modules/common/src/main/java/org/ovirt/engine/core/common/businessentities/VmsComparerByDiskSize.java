c1cfdfbccec1d8614d3e6d3ad5173acd220168a6
Tal Nisan <tnisan@redhat.com>
2012-05-01 00:14:49 +0300

    core: Converter comments removal
    Signed-off-by: Tal Nisan <tnisan@redhat.com>
    Change-Id: Iaa9b7d8e6380d90ee2d232d262af154464ef6f12

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmsComparerByDiskSize.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmsComparerByDiskSize.java
@@ -2,7 +2,6 @@ package org.ovirt.engine.core.common.businessentities;
 
 import java.io.Serializable;
 
-//C# TO JAVA CONVERTER TODO TASK: The interface type was changed to the closest equivalent Java type, but the methods implemented will need adjustment:
 public class VmsComparerByDiskSize implements java.util.Comparator<VM>, Serializable {
     private static final long serialVersionUID = -3546735706731409532L;
 
