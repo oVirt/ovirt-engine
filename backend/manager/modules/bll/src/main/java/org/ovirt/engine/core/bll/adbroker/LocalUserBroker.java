85005cc3d34c3cbc464854d4e56d32d4a4754ea5
Juan Hernandez <juan.hernandez@redhat.com>
2012-04-30 20:06:50 +0200

    core: Remove dependency on JNA
    Remove the dependency on JNA and the classes that use it (mostly
    local user authentication) as they are not needed in Linux.
    
    Change-Id: I4f47d5b3cf35afee95ddd048b3ffbadf1144adcf
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/adbroker/LocalUserBroker.java
+++
@@ -1,8 +0,0 @@
-package org.ovirt.engine.core.bll.adbroker;
-
-public class LocalUserBroker extends LdapBrokerBase {
-    @Override
-    protected String getBrokerType() {
-        return "LU";
-    }
-}
