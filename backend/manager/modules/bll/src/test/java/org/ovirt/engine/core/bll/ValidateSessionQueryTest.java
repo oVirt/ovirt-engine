e634a569053394f329c8ecf1d6e5626a102a5083
Allon Mureinik <amureini@redhat.com>
2012-05-01 17:34:36 +0300

    core: Fix broken ValidateSessionQueryTest
    The test was written in an unstandard way, which caused it to break when
    some other patch was integrated.
    This patch fixes it.
    
    Change-Id: Ibea401a9833edadc13a3f6392a997370694042fc
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/ValidateSessionQueryTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/ValidateSessionQueryTest.java
@@ -2,40 +2,34 @@ package org.ovirt.engine.core.bll;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
-
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 import org.ovirt.engine.core.common.users.VdcUser;
 import org.ovirt.engine.core.compat.Guid;
 
-public class ValidateSessionQueryTest extends AbstractQueryTest<VdcQueryParametersBase, ValidateSessionQuery<VdcQueryParametersBase>> {
+public class ValidateSessionQueryTest extends AbstractUserQueryTest<VdcQueryParametersBase, ValidateSessionQuery<VdcQueryParametersBase>> {
 
     @Test
     public void testSuccessfulSessionId() {
-        VdcQueryParametersBase params = new VdcQueryParametersBase();;
-        params.setSessionId("good_session_id");
+        when(params.getSessionId()).thenReturn("good_session_id");
 
-        ValidateSessionQuery<VdcQueryParametersBase> query = spy(new ValidateSessionQuery<VdcQueryParametersBase>(params));
-        when(query.getSessionUser("good_session_id")).thenReturn(new VdcUser(Guid.NewGuid(), "myUser", "myDomain"));
-        query.Execute();
-        assertTrue(query.getQueryReturnValue().getSucceeded());
-        assertTrue(query.getQueryReturnValue().getReturnValue() instanceof VdcUser);
-        VdcUser user = (VdcUser) query.getQueryReturnValue().getReturnValue();
+        when(getQuery().getSessionUser("good_session_id")).thenReturn(new VdcUser(Guid.NewGuid(), "myUser", "myDomain"));
+        getQuery().Execute();
+        assertTrue(getQuery().getQueryReturnValue().getSucceeded());
+        assertTrue(getQuery().getQueryReturnValue().getReturnValue() instanceof VdcUser);
+        VdcUser user = (VdcUser) getQuery().getQueryReturnValue().getReturnValue();
         assertTrue(user.getDomainControler().equals("myDomain"));
         assertTrue(user.getUserName().equals("myUser"));
     }
 
     @Test
     public void testUnsuccessfulSessionId() {
-        VdcQueryParametersBase params = new VdcQueryParametersBase();
-        params.setSessionId("bad_session_id");
+        when(params.getSessionId()).thenReturn("bad_session_id");
 
-        ValidateSessionQuery<VdcQueryParametersBase> query = new ValidateSessionQuery<VdcQueryParametersBase>(params);
-        query.Execute();
-        assertFalse(query.getQueryReturnValue().getSucceeded());
+        getQuery().Execute();
+        assertFalse(getQuery().getQueryReturnValue().getSucceeded());
     }
 
 }
