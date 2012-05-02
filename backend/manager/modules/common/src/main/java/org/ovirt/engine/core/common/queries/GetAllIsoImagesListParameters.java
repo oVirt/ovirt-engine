5a900792d261f19edc3e325a49f0f6711d6f17ae
Allon Mureinik <amureini@redhat.com>
2012-05-02 20:34:02 +0300

    core: Cleanup GetAllIsoImagesListParameters
    Replaced * import with FQCN import.
    
    Change-Id: I1e33d02a8b7ae362998dd2a8b46d55c7761f4b98
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetAllIsoImagesListParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetAllIsoImagesListParameters.java
@@ -1,6 +1,6 @@
 package org.ovirt.engine.core.common.queries;
 
-import org.ovirt.engine.core.compat.*;
+import org.ovirt.engine.core.compat.Guid;
 
 public class GetAllIsoImagesListParameters extends VdcQueryParametersBase {
     private static final long serialVersionUID = 6098440434536241071L;
