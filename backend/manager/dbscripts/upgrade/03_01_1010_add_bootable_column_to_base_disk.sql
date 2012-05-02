bf0f55c6c73577971c0b3b68baa8caf5d745e8fc
Eli Mesika <emesika@redhat.com>
2012-05-02 18:13:32 +0300

    core: fixing 03_01_1010_add_bootable_column_to_base_disk upgrade
    Rewrite teh script to update the boot column properly from images.
    
    Change-Id: I2890db572eccc69bc8b677cc406d51456103ff1a
    Signed-off-by: Eli Mesika <emesika@redhat.com>

--- backend/manager/dbscripts/upgrade/03_01_1010_add_bootable_column_to_base_disk.sql
+++ backend/manager/dbscripts/upgrade/03_01_1010_add_bootable_column_to_base_disk.sql
@@ -1,18 +1,6 @@
 select fn_db_add_column('base_disks', 'boot', 'boolean NOT NULL DEFAULT false');
 
-CREATE OR REPLACE FUNCTION base_disks_upgrade_boolean_03_01_1010()
-RETURNS void
-AS $function$
-BEGIN
-    INSERT INTO base_disks (boot)
-    SELECT boot from images
-    WHERE images.image_group_id = base_disks.id;
-END; $function$
-LANGUAGE plpgsql;
-
-select base_disks_upgrade_boolean_03_01_1010();
-
-DROP FUNCTION base_disks_upgrade_boolean_03_01_1010();
+update base_disks set boot = images.boot from images where images.image_group_id = disk_id;
 
 select fn_db_drop_column('images','boot');
 
