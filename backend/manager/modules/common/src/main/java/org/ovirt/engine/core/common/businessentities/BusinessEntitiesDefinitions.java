oVirt/ovirt-engine

commit 880077dea59c9ba4da0e6a8cef8497944aab8588

backend: I18N support (#786448)
backend part of:
https://bugzilla.redhat.com/786448

Fields which support UTF:
- Tag Name
- Bookmark Name
- Bookmar Search String
- Cluster Name

Fields which do NOT support UTF (e.g. enables only ASCII)

- Data Center Name/Description
- Host Name
- Host Address
- VM Name/Template Name
- VM Name/Template Description
- VM Pool Name
- NFS Storage Export Path
- Storage Domain Name
- Snapshot Description
- Disk Alias/Description

Change-Id: Ia5438e0e79f183b900d4bc61c809fa2680ca3c81
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/BusinessEntitiesDefinitions.java
@@ -11,6 +11,7 @@ public final class BusinessEntitiesDefinitions {
 
     // VM (vm_statis)
     public static final int VM_NAME_SIZE = 255;
+    public static final int VM_DESCRIPTION_SIZE = 255;
 
     // VM Pools (vm_pools)
     public static final int VM_POOL_NAME_SIZE = 255;
@@ -52,7 +53,7 @@ public final class BusinessEntitiesDefinitions {
     public static final int ROLE_NAME_SIZE = 126;
 
     // Tags (tags)
-    public static final int TAG_NAME_SIZE = 50;
+    public static final int TAG_NAME_SIZE = 40;
 
     // Quota
     public static final int QUOTA_NAME_SIZE = 65;

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/AddStorageServerConnectionCommand.java
@@ -9,6 +9,8 @@ import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.VDSStatus;
 import org.ovirt.engine.core.common.businessentities.storage_pool;
 import org.ovirt.engine.core.common.businessentities.storage_server_connections;
+import org.ovirt.engine.core.common.validation.LinuxMountPointConstraint;
+import org.ovirt.engine.core.common.validation.group.CreateEntity;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.core.dal.VdcBllMessages;
@@ -53,6 +55,11 @@ public class AddStorageServerConnectionCommand<T extends StorageServerConnection
         boolean returnValue = true;
         storage_server_connections paramConnection = getParameters().getStorageServerConnection();
         storage_server_connections currConnection = getConnection();
+        if (returnValue && paramConnection.getstorage_type() == StorageType.NFS
+                && !new LinuxMountPointConstraint().isValid(paramConnection.getconnection(), null)) {
+            returnValue = false;
+            addCanDoActionMessage("VALIDATION.STORAGE.CONNECTION.INVALID");
+        }
 
         if (StringHelper.isNullOrEmpty(currConnection.getid())
                 || DbFacade.getInstance().getStorageServerConnectionDAO().get(currConnection.getid()) == null) {
@@ -99,10 +106,17 @@ public class AddStorageServerConnectionCommand<T extends StorageServerConnection
         //
         // NOTE: this check must be the last check in this method
         //
-        if (returnValue && paramConnection.getstorage_type() != getParameters().getStorageServerConnection().getstorage_type()) {
+        if (returnValue
+                && paramConnection.getstorage_type() != getParameters().getStorageServerConnection().getstorage_type()) {
             paramConnection.setid(currConnection.getid());
             DbFacade.getInstance().getStorageServerConnectionDAO().update(paramConnection);
         }
         return returnValue;
+    }
+
+    @Override
+    protected List<Class<?>> getValidationGroups() {
+        addValidationGroup(CreateEntity.class);
+        return super.getValidationGroups();
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/RemoveStorageServerConnectionCommand.java
@@ -50,4 +50,5 @@ public class RemoveStorageServerConnectionCommand<T extends StorageServerConnect
     protected String getStorageConnection() {
         return getConnection().getconnection();
     }
+
 }

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddVmTemplateParameters.java
@@ -30,7 +30,7 @@ public class AddVmTemplateParameters extends VmTemplateParametersBase {
     @ValidName(message = "ACTION_TYPE_FAILED_NAME_MAY_NOT_CONTAIN_SPECIAL_CHARS")
     private String _name;
 
-    @Pattern(regexp = ValidationUtils.ONLY_ASCII_OR_NONE,
+    @Pattern(regexp = ValidationUtils.ONLY_I18N_ASCII_OR_NONE,
             message = "ACTION_TYPE_FAILED_DESCRIPTION_MAY_NOT_CONTAIN_SPECIAL_CHARS")
     private String _description;
 

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/CreateAllSnapshotsFromVmParameters.java
@@ -2,6 +2,7 @@ package org.ovirt.engine.core.common.action;
 
 import org.hibernate.validator.constraints.NotEmpty;
 import org.ovirt.engine.core.common.businessentities.Snapshot.SnapshotType;
+import org.ovirt.engine.core.common.validation.annotation.ValidDescription;
 import org.ovirt.engine.core.common.validation.group.CreateEntity;
 import org.ovirt.engine.core.compat.Guid;
 
@@ -11,6 +12,7 @@ public class CreateAllSnapshotsFromVmParameters extends VmOperationParameterBase
 
     @NotEmpty(groups = { CreateEntity.class },
             message = "VALIDATION.DISK_IMAGE.DESCRIPTION.NOT_EMPTY")
+    @ValidDescription(message = "VALIDATION.DISK_IMAGE.DESCRIPTION.NOT_ASCII", groups = { CreateEntity.class})
     private String _description;
 
     /**

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/StorageServerConnectionParametersBase.java
@@ -1,10 +1,14 @@
 package org.ovirt.engine.core.common.action;
 
-import org.ovirt.engine.core.compat.*;
-import org.ovirt.engine.core.common.businessentities.*;
+import javax.validation.Valid;
+
+import org.ovirt.engine.core.common.businessentities.storage_server_connections;
+import org.ovirt.engine.core.compat.Guid;
 
 public class StorageServerConnectionParametersBase extends StoragePoolParametersBase {
     private static final long serialVersionUID = 1516671099713952453L;
+
+    @Valid
     private storage_server_connections privateStorageServerConnection;
 
     public storage_server_connections getStorageServerConnection() {

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/BaseDisk.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.ovirt.engine.core.common.validation.annotation.ValidDescription;
 import org.ovirt.engine.core.common.validation.annotation.ValidName;
 import org.ovirt.engine.core.common.validation.group.CreateEntity;
 import org.ovirt.engine.core.common.validation.group.UpdateEntity;
@@ -40,7 +41,6 @@ public class BaseDisk extends IVdcQueryable implements BusinessEntity<Guid> {
      */
     private int internalDriveMapping;
 
-
     /**
      * The alias name of the disk.
      */
@@ -51,6 +51,8 @@ public class BaseDisk extends IVdcQueryable implements BusinessEntity<Guid> {
     /**
      * The description of the disk.
      */
+    @ValidDescription(message = "VALIDATION.DISK.DESCRIPTION.INVALID", groups = { CreateEntity.class
+            , UpdateEntity.class })
     private String diskDescription;
 
     /**
@@ -72,7 +74,6 @@ public class BaseDisk extends IVdcQueryable implements BusinessEntity<Guid> {
      * Should disk errors be propagated to the guest?
      */
     private PropagateErrors propagateErrors = PropagateErrors.Off;
-
 
     public BaseDisk() {
     }
@@ -172,7 +173,6 @@ public class BaseDisk extends IVdcQueryable implements BusinessEntity<Guid> {
     public void setShareable(boolean shareable) {
         this.shareable = shareable;
     }
-
 
     @Override
     public int hashCode() {

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VDSGroup.java
@@ -17,7 +17,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.ovirt.engine.core.common.businessentities.mapping.GuidType;
-import org.ovirt.engine.core.common.validation.annotation.ValidName;
+import org.ovirt.engine.core.common.validation.annotation.ValidI18NName;
 import org.ovirt.engine.core.common.validation.group.CreateEntity;
 import org.ovirt.engine.core.common.validation.group.UpdateEntity;
 import org.ovirt.engine.core.compat.Guid;
@@ -52,7 +52,7 @@ public class VDSGroup extends IVdcQueryable implements INotifyPropertyChanged, S
     @Size(min = 1, max = BusinessEntitiesDefinitions.CLUSTER_NAME_SIZE, message = "VALIDATION.VDS_GROUP.NAME.MAX",
             groups = {
             CreateEntity.class, UpdateEntity.class })
-    @ValidName(message = "VALIDATION.VDS_GROUP.NAME.INVALID", groups = { CreateEntity.class, UpdateEntity.class })
+    @ValidI18NName(message = "VALIDATION.VDS_GROUP.NAME.INVALID", groups = { CreateEntity.class, UpdateEntity.class })
     @Column(name = "name")
     private String name = ""; // GREGM Prevents NPE
 

commit e98e3b57630711ec8bf264a0f6289f8cdf03603e

core: Fix dal broken test due to reducing column size
The test was broken by commit 880077dea59c9ba4da0e6a8cef8497944aab8588
which modified the validated tag name size without updating the database
accordingly. That part of the commit is reverted.

Change-Id: I4e2850535020fe9bfe91749386dbfa5709ff73aa
Signed-off-by: Moti Asayag <masayag@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/BusinessEntitiesDefinitions.java
@@ -53,7 +53,7 @@ public final class BusinessEntitiesDefinitions {
     public static final int ROLE_NAME_SIZE = 126;
 
     // Tags (tags)
-    public static final int TAG_NAME_SIZE = 40;
+    public static final int TAG_NAME_SIZE = 50;
 
     // Quota
     public static final int QUOTA_NAME_SIZE = 65;