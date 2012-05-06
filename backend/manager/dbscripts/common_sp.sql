c4472f5bf2a205f978c1d8b083efacdec6a5cacf
Oved Ourfali <oourfali@redhat.com>
2012-05-06 16:47:38 +0300

    core: split configuration common stored procedure
    This commit adds a new "split configuration" function, called
    fn_db_split_configuration. The purpose of this function is to easy
    upgrading in case a configuration value that was once general, should
    now be version-specific.
    
    The input of the function is the option_name and the required new value.
    The result is having the current value for each possible version,
    besides the last version, which will get the input value.
    
    Change-Id: Ic5f828b140d9ced9675e5b9091ee27f5a6d9ffe7
    Acked-by: FirstName LastName <email>
    Signed-off-by: Oved Ourfali <oourfali@redhat.com>

--- backend/manager/dbscripts/common_sp.sql
+++ backend/manager/dbscripts/common_sp.sql
@@ -345,3 +345,39 @@ insert into permissions(id,role_id,ad_element_id,object_id,object_type_id) selec
 END; $BODY$
 
 LANGUAGE plpgsql;
+
+-- This function splits a config value: given a config value with one row for 'general', it creates new options
+-- with the old value, for each version, except the newest version, which gets the input value
+CREATE OR REPLACE FUNCTION fn_db_split_config_value(v_option_name character varying, v_new_option_value character varying)
+  RETURNS void AS
+$BODY$
+declare
+v_old_value varchar(4000);
+v_cur cursor for select distinct version from vdc_options where version <> 'general' order by version;
+v_version varchar(40);
+v_index integer;
+v_count integer;
+begin
+    v_old_value := option_value from vdc_options where option_name = v_option_name and version = 'general';
+    if (v_old_value IS NOT NULL) then
+        begin
+            v_count := count(distinct version) from vdc_options where version <> 'general';
+            v_index := 1;
+	    open v_cur;
+	    loop
+	        fetch v_cur into v_version;
+	        exit when not found;
+	        if (v_index = v_count) then
+	            insert into vdc_options (option_name, option_value, version) values (v_option_name, v_new_option_value, v_version);
+	        else
+                    insert into vdc_options (option_name, option_value, version) values (v_option_name, v_old_value, v_version);
+	        end if;
+	        v_index := v_index +1;
+	    end loop;
+	    close v_cur;
+            delete from vdc_options where option_name = v_option_name and version = 'general';
+        end;
+    end if;
+END; $BODY$
+
+LANGUAGE plpgsql
