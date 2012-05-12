05acd4f93eeb79cb97c05f1e9ad1dabe23ab576a
Gilad Chaplik <gchaplik@redhat.com>
2012-05-12 21:51:17 +0300

    webadmin: edit cluster policy doesn't open
    Change-Id: If818f056c1537ab6e01c57815116a2866c54ee2c
    Signed-off-by: Gilad Chaplik <gchaplik@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/popup/cluster/ClusterPolicyPopupPresenterWidget.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/popup/cluster/ClusterPolicyPopupPresenterWidget.java
@@ -1,7 +1,6 @@
 package org.ovirt.engine.ui.webadmin.section.main.presenter.popup.cluster;
 
 import org.ovirt.engine.ui.common.presenter.AbstractModelBoundPopupPresenterWidget;
-import org.ovirt.engine.ui.uicommonweb.models.clusters.ClusterListModel;
 import org.ovirt.engine.ui.uicommonweb.models.clusters.ClusterPolicyModel;
 
 import com.google.gwt.event.shared.EventBus;
@@ -12,18 +11,9 @@ public class ClusterPolicyPopupPresenterWidget extends AbstractModelBoundPopupPr
     public interface ViewDef extends AbstractModelBoundPopupPresenterWidget.ViewDef<ClusterPolicyModel> {
     }
 
-    private final ClusterListModel clusterListModel;
-
     @Inject
-    public ClusterPolicyPopupPresenterWidget(EventBus eventBus, ViewDef view, ClusterListModel clusterListModel) {
+    public ClusterPolicyPopupPresenterWidget(EventBus eventBus, ViewDef view) {
         super(eventBus, view);
-        this.clusterListModel = clusterListModel;
-    }
-
-    @Override
-    protected void onHide() {
-        super.onHide();
-        clusterListModel.ForceRefresh();
     }
 
 }
