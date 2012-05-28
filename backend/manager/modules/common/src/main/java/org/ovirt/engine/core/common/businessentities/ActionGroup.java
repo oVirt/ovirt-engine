8367df32b049455bcf185bde5e3e39849c27f1a4
Gilad Chaplik <gchaplik@redhat.com>
2012-05-28 16:58:54 +0300

    core: Support Appliction Mode for ActionGroup
    Adding property to action group, that indicates in which app mode it's supported.
    
    Change-Id: I87462cd1568798deb11323654df9b990750cd04b
    Signed-off-by: Gilad Chaplik <gchaplik@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/ActionGroup.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/ActionGroup.java
@@ -5,33 +5,34 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.ovirt.engine.core.common.VdcObjectType;
+import org.ovirt.engine.core.common.mode.ApplicationMode;
 
 public enum ActionGroup {
     // vm actions groups
-    CREATE_VM(1, RoleType.USER, VdcObjectType.VM, false),
-    DELETE_VM(2, RoleType.USER, VdcObjectType.VM, true),
-    EDIT_VM_PROPERTIES(3, RoleType.USER, VdcObjectType.VM, true),
-    VM_BASIC_OPERATIONS(4, RoleType.USER, VdcObjectType.VM, true),
-    CHANGE_VM_CD(5, RoleType.USER, VdcObjectType.VM, true),
-    MIGRATE_VM(6, RoleType.USER, VdcObjectType.VM, true),
+    CREATE_VM(1, RoleType.USER, VdcObjectType.VM, false, ApplicationMode.VirtOnly),
+    DELETE_VM(2, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
+    EDIT_VM_PROPERTIES(3, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
+    VM_BASIC_OPERATIONS(4, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
+    CHANGE_VM_CD(5, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
+    MIGRATE_VM(6, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
 
     /**
      * Connect to the console of a virtual machine, but only if no user
      * has connected before:
      */
-    CONNECT_TO_VM(7, RoleType.USER, VdcObjectType.VM, true),
+    CONNECT_TO_VM(7, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
 
-    IMPORT_EXPORT_VM(8, RoleType.ADMIN, VdcObjectType.VM, true),
-    CONFIGURE_VM_NETWORK(9, RoleType.USER, VdcObjectType.VM, true),
-    CONFIGURE_VM_STORAGE(10, RoleType.USER, VdcObjectType.VM, true),
-    MOVE_VM(11, RoleType.USER, VdcObjectType.VM, true),
-    MANIPULATE_VM_SNAPSHOTS(12, RoleType.USER, VdcObjectType.VM, true),
+    IMPORT_EXPORT_VM(8, RoleType.ADMIN, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
+    CONFIGURE_VM_NETWORK(9, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
+    CONFIGURE_VM_STORAGE(10, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
+    MOVE_VM(11, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
+    MANIPULATE_VM_SNAPSHOTS(12, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
 
     /**
      * Connect to the console of a virtual machine even if a different
      * user was connected before:
      */
-    RECONNECT_TO_VM(13, RoleType.USER, VdcObjectType.VM, true),
+    RECONNECT_TO_VM(13, RoleType.USER, VdcObjectType.VM, true, ApplicationMode.VirtOnly),
 
     // host (vds) actions groups
     CREATE_HOST(100, RoleType.ADMIN, VdcObjectType.VDS, true),
@@ -40,16 +41,16 @@ public enum ActionGroup {
     MANIPUTLATE_HOST(103, RoleType.ADMIN, VdcObjectType.VDS, true),
     CONFIGURE_HOST_NETWORK(104, RoleType.ADMIN, VdcObjectType.VDS, true),
     // templates actions groups
-    CREATE_TEMPLATE(200, RoleType.USER, VdcObjectType.VmTemplate, false),
-    EDIT_TEMPLATE_PROPERTIES(201, RoleType.USER, VdcObjectType.VmTemplate, true),
-    DELETE_TEMPLATE(202, RoleType.USER, VdcObjectType.VmTemplate, true),
-    COPY_TEMPLATE(203, RoleType.USER, VdcObjectType.VmTemplate, true),
-    CONFIGURE_TEMPLATE_NETWORK(204, RoleType.USER, VdcObjectType.VmTemplate, true),
+    CREATE_TEMPLATE(200, RoleType.USER, VdcObjectType.VmTemplate, false, ApplicationMode.VirtOnly),
+    EDIT_TEMPLATE_PROPERTIES(201, RoleType.USER, VdcObjectType.VmTemplate, true, ApplicationMode.VirtOnly),
+    DELETE_TEMPLATE(202, RoleType.USER, VdcObjectType.VmTemplate, true, ApplicationMode.VirtOnly),
+    COPY_TEMPLATE(203, RoleType.USER, VdcObjectType.VmTemplate, true, ApplicationMode.VirtOnly),
+    CONFIGURE_TEMPLATE_NETWORK(204, RoleType.USER, VdcObjectType.VmTemplate, true, ApplicationMode.VirtOnly),
     // vm pools actions groups
-    CREATE_VM_POOL(300, RoleType.USER, VdcObjectType.VmPool, false),
-    EDIT_VM_POOL_CONFIGURATION(301, RoleType.USER, VdcObjectType.VmPool, true),
-    DELETE_VM_POOL(302, RoleType.USER, VdcObjectType.VmPool, true),
-    VM_POOL_BASIC_OPERATIONS(303, RoleType.USER, VdcObjectType.VmPool, true),
+    CREATE_VM_POOL(300, RoleType.USER, VdcObjectType.VmPool, false, ApplicationMode.VirtOnly),
+    EDIT_VM_POOL_CONFIGURATION(301, RoleType.USER, VdcObjectType.VmPool, true, ApplicationMode.VirtOnly),
+    DELETE_VM_POOL(302, RoleType.USER, VdcObjectType.VmPool, true, ApplicationMode.VirtOnly),
+    VM_POOL_BASIC_OPERATIONS(303, RoleType.USER, VdcObjectType.VmPool, true, ApplicationMode.VirtOnly),
     // clusters actions groups
     CREATE_CLUSTER(400, RoleType.ADMIN, VdcObjectType.VdsGroups, true),
     EDIT_CLUSTER_CONFIGURATION(401, RoleType.ADMIN, VdcObjectType.VdsGroups, true),
@@ -60,39 +61,40 @@ public enum ActionGroup {
     MANIPULATE_ROLES(501, RoleType.ADMIN, VdcObjectType.User, true),
     MANIPULATE_PERMISSIONS(502, RoleType.USER, VdcObjectType.User, true),
     // storage domains actions groups
-    CREATE_STORAGE_DOMAIN(600, RoleType.ADMIN, VdcObjectType.Storage, true),
-    EDIT_STORAGE_DOMAIN_CONFIGURATION(601, RoleType.ADMIN, VdcObjectType.Storage, true),
-    DELETE_STORAGE_DOMAIN(602, RoleType.ADMIN, VdcObjectType.Storage, true),
-    MANIPULATE_STORAGE_DOMAIN(603, RoleType.ADMIN, VdcObjectType.Storage, true),
+    CREATE_STORAGE_DOMAIN(600, RoleType.ADMIN, VdcObjectType.Storage, true, ApplicationMode.VirtOnly),
+    EDIT_STORAGE_DOMAIN_CONFIGURATION(601, RoleType.ADMIN, VdcObjectType.Storage, true, ApplicationMode.VirtOnly),
+    DELETE_STORAGE_DOMAIN(602, RoleType.ADMIN, VdcObjectType.Storage, true, ApplicationMode.VirtOnly),
+    MANIPULATE_STORAGE_DOMAIN(603, RoleType.ADMIN, VdcObjectType.Storage, true, ApplicationMode.VirtOnly),
     // storage pool actions groups
-    CREATE_STORAGE_POOL(700, RoleType.ADMIN, VdcObjectType.StoragePool, true),
-    DELETE_STORAGE_POOL(701, RoleType.ADMIN, VdcObjectType.StoragePool, true),
-    EDIT_STORAGE_POOL_CONFIGURATION(702, RoleType.ADMIN, VdcObjectType.StoragePool, true),
-    CONFIGURE_STORAGE_POOL_NETWORK(703, RoleType.ADMIN, VdcObjectType.StoragePool, true),
+    CREATE_STORAGE_POOL(700, RoleType.ADMIN, VdcObjectType.StoragePool, true, ApplicationMode.VirtOnly),
+    DELETE_STORAGE_POOL(701, RoleType.ADMIN, VdcObjectType.StoragePool, true, ApplicationMode.VirtOnly),
+    EDIT_STORAGE_POOL_CONFIGURATION(702, RoleType.ADMIN, VdcObjectType.StoragePool, true, ApplicationMode.VirtOnly),
+    CONFIGURE_STORAGE_POOL_NETWORK(703, RoleType.ADMIN, VdcObjectType.StoragePool, true, ApplicationMode.VirtOnly),
 
     // engine generic
     CONFIGURE_ENGINE(800, RoleType.ADMIN, VdcObjectType.System, true),
 
     // Quota
-    CONFIGURE_QUOTA(900, RoleType.ADMIN, VdcObjectType.Quota, true),
-    CONSUME_QUOTA(901, RoleType.USER, VdcObjectType.Quota, true),
+    CONFIGURE_QUOTA(900, RoleType.ADMIN, VdcObjectType.Quota, true, ApplicationMode.VirtOnly),
+    CONSUME_QUOTA(901, RoleType.USER, VdcObjectType.Quota, true, ApplicationMode.VirtOnly),
 
     // Gluster
-    CREATE_GLUSTER_VOLUME(1000, RoleType.ADMIN, VdcObjectType.GlusterVolume, true),
-    MANIPULATE_GLUSTER_VOLUME(1001, RoleType.ADMIN, VdcObjectType.GlusterVolume, true),
-    DELETE_GLUSTER_VOLUME(1002, RoleType.ADMIN, VdcObjectType.GlusterVolume, true),
+    CREATE_GLUSTER_VOLUME(1000, RoleType.ADMIN, VdcObjectType.GlusterVolume, true, ApplicationMode.GlusterOnly),
+    MANIPULATE_GLUSTER_VOLUME(1001, RoleType.ADMIN, VdcObjectType.GlusterVolume, true, ApplicationMode.GlusterOnly),
+    DELETE_GLUSTER_VOLUME(1002, RoleType.ADMIN, VdcObjectType.GlusterVolume, true, ApplicationMode.GlusterOnly),
 
     // Disks action groups
-    CREATE_DISK(1100, RoleType.USER, VdcObjectType.Disk, false),
-    ATTACH_DISK(1101, RoleType.USER, VdcObjectType.Disk, true),
-    EDIT_DISK_PROPERTIES(1102, RoleType.USER, VdcObjectType.Disk, true),
-    CONFIGURE_DISK_STORAGE(1103, RoleType.USER, VdcObjectType.Disk, true),
-    DELETE_DISK(1104, RoleType.USER, VdcObjectType.Disk, true);
+    CREATE_DISK(1100, RoleType.USER, VdcObjectType.Disk, false, ApplicationMode.VirtOnly),
+    ATTACH_DISK(1101, RoleType.USER, VdcObjectType.Disk, true, ApplicationMode.VirtOnly),
+    EDIT_DISK_PROPERTIES(1102, RoleType.USER, VdcObjectType.Disk, true, ApplicationMode.VirtOnly),
+    CONFIGURE_DISK_STORAGE(1103, RoleType.USER, VdcObjectType.Disk, true, ApplicationMode.VirtOnly),
+    DELETE_DISK(1104, RoleType.USER, VdcObjectType.Disk, true, ApplicationMode.VirtOnly);
 
     private int id;
     private RoleType roleType;
     private VdcObjectType vdcObjectType;
     private boolean allowsViewingChildren;
+    private int applicationMode = ApplicationMode.AllModes.getValue();
     private static HashMap<Integer, ActionGroup> map = new HashMap<Integer, ActionGroup>(ActionGroup.values().length);
     private static HashMap<VdcObjectType, ArrayList<ActionGroup>> entityToActionGroupsMapping =
             new HashMap<VdcObjectType, ArrayList<ActionGroup>>();
@@ -114,6 +116,15 @@ public enum ActionGroup {
         roleType = type;
         vdcObjectType = objectType;
         this.allowsViewingChildren = allowsViewingChildren;
+    }
+
+    private ActionGroup(int value,
+            RoleType type,
+            VdcObjectType objectType,
+            boolean allowsViewingChildren,
+            ApplicationMode applicationMode) {
+        this(value, type, objectType, allowsViewingChildren);
+        this.applicationMode = applicationMode.getValue();;
     }
 
     private static void initEntitiesMap() {
@@ -143,5 +154,9 @@ public enum ActionGroup {
 
     public static HashMap<VdcObjectType, ArrayList<ActionGroup>> getEntityToActionGroupsMapping() {
         return entityToActionGroupsMapping;
+    }
+
+    public int getAvailableInModes() {
+        return applicationMode;
     }
 }
