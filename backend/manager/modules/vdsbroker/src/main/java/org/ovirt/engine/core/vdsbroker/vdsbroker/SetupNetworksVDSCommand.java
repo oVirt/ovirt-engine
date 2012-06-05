9aed12610149a823c7365555bd28b11d35d8a647
Mike Kolesnik <mkolesni@redhat.com>
2012-06-05 12:22:53 +0300

    core: Code cleanup SetupNetworksVDSCommand part 1
    Simple cleanup - extracted to methods instead of using comments.
    Also removeObj was changed to a contant field.
    
    Change-Id: I5ac56a3c68dc7ecc7fa6f3449c0b7990ce3e5350
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/SetupNetworksVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/SetupNetworksVDSCommand.java
@@ -1,6 +1,7 @@
 package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,18 +15,20 @@ import org.ovirt.engine.core.vdsbroker.xmlrpc.XmlRpcStruct;
 
 public class SetupNetworksVDSCommand<T extends SetupNetworksVdsCommandParameters> extends FutureVDSCommand<T> {
 
+    private static final Map<String, String> REMOVE_OBJ = Collections.singletonMap("remove", Boolean.TRUE.toString());
+
     public SetupNetworksVDSCommand(T parameters) {
         super(parameters);
     }
 
     @Override
     protected void ExecuteVdsBrokerCommand() {
-        XmlRpcStruct bonds = new XmlRpcStruct();
-        XmlRpcStruct networks = new XmlRpcStruct();
-        Map<String, String> removeObj = new HashMap<String, String>();
-        removeObj.put("remove", "true");
+        httpTask = getBroker().setupNetworks(generateNetworks(), generateBonds(), generateOptions());
+    }
 
-        // Networks
+    private XmlRpcStruct generateNetworks() {
+        XmlRpcStruct networks = new XmlRpcStruct();
+
         for (network net : getParameters().getNetworks()) {
             Map<String, String> opts = new HashMap<String, String>();
             VdsNetworkInterface i = findNetworkInterface(net.getname(), getParameters().getInterfaces(),
@@ -62,16 +65,19 @@ public class SetupNetworksVDSCommand<T extends SetupNetworksVdsCommandParameters
             networks.add(net.getname(), opts);
         }
 
-        // Removed Networks
         for (String net : getParameters().getRemovedNetworks()) {
-            networks.add(net, removeObj);
+            networks.add(net, REMOVE_OBJ);
         }
 
-        // Bonds
+        return networks;
+    }
+
+    private XmlRpcStruct generateBonds() {
+        XmlRpcStruct bonds = new XmlRpcStruct();
+
         for (VdsNetworkInterface bond : getParameters().getBonds()) {
             XmlRpcStruct opts = new XmlRpcStruct();
             opts.add("nics", getBondNics(bond, getParameters().getInterfaces()));
-            // opts.add("nics", "eth1,eth2");
 
             if (!StringHelper.isNullOrEmpty(bond.getBondOptions())) {
                 opts.add("BONDING_OPTS", bond.getBondOptions());
@@ -79,12 +85,14 @@ public class SetupNetworksVDSCommand<T extends SetupNetworksVdsCommandParameters
             bonds.add(bond.getName(), opts);
         }
 
-        // Removed Bonds
         for (VdsNetworkInterface bond : getParameters().getRemovedBonds()) {
-            bonds.add(bond.getName(), removeObj);
+            bonds.add(bond.getName(), REMOVE_OBJ);
         }
 
-        // Options
+        return bonds;
+    }
+
+    private XmlRpcStruct generateOptions() {
         XmlRpcStruct options = new XmlRpcStruct();
 
         if (getParameters().isCheckConnectivity()) {
@@ -98,9 +106,7 @@ public class SetupNetworksVDSCommand<T extends SetupNetworksVdsCommandParameters
             options.add(VdsProperties.connectivityTimeout,
                     (new Integer(getParameters().getConectivityTimeout())).toString());
         }
-
-        httpTask = getBroker().setupNetworks(networks, bonds, options);
-
+        return options;
     }
 
     private List<String> getBondNics(VdsNetworkInterface bond, List<VdsNetworkInterface> interfaces) {
