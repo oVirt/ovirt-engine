oVirt/ovirt-engine

commit 80583b2c4c76f390f87102bf514ec41eca6dd153

core : Last patch for removing internal_drive_mappings
The following patch is removing a usage of internal_drive_mappings at the ovirt.
There were couple of commands which were based on internal_drive_mapping, but actually not in use any more.
The following commands, queries were removed

Change-Id: I11e51e457124ba1e5ea33ffe81b8ea32758530f7
Signed-off-by: Michael Kublin <mkublin@redhat.com>

backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfTemplateWriter.java
@@ -158,8 +158,8 @@ public class OvfTemplateWriter extends OvfWriter {
         // item drive
         for (DiskImage image : _images) {
             _writer.WriteStartElement("Item");
-            _writer.WriteStartElement(RASD_URI, "Caption");
-            _writer.WriteRaw(String.format("Drive %1$d", image.getInternalDriveMapping()));
+            _writer.WriteStartElement("rasd:Caption");
+            _writer.WriteRaw(image.getDiskAlias());
             _writer.WriteEndElement();
             _writer.WriteStartElement(RASD_URI, "InstanceId");
             _writer.WriteRaw(image.getImageId().toString());
@@ -168,6 +168,7 @@ public class OvfTemplateWriter extends OvfWriter {
             _writer.WriteRaw(OvfHardware.DiskImage);
             _writer.WriteEndElement();
             _writer.WriteStartElement(RASD_URI, "HostResource");
+            _writer.WriteStartElement("rasd:HostResource");
             _writer.WriteRaw(image.getId() + "/" + image.getImageId());
             _writer.WriteEndElement();
             _writer.WriteStartElement(RASD_URI, "Parent");

backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfVmWriter.java
@@ -235,8 +235,8 @@ public class OvfVmWriter extends OvfWriter {
         // item drive
         for (DiskImage image : _images) {
             _writer.WriteStartElement("Item");
-            _writer.WriteStartElement(RASD_URI, "Caption");
-            _writer.WriteRaw(String.format("Drive %1$d", image.getInternalDriveMapping()));
+            _writer.WriteStartElement("rasd:Caption");
+            _writer.WriteRaw(image.getDiskAlias());
             _writer.WriteEndElement();
             _writer.WriteStartElement(RASD_URI, "InstanceId");
             _writer.WriteRaw(image.getImageId().toString());

commit b034ac55fb5270bf5c79d4dc3248d330e41a1356

core: Fix use of namespaces in OVF writers
Commit 80583b2c4c76f390f87102bf514ec41eca6dd153 introduced some
errors in the OVF writers that cause the OVF files to be generated
incorrectly.

Change-Id: I9e5833a41e85c5c15ae4ee21af83240bbb756875
Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfTemplateWriter.java
@@ -158,7 +158,7 @@ public class OvfTemplateWriter extends OvfWriter {
         // item drive
         for (DiskImage image : _images) {
             _writer.WriteStartElement("Item");
-            _writer.WriteStartElement("rasd:Caption");
+            _writer.WriteStartElement(RASD_URI, "Caption");
             _writer.WriteRaw(image.getDiskAlias());
             _writer.WriteEndElement();
             _writer.WriteStartElement(RASD_URI, "InstanceId");
@@ -168,7 +168,6 @@ public class OvfTemplateWriter extends OvfWriter {
             _writer.WriteRaw(OvfHardware.DiskImage);
             _writer.WriteEndElement();
             _writer.WriteStartElement(RASD_URI, "HostResource");
-            _writer.WriteStartElement("rasd:HostResource");
             _writer.WriteRaw(image.getId() + "/" + image.getImageId());
             _writer.WriteEndElement();
             _writer.WriteStartElement(RASD_URI, "Parent");

backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfVmWriter.java
@@ -235,7 +235,7 @@ public class OvfVmWriter extends OvfWriter {
         // item drive
         for (DiskImage image : _images) {
             _writer.WriteStartElement("Item");
-            _writer.WriteStartElement("rasd:Caption");
+            _writer.WriteStartElement(RASD_URI, "Caption");
             _writer.WriteRaw(image.getDiskAlias());
             _writer.WriteEndElement();
             _writer.WriteStartElement(RASD_URI, "InstanceId");