99d9e208f8cc0ae7b280eb46d272c4ec59d5912f
Tomas Jelinek <tjelinek@redhat.com>
2012-06-20 16:58:00 +0200

    engine: changed the IsVmTemlateWithSameNameExist to user
    Since the IsVmTemlateWithSameNameExist is called
    from user portal when editing the template, it
    needed to be a user query.
    
    It can be defined as a user query while not actually
    filtering the result, since it does not return any
    sensitive information. The same approach is used
    in IsVmWithSameNameExist.
    
    Change-Id: Ic03e9dfb2e00e7957f6a077917b85c41ffffcecd
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/VdcQueryType.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/VdcQueryType.java
@@ -65,7 +65,7 @@ public enum VdcQueryType implements Serializable {
     GetCACertificate(VdcQueryAuthType.User),
 
     // VM Templates queries
-    IsVmTemlateWithSameNameExist,
+    IsVmTemlateWithSameNameExist(VdcQueryAuthType.User),
     GetVmTemplate(VdcQueryAuthType.User),
     GetAllVmTemplates(VdcQueryAuthType.User),
     GetVmsByVmTemplateGuid,
