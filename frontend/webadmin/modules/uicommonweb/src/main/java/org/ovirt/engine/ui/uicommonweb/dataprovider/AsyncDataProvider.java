47995055c464bc9ce23c9609ba72822a145c6ef4
Daniel Erez <derez@redhat.com>
2012-06-26 13:17:29 +0300

    webadmin: calling GetLunsByVgId query by Host ID
    Edit Block Storage dialog:
    * Fetching LUNs stored in DB using 'GetLunsByVgId'.
    * Passing host's ID in query's parameters for calling GetDeviceList:
    (needed for updating path count for each LUN).
    
    Change-Id: Iaf817d0dc33a30b4cad2d7746c86ca9f813581da
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/dataprovider/AsyncDataProvider.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/dataprovider/AsyncDataProvider.java
@@ -1652,7 +1652,7 @@ public final class AsyncDataProvider {
         Frontend.RunQuery(VdcQueryType.GetoVirtISOs, new VdsIdParametersBase(id), aQuery);
     }
 
-    public static void GetLunsByVgId(AsyncQuery aQuery, String vgId) {
+    public static void GetLunsByVgId(AsyncQuery aQuery, String vgId, Guid vdsId) {
         aQuery.converterCallback = new IAsyncConverter() {
             @Override
             public Object Convert(Object source, AsyncQuery _asyncQuery)
@@ -1660,9 +1660,8 @@ public final class AsyncDataProvider {
                 return source != null ? (ArrayList<LUNs>) source : new ArrayList<LUNs>();
             }
         };
-        GetLunsByVgIdParameters tempVar = new GetLunsByVgIdParameters();
-        tempVar.setVgId(vgId);
-        Frontend.RunQuery(VdcQueryType.GetLunsByVgId, tempVar, aQuery);
+        GetLunsByVgIdParameters params = new GetLunsByVgIdParameters(vgId, vdsId);
+        Frontend.RunQuery(VdcQueryType.GetLunsByVgId, params, aQuery);
     }
 
     public static void GetAllTemplatesFromExportDomain(AsyncQuery aQuery, Guid storagePoolId, Guid storageDomainId) {
