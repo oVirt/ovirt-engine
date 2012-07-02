24ef33efbce7474f73b9bb2c032d66d06033c0e2
Sharad Mishra <snmishra@linux.vnet.ibm.com>
2012-07-02 14:50:09 -0700

    engine: Replacing StringHelper with StringUtils in VirtMonitoringStrategy.
    StringHelper is deprecated.
    
    Change-Id: I3e5e04d6990e87f80edc1fc02d7935c8405ac620
    Signed-off-by: Sharad Mishra <snmishra@linux.vnet.ibm.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/VirtMonitoringStrategy.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/VirtMonitoringStrategy.java
@@ -1,10 +1,10 @@
 package org.ovirt.engine.core.vdsbroker;
 
+import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.common.businessentities.NonOperationalReason;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.VDSStatus;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.compat.StringHelper;
 
 /**
  * This class defines virt strategy entry points, which are needed in host monitoring phase
@@ -49,7 +49,7 @@ public class VirtMonitoringStrategy implements MonitoringStrategy {
 
     @Override
     public boolean processHardwareCapabilitiesNeeded(VDS oldVds, VDS newVds) {
-        return !StringHelper.EqOp(oldVds.getcpu_flags(), newVds.getcpu_flags());
+        return !StringUtils.equals(oldVds.getcpu_flags(), newVds.getcpu_flags());
     }
 }
 
