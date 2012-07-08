2592f534a978c623a311fa296a6c610560505de9
Moti Asayag <masayag@redhat.com>
2012-07-08 09:58:36 +0300

    core: Code cleanup and adjustment of UpdateNetworkToVdsInterfaceCommand
    The patch performs some code cleanup and adjustments:
    1. Replace the deprecated StringHelper with StringUtils.
    2. Rename member names which start with an underscore.
    
    Change-Id: Ib97a482bc781aedacadbc1227a4ff2031a20995d
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateNetworkToVdsInterfaceCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateNetworkToVdsInterfaceCommand.java
@@ -3,6 +3,7 @@ package org.ovirt.engine.core.bll;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.action.UpdateNetworkToVdsParameters;
 import org.ovirt.engine.core.common.businessentities.NetworkBootProtocol;
@@ -16,9 +17,9 @@ import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
 import org.ovirt.engine.core.common.vdscommands.VdsIdAndVdsVDSCommandParametersBase;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.core.dal.VdcBllMessages;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.InterfaceDAO;
 import org.ovirt.engine.core.utils.IPAddress;
 import org.ovirt.engine.core.utils.NetworkUtils;
 import org.ovirt.engine.core.utils.dns.Dns;
@@ -26,11 +27,13 @@ import org.ovirt.engine.core.utils.linq.LinqUtils;
 import org.ovirt.engine.core.utils.linq.Predicate;
 import org.ovirt.engine.core.utils.threadpool.ThreadPoolUtil;
 
+@SuppressWarnings("serial")
 public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsParameters> extends VdsNetworkCommand<T> {
-    private List<VdsNetworkInterface> _interfaces;
-    private VDSReturnValue _retVal;
-    private boolean _editNetworkDone = false;
-    private boolean _editNetworkThreadFinish = false;
+
+    private List<VdsNetworkInterface> interfaces;
+    private VDSReturnValue retVal;
+    private boolean editNetworkDone = false;
+    private boolean editNetworkThreadFinish = false;
 
     public UpdateNetworkToVdsInterfaceCommand(T parameters) {
         super(parameters);
@@ -39,15 +42,15 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
     @Override
     protected void executeCommand() {
         String address = getParameters().getAddress();
-        String subnet = StringHelper.isNullOrEmpty(getParameters().getSubnet()) ? getParameters().getNetwork()
+        String subnet = StringUtils.isEmpty(getParameters().getSubnet()) ? getParameters().getNetwork()
                 .getsubnet() : getParameters().getSubnet();
-        String gateway = StringHelper.isNullOrEmpty(getParameters().getGateway()) ? "" : getParameters().getGateway();
+        String gateway = StringUtils.isEmpty(getParameters().getGateway()) ? "" : getParameters().getGateway();
         java.util.ArrayList<String> interfaceNames = new java.util.ArrayList<String>();
         for (VdsNetworkInterface i : getParameters().getInterfaces()) {
-            if (i.getBonded() != null && i.getBonded() || NetworkUtils.IsBondVlan(_interfaces, i)) {
+            if (i.getBonded() != null && i.getBonded() || NetworkUtils.IsBondVlan(interfaces, i)) {
                 getParameters().setBondName(NetworkUtils.StripVlan(i.getName()));
-                for (VdsNetworkInterface ix : _interfaces) {
-                    if (StringHelper.EqOp(ix.getBondName(), NetworkUtils.StripVlan(i.getName()))) {
+                for (VdsNetworkInterface ix : interfaces) {
+                    if (StringUtils.equals(ix.getBondName(), NetworkUtils.StripVlan(i.getName()))) {
                         interfaceNames.add(NetworkUtils.StripVlan(ix.getName()));
                     }
                 }
@@ -79,16 +82,16 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
             EditNetworkThreadCompat(parameters);
         }
 
-        if (_retVal != null && _editNetworkDone) {
+        if (retVal != null && editNetworkDone) {
             // update vds network data
-            _retVal = Backend
+            retVal = Backend
                     .getInstance()
                     .getResourceManager()
                     .RunVdsCommand(VDSCommandType.CollectVdsNetworkData,
                             new VdsIdAndVdsVDSCommandParametersBase(getParameters().getVdsId()));
 
-            if (_retVal.getSucceeded()) {
-                Guid groupId = DbFacade.getInstance().getVdsDAO().get(getParameters().getVdsId()).getvds_group_id();
+            if (retVal.getSucceeded()) {
+                Guid groupId = getVdsDAO().get(getParameters().getVdsId()).getvds_group_id();
                 AttachNetworkToVdsGroupCommand.SetNetworkStatus(groupId, getParameters().getNetwork());
                 setSucceeded(true);
             }
@@ -110,21 +113,21 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
 
     private void EditNetworkThreadCompat(NetworkVdsmVDSCommandParameters parameters) {
         try {
-            _retVal = Backend.getInstance().getResourceManager().RunVdsCommand(VDSCommandType.EditNetwork, parameters);
-            _editNetworkDone = true;
+            retVal = Backend.getInstance().getResourceManager().RunVdsCommand(VDSCommandType.EditNetwork, parameters);
+            editNetworkDone = true;
         } catch (RuntimeException e) {
             if (e instanceof VdcBLLException) {
                 getReturnValue().setFault(new VdcFault(e, ((VdcBLLException) e).getVdsError().getCode()));
             }
         } catch (java.lang.Exception e) {
         } finally {
-            _editNetworkThreadFinish = true;
+            editNetworkThreadFinish = true;
         }
     }
 
     protected void PollVds(Guid vdsId) {
         int retries = 10;
-        while (retries > 0 && _retVal == null && !_editNetworkThreadFinish) {
+        while (retries > 0 && retVal == null && !editNetworkThreadFinish) {
             retries--;
             try {
                 Backend.getInstance()
@@ -139,11 +142,11 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
     @Override
     protected boolean canDoAction() {
         String ifaceGateway = null;
-        _interfaces = DbFacade.getInstance().getInterfaceDAO().getAllInterfacesForVds(getParameters().getVdsId());
+        interfaces = getInterfaceDAO().getAllInterfacesForVds(getParameters().getVdsId());
 
         // check that interface exists
         for (final VdsNetworkInterface i : getParameters().getInterfaces()) {
-            VdsNetworkInterface iface = LinqUtils.firstOrNull(_interfaces, new Predicate<VdsNetworkInterface>() {
+            VdsNetworkInterface iface = LinqUtils.firstOrNull(interfaces, new Predicate<VdsNetworkInterface>() {
                 @Override
                 public boolean eval(VdsNetworkInterface x) {
                     return x.getName().equals(i.getName());
@@ -156,16 +159,16 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
             ifaceGateway = iface.getGateway();
         }
 
-        // check that the old netowrk name is not null
-        if (StringHelper.isNullOrEmpty(getParameters().getOldNetworkName())) {
+        // check that the old network name is not null
+        if (StringUtils.isEmpty(getParameters().getOldNetworkName())) {
             addCanDoActionMessage(VdcBllMessages.NETWROK_OLD_NETWORK_NOT_SPECIFIED);
             return false;
         }
 
-        VDS vds = DbFacade.getInstance().getVdsDAO().get(getParameters().getVdsId());
+        VDS vds = getVdsDAO().get(getParameters().getVdsId());
         if (vds.getstatus() != VDSStatus.Maintenance) {
             // check that the old network exists in host
-            VdsNetworkInterface iface = LinqUtils.firstOrNull(_interfaces, new Predicate<VdsNetworkInterface>() {
+            VdsNetworkInterface iface = LinqUtils.firstOrNull(interfaces, new Predicate<VdsNetworkInterface>() {
                 @Override
                 public boolean eval(VdsNetworkInterface i) {
                     if (i.getNetworkName() != null) {
@@ -181,7 +184,7 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
         }
 
         // check that the old network exists in host
-        VdsNetworkInterface ifacenet = LinqUtils.firstOrNull(_interfaces, new Predicate<VdsNetworkInterface>() {
+        VdsNetworkInterface ifacenet = LinqUtils.firstOrNull(interfaces, new Predicate<VdsNetworkInterface>() {
             @Override
             public boolean eval(VdsNetworkInterface i) {
                 if (i.getNetworkName() != null) {
@@ -195,15 +198,15 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
             return false;
         }
 
-        if (StringHelper.EqOp(getParameters().getOldNetworkName(), NetworkUtils.getEngineNetwork())
-                && !StringHelper.EqOp(getParameters().getNetwork().getname(), NetworkUtils.getEngineNetwork())) {
+        if (StringUtils.equals(getParameters().getOldNetworkName(), NetworkUtils.getEngineNetwork())
+                && !StringUtils.equals(getParameters().getNetwork().getname(), NetworkUtils.getEngineNetwork())) {
             getReturnValue().getCanDoActionMessages()
                     .add(VdcBllMessages.NETWORK_DEFAULT_UPDATE_NAME_INVALID.toString());
             return false;
         }
 
         if (!NetworkUtils.getEngineNetwork().equals(getParameters().getNetwork().getname())
-                && !StringHelper.isNullOrEmpty(getParameters().getGateway())) {
+                && StringUtils.isNotEmpty(getParameters().getGateway())) {
             if (!getParameters().getGateway().equals(ifaceGateway)) {
                 addCanDoActionMessage(VdcBllMessages.NETWORK_ATTACH_ILLEGAL_GATEWAY);
                 return false;
@@ -217,7 +220,7 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
         // check conectivity
         getParameters().setCheckConnectivity(getParameters().getCheckConnectivity());
         if (getParameters().getCheckConnectivity()) {
-            if (!StringHelper.EqOp(getParameters().getNetwork().getname(), NetworkUtils.getEngineNetwork())) {
+            if (!StringUtils.equals(getParameters().getNetwork().getname(), NetworkUtils.getEngineNetwork())) {
                 addCanDoActionMessage(VdcBllMessages.NETWORK_CHECK_CONNECTIVITY);
                 return false;
             }
@@ -227,7 +230,7 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
         if (getParameters().getBootProtocol() == NetworkBootProtocol.StaticIp) {
             Pattern IP_PATTERN = Pattern
                     .compile("^(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}$");
-            if (StringHelper.isNullOrEmpty(getParameters().getAddress())) {
+            if (StringUtils.isEmpty(getParameters().getAddress())) {
                 addCanDoActionMessage(VdcBllMessages.NETWROK_ADDR_MANDATORY_IN_STATIC_IP);
                 return false;
             }
@@ -235,12 +238,12 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
                 addCanDoActionMessage(VdcBllMessages.NETWROK_ADDR_IN_STATIC_IP_BAD_FORMAT);
                 return false;
             }
-            if (!StringHelper.isNullOrEmpty(getParameters().getGateway())
+            if (StringUtils.isNotEmpty(getParameters().getGateway())
                     && !IP_PATTERN.matcher(getParameters().getGateway()).matches()) {
                 addCanDoActionMessage(VdcBllMessages.NETWROK_ADDR_IN_GATEWAY_BAD_FORMAT);
                 return false;
             }
-            if (!StringHelper.isNullOrEmpty(getParameters().getSubnet())
+            if (StringUtils.isNotEmpty(getParameters().getSubnet())
                     && !IP_PATTERN.matcher(getParameters().getSubnet()).matches()) {
                 addCanDoActionMessage(VdcBllMessages.NETWROK_ADDR_IN_SUBNET_BAD_FORMAT);
                 return false;
@@ -254,5 +257,9 @@ public class UpdateNetworkToVdsInterfaceCommand<T extends UpdateNetworkToVdsPara
     public AuditLogType getAuditLogTypeValue() {
         return getSucceeded() ? AuditLogType.NETWORK_ATTACH_NETWORK_TO_VDS
                 : AuditLogType.NETWORK_ATTACH_NETWORK_TO_VDS_FAILED;
+    }
+
+    protected InterfaceDAO getInterfaceDAO() {
+        return DbFacade.getInstance().getInterfaceDAO();
     }
 }
