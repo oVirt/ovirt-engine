02690d81c349b505e7e00b24089be6d99c491b56
Yaniv Dary <ydary@redhat.com>
2012-07-09 14:14:54 +0300

    history: removed unused column from dwh views.
    Change-Id: Ic3b53028ae24d71a3a44f08301c79bb176ef5ff8
    Signed-off-by: Yaniv Dary <ydary@redhat.com>

--- backend/manager/dbscripts/create_dwh_views.sql
+++ backend/manager/dbscripts/create_dwh_views.sql
@@ -259,7 +259,6 @@ SELECT
 	cast(c.cpu_sys as smallint) as system_cpu_usage_percent,
 	cast(c.cpu_user as smallint) as user_cpu_usage_percent,
 	c.disks_usage,
-	b.last_start_time,
 	b.vm_ip,
 	b.guest_cur_user_name as current_user_name,
 	b.run_on_vds as currently_running_on_host
