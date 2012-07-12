c4c1e76b92b50c44bf9f247a585da97ff872138b
Juan Hernandez <juan.hernandez@redhat.com>
2012-07-12 10:27:44 +0200

    webadmin: Use AspectJ annotations instead of compiler
    The advantage of using the AspectJ annotations style instead of
    the AspectJ language and compiler is that there is no need to use
    an special maven plugin to do the build of the aspects.
    
    This also means that there is no need to use any special Eclipse
    plugin to support that maven pluging.
    
    Change-Id: I19fceace94978ada932f214f936041e1257fafa9
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- frontend/webadmin/modules/gwt-extension/src/main/java/org/ovirt/engine/ui/gwtextension/DontPrune.java
+++ frontend/webadmin/modules/gwt-extension/src/main/java/org/ovirt/engine/ui/gwtextension/DontPrune.java
@@ -10,7 +10,12 @@ import com.google.gwt.dev.jjs.impl.ControlFlowAnalyzer;
 //import com.google.gwt.dev.util.log.PrintWriterTreeLogger;
 //import com.google.gwt.core.ext.TreeLogger;
 
-public aspect DontPrune {
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class DontPrune {
   //PrintWriterTreeLogger logger = new PrintWriterTreeLogger();
 
   /**
@@ -23,18 +28,20 @@ public aspect DontPrune {
    * This pointcut captures the execution of the constructor of the liveness
    * analyzer.
    */
-  pointcut createLivenessAnalyzer(ControlFlowAnalyzer analyzer, JProgram program):
-    target(analyzer) &&
-    args(program) &&
-    execution(public ControlFlowAnalyzer.new(JProgram))
-  ;
+  @Pointcut(
+      "target(analyzer) && " +
+      "args(program) && " +
+      "execution(public com.google.gwt.dev.jjs.impl.ControlFlowAnalyzer.new(com.google.gwt.dev.jjs.ast.JProgram))"
+  )
+  public void createLivenessAnalyzer(ControlFlowAnalyzer analyzer, JProgram program) {}
 
   /**
-   * Just after the invocation of the liveness analyzer exclude all the fields 
+   * Just after the invocation of the liveness analyzer exclude all the fields
    * and methods inside the classes whose name match the regular expression given
    * in the system property.
    */
-  after(ControlFlowAnalyzer analyzer, JProgram program): createLivenessAnalyzer(analyzer, program) {
+  @After("createLivenessAnalyzer(analyzer, program)")
+  public void afterCreateLivenessAnalyzer(ControlFlowAnalyzer analyzer, JProgram program) {
     // Get the regular expression and warn the user if it is empty:
     String dontPruneRe = System.getProperty(GWT_DONTPRUNE);
     if (dontPruneRe == null) {
@@ -42,12 +49,12 @@ public aspect DontPrune {
       System.err.println("The system property \"" + GWT_DONTPRUNE + "\" that specifies the types not to be pruned wasn't set!");
       throw new RuntimeException("The system property \"" + GWT_DONTPRUNE + "\" that specifies the types not to be pruned wasn't set!");
     }
-    
+
     //logger.finest("Types matching \"" + dontPruneRe + "\" won't be pruned.");
 
     // Compile the regular expression:
     Pattern dontPrunePattern = Pattern.compile(dontPruneRe);
-    
+
     // Scan all the types and make sure that the ones matching the regular expression
     // are not pruned:
     for (JDeclaredType type : program.getDeclaredTypes()) {
