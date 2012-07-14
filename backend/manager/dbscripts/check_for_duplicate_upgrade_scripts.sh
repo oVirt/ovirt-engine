8918f7d4e2d8c3f545b7f2d0d3053af925f62115
Alon Bar-Lev <alonbl@redhat.com>
2012-07-14 23:42:39 +0300

    database: do not use git when building
    If we are not building on development machine, for example we
    use rpmbuild -tb <tarball>, we probably do not have git or
    we do not have git checkout to query.
    
    There is no actual need to use git, as we can use relative
    directories.
    
    Cleanup the check_for_duplicate_upgrade_scripts.sh script.
    
    Change-Id: If06ea52efb4da7c51dad8fc2b32043082ce13263
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

--- backend/manager/dbscripts/check_for_duplicate_upgrade_scripts.sh
+++ backend/manager/dbscripts/check_for_duplicate_upgrade_scripts.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ $(ls $(git rev-parse --show-toplevel)/backend/manager/dbscripts/upgrade | grep -P '\d{2}_\d{2}.\d{2,8}' -o | uniq -d | wc -l) -ne 0 ]; then
- version=$(ls $(git rev-parse --show-toplevel)/backend/manager/dbscripts/upgrade | grep -P '\d{2}_\d{2}.\d{2,8}' -o | uniq -d)
- echo "Found duplicate upgrade scripts with version ${version}, please resolve and retry"
- exit 1
+cd "$(dirname "$0")"
+
+OUT="$(ls upgrade | grep -P '\d{2}_\d{2}.\d{2,8}' -o | uniq -d)"
+
+if [ -n "${OUT}" ]; then
+	echo "Found duplicate upgrade scripts with version $(echo ${OUT}), please resolve and retry" >&2
+	exit 1
 fi
+
 exit 0
-
-
