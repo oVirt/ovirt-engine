oVirt/ovirt-engine

commit f7d691107453705bc65bc516480d2f6addb904ea

engine: Improve RemoveStorageDomainCommand
The following patch will perform a following improvements:
1. Disable the global transaction at RemoveStorageDomainCommand
2. Set in the memory lock on storage domain during an operation

Change-Id: Ide7dd501c26cb4735adfc57612da740398d66df9
Signed-off-by: Michael Kublin <mkublin@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/RemoveStorageDomainCommand.java
@@ -1,5 +1,10 @@
 package org.ovirt.engine.core.bll.storage;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.ovirt.engine.core.bll.LockIdNameAttribute;
+import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.action.DetachStorageDomainFromPoolParameters;
 import org.ovirt.engine.core.common.action.RemoveStorageDomainParameters;
@@ -12,12 +17,16 @@ import org.ovirt.engine.core.common.businessentities.storage_domains;
 import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.interfaces.VDSBrokerFrontend;
+import org.ovirt.engine.core.common.locks.LockingGroup;
 import org.ovirt.engine.core.common.vdscommands.FormatStorageDomainVDSCommandParameters;
 import org.ovirt.engine.core.common.vdscommands.RemoveVGVDSCommandParameters;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dal.VdcBllMessages;
+import org.ovirt.engine.core.utils.transaction.TransactionMethod;
 
+@LockIdNameAttribute
+@NonTransactiveCommandAttribute
 public class RemoveStorageDomainCommand<T extends RemoveStorageDomainParameters> extends StorageDomainCommandBase<T> {
     public RemoveStorageDomainCommand(T parameters) {
         super(parameters);
@@ -26,7 +35,7 @@ public class RemoveStorageDomainCommand<T extends RemoveStorageDomainParameters>
 
     @Override
     protected void executeCommand() {
-        storage_domains dom = getStorageDomain();
+        final storage_domains dom = getStorageDomain();
         VDS vds = getVds();
         boolean format = getParameters().getDoFormat();
 
@@ -50,9 +59,15 @@ public class RemoveStorageDomainCommand<T extends RemoveStorageDomainParameters>
             }
         }
 
-        getStorageHelper(dom).StorageDomainRemoved(dom.getStorageStaticData());
-        getDbFacade().getStorageDomainDynamicDAO().remove(dom.getId());
-        getDbFacade().getStorageDomainStaticDAO().remove(dom.getId());
+        executeInNewTransaction(new TransactionMethod<Object>() {
+            @Override
+            public Object runInTransaction() {
+                getStorageHelper(dom).StorageDomainRemoved(dom.getStorageStaticData());
+                getDbFacade().getStorageDomainDynamicDAO().remove(dom.getId());
+                getDbFacade().getStorageDomainStaticDAO().remove(dom.getId());
+                return null;
+            }
+        });
 
         setSucceeded(true);
     }
@@ -188,5 +203,10 @@ public class RemoveStorageDomainCommand<T extends RemoveStorageDomainParameters>
                             new RemoveVGVDSCommandParameters(vds.getId(), dom.getstorage())).getSucceeded();
         }
         return true;
+    }
+
+    @Override
+    protected Map<String, String> getExclusiveLocks() {
+        return Collections.singletonMap(getParameters().getStorageDomainId().toString(), LockingGroup.STORAGE.name());
     }
 }

commit 453322a1e9fc7c802bf51d738b21bbd4cf16cbbe

core: Fix broken RemoveStorageDomianTest
Commit f7d691107453705bc65bc516480d2f6addb904ea broke
RemoveStorageDomainTest, since the behavior of transaction management
was not defined in the test.

This commit fixes the test by simply running the code the would have
been run in the newly introduced autonomous transaction.

Change-Id: Ib2c3e39d76a87e849c92a75aaa00c50d332ed19f
Signed-off-by: Allon Mureinik <amureini@redhat.com>

backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/storage/RemoveStorageDomainTest.java
@@ -3,6 +3,7 @@ package org.ovirt.engine.core.bll.storage;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -16,7 +17,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.ovirt.engine.core.bll.interfaces.BackendInternal;
 import org.ovirt.engine.core.common.action.DetachStorageDomainFromPoolParameters;
 import org.ovirt.engine.core.common.action.RemoveStorageDomainParameters;
@@ -49,6 +52,7 @@ import org.ovirt.engine.core.dao.StoragePoolDAO;
 import org.ovirt.engine.core.dao.StoragePoolIsoMapDAO;
 import org.ovirt.engine.core.dao.VdsDAO;
 import org.ovirt.engine.core.utils.MockConfigRule;
+import org.ovirt.engine.core.utils.transaction.TransactionMethod;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RemoveStorageDomainTest {
@@ -77,6 +81,14 @@ public class RemoveStorageDomainTest {
                 spy(new RemoveStorageDomainCommand<RemoveStorageDomainParameters>(getParams(format)));
         doReturn(db).when(cmd).getDbFacade();
         doReturn(backend).when(cmd).getBackend();
+        doAnswer(new Answer<Object>() {
+
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                return ((TransactionMethod<?>) invocation.getArguments()[0]).runInTransaction();
+            }
+
+        }).when(cmd).executeInNewTransaction(any(TransactionMethod.class));
         return cmd;
     }
 