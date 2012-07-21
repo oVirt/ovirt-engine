61f6a094c19ba04eac0d5752cde8085ec92b7a37
Daniel Erez <derez@redhat.com>
2012-07-21 20:34:22 +0300

    webadmin: LunSelectionCell - update disable on render
    Update 'disable' attribute when needed (modified render method).
    
    Change-Id: Ibb247adf135a50f3d08b7fda1a14c45663e04aee
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/LunSelectionCell.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/LunSelectionCell.java
@@ -4,7 +4,6 @@ import org.ovirt.engine.ui.common.CommonApplicationResources;
 import org.ovirt.engine.ui.uicommonweb.models.storage.LunModel;
 
 import com.google.gwt.cell.client.AbstractCell;
-import com.google.gwt.cell.client.CheckboxCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
@@ -33,12 +32,13 @@ public class LunSelectionCell extends AbstractCell<LunModel> {
             imageCell.render(context, resources.logWarningImage(), sb);
         } else {
             sb.append(SafeHtmlUtils.fromTrustedString("<span style=\"padding-left: 1px;\">")); //$NON-NLS-1$
-            if (multiSelection) {
-                new CheckboxCell(true, false).render(context, value.getIsSelected(), sb);
-            }
-            else {
-                new RadioboxCell(true, false).render(context, value.getIsSelected(), sb);
-            }
+
+            String type = multiSelection ? "type='checkbox' " : "type='radio' "; //$NON-NLS-1$ //$NON-NLS-2$
+            String checked = value.getIsSelected() ? "checked='checked' " : ""; //$NON-NLS-1$ //$NON-NLS-2$
+            String disabled = value.getIsGrayedOut() ? "disabled='disabled' " : ""; //$NON-NLS-1$ //$NON-NLS-2$
+            String input = "<input " + type + checked + disabled + " tabindex='-1'/>"; //$NON-NLS-1$ //$NON-NLS-2$
+
+            sb.append(SafeHtmlUtils.fromTrustedString(input));
             sb.append(SafeHtmlUtils.fromTrustedString("</span>")); //$NON-NLS-1$
         }
     }
