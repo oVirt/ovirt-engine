a8f66a5074c2ac8286995d259c46d09e634a09a4
Allon Mureinik <amureini@redhat.com>
2012-08-02 18:22:45 +0300

    core: Improve AutoRecoveryManagerTest
    Before this patch, the test ran the OnTime() method and checked that no
    exception was thrown.
    
    This patch extends it functionality and used Mockito's verification
    capabilities to assert that the correct recovery actions are called.
    
    Change-Id: I5410ad3b5aa383ed0145b0b03ec1b86ef63fb011
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/AutoRecoveryManagerTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/AutoRecoveryManagerTest.java
@@ -1,45 +1,64 @@
 package org.ovirt.engine.core.bll;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.ovirt.engine.core.bll.interfaces.BackendInternal;
+import org.ovirt.engine.core.common.action.VdcActionParametersBase;
+import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.storage_domains;
+import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 import org.ovirt.engine.core.dao.StorageDomainDAO;
 import org.ovirt.engine.core.dao.VdsDAO;
 import org.ovirt.engine.core.utils.MockConfigRule;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AutoRecoveryManagerTest {
     @Rule
     public static MockConfigRule mcr = new MockConfigRule();
     private AutoRecoveryManager manager;
 
+    @Mock
+    private BackendInternal backendMock;
+
+    // Entities needing recovery
+    private List<VDS> vdss = new ArrayList<VDS>();
+    private List<storage_domains> storageDomains = new ArrayList<storage_domains>();
+
     @Before
     public void setup() {
         manager = spy(AutoRecoveryManager.getInstance());
-        final BackendInternal backendMock = mock(BackendInternal.class);
         doReturn(backendMock).when(manager).getBackend();
-
         final DbFacade dbFacadeMock = mock(DbFacade.class);
         doReturn(dbFacadeMock).when(manager).getDbFacade();
         final VdsDAO vdsDaoMock = mock(VdsDAO.class);
-        final ArrayList<VDS> vdss = new ArrayList<VDS>();
+
         final VDS vds = new VDS();
         vdss.add(vds);
         when(vdsDaoMock.listFailedAutorecoverables()).thenReturn(vdss);
         when(dbFacadeMock.getVdsDAO()).thenReturn(vdsDaoMock);
 
+        storage_domains domain = new storage_domains();
+        domain.setstorage_pool_id(Guid.NewGuid());
+        storageDomains.add(domain);
         final StorageDomainDAO storageDomainDaoMock = mock(StorageDomainDAO.class);
-        final ArrayList<storage_domains> storageDomains = new ArrayList<storage_domains>();
         when(storageDomainDaoMock.listFailedAutorecoverables()).thenReturn(storageDomains);
         when(dbFacadeMock.getStorageDomainDAO()).thenReturn(storageDomainDaoMock);
     }
@@ -47,5 +66,9 @@ public class AutoRecoveryManagerTest {
     @Test
     public void onTimer() {
         manager.onTimer();
+        verify(backendMock, times(vdss.size())).runInternalAction(eq(VdcActionType.ActivateVds),
+                any(VdcActionParametersBase.class));
+        verify(backendMock, times(storageDomains.size())).runInternalAction(eq(VdcActionType.ActivateStorageDomain),
+                any(VdcActionParametersBase.class));
     }
 }
