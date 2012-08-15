27caac32662a927cb3a4a2e73c78d438d43444b8
Oved Ourfali <oourfali@redhat.com>
2012-08-15 08:43:58 +0300

    core: returning InternalAuthenticateUserCommand as it is used
    Previous ACK removed this file, which is still in use.
    Returning it.
    
    Change-Id: I1e27744f2e0d2a5c7894b8143b46d89fc26a6af2
    Signed-off-by: Oved Ourfali <oourfali@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/adbroker/InternalAuthenticateUserCommand.java
@@ -0,0 +1,47 @@
+package org.ovirt.engine.core.bll.adbroker;
+
+import org.ovirt.engine.core.common.businessentities.AdUser;
+
+
+public class InternalAuthenticateUserCommand extends InternalBrokerCommandBase {
+    public InternalAuthenticateUserCommand(LdapUserPasswordBaseParameters parameters) {
+        super(parameters);
+    }
+
+    public String getUPNForUser(String userName, String domain) {
+        String UPN = userName;
+        if (!userName.contains("@")) {
+            UPN = userName + '@' + domain;
+        }
+        return UPN;
+    }
+
+    public String getUserNameForUPN(String UPN) {
+        String userName = UPN;
+        if (userName.contains("@")) {
+            userName = userName.split("@")[0];
+        }
+        return userName;
+    }
+
+    @Override
+    protected void ExecuteQuery() {
+        String userName = getParameters().getLoginName();
+        String password = getParameters().getPassword();
+        String domain = BrokerUtils.getLoginDomain(userName, getDomain());
+        String userUPN = getUPNForUser(userName, domain);
+        userName = getUserNameForUPN(userUPN);
+        UserAuthenticationResult result = InternalBrokerUtils.authenticate(userName, password, domain);
+
+        setSucceeded(result.isSuccessful());
+
+        if (result.isSuccessful()) {
+            AdUser user = InternalBrokerUtils.getUserByUPN(userUPN);
+            UserAuthenticationResult authResult = new UserAuthenticationResult(user);
+            setReturnValue(authResult);
+        } else {
+            setReturnValue(result);
+        }
+    }
+
+}
