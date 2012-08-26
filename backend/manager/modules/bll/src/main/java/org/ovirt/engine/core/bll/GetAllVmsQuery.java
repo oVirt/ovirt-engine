1999a96f7982867858f53db5c0ce711691f816da
Omer Frenkel <ofrenkel@redhat.com>
2012-08-26 17:33:33 +0300

    core: update guest agent info on GetAllVmsQuery (#848048)
    https://bugzilla.redhat.com/show_bug.cgi?id=848048
    
    GetAllVmsQuery did not return guest agent info,
    causing user-portal not sending vmLogon, so SSO couldn't work.
    
    Change-Id: I99fdfaba14c2fee767aed48d86cf149c10439175
    Signed-off-by: Omer Frenkel <ofrenkel@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllVmsQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllVmsQuery.java
@@ -1,5 +1,8 @@
 package org.ovirt.engine.core.bll;
 
+import java.util.List;
+
+import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 
 public class GetAllVmsQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
@@ -9,7 +12,11 @@ public class GetAllVmsQuery<P extends VdcQueryParametersBase> extends QueriesCom
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade()
-                .getVmDAO().getAll(getUserID(), getParameters().isFiltered()));
+        List<VM> vmsList = getDbFacade()
+                .getVmDAO().getAll(getUserID(), getParameters().isFiltered());
+        for (VM vm : vmsList) {
+            VmHandler.UpdateVmGuestAgentVersion(vm);
+        }
+        getQueryReturnValue().setReturnValue(vmsList);
     }
 }
