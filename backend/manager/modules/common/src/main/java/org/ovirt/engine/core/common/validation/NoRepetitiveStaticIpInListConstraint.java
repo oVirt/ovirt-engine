08c70957d1b2fde3e2a864c290529da4d0a6372b
Moti Asayag <masayag@redhat.com>
2012-08-30 16:05:50 +0300

    core: Remove IP validation check from repetitive con (#852076)
    https://bugzilla.redhat.com/852076
    
    The IP Address is being validate explicitly therefore no need to refer
    to it with repetitive IP constraint.
    
    Change-Id: Iadc8e065d36c57f1a7c71fc373ce00748ff26b41
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/validation/NoRepetitiveStaticIpInListConstraint.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/validation/NoRepetitiveStaticIpInListConstraint.java
@@ -3,15 +3,12 @@ package org.ovirt.engine.core.common.validation;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.ovirt.engine.core.common.businessentities.NetworkBootProtocol;
 import org.ovirt.engine.core.common.businessentities.VdsNetworkInterface;
-import org.ovirt.engine.core.common.utils.ValidationUtils;
 import org.ovirt.engine.core.common.validation.annotation.NoRepetitiveStaticIpInList;
 
 public class NoRepetitiveStaticIpInListConstraint implements ConstraintValidator<NoRepetitiveStaticIpInList, List<VdsNetworkInterface>> {
@@ -20,8 +17,9 @@ public class NoRepetitiveStaticIpInListConstraint implements ConstraintValidator
     public boolean isValid(List<VdsNetworkInterface> value, ConstraintValidatorContext context) {
         Collection<String> staticIps = new HashSet<String>();
         for (VdsNetworkInterface networkInterface : value) {
+            String address = networkInterface.getAddress();
             if (networkInterface.getBootProtocol() == NetworkBootProtocol.StaticIp
-                    && validAddress(networkInterface.getAddress())) {
+                    && address != null && !address.isEmpty()) {
                 if (staticIps.contains(networkInterface.getAddress())) {
                     return false;
                 } else {
@@ -30,16 +28,6 @@ public class NoRepetitiveStaticIpInListConstraint implements ConstraintValidator
             }
         }
         return true;
-    }
-
-    private boolean validAddress(String address) {
-        boolean isValid = false;
-        if (address != null) {
-            Pattern pattern = Pattern.compile(ValidationUtils.IP_PATTERN);
-            Matcher matcher = pattern.matcher(address);
-            isValid = matcher.matches();
-        }
-        return isValid;
     }
 
     @Override
