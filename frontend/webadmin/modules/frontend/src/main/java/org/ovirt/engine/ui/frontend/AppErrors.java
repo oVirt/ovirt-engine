79b67cbc9172566d1377365fb8b1355b3d5415bc
Mike Kolesnik <mkolesni@redhat.com>
2012-08-30 10:51:42 +0300

    engine: Add message for network exclusivity (#851134)
    https://bugzilla.redhat.com/851134
    
    On a Host's NIC only one of two configurations is possible:
      1. A single VM network.
      2. An optional single non-VM network + Any number of VLAN networks.
    
    This message represents this fact, and will be used to enforce that this
    constraint is kept.
    
    Change-Id: I893ccf86e3436e0fdf6903d4d5e0428facd924c2
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
+++ frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
@@ -1126,6 +1126,9 @@ public interface AppErrors extends ConstantsWithLookup {
     @DefaultStringValue("Cannot ${action} ${type}. The following Logical Networks don't exist in the Host's Cluster: ${NETWORKS_DONT_EXIST_IN_CLUSTER_LIST}.")
     String NETWORKS_DONT_EXIST_IN_CLUSTER();
 
+    @DefaultStringValue("Cannot ${action} ${type}. The following Network Interfaces can have only a single VM Logical Network, or at most one non-VM Logical Network and/or several VLAN Logical Networks: ${NETWORK_INTERFACES_NOT_EXCLUSIVELY_USED_BY_NETWORK_LIST}.")
+    String NETWORK_INTERFACES_NOT_EXCLUSIVELY_USED_BY_NETWORK();
+
     @DefaultStringValue("Cannot ${action} ${type}. The following Bonds consist of less than two Network Interfaces: ${NETWORK_BONDS_INVALID_SLAVE_COUNT_LIST}.")
     String NETWORK_BONDS_INVALID_SLAVE_COUNT();
 
