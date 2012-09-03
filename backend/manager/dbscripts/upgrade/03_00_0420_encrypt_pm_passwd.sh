edc7fd0bf8108cb168c24240f04a768d28563837
Eli Mesika <emesika@redhat.com>
2012-09-03 12:28:07 +0300

    core: rhevm-upgrade: encrypt vds_static pm_pass..
    rhevm-upgrade: encrypt vds_static pm_password field on upgrade to 3.1
    
    Adding check for store-utils.sh exit value and log an error message if
    password encryption fail for any host.
    In such a scenario, this host must be edited manually to set it PM
    properties.
    
    Change-Id: I5b99768d4bdec8673fc0c8c2b0f329f54701bede
    Signed-off-by: Eli Mesika <emesika@redhat.com>

--- backend/manager/dbscripts/upgrade/03_00_0420_encrypt_pm_passwd.sh
+++ backend/manager/dbscripts/upgrade/03_00_0420_encrypt_pm_passwd.sh
@@ -17,7 +17,7 @@ execute_command "${CMD}" "${DATABASE}" ${SERVERNAME} ${PORT} > /dev/null
 
 # get all hosts that have PM configured (vds_id and pm_password)
 filename=$(mktemp)
-CMD="select vds_id,pm_password from vds_static where pm_enabled = true;"
+CMD="select vds_id,vds_name,pm_password from vds_static where pm_enabled = true;"
 execute_command "${CMD}" "${DATABASE}" ${SERVERNAME} ${PORT} > ${filename}
 while read line
 do
@@ -26,12 +26,17 @@ do
       continue
   fi
   hostId=$(echo "${line}" | cut -d "|" -f1 | sed 's/^ *//g' | tr -d ' ')
-  pmPasswd=$(echo "${line}" | cut -d "|" -f2 | sed 's/^ *//g' | tr -d ' ')
+  hostName=$(echo "${line}" | cut -d "|" -f2 | sed 's/^ *//g' | tr -d ' ')
+  pmPasswd=$(echo "${line}" | cut -d "|" -f3 | sed 's/^ *//g' | tr -d ' ')
   if [ "$hostId" != "" -a "$pmPasswd" != "" ]; then
       # encrypt the password
       encryptedPasswd=$(${caBaseDir}/store-utils.sh -enc ${keystore}  ${passwd} ${alias} ${pmPasswd} ${ear})
-      # update the pm_password field for the given host
-      CMD="update vds_static set pm_password = '${encryptedPasswd}' where vds_id = '${hostId}';"
-      execute_command "${CMD}" "${DATABASE}" ${SERVERNAME} ${PORT} > /dev/null
+      if [ $? -ne 0 ]; then
+          echo "Failed to encrypt host ${hostName} Power Management password, please set host Power Management manually for this host."
+      else
+          # update the pm_password field for the given host
+          CMD="update vds_static set pm_password = '${encryptedPasswd}' where vds_id = '${hostId}';"
+          execute_command "${CMD}" "${DATABASE}" ${SERVERNAME} ${PORT} > /dev/null
+      fi
   fi
 done < ${filename}
