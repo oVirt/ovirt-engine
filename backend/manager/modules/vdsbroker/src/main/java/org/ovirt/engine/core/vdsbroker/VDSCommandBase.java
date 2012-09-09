oVirt/ovirt-engine

commit 35c2e7ceb5109c9349e3699c3f53414cbe0b4c4e

core: add vdsName to vdsBroker log info (#853883)
https://bugzilla.redhat.com/show_bug.cgi?id=853883

Logger gets VdsCommandBase.toString() method's output,
this patch adds call to getAdditionalInformation from
toString to add to log string VdsName.

Change-Id: Ieb0589bb5f5e157c04f85e46f6769d66ef98b2d1
Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/VDSCommandBase.java
@@ -46,7 +46,9 @@ public abstract class VDSCommandBase<P extends VDSParametersBase> extends VdcCom
 
     @Override
     public String toString() {
-        return String.format("%s(%s)", super.toString(),
+        String AddInfo = getAdditionalInformation();
+        return String.format("%s(%s %s)", super.toString(),
+                (!AddInfo.isEmpty() ? AddInfo + "," : ""),
                 (getParameters() != null ? getParameters().toString() : "null"));
     }
 
@@ -111,6 +113,8 @@ public abstract class VDSCommandBase<P extends VDSParametersBase> extends VdcCom
     private void logException(RuntimeException ex) {
         log.errorFormat("Command {0} execution failed. Exception: {1}", getCommandName(), ExceptionUtils.getMessage(ex));
     }
+
+    protected abstract String getAdditionalInformation();
 
     protected abstract void ExecuteVDSCommand();
 

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/IsVmDuringInitiatingVDSCommand.java
@@ -3,7 +3,7 @@ package org.ovirt.engine.core.vdsbroker;
 import org.ovirt.engine.core.common.vdscommands.*;
 
 public class IsVmDuringInitiatingVDSCommand<P extends IsVmDuringInitiatingVDSCommandParameters>
-        extends VDSCommandBase<P> {
+extends VDSCommandBase<P> {
     public IsVmDuringInitiatingVDSCommand(P parameters) {
         super(parameters);
     }
@@ -11,6 +11,11 @@ public class IsVmDuringInitiatingVDSCommand<P extends IsVmDuringInitiatingVDSCom
     @Override
     protected void ExecuteVDSCommand() {
         getVDSReturnValue().setReturnValue(ResourceManager.getInstance()
-                                                          .IsVmDuringInitiating(getParameters().getVmId()));
+                .IsVmDuringInitiating(getParameters().getVmId()));
+    }
+
+    @Override
+    protected String getAdditionalInformation() {
+        return "";
     }
 }

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/SetVmStatusVDSCommand.java
@@ -40,4 +40,9 @@ public class SetVmStatusVDSCommand<P extends SetVmStatusVDSCommandParameters> ex
         }
         DbFacade.getInstance().getVmDynamicDAO().update(vmDynamic);
     }
+
+    @Override
+    protected String getAdditionalInformation() {
+        return "";
+    }
 }

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/VdsBrokerCommand.java
@@ -62,7 +62,7 @@ public abstract class VdsBrokerCommand<P extends VdsIdVDSCommandParametersBase> 
     @Override
     protected String getAdditionalInformation() {
         if (getVds() != null) {
-            return String.format("Vds: %1$s", getVds().getvds_name());
+            return String.format("Vds = %1$s", getVds().getvds_name());
         } else {
             return super.getAdditionalInformation();
         }

commit 5177fbed84c71ae9cea07c3ee77c30e3faef176a

engine: Adding default implementation to abstract methods
Commit 35c2e7ceb5109c9349e3699c3f53414cbe0b4c4e broke the build by
introducing an abstract method which isn't implmemented by other
non-abstract classes.

Change-Id: I5a8478062b5f9ad545ad8bdf87d01fa05b6686f0
Signed-off-by: Moti Asayag <masayag@redhat.com>

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/VDSCommandBase.java
@@ -5,9 +5,9 @@ import org.ovirt.engine.core.common.errors.VDSError;
 import org.ovirt.engine.core.common.errors.VdcBllErrors;
 import org.ovirt.engine.core.common.vdscommands.VDSParametersBase;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
+import org.ovirt.engine.core.dal.VdcCommandBase;
 import org.ovirt.engine.core.utils.log.Log;
 import org.ovirt.engine.core.utils.log.LogFactory;
-import org.ovirt.engine.core.dal.VdcCommandBase;
 import org.ovirt.engine.core.vdsbroker.irsbroker.IRSErrorException;
 import org.ovirt.engine.core.vdsbroker.vdsbroker.VDSExceptionBase;
 import org.ovirt.engine.core.vdsbroker.vdsbroker.VDSNetworkException;
@@ -114,7 +114,9 @@ public abstract class VDSCommandBase<P extends VDSParametersBase> extends VdcCom
         log.errorFormat("Command {0} execution failed. Exception: {1}", getCommandName(), ExceptionUtils.getMessage(ex));
     }
 
-    protected abstract String getAdditionalInformation();
+    protected String getAdditionalInformation() {
+        return "";
+    }
 
     protected abstract void ExecuteVDSCommand();
 