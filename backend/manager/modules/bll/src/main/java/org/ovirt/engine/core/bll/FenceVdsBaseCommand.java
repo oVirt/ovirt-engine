cb564a3bd54dd5f3be137d91ce4dcdd67ab406bd
Eli Mesika <emesika@redhat.com>
2012-11-12 23:01:51 +0200

    core:Fence Host fails if something ...(#871768)
    Fence Host fails if something went wrong in FenceQuietTimeBetweenOperationsInSec window [180seconds]
    
    We have a configuration setting of Quiet Time Between PM Operations,
    this should control PM operations invoked by the user.
    The intension is to prevent subsequent calls to PM stop/start without
    waiting to a stable status.
    
    Adding a condition in the FenceVdsBaseCommand canDoAction validation ,
    not to check Quiet Time Between PM Operations if the command is internal
    (i.e. was invoked by the system and not by the user)
    
    Change-Id: I2cab006a6e911d1eaad888c5c4e41bb4aca68f03
    Signed-off-by: Eli Mesika <emesika@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/FenceVdsBaseCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/FenceVdsBaseCommand.java
@@ -100,8 +100,8 @@ public abstract class FenceVdsBaseCommand<T extends FenceVdsActionParameters> ex
                             .AddSeconds((Integer) Config.GetValue(ConfigValues.DisableFenceAtStartupInSec));
             java.util.Date now = new java.util.Date();
             if (waitTo.before(now) || waitTo.equals(now)) {
-                // Check Quiet time between PM operations, this is done only if parent command is not <Restart>
-                int secondsLeftToNextPmOp = ((getParameters().getParentCommand() == VdcActionType.RestartVds))
+                // Check Quiet time between PM operations, this is done only if command is not internal and parent command is not <Restart>
+                int secondsLeftToNextPmOp = (isInternalExecution() || (getParameters().getParentCommand() == VdcActionType.RestartVds))
                         ?
                         0
                         :
