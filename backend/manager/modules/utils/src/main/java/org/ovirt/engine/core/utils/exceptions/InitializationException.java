2919e4ac810e272d49614d9ef41139fafd53a5e5
Yair Zaslavsky <yzaslavs@redhat.com>
2012-12-03 14:20:28 +0200

    core: Removing jdk7 extended CTOR
    Change-Id: If25ef37b0d9ff79737e7dfe8283322acb70b1dcb
    Bug-Url: https://bugzilla.redhat.com/878964
    signed-off-by: Yair Zaslavsky <yzaslavs@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/exceptions/InitializationException.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/exceptions/InitializationException.java
@@ -20,12 +20,4 @@ public class InitializationException extends Exception {
     public InitializationException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    public InitializationException(String message,
-            Throwable cause,
-            boolean enableSuppression,
-            boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
 }
