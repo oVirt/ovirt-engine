oVirt/ovirt-engine

commit 1d3c67c9cf7846ab73e6411fb8aebff8a29f9cfb

core: better general VDSM exception handling
refactor error translation to separate network exceptions and protocol
exception so error on XML formatting could be propagated and logged
correctly.

Change-Id: Id2391ec81626cd984f64b9a738adca9be4116d5a
Signed-off-by: Roy Golan <rgolan@redhat.com>
Bug-Url: https://bugzilla.redhat.com/875725

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/VdsBrokerCommand.java
@@ -4,6 +4,7 @@ import java.net.ConnectException;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.errors.VDSError;
 import org.ovirt.engine.core.common.errors.VdcBLLException;
 import org.ovirt.engine.core.common.errors.VdcBllErrors;
 import org.ovirt.engine.core.common.vdscommands.VdsIdVDSCommandParametersBase;
@@ -19,6 +20,7 @@ import org.ovirt.engine.core.vdsbroker.xmlrpc.XmlRpcRunTimeException;
 public abstract class VdsBrokerCommand<P extends VdsIdVDSCommandParametersBase> extends BrokerCommandBase<P> {
     private final IVdsServer mVdsBroker;
     private VDS mVds;
+    private static final String msgFormat = "XML RPC error in command {0} ( {1} ), the error was: {2}, {3} ";
 
     /**
      * Construct the command using the parameters and the {@link VDS} which is loaded from the DB.
@@ -98,37 +100,27 @@ public abstract class VdsBrokerCommand<P extends VdsIdVDSCommandParametersBase> 
             PrintReturnValue();
             throw new VDSProtocolException(ex);
         } catch (XmlRpcRunTimeException ex) {
-            final String msgFormat = "XML RPC error in command {0} ( {1} ), the error was: {2}, {3} ";
-            if ((ExceptionUtils.getRootCause(ex) instanceof ConnectException)) {
+            Throwable rootCause = ExceptionUtils.getRootCause(ex);
+            if ((ex.isNetworkError() || rootCause instanceof ConnectException)) {
                 log.debugFormat(msgFormat,
                         getCommandName(),
                         getAdditionalInformation(),
                         ex.getMessage(),
-                        ExceptionUtils.getRootCauseMessage(ex));
+                        rootCause.getMessage());
+                PrintReturnValue();
+                throw new VDSNetworkException(ex);
             } else {
                 log.errorFormat(msgFormat,
                         getCommandName(),
                         getAdditionalInformation(),
                         ex.getMessage(),
-                        ExceptionUtils.getRootCauseMessage(ex));
+                        rootCause.getMessage());
+                PrintReturnValue();
+                VDSProtocolException vdsProtocolException = new VDSProtocolException(rootCause.getMessage());
+                vdsProtocolException.setVdsError(new VDSError(VdcBllErrors.PROTOCOL_ERROR, rootCause.getMessage()));
+                throw vdsProtocolException;
             }
-            PrintReturnValue();
-            throw new VDSNetworkException(ex);
         }
-        // catch (WebException ex)
-        // {
-        // // log this exception in debug becaue it is being logged again later.
-        // log.infoFormat("Failed in {0} method", getCommandName());
-        // log.info("Exception", ex);
-        // throw new VDSNetworkException(ex);
-        // }
-
-        // catch (NullReferenceException ex)
-        // {
-        // PrintReturnValue();
-        // //This is a workaround a bug in the xml-rpc package
-        // throw new VDSNetworkException(ex);
-        // }
 
         // TODO: look for invalid certificates error handling
         catch (RuntimeException e) {

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RunVmCommand.java
@@ -211,6 +211,7 @@ public class RunVmCommand<T extends RunVmParams> extends RunVmCommandBase<T>
                 switch (errorCode) {
                 case Done: // should never get here with errorCode = 'Done' though
                 case exist:
+                case PROTOCOL_ERROR: // probably wrong xml format sent.
                     throw e;
                 default:
                 }

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/errors/VdcBllErrors.java
@@ -354,6 +354,7 @@ public enum VdcBllErrors {
     // oVirt errors
     ENGINE(5001),
     DB(5002),
+    PROTOCOL_ERROR(5003),
     // The VDS does not exist in memory
     RESOURCE_MANAGER_VDS_NOT_FOUND(5004),
     IRS_IMAGE_STATUS_ILLEGAL(5006),

commit 6b7ae33a8ef9682adf00bf2495487d3617ffc99b

engine: revisit improving VDSM error handling
- fix a major issue caused by 1d3c67c9cf7846ab73e6411fb8aebff8a29f9cfb where VdsManager expects
  VdsNetworkException on network issue.
- improved the identifying network error by catching all IOExceptions.
  this ease catching corner case protocol error.
- refactor and clean VdsNetworkException and its tree to use a
  constructor with a Throwable - cleaned the unuseful "{ClassName}: "
  convention which is already implemented in Throwable toString().

Change-Id: I9df04879b15575842da1bd53c39c00a6623374e0
Signed-off-by: Roy Golan <rgolan@redhat.com>

backend/manager/modules/compat/src/main/java/org/ovirt/engine/core/compat/ApplicationException.java
@@ -11,6 +11,10 @@ public class ApplicationException extends RuntimeException {
         super(string);
     }
 
+    public ApplicationException(Throwable ex) {
+        super(ex);
+    }
+
     public ApplicationException(String string, Throwable ex) {
         super(string, ex);
     }

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/VDSExceptionBase.java
@@ -4,6 +4,9 @@ import org.ovirt.engine.core.compat.*;
 import org.ovirt.engine.core.common.errors.*;
 
 public class VDSExceptionBase extends ApplicationException {
+
+    private static final long serialVersionUID = 5709501011970689110L;
+
     private VDSError privateVdsError;
 
     public VDSError getVdsError() {
@@ -14,12 +17,12 @@ public class VDSExceptionBase extends ApplicationException {
         privateVdsError = value;
     }
 
-    // public VDSExceptionBase(SerializationInfo info, StreamingContext context)
-    // {
-    // super(info, context);
-    // }
-    public VDSExceptionBase(String errMessage, RuntimeException baseException) {
+    public VDSExceptionBase(String errMessage, Throwable baseException) {
         super(errMessage, baseException);
+    }
+
+    public VDSExceptionBase(Throwable baseException) {
+        super(baseException);
     }
 
     public VDSExceptionBase(String errMessage) {

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/VDSGenericException.java
@@ -3,17 +3,15 @@ package org.ovirt.engine.core.vdsbroker.vdsbroker;
 import org.ovirt.engine.core.common.errors.*;
 
 public class VDSGenericException extends VDSExceptionBase implements java.io.Serializable {
-    // protected VDSGenericException(SerializationInfo info, StreamingContext
-    // context)
-    // {
-    // super(info, context);
-    // }
-    public VDSGenericException(String message, RuntimeException baseException) {
+
+    private static final long serialVersionUID = 4778043822136178263L;
+
+    public VDSGenericException(String message, Throwable baseException) {
         super(message, baseException);
     }
 
-    public VDSGenericException(RuntimeException baseException) {
-        super("VDSGenericException: ", baseException);
+    public VDSGenericException(Throwable baseException) {
+        super(baseException);
     }
 
     public VDSGenericException(String errorStr) {

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/VDSNetworkException.java
@@ -1,13 +1,11 @@
 package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
 public class VDSNetworkException extends VDSGenericException implements java.io.Serializable {
-    // protected VDSNetworkException(SerializationInfo info, StreamingContext
-    // context)
-    // {
-    // super(info, context);
-    // }
-    public VDSNetworkException(RuntimeException baseException) {
-        super("VDSNetworkException: ", baseException);
+
+    private static final long serialVersionUID = -486466100359278549L;
+
+    public VDSNetworkException(Throwable baseException) {
+        super(baseException);
     }
 
     public VDSNetworkException(String errorStr) {

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/VdsBrokerCommand.java
@@ -1,7 +1,6 @@
 package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
-import java.net.ConnectException;
-
+import java.io.IOException;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.errors.VDSError;
@@ -101,25 +100,23 @@ public abstract class VdsBrokerCommand<P extends VdsIdVDSCommandParametersBase> 
             throw new VDSProtocolException(ex);
         } catch (XmlRpcRunTimeException ex) {
             Throwable rootCause = ExceptionUtils.getRootCause(ex);
-            if ((ex.isNetworkError() || rootCause instanceof ConnectException)) {
+            VDSNetworkException networkException = new VDSNetworkException(rootCause);
+            if ((ex.isNetworkError() || rootCause instanceof IOException)) {
                 log.debugFormat(msgFormat,
                         getCommandName(),
                         getAdditionalInformation(),
                         ex.getMessage(),
                         rootCause.getMessage());
-                PrintReturnValue();
-                throw new VDSNetworkException(ex);
             } else {
                 log.errorFormat(msgFormat,
                         getCommandName(),
                         getAdditionalInformation(),
                         ex.getMessage(),
                         rootCause.getMessage());
-                PrintReturnValue();
-                VDSProtocolException vdsProtocolException = new VDSProtocolException(rootCause.getMessage());
-                vdsProtocolException.setVdsError(new VDSError(VdcBllErrors.PROTOCOL_ERROR, rootCause.getMessage()));
-                throw vdsProtocolException;
+                networkException.setVdsError(new VDSError(VdcBllErrors.PROTOCOL_ERROR, rootCause.toString()));
             }
+            PrintReturnValue();
+            throw networkException;
         }
 
         // TODO: look for invalid certificates error handling