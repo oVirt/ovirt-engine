9486bfd3f6e9c110c106607e97a3c721677b95fe
Laszlo Hornyak <lhornyak@redhat.com>
2012-12-13 10:25:19 +0100

    engine: checkstyle checkstyle
    do not import the logger, it is not used
    
    Change-Id: Ieb3a57805badbba5978a9c9c7e5b08b468aa0f7f
    Signed-off-by: Laszlo Hornyak <lhornyak@redhat.com>

--- build-tools-root/ovirt-checkstyle-extension/src/main/java/checks/NlsCheck.java
+++ build-tools-root/ovirt-checkstyle-extension/src/main/java/checks/NlsCheck.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.logging.Log;
-
 import com.puppycrawl.tools.checkstyle.api.Check;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 
