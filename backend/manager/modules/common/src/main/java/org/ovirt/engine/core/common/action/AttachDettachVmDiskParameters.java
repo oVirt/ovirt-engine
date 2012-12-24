76334eb478f7823243fb89e663a7d2f16b52bf62
Vered Volansky <vvolansk@redhat.com>
2012-12-24 18:48:30 +0200

    core: cleanup AttachDettachVmDiskParameters c'tor
    AttachDettachVmDiskParameters has two c'tors. Implementation was
    separate although one could depend on the other, which is what happens
    now.
    
    Change-Id: Ifd3d5dd8adbfda6cf67f61b9f1fa2eb337fb6d73
    Signed-off-by: Vered Volansky <vvolansk@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AttachDettachVmDiskParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AttachDettachVmDiskParameters.java
@@ -7,16 +7,14 @@ public class AttachDettachVmDiskParameters extends VmDiskOperationParameterBase 
     private static final long serialVersionUID = 5640716432695539552L;
     private boolean isPlugUnPlug;
 
-    public AttachDettachVmDiskParameters(Guid vmId, Guid diskId) {
-        super(vmId, null);
-        setEntityId(diskId);
-        this.isPlugUnPlug = false;
-    }
-
     public AttachDettachVmDiskParameters(Guid vmId, Guid diskId, boolean isPlugUnPlug) {
         super(vmId, null);
         setEntityId(diskId);
         this.isPlugUnPlug = isPlugUnPlug;
+    }
+
+    public AttachDettachVmDiskParameters(Guid vmId, Guid diskId) {
+        this(vmId, diskId, false);
     }
 
     public void setPlugUnPlug(boolean isPlugUnPlug) {
