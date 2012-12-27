bae8b7e5100af402f5902731ac2f9cb41e38e0b1
Mike Kolesnik <mkolesni@redhat.com>
2012-12-27 09:16:08 +0200

    engine: Move hot plug command to network.vm package
    The ActivateDeactivateVmNicCommand should sit in this package as it
    handles VM NIC.
    
    Change-Id: I1bd93a7062ebd396911d88988ce41473682da475
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/ActivateDeactivateVmNicCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/ActivateDeactivateVmNicCommand.java
@@ -1,7 +1,9 @@
-package org.ovirt.engine.core.bll;
+package org.ovirt.engine.core.bll.network.vm;
 
 import java.util.List;
 
+import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
+import org.ovirt.engine.core.bll.VmCommand;
 import org.ovirt.engine.core.bll.utils.VmDeviceUtils;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.action.ActivateDeactivateVmNicParameters;
