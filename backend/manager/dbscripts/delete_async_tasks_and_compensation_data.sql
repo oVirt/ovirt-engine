4e96ec2fe1c98660cf5453e44ca1e66474b51e03
Maor Lipchuk <mlipchuk@redhat.com>
2012-12-30 17:16:08 +0200

    core: Upgrade will update image statuses to OK.
    As part of the upgrade process, engine cancel the tasks and
    removes them.
    part of this behaviour we also want to change the status of the disk
    to OK, to avoid cases that the VM cannot be restarted.
    
    The proposed solution changes all the LOCKED statuses of images to OK.
    
    Change-Id: I2a6bd270ebd2125252a4b155f050db103969cb5e
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/dbscripts/delete_async_tasks_and_compensation_data.sql
+++ backend/manager/dbscripts/delete_async_tasks_and_compensation_data.sql
@@ -2,7 +2,10 @@ truncate table  async_tasks;
 
 truncate table business_entity_snapshot;
 
-
+-- Change status from LOCKED to OK after upgrade since the tasks will be cacelled.
+UPDATE images
+SET imagestatus = 1
+WHERE imagestatus = 2;
 
 
 
