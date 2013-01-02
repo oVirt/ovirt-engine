8b7321bc3dbde67703eac9bd865218e8af258956
Moti Asayag <masayag@redhat.com>
2013-01-02 17:37:08 +0200

    core: Organize NetworkCluster class
    Move ctor and properties to the beginning of the class.
    
    Change-Id: I1eaef749c234dfa2898f467ba542e39e443a1481
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/network/NetworkCluster.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/network/NetworkCluster.java
@@ -37,60 +37,6 @@ public class NetworkCluster extends IVdcQueryable implements BusinessEntity<Netw
     public NetworkCluster() {
     }
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("NetworkCluster {id=")
-                .append(getId())
-                .append(", status=")
-                .append(getStatus())
-                .append(", display=")
-                .append(isDisplay())
-                .append(", required=")
-                .append(isRequired())
-                .append("}");
-        return builder.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (display ? 1231 : 1237);
-        result = prime * result + ((status == null) ? 0 : status.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + (required ? 11 : 13);
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        NetworkCluster other = (NetworkCluster) obj;
-        if (display != other.display)
-            return false;
-        if (status == null) {
-            if (other.status != null)
-                return false;
-        } else if (!status.equals(other.status))
-            return false;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (required != other.required) {
-            return false;
-        }
-        return true;
-    }
-
-
     public NetworkCluster(Guid clusterId, Guid networkId, NetworkStatus status, boolean display, boolean required) {
         id.setClusterId(clusterId);
         id.setNetworkId(networkId);
@@ -152,5 +98,58 @@ public class NetworkCluster extends IVdcQueryable implements BusinessEntity<Netw
     @Override
     public Object getQueryableId() {
         return getId();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("NetworkCluster {id=")
+                .append(getId())
+                .append(", status=")
+                .append(getStatus())
+                .append(", display=")
+                .append(isDisplay())
+                .append(", required=")
+                .append(isRequired())
+                .append("}");
+        return builder.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (display ? 1231 : 1237);
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + (required ? 11 : 13);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        NetworkCluster other = (NetworkCluster) obj;
+        if (display != other.display)
+            return false;
+        if (status == null) {
+            if (other.status != null)
+                return false;
+        } else if (!status.equals(other.status))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (required != other.required) {
+            return false;
+        }
+        return true;
     }
 }
