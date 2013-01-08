2e1fb7ade6261074579bb3f7ee60d9610450f7f1
Eli Mesika <emesika@redhat.com>
2013-01-08 18:50:19 +0200

    core: fix drop old uuid functions for pg9.x
    In pg9.x we can not drop the functions without dropping first the
    extension (uuid-ossp)
    This patch adds a DROP EXTENSION of uuid-ossp for pg 9.x
    
    Change-Id: I45ecf68b27cc04d4592b30e23c774b87e451cabc
    Signed-off-by: Eli Mesika <emesika@redhat.com>

--- backend/manager/dbscripts/drop_old_uuid_functions.sql
+++ backend/manager/dbscripts/drop_old_uuid_functions.sql
@@ -1,11 +1,26 @@
---Drops all old UUID functions since we have an internal implementation
-drop function if exists  uuid_nil();
-drop function if exists  uuid_ns_dns();
-drop function if exists  uuid_ns_url();
-drop function if exists  uuid_ns_oid();
-drop function if exists  uuid_ns_x500();
-drop function if exists  uuid_generate_v1();
-drop function if exists  uuid_generate_v1mc();
-drop function if exists  uuid_generate_v3(namespace uuid, name text);
-drop function if exists  uuid_generate_v4();
-drop function if exists  uuid_generate_v5(namespace uuid, name text);
+create function __temp_drop_old_uuid_functions()
+RETURNS void
+as $procedure$
+begin
+    -- removing first the extension from pg9.x installations
+    if exists (select 1 from information_schema.views where table_name = 'pg_available_extensions') then
+      -- The - is special character , so need to put -- in the name
+      EXECUTE 'DROP EXTENSION IF EXISTS uuid--ossp CASCADE;';
+    end if;
+
+    --Drops all old UUID functions since we have an internal implementation
+    drop function if exists  uuid_nil();
+    drop function if exists  uuid_ns_dns();
+    drop function if exists  uuid_ns_url();
+    drop function if exists  uuid_ns_oid();
+    drop function if exists  uuid_ns_x500();
+    drop function if exists  uuid_generate_v1();
+    drop function if exists  uuid_generate_v1mc();
+    drop function if exists  uuid_generate_v3(namespace uuid, name text);
+    drop function if exists  uuid_generate_v4();
+    drop function if exists  uuid_generate_v5(namespace uuid, name text);
+end; $procedure$
+LANGUAGE plpgsql;
+
+select __temp_drop_old_uuid_functions();
+drop function __temp_drop_old_uuid_functions();
