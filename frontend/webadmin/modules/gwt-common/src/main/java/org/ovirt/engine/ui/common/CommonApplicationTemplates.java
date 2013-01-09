b657f45a758848cde03308be56900200d505f051
Daniel Erez <derez@redhat.com>
2013-01-09 17:38:15 +0200

    webadmin: fixing buttons text misalignment
    Removed redundant 'margin-right:3px' from dialogButton template.
    * Causing an horizontal misalignment in text position
      on OK/Cancel/Close buttons. I.e. text is not aligned to center.
    * Checked on FF/Chorme/IE.
    
    Change-Id: I4a00915b9521e5c2d14dca97e57e8495e4ae6f90
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationTemplates.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationTemplates.java
@@ -32,7 +32,7 @@ public interface CommonApplicationTemplates extends SafeHtmlTemplates {
             "<td style='background: url({2});width:2px;'></td>" +
             "<td style='text-align:center;'>" +
             "<div class='{5} {6}' style='background: url({3}) repeat-x; height: 20px;'>" +
-            "<span style='vertical-align: middle; margin-right: 3px; line-height: 20px;'>{0}</span>{1}</div>" +
+            "<span style='vertical-align: middle; line-height: 20px;'>{0}</span>{1}</div>" +
             "</td>" +
             "<td style='background: url({4});width:2px;'></td>" +
             "</tr></table>")
