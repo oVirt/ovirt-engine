e2180a10a5e0aefa2df9e6ebbcbde2029bdb571f
Laszlo Hornyak <lhornyak@redhat.com>
2013-01-09 10:36:30 +0100

    engine: fix for VmHandlerTest
    Adds "quotaDefault" to VmHandler's list of updatable fields.
    
    Change-Id: I3dbe50e518a8fb310a3df59ecafab1e99963d21a
    Signed-off-by: Laszlo Hornyak <lhornyak@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VmHandler.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VmHandler.java
@@ -87,7 +87,8 @@ public class VmHandler {
                 "vmPayload", "balloonEnabled", "smartcardEnabled","deleteProtected","dbGeneration",
                 "images", // images list is relational entity - ignore value changes
                 "interfaces", // interfaces is relational entity - ignore value changes
-                "useHostCpuFlags"
+                "useHostCpuFlags",
+                "quotaDefault"
         });
         mUpdateVmsStatic.AddFields(
                 Arrays.asList(new Enum<?>[] { VMStatus.Down }),
