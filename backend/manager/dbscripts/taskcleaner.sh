01802407cb03a6c96932010671522bb3002beb46
Eli Mesika <emesika@redhat.com>
2013-01-15 11:38:17 +0200

    core:Async Tasks cleaner utility...
    Adding a utility to clean async tasks.
    The utility enables to
     Display
         All async tasks
         Only Zombie tasks
      Delete
          All tasks
          All Zombie tasks
          A task related to a given task id
          A Zombie task related to a given task id
          All tasks related to a given command id
          All Zombie tasks related to a given command id
       Flags may be added (-C, -J) to specify if Job Steps & Compensation
    data
       should be cleaned as well.
    
    Usage: taskcleaner.sh [-h] [-s server] [-p PORT]] [-d DATABASE] [-u
    USERNAME] [-l LOGFILE]  [-t taskId] [-c commandId] [-z] [-R] [-C]
    [-J] [-q] [-v]
    
    -s SERVERNAME - The database servername for the database  (def.
    localhost)
    -p PORT       - The database port for the database        (def.
    5432)
    -d DATABASE   - The database name                         (def.
    engine)
    -u USERNAME   - The admin username for the database.
    -l LOGFILE    - The logfile for capturing output          (def.
    taskcleaner.sh.log)
    -t TASK_ID    - Removes a task by its Task ID.
    -c COMMAND_ID - Removes all tasks related to the given Command
    Id.
    -z            - Removes/Displays a Zombie task.
    -R            - Removes all Zombie tasks.
    -C            - Clear related compensation entries.
    -J            - Clear related Job Steps.
    -q            - Quite mode, do not prompt for confirmation.
    -v            - Turn on verbosity
    (WARNING: lots of output)
    -h            - This help text.
    
    Change-Id: Idffc35eb5fca0e72c9e4c9e3a08c5b9799569fca
    Signed-off-by: Eli Mesika <emesika@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=891639
    Signed-off-by: Eli Mesika <emesika@redhat.com>

---
+++ backend/manager/dbscripts/taskcleaner.sh
@@ -0,0 +1,234 @@
+###############################################################################################################
+# The purpose of this utility is to display and clean asynchronous tasks and corresponding
+# Job steps/Compensation data.
+# The utility enables to
+# Display
+#     All async tasks
+#     Only Zombie tasks
+# Delete
+#     All tasks
+#     All Zombie tasks
+#     A task related to a given task id
+#     A Zombie task related to a given task id
+#     All tasks related to a given command id
+#     All Zombie tasks related to a given command id
+#  Flags may be added (-C, -J) to specify if Job Steps & Compensation data
+#  should be cleaned as well.
+###############################################################################################################
+
+#!/bin/bash
+pushd $(dirname ${0})
+
+#include db general functions
+source ./dbfunctions.sh
+source ./dbcustomfunctions.sh
+
+#setting defaults
+set_defaults
+
+
+usage() {
+    printf "Usage: ${ME} [-h] [-s server] [-p PORT]] [-d DATABASE] [-u USERNAME] [-l LOGFILE]  [-t taskId] [-c commandId] [-z] [-R] [-C] [-J] [-q] [-v]\n"
+    printf "\n"
+    printf "\t-s SERVERNAME - The database servername for the database  (def. ${SERVERNAME})\n"
+    printf "\t-p PORT       - The database port for the database        (def. ${PORT})\n"
+    printf "\t-d DATABASE   - The database name                         (def. ${DATABASE})\n"
+    printf "\t-u USERNAME   - The admin username for the database.\n"
+    printf "\t-l LOGFILE    - The logfile for capturing output          (def. ${LOGFILE})\n"
+    printf "\t-t TASK_ID    - Removes a task by its Task ID.\n"
+    printf "\t-c COMMAND_ID - Removes all tasks related to the given Command Id.\n"
+    printf "\t-z            - Removes/Displays a Zombie task.\n"
+    printf "\t-R            - Removes all Zombie tasks.\n"
+    printf "\t-C            - Clear related compensation entries.\n"
+    printf "\t-J            - Clear related Job Steps.\n"
+    printf "\t-q            - Quite mode, do not prompt for confirmation.\n"
+    printf "\t-v            - Turn on verbosity                         (WARNING: lots of output)\n"
+    printf "\t-h            - This help text.\n"
+    printf "\n"
+    popd
+    exit $ret
+}
+
+DEBUG () {
+    if $VERBOSE; then
+        printf "DEBUG: $*"
+    fi
+}
+
+#Using two variables for sql commands in order to control command priority where data should be removed first from
+#business_entity_snapshot and step table before removing it from the async_tasks table.
+CMD1="";
+CMD2="";
+TASK_ID=""
+COMMAND_ID=""
+ZOMBIES_ONLY=false
+CLEAR_ALL=false
+CLEAR_COMPENSATION=false
+CLEAR_JOB_STEPS=false
+QUITE_MODE=false
+FIELDS="task_id,task_type,status,started_at,result,action_type as command_type,command_id,step_id,storage_pool_id as DC"
+
+while getopts hs:d:u:p:l:t:c:zRCJqv option; do
+    case $option in
+        s) SERVERNAME=$OPTARG;;
+        p) PORT=$OPTARG;;
+        d) DATABASE=$OPTARG;;
+        u) USERNAME=$OPTARG;;
+        l) LOGFILE=$OPTARG;;
+        t) TASK_ID=$OPTARG;;
+        c) COMMAND_ID=$OPTARG;;
+        z) ZOMBIES_ONLY=true;;
+        R) CLEAR_ALL=true;;
+        C) CLEAR_COMPENSATION=true;;
+        J) CLEAR_JOB_STEPS=true;;
+        q) QUITE_MODE=true;;
+        v) VERBOSE=true;;
+        h) ret=0 && usage;;
+       \?) ret=1 && usage;;
+    esac
+done
+
+caution() {
+    if [ "${QUITE_MODE}" = "false" ]; then
+        # Highlight the expected results of selected operation.
+        echo $(tput smso) $1 $(tput rmso)
+        echo "Caution, this operation should be used with care. Please contact support prior to running this command"
+        echo "Are you sure you want to proceed? [y/n]"
+        read answer
+        if [ "${answer}" = "n" ]; then
+           echo "Please contact support for further assistance."
+           popd
+           exit 1
+        fi
+    fi
+}
+
+if [ "${TASK_ID}" != "" -o "${COMMAND_ID}" != "" -o  "${CLEAR_ALL}" = "true" -o "${CLEAR_COMPENSATION}" = "true" -o "${CLEAR_JOB_STEPS}" = "true" ]; then #delete operations block
+    if [ "${TASK_ID}" != "" ]; then
+        if [ "${ZOMBIES_ONLY}" = "true" ]; then
+            CMD2="SELECT DeleteAsyncTaskZombiesByTaskId('${TASK_ID}');"
+            if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                CMD1="SELECT DeleteJobStepsByTaskId('${TASK_ID}');"
+                if [ "${CLEAR_COMPENSATION}" = "true" ]; then
+                    caution "This will remove the given Zombie Task, its Job Steps and related Compensation data!!!"
+                    CMD1="${CMD1}SELECT DeleteEntitySnapshotByZombieTaskId('${TASK_ID}');"
+                else
+                    caution "This will remove the given Zombie Task and its related Job Steps!!!"
+                fi
+            else
+                if [ "${CLEAR_COMPENSATION}" = "true" ]; then
+                    caution "This will remove the given Zombie Task and related Compensation data!!!"
+                    CMD1="${CMD1}SELECT DeleteEntitySnapshotByZombieTaskId('${TASK_ID}');"
+                else
+                    caution "This will remove the given Zombie Task!!!"
+                fi
+            fi
+        else
+            CMD2="SELECT Deleteasync_tasks('${TASK_ID}');"
+            if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                CMD1="SELECT DeleteJobStepsByTaskId('${TASK_ID}');"
+                if [ "${CLEAR_COMPENSATION}" = "true" ]; then
+                    caution "This will remove the given Task its Job Steps and related Compensation data!!!"
+                    CMD1="${CMD1}SELECT DeleteEntitySnapshotByTaskId('${TASK_ID}');"
+                else
+                    caution "This will remove the given Task and its related Job Steps!!!"
+                fi
+            else
+                if [ "${CLEAR_COMPENSATION}" = "true" ]; then
+                    caution "This will remove the given Task and its related Compensation data!!!"
+                    CMD1="${CMD1}SELECT DeleteEntitySnapshotByTaskId('${TASK_ID}');"
+                else
+                    caution "This will remove the given Task!!!"
+                fi
+            fi
+        fi
+    elif [ "${COMMAND_ID}" != "" ]; then
+        if [ "${ZOMBIES_ONLY}" = "true" ]; then
+            CMD2="SELECT DeleteAsyncTaskZombiesByCommandId('${COMMAND_ID}');"
+            if [ "${CLEAR_COMPENSATION}" = "true" ]; then
+                CMD1="SELECT delete_entity_snapshot_by_command_id('${COMMAND_ID}');"
+                if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                    caution "This will remove all Zombie Tasks of the given Command its Job Steps and its related Compensation data!!!"
+                    CMD1="${CMD1}SELECT DeleteJobStepsByZombieCommandId('${COMMAND_ID}');"
+                else
+                    caution "This will remove all Zombie Tasks of the given Command and its related Compensation data!!!"
+                fi
+            else
+                if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                    caution "This will remove all Zombie Tasks of the given Command and its Job Steps!!!"
+                    CMD1="${CMD1}SELECT DeleteJobStepsByZombieCommandId('${COMMAND_ID}');"
+                else
+                    caution "This will remove all Zombie Tasks of the given Command!!!"
+                fi
+            fi
+        else
+            CMD2="SELECT DeleteAsyncTaskByCommandId('${COMMAND_ID}');"
+            if [ "${CLEAR_COMPENSATION}" = "true" ]; then
+                CMD1="SELECT delete_entity_snapshot_by_command_id('${COMMAND_ID}');"
+                if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                    caution "This will remove all Tasks of the given Command its Job Steps and its related Compensation data!!!"
+                    CMD1="${CMD1}SELECT DeleteJobStepsByCommandId('${COMMAND_ID}');"
+                else
+                    caution "This will remove all Tasks of the given Command and its related Compensation data!!!"
+                fi
+            else
+                if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                    caution "This will remove all Tasks of the given Command and its Job Steps!!!"
+                    CMD1="${CMD1}SELECT DeleteJobStepsByCommandId('${COMMAND_ID}');"
+                else
+                    caution "This will remove all Tasks of the given Command!!!"
+                fi
+            fi
+        fi
+    elif [ "${CLEAR_ALL}" = "true" ]; then
+        if [ "${ZOMBIES_ONLY}" = "true" ]; then
+            CMD2="SELECT DeleteAsyncTasksZombies();"
+            if [ "${CLEAR_COMPENSATION}" = "true" ]; then
+                CMD1="${CMD1}SELECT DeleteEntitySnapshotZombies();"
+                if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                    caution "This will remove all Zombie Tasks in async_tasks table, its related Job Steps and Compensation data!!!"
+                    CMD1="${CMD1}SELECT DeleteJobStepsZombies();"
+                else
+                    caution "This will remove all Zombie Tasks in async_tasks table and its related Compensation data!!!"
+                fi
+            else
+                if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                    caution "This will remove all Zombie Tasks in async_tasks table and its related Job Steps!!!"
+                    CMD1="${CMD1}SELECT DeleteJobStepsZombies();"
+                else
+                    caution "This will remove all Zombie Tasks in async_tasks table!!!"
+                fi
+            fi
+        else
+            CMD2="TRUNCATE TABLE async_tasks cascade;"
+            if [ "${CLEAR_COMPENSATION}" = "true" ]; then
+                CMD1="TRUNCATE TABLE business_entity_snapshot cascade;"
+                if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                    caution "This will remove all Tasks in async_tasks table, its related Job Steps and Compensation data!!!"
+                    CMD1="${CMD1}TRUNCATE TABLE step cascade;"
+                else
+                    caution "This will remove all async_tasks table content and its related Compensation data!!!"
+                fi
+            else
+                if [ "${CLEAR_JOB_STEPS}" = "true" ]; then
+                    caution "This will remove all Tasks in async_tasks table and its related Job Steps!!!"
+                    CMD1="${CMD1}TRUNCATE TABLE step cascade;"
+                else
+                    caution "This will remove all async_tasks table content!!!"
+                fi
+            fi
+        fi
+    else
+        usage
+    fi
+elif [ "${ZOMBIES_ONLY}" = "true" ]; then #only display operations block
+
+        CMD1="SELECT ${FIELDS} FROM GetAsyncTasksZombies();"
+else
+        CMD1="SELECT ${FIELDS} FROM GetAllFromasync_tasks();"
+fi
+
+psql -U ${USERNAME} -h ${SERVERNAME} -p ${PORT} -c "${CMD1}${CMD2}" -x ${DATABASE}
+
+popd
+exit $?
---
+++ backend/manager/dbscripts/taskcleaner_sp.sql
@@ -0,0 +1,106 @@
+/************************************************************************************************
+ The following are helper SP for taskcleaner utility and are not exposed to the application DAOs
+************************************************************************************************/
+CREATE OR REPLACE FUNCTION GetAsyncTasksZombies() RETURNS SETOF async_tasks
+   AS $procedure$
+DECLARE
+    zombie_task_life varchar;
+    zombie_date date;
+BEGIN
+   zombie_task_life = option_value FROM vdc_options WHERE option_name = 'AsyncTaskZombieTaskLifeInMinutes';
+   EXECUTE 'SELECT now() - interval ''' || zombie_task_life || ' minute'''  INTO zombie_date;
+
+   RETURN QUERY SELECT *
+   FROM async_tasks
+   WHERE started_at < zombie_date
+   ORDER BY command_id;
+
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteAsyncTaskZombiesByTaskId(v_task_id UUID) RETURNS VOID
+   AS $procedure$
+BEGIN
+    IF EXISTS (SELECT 1 FROM GetAsyncTasksZombies() WHERE task_id = v_task_id) THEN
+        PERFORM Deleteasync_tasks(v_task_id);
+    END IF;
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteAsyncTaskZombiesByCommandId(v_command_id UUID) RETURNS VOID
+   AS $procedure$
+BEGIN
+    IF EXISTS (SELECT 1 FROM GetAsyncTasksZombies() WHERE command_id = v_command_id) THEN
+        DELETE FROM async_tasks WHERE command_id = v_command_id;
+    END IF;
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteAsyncTaskByCommandId(v_command_id UUID) RETURNS VOID
+   AS $procedure$
+BEGIN
+        DELETE FROM async_tasks WHERE command_id = v_command_id;
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteAsyncTasksZombies() RETURNS VOID
+   AS $procedure$
+BEGIN
+    DELETE FROM async_tasks WHERE task_id in (SELECT task_id FROM GetAsyncTasksZombies());
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteJobStepsByTaskId(v_task_id UUID) RETURNS VOID
+   AS $procedure$
+BEGIN
+    DELETE FROM step WHERE external_id = v_task_id;
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteEntitySnapshotByZombieTaskId(v_task_id UUID) RETURNS VOID
+   AS $procedure$
+DECLARE
+    v_command_id UUID;
+BEGIN
+    v_command_id:=command_id FROM GetAsyncTasksZombies() WHERE task_id = v_task_id;
+    DELETE FROM business_entity_snapshot WHERE command_id = v_command_id;
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteEntitySnapshotByTaskId(v_task_id UUID) RETURNS VOID
+   AS $procedure$
+DECLARE
+    v_command_id UUID;
+BEGIN
+    v_command_id:=command_id FROM async_tasks WHERE task_id = v_task_id;
+    DELETE FROM business_entity_snapshot WHERE command_id = v_command_id;
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteEntitySnapshotZombies() RETURNS VOID
+   AS $procedure$
+BEGIN
+    DELETE FROM business_entity_snapshot WHERE command_id IN (SELECT command_id FROM GetAsyncTasksZombies());
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteJobStepsByZombieCommandId(v_command_id UUID) RETURNS VOID
+   AS $procedure$
+BEGIN
+    DELETE FROM step WHERE step_id IN (SELECT step_id from GetAsyncTasksZombies() WHERE command_id = v_command_id);
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteJobStepsByCommandId(v_command_id UUID) RETURNS VOID
+   AS $procedure$
+BEGIN
+    DELETE FROM step WHERE step_id IN (SELECT step_id from async_tasks WHERE command_id = v_command_id);
+END; $procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteJobStepsZombies() RETURNS VOID
+   AS $procedure$
+BEGIN
+    DELETE FROM step WHERE step_id IN (SELECT step_id FROM GetAsyncTasksZombies());
+END; $procedure$
+LANGUAGE plpgsql;
