oVirt/ovirt-engine

commit 29887aba0ec992151c4e964fe540d3bb3532292e

engine:  VM Linux Boot options aren't parsing properly
Steps to Reproduce:
1. have VM & init & kernel file on ISO domain
2. use Boot params =>  kernel/initrd params
3. put to initrd: "iso://rhel6.initrd " (without quotes, with space at the end)

After starting the VM it runs for a moment and then it crashes with VDSM error:
libvirtError('virDomainCreateXML() failed', conn=self).
Fixed by adding validation of the user input (on frontend) and setting of
VmBase parameters (on backend).

Change-Id: I9ccb4fc0b807c77bc17cfe02903341cdf4e51d54
Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=872178

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmBase.java
@@ -28,7 +28,7 @@ import org.ovirt.engine.core.compat.NGuid;
 public class VmBase extends IVdcQueryable implements BusinessEntity<Guid> {
     private static final long serialVersionUID = 1078548170257965614L;
     private ArrayList<DiskImage> images;
-    private ArrayList<DiskImage> diskList = new ArrayList<DiskImage>();
+    private final ArrayList<DiskImage> diskList = new ArrayList<DiskImage>();
     private List<VmNetworkInterface> interfaces;
     private Map<Guid, VmDevice> managedDeviceMap = new HashMap<Guid, VmDevice>();
     private List<VmDevice> unmanagedDeviceList = new ArrayList<VmDevice>();
@@ -126,14 +126,23 @@ public class VmBase extends IVdcQueryable implements BusinessEntity<Guid> {
 
     @Size(max = BusinessEntitiesDefinitions.GENERAL_MAX_SIZE)
     @Column(name = "kernel_url", length = BusinessEntitiesDefinitions.GENERAL_MAX_SIZE)
+    @Pattern(regexp = ValidationUtils.NO_TRIMMING_WHITE_SPACES_PATTERN,
+            message = "ACTION_TYPE_FAILED_LINUX_BOOT_PARAMS_MAY_NOT_CONTAIN_TRIMMING_WHITESPACES", groups = { CreateEntity.class,
+                    UpdateEntity.class })
     private String kernelUrl;
 
     @Size(max = BusinessEntitiesDefinitions.GENERAL_MAX_SIZE)
     @Column(name = "kernel_params", length = BusinessEntitiesDefinitions.GENERAL_MAX_SIZE)
+    @Pattern(regexp = ValidationUtils.NO_TRIMMING_WHITE_SPACES_PATTERN,
+            message = "ACTION_TYPE_FAILED_LINUX_BOOT_PARAMS_MAY_NOT_CONTAIN_TRIMMING_WHITESPACES", groups = { CreateEntity.class,
+                    UpdateEntity.class })
     private String kernelParams;
 
     @Size(max = BusinessEntitiesDefinitions.GENERAL_MAX_SIZE)
     @Column(name = "initrd_url", length = BusinessEntitiesDefinitions.GENERAL_MAX_SIZE)
+    @Pattern(regexp = ValidationUtils.NO_TRIMMING_WHITE_SPACES_PATTERN,
+            message = "ACTION_TYPE_FAILED_LINUX_BOOT_PARAMS_MAY_NOT_CONTAIN_TRIMMING_WHITESPACES", groups = { CreateEntity.class,
+                    UpdateEntity.class })
     private String initrdUrl;
 
     @Column(name = "allow_console_reconnect")

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/UnitVmModel.java
@@ -10,15 +10,14 @@ import java.util.List;
 
 import org.ovirt.engine.core.common.businessentities.BootSequence;
 import org.ovirt.engine.core.common.businessentities.DisplayType;
+import org.ovirt.engine.core.common.businessentities.QuotaEnforcementTypeEnum;
 import org.ovirt.engine.core.common.businessentities.UsbPolicy;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.VDSGroup;
 import org.ovirt.engine.core.common.businessentities.VmOsType;
-import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.businessentities.VmType;
 import org.ovirt.engine.core.common.businessentities.storage_domains;
 import org.ovirt.engine.core.common.businessentities.storage_pool;
-import org.ovirt.engine.core.common.businessentities.QuotaEnforcementTypeEnum;
 import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Event;
 import org.ovirt.engine.core.compat.EventArgs;
@@ -45,8 +44,8 @@ import org.ovirt.engine.ui.uicommonweb.validation.I18NNameValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.IValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.IntegerValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.LengthValidation;
-import org.ovirt.engine.ui.uicommonweb.validation.NotEmptyValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.NotEmptyQuotaValidation;
+import org.ovirt.engine.ui.uicommonweb.validation.NotEmptyValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.SpecialAsciiI18NOrNoneValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.ValidationResult;
 import org.ovirt.engine.ui.uicompat.ConstantsManager;
@@ -1977,83 +1976,79 @@ public class UnitVmModel extends Model {
             int maxMemSize = is64OsType ? get_MaxMemSize64() : get_MaxMemSize32();
 
             ValidateMemorySize(getMemSize(), maxMemSize, _minMemSize);
-            if (!(this.getBehavior() instanceof TemplateVmModelBehavior))
-            {
+            if (!(this.getBehavior() instanceof TemplateVmModelBehavior)) {
                 // Minimum 'Physical Memory Guaranteed' is 1MB
                 ValidateMemorySize(getMinAllocatedMemory(), (Integer) getMemSize().getEntity(), 1);
             }
         }
 
-        if ((Boolean) getIsAutoAssign().getEntity() == false)
-        {
+        if ((Boolean) getIsAutoAssign().getEntity() == false) {
             getDefaultHost().ValidateSelectedItem(new IValidation[] { new NotEmptyValidation() });
-        }
-        else
-        {
+        } else {
             getDefaultHost().setIsValid(true);
         }
-
-        VmTemplate template = (VmTemplate) getTemplate().getSelectedItem();
-        storage_domains storageDomain = (storage_domains) getStorageDomain().getSelectedItem();
 
         getTemplate().ValidateSelectedItem(new IValidation[] { new NotEmptyValidation() });
         getDisksAllocationModel().ValidateEntity(new IValidation[] {});
 
         getCdImage().setIsValid(true);
-        if (getCdImage().getIsChangable())
-        {
+        if (getCdImage().getIsChangable()) {
             getCdImage().ValidateSelectedItem(new IValidation[] { new NotEmptyValidation() });
         }
 
         getKernel_path().setIsValid(true);
         getKernel_parameters().setIsValid(true);
         getInitrd_path().setIsValid(true);
-        if (getKernel_path().getEntity() == null)
-        {
+        if (getKernel_path().getEntity() == null) {
             getKernel_path().setEntity(""); //$NON-NLS-1$
         }
-        if (getKernel_parameters().getEntity() == null)
-        {
+        if (getKernel_parameters().getEntity() == null) {
             getKernel_parameters().setEntity(""); //$NON-NLS-1$
         }
-        if (getInitrd_path().getEntity() == null)
-        {
+        if (getInitrd_path().getEntity() == null) {
             getInitrd_path().setEntity(""); //$NON-NLS-1$
         }
 
-        if (isLinux_Unassign_UnknownOS
-                && ((((String) getKernel_parameters().getEntity()).length() > 0 || ((String) getInitrd_path().getEntity()).length() > 0) && ((String) getKernel_path().getEntity()).length() == 0))
-        {
-            boolean kernelParamInvalid = false;
-            boolean inetdPathInvalid = false;
-            if (((String) getKernel_parameters().getEntity()).length() > 0)
-            {
-                getKernel_parameters().setIsValid(false);
-                kernelParamInvalid = true;
-            }
-            if (((String) getInitrd_path().getEntity()).length() > 0)
-            {
-                getInitrd_path().setIsValid(false);
-                inetdPathInvalid = true;
-            }
+        if (isLinux_Unassign_UnknownOS) {
+            getKernel_path().ValidateEntity(new IValidation[] { new NoTrimmingWhitespacesValidation() });
+            getInitrd_path().ValidateEntity(new IValidation[] { new NoTrimmingWhitespacesValidation() });
+            getKernel_parameters().ValidateEntity(new IValidation[] { new NoTrimmingWhitespacesValidation() });
 
-            String msg =
-                    ConstantsManager.getInstance()
-                            .getMessages()
-                            .invalidPath(kernelParamInvalid ? ConstantsManager.getInstance()
-                                    .getConstants()
-                                    .kernelInvalid() : "", //$NON-NLS-1$
-                                    kernelParamInvalid && inetdPathInvalid ? ConstantsManager.getInstance()
-                                            .getConstants()
-                                            .or() : "", //$NON-NLS-1$
-                                    inetdPathInvalid ? ConstantsManager.getInstance()
-                                            .getConstants()
-                                            .inetdInvalid() : ""); //$NON-NLS-1$
+            String kernelPath = (String) getKernel_path().getEntity();
+            String initrdPath = (String) getInitrd_path().getEntity();
+            String kernelParams = (String) getKernel_parameters().getEntity();
 
-            getKernel_path().setIsValid(false);
-            getInitrd_path().getInvalidityReasons().add(msg);
-            getKernel_parameters().getInvalidityReasons().add(msg);
-            getKernel_path().getInvalidityReasons().add(msg);
+            if ((kernelParams.length() > 0 || initrdPath.length() > 0) && kernelPath.length() == 0) {
+                boolean kernelParamInvalid = false;
+                boolean inetdPathInvalid = false;
+
+                if (kernelParams.length() > 0) {
+                    getKernel_parameters().setIsValid(false);
+                    kernelParamInvalid = true;
+                }
+                if (initrdPath.length() > 0) {
+                    getInitrd_path().setIsValid(false);
+                    inetdPathInvalid = true;
+                }
+
+                String msg =
+                        ConstantsManager.getInstance()
+                                .getMessages()
+                                .invalidPath(kernelParamInvalid ? ConstantsManager.getInstance()
+                                        .getConstants()
+                                        .kernelInvalid() : "", //$NON-NLS-1$
+                                        kernelParamInvalid && inetdPathInvalid ? ConstantsManager.getInstance()
+                                                .getConstants()
+                                                .or() : "", //$NON-NLS-1$
+                                        inetdPathInvalid ? ConstantsManager.getInstance()
+                                                .getConstants()
+                                                .inetdInvalid() : ""); //$NON-NLS-1$
+
+                getKernel_path().setIsValid(false);
+                getInitrd_path().getInvalidityReasons().add(msg);
+                getKernel_parameters().getInvalidityReasons().add(msg);
+                getKernel_path().getInvalidityReasons().add(msg);
+            }
         }
 
         boolean customPropertySheetValid = getCustomPropertySheet().validate();
@@ -2080,8 +2075,11 @@ public class UnitVmModel extends Model {
                 && getDefaultHost().getIsValid() && getMemSize().getIsValid() && getMinAllocatedMemory().getIsValid()
                 && getNumOfMonitors().getIsValid() && getDomain().getIsValid() && getUsbPolicy().getIsValid()
                 && getTimeZone().getIsValid() && getOSType().getIsValid() && getCdImage().getIsValid()
-                && getKernel_path().getIsValid() && behavior.Validate()
+                && getKernel_path().getIsValid()
+                && getKernel_parameters().getIsValid() && getInitrd_path().getIsValid()
+                && behavior.Validate()
                 && customPropertySheetValid && getQuota().getIsValid();
+
     }
 
     class TotalCpuCoresComposableValidation implements IValidation {

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/ValidationUtils.java
@@ -25,6 +25,7 @@ public class ValidationUtils {
     public static final String DOMAIN_NAME_PATTERN =
             "^([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}$";
     public static final String NO_WHITES_SPACE_PATTERN = "\\S+";
+    public static final String NO_TRIMMING_WHITE_SPACES_PATTERN = "^$|\\S.*\\S";
     public static final String IP_PATTERN =
             "^\\b((25[0-5]|2[0-4]\\d|[01]\\d\\d|\\d?\\d)\\.){3}(25[0-5]|2[0-4]\\d|[01]\\d\\d|\\d?\\d)\\b$|^$";
     // NULLABLE_MAC_ADDRESS can be valid mac address: xx:xx:xx:xx:xx:xx or empty string,

backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/ValidationUtilsTest.java
@@ -2,6 +2,8 @@ package org.ovirt.engine.core.common.utils;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 
 public class ValidationUtilsTest {
@@ -31,6 +33,22 @@ public class ValidationUtilsTest {
 
         for (String s : straInvalidHosts) {
             assertTrue("Invalid host name: " + s, !ValidationUtils.validHostname(s));
+        }
+    }
+
+    @Test
+    public void testTrimmingWhitespaces() {
+        String[] validStrings = {"", "aoeu", "a o e u ř", "%2123 o ^ ooe#"};
+        String[] inValidStrings = {" ", " aoeu", "a o e u ř ", " %2123 o ^ ooe##", " aoeu "};
+
+        for (String s : validStrings) {
+            assertTrue("Valid strings (no trimming whitespaces)",
+                    Pattern.matches(ValidationUtils.NO_TRIMMING_WHITE_SPACES_PATTERN, s));
+        }
+
+        for (String s : inValidStrings) {
+            assertTrue("Invalid strings (trimming whitespaces)",
+                    !Pattern.matches(ValidationUtils.NO_TRIMMING_WHITE_SPACES_PATTERN, s));
         }
     }
 }

backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dal/VdcBllMessages.java
@@ -176,6 +176,7 @@ public enum VdcBllMessages {
     ACTION_TYPE_FAILED_NAME_LENGTH_IS_TOO_LONG,
     ACTION_TYPE_FAILED_NAME_MAY_NOT_BE_EMPTY,
     ACTION_TYPE_FAILED_NAME_MAY_NOT_CONTAIN_SPECIAL_CHARS,
+    ACTION_TYPE_FAILED_LINUX_BOOT_PARAMS_MAY_NOT_CONTAIN_TRIMMING_WHITESPACES,
     ACTION_TYPE_FAILED_NAME_MAY_NOT_CONTAIN_SPECIAL_CHARS_OR_DASH,
     ACTION_TYPE_FAILED_INVALID_VDS_HOSTNAME,
     ACTION_TYPE_FAILED_HOSNAME_CANNOT_CHANGE,

backend/manager/modules/dal/src/main/resources/bundles/AppErrors.properties
@@ -629,6 +629,7 @@ ACTION_TYPE_FAILED_INVALID_CUSTOM_VM_PROPERTIES_DUPLICATE_KEYS=Cannot ${action} 
 ACTION_TYPE_FAILED_HOSNAME_CANNOT_CHANGE=Host Address can not be modified due to Security restrictions.  In order to change Host Address, Host has to be reinstalled
 CAN_DO_ACTION_DATABASE_CONNECTION_FAILURE=Action failed due to database connection failure. Please check connectivity to your Database server.
 ACTION_TYPE_FAILED_DESCRIPTION_MAY_NOT_CONTAIN_SPECIAL_CHARS=Cannot ${action} ${type}. The given description contains special characters.\nOnly alpha-numeric and some special characters that conform to the standard ASCII character set are allowed.
+ACTION_TYPE_FAILED_LINUX_BOOT_PARAMS_MAY_NOT_CONTAIN_TRIMMING_WHITESPACES=Cannot ${action} ${type}. Linux boot parameters contain trimming whitespace characters.
 ACTION_TYPE_FAILED_NAME_LENGTH_IS_TOO_LONG=Cannot ${action} ${type}. The given name is too long.
 ACTION_TYPE_FAILED_NAME_MAY_NOT_BE_EMPTY=Can not ${action} ${type}. The given name is empty.
 ACTION_TYPE_FAILED_NAME_MAY_NOT_CONTAIN_SPECIAL_CHARS=Can not ${action} ${type}. The given name contains special characters. Only lower-case and upper-case letters, numbers, '_', '-', '.' are allowed.

frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
@@ -1689,6 +1689,9 @@ public interface AppErrors extends ConstantsWithLookup {
     @DefaultStringValue("Cannot ${action} ${type}. The given description contains special characters.\nOnly alpha-numeric and some special characters that conform to the standard ASCII character set are allowed.")
     String ACTION_TYPE_FAILED_DESCRIPTION_MAY_NOT_CONTAIN_SPECIAL_CHARS();
 
+    @DefaultStringValue("Cannot ${action} ${type}. Linux boot parameters contain trimming whitespace characters.")
+    String ACTION_TYPE_FAILED_LINUX_BOOT_PARAMS_MAY_NOT_CONTAIN_TRIMMING_WHITESPACES();
+
     @DefaultStringValue("Cannot ${action} ${type}. The given name is too long.")
     String ACTION_TYPE_FAILED_NAME_LENGTH_IS_TOO_LONG();
 

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/validation/NoTrimmingWhitespacesValidation.java
@@ -1,0 +1,12 @@
+package org.ovirt.engine.ui.uicommonweb.validation;
+
+import org.ovirt.engine.ui.uicompat.ConstantsManager;
+
+public class NoTrimmingWhitespacesValidation extends RegexValidation {
+
+    public NoTrimmingWhitespacesValidation() {
+        setExpression("^$|^\\S.*\\S$"); //$NON-NLS-1$
+        setMessage(ConstantsManager.getInstance().getConstants().trimmingSpacesInField());
+    }
+
+}

commit 4e9901141a12193a9e38b1230b51d42ffb1b029d

engine: Fixed broken imports
This patch fixes broken imports introduced by commit
29887aba0ec992151c4e964fe540d3bb3532292e.

Change-Id: Idbb108755dd96752721a91cfc62fec08fe213aee
Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmBase.java
@@ -10,6 +10,7 @@ import javax.persistence.Column;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -17,6 +18,7 @@ import org.hibernate.annotations.Type;
 import org.ovirt.engine.core.common.businessentities.OvfExportOnlyField.ExportOption;
 import org.ovirt.engine.core.common.businessentities.network.VmNetworkInterface;
 import org.ovirt.engine.core.common.config.ConfigValues;
+import org.ovirt.engine.core.common.utils.ValidationUtils;
 import org.ovirt.engine.core.common.validation.annotation.IntegerContainedInConfigValueList;
 import org.ovirt.engine.core.common.validation.annotation.ValidDescription;
 import org.ovirt.engine.core.common.validation.group.CreateEntity;

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/UnitVmModel.java
@@ -44,6 +44,7 @@ import org.ovirt.engine.ui.uicommonweb.validation.I18NNameValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.IValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.IntegerValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.LengthValidation;
+import org.ovirt.engine.ui.uicommonweb.validation.NoTrimmingWhitespacesValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.NotEmptyQuotaValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.NotEmptyValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.SpecialAsciiI18NOrNoneValidation;