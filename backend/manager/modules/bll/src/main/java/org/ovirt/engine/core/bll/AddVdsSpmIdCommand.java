oVirt/ovirt-engine

commit 4b9a4cfaee613e3254735f06df339840ebc8c4fc

core: Validate maximum number of hosts in DC.(#771699)
http://bugzilla.redhat.com/show_bug.cgi?id=771699

when a host is added, there should be a check whether the maximum number
of
hosts attached to DC has been exceeded.

If max number of hosts is exceeded, user should be presented with an
appropriate message.

Change-Id: If375400f3e12e3e0452053dea12ac6e28bc0ff61
Signed-off-by: Liron Aravot <laravot@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVdsSpmIdCommand.java
@@ -6,6 +6,10 @@ import java.util.Map;
 
 import org.ovirt.engine.core.common.action.VdsActionParameters;
 import org.ovirt.engine.core.common.businessentities.vds_spm_id_map;
+import org.ovirt.engine.core.common.config.Config;
+import org.ovirt.engine.core.common.config.ConfigValues;
+import org.ovirt.engine.core.common.errors.VdcBllErrors;
+import org.ovirt.engine.core.common.errors.VdcFault;
 import org.ovirt.engine.core.common.locks.LockingGroup;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
@@ -13,8 +17,9 @@ import org.ovirt.engine.core.utils.linq.Function;
 import org.ovirt.engine.core.utils.linq.LinqUtils;
 
 @InternalCommandAttribute
-@LockIdNameAttribute(isWait=true)
+@LockIdNameAttribute(isWait = true)
 public class AddVdsSpmIdCommand<T extends VdsActionParameters> extends VdsCommand<T> {
+    private List<vds_spm_id_map> vds_spm_id_mapList;
 
     /**
      * Constructor for command creation when compensation is applied on startup
@@ -31,24 +36,36 @@ public class AddVdsSpmIdCommand<T extends VdsActionParameters> extends VdsComman
 
     @Override
     protected boolean canDoAction() {
-        return !Guid.Empty.equals(getVds().getstorage_pool_id())
-                && DbFacade.getInstance().getVdsSpmIdMapDao().get(getVdsId()) == null;
+        // check if vds already has vds spm id and storage pool exists
+        if (Guid.Empty.equals(getVds().getstorage_pool_id())) {
+            return false;
+        }
+        vds_spm_id_mapList = DbFacade.getInstance().getVdsSpmIdMapDao().getAll(
+                getVds().getstorage_pool_id());
+        if (vds_spm_id_mapList.size() >= Config.<Integer> GetValue(ConfigValues.MaxNumberOfHostsInStoragePool)) {
+            VdcFault fault = new VdcFault();
+            fault.setError(VdcBllErrors.ReachedMaxNumberOfHostsInDC);
+            fault.setMessage(Backend.getInstance()
+                    .getVdsErrorsTranslator()
+                    .TranslateErrorTextSingle(fault.getError().toString()));
+            getReturnValue().setFault(fault);
+            return false;
+        }
+        return true;
     }
 
     @Override
     protected void executeCommand() {
         // according to shaharf the first id is 1
         int selectedId = 1;
-        List<vds_spm_id_map> list = DbFacade.getInstance().getVdsSpmIdMapDao().getAll(
-                getVds().getstorage_pool_id());
-        List<Integer> map = LinqUtils.foreach(list, new Function<vds_spm_id_map, Integer>() {
+        List<Integer> list = LinqUtils.foreach(vds_spm_id_mapList, new Function<vds_spm_id_map, Integer>() {
             @Override
             public Integer eval(vds_spm_id_map vds_spm_id_map) {
                 return vds_spm_id_map.getvds_spm_id();
             }
         });
-        Collections.sort(map);
-        for (int id : map) {
+        Collections.sort(list);
+        for (int id : list) {
             if (selectedId == id) {
                 selectedId++;
             } else {

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateVdsGroupCommand.java
@@ -60,18 +60,9 @@ public class UpdateVdsGroupCommand<T extends VdsGroupOperationParameters> extend
                 && !oldGroup.getstorage_pool_id().equals(getVdsGroup().getstorage_pool_id()))
                 || (oldGroup.getstorage_pool_id() == null
                 && getVdsGroup().getstorage_pool_id() != null)) {
+            VdsActionParameters parameters = new VdsActionParameters();
             for (VDS vds : allForVdsGroup) {
-                VdsActionParameters parameters = new VdsActionParameters(vds.getId());
-                if (oldGroup.getstorage_pool_id() != null) {
-                    VdcReturnValueBase removeVdsSpmIdReturn =
-                            getBackend().runInternalAction(VdcActionType.RemoveVdsSpmId,
-                                    parameters);
-                    if (!removeVdsSpmIdReturn.getSucceeded()) {
-                        setSucceeded(false);
-                        getReturnValue().setFault(removeVdsSpmIdReturn.getFault());
-                        return;
-                    }
-                }
+                parameters.setVdsId(vds.getId());
                 if (getVdsGroup().getstorage_pool_id() != null) {
                     VdcReturnValueBase addVdsSpmIdReturn =
                             getBackend().runInternalAction(VdcActionType.AddVdsSpmId, parameters);
@@ -80,6 +71,12 @@ public class UpdateVdsGroupCommand<T extends VdsGroupOperationParameters> extend
                         getReturnValue().setFault(addVdsSpmIdReturn.getFault());
                         return;
                     }
+                }
+            }
+
+            if (oldGroup.getstorage_pool_id() != null) {
+                for (VDS vds : allForVdsGroup) {
+                    getVdsSpmIdMapDAO().removeByVdsAndStoragePool(vds.getId(), oldGroup.getstorage_pool_id().getValue());
                 }
             }
         }

backend/manager/dbscripts/vds_spm_id_map_sp.sql
@@ -38,6 +38,21 @@ LANGUAGE plpgsql;
 
 
 
+Create or replace FUNCTION DeleteByPoolvds_spm_id_map(v_vds_id UUID, v_storage_pool_id UUID)
+RETURNS VOID
+   AS $procedure$
+BEGIN
+
+   DELETE FROM vds_spm_id_map
+   WHERE vds_id = v_vds_id AND storage_pool_id = v_storage_pool_id;
+
+END; $procedure$
+LANGUAGE plpgsql;
+
+
+
+
+
 Create or replace FUNCTION GetAllFromvds_spm_id_map() RETURNS SETOF vds_spm_id_map
    AS $procedure$
 BEGIN

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/ChangeVDSClusterCommand.java
@@ -111,6 +111,7 @@ public class ChangeVDSClusterCommand<T extends ChangeVDSClusterParameters> exten
             setSucceeded(true);
             return;
         }
+
         // save the new cluster id
         TransactionSupport.executeInNewTransaction(new TransactionMethod<Void>() {
             @Override
@@ -123,6 +124,23 @@ public class ChangeVDSClusterCommand<T extends ChangeVDSClusterParameters> exten
                 return null;
             }
         });
+
+        getParameters().setCompensationEnabled(true);
+        getParameters().setTransactionScopeOption(TransactionScopeOption.RequiresNew);
+
+        if (targetStoragePool != null
+                && (getSourceCluster().getstorage_pool_id()== null || !targetStoragePool.getId().equals(getSourceCluster().getstorage_pool_id().getValue()))) {
+            VdcReturnValueBase addVdsSpmIdReturn =
+                    Backend.getInstance().runInternalAction(VdcActionType.AddVdsSpmId,
+                            getParameters(),
+                            new CommandContext(getCompensationContext()));
+            if (!addVdsSpmIdReturn.getSucceeded()) {
+                setSucceeded(false);
+                getReturnValue().setFault(addVdsSpmIdReturn.getFault());
+                return;
+            }
+        }
+
 
         if (getSourceCluster().supportsGlusterService() && getClusterUtils().hasServers(getSourceCluster().getId())) {
             if (!glusterHostRemove(getSourceCluster().getId())) {
@@ -137,32 +155,11 @@ public class ChangeVDSClusterCommand<T extends ChangeVDSClusterParameters> exten
             }
         }
 
-        // handle spm
-        getParameters().setCompensationEnabled(true);
-        getParameters().setTransactionScopeOption(TransactionScopeOption.RequiresNew);
-        if (getSourceCluster().getstorage_pool_id() != null) {
-            VdcReturnValueBase removeVdsSpmIdReturn =
-                    Backend.getInstance().runInternalAction(VdcActionType.RemoveVdsSpmId,
-                            getParameters(),
-                            new CommandContext(getCompensationContext()));
-            if (!removeVdsSpmIdReturn.getSucceeded()) {
-                setSucceeded(false);
-                getReturnValue().setFault(removeVdsSpmIdReturn.getFault());
-                return;
-            }
+        if (getSourceCluster().getstorage_pool_id() != null
+                && (targetStoragePool== null || !getSourceCluster().getstorage_pool_id().getValue().equals(targetStoragePool.getId()))) {
+            getVdsSpmIdMapDAO().removeByVdsAndStoragePool(getVds().getId(), getSourceCluster().getstorage_pool_id().getValue());
         }
 
-        if (targetStoragePool != null) {
-            VdcReturnValueBase addVdsSpmIdReturn =
-                    Backend.getInstance().runInternalAction(VdcActionType.AddVdsSpmId,
-                            getParameters(),
-                            new CommandContext(getCompensationContext()));
-            if (!addVdsSpmIdReturn.getSucceeded()) {
-                setSucceeded(false);
-                getReturnValue().setFault(addVdsSpmIdReturn.getFault());
-                return;
-            }
-        }
         setSucceeded(true);
     }
 

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandBase.java
@@ -72,6 +72,7 @@ import org.ovirt.engine.core.dal.job.ExecutionMessageDirector;
 import org.ovirt.engine.core.dao.BusinessEntitySnapshotDAO;
 import org.ovirt.engine.core.dao.GenericDao;
 import org.ovirt.engine.core.dao.StatusAwareDao;
+import org.ovirt.engine.core.dao.VdsSpmIdMapDAO;
 import org.ovirt.engine.core.utils.Deserializer;
 import org.ovirt.engine.core.utils.ReflectionUtils;
 import org.ovirt.engine.core.utils.SerializationFactory;
@@ -231,6 +232,10 @@ public abstract class CommandBase<T extends VdcActionParametersBase> extends Aud
 
     protected BusinessEntitySnapshotDAO getBusinessEntitySnapshotDAO() {
         return DbFacade.getInstance().getBusinessEntitySnapshotDao();
+    }
+
+    protected VdsSpmIdMapDAO getVdsSpmIdMapDAO() {
+        return DbFacade.getInstance().getVdsSpmIdMapDao();
     }
 
     /**

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RemoveVdsSpmIdCommand.java
@@ -1,41 +1,0 @@
-package org.ovirt.engine.core.bll;
-
-import org.ovirt.engine.core.common.action.VdsActionParameters;
-import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
-
-@InternalCommandAttribute
-public class RemoveVdsSpmIdCommand<T extends VdsActionParameters> extends AddVdsSpmIdCommand<T> {
-
-    /**
-     * Constructor for command creation when compensation is applied on startup
-     *
-     * @param commandId
-     */
-    public RemoveVdsSpmIdCommand(Guid commandId) {
-        super(commandId);
-    }
-
-    public RemoveVdsSpmIdCommand(T parametars) {
-        super(parametars);
-    }
-
-    @Override
-    protected void executeCommand() {
-        if (getParameters().isCompensationEnabled()) {
-            getCompensationContext().snapshotEntity(DbFacade.getInstance().getVdsSpmIdMapDao().get(getVdsId()));
-        }
-
-        DbFacade.getInstance().getVdsSpmIdMapDao().remove(getVdsId());
-        if (getParameters().isCompensationEnabled()) {
-            getCompensationContext().stateChanged();
-        }
-        setSucceeded(true);
-    }
-
-    @Override
-    protected boolean canDoAction() {
-        // check that there is spm id for this vds
-        return DbFacade.getInstance().getVdsSpmIdMapDao().get(getVdsId()) != null;
-    }
-}

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/VdcActionType.java
@@ -72,7 +72,6 @@ public enum VdcActionType {
     InitVdsOnUp(115),
     SetNonOperationalVds(117),
     AddVdsSpmId(119),
-    RemoveVdsSpmId(120),
     // Fencing (including RestartVds above)
     StartVds(121, ActionGroup.MANIPUTLATE_HOST),
     StopVds(122, ActionGroup.MANIPUTLATE_HOST),

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/errors/VdcBllErrors.java
@@ -306,6 +306,7 @@ public enum VdcBllErrors {
     ImportInfoError(801),
     ImportUnknownType(802),
     ExportError(803),
+    ReachedMaxNumberOfHostsInDC(804),
     ResourceNamespaceNotEmpty(850),
     ResourceTimeout(851),
     ResourceDoesNotExist(852),

commit ba263d0cc1ef9c50098b1f43d4a229bc510ac3ba

engine: AddVdsSpmIdCommand throw exception when...
AddVdsSpmIdCommand throw exception when trying to edit cluster and we have
more than one host attached to these cluster.
The bug is regression caused by commit 4b9a4cfaee613e3254735f06df339840ebc8c4fc.
The problem is the following:
UpdateVdsGroupCommand running inside transaction.
canDoAction() of AddVdsSpmIdCommand is running outside those transaction.
In canDoAction() was retrieved a list of already existed spm_ids.
execute() of AddVdsSpmIdCommand is running inside same transaction as UpdateVdsGroupCommand.
As a result a second AddVdsSpmIdCommand is retrieving a list of spm_ids but with out id inserted
by first AddVdsSpmIdCommand, because of it is still not committed and canDoAction() is running outside
transaction.

Solution: move appropriate logic to execute().
Also for internal command there are no meaning to put
some checks inside canDoAction(), the internal command is not logged and a result of canDoAction() will
not be passed to client.

Change-Id: I7f8aa6b6cb3dfc1508a99c53553afc2e15009b68
Signed-off-by: Michael Kublin <mkublin@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVdsSpmIdCommand.java
@@ -19,7 +19,6 @@ import org.ovirt.engine.core.utils.linq.LinqUtils;
 @InternalCommandAttribute
 @LockIdNameAttribute(isWait = true)
 public class AddVdsSpmIdCommand<T extends VdsActionParameters> extends VdsCommand<T> {
-    private List<vds_spm_id_map> vds_spm_id_mapList;
 
     /**
      * Constructor for command creation when compensation is applied on startup
@@ -36,26 +35,23 @@ public class AddVdsSpmIdCommand<T extends VdsActionParameters> extends VdsComman
 
     @Override
     protected boolean canDoAction() {
-        // check if vds already has vds spm id and storage pool exists
-        if (Guid.Empty.equals(getVds().getStoragePoolId())) {
-            return false;
-        }
-        vds_spm_id_mapList = DbFacade.getInstance().getVdsSpmIdMapDao().getAll(
-                getVds().getStoragePoolId());
-        if (vds_spm_id_mapList.size() >= Config.<Integer> GetValue(ConfigValues.MaxNumberOfHostsInStoragePool)) {
-            VdcFault fault = new VdcFault();
-            fault.setError(VdcBllErrors.ReachedMaxNumberOfHostsInDC);
-            fault.setMessage(Backend.getInstance()
-                    .getVdsErrorsTranslator()
-                    .TranslateErrorTextSingle(fault.getError().toString()));
-            getReturnValue().setFault(fault);
-            return false;
-        }
         return true;
     }
 
     @Override
     protected void executeCommand() {
+        List<vds_spm_id_map> vds_spm_id_mapList = DbFacade.getInstance().getVdsSpmIdMapDao().getAll(
+                getVds().getStoragePoolId());
+        if (vds_spm_id_mapList.size() >= Config.<Integer> GetValue(ConfigValues.MaxNumberOfHostsInStoragePool)) {
+            buildFaultResult();
+            return;
+        }
+        insertSpmIdToDb(vds_spm_id_mapList);
+
+        setSucceeded(true);
+    }
+
+    private void insertSpmIdToDb(List<vds_spm_id_map> vds_spm_id_mapList) {
         // according to shaharf the first id is 1
         int selectedId = 1;
         List<Integer> list = LinqUtils.foreach(vds_spm_id_mapList, new Function<vds_spm_id_map, Integer>() {
@@ -78,8 +74,15 @@ public class AddVdsSpmIdCommand<T extends VdsActionParameters> extends VdsComman
             getCompensationContext().snapshotNewEntity(newMap);
             getCompensationContext().stateChanged();
         }
+    }
 
-        setSucceeded(true);
+    private void buildFaultResult() {
+        VdcFault fault = new VdcFault();
+        fault.setError(VdcBllErrors.ReachedMaxNumberOfHostsInDC);
+        fault.setMessage(Backend.getInstance()
+                .getVdsErrorsTranslator()
+                .TranslateErrorTextSingle(fault.getError().toString()));
+        getReturnValue().setFault(fault);
     }
 
     @Override

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateVdsGroupCommand.java
@@ -60,17 +60,15 @@ public class UpdateVdsGroupCommand<T extends VdsGroupOperationParameters> extend
                 && !oldGroup.getStoragePoolId().equals(getVdsGroup().getStoragePoolId()))
                 || (oldGroup.getStoragePoolId() == null
                 && getVdsGroup().getStoragePoolId() != null)) {
-            VdsActionParameters parameters = new VdsActionParameters();
             for (VDS vds : allForVdsGroup) {
+                VdsActionParameters parameters = new VdsActionParameters();
                 parameters.setVdsId(vds.getId());
-                if (getVdsGroup().getStoragePoolId() != null) {
-                    VdcReturnValueBase addVdsSpmIdReturn =
-                            getBackend().runInternalAction(VdcActionType.AddVdsSpmId, parameters);
-                    if (!addVdsSpmIdReturn.getSucceeded()) {
-                        setSucceeded(false);
-                        getReturnValue().setFault(addVdsSpmIdReturn.getFault());
-                        return;
-                    }
+                VdcReturnValueBase addVdsSpmIdReturn =
+                        getBackend().runInternalAction(VdcActionType.AddVdsSpmId, parameters);
+                if (!addVdsSpmIdReturn.getSucceeded()) {
+                    setSucceeded(false);
+                    getReturnValue().setFault(addVdsSpmIdReturn.getFault());
+                    return;
                 }
             }
 