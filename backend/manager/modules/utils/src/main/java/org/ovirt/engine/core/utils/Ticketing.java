5d46dd50cccb248662cc18a08c9fe774db398173
Alon Bar-Lev <alonbl@redhat.com>
2013-01-31 16:53:34 +0200

    compat: convert to base64 without line feed
    This is regression of change Ie7a540d9c69, as the ticketing base64
    conversion should not contain line feed.
    
    Change-Id: I33790b47ede99eade178b488a66cd8cc280cb872
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/Ticketing.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/Ticketing.java
@@ -10,6 +10,6 @@ public final class Ticketing {
         byte[] arrRandom = new byte[9];
         secr.nextBytes(arrRandom);
         // encode password into Base64 text:
-        return Base64.encodeBase64String(arrRandom);
+        return new Base64(0).encodeToString(arrRandom);
     }
 }
