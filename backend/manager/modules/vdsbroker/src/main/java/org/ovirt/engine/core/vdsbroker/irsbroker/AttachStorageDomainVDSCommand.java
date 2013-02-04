586fbb5015efe062091a556d7e28f8d84e162526
Liron Aravot <laravot@redhat.com>
2013-02-04 10:10:41 +0200

    core: attach sd  - avoid failover in case of export domain
    When attaching an export storage domain and having a failure which
    isn't spm/master domain related, avoid failover.
    
    Change-Id: Iea8ae06d5327e46b5cdb7ed73239744a1ba6ffc3
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=896484
    Signed-off-by: Liron Aravot <laravot@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/AttachStorageDomainVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/AttachStorageDomainVDSCommand.java
@@ -1,6 +1,10 @@
 package org.ovirt.engine.core.vdsbroker.irsbroker;
 
+import org.ovirt.engine.core.common.businessentities.StorageDomainType;
+import org.ovirt.engine.core.common.businessentities.storage_domains;
 import org.ovirt.engine.core.common.vdscommands.*;
+import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.vdsbroker.vdsbroker.VDSExceptionBase;
 
 public class AttachStorageDomainVDSCommand<P extends AttachStorageDomainVDSCommandParameters>
         extends IrsBrokerCommand<P> {
@@ -13,5 +17,14 @@ public class AttachStorageDomainVDSCommand<P extends AttachStorageDomainVDSComma
         status = getIrsProxy().attachStorageDomain(getParameters().getStorageDomainId().toString(),
                 getParameters().getStoragePoolId().toString());
         ProceedProxyReturnValue();
+    }
+
+    @Override
+    protected VDSExceptionBase createDefaultConcreteException(String errorMessage) {
+        storage_domains domainFromDb = DbFacade.getInstance().getStorageDomainDao().get(getParameters().getStorageDomainId());
+        if (domainFromDb == null || domainFromDb.getstorage_domain_type() == StorageDomainType.ImportExport) {
+            return new IrsOperationFailedNoFailoverException(errorMessage);
+        }
+        return super.createDefaultConcreteException(errorMessage);
     }
 }
