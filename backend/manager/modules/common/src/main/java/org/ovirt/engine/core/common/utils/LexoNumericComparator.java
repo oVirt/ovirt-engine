25619c7021b476642c097f4415a0f150178eed6a
Lior Vernia <lvernia@redhat.com>
2013-02-07 18:23:19 +0200

    core: Added LexoNumericComparator utility
    This utility helps to sort strings which include numeric subsequences
    according to sensible ordering - sorting numeric subsequences
    numerically, and the rest lexicographically.
    
    A corresponding test class is added.
    
    Change-Id: I8c68446ea6f1865a51455a0a359df339bfb15bba
    Signed-off-by: Lior Vernia <lvernia@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/LexoNumericComparator.java
@@ -0,0 +1,102 @@
+package org.ovirt.engine.core.common.utils;
+
+import java.util.Comparator;
+
+/**
+ * This class may be used to sort strings that have numeric sequences in them. <br>
+ * <br>
+ * A common problem is that sorting such strings lexicographically (as any other string) results in an order that may be
+ * considered counter-intuitive, e.g. "Example10" will appear before "Example2" since '1' is lexicographically less than
+ * '2'. <br>
+ * <br>
+ * The method compare() deals with these strings by splitting them into alternating subsequences of digits and
+ * nondigits, sorting the digit sequences numerically and the nondigit sequences lexicographically. <br>
+ * <br>
+ * Nulls and empty strings are allowed; nulls are considered less than empty strings, and empty strings less than
+ * nonempty ones. <br>
+ * <br>
+ * It is assumed that a string always begins with a nondigit sequence; if it actually begins with a digit sequence, the
+ * behaviour is as if it started with an empty nondigit sequence.
+ */
+public class LexoNumericComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String str1, String str2) {
+        return comp(str1, str2);
+    }
+
+    public static int comp(String str1, String str2) {
+        if (str1 == null) {
+            return (str2 == null) ? 0 : -1;
+        } else if (str2 == null) {
+            return 1;
+        }
+
+        boolean digitTurn = false;
+        int begSeq1 = 0;
+        int begSeq2 = 0;
+
+        while (begSeq1 != str1.length()) {
+
+            // str1 and str2 have the same prefix but str1 has an extra sequence => str1 > str2
+            if (begSeq2 == str2.length()) {
+                return 1;
+            }
+
+            int endSeq1 = findEndOfSequence(str1, begSeq1, digitTurn);
+            int endSeq2 = findEndOfSequence(str2, begSeq2, digitTurn);
+            String seq1 = str1.substring(begSeq1, endSeq1);
+            String seq2 = str2.substring(begSeq2, endSeq2);
+            int compRes = compareSequence(seq1, seq2, digitTurn);
+            if (compRes != 0) {
+                return compRes;
+            }
+
+            digitTurn = !digitTurn;
+            begSeq1 = endSeq1;
+            begSeq2 = endSeq2;
+        }
+
+        // str1 and str2 have the same prefix but str2 has an extra sequence => str1 < str2
+        if (begSeq2 != str2.length()) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private static int compareSequence(String seq1, String seq2, boolean digitSequence) {
+        return digitSequence ? compDigitSequence(seq1, seq2) : compNonDigitSequence(seq1, seq2);
+    }
+
+    private static int compDigitSequence(String seq1, String seq2) {
+        int compRes = ((Integer) Integer.parseInt(seq1)).compareTo(Integer.parseInt(seq2));
+        return compRes == 0 ? compNonDigitSequence(seq1, seq2) : compRes;
+    }
+
+    private static int compNonDigitSequence(String seq1, String seq2) {
+        return seq1.compareTo(seq2);
+    }
+
+    private static int findEndOfSequence(String seq, int startIndex, boolean digitSequence) {
+        return digitSequence ? findEndOfDigitSequence(seq, startIndex) : findEndOfNonDigitSequence(seq, startIndex);
+    }
+
+    private static int findEndOfDigitSequence(String seq, int startIndex) {
+        for (int i = startIndex; i < seq.length(); ++i) {
+            if (!Character.isDigit(seq.charAt(i))) {
+                return i;
+            }
+        }
+        return seq.length();
+    }
+
+    private static int findEndOfNonDigitSequence(String seq, int startIndex) {
+        for (int i = startIndex; i < seq.length(); ++i) {
+            if (Character.isDigit(seq.charAt(i))) {
+                return i;
+            }
+        }
+        return seq.length();
+    }
+}
---
+++ backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/LexoNumericComparatorTest.java
@@ -0,0 +1,77 @@
+package org.ovirt.engine.core.common.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class LexoNumericComparatorTest {
+
+    private LexoNumericComparator comparator = new LexoNumericComparator();
+
+    private String left;
+    private String right;
+    private int expectedResult;
+
+    public LexoNumericComparatorTest(String left, String right, int expectedResult) {
+        this.left = left;
+        this.right = right;
+        this.expectedResult = expectedResult;
+    }
+
+    private void verifyResult(String left, String right, int expectedResult) {
+        assertEquals(String.format("Expected %1$s to be %3$s %2$s, but it wasn't.",
+                left,
+                right,
+                expectedResult == -1 ? "less than" : (expectedResult == 1 ? "greater than" : "equal to")),
+                Integer.signum(expectedResult),
+                Integer.signum(comparator.compare(left, right)));
+    }
+
+    @Test
+    public void runTest() {
+        verifyResult(left, right, expectedResult);
+        verifyResult(right, left, -expectedResult);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> comparisonParameters() {
+        return Arrays.asList(new Object[][] {
+                { null, null, 0 },
+                { null, "", -1 },
+                { "", "", 0 },
+                { "", "123", -1 },
+                { "123", "123", 0 },
+                { "123", "456", -1 },
+                { "12", "123", -1 },
+                { "012", "12", -1 },
+                { "2", "10", -1 },
+                { "123abc", "123abc", 0 },
+                { "123abc", "456abc", -1 },
+                { "12abc", "123abc", -1 },
+                { "012abc", "12abc", -1 },
+                { "2abc", "10abc", -1 },
+                { "123", "abc", -1 },
+                { "abc", "abc", 0 },
+                { "abc", "def", -1 },
+                { "ab", "abc", -1 },
+                { "123abc", "123def", -1 },
+                { "123ab", "123abc", -1 },
+                { "abc123", "abc123", 0 },
+                { "abc123", "def123", -1 },
+                { "ab123", "abc123", -1 },
+                { "abc123", "abc456", -1 },
+                { "abc12", "abc123", -1 },
+                { "abc012", "abc12", -1 },
+                { "abc2", "abc10", -1 },
+                { "abc123def", "abc123def", 0 },
+                { "abc123def", "abc123ghi", -1 },
+                { "abc123de", "abc123def", -1 }
+        });
+    }
+}
