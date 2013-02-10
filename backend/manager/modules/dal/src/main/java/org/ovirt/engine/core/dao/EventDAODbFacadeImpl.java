697b8fa5833f10db67c84bf6d091d74146d08890
Allon Mureinik <amureini@redhat.com>
2013-02-10 20:16:45 +0200

    core: Singleton row mappers for EventDAO
    Added singleton row mappers for EventDAO, as specified by
    http://www.ovirt.org/Backend_Coding_Standards .
    
    Change-Id: I3f44cf4eafacc2069fb9523d6b29ce777bcd9a6f
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/EventDAODbFacadeImpl.java
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/EventDAODbFacadeImpl.java
@@ -3,13 +3,14 @@ package org.ovirt.engine.core.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+
 import org.ovirt.engine.core.common.EventNotificationMethods;
 import org.ovirt.engine.core.common.businessentities.EventMap;
 import org.ovirt.engine.core.common.businessentities.EventNotificationMethod;
 import org.ovirt.engine.core.common.businessentities.event_subscriber;
 import org.ovirt.engine.core.compat.Guid;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 
 /**
  * <code>EventSubscriberDAODbFacadeImpl</code> provides an implementation of {@link EventSubscriberDAO} that uses the
@@ -17,27 +18,54 @@ import org.ovirt.engine.core.compat.Guid;
  */
 public class EventDAODbFacadeImpl extends BaseDAODbFacade implements EventDAO {
 
+    private static final class EventSubscriberRowMapper implements ParameterizedRowMapper<event_subscriber> {
+        public static final EventSubscriberRowMapper instance = new EventSubscriberRowMapper();
+
+        @Override
+        public event_subscriber mapRow(ResultSet rs, int rowNum)
+                throws SQLException {
+            event_subscriber entity = new event_subscriber();
+            entity.setevent_up_name(rs.getString("event_up_name"));
+            entity.setmethod_id(rs.getInt("method_id"));
+            entity.setmethod_address(rs.getString("method_address"));
+            entity.setsubscriber_id(Guid.createGuidFromString(rs
+                    .getString("subscriber_id")));
+            entity.settag_name(rs.getString("tag_name"));
+            return entity;
+        }
+    }
+
+    private static final class EventNotificationMethodRowMapper implements ParameterizedRowMapper<EventNotificationMethod> {
+        public static final EventNotificationMethodRowMapper instance = new EventNotificationMethodRowMapper();
+
+        @Override
+        public EventNotificationMethod mapRow(ResultSet rs, int rowNum) throws SQLException {
+            EventNotificationMethod entity = new EventNotificationMethod();
+            entity.setmethod_id(rs.getInt("method_id"));
+            entity.setmethod_type(EventNotificationMethods.EMAIL);
+            return entity;
+        }
+    }
+
+    private static final class EventMapRowMapper implements ParameterizedRowMapper<EventMap> {
+        public static final EventMapRowMapper instance = new EventMapRowMapper();
+
+        @Override
+        public EventMap mapRow(ResultSet rs, int rowNum) throws SQLException {
+            EventMap entity = new EventMap();
+            entity.setEventUpName(rs.getString("event_up_name"));
+            entity.setEventDownName(rs.getString("event_down_name"));
+            return entity;
+        }
+    }
+
     @SuppressWarnings("unchecked")
     @Override
     public List<event_subscriber> getAll() {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource();
-
-        ParameterizedRowMapper<event_subscriber> mapper = new ParameterizedRowMapper<event_subscriber>() {
-            @Override
-            public event_subscriber mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                event_subscriber entity = new event_subscriber();
-                entity.setevent_up_name(rs.getString("event_up_name"));
-                entity.setmethod_id(rs.getInt("method_id"));
-                entity.setmethod_address(rs.getString("method_address"));
-                entity.setsubscriber_id(Guid.createGuidFromString(rs
-                        .getString("subscriber_id")));
-                entity.settag_name(rs.getString("tag_name"));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("GetAllFromevent_subscriber", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetAllFromevent_subscriber",
+                EventSubscriberRowMapper.instance,
+                parameterSource);
     }
 
     @SuppressWarnings("unchecked")
@@ -45,80 +73,36 @@ public class EventDAODbFacadeImpl extends BaseDAODbFacade implements EventDAO {
     public List<event_subscriber> getAllForSubscriber(Guid id) {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
                 .addValue("subscriber_id", id);
-
-        ParameterizedRowMapper<event_subscriber> mapper = new ParameterizedRowMapper<event_subscriber>() {
-            @Override
-            public event_subscriber mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                event_subscriber entity = new event_subscriber();
-                entity.setevent_up_name(rs.getString("event_up_name"));
-                entity.setmethod_id(rs.getInt("method_id"));
-                entity.setmethod_address(rs.getString("method_address"));
-                entity.setsubscriber_id(Guid.createGuidFromString(rs
-                        .getString("subscriber_id")));
-                entity.settag_name(rs.getString("tag_name"));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("Getevent_subscriberBysubscriber_id", mapper, parameterSource);
+        return getCallsHandler().executeReadList("Getevent_subscriberBysubscriber_id",
+                EventSubscriberRowMapper.instance,
+                parameterSource);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public List<EventNotificationMethod> getAllEventNotificationMethods() {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource();
-
-        ParameterizedRowMapper<EventNotificationMethod> mapper =
-                new ParameterizedRowMapper<EventNotificationMethod>() {
-                    @Override
-                    public EventNotificationMethod mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        EventNotificationMethod entity = new EventNotificationMethod();
-                        entity.setmethod_id(rs.getInt("method_id"));
-                        entity.setmethod_type(EventNotificationMethods.EMAIL);
-                        return entity;
-                    }
-                };
-
-        return getCallsHandler().executeReadList("GetAllFromevent_notification_methods",mapper,parameterSource);
+        return getCallsHandler().executeReadList("GetAllFromevent_notification_methods",
+                EventNotificationMethodRowMapper.instance,
+                parameterSource);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public List<EventNotificationMethod> getEventNotificationMethodsById(int method_id) {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource().addValue("method_id", method_id);
-
-        ParameterizedRowMapper<EventNotificationMethod> mapper =
-                new ParameterizedRowMapper<EventNotificationMethod>() {
-                    @Override
-                    public EventNotificationMethod mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        EventNotificationMethod entity = new EventNotificationMethod();
-                        entity.setmethod_id(rs.getInt("method_id"));
-                        entity.setmethod_type(EventNotificationMethods.EMAIL);
-                        return entity;
-                    }
-                };
-
-        return getCallsHandler().executeReadList("GetEventNotificationMethodById", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetEventNotificationMethodById",
+                EventNotificationMethodRowMapper.instance,
+                parameterSource);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public List<EventNotificationMethod> getEventNotificationMethodsByType(String method_type) {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource().addValue("method_type", method_type);
-
-        ParameterizedRowMapper<EventNotificationMethod> mapper =
-                new ParameterizedRowMapper<EventNotificationMethod>() {
-                    @Override
-                    public EventNotificationMethod mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        EventNotificationMethod entity = new EventNotificationMethod();
-                        entity.setmethod_id(rs.getInt("method_id"));
-                        entity.setmethod_type(EventNotificationMethods.EMAIL);
-                        return entity;
-                    }
-                };
-
-        return getCallsHandler().executeReadList("GetEventNotificationMethodByType", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetEventNotificationMethodByType",
+                EventNotificationMethodRowMapper.instance,
+                parameterSource);
     }
 
     @Override
@@ -159,36 +143,14 @@ public class EventDAODbFacadeImpl extends BaseDAODbFacade implements EventDAO {
     @Override
     public List<EventMap> getAllEventMaps() {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource();
-
-        ParameterizedRowMapper<EventMap> mapper = new ParameterizedRowMapper<EventMap>() {
-            @Override
-            public EventMap mapRow(ResultSet rs, int rowNum) throws SQLException {
-                EventMap entity = new EventMap();
-                entity.setEventUpName(rs.getString("event_up_name"));
-                entity.setEventDownName(rs.getString("event_down_name"));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("GetAllFromevent_map", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetAllFromevent_map", EventMapRowMapper.instance, parameterSource);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public List<EventMap> getEventMapByName(String event_up_name) {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource().addValue("event_name", event_up_name);
-
-        ParameterizedRowMapper<EventMap> mapper = new ParameterizedRowMapper<EventMap>() {
-            @Override
-            public EventMap mapRow(ResultSet rs, int rowNum) throws SQLException {
-                EventMap entity = new EventMap();
-                entity.setEventUpName(rs.getString("event_up_name"));
-                entity.setEventDownName(rs.getString("event_down_name"));
-                return entity;
-            }
-        };
-
-        return getCallsHandler().executeReadList("GetEventMapByName", mapper, parameterSource);
+        return getCallsHandler().executeReadList("GetEventMapByName", EventMapRowMapper.instance, parameterSource);
     }
 }
 
