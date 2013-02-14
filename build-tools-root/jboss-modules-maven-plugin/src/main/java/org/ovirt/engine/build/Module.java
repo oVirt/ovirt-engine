9b3e223cf8b9828f96f36fb2e3490babef729b7f
Juan Hernandez <juan.hernandez@redhat.com>
2013-02-14 11:25:40 +0100

    core: Maven plugin to generate JBoss modules
    This change introduces a new maven plugin that generates .zip files
    containing JBoss modules and attaches them as artifacts.
    
    Change-Id: Ie9f3eb59fd67d4ce97f3e2802e7058fa76feb4eb
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ build-tools-root/jboss-modules-maven-plugin/src/main/java/org/ovirt/engine/build/Module.java
@@ -0,0 +1,65 @@
+package org.ovirt.engine.build;
+
+import org.apache.maven.artifact.Artifact;
+
+public class Module {
+    private String artifactId;
+    private String groupId;
+    private String moduleName;
+    private String moduleSlot;
+    private String resourcePath;
+
+    public Module() {
+        // Nothing.
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getModuleName() {
+        if (moduleName != null) {
+            return moduleName;
+        }
+        return groupId + "." + artifactId;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getModuleSlot() {
+        if (moduleSlot != null) {
+            return moduleSlot;
+        }
+        return "main";
+    }
+
+    public void setModuleSlot(String moduleSlot) {
+        this.moduleSlot = moduleSlot;
+    }
+
+    public String getResourcePath() {
+        if (resourcePath != null) {
+            return resourcePath;
+        }
+        return artifactId + ".jar";
+    }
+
+    public boolean matches(Artifact artifact) {
+        return artifact.getArtifactId().equals(artifactId) && artifact.getGroupId().equals(groupId);
+    }
+}
+
