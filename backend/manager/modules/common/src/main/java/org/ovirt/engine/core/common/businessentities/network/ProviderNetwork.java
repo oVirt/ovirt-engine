7875e2dd284463d9935fef517bee1362cdb40437
Mike Kolesnik <mkolesni@redhat.com>
2013-02-17 12:38:56 +0200

    core: Add ProviderNetwork class
    This class represents the details a network that is provided by an
    external provider, by linking the two and supplying the external ID of
    the network on the provider.
    
    Change-Id: I3800abecb6258c673d0272cbfdb00c121ef3d75c
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/network/ProviderNetwork.java
@@ -0,0 +1,80 @@
+package org.ovirt.engine.core.common.businessentities.network;
+
+import java.io.Serializable;
+
+import org.ovirt.engine.core.compat.Guid;
+
+public class ProviderNetwork implements Serializable {
+
+    private static final long serialVersionUID = -1080092693244154625L;
+
+    private Guid providerId;
+
+    private String externalId;
+
+    public ProviderNetwork() {
+    }
+
+    public ProviderNetwork(Guid providerId, String externalId) {
+        this.providerId = providerId;
+        this.externalId = externalId;
+    }
+
+    public Guid getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(Guid providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getExternalId() {
+        return externalId;
+    }
+
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getExternalId() == null) ? 0 : getExternalId().hashCode());
+        result = prime * result + ((getProviderId() == null) ? 0 : getProviderId().hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ProviderNetwork other = (ProviderNetwork) obj;
+        if (getExternalId() == null) {
+            if (other.getExternalId() != null)
+                return false;
+        } else if (!getExternalId().equals(other.getExternalId()))
+            return false;
+        if (getProviderId() == null) {
+            if (other.getProviderId() != null)
+                return false;
+        } else if (!getProviderId().equals(other.getProviderId()))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("ProviderNetwork [providerId=")
+                .append(getProviderId())
+                .append(", externalId=")
+                .append(getExternalId())
+                .append("]");
+        return builder.toString();
+    }
+}
