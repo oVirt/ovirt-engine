e03d6042ecd0aa0f04c912cafffef3567f18413a
Gilad Chaplik <gchaplik@redhat.com>
2013-02-17 16:14:07 +0200

    core: fix typo in java comment
    /s/MultipleActinosRunner/MultipleActionsRunner
    
    Change-Id: I071dda2aeb05c709cae36f663a4b74acf4757ba1
    Signed-off-by: Gilad Chaplik <gchaplik@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RunVmDelayer.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RunVmDelayer.java
@@ -3,7 +3,7 @@ package org.ovirt.engine.core.bll;
 import org.ovirt.engine.core.compat.Guid;
 
 /**
- * Some commands e.g RunVm may run as a bulk AKA {@link MultipleActinosRunner} and performs logic to <br>
+ * Some commands e.g RunVm may run as a bulk AKA {@link MultipleActionsRunner} and performs logic to <br>
  * count and reserve memory and CPU to assure there are both enough resources to complete them<br>
  * (i.e run a VM on a selected host) and that we don't exceed those for the subsequent executions.<br>
  * Moreover bulk operations may cause a pick in VDSM resources utilization and the engine can regulate <br>
