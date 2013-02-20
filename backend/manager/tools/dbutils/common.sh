93152a5cd957717269cbfcaf7b99ba486aaa62eb
Eli Mesika <emesika@redhat.com>
2013-02-20 10:39:12 +0200

    core: adding missing file for fkvalidator
    This file was missing when committing the fkvalidaor move from dbscripts
    to dbutils.
    
    Change-Id: Id30fd1437d479049ab3cfc55e3f87e2d5af56f9e
    Signed-off-by: Eli Mesika <emesika@redhat.com>

---
+++ backend/manager/tools/dbutils/common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set_defaults() {
+    ME=$(basename $0)
+    SERVERNAME="localhost"
+    PORT="5432"
+    DATABASE="engine"
+    USERNAME=""
+    VERBOSE=false
+    LOGFILE="$ME.log"
+
+    # When running in development environments the .pgpass file may not
+    # exist or might not be readable, so we should try to use the file
+    # stored in the home directory of the user instead:
+    PGPASSFILE="/etc/ovirt-engine/.pgpass"
+    if [ ! -r "${PGPASSFILE}" ]
+    then
+        PGPASSFILE="${HOME}/.pgpass"
+    fi
+    export PGPASSFILE
+}
+
