2cec04085eeb021c4c7cf1692851b6331da28677
Allon Mureinik <amureini@redhat.com>
2013-02-21 08:16:31 +0200

    core: JUnit 4 style for PropertyCompatTest
    Fixed PropertyCompatTest to use JUnit 4 style instead of JUnit 3 style -
    stopped extending TestCase and added @Test annotations.
    
    Change-Id: I7d3ee0866a05301f354e366a046197b465796d1c
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/compat/src/test/java/org/ovirt/engine/core/compat/PropertyCompatTest.java
+++ backend/manager/modules/compat/src/test/java/org/ovirt/engine/core/compat/PropertyCompatTest.java
@@ -1,19 +1,22 @@
 package org.ovirt.engine.core.compat;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import junit.framework.TestCase;
-
+import org.junit.Test;
 import org.ovirt.engine.core.compat.backendcompat.PropertyCompat;
 import org.ovirt.engine.core.compat.backendcompat.TypeCompat;
 
-public class PropertyCompatTest extends TestCase {
-    public String someValue = "Anakin";
+public class PropertyCompatTest {
+    private String someValue = "Anakin";
 
+    @Test
     public void testIt() {
         PropertyCompat pc = TypeCompat.GetProperty(this.getClass(), "someValue");
         String result = (String) pc.GetValue(this, "JarJar");
@@ -23,6 +26,7 @@ public class PropertyCompatTest extends TestCase {
         assertEquals("Null Value", "JarJar", result);
     }
 
+    @Test
     public void testGetAvailableValues() {
         Map<String, String> values = new HashMap<String, String>();
         Set<String> properties = new HashSet<String>(Arrays.asList("somevalue"));
