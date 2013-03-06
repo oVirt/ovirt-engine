310e8953344c8b150b6035dc2a69d9f98eb941eb
Arik Hadas <ahadas@redhat.com>
2013-03-06 09:55:30 +0200

    userportal: show the correct VM's console type
    The console type presented in VMs lists at the user-portal (basic +
    extended) is based on the default display type defined for the VMs,
    which is not always right because VM can be run in Run-Once mode
    with a display type which is different from the default.
    
    This patch ensure that the right console type is displayed for each VM
    in the user portal by changing the way the console type is determined -
    if the VM is running then we determine the console type according to the
    actual display type (presented by the display type field in VmDynamic),
    and according to the default display type, as before, only if the VM is
    not running.
    
    Change-Id: I312cfec646c276038669526140d75452bfed8267
    Bug-Url: https://bugzilla.redhat.com/907776
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/userportal/IUserPortalListModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/userportal/IUserPortalListModel.java
@@ -230,7 +230,8 @@ public abstract class IUserPortalListModel extends ListWithDetailsModel implemen
     }
 
     protected ConsoleModel determineConsoleModelFromVm(VM vm, ArrayList<ConsoleModel> cachedModels) {
-        return vm.getDefaultDisplayType() == DisplayType.vnc ? cachedModels.get(VNC_INDEX) : cachedModels.get(SPICE_INDEX);
+        DisplayType vmDisplayType = vm.isRunningOrPaused() ? vm.getDisplayType() : vm.getDefaultDisplayType();
+        return cachedModels.get(vmDisplayType == DisplayType.vnc ? VNC_INDEX : SPICE_INDEX);
     }
 
     protected void updateDefaultSelectedConsoleProtocol(VM vm) {
