2920deb56c983b7f05c493e87c5eab34227bd6f3
Eli Mesika <emesika@redhat.com>
2013-03-11 18:32:20 +0200

    core: Upgrade - Error Can't start the ovirt-engine-dwhd service
    Fix column name to _update_date instead of update_date
    Add column to all relevant tables in fixtures.xml
    
    Change-Id: If99b9dca0f1b83f1a815d4ebb112828a135e495a
    Signed-off-by: Eli Mesika <emesika@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=918152
    Signed-off-by: Eli Mesika <emesika@redhat.com>

--- backend/manager/dbscripts/upgrade/03_02_0490_add_update_column_to_statistics_tables.sql
+++ backend/manager/dbscripts/upgrade/03_02_0490_add_update_column_to_statistics_tables.sql
@@ -1,6 +1,6 @@
-select fn_db_add_column('vds_interface_statistics', 'update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
-select fn_db_add_column('vm_interface_statistics', 'update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
-select fn_db_add_column('vm_statistics', 'update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
-select fn_db_add_column('vds_statistics', 'update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
-select fn_db_add_column('disk_image_dynamic', 'update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
-select fn_db_add_column('storage_domain_dynamic', 'update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
+select fn_db_add_column('vds_interface_statistics', '_update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
+select fn_db_add_column('vm_interface_statistics', '_update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
+select fn_db_add_column('vm_statistics', '_update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
+select fn_db_add_column('vds_statistics', '_update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
+select fn_db_add_column('disk_image_dynamic', '_update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
+select fn_db_add_column('storage_domain_dynamic', '_update_date', 'TIMESTAMP WITH TIME ZONE default NULL');
