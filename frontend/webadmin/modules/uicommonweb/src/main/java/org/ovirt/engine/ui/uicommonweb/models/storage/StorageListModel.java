oVirt/ovirt-engine

commit bfd9299661c6f6adeea998e93232c8a8789b9dc0

core: add ability edit NFS path in webadmin
Add the option to edit NFS path and the overriden
additional options of storage domain (retransmissions, timeout, version)
in webadmin UI --> Storage tab.

Editing these properties is available only when
the storage domain is in maintenance status and is a data/master domain,
and for storage of nfs type. These are the only properties which are
editable in this status.

Editing is still available for storage domains in status Active/Mixed, but
only for name and description - like it was before this patch.

Related-To:https://bugzilla.redhat.com/show_bug.cgi?id=835543
Change-Id: Ifaff5344ff191d6bdf53cc706c7bb796167a56b3
Signed-off-by: Alissa Bonas <abonas@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/storage/StorageListModel.java
@@ -19,6 +19,7 @@ import org.ovirt.engine.core.common.businessentities.NfsVersion;
 import org.ovirt.engine.core.common.businessentities.StorageDomain;
 import org.ovirt.engine.core.common.businessentities.StorageDomainSharedStatus;
 import org.ovirt.engine.core.common.businessentities.StorageDomainStatic;
+import org.ovirt.engine.core.common.businessentities.StorageDomainStatus;
 import org.ovirt.engine.core.common.businessentities.StorageDomainType;
 import org.ovirt.engine.core.common.businessentities.StorageFormatType;
 import org.ovirt.engine.core.common.businessentities.StorageServerConnections;
@@ -360,17 +361,22 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
         model.getDataCenter().setIsChangable(false);
         model.getFormat().setIsChangable(false);
 
-        boolean isStorageActive = model.isStorageActive();
-
+        boolean isStorageEditable = model.isStorageActive() || model.isNewStorage();
         model.getHost().setIsChangable(false);
-        model.getName().setIsChangable(isStorageActive);
-        model.getAvailableStorageItems().setIsChangable(isStorageActive);
-        model.setIsChangable(isStorageActive);
+        model.getName().setIsChangable(isStorageEditable);
+        model.getDescription().setIsChangable(isStorageEditable);
+        model.getAvailableStorageItems().setIsChangable(isStorageEditable);
+        model.setIsChangable(isStorageEditable);
 
         IStorageModel item = null;
         switch (storage.getStorageType()) {
             case NFS:
                 item = PrepareNfsStorageForEdit(storage);
+                boolean isNfsPathEditable = isNfsPathEditable(storage);
+                isStorageEditable = isStorageEditable || isNfsPathEditable;
+                //when storage is active, only SPM can perform actions on it, thus it is set above that host is not changeable.
+                //If storage is editable but not active (maintenance) - any host can perform the edit so the changeable here is set based on that
+                model.getHost().setIsChangable(isNfsPathEditable);
                 break;
 
             case FCP:
@@ -411,8 +417,9 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
             }
         }
 
+
         UICommand command;
-        if (isStorageActive) {
+        if (isStorageEditable) {
             command = new UICommand("OnSave", this); //$NON-NLS-1$
             command.setTitle(ConstantsManager.getInstance().getConstants().ok());
             command.setIsDefault(true);
@@ -435,7 +442,11 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
     {
         final NfsStorageModel model = new NfsStorageModel();
         model.setRole(storage.getStorageDomainType());
-        model.setIsEditMode(true);
+
+        boolean isNfsPathEditable = isNfsPathEditable(storage);
+
+        model.getPath().setIsChangable(isNfsPathEditable);
+        model.getOverride().setIsChangable(isNfsPathEditable);
 
         AsyncDataProvider.GetStorageConnectionById(new AsyncQuery(null, new INewAsyncCallback() {
             @Override
@@ -447,7 +458,6 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
                 model.getTimeout().setEntity(connection.getNfsTimeo());
 
                 for (Object item : model.getVersion().getItems()) {
-
                     EntityModel itemModel = (EntityModel) item;
                     boolean noNfsVersion = itemModel.getEntity() == null && connection.getNfsVersion() == null;
                     boolean foundNfsVersion = itemModel.getEntity() != null &&
@@ -469,6 +479,10 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
         }), storage.getStorage(), true);
 
         return model;
+    }
+
+    private boolean isNfsPathEditable(StorageDomain storage) {
+        return (storage.getStorageDomainType() == StorageDomainType.Data || storage.getStorageDomainType() == StorageDomainType.Master) && storage.getStatus() == StorageDomainStatus.Maintenance;
     }
 
     private IStorageModel PrepareLocalStorageForEdit(StorageDomain storage)
@@ -560,9 +574,10 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
     private void PrepareSanStorageForEdit(final SanStorageModel model)
     {
         StorageModel storageModel = (StorageModel) getWindow();
-        boolean isStorageActive = storageModel.isStorageActive();
+        StorageDomain storage = (StorageDomain) getSelectedItem();
+        boolean isStorageEditable = storageModel.isStorageActive() || storageModel.isNewStorage();
 
-        if (isStorageActive) {
+        if (isStorageEditable) {
             storageModel.getHost().getSelectedItemChangedEvent().addListener(new IEventListener() {
                 @Override
                 public void eventRaised(Event ev, Object sender, EventArgs args) {
@@ -1112,11 +1127,15 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
             return false;
         }
 
+        boolean isEditAvailable;
         boolean isActive = storageDomain.getStorageDomainSharedStatus() == StorageDomainSharedStatus.Active
                 || storageDomain.getStorageDomainSharedStatus() == StorageDomainSharedStatus.Mixed;
+        boolean isInMaintenance = (storageDomain.getStatus() == StorageDomainStatus.Maintenance);
+        boolean isDataDomain = (storageDomain.getStorageDomainType() == StorageDomainType.Data) || (storageDomain.getStorageDomainType() == StorageDomainType.Master);
         boolean isBlockStorage = storageDomain.getStorageType().isBlockDomain();
 
-        return isBlockStorage ? true : isActive;
+        isEditAvailable = isActive || isBlockStorage || ( isInMaintenance && isDataDomain)  ;
+        return isEditAvailable;
     }
 
     @Override
@@ -1448,6 +1467,9 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
         storageDomain.setDescription((String) model.getDescription().getEntity());
         storageDomain.setStorageFormat((StorageFormatType) model.getFormat().getSelectedItem());
 
+        connection = new StorageServerConnections();
+        connection.setid(selectedItem.getStorage());
+
         if (isNew)
         {
             AsyncDataProvider.GetStorageDomainsByConnection(new AsyncQuery(this, new INewAsyncCallback() {
@@ -1472,18 +1494,54 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
         }
         else
         {
-            Frontend.RunAction(VdcActionType.UpdateStorageDomain, new StorageDomainManagementParameter(storageDomain),
+            StorageDomain storageDomain = (StorageDomain) getSelectedItem();
+            if(isNfsPathEditable(storageDomain)) {
+                updateNfsPath();
+            }
+            else {
+               Frontend.RunAction(VdcActionType.UpdateStorageDomain, new StorageDomainManagementParameter(this.storageDomain),
                 new IFrontendActionAsyncCallback() {
                     @Override
                     public void Executed(FrontendActionAsyncResult result) {
-
                         StorageListModel storageListModel = (StorageListModel) result.getState();
                         storageListModel.OnFinish(storageListModel.context, true, storageListModel.storageModel);
 
                     }
                 }, this);
+            }
         }
     }
+
+    private void updateNfsPath() {
+        StorageModel model = (StorageModel) getWindow();
+        NfsStorageModel nfsModel = (NfsStorageModel) model.getSelectedItem();
+        VDS host = (VDS) model.getHost().getSelectedItem();
+        Guid hostId = Guid.Empty;
+        Guid storagePoolId = Guid.Empty;
+        if(host != null) {
+           hostId = host.getId();
+           storagePoolId = host.getStoragePoolId();
+        }
+        connection.setconnection(path);
+        connection.setstorage_type(nfsModel.getType());
+        if ((Boolean) nfsModel.getOverride().getEntity()) {
+            connection.setNfsVersion((NfsVersion) ((EntityModel) nfsModel.getVersion().getSelectedItem()).getEntity());
+            connection.setNfsRetrans(nfsModel.getRetransmissions().AsConvertible().nullableShort());
+            connection.setNfsTimeo(nfsModel.getTimeout().AsConvertible().nullableShort());
+        }
+        StorageServerConnectionParametersBase parameters = new StorageServerConnectionParametersBase(connection, hostId);
+        parameters.setStoragePoolId(storagePoolId);
+        Frontend.RunAction(VdcActionType.UpdateStorageServerConnection, parameters,
+                new IFrontendActionAsyncCallback() {
+                    @Override
+                    public void Executed(FrontendActionAsyncResult result) {
+                        StorageListModel storageListModel = (StorageListModel) result.getState();
+                        storageListModel.OnFinish(storageListModel.context, true, storageListModel.storageModel);
+
+                    }
+                }, this);
+    }
+
 
     public void SaveNewNfsStorage()
     {

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/LockMessagesMatchUtil.java
@@ -5,7 +5,6 @@ import org.ovirt.engine.core.common.utils.Pair;
 import org.ovirt.engine.core.dal.VdcBllMessages;
 
 public class LockMessagesMatchUtil {
-
     public static final Pair<String, String> POOL         = makeLockingPair(LockingGroup.POOL);
     public static final Pair<String, String> VDS          = makeLockingPair(LockingGroup.VDS);
     public static final Pair<String, String> VDS_FENCE    = makeLockingPair(LockingGroup.VDS_FENCE);
@@ -15,6 +14,7 @@ public class LockMessagesMatchUtil {
     public static final Pair<String, String> VM_DISK_BOOT = makeLockingPair(LockingGroup.VM_DISK_BOOT);
     public static final Pair<String, String> VM_NAME      = makeLockingPair(LockingGroup.VM_NAME);
     public static final Pair<String, String> STORAGE      = makeLockingPair(LockingGroup.STORAGE);
+    public static final Pair<String, String> STORAGE_CONNECTION = makeLockingPair(LockingGroup.STORAGE_CONNECTION);
     public static final Pair<String, String> REGISTER_VDS = makeLockingPair(LockingGroup.REGISTER_VDS);
     public static final Pair<String, String> VM_SNAPSHOTS = makeLockingPair(LockingGroup.VM_SNAPSHOTS);
     public static final Pair<String, String> GLUSTER      = makeLockingPair(LockingGroup.GLUSTER);

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/AddStorageServerConnectionCommand.java
@@ -1,14 +1,19 @@
 package org.ovirt.engine.core.bll.storage;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.InternalCommandAttribute;
+import org.ovirt.engine.core.bll.LockIdNameAttribute;
+import org.ovirt.engine.core.bll.LockMessagesMatchUtil;
 import org.ovirt.engine.core.common.action.StorageServerConnectionParametersBase;
 import org.ovirt.engine.core.common.businessentities.StorageServerConnections;
 import org.ovirt.engine.core.common.businessentities.StorageType;
 import org.ovirt.engine.core.common.businessentities.VDSStatus;
 import org.ovirt.engine.core.common.businessentities.storage_pool;
+import org.ovirt.engine.core.common.utils.Pair;
 import org.ovirt.engine.core.common.validation.NfsMountPointConstraint;
 import org.ovirt.engine.core.common.validation.group.CreateEntity;
 import org.ovirt.engine.core.compat.Guid;
@@ -16,6 +21,7 @@ import org.ovirt.engine.core.dal.VdcBllMessages;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 @SuppressWarnings("serial")
+@LockIdNameAttribute(isReleaseAtEndOfExecute = true)
 @InternalCommandAttribute
 public class AddStorageServerConnectionCommand<T extends StorageServerConnectionParametersBase> extends
         ConnectStorageToVdsCommand<T> {
@@ -87,5 +93,12 @@ public class AddStorageServerConnectionCommand<T extends StorageServerConnection
     protected List<Class<?>> getValidationGroups() {
         addValidationGroup(CreateEntity.class);
         return super.getValidationGroups();
+    }
+
+    @Override
+    protected Map<String, Pair<String, String>> getExclusiveLocks() {
+        //lock the path to NFS to avoid at the same time if some other user tries to:
+        // add new storage domain to same path or edit another storage server connection to point to same path
+        return Collections.singletonMap(getParameters().getStorageServerConnection().getconnection(), LockMessagesMatchUtil.STORAGE_CONNECTION);
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/RemoveStorageServerConnectionCommand.java
@@ -1,10 +1,17 @@
 package org.ovirt.engine.core.bll.storage;
 
 import org.apache.commons.lang.StringUtils;
+import org.ovirt.engine.core.bll.LockIdNameAttribute;
+import org.ovirt.engine.core.bll.LockMessagesMatchUtil;
 import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
 import org.ovirt.engine.core.common.action.StorageServerConnectionParametersBase;
+import org.ovirt.engine.core.common.utils.Pair;
+
+import java.util.Collections;
+import java.util.Map;
 
 @NonTransactiveCommandAttribute
+@LockIdNameAttribute(isReleaseAtEndOfExecute = true)
 public class RemoveStorageServerConnectionCommand<T extends StorageServerConnectionParametersBase> extends DisconnectStorageServerConnectionCommand {
 
     public RemoveStorageServerConnectionCommand(T parameters) {
@@ -22,4 +29,10 @@ public class RemoveStorageServerConnectionCommand<T extends StorageServerConnect
         disconnectStorage();
         setSucceeded(true);
     }
+
+    @Override
+    protected Map<String, Pair<String, String>> getExclusiveLocks() {
+       return Collections.singletonMap(getConnection().getconnection(), LockMessagesMatchUtil.STORAGE_CONNECTION);
+    }
+
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/UpdateStorageServerConnectionCommand.java
@@ -1,0 +1,272 @@
+package org.ovirt.engine.core.bll.storage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ovirt.engine.core.bll.LockIdNameAttribute;
+import org.ovirt.engine.core.bll.LockMessagesMatchUtil;
+import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
+import org.ovirt.engine.core.bll.context.CompensationContext;
+import org.ovirt.engine.core.common.action.StorageServerConnectionParametersBase;
+import org.ovirt.engine.core.common.businessentities.StorageDomain;
+import org.ovirt.engine.core.common.businessentities.StorageDomainStatus;
+import org.ovirt.engine.core.common.businessentities.StorageDomainType;
+import org.ovirt.engine.core.common.businessentities.StoragePoolIsoMap;
+import org.ovirt.engine.core.common.businessentities.StoragePoolIsoMapId;
+import org.ovirt.engine.core.common.businessentities.StorageServerConnections;
+import org.ovirt.engine.core.common.businessentities.StorageType;
+import org.ovirt.engine.core.common.errors.VdcBllErrors;
+import org.ovirt.engine.core.common.errors.VdcFault;
+import org.ovirt.engine.core.common.utils.Pair;
+import org.ovirt.engine.core.common.validation.NfsMountPointConstraint;
+import org.ovirt.engine.core.common.vdscommands.ConnectStorageServerVDSCommandParameters;
+import org.ovirt.engine.core.common.vdscommands.GetStorageDomainStatsVDSCommandParameters;
+import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
+import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dal.VdcBllMessages;
+import org.ovirt.engine.core.dao.StorageDomainDAO;
+import org.ovirt.engine.core.dao.StorageDomainDynamicDAO;
+import org.ovirt.engine.core.dao.StoragePoolIsoMapDAO;
+import org.ovirt.engine.core.dao.StorageServerConnectionDAO;
+import org.ovirt.engine.core.utils.transaction.TransactionMethod;
+import org.ovirt.engine.core.utils.transaction.TransactionSupport;
+
+@NonTransactiveCommandAttribute(forceCompensation = true)
+@LockIdNameAttribute(isReleaseAtEndOfExecute = true)
+public class UpdateStorageServerConnectionCommand<T extends StorageServerConnectionParametersBase> extends StorageServerConnectionCommandBase<T> {
+    private List<StorageDomain> domains = null;
+
+    public UpdateStorageServerConnectionCommand(T parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected boolean canDoAction() {
+        StorageServerConnections newConnectionDetails = getParameters().getStorageServerConnection();
+
+        if (newConnectionDetails.getstorage_type() != StorageType.NFS) {
+            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_CONNECTION_UNSUPPORTED_ACTION_FOR_STORAGE);
+        }
+
+        // todo change it to use annotation - in future patch
+        // Check if the NFS path has a valid format
+        if (!new NfsMountPointConstraint().isValid(newConnectionDetails.getconnection(), null)) {
+            return failCanDoAction(VdcBllMessages.VALIDATION_STORAGE_CONNECTION_INVALID);
+        }
+
+        Guid vdsmId = getParameters().getVdsId();
+        if (vdsmId == null || vdsmId.equals(Guid.Empty)) {
+            return failCanDoAction(VdcBllMessages.VDS_EMPTY_NAME_OR_ID);
+        }
+
+        // Check if connection exists by id - otherwise there's nothing to update
+        String connectionId = newConnectionDetails.getid();
+
+        StorageServerConnections oldConnection = getStorageConnDao().get(connectionId);
+
+        if (oldConnection == null) {
+            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_CONNECTION_NOT_EXIST);
+        }
+
+        if (!newConnectionDetails.getstorage_type().equals(oldConnection.getstorage_type())) {
+            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_CONNECTION_UNSUPPORTED_CHANGE_STORAGE_TYPE);
+        }
+
+        if (!oldConnection.getconnection().equals(newConnectionDetails.getconnection())) {
+            // Check that there is no other connection with the new suggested path
+            List<StorageServerConnections> connections =
+                    getStorageConnDao().getAllForStorage(newConnectionDetails.getconnection());
+            if (!connections.isEmpty()) {
+                return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_CONNECTION_ALREADY_EXISTS);
+            }
+        }
+
+        if (domains == null) {
+            domains = getStorageDomainsByConnId(newConnectionDetails.getid());
+        }
+        if (domains.isEmpty()) {
+            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_DOMAIN_NOT_EXIST);
+        }
+        else if (domains.size() == 1) {
+            setStorageDomain(domains.get(0));
+        }
+        else {
+            String domainNames = createDomainNamesList(domains);
+            addCanDoActionMessage(String.format("$domainNames %1$s", domainNames));
+            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_CONNECTION_BELONGS_TO_SEVERAL_STORAGE_DOMAINS);
+        }
+
+        // Check that the storage domain is in proper state to be edited
+        if (!isConnectionEditable(getStorageDomain())) {
+            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_CONNECTION_UNSUPPORTED_ACTION_FOR_STORAGE);
+        }
+
+        return super.canDoAction();
+    }
+
+    protected String createDomainNamesList(List<StorageDomain> domains) {
+        // Build domain names list to display in the error
+        StringBuilder domainNames = new StringBuilder();
+        for (StorageDomain domain : domains) {
+            domainNames.append(domain.getStorageName());
+            domainNames.append(",");
+        }
+        // Remove the last "," after the last domain
+        domainNames.deleteCharAt(domainNames.length() - 1);
+        return domainNames.toString();
+    }
+
+    protected boolean isConnectionEditable(StorageDomain storageDomain) {
+        boolean isEditable =
+                (storageDomain.getStorageDomainType() == StorageDomainType.Data || storageDomain.getStorageDomainType() == StorageDomainType.Master)
+                        && storageDomain.getStatus() == StorageDomainStatus.Maintenance;
+        return isEditable;
+    }
+
+    @Override
+    protected void executeCommand() {
+        StoragePoolIsoMap map = getStoragePoolIsoMap();
+
+        changeStorageDomainStatusInTransaction(map, StorageDomainStatus.Locked);
+        // connect to the new path
+        boolean hasConnectStorageSucceeded = connectToStorage();
+        VDSReturnValue returnValueUpdatedStorageDomain = null;
+
+        if (!hasConnectStorageSucceeded) {
+            setSucceeded(false);
+            VdcFault f = new VdcFault();
+            f.setError(VdcBllErrors.StorageServerConnectionError);
+            getReturnValue().setFault(f);
+            return;
+        }
+        // update info such as free space - because we switched to a different server
+        returnValueUpdatedStorageDomain = getStatsForDomain();
+
+        if (returnValueUpdatedStorageDomain.getSucceeded()) {
+            final StorageDomain updatedStorageDomain = (StorageDomain) returnValueUpdatedStorageDomain.getReturnValue();
+            executeInNewTransaction(new TransactionMethod<Void>() {
+                @Override
+                public Void runInTransaction() {
+                    getStorageConnDao().update(getParameters().getStorageServerConnection());
+                    getStorageDomainDynamicDao().update(updatedStorageDomain.getStorageDynamicData());
+                    return null;
+                }
+            });
+
+            setSucceeded(true);
+        }
+        updateStatus(map, StorageDomainStatus.Maintenance);
+        disconnectFromStorage();
+    }
+
+    protected StoragePoolIsoMap getStoragePoolIsoMap() {
+        StoragePoolIsoMapId mapId = new StoragePoolIsoMapId(getStorageDomain().getId(),
+                getParameters().getStoragePoolId());
+        return getStoragePoolIsoMapDao().get(mapId);
+    }
+
+    protected void changeStorageDomainStatusInTransaction(final StoragePoolIsoMap map,
+            final StorageDomainStatus status) {
+        executeInNewTransaction(new TransactionMethod<StoragePoolIsoMap>() {
+            @Override
+            public StoragePoolIsoMap runInTransaction() {
+                CompensationContext context = getCompensationContext();
+                context.snapshotEntityStatus(map, map.getstatus());
+                updateStatus(map, status);
+                getCompensationContext().stateChanged();
+                return null;
+            }
+        });
+    }
+
+    protected void updateStatus(StoragePoolIsoMap map, StorageDomainStatus status) {
+        map.setstatus(status);
+        getStoragePoolIsoMapDao().updateStatus(map.getId(), map.getstatus());
+    }
+
+    protected void executeInNewTransaction(TransactionMethod<?> method) {
+        TransactionSupport.executeInNewTransaction(method);
+    }
+
+    protected boolean connectToStorage() {
+        ConnectStorageServerVDSCommandParameters newConnectionParametersForVdsm =
+                createParametersForVdsm(getParameters().getVdsId(),
+                        getParameters().getStoragePoolId(),
+                        getParameters().getStorageServerConnection().getstorage_type(),
+                        getParameters().getStorageServerConnection());
+        return runVdsCommand(VDSCommandType.ConnectStorageServer, newConnectionParametersForVdsm).getSucceeded();
+    }
+
+    protected void disconnectFromStorage() {
+        ConnectStorageServerVDSCommandParameters connectionParametersForVdsm =
+                createParametersForVdsm(getParameters().getVdsId(),
+                        getParameters().getStoragePoolId(),
+                        getParameters().getStorageServerConnection().getstorage_type(),
+                        getParameters().getStorageServerConnection());
+        boolean isDisconnectSucceeded = runVdsCommand(VDSCommandType.DisconnectStorageServer, connectionParametersForVdsm).getSucceeded();
+        if(!isDisconnectSucceeded) {
+            log.warn("Failed to disconnect storage connection " + getParameters().getStorageServerConnection());
+        }
+    }
+
+    protected VDSReturnValue getStatsForDomain() {
+        return runVdsCommand(VDSCommandType.GetStorageDomainStats,
+                new GetStorageDomainStatsVDSCommandParameters(getVds().getId(), getStorageDomain().getId()));
+    }
+
+    protected ConnectStorageServerVDSCommandParameters createParametersForVdsm(Guid vdsmId,
+            Guid storagePoolId,
+            StorageType storageType,
+            StorageServerConnections storageServerConnection) {
+        ConnectStorageServerVDSCommandParameters newConnectionParametersForVdsm =
+                new ConnectStorageServerVDSCommandParameters(vdsmId, storagePoolId, storageType,
+                        new ArrayList<StorageServerConnections>(Arrays
+                                .asList(storageServerConnection)));
+        return newConnectionParametersForVdsm;
+    }
+
+    protected StorageDomainDAO getStorageDomainDao() {
+        return getDbFacade().getStorageDomainDao();
+    }
+
+    protected StorageServerConnectionDAO getStorageConnDao() {
+        return getDbFacade().getStorageServerConnectionDao();
+    }
+
+    protected StorageDomainDynamicDAO getStorageDomainDynamicDao() {
+        return getDbFacade().getStorageDomainDynamicDao();
+    }
+
+    protected StoragePoolIsoMapDAO getStoragePoolIsoMapDao() {
+        return getDbFacade().getStoragePoolIsoMapDao();
+    }
+
+    protected List<StorageDomain> getStorageDomainsByConnId(String connectionId) {
+        return getStorageDomainDao().getAllByConnectionId(Guid.createGuidFromString(connectionId));
+    }
+
+    @Override
+    protected Map<String, Pair<String, String>> getExclusiveLocks() {
+        Map<String, Pair<String, String>> locks = new HashMap<String, Pair<String, String>>();
+        domains = getStorageDomainsByConnId(getParameters().getStorageServerConnection().getid());
+        if (!domains.isEmpty() && domains.size() == 1) {
+            setStorageDomain(domains.get(0));
+            locks.put(getStorageDomain().getId().toString(), LockMessagesMatchUtil.STORAGE);
+        }
+        // lock the path to NFS to avoid at the same time if some other user tries to:
+        // add new storage domain to same path or edit another storage server connection to point to same path
+        locks.put(getParameters().getStorageServerConnection().getconnection(),
+                LockMessagesMatchUtil.STORAGE_CONNECTION);
+        return locks;
+    }
+
+    @Override
+    protected void setActionMessageParameters() {
+        addCanDoActionMessage(VdcBllMessages.VAR__ACTION__UPDATE);
+        addCanDoActionMessage(VdcBllMessages.VAR__TYPE__STORAGE__DOMAIN);
+    }
+}

commit 35b2ea7f9ff3d5dfc5aa74a14578b4784aedccf0

webadmin: fix NPE on add NFS
Fix regression introduced by:
bfd9299661c6f6adeea998e93232c8a8789b9dc0

Change-Id: Ibf0f41ceb4d58997a918674bd1919337911d09ef
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/storage/StorageListModel.java
@@ -1470,9 +1470,6 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
         storageDomain.setDescription((String) model.getDescription().getEntity());
         storageDomain.setStorageFormat((StorageFormatType) model.getFormat().getSelectedItem());
 
-        connection = new StorageServerConnections();
-        connection.setid(selectedItem.getStorage());
-
         if (isNew)
         {
             AsyncDataProvider.GetStorageDomainsByConnection(new AsyncQuery(this, new INewAsyncCallback() {
@@ -1497,6 +1494,9 @@ public class StorageListModel extends ListWithDetailsModel implements ITaskTarge
         }
         else
         {
+            connection = new StorageServerConnections();
+            connection.setid(storageDomain.getStorage());
+
             StorageDomain storageDomain = (StorageDomain) getSelectedItem();
             if(isNfsPathEditable(storageDomain)) {
                 updateNfsPath();