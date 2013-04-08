3198656a43b6eddbabd5b5fb5d304d9178b1bf37
Allon Mureinik <amureini@redhat.com>
2013-04-08 19:46:08 +0300

    core: IrsServerWrapper formatting
    Used Eclipse's auto-formatting to apply @Override annotations to all the
    methods of IrsServerWrapper.
    
    Change-Id: I92219c5a1a1901951524f7a7666822bb8b8f6f27
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/IrsServerWrapper.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/IrsServerWrapper.java
@@ -20,6 +20,7 @@ public class IrsServerWrapper implements IIrsServer {
         return httpClient;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc createVolume(String sdUUID, String spUUID, String imgGUID, String size,
             int volFormat, int volType, int diskType, String volUUID, String descr, String srcImgGUID, String srcVolUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.createVolume(sdUUID, spUUID, imgGUID, size, volFormat,
@@ -28,6 +29,7 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc copyImage(String sdUUID, String spUUID, String vmGUID, String srcImgGUID,
             String srcVolUUID, String dstImgGUID, String dstVolUUID, String descr, String dstSdUUID, int volType,
             int volFormat, int preallocate, String postZero, String force) {
@@ -37,6 +39,7 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc setVolumeDescription(String sdUUID, String spUUID, String imgGUID, String volUUID,
             String description) {
         Map<String, Object> xmlRpcReturnValue = irsServer.setVolumeDescription(sdUUID, spUUID, imgGUID, volUUID,
@@ -45,6 +48,7 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc mergeSnapshots(String sdUUID, String spUUID, String vmGUID, String imgGUID,
             String ancestorUUID, String successorUUID, String postZero) {
         Map<String, Object> xmlRpcReturnValue = irsServer.mergeSnapshots(sdUUID, spUUID, vmGUID, imgGUID, ancestorUUID,
@@ -53,6 +57,7 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc deleteVolume(String sdUUID, String spUUID, String imgGUID, String[] volUUID,
             String postZero, String force) {
         Map<String, Object> xmlRpcReturnValue = irsServer.deleteVolume(sdUUID, spUUID, imgGUID, volUUID, postZero,
@@ -61,18 +66,21 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public OneImageInfoReturnForXmlRpc getVolumeInfo(String sdUUID, String spUUID, String imgGUID, String volUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.getVolumeInfo(sdUUID, spUUID, imgGUID, volUUID);
         OneImageInfoReturnForXmlRpc wrapper = new OneImageInfoReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public IrsStatsAndStatusXmlRpc getIrsStats() {
         Map<String, Object> xmlRpcReturnValue = irsServer.getStats();
         IrsStatsAndStatusXmlRpc wrapper = new IrsStatsAndStatusXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc exportCandidate(String sdUUID, String vmGUID, String[] volumesList, String vmMeta,
             String templateGUID, String templateVolGUID, String templateMeta, String expPath, String collapse,
             String force) {
@@ -82,6 +90,7 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc importCandidate(String sdUUID, String vmGUID, String templateGUID,
             String templateVolGUID, String path, String type, String force) {
         Map<String, Object> xmlRpcReturnValue = irsServer.importCandidate(sdUUID, vmGUID, templateGUID,
@@ -90,18 +99,21 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public IsoListReturnForXmlRpc getIsoList(String spUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.getIsoList(spUUID);
         IsoListReturnForXmlRpc wrapper = new IsoListReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public IsoListReturnForXmlRpc getFloppyList(String spUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.getFloppyList(spUUID);
         IsoListReturnForXmlRpc wrapper = new IsoListReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc extendVolume(String sdUUID, String spUUID, String imgGUID, String volUUID,
             int newSize) {
         Map<String, Object> xmlRpcReturnValue = irsServer.extendVolume(sdUUID, spUUID, imgGUID, volUUID, newSize);
@@ -109,12 +121,14 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public StorageStatusReturnForXmlRpc activateStorageDomain(String sdUUID, String spUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.activateStorageDomain(sdUUID, spUUID);
         StorageStatusReturnForXmlRpc wrapper = new StorageStatusReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc deactivateStorageDomain(String sdUUID, String spUUID, String msdUUID,
             int masterVersion) {
         Map<String, Object> xmlRpcReturnValue = irsServer.deactivateStorageDomain(sdUUID, spUUID, msdUUID,
@@ -123,30 +137,35 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc detachStorageDomain(String sdUUID, String spUUID, String msdUUID, int masterVersion) {
         Map<String, Object> xmlRpcReturnValue = irsServer.detachStorageDomain(sdUUID, spUUID, msdUUID, masterVersion);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc forcedDetachStorageDomain(String sdUUID, String spUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.forcedDetachStorageDomain(sdUUID, spUUID);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc attachStorageDomain(String sdUUID, String spUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.attachStorageDomain(sdUUID, spUUID);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc setStorageDomainDescription(String sdUUID, String description) {
         Map<String, Object> xmlRpcReturnValue = irsServer.setStorageDomainDescription(sdUUID, description);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StorageDomainListReturnForXmlRpc reconstructMaster(String spUUID, int hostSpmId, String msdUUID,
             String masterVersion) {
         Map<String, Object> xmlRpcReturnValue = irsServer.reconstructMaster(spUUID, hostSpmId, msdUUID, masterVersion);
@@ -154,36 +173,42 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc extendStorageDomain(String sdUUID, String spUUID, String[] devlist) {
         Map<String, Object> xmlRpcReturnValue = irsServer.extendStorageDomain(sdUUID, spUUID, devlist);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc extendStorageDomain(String sdUUID, String spUUID, String[] devlist, boolean force) {
         Map<String, Object> xmlRpcReturnValue = irsServer.extendStorageDomain(sdUUID, spUUID, devlist, force);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc setStoragePoolDescription(String spUUID, String description) {
         Map<String, Object> xmlRpcReturnValue = irsServer.setStoragePoolDescription(spUUID, description);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StoragePoolInfoReturnForXmlRpc getStoragePoolInfo(String spUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.getStoragePoolInfo(spUUID);
         StoragePoolInfoReturnForXmlRpc wrapper = new StoragePoolInfoReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc destroyStoragePool(String spUUID, int hostSpmId, String SCSIKey) {
         Map<String, Object> xmlRpcReturnValue = irsServer.destroyStoragePool(spUUID, hostSpmId, SCSIKey);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc deleteImage(String sdUUID, String spUUID, String imgGUID, String postZero,
             String force) {
         Map<String, Object> xmlRpcReturnValue = irsServer.deleteImage(sdUUID, spUUID, imgGUID, postZero, force);
@@ -191,6 +216,7 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc moveImage(String spUUID, String srcDomUUID, String dstDomUUID, String imgGUID,
             String vmGUID, int op, String postZero, String force) {
         Map<String, Object> xmlRpcReturnValue = irsServer.moveImage(spUUID, srcDomUUID, dstDomUUID, imgGUID, vmGUID,
@@ -199,24 +225,28 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc cloneImageStructure(String spUUID, String srcDomUUID, String imgGUID, String dstDomUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.cloneImageStructure(spUUID, srcDomUUID, imgGUID, dstDomUUID);
         OneUuidReturnForXmlRpc wrapper = new OneUuidReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public OneUuidReturnForXmlRpc syncImageData(String spUUID, String srcDomUUID, String imgGUID, String dstDomUUID, String syncType) {
         Map<String, Object> xmlRpcReturnValue = irsServer.syncImageData(spUUID, srcDomUUID, imgGUID, dstDomUUID, syncType);
         OneUuidReturnForXmlRpc wrapper = new OneUuidReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StorageDomainGuidListReturnForXmlRpc getImageDomainsList(String spUUID, String imgUUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.getImageDomainsList(spUUID, imgUUID);
         StorageDomainGuidListReturnForXmlRpc wrapper = new StorageDomainGuidListReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc setMaxHosts(int maxHosts) {
         Map<String, Object> xmlRpcReturnValue = irsServer.setMaxHosts(maxHosts);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
@@ -230,6 +260,7 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc removeVM(String spUUID, String vmGUID) {
         Map<String, Object> xmlRpcReturnValue = irsServer.removeVM(spUUID, vmGUID);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
@@ -243,18 +274,21 @@ public class IrsServerWrapper implements IIrsServer {
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc removeVM(String spUUID, String vmGUID, String storageDomainId) {
         Map<String, Object> xmlRpcReturnValue = irsServer.removeVM(spUUID, vmGUID, storageDomainId);
         StatusOnlyReturnForXmlRpc wrapper = new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public GetVmsInfoReturnForXmlRpc getVmsInfo(String storagePoolId, String storageDomainId, String[] VMIDList) {
         Map<String, Object> xmlRpcReturnValue = irsServer.getVmsInfo(storagePoolId, storageDomainId, VMIDList);
         GetVmsInfoReturnForXmlRpc wrapper = new GetVmsInfoReturnForXmlRpc(xmlRpcReturnValue);
         return wrapper;
     }
 
+    @Override
     public StatusOnlyReturnForXmlRpc upgradeStoragePool(String storagePoolId, String targetVersion) {
         Map<String, Object> xmlRpcReturnValue = irsServer.upgradeStoragePool(storagePoolId, targetVersion);
         return new StatusOnlyReturnForXmlRpc(xmlRpcReturnValue);
