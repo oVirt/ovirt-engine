e78fa0e3767f781a9ba383392c0db80ffaa92d1c
Allon Mureinik <amureini@redhat.com>
2013-04-10 08:32:55 +0300

    core: use JUnit 4 syntax in RegexTest
    Change-Id: I4633091a1b929af23fea333f2a1fa661b83bc575
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/compat/src/test/java/org/ovirt/engine/core/compat/RegexTest.java
+++ backend/manager/modules/compat/src/test/java/org/ovirt/engine/core/compat/RegexTest.java
@@ -1,31 +1,41 @@
 package org.ovirt.engine.core.compat;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class RegexTest extends TestCase {
+import org.junit.Test;
+
+public class RegexTest {
+
+    @Test
     public void testSimpleMatches() {
         Regex regex = new Regex("[0-9]");
         assertTrue("A number should match", regex.IsMatch("1"));
         assertFalse("A letter should not match", regex.IsMatch("a"));
     }
 
+    @Test
     public void testIsMatch() {
         assertTrue("A number should match", Regex.IsMatch("1", "[0-9]"));
         assertFalse("A letter should not match", Regex.IsMatch("a", "[0-9]"));
     }
 
+    @Test
     public void testIgnoreCaseOff() {
         Regex regex = new Regex("[A-Z]");
         assertTrue("A cap should match", regex.IsMatch("K"));
         assertFalse("A lowercase should not match", regex.IsMatch("k"));
     }
 
+    @Test
     public void testIgnoreCaseOn() {
         Regex regex = new Regex("[A-Z]", RegexOptions.IgnoreCase);
         assertTrue("A cap should match", regex.IsMatch("K"));
         assertTrue("A lowercase should  match", regex.IsMatch("k"));
     }
 
+    @Test
     public void testGroups() {
         Match match = Regex.Match("ABC,123", "([A-Z]+),([0-9]+)");
         assertEquals("First Group", "ABC", match.Groups().get(0).getValue());
