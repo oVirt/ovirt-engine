335df8bd674bff7f1f0f4bb94d2d0461f021180e
Allon Mureinik <amureini@redhat.com>
2013-04-11 17:52:40 +0300

    core: Remove the ISession interface
    This interface is not implemented nor used anywhere in the codebase.
    
    Change-Id: I354a9470e4ca9d541417f3418ff05b8cca52b002
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ISession.java
+++
@@ -1,7 +0,0 @@
-package org.ovirt.engine.core.utils;
-
-public interface ISession {
-    void SetSessionDataBySessionId(String sessionId, String key, Object value);
-
-    void SetSessionData(String key, Object value);
-}
