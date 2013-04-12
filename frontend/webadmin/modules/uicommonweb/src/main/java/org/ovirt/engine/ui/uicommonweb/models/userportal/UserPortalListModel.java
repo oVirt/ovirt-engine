oVirt/ovirt-engine

commit f4fcac9d9a6873ea294ab1f14c24a83a61264d07

engine: Frontend support for setting per-VM VncKeyboardLayout
Added combobox to EditVM dialog for setting the VncKeyboardLayout per
individual VM.

Change-Id: I8f143e5b8425805ea87651ec382f0bc95f584591
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=916866
Signed-off-by: Martin Betak <mbetak@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/userportal/UserPortalListModel.java
@@ -32,6 +32,7 @@ import org.ovirt.engine.core.common.businessentities.VmOsType;
 import org.ovirt.engine.core.common.businessentities.VmPool;
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.businessentities.VmType;
+import org.ovirt.engine.core.common.businessentities.VolumeType;
 import org.ovirt.engine.core.common.businessentities.storage_pool;
 import org.ovirt.engine.core.common.businessentities.network.VmNetworkInterface;
 import org.ovirt.engine.core.common.queries.GetAllDisksByVmIdParameters;
@@ -644,6 +645,7 @@ public class UserPortalListModel extends IUserPortalListModel implements IVmPool
         tempVar.setInitrdUrl(vm.getInitrdUrl());
         tempVar.setKernelUrl(vm.getKernelUrl());
         tempVar.setKernelParams(vm.getKernelParams());
+        tempVar.setVncKeyboardLayout(vm.getVncKeyboardLayout());
         VM newvm = tempVar;
 
         EntityModel displayProtocolSelectedItem = (EntityModel) model.getDisplayProtocol().getSelectedItem();
@@ -1337,6 +1339,7 @@ public class UserPortalListModel extends IUserPortalListModel implements IVmPool
         gettempVm().setIsoPath(model.getCdImage().getIsChangable() ? (String) model.getCdImage()
                 .getSelectedItem() : ""); //$NON-NLS-1$
         gettempVm().setAutoStartup((Boolean) model.getIsHighlyAvailable().getEntity());
+        gettempVm().setVncKeyboardLayout((String) model.getVncKeyboardLayout().getSelectedItem());
 
         gettempVm().setInitrdUrl((String) model.getInitrd_path().getEntity());
         gettempVm().setKernelUrl((String) model.getKernel_path().getEntity());

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/ConfigurationValues.java
@@ -83,6 +83,8 @@ public enum ConfigurationValues {
     CpuPinMigrationEnabled,
     MigrationSupportForNativeUsb(ConfigAuthType.User),
     MigrationNetworkEnabled,
+    VncKeyboardLayout(ConfigAuthType.User),
+    VncKeyboardLayoutValidValues(ConfigAuthType.User)
     ;
 
     public static enum ConfigAuthType {

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationConstants.java
@@ -370,6 +370,9 @@ public interface CommonApplicationConstants extends Constants {
     @DefaultStringValue("Protocol")
     String protocolVmPopup();
 
+    @DefaultStringValue("VNC Keyboard Layout")
+    String vncKeyboardLayoutVmPopup();
+
     @DefaultStringValue("USB Support")
     String usbPolicyVmPopup();
 

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationMessages.java
@@ -99,4 +99,7 @@ public interface CommonApplicationMessages extends Messages {
 
     @DefaultMessage("{0} sec")
     String refreshRateSeconds(Integer seconds);
+
+    @DefaultMessage("default [{0}]")
+    String globalVncKeyboardLayoutCaption(String currentDefault);
 }

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/AbstractVmPopupWidget.java
@@ -262,6 +262,11 @@ public abstract class AbstractVmPopupWidget extends AbstractModelBoundPopupWidge
     public ListModelListBoxEditor<Object> displayProtocolEditor;
 
     @UiField(provided = true)
+    @Path(value = "vncKeyboardLayout.selectedItem")
+    @WithElementId("vncKeyboardLayout")
+    public ListModelListBoxEditor<Object> vncKeyboardLayoutEditor;
+
+    @UiField(provided = true)
     @Path(value = "usbPolicy.selectedItem")
     @WithElementId("usbPolicy")
     public ListModelListBoxEditor<Object> usbSupportEditor;
@@ -634,6 +639,21 @@ public abstract class AbstractVmPopupWidget extends AbstractModelBoundPopupWidge
             }
         });
 
+        vncKeyboardLayoutEditor = new ListModelListBoxEditor<Object>(new AbstractRenderer<Object>() {
+
+            final String globalLayout = (String)AsyncDataProvider.GetConfigValuePreConverted(ConfigurationValues.VncKeyboardLayout);
+
+            @Override
+            public String render(Object object) {
+                if (object == null) {
+                    return messages.globalVncKeyboardLayoutCaption(globalLayout);
+                } else {
+                    return object.toString();
+                }
+            }
+
+        });
+
         // Host Tab
         specificHost = new RadioButton("runVmOnHostGroup"); //$NON-NLS-1$
         isAutoAssignEditor = new EntityModelRadioButtonEditor("runVmOnHostGroup"); //$NON-NLS-1$
@@ -714,6 +734,7 @@ public abstract class AbstractVmPopupWidget extends AbstractModelBoundPopupWidge
         // Console Tab
         consoleTab.setLabel(constants.consoleVmPopup());
         displayProtocolEditor.setLabel(constants.protocolVmPopup());
+        vncKeyboardLayoutEditor.setLabel(constants.vncKeyboardLayoutVmPopup());
         usbSupportEditor.setLabel(constants.usbPolicyVmPopup());
         numOfMonitorsEditor.setLabel(constants.monitorsVmPopup());
         allowConsoleReconnectEditor.setLabel(constants.allowConsoleReconnect());
@@ -1076,6 +1097,7 @@ public abstract class AbstractVmPopupWidget extends AbstractModelBoundPopupWidge
         // ==Console Tab==
         nextTabIndex = consoleTab.setTabIndexes(nextTabIndex);
         displayProtocolEditor.setTabIndex(nextTabIndex++);
+        vncKeyboardLayoutEditor.setTabIndex(nextTabIndex++);
         usbSupportEditor.setTabIndex(nextTabIndex++);
         numOfMonitorsEditor.setTabIndex(nextTabIndex++);
         isSmartcardEnabledEditor.setTabIndex(nextTabIndex++);

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/AbstractVmPopupWidget.ui.xml
@@ -307,6 +307,7 @@
 				<t:content>
 					<g:FlowPanel>
 						<e:ListModelListBoxEditor ui:field="displayProtocolEditor" />
+						<e:ListModelListBoxEditor ui:field="vncKeyboardLayoutEditor" />
 						<e:ListModelListBoxEditor ui:field="usbSupportEditor" />
 						<e:ListModelListBoxEditor ui:field="numOfMonitorsEditor" />
 						<e:EntityModelCheckBoxEditor ui:field="isSmartcardEnabledEditor" addStyleNames="{style.checkbox}" />

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/pools/PoolListModel.java
@@ -487,6 +487,7 @@ public class PoolListModel extends ListWithDetailsModel
                         desktop.setKernelParams((String) model.getKernel_parameters().getEntity());
                         desktop.setInitrdUrl((String) model.getInitrd_path().getEntity());
                         desktop.setMigrationSupport((MigrationSupport) (model.getMigrationMode().getSelectedItem()));
+                        desktop.setVncKeyboardLayout((String) model.getVncKeyboardLayout().getSelectedItem());
 
                         EntityModel displayProtocolSelectedItem =
                                 (EntityModel) model.getDisplayProtocol().getSelectedItem();

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/templates/TemplateListModel.java
@@ -527,6 +527,7 @@ public class TemplateListModel extends VmBaseListModel<VmTemplate> implements IS
         template.setKernelUrl((String) model.getKernel_path().getEntity());
         template.setKernelParams((String) model.getKernel_parameters().getEntity());
         template.setInitrdUrl((String) model.getInitrd_path().getEntity());
+        template.setVncKeyboardLayout((String) model.getVncKeyboardLayout().getSelectedItem());
 
         if (model.getQuota().getIsAvailable() && model.getQuota().getSelectedItem() != null) {
             template.setQuotaId(((Quota) model.getQuota().getSelectedItem()).getId());

commit 9d0cd22051b0f4c77ad05fc3ab70044736ecf193

userportal: Fix compilation error
Fix unused import checkstyle violation introduced by
f4fcac9d9a6873ea294ab1f14c24a83a61264d07

Change-Id: I167902214c3ed88fc8955883eb5b6197ed0c2795
Signed-off-by: Martin Betak <mbetak@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/userportal/UserPortalListModel.java
@@ -32,7 +32,6 @@ import org.ovirt.engine.core.common.businessentities.VmOsType;
 import org.ovirt.engine.core.common.businessentities.VmPool;
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.businessentities.VmType;
-import org.ovirt.engine.core.common.businessentities.VolumeType;
 import org.ovirt.engine.core.common.businessentities.storage_pool;
 import org.ovirt.engine.core.common.businessentities.network.VmNetworkInterface;
 import org.ovirt.engine.core.common.queries.GetAllDisksByVmIdParameters;