7bfb4bb7a66f676c0a8f982e8520551aa07f3ee6
Ofri Masad <omasad@redhat.com>
2013-04-18 17:46:05 +0300

    webadmin: Fix the phrasing of error message
    message was: Cannot edit VM. Dedicated Host Cluster is not identical to
    VM Cluster.
    
    changed to: Cannot edit VM. VM is pinned to a specific host. The host's
    cluster must be the same as the selected VM cluster.
    
    Change-Id: I298bacc9ce5c2ca9729d79ee3136a71360d329d5
    Bug-Url: https://bugzilla.redhat.com/950693
    Signed-off-by: Ofri Masad <omasad@redhat.com>

--- frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
+++ frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
@@ -433,7 +433,7 @@ public interface AppErrors extends ConstantsWithLookup {
     @DefaultStringValue("Cannot ${action} ${type}. Low disk space on target Storage Domain ${storageName}.")
     String ACTION_TYPE_FAILED_DISK_SPACE_LOW_ON_TARGET_STORAGE_DOMAIN();
 
-    @DefaultStringValue("Cannot ${action} ${type}. Dedicated Host Cluster is not identical to VM Cluster.")
+    @DefaultStringValue("Cannot ${action} ${type}. VM is pinned to a specific host. The host's cluster must be the same as the selected VM cluster.")
     String ACTION_TYPE_FAILED_DEDICATED_VDS_NOT_IN_SAME_CLUSTER();
 
     @DefaultStringValue("Cannot ${action} ${type}. The selected disk configuration is not supported.")
