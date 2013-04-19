2cdcd5bd054378002ca19a5dcfc8075fed3418e6
Allon Mureinik <amureini@redhat.com>
2013-04-19 21:12:45 +0300

    core: Remove unused ImagesHandlerActionParameters
    Change-Id: I15e2ed654331b812a41c6bd68d4a42d4f3077961
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/ImagesHandlerActionParameters.java
+++
@@ -1,8 +0,0 @@
-package org.ovirt.engine.core.common.action;
-
-public class ImagesHandlerActionParameters extends VdcActionParametersBase {
-    private static final long serialVersionUID = 586590899729374093L;
-
-    public ImagesHandlerActionParameters() {
-    }
-}
