4aaa77906dd950157904c80f1516a530b4bb1759
Shahar Havivi <shavivi@redhat.com>
2013-04-28 17:10:09 +0300

    findbugs: equals between different types
    Change-Id: I125e5df8def7a223f9469decc7aa43dab61f4a47
    Signed-off-by: Shahar Havivi <shavivi@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/test/java/org/ovirt/engine/api/restapi/resource/DiskStatisticalQueryTest.java
+++ backend/manager/modules/restapi/jaxrs/src/test/java/org/ovirt/engine/api/restapi/resource/DiskStatisticalQueryTest.java
@@ -12,10 +12,10 @@ import org.ovirt.engine.core.compat.Guid;
 
 public class DiskStatisticalQueryTest {
 
-    private static final double EPSILON = 0.001;
-    private static final double READ_LATENCY = 1.1111;
-    private static final double WRITE_LATENCY = 2.2222;
-    private static final double FLUSH_LATENCY = 3.3333;
+    private static final double EPSILON = 0.1;
+    private static final double READ_LATENCY = 1.1;
+    private static final double WRITE_LATENCY = 2.2;
+    private static final double FLUSH_LATENCY = 3.3;
     private static final int READ_RATE = 4;
     private static final int WRITE_RATE = 5;
 
@@ -47,19 +47,19 @@ public class DiskStatisticalQueryTest {
     }
 
     private void verifyStatistic(Statistic statistic) {
-        if (statistic.getName().equals(DiskStatisticalQuery.DATA_READ)) {
+        if (statistic.getName().equals(DiskStatisticalQuery.DATA_READ.getName())) {
             assertEquals(statistic.getValues().getValues().get(0).getDatum().intValue(), READ_RATE);
         }
-        if (statistic.getName().equals(DiskStatisticalQuery.DATA_WRITE)) {
+        if (statistic.getName().equals(DiskStatisticalQuery.DATA_WRITE.getName())) {
             assertEquals(statistic.getValues().getValues().get(0).getDatum().intValue(), WRITE_RATE);
         }
-        if (statistic.getName().equals(DiskStatisticalQuery.READ_LATENCY)) {
+        if (statistic.getName().equals(DiskStatisticalQuery.READ_LATENCY.getName())) {
             assertEquals(statistic.getValues().getValues().get(0).getDatum().doubleValue(), READ_LATENCY, EPSILON);
         }
-        if (statistic.getName().equals(DiskStatisticalQuery.WRITE_LATENCY)) {
+        if (statistic.getName().equals(DiskStatisticalQuery.WRITE_LATENCY.getName())) {
             assertEquals(statistic.getValues().getValues().get(0).getDatum().doubleValue(), WRITE_LATENCY, EPSILON);
         }
-        if (statistic.getName().equals(DiskStatisticalQuery.FLUSH_LATENCY)) {
+        if (statistic.getName().equals(DiskStatisticalQuery.FLUSH_LATENCY.getName())) {
             assertEquals(statistic.getValues().getValues().get(0).getDatum().doubleValue(), FLUSH_LATENCY, EPSILON);
         }
     }
