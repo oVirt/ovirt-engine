25e2f0a9250b924887725666f933436ec9a2c460
Allon Mureinik <amureini@redhat.com>
2013-05-09 08:05:48 +0300

    core: Remove IUserHanlder
    This interface is not used anywhere in the codebase.
    
    Change-Id: I2ba73f4cfc91f4f58c9eb3f2416c03abaef9c9a2
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/backendinterfaces/IUserHandler.java
+++
@@ -1,9 +0,0 @@
-package org.ovirt.engine.core.common.backendinterfaces;
-
-import org.ovirt.engine.core.common.action.*;
-
-public interface IUserHandler {
-    VdcReturnValueBase Login(LoginUserParameters parameters);
-
-    VdcReturnValueBase Logoff(LogoutUserParameters parameters);
-}
