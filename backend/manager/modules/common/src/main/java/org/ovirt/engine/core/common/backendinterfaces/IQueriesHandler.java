94bf83861158df250881e7ceb71fac13b06ef1c8
Allon Mureinik <amureini@redhat.com>
2013-05-22 20:04:54 +0300

    core: Remove unused IQueriesHandler interface
    Removed the IQueriesHandler interface - it was not used anywhere in the
    codebase.
    
    Change-Id: I61758821548a50d2d249beef5bbce5bb682d1317
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/backendinterfaces/IQueriesHandler.java
+++
@@ -1,9 +0,0 @@
-package org.ovirt.engine.core.common.backendinterfaces;
-
-public interface IQueriesHandler {
-    // List<AuditLog> GetVdsMessages(int vdsId);
-    // List<AuditLog> GetVmsMessages(Guid vmsId);
-    // List<AuditLog> GetUserMessages(Guid userId);
-    // List<AuditLog> GetEventMessages(Guid userId);
-    // List<AuditLog> GetTemplateMessages(Guid userId);
-}
