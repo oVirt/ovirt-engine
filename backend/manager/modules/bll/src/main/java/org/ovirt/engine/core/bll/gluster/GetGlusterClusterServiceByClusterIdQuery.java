9a7952443f5d31d7f77f4c7bd0c8b845c3d7537a
Shubhendu Tripathi <shtripat@redhat.com>
2013-06-03 13:05:20 +0530

    gluster: Added query for gluster cluster service
    Added the query GetGlusterClusterServiceByClusterIdAndServiceType for
    getting gluster cluster service based on cluster id and service type.
    
    Change-Id: I123e39fde8317a9227a1bd008c5afbc5dcb7691d
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/GetGlusterClusterServiceByClusterIdQuery.java
@@ -0,0 +1,29 @@
+package org.ovirt.engine.core.bll.gluster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ovirt.engine.core.common.businessentities.gluster.GlusterClusterService;
+import org.ovirt.engine.core.common.queries.gluster.GlusterServiceQueryParameters;
+
+public class GetGlusterClusterServiceByClusterIdQuery<P extends GlusterServiceQueryParameters> extends GlusterQueriesCommandBase<P> {
+
+    public GetGlusterClusterServiceByClusterIdQuery(P parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected void executeQueryCommand() {
+        List<GlusterClusterService> serviceList = null;
+
+        if(getParameters().getServiceType() == null) {
+            serviceList = getGlusterClusterServiceDao().getByClusterId(getParameters().getId());
+        } else {
+            serviceList = new ArrayList<GlusterClusterService>();
+            serviceList.add(getGlusterClusterServiceDao().getByClusterIdAndServiceType(getParameters().getId(),
+                        getParameters().getServiceType()));
+        }
+
+        getQueryReturnValue().setReturnValue(serviceList);
+    }
+}
