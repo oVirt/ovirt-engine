6d8496f62d3571d2332fa1bc837832e33cfc9d1a
Lior Vernia <lvernia@redhat.com>
2013-06-04 10:53:41 +0300

    core: Added provided object types to ProviderType
    Each ProviderType now reports what types of object it provides. This
    would facilitate many frontend operations.
    
    Change-Id: I8d30a98f0879fa8d7d291815ac603a979ceb91a3
    Signed-off-by: Lior Vernia <lvernia@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/ProviderType.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/ProviderType.java
@@ -1,20 +1,34 @@
 package org.ovirt.engine.core.common.businessentities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.ovirt.engine.core.common.VdcObjectType;
+
 /**
  * The provider type determines what external provider is used.
  */
 public enum ProviderType implements Identifiable {
-    OPENSTACK_NETWORK(0),
-    FOREMAN(1);
+    OPENSTACK_NETWORK(0, VdcObjectType.Network),
+    FOREMAN(1, VdcObjectType.VDS);
 
     private int value;
+    private Set<VdcObjectType> providedTypes;
 
-    private ProviderType(int value) {
+    private ProviderType(int value, VdcObjectType... providedTypes) {
         this.value = value;
+        this.providedTypes = new HashSet<VdcObjectType>();
+        for (VdcObjectType providedType : providedTypes) {
+            this.providedTypes.add(providedType);
+        }
     }
 
     @Override
     public int getValue() {
         return value;
+    }
+
+    public Set<VdcObjectType> getProvidedTypes() {
+        return providedTypes;
     }
 }
