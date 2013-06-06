81e2f687f62c692de38fbfe39fbf68971907ea81
Eli Mesika <emesika@redhat.com>
2013-06-06 16:21:07 +0300

    core: squash 03_00_0260_change_app_list_type_to_text.sql
    Change-Id: I475a8292d4a65326a61907c3fa77896b20da304b
    Signed-off-by: Eli Mesika <emesika@redhat.com>

--- backend/manager/dbscripts/upgrade/03_00_0260_change_app_list_type_to_text.sql
+++
@@ -1,3 +0,0 @@
--- change app_list length of vm_dynamic to unlimited
-select fn_db_change_column_type('images','app_list','varchar','text');
-select fn_db_change_column_type('vm_dynamic','app_list','varchar','text');
