0ee65bf19f287fe9dcd4a0d00794562196a404a6
Sahina Bose <sabose@redhat.com>
2013-06-14 11:29:36 +0530

    engine:Fixed FK violation on update of gluster event names
    A FK violation occurs during upgrade due to event_up name
    being present in event_subscriber
    
    Change-Id: If8d811be0e8eb52d3ebf2d1bfb4e391398b65abe
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- backend/manager/dbscripts/upgrade/03_02_0060_change_host_to_server_event_map.sql
+++ backend/manager/dbscripts/upgrade/03_02_0060_change_host_to_server_event_map.sql
@@ -1,3 +1,9 @@
 -- Change Host to Server in gluster related events
-update event_map set event_up_name = 'GLUSTER_SERVER_ADD_FAILED' where event_up_name = 'GLUSTER_HOST_ADD_FAILED';
-update event_map set event_up_name = 'GLUSTER_SERVER_REMOVE_FAILED' where event_up_name = 'GLUSTER_HOST_REMOVE_FAILED';
+insert into event_map(event_up_name, event_down_name) values('GLUSTER_SERVER_ADD_FAILED', 'UNASSIGNED');
+insert into event_map(event_up_name, event_down_name) values('GLUSTER_SERVER_REMOVE_FAILED', 'UNASSIGNED');
+
+update event_subscriber set event_up_name = 'GLUSTER_SERVER_ADD_FAILED' where event_up_name = 'GLUSTER_HOST_ADD_FAILED';
+update event_subscriber set event_up_name = 'GLUSTER_SERVER_REMOVE_FAILED' where event_up_name = 'GLUSTER_HOST_REMOVE_FAILED';
+
+delete from event_map where event_up_name='GLUSTER_HOST_ADD_FAILED';
+delete from event_map where event_up_name='GLUSTER_HOST_REMOVE_FAILED';
