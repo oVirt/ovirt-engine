b88f4504733810cc788bde6d46ef3b8f379b64b8
Sandro Bonazzola <sbonazzo@redhat.com>
2013-06-21 10:42:09 +0200

    packaging: setup: allow upgrade with 3.0 compatibility level
    Allow upgrade with datacenters and clusters with compatibility
    level set to 3.0 and above.
    
    Change-Id: I5d61318d35c6308e41c78577029a944f061f72c4
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- packaging/setup/plugins/ovirt-engine-common/core/misc.py
+++ packaging/setup/plugins/ovirt-engine-common/core/misc.py
@@ -70,7 +70,7 @@ class Plugin(plugin.PluginBase):
         )
         self.environment.setdefault(
             osetupcons.CoreEnv.UPGRADE_SUPPORTED_VERSIONS,
-            '3.2,3.3'
+            '3.0,3.1,3.2,3.3'
         )
 
         if self.environment[osetupcons.CoreEnv.DEVELOPER_MODE] is None:
