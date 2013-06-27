0396fe3edc5192b4aa604b4c8ced2db0f5c8f3e6
Lior Vernia <lvernia@redhat.com>
2013-06-27 13:19:05 +0300

    webadmin: Added ListModel backed ListBox column
    ListModelListBoxColumn (which uses ListModelListBoxCell to render its
    elements) represents a CellTable column that presents the items of a
    ListModel as a ListBox and allows the user to select an item among
    them.
    
    Change-Id: If9c04c5c32e9a36fa959af55399a44ff33d175d8
    Signed-off-by: Lior Vernia <lvernia@redhat.com>

---
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/ListModelListBoxCell.java
@@ -0,0 +1,116 @@
+package org.ovirt.engine.ui.common.widget.table.column;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ovirt.engine.ui.uicommonweb.models.ListModel;
+import org.ovirt.engine.ui.uicompat.Event;
+import org.ovirt.engine.ui.uicompat.EventArgs;
+import org.ovirt.engine.ui.uicompat.IEventListener;
+
+import com.google.gwt.cell.client.AbstractInputCell;
+import com.google.gwt.cell.client.SelectionCell;
+import com.google.gwt.cell.client.ValueUpdater;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.text.shared.Renderer;
+
+/**
+ * This class may be used to display a ListBox, backed by a ListModel, in cell widgets. It mimics the behaviour of a
+ * SelectionCell by passing on rendering requests and browser events to a member delegate. The SelectionCell options are
+ * set by rendering the items of a ListModel using the renderer passed to the constructor. Additional functionality
+ * includes enabling/disabling the ListBox according to the IsChangeable state of the ListModel.
+ *
+ * @param <T>
+ *            the ListModel item type.
+ */
+public class ListModelListBoxCell<T> extends AbstractInputCell<ListModel, String> {
+
+    private static final String PATTERN_SELECT = "<select"; //$NON-NLS-1$
+    private static final String REPLACEMENT_SELECT = "<select disabled"; //$NON-NLS-1$
+
+    private ListModel model;
+    private SelectionCell delegate;
+    private Map<String, T> entityByName;
+    private final Renderer<T> renderer;
+    private final IEventListener itemsChangedListener;
+
+    public ListModelListBoxCell(final Renderer<T> renderer) {
+        super("change"); //$NON-NLS-1$
+        this.renderer = renderer;
+        delegate = new SelectionCell(new ArrayList<String>()); // just to avoid null pointer in setSelection()
+
+        itemsChangedListener = new IEventListener() {
+
+            @Override
+            public void eventRaised(Event ev, Object sender, EventArgs args) {
+                setOptions();
+            }
+        };
+    }
+
+    public void edit(final ListModel model) {
+        if (this.model != null) {
+            this.model.getItemsChangedEvent().removeListener(itemsChangedListener);
+        }
+        this.model = model;
+        model.getItemsChangedEvent().addListener(itemsChangedListener);
+        setOptions();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void setOptions() {
+        Iterable<T> items = model.getItems();
+        if (items == null) {
+            items = new ArrayList<T>();
+        }
+        entityByName = new HashMap<String, T>();
+        List<String> options = new ArrayList<String>();
+
+        for (T entity : items) {
+            String entityName = renderer.render(entity);
+            entityByName.put(entityName, entity);
+            options.add(entityName);
+        }
+        delegate = new SelectionCell(options);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void render(Context context, ListModel value, SafeHtmlBuilder sb) {
+        SafeHtmlBuilder sbDelegate = new SafeHtmlBuilder();
+        delegate.render(context, renderer.render((T) value.getSelectedItem()), sbDelegate);
+        if (value.getIsChangable()) {
+            sb.append(sbDelegate.toSafeHtml());
+        } else {
+            sb.appendHtmlConstant(sbDelegate.toSafeHtml().asString().replaceFirst(PATTERN_SELECT, REPLACEMENT_SELECT));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onBrowserEvent(Context context,
+            Element parent,
+            final ListModel model,
+            NativeEvent event,
+            ValueUpdater<ListModel> valueUpdater) {
+
+        delegate.onBrowserEvent(context,
+                parent,
+                renderer.render((T) model.getSelectedItem()),
+                event,
+                new ValueUpdater<String>() {
+
+                    @Override
+                    public void update(String value) {
+                        if (value != null) {
+                            model.setSelectedItem(entityByName.get(value));
+                        }
+                    }
+                });
+    }
+
+}
---
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/ListModelListBoxColumn.java
@@ -0,0 +1,27 @@
+package org.ovirt.engine.ui.common.widget.table.column;
+
+import org.ovirt.engine.ui.uicommonweb.models.ListModel;
+
+import com.google.gwt.text.shared.Renderer;
+import com.google.gwt.user.cellview.client.Column;
+
+/**
+ * Column for displaying a ListBox backed by ListModel using {@link ListModelListBoxCell}.
+ *
+ * @param <T>
+ *            the row type.
+ * @param <S>
+ *            the ListModel item type.
+ */
+public abstract class ListModelListBoxColumn<T, S> extends Column<T, ListModel> {
+
+    public ListModelListBoxColumn(Renderer<S> renderer) {
+        super(new ListModelListBoxCell<S>(renderer));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void edit(ListModel model) {
+        ((ListModelListBoxCell<S>) getCell()).edit(model);
+    }
+
+}
