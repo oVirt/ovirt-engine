e9f24be6c5c21969173873f54ffe1b724f839b91
Sahina Bose <sabose@redhat.com>
2013-06-27 09:53:02 +0530

    engine: Host activate -check gluster availability
    If cluster supports gluster service, need to
    check if gluster is running, while activating host.
    If gluster command fails on server, the host state
    should not be changed to UP on activate.
    
    Change-Id: If673e32ed6480187ddcf6f9ecf8f0007e203b98d
    Bug-Url: https://bugzilla.redhat.com/975382
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/GlusterMonitoringStrategy.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/GlusterMonitoringStrategy.java
@@ -1,6 +1,12 @@
 package org.ovirt.engine.core.vdsbroker;
 
+import org.ovirt.engine.core.common.businessentities.NonOperationalReason;
 import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.VDSStatus;
+import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
+import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
+import org.ovirt.engine.core.common.vdscommands.VdsIdVDSCommandParametersBase;
+import org.ovirt.engine.core.compat.Guid;
 
 /**
  * This class defines gluster strategy entry points, which are needed in host monitoring phase
@@ -19,6 +25,18 @@ public class GlusterMonitoringStrategy implements MonitoringStrategy {
 
     @Override
     public void processSoftwareCapabilities(VDS vds) {
+        // check if gluster is running
+        VDSReturnValue returnValue = ResourceManager.getInstance().runVdsCommand(VDSCommandType.GlusterServersList,
+                new VdsIdVDSCommandParametersBase(vds.getId()));
+        if (!returnValue.getSucceeded()) {
+            vds.setStatus(VDSStatus.NonOperational);
+            vds.setNonOperationalReason(NonOperationalReason.GLUSTER_COMMAND_FAILED);
+            vdsNonOperational(vds, NonOperationalReason.GLUSTER_COMMAND_FAILED);
+        }
+    }
+
+    private void vdsNonOperational(VDS vds, NonOperationalReason reason) {
+        ResourceManager.getInstance().getEventListener().vdsNonOperational(vds.getId(), reason, true, true, Guid.Empty, null);
     }
 
     @Override
