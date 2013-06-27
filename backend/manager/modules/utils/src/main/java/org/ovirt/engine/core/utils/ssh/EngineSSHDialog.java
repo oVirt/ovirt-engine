8945e723ecc07813b7662cd206071dc0cc60c245
Yaniv Bronhaim <ybronhei@redhat.com>
2013-06-27 15:16:32 +0300

    Adding useDefaultKeyPair implementation to EngineSSHDialog
    Instead of using internal client object, set the keyPair for sshdialog
    internally.
    It fixes NPE when calling to useDefaultKeyPair without initialize the
    client object.
    
    Change-Id: I02edaee6bc6e804bb36d3ab4dabaa21faa926fa0
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ssh/EngineSSHDialog.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ssh/EngineSSHDialog.java
@@ -1,10 +1,13 @@
 package org.ovirt.engine.core.utils.ssh;
 
 import java.io.IOException;
-import java.security.KeyStoreException;
+import java.net.ConnectException;
+import java.security.KeyPair;
+import java.security.KeyStore;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.ovirt.engine.core.utils.crypt.EngineEncryptionUtils;
 
 /**
  * SSH dialog to be used with engine defaults
@@ -22,13 +25,23 @@ public class EngineSSHDialog extends SSHDialog {
      * @return fingerprint.
      */
     public String getHostFingerprint() throws IOException {
+        if (_client == null) {
+            throw new ConnectException("SSH is not connected");
+        }
         return ((EngineSSHClient)_client).getHostFingerprint();
     }
 
     /**
      * Use default engine ssh key.
      */
-    public void useDefaultKeyPair() throws KeyStoreException {
-        ((EngineSSHClient)_client).useDefaultKeyPair();
+    public void useDefaultKeyPair() {
+        KeyStore.PrivateKeyEntry entry = EngineEncryptionUtils.getPrivateKeyEntry();
+
+        setKeyPair(
+            new KeyPair(
+                entry.getCertificate().getPublicKey(),
+                entry.getPrivateKey()
+            )
+        );
     }
 }
