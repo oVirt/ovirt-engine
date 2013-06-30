84979df0cfed405613a0286583a8bc59d3e56a53
Allon Mureinik <amureini@redhat.com>
2013-06-30 15:29:29 +0300

    build: checkstyle to disallow final members
    Since GWT RPC does not handle final members properly, there is a need to
    implement a mechanism to enforce that no member in this package will be
    made final by mistake (unless it's a static final constant), as was
    done, e.g., in patch I53d6e5bbd66c4d67ec37326b545b65f086600169.
    
    This patch offers such a mechanism by providing a new checkstyle check
    which is only configured to run on the common and compat modules.
    
    Change-Id: I809e7f670cf86b47dbd7ae1fc6f73eff9837a129
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

---
+++ build-tools-root/ovirt-checkstyle-extension/src/main/java/checks/NoFinalMemberCheck.java
@@ -0,0 +1,46 @@
+package checks;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * Since GWT can't handle member variables which have the private modifier, this check was added to make sure it is not
+ * used in the packages that need to undergo GWT compilation.
+ */
+public class NoFinalMemberCheck extends Check {
+
+    private boolean run = true;
+
+    /** This check is not configurable */
+    @Override
+    public int[] getDefaultTokens() {
+        return new int[] { TokenTypes.VARIABLE_DEF};
+    }
+
+    @Override
+    public void visitToken(DetailAST aAST) {
+        if (run) {
+            if (aAST.getType() == TokenTypes.VARIABLE_DEF // A variable
+                    && aAST.getParent().getType() == TokenTypes.OBJBLOCK // which is a class variable
+                    && aAST.getParent().getParent().getType() != TokenTypes.ENUM_DEF // and not in an enum
+                    ) {
+
+                // find the modifiers
+                DetailAST child =  aAST.getFirstChild();
+                while (child != null) {
+                    // final is only allowed for statics (i.e., constants)
+                    if (child.branchContains(TokenTypes.FINAL) &&
+                            !child.branchContains(TokenTypes.LITERAL_STATIC)) {
+                        log(child.getLineNo(), child.getColumnNo(), "non-static final member variables are not allowed");
+                    }
+                    child = child.getNextSibling();
+                }
+            }
+        }
+    }
+
+    public void setRun(boolean run) {
+        this.run = run;
+    }
+}
