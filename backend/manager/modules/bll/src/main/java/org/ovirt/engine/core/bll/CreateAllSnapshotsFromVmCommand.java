oVirt/ovirt-engine

commit 268152abf19fbd64fd867bc5e5e40a57996aedc7

8. core: Change CreateAllSnapshotsFromVmCommand to persist all placeholders
This patch changes CreateAllSnapshotsFromVmCommand and its child
CreateSnapshotCommand to persist all async task placeholders
before executing the command on vdsm.

http://www.ovirt.org/Features/AsyncTaskManagerChanges_3.3
Change-Id: Ia0dda1086792f40a487488ac8a02b6b89dcfa9da
Signed-off-by: Ravi Nori <rnori@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateAllSnapshotsFromVmCommand.java
@@ -108,6 +108,16 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
     }
 
     @Override
+    protected void buildChildCommandInfos() {
+        Guid vmSnapshotId = Guid.newGuid();
+        for (DiskImage image : getDisksList()) {
+            addChildCommandInfo(image.getImageId(),
+                    VdcActionType.CreateSnapshot,
+                    buildCreateSnapshotParameters(image, vmSnapshotId));
+        }
+    }
+
+    @Override
     protected void executeVmCommand() {
         Guid newActiveSnapshotId = Guid.newGuid();
         Guid createdSnapshotId = getSnapshotDao().getId(getVmId(), SnapshotType.ACTIVE);
@@ -178,10 +188,7 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
     private void createSnapshotsForDisks(Guid vmSnapshotId) {
         for (DiskImage image : getDisksList()) {
 
-            VdcReturnValueBase vdcReturnValue = Backend.getInstance().runInternalAction(
-                            VdcActionType.CreateSnapshot,
-                            buildCreateSnapshotParameters(image, vmSnapshotId),
-                            ExecutionHandler.createDefaultContexForTasks(getExecutionContext()));
+            VdcReturnValueBase vdcReturnValue = executeChildCommand(image.getImageId());
 
             if (vdcReturnValue.getSucceeded()) {
                 getTaskIdList().addAll(vdcReturnValue.getInternalVdsmTaskIdList());

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddImageFromScratchCommand.java
@@ -30,6 +30,11 @@ public class AddImageFromScratchCommand<T extends AddImageFromScratchParameters>
     }
 
     @Override
+    protected void insertAsyncTaskPlaceHolders() {
+        persistAsyncTaskPlaceHolder(getParameters().getParentCommand());
+    }
+
+    @Override
     protected void executeCommand() {
         setImageGroupId(getParameters().getDiskInfo().getId());
         setDestinationImageId(Guid.newGuid());
@@ -71,7 +76,7 @@ public class AddImageFromScratchCommand<T extends AddImageFromScratchParameters>
     }
 
     protected boolean processImageInIrs() {
-        Guid taskId = persistAsyncTaskPlaceHolder(getParameters().getParentCommand());
+        Guid taskId = getAsyncTaskId();
 
         VDSReturnValue vdsReturnValue = runVdsCommand(
                         VDSCommandType.CreateImage,

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateSnapshotCommand.java
@@ -47,6 +47,11 @@ public class CreateSnapshotCommand<T extends ImagesActionsParametersBase> extend
     }
 
     @Override
+    protected void insertAsyncTaskPlaceHolders() {
+        persistAsyncTaskPlaceHolder(getParameters().getParentCommand());
+    }
+
+    @Override
     protected void executeCommand() {
         if (canCreateSnapshot()) {
             VDSReturnValue vdsReturnValue = performImageVdsmOperation();
@@ -88,7 +93,7 @@ public class CreateSnapshotCommand<T extends ImagesActionsParametersBase> extend
         VDSReturnValue vdsReturnValue = null;
 
         try {
-            Guid taskId = persistAsyncTaskPlaceHolder(getParameters().getParentCommand());
+            Guid taskId = getAsyncTaskId();
 
             vdsReturnValue =
                     Backend

commit 0bd59b7ecce496df3bc12aa33b8a0a46031b5f7b

core: Fix NPE when committing a snapshot.
A regression caused by commit 268152abf19fbd64fd867bc5e5e40a57996aedc7
made the commit snapshot to throw NPE when commiting a snapshot, since
the new snapshot id has been created twice, once for the snapshots table
and once for the vm_snapshot_id of the image.

The fix set the parameter only once.

Change-Id: If423dd90739cd14a66a7a62399f56465a2e30021
Bug-Url: https://bugzilla.redhat.com/981367
Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateAllSnapshotsFromVmCommand.java
@@ -70,6 +70,7 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
     /** flag that indicates whether memory snapshot is about to be created */
     private boolean prepareForMemorySnapshot;
     private String cachedSnapshotIsBeingTakenMessage;
+    private Guid newActiveSnapshotId = Guid.newGuid();
 
     protected CreateAllSnapshotsFromVmCommand(Guid commandId) {
         super(commandId);
@@ -110,17 +111,15 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
 
     @Override
     protected void buildChildCommandInfos() {
-        Guid vmSnapshotId = Guid.newGuid();
         for (DiskImage image : getDisksList()) {
             addChildCommandInfo(image.getImageId(),
                     VdcActionType.CreateSnapshot,
-                    buildCreateSnapshotParameters(image, vmSnapshotId));
+                    buildCreateSnapshotParameters(image));
         }
     }
 
     @Override
     protected void executeVmCommand() {
-        Guid newActiveSnapshotId = Guid.newGuid();
         Guid createdSnapshotId = getSnapshotDao().getId(getVmId(), SnapshotType.ACTIVE);
         getParameters().setSnapshotType(determineSnapshotType());
         getParameters().setInitialVmStatus(getVm().getStatus());
@@ -131,7 +130,7 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
 
         MemoryImageBuilder memoryImageBuilder = createMemoryImageBuilder();
         addSnapshotToDB(createdSnapshotId, memoryImageBuilder);
-        createSnapshotsForDisks(newActiveSnapshotId);
+        createSnapshotsForDisks();
         memoryImageBuilder.build();
 
         if (getTaskIdList().isEmpty()) {
@@ -186,9 +185,8 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
                 getCompensationContext());
     }
 
-    private void createSnapshotsForDisks(Guid vmSnapshotId) {
+    private void createSnapshotsForDisks() {
         for (DiskImage image : getDisksList()) {
-
             VdcReturnValueBase vdcReturnValue = executeChildCommand(image.getImageId());
 
             if (vdcReturnValue.getSucceeded()) {
@@ -200,7 +198,7 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
         }
     }
 
-    private ImagesActionsParametersBase buildCreateSnapshotParameters(DiskImage image, Guid snapshotId) {
+    private ImagesActionsParametersBase buildCreateSnapshotParameters(DiskImage image) {
         VdcActionType parentCommand = getParameters().getParentCommand() != VdcActionType.Unknown ?
                 getParameters().getParentCommand() : VdcActionType.CreateAllSnapshotsFromVm;
 
@@ -208,7 +206,7 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
         result.setDescription(getParameters().getDescription());
         result.setSessionId(getParameters().getSessionId());
         result.setQuotaId(image.getQuotaId());
-        result.setVmSnapshotId(snapshotId);
+        result.setVmSnapshotId(newActiveSnapshotId);
         result.setEntityInfo(getParameters().getEntityInfo());
         result.setParentCommand(parentCommand);
         result.setParentParameters(getParametersForTask(parentCommand, getParameters()));