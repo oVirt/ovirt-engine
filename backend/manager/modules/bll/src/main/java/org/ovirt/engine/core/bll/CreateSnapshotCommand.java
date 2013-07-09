528e60a274243cc0e70714c2c0da48d19afa9e15
Yair Zaslavsky <yzaslavs@redhat.com>
2013-07-10 02:58:18 +0300

    core: Creating SPM task in failed state ...
    Creating SPM task in failed state in case of network error in
    create snapshot command
    
    Bug-Url: https://bugzilla.redhat.com/982003
    Change-Id: I037f65b13eabffec39dbdc620a75c515e6ce796a
    Signed-off-by: Yair Zaslavsky <yzaslavs@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateSnapshotCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateSnapshotCommand.java
@@ -132,6 +132,9 @@ public class CreateSnapshotCommand<T extends ImagesActionsParametersBase> extend
             log.errorFormat(
                     "CreateSnapshotCommand::CreateSnapshotInIrsServer::Failed creating snapshot from image id -'{0}'",
                     getImage().getImageId());
+
+            AsyncTaskManager.logAndFailTaskOfCommandWithEmptyVdsmId(getAsyncTaskId(),
+                    "Create snapshot failed at VDSM. DB task ID is " + getAsyncTaskId());
             throw new VdcBLLException(VdcBllErrors.VolumeCreationError);
         }
 
