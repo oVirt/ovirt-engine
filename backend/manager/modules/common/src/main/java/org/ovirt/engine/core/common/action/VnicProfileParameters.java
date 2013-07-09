5fbaccc0030916fd21ecce950d044dcd674e3764
Moti Asayag <masayag@redhat.com>
2013-07-10 00:26:25 +0300

    core: Add parameter class for vnic profile commands
    The class will be using for vnic profile actions, e.g.:
    adding/updating/removing a vnic profile.
    
    Change-Id: Ia98a96e4413f1b9cbc7012c2e2fc6da70a4027e4
    Signed-off-by: Moti Asayag <masayag@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/VnicProfileParameters.java
@@ -0,0 +1,37 @@
+package org.ovirt.engine.core.common.action;
+
+import javax.validation.Valid;
+
+import org.ovirt.engine.core.common.businessentities.network.VnicProfile;
+
+public class VnicProfileParameters extends VdcActionParametersBase {
+
+    private static final long serialVersionUID = 7238781812367042839L;
+
+    @Valid
+    private VnicProfile vnicProfile;
+    private boolean publicUse;
+
+    public VnicProfileParameters() {
+    }
+
+    public VnicProfileParameters(VnicProfile vnicProfile) {
+        this.vnicProfile = vnicProfile;
+    }
+
+    public VnicProfile getVnicProfile() {
+        return vnicProfile;
+    }
+
+    public void setVnicProfile(VnicProfile vnicProfile) {
+        this.vnicProfile = vnicProfile;
+    }
+
+    public boolean isPublicUse() {
+        return publicUse;
+    }
+
+    public void setPublicUse(boolean publicUse) {
+        this.publicUse = publicUse;
+    }
+}
