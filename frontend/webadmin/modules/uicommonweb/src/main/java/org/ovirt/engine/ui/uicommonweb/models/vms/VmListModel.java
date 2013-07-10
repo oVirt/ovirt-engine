oVirt/ovirt-engine

commit 416cd92a94bbc95c8e5bca17f3707d2016208668

webadmin: Pass created_by_user_id field on VM Edit
Explicitly set created_by_user_id field to the original (unchanged)
value for VM upon edit to avoid backend error due to change to
non-editable field.

Change-Id: I129c7e2c48b1fda5cdd1719e5f6a1b95ec46843d
Signed-off-by: Martin Betak <mbetak@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/VmListModel.java
@@ -1979,6 +1979,8 @@ public class VmListModel extends VmBaseListModel<VM> implements ISupportSystemTr
         getcurrentVm().setCpuPinning((String) model.getCpuPinning()
                 .getEntity());
         getcurrentVm().setVncKeyboardLayout((String) model.getVncKeyboardLayout().getSelectedItem());
+        // explicitly pass non-editable field from the original VM
+        getcurrentVm().setCreatedByUserId(selectedItem.getCreatedByUserId());
 
         if ((Boolean) model.getIsAutoAssign().getEntity()) {
             getcurrentVm().setDedicatedVmForVds(null);

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/Cloner.java
@@ -195,6 +195,7 @@ public final class Cloner
         vm.setVmPoolName(instance.getVmPoolName());
         vm.setVmtGuid(instance.getVmtGuid());
         vm.setVmtName(instance.getVmtName());
+        vm.setCreatedByUserId(instance.getCreatedByUserId());
 
         return vm;
     }

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/templates/TemplateListModel.java
@@ -464,6 +464,7 @@ public class TemplateListModel extends VmBaseListModel<VmTemplate> implements IS
         template.setKernelParams((String) model.getKernel_parameters().getEntity());
         template.setInitrdUrl((String) model.getInitrd_path().getEntity());
         template.setVncKeyboardLayout((String) model.getVncKeyboardLayout().getSelectedItem());
+        template.setCreatedByUserId(selectedItem.getCreatedByUserId());
 
         if (model.getQuota().getIsAvailable() && model.getQuota().getSelectedItem() != null) {
             template.setQuotaId(((Quota) model.getQuota().getSelectedItem()).getId());

commit 895368955921eaa3cd2a33012c65b8b54d2cf7f9

webadmin: Fix NPE in New VM dialog
Regression caused by
416cd92a94bbc95c8e5bca17f3707d2016208668
which expected that the selected item is
always set in VmListModel.onSave(). The problem was
that it is true only for edit VM - not for new.

Change-Id: I1e653f428f8a058532030483e7bb115961d706c3
Signed-off-by: Martin Betak <mbetak@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/VmListModel.java
@@ -1979,8 +1979,6 @@ public class VmListModel extends VmBaseListModel<VM> implements ISupportSystemTr
         getcurrentVm().setCpuPinning((String) model.getCpuPinning()
                 .getEntity());
         getcurrentVm().setVncKeyboardLayout((String) model.getVncKeyboardLayout().getSelectedItem());
-        // explicitly pass non-editable field from the original VM
-        getcurrentVm().setCreatedByUserId(selectedItem.getCreatedByUserId());
 
         if ((Boolean) model.getIsAutoAssign().getEntity()) {
             getcurrentVm().setDedicatedVmForVds(null);
@@ -2094,6 +2092,9 @@ public class VmListModel extends VmBaseListModel<VM> implements ISupportSystemTr
             {
                 return;
             }
+
+            // explicitly pass non-editable field from the original VM
+            getcurrentVm().setCreatedByUserId(selectedItem.getCreatedByUserId());
 
             // runEditVM: should be true if Cluster hasn't changed or if
             // Cluster has changed and Editing it in the Backend has succeeded: