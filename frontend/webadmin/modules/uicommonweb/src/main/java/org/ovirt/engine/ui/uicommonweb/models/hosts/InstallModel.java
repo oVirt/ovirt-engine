1abb399612cab633beea90624d6d5a2d18ae9a17
Yaniv Bronhaim <ybronhei@redhat.com>
2013-07-11 13:52:33 +0300

    webadmin: host-deploy: Modifying Reinstall Host popup view
    1. Adding password or publicKey fields for authentication.
    2. Adding ssh fingerprint field.
    
    Change-Id: Ic33775720914908399492038f8ebf0d2a9b4524c
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/InstallModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/InstallModel.java
@@ -1,22 +1,30 @@
 package org.ovirt.engine.ui.uicommonweb.models.hosts;
 
+import org.ovirt.engine.core.common.action.VdsOperationActionParameters.AuthenticationMethod;
+import org.ovirt.engine.ui.frontend.AsyncQuery;
+import org.ovirt.engine.ui.frontend.INewAsyncCallback;
+import org.ovirt.engine.ui.uicommonweb.dataprovider.AsyncDataProvider;
 import org.ovirt.engine.ui.uicommonweb.models.EntityModel;
 import org.ovirt.engine.ui.uicommonweb.models.ListModel;
 import org.ovirt.engine.ui.uicommonweb.models.Model;
 import org.ovirt.engine.ui.uicommonweb.validation.IValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.NotEmptyValidation;
+import org.ovirt.engine.ui.uicompat.ConstantsManager;
+import org.ovirt.engine.ui.uicompat.UIConstants;
 
 @SuppressWarnings("unused")
 public class InstallModel extends Model {
 
-    private EntityModel privateRootPassword;
+    private static final UIConstants constants = ConstantsManager.getInstance().getConstants();
 
-    public EntityModel getRootPassword() {
-        return privateRootPassword;
+    private EntityModel privateUserPassword;
+
+    public EntityModel getUserPassword() {
+        return privateUserPassword;
     }
 
-    private void setRootPassword(EntityModel value) {
-        privateRootPassword = value;
+    private void setUserPassword(EntityModel value) {
+        privateUserPassword = value;
     }
 
     private ListModel privateOVirtISO;
@@ -49,25 +57,83 @@ public class InstallModel extends Model {
         hostVersion = value;
     }
 
+    private EntityModel privateUserName;
+
+    public EntityModel getUserName()
+    {
+        return privateUserName;
+    }
+
+    private void setUserName(EntityModel value)
+    {
+        privateUserName = value;
+    }
+
+    private EntityModel privatePublicKey;
+
+    public EntityModel getPublicKey()
+    {
+        return privatePublicKey;
+    }
+
+    private void setPublicKey(EntityModel value)
+    {
+        privatePublicKey = value;
+    }
+
+    private AuthenticationMethod hostAuthenticationMethod;
+
+    public void setAuthenticationMethod(AuthenticationMethod value) {
+        hostAuthenticationMethod = value;
+    }
+
+    public AuthenticationMethod getAuthenticationMethod() {
+        return hostAuthenticationMethod;
+    }
+
     public InstallModel() {
-        setRootPassword(new EntityModel());
+        setUserPassword(new EntityModel());
         setOVirtISO(new ListModel());
         setHostVersion(new EntityModel());
 
         setOverrideIpTables(new EntityModel());
         getOverrideIpTables().setEntity(false);
+        setUserName(new EntityModel());
+        getUserName().setEntity(constants.defaultUserName());
+        setPublicKey(new EntityModel());
+        getPublicKey().setEntity(constants.empty());
+        fetchPublicKey();
     }
 
     public boolean validate(boolean isOVirt) {
         getOVirtISO().setIsValid(true);
-        getRootPassword().setIsValid(true);
+        getUserPassword().setIsValid(true);
 
         if (isOVirt) {
             getOVirtISO().validateSelectedItem(new IValidation[] { new NotEmptyValidation() });
         } else {
-            getRootPassword().validateEntity(new IValidation[] { new NotEmptyValidation() });
+            if (getAuthenticationMethod() == AuthenticationMethod.Password) {
+                getUserPassword().validateEntity(new IValidation[] { new NotEmptyValidation() });
+            }
         }
 
-        return getRootPassword().getIsValid() && getOVirtISO().getIsValid();
+        return getUserPassword().getIsValid() && getOVirtISO().getIsValid();
+    }
+
+    public void fetchPublicKey() {
+        AsyncQuery aQuery = new AsyncQuery();
+        aQuery.setModel(this);
+        aQuery.asyncCallback = new INewAsyncCallback() {
+            @Override
+            public void onSuccess(Object model, Object result)
+            {
+                String pk = (String) result;
+                if (pk != null && pk.length() > 0)
+                {
+                    getPublicKey().setEntity(result);
+                }
+            }
+        };
+        AsyncDataProvider.getHostPublicKey(aQuery);
     }
 }
