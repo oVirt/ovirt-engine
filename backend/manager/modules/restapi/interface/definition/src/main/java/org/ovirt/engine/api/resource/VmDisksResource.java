e1a85d1451732eefc1ea0a0695f150f0dda16940
Ravi Nori <rnori@redhat.com>
2013-07-15 13:17:04 -0400

    restapi : API response sent not in XML format
    When sending a request to unplug a disk from vm via REST,
    the response received from API was not in XML format.
    
    Change-Id: I9e1a9e8c5283efcd4dba31b0f1fa0414adc266fb
    Bug-Url: https://bugzilla.redhat.com/981989
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- backend/manager/modules/restapi/interface/definition/src/main/java/org/ovirt/engine/api/resource/VmDisksResource.java
+++ backend/manager/modules/restapi/interface/definition/src/main/java/org/ovirt/engine/api/resource/VmDisksResource.java
@@ -4,12 +4,14 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import org.ovirt.engine.api.model.Action;
 import org.ovirt.engine.api.model.Disk;
 import org.ovirt.engine.api.model.Disks;
 
+@Produces({ApiMediaType.APPLICATION_XML, ApiMediaType.APPLICATION_JSON, ApiMediaType.APPLICATION_X_YAML})
 public interface VmDisksResource extends DevicesResource<Disk, Disks>{
 
     @Path("{iden}")
