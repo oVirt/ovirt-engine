oVirt/ovirt-engine

commit 4864cbe79e63475216baf165d3ff8681769822c0

core : AddVmFromSnapshot - use device information from snapshot.
When adding a vm from snapshot (clone of a snapshot), the device
information was taken from the vm current state and not from the
snapshot configuration - which caused the cloned vm to not be in the
desired state and possibly have missing devices.

Change-Id: I0611bbbef00fd2c2390ead5f3110e56dcaacd579
BUg-Url: https://bugzilla.redhat.com/show_bug.cgi?id=882644
Signed-off-by: Liron Aravot <laravot@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmFromSnapshotCommand.java
@@ -19,6 +19,7 @@ import org.ovirt.engine.core.common.businessentities.Snapshot;
 import org.ovirt.engine.core.common.businessentities.Snapshot.SnapshotStatus;
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VMStatus;
+import org.ovirt.engine.core.common.businessentities.VmDevice;
 import org.ovirt.engine.core.common.businessentities.network.VmNetworkInterface;
 import org.ovirt.engine.core.common.locks.LockingGroup;
 import org.ovirt.engine.core.common.queries.GetVmConfigurationBySnapshotQueryParams;
@@ -224,10 +225,9 @@ public class AddVmFromSnapshotCommand<T extends AddVmFromSnapshotParameters> ext
 
     @Override
     protected void copyVmDevices() {
-        VmDeviceUtils.copyVmDevices(getVmIdFromSnapshot(),
-                getVmId(),
-                newDiskImages,
-                _vmInterfaces);
+        List<VmDevice> devices = new ArrayList<VmDevice>(vmFromConfiguration.getVmUnamagedDeviceList());
+        devices.addAll(vmFromConfiguration.getManagedVmDeviceMap().values());
+        VmDeviceUtils.copyVmDevices(getVmIdFromSnapshot(), getVmId(), vmFromConfiguration, vmFromConfiguration.getStaticData(), true, devices, newDiskImages, _vmInterfaces);
     }
 
     @Override

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/utils/VmDeviceUtils.java
@@ -158,33 +158,20 @@ public class VmDeviceUtils {
         }
     }
 
-
     /**
-     * Copies relevant entries on "Vm from Template" or "Template from VM" creation.
-     *
-     * @param srcId
-     * @param dstId
-     * @param disks
-     *            The disks which were saved for the destination VM.
+     * Copy related data from the given VM/VmBase/VmDevice list to the destination VM/VmTemplate.
      */
-    public static void copyVmDevices(Guid srcId, Guid dstId, List<DiskImage> disks, List<VmNetworkInterface> ifaces) {
+    public static void copyVmDevices(Guid srcId, Guid dstId, VM vm, VmBase vmBase, boolean isVm, List<VmDevice> devicesDataToUse, List<DiskImage> disks, List<VmNetworkInterface> ifaces) {
         Guid id;
-        VM vm = DbFacade.getInstance().getVmDao().get(dstId);
-        VmBase vmBase = (vm != null) ? vm.getStaticData() : null;
         int diskCount = 0;
         int ifaceCount = 0;
-        boolean isVm = (vmBase != null);
-        if (!isVm) {
-            vmBase = DbFacade.getInstance().getVmTemplateDao().get(dstId);
-        }
-        List<VmDevice> devices = dao.getVmDeviceByVmId(srcId);
         String isoPath=vmBase.getIsoPath();
         // indicates that VM should have CD either from its own (iso_path) or from the snapshot it was cloned from.
         boolean shouldHaveCD = StringUtils.isNotEmpty(isoPath);
         // indicates if VM has already a non empty CD in DB
         boolean hasAlreadyCD = (!(DbFacade.getInstance().getVmDeviceDao().getVmDeviceByVmIdTypeAndDevice(vmBase.getId(), VmDeviceType.DISK.getName(), VmDeviceType.CDROM.getName())).isEmpty());
         boolean addCD = (!hasAlreadyCD && shouldHaveCD);
-        for (VmDevice device : devices) {
+        for (VmDevice device : devicesDataToUse) {
             id = Guid.NewGuid();
             Map<String, Object> specParams = new HashMap<String, Object>();
             if (srcId.equals(Guid.Empty)) {
@@ -268,6 +255,24 @@ public class VmDeviceUtils {
             }
 
         }
+    }
+    /**
+     * Copies relevant entries on "Vm from Template" or "Template from VM" creation.
+     *
+     * @param srcId
+     * @param dstId
+     * @param disks
+     *            The disks which were saved for the destination VM.
+     */
+    public static void copyVmDevices(Guid srcId, Guid dstId, List<DiskImage> disks, List<VmNetworkInterface> ifaces) {
+        VM vm = DbFacade.getInstance().getVmDao().get(dstId);
+        VmBase vmBase = (vm != null) ? vm.getStaticData() : null;
+        boolean isVm = (vmBase != null);
+        if (!isVm) {
+            vmBase = DbFacade.getInstance().getVmTemplateDao().get(dstId);
+        }
+        List<VmDevice> devices = dao.getVmDeviceByVmId(srcId);
+        copyVmDevices(srcId, dstId, vm, vmBase, isVm, devices, disks, ifaces);
     }
 
     private static void addVideoDevice(VM vm) {

commit 58720c1faf1d95f4e869b662fcbe2b3bd9f02889

engine: clone VM from snapshot corrupts original VM
The problem was in AddVmFromSnapshotCommand which calls
VmDeviceUtils.copyVmDevices which accepts the following params:
srcId, destId, destVm, destVmBase
but it was called with:
srcId, destId, srcVm, srcVmBase

In most cases the srcId and destId is used but in some cases
also the entities themselfs which caused to copy the devices
from source VM to itself which corrupted it.

This regression has been introduced by:
4864cbe79e63475216baf165d3ff8681769822c0

Change-Id: I60dddf57f7f2cf9ec8a3a429d97e84c3bbcc12b2
Bug-Url: https://bugzilla.redhat.com/982636
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmFromSnapshotCommand.java
@@ -241,8 +241,8 @@ public class AddVmFromSnapshotCommand<T extends AddVmFromSnapshotParameters> ext
         devices.addAll(vmFromConfiguration.getManagedVmDeviceMap().values());
         VmDeviceUtils.copyVmDevices(getVmIdFromSnapshot(),
                 getVmId(),
-                vmFromConfiguration,
-                vmFromConfiguration.getStaticData(),
+                getVm(),
+                getVm().getStaticData(),
                 true,
                 devices,
                 newDiskImages,