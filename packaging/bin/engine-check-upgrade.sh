53afc6a6659188ce08f1fcab3d88b09b881c9bd3
Alon Bar-Lev <alonbl@redhat.com>
2013-07-22 15:21:11 +0300

    packaging: setup: remove legacy setup
    Change-Id: I8ae205aa17eea390b82fe32be9f517cfb55dbe0f
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

---
+++ packaging/bin/engine-check-upgrade.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cat >&2 <<__EOF__
+engine-check-upgrade is obsoleted by engine-upgrade-check.
+please run engine-upgrade-check for upgrading product or update settings.
+__EOF__
+
+exit 1
---
+++ packaging/bin/engine-upgrade.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cat >&2 <<__EOF__
+engine-upgrade is obsoleted by engine-setup.
+please run engine-setup for upgrading product or update settings.
+__EOF__
+
+exit 1
