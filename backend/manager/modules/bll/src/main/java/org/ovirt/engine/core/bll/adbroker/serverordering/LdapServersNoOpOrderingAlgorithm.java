d9c27772f1e5e0aeaecc59878b0a41fd5bf247f5
Ravi Nori <rnori@redhat.com>
2013-08-07 13:31:28 -0400

    core : Users cannot log into UserPortal
    Issue with user login using Active Directory.
    After first failed login (bad/expire passwd) no other
    user from ad can login to the system.
    
    Change-Id: I2d7b169263581ea7c69bac4c503d804e14e944db
    Bug-Url: https://bugzilla.redhat.com/994604
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/adbroker/serverordering/LdapServersNoOpOrderingAlgorithm.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/adbroker/serverordering/LdapServersNoOpOrderingAlgorithm.java
@@ -5,6 +5,17 @@ import java.util.List;
 
 public class LdapServersNoOpOrderingAlgorithm extends LdapServersOrderingAlgorithm {
 
+    /**
+     * Override this since parent removes the server from the list and the
+     * reorderImpl implementation of this class does not add the server
+     * back to the list. So we end up with empty list of ldap servers.
+     * @param server
+     * @param servers
+     */
+    @Override
+    public void reorder(URI server, List<URI> servers) {
+    }
+
     @Override
     protected void reorderImpl(URI server, List<URI> restOfServers) {
     }
