549a62575caa6665cdedc1a240afabbde24863d5
Allon Mureinik <amureini@redhat.com>
2013-08-13 08:16:47 +0300

    core: Disable member init for GWT-compiled code
    Following the upgrade of GWT(P), if an entity's constructor is not
    explicitly referenced in the UI code the inline member initializations
    are not taken into account in GWT's serialization policy [1].
    
    This patch is the culmination of a series that suggests taking the safe
    route - instead of having the code (silently!) break if a call to the
    constructor is removed from the UI code, it simply disallows having such
    inline initializations.
    
    The previous patches in this set remove such initializers for various
    parts of the common and compat modules (broken up into smaller units for
    relative ease of review).
    This patch implements a checkstyle check to fail the build in compile
    time if such a dangerous initializer is introduced.
    
    [1] http://lists.ovirt.org/pipermail/engine-devel/2013-July/005256.html
    
    Change-Id: Iffd520ecb627e87254c4d325c85088179b0ef744
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

---
+++ build-tools-root/ovirt-checkstyle-extension/src/main/java/checks/NoMemberInitializationCheck.java
@@ -0,0 +1,66 @@
+package checks;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.ScopeUtils;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * <p>
+ * Checks if any class or object member is explicitly initialized instead of being initialized in the class'
+ * constructor.
+ * </p>
+ * <p>
+ * Rationale: Such calls are not interpreted by GWT's compiler, and if these objects are shared to the frontend, these
+ * members may not be initialized.
+ * </p>
+ * <p>
+ * This code was adapted from {@link com.puppycrawl.tools.checkstyle.checks.coding.ExplicitInitializationCheck}, and
+ * should be removed once such a check is integrated into the checkstyle project.
+ * </p>
+ */
+public class NoMemberInitializationCheck extends Check {
+    private boolean run = true;
+
+    @Override
+    public final int[] getDefaultTokens() {
+        return new int[] { TokenTypes.VARIABLE_DEF };
+    }
+
+    @Override
+    public final int[] getRequiredTokens() {
+        return getDefaultTokens();
+    }
+
+    @Override
+    public void visitToken(DetailAST aAST) {
+        if (!run) {
+            return;
+        }
+
+        // do not check local variables and
+        // fields declared in interface/annotations
+        if (ScopeUtils.isLocalVariableDef(aAST) || ScopeUtils.inInterfaceOrAnnotationBlock(aAST)) {
+            return;
+        }
+
+        final DetailAST assign = aAST.findFirstToken(TokenTypes.ASSIGN);
+        if (assign == null) {
+            // no assign - no check
+            return;
+        }
+
+        final DetailAST modifiers = aAST.findFirstToken(TokenTypes.MODIFIERS);
+        if ((modifiers != null) && modifiers.branchContains(TokenTypes.FINAL)) {
+            // do not check final variables
+            return;
+        }
+
+        final DetailAST ident = aAST.findFirstToken(TokenTypes.IDENT);
+        log(ident, "Initialization of members is not allowed", ident.getText());
+    }
+
+    public void setRun(boolean run) {
+        this.run = run;
+    }
+}
