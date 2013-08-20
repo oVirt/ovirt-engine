b1bd73eb5e871b90c058e5b94a886825cf3c8c32
Laszlo Hornyak <lhornyak@redhat.com>
2013-08-20 17:45:18 +0200

    engien: test for ClusterPolicyCRUDCommandTest
    test for ClusterPolicyCRUDCommandTest
    
    Change-Id: I9b370e4cc0a43645961b7693310695110f7f8281
    Signed-off-by: Laszlo Hornyak <lhornyak@redhat.com>

---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/commands/ClusterPolicyCRUDCommandTest.java
@@ -0,0 +1,55 @@
+package org.ovirt.engine.core.bll.scheduling.commands;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.ovirt.engine.core.common.config.ConfigValues;
+import org.ovirt.engine.core.common.scheduling.ClusterPolicy;
+import org.ovirt.engine.core.common.scheduling.parameters.ClusterPolicyCRUDParameters;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.utils.MockConfigRule;
+
+public class ClusterPolicyCRUDCommandTest {
+
+    @Rule
+    public MockConfigRule mockConfigRule =
+            new MockConfigRule(MockConfigRule.mockConfig(ConfigValues.EnableVdsLoadBalancing, false));
+
+    @Test
+    public void testCheckAddEditValidations() {
+        Guid clusterPolicyId = new Guid("e754440b-76a6-4099-8235-4565ab4b5521");
+        ClusterPolicy clusterPolicy = new ClusterPolicy();
+        clusterPolicy.setId(clusterPolicyId);
+        ClusterPolicyCRUDCommand command =
+                new ClusterPolicyCRUDCommand(new ClusterPolicyCRUDParameters(clusterPolicyId,
+                        clusterPolicy)) {
+
+                    @Override
+                    protected void executeCommand() {
+                    }
+                };
+        Assert.assertTrue(command.checkAddEditValidations());
+    }
+
+    @Test
+    public void testCheckAddEditValidationsFailOnParameters() {
+        Guid clusterPolicyId = new Guid("e754440b-76a6-4099-8235-4565ab4b5521");
+        ClusterPolicy clusterPolicy = new ClusterPolicy();
+        clusterPolicy.setId(clusterPolicyId);
+        HashMap<String, String> parameterMap = new HashMap<String, String>();
+        parameterMap.put("fail?", "sure, fail!");
+        clusterPolicy.setParameterMap(parameterMap);
+        ClusterPolicyCRUDCommand command =
+                new ClusterPolicyCRUDCommand(new ClusterPolicyCRUDParameters(clusterPolicyId,
+                        clusterPolicy)) {
+
+                    @Override
+                    protected void executeCommand() {
+                    }
+                };
+        Assert.assertFalse(command.checkAddEditValidations());
+    }
+
+}
