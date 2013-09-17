55f1522b4effd8794796649dac272b20f502a497
Allon Mureinik <amureini@redhat.com>
2013-09-17 20:31:31 +0300

    core: differentiate command test utils
    CommandAssertUtils and CanDoActionTestUtils contained duplicated logic
    as both were used to test canDoAction() methods.
    
    This patch moves all the (remaining) canDoAction() tests to use
    CanDoActionTestUtils and removes the redundant methods from
    CommandAssertUtils, leaving it as a utility to test execute() methods
    only.
    
    Change-Id: Iadce121f4cf3d04e4eab8200c679230e4b4adf8b
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/CommandAssertUtils.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/CommandAssertUtils.java
@@ -1,11 +1,6 @@
 package org.ovirt.engine.core.bll;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
-
-import org.ovirt.engine.core.common.errors.VdcBllMessages;
 
 /** A utility class for common command assertions */
 public class CommandAssertUtils {
@@ -15,27 +10,5 @@ public class CommandAssertUtils {
 
     public static void checkSucceeded(CommandBase<?> cmd, boolean expected) {
         assertEquals(expected, cmd.getReturnValue().getSucceeded());
-    }
-
-    public static void checkMessages(CommandBase<?> cmd, VdcBllMessages... expected) {
-        List<String> returned = cmd.getReturnValue().getCanDoActionMessages();
-        assertEquals("Wrong number of messages", expected.length, returned.size());
-        for (int i = 0; i < expected.length; i++) {
-            assertEquals("Wrong message", expected[i].toString(), returned.get(i));
-        }
-    }
-
-    /**
-     * This method checks if the return CDA messages contain the expected messages, it is different from checkMessages
-     * by the fact that it does not check by the order the parameters were given, the order is irrelevant most of the
-     * time and it does not check that the size of the returned messages matches the size of the expected messages, this
-     * comes in handy for example in scenarios where the CDA messages return a resolved parameters (e.g. '$VmName MyVM')
-     */
-    public static void checkMessagesContains(CommandBase<?> cmd, VdcBllMessages... expected) {
-        List<String> cdaMessages = cmd.getReturnValue().getCanDoActionMessages();
-        for (int i = 0; i < expected.length; i++) {
-            assertTrue("CanDoAction message does not contain the message " + expected[i],
-                    cdaMessages.contains(expected[i].toString()));
-        }
     }
 }
--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/ForceSelectSPMCommandTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/ForceSelectSPMCommandTest.java
@@ -1,10 +1,7 @@
 package org.ovirt.engine.core.bll;
 
-import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
-import static org.ovirt.engine.core.bll.CommandAssertUtils.checkMessages;
-import static org.ovirt.engine.core.bll.CommandAssertUtils.checkMessagesContains;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +23,6 @@ import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.AsyncTaskDAO;
 import org.ovirt.engine.core.dao.StoragePoolDAO;
 import org.ovirt.engine.core.dao.VdsDAO;
-
 
 /** A test case for the {@link ForceSelectSPMCommand} command */
 
@@ -58,51 +54,56 @@ public class ForceSelectSPMCommandTest {
     @Test
     public void testCDANonExistingVds() {
         doReturn(null).when(vdsDAOMock).get(vdsId);
-        assertFalse("canDoAction did not fail for non existing VDS", command.canDoAction());
-        checkMessages(command, VdcBllMessages.VDS_NOT_EXIST);
+        CanDoActionTestUtils.runAndAssertCanDoActionFailure("canDoAction did not fail for non existing VDS",
+                command, VdcBllMessages.VDS_NOT_EXIST);
     }
 
     @Test
     public void testCDAVdsNotUp() {
         vds.setStatus(VDSStatus.Down);
-        assertFalse("canDoAction did not fail for a VDS with a status different from UP", command.canDoAction());
-        checkMessagesContains(command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_VDS_NOT_UP);
+        CanDoActionTestUtils.runAndAssertCanDoActionFailure
+                ("canDoAction did not fail for a VDS with a status different from UP",
+                        command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_VDS_NOT_UP);
     }
 
     @Test
     public void testCDAStoragePoolValid() {
         vds.setId(Guid.newGuid());
-        assertFalse("canDoAction did not fail on mismatch Storage Pool", command.canDoAction());
-        checkMessagesContains(command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_VDS_NOT_IN_POOL);
+        CanDoActionTestUtils.runAndAssertCanDoActionFailure("canDoAction did not fail on mismatch Storage Pool",
+                command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_VDS_NOT_IN_POOL);
     }
 
     @Test
     public void testCDAVdsIsSPM() {
         vds.setSpmStatus(VdsSpmStatus.SPM);
-        assertFalse("canDoAction did not fail on a VDS that is already set as SPM", command.canDoAction());
-        checkMessagesContains(command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_VDS_ALREADY_SPM);
+        CanDoActionTestUtils.runAndAssertCanDoActionFailure
+                ("canDoAction did not fail on a VDS that is already set as SPM",
+                        command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_VDS_ALREADY_SPM);
     }
 
     @Test
     public void testCDAVdsSPMPrioritySetToNever() {
         vds.setVdsSpmPriority(BusinessEntitiesDefinitions.HOST_MIN_SPM_PRIORITY);
-        assertFalse("canDoAction did not fail on a VDS that is set to never be elected as SPM", command.canDoAction());
-        checkMessagesContains(command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_VDS_MARKED_AS_NEVER_SPM);
+        CanDoActionTestUtils.runAndAssertCanDoActionFailure
+                ("canDoAction did not fail on a VDS that is set to never be elected as SPM",
+                        command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_VDS_MARKED_AS_NEVER_SPM);
     }
 
     @Test
     public void testCDAStoragePoolNotUp() {
         storagePool.setStatus(StoragePoolStatus.Uninitialized);
-        assertFalse("canDoAction did not fail on a Storage Pool which is not up", command.canDoAction());
-        checkMessagesContains(command, VdcBllMessages.ACTION_TYPE_FAILED_IMAGE_REPOSITORY_NOT_FOUND);
+        CanDoActionTestUtils.runAndAssertCanDoActionFailure
+                ("canDoAction did not fail on a Storage Pool which is not up", command,
+                        VdcBllMessages.ACTION_TYPE_FAILED_IMAGE_REPOSITORY_NOT_FOUND);
     }
 
     @Test
     public void testCDAStoragePoolHasTasks() {
         List<Guid> tasks = Arrays.asList(Guid.newGuid());
         doReturn(tasks).when(asyncTaskDAOMock).getAsyncTaskIdsByStoragePoolId(storagePoolId);
-        assertFalse("canDoAction did not fail on a Storage Pool with running tasks", command.canDoAction());
-        checkMessagesContains(command, VdcBllMessages.CANNOT_FORCE_SELECT_SPM_STORAGE_POOL_HAS_RUNNING_TASKS);
+        CanDoActionTestUtils.runAndAssertCanDoActionFailure
+                ("canDoAction did not fail on a Storage Pool with running tasks", command,
+                        VdcBllMessages.CANNOT_FORCE_SELECT_SPM_STORAGE_POOL_HAS_RUNNING_TASKS);
     }
 
     private void createVDSandStoragePool() {
