9b59a7c58c5623f6a87b7358b04c01bfd38e2631
Moti Asayag <masayag@redhat.com>
2013-09-18 15:22:36 +0200

    restapi: Prevent NPE when getting statistics
    VDSM doesn't report statistics for all of the network
    interfaces (i.e. vlans, see [1] for more details)
    therefore the restapi shouldn't expect a wrapper not
    to be null and prevent autoboxing to avoid NPE.
    
    [1] https://bugzilla.redhat.com/675560
    
    Change-Id: Iab3422ef11f35b7a75d39f62cc5caa6f827ac7f4
    Bug-Url: https://bugzilla.redhat.com/993123
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/util/RxTxCalculator.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/util/RxTxCalculator.java
@@ -4,7 +4,7 @@ public class RxTxCalculator {
 
     public static Double percent2bytes(Integer speedValueInMegaBits, Double rxTxValueInPrecent) {
         if (speedValueInMegaBits == null || rxTxValueInPrecent == null) {
-            return null;
+            return 0.0;
         }
         return megaBitToByte(speedValueInMegaBits * rxTxValueInPrecent / 100);
     }
