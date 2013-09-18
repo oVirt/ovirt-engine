354457b74b9dad5c6a76500eb63c1ab0f2f8a8f1
Kanagaraj M <kmayilsa@redhat.com>
2013-09-18 17:43:37 +0530

    engine: fail stop volume if rebalance is running
    'Stop' gluster volume is not allowed, if there
    is a rebalance operation is going on that volume.
    
    Change-Id: I6deb52b358b2ef78c5f41df29d866085bdedb371
    Bug-Url: https://bugzilla.redhat.com/1008900
    Signed-off-by: Kanagaraj M <kmayilsa@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/StopGlusterVolumeCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/StopGlusterVolumeCommand.java
@@ -2,11 +2,14 @@ package org.ovirt.engine.core.bll.gluster;
 
 import org.ovirt.engine.core.bll.LockIdNameAttribute;
 import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
+import org.ovirt.engine.core.bll.gluster.tasks.GlusterTaskUtils;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.action.gluster.GlusterVolumeActionParameters;
+import org.ovirt.engine.core.common.asynctasks.gluster.GlusterTaskType;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterStatus;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeEntity;
 import org.ovirt.engine.core.common.errors.VdcBllMessages;
+import org.ovirt.engine.core.common.job.JobExecutionStatus;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
 import org.ovirt.engine.core.common.vdscommands.gluster.GlusterVolumeActionVDSParameters;
@@ -27,6 +30,8 @@ public class StopGlusterVolumeCommand extends GlusterVolumeCommandBase<GlusterVo
     protected void setActionMessageParameters() {
         addCanDoActionMessage(VdcBllMessages.VAR__ACTION__STOP);
         addCanDoActionMessage(VdcBllMessages.VAR__TYPE__GLUSTER_VOLUME);
+        addCanDoActionMessage(String.format("$volumeName %1$s", getGlusterVolumeName()));
+        addCanDoActionMessage(String.format("$vdsGroup %1$s", getVdsGroupName()));
     }
 
     @Override
@@ -41,6 +46,12 @@ public class StopGlusterVolumeCommand extends GlusterVolumeCommandBase<GlusterVo
             addCanDoActionMessage(String.format("$volumeName %1$s", volume.getName()));
             return false;
         }
+
+        if (GlusterTaskUtils.isTaskOfType(volume, GlusterTaskType.REBALANCE)
+                && GlusterTaskUtils.isTaskStatus(volume, JobExecutionStatus.STARTED)) {
+            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_GLUSTER_VOLUME_CANNOT_STOP_REBALANCE_IN_PROGRESS);
+        }
+
         return true;
     }
 
