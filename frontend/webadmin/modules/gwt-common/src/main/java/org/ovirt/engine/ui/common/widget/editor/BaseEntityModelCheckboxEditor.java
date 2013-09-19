oVirt/ovirt-engine

commit 09d246450ad7b7fbda1dcf54c806fdb8d181a201

frontend: Adjusted width for checkbox editors
This patch sets width to 100 % for checkbox editors. This causes
checkboxes to take all space of parent elements.

Change-Id: Ie23b4bd23e8f53e4d68e95876a1183b04a35b116
Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/BaseEntityModelCheckboxEditor.java
@@ -42,6 +42,7 @@ public abstract class BaseEntityModelCheckboxEditor<T> extends AbstractValidated
         if (useCheckBoxWidgetLabel) {
             getContentWidgetContainer().getElement().getStyle().setFloat(Float.LEFT);
             getLabelElement().getStyle().setDisplay(Display.NONE);
+            getContentWidgetContainer().getElement().getStyle().setWidth(100, Unit.PCT);
         }
     }
 

commit 579d574ad52e2b55ebd480d68e794964be082cf9

frontend: fix broken layout in new/edit vm -> console tab
This regression has been introduced by
09d246450ad7b7fbda1dcf54c806fdb8d181a201

This patch adds the possibility to turn off the feature
introduced in the mentioned patch.

Change-Id: If4f8ec3b9426059803a3331c47bffc7cad9ac6c5
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/BaseEntityModelCheckboxEditor.java
@@ -31,7 +31,11 @@ public abstract class BaseEntityModelCheckboxEditor<T> extends AbstractValidated
         this(labelAlign, contentWidget, new VisibilityRenderer.SimpleVisibilityRenderer());
     }
 
-    public BaseEntityModelCheckboxEditor(Align labelAlign, BaseEntityModelCheckbox<T> contentWidget, VisibilityRenderer visibilityRendere) {
+    public BaseEntityModelCheckboxEditor(Align labelAlign, BaseEntityModelCheckbox<T> contentWidget, VisibilityRenderer visibilityRenderer) {
+        this(labelAlign, contentWidget, visibilityRenderer, false);
+    }
+
+    public BaseEntityModelCheckboxEditor(Align labelAlign, BaseEntityModelCheckbox<T> contentWidget, VisibilityRenderer visibilityRendere, boolean useFullWidthIfAvailable) {
         super(contentWidget, visibilityRendere);
 
         this.editor = WidgetWithLabelEditor.of(getContentWidget().asEditor(), this);
@@ -42,7 +46,9 @@ public abstract class BaseEntityModelCheckboxEditor<T> extends AbstractValidated
         if (useCheckBoxWidgetLabel) {
             getContentWidgetContainer().getElement().getStyle().setFloat(Float.LEFT);
             getLabelElement().getStyle().setDisplay(Display.NONE);
-            getContentWidgetContainer().getElement().getStyle().setWidth(100, Unit.PCT);
+            if (useFullWidthIfAvailable) {
+                getContentWidgetContainer().getElement().getStyle().setWidth(100, Unit.PCT);
+            }
         }
     }
 

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/EntityModelCheckBoxEditor.java
@@ -20,4 +20,8 @@ public class EntityModelCheckBoxEditor extends BaseEntityModelCheckboxEditor<Obj
         super(labelAlign, new EntityModelCheckBox(), visibilityRenderer);
     }
 
+    public EntityModelCheckBoxEditor(Align labelAlign, VisibilityRenderer visibilityRenderer, boolean useFullWidthIfAvailable) {
+        super(labelAlign, new EntityModelCheckBox(), visibilityRenderer, useFullWidthIfAvailable);
+    }
+
 }

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/AbstractVmPopupWidget.java
@@ -615,12 +615,12 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
         isRunAndPauseEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer());
         isDeleteProtectedEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer());
         isSmartcardEnabledEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer());
-        isConsoleDeviceEnabledEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer());
+        isConsoleDeviceEnabledEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer(), true);
         cdAttachedEditor = new EntityModelCheckBoxEditor(Align.LEFT, new ModeSwitchingVisibilityRenderer());
         allowConsoleReconnectEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer());
         isSoundcardEnabledEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer());
         copyTemplatePermissionsEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer());
-        isMemoryBalloonDeviceEnabled = new EntityModelCheckBoxEditor(Align.RIGHT);
+        isMemoryBalloonDeviceEnabled = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer(), true);
         isSingleQxlEnabledEditor = new EntityModelCheckBoxEditor(Align.RIGHT, new ModeSwitchingVisibilityRenderer());
         cpuPinningInfo =
                 new InfoIcon(SafeHtmlUtils.fromTrustedString(applicationTemplates.italicFixedWidth("400px",//$NON-NLS-1$

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/AbstractVmPopupWidget.ui.xml
@@ -252,7 +252,7 @@
         .monitorsLabel {
             float: left;
             color: #333333;
-            width: 250px;
+            width: 265px;
         }
 
 		.hostSelector {