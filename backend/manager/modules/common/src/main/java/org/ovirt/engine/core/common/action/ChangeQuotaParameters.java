f7e1ed02463aef659f451fb52b2c0195705420e8
Gilad Chaplik <gchaplik@redhat.com>
2013-09-24 15:34:08 +0200

    core: failing to assign disk quota
    missing default c'tor in parameter class caused some of
    the parameters to be null.
    
    Change-Id: Id39828bf3116b26e85609488267b128a2dd5bab9
    Bug-Url: https://bugzilla.redhat.com/1000399
    Signed-off-by: Gilad Chaplik <gchaplik@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/ChangeQuotaParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/ChangeQuotaParameters.java
@@ -4,12 +4,12 @@ import org.ovirt.engine.core.compat.Guid;
 
 public class ChangeQuotaParameters extends VdcActionParametersBase {
 
-    private static final long serialVersionUID = -6356486086345390742L;
+    private static final long serialVersionUID = -6136710318980549386L;
 
-    private Guid storagePoolId;
-    private Guid containerId;
     private Guid quotaId;
     private Guid objectId;
+    private Guid containerId;
+    private Guid storagePoolId;
 
     /**
      * @param quotaId the new quota id
@@ -17,27 +17,46 @@ public class ChangeQuotaParameters extends VdcActionParametersBase {
      * @param containerId in case of vm it's cluster, in case of disk it's storage
      * @param storagePoolId
      */
-    public ChangeQuotaParameters(Guid quotaId, Guid entityId, Guid containerId, Guid storagePoolId){
+    public ChangeQuotaParameters(Guid quotaId, Guid entityId, Guid containerId, Guid storagePoolId) {
         this.quotaId = quotaId;
         this.objectId = entityId;
         this.containerId = containerId;
         this.storagePoolId = storagePoolId;
     }
 
+    public ChangeQuotaParameters() {
+    }
+
     public Guid getQuotaId() {
         return quotaId;
+    }
+
+    public void setQuotaId(Guid quotaId) {
+        this.quotaId = quotaId;
     }
 
     public Guid getObjectId() {
         return objectId;
     }
 
+    public void setObjectId(Guid objectId) {
+        this.objectId = objectId;
+    }
+
+    public Guid getContainerId() {
+        return containerId;
+    }
+
+    public void setContainerId(Guid containerId) {
+        this.containerId = containerId;
+    }
+
     public Guid getStoragePoolId() {
         return storagePoolId;
     }
 
-    public Guid getContainerId(){
-        return containerId;
+    public void setStoragePoolId(Guid storagePoolId) {
+        this.storagePoolId = storagePoolId;
     }
 
 }
