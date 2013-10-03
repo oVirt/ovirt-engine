6b1f95e73312d7552f0e998342f69d508d83431d
Sergey Gotliv <sgotliv@redhat.com>
2013-10-04 01:06:32 +0300

    engine: remove handling GetStorageDomainListError after getImagesList
    getImagesList API used by GetImagesListCommand can't return
    GetStorageDomainListError therefore special handling of this error
    is removed.
    
    Change-Id: I56d639d83bd24e6b3939933998d17c5498670b5a
    Signed-off-by: Sergey Gotliv <sgotliv@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/GetImagesListVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/GetImagesListVDSCommand.java
@@ -2,15 +2,13 @@ package org.ovirt.engine.core.vdsbroker.irsbroker;
 
 import java.util.ArrayList;
 
-import org.ovirt.engine.core.common.errors.VDSError;
-import org.ovirt.engine.core.common.errors.VdcBllErrors;
 import org.ovirt.engine.core.common.vdscommands.GetImagesListVDSCommandParameters;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.vdsbroker.vdsbroker.StatusForXmlRpc;
 
 public class GetImagesListVDSCommand<P extends GetImagesListVDSCommandParameters>
         extends IrsBrokerCommand<P> {
-    private ImagesListReturnForXmlRpc _result;
+    private ImagesListReturnForXmlRpc result;
 
     public GetImagesListVDSCommand(P parameters) {
         super(parameters);
@@ -18,10 +16,10 @@ public class GetImagesListVDSCommand<P extends GetImagesListVDSCommandParameters
 
     @Override
     protected void executeIrsBrokerCommand() {
-        _result = getIrsProxy().getImagesList(getParameters().getStorageDomainId().toString());
+        result = getIrsProxy().getImagesList(getParameters().getStorageDomainId().toString());
         proceedProxyReturnValue();
-        ArrayList<Guid> tempRetValue = new ArrayList<Guid>(_result.getImageList().length);
-        for (String id : _result.getImageList()) {
+        ArrayList<Guid> tempRetValue = new ArrayList<Guid>(result.getImageList().length);
+        for (String id : result.getImageList()) {
             tempRetValue.add(new Guid(id));
         }
         setReturnValue(tempRetValue);
@@ -29,25 +27,11 @@ public class GetImagesListVDSCommand<P extends GetImagesListVDSCommandParameters
 
     @Override
     protected StatusForXmlRpc getReturnStatus() {
-        return _result.mStatus;
+        return result.mStatus;
     }
 
     @Override
     protected Object getReturnValueFromBroker() {
-        return _result;
-    }
-
-    @Override
-    protected void proceedProxyReturnValue() {
-        VdcBllErrors returnStatus = GetReturnValueFromStatus(getReturnStatus());
-        switch (returnStatus) {
-        case GetStorageDomainListError:
-            getVDSReturnValue().setVdsError(new VDSError(returnStatus, getReturnStatus().mMessage));
-            getVDSReturnValue().setSucceeded(false);
-            break;
-        default:
-            super.proceedProxyReturnValue();
-            break;
-        }
+        return result;
     }
 }
