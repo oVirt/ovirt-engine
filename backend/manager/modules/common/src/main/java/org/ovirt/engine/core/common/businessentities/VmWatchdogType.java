30a50484b8d187a80eddb93b5a61f70c80a093e3
Martin Sivak <msivak@redhat.com>
2013-10-09 15:06:46 +0200

    core: Remove support for ib700 watchdog
    This watchdog is no longer supported as it is an ISA device.
    
    Change-Id: I83242f3ccd7c2fc6ed7c4321ecaa89fca0140093
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1014531
    Signed-off-by: Martin Sivak <msivak@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmWatchdogType.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmWatchdogType.java
@@ -1,8 +1,8 @@
 package org.ovirt.engine.core.common.businessentities;
 
 public enum VmWatchdogType {
-    i6300esb,
-    ib700;
+    i6300esb;
+
     public static VmWatchdogType getByName(String name) {
         if (name == null || name.length() == 0) {
             return null;
