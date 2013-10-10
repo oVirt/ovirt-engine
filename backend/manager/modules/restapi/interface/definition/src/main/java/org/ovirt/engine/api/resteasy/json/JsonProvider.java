e87a70e006b8638b206bad01c02918030b255a0c
Juan Hernandez <juan.hernandez@redhat.com>
2013-10-10 15:15:22 +0200

    restapi: Add support for JAXB annotations in JSON
    The default JSON provider included with RESTEasy 2.x doesn't take into
    account the JAXB annotations, in particular it doesn't take into account
    the annotations that specify the names of elements and this generates
    inconsistencies in the names used in the XML representation and in the
    JSON representation. In order to solve the problem this patch modifies
    the JSON provider used by the application so that it takes into account
    the JAXB annotations.
    
    Change-Id: Ica39bbedd427dbf2f298b1292fe9a14f3825d6e5
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- backend/manager/modules/restapi/interface/definition/src/main/java/org/ovirt/engine/api/resteasy/json/JsonProvider.java
+++ backend/manager/modules/restapi/interface/definition/src/main/java/org/ovirt/engine/api/resteasy/json/JsonProvider.java
@@ -16,33 +16,50 @@
 
 package org.ovirt.engine.api.resteasy.json;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
 
-import org.jboss.resteasy.plugins.providers.jackson.ResteasyJacksonProvider;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
 
 @Provider
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class JsonProvider extends ResteasyJacksonProvider
-{
+public class JsonProvider extends JacksonJsonProvider {
     public JsonProvider() {
+        // Note that we can't specify here the use of JAXB annotations as it is ignored if the mapper is changed later,
+        // thus we need to call the parent constructor first, without a mapper, and then create the mapper and pass it
+        // to the parent class once it is fully configured:
         super();
-        setMapper(CustomObjectMapper.get());
-    }
 
-    @Override
-    public boolean isReadable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
-        return super.isReadable(aClass, type, annotations, mediaType);
-    }
+        // Create a mapper that uses our custom bean factory:
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializerFactory(new CustomBeanFactory());
 
-    @Override
-    public boolean isWriteable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
-        return super.isWriteable(aClass, type, annotations, mediaType);
+        // We need the instrospector that takes into account the JAXB annotations, both for the serializer and for the
+        // deserializer:
+        JaxbAnnotationIntrospector introspector = new JaxbAnnotationIntrospector();
+
+        // Configure the serializer:
+        SerializationConfig serCfg = mapper.getSerializationConfig()
+            .withView(String.class)
+            .withAnnotationIntrospector(introspector)
+            .with(SerializationConfig.Feature.INDENT_OUTPUT)
+            .withSerializationInclusion(JsonSerialize.Inclusion.NON_DEFAULT);
+        mapper.setSerializationConfig(serCfg);
+
+        // Configure the deserializer:
+        DeserializationConfig deserCfg = mapper.getDeserializationConfig()
+            .withAnnotationIntrospector(introspector);
+        mapper.setDeserializationConfig(deserCfg);
+
+        // Pass the configured mapper to the parent class:
+        setMapper(mapper);
     }
 }
