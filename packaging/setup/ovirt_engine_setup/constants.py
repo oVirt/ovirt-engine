oVirt/ovirt-engine

commit 1fca5dd1160f882463546139fa3a05550a09e856

packaging: setup: disable AIO on CPUs without VT
Change-Id: I6f5d1edf045fdfb878e6cf2d2cfa8a2e74b10db8
Bug-Url: https://bugzilla.redhat.com/1010691
Signed-off-by: Alex Lourie <alourie@redhat.com>

packaging/setup/ovirt_engine_setup/constants.py
@@ -1056,6 +1056,7 @@ class RemoveEnv(object):
 @osetupattrsclass
 class AIOEnv(object):
     ENABLE = 'OVESETUP_AIO/enable'
+    CONTINUE_WITHOUT_AIO = 'OVESETUP_AIO/continueWithoutAIO'
 
     @osetupattrs(
         answerfile=True,
@@ -1069,6 +1070,7 @@ class AIOEnv(object):
     LOCAL_CLUSTER = 'OVESETUP_AIO/localCluster'
     LOCAL_HOST = 'OVESETUP_AIO/localHost'
     VDSM_CPU = 'OVESETUP_AIO/vdsmCpu'
+    SUPPORTED = 'OVESETUP_AIO/supported'
 
     STORAGE_DOMAIN_SD_UUID = 'OVESETUP_AIO/storageDomainSdUuid'
     STORAGE_DOMAIN_DEFAULT_DIR = 'OVESETUP_AIO/storageDomainDefaultDir'

packaging/setup/plugins/ovirt-engine-setup/all-in-one/core.py
@@ -56,6 +56,10 @@ class Plugin(plugin.PluginBase):
             osetupcons.AIOEnv.CONFIGURE,
             None
         )
+        self.environment.setdefault(
+            osetupcons.AIOEnv.CONTINUE_WITHOUT_AIO,
+            None
+        )
 
     @plugin.event(
         stage=plugin.Stages.STAGE_SETUP,
@@ -65,6 +69,48 @@ class Plugin(plugin.PluginBase):
     )
     def _setup(self):
         self._enabled = True
+
+    @plugin.event(
+        stage=plugin.Stages.STAGE_CUSTOMIZATION,
+        condition=lambda self: (
+            self._enabled and
+            self.environment[
+                osetupcons.AIOEnv.SUPPORTED
+            ] is False
+        ),
+        name=osetupcons.Stages.AIO_CONFIG_NOT_AVAILABLE,
+        before=(
+            osetupcons.Stages.AIO_CONFIG_AVAILABLE,
+        ),
+    )
+    def _continueSetupWithoutAIO(self):
+        if self.environment[
+            osetupcons.AIOEnv.CONTINUE_WITHOUT_AIO
+        ] is None:
+            self.environment[
+                osetupcons.AIOEnv.CONTINUE_WITHOUT_AIO
+            ] = dialog.queryBoolean(
+                dialog=self.dialog,
+                name='OVESETUP_CONTINUE_WITHOUT_AIO',
+                note=_(
+                    'Disabling all-in-one plugin because hardware '
+                    'supporting virtualization could not be detected. '
+                    'Do you want to continue setup without AIO plugin? '
+                    '(@VALUES@) [@DEFAULT@]: '
+                ),
+                prompt=True,
+                default=False,
+            )
+
+        if self.environment[
+            osetupcons.AIOEnv.CONTINUE_WITHOUT_AIO
+        ]:
+            self._enabled = False
+            self.environment[osetupcons.AIOEnv.CONFIGURE] = False
+        else:
+            raise RuntimeError(
+                _('Aborted by user.')
+            )
 
     @plugin.event(
         stage=plugin.Stages.STAGE_CUSTOMIZATION,

packaging/setup/plugins/ovirt-engine-setup/all-in-one/cpu.py
@@ -94,6 +94,7 @@ class Plugin(plugin.PluginBase):
     )
     def _init(self):
         self.environment[osetupcons.AIOEnv.VDSM_CPU] = None
+        self.environment[osetupcons.AIOEnv.SUPPORTED] = False
 
     @plugin.event(
         stage=plugin.Stages.STAGE_SETUP,
@@ -106,20 +107,9 @@ class Plugin(plugin.PluginBase):
         from ovirt_host_deploy import hardware
         virtualization = hardware.Virtualization()
         result = virtualization.detect()
-        if result == virtualization.DETECT_RESULT_UNSUPPORTED:
-            self.logger.warning(
-                _(
-                    'Disabling all-in-one plugin because hardware '
-                    'does not support virtualization'
-                )
-            )
-            self.environment[osetupcons.AIOEnv.ENABLE] = False
-        elif result == virtualization.DETECT_RESULT_SUPPORTED:
+        if result == virtualization.DETECT_RESULT_SUPPORTED:
             self.logger.info(_('Hardware supports virtualization'))
-        else:
-            self.logger.warning(
-                _('Cannot detect if hardware supports virtualization')
-            )
+            self.environment[osetupcons.AIOEnv.SUPPORTED] = True
 
     @plugin.event(
         stage=plugin.Stages.STAGE_VALIDATION,

commit f68269c441b577c27b1399c996602f462341ed5c

packaging: setup: fix AIO stage definition
This fixes breakage introduced by commit 1fca5dd11

Change-Id: I358674153a116991a9bb3e181e69e64366cfe709
Bug-Url: https://bugzilla.redhat.com/1010691
Signed-off-by: Alex Lourie <alourie@redhat.com>

packaging/setup/ovirt_engine_setup/constants.py
@@ -563,6 +563,7 @@ class Stages(object):
     DIALOG_TITLES_E_SUMMARY = 'osetup.dialog.titles.summary.end'
 
     AIO_CONFIG_AVAILABLE = 'osetup.aio.config.available'
+    AIO_CONFIG_NOT_AVAILABLE = 'osetup.aio.config.not.available'
     AIO_CONFIG_STORAGE = 'osetup.aio.config.storage'
     AIO_CONFIG_VDSM = 'osetup.aio.config.vdsm'
 