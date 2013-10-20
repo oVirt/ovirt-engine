14d13f8a2e9bc9bb1ce291acb2b2f9364d222fdd
Yedidyah Bar David <didi@redhat.com>
2013-10-20 09:09:55 +0300

    packaging: setup: No spaces in /etc/sysconfig/nfs
    Do not put spaces around '=' in lines added to /etc/sysconfig/nfs .
    Adding them breaks sysvinit-based nfs init sh scripts.
    
    Bug-Url: https://bugzilla.redhat.com/1013347
    Change-Id: Ib9e1442b764cc9aef88044b7c1595236903edecf
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-setup/system/nfs.py
+++ packaging/setup/plugins/ovirt-engine-setup/system/nfs.py
@@ -176,6 +176,7 @@ class Plugin(plugin.PluginBase):
                     content=content,
                     params=config.values,
                     changed_lines=changed_lines,
+                    new_line_tpl='{spaces}{param}={value}',
                 )
             )
         )
