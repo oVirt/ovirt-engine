oVirt/ovirt-engine

commit 625b7452d840793df5e72764193c98c5ba121cdf

engine: Add non-plugin RDP invocation
This patch adds the possibility to run RDP console without MS RDP
activeX plugin installed (can be used e.g. by Firefox in MS Windows).
For this the attachment servlet is used which generates RDP descriptors.

The feature is configured via console popup dialog. Default (per engine)
configuration is stored in vdc_options.

NOTE: The automatic login feature will not work with non-plugin
invocation, because there is no straightforward way to pass a password
in the RDP descriptor.

NOTE on the db upgrade.
Since the ClientConsoleModeDefault was related to SPICE only, this patch
renames this option to ConsoleModeSpiceDefault and adds similar option
for RDP (ClientModeRdpDefault).

Change-Id: Ibbfc1b143b7125c278d22326785476d71d53ec0d
Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=918890

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/uicommon/AbstractRdp.java
@@ -1,0 +1,137 @@
+package org.ovirt.engine.ui.common.uicommon;
+
+import org.ovirt.engine.ui.frontend.Frontend;
+
+
+public class AbstractRdp {
+    private String address;
+    private String guestID;
+
+    private Boolean fullScreen = true;
+    private String fullScreenTitle;
+    private Integer width = 640;
+    private Integer height = 480;
+    private Integer authenticationLevel = 2;
+    private Boolean enableCredSspSupport = false; // Disable 'Credential Security Support Provider (CredSSP)' to enable
+                                                  // SSO.
+    private boolean useLocalDrives;
+    private Boolean redirectPrinters = false;
+    private Boolean redirectClipboard = true;
+    private Boolean redirectSmartCards = false;
+
+    public String getVdcUserNameAndDomain() {
+        String username = Frontend.getLoggedInUser().getUserName();
+        String domain = Frontend.getLoggedInUser().getDomainControler();
+
+        return username.contains("@") ? username : username + "@" + domain;//$NON-NLS-1$ //$NON-NLS-2$
+    }
+
+    public String getVdcUserName() {
+        return Frontend.getLoggedInUser().getUserName();
+    }
+
+    public String getVdcUserPassword() {
+        String password = Frontend.getLoggedInUser().getPassword();
+        return password == null ? "" : password;
+    }
+
+    public String getVdcUserDomainController() {
+        return Frontend.getLoggedInUser().getDomainControler();
+    }
+
+    public Boolean getFullScreen() {
+        return fullScreen;
+    }
+
+    public void setFullScreen(Boolean fullScreen) {
+        this.fullScreen = fullScreen;
+    }
+
+    public String getFullScreenTitle() {
+        return fullScreenTitle;
+    }
+
+    public void setFullScreenTitle(String fullScreenTitle) {
+        this.fullScreenTitle = fullScreenTitle;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getAuthenticationLevel() {
+        return authenticationLevel;
+    }
+
+    public void setAuthenticationLevel(Integer authenticationLevel) {
+        this.authenticationLevel = authenticationLevel;
+    }
+
+    public Boolean getEnableCredSspSupport() {
+        return enableCredSspSupport;
+    }
+
+    public void setEnableCredSspSupport(Boolean enableCredSspSupport) {
+        this.enableCredSspSupport = enableCredSspSupport;
+    }
+
+    public Boolean getRedirectPrinters() {
+        return redirectPrinters;
+    }
+
+    public void setRedirectPrinters(Boolean redirectPrinters) {
+        this.redirectPrinters = redirectPrinters;
+    }
+
+    public Boolean getRedirectClipboard() {
+        return redirectClipboard;
+    }
+
+    public void setRedirectClipboard(Boolean redirectClipboard) {
+        this.redirectClipboard = redirectClipboard;
+    }
+
+    public Boolean getRedirectSmartCards() {
+        return redirectSmartCards;
+    }
+
+    public void setRedirectSmartCards(Boolean redirectSmartCards) {
+        this.redirectSmartCards = redirectSmartCards;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String value) {
+        address = value;
+    }
+
+    public String getGuestID() {
+        return guestID;
+    }
+
+    public void setGuestID(String value) {
+        guestID = value;
+    }
+
+    public boolean getUseLocalDrives() {
+        return useLocalDrives;
+    }
+
+    public void setUseLocalDrives(boolean value) {
+        useLocalDrives = value;
+    }
+}

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/uicommon/RdpPluginImpl.java
@@ -1,0 +1,139 @@
+package org.ovirt.engine.ui.common.uicommon;
+
+import java.util.logging.Logger;
+
+import org.ovirt.engine.ui.uicommonweb.ConsoleUtils;
+import org.ovirt.engine.ui.uicommonweb.TypeResolver;
+import org.ovirt.engine.ui.uicommonweb.models.ConsoleProtocol;
+import org.ovirt.engine.ui.uicommonweb.models.vms.ErrorCodeEventArgs;
+import org.ovirt.engine.ui.uicommonweb.models.vms.IRdpPlugin;
+import org.ovirt.engine.ui.uicommonweb.models.vms.RdpConsoleModel;
+import org.ovirt.engine.ui.uicompat.ConstantsManager;
+import org.ovirt.engine.ui.uicompat.Event;
+import org.ovirt.engine.ui.uicompat.EventArgs;
+import org.ovirt.engine.ui.uicompat.EventDefinition;
+import org.ovirt.engine.ui.uicompat.IEventListener;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+
+public class RdpPluginImpl extends AbstractRdp implements IRdpPlugin, IEventListener {
+
+    private static Logger logger = Logger.getLogger(RdpPluginImpl.class.getName());
+
+    private static EventDefinition RdpDisconnectedEventDefinition = new EventDefinition("RdpDisconnected", RdpConsoleModel.class); //$NON-NLS-1$
+    private final Event disconnectedEvent = new Event(RdpDisconnectedEventDefinition);
+    private RdpConsoleModel parentModel;
+
+    private final ConsoleUtils consoleUtils;
+
+    public RdpPluginImpl() {
+        super();
+        this.consoleUtils = (ConsoleUtils) TypeResolver.getInstance().resolve(ConsoleUtils.class);
+    }
+
+    @Override
+    public void connect() {
+        if (!consoleUtils.isBrowserPluginSupported(ConsoleProtocol.RDP)) {
+            Window.alert(ConstantsManager.getInstance().getConstants().rdpIsNotSupportedInYourBrowser()); //$NON-NLS-1$
+            return;
+        }
+
+        disconnectedEvent.addListener(this);
+        connectNatively();
+    }
+
+    @Override
+    public void setParentModel(RdpConsoleModel model) {
+        this.parentModel = model;
+    }
+
+    public String getRDPCabURL() {
+        return GWT.getModuleBaseURL() + "msrdp.cab";//$NON-NLS-1$
+    }
+
+    @Override
+    public void eventRaised(Event ev, Object sender, EventArgs args) {
+        if (disconnectedEvent.equals(ev)) {
+            rdpDisconnected(sender, (ErrorCodeEventArgs) args);
+        }
+    }
+
+    private void rdpDisconnected(Object sender, ErrorCodeEventArgs e) {
+        disconnectedEvent.removeListener(this);
+
+        if (e.getErrorCode() > 100) {
+            parentModel.raiseErrorEvent(e);
+        }
+    }
+
+    public native void connectNatively() /*-{
+                                 try {
+                                 var server = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getAddress()();
+                                 var fullScreen = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getFullScreen()();
+                                 var fullScreenTitle = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getFullScreenTitle()();
+                                 var width = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getWidth()();
+                                 var height = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getHeight()();
+                                 var authenticationLevel = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getAuthenticationLevel()();
+                                 var enableCredSspSupport = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getEnableCredSspSupport()();
+                                 var redirectDrives = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getUseLocalDrives()();
+                                 var redirectPrinters = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getRedirectPrinters()();
+                                 var redirectClipboard = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getRedirectClipboard()();
+                                 var redirectSmartCards = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getRedirectSmartCards()();
+                                    var disconnectedEvent = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::disconnectedEvent;
+                                    var userName = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getVdcUserNameAndDomain()();
+                                    var password = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getVdcUserPassword()();
+                                    var domain = this.@org.ovirt.engine.ui.common.uicommon.RdpPluginImpl::getVdcUserDomainController()();
+                                    var model = this;
+
+                                 //var MsRdpClient = document.getElementById("MsRdpClient_"+server);
+                                 //TODO: Only in DEBUG mode
+                                 //alert("Server [" + server + "] fullScreen? [" + fullScreen + "], FullScreen Title [" +
+                                 //fullScreenTitle + "], width [" + width + "], height [" + height + "], auth level [" + authenticationLevel + "] enableCredSSPSupport? [" +
+                                 //enableCredSspSupport + "] redirect drives? [" + redirectDrives + "], redirect printers? [" + redirectPrinters + "], redirect clipboard? [" +
+                                 //redirectClipboard + "], redirect SmartCards [" + redirectSmartCards + "]");
+
+                                    // Remove previous client object
+                                    var previousMsRdpClient = $wnd.document.getElementById('MsRdpClient_' + server);
+                                    if (previousMsRdpClient)
+                                        $wnd.document.body.removeChild(previousMsRdpClient);
+
+                                 var MsRdpClient = $wnd.document.createElement("object");
+                                 $wnd.document.body.appendChild(MsRdpClient);
+
+                                 MsRdpClient.id = "MsRdpClient_" + server;
+                                 MsRdpClient.classid = "CLSID:4eb89ff4-7f78-4a0f-8b8d-2bf02e94e4b2";
+                                 MsRdpClient.server = server;
+                                 MsRdpClient.FullScreen = fullScreen;
+                                 MsRdpClient.desktopWidth = screen.width;
+                                 MsRdpClient.desktopHeight = screen.height;
+                                 MsRdpClient.width = 1;
+                                 MsRdpClient.height = 1;
+                                    MsRdpClient.UserName = userName;
+                                    MsRdpClient.AdvancedSettings.ClearTextPassword = password;
+                                 MsRdpClient.AdvancedSettings5.AuthenticationLevel = authenticationLevel;
+                                 MsRdpClient.AdvancedSettings7.EnableCredSspSupport = enableCredSspSupport;
+                                 MsRdpClient.AdvancedSettings2.RedirectDrives = redirectDrives;
+                                 MsRdpClient.AdvancedSettings2.RedirectPrinters = redirectPrinters;
+                                 MsRdpClient.AdvancedSettings2.RedirectClipboard = redirectClipboard;
+                                 MsRdpClient.AdvancedSettings2.RedirectSmartCards = redirectSmartCards;
+                                 MsRdpClient.AdvancedSettings2.ConnectionBarShowRestoreButton = false;
+
+                                 function OnConnected() {
+                                 }
+
+                                 function OnDisconnected(disconnectCode){
+                                     var extendedDiscReason = MsRdpClient.ExtendedDisconnectReason;
+                                     var errorCodeEventArgs = @org.ovirt.engine.ui.uicommonweb.models.vms.ErrorCodeEventArgs::new(I)(disconnectCode);
+                                     disconnectedEvent.@org.ovirt.engine.ui.uicompat.Event::raise(Ljava/lang/Object;Lorg/ovirt/engine/ui/uicompat/EventArgs;)(model, errorCodeEventArgs);
+                                     $wnd.document.body.removeChild(MsRdpClient);
+                                 }
+
+                                 MsRdpClient.attachEvent('OnConnected', OnConnected);
+                                 MsRdpClient.attachEvent('OnDisconnected', OnDisconnected);
+
+                                 MsRdpClient.connect();
+                                 } catch(e) { alert(e); }
+                                 }-*/;
+
+}

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/RdpConsoleModel.java
@@ -1,68 +1,79 @@
 package org.ovirt.engine.ui.uicommonweb.models.vms;
 
 import org.ovirt.engine.core.common.businessentities.VMStatus;
+import org.ovirt.engine.core.common.queries.ConfigurationValues;
+import org.ovirt.engine.ui.uicommonweb.ConsoleUtils;
 import org.ovirt.engine.ui.uicommonweb.TypeResolver;
 import org.ovirt.engine.ui.uicommonweb.dataprovider.AsyncDataProvider;
+import org.ovirt.engine.ui.uicommonweb.models.ConsoleProtocol;
 import org.ovirt.engine.ui.uicompat.ConstantsManager;
-import org.ovirt.engine.ui.uicompat.Event;
-import org.ovirt.engine.ui.uicompat.EventArgs;
-import org.ovirt.engine.ui.uicompat.EventDefinition;
 
-public class RdpConsoleModel extends ConsoleModel
-{
-    public static EventDefinition RdpDisconnectedEventDefinition = new EventDefinition("RdpDisconnected", //$NON-NLS-1$
-            RdpConsoleModel.class);
+public class RdpConsoleModel extends ConsoleModel {
+
+    public enum ClientConsoleMode { Native, Plugin, Auto }
 
     private IRdp privaterdp;
+    private ClientConsoleMode clientConsoleMode;
 
-    public IRdp getrdp()
-    {
+    private final ConsoleUtils consoleUtils;
+
+    public IRdp getrdp() {
         return privaterdp;
     }
 
-    private void setrdp(IRdp value)
-    {
+    public ClientConsoleMode getClientConsoleMode() {
+        return clientConsoleMode;
+    }
+
+    public void setRdpImplementation(ClientConsoleMode consoleMode) {
+        this.clientConsoleMode = consoleMode;
+
+        switch (consoleMode) {
+            case Native:
+                setrdp((IRdp) TypeResolver.getInstance().resolve(IRdpNative.class));
+                break;
+            case Plugin:
+                setrdp((IRdp) TypeResolver.getInstance().resolve(IRdpPlugin.class));
+                break;
+            default:
+                IRdpPlugin impl = (IRdpPlugin) TypeResolver.getInstance().resolve(IRdpPlugin.class);
+                setrdp(consoleUtils.isBrowserPluginSupported(ConsoleProtocol.RDP) ? impl
+                        : (IRdp) TypeResolver.getInstance().resolve(IRdpNative.class));
+                break;
+        }
+    }
+
+    private void setrdp(IRdp value) {
         privaterdp = value;
     }
 
-    public RdpConsoleModel()
-    {
+    public RdpConsoleModel() {
         setTitle(ConstantsManager.getInstance().getConstants().RDPTitle());
-
-        setrdp((IRdp) TypeResolver.getInstance().resolve(IRdp.class));
+        this.consoleUtils = (ConsoleUtils) TypeResolver.getInstance().resolve(ConsoleUtils.class);
+        setRdpImplementation(
+                ClientConsoleMode.valueOf((String) AsyncDataProvider.getConfigValuePreConverted(ConfigurationValues.ClientModeRdpDefault)));
     }
 
     @Override
-    protected void connect()
-    {
-        if (getEntity() != null)
-        {
+    protected void connect() {
+        if (getEntity() != null) {
             getLogger().debug("Connecting to RDP console..."); //$NON-NLS-1$
 
             getrdp().setAddress(getEntity().getVmHost().split("[ ]", -1)[0]); //$NON-NLS-1$
             getrdp().setGuestID(getEntity().getId().toString());
 
-            // Subscribe to disconnect event
-            if (getrdp().getDisconnectedEvent() != null)
-            {
-                getrdp().getDisconnectedEvent().addListener(this);
-            }
-
             // Try to connect.
-            try
-            {
+            try {
                 getrdp().connect();
                 updateActionAvailability();
-            } catch (RuntimeException ex)
-            {
+            } catch (RuntimeException ex) {
                 getLogger().error("Exception on RDP connect", ex); //$NON-NLS-1$
             }
         }
     }
 
     @Override
-    protected void updateActionAvailability()
-    {
+    protected void updateActionAvailability() {
         super.updateActionAvailability();
 
         getConnectCommand().setIsExecutionAllowed(getEntity() != null
@@ -70,24 +81,7 @@ public class RdpConsoleModel extends ConsoleModel
                 && AsyncDataProvider.isWindowsOsType(getEntity().getVmOs()));
     }
 
-    @Override
-    public void eventRaised(Event ev, Object sender, EventArgs args)
-    {
-        super.eventRaised(ev, sender, args);
-
-        if (getrdp().getDisconnectedEvent() != null && ev.equals(getrdp().getDisconnectedEvent()))
-        {
-            rdp_Disconnected(sender, (ErrorCodeEventArgs) args);
-        }
-    }
-
-    private void rdp_Disconnected(Object sender, ErrorCodeEventArgs e)
-    {
-        getrdp().getDisconnectedEvent().removeListener(this);
-
-        if (e.getErrorCode() > 100)
-        {
-            getErrorEvent().raise(this, e);
-        }
+    public void raiseErrorEvent(ErrorCodeEventArgs e) {
+        getErrorEvent().raise(this, e);
     }
 }

backend/manager/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -21,6 +21,7 @@ PLEASE NOTE THAT THIS SCRIPT MUST REMAIN RE-ENTRANT!
 -- Rename existing configuration key names, values modifications are preserved
 ------------------------------------------------------------------------------------
 select fn_db_rename_config_key('AuditLogAgingThreashold', 'AuditLogAgingThreshold', 'general');
+select fn_db_rename_config_key('ClientConsoleModeDefault', 'ClientModeSpiceDefault', 'general');
 select fn_db_rename_config_key('PowerClientAutoApprovePatterns','AutoApprovePatterns','general');
 select fn_db_rename_config_key('PowerClientAutoRegistrationDefaultVdsGroupID','AutoRegistrationDefaultVdsGroupID','general');
 select fn_db_rename_config_key('PowerClientAutoInstallCertificateOnApprove','AutoInstallCertificateOnApprove','general');
@@ -517,8 +518,10 @@ select fn_db_add_config_value('WaitForVdsInitInSec','60','general');
 select fn_db_add_config_value('NetworkConnectivityCheckTimeoutInSeconds','120','general');
 -- AutoRecoveryConfiguration
 select fn_db_add_config_value('AutoRecoveryAllowedTypes','{\"storage domains\":\"true\",\"hosts\":\"true\"}','general');
--- Client console mode settings (Auto, Native, Plugin)
-select fn_db_add_config_value('ClientConsoleModeDefault','Auto','general');
+-- Spice client mode default settings (Auto, Native, Plugin)
+select fn_db_add_config_value('ClientModeSpiceDefault','Auto','general');
+-- Rdp client mode default settings (Auto, Native, Plugin)
+select fn_db_add_config_value('ClientModeRdpDefault','Auto','general');
 
 select fn_db_add_config_value('LogMaxPhysicalMemoryUsedThresholdInPercentage', '95', 'general');
 select fn_db_add_config_value('LogMaxCpuUsedThresholdInPercentage', '95', 'general');
@@ -750,6 +753,7 @@ select fn_db_delete_config_value('UseRtl8139_pv','2.2,3.0,3.1,3.2');
 select fn_db_delete_config_value('VdsFenceOptions','general');
 select fn_db_delete_config_value('VirtualMachineDomainName','general');
 select fn_db_delete_config_value('AuditLogAgingThreashold','general');
+select fn_db_delete_config_value('ClientConsoleModeDefault','general');
 select fn_db_delete_config_value('LiveStorageMigrationEnabled','3.0');
 select fn_db_delete_config_value('LiveStorageMigrationEnabled','3.1');
 select fn_db_delete_config_value('LiveStorageMigrationEnabled','3.2');

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/config/ConfigValues.java
@@ -1321,7 +1321,7 @@ public enum ConfigValues {
 
     @TypeConverterAttribute(String.class)
     @DefaultValueAttribute("Auto")
-    ClientConsoleModeDefault(501),
+    ClientModeSpiceDefault(501),
 
     @TypeConverterAttribute(Boolean.class)
     @DefaultValueAttribute("false")
@@ -1341,6 +1341,10 @@ public enum ConfigValues {
     @TypeConverterAttribute(String.class)
     @DefaultValueAttribute("/usr/bin/vdsm-tool vdsm-id")
     GetVdsmIdByVdsmToolCommand(505),
+
+    @TypeConverterAttribute(String.class)
+    @DefaultValueAttribute("Auto")
+    ClientModeRdpDefault(506),
 
     Invalid(65535);
 

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/ConfigurationValues.java
@@ -25,7 +25,8 @@ public enum ConfigurationValues {
     SpiceReleaseCursorKeys(ConfigAuthType.User),
     SpiceToggleFullScreenKeys(ConfigAuthType.User),
     SpiceProxyDefault(ConfigAuthType.User),
-    ClientConsoleModeDefault(ConfigAuthType.User),
+    ClientModeSpiceDefault(ConfigAuthType.User),
+    ClientModeRdpDefault(ConfigAuthType.User),
     HighUtilizationForEvenlyDistribute(ConfigAuthType.User),
     SpiceUsbAutoShare(ConfigAuthType.User),
     ImportDefaultPath,

backend/manager/modules/root/src/main/webapp/WEB-INF/web.xml
@@ -54,7 +54,7 @@
   </servlet>
   <servlet-mapping>
     <servlet-name>GetAttachmentServlet</servlet-name>
-    <url-pattern>/console.vv</url-pattern>
+    <url-pattern>/console/*</url-pattern>
   </servlet-mapping>
 
   <!-- Documentation: -->

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationConstants.java
@@ -1172,13 +1172,13 @@ public interface CommonApplicationConstants extends Constants {
     String spice();
 
     @DefaultStringValue("Auto")
-    String spiceImplAuto();
+    String auto();
 
     @DefaultStringValue("Native client")
-    String spiceImplNative();
+    String nativeClient();
 
     @DefaultStringValue("Browser plugin")
-    String spiceImplPlugin();
+    String browserPlugin();
 
     @DefaultStringValue("Pass Ctrl-Alt-Del to virtual machine")
     String ctrlAltDel();
@@ -1196,7 +1196,10 @@ public interface CommonApplicationConstants extends Constants {
     String ctrlAltDeletIsNotSupportedOnWindows();
 
     @DefaultStringValue("Your browser doesn't support SPICE plugin")
-    String pluginNotSupportedByBrowser();
+    String spicePluginNotSupportedByBrowser();
+
+    @DefaultStringValue("Your browser doesn't support RDP plugin")
+    String rdpPluginNotSupportedByBrowser();
 
     @DefaultStringValue("No SPICE proxy defined on system level")
     String spiceProxyCanBeEnabledOnlyWhenDefined();

commit 34d4bbcbe8f01297b8770bf69b445b08dcf1b1f7

userportal: error flow of RDP RdpPluginImpl broken
There were the following issues which caused the error not to be propagated:
- a regression caused by 625b7452d840793df5e72764193c98c5ba121cdf
  which did bring back the crashing of IE if the RDP connection is not
  successful (originally fixed by fa10641c3484ea10d0b1b33ef8e3d656b5e6dd35).

- by the same commit (625b7452d840793df5e72764193c98c5ba121cdf) the new method
  setParentModel has been introduced but it was not called. It caused a NPE on
  RdpPluginImpl.rdpDisconnected:66. Fixed by calling this method on the
  relevant places.

- after autologin the Frontend.getInstance().getLoginPassword() returns null
  which causes NPE on RdpPluginImpl:113 when setting this pass to
  ClearTextPassword. Fixed at AbstractRdp level because the null password for
  DbUser class can still be relevant - just needs to be handled differently on
  AbstractRdp level.

Change-Id: I85fc3b32c4f544f23864db2aefa5ebcd4d896b75
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1014710
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/uicommon/AbstractRdp.java
@@ -31,7 +31,8 @@ public class AbstractRdp {
     }
 
     public String getUserPassword() {
-        return Frontend.getInstance().getLoginPassword();
+        String password = Frontend.getInstance().getLoginPassword();
+        return password != null ? password : ""; //$NON-NLS-1$
     }
 
     public String getUserDomainController() {

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/uicommon/RdpPluginImpl.java
@@ -126,7 +126,6 @@ public class RdpPluginImpl extends AbstractRdp implements IRdpPlugin, IEventList
                                      var extendedDiscReason = MsRdpClient.ExtendedDisconnectReason;
                                      var errorCodeEventArgs = @org.ovirt.engine.ui.uicommonweb.models.vms.ErrorCodeEventArgs::new(I)(disconnectCode);
                                      disconnectedEvent.@org.ovirt.engine.ui.uicompat.Event::raise(Ljava/lang/Object;Lorg/ovirt/engine/ui/uicompat/EventArgs;)(model, errorCodeEventArgs);
-                                     $wnd.document.body.removeChild(MsRdpClient);
                                  }
 
                                  MsRdpClient.attachEvent('OnConnected', OnConnected);

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/RdpConsoleModel.java
@@ -35,10 +35,13 @@ public class RdpConsoleModel extends ConsoleModel {
                 setrdp((IRdp) TypeResolver.getInstance().resolve(IRdpNative.class));
                 break;
             case Plugin:
-                setrdp((IRdp) TypeResolver.getInstance().resolve(IRdpPlugin.class));
+                IRdpPlugin plugin = (IRdpPlugin) TypeResolver.getInstance().resolve(IRdpPlugin.class);
+                plugin.setParentModel(this);
+                setrdp(plugin);
                 break;
             default:
                 IRdpPlugin impl = (IRdpPlugin) TypeResolver.getInstance().resolve(IRdpPlugin.class);
+                impl.setParentModel(this);
                 setrdp(consoleUtils.isBrowserPluginSupported(ConsoleProtocol.RDP) ? impl
                         : (IRdp) TypeResolver.getInstance().resolve(IRdpNative.class));
                 break;