08be92e45f0ab142ae1e64d44acad29522f2768d
Ori Liel <oliel@redhat.com>
2013-11-11 15:23:12 +0200

    restapi: Remove Unnecessary Code
    Change-Id: I6d84b63a6f954015240865ffbb3d5fb95bab0bb1
    Signed-off-by: Ori Liel <oliel@redhat.com>

--- backend/manager/modules/restapi/interface/definition/src/main/java/org/ovirt/engine/api/resteasy/json/JsonProvider.java
+++ backend/manager/modules/restapi/interface/definition/src/main/java/org/ovirt/engine/api/resteasy/json/JsonProvider.java
@@ -16,9 +16,6 @@
 
 package org.ovirt.engine.api.resteasy.json;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -33,15 +30,5 @@ public class JsonProvider extends ResteasyJacksonProvider {
     public JsonProvider() {
         super();
         setMapper(CustomObjectMapper.get());
-    }
-
-    @Override
-    public boolean isReadable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
-        return super.isReadable(aClass, type, annotations, mediaType);
-    }
-
-    @Override
-    public boolean isWriteable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
-        return super.isWriteable(aClass, type, annotations, mediaType);
     }
 }
