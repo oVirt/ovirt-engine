555cfb764c248da2309107c72011b8384ba0963b
Greg Sheremeta <gshereme@redhat.com>
2013-11-19 22:00:41 -0500

    userportal, webadmin: run favicon bug workaround in Scheduler
    Run the Firefox favicon bug workaround in GWT Scheduler, since GWT-P
    place transition implementation uses deferred commands.
    
    Change-Id: I73278382894d02970022ceef8b95d8c2f3b62ec0
    Bug-Url: https://bugzilla.redhat.com/999436
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/place/ApplicationPlaceManager.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/place/ApplicationPlaceManager.java
@@ -7,6 +7,8 @@ import org.ovirt.engine.ui.common.auth.UserLoginChangeEvent;
 import org.ovirt.engine.ui.common.auth.UserLoginChangeEvent.UserLoginChangeHandler;
 import org.ovirt.engine.ui.common.uicommon.ClientAgentType;
 
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Node;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.shared.EventBus;
@@ -48,12 +50,17 @@ public abstract class ApplicationPlaceManager extends PlaceManagerImpl implement
     public void onValueChange(ValueChangeEvent<String> event) {
         super.onValueChange(event);
         if ("firefox".equalsIgnoreCase(clientAgentType.getBrowser())) { //$NON-NLS-1$
-            Node favicon = DOM.getElementById("id-link-favicon");  //$NON-NLS-1$
-            if (favicon != null) {
-                Node parent = favicon.getParentNode();
-                favicon.removeFromParent();
-                parent.appendChild(favicon);
-            }
+            Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+                @Override
+                public void execute() {
+                    Node favicon = DOM.getElementById("id-link-favicon");  //$NON-NLS-1$
+                    if (favicon != null) {
+                        Node parent = favicon.getParentNode();
+                        favicon.removeFromParent();
+                        parent.appendChild(favicon);
+                    }
+                }
+            });
         }
     }
 
