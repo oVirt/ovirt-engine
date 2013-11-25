16fb1ca88ffe746b45ae00ee894daf1fdea8d3dc
Allon Mureinik <amureini@redhat.com>
2013-11-25 21:54:13 +0200

    core: remove HandlersFactory.createVmPoolHandler
    Remove unused method.
    
    Change-Id: I5033305edcf4bc1b18ef69596b9079d0ad080a61
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/HandlersFactory.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/HandlersFactory.java
@@ -4,10 +4,6 @@ import org.ovirt.engine.core.common.interfaces.ITagsHandler;
 
 public final class HandlersFactory {
 
-    public static VmPoolHandler createVmPoolHandler() {
-        return new VmPoolHandler();
-    }
-
     public static ITagsHandler createTagsHandler() {
         return new TagsHandler();
     }
