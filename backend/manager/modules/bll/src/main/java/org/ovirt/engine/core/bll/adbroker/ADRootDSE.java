6421aabb5dbfea707f8a8e5570cca40630aaba7d
Allon Mureinik <amureini@redhat.com>
2013-11-26 09:14:35 +0200

    core: Remove unused code from ADRootDSE
    Change-Id: Ie7cc4ecb119fc9bdf36a62d1618648c5bdbef775
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/adbroker/ADRootDSE.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/adbroker/ADRootDSE.java
@@ -3,30 +3,15 @@ package org.ovirt.engine.core.bll.adbroker;
 import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
 
-
-
 /**
  * Holds information queried from a root DSE
  */
 public class ADRootDSE implements RootDSE {
 
-
-    private boolean mixedFunctionalityLevel;
     private String defaultNamingContext;
 
-    public ADRootDSE() {
-    }
-
-    public ADRootDSE(boolean mixedFunctionalityLevel, String defaultNamingContext) {
-        this.mixedFunctionalityLevel = mixedFunctionalityLevel;
-        this.defaultNamingContext = defaultNamingContext;
-    }
-
-    public ADRootDSE(Attributes rootDseRecords) throws NumberFormatException, NamingException {
-        boolean mixedMode =
-                Integer.parseInt(rootDseRecords.get(ADRootDSEAttributes.domainControllerFunctionality.name()).get().toString()) == 4;
+    public ADRootDSE(Attributes rootDseRecords) throws NamingException {
         String namingContext = rootDseRecords.get(ADRootDSEAttributes.defaultNamingContext.name()).get().toString();
-        this.mixedFunctionalityLevel = mixedMode;
         this.defaultNamingContext = namingContext;
     }
 
@@ -39,13 +24,4 @@ public class ADRootDSE implements RootDSE {
     public String getDefaultNamingContext() {
         return defaultNamingContext;
     }
-
-    public void setMixedFunctionalityLevel(boolean mixedFunctionalityLevel) {
-        this.mixedFunctionalityLevel = mixedFunctionalityLevel;
-    }
-
-    public boolean isMixedFunctionalityLevel() {
-        return mixedFunctionalityLevel;
-    }
-
 }
