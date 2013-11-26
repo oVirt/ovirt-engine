153fcfed10b7ddc724596a7536208dc15a323202
Allon Mureinik <amureini@redhat.com>
2013-11-26 09:05:32 +0200

    core: Remove unused TagsHandler methods
    Change-Id: I01e44ce5e7f71f8c2c32d1dbf8969e8c50824966
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/TagsHandler.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/TagsHandler.java
@@ -3,13 +3,8 @@ package org.ovirt.engine.core.bll;
 import org.ovirt.engine.core.common.businessentities.tags;
 import org.ovirt.engine.core.common.interfaces.ITagsHandler;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class TagsHandler implements ITagsHandler {
-
-    public void MoveTag(Guid tagId, Guid newParent) {
-        TagsDirector.getInstance().MoveTag(tagId, newParent);
-    }
 
     @Override
     public tags GetTagByTagName(String tagName) {
@@ -29,41 +24,5 @@ public class TagsHandler implements ITagsHandler {
     @Override
     public String GetTagNameAndChildrenNames(Guid tagId) {
         return TagsDirector.getInstance().GetTagNameAndChildrenNames(tagId);
-    }
-
-    public static boolean IsUserAttachedToTag(Guid tagId, Guid userId) {
-        return (DbFacade
-                .getInstance()
-                .getTagDao()
-                .getAllForUsersWithIds(
-                        TagsDirector.getInstance()
-                                .GetTagIdAndChildrenIds(tagId)).size() != 0);
-    }
-
-    public static boolean IsUserGroupAttachedToTag(Guid tagId, Guid groupId) {
-        return (DbFacade
-                .getInstance()
-                .getTagDao()
-                .getAllForUsersWithIds(
-                        TagsDirector.getInstance()
-                                .GetTagIdAndChildrenIds(tagId)).size() != 0);
-    }
-
-    public static boolean IsVmAttachedToTag(Guid tagId, Guid vmId) {
-        return (DbFacade
-                .getInstance()
-                .getTagDao()
-                .getAllVmTagsWithIds(
-                        TagsDirector.getInstance()
-                                .GetTagIdAndChildrenIds(tagId)).size() != 0);
-    }
-
-    public static boolean IsVdsAttachedToTag(Guid tagId, int vdsId) {
-        return (DbFacade
-                .getInstance()
-                .getTagDao()
-                .getAllForVdsWithIds(
-                        TagsDirector.getInstance()
-                                .GetTagIdAndChildrenIds(tagId)).size() != 0);
     }
 }
