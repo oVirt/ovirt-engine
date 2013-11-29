208a235806b014e7177c3b00b41303e835fc1631
Sahina Bose <sabose@redhat.com>
2013-11-29 12:42:50 +0530

    engine: Reinitialize VDSManager on change cluster
    When changing cluster, the VDSManager dictionary
    has to be updated.
    This is needed because the target cluster may support
    different services from the source cluster, and the
    monitoring strategies need to be updated.
    
    Change-Id: Icb25c12d35113af09c67d5fe6e275c6901725e54
    Bug-Url: https://bugzilla.redhat.com/968178
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/ChangeVDSClusterCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/ChangeVDSClusterCommand.java
@@ -135,6 +135,8 @@ public class ChangeVDSClusterCommand<T extends ChangeVDSClusterParameters> exten
                 staticData.setVdsGroupId(targetClusterId);
                 DbFacade.getInstance().getVdsStaticDao().update(staticData);
                 getCompensationContext().stateChanged();
+                // remove the server from resource manager and add it back
+                initializeVds();
                 return null;
             }
         });
