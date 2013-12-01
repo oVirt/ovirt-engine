0f5cc79585cb599bcf4b24643bb01734af950e1f
Allon Mureinik <amureini@redhat.com>
2013-12-01 10:21:02 +0200

    core: rm unused EventNotificationMethod ctors
    Change-Id: I2c148ac817ba66d1295a09e7f0771b841cdb602e
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/EventNotificationMethod.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/EventNotificationMethod.java
@@ -8,14 +8,6 @@ import org.ovirt.engine.core.common.utils.EnumUtils;
 public class EventNotificationMethod implements Serializable {
     private static final long serialVersionUID = -8648391842192154307L;
 
-    public EventNotificationMethod() {
-    }
-
-    public EventNotificationMethod(int method_id, String method_type) {
-        this.methodId = method_id;
-        this.methodType = method_type;
-    }
-
     private int methodId;
 
     public int getmethod_id() {
