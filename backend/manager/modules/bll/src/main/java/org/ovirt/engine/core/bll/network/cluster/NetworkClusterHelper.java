6cdd8965359cfa2d81b70a56aa60feeca499c430
Moti Asayag <masayag@redhat.com>
2013-12-02 22:25:52 +0200

    engine: Use VdsDao for fetching hosts in NetworkClusterHelper
    Instead of using the search engine for fetching host
    internally, a direct use of the VdsDao is done.
    
    In addition, skip DB call for the hosts if the network
    is not required.
    
    Change-Id: Icf6574700974b800d96627da9439d7793b7d31b2
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/cluster/NetworkClusterHelper.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/cluster/NetworkClusterHelper.java
@@ -5,16 +5,13 @@ import java.util.List;
 import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.Backend;
 import org.ovirt.engine.core.common.businessentities.VDS;
-import org.ovirt.engine.core.common.businessentities.VDSGroup;
 import org.ovirt.engine.core.common.businessentities.VDSStatus;
 import org.ovirt.engine.core.common.businessentities.network.Network;
 import org.ovirt.engine.core.common.businessentities.network.NetworkCluster;
 import org.ovirt.engine.core.common.businessentities.network.NetworkClusterId;
 import org.ovirt.engine.core.common.businessentities.network.NetworkStatus;
 import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
-import org.ovirt.engine.core.common.interfaces.SearchType;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
-import org.ovirt.engine.core.common.queries.SearchParameters;
 import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
@@ -36,24 +33,16 @@ public class NetworkClusterHelper {
      * @param net
      *            The network itself.
      */
-    @SuppressWarnings("unchecked")
     public static void setStatus(Guid vdsGroupId, final Network net) {
         NetworkStatus status = NetworkStatus.OPERATIONAL;
         NetworkCluster networkCluster =
                 DbFacade.getInstance().getNetworkClusterDao().get(new NetworkClusterId(vdsGroupId, net.getId()));
 
         if (networkCluster != null) {
-            VDSGroup vdsGroup = DbFacade.getInstance().getVdsGroupDao().get(vdsGroupId);
-
-            // Search all vds in cluster that have the specify network, if not the
-            // network is not active
-            SearchParameters searchParams = new SearchParameters("hosts: cluster = "
-                    + vdsGroup.getName(), SearchType.VDS);
-            searchParams.setMaxCount(Integer.MAX_VALUE);
-            List<VDS> vdsList = Backend.getInstance()
-                    .runInternalQuery(VdcQueryType.Search, searchParams).getReturnValue();
 
             if (networkCluster.isRequired()) {
+                // Search all vds in cluster that have the specified network, if not the network is not active
+                List<VDS> vdsList = DbFacade.getInstance().getVdsDao().getAllForVdsGroup(vdsGroupId);
                 for (VDS vds : vdsList) {
                     if (vds.getStatus() != VDSStatus.Up) {
                         continue;
@@ -67,6 +56,7 @@ public class NetworkClusterHelper {
                             return StringUtils.equals(i.getNetworkName(), net.getName());
                         }
                     });
+
                     if (iface == null) {
                         status = NetworkStatus.NON_OPERATIONAL;
                         break;
