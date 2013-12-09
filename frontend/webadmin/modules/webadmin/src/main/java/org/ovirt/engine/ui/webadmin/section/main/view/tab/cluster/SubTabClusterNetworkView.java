7ee19f4349f732c6f3ab7c203b2fa705b1451557
Anmol Babu <anbabu@redhat.com>
2013-12-09 16:06:54 +0530

    webadmin : Hide cluster logical networks virt actions in Gluster mode
    The menu options like "Add Network", "Manage
    Networks" and "Set as Display" and the dialogues coming from these
    menu options, are not applicable to RHS.So,this patch hides them
    in Gluster only mode.
    
    Change-Id: I2436c11b51f6707c70aef426e1e253ff958fa6dd
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1015020
    Signed-off-by: Anmol Babu <anbabu@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/cluster/SubTabClusterNetworkView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/cluster/SubTabClusterNetworkView.java
@@ -8,12 +8,14 @@ import java.util.Map;
 import org.ovirt.engine.core.common.businessentities.VDSGroup;
 import org.ovirt.engine.core.common.businessentities.network.Network;
 import org.ovirt.engine.core.common.businessentities.network.NetworkStatus;
+import org.ovirt.engine.core.common.mode.ApplicationMode;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.uicommon.model.SearchableDetailModelProvider;
 import org.ovirt.engine.ui.common.widget.table.column.EnumColumn;
 import org.ovirt.engine.ui.common.widget.table.column.SafeHtmlWithSafeHtmlTooltipColumn;
 import org.ovirt.engine.ui.common.widget.table.column.TextColumnWithTooltip;
 import org.ovirt.engine.ui.uicommonweb.UICommand;
+import org.ovirt.engine.ui.uicommonweb.models.ApplicationModeHelper;
 import org.ovirt.engine.ui.uicommonweb.models.clusters.ClusterListModel;
 import org.ovirt.engine.ui.uicommonweb.models.clusters.ClusterNetworkListModel;
 import org.ovirt.engine.ui.webadmin.ApplicationConstants;
@@ -133,26 +135,28 @@ public class SubTabClusterNetworkView extends AbstractSubTabTableView<VDSGroup, 
         };
         getTable().addColumn(descColumn, constants.descriptionNetwork(), "400px"); //$NON-NLS-1$
 
-        getTable().addActionButton(new WebAdminButtonDefinition<Network>(constants.addNetworkNetwork()) {
-            @Override
-            protected UICommand resolveCommand() {
-                return getDetailModel().getNewNetworkCommand();
-            }
-        });
+        if (ApplicationModeHelper.isModeSupported(ApplicationMode.VirtOnly)) {
+            getTable().addActionButton(new WebAdminButtonDefinition<Network>(constants.addNetworkNetwork()) {
+                @Override
+                protected UICommand resolveCommand() {
+                    return getDetailModel().getNewNetworkCommand();
+                }
+            });
 
-        getTable().addActionButton(new WebAdminButtonDefinition<Network>(constants.assignDetatchNetworksNework()) {
-            @Override
-            protected UICommand resolveCommand() {
-                return getDetailModel().getManageCommand();
-            }
-        });
+            getTable().addActionButton(new WebAdminButtonDefinition<Network>(constants.assignDetatchNetworksNework()) {
+                @Override
+                protected UICommand resolveCommand() {
+                    return getDetailModel().getManageCommand();
+                }
+            });
 
-        getTable().addActionButton(new WebAdminButtonDefinition<Network>(constants.setAsDisplayNetwork()) {
-            @Override
-            protected UICommand resolveCommand() {
-                return getDetailModel().getSetAsDisplayCommand();
-            }
-        });
+            getTable().addActionButton(new WebAdminButtonDefinition<Network>(constants.setAsDisplayNetwork()) {
+                @Override
+                protected UICommand resolveCommand() {
+                    return getDetailModel().getSetAsDisplayCommand();
+                }
+            });
+        }
     }
 
 }
