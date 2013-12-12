1ce485eeaa36ddafbf93e4687b667ceca019be95
Allon Mureinik <amureini@redhat.com>
2013-12-12 12:55:37 +0200

    core: Fix FB on AddExistingFileStorageDomain
    This patch reverts 5ccea20c205457f70cbd480f80455f600b386836 and offers a
    proper fix for the (bogus) NP_NULL_PARAM_DEREF error in
    AddExistingFileStorageDomainCommand.
    
    The domainFromIrs variable can NEVER be null, hence it's meaningless to
    protect against it.
    
    Change-Id: Id8c5a11ef872cac6aeebfa18bf28757fb4a8b26c
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/AddExistingFileStorageDomainCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/AddExistingFileStorageDomainCommand.java
@@ -81,8 +81,7 @@ public class AddExistingFileStorageDomainCommand<T extends StorageDomainManageme
                 new HSMGetStorageDomainInfoVDSCommandParameters(getVdsId(), getStorageDomain().getId())
         ).getReturnValue();
 
-        if (domainFromIrs != null
-                && domainFromIrs.getFirst().getStorageDomainType() != getStorageDomain().getStorageDomainType()) {
+        if (domainFromIrs.getFirst().getStorageDomainType() != getStorageDomain().getStorageDomainType()) {
             return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_CANNOT_CHANGE_STORAGE_DOMAIN_TYPE);
         }
 
@@ -91,19 +90,16 @@ public class AddExistingFileStorageDomainCommand<T extends StorageDomainManageme
 
     protected boolean concreteCheckExistingStorageDomain(Pair<StorageDomainStatic, SANState> domain) {
         boolean returnValue = false;
-
-        if (domain != null) {
-            StorageDomainStatic domainFromIrs = domain.getFirst();
-            if (StringUtils.isEmpty(getStorageDomain().getStorageStaticData().getStorage())
-                    && StringUtils.isEmpty(domainFromIrs.getStorage()) && domainFromIrs.getConnection() != null
-                    && getStorageDomain().getStorageStaticData().getConnection() != null) {
-                returnValue = (StringUtils.equals(domainFromIrs.getConnection().getconnection(), getStorageDomain()
-                        .getStorageStaticData().getConnection().getconnection()));
-            } else if (!StringUtils.isEmpty(getStorageDomain().getStorageStaticData().getStorage())
-                    && !StringUtils.isEmpty(domainFromIrs.getStorage())) {
-                returnValue = (StringUtils.equals(domainFromIrs.getStorage(), getStorageDomain().getStorageStaticData()
-                        .getStorage()));
-            }
+        StorageDomainStatic domainFromIrs = domain.getFirst();
+        if (StringUtils.isEmpty(getStorageDomain().getStorageStaticData().getStorage())
+                && StringUtils.isEmpty(domainFromIrs.getStorage()) && domainFromIrs.getConnection() != null
+                && getStorageDomain().getStorageStaticData().getConnection() != null) {
+            returnValue = (StringUtils.equals(domainFromIrs.getConnection().getconnection(), getStorageDomain()
+                    .getStorageStaticData().getConnection().getconnection()));
+        } else if (!StringUtils.isEmpty(getStorageDomain().getStorageStaticData().getStorage())
+                && !StringUtils.isEmpty(domainFromIrs.getStorage())) {
+            returnValue = (StringUtils.equals(domainFromIrs.getStorage(), getStorageDomain().getStorageStaticData()
+                    .getStorage()));
         }
         if (!returnValue) {
             addCanDoActionMessage(VdcBllMessages.ERROR_CANNOT_ADD_EXISTING_STORAGE_DOMAIN_CONNECTION_DATA_ILLEGAL);
