31474aa79075eebce1f39dce4dd13bf97507c932
Kanagaraj M <kmayilsa@redhat.com>
2013-12-24 14:39:26 +0530

    webadmin: removed volume rebalance actions
    Gluster Volume Rebalance feature is not fully implemented
    in ovirt-engine-3.3. Removing the actions for now
    
    Change-Id: I0768d48f432235f405e9a6d8fee36794e2a72628
    Bug-Url: https://bugzilla.redhat.com/1046000
    Signed-off-by: Kanagaraj M <kmayilsa@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/MainTabVolumeView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/MainTabVolumeView.java
@@ -1,16 +1,12 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.tab;
 
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeEntity;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeType;
 import org.ovirt.engine.core.common.businessentities.gluster.TransportType;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.uicommon.model.MainModelProvider;
-import org.ovirt.engine.ui.common.widget.action.ActionButtonDefinition;
-import org.ovirt.engine.ui.common.widget.action.CommandLocation;
 import org.ovirt.engine.ui.common.widget.table.column.EnumColumn;
 import org.ovirt.engine.ui.common.widget.table.column.TextColumnWithTooltip;
 import org.ovirt.engine.ui.uicommonweb.UICommand;
@@ -21,7 +17,6 @@ import org.ovirt.engine.ui.webadmin.ApplicationConstants;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.tab.MainTabVolumePresenter;
 import org.ovirt.engine.ui.webadmin.section.main.view.AbstractMainTabWithDetailsTableView;
 import org.ovirt.engine.ui.webadmin.widget.action.WebAdminButtonDefinition;
-import org.ovirt.engine.ui.webadmin.widget.action.WebAdminMenuBarButtonDefinition;
 import org.ovirt.engine.ui.webadmin.widget.table.column.VolumeActivityColumn;
 import org.ovirt.engine.ui.webadmin.widget.table.column.VolumeStatusColumn;
 
@@ -145,28 +140,5 @@ public class MainTabVolumeView extends AbstractMainTabWithDetailsTableView<Glust
                 return getMainModel().getOptimizeForVirtStoreCommand();
             }
         });
-        List<ActionButtonDefinition<GlusterVolumeEntity>> rebalanceSubActions = new LinkedList<ActionButtonDefinition<GlusterVolumeEntity>>();
-        rebalanceSubActions.add(new WebAdminButtonDefinition<GlusterVolumeEntity>(constants.startRebalance()) {
-            @Override
-            protected UICommand resolveCommand() {
-                return getMainModel().getStartRebalanceCommand();
-            }
-        });
-
-        rebalanceSubActions.add(new WebAdminButtonDefinition<GlusterVolumeEntity>(constants.stopRebalance()) {
-            @Override
-            protected UICommand resolveCommand() {
-                return getMainModel().getStopRebalanceCommand();
-            }
-        });
-
-        rebalanceSubActions.add(new WebAdminButtonDefinition<GlusterVolumeEntity>(constants.statusRebalance()) {
-            @Override
-            protected UICommand resolveCommand() {
-                return getMainModel().getStatusRebalanceCommand();
-            }
-        });
-
-        getTable().addActionButton(new WebAdminMenuBarButtonDefinition<GlusterVolumeEntity>(constants.rebalanceVolume(),rebalanceSubActions,CommandLocation.ContextAndToolBar));
     }
 }
