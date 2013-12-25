2c4ddd6f196577e8c994e68937f1d4527e765410
Moti Asayag <masayag@redhat.com>
2013-12-25 14:55:17 +0200

    engine: Initialize Mom Policies Severities
    When added audit log types for mom policies,
    their severities should have been initialized.
    
    Change-Id: I654845ec76bd876399b4206657a660eef4172612
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dal/dbbroker/auditloghandling/AuditLogDirector.java
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dal/dbbroker/auditloghandling/AuditLogDirector.java
@@ -61,6 +61,7 @@ public final class AuditLogDirector {
         initCommonSeverities();
         initClusterPolicySeverities();
         initExtrnalEvents();
+        initMomPoliciesSeverities();
     }
 
     private static void initProviderSeverities() {
