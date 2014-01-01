2161634fb20d3def0275d6d84fedae36b5e5873d
Moti Asayag <masayag@redhat.com>
2014-01-01 10:57:06 +0200

    engine: Add test for ValidName annotation
    Change-Id: Ica4ef1fd43a1c6144bb722b57a036a160d713c57
    Signed-off-by: Moti Asayag <masayag@redhat.com>

---
+++ backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/ValidNameValidatorTest.java
@@ -0,0 +1,67 @@
+package org.ovirt.engine.core.common.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.ovirt.engine.core.common.validation.annotation.ValidName;
+
+@RunWith(Parameterized.class)
+public class ValidNameValidatorTest {
+
+    private Validator validator;
+    private String name;
+    private boolean expectedResult;
+
+    public ValidNameValidatorTest(String name, boolean expectedResult) {
+        this.name = name;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void setup() throws Exception {
+        validator = ValidationUtils.getValidator();
+    }
+
+    @Test
+    public void checkName() {
+        Set<ConstraintViolation<ValidNameContainer>> result = validator.validate(new ValidNameContainer(name));
+        assertEquals(expectedResult, result.isEmpty());
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> namesParams() {
+        return Arrays.asList(new Object[][] {
+                { "abc", true },
+                { "123", true },
+                { "abc123", true },
+                { "123abc", true },
+                { null, true },
+                { " ", false },
+                { "", false },
+                { "abc ", false },
+                { " abc", false },
+                { "abc cde", false },
+                { "abc*", false }
+        });
+    }
+
+    private class ValidNameContainer {
+
+        @ValidName
+        private String name;
+
+        public ValidNameContainer(String name) {
+            this.name = name;
+        }
+    }
+}
