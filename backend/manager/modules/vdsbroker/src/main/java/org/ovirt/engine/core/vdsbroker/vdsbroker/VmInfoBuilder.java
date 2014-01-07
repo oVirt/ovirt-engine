oVirt/ovirt-engine

commit af23e740b091832d9f12dc47083fe89007ad9b87

engine: VM Device Type for Display Type
* This change added VM Device Type for Display Type in property file.
* The attribute VM Device Type for each OS is present in the os info
  property file.
* The attribute VM Device Type in the Display Type now is the default
  value if none is found in the os file.
* Also the os info attribute spiceSupport was removed, since it would
  be ambiguous. The list now is the OS supported displays.

Change-Id: I326f8aaf0de95332436499205f90336d2f4e73d7
Signed-off-by: Gustavo Pedrosa <gustavo.pedrosa@eldorado.org.br>
Signed-off-by: Leonardo Bianconi <leonardo.bianconi@eldorado.org.br>

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/VmInfoBuilder.java
@@ -29,6 +29,8 @@ import org.ovirt.engine.core.common.businessentities.network.NetworkQoS;
 import org.ovirt.engine.core.common.businessentities.network.VmInterfaceType;
 import org.ovirt.engine.core.common.businessentities.network.VmNic;
 import org.ovirt.engine.core.common.businessentities.network.VnicProfile;
+import org.ovirt.engine.core.common.osinfo.OsRepository;
+import org.ovirt.engine.core.common.utils.SimpleDependecyInjector;
 import org.ovirt.engine.core.common.utils.VmDeviceCommonUtils;
 import org.ovirt.engine.core.common.utils.VmDeviceType;
 import org.ovirt.engine.core.compat.Guid;
@@ -78,12 +80,17 @@ public class VmInfoBuilder extends VmInfoBuilderBase {
      */
     private void addVideoCardByDisplayType(DisplayType displayType, int numOfMonitors, boolean singleQxlPci) {
         Map<String, Object> struct = new HashMap<String, Object>();
+        VmDeviceType vmDeviceType = getOsRepository().getDisplayDevice(vm.getOs(), vm.getVdsGroupCompatibilityVersion(), displayType);
         // create a monitor as an unmanaged device
         struct.put(VdsProperties.Type, VmDeviceGeneralType.VIDEO.getValue());
-        struct.put(VdsProperties.Device, displayType.getVmDeviceType().getName());
+        struct.put(VdsProperties.Device, vmDeviceType);
         struct.put(VdsProperties.SpecParams, getNewMonitorSpecParams(displayType, numOfMonitors, singleQxlPci));
         struct.put(VdsProperties.DeviceId, String.valueOf(Guid.newGuid()));
         devices.add(struct);
+    }
+
+    private OsRepository getOsRepository() {
+        return SimpleDependecyInjector.getInstance().get(OsRepository.class);
     }
 
     /**

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/OsRepositoryQuery.java
@@ -42,8 +42,8 @@ public class OsRepositoryQuery<P extends OsQueryParameters> extends QueriesComma
             case GetMinimumOsRam:
                 setReturnValue(osRepository.getMinimumRam(getParameters().getOsId(), getParameters().getVersion()));
                 break;
-            case GetSpiceSupportMatrix:
-                setReturnValue(osRepository.getSpiceSupportMatrix());
+            case GetDisplayTypes:
+                setReturnValue(osRepository.getDisplayTypes());
                 break;
             case HasNicHotplugSupport:
                 setReturnValue(osRepository.hasNicHotplugSupport(getParameters().getOsId(), getParameters().getVersion()));

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/utils/VmDeviceUtils.java
@@ -77,9 +77,10 @@ public class VmDeviceUtils {
                 // add video device per each monitor
                 int monitors = entity.getSingleQxlPci() ? 1 : entity.getNumOfMonitors();
                 for (int i = 0; i<monitors; i++) {
+                    VmDeviceType vmDeviceType = osRepository.getDisplayDevice(entity.getOsId(), ClusterUtils.getCompatibilityVersion(entity), entity.getDefaultDisplayType());
                     addManagedDevice(new VmDeviceId(Guid.newGuid(), entity.getId()),
                             VmDeviceGeneralType.VIDEO,
-                            entity.getDefaultDisplayType().getVmDeviceType(),
+                            vmDeviceType,
                             getMemExpr(entity.getNumOfMonitors(), entity.getSingleQxlPci()),
                             true,
                             false,
@@ -453,10 +454,12 @@ public class VmDeviceUtils {
     }
 
     private static void addVideoDevice(VmBase vm) {
+        VmDeviceType vmDeviceType = osRepository.getDisplayDevice(vm.getOsId(), ClusterUtils.getCompatibilityVersion(vm), vm.getDefaultDisplayType());
+
         addManagedDevice(
                 new VmDeviceId(Guid.newGuid(), vm.getId()),
                 VmDeviceGeneralType.VIDEO,
-                vm.getDefaultDisplayType().getVmDeviceType(),
+                vmDeviceType,
                 getMemExpr(vm.getNumOfMonitors(), vm.getSingleQxlPci()),
                 true,
                 true,

backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/HotPlugDiskToVmCommandTest.java
@@ -13,10 +13,8 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -133,21 +131,12 @@ public class HotPlugDiskToVmCommandTest {
     @Test
     public void canDoActionFailedVirtIODisk() throws Exception {
         mockVmStatusUp();
-        mockSpiceSupportMatrix();
         when(osRepository.getOsName(0)).thenReturn("RHEL6");
         createNotVirtIODisk();
         assertFalse(command.canDoAction());
         assertTrue(command.getReturnValue()
                 .getCanDoActionMessages()
                 .contains(VdcBllMessages.ACTION_TYPE_FAILED_GUEST_OS_VERSION_IS_NOT_SUPPORTED.toString()));
-    }
-
-    private void mockSpiceSupportMatrix() {
-        Map<Integer, Map<Version, Boolean>> supportMatrix = new HashMap<>();
-        HashMap<Version, Boolean> chunk = new HashMap<>();
-        chunk.put(Version.v3_1, true);
-        supportMatrix.put(0, chunk);
-        when(osRepository.getSpiceSupportMatrix()).thenReturn(supportMatrix);
     }
 
     @Test

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/DisplayType.java
@@ -3,24 +3,24 @@ package org.ovirt.engine.core.common.businessentities;
 import org.ovirt.engine.core.common.utils.VmDeviceType;
 
 public enum DisplayType {
-    vnc(VmDeviceType.CIRRUS),
+    vnc(VmDeviceType.VGA),
     qxl(VmDeviceType.QXL);
 
-    private VmDeviceType vmDeviceType;
+    private VmDeviceType defaultVmDeviceType;
 
     public int getValue() {
         return this.ordinal();
     }
 
-    DisplayType(VmDeviceType vmDeviceType) {
-        this.vmDeviceType = vmDeviceType;
+    DisplayType(VmDeviceType defaultVmDeviceType) {
+        this.defaultVmDeviceType = defaultVmDeviceType;
     }
     public static DisplayType forValue(int value) {
         return values()[value];
     }
 
-    public VmDeviceType getVmDeviceType() {
-        return vmDeviceType;
+    public VmDeviceType getDefaultVmDeviceType() {
+        return defaultVmDeviceType;
     }
 
 }

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/osinfo/OsRepository.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
+import java.util.List;
 import java.util.Map;
 
 import org.ovirt.engine.core.common.utils.Pair;
+import org.ovirt.engine.core.common.businessentities.DisplayType;
+import org.ovirt.engine.core.common.utils.VmDeviceType;
 import org.ovirt.engine.core.compat.Version;
 
 /**
@@ -69,10 +72,19 @@ public interface OsRepository {
     public int getMaximumRam(int osId, Version version);
 
      /**
-      * @return map (osId -> compatibility version -> Boolean) that indicates SPICE support for all OSs and
+      * @return map (osId -> compatibility version -> display types list) for all OSs and
       * compatibility versions
      */
-    public Map<Integer, Map<Version, Boolean>> getSpiceSupportMatrix();
+    public Map<Integer, Map<Version, List<DisplayType>>> getDisplayTypes();
+
+    /**
+     * Get device type from display type of the OS
+     * @param osId
+     * @param version
+     * @param displayType
+     * @return
+     */
+    public VmDeviceType getDisplayDevice(int osId, Version version, DisplayType displayType);
 
     /**
      * Checks if that OS network devices support hotplug.

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/OsQueryParameters.java
@@ -38,7 +38,7 @@ public class OsQueryParameters extends VdcQueryParametersBase {
     }
 
     public enum OsRepositoryVerb {
-        GetSpiceSupportMatrix,
+        GetDisplayTypes,
         HasNicHotplugSupport,
         GetNicHotplugSupportMap,
         GetDiskHotpluggableInterfacesMap,

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/VmDeviceType.java
@@ -14,6 +14,7 @@ public enum VmDeviceType {
     SPICEVMC("spicevmc", "23"),
     QXL("qxl"),
     CIRRUS("cirrus"),
+    VGA("vga"),
     SOUND("sound"),
     ICH6("ich6"),
     AC97("ac97"),
@@ -72,5 +73,20 @@ public enum VmDeviceType {
         }
         return VmDeviceType.ICH6;
     }
-}
 
+    /**
+     * gets device type for a given device name
+     *
+     * @param name
+     * @return
+     */
+    public static VmDeviceType getByName(String name) {
+        for (VmDeviceType vmDeviceType : values()) {
+            if (name.equals(vmDeviceType.getName())) {
+                return vmDeviceType;
+            }
+        }
+        return null;
+    }
+
+}

commit c4afd6d50a5050318dc312759998a127d9aac0fb

engine: exception on run once when changed display protocol
Regression from: af23e740b091832d9f12dc47083fe89007ad9b87

The problem was that the VdsProperties.Device part of the video card was added
as VmDeviceType which was not possible to serialize.

Fixed by getting the String out of it the same way as it used to be.

Change-Id: I89127115b9925574a45e5d81a90bbad2c01e3d22
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/VmInfoBuilder.java
@@ -85,7 +85,7 @@ public class VmInfoBuilder extends VmInfoBuilderBase {
         VmDeviceType vmDeviceType = getOsRepository().getDisplayDevice(vm.getOs(), vm.getVdsGroupCompatibilityVersion(), displayType);
         // create a monitor as an unmanaged device
         struct.put(VdsProperties.Type, VmDeviceGeneralType.VIDEO.getValue());
-        struct.put(VdsProperties.Device, vmDeviceType);
+        struct.put(VdsProperties.Device, vmDeviceType.getName());
         struct.put(VdsProperties.SpecParams, getNewMonitorSpecParams(displayType, numOfMonitors, singleQxlPci));
         struct.put(VdsProperties.DeviceId, String.valueOf(Guid.newGuid()));
         devices.add(struct);