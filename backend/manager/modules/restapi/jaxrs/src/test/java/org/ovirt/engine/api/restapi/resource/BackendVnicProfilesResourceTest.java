5712b438cb6fbf311d710eabad0205b4ae244021
Moti Asayag <masayag@redhat.com>
2014-02-11 00:42:19 +0200

    restapi: Assure network existence when adding a vnic profile
    In order to create a vnic profile, the user must provide
    the network id to which he wishes to add the profile.
    If a network cannot be obtained by the id, the parameters
    should consider invalid and a proper message should be
    returned.
    
    The backend has logic for this scenario, however when performing
    this action with a user that its permissions should be verified,
    there is a generic "User is not authorized to perform this action"
    even though the message refers to a non-existing network entity.
    
    Change-Id: Ib517cc0fd04c4f0e12fd6bdbdca8c6277600e325
    Bug-Url: https://bugzilla.redhat.com/1031369
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/test/java/org/ovirt/engine/api/restapi/resource/BackendVnicProfilesResourceTest.java
+++ backend/manager/modules/restapi/jaxrs/src/test/java/org/ovirt/engine/api/restapi/resource/BackendVnicProfilesResourceTest.java
@@ -3,6 +3,8 @@ package org.ovirt.engine.api.restapi.resource;
 import java.util.List;
 
 import org.ovirt.engine.api.model.VnicProfile;
+import org.ovirt.engine.core.common.businessentities.network.Network;
+import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 import org.ovirt.engine.core.common.queries.VdcQueryType;
 
@@ -27,5 +29,14 @@ public class BackendVnicProfilesResourceTest extends AbstractBackendVnicProfiles
     @Override
     protected List<VnicProfile> getCollection() {
         return collection.list().getVnicProfiles();
+    }
+
+    @Override
+    protected void setUpNetworkQueryExpectations() {
+        setUpEntityQueryExpectations(VdcQueryType.GetNetworkById,
+                IdQueryParameters.class,
+                new String[] { "Id" },
+                new Object[] { NETWORK_ID },
+                new Network());
     }
 }
