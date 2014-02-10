e79f2afca490e179820884a3efa27ba83fdf231e
Vojtech Szocs <vszocs@redhat.com>
2014-02-10 19:20:25 +0100

    engine: added HelpTagType to HelpTag
    Added a HelpTagType enum, and references to it in HelpTag. It's not used in GWT code,
    but helptag.py can now use it to filter down its ini/diff files to only those
    tags required specifically by webadmin or userportal.
    
    Updated helptag.py to use the HelpTagType to filter the help tags it scans.
    
    Change-Id: I64a3702878b9d341d78119585b51129478bb6d37
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

---
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/help/HelpTagType.java
@@ -0,0 +1,9 @@
+package org.ovirt.engine.ui.uicommonweb.help;
+
+/**
+ * Type of {@code HelpTag}. Simply describes which applications use a {@code HelpTag}.
+ *
+ */
+public enum HelpTagType {
+    WEBADMIN, USERPORTAL, UNKNOWN, COMMON
+}
