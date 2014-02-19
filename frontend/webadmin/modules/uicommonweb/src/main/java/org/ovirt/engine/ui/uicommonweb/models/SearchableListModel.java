2757fa81633fa05c295181161ea95d5329e36605
Ravi Nori <rnori@redhat.com>
2014-02-18 19:13:05 -0500

    webadmin: search paging on event log does not use paging correctly
    search paging on event log does not use paging
    correctly If we browse to 2nd page from UI Events
    TAB by pressing the '>' button and then put
    "Vms: page 1" as the search text , we an error
    on server side
    
    Change-Id: I86787302601365e07f9d923f26d483c7bceb30ef
    Bug-Url: https://bugzilla.redhat.com/1064395
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/SearchableListModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/SearchableListModel.java
@@ -205,6 +205,7 @@ public abstract class SearchableListModel<T> extends ListModel<T> implements Gri
         if (!ObjectUtils.objectsEqual(searchString, value))
         {
             searchString = value;
+            pagingSearchString = null;
             searchStringChanged();
             onPropertyChanged(new PropertyChangedEventArgs("SearchString")); //$NON-NLS-1$
         }
@@ -659,16 +660,25 @@ public abstract class SearchableListModel<T> extends ListModel<T> implements Gri
 
     protected void searchNextPage()
     {
+        searchString = stripPageKeyword(searchString);
         setSearchStringPage(getNextSearchPageNumber());
         getSearchCommand().execute();
     }
 
     protected void searchPreviousPage()
     {
+        searchString = stripPageKeyword(searchString);
         setSearchStringPage(getPreviousSearchPageNumber());
         getSearchCommand().execute();
     }
 
+    private String stripPageKeyword(String str) {
+        int index = str.indexOf("page"); //$NON-NLS-1$
+        if (index == -1) {
+            return str;
+        }
+        return str.substring(0, index);
+    }
     protected boolean getNextSearchPageAllowed()
     {
         if (!getSearchNextPageCommand().getIsAvailable() || getItems() == null
