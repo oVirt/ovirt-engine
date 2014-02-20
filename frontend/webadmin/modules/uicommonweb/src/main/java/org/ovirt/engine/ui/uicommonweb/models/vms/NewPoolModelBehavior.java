oVirt/ovirt-engine

commit 604d22dc036d1f5a4431c8247ce246d0c0a0c109

frontend: template versions in popup dialogs
This patch adds template versions (base template + version instead of
just template) to all VM, pool and template dialogs

Change-Id: I26f3ddba7833dd257bc8a069a772ad192352d1a2
Signed-off-by: Arik Hadas <ahadas@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/NewPoolModelBehavior.java
@@ -9,9 +9,7 @@ import org.ovirt.engine.core.common.businessentities.VmBase;
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.ui.frontend.AsyncQuery;
 import org.ovirt.engine.ui.frontend.INewAsyncCallback;
-import org.ovirt.engine.ui.uicommonweb.Linq;
 import org.ovirt.engine.ui.uicommonweb.dataprovider.AsyncDataProvider;
-import org.ovirt.engine.ui.uicommonweb.models.ListModel;
 import org.ovirt.engine.ui.uicommonweb.models.SystemTreeItemModel;
 import org.ovirt.engine.ui.uicommonweb.validation.IValidation;
 import org.ovirt.engine.ui.uicommonweb.validation.NewPoolNameLengthValidation;
@@ -38,8 +36,30 @@ public class NewPoolModelBehavior extends PoolModelBehaviorBase {
     }
 
     @Override
-    protected void setupSelectedTemplate(ListModel model, List<VmTemplate> templates) {
-        getModel().getTemplate().setSelectedItem(Linq.<VmTemplate> firstOrDefault(templates));
+    public void postDataCenterWithClusterSelectedItemChanged() {
+        super.postDataCenterWithClusterSelectedItemChanged();
+
+        final DataCenterWithCluster dataCenterWithCluster =
+                (DataCenterWithCluster) getModel().getDataCenterWithClustersList().getSelectedItem();
+        StoragePool dataCenter = getModel().getSelectedDataCenter();
+        if (dataCenter == null) {
+            return;
+        }
+
+        AsyncDataProvider.getTemplateListByDataCenter(new AsyncQuery(this, new INewAsyncCallback() {
+            @Override
+            public void onSuccess(Object target1, Object returnValue1) {
+
+                List<VmTemplate> baseTemplates =
+                        filterNotBaseTemplates((List<VmTemplate>) returnValue1);
+
+                List<VmTemplate> filteredTemplates =
+                        AsyncDataProvider.filterTemplatesByArchitecture(baseTemplates,
+                                dataCenterWithCluster.getCluster().getArchitecture());
+
+                getModel().getBaseTemplate().setItems(filteredTemplates);
+            }
+        }), dataCenter.getId());
     }
 
     @Override

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationConstants.java
@@ -60,10 +60,19 @@ public interface CommonApplicationConstants extends Constants {
     String createAsSubTemplate();
 
     @DefaultStringValue("Root Template")
-    String baseTemplate();
+    String rootTemplate();
 
     @DefaultStringValue("Sub Version Name")
     String templateVersionName();
+
+    @DefaultStringValue("Template Sub Version")
+    String templateSubVersion();
+
+    @DefaultStringValue("latest")
+    String latest();
+
+    @DefaultStringValue("base template")
+    String baseTemplate();
 
     @DefaultStringValue("Disk ")
     String diskNamePrefix();

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/AbstractVmPopupWidget.java
@@ -20,6 +20,7 @@ import org.ovirt.engine.core.common.businessentities.VmPoolType;
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.businessentities.VmType;
 import org.ovirt.engine.core.common.queries.ConfigurationValues;
+import org.ovirt.engine.core.compat.StringFormat;
 import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.ui.common.CommonApplicationConstants;
 import org.ovirt.engine.ui.common.CommonApplicationMessages;
@@ -65,6 +66,7 @@ import org.ovirt.engine.ui.uicommonweb.models.vms.DiskModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.TimeZoneModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.UnitVmModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.key_value.KeyValueModel;
+import org.ovirt.engine.ui.uicompat.ConstantsManager;
 import org.ovirt.engine.ui.uicompat.EnumTranslator;
 import org.ovirt.engine.ui.uicompat.Event;
 import org.ovirt.engine.ui.uicompat.EventArgs;
@@ -168,6 +170,11 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
     @Path(value = "comment.entity")
     @WithElementId("comment")
     public StringEntityModelTextBoxEditor commentEditor;
+
+    @UiField(provided = true)
+    @Path(value = "baseTemplate.selectedItem")
+    @WithElementId("baseTemplate")
+    public ListModelTypeAheadListBoxEditor<VmTemplate> baseTemplateEditor;
 
     @UiField(provided = true)
     @Path(value = "template.selectedItem")
@@ -869,7 +876,7 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
                 },
                 new ModeSwitchingVisibilityRenderer());
 
-        templateEditor = new ListModelTypeAheadListBoxEditor<VmTemplate>(
+        baseTemplateEditor = new ListModelTypeAheadListBoxEditor<VmTemplate>(
                 new ListModelTypeAheadListBoxEditor.NullSafeSuggestBoxRenderer<VmTemplate>() {
 
                     @Override
@@ -887,6 +894,36 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
                 },
                 new ModeSwitchingVisibilityRenderer());
 
+        templateEditor = new ListModelTypeAheadListBoxEditor<VmTemplate>(
+                new ListModelTypeAheadListBoxEditor.NullSafeSuggestBoxRenderer<VmTemplate>() {
+
+                    @Override
+                    public String getReplacementStringNullSafe(VmTemplate data) {
+                        return getDisplayableTemplateVersionName(data);
+                    }
+
+                    @Override
+                    public String getDisplayStringNullSafe(VmTemplate data) {
+                        return typeAheadNameDescriptionTemplateNullSafe(
+                                getDisplayableTemplateVersionName(data),
+                                data.getDescription()
+                        );
+                    }
+
+                    private String getDisplayableTemplateVersionName(VmTemplate template) {
+                        String versionName = template.getTemplateVersionName();
+                        if (ConstantsManager.getInstance().getConstants().latestTemplateVersionName().equals(versionName)) {
+                            return constants.latest();
+                        }
+
+                        versionName = template.getId().equals(template.getBaseTemplateId()) ?
+                                constants.baseTemplate() : template.getTemplateVersionName();
+
+                        return (versionName == null ? "" : versionName) //$NON-NLS-1$
+                                + StringFormat.format(" (%d)", template.getTemplateVersionNumber()); //$NON-NLS-1$
+                    }
+                },
+                new ModeSwitchingVisibilityRenderer());
 
         oSTypeEditor = new ListModelListBoxEditor<Integer>(new AbstractRenderer<Integer>() {
             @Override
@@ -1017,7 +1054,8 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
         nameLabel.setText(constants.nameVmPopup());
         descriptionEditor.setLabel(constants.descriptionVmPopup());
         commentEditor.setLabel(constants.commentLabel());
-        templateEditor.setLabel(constants.basedOnTemplateVmPopup());
+        baseTemplateEditor.setLabel(constants.basedOnTemplateVmPopup());
+        templateEditor.setLabel(constants.templateSubVersion());
 
         oSTypeEditor.setLabel(constants.osVmPopup());
         vmTypeEditor.setLabel(constants.optimizedFor());
@@ -1447,6 +1485,7 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
         nextTabIndex = generalTab.setTabIndexes(nextTabIndex);
         quotaEditor.setTabIndex(nextTabIndex++);
         oSTypeEditor.setTabIndex(nextTabIndex++);
+        baseTemplateEditor.setTabIndex(nextTabIndex++);
         templateEditor.setTabIndex(nextTabIndex++);
 
         nameEditor.setTabIndex(nextTabIndex++);
@@ -1622,6 +1661,7 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
         quotaEditor.setEnabled(false);
         dataCenterWithClusterEditor.setEnabled(false);
         templateEditor.setEnabled(false);
+        baseTemplateEditor.setEnabled(false);
         vmTypeEditor.setEnabled(false);
     }
 

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/AbstractVmPopupWidget.ui.xml
@@ -356,7 +356,8 @@
 			<g:FlowPanel addStyleNames="{style.headerPanel}">
 				<e:ListModelTypeAheadListBoxEditor ui:field="dataCenterWithClusterEditor" />
 				<e:ListModelTypeAheadListBoxEditor ui:field="quotaEditor" />
-				<e:ListModelTypeAheadListBoxEditor ui:field="templateEditor"/>
+				<e:ListModelTypeAheadListBoxEditor ui:field="baseTemplateEditor"/>
+				<e:ListModelTypeAheadListBoxEditor ui:field="templateEditor" />
 				<e:ListModelListBoxEditor ui:field="oSTypeEditor" />
 				<e:ListModelListBoxEditor ui:field="vmTypeEditor" />
 			</g:FlowPanel>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/vm/VmMakeTemplatePopupWidget.java
@@ -214,7 +214,7 @@ public class VmMakeTemplatePopupWidget extends AbstractModelBoundPopupWidget<Uni
         copyVmPermissions.setLabel(constants.copyVmPermissions());
         disksAllocationLabel.setText(constants.disksAllocation());
         isSubTemplateEditor.setLabel(constants.createAsSubTemplate());
-        baseTemplateEditor.setLabel(constants.baseTemplate());
+        baseTemplateEditor.setLabel(constants.rootTemplate());
         templateVersionNameEditor.setLabel(constants.templateVersionName());
     }
 

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/pools/PoolListModel.java
@@ -51,9 +51,11 @@ import org.ovirt.engine.ui.uicompat.IEventListener;
 import org.ovirt.engine.ui.uicompat.IFrontendMultipleActionAsyncCallback;
 import org.ovirt.engine.ui.uicompat.ObservableCollection;
 import org.ovirt.engine.ui.uicompat.PropertyChangedEventArgs;
+import org.ovirt.engine.ui.uicompat.UIConstants;
 
 public class PoolListModel extends ListWithDetailsModel implements ISupportSystemTreeContext
 {
+    private final UIConstants constants = ConstantsManager.getInstance().getConstants();
 
     private UICommand privateNewCommand;
 
@@ -504,6 +506,8 @@ public class PoolListModel extends ListWithDetailsModel implements ISupportSyste
                         vm.setVmType(model.getVmType().getSelectedItem());
                         vm.setAllowConsoleReconnect(model.getAllowConsoleReconnect().getEntity());
                         vm.setVmInit(model.getVmInitModel().buildCloudInitParameters(model));
+
+                        vm.setUseLatestVersion(constants.latestTemplateVersionName().equals(model.getTemplate().getSelectedItem().getTemplateVersionName()));
 
                         AddVmPoolWithVmsParameters param =
                                 new AddVmPoolWithVmsParameters(pool, vm, model.getNumOfDesktops().getEntity(), 0);

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/userportal/UserPortalListModel.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.ovirt.engine.core.common.VdcActionUtils;
 import org.ovirt.engine.core.common.action.AddVmFromScratchParameters;
 import org.ovirt.engine.core.common.action.AddVmFromTemplateParameters;
@@ -82,9 +83,11 @@ import org.ovirt.engine.ui.uicompat.IFrontendActionAsyncCallback;
 import org.ovirt.engine.ui.uicompat.IFrontendMultipleQueryAsyncCallback;
 import org.ovirt.engine.ui.uicompat.ObservableCollection;
 import org.ovirt.engine.ui.uicompat.PropertyChangedEventArgs;
+import org.ovirt.engine.ui.uicompat.UIConstants;
 
 public class UserPortalListModel extends AbstractUserPortalListModel {
 
+    private final UIConstants constants = ConstantsManager.getInstance().getConstants();
     public static final EventDefinition searchCompletedEventDefinition;
     private Event privateSearchCompletedEvent;
 
@@ -1134,7 +1137,10 @@ public class UserPortalListModel extends AbstractUserPortalListModel {
                             UserPortalListModel userPortalListModel1 = (UserPortalListModel) model;
                             final UnitVmModel unitVmModel = (UnitVmModel) userPortalListModel1.getWindow();
 
-                            AddVmFromTemplateParameters param = new AddVmFromTemplateParameters(gettempVm(),
+                            VM vm = gettempVm();
+                            vm.setUseLatestVersion(constants.latestTemplateVersionName().equals(unitVmModel.getTemplate().getSelectedItem().getTemplateVersionName()));
+
+                            AddVmFromTemplateParameters param = new AddVmFromTemplateParameters(vm,
                                     unitVmModel.getDisksAllocationModel().getImageToDestinationDomainMap(),
                                     Guid.Empty);
                             param.setMakeCreatorExplicitOwner(true);
@@ -1149,7 +1155,10 @@ public class UserPortalListModel extends AbstractUserPortalListModel {
                 }
                 else
                 {
-                    VmManagementParametersBase param = new VmManagementParametersBase(gettempVm());
+                    VM vm = gettempVm();
+                    vm.setUseLatestVersion(constants.latestTemplateVersionName().equals(model.getTemplate().getSelectedItem().getTemplateVersionName()));
+
+                    VmManagementParametersBase param = new VmManagementParametersBase(vm);
                     param.setDiskInfoDestinationMap(model.getDisksAllocationModel().getImageToDestinationDomainMap());
                     param.setMakeCreatorExplicitOwner(true);
                     param.setCopyTemplatePermissions(model.getCopyPermissions().getEntity());
@@ -1170,20 +1179,27 @@ public class UserPortalListModel extends AbstractUserPortalListModel {
                         new IFrontendActionAsyncCallback() {
                             @Override
                             public void executed(FrontendActionAsyncResult result) {
-                                VmManagementParametersBase param = new VmManagementParametersBase(gettempVm());
+                                VM vm = gettempVm();
+                                vm.setUseLatestVersion(constants.latestTemplateVersionName().equals(model.getTemplate().getSelectedItem().getTemplateVersionName()));
+
+                                VmManagementParametersBase param = new VmManagementParametersBase(vm);
                                 param.setSoundDeviceEnabled(model.getIsSoundcardEnabled().getEntity());
                                 param.setConsoleEnabled(model.getIsConsoleDeviceEnabled().getEntity());
 
-                                Frontend.getInstance().runAction(VdcActionType.UpdateVm, param, new UnitVmModelNetworkAsyncCallback(model, defaultNetworkCreatingManager, gettempVm().getId()), this);
+                                Frontend.getInstance().runAction(VdcActionType.UpdateVm, param, new UnitVmModelNetworkAsyncCallback(model, defaultNetworkCreatingManager, vm.getId()), this);
                             }
                         }, this);
             }
             else
             {
-                VmManagementParametersBase param = new VmManagementParametersBase(gettempVm());
+                VM vm = gettempVm();
+                vm.setUseLatestVersion(constants.latestTemplateVersionName().equals(model.getTemplate().getSelectedItem().getTemplateVersionName()));
+
+                VmManagementParametersBase param = new VmManagementParametersBase(vm);
                 param.setSoundDeviceEnabled(model.getIsSoundcardEnabled().getEntity());
                 param.setConsoleEnabled(model.getIsConsoleDeviceEnabled().getEntity());
-                Frontend.getInstance().runAction(VdcActionType.UpdateVm, param, new UnitVmModelNetworkAsyncCallback(model, defaultNetworkCreatingManager, gettempVm().getId()), this);
+
+                Frontend.getInstance().runAction(VdcActionType.UpdateVm, param, new UnitVmModelNetworkAsyncCallback(model, defaultNetworkCreatingManager, vm.getId()), this);
             }
         }
     }

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/CloneVmFromSnapshotModelBehavior.java
@@ -1,46 +1,15 @@
 package org.ovirt.engine.ui.uicommonweb.models.vms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.ovirt.engine.core.common.businessentities.DiskImage;
-import org.ovirt.engine.core.common.businessentities.VmTemplate;
-import org.ovirt.engine.ui.frontend.AsyncQuery;
-import org.ovirt.engine.ui.frontend.INewAsyncCallback;
 import org.ovirt.engine.ui.uicommonweb.Linq;
-import org.ovirt.engine.ui.uicommonweb.dataprovider.AsyncDataProvider;
-import org.ovirt.engine.ui.uicommonweb.models.SystemTreeItemModel;
 
 @SuppressWarnings("unused")
 public class CloneVmFromSnapshotModelBehavior extends ExistingVmModelBehavior
 {
     public CloneVmFromSnapshotModelBehavior() {
         super(null);
-    }
-
-    @Override
-    public void initialize(SystemTreeItemModel systemTreeSelectedItem)
-    {
-        super.initialize(systemTreeSelectedItem);
-    }
-
-    public void initTemplate()
-    {
-        AsyncDataProvider.getTemplateById(new AsyncQuery(getModel(),
-                new INewAsyncCallback() {
-                    @Override
-                    public void onSuccess(Object target, Object returnValue) {
-
-                        UnitVmModel model = (UnitVmModel) target;
-                        VmTemplate template = (VmTemplate) returnValue;
-                        model.getTemplate()
-                                .setItems(new ArrayList<VmTemplate>(Arrays.asList(new VmTemplate[] { template })));
-                        model.getTemplate().setSelectedItem(template);
-                        model.getTemplate().setIsChangable(false);
-                    }
-                },
-                getModel().getHash()),
-                vm.getVmtGuid());
     }
 
     @Override

commit 80892e3dd7ec75c9114ec87cdf69d3275fcde7b4

webadmin: new pool dialog should not contain Blank template
This regression has been introduced by:
604d22dc036d1f5a4431c8247ce246d0c0a0c109

Fixed by moving back the code to filter out the Blank template from the new
pool dialog.

Change-Id: I6acdd3427e5c11b854bfd4c9c209c55da12b88a5
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1066646
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/NewPoolModelBehavior.java
@@ -1,5 +1,6 @@
 package org.ovirt.engine.ui.uicommonweb.models.vms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ovirt.engine.core.common.businessentities.DisplayType;
@@ -7,6 +8,7 @@ import org.ovirt.engine.core.common.businessentities.StoragePool;
 import org.ovirt.engine.core.common.businessentities.VDSGroup;
 import org.ovirt.engine.core.common.businessentities.VmBase;
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
+import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.ui.frontend.AsyncQuery;
 import org.ovirt.engine.ui.frontend.INewAsyncCallback;
 import org.ovirt.engine.ui.uicommonweb.dataprovider.AsyncDataProvider;
@@ -39,8 +41,7 @@ public class NewPoolModelBehavior extends PoolModelBehaviorBase {
     public void postDataCenterWithClusterSelectedItemChanged() {
         super.postDataCenterWithClusterSelectedItemChanged();
 
-        final DataCenterWithCluster dataCenterWithCluster =
-                (DataCenterWithCluster) getModel().getDataCenterWithClustersList().getSelectedItem();
+        final DataCenterWithCluster dataCenterWithCluster = getModel().getDataCenterWithClustersList().getSelectedItem();
         StoragePool dataCenter = getModel().getSelectedDataCenter();
         if (dataCenter == null) {
             return;
@@ -57,7 +58,14 @@ public class NewPoolModelBehavior extends PoolModelBehaviorBase {
                         AsyncDataProvider.filterTemplatesByArchitecture(baseTemplates,
                                 dataCenterWithCluster.getCluster().getArchitecture());
 
-                getModel().getBaseTemplate().setItems(filteredTemplates);
+                List<VmTemplate> templatesWithoutBlank = new ArrayList<VmTemplate>();
+                for (VmTemplate template : filteredTemplates) {
+                    if (!template.getId().equals(Guid.Empty)) {
+                        templatesWithoutBlank.add(template);
+                    }
+                }
+
+                getModel().getBaseTemplate().setItems(templatesWithoutBlank);
             }
         }), dataCenter.getId());
     }