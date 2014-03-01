2e6d71ed98dcda346ca8755a57773ef004aab813
Alon Bar-Lev <alonbl@redhat.com>
2014-03-01 20:10:00 +0200

    db: cleanup: remove unused fn_db_set_dbobjects_ownership
    Change-Id: I0a87b65a237527a84ff2a9382781e5f48923008e
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

--- packaging/dbscripts/dbcustomfunctions.sh
+++ packaging/dbscripts/dbcustomfunctions.sh
@@ -44,29 +44,6 @@ refresh_views() {
     execute_file "create_dwh_views.sql" ${DATABASE} ${SERVERNAME} ${PORT} > /dev/null
 }
 
-fn_db_set_dbobjects_ownership() {
-    cmd="select c.relname \
-         from   pg_class c join pg_roles r on r.oid = c.relowner join pg_namespace n on n.oid = c.relnamespace \
-         where  c.relkind in ('r','v','S') \
-         and    n.nspname = 'public' and r.rolname != '${DBOBJECT_OWNER}';"
-    res=$(execute_command "${cmd}" engine ${SERVERNAME} ${PORT})
-    if [ -n "${res}" ]; then
-        cmd=""
-        for tab in $(echo $res); do
-            cmd=${cmd}"alter table $tab owner to ${DBOBJECT_OWNER}; "
-        done
-        if [ -n "${cmd}" ]; then
-            echo -n "Changing ownership of objects in database '$DATABASE' to owner '$DBOBJECT_OWNER' ... "
-            res=$(execute_command "${cmd}" engine ${SERVERNAME} ${PORT})
-            if [ $? -eq 0 ]; then
-                echo "completed successfully."
-            else
-                return 1
-            fi
-        fi
-    fi
-}
-
 # Materilized views functions, override with empty implementation on DBs that not supporting that
 
 install_materialized_views_func() {
