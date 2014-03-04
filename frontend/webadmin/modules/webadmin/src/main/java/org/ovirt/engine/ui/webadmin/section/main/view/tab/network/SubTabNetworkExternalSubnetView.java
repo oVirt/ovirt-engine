28d8b6edd41a0786d6caba08bdd7e47aa3cc68fc
Mike Kolesnik <mkolesni@redhat.com>
2014-03-04 10:39:24 +0200

    webadmin: Add gateway & DNS servers columns
    Added the columns to the external subnet sub-tab under a network, in
    order to reflect to the user what are the current settings of these
    fields.
    
    Change-Id: I794ee197f31ef89f51f27074d5a8f2a1ca5e51d6
    Bug-Url: https://bugzilla.redhat.com/1064749
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/network/SubTabNetworkExternalSubnetView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/network/SubTabNetworkExternalSubnetView.java
@@ -1,14 +1,15 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.tab.network;
 
-import org.ovirt.engine.core.common.businessentities.network.NetworkView;
 import org.ovirt.engine.core.common.businessentities.network.ExternalSubnet;
 import org.ovirt.engine.core.common.businessentities.network.ExternalSubnet.IpVersion;
+import org.ovirt.engine.core.common.businessentities.network.NetworkView;
 import org.ovirt.engine.ui.common.uicommon.model.SearchableDetailModelProvider;
 import org.ovirt.engine.ui.common.widget.table.column.EnumColumn;
 import org.ovirt.engine.ui.common.widget.table.column.TextColumnWithTooltip;
 import org.ovirt.engine.ui.uicommonweb.UICommand;
-import org.ovirt.engine.ui.uicommonweb.models.networks.NetworkListModel;
 import org.ovirt.engine.ui.uicommonweb.models.networks.NetworkExternalSubnetListModel;
+import org.ovirt.engine.ui.uicommonweb.models.networks.NetworkListModel;
+import org.ovirt.engine.ui.uicompat.external.StringUtils;
 import org.ovirt.engine.ui.webadmin.ApplicationConstants;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.tab.network.SubTabNetworkExternalSubnetPresenter;
 import org.ovirt.engine.ui.webadmin.section.main.view.AbstractSubTabTableView;
@@ -37,7 +38,7 @@ public class SubTabNetworkExternalSubnetView extends AbstractSubTabTableView<Net
                         return object.getName();
                     }
                 };
-        getTable().addColumn(nameColumn, constants.nameExternalSubnet(), "300px"); //$NON-NLS-1$
+        getTable().addColumn(nameColumn, constants.nameExternalSubnet(), "200px"); //$NON-NLS-1$
 
         TextColumnWithTooltip<ExternalSubnet> cidrColumn =
                 new TextColumnWithTooltip<ExternalSubnet>() {
@@ -46,7 +47,7 @@ public class SubTabNetworkExternalSubnetView extends AbstractSubTabTableView<Net
                 return object.getCidr();
             }
         };
-        getTable().addColumn(cidrColumn, constants.cidrExternalSubnet(), "200px"); //$NON-NLS-1$
+        getTable().addColumn(cidrColumn, constants.cidrExternalSubnet(), "100px"); //$NON-NLS-1$
 
         TextColumnWithTooltip<ExternalSubnet> ipVersionColumn =
                 new EnumColumn<ExternalSubnet, IpVersion>() {
@@ -55,7 +56,27 @@ public class SubTabNetworkExternalSubnetView extends AbstractSubTabTableView<Net
                 return object.getIpVersion();
             }
         };
-        getTable().addColumn(ipVersionColumn, constants.ipVersionExternalSubnet(), "100px"); //$NON-NLS-1$
+        getTable().addColumn(ipVersionColumn, constants.ipVersionExternalSubnet(), "80px"); //$NON-NLS-1$
+
+        TextColumnWithTooltip<ExternalSubnet> gatewayColumn =
+                new TextColumnWithTooltip<ExternalSubnet>() {
+                    @Override
+                    public String getValue(ExternalSubnet object) {
+                        return object.getGateway();
+                    }
+                };
+
+        getTable().addColumn(gatewayColumn, constants.gatewayExternalSubnet(), "100px"); //$NON-NLS-1$
+
+        TextColumnWithTooltip<ExternalSubnet> dnsServersColumn =
+                new TextColumnWithTooltip<ExternalSubnet>() {
+                    @Override
+                    public String getValue(ExternalSubnet object) {
+                        return StringUtils.join(object.getDnsServers(), ", "); //$NON-NLS-1$
+                    }
+                };
+
+        getTable().addColumn(dnsServersColumn, constants.dnsServersExternalSubnet(), "200px"); //$NON-NLS-1$
 
         TextColumnWithTooltip<ExternalSubnet> externalIdColumn =
                 new TextColumnWithTooltip<ExternalSubnet>() {
