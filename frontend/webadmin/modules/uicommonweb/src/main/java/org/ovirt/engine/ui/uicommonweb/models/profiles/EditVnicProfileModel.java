2ccd397c246be10f141599e8734a61082f0da3bb
Alona Kaplan <alkaplan@redhat.com>
2014-03-05 09:26:27 +0200

    webadmin: Edit vNic Profile- disable pm if the profile is used by a vm
    Port mirroring checkbox should be disabled if the VNIC profile
    is used by a VM.
    
    Bug-Url: https://bugzilla.redhat.com/1031553
    Change-Id: I06b851dcfbae9cd1ba48fda20aa0d0db6c3cb6cc
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/profiles/EditVnicProfileModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/profiles/EditVnicProfileModel.java
@@ -1,9 +1,18 @@
 package org.ovirt.engine.ui.uicommonweb.models.profiles;
 
+import java.util.Collection;
+
 import org.ovirt.engine.core.common.action.VdcActionType;
+import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.network.VnicProfile;
+import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.common.queries.VdcQueryReturnValue;
+import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.Version;
+import org.ovirt.engine.ui.frontend.AsyncQuery;
+import org.ovirt.engine.ui.frontend.Frontend;
+import org.ovirt.engine.ui.frontend.INewAsyncCallback;
 import org.ovirt.engine.ui.uicommonweb.help.HelpTag;
 import org.ovirt.engine.ui.uicommonweb.models.EntityModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.key_value.KeyValueModel;
@@ -27,6 +36,8 @@ public class EditVnicProfileModel extends VnicProfileModel {
         getDescription().setEntity(profile.getDescription());
         getPortMirroring().setEntity(getProfile().isPortMirroring());
         getPublicUse().setIsAvailable(false);
+
+        updatePortMirroringChangability();
     }
 
     public EditVnicProfileModel(EntityModel sourceModel, Version dcCompatibilityVersion, VnicProfile profile, Guid dcId) {
@@ -46,5 +57,30 @@ public class EditVnicProfileModel extends VnicProfileModel {
     @Override
     protected VdcActionType getVdcActionType() {
         return VdcActionType.UpdateVnicProfile;
+    }
+
+    private void updatePortMirroringChangability() {
+        AsyncQuery asyncQuery = new AsyncQuery();
+        asyncQuery.asyncCallback = new INewAsyncCallback() {
+            @Override
+            public void onSuccess(Object model, Object ReturnValue)
+            {
+                Collection<VM> vms = (Collection<VM>) ((VdcQueryReturnValue) ReturnValue).getReturnValue();
+                if (vms != null && !vms.isEmpty()) {
+                    getPortMirroring().setChangeProhibitionReason(ConstantsManager.getInstance()
+                            .getConstants()
+                            .portMirroringNotChangedIfUsedByVms());
+                    getPortMirroring().setIsChangable(false);
+                }
+                stopProgress();
+            }
+        };
+
+        IdQueryParameters params =
+                new IdQueryParameters(getProfile().getId());
+        startProgress(null);
+        Frontend.getInstance().runQuery(VdcQueryType.GetVmsByVnicProfileId,
+                params,
+                asyncQuery);
     }
 }
