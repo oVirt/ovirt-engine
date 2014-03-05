e046ae9a00a2b2ef2af64656274b4a6c116bdee6
Leonardo Bianconi <leonardo.bianconi@eldorado.org.br>
2014-03-05 15:19:38 -0300

    core: Enable balloon by default - Patch 2 of 2
    The "New VM"/"New Pool" dialogs in the frontend were changed to enable
    the balloon device in guest OSes where the "balloon.enabled" osinfo
    property is enabled. This way the system administrator will have an
    usable VM by default even in problematic guest OSes.
    
    Change-Id: I7803e45f07539090e5527ebc81bbc6c31df4e120
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1069858
    Signed-off-by: Gustavo Pedrosa <gustavo.pedrosa@eldorado.org.br>
    Signed-off-by: Leonardo Bianconi <leonardo.bianconi@eldorado.org.br>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/UnitVmModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/UnitVmModel.java
@@ -1726,6 +1726,7 @@ public class UnitVmModel extends Model {
             {
                 dataCenterWithClusterSelectedItemChanged(sender, args);
                 updateDisplayProtocol();
+                updateMemoryBalloonDevice();
                 initUsbPolicy();
             }
             else if (sender == getTemplate())
@@ -1747,6 +1748,7 @@ public class UnitVmModel extends Model {
                 getBehavior().oSType_SelectedItemChanged();
                 getVmInitModel().osTypeChanged(getOSType().getSelectedItem());
                 updateDisplayProtocol();
+                updateMemoryBalloonDevice();
                 initUsbPolicy();
 
                 getBehavior().activateInstanceTypeManager();
@@ -2038,6 +2040,28 @@ public class UnitVmModel extends Model {
             getDisplayProtocol().setItems(displayProtocolOptions, oldDisplayProtocolEntity);
         } else if (displayProtocolOptions.size() > 0) {
             getDisplayProtocol().setItems(displayProtocolOptions, displayProtocolOptions.iterator().next());
+        }
+    }
+
+    private void updateMemoryBalloonDevice() {
+
+        VDSGroup cluster = getSelectedCluster();
+        Integer osType = getOSType().getSelectedItem();
+
+        if (cluster == null || osType == null) {
+            return;
+        }
+
+        boolean isBalloonEnabled = AsyncDataProvider.isBalloonEnabled(osType,
+                        cluster.getcompatibility_version());
+
+        getMemoryBalloonDeviceEnabled().setIsChangable(isBalloonEnabled);
+
+        getMemoryBalloonDeviceEnabled().setEntity(isBalloonEnabled);
+        if (!isBalloonEnabled) {
+            getBehavior().deactivateInstanceTypeManager();
+            getMemoryBalloonDeviceEnabled().setEntity(isBalloonEnabled);
+            getBehavior().activateInstanceTypeManager();
         }
     }
 
