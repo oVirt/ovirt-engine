00dde76d06de43f93333544da830a5d781dcc2e7
Eli Mesika <emesika@redhat.com>
2014-03-05 12:21:36 +0200

    core: adding missing default to a non-null column
    This patch fixes an issue in event_subscriber table that was added a
    notification_method column which does not allow nulls.
    
    If the table has any records before the upgrade, this will fail since
    the existing records will have NULL for the new added column.
    
    Change-Id: Id3c7ce30c32b9b38f08ff6c8c0eb8bfa02cf5066
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1072867
    Signed-off-by: Eli Mesika <emesika@redhat.com>

--- packaging/dbscripts/upgrade/03_05_0050_event_notification_methods.sql
+++ packaging/dbscripts/upgrade/03_05_0050_event_notification_methods.sql
@@ -3,14 +3,12 @@ ALTER TABLE event_subscriber DROP CONSTRAINT fk_event_subscriber_event_notificat
 DROP TABLE event_notification_methods ;
 
 -- save EventNotificationMethod as string with constraint.
-ALTER TABLE event_subscriber ADD COLUMN notification_method CHARACTER VARYING(32)
+ALTER TABLE event_subscriber ADD COLUMN notification_method CHARACTER VARYING(32) DEFAULT 'EMAIL'
   CHECK (notification_method IN ('EMAIL', 'SNMP_TRAP'));
 ALTER TABLE event_subscriber DROP CONSTRAINT pk_event_subscriber;
 ALTER TABLE event_subscriber
   ADD CONSTRAINT pk_event_subscriber PRIMARY KEY (subscriber_id, event_up_name, notification_method, tag_name);
 ALTER TABLE event_subscriber DROP COLUMN method_id;
-
-UPDATE event_subscriber SET notification_method = 'EMAIL';
 
 ALTER TABLE event_subscriber ALTER notification_method SET NOT NULL;
 
