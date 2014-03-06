oVirt/ovirt-engine

commit 53074bc0614da9d4ef18b83bd8ba12e463c3ec09

tools: Match transport names with EventNotificationMethod
Fixes different names in Smtp/Snmp.getName() and
EventNotificationMethod values which represents transport names in db.
If those names differ (current state), no events are sent through
notifier.

Change-Id: Ie93d2d371a6f29a33e32a776e74ada5f221cbb4e
Bug-Url: https://bugzilla.redhat.com/1071536
Signed-off-by: Martin Perina <mperina@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/EventNotificationMethod.java
@@ -1,6 +1,32 @@
 package org.ovirt.engine.core.common;
 
 public enum EventNotificationMethod {
-    EMAIL,
-    SNMP_TRAP
+    SMTP("smtp"),
+    SNMP("snmp");
+
+    /**
+     * External string representation (database, text configuration)
+     */
+    private String value;
+
+    /**
+     * Sets external string representation as lowercase name
+     */
+    private EventNotificationMethod(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Return string representation (database, configuration
+     */
+    public String getAsString() {
+        return value;
+    }
+
+    /**
+     * Returns enum value based on external string representation
+     */
+    public static EventNotificationMethod valueOfString(String value) {
+        return valueOf(value != null ? value.toUpperCase() : value);
+    }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/EventSubscriptionCommandBase.java
@@ -52,7 +52,7 @@ public abstract class EventSubscriptionCommandBase<T extends EventSubscriptionPa
         EventNotificationMethod notificationMethod = eventNotificationMethod;
 
         switch (notificationMethod) {
-        case EMAIL:
+        case SMTP:
             String mailAdress = (StringUtils.isEmpty(event_subscriber.getmethod_address())) ? user.getEmail()
                     : event_subscriber.getmethod_address();
 

backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/EventDAODbFacadeImpl.java
@@ -25,7 +25,7 @@ public class EventDAODbFacadeImpl extends BaseDAODbFacade implements EventDAO {
                 throws SQLException {
             event_subscriber entity = new event_subscriber();
             entity.setevent_up_name(rs.getString("event_up_name"));
-            entity.setevent_notification_method(EventNotificationMethod.valueOf(rs.getString("notification_method")));
+            entity.setevent_notification_method(EventNotificationMethod.valueOfString(rs.getString("notification_method")));
             entity.setmethod_address(rs.getString("method_address"));
             entity.setsubscriber_id(getGuidDefaultEmpty(rs, "subscriber_id"));
             entity.settag_name(rs.getString("tag_name"));
@@ -59,7 +59,7 @@ public class EventDAODbFacadeImpl extends BaseDAODbFacade implements EventDAO {
     public void subscribe(event_subscriber subscriber) {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
                 .addValue("event_up_name", subscriber.getevent_up_name())
-                .addValue("notification_method", subscriber.getevent_notification_method().name())
+                .addValue("notification_method", subscriber.getevent_notification_method().getAsString())
                 .addValue("method_address", subscriber.getmethod_address())
                 .addValue("subscriber_id", subscriber.getsubscriber_id())
                 .addValue("tag_name", subscriber.gettag_name());
@@ -71,7 +71,7 @@ public class EventDAODbFacadeImpl extends BaseDAODbFacade implements EventDAO {
     public void unsubscribe(event_subscriber subscriber) {
         MapSqlParameterSource parameterSource = getCustomMapSqlParameterSource()
                 .addValue("event_up_name", subscriber.getevent_up_name())
-                .addValue("notification_method", subscriber.getevent_notification_method().name())
+                .addValue("notification_method", subscriber.getevent_notification_method().getAsString())
                 .addValue("subscriber_id", subscriber.getsubscriber_id())
                 .addValue("tag_name", subscriber.gettag_name());
 

backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/EventDAOTest.java
@@ -32,7 +32,7 @@ public class EventDAOTest extends BaseDAOTestCase {
         newSubscriber = new Guid("9bf7c640-b620-456f-a550-0348f366544b");
         newSubscription = new event_subscriber();
         newSubscription.setsubscriber_id(newSubscriber);
-        newSubscription.setevent_notification_method(EventNotificationMethod.EMAIL);
+        newSubscription.setevent_notification_method(EventNotificationMethod.SMTP);
         newSubscription.setevent_up_name("TestRun");
         newSubscription.settag_name("farkle");
 

backend/manager/modules/dal/src/test/resources/fixtures.xml
@@ -4069,7 +4069,7 @@
         <row>
             <value>9bf7c640-b620-456f-a550-0348f366544a</value>
             <value>TestRun</value>
-            <value>EMAIL</value>
+            <value>smtp</value>
             <null />
             <value>testrun</value>
         </row>

backend/manager/tools/src/main/java/org/ovirt/engine/core/notifier/NotificationService.java
@@ -118,7 +118,7 @@ public class NotificationService implements Runnable {
                         new FirstMatchSimpleFilter.FilterEntry(
                                 EventsManager.DATABASE_UNREACHABLE,
                                 false,
-                                EventNotificationMethod.EMAIL.name(),
+                                EventNotificationMethod.SMTP.getAsString(),
                                 subscriber);
                     }
                 }

backend/manager/tools/src/main/java/org/ovirt/engine/core/notifier/transport/smtp/Smtp.java
@@ -172,14 +172,14 @@ public class Smtp extends Transport {
                                 message.getMessageBody()));
                     }
                     sendMail(attempt.address, message.getMessageSubject(), message.getMessageBody());
-                    notifyObservers(DispatchResult.success(attempt.event, attempt.address, EventNotificationMethod.EMAIL));
+                    notifyObservers(DispatchResult.success(attempt.event, attempt.address, EventNotificationMethod.SMTP));
                     iterator.remove();
                 } catch (Exception ex) {
                     attempt.retries++;
                     if (attempt.retries >= retries) {
                         notifyObservers(DispatchResult.failure(attempt.event,
                                 attempt.address,
-                                EventNotificationMethod.EMAIL,
+                                EventNotificationMethod.SMTP,
                                 ex.getMessage()));
                         iterator.remove();
                     }

backend/manager/tools/src/main/java/org/ovirt/engine/core/notifier/transport/snmp/Snmp.java
@@ -131,10 +131,10 @@ public class Snmp extends Transport {
                     )
                 );
                 snmp.send(v2pdu, target);
-                notifyObservers(DispatchResult.success(event, address, EventNotificationMethod.SNMP_TRAP));
+                notifyObservers(DispatchResult.success(event, address, EventNotificationMethod.SNMP));
             } catch (Exception e) {
                 log.error(e.getMessage());
-                notifyObservers(DispatchResult.failure(event, address, EventNotificationMethod.SNMP_TRAP, e.getMessage()));
+                notifyObservers(DispatchResult.failure(event, address, EventNotificationMethod.SNMP, e.getMessage()));
             }
         }
     }

commit afef2143d42da9eb1cf6acc9c202c870fadf7561

core: Fix FindBugs issue in EventNotificationMethod
Fixes FindBugs issue in EventNotificationMethod introduced in commit
53074bc0614da9d4ef18b83bd8ba12e463c3ec09

Change-Id: I3dafb358db93280d58e40ca1056230ae1dba1767
Signed-off-by: Martin Perina <mperina@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/EventNotificationMethod.java
@@ -27,6 +27,6 @@ public enum EventNotificationMethod {
      * Returns enum value based on external string representation
      */
     public static EventNotificationMethod valueOfString(String value) {
-        return valueOf(value != null ? value.toUpperCase() : value);
+        return value != null ? valueOf(value.toUpperCase()) : null;
     }
 }