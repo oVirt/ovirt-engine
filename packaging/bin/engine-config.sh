62f62104a90c673435e0bf2f20194c3c503307c0
Martin Perina <mperina@redhat.com>
2014-03-12 15:40:36 +0100

    tools: Sync engine-config man pages and help
    Synchronizes content of engine-config man page with information
    displayed by execution 'engine-config -h'.
    
    Change-Id: I16e16973cb24e7e6e3209d229e0cdabfd3ffbc84
    Bug-Url: https://bugzilla.redhat.com/1074299
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- packaging/bin/engine-config.sh
+++ packaging/bin/engine-config.sh
@@ -9,38 +9,48 @@
 
 usage () {
 	cat << __EOF__
-engine-config: get/set/list configuration
-USAGE:
-        engine-config ACTION [--cver=version] [-p | --properties=/path/to/alternate/property/file] [-c | --config=/path/to/alternate/config/file]
-Where:
-        ACTION              action to perform, see details below
-        version             relevant configuration version to use.
-        -p, --properties=   (optional) use the given alternate properties file.
-        -c, --config=       (optional) use the given alternate configuration file.
+Usage: engine-config <action> [<args>]
 
-        Available actions:
-        -l, --list
-                list available configuration keys.
-        -a, --all
-                get all available configuration values.
-        -g key, --get=key [--cver=version]
-                get the value of the given key for the given version. If a version is not given, the values of all existing versions are returned.
-        -s key=val [--cver=version], --set key=val [--cver=version]
-                set the value of the given key for the given version. The cver version is required for this action only when the version is not 'general'.
-        -h, --help
-                display this help and exit.
+AVAILABLE ACTIONS
+       -l, --list
+           List available configuration keys.
 
-### Notes:
-### 1. Passwords: password can be set in interactive mode ie:
-###        engine-config -s PasswordEntry=interactive
-###    or via file with one of the following options:
-###        engine-config -s PasswordEntry --admin-pass-file=/tmp/mypass
-###        engine-config -s PasswordEntry=/tmp/mypass
-###    PasswordEntry varies between the different password options
-###    See engine-config -h <OptionName> for more specific details
-### 2. In order for your change(s) to take effect,
-###    restart the oVirt engine service (using: 'service ovirt-engine restart').
-################################################################################
+       -a, --all
+           Get all available configuration values.
+
+       -g KEY, --get=KEY
+           Get the value of the given key for the given version. If a version is not given, the values of all existing versions are returned.
+
+       -s KEY=VALUE, --set KEY=VALUE
+           Set the value of the given key for the given version. The version is required for this action only when the version is not 'general'.
+
+       -h, --help
+           Show this help message and exit.
+
+OPTIONS
+       --cver=VERSION
+           Relevant configuration version to use.
+
+       -p PROP_FILE, --properties=PROP_FILE
+           Use the given alternate properties file.
+
+       -c CFG_FILE, --config=CFG_FILE
+           Use the given alternate configuration file.
+
+SETTING PASSWORDS
+       Passwords can be set in interactive mode:
+
+           engine-config -s PasswordEntry=interactive
+
+       or via file with one of the following options:
+
+           engine-config -s PasswordEntry --admin-pass-file=/tmp/mypass
+           engine-config -s PasswordEntry=/tmp/mypass
+
+       PasswordEntry varies between the different password options.
+
+NOTE
+       In order for your change(s) to take effect, restart the oVirt engine.
 __EOF__
 	return 0
 }
