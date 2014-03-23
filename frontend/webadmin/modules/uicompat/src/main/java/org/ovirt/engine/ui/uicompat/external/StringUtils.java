f1f60d76ae493c8f895a52a90b3ee010c775ce69
Mike Kolesnik <mkolesni@redhat.com>
2014-03-23 17:58:15 +0200

    webadmin: Add StringUtils.is{,Not}Empty
    Methods added to the extrenal class until StringUtils is supported
    normally.
    
    Conflicts:
    	frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/external/StringUtils.java
    
    Change-Id: Ia26668e73a42523f412b07ca3bcc8ca0578aa550
    Bug-Url: https://bugzilla.redhat.com/1064749
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>
    (cherry picked from commit 0200a000c630e5f083a27deb182ac7b9830fbd61)

--- frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/external/StringUtils.java
+++ frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/external/StringUtils.java
@@ -281,6 +281,53 @@ public class StringUtils {
     }
 
     /**
+     * <p>
+     * Checks if a String is empty ("") or null.
+     * </p>
+     *
+     * <pre>
+     * StringUtils.isEmpty(null)      = true
+     * StringUtils.isEmpty("")        = true
+     * StringUtils.isEmpty(" ")       = false
+     * StringUtils.isEmpty("bob")     = false
+     * StringUtils.isEmpty("  bob  ") = false
+     * </pre>
+     *
+     * <p>
+     * NOTE: This method changed in Lang version 2.0. It no longer trims the String. That functionality is available in
+     * isBlank().
+     * </p>
+     *
+     * @param str
+     *            the String to check, may be null
+     * @return <code>true</code> if the String is empty or null
+     */
+    public static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+
+    /**
+     * <p>
+     * Checks if a String is not empty ("") and not null.
+     * </p>
+     *
+     * <pre>
+     * StringUtils.isNotEmpty(null)      = false
+     * StringUtils.isNotEmpty("")        = false
+     * StringUtils.isNotEmpty(" ")       = true
+     * StringUtils.isNotEmpty("bob")     = true
+     * StringUtils.isNotEmpty("  bob  ") = true
+     * </pre>
+     *
+     * @param str
+     *            the String to check, may be null
+     * @return <code>true</code> if the String is not empty and not null
+     */
+    public static boolean isNotEmpty(String str) {
+        return !StringUtils.isEmpty(str);
+    }
+
+    /**
      * Joins the Strings of the provided array into a single String.
      *
      * @param array the array of Strings to join together
