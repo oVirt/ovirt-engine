f62e9a72cf533d307db8c4343948d45ed784856d
Arik Hadas <ahadas@redhat.com>
2014-03-24 17:23:22 +0200

    core: rerun HA VMs on automatic fence using AutoStartVmsRunner
    Before the engine sends fence request to VDSM, it tries to rerun the
    highly available VMs that ran on the host by invoking RunVmCommand
    directly. The problem is that we won't try to rerun the VM again if the
    RunVmCommand fails (and in case the host which is being fenced in the
    only one that was active in the cluster the command will fail for sure).
    The solution is to rerun the highly available VMs using
    AutoStartVmsRunner job in this flow as well.
    
    Change-Id: Ic259ff22c4b6f24b09eb973de6e31ebc51aa966b
    Bug-Url: https://bugzilla.redhat.com/1077706
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/FenceVdsBaseCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/FenceVdsBaseCommand.java
@@ -13,8 +13,6 @@ import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.job.ExecutionHandler;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.action.FenceVdsActionParameters;
-import org.ovirt.engine.core.common.action.RunVmParams;
-import org.ovirt.engine.core.common.action.VdcActionParametersBase;
 import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.businessentities.FenceActionType;
 import org.ovirt.engine.core.common.businessentities.FenceAgentOrder;
@@ -467,8 +465,7 @@ public abstract class FenceVdsBaseCommand<T extends FenceVdsActionParameters> ex
     }
 
     protected void restartVdsVms() {
-        java.util.ArrayList<VdcActionParametersBase> runVmParamsList =
-                new java.util.ArrayList<VdcActionParametersBase>();
+        List<Guid> autoStartVmIdsToRerun = new ArrayList<>();
         // restart all running vms of a failed vds.
         for (VM vm : mVmList) {
             destroyVmOnDestination(vm);
@@ -489,11 +486,11 @@ public abstract class FenceVdsBaseCommand<T extends FenceVdsActionParameters> ex
 
             // Handle highly available VMs
             if (vm.isAutoStartup()) {
-                runVmParamsList.add(new RunVmParams(vm.getId(), true));
+                autoStartVmIdsToRerun.add(vm.getId());
             }
         }
-        if (runVmParamsList.size() > 0) {
-            Backend.getInstance().runInternalMultipleActions(VdcActionType.RunVm, runVmParamsList);
+        if (!autoStartVmIdsToRerun.isEmpty()) {
+            AutoStartVmsRunner.getInstance().addVmsToRun(autoStartVmIdsToRerun);
         }
         setVm(null);
         setVmId(Guid.Empty);
