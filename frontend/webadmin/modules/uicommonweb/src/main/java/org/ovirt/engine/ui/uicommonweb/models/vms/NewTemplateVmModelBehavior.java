d1a0f63270ee0f36ac9a4952aaa7ea897fe515b1
Tomas Jelinek <tjelinek@redhat.com>
2014-04-09 09:50:28 +0200

    userportal,webadmin: validation error on create template
    It was not possible to create a new template. The problem was that:
    - UnitVmModel.validate() did not pass the CPU validation: new
      IntegerValidation(1, behavior.maxCpus) because the behavior.maxCpus was not
      inited.
    - the behavior.maxCpus was not inited because it is inited in the
      VmModelBehaviorBase.updateMaxNumOfVmCpus() which is called which is triggered
      by calling the VmModelBehaviorBase.updateNumOfSockets() which is supposed to
      be called from the specific behavior classes'
      postDataCenterWithClusterSelectedItemChanged.
    - The problem was that the
      NewTemplateVmModelBehavior.postDataCenterWithClusterSelectedItemChanged
      did not called the updateNumOfSockets()
    
    Change-Id: Ida04f94dcbe4d357f306eec60134661c43d9fcdf
    Bug-Url: https://bugzilla.redhat.com/1085754
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/NewTemplateVmModelBehavior.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/NewTemplateVmModelBehavior.java
@@ -277,6 +277,7 @@ public class NewTemplateVmModelBehavior extends VmModelBehaviorBase<UnitVmModel>
         updateVirtioScsiAvailability();
         updateOSValues();
         updateTemplate();
+        updateNumOfSockets();
     }
 
     @Override
