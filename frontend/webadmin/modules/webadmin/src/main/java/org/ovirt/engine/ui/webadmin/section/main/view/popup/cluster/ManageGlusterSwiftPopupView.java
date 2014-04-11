oVirt/ovirt-engine

commit 3067b72c2b577a6e5d3e061aef38e2e6aebef115

frontend: Remove rest of deprecated editors
Change-Id: Iea5ccf273df79c91914b095ba19275352b1a6877
Signed-off-by: Martin Betak <mbetak@redhat.com>

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/cluster/ManageGlusterSwiftPopupView.java
@@ -1,5 +1,6 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.popup.cluster;
 
+import com.google.gwt.text.shared.Parser;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterServerService;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterServiceStatus;
 import org.ovirt.engine.core.common.businessentities.gluster.ServiceType;
@@ -8,10 +9,9 @@ import org.ovirt.engine.ui.common.view.popup.AbstractModelBoundPopupView;
 import org.ovirt.engine.ui.common.widget.Align;
 import org.ovirt.engine.ui.common.widget.dialog.SimpleDialogPanel;
 import org.ovirt.engine.ui.common.widget.editor.EntityModelCellTable;
-import org.ovirt.engine.ui.common.widget.editor.EntityModelCheckBoxEditor;
-import org.ovirt.engine.ui.common.widget.editor.EntityModelLabelEditor;
-import org.ovirt.engine.ui.common.widget.editor.EntityModelRadioButtonEditor;
-import org.ovirt.engine.ui.common.widget.parser.EntityModelParser;
+import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelCheckBoxEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelLabelEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelRadioButtonEditor;
 import org.ovirt.engine.ui.common.widget.renderer.EnumRenderer;
 import org.ovirt.engine.ui.common.widget.table.column.EntityModelTextColumn;
 import org.ovirt.engine.ui.common.widget.table.column.EnumColumn;
@@ -37,6 +37,8 @@ import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.client.ui.Label;
 import com.google.inject.Inject;
 
+import java.text.ParseException;
+
 public class ManageGlusterSwiftPopupView extends AbstractModelBoundPopupView<ManageGlusterSwiftModel> implements ManageGlusterSwiftPopupPresenterWidget.ViewDef {
 
     interface Driver extends SimpleBeanEditorDriver<ManageGlusterSwiftModel, ManageGlusterSwiftPopupView> {
@@ -57,7 +59,7 @@ public class ManageGlusterSwiftPopupView extends AbstractModelBoundPopupView<Man
 
     @UiField(provided = true)
     @Path("swiftStatus.entity")
-    EntityModelLabelEditor swiftStatusEditor;
+    EntityModelLabelEditor<GlusterServiceStatus> swiftStatusEditor;
 
     @UiField(provided = true)
     @Path("startSwift.entity")
@@ -95,7 +97,12 @@ public class ManageGlusterSwiftPopupView extends AbstractModelBoundPopupView<Man
     }
 
     private void initEditors(ApplicationConstants constants) {
-        swiftStatusEditor = new EntityModelLabelEditor(new EnumRenderer(), new EntityModelParser());
+        swiftStatusEditor = new EntityModelLabelEditor<GlusterServiceStatus>(new EnumRenderer<GlusterServiceStatus>(), new Parser<GlusterServiceStatus>() {
+            @Override
+            public GlusterServiceStatus parse(CharSequence text) throws ParseException {
+                return GlusterServiceStatus.valueOf(text.toString().toUpperCase());
+            }
+        });
         startSwift = new EntityModelRadioButtonEditor("swift_action", Align.RIGHT); //$NON-NLS-1$
         stopSwift = new EntityModelRadioButtonEditor("swift_action", Align.RIGHT); //$NON-NLS-1$
         restartSwift = new EntityModelRadioButtonEditor("swift_action", Align.RIGHT); //$NON-NLS-1$
@@ -128,7 +135,7 @@ public class ManageGlusterSwiftPopupView extends AbstractModelBoundPopupView<Man
                     @Override
                     public Boolean getValue(EntityModel object) {
                         GlusterSwiftServiceModel swiftServiceModel = (GlusterSwiftServiceModel) object;
-                        return (Boolean) swiftServiceModel.getStartSwift().getEntity();
+                        return swiftServiceModel.getStartSwift().getEntity();
                     }
 
                     @Override
@@ -157,7 +164,7 @@ public class ManageGlusterSwiftPopupView extends AbstractModelBoundPopupView<Man
             @Override
             public Boolean getValue(EntityModel object) {
                 GlusterSwiftServiceModel swiftServiceModel = (GlusterSwiftServiceModel) object;
-                return (Boolean) swiftServiceModel.getStopSwift().getEntity();
+                return swiftServiceModel.getStopSwift().getEntity();
             }
 
             @Override
@@ -189,7 +196,7 @@ public class ManageGlusterSwiftPopupView extends AbstractModelBoundPopupView<Man
                     @Override
                     public Boolean getValue(EntityModel object) {
                         GlusterSwiftServiceModel swiftServiceModel = (GlusterSwiftServiceModel) object;
-                        return (Boolean) swiftServiceModel.getRestartSwift().getEntity();
+                        return swiftServiceModel.getRestartSwift().getEntity();
                     }
 
                     @Override

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/gluster/AddBrickPopupView.java
@@ -1,5 +1,6 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.popup.gluster;
 
+import com.google.gwt.text.shared.Parser;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterBrickEntity;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeType;
@@ -10,11 +11,11 @@ import org.ovirt.engine.ui.common.widget.Align;
 import org.ovirt.engine.ui.common.widget.UiCommandButton;
 import org.ovirt.engine.ui.common.widget.dialog.SimpleDialogPanel;
 import org.ovirt.engine.ui.common.widget.editor.EntityModelCellTable;
-import org.ovirt.engine.ui.common.widget.editor.EntityModelCheckBoxEditor;
-import org.ovirt.engine.ui.common.widget.editor.EntityModelLabelEditor;
-import org.ovirt.engine.ui.common.widget.editor.EntityModelTextBoxEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelCheckBoxEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelLabelEditor;
 import org.ovirt.engine.ui.common.widget.editor.ListModelListBoxEditor;
-import org.ovirt.engine.ui.common.widget.parser.EntityModelParser;
+import org.ovirt.engine.ui.common.widget.editor.generic.IntegerEntityModelTextBoxEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextBoxEditor;
 import org.ovirt.engine.ui.common.widget.renderer.EnumRenderer;
 import org.ovirt.engine.ui.common.widget.renderer.NullSafeRenderer;
 import org.ovirt.engine.ui.common.widget.table.column.EntityModelTextColumn;
@@ -39,6 +40,8 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.view.client.MultiSelectionModel;
 import com.google.inject.Inject;
 
+import java.text.ParseException;
+
 public class AddBrickPopupView extends AbstractModelBoundPopupView<VolumeBrickModel> implements AddBrickPopupPresenterWidget.ViewDef {
 
     interface Driver extends SimpleBeanEditorDriver<VolumeBrickModel, AddBrickPopupView> {
@@ -58,17 +61,17 @@ public class AddBrickPopupView extends AbstractModelBoundPopupView<VolumeBrickMo
     @UiField(provided = true)
     @Path(value = "volumeType.entity")
     @WithElementId
-    EntityModelLabelEditor volumeTypeEditor;
+    EntityModelLabelEditor<GlusterVolumeType> volumeTypeEditor;
 
     @UiField
     @Path(value = "replicaCount.entity")
     @WithElementId
-    EntityModelTextBoxEditor replicaCountEditor;
+    IntegerEntityModelTextBoxEditor replicaCountEditor;
 
     @UiField
     @Path(value = "stripeCount.entity")
     @WithElementId
-    EntityModelTextBoxEditor stripeCountEditor;
+    IntegerEntityModelTextBoxEditor stripeCountEditor;
 
     @UiField(provided = true)
     @Path(value = "force.entity")
@@ -78,12 +81,12 @@ public class AddBrickPopupView extends AbstractModelBoundPopupView<VolumeBrickMo
     @UiField(provided = true)
     @Path(value = "servers.selectedItem")
     @WithElementId
-    ListModelListBoxEditor<Object> serverEditor;
+    ListModelListBoxEditor<VDS> serverEditor;
 
     @UiField
     @Path(value = "brickDirectory.entity")
     @WithElementId
-    EntityModelTextBoxEditor exportDirEditor;
+    StringEntityModelTextBoxEditor exportDirEditor;
 
     @UiField
     @WithElementId
@@ -147,12 +150,17 @@ public class AddBrickPopupView extends AbstractModelBoundPopupView<VolumeBrickMo
     }
 
     private void initEditors() {
-        volumeTypeEditor = new EntityModelLabelEditor(new EnumRenderer(), new EntityModelParser());
-        forceEditor = new EntityModelCheckBoxEditor(Align.RIGHT);
-        serverEditor = new ListModelListBoxEditor<Object>(new NullSafeRenderer<Object>() {
+        volumeTypeEditor = new EntityModelLabelEditor<GlusterVolumeType>(new EnumRenderer<GlusterVolumeType>(), new Parser<GlusterVolumeType>() {
             @Override
-            public String renderNullSafe(Object object) {
-                return ((VDS) object).getHostName();
+            public GlusterVolumeType parse(CharSequence text) throws ParseException {
+                return GlusterVolumeType.valueOf(text.toString().toUpperCase());
+            }
+        });
+        forceEditor = new EntityModelCheckBoxEditor(Align.RIGHT);
+        serverEditor = new ListModelListBoxEditor<VDS>(new NullSafeRenderer<VDS>() {
+            @Override
+            public String renderNullSafe(VDS vds) {
+                return vds.getHostName();
             }
         });
 
@@ -267,7 +275,7 @@ public class AddBrickPopupView extends AbstractModelBoundPopupView<VolumeBrickMo
         moveBricksUpButton.setCommand(object.getMoveBricksUpCommand());
         moveBricksDownButton.setCommand(object.getMoveBricksDownCommand());
 
-        GlusterVolumeType volumeType = (GlusterVolumeType) object.getVolumeType().getEntity();
+        GlusterVolumeType volumeType = object.getVolumeType().getEntity();
         if (volumeType == GlusterVolumeType.DISTRIBUTED_REPLICATE) {
             infoLabel.setText(constants.distributedReplicateVolumeBrickInfoLabel());
         }
@@ -278,12 +286,12 @@ public class AddBrickPopupView extends AbstractModelBoundPopupView<VolumeBrickMo
             infoLabel.setText(null);
         }
 
-        forceWarningLabel.setVisible((Boolean) object.getForce().getEntity());
+        forceWarningLabel.setVisible(object.getForce().getEntity());
 
         object.getForce().getEntityChangedEvent().addListener(new IEventListener() {
             @Override
             public void eventRaised(Event ev, Object sender, EventArgs args) {
-                forceWarningLabel.setVisible((Boolean) object.getForce().getEntity());
+                forceWarningLabel.setVisible(object.getForce().getEntity());
             }
         });
     }

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/gluster/BrickAdvancedDetailsPopupView.java
@@ -1,5 +1,6 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.popup.gluster;
 
+import com.google.gwt.text.shared.Parser;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterClientInfo;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterStatus;
 import org.ovirt.engine.core.common.businessentities.gluster.Mempool;
@@ -14,7 +15,6 @@ import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelLabelEditor;
 import org.ovirt.engine.ui.common.widget.editor.generic.IntegerEntityModelLabelEditor;
 import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelLabelEditor;
 import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextAreaLabelEditor;
-import org.ovirt.engine.ui.common.widget.parser.EntityModelParser;
 import org.ovirt.engine.ui.common.widget.renderer.EnumRenderer;
 import org.ovirt.engine.ui.common.widget.table.column.EntityModelTextColumn;
 import org.ovirt.engine.ui.uicommonweb.models.ListModel;
@@ -31,6 +31,8 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Label;
 import com.google.inject.Inject;
+
+import java.text.ParseException;
 
 public class BrickAdvancedDetailsPopupView extends AbstractModelBoundPopupView<BrickAdvancedDetailsModel> implements BrickAdvancedDetailsPopupPresenterWidget.ViewDef {
 
@@ -193,7 +195,12 @@ public class BrickAdvancedDetailsPopupView extends AbstractModelBoundPopupView<B
     }
 
     private void initEditors() {
-        statusEditor = new EntityModelLabelEditor(new EnumRenderer(), new EntityModelParser());
+        statusEditor = new EntityModelLabelEditor<GlusterStatus>(new EnumRenderer<GlusterStatus>(), new Parser<GlusterStatus>() {
+            @Override
+            public GlusterStatus parse(CharSequence text) throws ParseException {
+                return GlusterStatus.valueOf(text.toString().toUpperCase());
+            }
+        });
         clientsTable = new EntityModelCellTable<ListModel>(false, true);
         memoryPoolsTable = new EntityModelCellTable<ListModel>(false, true);
     }

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/cluster/SubTabClusterGeneralView.java
@@ -2,13 +2,14 @@ package org.ovirt.engine.ui.webadmin.section.main.view.tab.cluster;
 
 import javax.inject.Inject;
 
+import com.google.gwt.text.shared.Parser;
 import org.ovirt.engine.core.common.businessentities.VDSGroup;
+import org.ovirt.engine.core.common.businessentities.gluster.GlusterServiceStatus;
 import org.ovirt.engine.ui.common.uicommon.model.DetailModelProvider;
 import org.ovirt.engine.ui.common.view.AbstractSubTabFormView;
 import org.ovirt.engine.ui.common.widget.UiCommandButton;
-import org.ovirt.engine.ui.common.widget.editor.EntityModelLabelEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelLabelEditor;
 import org.ovirt.engine.ui.common.widget.form.FormBuilder;
-import org.ovirt.engine.ui.common.widget.parser.EntityModelParser;
 import org.ovirt.engine.ui.common.widget.renderer.EnumRenderer;
 import org.ovirt.engine.ui.uicommonweb.models.clusters.ClusterGeneralModel;
 import org.ovirt.engine.ui.uicommonweb.models.clusters.ClusterListModel;
@@ -33,6 +34,8 @@ import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Widget;
+
+import java.text.ParseException;
 
 public class SubTabClusterGeneralView extends AbstractSubTabFormView<VDSGroup, ClusterListModel, ClusterGeneralModel>
         implements SubTabClusterGeneralPresenter.ViewDef, Editor<ClusterGeneralModel> {
@@ -60,7 +63,7 @@ public class SubTabClusterGeneralView extends AbstractSubTabFormView<VDSGroup, C
     HorizontalPanel glusterSwiftPanel;
 
     @UiField(provided = true)
-    EntityModelLabelEditor glusterSwiftStatusEditor;
+    EntityModelLabelEditor<GlusterServiceStatus> glusterSwiftStatusEditor;
 
     @UiField
     UiCommandButton manageGlusterSwiftButton;
@@ -86,7 +89,12 @@ public class SubTabClusterGeneralView extends AbstractSubTabFormView<VDSGroup, C
         // Inject a reference to the resources:
         this.resources = resources;
         this.form = new ClusterGeneralModelForm(modelProvider, constants);
-        glusterSwiftStatusEditor = new EntityModelLabelEditor(new EnumRenderer(), new EntityModelParser());
+        glusterSwiftStatusEditor = new EntityModelLabelEditor<GlusterServiceStatus>(new EnumRenderer<GlusterServiceStatus>(), new Parser<GlusterServiceStatus>() {
+            @Override
+            public GlusterServiceStatus parse(CharSequence text) throws ParseException {
+                return GlusterServiceStatus.valueOf(text.toString().toUpperCase());
+            }
+        });
 
         initWidget(ViewUiBinder.uiBinder.createAndBindUi(this));
         initManageGlusterSwift();

commit 07c7284efc22e4da7973687ed85cdb1740c84dcf

webadmin: [Fix] Cluster subtabs did not show up
Regression introduced by 3067b72c2b577a6e5d3e061aef38e2e6aebef115. The problem
was that the GlusterStatus parser expected that it will always get a valid
status - but it could get an empty String which caused an exception in
GlusterStatus.valueOf("")

Fixed by retuning null from this parser in case an empty string has to be
parsed. The renderer is able to take care of the null so it is not NPE
vulnerable anymore.

Change-Id: Iad3acc2369f8583b8ba78ed0efb5d1a05755d13c
Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/cluster/ManageGlusterSwiftPopupView.java
@@ -4,6 +4,7 @@ import com.google.gwt.text.shared.Parser;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterServerService;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterServiceStatus;
 import org.ovirt.engine.core.common.businessentities.gluster.ServiceType;
+import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.view.popup.AbstractModelBoundPopupView;
 import org.ovirt.engine.ui.common.widget.Align;
@@ -100,6 +101,9 @@ public class ManageGlusterSwiftPopupView extends AbstractModelBoundPopupView<Man
         swiftStatusEditor = new EntityModelLabelEditor<GlusterServiceStatus>(new EnumRenderer<GlusterServiceStatus>(), new Parser<GlusterServiceStatus>() {
             @Override
             public GlusterServiceStatus parse(CharSequence text) throws ParseException {
+                if (StringHelper.isNullOrEmpty(text.toString())) {
+                    return null;
+                }
                 return GlusterServiceStatus.valueOf(text.toString().toUpperCase());
             }
         });

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/gluster/AddBrickPopupView.java
@@ -4,6 +4,7 @@ import com.google.gwt.text.shared.Parser;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterBrickEntity;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeType;
+import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.idhandler.WithElementId;
 import org.ovirt.engine.ui.common.view.popup.AbstractModelBoundPopupView;
@@ -153,6 +154,9 @@ public class AddBrickPopupView extends AbstractModelBoundPopupView<VolumeBrickMo
         volumeTypeEditor = new EntityModelLabelEditor<GlusterVolumeType>(new EnumRenderer<GlusterVolumeType>(), new Parser<GlusterVolumeType>() {
             @Override
             public GlusterVolumeType parse(CharSequence text) throws ParseException {
+                if (StringHelper.isNullOrEmpty(text.toString())) {
+                    return null;
+                }
                 return GlusterVolumeType.valueOf(text.toString().toUpperCase());
             }
         });

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/gluster/BrickAdvancedDetailsPopupView.java
@@ -4,6 +4,7 @@ import com.google.gwt.text.shared.Parser;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterClientInfo;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterStatus;
 import org.ovirt.engine.core.common.businessentities.gluster.Mempool;
+import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.idhandler.WithElementId;
 import org.ovirt.engine.ui.common.view.popup.AbstractModelBoundPopupView;
@@ -198,6 +199,9 @@ public class BrickAdvancedDetailsPopupView extends AbstractModelBoundPopupView<B
         statusEditor = new EntityModelLabelEditor<GlusterStatus>(new EnumRenderer<GlusterStatus>(), new Parser<GlusterStatus>() {
             @Override
             public GlusterStatus parse(CharSequence text) throws ParseException {
+                if (StringHelper.isNullOrEmpty(text.toString())) {
+                    return null;
+                }
                 return GlusterStatus.valueOf(text.toString().toUpperCase());
             }
         });

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/cluster/SubTabClusterGeneralView.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import com.google.gwt.text.shared.Parser;
 import org.ovirt.engine.core.common.businessentities.VDSGroup;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterServiceStatus;
+import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.ui.common.uicommon.model.DetailModelProvider;
 import org.ovirt.engine.ui.common.view.AbstractSubTabFormView;
 import org.ovirt.engine.ui.common.widget.UiCommandButton;
@@ -92,6 +93,9 @@ public class SubTabClusterGeneralView extends AbstractSubTabFormView<VDSGroup, C
         glusterSwiftStatusEditor = new EntityModelLabelEditor<GlusterServiceStatus>(new EnumRenderer<GlusterServiceStatus>(), new Parser<GlusterServiceStatus>() {
             @Override
             public GlusterServiceStatus parse(CharSequence text) throws ParseException {
+                if (StringHelper.isNullOrEmpty(text.toString())) {
+                    return null;
+                }
                 return GlusterServiceStatus.valueOf(text.toString().toUpperCase());
             }
         });