25dacd5f2f7d2e13f687f24b663a408cf62c6bcb
Juan Hernandez <juan.hernandez@redhat.com>
2014-04-11 12:00:41 +0200

    restapi: Return additional info for 400 messages
    This was already done for JAXB exceptions, but is missing for RESTEasy
    JAXB marshall exceptions.
    
    Change-Id: I31a7a5f487c662cbcba3734153e9fcb120ba330d
    Bug-Url: https://bugzilla.redhat.com/867794
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/validation/JaxbMarshallExceptionMapper.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/validation/JaxbMarshallExceptionMapper.java
@@ -1,9 +1,5 @@
 package org.ovirt.engine.api.restapi.resource.validation;
 
-import org.jboss.resteasy.annotations.interception.ServerInterceptor;
-import org.jboss.resteasy.plugins.providers.jaxb.JAXBMarshalException;
-import org.ovirt.engine.api.model.Fault;
-
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
@@ -13,6 +9,9 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.annotations.interception.ServerInterceptor;
+import org.jboss.resteasy.plugins.providers.jaxb.JAXBMarshalException;
 
 @Provider
 @ServerInterceptor
@@ -28,14 +27,11 @@ public class JaxbMarshallExceptionMapper implements ExceptionMapper<JAXBMarshalE
     @Override
     public Response toResponse(JAXBMarshalException ex) {
         try {
-            Fault fault = new Fault();
-            fault.setReason("Bad Request");
-            fault.setDetail(ex.getCause().getMessage());
             return Response.status(Status.BAD_REQUEST)
-                    .entity(fault)
-                    .build();
+                .entity(new UsageFinder().getUsageMessage(application, uriInfo, request))
+                .build();
         } catch (Exception e) {
-            throw new WebApplicationException(e, Response.status(Status.INTERNAL_SERVER_ERROR).build());
+            throw new WebApplicationException(e, Response.status(Response.Status.INTERNAL_SERVER_ERROR).build());
         }
     }
 }
