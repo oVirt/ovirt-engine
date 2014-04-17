363df37755fbcd2b9542d664ff299bac15020eec
Lior Vernia <lvernia@redhat.com>
2014-04-17 16:05:01 +0300

    engine: Don't resurrect removed bonds for label
    If a network was attached via label to a bond being removed, the
    operation would still be blocked by the CanDoAction, this is now
    fixed.
    
    Change-Id: Ie5c0648999a80c313f6565673e7880a2b34f3e53
    Bug-Url: https://bugzilla.redhat.com/1084771
    Bug-Url: https://bugzilla.redhat.com/1084915
    Signed-off-by: Lior Vernia <lvernia@redhat.com>
    (cherry picked from commit 433fa09c87eb88d6d5e187f25d7a2dd1e9cb4c35)

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/SetupNetworksHelper.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/SetupNetworksHelper.java
@@ -112,7 +112,7 @@ public class SetupNetworksHelper {
 
         for (String network : removedNetworks) {
             VdsNetworkInterface nic = hostInterfacesByNetworkName.get(network);
-            if (nic != null) {
+            if (nic != null && !removedBonds.contains(nic.getName())) {
                 if (NetworkUtils.isVlan(nic)) {
                     nic = nicsByName.get(NetworkUtils.stripVlan(nic.getName()));
                     if (nic == null) {
