25c32ab8145f196810a4f84db41c4be93c9db8bb
Frantisek Kobzik <fkobzik@redhat.com>
2014-04-28 10:21:24 +0200

    frontend: console protocol priority
    this patch adds priority to ConsoleProtocol enum. This priority
    determines precedence of protocols when selecting default option in the
    ui.
    
    Change-Id: I9256ef44071f40552e47f03d829d95da810086ab
    Bug-Url: https://bugzilla.redhat.com/1086244
    Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/ConsoleProtocol.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/ConsoleProtocol.java
@@ -1,20 +1,32 @@
 package org.ovirt.engine.ui.uicommonweb.models;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import org.ovirt.engine.ui.uicommonweb.models.vms.ConsoleModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.RdpConsoleModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.SpiceConsoleModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.VncConsoleModel;
 
-
+/**
+ * Enum representing console protocol.
+ * Console protocol is determined by it's backing class and priority (if a VM supports more than one protocol at the
+ * same time, priority determines precedence of protocols).
+ * Protocols with higher number have higher priority.
+ */
 public enum ConsoleProtocol {
-    SPICE(SpiceConsoleModel.class),
-    RDP(RdpConsoleModel.class),
-    VNC(VncConsoleModel.class);
+
+    SPICE(SpiceConsoleModel.class, 3),
+    VNC(VncConsoleModel.class, 2),
+    RDP(RdpConsoleModel.class, 1);
 
     private final Class<? extends ConsoleModel> model;
+    private final int priority;
 
-    private ConsoleProtocol(Class<? extends ConsoleModel> model) {
+    private ConsoleProtocol(Class<? extends ConsoleModel> model, int priority) {
         this.model = model;
+        this.priority = priority;
     }
 
     public boolean isBackedBy(Class<? extends ConsoleModel> model) {
@@ -29,6 +41,28 @@ public enum ConsoleProtocol {
         }
 
         return null;
+    }
+
+    static class PriorityComparator implements Comparator<ConsoleProtocol> {
+        @Override
+        public int compare(ConsoleProtocol fst, ConsoleProtocol snd) {
+            if (fst == null && snd == null) {
+                return 0;
+            }
+            if (fst == null) {
+                return -1;
+            }
+            if (snd == null) {
+                return 1;
+            }
+            return fst.priority - snd.priority;
+        }
+    }
+
+    public static List<ConsoleProtocol> getProtocolsByPriority() {
+        List<ConsoleProtocol> consoleProtocols = Arrays.asList(ConsoleProtocol.values());
+        Collections.sort(consoleProtocols, new PriorityComparator());
+        return consoleProtocols;
     }
 
     public Class getBackingClass() {
