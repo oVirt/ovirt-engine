oVirt/ovirt-engine

commit 88756e3897ba467eb50055a7dda0c1afb11295d0

core: Remove usage of dynamic queries from the Backend
Dynamic queries are not optimized, and should only be used for UI search.
This patch will remove usage of dynamic queries from the backend.

Change-Id: I3df981a958fae36edd6ecf3cb2bb47b94b2c446a
Bug-Url: https://bugzilla.redhat.com/1061175
Signed-off-by: lzelkha@redhat.com <lzelkha@redhat.com>

backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/StorageDomainStaticDAODbFacadeImpl.java
@@ -30,6 +30,16 @@ public class StorageDomainStaticDAODbFacadeImpl extends DefaultGenericDaoDbFacad
     }
 
     @Override
+    public StorageDomainStatic getByName(String name, Guid userId, boolean filtered) {
+        return getCallsHandler().executeRead("Getstorage_domain_staticByNameFiltered",
+                StorageDomainStaticRowMapper.instance,
+                getCustomMapSqlParameterSource()
+                        .addValue("name", name)
+                        .addValue("user_id", userId)
+                        .addValue("filtered", filtered));
+    }
+
+    @Override
     public List<StorageDomainStatic> getAllForStoragePool(Guid id) {
         return getCallsHandler().executeReadList("Getstorage_domain_staticBystorage_pool_id",
                 StorageDomainStaticRowMapper.instance,

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmTemplatesByStoragePoolIdQuery.java
@@ -4,11 +4,7 @@ import java.util.List;
 
 import org.ovirt.engine.core.common.businessentities.StoragePool;
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
-import org.ovirt.engine.core.common.interfaces.SearchType;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
-import org.ovirt.engine.core.common.queries.SearchParameters;
-import org.ovirt.engine.core.common.queries.VdcQueryReturnValue;
-import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class GetVmTemplatesByStoragePoolIdQuery<P extends IdQueryParameters>
@@ -20,24 +16,17 @@ public class GetVmTemplatesByStoragePoolIdQuery<P extends IdQueryParameters>
     @Override
     protected void executeQueryCommand() {
         StoragePool pool = DbFacade.getInstance().getStoragePoolDao().get(getParameters().getId());
-        SearchParameters p = new SearchParameters(String.format("Templates: DataCenter = %1$s", pool.getName()),
-                SearchType.VmTemplate);
-        p.setMaxCount(Integer.MAX_VALUE);
-        VdcQueryReturnValue returnValue = Backend.getInstance().runInternalQuery(VdcQueryType.Search, p);
-
-        if (returnValue != null && returnValue.getSucceeded()) {
-            List<VmTemplate> templateList = returnValue.getReturnValue();
+        List<VmTemplate> templateList = DbFacade.getInstance().getVmTemplateDao().getAllForStorageDomain(pool.getId());
             // Load VmInit and disks
-            for(VmTemplate template: templateList) {
-                VmHandler.updateVmInitFromDB(template, true);
-                VmTemplateHandler.updateDisksFromDb(template);
-            }
-            VmTemplate blank = DbFacade.getInstance().getVmTemplateDao()
-                    .get(VmTemplateHandler.BLANK_VM_TEMPLATE_ID);
-            if (!templateList.contains(blank)) {
-                templateList.add(0, blank);
-            }
-            getQueryReturnValue().setReturnValue(templateList);
+        for (VmTemplate template : templateList) {
+            VmHandler.updateVmInitFromDB(template, true);
+            VmTemplateHandler.updateDisksFromDb(template);
         }
+        VmTemplate blank = DbFacade.getInstance().getVmTemplateDao()
+                .get(VmTemplateHandler.BLANK_VM_TEMPLATE_ID);
+        if (!templateList.contains(blank)) {
+            templateList.add(0, blank);
+        }
+        getQueryReturnValue().setReturnValue(templateList);
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/GetStorageDomainByNameQuery.java
@@ -1,0 +1,18 @@
+package org.ovirt.engine.core.bll.storage;
+
+import org.ovirt.engine.core.bll.QueriesCommandBase;
+import org.ovirt.engine.core.common.queries.NameQueryParameters;
+
+public class GetStorageDomainByNameQuery<P extends NameQueryParameters> extends QueriesCommandBase<P> {
+    public GetStorageDomainByNameQuery(P parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected void executeQueryCommand() {
+        getQueryReturnValue().setReturnValue(
+                getDbFacade().getStorageDomainStaticDao().getByName(getParameters().getName(),
+                        getUserID(),
+                        getParameters().isFiltered()));
+    }
+}

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/GetStoragePoolByDatacenterNameQuery.java
@@ -1,0 +1,18 @@
+package org.ovirt.engine.core.bll.storage;
+
+import org.ovirt.engine.core.bll.QueriesCommandBase;
+import org.ovirt.engine.core.common.queries.NameQueryParameters;
+
+public class GetStoragePoolByDatacenterNameQuery<P extends NameQueryParameters> extends QueriesCommandBase<P> {
+    public GetStoragePoolByDatacenterNameQuery(P parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected void executeQueryCommand() {
+        getQueryReturnValue()
+                .setReturnValue(getDbFacade()
+                        .getStoragePoolDao()
+                        .getByName(getParameters().getName(), false));
+    }
+}

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/VdcQueryType.java
@@ -207,10 +207,12 @@ public enum VdcQueryType implements Serializable {
 
     // Storage
     GetStorageDomainById(VdcQueryAuthType.User),
+    GetStorageDomainByName(VdcQueryAuthType.User),
     GetStorageServerConnectionById,
     GetAllStorageServerConnections,
     GetStorageServerConnectionsForDomain,
     GetStoragePoolById(VdcQueryAuthType.User),
+    GetStoragePoolByDatacenterName(VdcQueryAuthType.User),
     GetStorageDomainsByConnection,
     GetConnectionsByDataCenterAndStorageType,
     GetStorageDomainsByStoragePoolId(VdcQueryAuthType.User),

backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/StorageDomainStaticDAO.java
@@ -18,8 +18,19 @@ public interface StorageDomainStaticDAO extends GenericDao<StorageDomainStatic, 
     StorageDomainStatic getByName(String name);
 
     /**
+     * Retrieves the instance with the specified name.
+     * @param name
+     *            the domain name
+     * @param userId
+     *            the user id
+     * @param filtered
+     *            should the query use the user id
+     * @return the domain
+     */
+    StorageDomainStatic getByName(String name, Guid userId, boolean filtered);
+
+    /**
      * Retrieves all storage domains for the given storage pool.
-     *
      * @param pool
      *            the pool
      * @return the list of domains

backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/AbstractBackendActionableResource.java
@@ -19,14 +19,13 @@ import org.ovirt.engine.api.utils.LinkHelper;
 import org.ovirt.engine.core.common.action.VdcActionParametersBase;
 import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.action.VdcReturnValueBase;
+import org.ovirt.engine.core.common.businessentities.StorageDomainStatic;
 import org.ovirt.engine.core.common.businessentities.StoragePool;
 import org.ovirt.engine.core.common.businessentities.VDSGroup;
-import org.ovirt.engine.core.common.interfaces.SearchType;
 import org.ovirt.engine.core.common.queries.NameQueryParameters;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
-
 
 public abstract class AbstractBackendActionableResource <R extends BaseResource, Q /* extends IVdcQueryable */ >
     extends AbstractBackendSubResource<R, Q> {
@@ -224,7 +223,17 @@ public abstract class AbstractBackendActionableResource <R extends BaseResource,
 
     protected Guid lookupStorageDomainIdByName(String name) {
         if (!isFiltered()) {
-            return getEntity(org.ovirt.engine.core.common.businessentities.StorageDomain.class, SearchType.StorageDomain, "Storage: name=" + name).getId();
+            StorageDomainStatic storageDomain =
+                    getEntity(org.ovirt.engine.core.common.businessentities.StorageDomainStatic.class,
+                    VdcQueryType.GetStorageDomainByName,
+                    new NameQueryParameters(name),
+                    "Storage: name=" + name);
+
+            if (storageDomain != null) {
+                return storageDomain.getId();
+            }
+
+            return null;
         }
         else {
             List<org.ovirt.engine.core.common.businessentities.StorageDomain> storageDomains =

backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/AbstractBackendStorageDomainContentResource.java
@@ -9,8 +9,8 @@ import org.ovirt.engine.api.model.Disk;
 import org.ovirt.engine.core.common.businessentities.IVdcQueryable;
 import org.ovirt.engine.core.common.businessentities.StorageDomainType;
 import org.ovirt.engine.core.common.businessentities.VDSGroup;
-import org.ovirt.engine.core.common.interfaces.SearchType;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.common.queries.NameQueryParameters;
 import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
 
@@ -82,7 +82,10 @@ public abstract class AbstractBackendStorageDomainContentResource<C extends Base
     }
 
     protected VDSGroup lookupClusterByName(String name) {
-        return getEntity(VDSGroup.class, SearchType.Cluster, "Cluster: name=" + name);
+        return getEntity(VDSGroup.class,
+                VdcQueryType.GetVdsGroupByName,
+                new NameQueryParameters(name),
+                "Cluster: name=" + name);
     }
 
     protected VDSGroup lookupClusterById(String id) {

commit d5d5eaffbe2999e24e3702f588735b7ff04fa2cb

core: Fix issue with GetStorageDomainByNameQuery
Commit 88756e3897ba467eb50055a7dda0c1afb11295d0 introduced function GetStorageDomainByNameQuery
which faults at vm export. This patch fixes this issue.

Change-Id: Ifae06739c658a1f602421081590790b11bba5806
Bug-Url: https://bugzilla.redhat.com/1087028
Signed-off-by: lzelkha@redhat.com <lzelkha@redhat.com>

backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/StorageDomainStaticDAODbFacadeImpl.java
@@ -36,7 +36,7 @@ public class StorageDomainStaticDAODbFacadeImpl extends DefaultGenericDaoDbFacad
                 getCustomMapSqlParameterSource()
                         .addValue("name", name)
                         .addValue("user_id", userId)
-                        .addValue("filtered", filtered));
+                        .addValue("is_filtered", filtered));
     }
 
     @Override