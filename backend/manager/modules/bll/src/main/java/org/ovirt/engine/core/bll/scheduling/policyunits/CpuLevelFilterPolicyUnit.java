9b861ad79b4f9f9f4b476460311da15198c28da9
Tal Nisan <tnisan@redhat.com>
2014-04-29 17:22:49 +0300

    coverity: Fix NULL_RETURNS error in CpuLevelFilterPolicyUnit
    Change-Id: I732c1b272d6a8c1da871a4e41c484eb36c1a4e62
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/policyunits/CpuLevelFilterPolicyUnit.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/policyunits/CpuLevelFilterPolicyUnit.java
@@ -3,6 +3,7 @@ package org.ovirt.engine.core.bll.scheduling.policyunits;
 import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.CpuFlagsManagerHandler;
 import org.ovirt.engine.core.bll.scheduling.PolicyUnitImpl;
+import org.ovirt.engine.core.common.businessentities.ServerCpu;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.errors.VdcBllMessages;
@@ -23,7 +24,8 @@ public class CpuLevelFilterPolicyUnit extends PolicyUnitImpl {
         if (StringUtils.isNotEmpty(vm.getCpuName())) {
             List<VDS> hostsToRunOn = new ArrayList<VDS>();
             for (VDS host : hosts) {
-                String hostCpuName = CpuFlagsManagerHandler.FindMaxServerCpuByFlags(host.getCpuFlags(), host.getVdsGroupCompatibilityVersion()).getCpuName();
+                ServerCpu cpu = CpuFlagsManagerHandler.FindMaxServerCpuByFlags(host.getCpuFlags(), host.getVdsGroupCompatibilityVersion());
+                String hostCpuName = cpu == null ? null : cpu.getCpuName();
                 if (StringUtils.isNotEmpty(hostCpuName)) {
                     int compareResult = CpuFlagsManagerHandler.compareCpuLevels(vm.getCpuName(), hostCpuName, vm.getVdsGroupCompatibilityVersion());
                     if (compareResult <= 0) {
