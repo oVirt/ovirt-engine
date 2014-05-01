7d41cbbc96a1b1c24ee8d430f434d6b058f1b952
Alona Kaplan <alkaplan@redhat.com>
2014-05-01 14:11:36 +0300

    engine: Adjust GetAllSiblingVlanInterfacesQuery to accept any vlan name
    Changes done in the command-
    1. The query uses iface.getBaseInterface() and iface.getVlanId() instead
    of determining this values from the vlan device name (since its format is
    no longer- baseIface.vlanId).
    2. The query uses the new methods indroduced in NetworkUtils instead of
    the old ones (that should be removed in a following patch).
    
    Change-Id: I95a20887f8359bab66ea54747d21e05c310fc21c
    Bug-Url: https://bugzilla.redhat.com/1091863
    Bug-Url: https://bugzilla.redhat.com/999975
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetAllSiblingVlanInterfacesQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetAllSiblingVlanInterfacesQuery.java
@@ -8,6 +8,8 @@ import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
 import org.ovirt.engine.core.common.queries.InterfaceAndIdQueryParameters;
 import org.ovirt.engine.core.utils.NetworkUtils;
+import org.ovirt.engine.core.utils.linq.LinqUtils;
+import org.ovirt.engine.core.utils.linq.Predicate;
 
 /**
  * This query get vlan interface and return all it's siblings, i.e input: eth2.2
@@ -22,15 +24,22 @@ public class GetAllSiblingVlanInterfacesQuery<P extends InterfaceAndIdQueryParam
     @Override
     protected void executeQueryCommand() {
         ArrayList<VdsNetworkInterface> retVal = new ArrayList<VdsNetworkInterface>();
-        if (NetworkUtils.isVlan(getParameters().getInterface())) {
-            List<VdsNetworkInterface> vdsInterfaces =
-                    getDbFacade().getInterfaceDao().getAllInterfacesForVds(getParameters().getId());
+        List<VdsNetworkInterface> vdsInterfaces =
+                getDbFacade().getInterfaceDao().getAllInterfacesForVds(getParameters().getId());
+        VdsNetworkInterface iface = LinqUtils.firstOrNull(vdsInterfaces, new Predicate<VdsNetworkInterface>() {
+            @Override
+            public boolean eval(VdsNetworkInterface i) {
+                return i.getName().equals(getParameters().getInterface().getName());
+            }
+        });
+
+        if (iface != null && NetworkUtils.isVlan(iface)) {
             for (int i = 0; i < vdsInterfaces.size(); i++) {
                 if (NetworkUtils.isVlan(vdsInterfaces.get(i))
-                        && !StringUtils.equals(getParameters().getInterface().getName(), vdsInterfaces.get(i)
+                        && !StringUtils.equals(iface.getName(), vdsInterfaces.get(i)
                                 .getName())) {
-                    if (StringUtils.equals(NetworkUtils.stripVlan(getParameters().getInterface().getName()),
-                            NetworkUtils.stripVlan(vdsInterfaces.get(i).getName()))) {
+                    if (StringUtils.equals(iface.getBaseInterface(),
+                            vdsInterfaces.get(i).getBaseInterface())) {
                         retVal.add(vdsInterfaces.get(i));
                     }
                 }
