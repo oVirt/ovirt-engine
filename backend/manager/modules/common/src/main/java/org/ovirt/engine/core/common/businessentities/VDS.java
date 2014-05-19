oVirt/ovirt-engine

commit b5d64087e715f0df6eff7c8a66425ea1579cfeeb

common: add missing 'override' annotations
For VDS BE.

Change-Id: I3fc7e3ed07fb41887650abdcd733aa3e86530233
Signed-off-by: Gilad Chaplik <gchaplik@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VDS.java
@@ -97,6 +97,7 @@ public class VDS extends IVdcQueryable implements Serializable, BusinessEntityWi
                 && ObjectUtils.objectsEqual(vdsGroupGlusterService, other.vdsGroupGlusterService));
     }
 
+    @Override
     public VDS clone() {
         VDS vds = new VDS();
         vds.setVdsGroupId(getVdsGroupId());
@@ -316,10 +317,12 @@ public class VDS extends IVdcQueryable implements Serializable, BusinessEntityWi
         this.mVdsStatic.setHostName(value);
     }
 
+    @Override
     public String getComment() {
         return mVdsStatic.getComment();
     }
 
+    @Override
     public void setComment(String value) {
         mVdsStatic.setComment(value);
     }
@@ -1295,6 +1298,7 @@ public class VDS extends IVdcQueryable implements Serializable, BusinessEntityWi
         return maxSchedulingMemory;
     }
 
+    @Override
     public String toString() {
         // note that mVdsStatic may be null, so the getName with no null protection
         // is not enough, remove this once mVdsStatic can not be null

commit c6324d4e6ff2c0d11dbe154c9371d4ba48690f64

engine: fixed compile error in VDS.java
commit b5d64087e715f0df6eff7c8a66425ea1579cfeeb introduced an @Override
in the wrong place, causing compilation failure. Fixed.

Change-Id: Id5cdc8a68defe03bcdde3b5c444df6a14bdc4feb
Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VDS.java
@@ -98,7 +98,6 @@ public class VDS extends IVdcQueryable implements Serializable, BusinessEntityWi
                 && ObjectUtils.objectsEqual(vdsGroupGlusterService, other.vdsGroupGlusterService));
     }
 
-    @Override
     public VDS clone() {
         VDS vds = new VDS();
         vds.setVdsGroupId(getVdsGroupId());