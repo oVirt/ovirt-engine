1579d8c072ad0a1ae466fc026961a90cdd9dc0f2
Anmol Babu <anbabu@redhat.com>
2014-05-20 20:14:01 +0530

    engine : Introduce TimeConverter
    This patch introduces TimeUnitConverter that converts
    (approximately) the input difference in times(long)
    to a maximum expressable time unit.Currently it goes
    upto days.
    This patch is just a wrapper around the java concurrent
    TimeUnit.The wrapper is written with the intention of
    providing the additional facility of auto-converting
    the lower unit time/time interval into its highest
    possible expressible unit such that its value post
    conversion > 1
    Also,this patch just returns the integral magnitude and
    unit of the highest expressable unit.
    
    Change-Id: I045aaa4f93b345e0457e1a0ce2ccf0ceeb9071ad
    Signed-off-by: Anmol Babu <anbabu@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/TimeConverter.java
@@ -0,0 +1,60 @@
+package org.ovirt.engine.core.common.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class TimeConverter {
+
+    private static List<TimeUnit> orderedTimeUnits;
+
+    static {
+        orderedTimeUnits = new ArrayList<TimeUnit>();
+        for (TimeUnit timeUnit : TimeUnit.values()) {
+            orderedTimeUnits.add(timeUnit);
+        }
+        Collections.sort(orderedTimeUnits, Collections.reverseOrder(new Comparator<TimeUnit>() {
+            @Override
+            public int compare(TimeUnit unit1, TimeUnit unit2) {
+                if (TimeUnit.MICROSECONDS.convert(1L, unit1) < TimeUnit.MICROSECONDS.convert(1L, unit2)) {
+                    return -1;
+                }
+                return 1;
+            }
+        }));
+    }
+
+    /*
+     * This method converts an input time interval (interval) in an input unit(fromUnit) to a desired output
+     * unit(toUnit). It is basically a wrapper around the java.util.concurrent.TimeUnit 's convert function This is
+     * method is written just for ease of use as the java.util.concurrent.TimeUnit 's convert function usage is of the
+     * form TimeUnit.Minutes.convert(60L, TimeUnit.Seconds) to convert 60 seconds to a minuite
+     */
+
+    public static long convert(long interval, TimeUnit fromUnit, TimeUnit toUnit) {
+        return TimeUnit.valueOf(toUnit.name()).convert(interval, fromUnit);
+    }
+
+    /*
+     * This method is introduced for the purpose of making it easy to express the input interval of time in an input
+     * unit into its highest possible unit such that the integral value of the converted interval is of non-zero
+     * magnitude. ex : 60 * 1000 * 1000 microseconds = 1 min
+     */
+
+    public static Pair<Long, TimeUnit> autoConvert(long interval, TimeUnit fromUnit) {
+        Pair<Long, TimeUnit> result = new Pair<Long, TimeUnit>();
+        for (TimeUnit timeUnit : orderedTimeUnits) {
+            long tInterval = interval;
+            interval = convert(interval, fromUnit, timeUnit);
+            if (interval >= 1) {
+                result.setFirst(interval);
+                result.setSecond(timeUnit);
+                break;
+            }
+            interval = tInterval;
+        }
+        return result;
+    }
+}
---
+++ backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/TimeConverterTest.java
@@ -0,0 +1,33 @@
+package org.ovirt.engine.core.common.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+public class TimeConverterTest {
+    @Test
+    public void testConvertSecondsToMinutes() {
+        long seconds = 1800L;
+        Pair<Long, TimeUnit> expectedResults = TimeConverter.autoConvert(seconds, TimeUnit.SECONDS);
+        assertEquals(30L, expectedResults.getFirst().longValue());
+        assertEquals(TimeUnit.MINUTES, expectedResults.getSecond());
+    }
+
+    @Test
+    public void testConvertMinutesToHours() {
+        long minutes = 720L;
+        Pair<Long, TimeUnit> expectedResults = TimeConverter.autoConvert(minutes, TimeUnit.MINUTES);
+        assertEquals(12L, expectedResults.getFirst().longValue());
+        assertEquals(TimeUnit.HOURS, expectedResults.getSecond());
+    }
+
+    @Test
+    public void testConvertMillsecondsToMinutes() {
+        long milliSeconds = 60000L;
+        Pair<Long, TimeUnit> expectedResults = TimeConverter.autoConvert(milliSeconds, TimeUnit.MILLISECONDS);
+        assertEquals(1L, expectedResults.getFirst().longValue());
+        assertEquals(TimeUnit.MINUTES, expectedResults.getSecond());
+    }
+}
