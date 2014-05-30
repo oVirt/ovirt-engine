bb0c0f301934356c5a7d67b86371a62c5c454573
Jason Liao <chuan.liao@hp.com>
2014-05-30 15:04:49 +0800

    engine: NUMA feature transaction support
    Add transaction support for add/update VM NUMA nodes
    in add/update VM
    
    Change-Id: If84e59fa38c9174fe7150116bf3c8832f7fac1dd
    Bug-Url: https://bugzilla.redhat.com/1069303
    Signed-off-by: Jason Liao <chuan.liao@hp.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/VmNumaNodeOperationParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/VmNumaNodeOperationParameters.java
@@ -3,6 +3,8 @@ package org.ovirt.engine.core.common.action;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.ovirt.engine.core.common.businessentities.MigrationSupport;
+import org.ovirt.engine.core.common.businessentities.NumaTuneMode;
 import org.ovirt.engine.core.common.businessentities.VmNumaNode;
 import org.ovirt.engine.core.compat.Guid;
 
@@ -11,6 +13,10 @@ public class VmNumaNodeOperationParameters extends VmOperationParameterBase {
     private static final long serialVersionUID = -1955959985341097257L;
 
     private List<VmNumaNode> vmNumaNodeList;
+
+    private NumaTuneMode numaTuneMode;
+    private Guid dedicatedHost;
+    private MigrationSupport migrationSupport;
 
     public VmNumaNodeOperationParameters() {
     }
@@ -28,6 +34,30 @@ public class VmNumaNodeOperationParameters extends VmOperationParameterBase {
 
     public List<VmNumaNode> getVmNumaNodeList() {
         return vmNumaNodeList;
+    }
+
+    public NumaTuneMode getNumaTuneMode() {
+        return numaTuneMode;
+    }
+
+    public void setNumaTuneMode(NumaTuneMode numaTuneMode) {
+        this.numaTuneMode = numaTuneMode;
+    }
+
+    public Guid getDedicatedHost() {
+        return dedicatedHost;
+    }
+
+    public void setDedicatedHost(Guid dedicatedHost) {
+        this.dedicatedHost = dedicatedHost;
+    }
+
+    public MigrationSupport getMigrationSupport() {
+        return migrationSupport;
+    }
+
+    public void setMigrationSupport(MigrationSupport migrationSupport) {
+        this.migrationSupport = migrationSupport;
     }
 
 }
