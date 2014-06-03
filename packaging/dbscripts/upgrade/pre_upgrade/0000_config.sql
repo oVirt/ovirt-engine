d391f740bfe210bc9e6c5a016053ca323650cf11
Frantisek Kobzik <fkobzik@redhat.com>
2014-06-03 15:43:36 +0200

    core: Update default RNG sources
    This patch removes RANDOM source from "cluster required RNG sources
    default" option for 3.5 clusters (this option affects default state
    checkboxes in new cluster dialog in webadmin).
    
    The reason for this change is the fact EL 6 qemu doesn't support
    virtualized RNG devices, so not requiring any of RNG sources is safer.
    
    Change-Id: I93d64231f5cc1c70164f3991c980432d40a185f6
    Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/977079

--- packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
+++ packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -278,7 +278,7 @@ select fn_db_add_config_value('ClusterRequiredRngSourcesDefault','','3.1');
 select fn_db_add_config_value('ClusterRequiredRngSourcesDefault','','3.2');
 select fn_db_add_config_value('ClusterRequiredRngSourcesDefault','','3.3');
 select fn_db_add_config_value('ClusterRequiredRngSourcesDefault','','3.4');
-select fn_db_add_config_value('ClusterRequiredRngSourcesDefault','RANDOM','3.5');
+select fn_db_add_config_value('ClusterRequiredRngSourcesDefault','','3.5');
 
 select fn_db_add_config_value('SpiceCopyPasteToggleSupported','false','3.0');
 select fn_db_add_config_value('SpiceCopyPasteToggleSupported','false','3.1');
