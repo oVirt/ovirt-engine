14ed6ce6a7d4a6191f17d44ab5c70a86742e31d3
Frantisek Kobzik <fkobzik@redhat.com>
2014-06-04 18:58:29 +0200

    core: Extract hardcoded strings to constants
    This patch pulls hardcoded strings in PoolConditionFieldAutoCompleter to
    constants. They are used in follow up frontend patch (sorting columns in
    Pools main tab in webadmin application).
    
    Change-Id: I5ab8dc498dc7d8b0e797a26676c3bf92139a9e65
    Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1104790

--- backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/PoolConditionFieldAutoCompleter.java
+++ backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/PoolConditionFieldAutoCompleter.java
@@ -4,32 +4,40 @@ import org.ovirt.engine.core.common.businessentities.ArchitectureType;
 import org.ovirt.engine.core.common.businessentities.VmPoolType;
 
 public class PoolConditionFieldAutoCompleter extends BaseConditionFieldAutoCompleter {
+
+    public static final String NAME = "NAME";
+    public static final String DESCRIPTION = "DESCRIPTION";
+    public static final String TYPE = "TYPE";
+    public static final String CLUSTER = "CLUSTER";
+    public static final String DATACENTER = "DATACENTER";
+    public static final String ARCHITECTURE = "ARCHITECTURE";
+
     public PoolConditionFieldAutoCompleter() {
         // Building the basic vervs Dict
-        mVerbs.add("NAME");
-        mVerbs.add("DESCRIPTION");
-        mVerbs.add("TYPE");
-        mVerbs.add("CLUSTER");
-        mVerbs.add("DATACENTER");
-        mVerbs.add("ARCHITECTURE");
+        mVerbs.add(NAME);
+        mVerbs.add(DESCRIPTION);
+        mVerbs.add(TYPE);
+        mVerbs.add(CLUSTER);
+        mVerbs.add(DATACENTER);
+        mVerbs.add(ARCHITECTURE);
 
         // Building the autoCompletion Dict
         buildCompletions();
         // Building the types dict
-        getTypeDictionary().put("NAME", String.class);
-        getTypeDictionary().put("DESCRIPTION", String.class);
-        getTypeDictionary().put("TYPE", VmPoolType.class);
-        getTypeDictionary().put("CLUSTER", String.class);
-        getTypeDictionary().put("DATACENTER", String.class);
-        getTypeDictionary().put("ARCHITECTURE", ArchitectureType.class);
+        getTypeDictionary().put(NAME, String.class);
+        getTypeDictionary().put(DESCRIPTION, String.class);
+        getTypeDictionary().put(TYPE, VmPoolType.class);
+        getTypeDictionary().put(CLUSTER, String.class);
+        getTypeDictionary().put(DATACENTER, String.class);
+        getTypeDictionary().put(ARCHITECTURE, ArchitectureType.class);
 
         // building the ColumnName Dict
-        columnNameDict.put("NAME", "vm_pool_name");
-        columnNameDict.put("DESCRIPTION", "vm_pool_description");
-        columnNameDict.put("TYPE", "vm_pool_type");
-        columnNameDict.put("CLUSTER", "vds_group_name");
-        columnNameDict.put("DATACENTER", "storage_pool_name");
-        columnNameDict.put("ARCHITECTURE", "architecture");
+        columnNameDict.put(NAME, "vm_pool_name");
+        columnNameDict.put(DESCRIPTION, "vm_pool_description");
+        columnNameDict.put(TYPE, "vm_pool_type");
+        columnNameDict.put(CLUSTER, "vds_group_name");
+        columnNameDict.put(DATACENTER, "storage_pool_name");
+        columnNameDict.put(ARCHITECTURE, "architecture");
 
         // Building the validation dict
         buildBasicValidationTable();
@@ -43,9 +51,9 @@ public class PoolConditionFieldAutoCompleter extends BaseConditionFieldAutoCompl
     @Override
     public IConditionValueAutoCompleter getFieldValueAutoCompleter(String fieldName) {
         IConditionValueAutoCompleter retval = null;
-        if ("TYPE".equals(fieldName)) {
+        if (TYPE.equals(fieldName)) {
             retval = new EnumValueAutoCompleter(VmPoolType.class);
-        } else if ("ARCHITECTURE".equals(fieldName)) {
+        } else if (ARCHITECTURE.equals(fieldName)) {
             retval = new EnumValueAutoCompleter(ArchitectureType.class);
         }
         return retval;
