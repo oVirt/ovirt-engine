dfd361ed6a6ae4fff21d5ca4afc43732bccb4407
Frantisek Kobzik <fkobzik@redhat.com>
2014-06-04 18:12:49 +0200

    webadmin: Template main tab columns sorting
    This patch adds sorting of fields in Template main tab in webadmin. As a
    part of the patch, some existing *Column implementations were adjusted
    to extend SortableColumn class.
    
    Change-Id: I8c91564be9bc4119e405282d29a3aa00288126c6
    Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1104784

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/templates/TemplateListModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/templates/TemplateListModel.java
@@ -383,9 +383,14 @@ public class TemplateListModel extends VmBaseListModel<VmTemplate> implements IS
     @Override
     protected void syncSearch()
     {
-        SearchParameters tempVar = new SearchParameters(getSearchString(), SearchType.VmTemplate, isCaseSensitiveSearch());
+        SearchParameters tempVar = new SearchParameters(applySortOptions(getSearchString()), SearchType.VmTemplate, isCaseSensitiveSearch());
         tempVar.setMaxCount(getSearchPageSize());
         super.syncSearch(VdcQueryType.Search, tempVar);
+    }
+
+    @Override
+    public boolean supportsServerSideSorting() {
+        return true;
     }
 
     @Override
--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/MainTabTemplateView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/MainTabTemplateView.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.businessentities.VmTemplateStatus;
 import org.ovirt.engine.core.compat.StringFormat;
+import org.ovirt.engine.core.searchbackend.VmTemplateConditionFieldAutoCompleter;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.uicommon.model.MainModelProvider;
 import org.ovirt.engine.ui.common.widget.table.column.EnumColumn;
@@ -44,6 +45,7 @@ public class MainTabTemplateView extends AbstractMainTabWithDetailsTableView<VmT
                 return object.getName();
             }
         };
+        nameColumn.makeSortable(VmTemplateConditionFieldAutoCompleter.NAME);
         getTable().addColumn(nameColumn, constants.namePool(), "150px"); //$NON-NLS-1$
 
         TextColumnWithTooltip<VmTemplate> versionNameColumn = new TextColumnWithTooltip<VmTemplate>() {
@@ -58,6 +60,7 @@ public class MainTabTemplateView extends AbstractMainTabWithDetailsTableView<VmT
                         object.getTemplateVersionNumber());
             }
         };
+        versionNameColumn.makeSortable(VmTemplateConditionFieldAutoCompleter.VERSION_NAME_AND_NUMBER);
         getTable().addColumn(versionNameColumn, constants.versionTemplate(), "150px"); //$NON-NLS-1$
 
         CommentColumn<VmTemplate> commentColumn = new CommentColumn<VmTemplate>();
@@ -69,6 +72,7 @@ public class MainTabTemplateView extends AbstractMainTabWithDetailsTableView<VmT
                 return object.getCreationDate();
             }
         };
+        creationDateColumn.makeSortable(VmTemplateConditionFieldAutoCompleter.CREATIONDATE);
         getTable().addColumn(creationDateColumn, constants.creationDateTemplate(), "150px"); //$NON-NLS-1$
 
         TextColumnWithTooltip<VmTemplate> statusColumn = new EnumColumn<VmTemplate, VmTemplateStatus>() {
@@ -77,6 +81,7 @@ public class MainTabTemplateView extends AbstractMainTabWithDetailsTableView<VmT
                 return object.getStatus();
             }
         };
+        statusColumn.makeSortable(VmTemplateConditionFieldAutoCompleter.STATUS);
         getTable().addColumn(statusColumn, constants.statusTemplate(), "100px"); //$NON-NLS-1$
 
         TextColumnWithTooltip<VmTemplate> clusterColumn = new TextColumnWithTooltip<VmTemplate>() {
@@ -85,6 +90,7 @@ public class MainTabTemplateView extends AbstractMainTabWithDetailsTableView<VmT
                 return object.getVdsGroupName();
             }
         };
+        clusterColumn.makeSortable(VmTemplateConditionFieldAutoCompleter.CLUSTER);
         getTable().addColumn(clusterColumn, constants.clusterTemplate(), "150px"); //$NON-NLS-1$
 
         TextColumnWithTooltip<VmTemplate> dcColumn = new TextColumnWithTooltip<VmTemplate>() {
@@ -93,6 +99,7 @@ public class MainTabTemplateView extends AbstractMainTabWithDetailsTableView<VmT
                 return object.getStoragePoolName();
             }
         };
+        dcColumn.makeSortable(VmTemplateConditionFieldAutoCompleter.DATACENTER);
         getTable().addColumn(dcColumn, constants.dcTemplate(), "150px"); //$NON-NLS-1$
 
         TextColumnWithTooltip<VmTemplate> descriptionColumn = new TextColumnWithTooltip<VmTemplate>() {
@@ -101,6 +108,7 @@ public class MainTabTemplateView extends AbstractMainTabWithDetailsTableView<VmT
                 return object.getDescription();
             }
         };
+        descriptionColumn.makeSortable(VmTemplateConditionFieldAutoCompleter.DESCRIPTION);
         getTable().addColumn(descriptionColumn, constants.descriptionTemplate(), "150px"); //$NON-NLS-1$
 
         getTable().addActionButton(new WebAdminButtonDefinition<VmTemplate>(constants.editTemplate()) {
