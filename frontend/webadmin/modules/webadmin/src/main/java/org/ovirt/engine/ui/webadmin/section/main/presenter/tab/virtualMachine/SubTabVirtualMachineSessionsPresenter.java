238f264ec06354330da8bd8b9c5ff54760eb92df
Ma Like <like.ma@cs2c.com.cn>
2014-06-04 11:20:34 +0800

    webadmin: refresh the Session sub-tab of vm
    When select the Session sub-tab of a vm, if press F5
    to refresh the web page, it displays a blank page.This
    patch resolve the problem, it will display Virtual Machines
    source tab.
    
    Change-Id: I71d2e7b4c43b841bcdf05ec549efe50cbc9b4bda
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1104423
    Signed-off-by: Ma Like <like.ma@cs2c.com.cn>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/tab/virtualMachine/SubTabVirtualMachineSessionsPresenter.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/tab/virtualMachine/SubTabVirtualMachineSessionsPresenter.java
@@ -50,7 +50,7 @@ public class SubTabVirtualMachineSessionsPresenter
 
     @Override
     protected PlaceRequest getMainTabRequest() {
-        return PlaceRequestFactory.get(ApplicationPlaces.virtualMachineSessionsSubTabPlace);
+        return PlaceRequestFactory.get(ApplicationPlaces.virtualMachineMainTabPlace);
     }
 
     @ProxyEvent
