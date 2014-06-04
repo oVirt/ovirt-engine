930c93d475f4d5594cf28001738b65d664b65325
Frantisek Kobzik <fkobzik@redhat.com>
2014-06-04 16:41:32 +0200

    core: Extract hardcoded strings to constants
    This patch pulls hardcoded strings in
    VmTemplateConditionFieldAutoCompleter to constants. They are used in
    follow up frontend patch (sorting columns in Template main tab in webadmin
    application).
    
    Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>
    Change-Id: Ic5b840c5b44829d8e6e68d4839977e01258076ff
    Bug-Url: https://bugzilla.redhat.com/1104784

--- backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/VmTemplateConditionFieldAutoCompleter.java
+++ backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/VmTemplateConditionFieldAutoCompleter.java
@@ -9,65 +9,79 @@ import org.ovirt.engine.core.common.businessentities.VmTemplateStatus;
 import org.ovirt.engine.core.common.utils.SimpleDependecyInjector;
 
 public class VmTemplateConditionFieldAutoCompleter extends BaseConditionFieldAutoCompleter {
+
+    public static final String NAME = "NAME";
+    public static final String COMMENT = "COMMENT";
+    public static final String OS = "OS";
+    public static final String CREATIONDATE = "CREATIONDATE";
+    public static final String CHILDCOUNT = "CHILDCOUNT";
+    public static final String MEM = "MEM";
+    public static final String DESCRIPTION = "DESCRIPTION";
+    public static final String STATUS = "STATUS";
+    public static final String CLUSTER = "CLUSTER";
+    public static final String DATACENTER = "DATACENTER";
+    public static final String QUOTA = "QUOTA";
+    public static final String ARCHITECTURE = "ARCHITECTURE";
+    public static final String VMT_ID = "_VMT_ID";
+
     public VmTemplateConditionFieldAutoCompleter() {
-        mVerbs.add("NAME");
-        mVerbs.add("COMMENT");
-        mVerbs.add("OS");
-        mVerbs.add("CREATIONDATE");
-        mVerbs.add("CHILDCOUNT");
-        mVerbs.add("MEM");
-        mVerbs.add("DESCRIPTION");
-        mVerbs.add("STATUS");
-        mVerbs.add("CLUSTER");
-        mVerbs.add("DATACENTER");
-        mVerbs.add("QUOTA");
-        mVerbs.add("DESCRIPTION");
-        mVerbs.add("ARCHITECTURE");
+        mVerbs.add(NAME);
+        mVerbs.add(COMMENT);
+        mVerbs.add(OS);
+        mVerbs.add(CREATIONDATE);
+        mVerbs.add(CHILDCOUNT);
+        mVerbs.add(MEM);
+        mVerbs.add(DESCRIPTION);
+        mVerbs.add(STATUS);
+        mVerbs.add(CLUSTER);
+        mVerbs.add(DATACENTER);
+        mVerbs.add(QUOTA);
+        mVerbs.add(ARCHITECTURE);
 
         buildCompletions();
-        mVerbs.add("_VMT_ID");
+        mVerbs.add(VMT_ID);
         // Building the types dict
-        getTypeDictionary().put("NAME", String.class);
-        getTypeDictionary().put("COMMENT", String.class);
-        getTypeDictionary().put("OS", String.class);
-        getTypeDictionary().put("CREATIONDATE", Date.class);
-        getTypeDictionary().put("CHILDCOUNT", Integer.class);
-        getTypeDictionary().put("MEM", Integer.class);
-        getTypeDictionary().put("DESCRIPTION", String.class);
-        getTypeDictionary().put("STATUS", VmTemplateStatus.class);
-        getTypeDictionary().put("CLUSTER", String.class);
-        getTypeDictionary().put("DATACENTER", String.class);
-        getTypeDictionary().put("QUOTA", String.class);
-        getTypeDictionary().put("_VMT_ID", UUID.class);
-        getTypeDictionary().put("DESCRIPTION", String.class);
-        getTypeDictionary().put("ARCHITECTURE", ArchitectureType.class);
+        getTypeDictionary().put(NAME, String.class);
+        getTypeDictionary().put(COMMENT, String.class);
+        getTypeDictionary().put(OS, String.class);
+        getTypeDictionary().put(CREATIONDATE, Date.class);
+        getTypeDictionary().put(CHILDCOUNT, Integer.class);
+        getTypeDictionary().put(MEM, Integer.class);
+        getTypeDictionary().put(DESCRIPTION, String.class);
+        getTypeDictionary().put(STATUS, VmTemplateStatus.class);
+        getTypeDictionary().put(CLUSTER, String.class);
+        getTypeDictionary().put(DATACENTER, String.class);
+        getTypeDictionary().put(QUOTA, String.class);
+        getTypeDictionary().put(VMT_ID, UUID.class);
+        getTypeDictionary().put(DESCRIPTION, String.class);
+        getTypeDictionary().put(ARCHITECTURE, ArchitectureType.class);
 
         // building the ColumnName Dict
-        columnNameDict.put("NAME", "name");
-        columnNameDict.put("COMMENT", "free_text_comment");
-        columnNameDict.put("OS", "os");
-        columnNameDict.put("CREATIONDATE", "creation_date");
-        columnNameDict.put("CHILDCOUNT", "child_count");
-        columnNameDict.put("MEM", "mem_size_mb");
-        columnNameDict.put("DESCRIPTION", "description");
-        columnNameDict.put("STATUS", "status");
-        columnNameDict.put("CLUSTER", "vds_group_name");
-        columnNameDict.put("DATACENTER", "storage_pool_name");
-        columnNameDict.put("QUOTA", "quota_name");
-        columnNameDict.put("_VMT_ID", "vmt_guid");
-        columnNameDict.put("DESCRIPTION", "description");
-        columnNameDict.put("ARCHITECTURE", "architecture");
+        columnNameDict.put(NAME, "name");
+        columnNameDict.put(COMMENT, "free_text_comment");
+        columnNameDict.put(OS, "os");
+        columnNameDict.put(CREATIONDATE, "creation_date");
+        columnNameDict.put(CHILDCOUNT, "child_count");
+        columnNameDict.put(MEM, "mem_size_mb");
+        columnNameDict.put(DESCRIPTION, "description");
+        columnNameDict.put(STATUS, "status");
+        columnNameDict.put(CLUSTER, "vds_group_name");
+        columnNameDict.put(DATACENTER, "storage_pool_name");
+        columnNameDict.put(QUOTA, "quota_name");
+        columnNameDict.put(VMT_ID, "vmt_guid");
+        columnNameDict.put(DESCRIPTION, "description");
+        columnNameDict.put(ARCHITECTURE, "architecture");
 
-        notFreeTextSearchableFieldsList.add("OS");
+        notFreeTextSearchableFieldsList.add(OS);
         // Building the validation dict
         buildBasicValidationTable();
     }
 
     @Override
     public IAutoCompleter getFieldRelationshipAutoCompleter(String fieldName) {
-        if ("CREATIONDATE".equals(fieldName)) {
+        if (CREATIONDATE.equals(fieldName)) {
             return TimeConditionRelationAutoCompleter.INSTANCE;
-        } else if ("CHILDCOUNT".equals(fieldName) || "MEM".equals(fieldName)) {
+        } else if (CHILDCOUNT.equals(fieldName) || MEM.equals(fieldName)) {
             return NumericConditionRelationAutoCompleter.INSTANCE;
         } else {
             return StringConditionRelationAutoCompleter.INSTANCE;
@@ -76,15 +90,15 @@ public class VmTemplateConditionFieldAutoCompleter extends BaseConditionFieldAut
 
     @Override
     public IConditionValueAutoCompleter getFieldValueAutoCompleter(String fieldName) {
-        if ("OS".equals(fieldName)) {
+        if (OS.equals(fieldName)) {
             return SimpleDependecyInjector.getInstance().get(OsValueAutoCompleter.class);
-        } else if ("CREATIONDATE".equals(fieldName)) {
+        } else if (CREATIONDATE.equals(fieldName)) {
             return new DateEnumValueAutoCompleter(DateEnumForSearch.class);
-        } else if ("STATUS".equals(fieldName)) {
+        } else if (STATUS.equals(fieldName)) {
             return new EnumValueAutoCompleter(VmTemplateStatus.class);
-        } else if ("QUOTA".equals(fieldName)) {
+        } else if (QUOTA.equals(fieldName)) {
             return new NullableStringAutoCompleter();
-        } else if ("ARCHITECTURE".equals(fieldName)) {
+        } else if (ARCHITECTURE.equals(fieldName)) {
             return new EnumValueAutoCompleter(ArchitectureType.class);
         }
         return null;
