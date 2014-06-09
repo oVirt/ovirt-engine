a1a7943d69e36dbb921d9ed0e4fcf7f71d6f6304
Ravi Nori <rnori@redhat.com>
2014-06-09 12:40:11 -0400

    engine : Collapse all Async Command Callbacks
    Collapse methods in Poller and EndActionCallBack into
    AsyncCommandCallBack interface.
    
    Change-Id: I4a81f54a2a6eb989948956a73ec0e0919b8422c4
    Bug-Url: https://bugzilla.redhat.com/1083769
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/tasks/interfaces/AsyncCommandCallBack.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/tasks/interfaces/AsyncCommandCallBack.java
@@ -3,13 +3,16 @@ package org.ovirt.engine.core.bll.tasks.interfaces;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.ovirt.engine.core.bll.job.ExecutionContext;
+import org.ovirt.engine.core.common.action.VdcReturnValueBase;
 import org.ovirt.engine.core.common.asynctasks.AsyncTaskCreationInfo;
 import org.ovirt.engine.core.common.businessentities.AsyncTaskStatus;
 import org.ovirt.engine.core.compat.Guid;
 
-public interface Poller {
+public interface AsyncCommandCallBack {
 
     Map<Guid, AsyncTaskStatus> getAllTasksStatuses(Guid storagePoolID);
     ArrayList<AsyncTaskCreationInfo> getAllTasksInfo(Guid storagePoolID);
+    VdcReturnValueBase endAction(SPMTask task, ExecutionContext context);
 
 }
--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/tasks/interfaces/EndActionCallBack.java
+++
@@ -1,8 +0,0 @@
-package org.ovirt.engine.core.bll.tasks.interfaces;
-
-import org.ovirt.engine.core.bll.job.ExecutionContext;
-import org.ovirt.engine.core.common.action.VdcReturnValueBase;
-
-public interface EndActionCallBack {
-    public VdcReturnValueBase endAction(SPMTask task, ExecutionContext context);
-}
