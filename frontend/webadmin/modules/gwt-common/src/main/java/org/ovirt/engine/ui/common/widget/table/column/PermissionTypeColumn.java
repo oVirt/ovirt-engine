298b90227edcb17d0bd6d17bef982876b0a2ed0d
Lior Vernia <lvernia@redhat.com>
2014-06-15 15:17:51 +0300

    webadmin: Implement permission sorting by role icon
    Implemented sorting for permission subtabs according to the role icon
    (ordinary user is lesser than administrator).
    
    Change-Id: I3a2c97315bda060e44b662562e1e08652881d72f
    Signed-off-by: Lior Vernia <lvernia@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/PermissionTypeColumn.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/PermissionTypeColumn.java
@@ -1,5 +1,7 @@
 package org.ovirt.engine.ui.common.widget.table.column;
 
+import java.util.Comparator;
+
 import org.ovirt.engine.core.common.businessentities.Permissions;
 
 import com.google.gwt.resources.client.ImageResource;
@@ -8,6 +10,20 @@ import com.google.gwt.resources.client.ImageResource;
  * Image column that corresponds to XAML {@code PermissionTypeDataTemplate}.
  */
 public class PermissionTypeColumn extends ImageResourceColumn<Permissions> {
+
+    public PermissionTypeColumn() {
+        makeSortable(new Comparator<Permissions>() {
+
+            @Override
+            public int compare(Permissions o1, Permissions o2) {
+                if (getValue(o1).equals(getValue(o2))) {
+                    return 0;
+                } else {
+                    return (getCommonResources().userImage().equals(getValue(o1))) ? -1 : 1;
+                }
+            }
+        });
+    }
 
     @Override
     public ImageResource getValue(Permissions user) {
