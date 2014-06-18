oVirt/ovirt-engine

commit 1bc94d8c9a6dc52eaaef35e93e744fa3425b5c83

core: Introducing runInternalXXX at CommandBase
This patch addds runInternalXXX methods to CommandBase, and changes
ivocation of Backend.getInstance().runInternalXXX where possible

This will give the following benefits:

1. Less code
2. Will add the ability to introduce (during inheritence for example) specific
treatment to runInternalXXX
if needed
3. Ease of mocking

Change-Id: Ia7f3a3283836325eca72f9db3cc3115861a2d555
Signed-off-by: Yair Zaslavsky <yzaslavs@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/ProcessDownVmCommand.java
@@ -158,7 +158,11 @@ public class ProcessDownVmCommand<T extends IdParameters> extends CommandBase<T>
      * Update vm configuration with NEXT_RUN configuration, if exists
      * @param vmId
      */
+<<<<<<< HEAD
     private void applyNextRunConfiguration() {
+=======
+    private void ApplyNextRunConfiguration(Guid vmId) {
+>>>>>>> core: Introducing runInternalXXX at CommandBase
         // Remove snpashot first, in case other update is in progress, it will block this one with exclusive lock
         // and any newer update should be preffered to this one.
         Snapshot runSnap = getSnapshotDAO().get(getVmId(), SnapshotType.NEXT_RUN);
@@ -169,7 +173,12 @@ public class ProcessDownVmCommand<T extends IdParameters> extends CommandBase<T>
             // override creation date because the value in the config is the creation date of the config, not the vm
             getVm().setVmCreationDate(originalCreationDate);
 
+<<<<<<< HEAD
             Backend.getInstance().runInternalAction(VdcActionType.UpdateVm, createUpdateVmParameters());
+=======
+                runInternalAction(VdcActionType.UpdateVm, updateVmParams);
+            }
+>>>>>>> core: Introducing runInternalXXX at CommandBase
         }
     }
 
@@ -220,12 +229,21 @@ public class ProcessDownVmCommand<T extends IdParameters> extends CommandBase<T>
         return updateVmParams;
     }
 
+<<<<<<< HEAD
     private void removeVmStatelessImages() {
         if (getSnapshotDAO().exists(getVmId(), SnapshotType.STATELESS)) {
             log.infoFormat("Deleting snapshot for stateless vm {0}", getVmId());
             Backend.getInstance().runInternalAction(VdcActionType.RestoreStatelessVm,
                     new VmOperationParameterBase(getVmId()),
                     ExecutionHandler.createDefaultContexForTasks(getExecutionContext(), getLock()));
+=======
+    private void removeVmStatelessImages(Guid vmId, CommandContext context) {
+        if (DbFacade.getInstance().getSnapshotDao().exists(vmId, SnapshotType.STATELESS)) {
+            log.infoFormat("Deleting snapshot for stateless vm {0}", vmId);
+            runInternalAction(VdcActionType.RestoreStatelessVm,
+                    new VmOperationParameterBase(vmId),
+                    context);
+>>>>>>> core: Introducing runInternalXXX at CommandBase
         }
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddDiskCommand.java
@@ -422,7 +422,7 @@ public class AddDiskCommand<T extends AddDiskParameters> extends AbstractDiskVmC
             getCompensationContext().stateChanged();
         }
         VdcReturnValueBase tmpRetValue =
-                Backend.getInstance().runInternalAction(VdcActionType.AddImageFromScratch,
+                runInternalAction(VdcActionType.AddImageFromScratch,
                         parameters,
                         ExecutionHandler.createDefaultContexForTasks(getExecutionContext(), getLock()));
         // Setting lock to null because the lock is released in the child command
@@ -566,7 +566,7 @@ public class AddDiskCommand<T extends AddDiskParameters> extends AbstractDiskVmC
         if (Boolean.TRUE.equals(getParameters().getPlugDiskToVm()) && getVm() != null &&  getVm().getStatus() != VMStatus.Down)    {
             HotPlugDiskToVmParameters params = new HotPlugDiskToVmParameters(getVmId(), getParameters().getDiskInfo().getId());
             params.setShouldBeLogged(false);
-            VdcReturnValueBase returnValue = Backend.getInstance().runInternalAction(VdcActionType.HotPlugDiskToVm, params);
+            VdcReturnValueBase returnValue = runInternalAction(VdcActionType.HotPlugDiskToVm, params);
             if (!returnValue.getSucceeded()) {
                 AuditLogDirector.log(this, AuditLogType.USER_FAILED_HOTPLUG_DISK);
             }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddRoleWithActionGroupsCommand.java
@@ -78,7 +78,7 @@ public class AddRoleWithActionGroupsCommand<T extends RoleWithActionGroupsParame
             }
         });
 
-        VdcReturnValueBase attachAction = Backend.getInstance().runInternalAction(
+        VdcReturnValueBase attachAction = runInternalAction(
                 VdcActionType.AttachActionGroupsToRole,
                 new ActionGroupsToRoleParameter(getRole().getId(), getParameters().getActionGroups()));
         if (!attachAction.getCanDoAction() || !attachAction.getSucceeded()) {

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVdsCommand.java
@@ -140,7 +140,7 @@ public class AddVdsCommand<T extends AddVdsActionParameters> extends VdsCommand<
             tempVar.setCompensationEnabled(true);
             CompensationContext compensationContext = getCompensationContext();
             VdcReturnValueBase addVdsSpmIdReturn =
-                    Backend.getInstance().runInternalAction(VdcActionType.AddVdsSpmId,
+                    runInternalAction(VdcActionType.AddVdsSpmId,
                             tempVar,
                             new CommandContext(compensationContext));
             if (!addVdsSpmIdReturn.getSucceeded()) {
@@ -189,7 +189,7 @@ public class AddVdsCommand<T extends AddVdsActionParameters> extends VdsCommand<
             ThreadPoolUtil.execute(new Runnable() {
                 @Override
                 public void run() {
-                    Backend.getInstance().runInternalAction(VdcActionType.InstallVdsInternal,
+                    runInternalAction(VdcActionType.InstallVdsInternal,
                             installVdsParameters,
                             new CommandContext(installCtx));
                 }
@@ -228,7 +228,7 @@ public class AddVdsCommand<T extends AddVdsActionParameters> extends VdsCommand<
                 TransactionSupport.executeInNewTransaction(new TransactionMethod<VdcReturnValueBase>() {
                     @Override
                     public VdcReturnValueBase runInTransaction() {
-                        return Backend.getInstance().runInternalAction(VdcActionType.RemoveVds,
+                        return runInternalAction(VdcActionType.RemoveVds,
                                 new RemoveVdsParameters(oVirtId));
                     }
                 });

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmAndAttachToPoolCommand.java
@@ -55,7 +55,7 @@ public class AddVmAndAttachToPoolCommand<T extends AddVmAndAttachToPoolParameter
         parameters.setSessionId(getParameters().getSessionId());
         parameters.setDontAttachToDefaultTag(true);
 
-        return Backend.getInstance().runInternalAction(VdcActionType.AddVmFromScratch,
+        return runInternalAction(VdcActionType.AddVmFromScratch,
                         parameters,
                         ExecutionHandler.createDefaultContexForTasks(getExecutionContext()));
     }
@@ -78,7 +78,7 @@ public class AddVmAndAttachToPoolCommand<T extends AddVmAndAttachToPoolParameter
             parameters.setRngDevice(getParameters().getRngDevice());
         }
 
-        return Backend.getInstance().runInternalAction(VdcActionType.AddVm,
+        return runInternalAction(VdcActionType.AddVm,
                         parameters,
                         ExecutionHandler.createDefaultContexForTasks(getExecutionContext()));
     }
@@ -87,7 +87,7 @@ public class AddVmAndAttachToPoolCommand<T extends AddVmAndAttachToPoolParameter
         AddVmToPoolParameters parameters = new AddVmToPoolParameters(getParameters().getPoolId(),
                 vmStatic.getId());
         parameters.setShouldBeLogged(false);
-        setSucceeded(Backend.getInstance().runInternalAction(VdcActionType.AddVmToPool, parameters).getSucceeded());
+        setSucceeded(runInternalAction(VdcActionType.AddVmToPool, parameters).getSucceeded());
         addVmPermission();
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmAndCloneImageCommand.java
@@ -252,7 +252,7 @@ public abstract class AddVmAndCloneImageCommand<T extends VmManagementParameters
      * @return
      */
     protected VdcReturnValueBase executeChildCopyingCommand(VdcActionParametersBase parameters) {
-        VdcReturnValueBase result = Backend.getInstance().runInternalAction(
+        VdcReturnValueBase result = runInternalAction(
                 getChildActionType(),
                         parameters,
                         ExecutionHandler.createDefaultContexForTasks(getExecutionContext()));

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmFromScratchCommand.java
@@ -109,7 +109,7 @@ public class AddVmFromScratchCommand<T extends AddVmFromScratchParameters> exten
                 tempVar.setParentCommand(VdcActionType.AddVmFromScratch);
                 tempVar.setEntityInfo(getParameters().getEntityInfo());
                 tempVar.setParentParameters(getParameters());
-                tmpRetValue = Backend.getInstance().runInternalAction(
+                tmpRetValue = runInternalAction(
                                 VdcActionType.AddImageFromScratch,
                                 tempVar,
                                 ExecutionHandler.createDefaultContexForTasks(getExecutionContext()));

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmFromTemplateCommand.java
@@ -93,7 +93,7 @@ public class AddVmFromTemplateCommand<T extends AddVmFromTemplateParameters> ext
             }
             VmHandler.lockVm(getVm().getDynamicData(), getCompensationContext());
             for (DiskImage disk : getVmTemplate().getDiskTemplateMap().values()) {
-                VdcReturnValueBase result = Backend.getInstance().runInternalAction(
+                VdcReturnValueBase result = runInternalAction(
                                 VdcActionType.CreateCloneOfTemplate,
                                 buildCreateCloneOfTemplateParameters(disk),
                                 ExecutionHandler.createDefaultContexForTasks(getExecutionContext()));

commit a65bdb1de631494cdc34fd3ebdb0a8159c2bfd05

bll: fix incorrect merge caused from 1bc94d8c9a6
Change-Id: Ie97c1b93a8f791a4caecc201801a217f89b0e11f
Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/ProcessDownVmCommand.java
@@ -158,11 +158,7 @@ public class ProcessDownVmCommand<T extends IdParameters> extends CommandBase<T>
      * Update vm configuration with NEXT_RUN configuration, if exists
      * @param vmId
      */
-<<<<<<< HEAD
     private void applyNextRunConfiguration() {
-=======
-    private void ApplyNextRunConfiguration(Guid vmId) {
->>>>>>> core: Introducing runInternalXXX at CommandBase
         // Remove snpashot first, in case other update is in progress, it will block this one with exclusive lock
         // and any newer update should be preffered to this one.
         Snapshot runSnap = getSnapshotDAO().get(getVmId(), SnapshotType.NEXT_RUN);
@@ -173,12 +169,7 @@ public class ProcessDownVmCommand<T extends IdParameters> extends CommandBase<T>
             // override creation date because the value in the config is the creation date of the config, not the vm
             getVm().setVmCreationDate(originalCreationDate);
 
-<<<<<<< HEAD
-            Backend.getInstance().runInternalAction(VdcActionType.UpdateVm, createUpdateVmParameters());
-=======
-                runInternalAction(VdcActionType.UpdateVm, updateVmParams);
-            }
->>>>>>> core: Introducing runInternalXXX at CommandBase
+            runInternalAction(VdcActionType.UpdateVm, updateVmParams);
         }
     }
 
@@ -229,21 +220,12 @@ public class ProcessDownVmCommand<T extends IdParameters> extends CommandBase<T>
         return updateVmParams;
     }
 
-<<<<<<< HEAD
     private void removeVmStatelessImages() {
         if (getSnapshotDAO().exists(getVmId(), SnapshotType.STATELESS)) {
             log.infoFormat("Deleting snapshot for stateless vm {0}", getVmId());
-            Backend.getInstance().runInternalAction(VdcActionType.RestoreStatelessVm,
+            runInternalAction(VdcActionType.RestoreStatelessVm,
                     new VmOperationParameterBase(getVmId()),
                     ExecutionHandler.createDefaultContexForTasks(getExecutionContext(), getLock()));
-=======
-    private void removeVmStatelessImages(Guid vmId, CommandContext context) {
-        if (DbFacade.getInstance().getSnapshotDao().exists(vmId, SnapshotType.STATELESS)) {
-            log.infoFormat("Deleting snapshot for stateless vm {0}", vmId);
-            runInternalAction(VdcActionType.RestoreStatelessVm,
-                    new VmOperationParameterBase(vmId),
-                    context);
->>>>>>> core: Introducing runInternalXXX at CommandBase
         }
     }
 }