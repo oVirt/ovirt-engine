54d6a0bba7e9d62f2ec3bd16157ca2387b57ab58
Arik Hadas <ahadas@redhat.com>
2014-06-24 16:56:54 +0300

    core: init os-info before the compensate phase
    Change-Id: I0a66a7d19ad6b2e7d8aaa9341f112a8ed3f0f5da
    Bug-Url: https://bugzilla.redhat.com/1109749
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/Backend.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/Backend.java
@@ -183,6 +183,11 @@ public class Backend implements BackendInternal, BackendCommandObjectsHandler {
     @Override
     public void initialize() {
         log.info("Start initializing " + getClass().getSimpleName());
+        // initialize configuration utils to use DB
+        Config.setConfigUtils(new DBConfigUtils());
+        // we need to initialize os-info before the compensations take place because of VmPoolCommandBase#osRepository
+        initOsRepository();
+
         // When getting a proxy to this bean using JBoss embedded, the initialize method is called for each method
         // invocation on the proxy, as it is called by setup method which is @PostConstruct - the initialized flag
         // makes sure that initialization occurs only once per class (which is ok, as this is a @Service)
@@ -192,8 +197,6 @@ public class Backend implements BackendInternal, BackendCommandObjectsHandler {
             compensate();
             firstInitialization = false;
         }
-        // initialize configuration utils to use DB
-        Config.setConfigUtils(new DBConfigUtils());
 
         log.info("Running ovirt-engine " + Config.<String>getValue(ConfigValues.ProductRPMVersion));
         _resourceManger = new VDSBrokerFrontendImpl();
@@ -211,7 +214,6 @@ public class Backend implements BackendInternal, BackendCommandObjectsHandler {
 
         IsoDomainListSyncronizer.getInstance();
 
-        initOsRepository();
         initSearchDependencies();
         initHandlers();
         initVmPropertiesUtils();
