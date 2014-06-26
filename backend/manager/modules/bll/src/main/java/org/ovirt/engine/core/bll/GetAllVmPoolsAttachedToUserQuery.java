599b34754c22c5fd8232fc5824d92561160a3094
Tomas Jelinek <tjelinek@redhat.com>
2014-06-26 17:00:35 +0200

    core: UP does not load any VMs
    The GetAllVmPoolsAttachedToUserQuery missed one of it's constructors causing
    that the userportal extended view did not load any VMs.
    
    Change-Id: I537a44eb64cb0e9c4fe4f11786fdfa20a6706e30
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllVmPoolsAttachedToUserQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllVmPoolsAttachedToUserQuery.java
@@ -1,11 +1,17 @@
 package org.ovirt.engine.core.bll;
 
+import org.ovirt.engine.core.bll.context.EngineContext;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 
 
 public class GetAllVmPoolsAttachedToUserQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+
+    public GetAllVmPoolsAttachedToUserQuery(P parameters, EngineContext engineContext) {
+        super(parameters, engineContext);
+    }
+
     public GetAllVmPoolsAttachedToUserQuery(P parameters) {
-        super(parameters);
+        this(parameters, null);
     }
 
     @Override
