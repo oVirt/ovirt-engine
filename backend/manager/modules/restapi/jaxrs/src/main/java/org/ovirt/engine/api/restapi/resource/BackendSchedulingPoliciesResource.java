1415b5cc3f5fc2ff901308b8c53b9288b1afdc3e
Gilad Chaplik <gchaplik@redhat.com>
2014-06-26 13:56:14 +0300

    restapi: missing links in scheduling policies resource
    Change-Id: I693e228efc3aeac1cee9f0d7342248713f1c5432
    Bug-Url: https://bugzilla.redhat.com/1113482
    Signed-off-by: Gilad Chaplik <gchaplik@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendSchedulingPoliciesResource.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendSchedulingPoliciesResource.java
@@ -7,8 +7,8 @@ import javax.ws.rs.core.Response;
 
 import org.ovirt.engine.api.model.SchedulingPolicies;
 import org.ovirt.engine.api.model.SchedulingPolicy;
-import org.ovirt.engine.api.resource.SchedulingPolicyResource;
 import org.ovirt.engine.api.resource.SchedulingPoliciesResource;
+import org.ovirt.engine.api.resource.SchedulingPolicyResource;
 import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
@@ -19,7 +19,7 @@ import org.ovirt.engine.core.compat.Guid;
 
 public class BackendSchedulingPoliciesResource extends AbstractBackendCollectionResource<SchedulingPolicy, ClusterPolicy> implements SchedulingPoliciesResource {
 
-    static final String[] SUB_COLLECTIONS = {};
+    static final String[] SUB_COLLECTIONS = { "filters", "weights", "balances", "clusters" };
 
     public BackendSchedulingPoliciesResource() {
         super(SchedulingPolicy.class, ClusterPolicy.class, SUB_COLLECTIONS);
