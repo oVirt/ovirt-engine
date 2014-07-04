2ffa40877c2702586e293bae1a80ccf4883ea452
Martin Mucha <mmucha@redhat.com>
2014-07-04 11:56:36 +0200

    core: AuditLogDirectorTest 'revisit'.
    code was commented out 2 years ago --> this is dead code.
    
    -removing unused code
    -removing TOTO
    
    Change-Id: Ibf2d2358aa6e22ce10795c6f22b3874c43e814ab
    Signed-off-by: Martin Mucha <mmucha@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dal/dbbroker/auditloghandling/AuditLogDirectorTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dal/dbbroker/auditloghandling/AuditLogDirectorTest.java
@@ -1,10 +1,5 @@
 package org.ovirt.engine.core.dal.dbbroker.auditloghandling;
 
-/**
- * TODO:
- * Commented out test class in order to cancel dependency on PowerMock
- * This should be revisited.
- */
 import static org.mockito.Mockito.RETURNS_DEFAULTS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -16,64 +11,8 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-//@RunWith(PowerMockRunner.class)
-//@PrepareForTest({ AuditLogDirector.class })
 public class AuditLogDirectorTest {
-//
-//    @Mock
-//    DbFacade dbFacade;
-//    @Mock
-//    AuditLogDAO auditLogDao;
-//
-//    @Before
-//    public void initMocks() {
-//        initAuditLogDirectorMock();
-//        initDbFacadeMock();
-//    }
-//
-//    private void initDbFacadeMock() {
-//        when(dbFacade.getAuditLogDAO()).thenReturn(auditLogDao);
-//    }
-//
-//    private void initAuditLogDirectorMock() {
-//        PowerMockito.spy(AuditLogDirector.class);
-//        PowerMockito.when(AuditLogDirector.getDbFacadeInstance()).thenReturn(dbFacade);
-//    }
-//
-//
-//    /**
-//     * The test assures that audit loggable objects with timeout, which were created without an explicit log type, with
-//     * a common key parts, except of the log type, are treated separately.<br>
-//     * The test invokes two {@Code AuditLogDirector.log()} calls and verifies that each call insert an entry into
-//     * the database.<br>
-//     */
-    // @Test
-    // public void testLegalAuditLog() {
-    // AuditLogableBase logableObject1 = new AuditLogableBase();
-    // AuditLogDirector.log(logableObject1, AuditLogType.IRS_DISK_SPACE_LOW);
-    //
-    // AuditLogableBase logableObject2 = new AuditLogableBase();
-    // AuditLogDirector.log(logableObject2, AuditLogType.IRS_DISK_SPACE_LOW_ERROR);
-    // Mockito.verify(auditLogDao, Mockito.times(2)).save(Mockito.any(AuditLog.class));
-    // }
-//
-//    /**
-//     * The test assures that audit loggable objects with timeout, which were created without an explicit log type and
-//     * share the same key are treated in respect to each other by the timeout gaps between events.<br>
-//     * The test invokes two {@Code AuditLogDirector.log()} calls and verify that only one call inserts an entry
-//     * into the database.
-//     */
-//    @Test
-//    public void testIllegalAuditLog() {
-//        AuditLogableBase logableObject1 = new AuditLogableBase();
-//        AuditLogDirector.log(logableObject1, AuditLogType.VDS_SLOW_STORAGE_RESPONSE_TIME);
-//        Mockito.verify(auditLogDao, Mockito.times(1)).save(Mockito.any(AuditLog.class));
-//
-//        AuditLogDirector.log(logableObject1, AuditLogType.VDS_SLOW_STORAGE_RESPONSE_TIME);
-//        Mockito.verify(auditLogDao, Mockito.times(1)).save(Mockito.any(AuditLog.class));
-//    }
-//
+
     @Test
     public void testResolveUnknownVariable() {
         final String message = "This is my ${Variable}";
