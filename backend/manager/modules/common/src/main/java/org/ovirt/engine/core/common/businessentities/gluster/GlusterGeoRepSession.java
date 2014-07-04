fca73f8eeeac6ed2c26ac27b9ae340dee3ee3326
Anmol Babu <anbabu@redhat.com>
2014-07-04 23:17:54 +0530

    engine: gluster volume geo replication entities
    Gluster volume geo replication entities
    
    Change-Id: If86abee9276b8f2e4bdd4a4276dbbd76b9514c68
    Signed-off-by: Anmol Babu <anbabu@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/gluster/GlusterGeoRepSession.java
@@ -0,0 +1,121 @@
+package org.ovirt.engine.core.common.businessentities.gluster;
+
+import org.ovirt.engine.core.common.businessentities.BusinessEntityWithStatus;
+import org.ovirt.engine.core.common.businessentities.IVdcQueryable;
+import org.ovirt.engine.core.common.utils.ObjectUtils;
+import org.ovirt.engine.core.compat.Guid;
+
+public class GlusterGeoRepSession extends IVdcQueryable implements BusinessEntityWithStatus<Guid, GeoRepSessionStatus>{
+
+    private static final long serialVersionUID = 1L;
+
+    private Guid masterVolumeId;
+    private String sessionKey;
+    private String slaveHostName;
+    private Guid slaveNodeUuid;
+    private String slaveVolumeName;
+    private Guid sessionId;
+    private Guid slaveVolumeId;
+    private GeoRepSessionStatus status;
+
+    public Guid getMasterVolumeId() {
+        return masterVolumeId;
+    }
+
+    public void setMasterVolumeId(Guid masterVolumeId) {
+        this.masterVolumeId = masterVolumeId;
+    }
+
+    public String getSessionKey() {
+        return sessionKey;
+    }
+
+    public void setSessionKey(String sessionKey) {
+        this.sessionKey = sessionKey;
+    }
+
+    public String getSlaveHostName() {
+        return slaveHostName;
+    }
+
+    public void setSlaveHostName(String slaveHostName) {
+        this.slaveHostName = slaveHostName;
+    }
+
+    public Guid getSlaveNodeUuid() {
+        return slaveNodeUuid;
+    }
+
+    public void setSlaveNodeUuid(Guid slaveNodeUuid) {
+        this.slaveNodeUuid = slaveNodeUuid;
+    }
+
+    public String getSlaveVolumeName() {
+        return slaveVolumeName;
+    }
+
+    public void setSlaveVolumeName(String slaveVolumeName) {
+        this.slaveVolumeName = slaveVolumeName;
+    }
+
+    public Guid getSlaveVolumeId() {
+        return slaveVolumeId;
+    }
+
+    public void setSlaveVolumeId(Guid slaveVolumeId) {
+        this.slaveVolumeId = slaveVolumeId;
+    }
+
+    @Override
+    public GeoRepSessionStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public void setStatus(GeoRepSessionStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public Guid getId() {
+        return sessionId;
+    }
+
+    @Override
+    public void setId(Guid id) {
+        this.sessionId = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((masterVolumeId == null) ? 0 : masterVolumeId.hashCode());
+        result = prime * result + ((sessionId == null) ? 0 : sessionId.hashCode());
+        result = prime * result + ((sessionKey == null) ? 0 : sessionKey.hashCode());
+        result = prime * result + ((slaveHostName == null) ? 0 : slaveHostName.hashCode());
+        result = prime * result + ((slaveNodeUuid == null) ? 0 : slaveNodeUuid.hashCode());
+        result = prime * result + ((slaveVolumeId == null) ? 0 : slaveVolumeId.hashCode());
+        result = prime * result + ((slaveVolumeName == null) ? 0 : slaveVolumeName.hashCode());
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj != null) && (obj instanceof GlusterGeoRepSession)) {
+            GlusterGeoRepSession session = (GlusterGeoRepSession) obj;
+            if (ObjectUtils.objectsEqual(getId(), session.getId())
+                    && (ObjectUtils.objectsEqual(getMasterVolumeId(), session.getMasterVolumeId()))
+                    && (ObjectUtils.objectsEqual(getSessionKey(), session.getSessionKey()))
+                    && (ObjectUtils.objectsEqual(getSlaveHostName(), session.getSlaveHostName()))
+                    && (ObjectUtils.objectsEqual(getSlaveNodeUuid(), session.getSlaveNodeUuid()))
+                    && (ObjectUtils.objectsEqual(getSlaveVolumeId(), session.getSlaveVolumeId()))
+                    && (ObjectUtils.objectsEqual(getSlaveVolumeName(), session.getSlaveVolumeName()))
+                    && (ObjectUtils.objectsEqual(getStatus(), session.getStatus()))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/gluster/GlusterGeoRepSessionDetails.java
@@ -0,0 +1,171 @@
+package org.ovirt.engine.core.common.businessentities.gluster;
+
+import java.util.Date;
+
+import org.ovirt.engine.core.common.businessentities.BusinessEntity;
+import org.ovirt.engine.core.common.utils.ObjectUtils;
+import org.ovirt.engine.core.compat.Guid;
+
+public class GlusterGeoRepSessionDetails implements BusinessEntity<Guid>{
+
+    private static final long serialVersionUID = -8084667500866439692L;
+
+    private Guid sessionId;
+    private Guid masterBrickId;
+    private Guid slaveNodeUuid;
+    private String slaveHostName;
+    private GeoRepSessionStatus status;
+    private String checkPointStatus;
+    private GeoRepCrawlStatus crawlStatus;
+    private Long filesSynced;
+    private Long filesPending;
+    private Long bytesPending;
+    private Long deletesPending;
+    private Long filesSkipped;
+    private Date updatedAt;
+
+    public Long getFilesSynced() {
+        return filesSynced;
+    }
+
+    public Guid getMasterBrickId() {
+        return masterBrickId;
+    }
+
+    public void setMasterBrickId(Guid masterBrickId) {
+        this.masterBrickId = masterBrickId;
+    }
+
+    public Guid getSlaveNodeUuid() {
+        return slaveNodeUuid;
+    }
+
+    public void setSlaveNodeUuid(Guid slaveNodeUuid) {
+        this.slaveNodeUuid = slaveNodeUuid;
+    }
+
+    public String getSlaveHostName() {
+        return slaveHostName;
+    }
+
+    public void setSlaveHostName(String slaveNodeName) {
+        this.slaveHostName = slaveNodeName;
+    }
+
+    public GeoRepSessionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GeoRepSessionStatus status) {
+        this.status = status;
+    }
+
+    public String getCheckPointStatus() {
+        return checkPointStatus;
+    }
+
+    public void setCheckPointStatus(String checkPointStatus) {
+        this.checkPointStatus = checkPointStatus;
+    }
+
+    public GeoRepCrawlStatus getCrawlStatus() {
+        return crawlStatus;
+    }
+
+    public void setCrawlStatus(GeoRepCrawlStatus crawlStatus) {
+        this.crawlStatus = crawlStatus;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setFilesSynced(Long filesSynced) {
+        this.filesSynced = filesSynced;
+    }
+
+    public Long getFilesPending() {
+        return filesPending;
+    }
+
+    public void setFilesPending(Long filesPending) {
+        this.filesPending = filesPending;
+    }
+
+    public Long getBytesPending() {
+        return bytesPending;
+    }
+
+    public void setBytesPending(Long bytesPending) {
+        this.bytesPending = bytesPending;
+    }
+
+    public Long getDeletesPending() {
+        return deletesPending;
+    }
+
+    public void setDeletesPending(Long deletesPending) {
+        this.deletesPending = deletesPending;
+    }
+
+    public Long getFilesSkipped() {
+        return filesSkipped;
+    }
+
+    public void setFilesSkipped(Long filesSkipped) {
+        this.filesSkipped = filesSkipped;
+    }
+
+    @Override
+    public Guid getId() {
+        return sessionId;
+    }
+
+    @Override
+    public void setId(Guid id) {
+        this.sessionId = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj != null) &&
+                (obj instanceof GlusterGeoRepSessionDetails) &&
+                ObjectUtils.objectsEqual(getId(), ((GlusterGeoRepSessionDetails) obj).getId()) &&
+                ObjectUtils.objectsEqual(getMasterBrickId(), ((GlusterGeoRepSessionDetails) obj).getMasterBrickId()) &&
+                ObjectUtils.objectsEqual(getSlaveNodeUuid(), ((GlusterGeoRepSessionDetails) obj).getSlaveNodeUuid()) &&
+                ObjectUtils.objectsEqual(getSlaveHostName(), ((GlusterGeoRepSessionDetails) obj).getSlaveHostName()) &&
+                ObjectUtils.objectsEqual(getStatus(), ((GlusterGeoRepSessionDetails) obj).getStatus()) &&
+                ObjectUtils.objectsEqual(getCheckPointStatus(), ((GlusterGeoRepSessionDetails) obj).getCheckPointStatus()) &&
+                ObjectUtils.objectsEqual(getCrawlStatus(), ((GlusterGeoRepSessionDetails) obj).getCrawlStatus()) &&
+                ObjectUtils.objectsEqual(getFilesSynced(), ((GlusterGeoRepSessionDetails) obj).getFilesSynced()) &&
+                ObjectUtils.objectsEqual(getFilesPending(), ((GlusterGeoRepSessionDetails) obj).getFilesPending()) &&
+                ObjectUtils.objectsEqual(getFilesSkipped(), ((GlusterGeoRepSessionDetails) obj).getFilesSkipped()) &&
+                ObjectUtils.objectsEqual(getBytesPending(), ((GlusterGeoRepSessionDetails) obj).getBytesPending()) &&
+                ObjectUtils.objectsEqual(getDeletesPending(), ((GlusterGeoRepSessionDetails) obj).getDeletesPending()) &&
+                ObjectUtils.objectsEqual(getUpdatedAt(), ((GlusterGeoRepSessionDetails) obj).getUpdatedAt());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((sessionId == null) ? 0 : sessionId.hashCode());
+        result = prime * result + ((masterBrickId == null) ? 0 : masterBrickId.hashCode());
+        result = prime * result + ((slaveNodeUuid == null) ? 0 : slaveNodeUuid.hashCode());
+        result = prime * result + ((slaveHostName == null) ? 0 : slaveHostName.hashCode());
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        result = prime * result + ((checkPointStatus == null) ? 0 : checkPointStatus.hashCode());
+        result = prime * result + ((crawlStatus == null) ? 0 : crawlStatus.hashCode());
+        result = prime * result + ((filesSynced == null) ? 0 : filesSynced.hashCode());
+        result = prime * result + ((filesPending == null) ? 0 : filesPending.hashCode());
+        result = prime * result + ((filesSkipped == null) ? 0 : filesSkipped.hashCode());
+        result = prime * result + ((bytesPending == null) ? 0 : bytesPending.hashCode());
+        result = prime * result + ((deletesPending == null) ? 0 : deletesPending.hashCode());
+        result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+        return result;
+    }
+}
