168de7e6827617096d1b9af72381a00e590088a8
Tal Nisan <tnisan@redhat.com>
2014-07-09 17:32:20 +0300

    core: Add VdcActionType tests
    Added tests to:
    - Validate that every command values is unique
    - Validate that every command type in VdcActionType corresponds to an
      existing command class
    
    Change-Id: I53b51b359e14e2f1ebb6e518846f1cf00c8bbe6b
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/VdcActionTypeTest.java
@@ -0,0 +1,46 @@
+package org.ovirt.engine.core.bll;
+
+import org.junit.Test;
+import org.ovirt.engine.core.common.action.VdcActionType;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.junit.Assert.assertTrue;
+
+public class VdcActionTypeTest {
+    private static final int bitsetSize = 5000;
+
+    @Test
+    public void testAuditLogTypeValueUniqueness() {
+        BitSet bitset = new BitSet(bitsetSize);
+        Set<Integer> nonUniqueValues = new TreeSet<Integer>();
+
+        for (VdcActionType vat : VdcActionType.values()) {
+            if (bitset.get(vat.getValue())) {
+                nonUniqueValues.add(vat.getValue());
+            }
+            else {
+                bitset.set(vat.getValue());
+            }
+        }
+        assertTrue("VdcActionType contains the following non unique values: " + nonUniqueValues, nonUniqueValues.isEmpty());
+    }
+
+    @Test
+    public void testCommandClassExistance() {
+        List<VdcActionType> missingCommands = new ArrayList<>();
+        for (VdcActionType vat : VdcActionType.values()) {
+            if (vat != VdcActionType.Unknown) {
+                if (CommandsFactory.getCommandClass(vat.name()) == null){
+                    missingCommands.add(vat);
+                }
+            }
+        }
+        assertTrue("VdcActionType contains the following values that does not correspond to an existing command class: " + missingCommands,
+                missingCommands.isEmpty());
+    }
+}
