34e681801ea96ca1ab3a6a70f9ff6a94da491565
Daniel Erez <derez@redhat.com>
2014-07-13 19:33:21 +0300

    webadmin: disks sub-tab - fix columns creation
    Following sorting support, disks columns instantiation
    should be ensured to happen once. Otherwise, the columns get
    duplicated on type navigation (radio-buttons of all/images/directlun).
    
    Hence, applied the same solution introduced in [1] on MainTabDiskView
    to disks sub-tab as well.
    
    [1] http://gerrit.ovirt.org/29375/
    
    Change-Id: Idc774e26c9a3d954e9d7b8a4fee9206358269926
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/vm/BaseVmDiskListModelTable.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/vm/BaseVmDiskListModelTable.java
@@ -5,6 +5,8 @@ import org.ovirt.engine.core.common.businessentities.Disk.DiskStorageType;
 import org.ovirt.engine.ui.common.CommonApplicationConstants;
 import org.ovirt.engine.ui.common.system.ClientStorage;
 import org.ovirt.engine.ui.common.uicommon.model.SearchableTableModelProvider;
+import org.ovirt.engine.ui.common.widget.table.column.DiskSizeColumn;
+import org.ovirt.engine.ui.common.widget.table.column.TextColumnWithTooltip;
 import org.ovirt.engine.ui.common.widget.uicommon.AbstractModelBoundTableWidget;
 import org.ovirt.engine.ui.common.widget.uicommon.disks.DisksViewColumns;
 import org.ovirt.engine.ui.common.widget.uicommon.disks.DisksViewRadioGroup;
@@ -22,6 +24,19 @@ public class BaseVmDiskListModelTable<T extends VmDiskListModelBase> extends Abs
 
     private CommonApplicationConstants constants;
     private DisksViewRadioGroup disksViewRadioGroup;
+
+    private static TextColumnWithTooltip<Disk> aliasColumn;
+    private static DiskSizeColumn sizeColumn;
+    private static DiskSizeColumn actualSizeColumn;
+    private static TextColumnWithTooltip<Disk> allocationColumn;
+    private static TextColumnWithTooltip<Disk> dateCreatedColumn;
+    private static TextColumnWithTooltip<Disk> statusColumn;
+    private static TextColumnWithTooltip<Disk> lunIdColumn;
+    private static TextColumnWithTooltip<Disk> lunSerialColumn;
+    private static TextColumnWithTooltip<Disk> lunVendorIdColumn;
+    private static TextColumnWithTooltip<Disk> lunProductIdColumn;
+    private static TextColumnWithTooltip<Disk> interfaceColumn;
+    private static TextColumnWithTooltip<Disk> descriptionColumn;
 
     public BaseVmDiskListModelTable(
             SearchableTableModelProvider<Disk, T> modelProvider,
@@ -52,6 +67,7 @@ public class BaseVmDiskListModelTable<T extends VmDiskListModelBase> extends Abs
     public void initTable(CommonApplicationConstants constants) {
         this.constants = constants;
 
+        initTableColumns();
         initTableOverhead();
         handleRadioButtonClick(null);
 
@@ -73,13 +89,11 @@ public class BaseVmDiskListModelTable<T extends VmDiskListModelBase> extends Abs
         getModel().setItems(null);
         getModel().search();
 
-        getTable().enableColumnResizing();
-
         getTable().ensureColumnPresent(
                 DisksViewColumns.diskStatusColumn, constants.empty(), all || images || luns, "30px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getAliasColumn(null), constants.aliasDisk(), all || images || luns, "120px"); //$NON-NLS-1$
+                aliasColumn, constants.aliasDisk(), all || images || luns, "120px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
                 DisksViewColumns.bootableDiskColumn,
@@ -98,13 +112,13 @@ public class BaseVmDiskListModelTable<T extends VmDiskListModelBase> extends Abs
                 DisksViewColumns.lunDiskColumn.getHeaderHtml(), all, "30px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getSizeColumn(null), constants.provisionedSizeDisk(), all || images || luns, "110px"); //$NON-NLS-1$
+                sizeColumn, constants.provisionedSizeDisk(), all || images || luns, "110px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getActualSizeColumn(null), constants.sizeDisk(), images, "110px"); //$NON-NLS-1$
+                actualSizeColumn, constants.sizeDisk(), images, "110px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getAllocationColumn(null), constants.allocationDisk(), images, "125px"); //$NON-NLS-1$
+                allocationColumn, constants.allocationDisk(), images, "125px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
                 DisksViewColumns.storageDomainsColumn, constants.storageDomainDisk(), images, "125px"); //$NON-NLS-1$
@@ -113,34 +127,51 @@ public class BaseVmDiskListModelTable<T extends VmDiskListModelBase> extends Abs
                 DisksViewColumns.storageTypeColumn, constants.storageTypeDisk(), images, "100px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getDateCreatedColumn(null), constants.creationDateDisk(), images, "120px"); //$NON-NLS-1$
+                dateCreatedColumn, constants.creationDateDisk(), images, "120px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getLunIdColumn(null), constants.lunIdSanStorage(), luns, "130px"); //$NON-NLS-1$
+                lunIdColumn, constants.lunIdSanStorage(), luns, "130px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getLunSerialColumn(null), constants.serialSanStorage(), luns, "130px"); //$NON-NLS-1$
+                lunSerialColumn, constants.serialSanStorage(), luns, "130px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getLunVendorIdColumn(null), constants.vendorIdSanStorage(), luns, "130px"); //$NON-NLS-1$
+                lunVendorIdColumn, constants.vendorIdSanStorage(), luns, "130px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getLunProductIdColumn(null), constants.productIdSanStorage(), luns, "130px"); //$NON-NLS-1$
+                lunProductIdColumn, constants.productIdSanStorage(), luns, "130px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
                 DisksViewColumns.diskContainersColumn, constants.attachedToDisk(), all || images || luns, "110px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getInterfaceColumn(null), constants.interfaceDisk(), all || images || luns, "100px"); //$NON-NLS-1$
+                interfaceColumn, constants.interfaceDisk(), all || images || luns, "100px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
                 DisksViewColumns.diskAlignmentColumn, constants.diskAlignment(), all || images || luns, "100px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getStatusColumn(null), constants.statusDisk(), images, "80px"); //$NON-NLS-1$
+                statusColumn, constants.statusDisk(), images, "80px"); //$NON-NLS-1$
 
         getTable().ensureColumnPresent(
-                DisksViewColumns.getDescriptionColumn(null), constants.descriptionDisk(), all || images || luns, "90px"); //$NON-NLS-1$
+                descriptionColumn, constants.descriptionDisk(), all || images || luns, "90px"); //$NON-NLS-1$
 
+    }
+
+    void initTableColumns() {
+        getTable().enableColumnResizing();
+
+        aliasColumn = DisksViewColumns.getAliasColumn(null);
+        sizeColumn = DisksViewColumns.getSizeColumn(null);
+        actualSizeColumn = DisksViewColumns.getActualSizeColumn(null);
+        allocationColumn = DisksViewColumns.getAllocationColumn(null);
+        dateCreatedColumn = DisksViewColumns.getDateCreatedColumn(null);
+        statusColumn = DisksViewColumns.getStatusColumn(null);
+        lunIdColumn = DisksViewColumns.getLunIdColumn(null);
+        lunSerialColumn = DisksViewColumns.getLunSerialColumn(null);
+        lunVendorIdColumn = DisksViewColumns.getLunVendorIdColumn(null);
+        lunProductIdColumn = DisksViewColumns.getLunProductIdColumn(null);
+        interfaceColumn = DisksViewColumns.getInterfaceColumn(null);
+        descriptionColumn = DisksViewColumns.getDescriptionColumn(null);
     }
 }
