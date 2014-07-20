58b0d3bfba91251c98008e701ffaf86eb532912e
Idan Shaby <ishaby@redhat.com>
2014-07-20 16:39:28 +0300

    core: Removing Memory Volumes Before VM and Disks
    removeMemoryVolumes queries the VM and its disks and thus should come
    before we remove them.
    Note that removeMemoryVolumes can't be inside the transaction because
    it's not a pure DB operation, but also contains a VDSM (SPM) call.
    
    Change-Id: I24851385ab1c27a1da7fe8be04cfd9d8fb744b3f
    Signed-off-by: Idan Shaby <ishaby@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RemoveVmCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RemoveVmCommand.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.job.ExecutionContext;
 import org.ovirt.engine.core.bll.job.ExecutionHandler;
+import org.ovirt.engine.core.bll.memory.MemoryUtils;
 import org.ovirt.engine.core.bll.network.ExternalNetworkManager;
 import org.ovirt.engine.core.bll.quota.QuotaConsumptionParameter;
 import org.ovirt.engine.core.bll.quota.QuotaStorageConsumptionParameter;
@@ -50,8 +51,6 @@ import org.ovirt.engine.core.utils.transaction.TransactionSupport;
 @LockIdNameAttribute
 @NonTransactiveCommandAttribute(forceCompensation = true)
 public class RemoveVmCommand<T extends RemoveVmParameters> extends VmCommand<T> implements QuotaStorageDependent, TaskHandlerCommand<RemoveVmParameters> {
-
-    private Set<String> memoryStates;
 
     /**
      * Constructor for command creation when compensation is applied on startup
@@ -95,6 +94,8 @@ public class RemoveVmCommand<T extends RemoveVmParameters> extends VmCommand<T> 
             new ExternalNetworkManager(nic).deallocateIfExternal();
         }
 
+        removeMemoryVolumes();
+
         TransactionSupport.executeInNewTransaction(new TransactionMethod<Void>() {
             @Override
             public Void runInTransaction() {
@@ -123,12 +124,12 @@ public class RemoveVmCommand<T extends RemoveVmParameters> extends VmCommand<T> 
             }
         }
 
-        removeMemoryVolumes();
-
         return true;
     }
 
     private void removeMemoryVolumes() {
+        Set<String> memoryStates =
+                MemoryUtils.getMemoryVolumesFromSnapshots(getDbFacade().getSnapshotDao().getAll(getVmId()));
         for (String memoryState : memoryStates) {
             VdcReturnValueBase retVal = getBackend().runInternalAction(
                     VdcActionType.RemoveMemoryVolumes,
@@ -310,7 +311,7 @@ public class RemoveVmCommand<T extends RemoveVmParameters> extends VmCommand<T> 
         removeLunDisks();
         removeVmUsers();
         removeVmNetwork();
-        memoryStates = removeVmSnapshots();
+        removeVmSnapshots();
         removeVmStatic(getParameters().isRemovePermissions());
     }
 
