0d6d6c994547381dd8835e0384d52e589e4f49c7
Kobi Ianko <kobi@redhat.com>
2014-07-24 15:20:18 +0300

    core: added missing events to event_map
    The VDS_ACTIVATE_MANUAL_HA and CLUSTER_ALERT_HA_RESERVATION_DOWN
    were missing from EVENT_MAP table, causing error in the notification mechanism.
    
    Change-Id: I6e5efecc1f9cc9dc8bbcf879d7c172cde82a8dc6
    Bug-Url: https://bugzilla.redhat.com/1119285
    Signed-off-by: Kobi Ianko <kobi@redhat.com>
    Signed-off-by: Jiri Moskovcak <jmoskovc@redhat.com>

---
+++ packaging/dbscripts/upgrade/03_06_0340_add_vds_activate_manual_ha_to_event_map.sql
@@ -0,0 +1,2 @@
+insert into event_map (event_up_name, event_down_name) values ('VDS_ACTIVATE_MANUAL_HA', 'UNASSIGNED');
+insert into event_map (event_up_name, event_down_name) values ('CLUSTER_ALERT_HA_RESERVATION_DOWN', 'UNASSIGNED');
