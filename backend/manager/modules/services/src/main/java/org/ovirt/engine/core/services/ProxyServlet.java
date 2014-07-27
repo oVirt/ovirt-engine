9dde7a02fd3fe90b0c4334ec7ed94e832daca3d9
Alon Bar-Lev <alonbl@redhat.com>
2014-07-27 15:56:57 +0300

    services: add proxy servlet
    a proxy servlet that proxy into url appending query string.
    
    example of usage:
    
    <servlet>
      <servlet-name>google1</servlet-name>
      <servlet-class>org.ovirt.engine.core.services.ProxyServlet</servlet-class>
      <init-param>
        <param-name>url</param-name>
        <param-value>https://www.google.com/</param-value>
      </init-param>
      <init-param>
        <param-name>trustStoreType</param-name>
        <param-value>%{ENGINE_PKI_TRUST_STORE_TYPE}</param-value>
      </init-param>
      <init-param>
        <param-name>trustStore</param-name>
        <param-value>%{ENGINE_PKI_TRUST_STORE}</param-value>
      </init-param>
      <init-param>
        <param-name>trustStorePassword</param-name>
        <param-value>%{ENGINE_PKI_TRUST_STORE_PASSWORD}</param-value>
      </init-param>
    </servlet>
    <servlet-mapping>
      <servlet-name>google1</servlet-name>
      <url-pattern>/google1</url-pattern>
    </servlet-mapping>
    
    Change-Id: Ia26768ec7a0e06a20f657cae40f6a51303970b44
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

---
+++ backend/manager/modules/services/src/main/java/org/ovirt/engine/core/services/ProxyServlet.java
@@ -0,0 +1,65 @@
+package org.ovirt.engine.core.services;
+
+import javax.servlet.ServletException;
+
+import org.apache.commons.lang.StringUtils;
+import org.ovirt.engine.core.utils.EngineLocalConfig;
+import org.ovirt.engine.core.uutils.servlet.ProxyServletBase;
+
+public class ProxyServlet extends ProxyServletBase {
+
+    private static final long serialVersionUID = 209763132892410985L;
+
+    private static final String VERIFY_HOST_PRM = "verifyHost";
+    private static final String VERIFY_CHAIN_PRM = "verifyChain";
+    private static final String HTTPS_PROTOCOL_PRM = "httpsProtocol";
+    private static final String TRUST_MANAGER_ALGORITHM_PRM = "trustManagerAlgorithm";
+    private static final String TRUST_STORE_PRM = "trustStore";
+    private static final String TRUST_STORE_TYPE_PRM = "trustStoreType";
+    private static final String TRUST_STORE_PASSWORD_PRM = "trustStorePassword";
+    private static final String READ_TIMEOUT_PRM = "readTimeout";
+    private static final String URL_PRM = "url";
+
+    private String getConfigString(String name) {
+        String r = getServletConfig().getInitParameter(name);
+        if (r != null) {
+            r = EngineLocalConfig.getInstance().expandString(r.replaceAll("%\\{", "\\${"));
+        }
+        if (StringUtils.isEmpty(r)) {
+            r = null;
+        }
+        return r;
+    }
+
+    private Boolean getConfigBoolean(String name) {
+        Boolean r = null;
+        String s = getConfigString(name);
+        if (s != null) {
+            r = Boolean.valueOf(s);
+        }
+        return r;
+    }
+
+    private Integer getConfigInteger(String name) {
+        Integer r = null;
+        String s = getConfigString(name);
+        if (s != null) {
+            r = Integer.valueOf(s);
+        }
+        return r;
+    }
+
+    @Override
+    public void init() throws ServletException {
+        setVerifyHost(getConfigBoolean(VERIFY_HOST_PRM));
+        setVerifyChain(getConfigBoolean(VERIFY_CHAIN_PRM));
+        setHttpsProtocol(getConfigString(HTTPS_PROTOCOL_PRM));
+        setTrustManagerAlgorithm(getConfigString(TRUST_MANAGER_ALGORITHM_PRM));
+        setTrustStore(getConfigString(TRUST_STORE_PRM));
+        setTrustStoreType(getConfigString(TRUST_STORE_TYPE_PRM));
+        setTrustStorePassword(getConfigString(TRUST_STORE_PASSWORD_PRM));
+        setReadTimeout(getConfigInteger(READ_TIMEOUT_PRM));
+        setUrl(getConfigString(URL_PRM));
+    }
+
+}
