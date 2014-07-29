3e7e9825b0e3e20be5a54efa62656b39d9e88aac
Tomas Jelinek <tjelinek@redhat.com>
2014-07-29 10:26:12 +0200

    webadmin: VM's template information is inconsistent after upgrading to 3.4
    Having a thin provisioned VM created in 3.3 and updated it to 3.5 the VM
    General subtab showed:
    "TemplateName"
    But if created in 3.4 and later it showed:
    "TemplateName (Thin/Dependent)"
    
    This patch unifies it so also the older VMs show up in the new format.
    
    Change-Id: Ie55359039cf45a758f132e8c797f033f73222457
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1084940
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/VmTemplateNameRenderer.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/VmTemplateNameRenderer.java
@@ -12,6 +12,12 @@ public class VmTemplateNameRenderer {
     public String render(VM vm) {
         if (vm.getOriginalTemplateName() == null) {
             // fallback for VMs created before adding the support for original template
+            if (!Guid.Empty.equals(vm.getVmtGuid())) {
+                // it is a thin provisioned VM, just the original template is not filled
+                return messages.vmTemplateWithThinProvisioning(vm.getVmtName());
+            }
+
+            // no information if it was based on original template or a blank template - falling back show Blank as it was before 3.4
             return vm.getVmtName();
         } else if (Guid.Empty.equals(vm.getOriginalTemplateGuid())) {
             // created from blank template - don't wrap with the message
