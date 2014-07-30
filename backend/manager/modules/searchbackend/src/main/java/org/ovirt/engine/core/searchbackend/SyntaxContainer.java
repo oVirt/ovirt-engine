f6007e9a9b7c6921add6411ee2a3502835a78391
Daniel Erez <derez@redhat.com>
2014-07-30 15:18:59 +0300

    core: fixed search Disks when filtered by data-center
    Similar to fix in chnage-id I04e37501b29a17a634e31e8875d63577e5f9f401
    (and following change-id I862873171c6753f8c8863c10c336e981e39dc8cb),
    searching disks (or clusters) by a data-center looks like [1].
    
    I.e. the left outer join is being made on
    storage_pool_with_storage_domains; which leads to multiple
    records since a data-center can contain multiple storage domains.
    
    Therefore, reverted to previous search so it uses the
    'use-tag' syntax [2]. As a result, a similar issue with
    search Clusters when filtered by data-center has been
    fixed as well.
    
    [1]
    SELECT all_disks.* FROM all_disks
    LEFT OUTER JOIN storage_pool_with_storage_domain ON
    all_disks.storage_pool_id=storage_pool_with_storage_domain.id
    WHERE  storage_pool_with_storage_domain.name ILIKE 'dc35'
    AND all_disks.disk_storage_type = '0'
    
    [2]
    SELECT * FROM all_disks
    WHERE (disk_id IN (SELECT all_disks.disk_id FROM all_disks
    LEFT OUTER JOIN storage_pool_with_storage_domain ON
    all_disks.storage_pool_id=storage_pool_with_storage_domain.id
    WHERE (storage_pool_with_storage_domain.name ILIKE 'dc35'
    AND all_disks.disk_storage_type = '0')))
    
    Change-Id: I3a792fb63421c7589ed073fb18cfaac46a7dd3a9
    Bug-Url: https://bugzilla.redhat.com/1124584
    Bug-Url: https://bugzilla.redhat.com/1124593
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/SyntaxContainer.java
+++ backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/SyntaxContainer.java
@@ -23,6 +23,7 @@ public class SyntaxContainer implements Iterable<SyntaxObject> {
     public boolean isSearchUsingTags() {
         return mOrigText.contains("tag")
                 || (getSearchObjectStr() != null && (getSearchObjectStr().equals(SearchObjects.VDC_USER_OBJ_NAME)))
+                || (getCrossRefObjList().contains(SearchObjects.VDC_STORAGE_POOL_OBJ_NAME))
                 || (getCrossRefObjList().contains(SearchObjects.VDC_STORAGE_DOMAIN_OBJ_NAME));
     }
 
