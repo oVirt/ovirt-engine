oVirt/ovirt-engine

commit 7bb86ca81ace8960f0d34ab14fe85a93a4541b02

core: Add audit log when removing networks from iSCSI bond.
Adding an audit log to indicate the user that the removed networks
from the iSCSI bond, will only take affect, after the Hosts will be
moved to maintenance and activated again.

Change-Id: I087818bb48cc9243e76b2cb850e6262104537f3c
Bug-Url: https://bugzilla.redhat.com/1126428
Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/AuditLogType.java
@@ -210,6 +210,7 @@ public enum AuditLogType {
     USER_EXPORT_IMAGE_FINISHED_FAILURE(2032),
     HOT_SET_NUMBER_OF_CPUS(2033),
     FAILED_HOT_SET_NUMBER_OF_CPUS(2034),
+    USER_ISCSI_BOND_HOST_RESTART_WARNING(2035, AuditLogSeverity.WARNING),
 
     // Used only from SQL script, therefor should not have severity & message
     USER_RUN_UNLOCK_ENTITY_SCRIPT(2024),

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/EditIscsiBondCommand.java
@@ -11,9 +11,11 @@ import org.ovirt.engine.core.bll.validator.IscsiBondValidator;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.action.EditIscsiBondParameters;
 import org.ovirt.engine.core.common.businessentities.IscsiBond;
+import org.ovirt.engine.core.common.businessentities.network.Network;
 import org.ovirt.engine.core.common.errors.VdcBllMessages;
 import org.ovirt.engine.core.common.validation.group.UpdateEntity;
 import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogDirector;
 import org.ovirt.engine.core.utils.transaction.TransactionMethod;
 import org.ovirt.engine.core.utils.transaction.TransactionSupport;
 
@@ -24,6 +26,7 @@ public class EditIscsiBondCommand <T extends EditIscsiBondParameters> extends Ba
 
     private List<String> addedConnections = new ArrayList<>();
     private List<Guid> addedNetworks = new ArrayList<>();
+    private Set<Guid> removedNetworks = new HashSet<>();
 
     public EditIscsiBondCommand(T parameters) {
         super(parameters);
@@ -60,7 +63,7 @@ public class EditIscsiBondCommand <T extends EditIscsiBondParameters> extends Ba
                     getDbFacade().getIscsiBondDao().update(getIscsiBond());
                 }
 
-                updateNetworksIds();
+                removedNetworks = updateNetworksIds();
                 updateConnectionsIds();
 
                 return null;
@@ -71,10 +74,24 @@ public class EditIscsiBondCommand <T extends EditIscsiBondParameters> extends Ba
             connectAllHostsToStorage(getIscsiBond().getStorageConnectionIds());
         }
 
+        if (!removedNetworks.isEmpty()) {
+            addAuditLogForRemovedNetowrks(removedNetworks);
+        }
         setSucceeded(true);
     }
 
-    private void updateNetworksIds() {
+    private void addAuditLogForRemovedNetowrks(Set<Guid> removedNetworks) {
+        List<String> networkNames = new ArrayList<>();
+        for (Guid networkId : removedNetworks) {
+            Network network = getDbFacade().getNetworkDao().get(networkId);
+            networkNames.add(network.getName());
+        }
+        addCustomValue("NetworkNames", StringUtils.join(networkNames, ","));
+        addCustomValue("IscsiBondName", getIscsiBond().getName());
+        AuditLogDirector.log(this, AuditLogType.USER_ISCSI_BOND_HOST_RESTART_WARNING);
+    }
+
+    private Set<Guid> updateNetworksIds() {
         Set<Guid> beforeChangeNetworkIds = new HashSet<>(getExistingIscsiBond().getNetworkIds());
 
         for (Guid networkId : getIscsiBond().getNetworkIds()) {
@@ -87,6 +104,7 @@ public class EditIscsiBondCommand <T extends EditIscsiBondParameters> extends Ba
         for (Guid networkId : beforeChangeNetworkIds) {
             getDbFacade().getIscsiBondDao().removeNetworkFromIscsiBond(getExistingIscsiBond().getId(), networkId);
         }
+        return beforeChangeNetworkIds;
     }
 
     private void updateConnectionsIds() {

backend/manager/modules/dal/src/main/resources/bundles/AuditLogMessages.properties
@@ -94,6 +94,7 @@ USER_EXPORT_IMAGE_FINISHED_SUCCESS=User ${UserName} successfully exported image 
 USER_EXPORT_IMAGE_FINISHED_FAILURE=User ${UserName} failed to export image ${RepoImageName} to domain ${DestinationStorageDomainName}.
 HOT_SET_NUMBER_OF_CPUS=VM ${vmName} number of CPUs is hot set to ${numberOfCpus}
 FAILED_HOT_SET_NUMBER_OF_CPUS= Faild to hot set number of CPUS to VM ${vmName}. Underlying error message: ${ErrorMessage}
+USER_ISCSI_BOND_HOST_RESTART_WARNING=The following Networks has been removed from the iSCSI bond ${IscsiBondName}: ${NetworkNames}. for those changes to take affect, the hosts must be moved to maintenance and activated again.
 USER_MOVE_IMAGE_GROUP_FAILED_TO_DELETE_SRC_IMAGE=Possible failure while deleting ${DiskAlias} from the source Storage Domain ${StorageDomainName} during the move operation. The Storage Domain may be manually cleaned-up from possible leftovers (User:${UserName}).
 USER_MOVE_IMAGE_GROUP_FAILED_TO_DELETE_DST_IMAGE=Possible failure while clearing possible leftovers of ${DiskAlias} from the target Storage Domain ${StorageDomainName} after the move operation failed to copy the image to it properly. The Storage Domain may be manually cleaned-up from possible leftovers (User:${UserName}).
 USER_REMOVE_DISK_FROM_VM=Disk was removed from VM ${VmName} by ${UserName}.

commit a346cf7333f9736b1f6a7dceb2a382cc0d620956

core: Fix audit log constructor
Fix a regression merged in commit 7bb86ca81ace8960f0d34ab14fe85a93a4541b02.

Change-Id: I9b1b6130bddd380522cbe92287d9d27a2069c7af
Bug-Url: https://bugzilla.redhat.com/1126428
Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/AuditLogType.java
@@ -210,7 +210,7 @@ public enum AuditLogType {
     USER_EXPORT_IMAGE_FINISHED_FAILURE(2032),
     HOT_SET_NUMBER_OF_CPUS(2033),
     FAILED_HOT_SET_NUMBER_OF_CPUS(2034),
-    USER_ISCSI_BOND_HOST_RESTART_WARNING(2035, AuditLogSeverity.WARNING),
+    USER_ISCSI_BOND_HOST_RESTART_WARNING(2035),
 
     // Used only from SQL script, therefor should not have severity & message
     USER_RUN_UNLOCK_ENTITY_SCRIPT(2024),

backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dal/dbbroker/auditloghandling/AuditLogDirector.java
@@ -937,6 +937,7 @@ public final class AuditLogDirector {
         severities.put(AuditLogType.ISCSI_BOND_EDIT_FAILED, AuditLogSeverity.ERROR);
         severities.put(AuditLogType.ISCSI_BOND_REMOVE_SUCCESS, AuditLogSeverity.NORMAL);
         severities.put(AuditLogType.ISCSI_BOND_REMOVE_FAILED, AuditLogSeverity.ERROR);
+        severities.put(AuditLogType.USER_ISCSI_BOND_HOST_RESTART_WARNING, AuditLogSeverity.WARNING);
     }
 
     private static void initMessages() {