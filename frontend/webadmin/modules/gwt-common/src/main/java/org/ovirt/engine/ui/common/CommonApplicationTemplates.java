b0238c4650745a8fae5764b9f3d864200c162330
Alona Kaplan <alkaplan@redhat.com>
2014-08-10 14:30:59 +0300

    webadmin: The are no splitters in the Host>Interface header
    Change-Id: Icb276a805508b5f2729f609ec2861504a561acea
    Bug-Url: https://bugzilla.redhat.com/1115310
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationTemplates.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationTemplates.java
@@ -92,4 +92,7 @@ public interface CommonApplicationTemplates extends SafeHtmlTemplates {
 
     @Template("<span title='{1}'>{0}</span>")
     SafeHtml textAndTitle(String text, String title);
+
+    @Template("<div style='border-right: 1px solid #D7D7E1; height: 32px;'>{0}</div>")
+    SafeHtml nonResizeableColumnHeader(SafeHtml text);
 }
