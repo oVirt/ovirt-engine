d25ddcb4a4f3bd09d15f51c4d7dd151d83022d6a
Vered Volansky <vvolansk@redhat.com>
2014-08-14 14:23:01 +0300

    core: cleanup: Removed unused field
    VmCommand.MAX_NETWORK_INTERFACES_SUPPORTED is never used. Now removed.
    
    Change-Id: Ib0e27e4421ba07784e6f2d06d4ccd184bece4819
    Signed-off-by: Vered Volansky <vvolansk@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VmCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VmCommand.java
@@ -55,7 +55,6 @@ public abstract class VmCommand<T extends VmOperationParameterBase> extends Comm
     public static final String DELETE_PRIMARY_IMAGE_TASK_KEY = "DELETE_PRIMARY_IMAGE_TASK_KEY";
     public static final String DELETE_SECONDARY_IMAGES_TASK_KEY = "DELETE_SECONDARY_IMAGES_TASK_KEY";
     private static final int Kb = 1024;
-    protected final static int MAX_NETWORK_INTERFACES_SUPPORTED = 8;
 
     protected final OsRepository osRepository = SimpleDependecyInjector.getInstance().get(OsRepository.class);
 
