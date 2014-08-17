59ea91ec885c7432e261d4d12579f1e83180dda2
Daniel Erez <derez@redhat.com>
2014-08-17 15:00:45 +0300

    restapi: fix rsdl generation for import glance image
    Import action has been missing under
    '/api/storagedomains/{storagedomain:id}/images/{images:id}/'.
    Hence, added missing signature on ImagesResource interface.
    
    Change-Id: Icf119c8a9b30385d14c756217d5c3c7fead84472
    Bug-Url: https://bugzilla.redhat.com/1096496
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- backend/manager/modules/restapi/interface/definition/src/main/java/org/ovirt/engine/api/resource/ImagesResource.java
+++ backend/manager/modules/restapi/interface/definition/src/main/java/org/ovirt/engine/api/resource/ImagesResource.java
@@ -1,6 +1,7 @@
 package org.ovirt.engine.api.resource;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import org.ovirt.engine.api.model.Image;
@@ -9,5 +10,9 @@ import org.ovirt.engine.api.model.Images;
 @Path("/images")
 @Produces({ApiMediaType.APPLICATION_XML, ApiMediaType.APPLICATION_JSON, ApiMediaType.APPLICATION_X_YAML})
 public interface ImagesResource extends ReadOnlyResources<Image, Images> {
+
+    @Path("{identity}")
+    @Override
+    public ImageResource getDeviceSubResource(@PathParam("identity") String id);
 
 }
