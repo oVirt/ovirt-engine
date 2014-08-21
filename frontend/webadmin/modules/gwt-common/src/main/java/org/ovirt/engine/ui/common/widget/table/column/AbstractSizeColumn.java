01c7d2cf3416184bba4f6c7558383627fd0396d9
Daniel Erez <derez@redhat.com>
2014-08-22 00:50:13 +0300

    webadmin: fix client sorting for size columns
    Currently, size columns are sorted using LexoNumericComparator,
    which is fine for most cases except for '< 1 GB' value...
    Hence, overridden 'makeSortable' method to compare the size
    values numerically.
    
    Screenshot: http://i.imgur.com/KfJjdq8.png
    
    Change-Id: Iab8cdc171ff8f652ec0747212f6fd846cf95540e
    Bug-Url: https://bugzilla.redhat.com/1133264
    Signed-off-by: Daniel Erez <derez@redhat.com>

---
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/AbstractSizeColumn.java
@@ -0,0 +1,23 @@
+package org.ovirt.engine.ui.common.widget.table.column;
+
+import java.util.Comparator;
+
+import com.google.gwt.text.shared.Renderer;
+
+public abstract class AbstractSizeColumn<T> extends RenderedTextColumn<T, Long> {
+
+    public AbstractSizeColumn(Renderer<Long> renderer) {
+        super(renderer);
+    }
+
+    @Override
+    public void makeSortable() {
+        makeSortable(new Comparator<T>() {
+            @Override
+            public int compare(T object1, T object2) {
+                return getRawValue(object1).compareTo(getRawValue(object2));
+            }
+        });
+    }
+
+}
--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/StorageSizeColumn.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/StorageSizeColumn.java
@@ -3,7 +3,7 @@ package org.ovirt.engine.ui.common.widget.table.column;
 import org.ovirt.engine.core.common.utils.SizeConverter;
 import org.ovirt.engine.ui.common.widget.renderer.DiskSizeRenderer;
 
-public abstract class StorageSizeColumn<T> extends RenderedTextColumn<T, Long> {
+public abstract class StorageSizeColumn<T> extends AbstractSizeColumn<T> {
 
     public StorageSizeColumn() {
         super(new DiskSizeRenderer<Long>(SizeConverter.SizeUnit.GB) {
