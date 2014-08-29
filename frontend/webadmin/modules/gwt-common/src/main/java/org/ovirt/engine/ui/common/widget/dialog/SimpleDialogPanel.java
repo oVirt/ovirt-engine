oVirt/ovirt-engine

commit 42e9396e0fee373a1e3fa4ae233892691ef4ad44

userportal, webadmin: fix popup dialog header text spacing
Changed popup dialog header text to be spaced with CSS only.

Popup dialog header text was shifting left over the icon. This
was a regression. Hopefully permanently fixed by restructuring
the layout of the dialog header bar. Now when an icon is present,
CSS takes care of moving the header text over.

Also cleaned up some CSS class names for the dialog header.

Change-Id: I28ffde8e344b1bedc10310f6eb814a37a7aecefe
Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/dialog/SimpleDialogPanel.java
@@ -3,9 +3,6 @@ package org.ovirt.engine.ui.common.widget.dialog;
 import org.ovirt.engine.ui.uicommonweb.UICommand;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
-import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
@@ -20,8 +17,6 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class SimpleDialogPanel extends AbstractDialogPanel {
 
-    private static final int IMAGE_MARGIN = 10;
-
     interface WidgetUiBinder extends UiBinder<Widget, SimpleDialogPanel> {
         WidgetUiBinder uiBinder = GWT.create(WidgetUiBinder.class);
     }
@@ -31,8 +26,6 @@ public class SimpleDialogPanel extends AbstractDialogPanel {
         String footerButton();
 
         String contentWidget();
-
-        String headerTitleFloat();
     }
 
     @UiField
@@ -70,14 +63,12 @@ public class SimpleDialogPanel extends AbstractDialogPanel {
     @Override
     @UiChild(tagname = "header", limit = 1)
     public void setHeader(Widget widget) {
-        widget.addStyleName(style.headerTitleFloat());
         headerTitlePanel.insert(widget, 0); //Put the label at the front.
     }
 
     @UiChild(tagname = "logo", limit = 1)
     public void setLogo(Widget widget) {
         logoPanel.setWidget(widget);
-        updateTitlePadding();
     }
 
     @Override
@@ -85,19 +76,6 @@ public class SimpleDialogPanel extends AbstractDialogPanel {
     public void setContent(Widget widget) {
         contentPanel.setWidget(widget);
         widget.addStyleName(style.contentWidget());
-    }
-
-    void updateTitlePadding() {
-        Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-            @Override
-            public void execute() {
-                int logoWidth = logoPanel.getOffsetWidth();
-                if (logoWidth > 0) {
-                    logoWidth += IMAGE_MARGIN; //Add some pixels to account for margins/left style of the image div.
-                    headerTitlePanel.getParent().getElement().getStyle().setMarginLeft(logoWidth, Unit.PX);
-                }
-            }
-        });
     }
 
     @Override

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/dialog/SimpleDialogPanel.ui.xml
@@ -17,17 +17,16 @@
             right: 5px;
         }
 
-        .headerLeftPanel {
-            position: absolute;
-            left: 5px;
+        @external obrand_dialogHeaderLeftPanel;
+        .obrand_dialogHeaderLeftPanel {
         }
 
-        @external obrand_headerCenterPanel;
-        .obrand_headerCenterPanel {
+        @external obrand_dialogHeaderCenterPanel;
+        .obrand_dialogHeaderCenterPanel {
         }
 
-        @external obrand_headerTitle;
-        .obrand_headerTitle {
+        @external obrand_dialogHeaderTitle;
+        .obrand_dialogHeaderTitle {
         }
 
         .content {
@@ -61,10 +60,6 @@
             position: absolute;
         }
 
-        .headerTitleFloat {
-            float: left;
-        }
-
         .helpIconButton {
             background: transparent;
             border: 1px solid transparent;
@@ -91,8 +86,18 @@
             z-index: 1;
         }
 
-        @external obrand_dialogHeaderImage;
-        .obrand_dialogHeaderImage {
+        .logoPanel {
+            float: left;
+            position: relative;
+            left: -5px;
+        }
+
+        @external obrand_dialogHeaderRightPanel;
+        .obrand_dialogHeaderRightPanel {
+        }
+
+        @external obrand_dialogLogoImageWrapper;
+        .obrand_dialogLogoImageWrapper {
         }
 
         @external obrand_dialogLogoImage;
@@ -102,13 +107,25 @@
 
     <g:FlowPanel>
         <g:FlowPanel addStyleNames="{style.header}">
-            <g:SimplePanel ui:field="logoPanel" addStyleNames="{style.headerLeftPanel}">
-                <g:Image styleName='obrand_dialogLogoImage' url="clear.cache.gif" />
-            </g:SimplePanel>
-            <g:SimplePanel addStyleNames="{style.obrand_headerCenterPanel}" />
+            <g:Image styleName="obrand_dialogHeaderLeftPanel" url="clear.cache.gif" />
+            <g:FlowPanel addStyleNames="{style.obrand_dialogHeaderCenterPanel}" >
+                <g:SimplePanel ui:field="logoPanel" addStyleNames="{style.logoPanel}">
+                    <g:Image styleName="obrand_dialogLogoImage" url="clear.cache.gif" />
+                </g:SimplePanel>
+                <g:FlowPanel addStyleNames="{style.obrand_dialogHeaderTitle}">
+                    <g:FlowPanel ui:field="headerTitlePanel">
+                        <g:PushButton ui:field="helpIconButton" addStyleNames="{style.helpIconButton} sdp_helpIconButton_pfly_fix" visible="false">
+                            <g:upFace image='{resources.dialogIconHelp}' />
+                            <g:downFace image='{resources.dialogIconHelpDown}' />
+                            <g:upHoveringFace image='{resources.dialogIconHelpRollover}' />
+                        </g:PushButton>
+                    </g:FlowPanel>
+                </g:FlowPanel>
+            </g:FlowPanel>
             <g:SimplePanel addStyleNames="{style.headerRightPanel}">
                 <g:FlowPanel>
-                    <g:Image styleName='obrand_dialogHeaderImage' url="clear.cache.gif" />
+                    <g:Image styleName='obrand_dialogHeaderRightPanel' url="clear.cache.gif" />
+                    <g:Image styleName='obrand_dialogHeaderRightPanelDecoration' url="clear.cache.gif" />
                     <g:PushButton ui:field='closeIconButton' addStyleNames="{style.closeIconButton} closeIconButton_fix">
                         <g:upFace image='{resources.dialogIconClose}' />
                         <g:downFace image='{resources.dialogIconCloseDown}' />
@@ -116,15 +133,6 @@
                     </g:PushButton>
                 </g:FlowPanel>
             </g:SimplePanel>
-            <g:FlowPanel addStyleNames="{style.obrand_headerTitle}">
-                <g:FlowPanel ui:field="headerTitlePanel">
-                    <g:PushButton ui:field="helpIconButton" addStyleNames="{style.helpIconButton} sdp_helpIconButton_pfly_fix" visible="false">
-                        <g:upFace image='{resources.dialogIconHelp}' />
-                        <g:downFace image='{resources.dialogIconHelpDown}' />
-                        <g:upHoveringFace image='{resources.dialogIconHelpRollover}' />
-                    </g:PushButton>
-                </g:FlowPanel>
-            </g:FlowPanel>
         </g:FlowPanel>
 
         <g:SimplePanel ui:field="contentPanel" addStyleNames="{style.content} contentPanel_pfly_fix" />

packaging/branding/ovirt.brand/common.css
@@ -13,54 +13,72 @@ These classes allow you to modify the logo used in the login form. */
     display: block;
 }
 
-
 /* SimpleDialogPanel:
 These classes allow you to modify the header of all the popup windows
 in the user portal and web admin. The border is defined in gwt-common.css */
-.obrand_dialogHeaderImage {
-    background-image: url(images/dialog/dialog_header_image.png);
-    width: 118px;
-    height: 27px;
-    border: 0px;
-    display: block;
+
+/* left */
+.obrand_dialogHeaderLeftPanel {
+    position: absolute;
+    left: 5px;
+    width: 27px;
+    line-height: 27px;
+    background: url(images/dialog/dialog_header_TL.png);
 }
 
-.obrand_dialogLogoImage {
-    background-image: url(images/dialog/blank.png);
+/* center */
+.obrand_dialogHeaderCenterPanel {
+    position: absolute;
+    background: url(images/dialog/dialog_header_background_repetitive.png) repeat-x;
+    height: 27px;
+    line-height: 27px;
+    width: auto;
+    left: 20px;
+    right: 30px;
+}
+
+.obrand_dialogHeaderTitle {
+    text-align: left;
+    font-size: 14px;
+    font-weight: bold;
+    color: white;
+    position: relative;
+    height: 27px;
+    overflow: hidden;
+    z-index: 2;
+    margin-top: 1px;
+    left: -5px;
+}
+
+/* right */
+.obrand_dialogHeaderRightPanel {
+    background-image: url(images/dialog/dialog_header_TR.png);
     width: 27px;
     height: 27px;
     border: 0px;
     display: block;
 }
 
-.obrand_headerCenterPanel {
-    position: absolute;
-    background: url(images/dialog_header_background_repetitive.png) repeat-x;
-    height: 27px;
-    line-height: 27px;
-    width: auto;
-    left: 32px;
-    right: 123px;
+.obrand_dialogHeaderRightPanelDecoration {
+    /* hook for adding a decorative image to the far right of dialog headers. /*
+    /* currently not used. */
 }
 
-.obrand_headerTitle {
-    text-align: left;
-    font-size: 14px;
-    font-weight: bold;
-    color: white;
-    margin-left: 14px;
-    float: left;
-    position: relative;
-    margin-top: 1px;
+/* SimpleDialogPanel.ui.xml:
+This class allows you to change the default icon on popup windows.
+Currently no icon for most windows. */
+.obrand_dialogLogoImage {
+    border: 0px;
 }
-
 
 /* ErrorPopupView.ui.xml:
 This class allows you to change the error icon on the error popup window. */
 .obrand_dialogLogoErrorImage {
     background: url(images/dialog/error.png);
-    width: 27px;
-    height: 27px;
+    background-repeat: no-repeat;
+    background-position: 0 5px;
+    width: 16px;
+    height: 14px;
     border: 0px;
 }
 

packaging/branding/ovirt.brand/ovirt-patternfly-compat.css
@@ -393,10 +393,6 @@ a.dropdown-toggle {
 .epv_closeButton_pfly_fix {
     line-height: 12px !important;
 }
-.epv_titleLabel_pfly_fix {
-    position: relative !important;
-    left: 12px !important;
-}
 .gpv_flowPanel_pfly_fix {
     padding: 4px;
 }

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/guide/GuidePopupView.ui.xml
@@ -36,6 +36,10 @@
             padding-bottom: 15px;
         }
 
+        @external obrand_dialogLogoGuideImageWrapper;
+        .obrand_dialogLogoGuideImageWrapper {
+        }
+
         @external obrand_dialogLogoGuideImage;
         .obrand_dialogLogoGuideImage {
         }

commit 8b385bf860ec212dc0753ff18aa09aa9c61c2b02

webadmin: fixed context-sensitive help icon
commit 42e9396 broke the context-sensitive help icon. Fixed.
Also moved the header text up one pixel to align with the
fixed icon.

Change-Id: Iff8f6605a89dad1f57b15a63de2ffa8e67825418
Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/dialog/SimpleDialogPanel.java
@@ -26,6 +26,8 @@ public class SimpleDialogPanel extends AbstractDialogPanel {
         String footerButton();
 
         String contentWidget();
+
+        String obrand_dialogHeaderTitleElement();
     }
 
     @UiField
@@ -63,6 +65,7 @@ public class SimpleDialogPanel extends AbstractDialogPanel {
     @Override
     @UiChild(tagname = "header", limit = 1)
     public void setHeader(Widget widget) {
+        widget.addStyleName(style.obrand_dialogHeaderTitleElement());
         headerTitlePanel.insert(widget, 0); //Put the label at the front.
     }
 

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/dialog/SimpleDialogPanel.ui.xml
@@ -62,15 +62,14 @@
 
         .helpIconButton {
             background: transparent;
-            border: 1px solid transparent;
-            line-height: 32px;
+            border: none;
             vertical-align: middle;
             padding: 1px;
             cursor: pointer;
             z-index: 1;
             float: left;
             position: relative;
-            top: -5px;
+            top: -2px !important;
         }
 
         .closeIconButton {
@@ -100,8 +99,16 @@
         .obrand_dialogLogoImageWrapper {
         }
 
+        @external obrand_dialogHeaderTitleContainer;
+        .obrand_dialogHeaderTitleContainer {
+        }
+
         @external obrand_dialogLogoImage;
         .obrand_dialogLogoImage {
+        }
+
+        @external obrand_dialogHeaderTitleElement;
+        .obrand_dialogHeaderTitleElement {
         }
     </ui:style>
 
@@ -112,9 +119,9 @@
                 <g:SimplePanel ui:field="logoPanel" addStyleNames="{style.logoPanel}">
                     <g:Image styleName="obrand_dialogLogoImage" url="clear.cache.gif" />
                 </g:SimplePanel>
-                <g:FlowPanel addStyleNames="{style.obrand_dialogHeaderTitle}">
+                <g:FlowPanel addStyleNames="{style.obrand_dialogHeaderTitleContainer}">
                     <g:FlowPanel ui:field="headerTitlePanel">
-                        <g:PushButton ui:field="helpIconButton" addStyleNames="{style.helpIconButton} sdp_helpIconButton_pfly_fix" visible="false">
+                        <g:PushButton ui:field="helpIconButton" addStyleNames="{style.helpIconButton}" visible="false">
                             <g:upFace image='{resources.dialogIconHelp}' />
                             <g:downFace image='{resources.dialogIconHelpDown}' />
                             <g:upHoveringFace image='{resources.dialogIconHelpRollover}' />

packaging/branding/ovirt.brand/common.css
@@ -37,7 +37,7 @@ in the user portal and web admin. The border is defined in gwt-common.css */
     right: 30px;
 }
 
-.obrand_dialogHeaderTitle {
+.obrand_dialogHeaderTitleContainer {
     text-align: left;
     font-size: 14px;
     font-weight: bold;
@@ -46,8 +46,13 @@ in the user portal and web admin. The border is defined in gwt-common.css */
     height: 27px;
     overflow: hidden;
     z-index: 2;
-    margin-top: 1px;
+    margin-top: 0;
     left: -5px;
+}
+
+.obrand_dialogHeaderTitleElement {
+    float: left;
+    padding-right: 5px;
 }
 
 /* right */

packaging/branding/ovirt.brand/ovirt-patternfly-compat.css
@@ -301,9 +301,6 @@ div.avw_contentWidget_pfly_fix {
 .mtev_buttonPanel_pfly_fix label {
     padding-left: 3px !important;
 }
-.sdp_helpIconButton_pfly_fix {
-    top: -7px !important;
-}
 .tslp_sliderButton_pfly_fix {
     top: -3px !important;
 }