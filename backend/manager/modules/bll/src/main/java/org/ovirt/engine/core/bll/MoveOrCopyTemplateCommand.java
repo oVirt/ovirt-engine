f9f70c60f9d488921f9973a4e7d62d733f8b119e
Vered Volansky <vvolansk@redhat.com>
2014-09-02 10:31:46 +0300

    core: ExportVmTemplateCommand storage allocations
    MoveOrCopyTemplateCommand.canDoAction() small fix:
    Fixed storage domain space verification. Using StorageDomainValidator
    for some other storage verification, instead of (p)re-inventing the
    wheel.
    ExportVmTemplate inherits and therefore is affected by this.
    
    Change-Id: Ic5de81b318d5b8a2e0da918b4a44ae2003ca4b06
    Bug-Url: https://bugzilla.redhat.com/1136717
    Signed-off-by: Vered Volansky <vvolansk@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/MoveOrCopyTemplateCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/MoveOrCopyTemplateCommand.java
@@ -32,7 +32,6 @@ import org.ovirt.engine.core.common.businessentities.ImageOperation;
 import org.ovirt.engine.core.common.businessentities.OvfEntityData;
 import org.ovirt.engine.core.common.businessentities.Snapshot;
 import org.ovirt.engine.core.common.businessentities.StorageDomain;
-import org.ovirt.engine.core.common.businessentities.StorageDomainStatus;
 import org.ovirt.engine.core.common.businessentities.StoragePoolIsoMapId;
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VmDevice;
@@ -137,12 +136,11 @@ public class MoveOrCopyTemplateCommand<T extends MoveOrCopyParameters> extends S
                             getReturnValue().getCanDoActionMessages(), true, true, true, false, getTemplateDisks());
             if (retValue) {
                 setStoragePoolId(getVmTemplate().getStoragePoolId());
-                retValue =
-                        checkStorageDomain()
-                                && checkStorageDomainStatus(StorageDomainStatus.Active)
-                                && (getParameters().getForceOverride() || (!isImagesAlreadyOnTarget() && checkIfDisksExist(getTemplateDisks())))
-                                && checkFreeSpaceOnDestinationDomain(getStorageDomain(),
-                                        (int) getVmTemplate().getActualDiskSize());
+                StorageDomainValidator sdValidator = createStorageDomainValidator(getStorageDomain());
+                retValue = validate(sdValidator.isDomainExistAndActive())
+                        && validate(sdValidator.isDomainWithinThresholds())
+                        && (getParameters().getForceOverride() || (!isImagesAlreadyOnTarget() && checkIfDisksExist(getTemplateDisks())))
+                        && validateFreeSpaceOnDestinationDomain(sdValidator, getTemplateDisks());
             }
             if (retValue
                     && DbFacade.getInstance()
@@ -154,6 +152,10 @@ public class MoveOrCopyTemplateCommand<T extends MoveOrCopyParameters> extends S
             }
         }
         return retValue;
+    }
+
+    private StorageDomainValidator createStorageDomainValidator(StorageDomain storageDomain) {
+        return new StorageDomainValidator(storageDomain);
     }
 
     protected boolean validateUnregisteredEntity(IVdcQueryable entityFromConfiguration, OvfEntityData ovfEntityData) {
@@ -197,8 +199,8 @@ public class MoveOrCopyTemplateCommand<T extends MoveOrCopyParameters> extends S
         addCanDoActionMessage(VdcBllMessages.VAR__TYPE__VM_TEMPLATE);
     }
 
-    private boolean checkFreeSpaceOnDestinationDomain(StorageDomain domain, int requestedSizeGB) {
-        return validate(new StorageDomainValidator(domain).isDomainHasSpaceForRequest(requestedSizeGB));
+    private boolean validateFreeSpaceOnDestinationDomain(StorageDomainValidator storageDomainValidator, List<DiskImage> disksList) {
+        return validate(storageDomainValidator.hasSpaceForClonedDisks(disksList));
     }
 
     @Override
