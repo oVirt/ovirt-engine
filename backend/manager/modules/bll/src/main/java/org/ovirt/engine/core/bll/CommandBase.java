8ea49b5f36c784e22d1b31b09bc84e1f4ea45b4c
Ravi Nori <rnori@redhat.com>
2014-09-10 11:54:28 -0400

    engine: placeholders of child commands aren't cleared when failing during the CDA phase
    AsyncTask placeholders for child commands are not
    cleared when canDoAction returns false.
    
    Change-Id: Ie77a00b0071c92d074d837768af012c80374169f
    Bug-Url: https://bugzilla.redhat.com/1139678
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandBase.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandBase.java
@@ -344,6 +344,7 @@ public abstract class CommandBase<T extends VdcActionParametersBase> extends Aud
                 execute();
             } else {
                 getReturnValue().setCanDoAction(false);
+                clearChildAsyncTasksWithOutVdsmId();
             }
         } finally {
             freeLockExecute();
