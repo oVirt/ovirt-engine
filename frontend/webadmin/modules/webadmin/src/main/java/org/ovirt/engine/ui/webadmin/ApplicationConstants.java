f753f5961f108e7984233d3e71e4091e22329b1e
Tomer Saban <tsaban@redhat.com>
2014-09-10 16:43:46 +0300

    webadmin: Fix typo for QoS
    Fixed the "Write IOps" column from "Read IOps" -> "Write IOps".
    
    Change-Id: Ic65a15815cc276cd3ec9d0290946a97813c7a434
    Bug-Url: https://bugzilla.redhat.com/1139731
    Signed-off-by: Tomer Saban <tsaban@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/ApplicationConstants.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/ApplicationConstants.java
@@ -3786,7 +3786,7 @@ public interface ApplicationConstants extends CommonApplicationConstants {
     @DefaultStringValue("Read IOps")
     String storageQosIopsRead();
 
-    @DefaultStringValue("Read IOps")
+    @DefaultStringValue("Write IOps")
     String storageQosIopsWrite();
 
     @DefaultStringValue("New")
