3805d8ac35ab6bbb3e283c87cedd455ce3be61c0
Martin Perina <mperina@redhat.com>
2014-09-11 13:09:09 +0200

    tools: Adds helpers to configure java logging in tools
    1) Adds helper classes to configure java.util.logging in tools using
       command line parameters
    2) Adds tools-logging.properties with default logging configuration for
       tools
    3) Adds OVIRT_LOGGING_PROPERTIES environment variable which can be set
       by user to provide custom logging configuration
    
    Change-Id: Ie1c46ca552888b347a4ff810003e0fcc818f832f
    Bug-Url: https://bugzilla.redhat.com/1109871
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/log/JavaLoggingUtils.java
@@ -0,0 +1,61 @@
+package org.ovirt.engine.core.utils.log;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * Contains methods for runtime java.util.logging setup
+ */
+public class JavaLoggingUtils {
+    /**
+     * Instance of org.ovirt logger. We need to keep instance of it to prevent OpenJDK incompatibility described at
+     * http://findbugs.sourceforge.net/bugDescriptions.html#LG_LOST_LOGGER_DUE_TO_WEAK_REFERENCE
+     */
+    private static final Logger OVIRT_LOGGER = Logger.getLogger("org.ovirt");
+
+    /**
+     * Parses logging level from case insensitive string.
+     *
+     * @param levelName
+     *            specified level name
+     * @exception java.lang.IllegalArgumentException
+     *                if unknown level is specified
+     */
+    public static Level parseLevel(String levelName) {
+        if (levelName == null) {
+            throw new IllegalArgumentException("Invalid log level value: 'null'");
+        }
+        return Level.parse(levelName.toUpperCase());
+    }
+
+    /**
+     * Adds file handler with specified file to root logger
+     *
+     * @param fileName
+     *            file name to log into
+     */
+    public static void addFileHandler(String fileName) {
+        try {
+            FileHandler fh = new FileHandler(fileName);
+            fh.setFormatter(new SimpleFormatter());
+            Logger.getLogger("").addHandler(fh);
+        } catch (SecurityException | IOException ex) {
+            throw new IllegalArgumentException(
+                    String.format("Error accessing log file '%s': '%s'", fileName, ex.getMessage()),
+                    ex);
+        }
+    }
+
+    /**
+     * Sets level of org.ovirt logger
+     *
+     * @param levelName
+     *            log level to use
+     */
+    public static void setLogLevel(String levelName) {
+        OVIRT_LOGGER.setLevel(parseLevel(levelName));
+    }
+}
