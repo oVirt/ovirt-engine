59e067303bcbff9c9992d0846d8525c76ced6897
Mooli Tayer <mtayer@redhat.com>
2014-09-14 14:20:42 +0300

    notifier: modify the oid schema for SNMP notification.
    MIB syntax tested using:
    $ smilint -p packaging/doc/mibs/REDHAT-MIB.txt -l 6 \
              packaging/doc/mibs/OVIRT-MIB.txt
    
    This patch is intended for ovirt-engine-3.5, and it factors
    out the logging changes the happend recently on master.
    
    Change-Id: Ic8a06063f8ebbbe4d05783cb25036e870a6be3b7
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1136818
    Signed-off-by: Mooli Tayer <mtayer@redhat.com>

--- backend/manager/tools/src/main/java/org/ovirt/engine/core/notifier/transport/snmp/Snmp.java
+++ backend/manager/tools/src/main/java/org/ovirt/engine/core/notifier/transport/snmp/Snmp.java
@@ -2,6 +2,7 @@ package org.ovirt.engine.core.notifier.transport.snmp;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +14,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.EventNotificationMethod;
+import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.notifier.NotificationServiceException;
 import org.ovirt.engine.core.notifier.dao.DispatchResult;
 import org.ovirt.engine.core.notifier.filter.AuditLogEvent;
@@ -20,7 +22,9 @@ import org.ovirt.engine.core.notifier.transport.Transport;
 import org.ovirt.engine.core.notifier.utils.NotificationProperties;
 import org.snmp4j.CommunityTarget;
 import org.snmp4j.PDU;
+import org.snmp4j.Target;
 import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.smi.Integer32;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.TimeTicks;
@@ -38,7 +42,35 @@ public class Snmp extends Transport {
     private static final int ENTERPRISE_SPECIFIC = 6;
     private static final Pattern PROFILE_PATTERN = Pattern.compile(SNMP_MANAGERS + "(|_(?<profile>.*))");
 
+    /** OIDs - See OVIRT-MIB.txt */
+    public static final int AUDIT = 1;
+
+    private static final OID OBJECTS_AUDIT = new OID(new int[] {2, 1});
+
+    public static final int INSTANCE_ID = 1;
+    public static final int NAME = 2;
+    public static final int ID = 3;
+    public static final int SEVERITY = 4;
+    public static final int MESSAGE = 5;
+    public static final int STATUS = 6;
+    public static final int DATETIME = 7;
+    public static final int USERNAME = 100;
+    public static final int USER_ID = 101;
+    public static final int VM_NAME = 102;
+    public static final int VM_ID = 103;
+    public static final int VDS_NAME = 104;
+    public static final int VDS_ID = 105;
+    public static final int VM_TEMPLATE_NAME = 106;
+    public static final int VM_TEMPLATE_ID = 107;
+    public static final int STORAGE_POOL_NAME = 108;
+    public static final int STORAGE_POOL_ID = 109;
+    public static final int STORAGE_DOMAIN_NAME = 110;
+    public static final int STORAGE_DOMAIN_ID = 111;
+
+
     private final Map<String, Profile> profiles = new HashMap<>();
+
+    private static final String ISO8601 = "yyyy-MM-dd'T'HH:mm'Z'";
     private org.snmp4j.Snmp snmp = null;
     private boolean active = false;
     private static long nanoStart = System.nanoTime();
@@ -97,66 +129,10 @@ public class Snmp extends Transport {
         // PDU class is for SNMPv2c units
         PDU v2pdu = new PDU();
         v2pdu.setType(PDU.TRAP);
-        int auditLogTypeVal = AuditLogType.UNASSIGNED.getValue();
-        try {
-            // TODO mtayer: what about db? add to audit log type
 
-            auditLogTypeVal = AuditLogType.valueOf(event.getName()).getValue();
-        } catch (IllegalArgumentException e) {
-            log.warn("Could not find event: " + event.getName() + " in auditLogTypes");
-        }
-        OID trapOID = SnmpConstants.getTrapOID(profile.oid, ENTERPRISE_SPECIFIC, auditLogTypeVal);
-        v2pdu.add(new VariableBinding(SnmpConstants.snmpTrapOID, trapOID));
-        v2pdu.add(new VariableBinding(SnmpConstants.sysUpTime,
-                                      new TimeTicks((System.nanoTime() - nanoStart) / 10000000)));
-        v2pdu.add(new VariableBinding(
-                new OID(trapOID).append(0),
-                new OctetString(event.getMessage())));
-        v2pdu.add(new VariableBinding(
-                new OID(trapOID).append(1),
-                new OctetString(event.getSeverity().name())));
-        v2pdu.add(new VariableBinding(
-                new OID(trapOID).append(2),
-                new OctetString(event.getType().name())));
-        v2pdu.add(new VariableBinding(
-                new OID(trapOID).append(3),
-                new OctetString(event.getLogTime().toString())));
-        v2pdu.add(new VariableBinding(
-                new OID(trapOID).append(4),
-                new OctetString(event.getLogTypeName())));
-        if (!StringUtils.isEmpty(event.getUserName())) {
-            v2pdu.add(new VariableBinding(
-                    new OID(trapOID).append(5),
-                    new OctetString(event.getUserName())));
-        }
-        if (!StringUtils.isEmpty(event.getVmName())) {
-            v2pdu.add(new VariableBinding(
-                    new OID(trapOID).append(6),
-                    new OctetString(event.getVmName())));
-        }
-        if (!StringUtils.isEmpty(event.getVdsName())) {
-            v2pdu.add(new VariableBinding(
-                    new OID(trapOID).append(7),
-                    new OctetString(event.getVdsName())));
-        }
-        if (!StringUtils.isEmpty(event.getVmTemplateName())) {
-            v2pdu.add(new VariableBinding(
-                    new OID(trapOID).append(8),
-                    new OctetString(event.getVmTemplateName())));
-        }
-        if (!StringUtils.isEmpty(event.getStoragePoolName())) {
-            v2pdu.add(new VariableBinding(
-                    new OID(trapOID).append(9),
-                    new OctetString(event.getStoragePoolName())));
-        }
-        if (!StringUtils.isEmpty(event.getStorageDomainName())) {
-            v2pdu.add(new VariableBinding(
-                    new OID(trapOID).append(10),
-                    new OctetString(event.getStorageDomainName())));
-        }
-        CommunityTarget target = new CommunityTarget();
-        target.setCommunity(profile.community);
-        target.setVersion(SnmpConstants.version2c);
+        addPayload(v2pdu, event, profile);
+
+        Target target = createTarget(profile);
         for (Host host : profile.hosts) {
             try {
                 log.info(String.format("Generate an snmp trap for event: %s to address: %s ",
@@ -173,6 +149,73 @@ public class Snmp extends Transport {
                 log.error(e.getMessage());
                 notifyObservers(DispatchResult.failure(event, address, EventNotificationMethod.SNMP, e.getMessage()));
             }
+        }
+    }
+
+    private CommunityTarget createTarget(Profile profile) {
+        CommunityTarget target = new CommunityTarget();
+        target.setCommunity(profile.community);
+        target.setVersion(SnmpConstants.version2c);
+        return target;
+    }
+
+    private void addPayload(PDU v2pdu, AuditLogEvent event, Profile profile) {
+
+        v2pdu.add(new VariableBinding(SnmpConstants.sysUpTime,
+                                      new TimeTicks((System.nanoTime() - nanoStart) / 10000000)));
+        // { [baseoid] notifications(0) audit(1) }
+        v2pdu.add(new VariableBinding(SnmpConstants.snmpTrapOID,
+                                      SnmpConstants.getTrapOID(new OID(profile.oid),
+                                                               ENTERPRISE_SPECIFIC,
+                                                               AUDIT)));
+
+        int auditLogId = AuditLogType.UNASSIGNED.getValue();
+        try {
+            auditLogId = AuditLogType.valueOf(event.getName()).getValue();
+        } catch (IllegalArgumentException e) {
+            log.warn("Could not find event: " + event.getName() + " in auditLogTypes");
+        }
+        // { [baseoid] objects(2) audit(1) }
+        OID auditObjects = new OID(profile.oid).append(OBJECTS_AUDIT);
+
+        addString(v2pdu, auditObjects, INSTANCE_ID, Long.toString(event.getId()), true);
+        addString(v2pdu, auditObjects, NAME, event.getName(), true);
+        addInt(v2pdu, auditObjects, ID, auditLogId, true);
+        addInt(v2pdu, auditObjects, SEVERITY, event.getSeverity().getValue(), true);
+        addString(v2pdu, auditObjects, MESSAGE, event.getMessage(), true);
+        addInt(v2pdu, auditObjects, STATUS, event.getType().getValue(), true);
+        addString(v2pdu, auditObjects, DATETIME, new SimpleDateFormat(ISO8601).format(event.getLogTime()), true);
+
+        // Optional pdu:
+        addString(v2pdu, auditObjects, USERNAME, event.getUserName(), false);
+        addUuid(v2pdu, auditObjects, USER_ID, event.getUserId());
+        addString(v2pdu, auditObjects, VM_NAME, event.getVmName(), false);
+        addUuid(v2pdu, auditObjects, VM_ID, event.getVmId());
+        addString(v2pdu, auditObjects, VDS_NAME, event.getVdsName(), false);
+        addUuid(v2pdu, auditObjects, VDS_ID, event.getVdsId());
+        addString(v2pdu, auditObjects, VM_TEMPLATE_NAME, event.getVmTemplateName(), false);
+        addUuid(v2pdu, auditObjects, VM_TEMPLATE_ID, event.getVmTemplateId());
+        addString(v2pdu, auditObjects, STORAGE_POOL_NAME, event.getStoragePoolName(), false);
+        addUuid(v2pdu, auditObjects, STORAGE_POOL_ID, event.getStoragePoolId());
+        addString(v2pdu, auditObjects, STORAGE_DOMAIN_NAME, event.getStorageDomainName(), false);
+        addUuid(v2pdu, auditObjects, STORAGE_DOMAIN_ID, event.getStorageDomainId());
+    }
+
+    private void addString(PDU v2pdu, OID prefix, int suffix, String val, boolean allowEmpty) {
+        if (allowEmpty || !StringUtils.isEmpty(val)) {
+            v2pdu.add(new VariableBinding(new OID(prefix).append(suffix), new OctetString(val == null ? "" : val)));
+        }
+    }
+
+    private void addInt(PDU v2pdu, OID prefix, int suffix, Integer val, boolean allowEmpty) {
+        if (allowEmpty || val != null) {
+            v2pdu.add(new VariableBinding(new OID(prefix).append(suffix), new Integer32(val == null ? 0 : val)));
+        }
+    }
+
+    private void addUuid(PDU v2pdu, final OID prefix, int suffix, Guid val) {
+        if (!Guid.isNullOrEmpty(val)) {
+            addString(v2pdu, prefix, suffix, val.toString(), false);
         }
     }
 
