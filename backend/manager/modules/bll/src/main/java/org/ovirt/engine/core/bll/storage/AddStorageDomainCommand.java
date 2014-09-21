57980151185e50200f148fba090911ccbbef8476
Tal Nisan <tnisan@redhat.com>
2014-09-21 14:29:23 +0300

    core: Remove storage format validation when adding unattached data domain
    Remove storage format validation when adding an unattached data domain with
    a host that doesn't have a data center
    
    Change-Id: I4feea2d476e89e89d2b1ad572ce64904063a0914
    Bug-Url: https://bugzilla.redhat.com/1141784
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/AddStorageDomainCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/AddStorageDomainCommand.java
@@ -187,8 +187,10 @@ public abstract class AddStorageDomainCommand<T extends StorageDomainManagementP
         StorageFormatType storageFormat = getStorageDomain().getStorageFormat();
         StoragePool targetStoragePool = getTargetStoragePool();
 
+        // No reason to check supported storage format if we don't have a pool, the storage format will be validated
+        // upon the future attachment of the the created domain to a pool
         if (targetStoragePool == null) {
-            return false;
+            return true;
         }
 
         Set<StorageFormatType> supportedStorageFormats =
