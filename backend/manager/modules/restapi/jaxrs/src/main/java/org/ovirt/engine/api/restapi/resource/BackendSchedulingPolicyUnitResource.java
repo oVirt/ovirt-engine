10d1285b1c931376ce912b2d341b9b948a751d62
Gilad Chaplik <gchaplik@redhat.com>
2014-09-28 17:11:56 +0300

    engine: fix api/schedulingpolicyunits/policy_unit_id
    Change-Id: Ic3cf7127d437506c842b0be93dc4ed29198b7937
    Bug-Url: https://bugzilla.redhat.com/1142916
    Signed-off-by: Gilad Chaplik <gchaplik@redhat.com>
    (cherry picked from commit 0626ac18c35ffaf19708bdd9d313b92282799ed8)

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendSchedulingPolicyUnitResource.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendSchedulingPolicyUnitResource.java
@@ -2,17 +2,17 @@ package org.ovirt.engine.api.restapi.resource;
 
 import org.ovirt.engine.api.model.SchedulingPolicyUnit;
 import org.ovirt.engine.api.resource.SchedulingPolicyUnitResource;
+import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.common.scheduling.PolicyUnit;
 
 public class BackendSchedulingPolicyUnitResource extends AbstractBackendSubResource<SchedulingPolicyUnit, PolicyUnit> implements
         SchedulingPolicyUnitResource {
 
     private static final String[] SUB_COLLECTIONS = {};
-    private final PolicyUnit policyUnit;
 
-    public BackendSchedulingPolicyUnitResource(String id, PolicyUnit policyUnit) {
+    public BackendSchedulingPolicyUnitResource(String id) {
         super(id, SchedulingPolicyUnit.class, PolicyUnit.class, SUB_COLLECTIONS);
-        this.policyUnit = policyUnit;
     }
 
     @Override
@@ -22,7 +22,7 @@ public class BackendSchedulingPolicyUnitResource extends AbstractBackendSubResou
 
     @Override
     public SchedulingPolicyUnit get() {
-        return map(policyUnit);
+        return performGet(VdcQueryType.GetPolicyUnitById, new IdQueryParameters(guid));
     }
 
 }
