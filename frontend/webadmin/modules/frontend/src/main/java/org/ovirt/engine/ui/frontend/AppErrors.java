5712f28ae3ff3dd6954ee22bd9e06d4bf4aff568
Gilad Chaplik <gchaplik@redhat.com>
2014-09-28 10:50:27 +0300

    core: fix typo in error message
    Capital letter for profile error message.
    
    Change-Id: I4f60451e00c27470ce343c485761c7f2561d1b58
    Bug-Url: https://bugzilla.redhat.com/1142887
    Signed-off-by: Gilad Chaplik <gchaplik@redhat.com>

--- frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
+++ frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
@@ -3132,10 +3132,10 @@ public interface AppErrors extends ConstantsWithLookup {
     @DefaultStringValue("Cannot ${action} ${type}. Disk Profile doesn't match provided Storage Domain.")
     String ACTION_TYPE_DISK_PROFILE_NOT_MATCH_STORAGE_DOMAIN();
 
-    @DefaultStringValue("Cannot ${action} ${type}. remove last CPU profile in Cluster.")
+    @DefaultStringValue("Cannot ${action} ${type}. Cannot remove last CPU profile in Cluster.")
     String ACTION_TYPE_CANNOT_REMOVE_LAST_CPU_PROFILE_IN_CLUSTER();
 
-    @DefaultStringValue("Cannot ${action} ${type}. remove last Disk profile in Storage Domain.")
+    @DefaultStringValue("Cannot ${action} ${type}. Cannot remove last Disk profile in Storage Domain.")
     String ACTION_TYPE_CANNOT_REMOVE_LAST_DISK_PROFILE_IN_STORAGE_DOMAIN();
 
     @DefaultStringValue("Cannot ${action}. New disk size cannot be smaller than the current.")
