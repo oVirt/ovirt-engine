42a29c9cd8e418021a34619aedfd3a706c6cd1b1
Vitor de Lima <vdelima@redhat.com>
2014-09-30 09:37:41 -0300

    webadmin: Set Migrate On Error in Data Center Guide Me
    The Add Cluster dialog in the Data Center "Guide Me" was not setting
    the chosen Migrate On Error action in the VDSGroup instance that is
    sent to the AddVdsGroup command.
    
    Change-Id: I7498ae26f1ff2fcd90e547aae48fb66e25bb4a43
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1142658
    Signed-off-by: Vitor de Lima <vdelima@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/datacenters/DataCenterGuideModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/datacenters/DataCenterGuideModel.java
@@ -1365,6 +1365,7 @@ public class DataCenterGuideModel extends GuideModel implements ITaskTarget
         cluster.setmax_vds_memory_over_commit(model.getMemoryOverCommit());
         cluster.setTransparentHugepages(version.compareTo(new Version("3.0")) >= 0); //$NON-NLS-1$
         cluster.setcompatibility_version(version);
+        cluster.setMigrateOnError(model.getMigrateOnErrorOption());
         cluster.setVirtService(model.getEnableOvirtService().getEntity());
         cluster.setGlusterService(model.getEnableGlusterService().getEntity());
         cluster.setOptionalReasonRequired(model.getEnableOptionalReason().getEntity());
