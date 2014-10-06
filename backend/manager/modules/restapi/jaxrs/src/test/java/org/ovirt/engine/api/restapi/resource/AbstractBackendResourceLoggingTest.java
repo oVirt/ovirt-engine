5a18e3198bda124891306cd946277722641763e4
lzelkha@redhat.com <lzelkha@redhat.com>
2014-10-06 11:08:32 +0300

    core: Add JPA infrastructure to oVirt
    Adding JPA infrastructure (dependencies, persistence.xml) to oVirt.
    Future patches will migrate entities to use JPA
    
    Change-Id: I2440162230449d0c335a2c2d418fb9ff7e854793
    Bug-Url: https://bugzilla.redhat.com/1159621
    Signed-off-by: lzelkha@redhat.com <lzelkha@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/test/java/org/ovirt/engine/api/restapi/resource/AbstractBackendResourceLoggingTest.java
+++ backend/manager/modules/restapi/jaxrs/src/test/java/org/ovirt/engine/api/restapi/resource/AbstractBackendResourceLoggingTest.java
@@ -1,34 +1,37 @@
 package org.ovirt.engine.api.restapi.resource;
 
-import static org.easymock.classextension.EasyMock.expect;
-import static org.powermock.api.easymock.PowerMock.createMock;
-import static org.powermock.api.easymock.PowerMock.mockStatic;
-import static org.powermock.api.easymock.PowerMock.replayAll;
-import static org.powermock.api.easymock.PowerMock.verifyAll;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.Mockito;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest( {LoggerFactory.class })
 public abstract class AbstractBackendResourceLoggingTest extends Assert {
 
     @After
     public void tearDown() {
-        verifyAll();
+    }
+
+    private void setFinalStatic(Field field, Object newValue) throws Exception {
+        field.setAccessible(true);
+
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+        field.set(null, newValue);
     }
 
     protected void setUpLogExpectations(boolean debug) {
-        mockStatic(LoggerFactory.class);
-        Logger log = createMock(Logger.class);
-        expect(LoggerFactory.getLogger(AbstractBackendResource.class)).andReturn(log);
-        expect(log.isDebugEnabled()).andReturn(debug).anyTimes();
-
-        replayAll();
+        try {
+            Field field = BaseBackendResource.class.getDeclaredField("log");
+            Logger logger = Mockito.mock(Logger.class);
+            Mockito.when(logger.isDebugEnabled()).thenReturn(debug);
+            setFinalStatic(field, logger);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
