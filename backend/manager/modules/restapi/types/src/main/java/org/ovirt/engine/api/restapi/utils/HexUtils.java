4695323b559e6d1608c3d9c085c5a03f04cbfaf6
Juan Hernandez <juan.hernandez@redhat.com>
2014-10-09 16:28:05 +0200

    restapi: Add HexUtils
    This patch adds a utility class used to convert hexadecimal identifiers
    to strings and the other way around. It will be used by resources that
    don't have a identifier, so the identifier will be generated as the hex
    encoding of the name.
    
    Change-Id: I703763e11d26f2a1a0c6b0984249acc8a6744f95
    Related-To: https://bugzilla.redhat.com/1132259
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ backend/manager/modules/restapi/types/src/main/java/org/ovirt/engine/api/restapi/utils/HexUtils.java
@@ -0,0 +1,43 @@
+/*
+* Copyright (c) 2014 Red Hat, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.ovirt.engine.api.restapi.utils;
+
+import java.nio.charset.Charset;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+public class HexUtils {
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+
+    public static String string2hex(String text) {
+        return Hex.encodeHexString(text.getBytes(UTF_8));
+    }
+
+    private static RuntimeException notValidHex(String hex, Exception exception) {
+        return new IllegalArgumentException("The string \"" + hex + "\" isn't a valid hexadecimal value", exception);
+    }
+
+    public static String hex2string(String hex) {
+        try {
+            return new String(Hex.decodeHex(hex.toCharArray()), UTF_8);
+        }
+        catch (DecoderException exception) {
+            throw notValidHex(hex, exception);
+        }
+    }
+}
