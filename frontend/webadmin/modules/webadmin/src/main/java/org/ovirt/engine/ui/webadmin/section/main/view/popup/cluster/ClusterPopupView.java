8ca8d32d9b6ae7cd03e3f34b6a977f898645d4cf
Martin Perina <mperina@redhat.com>
2014-10-12 13:34:46 +0200

    webadmin: Fix alignment issues in Cluster Detail popup
    Fixes alignment issues in Cluster Detail popup caused by the length of
    text in French translation.
    
    Change-Id: I34048f9485bffe2066e527e710bf9f3eff687e07
    Bug-Url: https://bugzilla.redhat.com/1150599
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/cluster/ClusterPopupView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/cluster/ClusterPopupView.java
@@ -581,10 +581,10 @@ public class ClusterPopupView extends AbstractModelBoundPopupView<ClusterModel> 
         fencingEnabledCheckBox.getContentWidgetContainer().setWidth("150px"); //$NON-NLS-1$
 
         skipFencingIfSDActiveCheckBox = new EntityModelCheckBoxEditor(Align.RIGHT);
-        skipFencingIfSDActiveCheckBox.getContentWidgetContainer().setWidth("350px"); //$NON-NLS-1$
+        skipFencingIfSDActiveCheckBox.getContentWidgetContainer().setWidth("450px"); //$NON-NLS-1$
 
         skipFencingIfConnectivityBrokenCheckBox = new EntityModelCheckBoxEditor(Align.RIGHT);
-        skipFencingIfConnectivityBrokenCheckBox.getContentWidgetContainer().setWidth("355px"); //$NON-NLS-1$
+        skipFencingIfConnectivityBrokenCheckBox.getContentWidgetContainer().setWidth("420px"); //$NON-NLS-1$
     }
 
     private void initInfoIcons(ApplicationResources resources, ApplicationConstants constants, ApplicationTemplates templates) {
