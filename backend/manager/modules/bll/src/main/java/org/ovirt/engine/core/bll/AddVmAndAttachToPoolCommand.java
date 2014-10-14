02b0a027261a4a25335db7f4264d5fffbf3b95bc
Arik Hadas <ahadas@redhat.com>
2014-10-14 16:31:49 +0300

    core: refactoring in AddVmAndAttachToPool
    Extract the parameters initialization from the execute method to
    separate method.
    
    Change-Id: Iace5caf71c6692ab6700240aeacb0ef696873e2c
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmAndAttachToPoolCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmAndAttachToPoolCommand.java
@@ -35,13 +35,12 @@ public class AddVmAndAttachToPoolCommand<T extends AddVmAndAttachToPoolParameter
     @Override
     protected void executeCommand() {
         VmStatic vmStatic = getParameters().getVmStaticData();
-        VdcReturnValueBase returnValueFromAddVm;
+        VdcActionType action = VmTemplateHandler.BLANK_VM_TEMPLATE_ID.equals(vmStatic.getVmtGuid()) ?
+                VdcActionType.AddVmFromScratch : VdcActionType.AddVm;
 
-        if (VmTemplateHandler.BLANK_VM_TEMPLATE_ID.equals(vmStatic.getVmtGuid())) {
-            returnValueFromAddVm = addVmFromScratch(vmStatic);
-        } else {
-            returnValueFromAddVm = addVm(vmStatic);
-        }
+        VdcReturnValueBase returnValueFromAddVm = runInternalActionWithTasksContext(
+                action,
+                buildAddVmParameters(action));
 
         if (returnValueFromAddVm.getSucceeded()) {
             getTaskIdList().addAll(returnValueFromAddVm.getInternalVdsmTaskIdList());
@@ -54,37 +53,36 @@ public class AddVmAndAttachToPoolCommand<T extends AddVmAndAttachToPoolParameter
         return null;
     }
 
-    private VdcReturnValueBase addVmFromScratch(VmStatic vmStatic) {
-        AddVmParameters parameters = new AddVmParameters(vmStatic);
-        parameters.setDiskInfoList(getParameters().getDiskInfoList());
-        parameters.setStorageDomainId(getParameters().getStorageDomainId());
-        parameters.setSessionId(getParameters().getSessionId());
-        parameters.setDontAttachToDefaultTag(true);
+    private AddVmParameters buildAddVmParameters(VdcActionType action) {
+        AddVmParameters parameters = new AddVmParameters(getParameters().getVmStaticData());
         parameters.setDiskOperatorAuthzPrincipalDbId(getParameters().getDiskOperatorAuthzPrincipalDbId());
-        return runInternalActionWithTasksContext(VdcActionType.AddVmFromScratch, parameters);
-    }
 
-    private VdcReturnValueBase addVm(VmStatic vmStatic) {
-        AddVmParameters parameters = new AddVmParameters(vmStatic);
-        if (StringUtils.isEmpty(getParameters().getSessionId())) {
-            parameters.setParametersCurrentUser(getCurrentUser());
-        } else {
+        if (action == VdcActionType.AddVmFromScratch) {
+            parameters.setDiskInfoList(getParameters().getDiskInfoList());
+            parameters.setStorageDomainId(getParameters().getStorageDomainId());
             parameters.setSessionId(getParameters().getSessionId());
+            parameters.setDontAttachToDefaultTag(true);
         }
-        parameters.setDiskOperatorAuthzPrincipalDbId(getParameters().getDiskOperatorAuthzPrincipalDbId());
-        parameters.setDontAttachToDefaultTag(true);
-        parameters.setDiskInfoDestinationMap(diskInfoDestinationMap);
-        parameters.setSoundDeviceEnabled(getParameters().isSoundDeviceEnabled());
-        parameters.setConsoleEnabled(getParameters().isConsoleEnabled());
-        parameters.setVirtioScsiEnabled(getParameters().isVirtioScsiEnabled());
-        parameters.setBalloonEnabled(getParameters().isBalloonEnabled());
+        else {
+            if (StringUtils.isEmpty(getParameters().getSessionId())) {
+                parameters.setParametersCurrentUser(getCurrentUser());
+            } else {
+                parameters.setSessionId(getParameters().getSessionId());
+            }
+            parameters.setDontAttachToDefaultTag(true);
+            parameters.setDiskInfoDestinationMap(diskInfoDestinationMap);
+            parameters.setSoundDeviceEnabled(getParameters().isSoundDeviceEnabled());
+            parameters.setConsoleEnabled(getParameters().isConsoleEnabled());
+            parameters.setVirtioScsiEnabled(getParameters().isVirtioScsiEnabled());
+            parameters.setBalloonEnabled(getParameters().isBalloonEnabled());
 
-        if (getParameters().isUpdateRngDevice()) {
-            parameters.setUpdateRngDevice(true);
-            parameters.setRngDevice(getParameters().getRngDevice());
+            if (getParameters().isUpdateRngDevice()) {
+                parameters.setUpdateRngDevice(true);
+                parameters.setRngDevice(getParameters().getRngDevice());
+            }
         }
 
-        return runInternalActionWithTasksContext(VdcActionType.AddVm, parameters);
+        return parameters;
     }
 
     private void addVmToPool(VmStatic vmStatic) {
