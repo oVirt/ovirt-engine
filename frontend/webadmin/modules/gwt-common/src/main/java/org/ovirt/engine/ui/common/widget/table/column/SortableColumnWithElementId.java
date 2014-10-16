6e06b76253c2e02ea232f303632ef8e823995a30
Greg Sheremeta <gshereme@redhat.com>
2014-10-16 14:57:57 -0400

    webadmin: use new element-id-capable SortedColumn for vm statustext
    In the VM main tab, the 'status text' column lost its static element id,
    probably when it was made into a SortedColumn or a composite column.
    Added infrastructure for a SortedColumnWithElementId and switched the
    column to that.
    
    Change-Id: I97bb0a04d46e94efede5f46af35ebcee01a9dd70
    Bug-Url: https://bugzilla.redhat.com/1148530
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

---
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/SortableColumnWithElementId.java
@@ -0,0 +1,28 @@
+package org.ovirt.engine.ui.common.widget.table.column;
+
+/**
+ * A {@link SortableColumn} that implements ColumnWithElementId (has an id).
+ *
+ * @param <T>
+ *            Table row data type.
+ * @param <C>
+ *            Cell data type.
+ */
+public abstract class SortableColumnWithElementId<T, C> extends SortableColumn<T, C> implements ColumnWithElementId {
+
+    public SortableColumnWithElementId(CellWithElementId<C> cell) {
+        super(cell);
+    }
+
+    @Override
+    public CellWithElementId<C> getCell() {
+        return (CellWithElementId<C>) super.getCell();
+    }
+
+    @Override
+    public void configureElementId(String elementIdPrefix, String columnId) {
+        getCell().setElementIdPrefix(elementIdPrefix);
+        getCell().setColumnId(columnId);
+    }
+
+}
