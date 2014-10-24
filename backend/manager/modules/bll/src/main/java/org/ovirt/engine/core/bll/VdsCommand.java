62598b7b94dfe19698270fce000436fb6575673e
Martin Perina <mperina@redhat.com>
2014-10-24 13:02:38 +0200

    core: Fix host deploy logs path displayed in Events tab
    Fixes host deploy logs path displayed in Events tab in case of
    unsuccessfull deployment of host.
    
    Change-Id: Ifcba8acd649bb97dc793a2f82062f9bc71a36720
    Bug-Url: https://bugzilla.redhat.com/1155603
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VdsCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VdsCommand.java
@@ -286,7 +286,7 @@ public abstract class VdsCommand<T extends VdsActionParameters> extends CommandB
 
     protected String getErrorMessage(String msg) {
         return !StringUtils.isEmpty(msg) ? msg : String.format(
-            "Please refer to %1$s/engine.log and log logs under %1$s/ovirt-engine/host-deploy/ for further details.",
+            "Please refer to %1$s/engine.log and log logs under %1$s/host-deploy/ for further details.",
             EngineLocalConfig.getInstance().getLogDir()
         );
     }
