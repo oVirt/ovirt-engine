b0758f0f98f4c51c619b2803b3564fba9b7c68d3
Yair Zaslavsky <yzaslavs@redhat.com>
2014-10-30 14:24:47 +0200

    core: Store only a single certificate
    Change-Id: Ic9bd8cd7f913cf23eca839452b6e113f749966f7
    Signed-off-by: Yair Zaslavsky <yzaslavs@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/ExternalTrustStoreInitializer.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/ExternalTrustStoreInitializer.java
@@ -3,8 +3,10 @@ package org.ovirt.engine.core.bll.provider;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
 
 import org.ovirt.engine.core.utils.EngineLocalConfig;
 import org.ovirt.engine.core.utils.log.Log;
@@ -14,55 +16,49 @@ public class ExternalTrustStoreInitializer {
 
     private static final Log log = LogFactory.getLog(ExternalTrustStoreInitializer.class);
 
-    private static String getTrustStorePath() {
-        File varDir = EngineLocalConfig.getInstance().getVarDir();
-        return varDir + "/" + "external_truststore";
-    }
-
-    public static void init() {
-        File trustStoreFile = new File(getTrustStorePath());
-        if (!trustStoreFile.exists()) {
-            try (OutputStream out = new FileOutputStream(trustStoreFile)){
-                String password = EngineLocalConfig.getInstance().getPKITrustStorePassword();
-                KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-                // Passing null stream will create a new empty trust store
-                trustStore.load(null, password.toCharArray());
-                trustStore.store(out, password.toCharArray());
-            } catch (Exception e) {
-                log.error("Creation of the external trust store failed.", e);
-            }
-        }
-    }
-
     public static KeyStore getTrustStore() {
-        KeyStore ks = null;
         try {
-            ks = EngineLocalConfig.getInstance().getExternalProvidersTrustStore().exists() ?
-                            KeyStore.getInstance(EngineLocalConfig.getInstance().getExternalProvidersTrustStoreType())
-                            : null;
-            if (ks != null) {
+            KeyStore ks = KeyStore.getInstance(EngineLocalConfig.getInstance().getExternalProvidersTrustStoreType());
+            if (!EngineLocalConfig.getInstance().getExternalProvidersTrustStore().exists()) {
+                ks.load(null);
+            } else {
                 try (FileInputStream ksFileInputStream =
                         new FileInputStream(EngineLocalConfig.getInstance().getExternalProvidersTrustStore())) {
-                        ks.load(ksFileInputStream, EngineLocalConfig.getInstance()
+                    ks.load(ksFileInputStream, EngineLocalConfig.getInstance()
                             .getExternalProvidersTrustStorePassword()
                             .toCharArray());
                 }
             }
+            return ks;
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
-        return ks;
-
     }
 
-    public static void setTrustStore(KeyStore keystore) {
-        try (OutputStream out = new FileOutputStream(getTrustStorePath())) {
-            // TODO: do not use password of other store
-            String password = EngineLocalConfig.getInstance().getPKITrustStorePassword();
-            keystore.store(out, password.toCharArray());
-        }
-        catch (Exception e) {
+    public static void addCertificate(Certificate cert) throws CertificateEncodingException, KeyStoreException {
+        KeyStore keystore = getTrustStore();
+        keystore.setCertificateEntry(Integer.toString(cert.hashCode()), cert);
+
+        File trustStoreFile = EngineLocalConfig.getInstance().getExternalProvidersTrustStore();
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("keystore", ".tmp", trustStoreFile.getParentFile());
+            try (FileOutputStream out = new FileOutputStream(tempFile)) {
+                keystore.store(out, EngineLocalConfig.getInstance()
+                        .getExternalProvidersTrustStorePassword()
+                        .toCharArray());
+            }
+            if (!tempFile.renameTo(trustStoreFile.getAbsoluteFile())) {
+                throw new RuntimeException(String.format("Failed to save trust store to file %1$s",
+                        trustStoreFile.getAbsolutePath()));
+            }
+            tempFile = null;
+        } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            if (tempFile != null && !tempFile.delete()) {
+                log.error(String.format("Cannot delete '%1$s'", tempFile.getAbsolutePath()));
+            }
         }
     }
 }
