f95a9c5c9c73e4d073ca188a303d4a7926168904
Moti Asayag <masayag@redhat.com>
2014-11-16 11:26:29 +0200

    engine: Remove nics from CollectHostNetworkDataVdsCommandParameters
    There is no longer use of the nics from the parameters, as nics will
    be persisted as part of the SetupNetworksCommands.
    
    In addition, this class should be removed and use the
    VdsIdAndVdsVDSCommandParametersBase, as part of a following patch.
    
    Change-Id: Id4290345096cdf84ea4e962fea09be455a3d6286
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/vdscommands/CollectHostNetworkDataVdsCommandParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/vdscommands/CollectHostNetworkDataVdsCommandParameters.java
@@ -1,29 +1,13 @@
 package org.ovirt.engine.core.common.vdscommands;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.ovirt.engine.core.common.businessentities.VDS;
-import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
 
 public class CollectHostNetworkDataVdsCommandParameters extends VdsIdAndVdsVDSCommandParametersBase {
-
-    private List<VdsNetworkInterface> nics;
 
     public CollectHostNetworkDataVdsCommandParameters() {
     }
 
     public CollectHostNetworkDataVdsCommandParameters(VDS vds) {
         super(vds);
-        nics = Collections.emptyList();
-    }
-
-    public CollectHostNetworkDataVdsCommandParameters(VDS vds, List<VdsNetworkInterface> nics) {
-        super(vds);
-        this.nics = nics;
-    }
-
-    public List<VdsNetworkInterface> getInterfaces() {
-        return nics;
     }
 }
