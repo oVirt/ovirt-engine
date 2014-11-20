2ffabb2eca77eb58657265f68c80f69beb486e9d
Yair Zaslavsky <yzaslavs@redhat.com>
2014-11-20 10:42:54 +0200

    Revert "core: Add foreign keys from async_tasks to command_entities"
    This reverts commit cff91e9211234966692c43a1dfc211858d72a2b7.
    
    Change-Id: I0bedf18c44368297da6201530ea9321496bd5be6
    Signed-off-by: Yair Zaslavsky <yzaslavs@redhat.com>

---
+++ packaging/dbscripts/upgrade/03_06_0570_revert_async_tasks_fk.sql
@@ -0,0 +1,3 @@
+SELECT  fn_db_drop_constraint('async_tasks', 'fk_async_tasks_command_entities_command_id');
+SELECT  fn_db_drop_constraint('async_tasks', 'fk_async_tasks_command_entities_root_command_id');
+
