oVirt/ovirt-engine

commit 60331c40535c95950274984d89177643df5686db

ui: adding discover host integration to AddHost view
This patch adds sublist under AddHost to allow picking and provision
foreman hosts.

Wiki: http://www.ovirt.org/Features/AdvancedForemanIntegration

Change-Id: I0faf667019760326019368f044ee16265d41a620
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1083993
Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/EditHostModel.java
@@ -55,7 +55,7 @@ public class EditHostModel extends HostModel {
     }
 
     @Override
-    protected void updateHosts() {
+    public void updateHosts() {
     }
 
     @Override

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/HostModel.java
@@ -91,6 +91,8 @@ public abstract class HostModel extends Model
         return getHostId() == null;
     }
 
+    public boolean getIsProvisioning() { return getExternalDiscoveredHosts().getSelectedItem() != null; }
+
     private Guid privateHostId;
 
     public Guid getHostId()
@@ -149,6 +151,24 @@ public abstract class HostModel extends Model
     private void setPkSection(EntityModel value)
     {
         privatePkSection = value;
+    }
+
+    private EntityModel privateDiscoveredHostSection;
+
+    public EntityModel getDiscoveredHostSection() { return privateDiscoveredHostSection; }
+
+    private void setDiscoveredHostSection(EntityModel value)
+    {
+        privateDiscoveredHostSection = value;
+    }
+
+    private EntityModel privateProvisionedHostSection;
+
+    public EntityModel getProvisionedHostSection() { return privateProvisionedHostSection; }
+
+    private void setProvisionedHostSection(EntityModel value)
+    {
+        privateProvisionedHostSection = value;
     }
 
     private EntityModel privatePasswordSection;
@@ -798,6 +818,24 @@ public abstract class HostModel extends Model
         privateExternalHostName = value;
     }
 
+    private ListModel privateExternalDiscoveredHosts;
+
+    public ListModel getExternalDiscoveredHosts() { return privateExternalDiscoveredHosts; }
+
+    protected void setExternalDiscoveredHosts(ListModel value) { privateExternalDiscoveredHosts = value; }
+
+    private ListModel privateExternalHostGroups;
+
+    public ListModel getExternalHostGroups() { return privateExternalHostGroups; }
+
+    protected void setExternalHostGroups(ListModel value) { privateExternalHostGroups = value; }
+
+    private ListModel privateExternalComputeResource;
+
+    public ListModel getExternalComputeResource() { return privateExternalComputeResource; }
+
+    protected void setExternalComputeResource(ListModel value) { privateExternalComputeResource = value; }
+
     private EntityModel<String> privateComment;
 
     public EntityModel<String> getComment()
@@ -840,6 +878,18 @@ public abstract class HostModel extends Model
 
     private void setNetworkProviderModel(HostNetworkProviderModel value) {
         networkProviderModel = value;
+    }
+
+    private EntityModel<Boolean> isDiscorveredHosts;
+
+    public EntityModel<Boolean> getIsDiscorveredHosts()
+    {
+        return isDiscorveredHosts;
+    }
+
+    public void setDiscorveredHosts(EntityModel<Boolean> value)
+    {
+        isDiscorveredHosts = value;
     }
 
     public ListModel<Provider<org.ovirt.engine.core.common.businessentities.OpenstackNetworkProviderProperties>> getNetworkProviders() {
@@ -913,6 +963,8 @@ public abstract class HostModel extends Model
         setHost(new EntityModel<String>());
         setPkSection(new EntityModel());
         setPasswordSection(new EntityModel());
+        setDiscoveredHostSection(new EntityModel());
+        setProvisionedHostSection(new EntityModel());
         setAuthSshPort(new EntityModel<Integer>());
         getAuthSshPort().setEntity(Integer.parseInt(constants.defaultHostSSHPort()));
         setUserName(new EntityModel<String>());
@@ -954,7 +1006,11 @@ public abstract class HostModel extends Model
         getProviderSearchFilter().setIsAvailable(false);
         setProviderSearchFilterLabel(new EntityModel<String>());
         getProviderSearchFilterLabel().setIsAvailable(false);
+        setExternalDiscoveredHosts(new ListModel());
+        setExternalHostGroups(new ListModel());
+        setExternalComputeResource(new ListModel());
         getUpdateHostsCommand().setIsExecutionAllowed(false);
+
         // Initialize primary PM fields.
         setManagementIp(new EntityModel<String>());
         setPmUserName(new EntityModel<String>());
@@ -1028,6 +1084,7 @@ public abstract class HostModel extends Model
         fetchPublicKey();
 
         setNetworkProviderModel(new HostNetworkProviderModel());
+        setDiscorveredHosts(new EntityModel<Boolean>());
     }
 
     private void proxyUp() {
@@ -1769,7 +1826,9 @@ public abstract class HostModel extends Model
             getCluster().setSelectedItem(Linq.firstOrDefault(clusters));
         }
 
-        if (vds.getStatus() != VDSStatus.Maintenance && vds.getStatus() != VDSStatus.PendingApproval) {
+        if (vds.getStatus() != VDSStatus.Maintenance &&
+            vds.getStatus() != VDSStatus.PendingApproval &&
+            vds.getStatus() != VDSStatus.InstallingOS) {
             setAllowChangeHostPlacementPropertiesWhenNotInMaintenance();
         }
         else if (selectedSystemTreeItem != null)
@@ -1802,6 +1861,16 @@ public abstract class HostModel extends Model
         }
     }
 
+    public void cleanHostParametersFields() {
+        getName().setEntity(constants.empty());
+        getComment().setEntity(constants.empty());
+        getAuthSshPort().setEntity(Integer.parseInt(constants.defaultHostSSHPort()));
+        getHost().setEntity(constants.empty());
+        getUserPassword().setEntity(constants.empty());
+        getFetchSshFingerprint().setEntity(constants.empty());
+
+    }
+
     protected abstract boolean showInstallationProperties();
 
     protected abstract boolean showTransportProperties();
@@ -1816,7 +1885,7 @@ public abstract class HostModel extends Model
 
     protected abstract void setAllowChangeHostPlacementPropertiesWhenNotInMaintenance();
 
-    protected abstract void updateHosts();
+    public abstract void updateHosts();
 
     protected abstract void setPort(VDS vds);
 

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/NewHostModel.java
@@ -2,6 +2,9 @@ package org.ovirt.engine.ui.uicommonweb.models.hosts;
 
 import java.util.ArrayList;
 
+import org.ovirt.engine.core.common.businessentities.ExternalComputeResource;
+import org.ovirt.engine.core.common.businessentities.ExternalDiscoveredHost;
+import org.ovirt.engine.core.common.businessentities.ExternalHostGroup;
 import org.ovirt.engine.core.common.businessentities.Provider;
 import org.ovirt.engine.core.common.businessentities.ProviderType;
 import org.ovirt.engine.core.common.businessentities.StoragePool;
@@ -22,46 +25,82 @@ public class NewHostModel extends HostModel {
     public static final int NewHostDefaultPort = 54321;
     public NewHostModel() {
         super();
-        getExternalHostName().getSelectedItemChangedEvent().addListener(this);
+        getExternalHostName().getSelectedItemChangedEvent().addListener(new IEventListener() {
+            @Override
+            public void eventRaised(Event ev, Object sender, EventArgs args) {
+                hostName_SelectedItemChanged();
+            }
+        });
+
         getExternalHostName().setIsAvailable(ApplicationModeHelper.getUiMode() != ApplicationMode.GlusterOnly);
+        getExternalDiscoveredHosts().getSelectedItemChangedEvent().addListener(new IEventListener() {
+            @Override
+            public void eventRaised(Event ev, Object sender, EventArgs args) {
+                discoverHostName_SelectedItemChanged();
+            }
+        });
+
+        getExternalDiscoveredHosts().setIsAvailable(ApplicationModeHelper.getUiMode() != ApplicationMode.GlusterOnly);
+        getIsDiscorveredHosts().getEntityChangedEvent().addListener(new IEventListener() {
+            @Override
+            public void eventRaised(Event ev, Object sender, EventArgs args) {
+                cleanHostParametersFields();
+                if (getIsDiscorveredHosts().getEntity() != null) {
+                    updateHostList(getIsDiscorveredHosts().getEntity());
+                }
+            }
+        });
+
         getExternalHostProviderEnabled().setIsAvailable(ApplicationModeHelper.getUiMode() != ApplicationMode.GlusterOnly);
-        getProviders().getSelectedItemChangedEvent().addListener(this);
+        getProviders().getSelectedItemChangedEvent().addListener(new IEventListener() {
+            @Override
+            public void eventRaised(Event ev, Object sender, EventArgs args) {
+                providers_SelectedItemChanged();
+            }
+        });
+
         getProviders().setIsAvailable(ApplicationModeHelper.getUiMode() != ApplicationMode.GlusterOnly);
         getProviderSearchFilter().setIsAvailable(ApplicationModeHelper.getUiMode() != ApplicationMode.GlusterOnly);
         getProviderSearchFilterLabel().setIsAvailable(ApplicationModeHelper.getUiMode() != ApplicationMode.GlusterOnly);
-        IEventListener externalHostsListener = new IEventListener() {
-            @Override
-            public void eventRaised(Event ev, Object sender, EventArgs args) {
-                UpdateExternalHostModels();
-            }
-        };
-        getExternalHostProviderEnabled().getEntityChangedEvent().addListener(externalHostsListener);
         getExternalHostProviderEnabled().setEntity(false);
-        getExternalHostName().setIsChangable(false);
         setEnableSearchHost(false);
     }
 
+    // Define events:
+
     private void hostName_SelectedItemChanged()
     {
-        VDS host = getExternalHostName().getSelectedItem();
-
-        if (host == null)
-        {
-            host = new VDS();
+        VDS vds = (VDS) getExternalHostName().getSelectedItem();
+        if (vds == null) {
+            vds = new VDS();
         }
-        updateModelFromVds(host, null, false, null);
+        updateModelFromVds(vds, null, false, null);
+        getHost().setIsChangable(false);
+    }
+
+    private void discoverHostName_SelectedItemChanged() {
+        ExternalDiscoveredHost dhost = (ExternalDiscoveredHost) getExternalDiscoveredHosts().getSelectedItem();
+        VDS vds = new VDS();
+        if (dhost != null) {
+            vds.setVdsName(dhost.getName());
+            vds.setHostName(dhost.getIp());
+        }
+        updateModelFromVds(vds, null, false, null);
+        getName().setIsChangable(true);
     }
 
     private void providers_SelectedItemChanged() {
         Provider provider = getProviders().getSelectedItem();
         setEnableSearchHost(provider != null);
-        getExternalHostName().setItems(null);
-        getExternalHostName().setIsChangable(false);
     }
 
-    private void updateHostList(String searchFilter) {
+    private void updateHostList(boolean isDiscovered) {
         Provider provider = getProviders().getSelectedItem();
-        if (provider != null ) {
+        if (provider == null)  {
+            return;
+        }
+
+        if (!isDiscovered) {
             AsyncQuery getHostsQuery = new AsyncQuery();
             getHostsQuery.asyncCallback = new INewAsyncCallback() {
                 @Override
@@ -69,50 +108,70 @@ public class NewHostModel extends HostModel {
                 {
                     ArrayList<VDS> hosts = (ArrayList<VDS>) result;
                     ListModel<VDS> hostNameListModel = getExternalHostName();
-                    hosts.add(0, null);
                     hostNameListModel.setItems(hosts);
                     hostNameListModel.setIsChangable(true);
                     setEnableSearchHost(true);
                 }
             };
-            AsyncDataProvider.getExternalProviderHostList(getHostsQuery, provider.getId(), true, searchFilter);
+            AsyncDataProvider.getExternalProviderHostList(getHostsQuery, provider.getId(), true, getProviderSearchFilter().getEntity());
         } else {
-            getExternalHostName().setItems(null);
-            getExternalHostName().setIsChangable(false);
-            setEnableSearchHost(false);
-        }
-    }
-
-    private void UpdateExternalHostModels()
-    {
-        boolean enabled = getExternalHostProviderEnabled().getEntity();
-        if (enabled && getProviders().getItems() == null) {
-            AsyncQuery getProvidersQuery = new AsyncQuery();
-            getProvidersQuery.asyncCallback = new INewAsyncCallback() {
+            AsyncQuery getDiscoveredHostsQuery = new AsyncQuery();
+            getDiscoveredHostsQuery.asyncCallback = new INewAsyncCallback() {
                 @Override
                 public void onSuccess(Object model, Object result)
                 {
-                    ArrayList<Provider> providers = (ArrayList<Provider>) result;
-                    ListModel<Provider> providersListModel = getProviders();
-                    providersListModel.setItems(providers);
-                    providersListModel.setIsChangable(true);
+                    ArrayList<ExternalDiscoveredHost> hosts = (ArrayList<ExternalDiscoveredHost>) result;
+                    ListModel externalDiscoveredHostsListModel = getExternalDiscoveredHosts();
+                    externalDiscoveredHostsListModel.setItems(hosts);
+                    externalDiscoveredHostsListModel.setIsChangable(true);
                 }
             };
-            AsyncDataProvider.getAllProvidersByType(getProvidersQuery, ProviderType.FOREMAN);
+            AsyncDataProvider.getExternalProviderDiscoveredHostList(getDiscoveredHostsQuery, provider);
+
+            AsyncQuery getHostGroupsQuery = new AsyncQuery();
+            getHostGroupsQuery.asyncCallback = new INewAsyncCallback() {
+                @Override
+                public void onSuccess(Object model, Object result)
+                {
+                    ArrayList<ExternalHostGroup> hostGroups = (ArrayList<ExternalHostGroup>) result;
+                    ListModel externalHostGroupsListModel = getExternalHostGroups();
+                    externalHostGroupsListModel.setItems(hostGroups);
+                    externalHostGroupsListModel.setIsChangable(true);
+                }
+            };
+            AsyncDataProvider.getExternalProviderHostGroupList(getHostGroupsQuery, provider);
+
+            AsyncQuery getComputeResourceQuery = new AsyncQuery();
+            getComputeResourceQuery .asyncCallback = new INewAsyncCallback() {
+                @Override
+                public void onSuccess(Object model, Object result)
+                {
+                    ArrayList<ExternalComputeResource> computeResources = (ArrayList<ExternalComputeResource>) result;
+                    ListModel externalComputeResourceListModel = getExternalComputeResource();
+                    externalComputeResourceListModel.setItems(computeResources);
+                    externalComputeResourceListModel.setIsChangable(true);
+                }
+            };
+            AsyncDataProvider.getExternalProviderComputeResourceList(getComputeResourceQuery, provider);
         }
-        setHostProviderWidgetsAvailability(enabled);
     }
 
-    @Override
-    public void eventRaised(Event ev, Object sender, EventArgs args)
-    {
-        super.eventRaised(ev, sender, args);
-
-        if (ev.matchesDefinition(ListModel.selectedItemChangedEventDefinition) && sender == getExternalHostName()) {
-            hostName_SelectedItemChanged();
-        } else if (ev.matchesDefinition(ListModel.selectedItemChangedEventDefinition) && sender == getProviders()) {
-            providers_SelectedItemChanged();
-        }
+    private void updateExternalHostModels() {
+        AsyncQuery getProvidersQuery = new AsyncQuery();
+        getProvidersQuery.asyncCallback = new INewAsyncCallback() {
+            @Override
+            public void onSuccess(Object model, Object result)
+            {
+                ArrayList<Provider> providers = (ArrayList<Provider>) result;
+                ListModel<Provider> providersListModel = getProviders();
+                providersListModel.setItems(providers);
+                providersListModel.setIsChangable(true);
+                providersListModel.setSelectedItem(providers.get(0));
+                getIsDiscorveredHosts().setEntity(null);
+                getIsDiscorveredHosts().setEntity(true);
+            }
+        };
+        AsyncDataProvider.getAllProvidersByType(getProvidersQuery, ProviderType.FOREMAN);
     }
 
     @Override
@@ -140,8 +199,8 @@ public class NewHostModel extends HostModel {
     }
 
     @Override
-    protected void updateHosts() {
-        updateHostList(getProviderSearchFilter().getEntity());
+    public void updateHosts() {
+        updateExternalHostModels();
     }
 
     @Override
@@ -165,14 +224,6 @@ public class NewHostModel extends HostModel {
         getProviderSearchFilter().setIsChangable(value);
         getProviderSearchFilterLabel().setIsChangable(value);
         getUpdateHostsCommand().setIsExecutionAllowed(value);
-    }
-
-    private void setHostProviderWidgetsAvailability(boolean enabled) {
-        getProviders().setIsAvailable(enabled);
-        getProviderSearchFilter().setIsAvailable(enabled);
-        getProviderSearchFilterLabel().setIsAvailable(enabled);
-        getExternalHostName().setIsAvailable(enabled);
-        getExternalHostName().setSelectedItem(null);
     }
 
     @Override

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/dataprovider/AsyncDataProvider.java
@@ -32,6 +32,9 @@ import org.ovirt.engine.core.common.businessentities.Disk;
 import org.ovirt.engine.core.common.businessentities.DiskImage;
 import org.ovirt.engine.core.common.businessentities.DiskInterface;
 import org.ovirt.engine.core.common.businessentities.DisplayType;
+import org.ovirt.engine.core.common.businessentities.ExternalComputeResource;
+import org.ovirt.engine.core.common.businessentities.ExternalDiscoveredHost;
+import org.ovirt.engine.core.common.businessentities.ExternalHostGroup;
 import org.ovirt.engine.core.common.businessentities.IVdcQueryable;
 import org.ovirt.engine.core.common.businessentities.ImageFileType;
 import org.ovirt.engine.core.common.businessentities.LUNs;
@@ -2972,8 +2975,7 @@ public final class AsyncDataProvider {
             @Override
             public Object Convert(Object source, AsyncQuery _asyncQuery)
             {
-                if (source == null)
-                {
+                if (source == null) {
                     return new ArrayList<VDS>();
                 }
                 return source;
@@ -2988,13 +2990,62 @@ public final class AsyncDataProvider {
                 aQuery);
     }
 
+    public static void getExternalProviderDiscoveredHostList(AsyncQuery aQuery, Provider provider) {
+        aQuery.converterCallback = new IAsyncConverter() {
+            @Override
+            public Object Convert(Object source, AsyncQuery _asyncQuery)
+            {
+                if (source == null) {
+                    return new ArrayList<ExternalDiscoveredHost>();
+                }
+                return source;
+            }
+        };
+        ProviderQueryParameters params = new ProviderQueryParameters();
+        params.setProvider(provider);
+        Frontend.getInstance().runQuery(VdcQueryType.GetDiscoveredHostListFromExternalProvider, params, aQuery);
+    }
+
+    public static void getExternalProviderHostGroupList(AsyncQuery aQuery, Provider provider) {
+        aQuery.converterCallback = new IAsyncConverter() {
+            @Override
+            public Object Convert(Object source, AsyncQuery _asyncQuery)
+            {
+                if (source == null) {
+                    return new ArrayList<ExternalHostGroup>();
+                }
+                return source;
+            }
+        };
+
+        ProviderQueryParameters params = new ProviderQueryParameters();
+        params.setProvider(provider);
+        Frontend.getInstance().runQuery(VdcQueryType.GetHostGroupsFromExternalProvider, params, aQuery);
+    }
+
+    public static void getExternalProviderComputeResourceList(AsyncQuery aQuery, Provider provider) {
+        aQuery.converterCallback = new IAsyncConverter() {
+            @Override
+            public Object Convert(Object source, AsyncQuery _asyncQuery)
+            {
+                if (source == null) {
+                    return new ArrayList<ExternalComputeResource>();
+                }
+                return source;
+            }
+        };
+
+        ProviderQueryParameters params = new ProviderQueryParameters();
+        params.setProvider(provider);
+        Frontend.getInstance().runQuery(VdcQueryType.GetComputeResourceFromExternalProvider, params, aQuery);
+    }
+
     public static void getAllProviders(AsyncQuery aQuery) {
         aQuery.converterCallback = new IAsyncConverter() {
             @Override
             public Object Convert(Object source, AsyncQuery _asyncQuery)
             {
-                if (source == null)
-                {
+                if (source == null) {
                     return new ArrayList<Provider>();
                 }
                 Collections.sort((List<Provider>) source, new NameableComparator());
@@ -3029,8 +3080,7 @@ public final class AsyncDataProvider {
             @Override
             public Object Convert(Object source, AsyncQuery _asyncQuery)
             {
-                if (source == null)
-                {
+                if (source == null) {
                     return new ArrayList<Provider>();
                 }
                 return source;

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/HostListModel.java
@@ -28,6 +28,9 @@ import org.ovirt.engine.core.common.action.VdcReturnValueBase;
 import org.ovirt.engine.core.common.action.VdsActionParameters;
 import org.ovirt.engine.core.common.businessentities.BusinessEntitiesDefinitions;
 import org.ovirt.engine.core.common.businessentities.DbUser;
+import org.ovirt.engine.core.common.businessentities.ExternalComputeResource;
+import org.ovirt.engine.core.common.businessentities.ExternalDiscoveredHost;
+import org.ovirt.engine.core.common.businessentities.ExternalHostGroup;
 import org.ovirt.engine.core.common.businessentities.FenceActionType;
 import org.ovirt.engine.core.common.businessentities.Provider;
 import org.ovirt.engine.core.common.businessentities.RoleType;
@@ -966,6 +969,19 @@ public class HostListModel extends ListWithDetailsModel implements ISupportSyste
             if (networkProvider != null) {
                 parameters.setProviderId(networkProvider.getId());
                 parameters.setNetworkMappings(model.getInterfaceMappings().getEntity());
+            }
+
+            if (model.getIsProvisioning()) {
+                Provider provider = (Provider) model.getProviders().getSelectedItem();
+                ExternalHostGroup hostGroup = (ExternalHostGroup) model.getExternalHostGroups().getSelectedItem();
+                ExternalComputeResource computeResource = (ExternalComputeResource) model.getExternalComputeResource().getSelectedItem();
+                ExternalDiscoveredHost discoveredHost = (ExternalDiscoveredHost)model.getExternalDiscoveredHosts().getSelectedItem();
+                parameters.initVdsActionParametersForProvision(
+                        provider.getId(),
+                        hostGroup,
+                        computeResource,
+                        discoveredHost.getMac(),
+                        discoveredHost.getName());
             }
 
             Frontend.getInstance().runAction(VdcActionType.AddVds, parameters,

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/providers/ProviderModel.java
@@ -138,8 +138,7 @@ public class ProviderModel extends Model {
     }
 
     private boolean isTypeRequiresAuthentication() {
-        ProviderType type = getType().getSelectedItem();
-        return type == ProviderType.FOREMAN;
+        return false;
     }
 
     private String getDefaultUrl(ProviderType type) {

frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/Enums.java
@@ -101,6 +101,8 @@ public interface Enums extends LocalizedEnums {
 
     String VDSStatus___Connecting();
 
+    String VDSStatus___InstallingOS();
+
     String VdsTransparentHugePages___Never();
 
     String VdsTransparentHugePages___MAdvise();

frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/UIConstants.java
@@ -2015,6 +2015,12 @@ public interface UIConstants extends com.google.gwt.i18n.client.Constants {
     @DefaultStringValue("Secondary")
     String secondaryPmVariant();
 
+    @DefaultStringValue("Discovered Hosts")
+    String externalHostsDiscovered();
+
+    @DefaultStringValue("Provisioned Hosts")
+    String externalHostsProvisioned();
+
     @DefaultStringValue("Eject")
     String eject();
 
@@ -2298,5 +2304,8 @@ public interface UIConstants extends com.google.gwt.i18n.client.Constants {
 
     @DefaultStringValue("Random Number Generator is not supported by your cluster compatibility version.")
     String rngNotSupportedByClusterCV();
+
+    @DefaultStringValue("Random Number Generator not supported for this cluster level or is disabled in the engine config.")
+    String rngNotSupported();
 }
 

commit 91ad04b9062923f92b94bcafacc19e74c3e6a2fd

ui: Fix search button logic
Commit 60331c40535c95950274984d89177643df5686db broke the search button
logic. This patch returns the same refresh logic that was next to the
current changes.

Change-Id: Ia86fa287a050e030d28ab96eeecfb888dd1cb22b
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1165669
Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/EditHostModel.java
@@ -59,6 +59,10 @@ public class EditHostModel extends HostModel {
     }
 
     @Override
+    protected void updateProvisionedHosts() {
+    }
+
+    @Override
     public boolean showExternalProviderPanel() {
         return false;
     }

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/HostModel.java
@@ -886,12 +886,12 @@ public abstract class HostModel extends Model implements HasValidatedTabs
         setUpdateHostsCommand(new UICommand("", new ICommandTarget() { //$NON-NLS-1$
             @Override
             public void executeCommand(UICommand command) {
-                updateHosts();
+                updateProvisionedHosts();
             }
 
             @Override
             public void executeCommand(UICommand uiCommand, Object... parameters) {
-                updateHosts();
+                updateProvisionedHosts();
             }
         }));
         setProxyUpCommand(new UICommand("Up", new ICommandTarget() {    //$NON-NLS-1$
@@ -1875,6 +1875,8 @@ public abstract class HostModel extends Model implements HasValidatedTabs
     protected abstract void setAllowChangeHostPlacementPropertiesWhenNotInMaintenance();
 
     public abstract void updateHosts();
+
+    protected abstract void updateProvisionedHosts();
 
     protected abstract void setPort(VDS vds);
 

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/hosts/NewHostModel.java
@@ -147,6 +147,30 @@ public class NewHostModel extends HostModel {
         AsyncDataProvider.getInstance().getExternalProviderComputeResourceList(getComputeResourceQuery, provider);
     }
 
+    private void updateDiscoveredHostList(String searchFilter) {
+        Provider provider = getProviders().getSelectedItem();
+        if (provider != null ) {
+            AsyncQuery getHostsQuery = new AsyncQuery();
+            getHostsQuery.asyncCallback = new INewAsyncCallback() {
+                @Override
+                public void onSuccess(Object model, Object result)
+                {
+                    ArrayList<VDS> hosts = (ArrayList<VDS>) result;
+                    ListModel<VDS> hostNameListModel = getExternalHostName();
+                    hosts.add(0, null);
+                    hostNameListModel.setItems(hosts);
+                    hostNameListModel.setIsChangable(true);
+                    setEnableSearchHost(true);
+                }
+            };
+            AsyncDataProvider.getInstance().getExternalProviderHostList(getHostsQuery, provider.getId(), true, searchFilter);
+        } else {
+            getExternalHostName().setItems(null);
+            getExternalHostName().setIsChangable(false);
+            setEnableSearchHost(false);
+        }
+    }
+
     private void updateExternalHostModels() {
         AsyncQuery getProvidersQuery = new AsyncQuery();
         getProvidersQuery.asyncCallback = new INewAsyncCallback() {
@@ -191,6 +215,11 @@ public class NewHostModel extends HostModel {
     @Override
     public void updateHosts() {
         updateExternalHostModels();
+    }
+
+    @Override
+    protected void updateProvisionedHosts() {
+        updateDiscoveredHostList(getProviderSearchFilter().getEntity());
     }
 
     @Override