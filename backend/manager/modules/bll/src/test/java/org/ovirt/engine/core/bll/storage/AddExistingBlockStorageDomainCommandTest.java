14252c31fa82296f851a660ff1ee5a5f34b1465f
Maor Lipchuk <mlipchuk@redhat.com>
2014-11-25 14:54:46 +0200

    core: Adding an existing block Storage Domain should be blocked
    Adding an existing Storage Domain should be blocked by a CDA message
    
    Change-Id: Iba5194e4c87ba96a7482fa52b18a2e9812663c7e
    Bug-Url: https://bugzilla.redhat.com/1148803
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/storage/AddExistingBlockStorageDomainCommandTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/storage/AddExistingBlockStorageDomainCommandTest.java
@@ -1,22 +1,29 @@
 package org.ovirt.engine.core.bll.storage;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyList;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.ovirt.engine.core.common.action.StorageDomainManagementParameter;
 import org.ovirt.engine.core.common.businessentities.LUNs;
 import org.ovirt.engine.core.common.businessentities.StorageDomainStatic;
+import org.ovirt.engine.core.common.errors.VdcBllMessages;
 import org.ovirt.engine.core.compat.Guid;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.StorageDomainStaticDAO;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AddExistingBlockStorageDomainCommandTest {
@@ -24,11 +31,19 @@ public class AddExistingBlockStorageDomainCommandTest {
     private AddExistingBlockStorageDomainCommand<StorageDomainManagementParameter> command;
     private StorageDomainManagementParameter parameters;
 
+    @Mock
+    private DbFacade dbFacade;
+
+    @Mock
+    private StorageDomainStaticDAO storageDomainStaticDAO;
+
     @Before
     public void setUp() {
         parameters = new StorageDomainManagementParameter(getStorageDomain());
         parameters.setVdsId(Guid.newGuid());
         command = spy(new AddExistingBlockStorageDomainCommand<>(parameters));
+        doReturn(dbFacade).when(command).getDbFacade();
+        doReturn(storageDomainStaticDAO).when(command).getStorageDomainStaticDAO();
 
         doNothing().when(command).addStorageDomainInDb();
         doNothing().when(command).updateStorageDomainDynamicFromIrs();
@@ -37,9 +52,20 @@ public class AddExistingBlockStorageDomainCommandTest {
 
     @Test
     public void testAddExistingBlockDomainSuccessfully() {
+        when(command.getStorageDomainStaticDAO().get(any(Guid.class))).thenReturn(null);
         when(command.getLUNsFromVgInfo(parameters.getStorageDomain().getStorage())).thenReturn(getLUNs());
         command.executeCommand();
         assertTrue(command.getReturnValue().getSucceeded());
+    }
+
+    @Test
+    public void testAlreadyExistStorageDomain() {
+        when(command.getStorageDomainStaticDAO().get(any(Guid.class))).thenReturn(getStorageDomain());
+        assertFalse("Storage Domain already exists", command.canAddDomain());
+        assertTrue("Import block Storage Domain should have failed due to already existing Storage Domain",
+                command.getReturnValue()
+                        .getCanDoActionMessages()
+                        .contains(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_DOMAIN_ALREADY_EXIST.toString()));
     }
 
     private static StorageDomainStatic getStorageDomain() {
