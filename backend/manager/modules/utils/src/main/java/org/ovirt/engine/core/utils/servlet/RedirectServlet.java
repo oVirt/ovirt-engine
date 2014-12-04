92ce3a199d48f6ecce73b7231e435be13ffb6293
Alon Bar-Lev <alonbl@redhat.com>
2014-12-04 17:17:24 +0200

    utils: RedirectServlet: replace localhost to engine host
    as the redirect happens at client side, the client needs explicit host
    name, while localhost may appear at engine configuration in regard of
    where resource may be.
    
    to overcome this, replace localhost with the Host header or if not
    available with known engine name.
    
    Change-Id: I155489de230124339be4f2493686c83ebaee3a68
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/servlet/RedirectServlet.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/servlet/RedirectServlet.java
@@ -1,6 +1,11 @@
 package org.ovirt.engine.core.utils.servlet;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -26,26 +31,62 @@ import org.ovirt.engine.core.utils.EngineLocalConfig;
 public class RedirectServlet extends HttpServlet {
     private static final long serialVersionUID = -1794616863361241804L;
 
+    private static final String url404Default = EngineLocalConfig.getInstance().getEngineURI() + "/404.html";
+    private static final Pattern urlPattern = Pattern.compile(
+        "((?<prefix>(\\w*://)([^/]*?@)?)?(?<host>(\\[[\\da-fA-F:]+\\])|([^/:]+)?))?(?<suffix>.*)"
+    );
+
+    private static final String LOCALHOST_PLACEHOLDER = "@__LOCALHOST__@";
     private static final String TARGET_URL = "targetUrl";
     private static final String URL404 = "url404";
+    private static final Set localHostNames = new HashSet(Arrays.asList(
+        "localhost",
+        "localhost.localdomain",
+        "localhost4",
+        "localhost4.localdomain",
+        "localhost4.localdomain4",
+        "localhost6",
+        "localhost6.localdomain",
+        "localhost6.localdomain6",
+        "ip4-localhost",
+        "ip4-loopback",
+        "ip6-localhost",
+        "ip6-loopback"
+    ));
 
     private String targetUrl;
     private String url404;
-    private static final String url404Default = EngineLocalConfig.getInstance().getEngineURI() + "/404.html";
+
+    private String injectLocalHost(String url) {
+        Matcher m = urlPattern.matcher(url);
+        if (m.matches()) {
+            String host = m.group("host");
+            if (host != null) {
+                if (localHostNames.contains(host) || host.startsWith("127.") || host.equals("::1")) {
+                    url = String.format(
+                        "%s%s%s",
+                        m.group("prefix") != null ? m.group("prefix") : "",
+                        LOCALHOST_PLACEHOLDER,
+                        m.group("suffix") != null ? m.group("suffix") : ""
+                    );
+                }
+            }
+        }
+        return url;
+    }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
 
         // we use %{x} convention to avoid conflict with jboss properties
-        targetUrl = EngineLocalConfig.getInstance().expandString(
-                config.getInitParameter(TARGET_URL).replaceAll("%\\{", "\\${"));
-        String url404ConfigValue = config.getInitParameter(URL404);
-        if (StringUtils.isNotEmpty(url404ConfigValue)) {
-            url404 = EngineLocalConfig.getInstance().expandString(url404ConfigValue.replaceAll("%\\{", "\\${"));
-        } else {
-            url404 = url404Default;
+        targetUrl = injectLocalHost(EngineLocalConfig.getInstance().expandString(
+                config.getInitParameter(TARGET_URL).replaceAll("%\\{", "\\${")));
+        url404 = url404Default;
+        if (StringUtils.isNotEmpty(config.getInitParameter(URL404))) {
+            url404 = config.getInitParameter(URL404);
         }
+        url404 = injectLocalHost(EngineLocalConfig.getInstance().expandString(url404.replaceAll("%\\{", "\\${")));
     }
 
     @Override
@@ -68,6 +109,22 @@ public class RedirectServlet extends HttpServlet {
         } else {
             redirectUrl = url404;
         }
-        response.sendRedirect(redirectUrl);
+        String localhost = request.getHeader("Host");
+        if (localhost != null) {
+            if (localhost.charAt(0) == '[') {
+                int n = localhost.indexOf("]");
+                if (n != -1) {
+                    localhost = localhost.substring(0, n+1);
+                }
+            } else {
+                int n = localhost.indexOf(":");
+                if (n != -1) {
+                    localhost = localhost.substring(0, n);
+                }
+            }
+        } else {
+            localhost = EngineLocalConfig.getInstance().getHost();
+        }
+        response.sendRedirect(redirectUrl.replace(LOCALHOST_PLACEHOLDER, localhost));
     }
 }
