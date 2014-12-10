1ab51e8b2635df242cfd0452f2a99462149866c8
Juan Hernandez <juan.hernandez@redhat.com>
2014-12-10 18:54:10 +0100

    restapi: Don't validate missing cloud-init enums
    Currently we blindly try to validate enums included in cloud-init
    specifications, but they may be missing. As they aren't declared as
    mandatory this patch changes the validator so it won't try to validate
    them when they are missing.
    
    Change-Id: If7c29a92909fb9f436b06d2ed534fe2cbc34a293
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/validation/CloudInitValidator.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/validation/CloudInitValidator.java
@@ -5,7 +5,9 @@ import static org.ovirt.engine.api.common.util.EnumValidator.validateEnum;
 import org.ovirt.engine.api.model.BootProtocol;
 import org.ovirt.engine.api.model.CloudInit;
 import org.ovirt.engine.api.model.File;
+import org.ovirt.engine.api.model.Files;
 import org.ovirt.engine.api.model.NIC;
+import org.ovirt.engine.api.model.Nics;
 import org.ovirt.engine.api.model.PayloadEncoding;
 
 @ValidatedClass(clazz = CloudInit.class)
@@ -15,18 +17,21 @@ public class CloudInitValidator implements Validator<CloudInit> {
     public void validateEnums(CloudInit model) {
         if (model != null) {
             if (model.isSetNetworkConfiguration()) {
-                if (model.getNetworkConfiguration().isSetNics()
-                        && !model.getNetworkConfiguration().getNics().getNics().isEmpty()) {
-                    for (NIC iface : model.getNetworkConfiguration().getNics().getNics()) {
-                        validateEnum(BootProtocol.class, iface.getBootProtocol(), true);
+                Nics nics = model.getNetworkConfiguration().getNics();
+                if (nics != null && !nics.getNics().isEmpty()) {
+                    for (NIC nic : nics.getNics()) {
+                        if (nic.isSetBootProtocol()) {
+                            validateEnum(BootProtocol.class, nic.getBootProtocol(), true);
+                        }
                     }
                 }
             }
-            if (model.isSetFiles()
-                    && model.getFiles().isSetFiles()
-                    && !model.getFiles().getFiles().isEmpty()) {
-                for (File file : model.getFiles().getFiles()) {
-                    validateEnum(PayloadEncoding.class, file.getType(), true);
+            Files files = model.getFiles();
+            if (files != null && !files.getFiles().isEmpty()) {
+                for (File file : files.getFiles()) {
+                    if (file.isSetType()) {
+                        validateEnum(PayloadEncoding.class, file.getType(), true);
+                    }
                 }
             }
         }
