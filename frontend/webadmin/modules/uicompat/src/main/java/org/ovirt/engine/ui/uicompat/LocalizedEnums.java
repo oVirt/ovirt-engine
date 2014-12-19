902d8c8b86ad767a8c945294729309d338fa0310
Shubhendu Tripathi <shtripat@redhat.com>
2014-12-19 13:24:38 +0530

    gluster: BLL command to set volume snapshot config
    Introduced BLL command to set the volume snapshot configurations
    
    Change-Id: I37b6f4dd02ca4258e5e40e7da11f0a657ac6b330
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

--- frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/LocalizedEnums.java
+++ frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/LocalizedEnums.java
@@ -1,5 +1,7 @@
 package org.ovirt.engine.ui.uicompat;
 
+import org.ovirt.engine.core.common.businessentities.NumaTuneMode;
+import org.ovirt.engine.core.common.AuditLogSeverity;
 import com.google.gwt.i18n.client.ConstantsWithLookup;
 
 public interface LocalizedEnums extends ConstantsWithLookup {
@@ -478,6 +480,12 @@ public interface LocalizedEnums extends ConstantsWithLookup {
     String AuditLogType___GLUSTER_VOLUME_SNAPSHOT_RESTORED();
 
     String AuditLogType___GLUSTER_VOLUME_SNAPSHOT_RESTORE_FAILED();
+
+    String AuditLogType___GLUSTER_VOLUME_SNAPSHOT_CONFIG_UPDATED();
+
+    String AuditLogType___GLUSTER_VOLUME_SNAPSHOT_CONFIG_UPDATE_FAILED();
+
+    String AuditLogType___GLUSTER_VOLUME_SNAPSHOT_CONFIG_UPDATE_FAILED_PARTIALLY();
 
     String VdcActionType___ActivateVds();
 
