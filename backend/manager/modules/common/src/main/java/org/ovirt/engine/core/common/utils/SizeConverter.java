3538d61e4d85eddcbf2def45365ab316c77548f6
Anmol Babu <anbabu@redhat.com>
2014-12-20 14:40:16 +0530

    engine : Math operations on SizeConverter converted sizes
    Facilitate Math operations on SizeConverter converted sizes
    
    Change-Id: Ie9fd4e7b7b9411cf17d66037e6424a979c2d037f
    Signed-off-by: Anmol Babu <anbabu@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/SizeConverter.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/SizeConverter.java
@@ -12,6 +12,7 @@ public class SizeConverter {
     public static final long BYTES_IN_KB = 1024L;
     public static final long BYTES_IN_MB = 1024L * 1024L;
     public static final long BYTES_IN_GB = 1024L * 1024L * 1024L;
+    public static final long BYTES_IN_TB = BYTES_IN_GB * 1024L;
 
     public SizeConverter() {
 
@@ -21,7 +22,8 @@ public class SizeConverter {
         BYTES(1),
         KB(2),
         MB(3),
-        GB(4);
+        GB(4),
+        TB(5);
 
         private long unitWeight;
 
@@ -49,14 +51,22 @@ public class SizeConverter {
         }
 
         public SizeUnit getUnit(long weight) {
-            return (SizeUnit) weightToUnit.get((int) weight).getSecond();
+            return weightToUnit.get((int) weight).getSecond();
+        }
+
+        public static SizeUnit getMaxHandledUnit() {
+            return weightToUnit.get(0).getSecond();
+        }
+
+        public static SizeUnit getMinHandledUnit() {
+            return weightToUnit.get(weightToUnit.size() - 1).getSecond();
         }
     };
 
     public static Number convert(long size, SizeUnit fromUnit, SizeUnit toUnit) {
         long fromType = fromUnit.getUnitWeight();
         long toType = toUnit.getUnitWeight();
-        return (size) * ((Math.pow(CONVERT_FACTOR, fromType)) / (Math.pow(CONVERT_FACTOR, toType)));
+        return size * (Math.pow(CONVERT_FACTOR, fromType) / Math.pow(CONVERT_FACTOR, toType));
     }
 
     public static Pair<SizeUnit, Double> autoConvert(long size, SizeUnit inUnit) {
@@ -67,5 +77,46 @@ public class SizeConverter {
             }
         }
         return new Pair<SizeConverter.SizeUnit, Double>(SizeUnit.BYTES, (double)size);
+    }
+
+    public static SizeUnit leastUnitInList(List<Pair<SizeUnit, Double>> operands) {
+        SizeUnit leastUnit = SizeUnit.getMaxHandledUnit();
+        for(Pair<SizeUnit, Double> operand : operands) {
+            if(operand.getFirst().getUnitWeight() < leastUnit.getUnitWeight()) {
+                leastUnit = operand.getFirst();
+            }
+        }
+        return leastUnit;
+    }
+
+    public static SizeUnit maxUnitInList(List<Pair<SizeUnit, Double>> operands) {
+        SizeUnit maxUnit = SizeUnit.getMinHandledUnit();
+        for(Pair<SizeUnit, Double> operand : operands) {
+            if(operand.getFirst().getUnitWeight() > maxUnit.getUnitWeight()) {
+                maxUnit = operand.getFirst();
+            }
+        }
+        return maxUnit;
+    }
+
+    @SafeVarargs
+    public static List<Pair<SizeUnit, Double>> getMathOperationSafeOperands(Pair<SizeUnit, Double>... operands) {
+        List<Pair<SizeUnit, Double>>  operationReadyOperands = new ArrayList<>();
+        List<Pair<SizeUnit, Double>> convertedOperands = new ArrayList<>();
+        for (Pair<SizeUnit, Double> operand : operands) {
+            convertedOperands.add(autoConvert(operand.getSecond().longValue(), operand.getFirst()));
+        }
+
+        SizeUnit finalUnit = leastUnitInList(convertedOperands);
+
+        for (Pair<SizeUnit, Double> operand : convertedOperands) {
+            if(operand.getFirst() != finalUnit) {
+                operationReadyOperands.add(new Pair<SizeConverter.SizeUnit, Double>(finalUnit,
+                        convert(operand.getSecond().longValue(), operand.getFirst(), finalUnit).doubleValue()));
+            } else {
+                operationReadyOperands.add(operand);
+            }
+        }
+        return operationReadyOperands;
     }
 }
