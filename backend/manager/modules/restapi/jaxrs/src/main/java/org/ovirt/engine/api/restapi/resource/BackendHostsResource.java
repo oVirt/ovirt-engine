oVirt/ovirt-engine

commit 31cd7918c9c5eb5c2d6ddf5526b366fdb2d0f4bc

Host Power-Management Refactor (#977674)
This patch refactors Power-Management to work with 'n' fence agents.

Fence agents now have their own table in the database. Each fence agent has an 'order'
field, and agents are run sequentially according to their order until the fence
operation  succeeds. When 2 or more agents have the same order they are run concurrently.

Power-Management health check was also modified to consider n agents.

API was enhanced to include 'n' or more agents. Now fence-agents have their
own API context: .../api/hosts/xxx/fenceagents. Agents can by added, updated and
deleted.

Change-Id: Id0b384347a52db8b0aa6ae684fad40a5491dd2f7
Signed-off-by: Ori Liel <oliel@redhat.com>
Signed-off-by: emesika <emesika@redhat.com>

backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendHostsResource.java
@@ -1,5 +1,6 @@
 package org.ovirt.engine.api.restapi.resource;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -7,6 +8,7 @@ import javax.ws.rs.core.Response;
 
 import org.ovirt.engine.api.common.util.DetailHelper;
 import org.ovirt.engine.api.model.Action;
+import org.ovirt.engine.api.model.Agent;
 import org.ovirt.engine.api.model.Certificate;
 import org.ovirt.engine.api.model.Cluster;
 import org.ovirt.engine.api.model.Host;
@@ -16,11 +18,13 @@ import org.ovirt.engine.api.model.Statistic;
 import org.ovirt.engine.api.model.Statistics;
 import org.ovirt.engine.api.resource.HostResource;
 import org.ovirt.engine.api.resource.HostsResource;
+import org.ovirt.engine.api.restapi.types.FenceAgentMapper;
 import org.ovirt.engine.api.utils.LinkHelper;
 import org.ovirt.engine.core.common.action.AddVdsActionParameters;
 import org.ovirt.engine.core.common.action.RemoveVdsParameters;
 import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.action.VdsOperationActionParameters;
+import org.ovirt.engine.core.common.businessentities.FenceAgent;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.VDSGroup;
 import org.ovirt.engine.core.common.businessentities.VdsStatic;
@@ -40,7 +44,8 @@ public class BackendHostsResource extends AbstractBackendCollectionResource<Host
 
     private static final Logger log = LoggerFactory.getLogger(BackendHostsResource.class);
     private static final String DEFAULT_NAME = "Default";
-    static final String[] SUB_COLLECTIONS = { "storage", "nics", "numanodes", "tags", "permissions", "statistics", "hooks" };
+    static final String[] SUB_COLLECTIONS = { "storage", "nics", "numanodes", "tags", "permissions", "statistics",
+            "hooks", "fenceagents" };
     static final String GLUSTERONLY_MODE_COLLECTIONS_TO_HIDE = "storage";
 
     public BackendHostsResource() {
@@ -90,12 +95,21 @@ public class BackendHostsResource extends AbstractBackendCollectionResource<Host
         validateParameters(host, "name", "address");
         VdsStatic staticHost = getMapper(Host.class, VdsStatic.class).map(host, null);
         staticHost.setVdsGroupId(getClusterId(host));
-        AddVdsActionParameters addParams = new AddVdsActionParameters(staticHost, host.getRootPassword());
+        AddVdsActionParameters addParams =
+                new AddVdsActionParameters(getMapper(Host.class, VdsStatic.class).map(host, null),
+                        host.getRootPassword());
         if (host.isSetOverrideIptables()) {
             addParams.setOverrideFirewall(host.isOverrideIptables());
         }
         if (host.isSetRebootAfterInstallation()) {
             addParams.setRebootAfterInstallation(host.isRebootAfterInstallation());
+        }
+        if (host.isSetPowerManagement() && host.getPowerManagement().isSetAgents()) {
+            List<FenceAgent> agents = new LinkedList<FenceAgent>();
+            for (Agent agent : host.getPowerManagement().getAgents().getAgents()) {
+                agents.add(FenceAgentMapper.map(agent, null));
+            }
+            addParams.setFenceAgents(agents);
         }
         addParams = (AddVdsActionParameters) getMapper
                 (Host.class, VdsOperationActionParameters.class).map(host, (VdsOperationActionParameters) addParams);

commit b91721acb0dfa69979a42861c93832e003172baa

core:fix adding host from REST API
Fixing a regression resulted from commit
31cd7918c9c5eb5c2d6ddf5526b366fdb2d0f4bc (PM refactoring)

The regression caused a host added using cluster name to fail.

Change-Id: I527e79102ae7af5d2bb6703e20dd388677da30c2
Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1176391
Signed-off-by: emesika <emesika@redhat.com>

backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendHostsResource.java
@@ -95,9 +95,7 @@ public class BackendHostsResource extends AbstractBackendCollectionResource<Host
         validateParameters(host, "name", "address");
         VdsStatic staticHost = getMapper(Host.class, VdsStatic.class).map(host, null);
         staticHost.setVdsGroupId(getClusterId(host));
-        AddVdsActionParameters addParams =
-                new AddVdsActionParameters(getMapper(Host.class, VdsStatic.class).map(host, null),
-                        host.getRootPassword());
+        AddVdsActionParameters addParams = new AddVdsActionParameters(staticHost, host.getRootPassword());
         if (host.isSetOverrideIptables()) {
             addParams.setOverrideFirewall(host.isOverrideIptables());
         }