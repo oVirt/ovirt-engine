8c7e49ba5336413d595e1da7dd7e1fa30c05e9c2
Tal Nisan <tnisan@redhat.com>
2014-12-31 16:50:34 +0200

    core: Filter import block storage domains by LUN id
    Patch Id82e5e1d3df8ce265833efb3fc4b9050ca73b679 introduced filtering and
    blocking import of an existing block domain by its PV id. The hosted engine
    setup only populates the LUN id of the hosted engine disk thus the filtering
    was changed to be based on the LUN id
    
    Change-Id: Ic1082319e68e8c5a42f8f177e13d662829753f8c
    Bug-Url: https://bugzilla.redhat.com/1157243
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/GetUnregisteredBlockStorageDomainsQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/GetUnregisteredBlockStorageDomainsQuery.java
@@ -209,7 +209,7 @@ public class GetUnregisteredBlockStorageDomainsQuery<P extends GetUnregisteredBl
         List<StorageDomain> storageDomains = new ArrayList<>();
 
         // Get existing PhysicalVolumes.
-        List<String> existingPhysicalVolumeIds = Entities.getPhysicalVolumesFromLunsList(getLunDAO().getAll());
+        List<String> existingLunIds = Entities.getIds(getLunDAO().getAll());
 
         for (String vgID : vgIDs) {
             VDSReturnValue returnValue = null;
@@ -223,8 +223,8 @@ public class GetUnregisteredBlockStorageDomainsQuery<P extends GetUnregisteredBl
             }
 
             ArrayList<LUNs> luns = (ArrayList<LUNs>) returnValue.getReturnValue();
-            List<String> physicalVolumeIdsOnStorage = Entities.getPhysicalVolumesFromLunsList(luns);
-            if (CollectionUtils.containsAny(physicalVolumeIdsOnStorage, existingPhysicalVolumeIds)) {
+            List<String> lunIdsOnStorage = Entities.getIds(luns);
+            if (CollectionUtils.containsAny(lunIdsOnStorage, existingLunIds)) {
                 log.infoFormat("There are existing luns in the system which are part of VG id '{0}'", vgID);
                 continue;
             }
