c285085926b580ba6c5c9a70bb660f7e645f4ac3
Yevgeny Zaspitsky <yzaspits@redhat.com>
2015-01-20 13:58:14 +0200

    engine: Add GetDefaultManagementNetwork query
    Add GetDefaultManagementNetwork query.
    
    Change-Id: I723c5506768c88185e1a2afba766cd23d097703d
    Signed-off-by: Yevgeny Zaspitsky <yzaspits@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/dc/GetDefaultManagementNetworkQuery.java
@@ -0,0 +1,25 @@
+package org.ovirt.engine.core.bll.network.dc;
+
+import javax.inject.Inject;
+
+import org.ovirt.engine.core.bll.QueriesCommandBase;
+import org.ovirt.engine.core.bll.network.cluster.DefaultManagementNetworkFinder;
+import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.compat.Guid;
+
+public class GetDefaultManagementNetworkQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+
+    @Inject
+    private DefaultManagementNetworkFinder defaultManagementNetworkFinder;
+
+    public GetDefaultManagementNetworkQuery(P parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected void executeQueryCommand() {
+        final Guid dataCenterId = getParameters().getId();
+
+        getQueryReturnValue().setReturnValue(defaultManagementNetworkFinder.findDefaultManagementNetwork(dataCenterId));
+    }
+}
