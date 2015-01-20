65d74dea4803b582bc9ee22923cbdb677c98b699
Moti Asayag <masayag@redhat.com>
2015-01-20 23:26:18 +0200

    core: Javanize AddVdsActionParameters
    * Format the class
    * Maintain naming conventions
    * Remove redundant code
    * Organize the class
    
    Change-Id: Idcb067baa3b6f4f3be057d881e29c29e22dcad21
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddVdsActionParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddVdsActionParameters.java
@@ -11,55 +11,51 @@ import org.ovirt.engine.core.compat.Guid;
 public class AddVdsActionParameters extends VdsOperationActionParameters {
     private static final long serialVersionUID = 8452910234577071082L;
 
-    private Guid vdsId;
-    private boolean privateAddPending;
+    private Guid hostId;
+    private boolean pending;
     private boolean glusterPeerProbeNeeded;
-    private boolean privateAddProvisioned;
+    private boolean provisioned;
     private ExternalHostGroup hostGroup;
     private String hostMac;
     private String discoverName;
     private ExternalComputeResource computeResource;
     private String discoverIp;
-    public String getDiscoverIp() { return discoverIp; }
     private List<FenceAgent> fenceAgents;
-    public ExternalHostGroup getHostGroup() { return hostGroup; };
-    public String getHostMac() { return hostMac; };
-    public String getDiscoverName() { return discoverName; };
-    public ExternalComputeResource getComputeResource() { return computeResource; }
+
+    public AddVdsActionParameters() {
+        glusterPeerProbeNeeded = true;
+    }
 
     public AddVdsActionParameters(VdsStatic vdsStatic, String password) {
         super(vdsStatic, password);
         glusterPeerProbeNeeded = true;
-        privateAddProvisioned = false;
     }
 
-    public void initVdsActionParametersForProvision(Guid pid,
-                                                    ExternalHostGroup hg,
-                                                    ExternalComputeResource cr,
-                                                    String mac,
-                                                    String discover_name,
-                                                    String discover_ip) {
-        privateAddProvisioned = true;
-        hostMac = mac;
-        hostGroup = hg;
-        getVdsStaticData().setHostProviderId(pid);
-        discoverName = discover_name;
-        computeResource = cr;
-        discoverIp = discover_ip;
+    public void initVdsActionParametersForProvision(Guid providerId,
+            ExternalHostGroup hostGroup,
+            ExternalComputeResource computeResource,
+            String hostMac,
+            String discoverName,
+            String discoverIp) {
+        provisioned = true;
+        this.hostMac = hostMac;
+        this.hostGroup = hostGroup;
+        this.getVdsStaticData().setHostProviderId(providerId);
+        this.discoverName = discoverName;
+        this.computeResource = computeResource;
+        this.discoverIp = discoverIp;
     }
 
     public boolean getAddProvisioned() {
-        return privateAddProvisioned;
+        return provisioned;
     }
 
-    public AddVdsActionParameters() { glusterPeerProbeNeeded = true; }
-
     public void setVdsForUniqueId(Guid serverForUniqueId) {
-        this.vdsId = serverForUniqueId;
+        this.hostId = serverForUniqueId;
     }
 
     public Guid getVdsForUniqueId() {
-        return vdsId;
+        return hostId;
     }
 
     public void setGlusterPeerProbeNeeded(boolean glusterPeerProbeNeeded) {
@@ -67,15 +63,15 @@ public class AddVdsActionParameters extends VdsOperationActionParameters {
     }
 
     public boolean isGlusterPeerProbeNeeded() {
-        return this.glusterPeerProbeNeeded;
+        return glusterPeerProbeNeeded;
     }
 
     public boolean getAddPending() {
-        return privateAddPending;
+        return pending;
     }
 
     public void setAddPending(boolean value) {
-        privateAddPending = value;
+        pending = value;
     }
 
     public List<FenceAgent> getFenceAgents() {
@@ -84,5 +80,25 @@ public class AddVdsActionParameters extends VdsOperationActionParameters {
 
     public void setFenceAgents(List<FenceAgent> fenceAgents) {
         this.fenceAgents = fenceAgents;
+    }
+
+    public String getDiscoverIp() {
+        return discoverIp;
+    }
+
+    public ExternalHostGroup getHostGroup() {
+        return hostGroup;
+    }
+
+    public String getHostMac() {
+        return hostMac;
+    }
+
+    public String getDiscoverName() {
+        return discoverName;
+    }
+
+    public ExternalComputeResource getComputeResource() {
+        return computeResource;
     }
 }
