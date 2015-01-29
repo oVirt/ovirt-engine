cb2f50287d0060bf46f7e5dac053341af5131191
jenkins <jenkins@ovirt.org>
2015-01-29 09:07:11 -0700

    core: remove irrelevant rows generated by pg_dump
    Those rows where not recognized correctly from engine-setup
    
    Change-Id: I3e62040ef549feb791c9f694431e2bb98e740a66
    Signed-off-by: emesika <emesika@redhat.com>

--- packaging/dbscripts/data/0080_insert_images.sql
+++ packaging/dbscripts/data/0080_insert_images.sql
@@ -2,14 +2,6 @@
 -- PostgreSQL database dump
 --
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
-SET search_path = public, pg_catalog;
 
 --
 -- Data for Name: images; Type: TABLE DATA; Schema: public; Owner: engine
