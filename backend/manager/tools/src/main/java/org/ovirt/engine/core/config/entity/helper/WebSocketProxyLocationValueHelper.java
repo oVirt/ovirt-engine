ee7c76a1555b1fe3e39721e86b8b2713b9dc1d55
Frantisek Kobzik <fkobzik@redhat.com>
2015-02-03 17:40:20 +0100

    tools: Validate WebSocketProxy config value
    Change-Id: I84b02d25a15762ba24544bd678328e6532d360ac
    Bug-Url: https://bugzilla.redhat.com/1184164
    Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>

---
+++ backend/manager/tools/src/main/java/org/ovirt/engine/core/config/entity/helper/WebSocketProxyLocationValueHelper.java
@@ -0,0 +1,38 @@
+package org.ovirt.engine.core.config.entity.helper;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.commons.lang.StringUtils;
+import org.ovirt.engine.core.config.entity.ConfigKey;
+
+public class WebSocketProxyLocationValueHelper extends StringValueHelper {
+
+    @Override
+    public ValidationResult validate(ConfigKey key, String value) {
+        if (StringUtils.isBlank(value)) {
+            return new ValidationResult(false, "The WebSocketProxy can't be empty.");
+        }
+
+        if ("Off".equals(value) || validHostPort(value)) {
+            return new ValidationResult(true);
+        }
+
+        return new ValidationResult(false, "Correct values are: Off (proxy is not deployed), " +
+                "Engine:<port> (Engine is reserved keyword meaning proxy is deployed on the same machine as " +
+                "the engine (on given port)), Host:<port> (Host is reserved keyword meaning proxy is deployed " +
+                "on each host on given port (if the deployment has more hosts, proxy must be deployed on each " +
+                "of them)), <hostname>:<port> (proxy is deployed on a machine identified by given hostname or ip " +
+                "and port).");
+    }
+
+    private boolean validHostPort(String value) {
+        try {
+            URI uri = new URI("foo://" + value);
+            return 0 < uri.getPort() && uri.getPort() < 65636;
+        } catch (URISyntaxException e) {
+            return false;
+        }
+    }
+
+}
+
---
+++ backend/manager/tools/src/test/java/org/ovirt/engine/core/config/entity/helper/WebSocketProxyLocationValueHelperTest.java
@@ -0,0 +1,96 @@
+package org.ovirt.engine.core.config.entity.helper;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class WebSocketProxyLocationValueHelperTest {
+
+    @Test
+    public void testValidateNull() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertFalse(helper.validate(null, null).isOk());
+    }
+
+    @Test
+    public void testValidateEmpty() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertFalse(helper.validate(null, "").isOk());
+    }
+
+    @Test
+    public void testValidateIncomplete() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertFalse(helper.validate(null, "myengine:").isOk());
+    }
+
+    @Test
+    public void testNoColons() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertFalse(helper.validate(null, "myengine-blah").isOk());
+    }
+
+    @Test
+    public void testValidateNegativePort() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertFalse(helper.validate(null, "myengine.com:-6100").isOk());
+    }
+
+
+    @Test
+    public void testValidateNonIntPort() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertFalse(helper.validate(null, "myengine.com:3.14156").isOk());
+    }
+
+    @Test
+    public void testValidatePortTooBig() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertFalse(helper.validate(null, "myengine.com:314156").isOk());
+    }
+
+    @Test
+    public void testValidateHostPort() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertTrue(helper.validate(null, "myengine.com:6100").isOk());
+    }
+
+    @Test
+    public void testValidateIp4() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertTrue(helper.validate(null, "192.168.1.1:6100").isOk());
+    }
+
+    @Test
+    public void testValidateIp6Localhost() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertTrue(helper.validate(null, "[::1]:6100").isOk());
+    }
+
+    @Test
+    public void testValidateIp6() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertTrue(helper.validate(null, "[2001:0db8:85a3:08d3:1319:8a2e:0370:7344]:1234").isOk());
+    }
+
+    @Test
+    public void testValidateOff() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertTrue(helper.validate(null, "Off").isOk());
+    }
+
+    @Test
+    public void testValidateEngine() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertTrue(helper.validate(null, "Engine:6100").isOk());
+    }
+
+    @Test
+    public void testValidateHost() throws Exception {
+        WebSocketProxyLocationValueHelper helper = new WebSocketProxyLocationValueHelper();
+        assertTrue(helper.validate(null, "Host:6100").isOk());
+    }
+
+}
