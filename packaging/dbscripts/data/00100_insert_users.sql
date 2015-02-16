523e409badef20662ac58ecff78dd6614b1aa830
emesika <emesika@redhat.com>
2015-02-16 16:16:42 +0200

    core: fixing users data insertion
    Changed hex value in insert users script to a call to the PG decode
    function since the hex value did not pass in PG 8.4.x on RHEL
    
    Change-Id: I9b522fa1c47df034dce80243440a77fd364d7352
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1192436
    Signed-off-by: emesika <emesika@redhat.com>

--- packaging/dbscripts/data/00100_insert_users.sql
+++ packaging/dbscripts/data/00100_insert_users.sql
@@ -7,7 +7,7 @@
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: engine
 --
 
-INSERT INTO users (user_id, name, surname, domain, username, groups, department, role, email, note, status, last_admin_check_status, group_ids, external_id) VALUES ('fdfc627c-d875-11e0-90f0-83df133b58cc', 'admin', NULL, 'internal', 'admin@internal', '', NULL, NULL, NULL, NULL, 1, false, NULL, '\xfdfc627cd87511e090f083df133b58cc');
+INSERT INTO users (user_id, name, surname, domain, username, groups, department, role, email, note, status, last_admin_check_status, group_ids, external_id) VALUES ('fdfc627c-d875-11e0-90f0-83df133b58cc', 'admin', NULL, 'internal', 'admin@internal', '', NULL, NULL, NULL, NULL, 1, false, NULL, decode('fdfc627cd87511e090f083df133b58cc','hex'));
 
 
 --
