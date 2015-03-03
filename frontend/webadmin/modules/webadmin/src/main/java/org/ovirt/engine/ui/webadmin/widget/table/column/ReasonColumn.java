4632460a2e0956a2d6eba638e9483ae0628000ed
Alona Kaplan <alkaplan@redhat.com>
2015-03-03 10:03:16 +0200

    webadmin: Showing the tooltip of the reason column
    Change-Id: Ie63a98a880b67bdfdb6a57ad840f4f2efed9155c
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/table/column/ReasonColumn.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/table/column/ReasonColumn.java
@@ -1,27 +1,28 @@
 package org.ovirt.engine.ui.webadmin.widget.table.column;
 
-import com.google.gwt.resources.client.ImageResource;
+import org.ovirt.engine.ui.common.widget.table.column.AbstractSafeHtmlColumn;
+import org.ovirt.engine.ui.webadmin.ApplicationResources;
+import org.ovirt.engine.ui.webadmin.ApplicationTemplates;
 
-public abstract class ReasonColumn<T> extends AbstractWebAdminImageResourceColumn<T> {
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+
+public abstract class ReasonColumn<T> extends AbstractSafeHtmlColumn<T> {
+
+    ApplicationResources resources = GWT.create(ApplicationResources.class);
+    ApplicationTemplates templates = GWT.create(ApplicationTemplates.class);
 
     @Override
-    public ImageResource getValue(T value) {
-        setTitle(getReason(value));
+    public SafeHtml getValue(T value) {
         if (getReason(value) != null && !getReason(value).trim().isEmpty()) {
-            return getApplicationResources().commentImage();
+            return templates.inlineImageWithTitle(SafeHtmlUtils.fromTrustedString(AbstractImagePrototype.create(resources.commentImage())
+                    .getHTML()),
+                    getReason(value));
         }
         return null;
     }
 
     protected abstract String getReason(T value);
-
-    @Override
-    public ImageResource getDefaultImage() {
-        return getApplicationResources().commentImage();
-    }
-
-    @Override
-    public String getDefaultTitle() {
-        return CONSTANTS.reasonLabel();
-    }
 }
