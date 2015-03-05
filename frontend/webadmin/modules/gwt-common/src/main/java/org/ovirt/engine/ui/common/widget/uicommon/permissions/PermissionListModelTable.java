c0a8d3f38e56c0b71fbb083b7ad54811357a1b85
Roy Golan <rgolan@redhat.com>
2015-03-05 14:59:45 +0200

    webadmin: extract methods in permissionsListModel
    bring order and readability to the PermissionsListModel
    
    Change-Id: If910d6d2675f28e3d6c9c6df8e67f6801e2d42a5
    Bug-Url: https://bugzilla.redhat.com/1080029
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/permissions/PermissionListModelTable.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/permissions/PermissionListModelTable.java
@@ -30,9 +30,23 @@ public class PermissionListModelTable<P extends PermissionListModel<?>> extends 
     @Override
     public void initTable() {
         getTable().enableColumnResizing();
+        //add columns
+        addTypeColumn(constants);
+        addUserColumn(constants);
+        addAuthzColumn(constants);
+        addNameSpaceColumn(constants);
+        addRoleColumn(constants);
+        addCreationDateColum(constants);
+        // add buttons
+        addAddPermissionsButton(constants);
+        addRemovePermissionsButton(constants);
+    }
 
+    private void addTypeColumn(CommonApplicationConstants constants) {
         getTable().addColumn(new PermissionTypeColumn(), constants.empty(), "30px"); //$NON-NLS-1$
+    }
 
+    private void addUserColumn(CommonApplicationConstants constants) {
         AbstractTextColumn<Permission> userColumn = new AbstractTextColumn<Permission>() {
             @Override
             public String getValue(Permission object) {
@@ -41,7 +55,9 @@ public class PermissionListModelTable<P extends PermissionListModel<?>> extends 
         };
         userColumn.makeSortable();
         getTable().addColumn(userColumn, constants.userPermission(), "300px"); //$NON-NLS-1$
+    }
 
+    private void addAuthzColumn(CommonApplicationConstants constants) {
         AbstractTextColumn<Permission> authzColumn = new AbstractTextColumn<Permission>() {
             @Override
             public String getValue(Permission object) {
@@ -50,7 +66,9 @@ public class PermissionListModelTable<P extends PermissionListModel<?>> extends 
         };
         authzColumn.makeSortable();
         getTable().addColumn(authzColumn, constants.authz(), "150px"); //$NON-NLS-1$
+    }
 
+    private void addNameSpaceColumn(CommonApplicationConstants constants) {
         AbstractTextColumn<Permission> namespaceColumn = new AbstractTextColumn<Permission>() {
             @Override
             public String getValue(Permission object) {
@@ -59,8 +77,10 @@ public class PermissionListModelTable<P extends PermissionListModel<?>> extends 
         };
         namespaceColumn.makeSortable();
         getTable().addColumn(namespaceColumn, constants.namespace(), "150px"); //$NON-NLS-1$
+    }
 
-        AbstractTextColumn<Permission> roleColumn = new AbstractTextColumn<Permission>() {
+        private void addRoleColumn(CommonApplicationConstants constants) {
+            AbstractTextColumn<Permission> roleColumn = new AbstractTextColumn<Permission>() {
             @Override
             public String getValue(Permission object) {
                 return object.getRoleName();
@@ -68,23 +88,30 @@ public class PermissionListModelTable<P extends PermissionListModel<?>> extends 
         };
         roleColumn.makeSortable();
         getTable().addColumn(roleColumn, constants.rolePermission(), "300px"); //$NON-NLS-1$
+    }
 
-        AbstractTextColumn<Permission> creationDateColumn = new AbstractRenderedTextColumn<Permission, Date>(new FullDateTimeRenderer()) {
-            @Override
-            public Date getRawValue(Permission object) {
-                return new Date(object.getCreationDate() * 1000); // GWT doesn't have TimeUnit.java
-            }
+    private void addCreationDateColum(CommonApplicationConstants constants) {
+            AbstractTextColumn<Permission> creationDateColumn = new AbstractRenderedTextColumn<Permission, Date>(new FullDateTimeRenderer()) {
+                @Override
+                public Date getRawValue(Permission object) {
+                    return new Date(object.getCreationDate() * 1000); // GWT doesn't have TimeUnit.java
+                }
         };
         getTable().addColumn(creationDateColumn, constants.permissionsCreationDate(), "300px"); //$NON-NLS-1$
         creationDateColumn.makeSortable();
+    }
 
+    private void addAddPermissionsButton(final CommonApplicationConstants constants) {
         getTable().addActionButton(new UiCommandButtonDefinition<Permission>(getEventBus(), constants.addPermission()) {
             @Override
             protected UICommand resolveCommand() {
                 return getModel().getAddCommand();
             }
         });
-        getTable().addActionButton(new UiCommandButtonDefinition<Permission>(getEventBus(), constants.removePermission()) {
+    }
+
+        private void addRemovePermissionsButton(final CommonApplicationConstants constants) {
+            getTable().addActionButton(new UiCommandButtonDefinition<Permission>(getEventBus(), constants.removePermission()) {
             @Override
             protected UICommand resolveCommand() {
                 return getModel().getRemoveCommand();
