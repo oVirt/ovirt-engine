6d1cbe65a96a67675c71811986cb9158591c8fdc
Moti Asayag <masayag@redhat.com>
2015-03-10 14:02:35 +0200

    engine: Add missing type argument
    Change-Id: Ie01a2a2dd51f38d438c780f5010b81fdd72cbd70
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/CommandMocks.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/CommandMocks.java
@@ -1,13 +1,13 @@
 package org.ovirt.engine.core;
 
+import static org.mockito.Mockito.doReturn;
+
 import org.ovirt.engine.core.bll.CommandBase;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
-import static org.mockito.Mockito.doReturn;
-
 public class CommandMocks {
 
-    public static void mockDbFacade(CommandBase cmdMock, DbFacade dbFacade) {
+    public static void mockDbFacade(CommandBase<?> cmdMock, DbFacade dbFacade) {
         doReturn(dbFacade).when(cmdMock).getDbFacade();
     }
 
