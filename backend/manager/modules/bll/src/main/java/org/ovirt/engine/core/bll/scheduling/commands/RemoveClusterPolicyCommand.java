512221299b0e3250d1abe0ef4da59e1836fd757e
Dudi Maroshi <dudi@redhat.com>
2015-03-11 14:09:23 +0200

    Engine: Scheduling-policy removal error message correction
    When removing shceduling policy attached to cluster.
    Provide the correct error message.
    Append the list of attached clusters to error message.
    
    Bug-Url: https://bugzilla.redhat.com/1189096
    Change-Id: I3819081305c42949f490072d54b3b640a86906ea
    Signed-off-by: Dudi Maroshi <dudi@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/commands/RemoveClusterPolicyCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/commands/RemoveClusterPolicyCommand.java
@@ -1,7 +1,12 @@
 package org.ovirt.engine.core.bll.scheduling.commands;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.scheduling.SchedulingManager;
 import org.ovirt.engine.core.common.AuditLogType;
+import org.ovirt.engine.core.common.businessentities.VDSGroup;
 import org.ovirt.engine.core.common.errors.VdcBllMessages;
 import org.ovirt.engine.core.common.scheduling.parameters.ClusterPolicyCRUDParameters;
 
@@ -17,9 +22,19 @@ public class RemoveClusterPolicyCommand extends ClusterPolicyCRUDCommand {
             return false;
         }
         if (SchedulingManager.getInstance().getClustersByClusterPolicyId(getParameters().getClusterPolicyId()).size() > 0) {
-            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_CLUSTER_POLICY_PARAMETERS_INVALID);
+            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_CLUSTER_POLICY_INUSE,
+                    String.format("$clusters %1$s", clustersListIntoTokenizedString()));
         }
         return true;
+    }
+
+    private String clustersListIntoTokenizedString() {
+        List<VDSGroup> attachedClustersList =
+                SchedulingManager.getInstance().getClustersByClusterPolicyId(getParameters().getClusterPolicyId());
+        List<String> clusterNamesList = new LinkedList<>();
+        for (VDSGroup vdsGroup : attachedClustersList)
+            clusterNamesList.add(vdsGroup.getName());
+        return StringUtils.join(clusterNamesList, ',');
     }
 
     @Override
