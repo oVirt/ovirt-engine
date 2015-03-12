e77cc1860d043b6c2369d012a5d4c8c9120b52e1
Moti Asayag <masayag@redhat.com>
2015-03-12 21:14:52 +0200

    engine: Use formatter for Command
    Change-Id: I03554ca006cce684a57fbf0720e3382f0d018bf4
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/tasks/interfaces/Command.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/tasks/interfaces/Command.java
@@ -4,6 +4,8 @@ import org.ovirt.engine.core.common.action.VdcActionParametersBase;
 import org.ovirt.engine.core.common.action.VdcReturnValueBase;
 
 public interface Command<T extends VdcActionParametersBase> {
+
     VdcReturnValueBase endAction();
+
     T getParameters();
 }
