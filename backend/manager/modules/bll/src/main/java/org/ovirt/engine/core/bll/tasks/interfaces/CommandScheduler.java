996c4fbc55b6909dc7966ec82e5757f2632d6f04
Moti Asayag <masayag@redhat.com>
2015-03-12 21:10:31 +0200

    engine: Remove default modifiers in CommandScheduler
    The public abstract are default modifiers in interface and no
    need to specify it.
    
    Change-Id: Ie58cc4712b190d6a3ca433f7b83245bd4ed19d51
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/tasks/interfaces/CommandScheduler.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/tasks/interfaces/CommandScheduler.java
@@ -1,12 +1,14 @@
 package org.ovirt.engine.core.bll.tasks.interfaces;
 
+import java.util.concurrent.Future;
+
 import org.ovirt.engine.core.bll.context.CommandContext;
 import org.ovirt.engine.core.common.action.VdcActionParametersBase;
 import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.action.VdcReturnValueBase;
 
-import java.util.concurrent.Future;
-
 public interface CommandScheduler {
-    public abstract Future<VdcReturnValueBase> executeAsyncCommand(VdcActionType actionType, VdcActionParametersBase parameters, CommandContext cmdContext);
+    Future<VdcReturnValueBase> executeAsyncCommand(VdcActionType actionType,
+            VdcActionParametersBase parameters,
+            CommandContext cmdContext);
 }
