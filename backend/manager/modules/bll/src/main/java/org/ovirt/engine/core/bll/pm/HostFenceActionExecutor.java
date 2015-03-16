325b469aea58813c7b45ab4b1ed59a1cd33c71b5
Martin Perina <mperina@redhat.com>
2015-03-16 17:16:26 +0100

    core: Introduce HostFenceActionExecutor
    Introduces HostFenceActionExecutor which is responsible to
    execute fence action on specified host.
    
    Change-Id: I15bbc7e4a7a297173d3486394ac53587306078b1
    Bug-Url: https://bugzilla.redhat.com/1182510
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/pm/HostFenceActionExecutor.java
@@ -0,0 +1,124 @@
+package org.ovirt.engine.core.bll.pm;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.ovirt.engine.core.common.businessentities.FenceAgent;
+import org.ovirt.engine.core.common.businessentities.FencingPolicy;
+import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.pm.FenceActionType;
+import org.ovirt.engine.core.common.businessentities.pm.FenceOperationResult;
+import org.ovirt.engine.core.common.businessentities.pm.FenceOperationResult.Status;
+import org.ovirt.engine.core.common.businessentities.pm.PowerStatus;
+
+/**
+ * It manages:
+ * <ul>
+ *     <li>Iteration on specified fence agents using {@code PowerManagementHelper.AgentsIterator}</li>
+ *     <li>Execution of fence action using all existing fence agents for the host</li>
+ *     <li>Execution of fence action using only specified fence agents</li>
+ *     <li>Usage of {@code SingleAgentFenceActionExecutor} and {@code ConcurrentAgentsFenceActionExecutor}
+ *         to execute fence action for particular fence agent(s)</li>
+ * </ul>
+ */
+public class HostFenceActionExecutor {
+    /**
+     * Host which the action is executed for
+     */
+    private final VDS fencedHost;
+
+    /**
+     * Fencing policy applied during action execution
+     */
+    private final FencingPolicy fencingPolicy;
+
+    public HostFenceActionExecutor(VDS fencedHost) {
+        this(fencedHost, null);
+    }
+
+    public HostFenceActionExecutor(VDS fencedHost, FencingPolicy fencingPolicy) {
+        this.fencedHost = fencedHost;
+        this.fencingPolicy = fencingPolicy;
+    }
+
+    /**
+     * Executes specified fence action on the {@code fencedHost} using fence agents defined for the host
+     *
+     * @param fenceAction
+     *            specified fence action
+     * @return result of the action
+     */
+    public FenceOperationResult fence(FenceActionType fenceAction) {
+        if (fencedHost.getFenceAgents() == null || fencedHost.getFenceAgents().isEmpty()) {
+            return new FenceOperationResult(
+                    Status.ERROR,
+                    PowerStatus.UNKNOWN,
+                    String.format(
+                            "Invalid fence agents defined for host '%s'.",
+                            fencedHost.getHostName()));
+        }
+        return fence(fenceAction, fencedHost.getFenceAgents());
+    }
+
+    /**
+     * Executes status fence action on fence agents defined for {@code fencedHost} to determine if it's powered off
+     *
+     * @return {@code true} if host is powered off, otherwise {@code false}
+     */
+    public boolean isHostPoweredOff() {
+        FenceOperationResult result = fence(FenceActionType.STATUS);
+        return result.getStatus() == FenceOperationResult.Status.SUCCESS
+                && result.getPowerStatus() == PowerStatus.OFF;
+    }
+
+    /**
+     * Executes status fence action on the {@code fencedHost} using only specified fence agent
+     *
+     * @param fenceAgent
+     *            specified fence agent
+     * @return result of the action
+     */
+    public FenceOperationResult getFenceAgentStatus(FenceAgent fenceAgent) {
+        if (fenceAgent == null) {
+            return new FenceOperationResult(
+                    Status.ERROR,
+                    PowerStatus.UNKNOWN,
+                    "Invalid fence agent specified.");
+        }
+        return fence(FenceActionType.STATUS, Arrays.asList(fenceAgent));
+    }
+
+    /**
+     * Executes specified fence action using specified fence agents
+     */
+    protected FenceOperationResult fence(FenceActionType fenceAction, List<FenceAgent> fenceAgents) {
+        PowerManagementHelper.AgentsIterator iterator = createFenceAgentsIterator(fenceAgents);
+        FenceOperationResult result = null;
+        while (iterator.hasNext()) {
+            result = createFenceActionExecutor(iterator.next()).fence(fenceAction);
+            if (result.getStatus() == Status.SUCCESS) {
+                break;
+            }
+        }
+        return result;
+
+    }
+
+    /**
+     * Creates fence agents iterator
+     */
+    protected PowerManagementHelper.AgentsIterator createFenceAgentsIterator(List<FenceAgent> fenceAgents) {
+        return PowerManagementHelper.getAgentsIterator(fenceAgents);
+    }
+
+    /**
+     * Creates instance of {@code FenceActionExecutor} according to specified fence agents
+     */
+    FenceActionExecutor createFenceActionExecutor(List<FenceAgent> fenceAgents) {
+        if (fenceAgents.size() == 1) {
+            return new SingleAgentFenceActionExecutor(fencedHost, fenceAgents.get(0), fencingPolicy);
+        } else {
+            return new ConcurrentAgentsFenceActionExecutor(fencedHost, fenceAgents, fencingPolicy);
+        }
+    }
+}
---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/pm/HostFenceActionExecutorTest.java
@@ -0,0 +1,263 @@
+package org.ovirt.engine.core.bll.pm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.ovirt.engine.core.common.businessentities.FenceAgent;
+import org.ovirt.engine.core.common.businessentities.FencingPolicy;
+import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.pm.FenceActionType;
+import org.ovirt.engine.core.common.businessentities.pm.FenceOperationResult;
+import org.ovirt.engine.core.common.businessentities.pm.FenceOperationResult.Status;
+import org.ovirt.engine.core.common.businessentities.pm.PowerStatus;
+import org.ovirt.engine.core.compat.Guid;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HostFenceActionExecutorTest {
+    @Mock
+    VDS fencedHost;
+
+    @Mock
+    SingleAgentFenceActionExecutor agentExecutor1;
+
+    @Mock
+    SingleAgentFenceActionExecutor agentExecutor2;
+
+    HostFenceActionExecutor executor;
+
+    List<FenceAgent> fenceAgents;
+
+    @Before
+    public void setup() {
+        doReturn("host1").when(fencedHost).getHostName();
+
+        executor = spy(new HostFenceActionExecutor(fencedHost, new FencingPolicy()));
+        doReturn(agentExecutor1)
+                .doReturn(agentExecutor2)
+                .when(executor).createFenceActionExecutor(any(List.class));
+    }
+
+    /**
+     * Test successful fence action when the 1st of 2 sequential fence agents returns success
+     */
+    @Test
+    public void successfulFenceWith1stSuccess() {
+        mockFenceAgents();
+
+        // result of fence action invoked on 1st sequential agent
+        mockFenceResult(
+                agentExecutor1,
+                new FenceOperationResult(Status.SUCCESS, PowerStatus.ON));
+
+        // result of fence action invoked on 2nd sequential agent - set it to error, so we verify that it's not used
+        mockFenceResult(
+                agentExecutor2,
+                new FenceOperationResult(Status.ERROR, PowerStatus.UNKNOWN));
+
+        FenceOperationResult result = executor.fence(FenceActionType.STATUS);
+
+        assertEquals(Status.SUCCESS, result.getStatus());
+        assertEquals(PowerStatus.ON, result.getPowerStatus());
+    }
+
+    /**
+     * Test successful fence action when the 1st of 2 sequential fence agents returns error and 2nd one return success
+     */
+    @Test
+    public void successfulFenceWith1stError2ndSuccess() {
+        mockFenceAgents();
+
+        // result of fence action invoked on 1st sequential agent
+        mockFenceResult(
+                agentExecutor1,
+                new FenceOperationResult(Status.ERROR, PowerStatus.UNKNOWN));
+
+        // result of fence action invoked on 2nd sequential agent
+        mockFenceResult(
+                agentExecutor2,
+                new FenceOperationResult(Status.SUCCESS, PowerStatus.ON));
+
+        FenceOperationResult result = executor.fence(FenceActionType.STATUS);
+
+        assertEquals(Status.SUCCESS, result.getStatus());
+        assertEquals(PowerStatus.ON, result.getPowerStatus());
+    }
+
+    /**
+     * Test failed fence action when both of 2 sequential fence agents return error
+     */
+    @Test
+    public void failedFenceWithAllError() {
+        mockFenceAgents();
+
+        // result of fence action invoked on 1st sequential agent
+        mockFenceResult(
+                agentExecutor1,
+                new FenceOperationResult(Status.ERROR, PowerStatus.UNKNOWN));
+
+        // result of fence action invoked on 2nd sequential agent
+        mockFenceResult(
+                agentExecutor2,
+                new FenceOperationResult(Status.ERROR, PowerStatus.UNKNOWN));
+
+        FenceOperationResult result = executor.fence(FenceActionType.STATUS);
+
+        assertEquals(Status.ERROR, result.getStatus());
+        assertEquals(PowerStatus.UNKNOWN, result.getPowerStatus());
+    }
+
+    /**
+     * Test successful status action using specified fence agent
+     */
+    @Test
+    public void successfulStatusWithSpecifiedFenceAgent() {
+        // result of fence action invoked on specified agent
+        mockFenceResult(
+                agentExecutor1,
+                new FenceOperationResult(Status.SUCCESS, PowerStatus.ON));
+
+        FenceOperationResult result = executor.getFenceAgentStatus(createFenceAgent(1));
+
+        assertEquals(Status.SUCCESS, result.getStatus());
+        assertEquals(PowerStatus.ON, result.getPowerStatus());
+    }
+
+    /**
+     * Test failed status action using specified fence agent
+     */
+    @Test
+    public void failedStatusWithSpecifiedFenceAgent() {
+        // result of fence action invoked on specified agent
+        mockFenceResult(
+                agentExecutor1,
+                new FenceOperationResult(Status.ERROR, PowerStatus.UNKNOWN));
+
+        FenceOperationResult result = executor.getFenceAgentStatus(createFenceAgent(1));
+
+        assertEquals(Status.ERROR, result.getStatus());
+        assertEquals(PowerStatus.UNKNOWN, result.getPowerStatus());
+    }
+
+    /**
+     * Test that host is powered off when status action returns power off
+     */
+    @Test
+    public void hostIsPoweredOffWhenFenceStatusReturnsOff() {
+        mockFenceAgent();
+
+        // result of fence action invoked on specified agent
+        mockFenceResult(
+                agentExecutor1,
+                new FenceOperationResult(Status.SUCCESS, PowerStatus.OFF));
+
+        assertTrue(executor.isHostPoweredOff());
+    }
+
+    /**
+     * Test that host is not powered off when status action returns power on
+     */
+    @Test
+    public void hostIsNotPoweredOffWhenFenceStatusReturnsOn() {
+        mockFenceAgent();
+
+        // result of fence action invoked on specified agent
+        mockFenceResult(
+                agentExecutor1,
+                new FenceOperationResult(Status.SUCCESS, PowerStatus.ON));
+
+        assertFalse(executor.isHostPoweredOff());
+    }
+
+
+    /**
+     * Test that host is not powered off when status action returns error
+     */
+    @Test
+    public void hostIsNotPoweredOffWhenFenceStatusFailed() {
+        mockFenceAgent();
+
+        // result of fence action invoked on specified agent
+        mockFenceResult(
+                agentExecutor1,
+                new FenceOperationResult(Status.ERROR, PowerStatus.UNKNOWN));
+
+        assertFalse(executor.isHostPoweredOff());
+    }
+
+    /**
+     * Tests {@code SingleAgentFenceActionExecutor} creation for single fence agent
+     */
+    @Test
+    public void testSingleAgentFenceActionExecutorUsage() {
+        HostFenceActionExecutor executor = new HostFenceActionExecutor(fencedHost, new FencingPolicy());
+
+        assertTrue(
+                executor.createFenceActionExecutor(createSingleAgentList(1))
+                        instanceof SingleAgentFenceActionExecutor);
+    }
+
+    /**
+     * Tests {@code ConcurrentAgentsFenceActionExecutor} creation for concurrent fence agents
+     */
+    @Test
+    public void testConcurrentAgentsFenceActionExecutorUsage() {
+        HostFenceActionExecutor executor = new HostFenceActionExecutor(fencedHost, new FencingPolicy());
+
+        assertTrue(
+                executor.createFenceActionExecutor(createConcurrentAgentsList(2, 1))
+                        instanceof ConcurrentAgentsFenceActionExecutor);
+    }
+
+    protected FenceAgent createFenceAgent(int order) {
+        FenceAgent agent = new FenceAgent();
+        agent.setId(Guid.newGuid());
+        agent.setOrder(order);
+        return agent;
+    }
+
+    protected List<FenceAgent> createSingleAgentList(int order) {
+        List<FenceAgent> fenceAgents = new ArrayList<>();
+        fenceAgents.add(createFenceAgent(order));
+        return fenceAgents;
+    }
+
+    protected List<FenceAgent> createConcurrentAgentsList(int count, int order) {
+        List<FenceAgent> fenceAgents = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            fenceAgents.add(createFenceAgent(order));
+        }
+        return fenceAgents;
+    }
+
+    protected List<FenceAgent> create2SequentialFenceAgents() {
+        List<FenceAgent> fenceAgents = createSingleAgentList(1);
+        fenceAgents.addAll(createSingleAgentList(2));
+        return fenceAgents;
+    }
+
+    protected void mockFenceResult(FenceActionExecutor executor, FenceOperationResult result) {
+        doReturn(result).when(executor).fence(any(FenceActionType.class));
+    }
+
+    protected void mockFenceAgents() {
+        fenceAgents = create2SequentialFenceAgents();
+        doReturn(fenceAgents).when(fencedHost).getFenceAgents();
+    }
+
+    protected void mockFenceAgent() {
+        fenceAgents = createSingleAgentList(1);
+        doReturn(fenceAgents).when(fencedHost).getFenceAgents();
+    }
+}
