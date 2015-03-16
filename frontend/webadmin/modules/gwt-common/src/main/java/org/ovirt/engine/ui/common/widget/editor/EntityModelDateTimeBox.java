oVirt/ovirt-engine

commit 5b50b18cdd497a2f472cd950581a981759cde385

webadmin : gwt-bootstrap DateTimeBox widget
This patch adds gwt-bootstrap DateTimeBox widget.

Change-Id: I92f2a9eced817d8ed040901255983e5673990f1f
Signed-off-by: Anmol Babu <anbabu@redhat.com>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/EntityModelDateTimeBox.java
@@ -1,0 +1,75 @@
+package org.ovirt.engine.ui.common.widget.editor;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import com.google.gwt.editor.client.adapters.TakesValueEditor;
+import com.google.gwt.user.client.ui.HasConstrainedValue;
+
+/**
+ * EntityModel bound Gwt-Bootstrap DateTimePicker that uses {@link GwtBootstrapDateTimePicker}.
+ */
+public class EntityModelDateTimeBox extends GwtBootstrapDateTimePicker implements EditorWidget<Date, TakesValueEditor<Date>>, HasConstrainedValue<Date> {
+
+    private TakesConstrainedValueEditor<Date> editor;
+
+    private int tabIndex;
+
+    private boolean enabled;
+
+    private char accessKey;
+
+    public EntityModelDateTimeBox(String format, boolean autoClose) {
+        super(format, autoClose);
+    }
+
+    public EntityModelDateTimeBox() {
+        super();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public int getTabIndex() {
+        return tabIndex;
+    }
+
+    @Override
+    public void setAccessKey(char key) {
+        accessKey = key;
+    }
+
+    @Override
+    public void setTabIndex(int index) {
+        this.tabIndex = index;
+    }
+
+    public char getAccessKey() {
+        return accessKey;
+    }
+
+    @Override
+    public TakesValueEditor<Date> asEditor() {
+        if (editor == null) {
+            editor = TakesConstrainedValueEditor.of(this, this, this);
+        }
+        return editor;
+    }
+
+    @Override
+    public void setAcceptableValues(Collection<Date> values) {
+        List<Date> allowedDates = (List<Date>) values;
+        Collections.sort(allowedDates);
+        setDateRange(allowedDates.get(0), allowedDates.get(allowedDates.size() - 1));
+    }
+}

frontend/webadmin/modules/gwt-common/pom.xml
@@ -59,6 +59,12 @@
       <version>${gwtbootstrap3.version}</version>
       <scope>provided</scope>
     </dependency>
+    <dependency>
+      <groupId>org.gwtbootstrap3</groupId>
+      <artifactId>gwtbootstrap3-extras</artifactId>
+      <version>${gwtbootstrap3.version}</version>
+      <scope>provided</scope>
+    </dependency>
     <!-- oVirt dependencies -->
     <dependency>
       <groupId>${engine.groupId}</groupId>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/GwtCommon.gwt.xml
@@ -8,6 +8,7 @@
     <inherits name="com.google.gwt.inject.Inject" />
 
     <inherits name="org.gwtbootstrap3.GwtBootstrap3CDN"/>
+    <inherits name="org.gwtbootstrap3.extras.datetimepicker.DateTimePicker"/>
     <inherits name="com.google.gwt.uibinder.GinUiBinder" />
 
     <!-- Inherit GWTP MVP module with automatic EntryPoint support -->

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/EntityModelDateTimeBoxEditor.java
@@ -1,0 +1,48 @@
+package org.ovirt.engine.ui.common.widget.editor;
+
+import java.util.Date;
+import java.util.List;
+
+import org.ovirt.engine.ui.common.widget.AbstractValidatedWidgetWithLabel;
+
+import com.google.gwt.dom.client.Style.BorderStyle;
+import com.google.gwt.editor.client.IsEditor;
+
+/**
+ * EntityModel bound editor that uses {@link EntityModelDateTimeBox}.
+ */
+public class EntityModelDateTimeBoxEditor extends AbstractValidatedWidgetWithLabel<Date, EntityModelDateTimeBox> implements IsEditor<WidgetWithLabelEditor<Date, EntityModelDateTimeBoxEditor>> {
+
+    private final WidgetWithLabelEditor<Date, EntityModelDateTimeBoxEditor> editor;
+
+    public EntityModelDateTimeBoxEditor() {
+        this(null, true);
+    }
+
+    public EntityModelDateTimeBoxEditor(String format, boolean autoClose) {
+        super(new EntityModelDateTimeBox(format == null ? GwtBootstrapDateTimePicker.DEFAULT_DATE_TIME_FORMAT : format, autoClose));
+        this.editor = WidgetWithLabelEditor.of(getContentWidget().asEditor(), this);
+    }
+
+    @Override
+    public EntityModelDateTimeBox getContentWidget() {
+        return super.getContentWidget();
+    }
+
+    @Override
+    public WidgetWithLabelEditor<Date, EntityModelDateTimeBoxEditor> asEditor() {
+        return editor;
+    }
+
+    @Override
+    public void markAsValid() {
+        super.markAsValid();
+        getValidatedWidgetStyle().setBorderStyle(BorderStyle.NONE);
+    }
+
+    @Override
+    public void markAsInvalid(List<String> validationHints) {
+        super.markAsInvalid(validationHints);
+    }
+
+}

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/GwtBootstrapDateTimePicker.java
@@ -1,0 +1,122 @@
+package org.ovirt.engine.ui.common.widget.editor;
+
+import java.util.Date;
+
+import org.gwtbootstrap3.extras.datetimepicker.client.ui.DateTimePicker;
+import org.gwtbootstrap3.extras.datetimepicker.client.ui.base.constants.DateTimePickerView;
+import org.gwtbootstrap3.extras.datetimepicker.client.ui.base.events.ChangeDateEvent;
+import org.gwtbootstrap3.extras.datetimepicker.client.ui.base.events.ChangeDateHandler;
+
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.TakesValue;
+import com.google.gwt.user.client.ui.Widget;
+
+public class GwtBootstrapDateTimePicker implements TakesValue<Date> {
+    public static final String DEFAULT_DATE_TIME_FORMAT = "mm/dd/yyyy hh:ii";//$NON-NLS-1$
+    public static final String DEFAULT_TIME_FORMAT = "hh:ii P";//$NON-NLS-1$
+    public static final String DEFAULT_DATE_FORMAT = "mm/dd/yyyy";//$NON-NLS-1$
+    private final DateTimePicker dateTimePicker = new DateTimePicker();
+    private String dateTimeFormat;
+
+    public GwtBootstrapDateTimePicker() {
+        this(DEFAULT_DATE_TIME_FORMAT, true);
+    }
+
+    public GwtBootstrapDateTimePicker(String format, boolean autoClose) {
+        dateTimePicker.setFormat(format);
+        dateTimePicker.setAutoClose(autoClose);
+        dateTimePicker.setShowTodayButton(true);
+        this.dateTimeFormat = format;
+        this.dateTimePicker.addChangeDateHandler(new ChangeDateHandler() {
+            @Override
+            public void onChangeDate(ChangeDateEvent evt) {
+                ValueChangeEvent.fire(dateTimePicker, dateTimePicker.getValue());
+            }
+        });
+    }
+
+    public String getDateTimeFormat() {
+        return dateTimeFormat;
+    }
+
+    public void setDateTimeFormat(String dateTimeFormat) {
+        this.dateTimeFormat = dateTimeFormat;
+        dateTimePicker.setFormat(dateTimeFormat);
+        dateTimePicker.reload();
+    }
+
+    public HandlerRegistration addValueChangeHandler(ValueChangeHandler<Date> handler) {
+        return dateTimePicker.addValueChangeHandler(handler);
+    }
+
+    public void setValue(Date value, boolean fireEvents) {
+        dateTimePicker.setValue(value, fireEvents);
+    }
+
+    public void showDateOnly() {
+        show(DateTimePickerView.MONTH, DateTimePickerView.MONTH, DateTimePickerView.MONTH);
+    }
+
+    public void showTimeOnly() {
+        show(DateTimePickerView.DAY, DateTimePickerView.HOUR, DateTimePickerView.DAY);
+    }
+
+    public void showDateAndTime() {
+        show(DateTimePickerView.MONTH, DateTimePickerView.HOUR, DateTimePickerView.MONTH);
+    }
+
+    public void show(DateTimePickerView startView, DateTimePickerView minView, DateTimePickerView maxView) {
+        dateTimePicker.setStartView(maxView);
+        dateTimePicker.setMinView(minView);
+        dateTimePicker.setMaxView(maxView);
+        dateTimePicker.reload();
+    }
+
+    @Override
+    public void setValue(Date value) {
+        dateTimePicker.setValue(value);
+    }
+
+    public Widget asWidget() {
+        return dateTimePicker.asWidget();
+    }
+
+    public HandlerRegistration addKeyUpHandler(KeyUpHandler handler) {
+        return dateTimePicker.addDomHandler(handler, KeyUpEvent.getType());
+    }
+
+    public HandlerRegistration addKeyDownHandler(KeyDownHandler handler) {
+        return dateTimePicker.addDomHandler(handler, KeyDownEvent.getType());
+    }
+
+    public HandlerRegistration addKeyPressHandler(KeyPressHandler handler) {
+        return dateTimePicker.addDomHandler(handler, KeyPressEvent.getType());
+    }
+
+    public void fireEvent(GwtEvent<?> event) {
+        dateTimePicker.fireEvent(event);
+    }
+
+    @Override
+    public Date getValue() {
+        return dateTimePicker.getValue();
+    }
+
+    public void setFocus(boolean focused) {
+        dateTimePicker.getTextBox().setFocus(focused);
+    }
+
+    public void setDateRange(Date startDate, Date stopDate) {
+        dateTimePicker.setStartDate(startDate);
+        dateTimePicker.setEndDate(stopDate);
+    }
+}

frontend/webadmin/modules/userportal-gwtp/pom.xml
@@ -64,6 +64,12 @@
       <version>${gwtbootstrap3.version}</version>
       <scope>provided</scope>
     </dependency>
+    <dependency>
+      <groupId>org.gwtbootstrap3</groupId>
+      <artifactId>gwtbootstrap3-extras</artifactId>
+      <version>${gwtbootstrap3.version}</version>
+      <scope>provided</scope>
+    </dependency>
     <!-- oVirt dependencies -->
     <dependency>
       <groupId>${engine.groupId}</groupId>

frontend/webadmin/modules/webadmin/pom.xml
@@ -64,6 +64,12 @@
       <version>${gwtbootstrap3.version}</version>
       <scope>provided</scope>
     </dependency>
+    <dependency>
+      <groupId>org.gwtbootstrap3</groupId>
+      <artifactId>gwtbootstrap3-extras</artifactId>
+      <version>${gwtbootstrap3.version}</version>
+      <scope>provided</scope>
+    </dependency>
     <!-- oVirt dependencies -->
     <dependency>
       <groupId>${engine.groupId}</groupId>

commit 642c1c06e4357e5d98d4c8621fc172d0c3168d5c

userportal, webadmin: fix findbugs questionable cast
Commit 5b50b18 introduced a findbugs error for 'Questionable cast'.
Attempting to fix it.

Change-Id: Ie55668532f99ee87727dfcadeed94a80a22787ff
Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/EntityModelDateTimeBox.java
@@ -68,8 +68,10 @@ public class EntityModelDateTimeBox extends GwtBootstrapDateTimePicker implement
 
     @Override
     public void setAcceptableValues(Collection<Date> values) {
-        List<Date> allowedDates = (List<Date>) values;
-        Collections.sort(allowedDates);
-        setDateRange(allowedDates.get(0), allowedDates.get(allowedDates.size() - 1));
+        if (values instanceof List<?>) {
+            List<Date> allowedDates = (List<Date>) values;
+            Collections.sort(allowedDates);
+            setDateRange(allowedDates.get(0), allowedDates.get(allowedDates.size() - 1));
+        }
     }
 }