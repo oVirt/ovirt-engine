67a88015c6304a1ff38e72ade450518a01fefe32
Greg Sheremeta <gshereme@redhat.com>
2015-03-16 20:58:50 -0400

    webadmin: display contents of comments instead of icon
    Changed all of the comment columns in grids to display the comment
    contents instead of the icon.
    
    Change-Id: If3fc02811992fefe1723e9cd273e057e3639e310
    Bug-Url: https://bugzilla.redhat.com/1122990
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/table/column/CommentColumn.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/table/column/CommentColumn.java
@@ -1,54 +1,26 @@
 package org.ovirt.engine.ui.webadmin.widget.table.column;
 
 import org.ovirt.engine.core.common.businessentities.Commented;
-import org.ovirt.engine.ui.common.widget.table.column.AbstractImageResourceColumn;
-import org.ovirt.engine.ui.webadmin.ApplicationResources;
-import org.ovirt.engine.ui.webadmin.gin.AssetProvider;
-
-import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import org.ovirt.engine.ui.common.widget.table.column.AbstractTextColumn;
 
 /**
- * Column that render a comment image (yellow paper icon) that, when hovered, shows the
- * actual comment in a tooltip.
+ * Column that renders getComment() of a Commented.
  *
- * @param <T> row type
+ * @param <T> row type, must implement Commented
  */
-public class CommentColumn<T extends Commented> extends AbstractImageResourceColumn<T> {
-
-    private final static ApplicationResources resources = AssetProvider.getResources();
+public class CommentColumn<T extends Commented> extends AbstractTextColumn<T> {
 
     /**
-     * Using some row value of type T, build an ImageResource to render in this column.
+     * Using some row value of type T extends Commented, simply get the comment.
      *
      * @see com.google.gwt.user.cellview.client.Column#getValue(java.lang.Object)
      */
     @Override
-    public ImageResource getValue(T value) {
+    public String getValue(T value) {
         if (value != null && value.getComment() != null && !value.getComment().isEmpty()) {
-            return resources.commentImage();
+            return value.getComment();
         }
         return null;
     }
 
-    /**
-     * Use the comment icon.
-     */
-    public ImageResource getDefaultImage() {
-        return resources.commentImage();
-    }
-
-    /**
-     * Using some row value of type T, build a SafeHtml tooltip to render when this column is moused over.
-     *
-     * @see org.ovirt.engine.ui.common.widget.table.column.AbstractColumn#getTooltip(java.lang.Object)
-     */
-    @Override
-    public SafeHtml getTooltip(T value) {
-        if (value == null || value.getComment() == null) {
-            return null;
-        }
-        return SafeHtmlUtils.fromString(value.getComment());
-    }
 }
