7a3633a0c31ffe5d0945fe356dcda45267a6a8cd
Daniel Erez <derez@redhat.com>
2015-03-18 19:42:22 +0200

    core: ProviderType - adding OPENSTACK_VOLUME
    Adding OPENSTACK_VOLUME to ProviderType enum.
    
    Change-Id: If9424947dbb072565b61cbf820dad85f3b17ba93
    Bug-Url: https://bugzilla.redhat.com/1185826
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/ProviderType.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/ProviderType.java
@@ -11,7 +11,8 @@ import org.ovirt.engine.core.common.VdcObjectType;
 public enum ProviderType implements Identifiable {
     OPENSTACK_NETWORK(0, VdcObjectType.Network),
     FOREMAN(1, VdcObjectType.VDS),
-    OPENSTACK_IMAGE(2, VdcObjectType.Storage);
+    OPENSTACK_IMAGE(2, VdcObjectType.Storage),
+    OPENSTACK_VOLUME(3, VdcObjectType.Storage);
 
     private int value;
     private Set<VdcObjectType> providedTypes;
