f9dec248209ce59918cad594e3231e1acccd197e
Daniel Erez <derez@redhat.com>
2015-03-22 15:39:37 +0200

    core: add CinderVolumeStatus enum
    Added CinderVolumeStatus enum that corresponds with
    Cinder volume statuses.
    
    Change-Id: I86f663a8ded9bb64c1f7e6aa852541fe353112d8
    Bug-Url: https://bugzilla.redhat.com/1185826
    Signed-off-by: Daniel Erez <derez@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/storage/CinderVolumeStatus.java
@@ -0,0 +1,43 @@
+package org.ovirt.engine.core.common.businessentities.storage;
+
+import org.ovirt.engine.core.common.businessentities.Nameable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum CinderVolumeStatus implements Nameable {
+    Creating("creating"),
+    Available("available"),
+    Attaching("attaching"),
+    InUse("in-use"),
+    Deleting("deleting"),
+    Error("error"),
+    ErrorDeleting("error_deleting"),
+    BackingUp("backing-up"),
+    RestoringBackup("restoring-backup"),
+    ErrorRestoring("error_restoring"),
+    Extending("extending"),
+    ErrorExtending("error_extending");
+
+    private String name;
+    private static Map<String, CinderVolumeStatus> mappings;
+
+    static {
+        mappings = new HashMap<String, CinderVolumeStatus>();
+        for (CinderVolumeStatus status : values()) {
+            mappings.put(status.getName(), status);
+        }
+    }
+
+    private CinderVolumeStatus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static CinderVolumeStatus forValue(String name) {
+        return mappings.get(name);
+    }
+}
