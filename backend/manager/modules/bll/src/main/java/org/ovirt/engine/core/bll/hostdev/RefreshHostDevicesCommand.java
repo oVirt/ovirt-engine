210c209c7a247e989d80d7e70c08482969e2ecde
Alona Kaplan <alkaplan@redhat.com>
2015-03-25 13:41:26 +0200

    engine: create/update host_nic_vfsConfig according to HostDevListByCaps
    Change-Id: I60ab755a75d93d4bafa5be984b267104f4636197
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/hostdev/RefreshHostDevicesCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/hostdev/RefreshHostDevicesCommand.java
@@ -1,24 +1,31 @@
 package org.ovirt.engine.core.bll.hostdev;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
 import org.ovirt.engine.core.bll.RefreshHostInfoCommandBase;
 import org.ovirt.engine.core.bll.context.CommandContext;
+import org.ovirt.engine.core.bll.network.host.HostNicVfsConfigHelper;
 import org.ovirt.engine.core.common.action.VdsActionParameters;
 import org.ovirt.engine.core.common.businessentities.HostDevice;
+import org.ovirt.engine.core.common.businessentities.network.HostNicVfsConfig;
+import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
 import org.ovirt.engine.core.common.errors.VdcBllMessages;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
 import org.ovirt.engine.core.common.vdscommands.VdsIdAndVdsVDSCommandParametersBase;
+import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.network.HostNicVfsConfigDao;
 import org.ovirt.engine.core.utils.transaction.TransactionMethod;
 import org.ovirt.engine.core.utils.transaction.TransactionSupport;
 import org.ovirt.engine.core.vdsbroker.ResourceManager;
-
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @NonTransactiveCommandAttribute
 public class RefreshHostDevicesCommand<T extends VdsActionParameters> extends RefreshHostInfoCommandBase<T> {
@@ -28,6 +35,12 @@ public class RefreshHostDevicesCommand<T extends VdsActionParameters> extends Re
 
     @Inject
     private DbFacade dbFacade;
+
+    @Inject
+    private HostNicVfsConfigDao hostNicVfsConfigDao;
+
+    @Inject
+    private HostNicVfsConfigHelper hostNicVfsConfigHelper;
 
     public RefreshHostDevicesCommand(T parameters) {
         super(parameters);
@@ -49,7 +62,7 @@ public class RefreshHostDevicesCommand<T extends VdsActionParameters> extends Re
         List<HostDevice> oldDevices = dbFacade.getHostDeviceDao().getHostDevicesByHostId(getVdsId());
 
         Map<String, HostDevice> fetchedMap = groupDevicesByName(fetchedDevices);
-        Map<String, HostDevice> oldMap = groupDevicesByName(oldDevices);
+        final Map<String, HostDevice> oldMap = groupDevicesByName(oldDevices);
 
         final List<HostDevice> newDevices = new ArrayList<>();
         final List<HostDevice> changedDevices = new ArrayList<>();
@@ -77,6 +90,9 @@ public class RefreshHostDevicesCommand<T extends VdsActionParameters> extends Re
             public Void runInTransaction() {
                 dbFacade.getHostDeviceDao().saveAllInBatch(newDevices);
                 dbFacade.getHostDeviceDao().updateAllInBatch(changedDevices);
+
+                handleHostNicVfsConfigUpdate(oldMap, newDevices, changedDevices, removedDevices);
+
                 dbFacade.getHostDeviceDao().removeAllInBatch(removedDevices);
 
                 return null;
@@ -84,6 +100,78 @@ public class RefreshHostDevicesCommand<T extends VdsActionParameters> extends Re
         });
 
         setSucceeded(true);
+    }
+
+    private void handleHostNicVfsConfigUpdate(final Map<String, HostDevice> oldMap,
+            final List<HostDevice> newDevices,
+            final List<HostDevice> changedDevices,
+            final List<HostDevice> removedDevices) {
+        final List<HostNicVfsConfig> newHostNicVfsConfigs = new ArrayList<>();
+        final List<HostNicVfsConfig> removedHostNicVfsConfigs = new ArrayList<>();
+
+        for (HostDevice device : newDevices) {
+            if (hostNicVfsConfigHelper.isSriovDevice(device)) {
+                addToListIfNotNull(getHostNicVfsConfigToAdd(device), newHostNicVfsConfigs);
+            }
+        }
+
+        for (HostDevice device : changedDevices) {
+            HostDevice oldDevice = oldMap.get(device.getDeviceName());
+
+            if (!hostNicVfsConfigHelper.isSriovDevice(oldDevice)
+                    && hostNicVfsConfigHelper.isSriovDevice(device)) {
+                addToListIfNotNull(getHostNicVfsConfigToAdd(device), newHostNicVfsConfigs);
+            }
+
+            if (hostNicVfsConfigHelper.isSriovDevice(oldDevice)
+                    && !hostNicVfsConfigHelper.isSriovDevice(device)) {
+                addToListIfNotNull(getHostNicVfsConfigToRemove(device), removedHostNicVfsConfigs);
+            }
+        }
+
+        for (HostDevice device : removedDevices) {
+            if (hostNicVfsConfigHelper.isSriovDevice(device)) {
+                addToListIfNotNull(getHostNicVfsConfigToRemove(device), removedHostNicVfsConfigs);
+            }
+        }
+
+        if (!newHostNicVfsConfigs.isEmpty()) {
+            hostNicVfsConfigDao.saveAllInBatch(newHostNicVfsConfigs);
+        }
+
+        if (!removedHostNicVfsConfigs.isEmpty()) {
+            hostNicVfsConfigDao.removeAllInBatch(removedHostNicVfsConfigs);
+        }
+    }
+
+    private <E> void addToListIfNotNull(E element, Collection<E> collection) {
+        if (element != null) {
+            collection.add(element);
+        }
+    }
+
+    private HostNicVfsConfig getHostNicVfsConfigToAdd(HostDevice device) {
+        VdsNetworkInterface nic = hostNicVfsConfigHelper.getNicByPciDevice(device);
+
+        if (nic == null) {
+            return null;
+        }
+
+        HostNicVfsConfig hostNicVfsConfig = new HostNicVfsConfig();
+        hostNicVfsConfig.setId(Guid.newGuid());
+        hostNicVfsConfig.setNicId(nic.getId());
+        hostNicVfsConfig.setAllNetworksAllowed(true);
+
+        return hostNicVfsConfig;
+    }
+
+    private HostNicVfsConfig getHostNicVfsConfigToRemove(HostDevice device) {
+        VdsNetworkInterface nic = hostNicVfsConfigHelper.getNicByPciDevice(device);
+        if (nic == null) {
+            return null;
+        }
+
+        return hostNicVfsConfigDao.getByNicId(nic.getId());
     }
 
     @Override
