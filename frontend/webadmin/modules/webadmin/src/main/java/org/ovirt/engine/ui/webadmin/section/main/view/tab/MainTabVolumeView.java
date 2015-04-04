38b70997d110497693be63af94363cda2583b73f
Anmol Babu <anbabu@redhat.com>
2015-04-04 19:24:59 +0530

    webadmin : Fix Volumes tab space used column
    Fix Volumes tab space used column
    
    Change-Id: I48d0961cf0bf3d697069ac7902a3b39ee07e1335
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1140132
    Signed-off-by: Anmol Babu <anbabu@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/MainTabVolumeView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/MainTabVolumeView.java
@@ -141,7 +141,7 @@ public class MainTabVolumeView extends AbstractMainTabWithDetailsTableView<Glust
             public GlusterVolumeEntity getValue(GlusterVolumeEntity object) {
                 return object;
             }
-        }, constants.volumeCapacity(), "60px");//$NON-NLS-1$
+        }, constants.volumeCapacity(), "100px");//$NON-NLS-1$
 
         getTable().addColumn(new VolumeActivityColumn<GlusterVolumeEntity>(new VolumeActivityCompositeCell<GlusterTaskSupport>(compositeList) {
             @Override
