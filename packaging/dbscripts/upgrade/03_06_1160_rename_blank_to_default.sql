4db68e865e22c332f1e53ea6d5851d3428d1866b
Tomas Jelinek <tjelinek@redhat.com>
2015-04-09 14:52:12 +0200

    core: removed the content of the script changing blank template name
    Renaming the Blank to Default caused issues with backward compatibility of the
    REST API.
    
    Fixed by removing the content of the script so it will have no effect - did not
    delete it because there are newer scripts after this merged and it would create
    inconsistencies in numbering.
    
    Change-Id: I90b88dbdc314efb3db68498db3632c46058a3585
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

--- packaging/dbscripts/upgrade/03_06_1160_rename_blank_to_default.sql
+++ packaging/dbscripts/upgrade/03_06_1160_rename_blank_to_default.sql
@@ -1,3 +1,1 @@
--- null after the decouple_blank_from_cluster ran, 00000000-0000-0000-0000-000000000000 before.
--- this way it is not dependent on the order
-update vm_static set vm_name = 'Default', description = 'Default template' where vm_guid='00000000-0000-0000-0000-000000000000' or vm_guid is null;
+-- empty script - the content had to be removed because it broke backward compatibility
