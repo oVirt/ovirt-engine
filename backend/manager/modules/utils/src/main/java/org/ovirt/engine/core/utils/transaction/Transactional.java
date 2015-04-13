3059215c84ace5dec2cfb9500d91dc7e99af0d4e
lzelkha@redhat.com <lzelkha@redhat.com>
2015-04-13 10:59:01 +0300

    core: CDI Transaction Interceptor
    Allow developers to mark CDI beans and their methods as
    transactional, and make sure that methods will run
    in a transactional context
    
    Change-Id: I94bf3b41764b137e5456995d393f023d3564e301
    Bug-Url: https://bugzilla.redhat.com/1159621
    Signed-off-by: lzelkha@redhat.com <lzelkha@redhat.com>

---
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/transaction/Transactional.java
@@ -0,0 +1,25 @@
+package org.ovirt.engine.core.utils.transaction;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.enterprise.util.Nonbinding;
+import javax.interceptor.InterceptorBinding;
+
+import org.ovirt.engine.core.compat.TransactionScopeOption;
+
+/**
+ * Use this annotation on a class to mark that all of its methods require running in a transactional scope. Use it on a
+ * method to make sure that only this specific method runs in a transactional scope. Default transaction scope if
+ * Required
+ */
+@Inherited
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@InterceptorBinding
+public @interface Transactional {
+    @Nonbinding TransactionScopeOption propogation() default TransactionScopeOption.Required;
+}
