b218d989d795679b23a620710227451d2a292707
Maor Lipchuk <mlipchuk@redhat.com>
2015-05-03 21:37:38 +0300

    core: Refactor register of OVF_STORE disks
    Refactor the OVF_STORE disks' registration on attach Storage Domain
    process, by moving the code to StorageHandlingCommandBase,
    so it can be used also for initializing a new Data Center.
    
    Change-Id: I0d24f059c0073d8744b46b0d0697e323f63ce119
    Bug-Url: https://bugzilla.redhat.com/1217339
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/storage/AttachStorageDomainToPoolCommandTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/storage/AttachStorageDomainToPoolCommandTest.java
@@ -82,8 +82,10 @@ public class AttachStorageDomainToPoolCommandTest {
 
     @Test
     public void statusSetInMap() {
+        Guid storageDomainId = Guid.newGuid();
+        Guid poolId = Guid.newGuid();
         AttachStorageDomainToPoolParameters params =
-                new AttachStorageDomainToPoolParameters(Guid.newGuid(), Guid.newGuid());
+                new AttachStorageDomainToPoolParameters(storageDomainId, poolId);
         AttachStorageDomainToPoolCommand<AttachStorageDomainToPoolParameters> cmd =
                 spy(new AttachStorageDomainToPoolCommand<AttachStorageDomainToPoolParameters>(params));
 
@@ -96,12 +98,13 @@ public class AttachStorageDomainToPoolCommandTest {
         when(dbFacade.getStorageDomainDao()).thenReturn(storageDomainDAO);
         when(dbFacade.getStorageDomainStaticDao()).thenReturn(storageDomainStaticDAO);
         StoragePool pool = new StoragePool();
+        pool.setId(poolId);
         pool.setStatus(StoragePoolStatus.Up);
         when(storagePoolDAO.get(any(Guid.class))).thenReturn(pool);
         when(isoMapDAO.get(any(StoragePoolIsoMapId.class))).thenReturn(map);
         when(storageDomainDAO.getForStoragePool(any(Guid.class), any(Guid.class))).thenReturn(new StorageDomain());
         when(storageDomainStaticDAO.get(any(Guid.class))).thenReturn(new StorageDomainStatic());
-
+        doReturn(pool.getId()).when(cmd).getStoragePoolIdFromVds();
         doReturn(backendInternal).when(cmd).getBackend();
         when(backendInternal.getResourceManager()).thenReturn(vdsBrokerFrontend);
         VdcReturnValueBase vdcReturnValue = new VdcReturnValueBase();
@@ -117,6 +120,8 @@ public class AttachStorageDomainToPoolCommandTest {
         when(vdsDAO.get(any(Guid.class))).thenReturn(vds);
         doReturn(getUnregisteredList()).when(cmd).getEntitiesFromStorageOvfDisk();
         doReturn(Collections.<DiskImage>emptyList()).when(cmd).getAllOVFDisks();
+        doReturn(getUnregisteredList()).when(cmd).getEntitiesFromStorageOvfDisk(storageDomainId, pool.getId());
+        doReturn(Collections.<DiskImage>emptyList()).when(cmd).getAllOVFDisks(storageDomainId, pool.getId());
         doAnswer(new Answer<Object>() {
             @Override
             public Object answer(InvocationOnMock invocation) throws Throwable {
