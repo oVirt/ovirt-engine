329f509a4b18f5aed59f02d33a6b3f0fa67f5e4c
emesika <emesika@redhat.com>
2015-05-03 17:57:37 +0300

    core[Enum]: Adding external status enum
    This patch adds a new enum: ExternalStatus
    
    Change-Id: Ia46af5206938025ad3c487cc1998b32301783cbf
    Signed-off-by: emesika <emesika@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/ExternalStatus.java
@@ -0,0 +1,34 @@
+package org.ovirt.engine.core.common.businessentities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum ExternalStatus implements Identifiable {
+    Ok(0),
+    Info(1),
+    Warning(2),
+    Error(3),
+    Failure(4);
+
+    private static final Map<Integer, ExternalStatus> mappings = new HashMap<Integer, ExternalStatus>();
+    private int id;
+
+    static {
+        for (ExternalStatus status : values()) {
+            mappings.put(status.getValue(), status);
+        }
+    }
+
+    private ExternalStatus(int value) {
+        id = value;
+    }
+
+    @Override
+    public int getValue() {
+        return id;
+    }
+
+    public static ExternalStatus forValue(int value) {
+        return mappings.get(value);
+    }
+}
