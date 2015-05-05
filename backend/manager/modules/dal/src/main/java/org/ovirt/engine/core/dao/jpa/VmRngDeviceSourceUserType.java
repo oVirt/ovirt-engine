37ec0a768565f4bf7201e1d856087bf1ed5e191f
lzelkha@redhat.com <lzelkha@redhat.com>
2015-05-05 09:15:49 +0300

    core: Add JPA UserTypes
    The VDS patch requires some UserTypes. To simplify things, those UserTypes
    are added as a separate patch
    
    Change-Id: I30f76493ec72e3c2540e43bef2c095a7ed1c38aa
    Bug-Url: https://bugzilla.redhat.com/1159621
    Signed-off-by: lzelkha@redhat.com <lzelkha@redhat.com>

---
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/jpa/VmRngDeviceSourceUserType.java
@@ -0,0 +1,84 @@
+package org.ovirt.engine.core.dao.jpa;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Objects;
+import java.util.Set;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.usertype.UserType;
+import org.ovirt.engine.core.common.businessentities.VmRngDevice;
+
+public class VmRngDeviceSourceUserType implements UserType {
+    @Override
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return cached;
+    }
+
+    @Override
+    public Object deepCopy(Object val) throws HibernateException {
+        return val;
+    }
+
+    @Override
+    public Serializable disassemble(Object val) throws HibernateException {
+        return (Serializable) val;
+    }
+
+    @Override
+    public boolean equals(Object x, Object y) throws HibernateException {
+        return Objects.equals(x, y);
+    }
+
+    @Override
+    public int hashCode(Object value) throws HibernateException {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public boolean isMutable() {
+        return true;
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor si, Object owner)
+            throws HibernateException, SQLException {
+
+        Object value = rs.getObject(names[0]);
+        if (value == null)
+            return null;
+
+        return VmRngDevice.csvToSourcesSet(value.toString());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void nullSafeSet(PreparedStatement stmt, Object value, int index, SessionImplementor si)
+            throws HibernateException, SQLException {
+        if (value == null) {
+            stmt.setNull(index, Types.VARCHAR);
+        } else {
+            stmt.setString(index, VmRngDevice.sourcesToCsv((Set<VmRngDevice.Source>) value));
+        }
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Class returnedClass() {
+        return Set.class;
+    }
+
+    @Override
+    public int[] sqlTypes() {
+        return new int[] { Types.VARCHAR };
+    }
+}
