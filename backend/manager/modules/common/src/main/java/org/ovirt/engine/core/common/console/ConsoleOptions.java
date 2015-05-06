oVirt/ovirt-engine

commit ece8952062f708245ddc527987eeb9c00d3b3fc4

engine: Phase 1: Move console options to backend
This is the 1st step for generating .vv files on backend. The patch
extracts the ConsoleOptions class to backend.

Changes:
- Add ConsoleOptions class. It contains settings for both SPICE and VNC
  protocols (Instead of storing/duplicating them in AbstractSpice /
  AbstractVnc).
- Delegating console settings in AbstractSpice and AbstractVnc to
  ConsoleOptions class (in the next follow up, console clients will use
  ConsoleOptions directly).
- Extracted ticket validity timeout to constant as this parameter is not
  changing. This eliminates some spaghetti code. When we need variable
  timeout, this can be easily changed.
- Same goes for secure attention mapping hotkey (aka "remap
  ctrl-alt-del" to "ctrl-alt-end"). It's not configurable so far so
  there is no need to maintain the code which does nothing.
- unified common options between VNC and SPICE (like host, port, title
  etc.).

Change-Id: Ib06110fd2b36f96f1baa55b6176240cd27a2cbf9
Signed-off-by: Frantisek Kobzik <fkobzik@redhat.com>
Bug-Url: https://bugzilla.redhat.com/1128763

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/console/ConsoleOptions.java
@@ -1,0 +1,425 @@
+package org.ovirt.engine.core.common.console;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.ovirt.engine.core.common.businessentities.GraphicsType;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.compat.StringHelper;
+
+public class ConsoleOptions implements Serializable{
+
+    private Guid vmId;
+    private GraphicsType graphicsType;
+
+    // general
+    private String host;
+    private Integer port;
+    private String ticket;
+    private String title;
+    private boolean remapCtrlAltDelete;
+    private String toggleFullscreenHotKey;
+    private String releaseCursorHotKey;
+
+    // spice
+    private boolean fullScreen;
+    private int numberOfMonitors;
+    private int usbListenPort;
+    private boolean usbAutoShare;
+    protected String usbFilter;
+    private boolean adminConsole;
+    private String guestHostName;
+    private int securePort;
+    private String sslChanels;
+    private String cipherSuite;
+    private String hostSubject;
+    private String trustStore;
+    private String[] localizedStrings;
+    private String menu;
+    private boolean noTaskMgrExecution;
+    private WanColorDepth wanColorDepth;
+    private boolean wanOptionsEnabled;
+    private boolean smartcardEnabled = false;
+    private String spiceProxy = null;
+    private int ticketValiditySeconds;
+    // the user can choose to disable the smartcard even when it is enabled, but can not choose to enable it, when it is
+    // disabled
+    private boolean smartcardEnabledOverridden = false;
+    // even the spice proxy is globally configured, user can choose to disable it for specific VM
+    private boolean spiceProxyEnabled;
+    private List<WanDisableEffects> wanDisableEffects;
+
+    public static final int TICKET_VALIDITY_SECONDS = 120;
+    public static final String SECURE_ATTENTION_MAPPING = "ctrl+alt+end";
+
+    public ConsoleOptions() {
+        setWanDisableEffects(new ArrayList<WanDisableEffects>());
+        setWanColorDepth(WanColorDepth.depth16);
+        setWanOptionsEnabled(false);
+        setNoTaskMgrExecution(false);
+        // todo in follow-up (Phase 3): setup remap c-a-d default
+    }
+
+    public ConsoleOptions(GraphicsType graphicsType) {
+        this();
+        this.graphicsType = graphicsType;
+    }
+
+    public Guid getVmId() {
+        return vmId;
+    }
+
+    public void setVmId(Guid vmId) {
+        this.vmId = vmId;
+    }
+
+    public GraphicsType getGraphicsType() {
+        return graphicsType;
+    }
+
+    public void setGraphicsType(GraphicsType graphicsType) {
+        this.graphicsType = graphicsType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+
+    public boolean isRemapCtrlAltDelete() {
+        return remapCtrlAltDelete;
+    }
+
+    public void setRemapCtrlAltDelete(boolean remapCtrlAltDelete) {
+        this.remapCtrlAltDelete = remapCtrlAltDelete;
+    }
+
+    public String getToggleFullscreenHotKey() {
+        return toggleFullscreenHotKey;
+    }
+
+    public void setToggleFullscreenHotKey(String toggleFullscreenHotKey) {
+        this.toggleFullscreenHotKey = toggleFullscreenHotKey;
+    }
+
+    public String getReleaseCursorHotKey() {
+        return releaseCursorHotKey;
+    }
+
+    public void setReleaseCursorHotKey(String releaseCursorHotKey) {
+        this.releaseCursorHotKey = releaseCursorHotKey;
+    }
+
+    public boolean isFullScreen() {
+        return fullScreen;
+    }
+
+    public void setFullScreen(boolean fullScreen) {
+        this.fullScreen = fullScreen;
+    }
+
+    public int getNumberOfMonitors() {
+        return numberOfMonitors;
+    }
+
+    public void setNumberOfMonitors(int numberOfMonitors) {
+        this.numberOfMonitors = numberOfMonitors;
+    }
+
+    public int getUsbListenPort() {
+        return usbListenPort;
+    }
+
+    public void setUsbListenPort(int usbListenPort) {
+        this.usbListenPort = usbListenPort;
+    }
+
+    public boolean isUsbAutoShare() {
+        return usbAutoShare;
+    }
+
+    public void setUsbAutoShare(boolean usbAutoShare) {
+        this.usbAutoShare = usbAutoShare;
+    }
+
+    public String getUsbFilter() {
+        return usbFilter;
+    }
+
+    public void setUsbFilter(String usbFilter) {
+        this.usbFilter = usbFilter;
+    }
+
+    public boolean isAdminConsole() {
+        return adminConsole;
+    }
+
+    public void setAdminConsole(boolean adminConsole) {
+        this.adminConsole = adminConsole;
+    }
+
+    public String getGuestHostName() {
+        return guestHostName;
+    }
+
+    public void setGuestHostName(String guestHostName) {
+        this.guestHostName = guestHostName;
+    }
+
+    public int getSecurePort() {
+        return securePort & 0xffff;
+    }
+
+    public void setSecurePort(int securePort) {
+        this.securePort = securePort;
+    }
+
+    public String getSslChanels() {
+        return sslChanels;
+    }
+
+    public void setSslChanels(String sslChanels) {
+        this.sslChanels = adjustLegacySecureChannels(sslChanels);
+    }
+
+    public String getCipherSuite() {
+        return cipherSuite;
+    }
+
+    public void setCipherSuite(String cipherSuite) {
+        this.cipherSuite = cipherSuite;
+    }
+
+    public String getHostSubject() {
+        return hostSubject;
+    }
+
+    public void setHostSubject(String hostSubject) {
+        this.hostSubject = hostSubject;
+    }
+
+    public String getTrustStore() {
+        return trustStore;
+    }
+
+    public void setTrustStore(String trustStore) {
+        this.trustStore = trustStore;
+    }
+
+    public String[] getLocalizedStrings() {
+        return localizedStrings;
+    }
+
+    public void setLocalizedStrings(String[] localizedStrings) {
+        this.localizedStrings = localizedStrings;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public boolean isNoTaskMgrExecution() {
+        return noTaskMgrExecution;
+    }
+
+    public void setNoTaskMgrExecution(boolean noTaskMgrExecution) {
+        this.noTaskMgrExecution = noTaskMgrExecution;
+    }
+
+    public WanColorDepth getWanColorDepth() {
+        return wanColorDepth;
+    }
+
+    public int colorDepthAsInt() {
+        if (getWanColorDepth() != null) {
+            return getWanColorDepth().asInt();
+        }
+
+        return ConsoleOptions.WanColorDepth.depth16.asInt();
+    }
+
+    public void setWanColorDepth(WanColorDepth wanColorDepth) {
+        this.wanColorDepth = wanColorDepth;
+    }
+
+    public boolean isWanOptionsEnabled() {
+        return wanOptionsEnabled;
+    }
+
+    public void setWanOptionsEnabled(boolean wanOptionsEnabled) {
+        this.wanOptionsEnabled = wanOptionsEnabled;
+    }
+
+    public boolean isSmartcardEnabled() {
+        return smartcardEnabled;
+    }
+
+    public void setSmartcardEnabled(boolean smartcardEnabled) {
+        this.smartcardEnabled = smartcardEnabled;
+    }
+
+    public boolean passSmartcardOption() {
+        return isSmartcardEnabled() && !isSmartcardEnabledOverridden();
+    }
+
+    public String getSpiceProxy() {
+        return spiceProxy;
+    }
+
+    public void setSpiceProxy(String spiceProxy) {
+        this.spiceProxy = spiceProxy;
+    }
+
+    public int getTicketValiditySeconds() {
+        return ticketValiditySeconds;
+    }
+
+    public void setTicketValiditySeconds(int ticketValiditySeconds) {
+        this.ticketValiditySeconds = ticketValiditySeconds;
+    }
+
+    /**
+     * Returns true if the user has choosen to disable the smartcard even it is by default enabled
+     */
+    public boolean isSmartcardEnabledOverridden() {
+        return smartcardEnabledOverridden;
+    }
+
+    public void setSmartcardEnabledOverridden(boolean smartcardEnabledOverridden) {
+        this.smartcardEnabledOverridden = smartcardEnabledOverridden;
+    }
+
+    public boolean isSpiceProxyEnabled() {
+        return spiceProxyEnabled;
+    }
+
+    public void setSpiceProxyEnabled(boolean spiceProxyEnabled) {
+        this.spiceProxyEnabled = spiceProxyEnabled;
+    }
+
+    public List<WanDisableEffects> getWanDisableEffects() {
+        return wanDisableEffects;
+    }
+
+    public void setWanDisableEffects(List<WanDisableEffects> wanDisableEffects) {
+        this.wanDisableEffects = wanDisableEffects;
+    }
+
+
+    public enum WanColorDepth {
+        depth16(16),
+        depth32(32);
+        private final int depth;
+
+        private WanColorDepth(int depth) {
+            this.depth = depth;
+        }
+        public static WanColorDepth fromInt(int integerDepth) {
+            for (WanColorDepth value : values()) {
+                if (value.depth == integerDepth) {
+                    return value;
+                }
+            }
+
+            throw new IllegalArgumentException("Illegal int value: " + integerDepth);
+        }
+        public int asInt() {
+            return depth;
+        }
+    }
+
+    public enum WanDisableEffects {
+        animation("animation"),
+        wallpaper("wallpaper"),
+        fontSmooth("font-smooth"),
+        all("all");
+
+        private final String stringValue;
+
+        private WanDisableEffects(String stringValue) {
+            this.stringValue = stringValue;
+        }
+
+        public static WanDisableEffects fromString(String str) {
+            for (WanDisableEffects value : values()) {
+                if (value.stringValue.equals(str)) {
+                    return value;
+                }
+            }
+
+            throw new IllegalArgumentException("Illegal string value: " + str);
+        }
+
+        public String asString() {
+            return stringValue;
+        }
+    }
+
+    /**
+     * Reformat secure channels string if they are in legacy ('s'-prefixed) format.
+     * @param legacySecureChannels (e.g. "smain,sinput")
+     * @return secure channels in correct format (e.g. "main,input")
+     */
+    static String adjustLegacySecureChannels(String legacySecureChannels) {
+        if (StringHelper.isNullOrEmpty(legacySecureChannels)) {
+            return legacySecureChannels;
+        }
+
+        String secureChannels = legacySecureChannels;
+        List<String> legacyChannels = Arrays.asList(
+                new String[]{"smain", "sdisplay", "sinputs", "scursor", "splayback", "srecord", "ssmartcard", "susbredir"});
+
+        for (String channel : legacyChannels) {
+            secureChannels = secureChannels.replace(channel, channel.substring(1));
+        }
+
+        return secureChannels;
+    }
+
+    public String disableEffectsAsString() {
+        StringBuffer disableEffectsBuffer = new StringBuffer("");
+        int countdown = getWanDisableEffects().size();
+        for (ConsoleOptions.WanDisableEffects disabledEffect : getWanDisableEffects()) {
+            disableEffectsBuffer.append(disabledEffect.asString());
+
+            if (countdown != 1) {
+                disableEffectsBuffer.append(", ");
+            }
+            countdown--;
+        }
+
+        return disableEffectsBuffer.toString();
+    }
+}

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/uicommon/SpiceHtml5Impl.java
@@ -20,8 +20,8 @@ public class SpiceHtml5Impl extends AbstractSpice implements ISpiceHtml5 {
 
     @Override
     public void connect() {
-        boolean sslTarget = securePort == -1 ? false : true;
-        WebClientConsoleInvoker invoker = new WebClientConsoleInvoker(CLIENT_PAGE, config, getHost(), String.valueOf(sslTarget ? securePort : port), getPassword(), sslTarget);
+        boolean sslTarget = consoleOptions.getSecurePort() == -1 ? false : true;
+        WebClientConsoleInvoker invoker = new WebClientConsoleInvoker(CLIENT_PAGE, config, getHost(), String.valueOf(sslTarget ? consoleOptions.getSecurePort() : consoleOptions.getPort()), getPassword(), sslTarget);
         invoker.invokeClient();
     }
 

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/presenter/popup/ConsolePopupPresenterWidget.java
@@ -1,5 +1,10 @@
 package org.ovirt.engine.ui.common.presenter.popup;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.Window;
+import org.ovirt.engine.core.common.console.ConsoleOptions;
 import org.ovirt.engine.ui.common.CommonApplicationConstants;
 import org.ovirt.engine.ui.common.gin.AssetProvider;
 import org.ovirt.engine.ui.common.presenter.AbstractModelBoundPopupPresenterWidget;
@@ -19,14 +24,10 @@ import org.ovirt.engine.ui.uicompat.Event;
 import org.ovirt.engine.ui.uicompat.EventArgs;
 import org.ovirt.engine.ui.uicompat.IEventListener;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.EventBus;
-import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.gwtplatform.dispatch.annotation.GenEvent;
 
@@ -148,7 +149,7 @@ public class ConsolePopupPresenterWidget extends AbstractModelBoundPopupPresente
                 : model.getVmConsoles().getVm().getVmPoolName(); // for pool dialogs display pool name
 
         getView().setVmName(vmName);
-        getView().setCtrlAltDeleteRemapHotkey(consoleUtils.getRemapCtrlAltDelHotkey());
+        getView().setCtrlAltDeleteRemapHotkey(ConsoleOptions.SECURE_ATTENTION_MAPPING);
 
         super.init(model);
     }

commit c84e09aaf83458e2e267b141dc33d472f950abb3

frontend: SPICE port is sent as 65535 to spice_html5
Regression introduced by ece8952062f708245ddc527987eeb9c00d3b3fc4 where the
check securePort == -1 ? false : true has been replaced by
consoleOptions.getSecurePort().

The problem is that the securePort has been used instead of getSecurePort
intentionally since the getter returns "securePort & 0xffff" not to send
negative value to the spice client.

Fixed by providing also a getter to get the raw walue

Change-Id: I65a8393a465ebee988f03ec56efa0319626fa0e1
Bug-Url: https://bugzilla.redhat.com/1210379
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/console/ConsoleOptions.java
@@ -201,6 +201,10 @@ public class ConsoleOptions implements Serializable{
         this.securePort = securePort;
     }
 
+    public int getRawSecurePort() {
+        return securePort;
+    }
+
     public String getSslChanels() {
         return sslChanels;
     }

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/uicommon/SpiceHtml5Impl.java
@@ -14,7 +14,7 @@ public class SpiceHtml5Impl extends AbstractSpice implements ISpiceHtml5 {
 
     @Override
     public void invokeClient() {
-        boolean sslTarget = consoleOptions.getSecurePort() != -1;
+        boolean sslTarget = consoleOptions.getRawSecurePort() != -1;
         int port = sslTarget ? consoleOptions.getSecurePort() : consoleOptions.getPort();
         WebClientConsoleInvoker invoker = new WebClientConsoleInvoker(CLIENT_PAGE, getConfig(), getOptions().getHost(), port, getOptions().getTicket(), sslTarget);
         invoker.invokeClient();