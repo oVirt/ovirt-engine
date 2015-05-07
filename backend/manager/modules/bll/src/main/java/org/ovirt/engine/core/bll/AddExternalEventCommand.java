75eec616c46758029a46d1686efb2dfeeceb381d
emesika <emesika@redhat.com>
2015-05-07 16:31:04 +0300

    core[BLL]: Adding external status to command
    This patch handles the required changes in the AddExternalEventCommand
    
    Change-Id: I271aa3f841a3820482d89a1675e28dcb75540277
    Signed-off-by: emesika <emesika@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddExternalEventCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddExternalEventCommand.java
@@ -4,15 +4,22 @@ import java.util.List;
 
 import org.ovirt.engine.core.bll.utils.PermissionSubject;
 import org.ovirt.engine.core.common.AuditLogType;
+import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.common.action.AddExternalEventParameters;
+import org.ovirt.engine.core.common.businessentities.ActionGroup;
 import org.ovirt.engine.core.common.businessentities.AuditLog;
 import org.ovirt.engine.core.common.errors.VdcBllMessages;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AlertDirector;
 import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogableBase;
+import org.ovirt.engine.core.dao.VdsDynamicDAO;
+
+import javax.inject.Inject;
 
 public class AddExternalEventCommand<T extends AddExternalEventParameters> extends ExternalEventCommandBase<T> {
     private static final String OVIRT="oVirt";
+
+    @Inject VdsDynamicDAO hostDao;
 
     public AddExternalEventCommand(T parameters) {
         super(parameters);
@@ -55,10 +62,24 @@ public class AddExternalEventCommand<T extends AddExternalEventParameters> exten
             setActionReturnValue(auditLog.getAuditLogId());
             setSucceeded(true);
         }
+        // Update host external status if set
+        if (hasHostExternalStatus()) {
+            hostDao.updateExternalStatus(getParameters().getEvent().getVdsId(), getParameters().getExternalStatus());
+        }
     }
 
     @Override
     public List<PermissionSubject> getPermissionCheckSubjects() {
-        return getPermissionList(getParameters().getEvent());
+        List<PermissionSubject> permissionList = getPermissionList(getParameters().getEvent());
+        // check for external host status modification
+        if (hasHostExternalStatus()) {
+            permissionList.add(new PermissionSubject(getParameters().getEvent().getVdsId(),
+                    VdcObjectType.VDS, ActionGroup.EDIT_HOST_CONFIGURATION));
+        }
+        return permissionList;
+    }
+
+    private boolean hasHostExternalStatus() {
+        return getParameters().getEvent().getVdsId() != null && getParameters().getExternalStatus() != null;
     }
 }
--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddExternalEventParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddExternalEventParameters.java
@@ -1,21 +1,36 @@
 package org.ovirt.engine.core.common.action;
 
 import org.ovirt.engine.core.common.businessentities.AuditLog;
+import org.ovirt.engine.core.common.businessentities.ExternalStatus;
 
 public class AddExternalEventParameters extends VdcActionParametersBase {
     private static final long serialVersionUID = 7023971593753015624L;
 
     private AuditLog event;
+    private ExternalStatus externalStatus;
 
     public AuditLog getEvent() {
         return event;
     }
 
-    public AddExternalEventParameters() {
+    public ExternalStatus getExternalStatus() {
+        return externalStatus;
     }
 
+    public void setExternalStatus(ExternalStatus externalStatus) {
+        this.externalStatus = externalStatus;
+    }
+
+    public AddExternalEventParameters() {
+
+    }
     public AddExternalEventParameters(AuditLog event) {
         super();
         this.event = event;
+    }
+
+    public AddExternalEventParameters(AuditLog event, ExternalStatus externalStatus) {
+        this(event);
+        this.externalStatus = externalStatus;
     }
 }
