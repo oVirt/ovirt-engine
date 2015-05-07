f282ade1d1f5a53846095a486c06267990965d60
Daniel Erez <derez@redhat.com>
2015-05-07 15:51:23 +0300

    core: introduce LibvirtSecret business entity
    Added LibvirtSecret business entity.
    
    Change-Id: Ifd26c1c1ac8328f6c5a82d9531afa3ff3d0ff837
    Bug-Url: https://bugzilla.redhat.com/1185826
    Signed-off-by: Daniel Erez <derez@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/storage/LibvirtSecret.java
@@ -0,0 +1,105 @@
+package org.ovirt.engine.core.common.businessentities.storage;
+
+import org.ovirt.engine.core.common.businessentities.BusinessEntity;
+import org.ovirt.engine.core.common.businessentities.IVdcQueryable;
+import org.ovirt.engine.core.compat.Guid;
+
+import java.util.Date;
+
+public class LibvirtSecret implements IVdcQueryable, BusinessEntity<Guid> {
+
+    private Guid id;
+    private String value;
+    private LibvirtSecretUsageType usageType;
+    private String description;
+    private Guid providerId;
+    private Date creationDate;
+
+    public LibvirtSecret() {
+    }
+
+    public Guid getId() {
+        return id;
+    }
+
+    public void setId(Guid id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public LibvirtSecretUsageType getUsageType() {
+        return usageType;
+    }
+
+    public void setUsageType(LibvirtSecretUsageType usageType) {
+        this.usageType = usageType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Guid getProviderId() {
+        return providerId;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setProviderId(Guid providerId) {
+        this.providerId = providerId;
+    }
+
+    public Object getQueryableId() {
+        return getId();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        LibvirtSecret that = (LibvirtSecret) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null)
+            return false;
+        if (value != null ? !value.equals(that.value) : that.value != null)
+            return false;
+        if (usageType != that.usageType)
+            return false;
+        if (description != null ? !description.equals(that.description) : that.description != null)
+            return false;
+        if (providerId != null ? !providerId.equals(that.providerId) : that.providerId != null)
+            return false;
+        return !(creationDate != null ? !creationDate.equals(that.creationDate) : that.creationDate != null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + (usageType != null ? usageType.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (providerId != null ? providerId.hashCode() : 0);
+        result = 31 * result + (creationDate != null ? creationDate.hashCode() : 0);
+        return result;
+    }
+}
