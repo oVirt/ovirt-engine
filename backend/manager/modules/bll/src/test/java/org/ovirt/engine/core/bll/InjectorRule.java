2d6bcf0705ab3dcef7a868bc5e4db99e1b00fe23
Roy Golan <rgolan@redhat.com>
2015-05-11 11:40:39 +0300

    core: create a Junit rule for o.o.e.c.di.Injector
    add an InjectorRule to use when testing legacy code with static
    injection.
    
    Change-Id: I8276e8514135f466b59fae384678b2a0a54cf854
    Signed-off-by: Roy Golan <rgolan@redhat.com>

---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/InjectorRule.java
@@ -0,0 +1,33 @@
+package org.ovirt.engine.core.bll;
+
+import org.junit.rules.TestWatcher;
+import org.mockito.Mockito;
+import org.ovirt.engine.core.di.Injector;
+
+import java.lang.reflect.Field;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class InjectorRule extends TestWatcher {
+
+    // create a new injector instance
+    private Injector mockedInjector = mock(Injector.class);
+
+    public InjectorRule() {
+        try {
+            // set the internal injector
+            Field holdingMember = Injector.class.getDeclaredField("injector");
+            holdingMember.setAccessible(true);
+            holdingMember.set(Injector.class, mockedInjector);
+        } catch (Exception e) {
+            // if something bad happened the test shouldn't run
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> void bind(Class<T> pureClsType, T instance) {
+        when(mockedInjector.instanceOf(pureClsType)).thenReturn(instance);
+    }
+
+}
