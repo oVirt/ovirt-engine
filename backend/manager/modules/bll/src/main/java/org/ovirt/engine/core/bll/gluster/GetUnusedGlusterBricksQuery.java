df495110feb369d4a926f862ffed936cbabdce80
Ramesh Nachimuthu <rnachimu@redhat.com>
2015-05-12 16:30:01 +0530

    gluster: don't return used bricks in GetUnusedGlusterBricksQuery
      There is an issue with the filtering logic in query
    GetUnusedGlusterBricksQuery. Brick directory will not be just
    brick mount point, it will be a directory inside the brick
    mount point. Hence changing the logic accordigly
    
    Change-Id: I2fb08502a31b02484f9d902172832ccdfa4ef48a
    Bug-Url: https://bugzilla.redhat.com/1216318
    Signed-off-by: Ramesh Nachimuthu <rnachimu@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/GetUnusedGlusterBricksQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/GetUnusedGlusterBricksQuery.java
@@ -1,9 +1,7 @@
 package org.ovirt.engine.core.bll.gluster;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterBrickEntity;
@@ -23,30 +21,35 @@ public class GetUnusedGlusterBricksQuery<P extends VdsIdParametersBase> extends 
         List<StorageDevice> storageDevicesInHost =
                 getDbFacade().getStorageDeviceDao().getStorageDevicesInHost(getParameters().getVdsId());
         getQueryReturnValue().setReturnValue(getUnUsedBricks(storageDevicesInHost));
-
     }
 
     private List<StorageDevice> getUnUsedBricks(List<StorageDevice> storageDevicesInHost) {
         List<GlusterBrickEntity> usedBricks =
                 getDbFacade().getGlusterBrickDao().getGlusterVolumeBricksByServerId(getParameters().getVdsId());
         List<StorageDevice> freeBricks = new ArrayList<StorageDevice>();
-        Set<String> bricksDir = new HashSet<String>();
-        for (GlusterBrickEntity brick : usedBricks) {
-            bricksDir.add(brick.getBrickDirectory());
-        }
-
         for (StorageDevice storageDevice : storageDevicesInHost) {
             if (storageDevice.getMountPoint() != null
                     && !storageDevice.getMountPoint().isEmpty()
                     && (storageDevice.getMountPoint()
                             .startsWith(Config.<String> getValue(ConfigValues.GlusterDefaultBrickMountPoint))
                     || storageDevice.isGlusterBrick())
-                    && !bricksDir.contains(storageDevice.getMountPoint())) {
+                    && !isBrickUsed(usedBricks, storageDevice.getMountPoint())) {
                 freeBricks.add(storageDevice);
             }
         }
 
         return freeBricks;
+    }
+
+    private boolean isBrickUsed(List<GlusterBrickEntity> usedBricks, String mountPoint) {
+        for (GlusterBrickEntity brick : usedBricks) {
+            // Checks if the given mount point is already part of any Gluster brick directory.
+            // Brick directory may be any directory inside the mount points, so we are using brickDir.startsWith()
+            if (brick.getBrickDirectory().startsWith(mountPoint)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
