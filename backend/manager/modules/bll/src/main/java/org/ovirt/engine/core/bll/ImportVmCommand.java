0163078396f44307b7877d8737fab2ce2a63654b
Maor Lipchuk <mlipchuk@redhat.com>
2015-05-16 12:07:35 +0300

    core: Avoid using export validation for unregistred VMs
    Move validation of ACTION_TYPE_FAILED_VM_NOT_FOUND_ON_EXPORT_DOMAIN to
    be used for VMs which are not an unregistered entities.
    
    Change-Id: I2df389156b156eadf6d4dd1244fa01b449d7d94e
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/ImportVmCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/ImportVmCommand.java
@@ -299,16 +299,14 @@ public class ImportVmCommand<T extends ImportVmParameters> extends ImportVmComma
             if (validator.isDomainExistAndActive().isValid() && getSourceDomain().getStorageDomainType() != StorageDomainType.ImportExport) {
                 return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_STORAGE_DOMAIN_TYPE_ILLEGAL);
             }
+            VM vm = getVmFromExportDomain(getParameters().getVmId());
+            if (vm == null) {
+                return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_VM_NOT_FOUND_ON_EXPORT_DOMAIN);
+            }
+            // At this point we should work with the VM that was read from
+            // the OVF because the VM from the parameters may lack images
+            setVmFromExportDomain(vm);
         }
-
-        VM vm = getVmFromExportDomain(getParameters().getVmId());
-        if (vm == null) {
-            return failCanDoAction(VdcBllMessages.ACTION_TYPE_FAILED_VM_NOT_FOUND_ON_EXPORT_DOMAIN);
-        }
-
-        // At this point we should work with the VM that was read from
-        // the OVF because the VM from the parameters may lack images
-        setVmFromExportDomain(vm);
 
         if (!validateImages(domainsMap)) {
             return false;
