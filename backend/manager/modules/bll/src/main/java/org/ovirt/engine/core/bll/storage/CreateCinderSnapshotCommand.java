e6f32ec3978bff6030b560453cdf83df57e07c0d
Maor Lipchuk <mlipchuk@redhat.com>
2015-05-17 05:41:39 +0300

    core: Create cinder snapshot support
    Add Cinder snapshot disk call back
    also introducing create snapshot for Cinder disk
    
    Change-Id: I986b41aba646c84d0d9d7fe771957cba6faf777a
    Bug-Url: https://bugzilla.redhat.com/1185826
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/CreateCinderSnapshotCommand.java
@@ -0,0 +1,168 @@
+package org.ovirt.engine.core.bll.storage;
+
+import java.util.Date;
+
+import org.ovirt.engine.core.bll.BaseImagesCommand;
+import org.ovirt.engine.core.bll.ImagesHandler;
+import org.ovirt.engine.core.bll.InternalCommandAttribute;
+import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
+import org.ovirt.engine.core.bll.context.CommandContext;
+import org.ovirt.engine.core.bll.tasks.interfaces.CommandCallback;
+import org.ovirt.engine.core.common.action.ImagesContainterParametersBase;
+import org.ovirt.engine.core.common.businessentities.storage.CinderDisk;
+import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
+import org.ovirt.engine.core.common.businessentities.storage.ImageStatus;
+import org.ovirt.engine.core.common.businessentities.storage.VolumeClassification;
+import org.ovirt.engine.core.common.businessentities.storage.VolumeFormat;
+import org.ovirt.engine.core.common.businessentities.storage.VolumeType;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.utils.transaction.TransactionMethod;
+import org.ovirt.engine.core.utils.transaction.TransactionSupport;
+
+@InternalCommandAttribute
+@NonTransactiveCommandAttribute
+public class CreateCinderSnapshotCommand<T extends ImagesContainterParametersBase> extends BaseImagesCommand<T> {
+
+    private CinderDisk disk;
+
+    public CreateCinderSnapshotCommand(T parameters) {
+        this(parameters, null);
+    }
+
+    public CreateCinderSnapshotCommand(T parameters, CommandContext commandContext) {
+        super(parameters, commandContext);
+    }
+
+    private CinderDisk cloneDiskForSnapshot() {
+        CinderDisk cinderDependentVolume = getDisk();
+        initCinderDependentVolume(cinderDependentVolume);
+        String snapshotId = getCinderBroker().createSnapshot(cinderDependentVolume);
+        Guid destinationImageId = Guid.createGuidFromString(snapshotId);
+        getParameters().setDestinationImageId(destinationImageId);
+        cinderDependentVolume.setImageId(destinationImageId);
+        return cinderDependentVolume;
+    }
+
+    private void initCinderDependentVolume(CinderDisk newCinderVolume) {
+        // override volume type and volume format to Unassigned and unassigned for Cinder.
+        newCinderVolume.setVolumeType(VolumeType.Unassigned);
+        newCinderVolume.setvolumeFormat(VolumeFormat.Unassigned);
+        newCinderVolume.setImageStatus(ImageStatus.LOCKED);
+        newCinderVolume.setCreationDate(new Date());
+        newCinderVolume.setLastModifiedDate(new Date());
+        newCinderVolume.setQuotaId(getParameters().getQuotaId());
+        newCinderVolume.setDiskProfileId(getParameters().getDiskProfileId());
+        newCinderVolume.setActive(Boolean.FALSE);
+
+        // Get the last snapshot to be the parent of the new volume.
+        DiskImage leaf = ImagesHandler.getSnapshotLeaf(getDiskImage().getId());
+        newCinderVolume.setParentId(leaf.getImageId());
+    }
+
+    /**
+     * By default old image must be replaced by new one
+     */
+    protected void processOldImageFromDb() {
+        getCompensationContext().snapshotEntity(getDiskImage().getImage());
+        getParameters().setOldLastModifiedValue(getDiskImage().getLastModified());
+        getDiskImage().setLastModified(new Date());
+        getDiskImage().setVmSnapshotId(getParameters().getVmSnapshotId());
+        getImageDao().update(getDiskImage().getImage());
+        getCompensationContext().stateChanged();
+    }
+
+    @Override
+    protected void executeCommand() {
+        final CinderDisk newCinderVolume = cloneDiskForSnapshot();
+        TransactionSupport.executeInNewTransaction(new TransactionMethod<Void>() {
+            @Override
+            public Void runInTransaction() {
+                processOldImageFromDb();
+                newCinderVolume.setVolumeClassification(VolumeClassification.Snapshot);
+                addDiskImageToDb(newCinderVolume, getCompensationContext(), Boolean.FALSE);
+                setActionReturnValue(newCinderVolume);
+                setSucceeded(true);
+                return null;
+            }
+        });
+
+        getReturnValue().setActionReturnValue(newCinderVolume.getImageId());
+        persistCommand(getParameters().getParentCommand(), true);
+        setSucceeded(true);
+        return;
+    }
+
+    @Override
+    protected void endSuccessfully() {
+        if (!getParameters().isLeaveLocked()) {
+            getDestinationDiskImage().setImageStatus(ImageStatus.OK);
+            getImageDao().update(getDestinationDiskImage().getImage());
+        }
+        if (!getParameters().isParentHasTasks()) {
+            getBackend().endAction(getParameters().getParentCommand(),
+                    getParameters().getParentParameters(),
+                    getContext().clone().withoutCompensationContext().withoutExecutionContext().withoutLock());
+        }
+        setSucceeded(true);
+    }
+
+    @Override
+    protected void endWithFailure() {
+        // TODO: Add revert tasks for Cinder
+        if (isDestinationImageExists(getDestinationDiskImage().getId()) &&
+                (isImageSnapshot(getDestinationDiskImage()))) {
+            updateLastModifiedInParent(getDestinationDiskImage().getParentId());
+        }
+        super.endWithFailure();
+        if (!getParameters().isParentHasTasks()) {
+            getParameters().getParentParameters().setTaskGroupSuccess(false);
+            getBackend().endAction(getParameters().getParentCommand(),
+                    getParameters().getParentParameters(),
+                    getContext().clone().withoutCompensationContext().withoutExecutionContext().withoutLock());
+        }
+    }
+
+    protected CinderDisk getDisk() {
+        if (disk == null) {
+            disk = (CinderDisk) getDiskDao().get(getImageId());
+        }
+        return disk;
+    }
+
+    @Override
+    public Guid getStorageDomainId() {
+        return getDisk().getStorageIds().get(0);
+    }
+
+    @Override
+    protected void lockImage() {
+        ImagesHandler.updateImageStatus(getParameters().getImageId(), ImageStatus.LOCKED);
+    }
+
+    private void updateLastModifiedInParent(Guid parentId) {
+        DiskImage previousSnapshot = getDiskImageDao().getSnapshotById(parentId);
+
+        // If the old description of the snapshot got overriden, we should restore the previous description
+        if (getParameters().getOldLastModifiedValue() != null) {
+            previousSnapshot.setLastModified(getParameters().getOldLastModifiedValue());
+        }
+        getImageDao().update(previousSnapshot.getImage());
+    }
+
+    private boolean isImageSnapshot(DiskImage destinationImage) {
+        // Empty Guid, means new disk rather than snapshot, so no need to add a map to the db for new disk.
+        return !destinationImage.getParentId().equals(Guid.Empty)
+                && !destinationImage.getParentId().equals(destinationImage.getImageTemplateId());
+    }
+
+    private boolean isDestinationImageExists(Guid destinationId) {
+        return getDestinationDiskImage() != null
+                && !getVmDAO().getVmsListForDisk(destinationId, false).isEmpty();
+    }
+
+    @Override
+    public CommandCallback getCallback() {
+        return new CreateCinderSnapshotCommandCallback();
+    }
+
+}
---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/CreateCinderSnapshotCommandCallback.java
@@ -0,0 +1,91 @@
+package org.ovirt.engine.core.bll.storage;
+
+import java.util.List;
+
+import org.ovirt.engine.core.bll.tasks.CommandCoordinatorUtil;
+import org.ovirt.engine.core.common.action.ImagesContainterParametersBase;
+import org.ovirt.engine.core.common.businessentities.storage.CinderDisk;
+import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
+import org.ovirt.engine.core.common.businessentities.storage.ImageStatus;
+import org.ovirt.engine.core.compat.CommandStatus;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.BaseDiskDao;
+import org.ovirt.engine.core.dao.DiskDao;
+import org.ovirt.engine.core.dao.DiskImageDAO;
+import org.ovirt.engine.core.dao.ImageDao;
+
+public class CreateCinderSnapshotCommandCallback extends AbstractCinderDiskCommandCallback<CreateCinderSnapshotCommand<ImagesContainterParametersBase>> {
+
+    @Override
+    public void doPolling(Guid cmdId, List<Guid> childCmdIds) {
+        super.doPolling(cmdId, childCmdIds);
+
+        ImageStatus imageStatus = getCinderBroker().getSnapshotStatus(getDiskId());
+        DiskImage disk = getDisk();
+        if (imageStatus != null && imageStatus != disk.getImageStatus()) {
+            switch (imageStatus) {
+            case OK:
+                getCommand().setCommandStatus(CommandStatus.SUCCEEDED);
+                break;
+            case ILLEGAL:
+                getCommand().setCommandStatus(CommandStatus.FAILED);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onFailed(Guid cmdId, List<Guid> childCmdIds) {
+        log.error("Failed adding a Cinder snapshot. snapshot ID: {}", getDiskId());
+        super.onFailed(cmdId, childCmdIds);
+        getCommand().getParameters().setTaskGroupSuccess(false);
+        onFinish(cmdId);
+    }
+
+    @Override
+    public void onSucceeded(Guid cmdId, List<Guid> childCmdIds) {
+        super.onSucceeded(cmdId, childCmdIds);
+        onFinish(cmdId);
+    }
+
+    private void onFinish(Guid cmdId) {
+        getCommand().endAction();
+        CommandCoordinatorUtil.removeAllCommandsInHierarchy(cmdId);
+    }
+
+    @Override
+    protected Guid getDiskId() {
+        // We actually using the destination image id and not the disk id, but the API is strict.
+        return getCommand().getParameters().getDestinationImageId();
+    }
+
+    @Override
+    protected CinderDisk getDisk() {
+        if (disk == null) {
+            disk = (CinderDisk) getDiskImageDAO().getSnapshotById(getDiskId());
+        }
+        return disk;
+    }
+
+    protected DiskDao getDiskDao() {
+        return DbFacade.getInstance().getDiskDao();
+    }
+
+    protected ImageDao getImageDao() {
+        return DbFacade.getInstance().getImageDao();
+    }
+
+    protected BaseDiskDao getBaseDiskDao() {
+        return DbFacade.getInstance().getBaseDiskDao();
+    }
+
+    protected DiskImageDAO getDiskImageDAO() {
+        return DbFacade.getInstance().getDiskImageDao();
+    }
+
+    @Override
+    protected CinderBroker getCinderBroker() {
+        return getCommand().getCinderBroker();
+    }
+}
