021addd5e5c40309ee96accedbfdee886e149b39
Shubhendu Tripathi <shtripat@redhat.com>
2015-05-25 16:10:43 +0530

    gluster: Fixed an issue with query specific to 3.5
    ovirt-engine-3.5 does not support injection of DAO
    objects in DB queries. Fixed the same.
    
    Change-Id: If3f37e03cdd25d32ba95698d57f85a273c848afb
    Bug-URL: https://bugzilla.redhat.com/1224685
    Signed-off-by: Shubhendu Tripathi <shtripat@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllClustersHavingHostsQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllClustersHavingHostsQuery.java
@@ -1,20 +1,14 @@
 package org.ovirt.engine.core.bll;
 
-import javax.inject.Inject;
-
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
-import org.ovirt.engine.core.dao.VdsGroupDAO;
 
 public class GetAllClustersHavingHostsQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
-    @Inject
-    private VdsGroupDAO clusterDao;
-
     public GetAllClustersHavingHostsQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        setReturnValue(clusterDao.getClustersHavingHosts());
+        setReturnValue(getDbFacade().getVdsGroupDao().getClustersHavingHosts());
     }
 }
