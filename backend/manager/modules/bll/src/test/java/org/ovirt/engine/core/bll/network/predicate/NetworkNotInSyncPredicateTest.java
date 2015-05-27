52b75a56c5e896d1100bcee90184dff2a28f619c
Eliraz Levi <elevi@redhat.com>
2015-05-27 19:17:09 +0300

    engine: Introduce syncAllHostNetwork command
    the command will sync all host's networks using
    PersistentHostSetupCommand
    
    Change-Id: I200bd53d77d15a11a3279d2aa147b92fc8791b95
    Bug-Url: https://bugzilla.redhat.com/1194297
    Signed-off-by: Eliraz Levi <elevi@redhat.com>

---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/predicate/NetworkNotInSyncPredicateTest.java
@@ -0,0 +1,41 @@
+package org.ovirt.engine.core.bll.network.predicate;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.ovirt.engine.core.common.businessentities.network.NetworkAttachment;
+import org.ovirt.engine.core.common.businessentities.network.ReportedConfigurations;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NetworkNotInSyncPredicateTest {
+
+    @Mock
+    private NetworkAttachment mockNetworkAttachment;
+
+    @Mock
+    private ReportedConfigurations mockReportedConfigurations;
+
+    private NetworkNotInSyncPredicate underTest = new NetworkNotInSyncPredicate();
+
+    private void initSyncStatus(NetworkAttachment networkAttachment, boolean sync) {
+        when(networkAttachment.getReportedConfigurations()).thenReturn(mockReportedConfigurations);
+        when(mockReportedConfigurations.isNetworkInSync()).thenReturn(sync);
+    }
+
+    @Test
+    public void checkNotSync() {
+        initSyncStatus(mockNetworkAttachment, false);
+        Assert.assertTrue(underTest.eval(mockNetworkAttachment));
+    }
+
+    @Test
+    public void checkSync() {
+        initSyncStatus(mockNetworkAttachment, true);
+        Assert.assertFalse(underTest.eval(mockNetworkAttachment));
+    }
+
+}
