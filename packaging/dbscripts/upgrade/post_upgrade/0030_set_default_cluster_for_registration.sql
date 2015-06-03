f44f3c438dd1feb611e51b0ca5712a8d0230d045
emesika <emesika@redhat.com>
2015-06-03 18:26:54 +0300

    db:set AutoRegistrationDefaultVdsGroupID correctly
    This patch fixes an issue in post-upgrade when attempting to set
    AutoRegistrationDefaultVdsGroupID in vdc_options to the value of the
    default cluster fails and returns null when the default cluster name was
    changed from 'Default' to something else.
    
    In that case we will set the configuration value to the first cluster id
    we can find.
    
    Change-Id: If1aa05a0d24866dfad3a9d18d90262ec07d2fdac
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1226944
    Signed-off-by: emesika <emesika@redhat.com>

--- packaging/dbscripts/upgrade/post_upgrade/0030_set_default_cluster_for_registration.sql
+++ packaging/dbscripts/upgrade/post_upgrade/0030_set_default_cluster_for_registration.sql
@@ -1,5 +1,22 @@
+CREATE OR REPLACE FUNCTION __temp_set_default_cluster_for_registration()
+  RETURNS void AS
+$BODY$
+BEGIN
+    IF EXISTS (select 1 from vds_groups where name ilike 'Default') THEN
+        update vdc_options
+        set option_value = (select vds_group_id from vds_groups where name ilike 'Default')
+        where option_name = 'AutoRegistrationDefaultVdsGroupID' and version = 'general';
+    ELSE
+        -- Default cluster name was changed from 'Default' to something else
+        -- taking the first cluster we found as default
+        update vdc_options
+        set option_value = (select vds_group_id from vds_groups LIMIT 1)
+        where option_name = 'AutoRegistrationDefaultVdsGroupID' and version = 'general';
+    END IF;
+END; $BODY$
+LANGUAGE plpgsql;
 
-update vdc_options
-set option_value = (select vds_group_id from vds_groups where name ilike 'Default')
-where option_name = 'AutoRegistrationDefaultVdsGroupID' and version = 'general';
+select __temp_set_default_cluster_for_registration();
+drop function __temp_set_default_cluster_for_registration();
+
 
