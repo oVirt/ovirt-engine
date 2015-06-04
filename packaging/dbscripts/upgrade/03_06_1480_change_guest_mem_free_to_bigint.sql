cb9282fcedb19509cd3f9f383aea55c083108337
Vinzenz Feenstra <vfeenstr@redhat.com>
2015-06-04 15:14:50 +0200

    db: Fix size of guest memory information from INTEGER => BIGINT
    When using the maximum supported amount of RAM for a VM this value
    can overflow.
    
    Change-Id: I88a5a0e73a1f41caabb1fa88e7fb140c651056db
    Bug-Url: https://bugzilla.redhat.com/1024010
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

---
+++ packaging/dbscripts/upgrade/03_06_1480_change_guest_mem_free_to_bigint.sql
@@ -0,0 +1,3 @@
+SELECT fn_db_change_column_type('vm_dynamic','guest_mem_free','integer','bigint');
+SELECT fn_db_change_column_type('vm_dynamic','guest_mem_buffered','integer','bigint');
+SELECT fn_db_change_column_type('vm_dynamic','guest_mem_cached','integer','bigint');
