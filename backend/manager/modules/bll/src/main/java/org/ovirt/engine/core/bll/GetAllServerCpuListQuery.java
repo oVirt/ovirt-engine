dfccf70417b24b2d69904faee55fa5359ea3c544
Tomas Jelinek <tjelinek@redhat.com>
2015-06-08 17:16:24 +0200

    core: deleted old commented C# code
    Not needed, removed
    
    Change-Id: Ibdbb1dfeaa087d31b02b702014688a5330af93c0
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllServerCpuListQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllServerCpuListQuery.java
@@ -2,53 +2,6 @@ package org.ovirt.engine.core.bll;
 
 import org.ovirt.engine.core.common.queries.GetAllServerCpuListParameters;
 
-//class CanDoActionQuery : QueriesCommandBase
-//{
-//    internal CanDoActionQuery(CanDoActionParameters parameters)
-//        : base(parameters)
-//    {
-
-//    }
-
-//    private CanDoActionParameters _params
-//    {
-//        get
-//        {
-//            return Parameters as CanDoActionParameters;
-//        }
-//    }
-
-//    protected override void ExecuteQueryCommand()
-//    {
-//        List<string> reasons;
-//        QueryReturnValue.ReturnValue = CommandsFactory.canDoActionWithParameters(_params.Action, _params.Id, out reasons, null);
-//        (QueryReturnValue as VdcCanDoQueryReturnValue).Messages = reasons;
-//    }
-//}
-
-//class CanDoActionWithParametersQuery : QueriesCommandBase
-//{
-//    internal CanDoActionWithParametersQuery(CanDoActionWithParametersParameters parameters)
-//        : base(parameters)
-//    {
-//    }
-
-//    private CanDoActionWithParametersParameters _params
-//    {
-//        get
-//        {
-//            return Parameters as CanDoActionWithParametersParameters;
-//        }
-//    }
-
-//    protected override void ExecuteQueryCommand()
-//    {
-//        List<string> reasons;
-//        QueryReturnValue.ReturnValue = CommandsFactory.canDoActionWithParameters(_params.Action, _params.Id, out reasons, _params.AdditionalParameters);
-//        (QueryReturnValue as VdcCanDoQueryReturnValue).Messages = reasons;
-//    }
-//}
-
 public class GetAllServerCpuListQuery<P extends GetAllServerCpuListParameters> extends QueriesCommandBase<P> {
     public GetAllServerCpuListQuery(P parameters) {
         super(parameters);
