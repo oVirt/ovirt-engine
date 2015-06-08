9a852b74d8feec37241df376b7faaea4e4452276
Eliraz Levi <elevi@redhat.com>
2015-06-08 16:46:43 +0300

    engine: Introduce PersistentHostSetupNetworksParameters
    Needed for proper logging of the modified networks.
    
    Change-Id: I0387b64f89e02f2e0e3561e6322c0a5cdbbdba5e
    Bug-Url: https://bugzilla.redhat.com/1194297
    Signed-off-by: Eliraz Levi <elevi@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/PersistentHostSetupNetworksCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/PersistentHostSetupNetworksCommand.java
@@ -1,17 +1,22 @@
 package org.ovirt.engine.core.bll.network.host;
 
+import java.util.Collection;
+
+import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
 import org.ovirt.engine.core.bll.VdsCommand;
 import org.ovirt.engine.core.bll.context.CommandContext;
+import org.ovirt.engine.core.bll.network.host.function.NetworkNameFromNetworkAttachmentTransformationFunction;
 import org.ovirt.engine.core.common.AuditLogType;
-import org.ovirt.engine.core.common.action.HostSetupNetworksParameters;
+import org.ovirt.engine.core.common.action.PersistentHostSetupNetworksParameters;
 import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.action.VdcReturnValueBase;
 import org.ovirt.engine.core.common.action.VdsActionParameters;
 import org.ovirt.engine.core.common.errors.EngineMessage;
+import org.ovirt.engine.core.utils.linq.LinqUtils;
 
 @NonTransactiveCommandAttribute
-public class PersistentHostSetupNetworksCommand<T extends HostSetupNetworksParameters> extends VdsCommand<T> {
+public class PersistentHostSetupNetworksCommand<T extends PersistentHostSetupNetworksParameters> extends VdsCommand<T> {
 
     public PersistentHostSetupNetworksCommand(T parameters) {
         this(parameters, null);
@@ -20,6 +25,24 @@ public class PersistentHostSetupNetworksCommand<T extends HostSetupNetworksParam
     public PersistentHostSetupNetworksCommand(T parameters, CommandContext commandContext) {
         super(parameters, commandContext);
         setVdsId(parameters.getVdsId());
+    }
+
+    public int getSequence() {
+        return getParameters().getSequence();
+    }
+
+    public int getTotal() {
+        return getParameters().getTotal();
+    }
+
+    public String getNetworkNames() {
+        if (getParameters().getNetworkAttachments().size() == 0) {
+            getParameters().setNetworkNames(StringUtils.EMPTY);
+        }
+        else if (StringUtils.isEmpty(getParameters().getNetworkNames())) {
+            updateModifiedNetworksNames();
+        }
+        return getParameters().getNetworkNames();
     }
 
     @Override
@@ -31,7 +54,7 @@ public class PersistentHostSetupNetworksCommand<T extends HostSetupNetworksParam
     @Override
     protected void executeCommand() {
         if (getParameters().getShouldBeLogged()) {
-            auditLogDirector.log(this, AuditLogType.PERSIST_NETWORK_ON_HOST);
+            auditLogDirector.log(this, AuditLogType.PERSIST_HOST_SETUP_NETWORK_ON_HOST);
         }
 
         VdcReturnValueBase returnValue =
@@ -50,6 +73,13 @@ public class PersistentHostSetupNetworksCommand<T extends HostSetupNetworksParam
         }
 
         setSucceeded(returnValue.getSucceeded());
+    }
+
+    private void updateModifiedNetworksNames() {
+        Collection<String> networkNames =
+                LinqUtils.transformToList(getParameters().getNetworkAttachments(),
+                        new NetworkNameFromNetworkAttachmentTransformationFunction());
+        getParameters().setNetworkNames(StringUtils.join(networkNames, ", "));
     }
 
     private boolean checkForChanges() {
