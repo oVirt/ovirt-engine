c273cd696323bab805bc3396b33b8953aa18f518
Vered Volansky <vvolansk@redhat.com>
2015-06-18 17:31:45 +0300

    webadmin: Change NfsStorageView.ui.xml syntax
    Replace HTML with gwt elements.
    Changed expander visibility to align with other fields in the view.
    
    Change-Id: I78a2c4b65534dd6058b2fddfcef1b9369e76f93a
    Signed-off-by: Vered Volansky <vvolansk@redhat.com>

---
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/generic/ShortEntityModelTextBoxEditor.java
@@ -0,0 +1,28 @@
+package org.ovirt.engine.ui.common.widget.editor.generic;
+
+import java.util.Arrays;
+
+import org.ovirt.engine.ui.common.widget.VisibilityRenderer;
+import org.ovirt.engine.ui.common.widget.parser.generic.ToShortEntityModelParser;
+import org.ovirt.engine.ui.uicompat.ConstantsManager;
+
+/**
+ * Composite Editor that uses {@link EntityModelTextBox}.
+ */
+public class ShortEntityModelTextBoxEditor extends NumberEntityModelTextBoxEditor<Short> {
+
+    public ShortEntityModelTextBoxEditor(VisibilityRenderer visibilityRenderer) {
+        super(new EntityModelTextBox<Short>(new ToStringEntityModelRenderer<Short>(), new ToShortEntityModelParser()), visibilityRenderer);
+    }
+
+    public ShortEntityModelTextBoxEditor() {
+        super(new ToStringEntityModelRenderer<Short>(), new ToShortEntityModelParser());
+    }
+
+    @Override
+    protected void handleInvalidState() {
+        //Be sure to call super.handleInvalidstate to make sure the editor valid state is properly updated.
+        super.handleInvalidState();
+        markAsInvalid(Arrays.asList(ConstantsManager.getInstance().getConstants().thisFieldMustContainIntegerNumberInvalidReason()));
+    }
+}
--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/NfsStorageView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/NfsStorageView.java
@@ -1,36 +1,36 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.popup.storage;
 
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.UIObject;
 import org.ovirt.engine.core.common.businessentities.NfsVersion;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.idhandler.WithElementId;
 import org.ovirt.engine.ui.common.widget.Align;
 import org.ovirt.engine.ui.common.widget.VisibilityRenderer;
 import org.ovirt.engine.ui.common.widget.dialog.AdvancedParametersExpander;
-import org.ovirt.engine.ui.common.widget.editor.ListModelListBoxOnlyEditor;
+import org.ovirt.engine.ui.common.widget.editor.ListModelListBoxEditor;
 import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelCheckBoxEditor;
-import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelTextBoxOnlyEditor;
-import org.ovirt.engine.ui.common.widget.editor.generic.ShortEntityModelTextBoxOnlyEditor;
-import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextBoxOnlyEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.ShortEntityModelTextBoxEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextBoxEditor;
 import org.ovirt.engine.ui.common.widget.uicommon.storage.AbstractStorageView;
 import org.ovirt.engine.ui.uicommonweb.models.EntityModel;
 import org.ovirt.engine.ui.uicommonweb.models.storage.NfsStorageModel;
+import org.ovirt.engine.ui.uicompat.ConstantsManager;
 import org.ovirt.engine.ui.uicompat.Event;
 import org.ovirt.engine.ui.uicompat.IEventListener;
 import org.ovirt.engine.ui.uicompat.PropertyChangedEventArgs;
+import org.ovirt.engine.ui.uicompat.UIConstants;
 import org.ovirt.engine.ui.webadmin.ApplicationConstants;
 import org.ovirt.engine.ui.webadmin.gin.AssetProvider;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style;
-import com.google.gwt.dom.client.TableElement;
 import com.google.gwt.editor.client.SimpleBeanEditorDriver;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.text.shared.AbstractRenderer;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.UIObject;
-import com.google.gwt.user.client.ui.ValueBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
@@ -52,13 +52,9 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
     WidgetStyle style;
 
     @UiField
-    @WithElementId
     @Path(value = "path.entity")
-    StringEntityModelTextBoxOnlyEditor pathEditor;
-
-    @UiField
-    @Ignore
-    Label pathLabel;
+    @WithElementId("path")
+    StringEntityModelTextBoxEditor pathEditor;
 
     @UiField
     @Ignore
@@ -70,7 +66,7 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
 
     @UiField
     @Ignore
-    Label warningLabel;
+    VerticalPanel expanderContent;
 
     @UiField(provided = true)
     @Path(value = "override.entity")
@@ -79,58 +75,36 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
 
     @UiField
     @Ignore
-    TableElement expanderContent;
+    Label warningLabel;
 
     @UiField(provided = true)
-    @WithElementId
     @Path(value = "version.selectedItem")
-    ListModelListBoxOnlyEditor<EntityModel<NfsVersion>> versionEditor;
+    @WithElementId("version")
+    ListModelListBoxEditor<EntityModel<NfsVersion>> versionListEditor;
 
     @UiField
-    @Ignore
-    StringEntityModelTextBoxOnlyEditor versionReadOnlyEditor;
-
-    @UiField
-    @Ignore
-    Label versionLabel;
-
-    @UiField
-    @WithElementId
     @Path(value = "retransmissions.entity")
-    ShortEntityModelTextBoxOnlyEditor retransmissionsEditor;
+    @WithElementId("retransmissions")
+    ShortEntityModelTextBoxEditor retransmissionsEditor;
 
     @UiField
-    @Ignore
-    Label retransmissionsLabel;
-
-    @UiField
-    @WithElementId
     @Path(value = "timeout.entity")
-    ShortEntityModelTextBoxOnlyEditor timeoutEditor;
+    @WithElementId("timeout")
+    ShortEntityModelTextBoxEditor timeoutEditor;
 
     @UiField
-    @Ignore
-    Label timeoutLabel;
-
-    @UiField
-    @WithElementId
     @Path(value = "mountOptions.entity")
-    StringEntityModelTextBoxOnlyEditor mountOptionsEditor;
-
-    @UiField
-    @Ignore
-    Label mountOptionsLabel;
-
-    @UiField
-    Label message;
+    @WithElementId
+    StringEntityModelTextBoxEditor mountOptionsEditor;
 
     private final Driver driver = GWT.create(Driver.class);
 
     private final static ApplicationConstants constants = AssetProvider.getConstants();
+    private final static UIConstants uiConstants = ConstantsManager.getInstance().getConstants();
 
     @Inject
     public NfsStorageView() {
-        initEditors();
+        initListBoxEditors();
         initWidget(ViewUiBinder.uiBinder.createAndBindUi(this));
         localize();
         initExpander();
@@ -140,19 +114,21 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
     }
 
     private void initExpander() {
-        expander.initWithContent(expanderContent);
+        expander.initWithContent(expanderContent.getElement());
     }
 
     void addStyles() {
         pathEditor.addContentWidgetContainerStyleName(style.pathEditorContent());
-
-        expanderContent.setClassName(style.expanderContent());
-
+        expanderContent.setStyleName(style.expanderContent());
+        versionListEditor.setStyleName(style.versionListEditor());
+        retransmissionsEditor.setStyleName(style.storageTextBoxEditor());
+        timeoutEditor.setStyleName(style.storageTextBoxEditor());
+        mountOptionsEditor.setStyleName(style.mountTextBoxEditor());
     }
 
-    void initEditors() {
+    void initListBoxEditors() {
 
-        versionEditor = new ListModelListBoxOnlyEditor<EntityModel<NfsVersion>>(new AbstractRenderer<EntityModel<NfsVersion>>() {
+        versionListEditor = new ListModelListBoxEditor<>(new AbstractRenderer<EntityModel<NfsVersion>>() {
             @Override
             public String render(EntityModel<NfsVersion> model) {
                 return model.getTitle();
@@ -163,14 +139,15 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
     }
 
     void localize() {
-        pathLabel.setText(constants.storagePopupNfsPathLabel());
+        pathEditor.setLabel(constants.storagePopupNfsPathLabel());
         pathExampleLabel.setText(constants.storagePopupNfsPathExampleLabel());
         warningLabel.setText(constants.advancedOptionsLabel());
         overrideEditor.setLabel(constants.storagePopupNfsOverrideLabel());
-        versionLabel.setText(constants.storagePopupNfsVersionLabel());
-        retransmissionsLabel.setText(constants.storagePopupNfsRetransmissionsLabel());
-        timeoutLabel.setText(constants.storagePopupNfsTimeoutLabel());
-        mountOptionsLabel.setText(constants.storagePopupAdditionalMountOptionsLabel());
+        versionListEditor.setLabel(constants.storagePopupNfsVersionLabel());
+        retransmissionsEditor.setLabel(constants.storagePopupNfsRetransmissionsLabel());
+        timeoutEditor.setLabel(constants.storagePopupNfsTimeoutLabel());
+        mountOptionsEditor.setLabel(constants.storagePopupAdditionalMountOptionsLabel());
+        mountOptionsEditor.setLabelTooltip(uiConstants.mountOptionsHint());
         expander.setTitleWhenCollapsed(constants.storagePopupConnectionLabel());
         expander.setTitleWhenExpanded(constants.storagePopupConnectionLabel());
     }
@@ -180,7 +157,7 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
         driver.edit(object);
 
         EntityModel version = (EntityModel) object.getVersion().getSelectedItem();
-        versionReadOnlyEditor.asValueBox().setValue(version != null ? version.getTitle() : null);
+        versionListEditor.asEditor().setValue(version);
 
         pathExampleLabel.setVisible(object.getPath().getIsChangable());
 
@@ -193,18 +170,11 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
             }
         });
 
-        styleTextBoxEditor(pathEditor, object.getPath().getIsChangable());
-        styleTextBoxEditor(timeoutEditor,  object.getOverride().getIsChangable());
-        styleTextBoxEditor(retransmissionsEditor, object.getOverride().getIsChangable());
-        styleTextBoxEditor(versionReadOnlyEditor, object.getOverride().getIsChangable());
-        styleTextBoxEditor(mountOptionsEditor, object.getOverride().getIsChangable());
-
-        setElementVisibility(versionEditor,  object.getOverride().getIsChangable());
-        setElementVisibility(versionReadOnlyEditor, !object.getOverride().getIsChangable());
-        setElementVisibility(versionLabel, object.getVersion().getIsAvailable());
-        setElementVisibility(retransmissionsLabel, object.getRetransmissions().getIsAvailable());
-        setElementVisibility(timeoutLabel, object.getTimeout().getIsAvailable());
-        setElementVisibility(mountOptionsLabel, object.getMountOptions().getIsAvailable());
+        setElementVisibility(pathEditor, object.getPath().getIsAvailable());
+        setElementVisibility(versionListEditor, object.getVersion().getIsAvailable());
+        setElementVisibility(retransmissionsEditor, object.getRetransmissions().getIsAvailable());
+        setElementVisibility(timeoutEditor, object.getTimeout().getIsAvailable());
+        setElementVisibility(mountOptionsEditor, object.getTimeout().getIsAvailable());
 
         // When all advanced fields are unavailable - hide the expander.
         boolean anyField = object.getVersion().getIsAvailable()
@@ -220,36 +190,26 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
         return driver.flush();
     }
 
-    interface WidgetStyle extends CssResource {
-
-        String pathEditorContent();
-
-        String expanderContent();
-    }
-
     @Override
     public void focus() {
         pathEditor.setFocus(true);
     }
 
-    /*
-     * Makes a provided editor look like label (enabled, read-only textbox).
-     */
-    private <T> void styleTextBoxEditor(EntityModelTextBoxOnlyEditor<T> editor, boolean enabled) {
-
-        if (!enabled) {
-
-            editor.setEnabled(true);
-
-            ValueBox<T> valueBox = editor.asValueBox();
-            valueBox.setReadOnly(true);
-            valueBox.getElement().getStyle().setBorderWidth(0, Style.Unit.PX);
-        }
+    private void setElementVisibility(UIObject object, boolean value) {
+        object.getElement().getStyle().setDisplay(value ? Style.Display.BLOCK : Style.Display.NONE);
     }
 
-    private void setElementVisibility(UIObject object, boolean value) {
+    interface WidgetStyle extends CssResource {
 
-        object.getElement().getStyle().setDisplay(value ? Style.Display.BLOCK : Style.Display.NONE);
+        String pathEditorContent();
+
+        String expanderContent();
+
+        String storageTextBoxEditor();
+
+        String versionListEditor();
+
+        String mountTextBoxEditor();
     }
 
 }
