ed7e5e7597bf420f16aa7b243dddd7eee7b5086b
Alon Bar-Lev <alonbl@redhat.com>
2015-06-18 11:47:40 +0300

    uutils: crypto: add EnvelopePBE
    Change-Id: I029b01a4d65fb82cdaa04ee1ba10ffc6475b216d
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

---
+++ backend/manager/modules/uutils/src/main/java/org/ovirt/engine/core/uutils/crypto/EnvelopePBE.java
@@ -0,0 +1,83 @@
+package org.ovirt.engine.core.uutils.crypto;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.type.TypeFactory;
+
+public class EnvelopePBE {
+
+    private static final String ARTIFACT_KEY = "artifact";
+    private static final String VERSION_KEY = "version";
+    private static final String ALGORITHM_KEY = "algorithm";
+    private static final String SALT_KEY = "salt";
+    private static final String ITERATIONS_KEY = "iterations";
+    private static final String SECRET_KEY = "secret";
+
+    private static final String ARTIFACT = "EnvelopePBE";
+    private static final String VERSION = "1";
+
+    public static boolean check(String blob, String password) throws IOException, GeneralSecurityException {
+        final Map<String, String> map = new ObjectMapper().readValue(
+            Base64.decodeBase64(blob),
+            TypeFactory.defaultInstance().constructMapType(HashMap.class, String.class, String.class)
+        );
+
+        if (!ARTIFACT.equals(map.get(ARTIFACT_KEY))) {
+            throw new IllegalArgumentException(String.format("Invalid artifact '%s'", map.get(ARTIFACT_KEY)));
+        }
+        if (!VERSION.equals(map.get(VERSION_KEY))) {
+            throw new IllegalArgumentException(String.format("Invalid version '%s'", map.get(VERSION_KEY)));
+        }
+
+        byte[] salt = Base64.decodeBase64(map.get(SALT_KEY));
+        return Arrays.equals(
+            Base64.decodeBase64(map.get(SECRET_KEY)),
+            SecretKeyFactory.getInstance(map.get(ALGORITHM_KEY)).generateSecret(
+                new PBEKeySpec(
+                    password.toCharArray(),
+                    salt,
+                    Integer.parseInt(map.get(ITERATIONS_KEY)),
+                    salt.length * 8
+                )
+            ).getEncoded()
+        );
+    }
+
+    public static String encode(String algorithm, int keySize, int iterations, String randomProvider, String password) throws IOException, GeneralSecurityException {
+        final Base64 base64 = new Base64(0);
+        final Map<String, String> map = new HashMap<String, String>();
+
+        byte[] salt = new byte[keySize/8];
+        SecureRandom.getInstance(randomProvider == null ? "SHA1PRNG" : randomProvider).nextBytes(salt);
+
+        map.put(ARTIFACT_KEY, ARTIFACT);
+        map.put(VERSION_KEY, VERSION);
+        map.put(ALGORITHM_KEY, algorithm);
+        map.put(SALT_KEY, base64.encodeToString(salt));
+        map.put(ITERATIONS_KEY, Integer.toString(iterations));
+        map.put(
+            SECRET_KEY,
+            base64.encodeToString(
+                SecretKeyFactory.getInstance(algorithm).generateSecret(
+                    new PBEKeySpec(
+                        password.toCharArray(),
+                        salt,
+                        iterations,
+                        salt.length*8
+                    )
+                ).getEncoded()
+            )
+        );
+        return base64.encodeToString(new ObjectMapper().writeValueAsString(map).getBytes(Charset.forName("UTF-8")));
+    }
+}
---
+++ backend/manager/modules/uutils/src/test/java/org/ovirt/engine/core/uutils/crypto/EnvelopePBETest.java
@@ -0,0 +1,65 @@
+package org.ovirt.engine.core.uutils.crypto;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Test;
+
+public class EnvelopePBETest {
+
+    @Test
+    public void test1() throws Exception {
+
+        Random random = new Random();
+
+        for (int i=1;i<100;i++) {
+            byte r[] = new byte[i];
+            random.nextBytes(r);
+            String password = new Base64(0).encodeToString(r);
+            String encoded = EnvelopePBE.encode("PBKDF2WithHmacSHA1", 256, 4000, null, password);
+            assertTrue(
+                EnvelopePBE.check(
+                    encoded,
+                    password
+                )
+            );
+            assertFalse(
+                EnvelopePBE.check(
+                    encoded,
+                    password + "A"
+                )
+            );
+        }
+    }
+
+    @Test
+    public void test2() throws Exception {
+        String encoded = "eyJhcnRpZmFjdCI6IkVudmVsb3BlUEJFIiwiaXRlcmF0aW9ucyI6IjQwMDAiLCJzZWNyZXQiOiJNWE5nZVlwNUxSWFNRZmRMYzNDOHRVNWRINFZOODExb0czNjlrU0FLOHAwPSIsInNhbHQiOiJudXFaa2M3dlMrNUNkRzVQaEMvWE5ycDMzd0luTnNnZGZIVXlQRE5RS05rPSIsImFsZ29yaXRobSI6IlBCS0RGMldpdGhIbWFjU0hBMSIsInZlcnNpb24iOiIxIn0=";
+        assertTrue(
+            EnvelopePBE.check(
+                encoded,
+                "password"
+            )
+        );
+        assertFalse(
+            EnvelopePBE.check(
+                encoded,
+                "bad"
+            )
+        );
+    }
+
+    @Test
+    public void test3() throws Exception {
+        String password = "password";
+
+        assertFalse(
+            EnvelopePBE.encode("PBKDF2WithHmacSHA1", 256, 4000, null, password).equals(
+                EnvelopePBE.encode("PBKDF2WithHmacSHA1", 256, 4000, null, password)
+            )
+        );
+    }
+}
