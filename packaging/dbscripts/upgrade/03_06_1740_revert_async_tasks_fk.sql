cb53468fa0d21576c25dad8346ae638f0be265f6
Oved Ourfali <oourfali@redhat.com>
2015-07-14 05:13:41 -0400

    Revert "core:Reintroducing FK to async_tasks table"
    This reverts commit dc1c9baad3379ad0a5d152136df16d70262a9820.
    
    Change-Id: I67300968c06d585974ead8035f58f19964798fb5
    Bug-Url: https://bugzilla.redhat.com/1242784
    Signed-off-by: Oved Ourfali <oourfali@redhat.com>

---
+++ packaging/dbscripts/upgrade/03_06_1740_revert_async_tasks_fk.sql
@@ -0,0 +1,4 @@
+-- Reverting constraints til proper examination of issues introduced by it
+SELECT  fn_db_drop_constraint('async_tasks', 'fk_async_tasks_command_entities_command_id');
+SELECT  fn_db_drop_constraint('async_tasks', 'fk_async_tasks_command_entities_root_command_id');
+
