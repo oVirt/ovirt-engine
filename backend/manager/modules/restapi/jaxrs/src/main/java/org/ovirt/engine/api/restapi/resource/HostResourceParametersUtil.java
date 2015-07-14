b82057a5faaae6f0b041f67a0762f4ecaa2425bc
Roy Golan <rgolan@redhat.com>
2015-07-14 09:26:38 +0300

    restapi: Support host install/reinstall of HE
    Expose 2 matrix parameters when adding/installing a host:
    - POST api/hosts;deploy_hosted_engine
      will pass a DEPLOY action to the underlying installation and if there
      is a hosted engine setup that will deploy the host as part of the
      hosted engine HA cluster.
    - POST api/hosts;undeploy_hosted_engine
      Essentially removing the hosted engine HA component and removing this
      host from the HA cluster. i.e it will not run the hosted engine
      anymore.
    
    Change-Id: Ib2f851f4fec582506f3a3df07407d62385cb9a87
    Bug-Url: https://bugzilla.redhat.com/1167262
    Signed-off-by: Roy Golan <rgolan@redhat.com>
    Depends-On: https://gerrit.ovirt.org/55920

---
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/HostResourceParametersUtil.java
@@ -0,0 +1,31 @@
+package org.ovirt.engine.api.restapi.resource;
+
+import static org.ovirt.engine.api.common.util.ParametersHelper.getBooleanParameter;
+
+import org.ovirt.engine.core.common.businessentities.HostedEngineDeployConfiguration;
+
+public class HostResourceParametersUtil {
+    public static final String QUERY_PARAM_DEPLOY_HOSTED_ENGINE = "deploy_hosted_engine";
+    public static final String QUERY_PARAM_UNDEPLOY_HOSTED_ENGINE = "undeploy_hosted_engine";
+
+    public static HostedEngineDeployConfiguration getHostedEngineDeployConfiguration(BaseBackendResource resource) {
+        // deploy?
+        if (getBooleanParameter(resource.getHttpHeaders(),
+                resource.getUriInfo(),
+                QUERY_PARAM_DEPLOY_HOSTED_ENGINE,
+                true,
+                false)) {
+            return new HostedEngineDeployConfiguration(HostedEngineDeployConfiguration.Action.DEPLOY);
+        }
+        // undeploy?
+        if (getBooleanParameter(resource.getHttpHeaders(),
+                resource.getUriInfo(),
+                QUERY_PARAM_UNDEPLOY_HOSTED_ENGINE,
+                true,
+                false)) {
+            return new HostedEngineDeployConfiguration(HostedEngineDeployConfiguration.Action.UNDEPLOY);
+        }
+        // null will be safely ignored and is backward compatible
+        return null;
+    }
+}
