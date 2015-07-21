8894da6025e7ce737f0ea3f3cf74e00160a2625c
Shmuel Melamud <smelamud@redhat.com>
2015-07-21 17:20:51 +0300

    core: Compensation of active snapshot ID change
    If exception is thrown inside CreateAllSnapshotsFromVmCommand, we need
    to compensate the change of active snapshot ID that is performed during
    snapshot creation. Otherwise, link between disk images and the active
    snapshot is lost.
    
    Change-Id: I432859105d31f74f10711f6a63b188d325a43e30
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1274717
    Signed-off-by: Shmuel Melamud <smelamud@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateAllSnapshotsFromVmCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CreateAllSnapshotsFromVmCommand.java
@@ -199,11 +199,9 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
 
     @Override
     protected void executeVmCommand() {
-        Guid createdSnapshotId = getSnapshotDao().getId(getVmId(), SnapshotType.ACTIVE);
         getParameters().setSnapshotType(determineSnapshotType());
 
-        getSnapshotDao().updateId(createdSnapshotId, newActiveSnapshotId);
-
+        Guid createdSnapshotId = updateActiveSnapshotId();
         setActionReturnValue(createdSnapshotId);
 
         MemoryImageBuilder memoryImageBuilder = getMemoryImageBuilder();
@@ -218,6 +216,25 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
         }
 
         setSucceeded(true);
+    }
+
+    private Guid updateActiveSnapshotId() {
+        final Snapshot activeSnapshot = getSnapshotDao().get(getVmId(), SnapshotType.ACTIVE);
+        final Guid activeSnapshotId = activeSnapshot.getId();
+
+        TransactionSupport.executeInScope(TransactionScopeOption.Required, new TransactionMethod<Void>() {
+            @Override
+            public Void runInTransaction() {
+                getCompensationContext().snapshotEntity(activeSnapshot);
+                getSnapshotDao().updateId(activeSnapshotId, newActiveSnapshotId);
+                activeSnapshot.setId(newActiveSnapshotId);
+                getCompensationContext().snapshotNewEntity(activeSnapshot);
+                getCompensationContext().stateChanged();
+                return null;
+            }
+        });
+
+        return activeSnapshotId;
     }
 
     public Guid getStorageDomainIdForVmMemory(List<DiskImage> memoryDisksList) {
