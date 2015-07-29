oVirt/ovirt-engine

commit b5295bc0a0bf9a19ffd97980be5d7345c3a0c3d4

userportal/webadmin: cluster parameters override - UI enhancements
- In the 'edit\add VM\Template\Pool' window, under the custom
  cluster-parameters comboboxes, added the name of the current
  cluster value instead of just 'Use Cluster Default'.

- Added a warning when saving a VM and the cpu-model isn't supported
  by the cluster.

- Added a warning when lowering the cluster cpu-level while there are
  VMs in it with a cpu-level which is higher than the new cluster value.

Change-Id: I2e1c458dc4e4b6e61d2a1b92327287166a12afa2
Bug-Url: https://bugzilla.redhat.com/838487
Signed-off-by: Eldan Shachar <eshachar@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/templates/TemplateListModel.java
@@ -591,14 +591,14 @@ public class TemplateListModel extends VmBaseListModel<Void, VmTemplate> impleme
 
     private void onSave()
     {
-        UnitVmModel model = (UnitVmModel) getWindow();
+        final UnitVmModel model = (UnitVmModel) getWindow();
 
         if (!model.validate())
         {
             return;
         }
 
-        String name = model.getName().getEntity();
+        final String name = model.getName().getEntity();
 
         boolean isBaseTemplate = false;
 
@@ -614,14 +614,47 @@ public class TemplateListModel extends VmBaseListModel<Void, VmTemplate> impleme
                         @Override
                         public void onSuccess(Object target, Object returnValue) {
 
-                            TemplateListModel templateListModel = (TemplateListModel) target;
                             boolean isNameUnique = (Boolean) returnValue;
-                            templateListModel.postNameUniqueCheck(isNameUnique);
+
+                            if (model.getBehavior().isExistingTemplateBehavior()) {
+                                selectedItem = ((TemplateVmModelBehavior) model.getBehavior()).getVmTemplate();
+                            } else {
+                                selectedItem = ((ExistingBlankTemplateModelBehavior) model.getBehavior()).getVmTemplate();
+                            }
+
+                            if (!isNameUnique && name.compareToIgnoreCase(selectedItem.getName()) != 0) {
+                                model.getName()
+                                        .getInvalidityReasons()
+                                        .add(ConstantsManager.getInstance()
+                                                .getConstants()
+                                                .nameMustBeUniqueInvalidReason());
+                                model.getName().setIsValid(false);
+                                model.setValidTab(TabName.GENERAL_TAB, false);
+                                return;
+                            }
+
+                            String selectedCpu = model.getCustomCpu().getSelectedItem();
+                            if (selectedCpu != null && !selectedCpu.isEmpty()  && !model.getCustomCpu().getItems().contains(selectedCpu)) {
+                                ConfirmationModel confirmModel = new ConfirmationModel();
+                                confirmModel.setTitle(ConstantsManager.getInstance().getConstants().vmUnsupportedCpuTitle());
+                                confirmModel.setMessage(ConstantsManager.getInstance().getConstants().vmUnsupportedCpuMessage());
+                                confirmModel.setHelpTag(HelpTag.edit_unsupported_cpu);
+                                confirmModel.setHashName("edit_unsupported_cpu"); //$NON-NLS-1$
+
+                                confirmModel.getCommands().add(new UICommand("postNameUniqueCheck", TemplateListModel.this) //$NON-NLS-1$
+                                        .setTitle(ConstantsManager.getInstance().getConstants().ok())
+                                        .setIsDefault(true));
+
+                                confirmModel.getCommands()
+                                        .add(UICommand.createCancelUiCommand("CancelConfirmation", TemplateListModel.this)); //$NON-NLS-1$
+
+                                setConfirmWindow(confirmModel);
+                            }
 
                         }
                     }), name, model.getSelectedDataCenter() == null ? null : model.getSelectedDataCenter().getId());
         } else {
-            postNameUniqueCheck(true);
+            postNameUniqueCheck();
         }
     }
 
@@ -643,7 +676,7 @@ public class TemplateListModel extends VmBaseListModel<Void, VmTemplate> impleme
         validateVm(model, name);
     }
 
-    public void postNameUniqueCheck(boolean isNameUnique)
+    public void postNameUniqueCheck()
     {
         final UnitVmModel model = (UnitVmModel) getWindow();
 
@@ -667,18 +700,6 @@ public class TemplateListModel extends VmBaseListModel<Void, VmTemplate> impleme
                 ? null
                 : IconUtils.filterPredefinedIcons(model.getIcon().getEntity().getIcon());
 
-        String name = model.getName().getEntity();
-
-        // Check name unicitate.
-        if (!isNameUnique && name.compareToIgnoreCase(template.getName()) != 0)
-        {
-            model.getName()
-                    .getInvalidityReasons()
-                    .add(ConstantsManager.getInstance().getConstants().nameMustBeUniqueInvalidReason());
-            model.getName().setIsValid(false);
-            model.setValidTab(TabName.GENERAL_TAB, false);
-            return;
-        }
 
         // Save changes.
         buildTemplateOnSave(model, template);
@@ -901,6 +922,10 @@ public class TemplateListModel extends VmBaseListModel<Void, VmTemplate> impleme
         else if ("OnSaveVm".equals(command.getName())) //$NON-NLS-1$
         {
             onSaveVm();
+        }
+        else if ("postNameUniqueCheck".equals(command.getName())) { //$NON-NLS-1$
+            postNameUniqueCheck();
+            setConfirmWindow(null);
         }
         else if ("OnRemove".equals(command.getName())) //$NON-NLS-1$
         {

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetClusterUnsupportedVmsCpusQuery.java
@@ -1,0 +1,34 @@
+package org.ovirt.engine.core.bll;
+
+import org.ovirt.engine.core.common.businessentities.VM;
+import org.ovirt.engine.core.common.queries.GetClusterUnsupportedVmsCpusParameters;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The query checks if all of the VMs in a given cluster are compatible with a given cpu level.
+ * The query returns the list of VMs+CPUs which are not compatible with the new CPU.
+ */
+public class GetClusterUnsupportedVmsCpusQuery<P extends GetClusterUnsupportedVmsCpusParameters> extends QueriesCommandBase<P> {
+    public GetClusterUnsupportedVmsCpusQuery(P parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected void executeQueryCommand() {
+        List<VM> vmsForCluster = getDbFacade().getInstance().getVmDao().getAllForVdsGroup(getParameters().getVdsGroupId());
+
+        Map<String, String> highCpuVms = new HashMap<>();
+        for (VM vm : vmsForCluster) {
+            if (vm.getCustomCpuName() != null && !vm.getCustomCpuName().isEmpty()) {
+                String vmCpuName = CpuFlagsManagerHandler.getCpuNameByCpuId(vm.getCustomCpuName(), CpuFlagsManagerHandler.getLatestDictionaryVersion());
+                if (vmCpuName == null || CpuFlagsManagerHandler.compareCpuLevels(vmCpuName, getParameters().getNewCpuName(), CpuFlagsManagerHandler.getLatestDictionaryVersion()) > 0) {
+                    highCpuVms.put(vm.getName(), vm.getCustomCpuName());
+                }
+            }
+        }
+        getQueryReturnValue().setReturnValue(highCpuVms);
+    }
+}

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetClusterUnsupportedVmsCpusParameters.java
@@ -1,0 +1,27 @@
+package org.ovirt.engine.core.common.queries;
+
+import org.ovirt.engine.core.compat.Guid;
+
+public class GetClusterUnsupportedVmsCpusParameters extends VdcQueryParametersBase {
+
+    private static final long serialVersionUID = -8663922280262494306L;
+
+    private Guid vdsGroupId;
+    private String newCpuName;
+
+    public GetClusterUnsupportedVmsCpusParameters(Guid vdsGroupId, String newCpuName) {
+        this.vdsGroupId = vdsGroupId;
+        this.newCpuName = newCpuName;
+    }
+
+    public GetClusterUnsupportedVmsCpusParameters() {
+    }
+
+    public Guid getVdsGroupId() {
+        return vdsGroupId;
+    }
+
+    public String getNewCpuName() {
+        return newCpuName;
+    }
+}

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/VdcQueryType.java
@@ -269,6 +269,7 @@ public enum VdcQueryType implements Serializable {
     // License queries
     GetAllServerCpuList,
     GetSupportedCpuList(VdcQueryAuthType.User),
+    GetClusterUnsupportedVmsCpus,
 
     // Multi Level Administration queries
     GetAllRoles(VdcQueryAuthType.User),

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationConstants.java
@@ -568,10 +568,10 @@ public interface CommonApplicationConstants extends Constants {
     @DefaultStringValue("Comment")
     String commentLabel();
 
-    @DefaultStringValue("Emulated Machine override")
+    @DefaultStringValue("Custom Emulated Machine")
     String emulatedMachineLabel();
 
-    @DefaultStringValue("CPU Model override")
+    @DefaultStringValue("Custom CPU Type")
     String cpuModelLabel();
 
     @DefaultStringValue("Use cluster default")

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/ListModelTypeAheadChangeableListBox.java
@@ -18,7 +18,7 @@ public class ListModelTypeAheadChangeableListBox extends ListModelTypeAheadListB
     public ListModelTypeAheadChangeableListBox(SuggestBoxRenderer<String> renderer, boolean autoAddToValidValues, String nullReplacementText) {
         super(renderer, autoAddToValidValues, new SuggestionMatcher.StartWithSuggestionMatcher());
         if (nullReplacementText == null) {
-            nullReplacementText = ""; //$NON-NLS-1$
+            this.nullReplacementText = ""; //$NON-NLS-1$
         } else {
             this.nullReplacementText = nullReplacementText;
         }
@@ -50,5 +50,13 @@ public class ListModelTypeAheadChangeableListBox extends ListModelTypeAheadListB
         String lastText = suggestBox.getText();
         super.showAllSuggestions();
         suggestBox.setText(lastText);
+    }
+
+    protected void setNullReplacementString(String nullReplacementText) {
+        this.nullReplacementText = nullReplacementText;
+    }
+
+    private String getNullReplacementString() {
+        return nullReplacementText;
     }
 }

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/ListModelTypeAheadChangeableListBoxEditor.java
@@ -49,4 +49,8 @@ public class ListModelTypeAheadChangeableListBoxEditor extends AbstractValidated
             return data; // for string objects the replacement value is the object itself (null safe)
         }
     }
+
+    public void setNullReplacementString(String nullReplacementString) {
+        super.getContentWidget().setNullReplacementString(nullReplacementString);
+    }
 }

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/AbstractVmPopupWidget.java
@@ -1327,7 +1327,7 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
                     @Override
                     public String getDisplayStringNullSafe(String data) {
                         if (data == null || data.trim().isEmpty()) {
-                            data = constants.clusterDefaultOption();
+                            data = getDefaultEmulatedMachineLabel();
                         }
                         return typeAheadNameTemplateNullSafe(data);
                     }
@@ -1342,7 +1342,7 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
                     @Override
                     public String getDisplayStringNullSafe(String data) {
                         if (data == null || data.trim().isEmpty()) {
-                            data = constants.clusterDefaultOption();
+                            data = getDefaultCpuTypeLabel();
                         }
                         return typeAheadNameTemplateNullSafe(data);
                     }
@@ -1787,6 +1787,34 @@ public abstract class AbstractVmPopupWidget extends AbstractModeSwitchingPopupWi
                 rngPanel.setVisible(object.getIsRngEnabled().getEntity());
             }
         });
+
+        object.getDataCenterWithClustersList().getSelectedItemChangedEvent().addListener(new IEventListener<EventArgs>() {
+            @Override
+            public void eventRaised(Event<? extends EventArgs> ev, Object sender, EventArgs args) {
+                emulatedMachine.setNullReplacementString(getDefaultEmulatedMachineLabel());
+                customCpu.setNullReplacementString(getDefaultCpuTypeLabel());
+            }
+        });
+    }
+
+    private String getDefaultEmulatedMachineLabel() {
+        VDSGroup vdsGroup = getModel().getSelectedCluster();
+        String newClusterEmulatedMachine = constants.clusterDefaultOption();
+        if (vdsGroup != null) {
+            String emulatedMachine = (vdsGroup.getEmulatedMachine() == null) ? "" : vdsGroup.getEmulatedMachine(); //$NON-NLS-1$
+            newClusterEmulatedMachine +=  "(" + emulatedMachine + ")"; //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        return newClusterEmulatedMachine;
+    }
+
+    private String getDefaultCpuTypeLabel() {
+        VDSGroup vdsGroup = getModel().getSelectedCluster();
+        String newClusterCpuModel = constants.clusterDefaultOption();
+        if (vdsGroup != null) {
+            String cpuName = (vdsGroup.getCpuName() == null) ? "" : vdsGroup.getCpuName(); //$NON-NLS-1$
+            newClusterCpuModel += "(" + cpuName + ")"; //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        return newClusterCpuModel;
     }
 
     /**

commit 3904ec2472a95af087445a7a1200b710843f9c14

webadmin: edit template dialog not working
Regression introduced by b5295bc which added the handling of the case when the
custom CPU is changed but did not add the same handling when not.

Change-Id: I5fcc9d6c94c12a3dfc13f2bf81ad024fd0268492
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/templates/TemplateListModel.java
@@ -608,6 +608,8 @@ public class TemplateListModel extends VmBaseListModel<Void, VmTemplate> impleme
                                         .add(UICommand.createCancelUiCommand("CancelConfirmation", TemplateListModel.this)); //$NON-NLS-1$
 
                                 setConfirmWindow(confirmModel);
+                            } else {
+                                postNameUniqueCheck();
                             }
 
                         }