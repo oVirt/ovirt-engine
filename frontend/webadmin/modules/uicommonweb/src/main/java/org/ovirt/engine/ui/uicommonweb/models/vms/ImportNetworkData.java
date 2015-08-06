a2c0a880183781da7ef5fe43d01a3af9b69766af
Arik Hadas <ahadas@redhat.com>
2015-08-06 16:38:14 +0300

    webadmin: fix network interfaces of imported vm using v2v
    The network interfaces for a VM that is imported using virt-v2v were
    not defined correctly. This patch fixes it by setting the network
    name and profile properly.
    
    In addition, a new column is added the the network interfaces sub-tab
    of the import VM dialog in order to show the name of the network that
    the interface is declared on in the remote environment.
    
    Change-Id: I804e988c97cf72924c82d8bb1ca4c6cab4caeebb
    Bug-Url: https://bugzilla.redhat.com/1236075
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/ImportNetworkData.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/ImportNetworkData.java
@@ -1,18 +1,75 @@
 package org.ovirt.engine.ui.uicommonweb.models.vms;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import org.ovirt.engine.core.common.businessentities.network.Network;
+import org.ovirt.engine.core.common.businessentities.network.VnicProfileView;
 
 public class ImportNetworkData {
 
-    private List<Network> networks;
+    private List<String> networkNames;
+    private String selectedNetworkName;
+    private List<VnicProfileView> networkProfiles;
+    private List<VnicProfileView> filteredNetworkProfiles;
+    private VnicProfileView selectedNetworkProfile;
 
-    public List<Network> getNetworks() {
-        return networks;
+    public ImportNetworkData(List<VnicProfileView> networkProfiles) {
+        this.networkProfiles = networkProfiles;
+        Set<String> networkNames = new HashSet<>();
+        for (VnicProfileView networkProfile : networkProfiles) {
+            networkNames.add(networkProfile.getNetworkName());
+        }
+        setNetworkNames(new ArrayList<>(networkNames));
     }
 
-    public void setNetworks(List<Network> networks) {
-        this.networks = networks;
+    public List<VnicProfileView> getFilteredNetworkProfiles() {
+        return filteredNetworkProfiles;
+    }
+
+    public void setSelectedNetworkProfile(String networkProfileName) {
+        for (VnicProfileView profile : networkProfiles) {
+            if (profile.getName().equals(networkProfileName)) {
+                setSelectedNetworkProfile(profile);
+                break;
+            }
+        }
+    }
+
+    public VnicProfileView getSelectedNetworkProfile() {
+        if (selectedNetworkProfile != null) {
+            return selectedNetworkProfile;
+        }
+        return !filteredNetworkProfiles.isEmpty() ? filteredNetworkProfiles.get(0) : null;
+    }
+
+    private void setSelectedNetworkProfile(VnicProfileView networkProfile) {
+        selectedNetworkProfile = networkProfile;
+    }
+
+    public List<String> getNetworkNames() {
+        return networkNames;
+    }
+
+    private void setNetworkNames(List<String> networkNames) {
+        this.networkNames = networkNames;
+        if (!networkNames.isEmpty()) {
+            setSelectedNetworkName(networkNames.get(0));
+        }
+    }
+
+    public void setSelectedNetworkName(String networkName) {
+        selectedNetworkName = networkName;
+        filteredNetworkProfiles = new ArrayList<>();
+        for (VnicProfileView profile : networkProfiles) {
+            if (profile.getNetworkName().equals(networkName)) {
+                filteredNetworkProfiles.add(profile);
+            }
+        }
+    }
+
+    public String getSelectedNetworkName() {
+        return selectedNetworkName;
     }
 }
