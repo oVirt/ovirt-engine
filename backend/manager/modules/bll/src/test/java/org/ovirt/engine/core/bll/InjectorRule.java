7c36d8e43664bdc34709c2647dfb6fe059742cdd
Yevgeny Zaspitsky <yzaspits@redhat.com>
2015-08-06 16:56:21 +0300

    engine: Reset InjectorRule after each test case
    Reset InjectorRule after each test case.
    That should discover all tests that rely on
    the order of tests execution.
    
    Change-Id: I5759a023ad23a2bf1088d78247889ac149bf7c35
    Signed-off-by: Yevgeny Zaspitsky <yzaspits@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/InjectorRule.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/InjectorRule.java
@@ -1,19 +1,25 @@
 package org.ovirt.engine.core.bll;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
 
 import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.ovirt.engine.core.di.Injector;
 
 public class InjectorRule extends TestWatcher {
 
     // create a new injector instance
-    private Injector mockedInjector = mock(Injector.class);
+    private final Injector mockedInjector;
 
     public InjectorRule() {
+        mockedInjector = mock(Injector.class);
+    }
+
+    private void overrideInjector(Injector mockedInjector) {
         try {
             // set the internal injector
             Field holdingMember = Injector.class.getDeclaredField("injector");
@@ -25,7 +31,15 @@ public class InjectorRule extends TestWatcher {
         }
     }
 
+    @Override
+    protected void finished(Description description) {
+        super.finished(description);
+        overrideInjector(null);
+        reset(mockedInjector);
+    }
+
     public <T> void bind(Class<T> pureClsType, T instance) {
+        overrideInjector(mockedInjector);
         when(mockedInjector.instanceOf(pureClsType)).thenReturn(instance);
     }
 
