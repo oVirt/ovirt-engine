447941e07f728ccf1d47328f83d1378d4c4549fc
Allon Mureinik <amureini@redhat.com>
2015-08-06 16:58:24 +0300

    core: RpmVersionTest asserts
    Cleaned up asserts in RpmVersionTest to use assertEquals and
    assertNotEquals with the right order of arguments to make the test
    easier to read and understand.
    
    Change-Id: Ia733f266d07c341e09b36b40defdc11f21d4f7ac
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/compat/src/test/java/org/ovirt/engine/core/compat/RpmVersionTest.java
+++ backend/manager/modules/compat/src/test/java/org/ovirt/engine/core/compat/RpmVersionTest.java
@@ -1,8 +1,7 @@
 package org.ovirt.engine.core.compat;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -10,27 +9,27 @@ public class RpmVersionTest {
 
     @Test
     public void rpmVersionTest() {
-        assertEquals(new RpmVersion("rhev-agent-2.3.235-1.el6").getValue(), "2.3.235.0");
-        assertEquals(new RpmVersion("rhev-agent-2.3.17-1.el6").getValue(), "2.3.17.0");
-        assertEquals(new RpmVersion("glibc-devel-2.133.4.5-1.x86_64").getValue(), "2.133.4.5");
-        assertEquals(new RpmVersion("test-javadb-common-10.6.2-1.1.i386").getValue(), "10.6.2.0");
-        assertEquals(new RpmVersion("test-javadb-common-10.6.2.3.3-1.1.i386").getValue(), "10.6.2.3");
-        assertTrue(new RpmVersion("").getValue().length() == 0);
-        assertTrue(new RpmVersion(null).getValue().length() == 0);
+        assertEquals("2.3.235.0", new RpmVersion("rhev-agent-2.3.235-1.el6").getValue());
+        assertEquals("2.3.17.0", new RpmVersion("rhev-agent-2.3.17-1.el6").getValue());
+        assertEquals("2.133.4.5", new RpmVersion("glibc-devel-2.133.4.5-1.x86_64").getValue());
+        assertEquals("10.6.2.0", new RpmVersion("test-javadb-common-10.6.2-1.1.i386").getValue());
+        assertEquals("10.6.2.3", new RpmVersion("test-javadb-common-10.6.2.3.3-1.1.i386").getValue());
+        assertEquals(0, new RpmVersion("").getValue().length());
+        assertEquals(0, new RpmVersion(null).getValue().length());
     }
 
     @Test
     public void agentVersionTest() {
-        assertEquals(new RpmVersion("RHEV-Agent 2.3.395", "RHEV-Agent", true).getValue(), "2.3.395.0");
-        assertEquals(new RpmVersion("rhev-agent-2.2-1.el6", "RHEV-Agent", true).getValue(), "2.2.0.0");
-        assertEquals(new RpmVersion("rhev-agent-2.3.7-1.el6", "RHEV-Agent", true).getValue(), "2.3.7.0");
-        assertEquals(new RpmVersion("rhev-agent-2.3.7.10-1.el6", "RHEV-Agent", true).getValue(), "2.3.7.10");
-        assertEquals(new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", true).getValue(), "2.3.7.10");
-        assertEquals(new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "rhev-agent", false).getValue(), "2.3.7.10");
-        assertEquals(new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", false).getValue(), "2.3.7.10");
-        assertFalse("0.0.0.0".equals(new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", false).getValue()));
-        assertTrue(new RpmVersion("", "", false).getValue().length() == 0);
-        assertTrue(new RpmVersion(null, null, false).getValue().length() == 0);
+        assertEquals("2.3.395.0", new RpmVersion("RHEV-Agent 2.3.395", "RHEV-Agent", true).getValue());
+        assertEquals("2.2.0.0", new RpmVersion("rhev-agent-2.2-1.el6", "RHEV-Agent", true).getValue());
+        assertEquals("2.3.7.0", new RpmVersion("rhev-agent-2.3.7-1.el6", "RHEV-Agent", true).getValue());
+        assertEquals("2.3.7.10", new RpmVersion("rhev-agent-2.3.7.10-1.el6", "RHEV-Agent", true).getValue());
+        assertEquals("2.3.7.10", new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", true).getValue());
+        assertEquals("2.3.7.10", new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "rhev-agent", false).getValue());
+        assertEquals("2.3.7.10", new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", false).getValue());
+        assertNotEquals("0.0.0.0", new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", false).getValue());
+        assertEquals(0, new RpmVersion("", "", false).getValue().length());
+        assertEquals(0, new RpmVersion(null, null, false).getValue().length());
     }
 
     @Test
@@ -42,13 +41,13 @@ public class RpmVersionTest {
 
     @Test
     public void testRpmRelease() {
-        assertEquals(new RpmVersion("ovirt-node-iso-2.6.0-20130212.fc18.noarch").getRpmRelease(),
-                "20130212.fc18.noarch");
-        assertEquals(new RpmVersion("test-javadb-common-10.2.2.2.2-1.1.i386").getRpmRelease(), "1.1.i386");
-        assertEquals(new RpmVersion("java-1.7.0-openjdk-devel-1.7.0.25-2.3.10.4.fc18.x86_64").getRpmRelease(),
-                "2.3.10.4.fc18.x86_64");
-        assertEquals(new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", true).getRpmRelease(), "1.el6");
-        assertEquals(new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "rhev-agent", false).getRpmRelease(), "1.el6");
+        assertEquals("20130212.fc18.noarch",
+                new RpmVersion("ovirt-node-iso-2.6.0-20130212.fc18.noarch").getRpmRelease());
+        assertEquals("1.1.i386", new RpmVersion("test-javadb-common-10.2.2.2.2-1.1.i386").getRpmRelease());
+        assertEquals("2.3.10.4.fc18.x86_64",
+                new RpmVersion("java-1.7.0-openjdk-devel-1.7.0.25-2.3.10.4.fc18.x86_64").getRpmRelease());
+        assertEquals("1.el6", new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", true).getRpmRelease());
+        assertEquals("1.el6", new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "rhev-agent", false).getRpmRelease());
     }
 
     @Test
@@ -56,21 +55,21 @@ public class RpmVersionTest {
         RpmVersion rpm1 = new RpmVersion("test-javadb-common-10.2.2.2.2-1.1.i386");
         RpmVersion rpm2 = new RpmVersion("test-javadb-common-10.2.2.2.2-1.1.i386");
         RpmVersion rpm3 = new RpmVersion("test-javadb-common-10.2.2.2.3-1.1.i386");
-        assertTrue(rpm1.equals(rpm2));
-        assertTrue(rpm2.equals(rpm1));
-        assertFalse(rpm2.equals(rpm3));
-        assertFalse(rpm3.equals(rpm2));
+        assertEquals(rpm1, rpm2);
+        assertEquals(rpm2, rpm1);
+        assertNotEquals(rpm2, rpm3);
+        assertNotEquals(rpm3, rpm2);
 
         RpmVersion version1 = new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "RHEV-Agent", false);
         RpmVersion version2 = new RpmVersion("rhev-agent-2.3.7.10.3-2.el6", "RHEV-Agent", false);
-        assertFalse(version1.equals(version2));
+        assertNotEquals(version1, version2);
 
         RpmVersion version3 = new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", "", false);
         RpmVersion version4 = new RpmVersion("rhev-agent-2.3.7.10.3-1.el6", null, false);
-        assertTrue(version3.equals(version4));
+        assertEquals(version3, version4);
 
         RpmVersion version5 = new RpmVersion(null, null, false);
         RpmVersion version6 = new RpmVersion(null, null, false);
-        assertTrue(version5.equals(version6));
+        assertEquals(version5, version6);
     }
 }
