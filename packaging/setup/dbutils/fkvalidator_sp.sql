0c88caa993887eae9828f86ecd6847c08a54ca5c
Ondra Machacek <omachace@redhat.com>
2015-08-20 16:51:48 +0200

    packaging: dbutils: add schema names to tables in fkvalidator
    This change is needed since aaa-jdbc using different schema names
    for the aaa-jdbc users/group
    
    Change-Id: I69a6ef8429abb99aeed4ab47bd40d049b9fa457a
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1254639
    Signed-off-by: Ondra Machacek <omachace@redhat.com>
    (cherry picked from commit 030fddf56b14d3882e48b1bda3677af8fc9067f5)

--- packaging/setup/dbutils/fkvalidator_sp.sql
+++ packaging/setup/dbutils/fkvalidator_sp.sql
@@ -4,7 +4,7 @@ SET client_min_messages=ERROR;
 DROP TYPE IF EXISTS fk_info_rs CASCADE;
 DROP FUNCTION IF EXISTS fn_db_validate_fks(boolean,boolean);
 CREATE TYPE fk_info_rs AS
-    (table_name varchar, table_col varchar, fk_table_name varchar, fk_col varchar, fk_violation varchar, fk_status integer);
+    (schema_name varchar, table_name varchar, table_col varchar, fk_table_name varchar, fk_col varchar, fk_violation varchar, fk_status integer);
 CREATE OR REPLACE FUNCTION fn_db_validate_fks(v_fix_it boolean, v_verbose boolean)
 returns SETOF fk_info_rs
 AS $procedure$
@@ -16,16 +16,18 @@ DECLARE
     v_record fk_info_rs%ROWTYPE;
     v_cur CURSOR FOR
     SELECT
+        n.nspname as schema_name,
         c.relname as table_name,
         substring(substring ((select pg_get_constraintdef(r.oid)) from '[a-zA-Z0-9_, \-][(][a-zA-Z0-9_, \-]+[)]') from 2) as table_col,
         c2.relname AS fk_table_name,
         substring ((select pg_get_constraintdef(r.oid)) from ' [(][a-zA-Z0-9_, \-]+[)] ') as fk_col
-    FROM pg_class c, pg_class c2, pg_constraint r
+    FROM pg_class c, pg_class c2, pg_constraint r, pg_catalog.pg_namespace n
     WHERE c.relname in (select table_name from information_schema.tables
     where table_schema not in ('pg_catalog','information_schema') and table_type = 'BASE TABLE') AND
         r.confrelid = c.oid AND
         r.contype = 'f' AND
         c2.oid = r.conrelid AND
+        n.oid = c.relnamespace AND
         pg_get_constraintdef(r.oid) not ilike '%ON DELETE SET %'
     ORDER BY  table_name;
 
@@ -37,14 +39,14 @@ BEGIN
         v_record.fk_violation := '';
         v_record.fk_status := 0;
         IF (v_fix_it) THEN
-            v_sql := 'delete from ' || v_record.fk_table_name ||
+            v_sql := 'delete from ' || v_record.schema_name || '.'  || v_record.fk_table_name ||
                       ' where ' || v_record.fk_col || 'IS NOT NULL and '  || v_record.fk_col || ' not in (select ' ||
-                      trim(both '()' from v_record.table_col) || ' from ' || v_record.table_name || ');';
+                      trim(both '()' from v_record.table_col) || ' from ' || v_record.schema_name || '.'  || v_record.table_name || ');';
             v_msg := 'Fixing violation/s found in ' ||  v_record.fk_table_name ;
         ELSE
-            v_sql := 'select ' || v_record.fk_col || ' from ' || v_record.fk_table_name ||
+            v_sql := 'select ' ||  v_record.fk_col || ' from ' || v_record.schema_name || '.'  || v_record.fk_table_name ||
                       ' where ' || v_record.fk_col || 'IS NOT NULL and ' || v_record.fk_col || ' not in (select ' ||
-                      trim(both '()' from v_record.table_col) || ' from ' || v_record.table_name || ');';
+                      trim(both '()' from v_record.table_col) || ' from ' || v_record.schema_name || '.'  || v_record.table_name || ');';
             v_msg := 'Constraint violation found in  ' ||  v_record.fk_table_name || v_record.fk_col;
         END IF;
         EXECUTE v_sql;
