8dd6866326cff043bec6d94bb8d7da3fe336ebb1
Tomas Jelinek <tjelinek@redhat.com>
2015-08-25 09:44:43 +0200

    webadmin: fix VM dialog for German translation
    - Changed the the SerialNumberPolicy to use the standard way of showing the question mark
    - made the width of the dialog bigger
    
    Change-Id: I311377b5befc4ca4bbe89381bb9ce4472f19de2d
    Bug-Url: https://bugzilla.redhat.com/1255678
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/vm/SerialNumberPolicyWidget.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/popup/vm/SerialNumberPolicyWidget.java
@@ -1,16 +1,18 @@
 package org.ovirt.engine.ui.common.widget.uicommon.popup.vm;
 
 import org.ovirt.engine.core.common.businessentities.SerialNumberPolicy;
+import org.ovirt.engine.ui.common.CommonApplicationConstants;
 import org.ovirt.engine.ui.common.CommonApplicationMessages;
 import org.ovirt.engine.ui.common.CommonApplicationTemplates;
 import org.ovirt.engine.ui.common.gin.AssetProvider;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
-import org.ovirt.engine.ui.common.widget.Align;
+import org.ovirt.engine.ui.common.idhandler.WithElementId;
+import org.ovirt.engine.ui.common.widget.EntityModelWidgetWithInfo;
 import org.ovirt.engine.ui.common.widget.VisibilityRenderer;
-import org.ovirt.engine.ui.common.widget.dialog.InfoIcon;
 import org.ovirt.engine.ui.common.widget.editor.EnumRadioEditor;
-import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelCheckBoxEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.EntityModelCheckBoxOnlyEditor;
 import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextBoxOnlyEditor;
+import org.ovirt.engine.ui.common.widget.label.EnableableFormLabel;
 import org.ovirt.engine.ui.common.widget.uicommon.popup.AbstractModelBoundPopupWidget;
 import org.ovirt.engine.ui.uicommonweb.models.vms.SerialNumberPolicyModel;
 import com.google.gwt.core.client.GWT;
@@ -45,16 +47,17 @@ public class SerialNumberPolicyWidget extends AbstractModelBoundPopupWidget<Seri
     @UiField
     public Style style;
 
-    @UiField(provided = true)
     @Path("overrideSerialNumberPolicy.entity")
-    public EntityModelCheckBoxEditor overrideSerialNumberPolicy;
-
-    @UiField(provided = true)
-    public InfoIcon serialNumberInfoIcon;
+    @WithElementId
+    public EntityModelCheckBoxOnlyEditor overrideSerialNumberPolicy;
 
     @UiField(provided = true)
     @Path("serialNumberPolicy.entity")
     public EnumRadioEditor<SerialNumberPolicy> serialNumberPolicy;
+
+    @UiField(provided = true)
+    @Ignore
+    public EntityModelWidgetWithInfo overrideSerialNumberPolicyWithInfo;
 
     @UiField
     @Path("customSerialNumber.entity")
@@ -62,12 +65,18 @@ public class SerialNumberPolicyWidget extends AbstractModelBoundPopupWidget<Seri
 
     private final static CommonApplicationTemplates templates = AssetProvider.getTemplates();
     private final static CommonApplicationMessages messages = AssetProvider.getMessages();
+    private final static CommonApplicationConstants constants = AssetProvider.getConstants();
 
     @Inject
     public SerialNumberPolicyWidget() {
-        overrideSerialNumberPolicy = new EntityModelCheckBoxEditor(Align.RIGHT);
+        overrideSerialNumberPolicy = new EntityModelCheckBoxOnlyEditor();
+        EnableableFormLabel label = new EnableableFormLabel();
+        label.setText(constants.overrideSerialNumberPolicy());
+        overrideSerialNumberPolicyWithInfo = new EntityModelWidgetWithInfo(label, overrideSerialNumberPolicy);
+        overrideSerialNumberPolicyWithInfo.setExplanation(templates.italicText(messages.serialNumberInfo()));
+
         serialNumberPolicy = new EnumRadioEditor<>(SerialNumberPolicy.class);
-        serialNumberInfoIcon = new InfoIcon(templates.italicText(messages.serialNumberInfo()));
+
 
         initWidget(ViewUiBinder.uiBinder.createAndBindUi(this));
         driver.initialize(this);
