41a5e30db08ed0431323fae6fb1a9e128f1d4c4f
Idan Shaby <ishaby@redhat.com>
2015-09-06 16:22:55 +0300

    core: implement MetadataDiskDescriptionHandler as a singleton
    This patch is re-implementing MetadataDiskDescriptionHandler to be a
    singleton so that it would be easier to test it.
    
    Change-Id: I2832b4803d9a2b96a0e63973e0571c1b46892d3e
    Related-To: https://bugzilla.redhat.com/1249130
    Related-To: https://bugzilla.redhat.com/1219680
    Signed-off-by: Idan Shaby <ishaby@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateVmDiskCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateVmDiskCommand.java
@@ -488,7 +488,7 @@ public class UpdateVmDiskCommand<T extends UpdateVmDiskParameters> extends Abstr
     }
 
     private String getJsonDiskDescription() throws IOException {
-        return MetadataDiskDescriptionHandler.getJsonDiskDescription(getParameters().getDiskInfo());
+        return MetadataDiskDescriptionHandler.getInstance().getJsonDiskDescription(getParameters().getDiskInfo());
     }
 
     protected void updateDiskProfile() {
