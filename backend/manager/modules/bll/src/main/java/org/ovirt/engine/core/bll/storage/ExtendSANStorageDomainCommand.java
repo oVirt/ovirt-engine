4cecf1971931b91621a5cc7465a4a93d598bef23
Fred Rolland <frolland@redhat.com>
2015-09-13 11:27:54 +0300

    engine: Add lock on Extend SD command
    In order to prevent to run the Extend Storage Domain command twice at
    the same time, an exclusive lock is added to the command.
    
    Running this command twice will put the VG in a partial state, creating
    a PV twice on the same device.
    
    Change-Id: Ib9062ba235daa1d25298f1d295b17d15e1528c80
    Backport-To: 3.6
    Bug-Url: https://bugzilla.redhat.com/1265906
    Signed-off-by: Fred Rolland <frolland@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/ExtendSANStorageDomainCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/ExtendSANStorageDomainCommand.java
@@ -1,11 +1,15 @@
 package org.ovirt.engine.core.bll.storage;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 
+import org.ovirt.engine.core.bll.LockMessagesMatchUtil;
 import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
 import org.ovirt.engine.core.bll.storage.ConnectAllHostsToLunCommand.ConnectAllHostsToLunCommandReturnValue;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.action.ExtendSANStorageDomainParameters;
+import org.ovirt.engine.core.common.action.LockProperties;
 import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.businessentities.LUNs;
 import org.ovirt.engine.core.common.businessentities.StorageDomainStatus;
@@ -13,6 +17,8 @@ import org.ovirt.engine.core.common.businessentities.StorageType;
 import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.errors.VdcBllMessages;
+import org.ovirt.engine.core.common.locks.LockingGroup;
+import org.ovirt.engine.core.common.utils.Pair;
 import org.ovirt.engine.core.common.vdscommands.ExtendStorageDomainVDSCommandParameters;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.compat.Guid;
@@ -108,5 +114,17 @@ public class ExtendSANStorageDomainCommand<T extends ExtendSANStorageDomainParam
     public AuditLogType getAuditLogTypeValue() {
         return getSucceeded() ? AuditLogType.USER_EXTENDED_STORAGE_DOMAIN
                 : AuditLogType.USER_EXTENDED_STORAGE_DOMAIN_FAILED;
+    }
+
+    @Override
+    protected Map<String, Pair<String, String>> getExclusiveLocks() {
+        return Collections.singletonMap(getParameters().getStorageDomainId().toString(),
+                LockMessagesMatchUtil.makeLockingPair(LockingGroup.STORAGE,
+                        VdcBllMessages.ACTION_TYPE_FAILED_OBJECT_LOCKED));
+    }
+
+    @Override
+    protected LockProperties applyLockProperties(LockProperties lockProperties) {
+        return lockProperties.withScope(LockProperties.Scope.Execution);
     }
 }
