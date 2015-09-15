a040070800d58864aefe1d886f0aefb5bc4837b4
Jakub Niedermertl <jniederm@redhat.com>
2015-09-15 19:22:06 +0200

    core: Permission based icon filtering: database part
    I allows to non-admin user to list icons using REST. Returned icons are
    union of predefined icons and icons that are associated with some entity
    the user has permission to (vm or tempate).
    
    Change-Id: I53d8cf17f91b67ae53ab90d7e929f0cf2a9810fb
    Bug-Url: https://bugzilla.redhat.com/1263758
    Signed-off-by: Jakub Niedermertl <jniederm@redhat.com>

--- packaging/dbscripts/vm_icons_sp.sql
+++ packaging/dbscripts/vm_icons_sp.sql
@@ -19,6 +19,41 @@ BEGIN
 END; $procedure$
 LANGUAGE plpgsql;
 
+Create or replace FUNCTION GetAllFromVmIconsFiltered(v_user_id UUID, v_is_filtered boolean) RETURNS SETOF vm_icons STABLE
+AS $procedure$
+BEGIN
+    IF v_is_filtered THEN
+        RETURN QUERY
+            -- icons associated with owned vms and templates
+            SELECT DISTINCT vm_icons.*
+            FROM
+                (SELECT * FROM user_vm_template_permissions_view
+                 UNION
+                 SELECT * FROM user_vm_permissions_view)  AS permissions,
+                vm_static,
+                vm_icons
+            WHERE
+                permissions.entity_id = vm_static.vm_guid
+                AND vm_static.entity_type IN ('TEMPLATE', 'VM')
+                AND (vm_icons.id = vm_static.small_icon_id OR vm_icons.id = vm_static.large_icon_id)
+                AND permissions.user_id = v_user_id
+        UNION
+            -- predefined icons
+            SELECT vm_icons.*
+            FROM
+                vm_icons,
+                vm_icon_defaults
+            WHERE
+                vm_icon_defaults.small_icon_id = vm_icons.id
+                OR vm_icon_defaults.large_icon_id = vm_icons.id;
+    ELSE
+        RETURN QUERY
+            SELECT *
+            FROM vm_icons;
+    END IF;
+END; $procedure$
+LANGUAGE plpgsql;
+
 Create or replace FUNCTION InsertVmIcon(
     v_id UUID,
     v_data_url TEXT)
