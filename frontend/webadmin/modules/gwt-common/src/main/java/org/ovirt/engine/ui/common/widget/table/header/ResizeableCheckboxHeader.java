64a8f9686460b3cc6cfee020857220a0a4e213c5
Alexander Wels <awels@redhat.com>
2015-09-17 15:40:57 -0400

    webadmin: check box header
    - The ResizeableCheckboxHeader was not propagating the
      'change' event handling causing some header check boxes
      to not react to changes. This patch fixes the issue.
      Unfortunately I have to 'polute' the ResizableHeader to
      handle change as constructor is the only place that allows
      one to pass in the checkbox that handles the event. This
      means that the method that creates the checkbox has to be
      static, and you can't override a static method.
    
    Change-Id: Ib302582c57055705c7348112d99b49f5ec6a8841
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1257168
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/header/ResizeableCheckboxHeader.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/header/ResizeableCheckboxHeader.java
@@ -1,12 +1,7 @@
 package org.ovirt.engine.ui.common.widget.table.header;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.ovirt.engine.ui.common.widget.table.cell.SafeHtmlCell;
 import org.ovirt.engine.ui.common.widget.table.resize.HasResizableColumns;
 import com.google.gwt.cell.client.Cell.Context;
-import com.google.gwt.dom.client.BrowserEvents;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
@@ -22,19 +17,6 @@ public class ResizeableCheckboxHeader<T> extends ResizableHeader<T> {
         super(new SafeHtmlHeader(SafeHtmlUtils.fromSafeConstant(""), checkboxHeader.getTooltip(), //$NON-NLS-1$
                 createSafeHtmlCell()), column, table, true);
         this.checkboxHeaderDelegate = checkboxHeader;
-    }
-
-    public static SafeHtmlCell createSafeHtmlCell() {
-        return new SafeHtmlCell() {
-            @Override
-            public Set<String> getConsumedEvents() {
-                Set<String> set = new HashSet<String>(super.getConsumedEvents());
-                set.add(BrowserEvents.CLICK); // for sorting
-                set.add(BrowserEvents.MOUSEMOVE); // for changing mouse cursor
-                set.add(BrowserEvents.CHANGE); // for checkbox toggle
-                return set;
-            }
-        };
     }
 
     @Override
