4bb0a2e49c4dc5a1f90bac15340b6ba4dbf99f12
Allon Mureinik <amureini@redhat.com>
2015-09-20 18:00:22 +0300

    core: ConnectAllHostsToLunCommand#validateConnectedLuns
    Replaced fully qualified @throws definition with the simple class name,
    as EngineException is already imported.
    
    Change-Id: I5d31af8409f801b67bf5ddace925619b266396c9
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/ConnectAllHostsToLunCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/ConnectAllHostsToLunCommand.java
@@ -182,7 +182,7 @@ public class ConnectAllHostsToLunCommand<T extends ExtendSANStorageDomainParamet
      *            - luns ids which we wants to check
      * @return - true if all luns are connected to the host, false otherwise
      *
-     * @throws org.ovirt.engine.core.common.errors.EngineException
+     * @throws EngineException
      */
     private boolean validateConnectedLuns(VDS vds, List<String> processedLunIds) {
         Map<String, Boolean> res;
