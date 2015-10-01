448153eba14e136c7e78eb36d96a3ecc5d83763d
David Caro <dcaroest@redhat.com>
2015-10-01 10:22:25 +0200

    Not archiving the rpm for check* stages
    We already have the build-artifacts stage that is being archived and
    published, archiving the check-patch and check-merged take too much
    space on the ci system
    
    Change-Id: Ia7a36a46f1a43e4c80fa4390ff0fc8c4fe20f906
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -81,5 +81,3 @@ rpmbuild \
 # Store any relevant artifacts in exported-artifacts for the ci system to
 # archive
 [[ -d exported-artifacts ]] || mkdir -p exported-artifacts
-find output -iname \*rpm -exec mv "{}" exported-artifacts/ \;
-mv ./*tar.gz exported-artifacts/
