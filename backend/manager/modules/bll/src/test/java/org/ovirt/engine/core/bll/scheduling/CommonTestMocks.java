2fe2fb5550c670b5064c169de0003d4d34512b9b
Roman Mohr <rmohr@redhat.com>
2015-10-09 13:21:15 +0200

    Initial Integration test with arquillian and working automatic rollback
     * Bump of maven-surefire-plugin to 2.12.3 to make groups/categories work
     * Tests annotated with @Category(IntegrationTest.class) will not be run
       when running unit tests
     * Integration tests which inherit from TransactionalTestBase have
       access to a fully working dao layer
     * Add maven-failsafe-plugin for running integration tests
    
    To run integration tests type:
    
       mvn clean verify -DskipITs=false
    
    Note that 'skipITs' is set to false for backward compitibility only
    (e.g. Jenkins jobs). A standard maven execution should execute all
    tests which are there. Tests should normally only be skipped when it is
    an explicit decision.
    
    Change-Id: I8f87fb5ad35b1d411fc114e3296b2a3d07ecae3d
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/CommonTestMocks.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/CommonTestMocks.java
@@ -5,55 +5,28 @@ import static org.mockito.Mockito.mock;
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
 
-import org.ovirt.engine.core.bll.CpuFlagsManagerHandler;
 import org.ovirt.engine.core.bll.aaa.SessionDataContainer;
 import org.ovirt.engine.core.bll.aaa.SsoSessionUtils;
 import org.ovirt.engine.core.bll.hostdev.HostDeviceManager;
-import org.ovirt.engine.core.bll.interfaces.BackendInternal;
 import org.ovirt.engine.core.bll.quota.QuotaManager;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
-import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogDirector;
-import org.ovirt.engine.core.dao.ClusterDao;
-import org.ovirt.engine.core.dao.EngineSessionDao;
+import org.ovirt.engine.core.dal.dbbroker.DbFacadeLocator;
 import org.ovirt.engine.core.dao.JobDao;
-import org.ovirt.engine.core.dao.LabelDao;
-import org.ovirt.engine.core.dao.VdsDynamicDao;
-import org.ovirt.engine.core.dao.scheduling.PolicyUnitDao;
 import org.ovirt.engine.core.di.Injector;
-import org.ovirt.engine.core.vdsbroker.ResourceManager;
 
 @Singleton
 public class CommonTestMocks {
 
     // providers of common dependencies
     @Produces
-    private BackendInternal backendInternal = mock(BackendInternal.class);
-    @Produces
-    private DbFacade dbFacade = mock(DbFacade.class);
-    @Produces
-    private AuditLogDirector auditLogDirector = mock(AuditLogDirector.class);
-    @Produces
-    private ResourceManager resourceManager = mock(ResourceManager.class);
-    @Produces
-    private PolicyUnitDao policyUnitDao = mock(PolicyUnitDao.class);
+    private DbFacadeLocator dbFacadeLocator = mock(DbFacadeLocator.class);
     @Produces
     private QuotaManager quotaManager = mock(QuotaManager.class);
     @Produces
-    private EngineSessionDao engineSessionDao = mock(EngineSessionDao.class);
-    @Produces
-    private CpuFlagsManagerHandler cpuFlagsManagerHandler = mock(CpuFlagsManagerHandler.class);
-    @Produces
-    private ClusterDao clusterDao = mock(ClusterDao.class);
-    @Produces
     private HostDeviceManager hostDeviceManager = mock(HostDeviceManager.class);
-    @Produces
-    private VdsDynamicDao vdsDynamicDao = mock(VdsDynamicDao.class);
-    @Produces
-    private SsoSessionUtils ssoSessionUtils = mock(SsoSessionUtils.class);
     @Produces
     private JobDao jobDao = mock(JobDao.class);
     @Produces
-    private LabelDao labelDao = mock(LabelDao.class);
+    SsoSessionUtils sessionUtils = mock(SsoSessionUtils.class);
 
     public static Class<?>[] commonClasses() {
         return new Class<?>[] {
--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/SchedulingManagerTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/SchedulingManagerTest.java
@@ -1,95 +1,82 @@
 package org.ovirt.engine.core.bll.scheduling;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.ovirt.engine.core.common.utils.MockConfigRule.mockConfig;
 
-import java.util.Collections;
+import java.util.HashSet;
 
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ArchivePaths;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.experimental.categories.Category;
 import org.mockito.Spy;
+import org.ovirt.engine.arquillian.IntegrationTest;
+import org.ovirt.engine.arquillian.TransactionalTestBase;
+import org.ovirt.engine.core.bll.CpuFlagsManagerHandler;
+import org.ovirt.engine.core.bll.interfaces.BackendInternal;
 import org.ovirt.engine.core.bll.network.host.NetworkDeviceHelper;
 import org.ovirt.engine.core.bll.scheduling.external.ExternalSchedulerDiscovery;
 import org.ovirt.engine.core.common.config.ConfigValues;
-import org.ovirt.engine.core.common.scheduling.ClusterPolicy;
-import org.ovirt.engine.core.common.scheduling.PolicyUnit;
 import org.ovirt.engine.core.common.utils.MockConfigRule;
+import org.ovirt.engine.core.compat.Version;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
-import org.ovirt.engine.core.dao.scheduling.ClusterPolicyDao;
-import org.ovirt.engine.core.dao.scheduling.PolicyUnitDao;
-import org.ovirt.engine.core.di.Injector;
+import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogDirector;
+import org.ovirt.engine.core.vdsbroker.ResourceManager;
 
-@RunWith(Arquillian.class)
-public class SchedulingManagerTest {
+@Category(IntegrationTest.class)
+public class SchedulingManagerTest extends TransactionalTestBase {
 
     @Rule
     public MockConfigRule mockConfigRule = new MockConfigRule(
-            MockConfigRule.mockConfig(ConfigValues.ExternalSchedulerEnabled, false),
-            MockConfigRule.mockConfig(ConfigValues.EnableVdsLoadBalancing, false),
-            MockConfigRule.mockConfig(ConfigValues.MaxSchedulerWeight, Integer.MAX_VALUE),
-            MockConfigRule.mockConfig(ConfigValues.SpmVmGraceForEvenGuestDistribute, 10),
-            MockConfigRule.mockConfig(ConfigValues.MigrationThresholdForEvenGuestDistribute, 5),
-            MockConfigRule.mockConfig(ConfigValues.HighVmCountForEvenGuestDistribute, 10)
-            );
+            mockConfig(ConfigValues.ExternalSchedulerEnabled, false),
+            mockConfig(ConfigValues.EnableVdsLoadBalancing, false),
+            mockConfig(ConfigValues.MaxSchedulerWeight, 381),
+            mockConfig(ConfigValues.SupportedClusterLevels, new HashSet<Version>()),
+            mockConfig(ConfigValues.SpmVmGraceForEvenGuestDistribute, 5),
+            mockConfig(ConfigValues.MigrationThresholdForEvenGuestDistribute, 5),
+            mockConfig(ConfigValues.HighVmCountForEvenGuestDistribute, 5)
+    );
 
     @Inject @Spy
     private Instance<SchedulingManager> schedulingManager;
-
     @Inject
     private DbFacade dbFacade;
-    @Inject
-    private Injector injector;
 
-    @Produces
-    private NetworkDeviceHelper networkDeviceHelper = mock(NetworkDeviceHelper.class);
-
-    @Before
-    public void initTest() {
-        PolicyUnitDao policyUnitDao = mock(PolicyUnitDao.class);
-        ClusterPolicyDao clusterPolicyDao = mock(ClusterPolicyDao.class);
-        when(dbFacade.getPolicyUnitDao()).thenReturn(policyUnitDao);
-        when(dbFacade.getClusterPolicyDao()).thenReturn(clusterPolicyDao);
-        when(policyUnitDao.getAll()).thenReturn(Collections.<PolicyUnit> emptyList());
-        when(clusterPolicyDao.getAll(any())).thenReturn(Collections.<ClusterPolicy> emptyList());
-    }
-
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClasses(
-                        Injector.class,
-                        CommonTestMocks.class,
-                        SchedulingManager.class,
-                        ExternalSchedulerDiscovery.class,
-                        BasicMigrationHandler.class
-                        )
-                .addClasses(
-                        CommonTestMocks.commonClasses()
-                )
-                .addAsManifestResource(
-                        EmptyAsset.INSTANCE,
-                        ArchivePaths.create("beans.xml")
-                );
+    @Deployment(name = "SchedulingManagerTest")
+    public static JavaArchive deploy() {
+        Class<?>[] classes = {
+                SchedulingManager.class,
+                Mocks.class,
+                AuditLogDirector.class,
+                MigrationHandler.class,
+                BasicMigrationHandler.class,
+                CpuFlagsManagerHandler.class,
+                ExternalSchedulerDiscovery.class
+        };
+        return createDeployment(classes);
     }
 
     @Test
     public void testSchedule() throws Exception {
         assertNotNull(schedulingManager.get());
         verify(schedulingManager.get()).init();
+    }
+
+    @Singleton
+    private static class Mocks {
+        @Produces @Singleton
+        private ResourceManager resourceManager = mock(ResourceManager.class);
+        @Produces @Singleton
+        private BackendInternal backendInternal = mock(BackendInternal.class);
+        @Produces @Singleton
+        private NetworkDeviceHelper networkDeviceHelper = mock(NetworkDeviceHelper.class);
     }
 }
