397702f705afa53ad6307bb3de7b4df7e7b658f4
Allon Mureinik <amureini@redhat.com>
2015-10-13 11:54:15 +0300

    core: StorageDomain parentheses
    Removed redundant parentheses that just hinder readability.
    
    Change-Id: I7645b0ca5d9d8cae26c545d85e13409f70801fd6
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/StorageDomain.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/StorageDomain.java
@@ -355,14 +355,14 @@ public class StorageDomain implements IVdcQueryable, BusinessEntityWithStatus<Gu
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + (getId() == null ? 0 : getId().hashCode());
         result = prime * result + committedDiskSize;
-        result = prime * result + ((dynamicData == null) ? 0 : dynamicData.hashCode());
-        result = prime * result + ((staticData == null) ? 0 : staticData.hashCode());
-        result = prime * result + ((storageDomainSharedStatus == null) ? 0 : storageDomainSharedStatus.hashCode());
+        result = prime * result + (dynamicData == null ? 0 : dynamicData.hashCode());
+        result = prime * result + (staticData == null ? 0 : staticData.hashCode());
+        result = prime * result + (storageDomainSharedStatus == null ? 0 : storageDomainSharedStatus.hashCode());
         result = prime * result + storageDomainOverCommitPercent;
-        result = prime * result + ((storagePoolIsoMapData == null) ? 0 : storagePoolIsoMapData.hashCode());
-        result = prime * result + ((totalDiskSize == null) ? 0 : totalDiskSize.hashCode());
+        result = prime * result + (storagePoolIsoMapData == null ? 0 : storagePoolIsoMapData.hashCode());
+        result = prime * result + (totalDiskSize == null ? 0 : totalDiskSize.hashCode());
         return result;
     }
 
@@ -378,11 +378,11 @@ public class StorageDomain implements IVdcQueryable, BusinessEntityWithStatus<Gu
             return false;
         }
         StorageDomain other = (StorageDomain) obj;
-        return (ObjectUtils.objectsEqual(getId(), other.getId())
+        return ObjectUtils.objectsEqual(getId(), other.getId())
                 && committedDiskSize == other.committedDiskSize
                 && storageDomainSharedStatus == other.storageDomainSharedStatus
                 && storageDomainOverCommitPercent == other.storageDomainOverCommitPercent
-                && ObjectUtils.objectsEqual(totalDiskSize, other.totalDiskSize));
+                && ObjectUtils.objectsEqual(totalDiskSize, other.totalDiskSize);
     }
 
     @Override
