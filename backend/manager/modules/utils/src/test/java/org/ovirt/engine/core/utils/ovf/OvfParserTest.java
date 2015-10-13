5613beb6880815fe48b8cb829954c4f47d7bb24a
Allon Mureinik <amureini@redhat.com>
2015-10-13 13:39:30 +0300

    core: OvfParserTest formatting
    Removed useless line break in the test.
    
    Change-Id: I69dfe2a4fbe659db2d4343fec2de8568fb377e58
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/utils/src/test/java/org/ovirt/engine/core/utils/ovf/OvfParserTest.java
+++ backend/manager/modules/utils/src/test/java/org/ovirt/engine/core/utils/ovf/OvfParserTest.java
@@ -9,8 +9,7 @@ public class OvfParserTest {
 
     @Test
     public void UtcDateStringToLocaDate_nodep() {
-        Date date =
-                OvfParser.UtcDateStringToLocaDate("1984/06/19 14:25:11");
+        Date date = OvfParser.UtcDateStringToLocaDate("1984/06/19 14:25:11");
         Assert.assertEquals(456503111000L, date.getTime());
     }
 }
