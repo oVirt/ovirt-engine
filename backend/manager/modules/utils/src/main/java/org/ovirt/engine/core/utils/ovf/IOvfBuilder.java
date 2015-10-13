836b2717216e681656880f0228fa134544f29078
Allon Mureinik <amureini@redhat.com>
2015-10-13 15:22:30 +0300

    core: IOvfBuilder constants' modifiers
    Change-Id: I16c2e2df3b9fd894db2fcbe33d990a511fe58722
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/IOvfBuilder.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/IOvfBuilder.java
@@ -2,16 +2,16 @@ package org.ovirt.engine.core.utils.ovf;
 
 public interface IOvfBuilder {
     // Namespace URIs:
-    public final String OVF_URI = "http://schemas.dmtf.org/ovf/envelope/1/";
-    public final String RASD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData";
-    public final String VSSD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData";
-    public final String XSI_URI = "http://www.w3.org/2001/XMLSchema-instance";
+    public static final String OVF_URI = "http://schemas.dmtf.org/ovf/envelope/1/";
+    public static final String RASD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData";
+    public static final String VSSD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData";
+    public static final String XSI_URI = "http://www.w3.org/2001/XMLSchema-instance";
 
     // Namespace prefixes:
-    public final String OVF_PREFIX = "ovf";
-    public final String RASD_PREFIX = "rasd";
-    public final String VSSD_PREFIX = "vssd";
-    public final String XSI_PREFIX = "xsi";
+    public static final String OVF_PREFIX = "ovf";
+    public static final String RASD_PREFIX = "rasd";
+    public static final String VSSD_PREFIX = "vssd";
+    public static final String XSI_PREFIX = "xsi";
 
     void buildReference();
 
