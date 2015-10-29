d8ec15e955aa75457b183ee1becbe34d80b05252
Eliraz Levi <elevi@redhat.com>
2015-10-29 16:10:48 +0200

    engine: Inroduce DetachNetworkUsedByVmValidator
    The Validator is creating a validation result
    according to the given numbers of vms and
    networks.
    
    Change-Id: I90fab7507525cba000f7e2af4d82c45845ebbaa7
    Bug-Url: https://bugzilla.redhat.com/1216896
    Signed-off-by: Eliraz Levi <elevi@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/validator/network/DetachNetworkUsedByVmValidator.java
@@ -0,0 +1,52 @@
+package org.ovirt.engine.core.bll.validator.network;
+
+import java.util.List;
+
+import org.ovirt.engine.core.bll.ValidationResult;
+import org.ovirt.engine.core.common.errors.EngineMessage;
+import org.ovirt.engine.core.utils.ReplacementUtils;
+import org.ovirt.engine.core.utils.linq.LinqUtils;
+
+public class DetachNetworkUsedByVmValidator {
+
+    static final String VAR_NETWORK_NAME = "networkName";
+    static final String VAR_NETWORK_NAMES = "networkNames";
+    static final String VAR_VM_NAME = "vmName";
+    static final String VAR_VM_NAMES = "vmNames";
+
+    private final List<String> vmNamesList;
+    private final List<String> removedNetworks;
+
+    public DetachNetworkUsedByVmValidator(List<String> vmNamesList, List<String> removedNetworks) {
+        this.vmNamesList = vmNamesList;
+        this.removedNetworks = removedNetworks;
+    }
+
+    public ValidationResult validate() {
+        if (vmNamesList.isEmpty()) {
+            return ValidationResult.VALID;
+        }
+        final int vmsAmount = vmNamesList.size();
+        final int removedNetworksAmount = removedNetworks.size();
+        if (vmsAmount > 1 && removedNetworksAmount > 1) {
+            return new ValidationResult(EngineMessage.MULTIPLE_NETWORKS_CANNOT_DETACH_NETWORKS_USED_BY_VMS,
+                    LinqUtils.concat(ReplacementUtils.replaceWith(VAR_NETWORK_NAMES, removedNetworks),
+                            ReplacementUtils.replaceWith(VAR_VM_NAMES, vmNamesList)));
+        } else if (vmsAmount > 1) {
+            return new ValidationResult(EngineMessage.NETWORK_CANNOT_DETACH_NETWORK_USED_BY_VMS,
+                    LinqUtils.concat(
+                            ReplacementUtils.replaceWith(VAR_NETWORK_NAME, removedNetworks),
+                            ReplacementUtils.replaceWith(VAR_VM_NAMES, vmNamesList)));
+        } else if (removedNetworksAmount > 1) {
+            return new ValidationResult(EngineMessage.MULTIPLE_NETWORKS_CANNOT_DETACH_NETWORKS_USED_BY_SINGLE_VM,
+                    LinqUtils.concat(
+                            ReplacementUtils.replaceWith(VAR_NETWORK_NAMES, removedNetworks),
+                            ReplacementUtils.replaceWith(VAR_VM_NAME, vmNamesList)));
+        } else {
+            return new ValidationResult(EngineMessage.NETWORK_CANNOT_DETACH_NETWORK_USED_BY_SINGLE_VM,
+                    LinqUtils.concat(
+                            ReplacementUtils.replaceWith(VAR_NETWORK_NAME, removedNetworks),
+                            ReplacementUtils.replaceWith(VAR_VM_NAME, vmNamesList)));
+        }
+    }
+}
---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/validator/network/DetachNetworkUsedByVmValidatorTest.java
@@ -0,0 +1,64 @@
+package org.ovirt.engine.core.bll.validator.network;
+
+import static org.junit.Assert.assertThat;
+import static org.ovirt.engine.core.bll.validator.ValidationResultMatchers.failsWith;
+import static org.ovirt.engine.core.utils.ReplacementUtils.replaceWith;
+import static org.ovirt.engine.core.utils.linq.LinqUtils.concat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.ovirt.engine.core.common.errors.EngineMessage;
+
+public class DetachNetworkUsedByVmValidatorTest {
+    private static final String NETWORK_A = "networkA";
+    private static final String NETWORK_B = "networkB";
+    private static final String VM_A = "vmA";
+    private static final String VM_B = "vmB";
+    private DetachNetworkUsedByVmValidator underTest;
+
+    @Test
+    public void testValidateNotRemovingUsedNetworkByVmsSingleNetworkMultipleVms() {
+        final List<String> vmsNames = Arrays.asList(VM_A, VM_B);
+        final List<String> removedNetworks = Arrays.asList(NETWORK_A);
+        underTest = new DetachNetworkUsedByVmValidator(vmsNames, removedNetworks);
+        assertThat(underTest.validate(),
+                failsWith(EngineMessage.NETWORK_CANNOT_DETACH_NETWORK_USED_BY_VMS,
+                        concat(replaceWith(DetachNetworkUsedByVmValidator.VAR_VM_NAMES, vmsNames),
+                                replaceWith(DetachNetworkUsedByVmValidator.VAR_NETWORK_NAME, removedNetworks))));
+    }
+
+    @Test
+    public void testValidateNotRemovingUsedNetworkByVmsSingleNetworkSingleVm() {
+        final List<String> vmsNames = Arrays.asList(VM_A);
+        final List<String> removedNetworks = Arrays.asList(NETWORK_A);
+        underTest = new DetachNetworkUsedByVmValidator(vmsNames, removedNetworks);
+        assertThat(underTest.validate(),
+                failsWith(EngineMessage.NETWORK_CANNOT_DETACH_NETWORK_USED_BY_SINGLE_VM,
+                        concat(replaceWith(DetachNetworkUsedByVmValidator.VAR_VM_NAME, vmsNames),
+                                replaceWith(DetachNetworkUsedByVmValidator.VAR_NETWORK_NAME, removedNetworks))));
+    }
+
+    @Test
+    public void testValidateNotRemovingUsedNetworkByVmsMultipleNetworkSingleVm() {
+        final List<String> vmsNames = Arrays.asList(VM_A);
+        final List<String> removedNetworks = Arrays.asList(NETWORK_A, NETWORK_B);
+        underTest = new DetachNetworkUsedByVmValidator(vmsNames, removedNetworks);
+        assertThat(underTest.validate(),
+                failsWith(EngineMessage.MULTIPLE_NETWORKS_CANNOT_DETACH_NETWORKS_USED_BY_SINGLE_VM,
+                        concat(replaceWith(DetachNetworkUsedByVmValidator.VAR_NETWORK_NAMES, removedNetworks),
+                                replaceWith(DetachNetworkUsedByVmValidator.VAR_VM_NAME, vmsNames))));
+    }
+
+    @Test
+    public void testValidateNotRemovingUsedNetworkByVmsMultipleNetworkMultipleVm() {
+        final List<String> vmsNames = Arrays.asList(VM_A, VM_B);
+        final List<String> removedNetworks = Arrays.asList(NETWORK_A, NETWORK_B);
+        underTest = new DetachNetworkUsedByVmValidator(vmsNames, removedNetworks);
+        assertThat(underTest.validate(),
+                failsWith(EngineMessage.MULTIPLE_NETWORKS_CANNOT_DETACH_NETWORKS_USED_BY_VMS,
+                        concat(replaceWith(DetachNetworkUsedByVmValidator.VAR_NETWORK_NAMES, removedNetworks),
+                                replaceWith(DetachNetworkUsedByVmValidator.VAR_VM_NAMES, vmsNames))));
+    }
+}
