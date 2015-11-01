oVirt/ovirt-engine

commit 779d378e2b51510b54950e8f1f384382bfc956a5

core: replaced deprecated check for 'allowedDuplicates'.
allowed duplicates setting were read from engine config, which was
[the allowed duplicates option] deprecated in 3.6, and MAC pool
property should be consulted instead.

Change-Id: Ie9d54afbeb424d6a13a853b9e4d5f4878c8bcc1a
Bug-Url: https://bugzilla.redhat.com/1212461
Signed-off-by: Martin Mucha <mmucha@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/utils/VmDeviceUtils.java
@@ -12,6 +12,8 @@ import java.util.Set;
 import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.VmHandler;
 import org.ovirt.engine.core.bll.network.VmInterfaceManager;
+import org.ovirt.engine.core.bll.network.macpoolmanager.MacPoolManagerStrategy;
+import org.ovirt.engine.core.bll.network.macpoolmanager.MacPoolPerDcSingleton;
 import org.ovirt.engine.core.bll.validator.VirtIoRngValidator;
 import org.ovirt.engine.core.common.action.VmManagementParametersBase;
 import org.ovirt.engine.core.common.businessentities.DisplayType;
@@ -141,12 +143,12 @@ public class VmDeviceUtils {
      */
     public static VmDevice addCdDevice(Guid vmId, String cdPath) {
         return addManagedDevice(
-                new VmDeviceId(Guid.newGuid(), vmId),
-                VmDeviceGeneralType.DISK,
-                VmDeviceType.CDROM,
-                getCdDeviceSpecParams("", cdPath),
-                true,
-                true);
+            new VmDeviceId(Guid.newGuid(), vmId),
+            VmDeviceGeneralType.DISK,
+            VmDeviceType.CDROM,
+            getCdDeviceSpecParams("", cdPath),
+            true,
+            true);
     }
 
     /**
@@ -369,12 +371,12 @@ public class VmDeviceUtils {
      */
     public static VmDevice addVirtioScsiController(Guid vmId) {
         return addManagedDevice(
-                new VmDeviceId(Guid.newGuid(), vmId),
-                VmDeviceGeneralType.CONTROLLER,
-                VmDeviceType.VIRTIOSCSI,
-                EMPTY_SPEC_PARAMS,
-                true,
-                false);
+            new VmDeviceId(Guid.newGuid(), vmId),
+            VmDeviceGeneralType.CONTROLLER,
+            VmDeviceType.VIRTIOSCSI,
+            EMPTY_SPEC_PARAMS,
+            true,
+            false);
     }
 
     /**
@@ -644,15 +646,15 @@ public class VmDeviceUtils {
      */
     public static VmDevice addInterface(Guid vmId, Guid deviceId, boolean plugged, boolean hostDev, String address) {
         return addManagedDevice(
-                new VmDeviceId(deviceId, vmId),
-                VmDeviceGeneralType.INTERFACE,
-                hostDev ? VmDeviceType.HOST_DEVICE : VmDeviceType.BRIDGE,
-                EMPTY_SPEC_PARAMS,
-                plugged,
-                false,
-                address,
-                null,
-                false);
+            new VmDeviceId(deviceId, vmId),
+            VmDeviceGeneralType.INTERFACE,
+            hostDev ? VmDeviceType.HOST_DEVICE : VmDeviceType.BRIDGE,
+            EMPTY_SPEC_PARAMS,
+            plugged,
+            false,
+            address,
+            null,
+            false);
     }
 
     /**
@@ -661,15 +663,25 @@ public class VmDeviceUtils {
      * The network interface cannot be plugged if another plugged network interface has the same MAC address.
      *
      * @param iface the network interface to be checked
+     * @param vdsGroupId cluster id.
      * @return true if the network interface can be plugged, false otherwise
      */
-    private static boolean canPlugInterface(VmNic iface) {
-        VmInterfaceManager vmIfaceManager = new VmInterfaceManager();
-        if (vmIfaceManager.existsPluggedInterfaceWithSameMac(iface)) {
-            vmIfaceManager.auditLogMacInUseUnplug(iface);
+    private static boolean canPlugInterface(VmNic iface, Guid vdsGroupId) {
+        VDSGroup vdsGroup = dbFacade.getVdsGroupDao().get(vdsGroupId);
+        Guid dataCenterId = vdsGroup == null ? null : vdsGroup.getStoragePoolId();
+        boolean canPlugInterface = canPlugInterfaceInDc(iface, dataCenterId);
+        if (!canPlugInterface) {
+            new VmInterfaceManager().auditLogMacInUseUnplug(iface);
+        }
+        return canPlugInterface;
+    }
+
+    private static boolean canPlugInterfaceInDc(VmNic iface, Guid dataCenterId) {
+        if (dataCenterId == null) {
             return false;
         } else {
-            return true;
+            MacPoolManagerStrategy pool = MacPoolPerDcSingleton.getInstance().poolForDataCenter(dataCenterId);
+            return !pool.isMacInUse(iface.getMacAddress());
         }
     }
 
@@ -1633,6 +1645,7 @@ public class VmDeviceUtils {
      * @param vmDevicesToUpdate    list of devices to be updated in the DB
      */
     private static void addImportedInterfaces(VmBase vmBase, List<VmDevice> vmDevicesToUpdate) {
+
         for (VmNic iface : vmBase.getInterfaces()) {
             Guid deviceId = iface.getId();
             VmDevice vmDevice = addInterface(vmBase.getId(), deviceId, true, iface.isPassthrough(),
@@ -1644,7 +1657,7 @@ public class VmDeviceUtils {
                 exportedDevice = vmDevice;
             }
 
-            exportedDevice.setIsPlugged(exportedDevice.getIsPlugged() && canPlugInterface(iface));
+            exportedDevice.setIsPlugged(exportedDevice.getIsPlugged() && canPlugInterface(iface, vmBase.getVdsGroupId()));
             updateImportedVmDevice(vmBase, vmDevice, deviceId, vmDevicesToUpdate);
         }
     }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/VmInterfaceManager.java
@@ -184,23 +184,6 @@ public class VmInterfaceManager {
         return vmNames;
     }
 
-    /***
-     * Returns whether or not there is a plugged network interface with the same MAC address as the given interface
-     *
-     * @param interfaceToPlug
-     *            the network interface that needs to be plugged
-     * @return <code>true</code> if the MAC is used by another plugged network interface, <code>false</code> otherwise.
-     */
-    public boolean existsPluggedInterfaceWithSameMac(VmNic interfaceToPlug) {
-        List<VmNic> vmNetworkIntrefaces = getVmNicDao().getPluggedForMac(interfaceToPlug.getMacAddress());
-        for (VmNic vmNetworkInterface : vmNetworkIntrefaces) {
-            if (!interfaceToPlug.getId().equals(vmNetworkInterface.getId())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     /**
      * Sorts the list of NICs. The comparison is done either via PCI address, MAC address, or name, depending
      * on the information we have available

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/macpoolmanager/MacPoolManagerRanges.java
@@ -161,6 +161,11 @@ public class MacPoolManagerRanges implements MacPoolManagerStrategy {
         }
     }
 
+    @Override
+    public boolean isDuplicateMacAddressesAllowed() {
+        return this.allowDuplicates;
+    }
+
 
     private void checkIfInitialized() {
         if (!initialized) {

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/macpoolmanager/MacPoolManagerStrategy.java
@@ -58,4 +58,7 @@ public interface MacPoolManagerStrategy {
      * @throws org.ovirt.engine.core.common.errors.EngineException if mac address cannot be allocated.
      */
     List<String> allocateMacAddresses(int numberOfAddresses);
+
+    boolean isDuplicateMacAddressesAllowed();
+
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/AbstractVmInterfaceCommand.java
@@ -6,6 +6,7 @@ import org.ovirt.engine.core.bll.ValidationResult;
 import org.ovirt.engine.core.bll.VmCommand;
 import org.ovirt.engine.core.bll.VmHandler;
 import org.ovirt.engine.core.bll.utils.VmDeviceUtils;
+import org.ovirt.engine.core.bll.validator.MacAddressValidator;
 import org.ovirt.engine.core.common.action.ActivateDeactivateVmNicParameters;
 import org.ovirt.engine.core.common.action.AddVmInterfaceParameters;
 import org.ovirt.engine.core.common.action.PlugAction;
@@ -16,8 +17,6 @@ import org.ovirt.engine.core.common.businessentities.VMStatus;
 import org.ovirt.engine.core.common.businessentities.network.VmNetworkInterface;
 import org.ovirt.engine.core.common.businessentities.network.VmNic;
 import org.ovirt.engine.core.common.businessentities.storage.Disk;
-import org.ovirt.engine.core.common.config.Config;
-import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.errors.EngineError;
 import org.ovirt.engine.core.common.errors.EngineException;
 import org.ovirt.engine.core.common.errors.EngineMessage;
@@ -63,10 +62,7 @@ public abstract class AbstractVmInterfaceCommand<T extends AddVmInterfaceParamet
     }
 
     protected ValidationResult macAvailable() {
-        Boolean allowDupMacs = Config.<Boolean> getValue(ConfigValues.AllowDuplicateMacAddresses);
-        return getMacPool().isMacInUse(getMacAddress()) && !allowDupMacs
-                ? new ValidationResult(EngineMessage.NETWORK_MAC_ADDRESS_IN_USE)
-                : ValidationResult.VALID;
+        return new MacAddressValidator(getMacPool(), getMacAddress()).isMacAssignableValidator();
     }
 
     protected boolean uniqueInterfaceName(List<VmNic> interfaces) {

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/ActivateDeactivateVmNicCommand.java
@@ -8,11 +8,11 @@ import org.ovirt.engine.core.bll.ValidationResult;
 import org.ovirt.engine.core.bll.VmCommand;
 import org.ovirt.engine.core.bll.context.CommandContext;
 import org.ovirt.engine.core.bll.network.ExternalNetworkManager;
-import org.ovirt.engine.core.bll.network.VmInterfaceManager;
 import org.ovirt.engine.core.bll.network.cluster.NetworkHelper;
 import org.ovirt.engine.core.bll.provider.ProviderProxyFactory;
 import org.ovirt.engine.core.bll.provider.network.NetworkProviderProxy;
 import org.ovirt.engine.core.bll.utils.VmDeviceUtils;
+import org.ovirt.engine.core.bll.validator.MacAddressValidator;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.action.ActivateDeactivateVmNicParameters;
 import org.ovirt.engine.core.common.action.PlugAction;
@@ -24,8 +24,6 @@ import org.ovirt.engine.core.common.businessentities.network.Network;
 import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
 import org.ovirt.engine.core.common.businessentities.network.VmInterfaceType;
 import org.ovirt.engine.core.common.businessentities.network.VnicProfile;
-import org.ovirt.engine.core.common.config.Config;
-import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.errors.EngineException;
 import org.ovirt.engine.core.common.errors.EngineMessage;
 import org.ovirt.engine.core.common.vdscommands.VmNicDeviceVDSParameters;
@@ -229,12 +227,7 @@ public class ActivateDeactivateVmNicCommand<T extends ActivateDeactivateVmNicPar
     }
 
     protected ValidationResult macAvailable() {
-        Boolean allowDupMacs = Config.<Boolean> getValue(ConfigValues.AllowDuplicateMacAddresses);
-        VmInterfaceManager vmInterfaceManager = new VmInterfaceManager(getMacPool());
-        if (allowDupMacs || !vmInterfaceManager.existsPluggedInterfaceWithSameMac(getParameters().getNic())) {
-            return ValidationResult.VALID;
-        } else {
-            return new ValidationResult(EngineMessage.NETWORK_MAC_ADDRESS_IN_USE);
-        }
+        String macAddress = getParameters().getNic().getMacAddress();
+        return new MacAddressValidator(getMacPool(), macAddress).isMacAssignableValidator();
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/validator/MacAddressValidator.java
@@ -1,0 +1,22 @@
+package org.ovirt.engine.core.bll.validator;
+
+import org.ovirt.engine.core.bll.ValidationResult;
+import org.ovirt.engine.core.bll.network.macpoolmanager.MacPoolManagerStrategy;
+import org.ovirt.engine.core.common.errors.EngineMessage;
+
+public class MacAddressValidator {
+    private final MacPoolManagerStrategy macPool;
+    private final String macAddress;
+
+    public MacAddressValidator(MacPoolManagerStrategy macPool, String macAddress) {
+        this.macPool = macPool;
+        this.macAddress = macAddress;
+    }
+
+    public ValidationResult isMacAssignableValidator() {
+        Boolean allowDupMacs = macPool.isDuplicateMacAddressesAllowed();
+        boolean macIsAlreadyUsed = macPool.isMacInUse(macAddress);
+        boolean illegalDuplicateMacUsage = macIsAlreadyUsed && !allowDupMacs;
+        return ValidationResult.failWith(EngineMessage.NETWORK_MAC_ADDRESS_IN_USE).when(illegalDuplicateMacUsage);
+    }
+}

commit a7e2bf7706a6f24ad6cfb4e4f47c697c46ab1867

core: fixing NPE when importing nic with null valued mac
Error fix of bug introduced by:
779d378e2b51510b54950e8f1f384382bfc956a5

In former code (prior to mentioned erroneous patch) was null valued
mac used to gather nics from db, which indeed return no nics, thus no
match was found and true returned. With removal of this call error
was made; when mac of passed in nic is null, true must be returned.

Change-Id: If372e00c35954c4f52b9b7f097a837a70022bf7b
Bug-Url: https://bugzilla.redhat.com/1270003
Signed-off-by: Martin Mucha <mmucha@redhat.com>
Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/utils/VmDeviceUtils.java
@@ -670,20 +670,25 @@ public class VmDeviceUtils {
     private static boolean canPlugInterface(VmNic iface, Guid vdsGroupId) {
         VDSGroup vdsGroup = dbFacade.getVdsGroupDao().get(vdsGroupId);
         Guid dataCenterId = vdsGroup == null ? null : vdsGroup.getStoragePoolId();
-        boolean canPlugInterface = canPlugInterfaceInDc(iface, dataCenterId);
+        String macAddress = iface.getMacAddress();
+        boolean canPlugInterface = canPlugInterfaceInDc(macAddress, dataCenterId);
         if (!canPlugInterface) {
             new VmInterfaceManager().auditLogMacInUseUnplug(iface);
         }
         return canPlugInterface;
     }
 
-    private static boolean canPlugInterfaceInDc(VmNic iface, Guid dataCenterId) {
+    private static boolean canPlugInterfaceInDc(String macAddress, Guid dataCenterId) {
         if (dataCenterId == null) {
             return false;
-        } else {
-            MacPoolManagerStrategy pool = MacPoolPerDcSingleton.getInstance().poolForDataCenter(dataCenterId);
-            return !pool.isMacInUse(iface.getMacAddress());
         }
+
+        if (macAddress == null) {
+            return true;
+        }
+
+        MacPoolManagerStrategy pool = MacPoolPerDcSingleton.getInstance().poolForDataCenter(dataCenterId);
+        return !pool.isMacInUse(macAddress);
     }
 
     /*