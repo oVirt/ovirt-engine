2efc8c5e2d46e83b418da31e7871d7e85c07b9a7
Martin Mucha <mmucha@redhat.com>
2015-11-04 11:57:28 +0100

    core: update network status only if there's at least one host with up status
    Change-Id: Id45135af63e7cf11ffbc22292139fdcc12b3f7dc
    Bug-Url: https://bugzilla.redhat.com/1250063
    Signed-off-by: Martin Mucha <mmucha@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/cluster/NetworkClusterHelper.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/cluster/NetworkClusterHelper.java
@@ -1,5 +1,6 @@
 package org.ovirt.engine.core.bll.network.cluster;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -15,11 +16,10 @@ import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.VdsDao;
 import org.ovirt.engine.core.dao.network.NetworkAttachmentDao;
 import org.ovirt.engine.core.dao.network.NetworkClusterDao;
 import org.ovirt.engine.core.di.Injector;
-import org.ovirt.engine.core.utils.linq.LinqUtils;
-import org.ovirt.engine.core.utils.linq.Predicate;
 
 /**
  * Class to hold common static methods that are used in several different places.
@@ -33,12 +33,16 @@ public class NetworkClusterHelper {
         this.networkCluster = networkCluster;
     }
 
-    private NetworkClusterDao getNetworkClusterDao() {
+    private static NetworkClusterDao getNetworkClusterDao() {
         return DbFacade.getInstance().getNetworkClusterDao();
     }
 
     private NetworkAttachmentDao getNetworkAttachmentDao() {
         return Injector.get(NetworkAttachmentDao.class);
+    }
+
+    private static VdsDao getVdsDao() {
+        return Injector.get(VdsDao.class);
     }
 
     private NetworkCluster getManagementNetworkCluster() {
@@ -75,48 +79,111 @@ public class NetworkClusterHelper {
     }
 
     /**
-     * Set the network on cluster status in the DB to {@link NetworkStatus#OPERATIONAL} or
-     * {@link NetworkStatus#NON_OPERATIONAL} depending on the network state in the cluster (if it's implemented by all
-     * the hosts or not) and if it's required or not.
+     * Updates status of network. Update is performed only if there's at least one host in cluster, which [host]
+     * has status {@link VDSStatus#Up}. If network is nonrequired, it set to {@link NetworkStatus#OPERATIONAL}.
+     * Otherwise all hosts having status {@link VDSStatus#Up} in given cluster are scanned, whether all of them has
+     * network of <em>networkName</em> attached to some nic. If so, <em>networkCluster</em> is marked as
+     * {@link NetworkStatus#OPERATIONAL}, if not it's marked as {@link NetworkStatus#NON_OPERATIONAL}.
      *
-     * @param vdsGroupId
-     *            The cluster ID where the network is attached.
-     * @param net
-     *            The network itself.
+     *
+     * @param clusterId clusterId of cluster to which <em>network</em> belongs.
+     * @param network network to update.
      */
-    public static void setStatus(Guid vdsGroupId, final Network net) {
-        NetworkStatus status = NetworkStatus.OPERATIONAL;
-        NetworkCluster networkCluster =
-                DbFacade.getInstance().getNetworkClusterDao().get(new NetworkClusterId(vdsGroupId, net.getId()));
-
-        if (networkCluster != null) {
-
+    public static void setStatus(Guid clusterId, final Network network) {
+        NetworkCluster networkCluster = getNetworkClusterDao().get(new NetworkClusterId(clusterId, network.getId()));
+        boolean doUpdateNetworkClusterStatus = networkCluster != null;
+        if (doUpdateNetworkClusterStatus) {
             if (networkCluster.isRequired()) {
-                // Search all vds in cluster that have the specified network, if not the network is not active
-                List<VDS> vdsList = DbFacade.getInstance().getVdsDao().getAllForVdsGroup(vdsGroupId);
-                for (VDS vds : vdsList) {
-                    if (vds.getStatus() != VDSStatus.Up) {
-                        continue;
-                    }
-                    List<VdsNetworkInterface> interfaces = Backend.getInstance()
-                            .runInternalQuery(VdcQueryType.GetVdsInterfacesByVdsId,
-                                    new IdQueryParameters(vds.getId())).getReturnValue();
-                    VdsNetworkInterface iface = LinqUtils.firstOrNull(interfaces, new Predicate<VdsNetworkInterface>() {
-                        @Override
-                        public boolean eval(VdsNetworkInterface i) {
-                            return StringUtils.equals(i.getNetworkName(), net.getName());
-                        }
-                    });
-
-                    if (iface == null) {
-                        status = NetworkStatus.NON_OPERATIONAL;
-                        break;
-                    }
-                }
+                updateStatusOfRequiredNetworkCluster(networkCluster, clusterId, network.getName());
+            } else {
+                updateNetworkClusterStatus(networkCluster, NetworkStatus.OPERATIONAL);
             }
+        }
+    }
 
-            networkCluster.setStatus(status);
-            DbFacade.getInstance().getNetworkClusterDao().updateStatus(networkCluster);
+
+    /**
+     * updates status of required network in cluster. See this {@link #setStatus(Guid, Network) javadoc} for details.
+     */
+    private static void updateStatusOfRequiredNetworkCluster(NetworkCluster networkCluster, Guid clusterId, String networkName) {
+        List<VDS> hostsInCluster = getVdsDao().getAllForVdsGroup(clusterId);
+        List<VDS> hostsWithUpStatusInCluster = getHostsWithUpStatus(hostsInCluster);
+        boolean atLeastOneHostIsUp = !hostsWithUpStatusInCluster.isEmpty();
+
+        if (atLeastOneHostIsUp) {
+            NetworkStatus networkStatusToSet =
+                    atLeastOneHostDoesNotHaveNetworkAttached(hostsWithUpStatusInCluster, networkName)
+                            ? NetworkStatus.NON_OPERATIONAL
+                            : NetworkStatus.OPERATIONAL;
+
+            updateNetworkClusterStatus(networkCluster, networkStatusToSet);
+        }
+    }
+
+    /**
+     *
+     * @param hosts list of hosts to check
+     * @param networkName name of network
+     * @return true if there's at least one host, which does not have given network attached to one of its nics.
+     */
+    private static boolean atLeastOneHostDoesNotHaveNetworkAttached(List<VDS> hosts, String networkName) {
+        for (VDS host : hosts) {
+            List<VdsNetworkInterface> hostInterfaces = getHostInterfaces(host.getId());
+            if (!hostHasInterfaceWithGivenNetwork(networkName, hostInterfaces)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean hostHasInterfaceWithGivenNetwork(String networkName,
+            List<VdsNetworkInterface> hostInterfaces) {
+
+        for (VdsNetworkInterface hostInterface : hostInterfaces) {
+            if (StringUtils.equals(hostInterface.getNetworkName(), networkName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param hostId id of host.
+     * @return all <em>VdsNetworkInterface</em> records for given hostId.
+     */
+    private static List<VdsNetworkInterface> getHostInterfaces(Guid hostId) {
+        IdQueryParameters params = new IdQueryParameters(hostId);
+        return Backend.getInstance().runInternalQuery(VdcQueryType.GetVdsInterfacesByVdsId, params).getReturnValue();
+    }
+
+    /**
+     *
+     * @param hosts hosts to filter
+     * @return all hosts which has status {@link VDSStatus#Up}.
+     */
+    private static List<VDS> getHostsWithUpStatus(List<VDS> hosts) {
+        List<VDS> hostsWhichAreUp = new ArrayList<>();
+        for (VDS host : hosts) {
+            if (host.getStatus() == VDSStatus.Up) {
+                hostsWhichAreUp.add(host);
+            }
+        }
+
+        return hostsWhichAreUp;
+    }
+
+    /**
+     * Updates <em>networkCluster</em> with new <em>networkStatus</em> and persists passed <em>networkCluster</em> if
+     * <em>newStatus</em> is different from current one saving DB roundtrip if no update is needed.
+     *
+     * @param networkCluster NetworkCluster record to update
+     * @param newStatus network status to be set
+     */
+    private static void updateNetworkClusterStatus(NetworkCluster networkCluster, NetworkStatus newStatus) {
+        if (networkCluster.getStatus() != newStatus) {
+            networkCluster.setStatus(newStatus);
+            getNetworkClusterDao().updateStatus(networkCluster);
         }
     }
 
