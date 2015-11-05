3c9380c5217f3b7060595ecaadbef99f9453c6dc
Moti Asayag <masayag@redhat.com>
2015-11-05 14:54:30 +0200

    engine: Invoke UpgradeOvirtNodeCommand when image is provided
    When /api/hosts/{host:id}/install action is invoked for ovirt-node,
    and the user provides also an image, the desired action should be
    an upgrade of the ovirt-node to the specified image. For that to
    happen, the invoked command by the backend should be
    UpgradeOvirtNodeCommand.
    
    Bug-Url: https://bugzilla.redhat.com/1278467
    Change-Id: I53044e77e647882a8775c2bee7599eb220ad0f4e
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendHostResource.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendHostResource.java
@@ -141,6 +141,7 @@ public class BackendHostResource extends AbstractBackendActionableResource<Host,
             params.setReinstallOrUpgrade(true);
             if (action.isSetImage()) {
                 params.setoVirtIsoFile(action.getImage());
+                return doAction(VdcActionType.UpgradeOvirtNode, params, action);
             }
         }
         return doAction(VdcActionType.UpdateVds,
