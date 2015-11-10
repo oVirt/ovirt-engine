oVirt/ovirt-engine

commit 9dd1711da87bbc7dca9b6e7d6c84e9ac06396af8

webadmin: Change PosixStorageView.ui.xml syntax
Replace HTML with gwt elements.

Signed-off-by: Vered Volansky <vvolansk@redhat.com>
Change-Id: I7f1d3166d20c5db5d1d730dc5b167f04abd8be09

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/NfsStorageView.ui.xml
@@ -40,7 +40,7 @@
     }
 
     .expanderContent {
-        margin-left: 30px;
+        margin-left: 20px;
         display: none;
     }
 

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/PosixStorageView.java
@@ -2,7 +2,7 @@ package org.ovirt.engine.ui.webadmin.section.main.view.popup.storage;
 
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.idhandler.WithElementId;
-import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextBoxOnlyEditor;
+import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextBoxEditor;
 import org.ovirt.engine.ui.common.widget.uicommon.storage.AbstractStorageView;
 import org.ovirt.engine.ui.uicommonweb.models.EntityModel;
 import org.ovirt.engine.ui.uicommonweb.models.storage.PosixStorageModel;
@@ -13,14 +13,12 @@ import org.ovirt.engine.ui.webadmin.ApplicationConstants;
 import org.ovirt.engine.ui.webadmin.gin.AssetProvider;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style;
 import com.google.gwt.editor.client.SimpleBeanEditorDriver;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ValueBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
@@ -44,38 +42,23 @@ public class PosixStorageView extends AbstractStorageView<PosixStorageModel> {
     WidgetStyle style;
 
     @UiField
-    @WithElementId
     @Path(value = "path.entity")
-    StringEntityModelTextBoxOnlyEditor pathEditor;
-
-    @UiField
-    @Ignore
-    Label pathLabel;
+    @WithElementId("path")
+    StringEntityModelTextBoxEditor pathEditor;
 
     @UiField
     @Ignore
     Label pathExampleLabel;
 
     @UiField
-    @WithElementId
     @Path(value = "vfsType.entity")
-    StringEntityModelTextBoxOnlyEditor vfsTypeEditor;
+    @WithElementId("vfsType")
+    StringEntityModelTextBoxEditor vfsTypeEditor;
 
     @UiField
-    @Ignore
-    Label vfsTypeLabel;
-
-    @UiField
-    @WithElementId
     @Path(value = "mountOptions.entity")
-    StringEntityModelTextBoxOnlyEditor mountOptionsEditor;
-
-    @UiField
-    @Ignore
-    Label mountOptionsLabel;
-
-    @UiField
-    Label message;
+    @WithElementId("mountOptions")
+    StringEntityModelTextBoxEditor mountOptionsEditor;
 
     @UiField
     Image nfsPosixAlertIcon;
@@ -93,13 +76,15 @@ public class PosixStorageView extends AbstractStorageView<PosixStorageModel> {
 
     void addStyles() {
         pathEditor.addContentWidgetContainerStyleName(style.pathEditorContent());
+        vfsTypeEditor.addContentWidgetContainerStyleName(style.vfsTypeTextBoxEditor());
+        mountOptionsEditor.addContentWidgetContainerStyleName(style.mountOptionsTextBoxEditor());
     }
 
     void localize() {
-        pathLabel.setText(constants.storagePopupPosixPathLabel());
+        pathEditor.setLabel(constants.storagePopupPosixPathLabel());
         pathExampleLabel.setText(constants.storagePopupPosixPathExampleLabel());
-        vfsTypeLabel.setText(constants.storagePopupVfsTypeLabel());
-        mountOptionsLabel.setText(constants.storagePopupMountOptionsLabel());
+        vfsTypeEditor.setLabel(constants.storagePopupVfsTypeLabel());
+        mountOptionsEditor.setLabel(constants.storagePopupMountOptionsLabel());
         nfsPosixAlertIcon.setTitle(constants.storagePopupPosixNfsWarningLabel());
     }
 
@@ -119,30 +104,9 @@ public class PosixStorageView extends AbstractStorageView<PosixStorageModel> {
 
         pathExampleLabel.setVisible(object.getPath().getIsAvailable() && object.getPath().getIsChangable());
 
-        StyleTextBoxEditor(pathEditor, object.getPath());
-        StyleTextBoxEditor(vfsTypeEditor, object.getVfsType());
-        StyleTextBoxEditor(mountOptionsEditor, object.getMountOptions());
-
         if (!object.getVfsType().getEntityChangedEvent().getListeners().contains(vfsTypeListener)) {
             object.getVfsType().getEntityChangedEvent().addListener(vfsTypeListener);
         }
-    }
-
-    /*
-    Makes a provided editor look like label (enabled, read-only textbox).
-     */
-    private void StyleTextBoxEditor(StringEntityModelTextBoxOnlyEditor editor, EntityModel model) {
-
-        if (!model.getIsChangable()) {
-
-            editor.setEnabled(true);
-
-            ValueBox<String> valueBox = editor.asValueBox();
-            valueBox.setReadOnly(true);
-            valueBox.getElement().getStyle().setBorderWidth(0, Style.Unit.PX);
-        }
-
-        editor.asValueBox().setTitle(model.getTitle());
     }
 
     @Override
@@ -153,6 +117,10 @@ public class PosixStorageView extends AbstractStorageView<PosixStorageModel> {
     interface WidgetStyle extends CssResource {
 
         String pathEditorContent();
+
+        String vfsTypeTextBoxEditor();
+
+        String mountOptionsTextBoxEditor();
     }
 
     @Override

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/PosixStorageView.ui.xml
@@ -14,13 +14,16 @@
             font-family: Verdana, sans-serif;
         }
 
-		.pathEditorContent {
-			width: 565px;
-		}
+        .pathEditorContent {
+            width: 540px;
+            margin-left: 87px;
+            line-height: 26px !important;
+        }
 
         .pathExampleLabel {
-            margin: 2px 0px 0px 10px;
+            margin: 0px 0px 0px 125px;
             font-size: 8pt;
+            width: 450px;
         }
 
 		.errorLabel {
@@ -29,51 +32,30 @@
 			margin-left: 5px;
 		}
 
-        .nfsPosixAlertIcon {
-            margin-top: 10px;
+        .vfsTypeTextBoxEditor {
+            width: 270px;
+            margin-left: 57px;
+            line-height: 26px !important;
+        }
+
+        .mountOptionsTextBoxEditor {
+            width: 270px;
+            margin-left: 29px;
+            line-height: 26px !important;
         }
 
 	</ui:style>
 
     <g:VerticalPanel addStyleNames="{style.content}">
-        <g:HTMLPanel>
-            <table>
-                <tr>
-                    <td nowrap="nowrap">
-                        <g:Label ui:field="pathLabel" addStyleNames="{style.label}"/>
-                    </td>
-                    <td>
-                        <ge:StringEntityModelTextBoxOnlyEditor ui:field="pathEditor" addStyleNames="table_contentWidget_pfly_fix"/>
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td nowrap="nowrap">
-                        <g:Label ui:field="pathExampleLabel" addStyleNames="{style.pathExampleLabel}, generalDialogComment"/>
-                    </td>
-                </tr>
-                <tr>
-                    <td nowrap="nowrap">
-                        <g:Label ui:field="vfsTypeLabel" addStyleNames="{style.label}"/>
-                    </td>
-                    <td>
-                        <g:HorizontalPanel>
-                            <ge:StringEntityModelTextBoxOnlyEditor ui:field="vfsTypeEditor" addStyleNames="table_contentWidget_pfly_fix"/>
-                            <g:Image ui:field="nfsPosixAlertIcon" resource='{resources.alertImage}' visible="false" addStyleNames="{style.nfsPosixAlertIcon}"/>
-                        </g:HorizontalPanel>
-                    </td>
-                </tr>
-                <tr>
-                    <td nowrap="nowrap">
-                        <g:Label ui:field="mountOptionsLabel" addStyleNames="{style.label}"/>
-                    </td>
-                    <td>
-                        <ge:StringEntityModelTextBoxOnlyEditor ui:field="mountOptionsEditor" addStyleNames="table_contentWidget_pfly_fix"/>
-                    </td>
-                </tr>
-            </table>
-        </g:HTMLPanel>
-        <g:Label ui:field="message" addStyleNames="{style.errorLabel}"/>
+        <ge:StringEntityModelTextBoxEditor ui:field="pathEditor" addStyleNames="table_contentWidget_pfly_fix"/>
+        <g:Label ui:field="pathExampleLabel" addStyleNames="{style.pathExampleLabel}, generalDialogComment"/>
+        <g:HorizontalPanel>
+            <ge:StringEntityModelTextBoxEditor ui:field="vfsTypeEditor" addStyleNames="table_contentWidget_pfly_fix"/>
+            <g:Image ui:field="nfsPosixAlertIcon" resource='{resources.alertImage}' visible="false"/>
+        </g:HorizontalPanel>
+        <g:HorizontalPanel>
+            <ge:StringEntityModelTextBoxEditor ui:field="mountOptionsEditor" addStyleNames="table_contentWidget_pfly_fix"/>
+        </g:HorizontalPanel>
     </g:VerticalPanel>
 
 </ui:UiBinder>

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/StoragePopupView.ui.xml
@@ -73,7 +73,7 @@
 		}
 
         .advancedParametersExpanderContent {
-            margin-left: 15px;
+            margin-left: 20px;
             display: none;
         }
 

commit a08a7d1e9ea5607013a9da3078519e2b0968d561

webadmin: Add missing warning message in storage domain popup
Patches c273cd696323bab805bc3396b33b8953aa18f518 and
9dd1711da87bbc7dca9b6e7d6c84e9ac06396af8 introduced changes to the NFS and
Posix storage view but by mistake removed also the error message in those
views causing the storage popup not to show errors in storage domain
creation/edit among those errors the error of a path that already exists in
the system, this patch restores the error label so the error will appear

Change-Id: I74274212099014a5eb5f081629e39a705bb4b6fe
Signed-off-by: Tal Nisan <tnisan@redhat.com>
Bug-Url: https://bugzilla.redhat.com/1279192

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/NfsStorageView.java
@@ -88,6 +88,9 @@ public class NfsStorageView extends AbstractStorageView<NfsStorageModel> {
     @WithElementId
     StringEntityModelTextBoxEditor mountOptionsEditor;
 
+    @UiField
+    Label message;
+
     private final Driver driver = GWT.create(Driver.class);
 
     private final static ApplicationConstants constants = AssetProvider.getConstants();

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/NfsStorageView.ui.xml
@@ -65,6 +65,7 @@
             <ge:ShortEntityModelTextBoxEditor ui:field="timeoutEditor"/>
             <ge:StringEntityModelTextBoxEditor ui:field="mountOptionsEditor"/>
         </g:VerticalPanel>
+        <g:Label ui:field="message" addStyleNames="{style.errorLabel}"/>
     </g:VerticalPanel>
 
 </ui:UiBinder>

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/PosixStorageView.java
@@ -60,6 +60,9 @@ public class PosixStorageView extends AbstractStorageView<PosixStorageModel> {
     StringEntityModelTextBoxEditor mountOptionsEditor;
 
     @UiField
+    Label message;
+
+    @UiField
     Image nfsPosixAlertIcon;
 
     private final Driver driver = GWT.create(Driver.class);

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/storage/PosixStorageView.ui.xml
@@ -53,6 +53,7 @@
         <g:HorizontalPanel>
             <ge:StringEntityModelTextBoxEditor ui:field="mountOptionsEditor"/>
         </g:HorizontalPanel>
+        <g:Label ui:field="message" addStyleNames="{style.errorLabel}"/>
     </g:VerticalPanel>
 
 </ui:UiBinder>