a0e88038ad83f4b98755f42567d60bb689823901
Marek Libra <mlibra@redhat.com>
2015-11-11 10:03:31 +0100

    core: Bootable disk index considers CDROM on PPC
    PPC specific: The CDROM is mapped to sda (or index=0).
    This patch handles setting of additional bootable disk's index on PPC.
    
    Change-Id: I32ad475a311c9be4b8a38b5ec616e12723ecf305
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1274677
    Signed-off-by: Marek Libra <mlibra@redhat.com>

---
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/architecture/GetBootableDiskIndex.java
@@ -0,0 +1,34 @@
+package org.ovirt.engine.core.vdsbroker.architecture;
+
+
+import org.ovirt.engine.core.utils.archstrategy.ArchCommand;
+
+public class GetBootableDiskIndex implements ArchCommand {
+    private int diskIndex;
+    private int numOfReservedScsiIndexes;
+
+    public GetBootableDiskIndex(int numOfReservedScsiIndexes) {
+        this.numOfReservedScsiIndexes = numOfReservedScsiIndexes;
+    }
+
+    /**
+     * CDROM is mapped to IDE, the index of first boot disk is 0
+     */
+    @Override
+    public void runForX86_64() {
+        diskIndex = 0;
+    }
+
+    /**
+     *  On PPC, the index 0 is reserved for CDROM and is mapped to sda.
+     *  The boot disk is mapped one behind.
+     */
+    @Override
+    public void runForPPC64() {
+        diskIndex = numOfReservedScsiIndexes;
+    }
+
+    public int returnValue() {
+        return diskIndex;
+    }
+}
