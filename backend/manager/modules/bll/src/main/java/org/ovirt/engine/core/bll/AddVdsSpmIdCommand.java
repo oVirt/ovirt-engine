bea1ff9ccb5cd50d979e843e719217468dee7384
Allon Mureinik <amureini@redhat.com>
2015-11-23 22:57:56 +0200

    core: Test AddVdsSpmIdCommand#insertSpmIdToDb
    Introduced a simple test case for the insertSpmIdToDb method.
    It receives a list of existing vds_spm_ids and verifies that the correct
    new one is inserted into the mocked database.
    
    Change-Id: I1c77c1a0d732d0c7351449bddf5bde1e85aba70a
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVdsSpmIdCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVdsSpmIdCommand.java
@@ -58,7 +58,7 @@ public class AddVdsSpmIdCommand<T extends VdsActionParameters> extends VdsComman
         setSucceeded(true);
     }
 
-    private void insertSpmIdToDb(List<vds_spm_id_map> vds_spm_id_mapList) {
+    protected void insertSpmIdToDb(List<vds_spm_id_map> vds_spm_id_mapList) {
         // according to shaharf the first id is 1
         int selectedId = 1;
         List<Integer> list = LinqUtils.transformToList(vds_spm_id_mapList, new Function<vds_spm_id_map, Integer>() {
---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/AddVdsSpmIdCommandTest.java
@@ -0,0 +1,72 @@
+package org.ovirt.engine.core.bll;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.ovirt.engine.core.bll.context.CommandContext;
+import org.ovirt.engine.core.bll.context.EngineContext;
+import org.ovirt.engine.core.common.action.VdsActionParameters;
+import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.vds_spm_id_map;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.VdsSpmIdMapDao;
+
+public class AddVdsSpmIdCommandTest extends BaseCommandTest {
+
+    private AddVdsSpmIdCommand<VdsActionParameters> cmd;
+    private Guid spId;
+    private Guid vdsId;
+
+    @Mock
+    private VdsSpmIdMapDao vdsSpmIdMapDao;
+
+    @Before
+    public void setUp() {
+        VdsActionParameters params = new VdsActionParameters();
+        CommandContext ctx = new CommandContext(new EngineContext());
+        cmd = spy(new AddVdsSpmIdCommand<>(params, ctx));
+        doReturn(vdsSpmIdMapDao).when(cmd).getVdsSpmIdMapDao();
+
+        spId = Guid.newGuid();
+        vdsId = Guid.newGuid();
+        VDS vds = new VDS();
+        vds.setId(vdsId);
+        vds.setStoragePoolId(spId);
+        cmd.setVds(vds);
+    }
+
+    @Test
+    public void fromOne() {
+        insertSpmIdToDb(3, 1, 2);
+    }
+
+    @Test
+    public void oneMissing() {
+        insertSpmIdToDb(1, 4, 3);
+    }
+
+    @Test
+    public void empty() {
+        insertSpmIdToDb(1);
+    }
+
+    @Test
+    public void hole() {
+        insertSpmIdToDb(2, 3, 1, 4);
+    }
+
+    private void insertSpmIdToDb(int expected, int... given) {
+        List<vds_spm_id_map> list =
+                Arrays.stream(given).mapToObj(i -> new vds_spm_id_map(spId, vdsId, i)).collect(Collectors.toList());
+        cmd.insertSpmIdToDb(list);
+        verify(vdsSpmIdMapDao).save(new vds_spm_id_map(spId, vdsId, expected));
+    }
+}
