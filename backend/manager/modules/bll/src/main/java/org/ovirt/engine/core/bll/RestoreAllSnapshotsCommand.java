32b5d9a7784c54b7453208a298722bc4d34385bc
Maor Lipchuk <mlipchuk@redhat.com>
2015-11-30 11:55:14 +0200

    core: Rename parameter cinderDisks to cinderDisksToRestore.
    Change-Id: I7a1cf1261f867c517c6d87e1b3ed01629d60e335
    Bug-Url: https://bugzilla.redhat.com/1256048
    Bug-Url: https://bugzilla.redhat.com/1256049
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RestoreAllSnapshotsCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RestoreAllSnapshotsCommand.java
@@ -112,11 +112,11 @@ public class RestoreAllSnapshotsCommand<T extends RestoreAllSnapshotsParameters>
         restoreSnapshotAndRemoveObsoleteSnapshots(getSnapshot());
 
         boolean succeeded = true;
-        List<CinderDisk> cinderDisks = new ArrayList<>();
+        List<CinderDisk> cinderDisksToRestore = new ArrayList<>();
         for (DiskImage image : imagesToRestore) {
             if (image.getImageStatus() != ImageStatus.ILLEGAL) {
                 if (image.getDiskStorageType() == DiskStorageType.CINDER) {
-                    cinderDisks.add((CinderDisk) image);
+                    cinderDisksToRestore.add((CinderDisk) image);
                     continue;
                 }
                 ImagesContainterParametersBase params = new RestoreFromSnapshotParameters(image.getImageId(),
@@ -130,7 +130,7 @@ public class RestoreAllSnapshotsCommand<T extends RestoreAllSnapshotsParameters>
             }
         }
 
-        if (!restoreCinder(cinderDisks, removedSnapshotId)) {
+        if (!restoreCinder(cinderDisksToRestore, removedSnapshotId)) {
             log.error("Error to restore Cinder volumes snapshots");
         }
 
