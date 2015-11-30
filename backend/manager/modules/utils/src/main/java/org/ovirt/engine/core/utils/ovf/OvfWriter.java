e3a516c66877ecf439079b9dc3bb03de72de1cb2
Maor Lipchuk <mlipchuk@redhat.com>
2015-11-30 14:09:38 +0200

    core: Add cinder volume type for VM's disks' OVF.
    Add cinder volume type to update the Cinder disk with the relevant
    cinder volume type, once the snapshot gets commited or undo.
    
    Change-Id: I835237f987112d902b3ca30c827f5b38fa3b3be7
    Bug-Url: https://bugzilla.redhat.com/1256048
    Bug-Url: https://bugzilla.redhat.com/1256049
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfWriter.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/OvfWriter.java
@@ -84,6 +84,7 @@ public abstract class OvfWriter implements IOvfBuilder {
             _writer.writeAttributeString(OVF_URI, "size", String.valueOf(image.getSize()));
             _writer.writeAttributeString(OVF_URI, "description", StringUtils.defaultString(image.getDescription()));
             _writer.writeAttributeString(OVF_URI, "disk_storage_type", image.getDiskStorageType().name());
+            _writer.writeAttributeString(OVF_URI, "cinder_volume_type", StringUtils.defaultString(image.getCinderVolumeType()));
             _writer.writeEndElement();
 
         }
