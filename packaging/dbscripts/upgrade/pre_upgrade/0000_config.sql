e5f1b490c2086cf7c90123fa1d8e2e66c203714f
emesika <emesika@redhat.com>
2015-12-02 00:12:25 +0200

    core: fix incorrect json value in config
    AutoRecoveryAllowedTypes config key value is  {\"storage
    domains\":\"true\",\"hosts\":\"true\"}
    
    This is not a legal JSON format and generates the following error upon
    application start:
    
     ERROR [org.ovirt.engine.core.dal.dbbroker.generic.DBConfigUtils]
    (ServerService Thread Pool -- 43) [] Error parsing option
    'AutoRecoveryAllowedTypes' value: org.codehaus.jackson.JsonParse
    Exception: Unexpected character ('\' (code 92)): was expecting
    double-quote to start field name
     at [Source: java.io.StringReader@3198de41; line: 1, column: 3]
    
    Change-Id: Iba1ffefd646219dbeb8909ab620a44f960fb97c1
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1287589
    Signed-off-by: emesika <emesika@redhat.com>

--- packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
+++ packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -639,7 +639,7 @@ select fn_db_add_config_value('WaitForVdsInitInSec','60','general');
 --The default network connectivity check timeout
 select fn_db_add_config_value('NetworkConnectivityCheckTimeoutInSeconds','120','general');
 -- AutoRecoveryConfiguration
-select fn_db_add_config_value('AutoRecoveryAllowedTypes','{\"storage domains\":\"true\",\"hosts\":\"true\"}','general');
+select fn_db_add_config_value('AutoRecoveryAllowedTypes','{"storage domains":"true","hosts":"true"}','general');
 -- SPICE client mode default settings (Auto, Native, Plugin)
 select fn_db_add_config_value('ClientModeSpiceDefault','Native','general');
 -- VNC client mode default settings (Native, NoVnc)
@@ -775,7 +775,7 @@ select fn_db_add_config_value_for_versions_up_to('InitialSizeSparseDiskSupported
 --                  Update with override section
 ------------------------------------------------------------------------------------
 
-select fn_db_update_config_value('AutoRecoveryAllowedTypes','{\"storage domains\":\"true\",\"hosts\":\"true\"}','general');
+select fn_db_update_config_value('AutoRecoveryAllowedTypes','{"storage domains":"true","hosts":"true"}','general');
 select fn_db_update_config_value('BootstrapMinimalVdsmVersion','4.9','general');
 select fn_db_update_config_value('DBEngine','Postgres','general');
 select fn_db_update_config_value('DefaultTimeZone','(GMT) GMT Standard Time','general');
