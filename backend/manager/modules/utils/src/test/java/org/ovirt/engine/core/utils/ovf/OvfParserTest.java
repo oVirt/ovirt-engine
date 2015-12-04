29341a9b867bc0dbb960db0115d1274a2c7435af
Allon Mureinik <amureini@redhat.com>
2015-12-04 17:55:24 +0200

    core: OvfParserTest methods naming conventions
    Fixed naming conventions of the OvfParserTest class' methods
    as per the recommended Java naming conventions.
    
    Change-Id: Ie8fabbdaa7b20efa7e78d372a9061854e608c158
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/utils/src/test/java/org/ovirt/engine/core/utils/ovf/OvfParserTest.java
+++ backend/manager/modules/utils/src/test/java/org/ovirt/engine/core/utils/ovf/OvfParserTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class OvfParserTest {
 
     @Test
-    public void UtcDateStringToLocaDate_nodep() {
+    public void utcDateStringToLocaDateNoDep() {
         Date date = OvfParser.utcDateStringToLocaDate("1984/06/19 14:25:11");
         Assert.assertEquals(456503111000L, date.getTime());
     }
