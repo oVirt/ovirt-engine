1660f39789237be2d304c6e68de4928e99d399c9
Daniel Erez <derez@redhat.com>
2015-12-07 15:21:53 +0200

    core: preview snapshot with Cinder disks - failure flow
    TryBackToAllSnapshotsOfVmCommand:
    Use ConcurrentChildCommandsExecutionCallback instead of invoking
    parent's endAction on TryBackToAllCinderSnapshotsCommand/
    TryBackToCinderSnapshotCommand.
    
    Change-Id: I66a998962fcf2c242b31e65d8b03402ddfad5810
    Bug-Url: https://bugzilla.redhat.com/1287771
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/snapshots/TryBackToAllSnapshotsOfVmCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/snapshots/TryBackToAllSnapshotsOfVmCommand.java
@@ -10,6 +10,7 @@ import java.util.concurrent.Future;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
+import org.ovirt.engine.core.bll.ConcurrentChildCommandsExecutionCallback;
 import org.ovirt.engine.core.bll.LockMessagesMatchUtil;
 import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
 import org.ovirt.engine.core.bll.VmCommand;
@@ -18,6 +19,7 @@ import org.ovirt.engine.core.bll.context.CommandContext;
 import org.ovirt.engine.core.bll.storage.connection.CINDERStorageHelper;
 import org.ovirt.engine.core.bll.storage.disk.image.ImagesHandler;
 import org.ovirt.engine.core.bll.tasks.CommandCoordinatorUtil;
+import org.ovirt.engine.core.bll.tasks.interfaces.CommandCallback;
 import org.ovirt.engine.core.bll.validator.VmValidator;
 import org.ovirt.engine.core.bll.validator.storage.CinderDisksValidator;
 import org.ovirt.engine.core.bll.validator.storage.DiskImagesValidator;
@@ -73,6 +75,15 @@ public class TryBackToAllSnapshotsOfVmCommand<T extends TryBackToAllSnapshotsOfV
     public TryBackToAllSnapshotsOfVmCommand(T parameters, CommandContext commandContext) {
         super(parameters, commandContext);
         parameters.setEntityInfo(new EntityInfo(VdcObjectType.VM, getVmId()));
+    }
+
+    @Override
+    public void init() {
+        // No need to filter the images for partial preview as being done in the execute phase since the callback can
+        // also support no child commands, this should be changed once all commands will facilitate the CoCo
+        // infrastructure.
+        getParameters().setUseCinderCommandCallback(
+                !ImagesHandler.filterDisksBasedOnCinder(getImagesToPreview()).isEmpty());
     }
 
     @Override
@@ -412,4 +423,8 @@ public class TryBackToAllSnapshotsOfVmCommand<T extends TryBackToAllSnapshotsOfV
         return super.getSnapshotName();
     }
 
+    @Override
+    public CommandCallback getCallback() {
+        return getParameters().isUseCinderCommandCallback() ? new ConcurrentChildCommandsExecutionCallback() : null;
+    }
 }
