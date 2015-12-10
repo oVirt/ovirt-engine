93ab9846b6bce4612455b775c32825c147c12213
Vojtech Szocs <vszocs@redhat.com>
2015-12-10 20:38:55 +0100

    webadmin,userportal: Improve GWT UI build logs
    This patch tries to make GWT UI build logs less confusing.
    
    All changes below apply *only* to JVM running GWT compiler,
    which is spawned by gwt-maven-plugin during the Maven build.
    
    1. Use custom AspectJ load-time-weaver message handler in
       aop.xml that prints logs to std-out instead of std-err.
    
       (mvn) logs before:
    
          [ERROR] [..] info AspectJ Weaver Version ..
          [ERROR] [..] info register classloader ..
          [ERROR] [..] info using configuration ..
          [ERROR] [..] info register aspect ..
    
       (mvn) logs now:
    
          [INFO] info AspectJ Weaver Version ..
          [INFO] info register classloader ..
          [INFO] info using configuration ..
          [INFO] info register aspect ..
    
    2. Use custom java.util.logging configuration that
       prints logs to std-out instead of std-err.
    
       (mvn) logs before:
    
          [ERROR] .. java.util.prefs.FileSystemPreferences..
          [ERROR] INFO: Created user preferences directory.
    
       (mvn) logs now:
    
          [INFO] .. java.util.prefs.FileSystemPreferences..
          [INFO] INFO: Created user preferences directory.
    
    3. Added TODO in frontend aggregator module's pom.xml
       about aop.xml being applied twice (not a big deal,
       we can address this in future)
    
    Change-Id: I1fed599a1190ceff6c5dc8bf6fab6f6e499bec1f
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

---
+++ frontend/webadmin/modules/gwt-extension/src/main/java/org/ovirt/engine/ui/gwtextension/AspectJLogHandler.java
@@ -0,0 +1,28 @@
+package org.ovirt.engine.ui.gwtextension;
+
+import org.aspectj.bridge.AbortException;
+import org.aspectj.bridge.IMessage;
+import org.aspectj.bridge.IMessage.Kind;
+import org.aspectj.bridge.IMessageHandler;
+
+public class AspectJLogHandler implements IMessageHandler {
+
+    @Override
+    public boolean handleMessage(IMessage message) throws AbortException {
+        return SYSTEM_OUT.handleMessage(message);
+    }
+
+    @Override
+    public boolean isIgnoring(Kind kind) {
+        return kind == IMessage.DEBUG;
+    }
+
+    @Override
+    public void dontIgnore(Kind kind) {
+    }
+
+    @Override
+    public void ignore(Kind kind) {
+    }
+
+}
---
+++ frontend/webadmin/modules/gwt-extension/src/main/java/org/ovirt/engine/ui/gwtextension/JavaLoggingConfig.java
@@ -0,0 +1,24 @@
+package org.ovirt.engine.ui.gwtextension;
+
+import java.io.OutputStream;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class JavaLoggingConfig {
+
+    public JavaLoggingConfig() {
+        // This removes all log handlers, including default console handler
+        LogManager.getLogManager().reset();
+
+        // Use custom console handler that outputs its messages to std-out
+        Logger.getLogger("").addHandler(new ConsoleHandler() {
+            @Override
+            protected synchronized void setOutputStream(OutputStream out) throws SecurityException {
+                // Can't do this via constructor
+                super.setOutputStream(System.out);
+            }
+        });
+    }
+
+}
