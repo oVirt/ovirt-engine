9384fab1e6407536afb94dbefc1cc25bf5fa3b53
Roy Golan <rgolan@redhat.com>
2015-12-10 10:54:16 +0200

    core: Adding convenience methods to Version.java
    Version.compare(version) might be comfusing so adding 4 convenience
    methods:
    
    greater(version)
    less(version)
    greaterOrEquals(version)
    lessOrEquals(version)
    
    Change-Id: I04a3b17859776f896b41503f5edd83c6f003cb40
    Bug-Url: https://bugzilla.redhat.com/1269768
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- backend/manager/modules/compat/src/test/java/org/ovirt/engine/core/compat/VersionTest.java
+++ backend/manager/modules/compat/src/test/java/org/ovirt/engine/core/compat/VersionTest.java
@@ -23,4 +23,35 @@ public class VersionTest {
         Assert.assertFalse(new Version().equals("foo"));
         Assert.assertFalse(new Version().equals(1d));
     }
+
+    @Test
+    public void compare() {
+        Assert.assertTrue(Version.v3_5.compareTo(Version.v3_6) == -1);
+        Assert.assertTrue(Version.v3_6.compareTo(Version.v3_5) == 1);
+        Assert.assertTrue(Version.v3_6.compareTo(new Version("3.6")) == 0);
+    }
+
+    @Test
+    public void biggerThan() {
+        Assert.assertFalse(Version.v3_5.greater(Version.v3_6));
+        Assert.assertTrue(Version.v3_6.greater(Version.v3_5));
+    }
+    @Test
+    public void smallerThan() {
+        Assert.assertTrue(Version.v3_5.less(Version.v3_6));
+        Assert.assertFalse(Version.v3_6.less(Version.v3_5));
+    }
+
+    @Test
+    public void biggerThanOrEquals() {
+        Assert.assertFalse(Version.v3_5.greaterOrEquals(Version.v3_6));
+        Assert.assertTrue(Version.v3_6.greaterOrEquals(Version.v3_5));
+        Assert.assertTrue(Version.v3_6.greaterOrEquals(new Version("3.5")));
+    }
+    @Test
+    public void smallerThanOrEquals() {
+        Assert.assertTrue(Version.v3_5.lessOrEquals(new Version("3.5")));
+        Assert.assertTrue(Version.v3_5.lessOrEquals(Version.v3_6));
+        Assert.assertFalse(Version.v3_6.lessOrEquals(Version.v3_5));
+    }
 }
