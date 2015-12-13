21f21392817a034ecdd5b1c19251c5d934e07c10
Allon Mureinik <amureini@redhat.com>
2015-12-13 10:18:43 +0200

    core: Remove ListUtils#filter
    Removed the unused ListUtils#filter method, its related inner classes
    and its test.
    
    Change-Id: I1e6f9ffec0ada90e5fff23ef024f4638c3846ce6
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/ListUtils.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/ListUtils.java
@@ -12,66 +12,6 @@ import java.util.Set;
  */
 public class ListUtils {
     /**
-     * A generic interface that can be used to filter search results.
-     * @param <T>
-     *            type parameter for the list to be filtered
-     */
-    public interface Filter<T> {
-        /**
-         * Filter the list.
-         *
-         * @param data
-         * @return filtered results or null
-         */
-        List<T> filter(List<T> data);
-    }
-
-    public interface Predicate<T> {
-        boolean evaluate(T obj);
-    }
-
-    /**
-     * Creates a filteres list with the elements evaluated to <strong>true</strong>.
-     *
-     * @param <T>
-     */
-    public static class PredicateFilter<T> implements Filter<T> {
-
-        public PredicateFilter(Predicate<T> predicate) {
-            super();
-            this.predicate = predicate;
-        }
-
-        Predicate<T> predicate;
-
-        @Override
-        public List<T> filter(final List<T> data) {
-            final ArrayList<T> ret = new ArrayList<>();
-            for (final T obj : data) {
-                if (predicate.evaluate(obj)) {
-                    ret.add(obj);
-                }
-            }
-            return ret;
-        }
-
-    }
-
-    /**
-     * Filter a list.
-     * @param list  the list to filter
-     * @param filter the filter to use
-     * @return the filtered list or null if the original list was null
-     */
-    public static <T> List<T> filter(final List<T> list, final Filter<T> filter) {
-        if (list == null) {
-            return null;
-        } else {
-            return filter == null ? list : filter.filter(list);
-        }
-    }
-
-    /**
      * Compares two lists for equality of all their elements. Returns true if both lists are of same size and every
      * element in first list has an equal in the second.
      *
--- backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/ListUtilsTest.java
+++ backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/ListUtilsTest.java
@@ -12,39 +12,6 @@ import org.junit.Test;
 
 public class ListUtilsTest {
     @Test
-    public void filter() {
-        Assert.assertNull("null list must result in null", ListUtils.filter(null, null));
-        Assert.assertNull("null list must result in null, even if the filter is not null", ListUtils.filter(null, null));
-        Assert.assertTrue("empty list must be an empty list", ListUtils.filter(new ArrayList<String>(), null).isEmpty());
-        Assert.assertTrue("empty list must be an empty list",
-                ListUtils.filter(new ArrayList<String>(), new ListUtils.Filter<String>() {
-
-                    @Override
-                    public List<String> filter(List<String> data) {
-                        return data;
-                    }
-                }).isEmpty());
-
-        List<String> testList = new ArrayList<>();
-        testList.add("bla");
-        testList.add("bli");
-        testList.add("blu");
-        testList.add("blo");
-        testList.add("foo");
-        testList.add("bar");
-        List<String> filtered =
-                ListUtils.filter(testList, new ListUtils.PredicateFilter<>(new ListUtils.Predicate<String>() {
-                    @Override
-                    public boolean evaluate(String obj) {
-                        return obj.endsWith("a");
-                    }
-                }));
-
-        Assert.assertEquals("only one element should match the criteria", 1, filtered.size());
-        Assert.assertEquals("and it should be 'bla'", "bla", filtered.get(0));
-    }
-
-    @Test
     public void testListsEqual() {
         List<String> lst1 = Arrays.asList("a", "b", "c");
 
