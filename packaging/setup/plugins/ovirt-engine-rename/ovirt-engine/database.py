20e453c38487df83b9a4989f089f2512b97779a3
Yedidyah Bar David <didi@redhat.com>
2015-12-16 16:24:54 +0200

    packaging: rename: Handle storage domains more nicely
    Check all NFS, not only iso, in case data domains are also on engine.
    Notify user about active machines/domains on engine machine.
    
    Change-Id: I5a23ca3bb75efb23227c761b2aa4369d5828cf7f
    Bug-Url: https://bugzilla.redhat.com/1250556
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-rename/ovirt-engine/database.py
+++ packaging/setup/plugins/ovirt-engine-rename/ovirt-engine/database.py
@@ -1,6 +1,6 @@
 #
 # ovirt-engine-setup -- ovirt engine setup
-# Copyright (C) 2013-2015 Red Hat, Inc.
+# Copyright (C) 2013-2016 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -68,6 +68,173 @@ class Plugin(plugin.PluginBase):
             if connection is not None:
                 connection.commit()
 
+    def _local_domains_in_use(self):
+        res = False
+        dbovirtutils = database.OvirtUtils(
+            plugin=self,
+            dbenvkeys=oenginecons.Const.ENGINE_DB_ENV_KEYS,
+        )
+        dbovirtutils.tryDatabaseConnect()
+        dbstatement = database.Statement(
+            dbenvkeys=oenginecons.Const.ENGINE_DB_ENV_KEYS,
+            environment=self.environment,
+        )
+        self._my_domains = []
+        rows = dbstatement.execute(
+            statement="""
+                select
+                    s.id,
+                    s.status,
+                    s.storage,
+                    storage_name,
+                    connection,
+                    storage_domain_type
+                from
+                    storage_domains s,
+                    storage_server_connections c
+                where
+                    s.storage = c.id and
+                    s.storage_type=%(storage_type)s
+            """,
+            args=dict(
+                storage_type=domains.StorageType.NFS,
+            ),
+            ownConnection=True,
+        )
+        for row in rows:
+            host, path = row['connection'].split(':', 1)
+            if host == self._current_fqdn:
+                self._my_domains.append(row)
+
+        if self._my_domains:
+            self.logger.info(_('Engine machine hosting Storage Domains'))
+
+        vms_with_iso = []
+        for dom in self._my_domains:
+            rows = dbstatement.execute(
+                statement="""
+                    select
+                        distinct vm_name
+                    from
+                        storage_domains s,
+                        vms,
+                        vds_groups v,
+                        storage_pool sp,
+                        storage_domains sd
+                    where
+                        sd.storage_pool_id = sp.id and
+                        sp.id = v.storage_pool_id and
+                        v.vds_group_id = vms.vds_group_id and
+                        sd.id = %(sd_id)s and
+                        vms.current_cd != '' and
+                        vms.status > 0
+                """,
+                args=dict(
+                    sd_id=dom['id'],
+                ),
+                ownConnection=True,
+            )
+            vms_with_iso.extend([r['vm_name'] for r in rows])
+        if vms_with_iso:
+            res = True
+            self.dialog.note(
+                text=_(
+                    'The following Virtual Machines have attached ISO images '
+                    'from one or more of the below Storage Domains:\n'
+                    '\n'
+                    '{vms_with_iso}\n'
+                    '\n'
+                    'Needed action: They should be shut down, and/or '
+                    'have the ISO images disconnected (e.g. by ejecting them).'
+                    '\n\n'
+                ).format(
+                    vms_with_iso='\n'.join(sorted(set(vms_with_iso))),
+                ),
+            )
+
+        vms_with_disks = []
+        for dom in self._my_domains:
+            rows = dbstatement.execute(
+                statement="""
+                    select
+                        distinct vm_name
+                    from
+                        all_disks_for_vms adfv,
+                        vms
+                    where
+                        storage_id = %(sd_id)s and
+                        adfv.vm_id = vms.vm_guid and
+                        vms.status > 0
+                """,
+                args=dict(
+                    sd_id=dom['id'],
+                ),
+                ownConnection=True,
+            )
+            vms_with_disks.extend([r['vm_name'] for r in rows])
+        if vms_with_disks:
+            res = True
+            self.dialog.note(
+                text=_(
+                    'The following Virtual Machines have attached disk '
+                    'images from one or more of the below Storage Domains:\n'
+                    '\n'
+                    '{vms_with_disks}\n'
+                    '\n'
+                    'Needed action: They should be shut down.'
+                    '\n\n'
+                ).format(
+                    vms_with_disks='\n'.join(sorted(set(vms_with_disks))),
+                ),
+            )
+
+        active_domains = [
+            r for r in self._my_domains
+            if r['status'] == domains.StorageDomainStatus.ACTIVE
+        ]
+        if active_domains:
+            res = True
+            self.dialog.note(
+                text=_(
+                    'The following Storage Domains use the engine '
+                    'machine as an NFS server, and are active:\n'
+                    '\n'
+                    '{domains}\n'
+                    '\n'
+                    'Needed action: They should be moved to Maintenance.'
+                    '\n\n'
+                ).format(
+                    domains='\n'.join(
+                        sorted(
+                            [d['storage_name'] for d in active_domains]
+                        )
+                    )
+                ),
+            )
+
+        if not res and self._my_domains:
+            # Lastly inform the user if we are going to rename
+            # local storage domains (which are not in use)
+            self.dialog.note(
+                text=_(
+                    'The following Storage Domains use the engine '
+                    'machine as an NFS server:\n'
+                    '\n'
+                    '{domains}\n'
+                    '\n'
+                    'They will be modified to use the new name.\n'
+                    '\n'
+                ).format(
+                    domains='\n'.join(
+                        sorted(
+                            [d['storage_name'] for d in self._my_domains]
+                        )
+                    )
+                ),
+            )
+
+        return res
+
     def __init__(self, context):
         super(Plugin, self).__init__(context=context)
 
@@ -78,6 +245,26 @@ class Plugin(plugin.PluginBase):
         self.environment[otopicons.CoreEnv.MAIN_TRANSACTION].append(
             self.DBTransaction(self)
         )
+        self._current_fqdn = self.environment[osetupcons.ConfigEnv.FQDN]
+        self._my_domains = []
+
+    @plugin.event(
+        stage=plugin.Stages.STAGE_VALIDATION,
+    )
+    def _validation(self):
+        while self._local_domains_in_use():
+            self.dialog.queryString(
+                name='OVESETUP_RENAME_LOCAL_STORAGE_USED',
+                note=_(
+                    'Some storage domains hosted on the engine machine '
+                    'are in use. Please complete the above detailed '
+                    'actions.\n'
+                    'To abort, simply kill this utility with ^C.\n'
+                    'Press Enter to continue: '
+                ),
+                prompt=True,
+                default='y',  # Allow just pressing Enter
+            )
 
     @plugin.event(
         stage=plugin.Stages.STAGE_MISC,
@@ -96,59 +283,40 @@ class Plugin(plugin.PluginBase):
         ] = self.environment[oenginecons.EngineDBEnv.STATEMENT].connect()
 
     @plugin.event(
-        stage=plugin.Stages.STAGE_VALIDATION,
+        stage=plugin.Stages.STAGE_MISC,
+        after=(
+            oengcommcons.Stages.DB_CONNECTION_AVAILABLE,
+        ),
+        condition=lambda self: self._my_domains,
     )
-    def _validation(self):
-        dbovirtutils = database.OvirtUtils(
-            plugin=self,
-            dbenvkeys=oenginecons.Const.ENGINE_DB_ENV_KEYS,
-        )
-        dbovirtutils.tryDatabaseConnect()
-        dbstatement = database.Statement(
-            dbenvkeys=oenginecons.Const.ENGINE_DB_ENV_KEYS,
-            environment=self.environment,
-        )
-        my_domains = []
-        rows = dbstatement.execute(
-            statement="""
-                select
-                    storage_name,
-                    connection
-                from
-                    storage_domain_static s,
-                    storage_server_connections c
-                where
-                    s.storage = c.id and
-                    s.storage_type=%(storage_type)s and
-                    s.storage_domain_type=%(storage_domain_type)s
-            """,
-            args=dict(
-                storage_type=domains.StorageType.NFS,
-                storage_domain_type=domains.StorageDomainType.ISO,
-            ),
-            ownConnection=True,
-        )
-        for row in rows:
-            host, path = row['connection'].split(':', 1)
-            if host == self.environment[osetupcons.ConfigEnv.FQDN]:
-                my_domains.append(row['storage_name'])
-        if my_domains:
-            self.logger.warning(_('Engine host hosting Storage Domains'))
-            self.dialog.note(
-                text=_(
-                    'The following Storage Domains use the engine host\n'
-                    'as an NFS server:\n'
-                    '\n'
-                    '{domains}\n'
-                    '\n'
-                    'Cannot rename the engine host. Please backup relevant\n'
-                    'data if needed, remove all of these domains, and then\n'
-                    'run this utility again.\n'
-                ).format(
-                    domains='\n'.join(sorted(my_domains))
-                ),
+    def _rename_storage(self):
+        for row in self._my_domains:
+            self.logger.debug(
+                'Modifying connection of Storage Domain id %(id)s '
+                'name %(storage_name)s',
+                row
             )
-            raise RuntimeError(_('Cannot rename host hosting Storage Domains'))
+            self.environment[
+                oenginecons.EngineDBEnv.STATEMENT
+            ].execute(
+                statement="""
+                    update storage_server_connections
+                    set connection = regexp_replace(
+                        connection,
+                        '^{old_fqdn}:',
+                        '{new_fqdn}:'
+                    )
+                    where id = %(storage_id)s
+                """.format(
+                    old_fqdn=self._current_fqdn,
+                    new_fqdn=self.environment[
+                        osetupcons.RenameEnv.FQDN
+                    ],
+                ),
+                args=dict(
+                    storage_id=row['storage'],
+                )
+            )
 
 
 # vim: expandtab tabstop=4 shiftwidth=4
