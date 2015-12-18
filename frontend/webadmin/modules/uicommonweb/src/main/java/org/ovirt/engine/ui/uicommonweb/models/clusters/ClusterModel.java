e0e3f3363bb3e78370a4c8d09876f4fe5fe7e5bd
Sahina Bose <sabose@redhat.com>
2015-12-18 16:39:38 +0530

    webadmin: fixing cluster and tuned profile selection
    While editing a cluster(with multiple possible compat versions)
    the version and tuned profile are not selected correctly
    
    Change-Id: I179ef2f162e1c218d3d108e101f0279fc6f440f8
    Bug-Url: https://bugzilla.redhat.com/1290748
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/clusters/ClusterModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/clusters/ClusterModel.java
@@ -860,25 +860,27 @@ public class ClusterModel extends EntityModel<VDSGroup>
                 final String oldSelectedProfile = glusterTunedProfile.getSelectedItem();
                 glusterTunedProfile.setItems(glusterTunedProfiles);
                 glusterTunedProfile.setIsAvailable(glusterTunedProfile.getItems().size() > 0);
+                String newSelectedItem = null;
                 if (oldSelectedProfile != null) {
-                    String newSelectedItem =
+                    newSelectedItem =
                             Linq.firstOrDefault(glusterTunedProfiles, new IPredicate<String>() {
                                 @Override
                                 public boolean match(String item) {
                                     return item.equals(oldSelectedProfile);
                                 }
                             });
-                    if (newSelectedItem != null) {
-                        glusterTunedProfile.setSelectedItem(newSelectedItem);
-                    } else if (getIsEdit()) {
-                        glusterTunedProfile.setSelectedItem(Linq.firstOrDefault(glusterTunedProfiles, new IPredicate<String>() {
-                            @Override
-                            public boolean match(String item) {
-                                return item.equals(getEntity().getGlusterTunedProfile());
-                            }
-                        }));
-                    }
                 }
+                if (newSelectedItem != null) {
+                    glusterTunedProfile.setSelectedItem(newSelectedItem);
+                } else if (getIsEdit()) {
+                    glusterTunedProfile.setSelectedItem(Linq.firstOrDefault(glusterTunedProfiles, new IPredicate<String>() {
+                        @Override
+                        public boolean match(String item) {
+                            return item.equals(getEntity().getGlusterTunedProfile());
+                        }
+                    }));
+                }
+
             }
         }));
     }
@@ -1806,9 +1808,9 @@ public class ClusterModel extends EntityModel<VDSGroup>
                 ArrayList<Version> versions = (ArrayList<Version>) result;
                 Version selectedVersion = clusterModel.getVersion().getSelectedItem();
                 clusterModel.getVersion().setItems(versions);
-                if (selectedVersion == null ||
+                if (!clusterModel.getIsEdit() && (selectedVersion == null ||
                         !versions.contains(selectedVersion) ||
-                        selectedVersion.compareTo(selectedDataCenter.getcompatibility_version()) > 0)
+                        selectedVersion.compareTo(selectedDataCenter.getcompatibility_version()) > 0))
                 {
                     if(ApplicationModeHelper.getUiMode().equals(ApplicationMode.GlusterOnly)){
                         clusterModel.getVersion().setSelectedItem(Linq.selectHighestVersion(versions));
