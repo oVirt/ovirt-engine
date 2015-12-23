4b797e499391988a2b4d9d01c381c2666972e0db
Martin Mucha <mmucha@redhat.com>
2015-12-23 21:37:19 +0100

    core: fix badly generated macs in db
    Note: this patch replaces former one
    (I4a1b37da30ce4ad6d25c1d510eb5c09e17525f1b)
    because different approach to fix this issue was requested.
    
    There was .py script¹ (now deleted) which was used to generate mac
    ranges in some circumstances. However there was a bug in it, so some
    users may end up with MAC address like 00:1a:4a:1:f:ff generated
    (fourth and fifth group can be formed just by one char).
    This MAC got without any validation into DB and engine did not detect
    this problem — since engine code just removes colons and parse value
    as hex. So in that case user ended up with different mac ranges than
    intended. In 3.6 code validations were added to UI, where such MAC
    will fail when user just opens DC edit dialog and confirm it without
    any change.
    
    Because code did not fail, user could continue using such setting,
    but actually different range was used. For example if generated
    range was 00:1a:4a:1:f:00-00:1a:4a:1:f:ff, engine actually
    used range was: 00:00:1a:4a:1f:00-00:00:1a:4a:1f:ff.
    This patch tries to fix db entries in same way.
    
    description of sql udpate:
    Patch is little bit convoluted because it tries not to use stored
    procedures. Flow is:
    • first colons are removed
    • missing zeroes are added to the left of string to reach total of
    12 chars
    • after each two chars new colon is added; this creates string
    enging with colon
    • extra last character is removed.
    
    [1] ./packaging/setup/plugins/ovirt-engine-setup/config/macrange.py
    
    Change-Id: I962d96dbde5afaf19c94763ac51166c958a2f0bd
    Bug-Url: https://bugzilla.redhat.com/1286132
    Signed-off-by: Martin Mucha <mmucha@redhat.com>

---
+++ packaging/dbscripts/upgrade/03_06_1980_fix_invalid_macs.sql
@@ -0,0 +1,28 @@
+UPDATE
+  mac_pool_ranges
+SET
+  from_mac = ( SELECT
+                substr(
+                  regexp_replace(
+                    lpad(replace(from_mac, ':', ''),
+                      12,
+                      '0'),
+                    '(..)',
+                    '\1:',
+                    'g'),
+                1,
+                17)
+              ),
+  to_mac = (SELECT
+              substr(
+                regexp_replace(
+                  lpad(replace(to_mac, ':', ''),
+                    12,
+                    '0'),
+                  '(..)',
+                  '\1:',
+                  'g'),
+              1,
+              17)
+            )
+;
