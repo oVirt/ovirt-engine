85a86cd5f75998a769349edd0ef462fbd32a56ac
Arik Hadas <ahadas@redhat.com>
2015-12-27 12:18:15 +0200

    webadmin: add missing initialization of vm-init model for pool
    Missing initialization of VM-init model in the new VM pool dialog caused
    properties in the model to be uninitialized and therefore to produce
    NPEs. This patch adds default initialization of the model for new pool
    dialog the same way it happens for new VM dialog.
    
    Change-Id: I16d8ef192759863cb84d0f54f0daeea165000d22
    Bug-Url: https://bugzilla.redhat.com/1289961
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/NewPoolModelBehavior.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/NewPoolModelBehavior.java
@@ -32,6 +32,8 @@ public class NewPoolModelBehavior extends PoolModelBehaviorBase {
         templateValidate();
 
         instanceTypeManager = new NewPoolInstanceTypeManager(getModel());
+
+        getModel().getVmInitModel().init(null);
     }
 
     @Override
