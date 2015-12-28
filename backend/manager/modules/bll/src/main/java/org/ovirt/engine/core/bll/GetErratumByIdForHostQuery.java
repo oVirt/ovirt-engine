85a4b53aae3a42ce940947edec51da62a05556cc
Moti Asayag <masayag@redhat.com>
2015-12-28 14:38:18 +0200

    engine: Use injection instead of static access
    Change-Id: I952ff108e4fc8cdcbd9050206e045e22e1f2684d
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetErratumByIdForHostQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetErratumByIdForHostQuery.java
@@ -1,12 +1,22 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.host.provider.HostProviderProxy;
 import org.ovirt.engine.core.bll.provider.ProviderProxyFactory;
 import org.ovirt.engine.core.common.businessentities.Provider;
 import org.ovirt.engine.core.common.businessentities.VdsStatic;
 import org.ovirt.engine.core.common.queries.HostErratumQueryParameters;
+import org.ovirt.engine.core.dao.VdsStaticDao;
+import org.ovirt.engine.core.dao.provider.ProviderDao;
 
 public class GetErratumByIdForHostQuery<P extends HostErratumQueryParameters> extends QueriesCommandBase<P> {
+
+    @Inject
+    private VdsStaticDao hostStaticDao;
+
+    @Inject
+    private ProviderDao providerDao;
 
     public GetErratumByIdForHostQuery(P parameters) {
         super(parameters);
@@ -14,12 +24,12 @@ public class GetErratumByIdForHostQuery<P extends HostErratumQueryParameters> ex
 
     @Override
     protected void executeQueryCommand() {
-        VdsStatic host = getDbFacade().getVdsStaticDao().get(getParameters().getId());
+        VdsStatic host = hostStaticDao.get(getParameters().getId());
         if (host == null || host.getHostProviderId() == null) {
             return;
         }
 
-        Provider<?> provider = getDbFacade().getProviderDao().get(host.getHostProviderId());
+        Provider<?> provider = providerDao.get(host.getHostProviderId());
         if (provider != null) {
             HostProviderProxy proxy = ProviderProxyFactory.getInstance().create(provider);
             getQueryReturnValue().setReturnValue(proxy.getErratumForHost(host.getHostName(),
