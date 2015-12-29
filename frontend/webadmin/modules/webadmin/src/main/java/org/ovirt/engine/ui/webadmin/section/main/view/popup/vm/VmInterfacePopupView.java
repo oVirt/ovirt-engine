05047bc63191db015b96c29bc4498754070b75e2
Marcin Mirecki <mmirecki@redhat.com>
2015-12-29 16:33:24 +0100

    webadmin: enlarging vm nic edit panel to fit content in all browsers
    The content of the vm nic panel does not fit in the panel
    in chrome, causing the scrollbar to show, which messes up
    the alignment of inner elements. Therefore it must be
    enlarged.
    
    Change-Id: I81ea1b8a9348511973a0b0d92015af8ba7bc348d
    Bug-Url: https://bugzilla.redhat.com/1183462
    Signed-off-by: Marcin Mirecki <mmirecki@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/vm/VmInterfacePopupView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/vm/VmInterfacePopupView.java
@@ -19,8 +19,9 @@ public class VmInterfacePopupView extends AbstractModelBoundWidgetPopupView<VmIn
     public VmInterfacePopupView(EventBus eventBus) {
         super(eventBus,
                 new NetworkInterfacePopupWidget(eventBus), "510px", //$NON-NLS-1$
-                "320px"); //$NON-NLS-1$
+                "380px"); //$NON-NLS-1$
         ViewIdHandler.idHandler.generateAndSetIds(this);
+
     }
 
 }
