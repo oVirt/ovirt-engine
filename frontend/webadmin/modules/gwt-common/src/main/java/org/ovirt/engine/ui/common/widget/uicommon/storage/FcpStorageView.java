3d71c98100fe5aa7604e96366759b1ad7ecd3418
Idan Shaby <ishaby@redhat.com>
2016-01-13 09:13:51 +0200

    frontend: fix FcpStorageView faults
    FcpStorageView has four faults that are fixed in this patch:
    1. It extends from AbstractSanStorageView, which is
    useless since it's the only subclass of AbstractSanStorageView.
    2. The corresponding ui.xml file is AbstractSanStorageView.ui.xml (and
    not FcpStorageView.ui.xml), which causes the editor to ignore the label
    "errorMessage" in FcpStorageView and to malfunction.
    3. The @Path annotation of the label "errorMessage" is "GetLUNsFailure",
    while the corresponding string field in the model, SanStorageModel, is
    "getLUNsFailure".
    4. AbstractSanStorageView extends from
    AbstractStorageView<SanStorageModelBase>, while the model of
    FcpStorageView is SanStorageModel.
    
    This patch:
    - Merges AbstractSanStorageView into FcpStorageView.
    - Renames the AbstractSanStorageView.ui.xml to FcpStorageView.ui.xml.
    - Renames the Path annotation of the label "errorMessage" to
    "getLUNsFailure".
    - Replaces the generics type of AbstractStorageView to SanStorageModel.
    
    As a consequence, the label "errorMessage" is finally shown when it's
    supposed to be shown in the FcpStorageView.
    
    Change-Id: Idff765f50ce3d461c18f0db428687e7a27e8b64c
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1297689
    Signed-off-by: Idan Shaby <ishaby@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/storage/FcpStorageView.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/storage/FcpStorageView.java
@@ -1,33 +1,103 @@
 package org.ovirt.engine.ui.common.widget.uicommon.storage;
 
+import java.util.List;
+
+import org.ovirt.engine.ui.common.widget.HasValidation;
+import org.ovirt.engine.ui.common.widget.ValidatedPanelWidget;
+import org.ovirt.engine.ui.uicommonweb.models.Model;
+import org.ovirt.engine.ui.uicommonweb.models.storage.SanStorageModel;
 import org.ovirt.engine.ui.uicommonweb.models.storage.SanStorageModelBase;
+import org.ovirt.engine.ui.uicompat.Event;
+import org.ovirt.engine.ui.uicompat.IEventListener;
+import org.ovirt.engine.ui.uicompat.PropertyChangedEventArgs;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.editor.client.SimpleBeanEditorDriver;
+import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 
-public class FcpStorageView extends AbstractSanStorageView {
+public class FcpStorageView extends AbstractStorageView<SanStorageModel> implements HasValidation {
 
     @UiField
-    @Path(value = "GetLUNsFailure")
+    @Path(value = "getLUNsFailure")
     Label errorMessage;
 
-    SanStorageLunToTargetList sanStorageLunToTargetList;
+    @UiField
+    ValidatedPanelWidget contentPanel;
 
-    double panelHeight = 378;
-    double listHeight = 340;
+    private final Driver driver = GWT.create(Driver.class);
+
+    private SanStorageLunToTargetList sanStorageLunToTargetList;
+
+    private double panelHeight = 378;
+
+    private double listHeight = 340;
 
     public FcpStorageView(boolean multiSelection) {
-        super(multiSelection);
+        initWidget(ViewUiBinder.uiBinder.createAndBindUi(this));
+        driver.initialize(this);
+        this.multiSelection = multiSelection;
     }
 
     public FcpStorageView(boolean multiSelection, double panelHeight, double listHeight) {
-        super(multiSelection);
+        this(multiSelection);
 
         this.panelHeight = panelHeight;
         this.listHeight = listHeight;
     }
 
     @Override
+    public void edit(final SanStorageModel object) {
+        driver.edit(object);
+
+        initLists(object);
+
+        // Add event handlers
+        object.getPropertyChangedEvent().addListener(new IEventListener<PropertyChangedEventArgs>() {
+            @Override
+            public void eventRaised(Event<? extends PropertyChangedEventArgs> ev, Object sender, PropertyChangedEventArgs args) {
+                String propName = args.propertyName;
+                if (propName.equals("IsValid")) { //$NON-NLS-1$
+                    onIsValidPropertyChange(object);
+                }
+            }
+        });
+    }
+
+    void onIsValidPropertyChange(Model model) {
+        if (model.getIsValid()) {
+            markAsValid();
+        } else {
+            markAsInvalid(model.getInvalidityReasons());
+        }
+    }
+
+    @Override
+    public void markAsValid() {
+        contentPanel.markAsValid();
+    }
+
+    @Override
+    public void markAsInvalid(List<String> validationHints) {
+        contentPanel.markAsInvalid(validationHints);
+    }
+
+    @Override
+    public boolean isValid() {
+        return contentPanel.isValid();
+    }
+
+    @Override
+    public SanStorageModel flush() {
+        return driver.flush();
+    }
+
+    @Override
+    public void focus() {
+    }
+
     protected void initLists(SanStorageModelBase object) {
         // Create and update storage list
         sanStorageLunToTargetList = new SanStorageLunToTargetList(object, true, multiSelection);
@@ -39,5 +109,12 @@ public class FcpStorageView extends AbstractSanStorageView {
 
         // Add view widget to panel
         contentPanel.setWidget(sanStorageLunToTargetList);
+    }
+
+    interface Driver extends SimpleBeanEditorDriver<SanStorageModel, FcpStorageView> {
+    }
+
+    interface ViewUiBinder extends UiBinder<Widget, FcpStorageView> {
+        ViewUiBinder uiBinder = GWT.create(ViewUiBinder.class);
     }
 }
