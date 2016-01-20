4a74d18428e25efeb7a9c1785a8ec795d035086e
Tal Nisan <tnisan@redhat.com>
2016-01-20 16:14:36 +0200

    core: Add test to PowerManagementUtilsTest
    Change-Id: I82cfdc0b98ae4e5c71a6177770858c96ca3a05c4
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/pm/PowerManagementUtilsTest.java
+++ backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/pm/PowerManagementUtilsTest.java
@@ -1,6 +1,7 @@
 package org.ovirt.engine.core.common.utils.pm;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Map;
 
@@ -12,10 +13,15 @@ public class PowerManagementUtilsTest {
     public void emptyStringMakesEmptyMap() {
         final String empty = "";
         Map<String, String> map = PowerManagementUtils.pmOptionsStringToMap(empty);
-        mapIsEmpty(map);
+        assertTrue(map.isEmpty());
     }
 
-    private void mapIsEmpty(final Map<String, String> map) {
-        assertEquals(0, map.size());
+    @Test
+    public void testStringToMap() {
+        final String str = "ipport=666,secure=true";
+        Map<String, String> map = PowerManagementUtils.pmOptionsStringToMap(str);
+        assertEquals(2, map.size());
+        assertEquals("666", map.get("ipport"));
+        assertEquals("true", map.get("secure"));
     }
 }
