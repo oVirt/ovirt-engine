a2723f5af34352479f0be7036a2356b8c89d13d9
Martin Mucha <mmucha@redhat.com>
2016-01-25 19:26:10 +0100

    core: missing constructor
    absence of constructor which accepts EngineContext parameter can lead
    to runtime exceptions.
    
    Change-Id: I81ed218e127723855ecc8e2c71ce66b501f047fb
    Bug-Url: https://bugzilla.redhat.com/1299630
    Signed-off-by: Martin Mucha <mmucha@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetAllChildVlanInterfacesQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetAllChildVlanInterfacesQuery.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.ovirt.engine.core.bll.QueriesCommandBase;
+import org.ovirt.engine.core.bll.context.EngineContext;
 import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
 import org.ovirt.engine.core.common.queries.InterfaceAndIdQueryParameters;
 import org.ovirt.engine.core.utils.NetworkUtils;
@@ -12,10 +13,13 @@ import org.ovirt.engine.core.utils.NetworkUtils;
  * This query get interface and return all it's interface vlans, i.e input: eth2
  * return: eth2.4 eth2.5
  */
-public class GetAllChildVlanInterfacesQuery<P extends InterfaceAndIdQueryParameters>
-        extends QueriesCommandBase<P> {
+public class GetAllChildVlanInterfacesQuery<P extends InterfaceAndIdQueryParameters> extends QueriesCommandBase<P> {
     public GetAllChildVlanInterfacesQuery(P parameters) {
         super(parameters);
+    }
+
+    public GetAllChildVlanInterfacesQuery(P parameters, EngineContext engineContext) {
+        super(parameters, engineContext);
     }
 
     @Override
