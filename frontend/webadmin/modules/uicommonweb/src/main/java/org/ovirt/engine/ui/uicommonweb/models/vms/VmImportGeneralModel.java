oVirt/ovirt-engine

commit d35db9a644a8b00590553c8ba796c3378b949810

webadmin: fix import/general sub-tab
The general sub-tab in the import dialog was broken by recent changes in
the frontend - it didn't present the VM information.

This patch fixes the sub-tab that shows general information on VM in the
import dialog. I changed the sub-tab to be separate component that
doesn't inherit the VM general sub-tab in the virtual machines tab
because they differ in:

1. Layout - in the sub-tab which is presented in the import dialog, we
don't need to have the alerts tab since warnings/errors will be shown
differently

2. Items - the presented fields are different and over-time they are
expected to be changed in a way that they will be even more different

3. Configurability - in the import sub-tab some fields are editable.
currently only the name and the operating system but there should be
more, while in the general sub-tab nothing is editable

Change-Id: Ied6e9619ba6f539e42600d3650adff2b9272ac9e
Signed-off-by: Arik Hadas <ahadas@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/VmImportGeneralModel.java
@@ -1,9 +1,579 @@
 package org.ovirt.engine.ui.uicommonweb.models.vms;
 
-public class VmImportGeneralModel extends VmGeneralModel {
+import java.util.ArrayList;
 
-    @SuppressWarnings("unchecked")
-    public void setEntity(ImportVmData value) {
-        super.setEntity(value == null ? null : value.getVm());
+import org.ovirt.engine.core.common.businessentities.QuotaEnforcementTypeEnum;
+import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.VM;
+import org.ovirt.engine.core.common.businessentities.VmPauseStatus;
+import org.ovirt.engine.core.common.interfaces.SearchType;
+import org.ovirt.engine.core.common.queries.SearchParameters;
+import org.ovirt.engine.core.common.queries.VdcQueryReturnValue;
+import org.ovirt.engine.core.common.queries.VdcQueryType;
+import org.ovirt.engine.core.common.utils.ObjectUtils;
+import org.ovirt.engine.core.compat.StringHelper;
+import org.ovirt.engine.ui.frontend.AsyncQuery;
+import org.ovirt.engine.ui.frontend.Frontend;
+import org.ovirt.engine.ui.frontend.INewAsyncCallback;
+import org.ovirt.engine.ui.uicommonweb.dataprovider.AsyncDataProvider;
+import org.ovirt.engine.ui.uicompat.ConstantsManager;
+import org.ovirt.engine.ui.uicompat.EnumTranslator;
+import org.ovirt.engine.ui.uicompat.PropertyChangedEventArgs;
+
+public class VmImportGeneralModel extends AbstractGeneralModel<ImportVmData> {
+    private static final VmTemplateNameRenderer vmTemplateNameRenderer = new VmTemplateNameRenderer();
+    private static EnumTranslator translator = EnumTranslator.getInstance();
+
+    private String name;
+    private String description;
+    private String template;
+    private String definedMemory;
+    private String guestFreeCachedBufferedMemInfo;
+    private String minAllocatedMemory;
+    private String os;
+    private String defaultDisplayType;
+    private String quotaName;
+    private boolean quotaAvailable;
+    private int monitorCount;
+    private boolean hasDomain;
+    private boolean hasTimeZone;
+    private String usbPolicy;
+    private String cpuInfo;
+    private int guestCpuCount;
+    private boolean isHighlyAvailable;
+    private String priority;
+    private boolean hasAlert;
+    private String alert;
+    private String domain;
+    private String storageDomain;
+    private String timeZone;
+    private boolean hasDefaultHost;
+    private String defaultHost;
+    private boolean hasCustomProperties;
+    private String customProperties;
+    private String vmId;
+    private String fqdn;
+    private String compatibilityVersion;
+
+    public VmImportGeneralModel() {
+    }
+
+    @Override
+    protected void onEntityChanged() {
+        super.onEntityChanged();
+        if (getEntity() != null) {
+            updateProperties();
+        }
+    }
+
+    public String getOrigin() {
+        return translator.translate(getEntity().getVm().getOrigin());
+    }
+
+    private void updateProperties() {
+        VM vm = getEntity().getVm();
+
+        super.updateProperties(vm.getId());
+
+        setName(vm.getName());
+        setDescription(vm.getVmDescription());
+        setQuotaName(vm.getQuotaName() != null ? vm.getQuotaName() : ""); //$NON-NLS-1$
+        setQuotaAvailable(vm.getQuotaEnforcementType() != null
+        && !vm.getQuotaEnforcementType().equals(QuotaEnforcementTypeEnum.DISABLED));
+
+        setTemplate(vmTemplateNameRenderer.render(vm));
+
+        setDefinedMemory(vm.getVmMemSizeMb() + " MB"); //$NON-NLS-1$
+        setMinAllocatedMemory(vm.getMinAllocatedMem() + " MB"); //$NON-NLS-1$
+
+        if(vm.isRunningOrPaused() && vm.getGuestMemoryBuffered() != null && vm.getGuestMemoryCached() != null  && vm.getGuestMemoryFree() != null) {
+            setGuestFreeCachedBufferedMemInfo((vm.getGuestMemoryFree() / 1024L) + " / " // $NON-NLS-1$
+                    + (vm.getGuestMemoryBuffered() / 1024L)  + " / " // $NON-NLS-1$
+                    + (vm.getGuestMemoryCached() / 1024L) + " MB"); //$NON-NLS-1$
+        }
+        else {
+            setGuestFreeCachedBufferedMemInfo(null); // Handled in form
+        }
+
+        setOS(AsyncDataProvider.getInstance().getOsName(vm.getVmOsId()));
+
+        setDefaultDisplayType(translator.translate(vm.getDefaultDisplayType()));
+
+        setIsHighlyAvailable(vm.isAutoStartup());
+
+        setPriority(AsyncDataProvider.getInstance().priorityToString(vm.getPriority()));
+
+        setMonitorCount(vm.getNumOfMonitors());
+
+        setUsbPolicy(translator.translate(vm.getUsbPolicy()));
+
+        setCpuInfo(ConstantsManager.getInstance().getMessages().cpuInfoLabel(
+                vm.getNumOfCpus(),
+                vm.getNumOfSockets(),
+                vm.getCpuPerSocket()));
+
+        setGuestCpuCount(vm.getGuestCpuCount());
+
+        setHasDomain(AsyncDataProvider.getInstance().isWindowsOsType(vm.getVmOsId()));
+        if (vm.getVmInit() != null) {
+            setDomain(vm.getVmInit().getDomain());
+        }
+
+        setHasTimeZone(AsyncDataProvider.getInstance().isWindowsOsType(vm.getVmOsId()));
+        setTimeZone(vm.getTimeZone());
+
+        setHasCustomProperties(!StringHelper.isNullOrEmpty(vm.getCustomProperties()));
+        setCustomProperties(getHasCustomProperties() ? "Configured" : "Not-Configured"); //$NON-NLS-1$ //$NON-NLS-2$
+
+        setCompatibilityVersion(vm.getVdsGroupCompatibilityVersion() != null ?
+                vm.getVdsGroupCompatibilityVersion().toString() : ""); //$NON-NLS-1$
+
+        setVmId(vm.getId().toString());
+        setFqdn(vm.getVmFQDN());
+
+        setHasAlert(vm.getVmPauseStatus() != VmPauseStatus.NONE && vm.getVmPauseStatus() != VmPauseStatus.NOERR);
+        if (getHasAlert()) {
+            setAlert(translator.translate(vm.getVmPauseStatus()));
+        }
+        else {
+            setAlert(null);
+        }
+
+        setHasDefaultHost(vm.getDedicatedVmForVds() != null);
+        if (getHasDefaultHost()) {
+            Frontend.getInstance().runQuery(VdcQueryType.Search, new SearchParameters("Host: cluster = " + vm.getVdsGroupName() //$NON-NLS-1$
+                    + " sortby name", SearchType.VDS), new AsyncQuery(this, //$NON-NLS-1$
+                            new INewAsyncCallback() {
+                        @Override
+                        public void onSuccess(Object target, Object returnValue) {
+
+                            VmGeneralModel model = (VmGeneralModel) target;
+                            VM localVm = model.getEntity();
+                            if (localVm == null)
+                            {
+                                return;
+                            }
+                            ArrayList<VDS> hosts = ((VdcQueryReturnValue) returnValue).getReturnValue();
+                            for (VDS host : hosts)
+                            {
+                                if (localVm.getDedicatedVmForVds() != null
+                                        && host.getId().equals(localVm.getDedicatedVmForVds()))
+                                {
+                                    model.setDefaultHost(host.getName());
+                                    break;
+                                }
+                            }
+
+                        }
+                    }));
+        }
+        else {
+            setDefaultHost(ConstantsManager.getInstance().getConstants().anyHostInCluster());
+        }
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String value)
+    {
+        if (!ObjectUtils.objectsEqual(name, value))
+        {
+            name = value;
+            onPropertyChanged(new PropertyChangedEventArgs("Name")); //$NON-NLS-1$
+        }
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String value)
+    {
+        if (!ObjectUtils.objectsEqual(description, value))
+        {
+            description = value;
+            onPropertyChanged(new PropertyChangedEventArgs("Description")); //$NON-NLS-1$
+        }
+    }
+
+    public String getTemplate()
+    {
+        return template;
+    }
+
+    public void setTemplate(String value)
+    {
+        if (!ObjectUtils.objectsEqual(template, value))
+        {
+            template = value;
+            onPropertyChanged(new PropertyChangedEventArgs("Template")); //$NON-NLS-1$
+        }
+    }
+
+    public String getDefinedMemory()
+    {
+        return definedMemory;
+    }
+
+    public void setDefinedMemory(String value)
+    {
+        if (!ObjectUtils.objectsEqual(definedMemory, value))
+        {
+            definedMemory = value;
+            onPropertyChanged(new PropertyChangedEventArgs("DefinedMemory")); //$NON-NLS-1$
+        }
+    }
+
+    public String getGuestFreeCachedBufferedMemInfo()
+    {
+        return guestFreeCachedBufferedMemInfo;
+    }
+
+    public void setGuestFreeCachedBufferedMemInfo(String value)
+    {
+        if (!ObjectUtils.objectsEqual(guestFreeCachedBufferedMemInfo, value))
+        {
+            guestFreeCachedBufferedMemInfo = value;
+            onPropertyChanged(new PropertyChangedEventArgs("GuestFreeCachedBufferedMemInfo")); //$NON-NLS-1$
+        }
+    }
+
+    public String getMinAllocatedMemory()
+    {
+        return minAllocatedMemory;
+    }
+
+    public void setMinAllocatedMemory(String value)
+    {
+        if (!ObjectUtils.objectsEqual(minAllocatedMemory, value))
+        {
+            minAllocatedMemory = value;
+            onPropertyChanged(new PropertyChangedEventArgs("MinAllocatedMemory")); //$NON-NLS-1$
+        }
+    }
+
+    public String getOS()
+    {
+        return os;
+    }
+
+    public void setOS(String value)
+    {
+        if (!ObjectUtils.objectsEqual(os, value))
+        {
+            os = value;
+            onPropertyChanged(new PropertyChangedEventArgs("OS")); //$NON-NLS-1$
+        }
+    }
+
+    public String getDefaultDisplayType()
+    {
+        return defaultDisplayType;
+    }
+
+    public void setDefaultDisplayType(String value)
+    {
+        if (!ObjectUtils.objectsEqual(defaultDisplayType, value))
+        {
+            defaultDisplayType = value;
+            onPropertyChanged(new PropertyChangedEventArgs("DefaultDisplayType")); //$NON-NLS-1$
+        }
+    }
+
+    public String getQuotaName() {
+        return quotaName;
+    }
+
+    public void setQuotaName(String quotaName) {
+        this.quotaName = quotaName;
+    }
+
+    public boolean isQuotaAvailable() {
+        return quotaAvailable;
+    }
+
+    public void setQuotaAvailable(boolean quotaAvailable) {
+        this.quotaAvailable = quotaAvailable;
+    }
+
+    public int getMonitorCount()
+    {
+        return monitorCount;
+    }
+
+    public void setMonitorCount(int value)
+    {
+        if (monitorCount != value)
+        {
+            monitorCount = value;
+            onPropertyChanged(new PropertyChangedEventArgs("MonitorCount")); //$NON-NLS-1$
+        }
+    }
+
+    public boolean getHasDomain()
+    {
+        return hasDomain;
+    }
+
+    public void setHasDomain(boolean value)
+    {
+        if (hasDomain != value)
+        {
+            hasDomain = value;
+            onPropertyChanged(new PropertyChangedEventArgs("HasDomain")); //$NON-NLS-1$
+        }
+    }
+
+    public boolean getHasTimeZone()
+    {
+        return hasTimeZone;
+    }
+
+    public void setHasTimeZone(boolean value)
+    {
+        if (hasTimeZone != value)
+        {
+            hasTimeZone = value;
+            onPropertyChanged(new PropertyChangedEventArgs("HasTimeZone")); //$NON-NLS-1$
+        }
+    }
+
+    public String getUsbPolicy()
+    {
+        return usbPolicy;
+    }
+
+    public void setUsbPolicy(String value)
+    {
+        if (!ObjectUtils.objectsEqual(usbPolicy, value))
+        {
+            usbPolicy = value;
+            onPropertyChanged(new PropertyChangedEventArgs("UsbPolicy")); //$NON-NLS-1$
+        }
+    }
+
+    public String getCpuInfo()
+    {
+        return cpuInfo;
+    }
+
+    public void setCpuInfo(String value)
+    {
+        if (!ObjectUtils.objectsEqual(cpuInfo, value))
+        {
+            cpuInfo = value;
+            onPropertyChanged(new PropertyChangedEventArgs("CpuInfo")); //$NON-NLS-1$
+        }
+    }
+
+    public String getGuestCpuCount() {
+        if (guestCpuCount > 0) {
+            return String.valueOf(guestCpuCount);
+        } else {
+            return ConstantsManager.getInstance().getConstants().notAvailableLabel();
+        }
+    }
+
+    public void setGuestCpuCount(int value) {
+        guestCpuCount = value;
+    }
+
+    public boolean getIsHighlyAvailable()
+    {
+        return isHighlyAvailable;
+    }
+
+    public void setIsHighlyAvailable(boolean value)
+    {
+        if (isHighlyAvailable != value)
+        {
+            isHighlyAvailable = value;
+            onPropertyChanged(new PropertyChangedEventArgs("IsHighlyAvailable")); //$NON-NLS-1$
+        }
+    }
+
+    public String getPriority()
+    {
+        return priority;
+    }
+
+    public void setPriority(String value)
+    {
+        if (!ObjectUtils.objectsEqual(priority, value))
+        {
+            priority = value;
+            onPropertyChanged(new PropertyChangedEventArgs("Priority")); //$NON-NLS-1$
+        }
+    }
+
+    public boolean getHasAlert()
+    {
+        return hasAlert;
+    }
+
+    public void setHasAlert(boolean value)
+    {
+        if (hasAlert != value)
+        {
+            hasAlert = value;
+            onPropertyChanged(new PropertyChangedEventArgs("HasAlert")); //$NON-NLS-1$
+        }
+    }
+
+    public String getAlert()
+    {
+        return alert;
+    }
+
+    public void setAlert(String value)
+    {
+        if (!ObjectUtils.objectsEqual(alert, value))
+        {
+            alert = value;
+            onPropertyChanged(new PropertyChangedEventArgs("Alert")); //$NON-NLS-1$
+        }
+    }
+
+    public String getDomain()
+    {
+        return domain;
+    }
+    public void setDomain(String value)
+    {
+        if (!ObjectUtils.objectsEqual(domain, value))
+        {
+            domain = value;
+            onPropertyChanged(new PropertyChangedEventArgs("Domain")); //$NON-NLS-1$
+        }
+    }
+
+    public String getStorageDomain()
+    {
+        return storageDomain;
+    }
+
+    public void setStorageDomain(String value)
+    {
+        if (!ObjectUtils.objectsEqual(storageDomain, value))
+        {
+            storageDomain = value;
+            onPropertyChanged(new PropertyChangedEventArgs("StorageDomain")); //$NON-NLS-1$
+        }
+    }
+
+    public String getTimeZone()
+    {
+        return timeZone;
+    }
+
+    public void setTimeZone(String value)
+    {
+        if (!ObjectUtils.objectsEqual(timeZone, value))
+        {
+            timeZone = value;
+            onPropertyChanged(new PropertyChangedEventArgs("TimeZone")); //$NON-NLS-1$
+        }
+    }
+
+    public boolean getHasDefaultHost()
+    {
+        return hasDefaultHost;
+    }
+
+    public void setHasDefaultHost(boolean value)
+    {
+        if (hasDefaultHost != value)
+        {
+            hasDefaultHost = value;
+            onPropertyChanged(new PropertyChangedEventArgs("HasDefaultHost")); //$NON-NLS-1$
+        }
+    }
+
+    public String getDefaultHost()
+    {
+        return defaultHost;
+    }
+
+    public void setDefaultHost(String value)
+    {
+        if (!ObjectUtils.objectsEqual(defaultHost, value))
+        {
+            defaultHost = value;
+            onPropertyChanged(new PropertyChangedEventArgs("DefaultHost")); //$NON-NLS-1$
+        }
+    }
+
+    public boolean getHasCustomProperties()
+    {
+        return hasCustomProperties;
+    }
+
+    public void setHasCustomProperties(boolean value)
+    {
+        if (hasCustomProperties != value)
+        {
+            hasCustomProperties = value;
+            onPropertyChanged(new PropertyChangedEventArgs("HasCustomProperties")); //$NON-NLS-1$
+        }
+    }
+
+    public String getCustomProperties()
+    {
+        return customProperties;
+    }
+
+    public void setCustomProperties(String value)
+    {
+        if (!ObjectUtils.objectsEqual(customProperties, value))
+        {
+            customProperties = value;
+            onPropertyChanged(new PropertyChangedEventArgs("CustomProperties")); //$NON-NLS-1$
+        }
+    }
+
+    public String getVmId()
+    {
+        return vmId;
+    }
+
+    public void setVmId(String value)
+    {
+        if (!ObjectUtils.objectsEqual(vmId, value))
+        {
+            vmId = value;
+            onPropertyChanged(new PropertyChangedEventArgs("VmId")); //$NON-NLS-1$
+        }
+    }
+
+    public String getFqdn()
+    {
+        return fqdn;
+    }
+
+    public void setFqdn(String value)
+    {
+        if (!ObjectUtils.objectsEqual(fqdn, value))
+        {
+            fqdn = value;
+            onPropertyChanged(new PropertyChangedEventArgs("FQDN")); //$NON-NLS-1$
+        }
+    }
+
+    public String getCompatibilityVersion()
+    {
+        return compatibilityVersion;
+    }
+
+    public void setCompatibilityVersion(String value)
+    {
+        if (!ObjectUtils.objectsEqual(compatibilityVersion, value))
+        {
+            compatibilityVersion = value;
+            onPropertyChanged(new PropertyChangedEventArgs("CompatibilityVersion")); //$NON-NLS-1$
+        }
     }
 }

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/vm/VmImportGeneralModelForm.java
@@ -1,0 +1,117 @@
+package org.ovirt.engine.ui.common.widget.uicommon.vm;
+
+import org.ovirt.engine.ui.common.CommonApplicationConstants;
+import org.ovirt.engine.ui.common.gin.AssetProvider;
+import org.ovirt.engine.ui.common.uicommon.model.ModelProvider;
+import org.ovirt.engine.ui.common.widget.form.FormItem;
+import org.ovirt.engine.ui.common.widget.form.FormItem.DefaultValueCondition;
+import org.ovirt.engine.ui.common.widget.label.TextBoxLabel;
+import org.ovirt.engine.ui.common.widget.uicommon.AbstractModelBoundFormWidget;
+import org.ovirt.engine.ui.uicommonweb.models.vms.VmImportGeneralModel;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.SimpleBeanEditorDriver;
+
+public class VmImportGeneralModelForm extends AbstractModelBoundFormWidget<VmImportGeneralModel> {
+
+    interface Driver extends SimpleBeanEditorDriver<VmImportGeneralModel, VmImportGeneralModelForm> {
+    }
+
+    TextBoxLabel name = new TextBoxLabel();
+    TextBoxLabel description = new TextBoxLabel();
+    @Path("OS")
+    TextBoxLabel os = new TextBoxLabel();
+    TextBoxLabel template = new TextBoxLabel();
+    TextBoxLabel definedMemory = new TextBoxLabel();
+    TextBoxLabel defaultDisplayType = new TextBoxLabel();
+    TextBoxLabel priority = new TextBoxLabel();
+    TextBoxLabel minAllocatedMemory = new TextBoxLabel();
+    TextBoxLabel guestFreeCachedBufferedMemInfo = new TextBoxLabel();
+    TextBoxLabel usbPolicy = new TextBoxLabel();
+    TextBoxLabel defaultHost = new TextBoxLabel();
+    TextBoxLabel customProperties = new TextBoxLabel();
+    TextBoxLabel domain = new TextBoxLabel();
+    TextBoxLabel compatibilityVersion = new TextBoxLabel();
+    TextBoxLabel vmId = new TextBoxLabel();
+    TextBoxLabel fqdn = new TextBoxLabel();
+    TextBoxLabel cpuInfo = new TextBoxLabel();
+    TextBoxLabel guestCpuCount = new TextBoxLabel();
+    TextBoxLabel quotaName = new TextBoxLabel();
+    TextBoxLabel origin = new TextBoxLabel();
+
+    @Ignore
+    TextBoxLabel monitorCount = new TextBoxLabel();
+
+    private final static CommonApplicationConstants constants = AssetProvider.getConstants();
+    private final Driver driver = GWT.create(Driver.class);
+
+    public VmImportGeneralModelForm(ModelProvider<VmImportGeneralModel> modelProvider) {
+        super(modelProvider, 3, 7);
+    }
+
+    @Override
+    protected void doEdit(VmImportGeneralModel model) {
+        driver.edit(model);
+
+        // Required because of type conversion
+        monitorCount.setText(Integer.toString(getModel().getMonitorCount()));
+    }
+
+    public void initialize() {
+        driver.initialize(this);
+
+        formBuilder.addFormItem(new FormItem(constants.nameVm(), name, 0, 0));
+        formBuilder.addFormItem(new FormItem(constants.osVm(), os, 1, 0));
+        formBuilder.addFormItem(new FormItem(constants.descriptionVm(), description, 2, 0));
+        formBuilder.addFormItem(new FormItem(constants.templateVm(), template, 3, 0));
+        formBuilder.addFormItem(new FormItem(constants.videoType(), defaultDisplayType, 4, 0));
+        formBuilder.addFormItem(new FormItem(constants.priorityVm(), priority, 5, 0));
+        formBuilder.addFormItem(new FormItem(constants.definedMemoryVm(), definedMemory, 6, 0));
+
+        formBuilder.addFormItem(new FormItem(constants.physMemGauranteedVm(), minAllocatedMemory, 0, 1));
+        formBuilder.addFormItem(new FormItem(constants.guestFreeCachedBufferedMemInfo(), guestFreeCachedBufferedMemInfo, 1, 1)
+        .withDefaultValue(constants.notConfigured(), new DefaultValueCondition() {
+            @Override
+            public boolean showDefaultValue() {
+                return getModel().getGuestFreeCachedBufferedMemInfo() == null;
+            }
+        }));
+        formBuilder.addFormItem(new FormItem(constants.numOfCpuCoresVm(), cpuInfo, 2, 1));
+        formBuilder.addFormItem(new FormItem(constants.GuestCpuCount(), guestCpuCount, 3, 1));
+        formBuilder.addFormItem(new FormItem(constants.numOfMonitorsVm(), monitorCount, 4, 1));
+        formBuilder.addFormItem(new FormItem(constants.usbPolicyVm(), usbPolicy, 5, 1));
+        formBuilder.addFormItem(new FormItem(constants.originVm(), origin, 6, 1));
+
+        formBuilder.addFormItem(new FormItem(constants.runOnVm(), defaultHost, 0, 2));
+        formBuilder.addFormItem(new FormItem(constants.customPropertiesVm(), customProperties, 1, 2));
+        formBuilder.addFormItem(new FormItem(constants.clusterCompatibilityVersionVm(), compatibilityVersion, 2, 2));
+        formBuilder.addFormItem(new FormItem(constants.vmId(), vmId, 3, 2));
+
+        formBuilder.addFormItem(new FormItem(constants.quotaVm(), quotaName, 4, 2) {
+            @Override
+            public boolean getIsAvailable() {
+                return getModel().isQuotaAvailable();
+            }
+        }.withDefaultValue(constants.notConfigured(), new DefaultValueCondition() {
+            @Override
+            public boolean showDefaultValue() {
+                String quotaName = getModel().getQuotaName();
+                return quotaName == null || "".equals(quotaName);
+            }
+        }));
+        formBuilder.addFormItem(new FormItem(constants.domainVm(), domain, 5, 2) {
+            @Override
+            public boolean getIsAvailable() {
+                return getModel().getHasDomain();
+            }
+        });
+
+        formBuilder.addFormItem(new FormItem(constants.fqdn(), fqdn, 6, 2) {
+            @Override
+            public boolean getIsAvailable() {
+                String fqdn = getModel().getFqdn();
+                return !(fqdn == null || fqdn.isEmpty());
+            }
+        });
+    }
+}

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/gin/uicommon/VirtualMachineModule.java
@@ -26,15 +26,16 @@ import org.ovirt.engine.ui.uicommonweb.models.Model;
 import org.ovirt.engine.ui.uicommonweb.models.configure.PermissionListModel;
 import org.ovirt.engine.ui.uicommonweb.models.configure.scheduling.affinity_groups.list.VmAffinityGroupListModel;
 import org.ovirt.engine.ui.uicommonweb.models.storage.ImportCloneModel;
-import org.ovirt.engine.ui.uicommonweb.models.vms.ImportVmFromExportDomainModel;
-import org.ovirt.engine.ui.uicommonweb.models.vms.ImportVmsModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.AttachDiskModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.EditDiskModel;
+import org.ovirt.engine.ui.uicommonweb.models.vms.ImportVmFromExportDomainModel;
+import org.ovirt.engine.ui.uicommonweb.models.vms.ImportVmsModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.NewDiskModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.VmAppListModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.VmDiskListModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.VmEventListModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.VmGeneralModel;
+import org.ovirt.engine.ui.uicommonweb.models.vms.VmImportGeneralModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.VmInterfaceListModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.VmListModel;
 import org.ovirt.engine.ui.uicommonweb.models.vms.VmSessionsModel;
@@ -378,6 +379,7 @@ public class VirtualMachineModule extends AbstractGinModule {
     protected void configure() {
         bind(new TypeLiteral<VmListModel<Void>>() {}).in(Singleton.class);
         bind(VmGeneralModel.class).in(Singleton.class);
+        bind(VmImportGeneralModel.class).in(Singleton.class);
         bind(VmSessionsModel.class).in(Singleton.class);
         bind(VmDiskListModel.class).in(Singleton.class);
         bind(VmInterfaceListModel.class).in(Singleton.class);
@@ -390,6 +392,8 @@ public class VirtualMachineModule extends AbstractGinModule {
         // Form Detail Models
         bind(new TypeLiteral<DetailModelProvider<VmListModel<Void>, VmGeneralModel>>(){})
             .to(new TypeLiteral<DetailTabModelProvider<VmListModel<Void>, VmGeneralModel>>(){}).in(Singleton.class);
+        bind(new TypeLiteral<DetailModelProvider<ImportVmsModel, VmImportGeneralModel>>(){})
+            .to(new TypeLiteral<DetailTabModelProvider<ImportVmsModel, VmImportGeneralModel>>(){}).in(Singleton.class);
         bind(new TypeLiteral<DetailModelProvider<VmListModel<Void>, VmSessionsModel>>(){})
            .to(new TypeLiteral<DetailTabModelProvider<VmListModel<Void>, VmSessionsModel>>(){}).in(Singleton.class);
         // Search-able Detail Models

commit c281caeec60d17d020a5cbcb8a30d642dcb2bf2b

webadmin: Fix ClassCastException when importing VMs with pinned host
Fixed regression introduced in commit d35db9a644a8b00590553c8ba796c3378b949810
that changed VmImportGeneralModel to no longer extend VmGeneralModel, but still
treated it as such when casting the `target` of async callback.

Chnaged async callback to use outer class instead of the legacy `target`
parameter.

Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1293269
Change-Id: I390cc0f57c237c28ee1795e77fae7294d51ffec0
Signed-off-by: Martin Betak <mbetak@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/VmImportGeneralModel.java
@@ -194,15 +194,14 @@ public class VmImportGeneralModel extends AbstractGeneralModel<ImportVmData> {
                         @Override
                         public void onSuccess(Object target, Object returnValue) {
 
-                            VmGeneralModel model = (VmGeneralModel) target;
-                            VM localVm = model.getEntity();
+                            VM localVm = getEntity() != null ? getEntity().getVm() : null;
                             if (localVm == null) {
                                 return;
                             }
                             ArrayList<VDS> hosts = ((VdcQueryReturnValue) returnValue).getReturnValue();
                             for (VDS host : hosts) {
                                 if (localVm.getDedicatedVmForVdsList().contains(host.getId())) {
-                                    model.setDefaultHost(host.getName());
+                                    setDefaultHost(host.getName());
                                     break;
                                 }
                             }