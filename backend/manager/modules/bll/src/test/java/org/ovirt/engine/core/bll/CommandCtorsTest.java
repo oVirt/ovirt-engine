910316e25e6b9ff7aa944efb842821a5cfc0648c
Idan Shaby <ishaby@redhat.com>
2016-01-31 16:57:27 +0200

    backend: remove redundant test testInternalAnnotationCtors
    CommandCtorsTest.testInternalAnnotationCtors verifies that all the
    commands that are annotated with @InternalCommandAttribute contain a
    constructor which its last argument is CommandContext (only those who
    are listed in VdcActionType, i.e, not abstract command classes).
    
    Since testCommandMandatoryConstructorsExistence verifies that all these
    commands contain the mandatory constructor (a constructor that
    receives parameters and context objects), it also covers the validation
    of testInternalAnnotationCtors.
    
    Change-Id: Iab7899fb5d18c9ca79e2276734e371885a41f86d
    Signed-off-by: Idan Shaby <ishaby@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/CommandCtorsTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/CommandCtorsTest.java
@@ -2,7 +2,6 @@ package org.ovirt.engine.core.bll;
 
 import static org.hamcrest.Matchers.empty;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
@@ -47,27 +46,6 @@ public class CommandCtorsTest {
         // Signature for a constructor that receives parameters and context objects.
         mandatoryConstructorSignature =
                 createConstructorSignaturePredicate(VdcActionParametersBase.class, CommandContext.class);
-    }
-
-    @Test
-    public void testInternalAnnotationCtors() {
-
-        for (VdcActionType commandType : VdcActionType.values()) {
-            if (!commandType.equals(VdcActionType.Unknown)) {
-                Class<CommandBase<? extends VdcActionParametersBase>> commandClass = CommandsFactory.getCommandClass(commandType.toString());
-                if (commandClass != null) {
-                    if (commandClass.isAnnotationPresent(InternalCommandAttribute.class)) {
-                        boolean foundContextCtor = false;
-                        for (Constructor<?> ctor : commandClass.getConstructors()) {
-                          if (ctor.getParameterTypes()[ctor.getParameterTypes().length-1].equals(CommandContext.class)) {
-                              foundContextCtor = true;
-                          }
-                        }
-                        assertTrue("no context ctor for class " + commandClass.getSimpleName(), foundContextCtor);
-                    }
-                }
-            }
-        }
     }
 
     /**
