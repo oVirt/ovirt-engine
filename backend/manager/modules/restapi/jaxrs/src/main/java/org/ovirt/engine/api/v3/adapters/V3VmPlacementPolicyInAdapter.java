278a6a9e5b26435c584b7a184e1d34881c2cde05
Juan Hernandez <juan.hernandez@redhat.com>
2016-02-03 10:56:52 +0100

    restapi: V3 compatibility for pinnig to one host
    In version 4 pinning to a host is done using a "hosts" element,
    containing exactly one "host" element. In version 3 this could also be
    done using directly only the "host" element. Now that the API supports
    multiple versions this can be rea-added to version 3 without touching
    the V4 implementation.
    
    Change-Id: Idaba066fe876fada4138ed3d97902f96e92042c3
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/v3/adapters/V3VmPlacementPolicyInAdapter.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/v3/adapters/V3VmPlacementPolicyInAdapter.java
@@ -18,6 +18,7 @@ package org.ovirt.engine.api.v3.adapters;
 
 import static org.ovirt.engine.api.v3.adapters.V3InAdapters.adaptIn;
 
+import org.ovirt.engine.api.model.Host;
 import org.ovirt.engine.api.model.Hosts;
 import org.ovirt.engine.api.model.VmPlacementPolicy;
 import org.ovirt.engine.api.v3.V3Adapter;
@@ -34,6 +35,15 @@ public class V3VmPlacementPolicyInAdapter implements V3Adapter<V3VmPlacementPoli
             to.setHosts(new Hosts());
             to.getHosts().getHosts().addAll(adaptIn(from.getHosts().getHosts()));
         }
+
+        // V3 allowed specifying only one host, using "host" instead of "hosts":
+        if (from.isSetHost() && !from.isSetHosts()) {
+            Hosts hosts = new Hosts();
+            Host host = adaptIn(from.getHost());
+            hosts.getHosts().add(host);
+            to.setHosts(hosts);
+        }
+
         return to;
     }
 }
--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/v3/adapters/V3VmPlacementPolicyOutAdapter.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/v3/adapters/V3VmPlacementPolicyOutAdapter.java
@@ -18,8 +18,12 @@ package org.ovirt.engine.api.v3.adapters;
 
 import static org.ovirt.engine.api.v3.adapters.V3OutAdapters.adaptOut;
 
+import java.util.List;
+
+import org.ovirt.engine.api.model.Host;
 import org.ovirt.engine.api.model.VmPlacementPolicy;
 import org.ovirt.engine.api.v3.V3Adapter;
+import org.ovirt.engine.api.v3.types.V3Host;
 import org.ovirt.engine.api.v3.types.V3Hosts;
 import org.ovirt.engine.api.v3.types.V3VmPlacementPolicy;
 
@@ -33,6 +37,13 @@ public class V3VmPlacementPolicyOutAdapter implements V3Adapter<VmPlacementPolic
         if (from.isSetHosts()) {
             to.setHosts(new V3Hosts());
             to.getHosts().getHosts().addAll(adaptOut(from.getHosts().getHosts()));
+
+            // V3 allowed specifying only one host, using the "host" element instead of "hosts":
+            List<Host> hosts = from.getHosts().getHosts();
+            if (hosts.size() == 1) {
+                V3Host host = adaptOut(hosts.get(0));
+                to.setHost(host);
+            }
         }
         return to;
     }
