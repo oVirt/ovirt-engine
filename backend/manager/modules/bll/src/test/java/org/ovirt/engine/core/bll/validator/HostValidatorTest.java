49b09ed2bb2ebc8f190758bbc5d7fec06560a7cd
Moti Asayag <masayag@redhat.com>
2016-02-10 23:56:45 +0200

    core: Block adding host with XML-RPC protocol to 3.6 cluster
    Cluster levels 3.6 and above support only JSON-RPC protocol.
    Attempt to add a host which is configured to work with XML-RPC
    to 3.6 cluster and above, should be blocked.
    
    Change-Id: I7a6582113ba323b54361ddf4e49cceb0ee52da00
    Bug-Url: https://bugzilla.redhat.com/1289868
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/validator/HostValidatorTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/validator/HostValidatorTest.java
@@ -348,6 +348,14 @@ public class HostValidatorTest {
     }
 
     @Test
+    public void testValidateXmlProtocolSupportedForCluster() {
+        VDSGroup cluster = mock(VDSGroup.class);
+        when(cluster.getCompatibilityVersion()).thenReturn(Version.v3_5);
+        validator = mockHostForProtocol(VdsProtocol.XML);
+        assertThat(validator.protocolIsNotXmlrpc(cluster), isValid());
+    }
+
+    @Test
     public void testValidateJsonProtocolForCluster() {
         VDSGroup cluster = mock(VDSGroup.class);
         when(cluster.getCompatibilityVersion()).thenReturn(Version.v3_6);
