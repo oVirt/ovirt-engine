baab1d74d976db6b00f0c087514be0512d9e6607
Maor Lipchuk <mlipchuk@redhat.com>
2016-02-14 03:06:46 +0200

    core: Set parent command for child comamnd.
    CreateAllSnapshotsFromVmCommand should initialize its child commands with
    parent action type and parameters of CreateAllSnapshotsFromVmCommand
    
    Change-Id: I0d38dd9904a3baad487114302dd75da87bb8122f
    Bug-Url: https://bugzilla.redhat.com/1305809
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/snapshots/CreateAllSnapshotsFromVmCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/snapshots/CreateAllSnapshotsFromVmCommand.java
@@ -346,7 +346,10 @@ public class CreateAllSnapshotsFromVmCommand<T extends CreateAllSnapshotsFromVmP
         createParams.setStorageDomainId(cinderDisk.getStorageIds().get(0));
         createParams.setDescription(getParameters().getDescription());
         createParams.setSnapshotType(getParameters().getSnapshotType());
-        return withRootCommandInfo(createParams);
+        createParams.setSnapshotType(getParameters().getSnapshotType());
+        createParams.setParentCommand(getActionType());
+        createParams.setParentParameters(getParameters());
+        return createParams;
     }
 
     private void fastForwardDisksToActiveSnapshot() {
