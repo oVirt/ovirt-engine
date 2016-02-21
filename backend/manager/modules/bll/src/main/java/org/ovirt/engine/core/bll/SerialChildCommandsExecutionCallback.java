53306594d2ec4b2a64a816df198bacd195fcb94b
Liron Aravot <laravot@redhat.com>
2016-02-21 17:21:43 +0200

    core: removing ChildCommandsCallbackBase
    Currently there is no need to keep ConcurrentChildCommandsExecutionCallback and
    ChildCommandsCallbackBase as separate classes, therefore this patch removes
    ChildCommandsCallbackBase.
    
    Change-Id: I5e6a7b24bcf4a5e27b0455c82b5bbbf194c18680
    Signed-off-by: Liron Aravot <laravot@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/SerialChildCommandsExecutionCallback.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/SerialChildCommandsExecutionCallback.java
@@ -4,16 +4,12 @@ import java.util.List;
 
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.backendcompat.CommandExecutionStatus;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A callback for commands that are executing their child commands serially. Note that this callback supports execution
  * of child commands until a failure or until successful completion.
  */
-public class SerialChildCommandsExecutionCallback extends ChildCommandsCallbackBase {
-
-    private static final Logger log = LoggerFactory.getLogger(SerialChildCommandsExecutionCallback.class);
+public class SerialChildCommandsExecutionCallback extends ConcurrentChildCommandsExecutionCallback {
 
     @Override
     protected void childCommandsExecutionEnded(CommandBase<?> command,
@@ -21,7 +17,6 @@ public class SerialChildCommandsExecutionCallback extends ChildCommandsCallbackB
             List<Guid> childCmdIds,
             CommandExecutionStatus status,
             int completedChildren) {
-        Guid cmdId = command.getCommandId();
         SerialChildExecutingCommand serialChildExecutingCommand = (SerialChildExecutingCommand) command;
         if (!anyFailed || serialChildExecutingCommand.ignoreChildCommandFailure()) {
             try {
@@ -30,18 +25,11 @@ public class SerialChildCommandsExecutionCallback extends ChildCommandsCallbackB
                     return;
                 }
             } catch (Exception e) {
-                log.info("Command '{}' id: '{}' failed when attempting to perform the next operation, marking as FAILED '{}'",
-                        command.getActionType(),
-                        cmdId,
-                        childCmdIds,
-                        command.getCommandStatus());
                 serialChildExecutingCommand.handleFailure();
-                anyFailed = true;
+                throw e;
             }
         } else {
             serialChildExecutingCommand.handleFailure();
         }
-
-        setCommandEndStatus(command, anyFailed, status, childCmdIds);
     }
 }
