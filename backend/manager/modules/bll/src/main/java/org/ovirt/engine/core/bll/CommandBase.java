df9aa9132341d0d07cad2ed202616ed73973e844
Maor Lipchuk <mlipchuk@redhat.com>
2016-02-23 11:31:16 +0200

    core: Remove condition for AsyncTaskType.notSupported.
    In some cases the engine calls commands which
    have async calls to VDSM but in some scenarios, 
    those async calls, eventually are not getting called.
    
    In case that the async calls are not getting called,
    the engine should set the command as succeeded since
     there are no tasks it should wait for to determined
     the command's status.
    
    Change-Id: I49b7f506dc1142c3a559d0e6f64813e2ade02254
    Bug-Url: https://bugzilla.redhat.com/1310390
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandBase.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandBase.java
@@ -1322,8 +1322,7 @@ public abstract class CommandBase<T extends VdcActionParametersBase>
             } else {
                 // if the command is not an async task and has no custom callback
                 // set the status to ENDED_SUCCESSFULLY if the status is ACTIVE
-                if (getTaskType() == AsyncTaskType.notSupported &&
-                        getReturnValue().getVdsmTaskIdList().isEmpty() &&
+                if (getReturnValue().getVdsmTaskIdList().isEmpty() &&
                         getCallback() == null &&
                         commandStatus == CommandStatus.ACTIVE) {
                     setCommandStatus(CommandStatus.ENDED_SUCCESSFULLY);
