ab9173a856ba3df748fe789615bce9e103f14640
Jakub Niedermertl <jniederm@redhat.com>
2016-02-24 19:37:21 +0100

    core: Console stealing fix
    Only users with permission "VM" > "Administrative Operations" >
    "Override opened console session" (in code ActionGroup.RECONNECT_TO_VM)
    are able to connect to console someone else already have connected to
    (before next restart). This applies to if "Edit" VM dialog > "Console"
    tab > "Advanced parameters" > "Disable strict user checking" is disabled
    (in code VmBase#allowConsoleReconnect == false).
    
    If Strict user checking is disabled, anyone can steal console form
    anyone.
    
    Method SetVmTicketCommand#checkPermissions was removed and thus its
    tests were removed.
    
    "General command failure" was removed. Any error in SetVmTicketCommand
    that is not related to unauthorized console stealing is reporated by
    ConfigureConsoleOptionsQuery as EngineMessage.SETTING_VM_TICKET_FAILED.
    
    Previously it might happen that unauthorized attempt to steal console
    resulted in locked screen for current console user. This was caused by
    firing VDSM command "desktopLogin" earlier than execution
    SetVmTicketCommand (which actually verifies the permissions).
    
    It is solved by fixing frontend permission checking that prevents firing
    any command if SetVmTicketCommand would fail on permission check. User
    is notified about console being used by other user by pure fronend
    dialog.
    
      VM#getConsoleCurrentUserName() was replace by
      VM#getConsoleCurrentUserId() in ConsoleModel
      #executeCommandWithConsoleSafenessWarning() as
      getConsoleCurrentUserName() is not reliable (it is nulled in
      VdsEventListener#processOnClientIpChange()).
    
    Change-Id: Ib47107b2e2f85e4d05b13c3fe35ab4ad449d0ca5
    Bug-Url: https://bugzilla.redhat.com/1297018
    Signed-off-by: Jakub Niedermertl <jniederm@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/SetVmTicketCommandTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/SetVmTicketCommandTest.java
@@ -2,29 +2,11 @@ package org.ovirt.engine.core.bll;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.ovirt.engine.core.bll.utils.PermissionSubject;
-import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.common.action.SetVmTicketParameters;
-import org.ovirt.engine.core.common.businessentities.ActionGroup;
-import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.aaa.DbUser;
-import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
 public class SetVmTicketCommandTest {
     // The command that will be tested:
@@ -71,72 +53,4 @@ public class SetVmTicketCommandTest {
         command.setCurrentUser(user);
         assertEquals(command.getConsoleUserName(), "Legolas@MiddleEarth.com");
     }
-
-    @Test
-    public void checkPermissionsAdminStealsFromOtherAdmin() {
-        testCheckPermissions(true, true, true);
-    }
-
-    @Test
-    public void checkPermissionsUserStealsFromOtherAdmin() {
-        testCheckPermissions(false, true, false);
-    }
-
-    @Test
-    public void checkPermissionsAdminStealsFromOtherUser() {
-        testCheckPermissions(true, false, true);
-    }
-
-    @Test
-    public void checkPermissionsUserStealsFromOtherUser() {
-        testCheckPermissions(false, false, true);
-    }
-
-    private void testCheckPermissions(boolean isStealerAdmin, boolean isCurrentConsoleUserAdmin, boolean stealingAllowed) {
-        final Guid vmId = Guid.createGuidFromString("00000000-0000-0000-0000-000000000001");
-        final Guid currentUserId = Guid.createGuidFromString("00000000-0000-0000-0000-000000000002");
-        Guid vmConsoleUserId = Guid.createGuidFromString("00000000-0000-0000-0000-000000000003");
-        final DbUser user = mock(DbUser.class);
-        when(user.getId()).thenReturn(currentUserId);
-        when(user.isAdmin()).thenReturn(isStealerAdmin);
-        SetVmTicketCommand<SetVmTicketParameters> commandSpy = spy(command);
-        when(commandSpy.getCurrentUser()).thenReturn(user);
-        VM vm = mock(VM.class);
-        when(vm.getId()).thenReturn(vmId);
-        when(vm.getConsoleUserId()).thenReturn(vmConsoleUserId);
-        when(vm.getAllowConsoleReconnect()).thenReturn(false);
-        when(commandSpy.getVm()).thenReturn(vm);
-        DbUser vmConsoleUser = mock(DbUser.class);
-        when(vmConsoleUser.getId()).thenReturn(currentUserId);
-        when(vmConsoleUser.isAdmin()).thenReturn(isCurrentConsoleUserAdmin);
-        DbFacade dbFacade = mock(DbFacade.class, RETURNS_DEEP_STUBS);
-        when(dbFacade.getDbUserDao().get(eq(vmConsoleUserId))).thenReturn(vmConsoleUser);
-        when(commandSpy.getDbFacade()).thenReturn(dbFacade);
-        doAnswer(new Answer<Boolean>() {
-            @Override
-            public Boolean answer(InvocationOnMock invocation) throws Throwable {
-                if (Arrays.deepEquals(invocation.getArguments(), new Object[] {
-                        currentUserId, ActionGroup.CONNECT_TO_VM, vmId, VdcObjectType.VM
-                })) {
-                    return true;
-                }
-                if (Arrays.deepEquals(invocation.getArguments(), new Object[] {
-                        currentUserId, ActionGroup.RECONNECT_TO_VM, vmId, VdcObjectType.VM
-                })) {
-                    return true;
-                }
-                throw new RuntimeException(
-                        String.format("Unexpected arguments: %s", Arrays.toString(invocation.getArguments())));
-            }
-        }).when(commandSpy).checkUserAuthorization(any(Guid.class), any(ActionGroup.class), any(Guid.class), any(VdcObjectType.class));
-        List<PermissionSubject> permissionSubjects = Arrays.asList(
-                new PermissionSubject(vmId, VdcObjectType.VM, ActionGroup.CONNECT_TO_VM),
-                new PermissionSubject(vmId, VdcObjectType.VM, ActionGroup.RECONNECT_TO_VM)
-        );
-
-        boolean permissionsValid = commandSpy.checkPermissions(permissionSubjects);
-        assertEquals(stealingAllowed, permissionsValid);
-    }
-
-
 }
