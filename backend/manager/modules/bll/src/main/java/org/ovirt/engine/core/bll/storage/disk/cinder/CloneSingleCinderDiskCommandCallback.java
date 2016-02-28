1e897edb3a2eee1f91f0d072adcc7faab73ec457
Fred Rolland <frolland@redhat.com>
2016-02-28 13:16:21 +0200

    engine: Refactor CloneSingleCinderDiskCommand CB
    Refactor CloneSingleCinderDiskCommandCallBack to inherit from
    ConcurrentChildCommandsExecutionCallback to minimize code duplication
    and ease future refactors.
    
    Change-Id: I830eb4069c77ad8310ad042d741a37b290738598
    Signed-off-by: Fred Rolland <frolland@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/cinder/CloneSingleCinderDiskCommandCallback.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/cinder/CloneSingleCinderDiskCommandCallback.java
@@ -2,68 +2,40 @@ package org.ovirt.engine.core.bll.storage.disk.cinder;
 
 import java.util.List;
 
+import org.ovirt.engine.core.bll.CommandBase;
+import org.ovirt.engine.core.bll.ConcurrentChildCommandsExecutionCallback;
 import org.ovirt.engine.core.bll.storage.disk.image.BaseImagesCommand;
 import org.ovirt.engine.core.common.action.ImagesContainterParametersBase;
 import org.ovirt.engine.core.common.businessentities.storage.CinderDisk;
 import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.common.businessentities.storage.ImageStatus;
-import org.ovirt.engine.core.compat.CommandStatus;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
-import org.ovirt.engine.core.dao.DiskDao;
+import org.ovirt.engine.core.compat.backendcompat.CommandExecutionStatus;
 
-public class CloneSingleCinderDiskCommandCallback<T extends BaseImagesCommand<? extends ImagesContainterParametersBase>> extends AbstractCinderDiskCommandCallback<T> {
+public class CloneSingleCinderDiskCommandCallback extends ConcurrentChildCommandsExecutionCallback {
+
 
     @Override
-    public void doPolling(Guid cmdId, List<Guid> childCmdIds) {
-        super.doPolling(cmdId, childCmdIds);
+    protected void childCommandsExecutionEnded(CommandBase<?> command,
+            boolean anyFailed,
+            List<Guid> childCmdIds,
+            CommandExecutionStatus status,
+            int completedChildren) {
 
-        ImageStatus imageStatus = getCinderBroker().getDiskStatus(getDiskId());
-        DiskImage disk = getDisk();
+        BaseImagesCommand<ImagesContainterParametersBase> cloneCinderDiskCommand =
+                (BaseImagesCommand<ImagesContainterParametersBase>) command;
+        ImagesContainterParametersBase parameters = cloneCinderDiskCommand.getParameters();
+        ImageStatus imageStatus = cloneCinderDiskCommand.getCinderBroker().getDiskStatus(parameters.getDestinationImageId());
+        DiskImage disk = (CinderDisk) command.getDiskDao().get(parameters.getContainerId());
         if (imageStatus != null && imageStatus != disk.getImageStatus()) {
             switch (imageStatus) {
             case OK:
-                getCommand().setCommandStatus(CommandStatus.SUCCEEDED);
+                setCommandEndStatus(command, false, status, childCmdIds);
                 break;
             case ILLEGAL:
-                getCommand().setCommandStatus(CommandStatus.FAILED);
+                setCommandEndStatus(command, true, status, childCmdIds);
                 break;
             }
         }
-    }
-
-    @Override
-    public void onFailed(Guid cmdId, List<Guid> childCmdIds) {
-        super.onFailed(cmdId, childCmdIds);
-        getCommand().getParameters().setTaskGroupSuccess(false);
-        getCommand().endAction();
-    }
-
-    @Override
-    public void onSucceeded(Guid cmdId, List<Guid> childCmdIds) {
-        super.onSucceeded(cmdId, childCmdIds);
-        getCommand().endAction();
-    }
-
-    @Override
-    protected Guid getDiskId() {
-        return getCommand().getParameters().getDestinationImageId();
-    }
-
-    @Override
-    protected CinderDisk getDisk() {
-        if (disk == null) {
-            disk = (CinderDisk) getDiskDao().get(getCommand().getParameters().getContainerId());
-        }
-        return disk;
-    }
-
-    protected DiskDao getDiskDao() {
-        return DbFacade.getInstance().getDiskDao();
-    }
-
-    @Override
-    protected CinderBroker getCinderBroker() {
-        return getCommand().getCinderBroker();
     }
 }
