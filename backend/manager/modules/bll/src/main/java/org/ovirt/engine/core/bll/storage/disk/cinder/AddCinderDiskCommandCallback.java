2716cb35045599bce4a7ad8356aac438e04c34ca
Fred Rolland <frolland@redhat.com>
2016-02-28 11:20:31 +0200

    engine: Refactor AddCinderDiskCommandCallback
    Refactor AddCinderDiskCommandCallback to inherit from
    ConcurrentChildCommandsExecutionCallback to minimize code duplication
    and ease future refactors.
    
    Change-Id: I97f54ce9f2afe290f7bf38d503706dd57f7a182a
    Signed-off-by: Fred Rolland <frolland@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/cinder/AddCinderDiskCommandCallback.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/cinder/AddCinderDiskCommandCallback.java
@@ -2,74 +2,43 @@ package org.ovirt.engine.core.bll.storage.disk.cinder;
 
 import java.util.List;
 
-import org.ovirt.engine.core.bll.tasks.CommandCoordinatorUtil;
+import org.ovirt.engine.core.bll.CommandBase;
+import org.ovirt.engine.core.bll.ConcurrentChildCommandsExecutionCallback;
 import org.ovirt.engine.core.common.action.AddDiskParameters;
 import org.ovirt.engine.core.common.businessentities.storage.CinderDisk;
 import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.common.businessentities.storage.ImageStatus;
-import org.ovirt.engine.core.compat.CommandStatus;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
-import org.ovirt.engine.core.dao.DiskDao;
+import org.ovirt.engine.core.compat.backendcompat.CommandExecutionStatus;
 
-public class AddCinderDiskCommandCallback extends AbstractCinderDiskCommandCallback<AddCinderDiskCommand<AddDiskParameters>> {
+public class AddCinderDiskCommandCallback extends ConcurrentChildCommandsExecutionCallback {
 
     @Override
-    public void doPolling(Guid cmdId, List<Guid> childCmdIds) {
-        super.doPolling(cmdId, childCmdIds);
+    protected void childCommandsExecutionEnded(CommandBase<?> command,
+            boolean anyFailed,
+            List<Guid> childCmdIds,
+            CommandExecutionStatus status,
+            int completedChildren) {
 
-        ImageStatus imageStatus = getCinderBroker().getDiskStatus(getDiskId());
-        DiskImage disk = getDisk();
+        AddCinderDiskCommand<AddDiskParameters> addCinderDiskCommand =
+                (AddCinderDiskCommand<AddDiskParameters>) command;
+        Guid diskId = getDiskId(addCinderDiskCommand);
+        ImageStatus imageStatus = addCinderDiskCommand.getCinderBroker().getDiskStatus(diskId);
+        DiskImage disk = (CinderDisk) command.getDiskDao().get(diskId);
         if (imageStatus != null && imageStatus != disk.getImageStatus()) {
             switch (imageStatus) {
-                case OK:
-                    getCommand().setCommandStatus(CommandStatus.SUCCEEDED);
-                    break;
-                case ILLEGAL:
-                    getCommand().setCommandStatus(CommandStatus.FAILED);
-                    break;
+            case OK:
+                setCommandEndStatus(command, false, status, childCmdIds);
+                break;
+            case ILLEGAL:
+                setCommandEndStatus(command, true, status, childCmdIds);
+                break;
             }
         }
     }
 
-    @Override
-    public void onFailed(Guid cmdId, List<Guid> childCmdIds) {
-        super.onFailed(cmdId, childCmdIds);
-
-        log.error("Failed adding disk to Cinder. ID: {}", getDiskId());
-        getCommand().getParameters().setTaskGroupSuccess(false);
-        getCommand().endAction();
-        CommandCoordinatorUtil.removeAllCommandsInHierarchy(cmdId);
+    protected Guid getDiskId(AddCinderDiskCommand<AddDiskParameters> addCinderDiskCommand) {
+        return addCinderDiskCommand.getParameters().getDiskInfo().getId();
     }
 
-    @Override
-    public void onSucceeded(Guid cmdId, List<Guid> childCmdIds) {
-        super.onSucceeded(cmdId, childCmdIds);
-
-        log.info("Disk has been successfully added to Cinder. ID: {}", getDiskId());
-        getCommand().endAction();
-        CommandCoordinatorUtil.removeAllCommandsInHierarchy(cmdId);
-    }
-
-    @Override
-    protected Guid getDiskId() {
-        return getCommand().getParameters().getDiskInfo().getId();
-    }
-
-    @Override
-    protected CinderDisk getDisk() {
-        if (disk == null) {
-            disk = (CinderDisk) getDiskDao().get(getDiskId());
-        }
-        return disk;
-    }
-
-    protected DiskDao getDiskDao() {
-        return DbFacade.getInstance().getDiskDao();
-    }
-
-    @Override
-    protected CinderBroker getCinderBroker() {
-        return getCommand().getCinderBroker();
-    }
 }
