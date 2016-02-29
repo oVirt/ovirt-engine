96b976e9b0ebbd5bf3d41eab0b06a0b34e3a3f9d
Shmuel Melamud <smelamud@redhat.com>
2016-02-29 14:14:15 +0200

    engine: Leave cpuProfileId untouched after reading a snapshot
    cpuProfileId is not stored in OVF, so after reading a snapshot it must
    be left untouched, not set to null.
    
    Change-Id: I5c2fcf0b7a5d1eeaf9bd07b99aaa748f30dc51d7
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1238742
    Signed-off-by: Shmuel Melamud <smelamud@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/snapshots/SnapshotsManager.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/snapshots/SnapshotsManager.java
@@ -556,6 +556,7 @@ public class SnapshotsManager {
 
             // These fields are not saved in the OVF, so get them from the current VM.
             vm.setIsoPath(oldVmStatic.getIsoPath());
+            vm.setCpuProfileId(oldVmStatic.getCpuProfileId());
             vm.setClusterId(oldVmStatic.getClusterId());
             // The VM configuration does not hold the vds group Id.
             // It is necessary to fetch the vm static from the Db, in order to get this information
