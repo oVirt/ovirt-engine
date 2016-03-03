1173e67f8e95a587cb0bfb6c2fa1623127b79897
Arik Hadas <ahadas@redhat.com>
2016-03-03 17:01:28 +0200

    core: refactor vm jobs monitoring
    1. Rename occurrences of vm-jobs to jobs. In this context, all the
    jobs are supposed to be of type VmJob.
    
    2. Rename updateVmJobs methods to processVmJobs - this name was ok
    in VmAnalyzer but not in this context.
    
    3. Split saveVmJobsToDb to two methods: updateJobs and removeJobs.
    
    4. Make processVmJobs private so it will not be called from
    outside of this class.
    
    5. Extract (and simplify) the fetch of existing vm-jobs from the
    database into a separate method that can be mocked.
    
    Change-Id: I0833d2106ff88b7529166bb004234ac6b83c80be
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/monitoring/VmJobsMonitoring.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/monitoring/VmJobsMonitoring.java
@@ -1,5 +1,7 @@
 package org.ovirt.engine.core.vdsbroker.monitoring;
 
+import static java.util.stream.Collectors.toMap;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -8,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -29,62 +30,63 @@ public class VmJobsMonitoring {
 
     private static final Logger log = LoggerFactory.getLogger(VmJobsMonitoring.class);
 
-    void process(Map<Guid, List<VmJob>> vmIdToVmJobs) {
-        if (vmIdToVmJobs.isEmpty()) {
+    void process(Map<Guid, List<VmJob>> vmIdToJobs) {
+        if (vmIdToJobs.isEmpty()) {
             return;
         }
-        Map<Guid, VmJob> vmJobsToUpdate = new HashMap<>();
-        List<Guid> vmJobIdsToRemove = new ArrayList<>();
-        vmIdToVmJobs.entrySet().forEach(entry -> updateVmJobs(
-                entry.getKey(), entry.getValue(), vmJobsToUpdate, vmJobIdsToRemove));
-        saveVmJobsToDb(vmJobsToUpdate.values(), vmJobIdsToRemove);
+        Map<Guid, VmJob> jobsToUpdate = new HashMap<>();
+        List<Guid> jobIdsToRemove = new ArrayList<>();
+        vmIdToJobs.entrySet().forEach(entry -> processVmJobs(
+                entry.getKey(), entry.getValue(), jobsToUpdate, jobIdsToRemove));
+        updateJobs(jobsToUpdate.values());
+        removeJobs(jobIdsToRemove);
     }
 
-    void updateVmJobs(
-            Guid vmId,
-            List<VmJob> vmJobs,
-            Map<Guid, VmJob> vmJobsToUpdate,
-            List<Guid> vmJobIdsToRemove) {
-        if (vmJobs == null) {
+    private void processVmJobs(Guid vmId, List<VmJob> jobs, Map<Guid, VmJob> jobsToUpdate, List<Guid> jobIdsToRemove) {
+        if (jobs == null) {
             // If no vmJobs key was returned, we can't presume anything about the jobs; save them all
             log.debug("No vmJob data returned from VDSM, preserving existing jobs");
             return;
         }
-        List<Guid> existingVmJobIds = vmJobDao.getAllIds();
 
         // Only jobs that were in the DB before our update may be updated/removed;
         // others are completely ignored for the time being
-        Map<Guid, VmJob> jobsFromDb = vmJobDao.getAllForVm(vmId).stream()
-                .filter(job -> existingVmJobIds.contains(job.getId()))
-                .collect(Collectors.toMap(VmJob::getId, Function.identity()));
+        List<VmJob> jobsInDb = getExistingJobsForVm(vmId);
+        Map<Guid, VmJob> jobIdToJobInDb = jobsInDb.stream().collect(toMap(VmJob::getId, Function.identity()));
 
         Set<Guid> vmJobIdsToIgnore = new HashSet<>();
-        vmJobs.stream()
-        .filter(job -> jobsFromDb.containsKey(job.getId()))
+        jobs.stream()
+        .filter(job -> jobIdToJobInDb.containsKey(job.getId()))
         .forEach(job -> {
-            if (jobsFromDb.get(job.getId()).equals(job)) {
+            if (jobIdToJobInDb.get(job.getId()).equals(job)) {
                 // Same data, no update needed.  It would be nice if a caching
                 // layer would take care of this for us.
                 vmJobIdsToIgnore.add(job.getId());
                 log.info("VM job '{}': In progress (no change)", job.getId());
             } else {
-                vmJobsToUpdate.put(job.getId(), job);
+                jobsToUpdate.put(job.getId(), job);
                 log.info("VM job '{}': In progress, updating", job.getId());
             }
         });
 
         // Any existing jobs not saved need to be removed
-        jobsFromDb.keySet().stream()
-        .filter(jobId -> !vmJobsToUpdate.containsKey(jobId) && !vmJobIdsToIgnore.contains(jobId))
+        jobIdToJobInDb.keySet().stream()
+        .filter(jobId -> !jobsToUpdate.containsKey(jobId) && !vmJobIdsToIgnore.contains(jobId))
         .forEach(jobId -> {
-            vmJobIdsToRemove.add(jobId);
+            jobIdsToRemove.add(jobId);
             log.info("VM job '{}': Deleting", jobId);
         });
     }
 
-    void saveVmJobsToDb(Collection<VmJob> vmJobsToUpdate, List<Guid> vmJobIdsToRemove) {
-        vmJobDao.updateAllInBatch(vmJobsToUpdate);
+    List<VmJob> getExistingJobsForVm(Guid vmId) {
+        return vmJobDao.getAllForVm(vmId);
+    }
 
+    void updateJobs(Collection<VmJob> vmJobsToUpdate) {
+        vmJobDao.updateAllInBatch(vmJobsToUpdate);
+    }
+
+    void removeJobs(List<Guid> vmJobIdsToRemove) {
         if (!vmJobIdsToRemove.isEmpty()) {
             TransactionSupport.executeInScope(TransactionScopeOption.Required, () -> {
                 vmJobDao.removeAll(vmJobIdsToRemove);
