a298320a1a66920c306ef3de27065d4db872259a
Allon Mureinik <amureini@redhat.com>
2016-03-09 19:40:24 +0200

    core: Cleanup engine-config.properties
    engine-config.properties should represent a subset of ConfigValues users
    are able to control easily. Several careless patches have left obsolete
    references in it that do not represent valid config values any more and
    just confuse users.
    
    This patch removes these obsolete entries and introduces a unit test
    which evaluates packaging/etc/engine-config/engine-config.properties
    against the ConfigValues enum to ensure such mistakes don't happen
    again.
    
    Change-Id: I75f30d45e80bb3c8918a16148523b1e405185abc
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

---
+++ backend/manager/tools/src/test/java/org/ovirt/engine/core/config/EngineConfigPropertiesTest.java
@@ -0,0 +1,53 @@
+package org.ovirt.engine.core.config;
+
+import static org.hamcrest.Matchers.empty;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.ovirt.engine.core.common.config.ConfigValues;
+
+/**
+ * A test case to validate that the engine-config.properties files doesn't contain wrong/redundant entries
+ */
+public class EngineConfigPropertiesTest {
+    private static final String PROPERTIES_PATH = System.getProperty("engine-config.properties.production.file");
+    private static PropertiesConfiguration pc;
+
+    @BeforeClass
+    public static void setUpClass() {
+        try {
+            pc = new PropertiesConfiguration(PROPERTIES_PATH);
+        } catch (ConfigurationException e) {
+            fail(String.format("Failed to load configuration from the engine-config.properties.production.file " +
+                    "environment variable set to %s.", PROPERTIES_PATH));
+        }
+    }
+
+    @Test
+    public void testKeysAreRealConfigValues() {
+        Set<String> badKeys = new HashSet<>();
+        Iterator<String> keyIter = pc.getKeys();
+        while (keyIter.hasNext()) {
+            String key = keyIter.next();
+            String confValueName = key.split("\\.")[0];
+
+            try {
+                ConfigValues.valueOf(confValueName);
+            } catch (IllegalArgumentException e) {
+                badKeys.add(confValueName);
+            }
+        }
+
+        assertThat(String.format("Found keys in %s that don't have corresponding ConfigValues", PROPERTIES_PATH),
+                badKeys,
+                empty());
+    }
+}
