65dbd452ae50c518128172bc8a913823ecfa4616
Martin Mucha <mmucha@redhat.com>
2016-03-11 13:57:37 +0100

    core: do not ping too often
    Code has a bug causing polling (with FutureVDSCommandType.Poll / ping
    verb) too bursty when host is responding fast.
    
    Changes:
    1. method waitBetweenPolls is moved from end of method to the start.
    When wait is needed, it's the first thing which is done.
    2. field lastTimeBeforePoll is introduced. After waiting this field is
    set with current time. In next execution, waitBetweenPolls will wait
    if requested POLLING_BREAK is not met.
    3. in waitBetweenPolls while is introduced, because Thread.sleep does
    not guarantee to wait for requested time period, actually it may
    sometimes return way sooner than that (see comment in method).
    
    Not that important changes:
    4. extracted reading value from task / handling timeouts to single
    method for code clarity
    5. added trace logging for next time this class misbehaves.
    
    Change-Id: I2569376eae0b67591f669438a9742b34ba7ff568
    Bug-Url: https://bugzilla.redhat.com/1320606
    Signed-off-by: Martin Mucha <mmucha@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/HostSetupNetworkPoller.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/HostSetupNetworkPoller.java
@@ -11,44 +11,75 @@ import org.ovirt.engine.core.common.vdscommands.FutureVDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
 import org.ovirt.engine.core.common.vdscommands.VdsIdVDSCommandParametersBase;
 import org.ovirt.engine.core.compat.Guid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The {@code HostSetupNetworkPoller} class uses {@link FutureVDSCall} to poll the VDSM (with ping verb).
  */
 public class HostSetupNetworkPoller {
 
-    /** Time between polling attempts, to prevent flooding the host/network. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(HostSetupNetworkPoller.class);
+
+    /**
+     * Time millis between polling attempts, to prevent flooding the host/network.
+     */
     private static final long POLLING_BREAK = 500;
+    private static final long POLLING_BREAK_IN_NANOS = TimeUnit.MILLISECONDS.toNanos(POLLING_BREAK);
+    private Long timestampOfEndOfPreviousInvocation;
 
     public boolean poll(Guid hostId) {
-        long timeBeforePoll = System.currentTimeMillis();
+        waitBetweenPolls();
+
+        LOGGER.trace("Request to do poll for host {}.", hostId);
         FutureVDSCall<VDSReturnValue> task =
                 Backend.getInstance()
                         .getResourceManager()
                         .runFutureVdsCommand(FutureVDSCommandType.Poll, new VdsIdVDSCommandParametersBase(hostId));
-        try {
-            VDSReturnValue returnValue =
-                    task.get(Config.<Integer> getValue(ConfigValues.SetupNetworksPollingTimeout), TimeUnit.SECONDS);
-            if (returnValue.getSucceeded()) {
-                return true;
-            }
+        LOGGER.trace("FutureVDSCommandType.Poll executed for host{}.", hostId);
 
-            waitBeforeNextPoll(timeBeforePoll);
-        } catch (TimeoutException e) {
-            // ignore failure. network can go down due to VDSM changing the network. No need to suspend between polls is
-            // timeout has reached.
-        }
-
-        return false;
+        boolean succeeded = getValue(task);
+        LOGGER.trace("Result of FutureVDSCommandType.Poll for host {}: {}", hostId, succeeded);
+        timestampOfEndOfPreviousInvocation = currentTimestamp();
+        return succeeded;
     }
 
-    public void waitBeforeNextPoll(long timeBeforePoll) {
-        if (System.currentTimeMillis() - timeBeforePoll < POLLING_BREAK) {
+    /**
+     * @param task future to get value from.
+     *
+     * @return true if execution of task was a success, false if execution was not successful or wasn't completed in
+     * given timeout.
+     */
+    private boolean getValue(FutureVDSCall<VDSReturnValue> task) {
+        try {
+            VDSReturnValue vdsReturnValue =
+                    task.get(Config.<Integer> getValue(ConfigValues.SetupNetworksPollingTimeout), TimeUnit.SECONDS);
+            return vdsReturnValue.getSucceeded();
+        } catch (TimeoutException e) {
+            // VDSReturn value did not become available in given timeout. Ignore failure.
+            // network can go down due to VDSM changing the network.
+            return false;
+        }
+    }
+
+    private void waitBetweenPolls() {
+        /*
+         * Please notice, that thread sleep does not guarantee to return *only after* required time period passed.
+         * It may return sooner. When testing with required 500ms sleep, thread awaken after ~ 200ms, which is not even
+         * close.
+        * */
+        while (timestampOfEndOfPreviousInvocation != null
+                && currentTimestamp() - timestampOfEndOfPreviousInvocation < POLLING_BREAK_IN_NANOS) {
             try {
                 Thread.sleep(POLLING_BREAK);
             } catch (InterruptedException e) {
                 // ignore.
             }
         }
+
+    }
+
+    private long currentTimestamp() {
+        return System.nanoTime();
     }
 }
