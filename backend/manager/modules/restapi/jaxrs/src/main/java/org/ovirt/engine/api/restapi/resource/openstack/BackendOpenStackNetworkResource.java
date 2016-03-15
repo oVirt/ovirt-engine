9b5333330466d14449d1e4fa844641f61c36f3de
mirecki <mmirecki@redhat.com>
2016-03-15 22:49:24 +0100

    restapi: Adding external network import
    Adding the ability to import external networks from a provider
    This patch is related to the ovirt-engine-api-models patch:
    https://gerrit.ovirt.org/54782
    
    Change-Id: I0212b809b988a1b49837415fbe4d4776ca9a945e
    Signed-off-by: mirecki <mmirecki@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1317581

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/openstack/BackendOpenStackNetworkResource.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/openstack/BackendOpenStackNetworkResource.java
@@ -18,6 +18,7 @@ package org.ovirt.engine.api.restapi.resource.openstack;
 
 import static org.ovirt.engine.api.restapi.resource.openstack.BackendOpenStackNetworksResource.SUB_COLLECTIONS;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -29,9 +30,13 @@ import org.ovirt.engine.api.model.OpenStackNetworkProvider;
 import org.ovirt.engine.api.resource.openstack.OpenstackNetworkResource;
 import org.ovirt.engine.api.resource.openstack.OpenstackSubnetsResource;
 import org.ovirt.engine.api.restapi.resource.AbstractBackendActionableResource;
+import org.ovirt.engine.core.common.action.AddNetworkStoragePoolParameters;
+import org.ovirt.engine.core.common.action.VdcActionType;
+import org.ovirt.engine.core.common.businessentities.StoragePool;
 import org.ovirt.engine.core.common.businessentities.network.Network;
 import org.ovirt.engine.core.common.businessentities.network.ProviderNetwork;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.common.queries.NameQueryParameters;
 import org.ovirt.engine.core.common.queries.VdcQueryReturnValue;
 import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
@@ -48,39 +53,8 @@ public class BackendOpenStackNetworkResource
 
     @Override
     public OpenStackNetwork get() {
-        // The backend doesn't provide a mechanism to get a specific network from a provider, so we have to get all and
-        // then iterate to find the requested one:
-        Network network = null;
-        try {
-            IdQueryParameters parameters = new IdQueryParameters(asGuid(providerId));
-            VdcQueryReturnValue result = runQuery(VdcQueryType.GetAllExternalNetworksOnProvider, parameters);
-            if (!result.getSucceeded()) {
-                backendFailure(result.getExceptionString());
-            }
-            Map<Network, Set<Guid>> networks = result.getReturnValue();
-            if (networks != null) {
-                for (Map.Entry<Network, Set<Guid>> entry : networks.entrySet()) {
-                    Network current = entry.getKey();
-                    ProviderNetwork providedBy = current.getProvidedBy();
-                    if (Objects.equals(providedBy.getExternalId(), id)) {
-                        network = current;
-                        break;
-                    }
-                }
-            }
-        }
-        catch (Exception exception) {
-            return handleError(exception, false);
-        }
-        if (network == null) {
-            notFound();
-        }
+        Network network = getNetwork();
         return addLinks(populate(map(network), network));
-    }
-
-    @Override
-    public Response doImport(Action action) {
-        return null;
     }
 
     @Override
@@ -100,5 +74,86 @@ public class BackendOpenStackNetworkResource
     protected Guid asGuidOr404(String id) {
         // The identifier of an OpenStack image isn't a UUID.
         return null;
+    }
+
+    @Override
+    public Response doImport(Action action) {
+        validateParameters(action, "dataCenter.id|name");
+
+        Guid dataCenterId = getDataCenterId(action);
+
+        ProviderNetwork providerNetwork = new ProviderNetwork(new Guid(providerId), id);
+
+        Network network = new Network();
+        network.setProvidedBy(providerNetwork);
+        network.setDataCenterId(dataCenterId);
+        network.setName(getNetwork().getName());
+
+        AddNetworkStoragePoolParameters parameters = new AddNetworkStoragePoolParameters(dataCenterId, network);
+        return doAction(VdcActionType.AddNetwork, parameters, action);
+    }
+
+    private Network getNetwork() {
+        // The backend doesn't provide a mechanism to get a specific network from a provider, so we have to get all and
+        // then iterate to find the requested one:
+        Map<Network, Set<Guid>> networks = getAllNetworks();
+        return findCurrentNetwork(networks);
+    }
+
+    private Network findCurrentNetwork(Map<Network, Set<Guid>> networks) {
+        if (networks != null) {
+            for (Map.Entry<Network, Set<Guid>> entry : networks.entrySet()) {
+                Network current = entry.getKey();
+                ProviderNetwork providedBy = current.getProvidedBy();
+                if (Objects.equals(providedBy.getExternalId(), id)) {
+                    return current;
+                }
+            }
+        }
+        // This will never return but always throw a WebApplicationException
+        notFound();
+        return null;
+    }
+
+    private Map<Network, Set<Guid>> getAllNetworks() {
+        IdQueryParameters parameters = new IdQueryParameters(asGuid(providerId));
+        VdcQueryReturnValue result = runQuery(VdcQueryType.GetAllExternalNetworksOnProvider, parameters);
+
+        checkResultAndThrowExceptionIfFailed(result);
+
+        Map<Network, Set<Guid>> networks = result.getReturnValue();
+        return networks;
+    }
+
+    private Guid getDataCenterId(Action action) {
+        String dcIdFromAction = action.getDataCenter().getId();
+        if (dcIdFromAction != null) {
+            return new Guid(dcIdFromAction);
+        }
+        return getDataCenterIdByName(action.getDataCenter().getName());
+    }
+
+    private Guid getDataCenterIdByName(String dataCenterName) {
+        NameQueryParameters parameters = new NameQueryParameters(dataCenterName);
+        VdcQueryReturnValue result = runQuery(VdcQueryType.GetStoragePoolByDatacenterName, parameters);
+
+        checkResultAndThrowExceptionIfFailed(result);
+
+        List<StoragePool> dataCenters = result.getReturnValue();
+        if (dataCenters.isEmpty()){
+            // This will always throw a WebApplicationException
+            notFound();
+        }
+        return dataCenters.get(0).getId();
+    }
+
+    private void checkResultAndThrowExceptionIfFailed(VdcQueryReturnValue result){
+        if (!result.getSucceeded()) {
+            try {
+                backendFailure(result.getExceptionString());
+            } catch (BackendFailureException e){
+                handleError(e, false);
+            }
+        }
     }
 }
