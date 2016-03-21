662109281c87f031d6eb0aadfd2feb4f493076c7
Ravi Nori <rnori@redhat.com>
2016-03-21 14:44:19 -0400

    aaa: Add service to invoke SSO Logout
    Add a service sso-logout to invoke
    logout given an access token and scope
    
    Change-Id: Idd11e1af4d705b8b6ee6e7a9e621670c3f79dad5
    Bug-Url: https://bugzilla.redhat.com/1092744
    Signed-off-by: Ravi Nori <rnori@redhat.com>

---
+++ backend/manager/modules/services/src/main/java/org/ovirt/engine/core/services/SSOLogoutServlet.java
@@ -0,0 +1,49 @@
+package org.ovirt.engine.core.services;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.ovirt.engine.core.aaa.SSOOAuthServiceUtils;
+import org.ovirt.engine.core.utils.serialization.json.JsonObjectSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SSOLogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 9210030009170727847L;
+
+    private static final Logger log = LoggerFactory.getLogger(SSOLogoutServlet.class);
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        log.debug("Entered SSOLogoutServlet");
+        String token = request.getParameter("token");
+        String scope = request.getParameter("scope");
+
+        Map<String, Object> revokeResponse =  SSOOAuthServiceUtils.revoke(token, scope);
+        String error = (String) revokeResponse.get("error");
+        if (StringUtils.isNotEmpty(error)) {
+            log.error("Unable to logout user: {}", error);
+        }
+
+        try (OutputStream os = response.getOutputStream()) {
+            String jsonPayload = new JsonObjectSerializer().serialize(revokeResponse);
+            response.setContentType("application/json");
+            byte[] jsonPayloadBytes = jsonPayload.getBytes(StandardCharsets.UTF_8.name());
+            response.setContentLength(jsonPayloadBytes.length);
+            os.write(jsonPayloadBytes);
+            log.trace("Sending json data {}", jsonPayload);
+        }
+
+        log.debug("Exiting SSOLogoutServlet");
+    }
+
+}
