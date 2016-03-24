oVirt/ovirt-engine

commit eefc4d0488fe43d4f11e40335253b5a358230878

frontend: Implementation support for GuestOsInfo and Timezone reporting
This patch refactors the Sessions tab to a Guest Information Tab.
The data in the general tab is already quite extensive and therefore
needs no more additions.

The sessions tab so far had only 3 items, these three items would stay there
and we'll add more information reported by the guest agent to this tab instead.

This patch reports now guest os information such as:
- Guest OS Type
- Guest OS Architecture
- Guest OS Version
- Guest OS Timezone

Change-Id: Ia7f9ae95b2b9fe9affa27886a7981bcdffabee49
Bug-Url: https://bugzilla.redhat.com/869296
Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/SystemTreeModel.java
@@ -629,7 +629,7 @@ public class SystemTreeModel extends SearchableListModel<Void, SystemTreeItemMod
         SystemTreeItemModel sessionsItem = new SystemTreeItemModel();
         sessionsItem.setType(SystemTreeItemType.Sessions);
         sessionsItem.setApplicationMode(ApplicationMode.AllModes);
-        sessionsItem.setTitle(ConstantsManager.getInstance().getConstants().sessionsTitle());
+        sessionsItem.setTitle(ConstantsManager.getInstance().getConstants().guestInformationTitle());
         systemItem.addChild(sessionsItem);
 
         if (!ApplicationModeHelper.getUiMode().equals(ApplicationMode.AllModes)) {

frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/UIConstants.java
@@ -735,8 +735,8 @@ public interface UIConstants extends com.google.gwt.i18n.client.Constants {
     @DefaultStringValue("Monitor")
     String monitorTitle();
 
-    @DefaultStringValue("Sessions")
-    String sessionsTitle();
+    @DefaultStringValue("Guest Information")
+    String guestInformationTitle();
 
     @DefaultStringValue("User Sessions")
     String userSessionsTitle();

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/CommonApplicationConstants.java
@@ -2226,4 +2226,19 @@ public interface CommonApplicationConstants extends Constants {
 
     @DefaultStringValue("Unknown")
     String unknownDeviceGeneralType();
+
+    @DefaultStringValue("Architecture")
+    String guestOsArchitecture();
+
+    @DefaultStringValue("Type")
+    String guestOsType();
+
+    @DefaultStringValue("Operating System")
+    String guestOperatingSystem();
+
+    @DefaultStringValue("Kernel Version")
+    String guestOsKernelInfo();
+
+    @DefaultStringValue("Timezone")
+    String guestOsTimezone();
 }

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/vm/VmGuestInfoModelForm.java
@@ -1,0 +1,87 @@
+package org.ovirt.engine.ui.common.widget.uicommon.vm;
+
+import org.ovirt.engine.core.common.businessentities.OsType;
+import org.ovirt.engine.ui.common.CommonApplicationConstants;
+import org.ovirt.engine.ui.common.gin.AssetProvider;
+import org.ovirt.engine.ui.common.uicommon.model.ModelProvider;
+import org.ovirt.engine.ui.common.widget.form.FormItem;
+import org.ovirt.engine.ui.common.widget.label.TextBoxLabel;
+import org.ovirt.engine.ui.common.widget.uicommon.AbstractModelBoundFormWidget;
+import org.ovirt.engine.ui.uicommonweb.models.vms.VmGuestInfoModel;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.SimpleBeanEditorDriver;
+
+public class VmGuestInfoModelForm extends AbstractModelBoundFormWidget<VmGuestInfoModel> {
+
+    interface Driver extends SimpleBeanEditorDriver<VmGuestInfoModel, VmGuestInfoModelForm> {
+    }
+
+    private final Driver driver = GWT.create(Driver.class);
+
+    TextBoxLabel guestUserName = new TextBoxLabel();
+    TextBoxLabel guestOsArch = new TextBoxLabel();
+    TextBoxLabel guestOsType = new TextBoxLabel();
+    TextBoxLabel guestOsNamedVersion = new TextBoxLabel();
+    TextBoxLabel guestOsKernelVersion = new TextBoxLabel();
+    TextBoxLabel guestOsTimezone = new TextBoxLabel();
+    TextBoxLabel consoleUserName = new TextBoxLabel();
+    TextBoxLabel clientIp = new TextBoxLabel();
+
+    private final static CommonApplicationConstants constants = AssetProvider.getConstants();
+
+    public VmGuestInfoModelForm(ModelProvider<VmGuestInfoModel> modelProvider) {
+        super(modelProvider, 3, 4);
+        driver.initialize(this);
+
+        // First row - OS Info
+        formBuilder.addFormItem(new FormItem(constants.guestOsType(), guestOsType, 0, 0)
+            .withDefaultValue(constants.unknown(), new FormItem.DefaultValueCondition() {
+                @Override
+                public boolean showDefaultValue() {
+                    return getModel().getGuestOsType().equals(OsType.Other.toString());
+                }
+            }));
+        formBuilder.addFormItem(new FormItem(constants.guestOsArchitecture(), guestOsArch, 1, 0)
+            .withDefaultValue(constants.unknown(), new FormItem.DefaultValueCondition() {
+                @Override
+                public boolean showDefaultValue() {
+                    return getModel().getGuestOsType().equals(OsType.Other.toString());
+                }
+            }));
+        formBuilder.addFormItem(new FormItem(constants.guestOperatingSystem(), guestOsNamedVersion, 2, 0)
+            .withDefaultValue(constants.unknown(), new FormItem.DefaultValueCondition() {
+                @Override
+                public boolean showDefaultValue() {
+                    return getModel().getGuestOsType().equals(OsType.Other.toString());
+                }
+            }));
+        // The kernel version is only reported and displayed for Linux based systems
+        formBuilder.addFormItem(new FormItem(constants.guestOsKernelInfo(), guestOsKernelVersion, 3, 0) {
+            @Override
+            public boolean getIsAvailable() {
+                return getModel().getGuestOsType().equals(OsType.Linux.toString());
+            }
+        });
+
+        // Second row - Timezone Info
+        formBuilder.addFormItem(new FormItem(constants.guestOsTimezone(), guestOsTimezone, 0, 1)
+            .withDefaultValue(constants.unknown(), new FormItem.DefaultValueCondition() {
+                @Override
+                public boolean showDefaultValue() {
+                    return getModel().getGuestOsType().equals(OsType.Other.toString());
+                }
+            }));
+
+        // Third row - Logged In User & Console Info
+        formBuilder.addFormItem(new FormItem(constants.loggedInUserVm(), guestUserName, 0, 2));
+        formBuilder.addFormItem(new FormItem(constants.consoleConnectedUserVm(), consoleUserName, 1, 2));
+        formBuilder.addFormItem(new FormItem(constants.consoleConnectedClientIp(), clientIp, 2, 2));
+    }
+
+    @Override
+    protected void doEdit(VmGuestInfoModel model) {
+        driver.edit(model);
+    }
+
+}

frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/uicommon/vm/VmSessionsModelForm.java
@@ -1,41 +1,0 @@
-package org.ovirt.engine.ui.common.widget.uicommon.vm;
-
-import org.ovirt.engine.ui.common.CommonApplicationConstants;
-import org.ovirt.engine.ui.common.gin.AssetProvider;
-import org.ovirt.engine.ui.common.uicommon.model.ModelProvider;
-import org.ovirt.engine.ui.common.widget.form.FormItem;
-import org.ovirt.engine.ui.common.widget.label.TextBoxLabel;
-import org.ovirt.engine.ui.common.widget.uicommon.AbstractModelBoundFormWidget;
-import org.ovirt.engine.ui.uicommonweb.models.vms.VmSessionsModel;
-
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.editor.client.SimpleBeanEditorDriver;
-
-public class VmSessionsModelForm extends AbstractModelBoundFormWidget<VmSessionsModel> {
-
-    interface Driver extends SimpleBeanEditorDriver<VmSessionsModel, VmSessionsModelForm> {
-    }
-
-    private final Driver driver = GWT.create(Driver.class);
-
-    TextBoxLabel guestUserName = new TextBoxLabel();
-    TextBoxLabel consoleUserName = new TextBoxLabel();
-    TextBoxLabel clientIp = new TextBoxLabel();
-
-    private final static CommonApplicationConstants constants = AssetProvider.getConstants();
-
-    public VmSessionsModelForm(ModelProvider<VmSessionsModel> modelProvider) {
-        super(modelProvider, 3, 1);
-        driver.initialize(this);
-
-        formBuilder.addFormItem(new FormItem(constants.loggedInUserVm(), guestUserName, 0, 0));
-        formBuilder.addFormItem(new FormItem(constants.consoleConnectedUserVm(), consoleUserName, 0, 1));
-        formBuilder.addFormItem(new FormItem(constants.consoleConnectedClientIp(), clientIp, 0, 2));
-    }
-
-    @Override
-    protected void doEdit(VmSessionsModel model) {
-        driver.edit(model);
-    }
-
-}

frontend/webadmin/modules/gwt-common/src/main/resources/org/ovirt/engine/core/Common.gwt.xml
@@ -184,8 +184,8 @@
         <include name="common/businessentities/SnapshotActionEnum.java"/>
         <include name="common/businessentities/VmRngDevice.java" />
         <include name="common/businessentities/UserProfile.java" />
-        <include name="common/businessentities/OsType.java" />
         <include name="common/locks/LockInfo.java" />
+        <include name="common/businessentities/OsType.java" />
         <include name="common/businessentities/VmHostDevice.java" />
         <include name="common/businessentities/HostDevice.java" />
         <include name="common/businessentities/HostDeviceId.java" />

frontend/webadmin/modules/gwt-common/src/main/resources/org/ovirt/engine/core/Compat.gwt.xml
@@ -19,6 +19,7 @@
 		<include name="compat/KeyValuePairCompat.java" />
 		<include name="compat/NotImplementedException.java" />
 		<include name="compat/TimeSpan.java" />
+		<include name="compat/WindowsJavaTimezoneMapping.java" />
 
 		<include name="compat/DateTime.java" />
 		<include name="compat/DayOfWeek.java" />

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/help/HelpTag.java
@@ -462,7 +462,7 @@ public enum HelpTag {
 
     services("services", HelpTagType.UNKNOWN), //$NON-NLS-1$
 
-    sessions("sessions", HelpTagType.UNKNOWN), //$NON-NLS-1$
+    guest_info("guest_info", HelpTagType.UNKNOWN), //$NON-NLS-1$
 
     engine_sessions("engine_sessions", HelpTagType.UNKNOWN), //$NON-NLS-1$
 

commit 2e0af1f77a71bcd281f48f3d36ad1b2e07b6c010

webadmin: Sessions in left tree was called 'Guest Information'
Regression introduced by: eefc4d0488fe43d4f11e40335253b5a358230878

The eefc4d0488fe43d4f11e40335253b5a358230878 have introduced a new subtab
called "Guest Information" but accidentally have renamed the "Sessions" tree
item to it.

Change-Id: I3dd40329870f1786c460490a877f4bfb9ea38d3b
Bug-Url: https://bugzilla.redhat.com/1318936
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/SystemTreeModel.java
@@ -616,7 +616,7 @@ public class SystemTreeModel extends SearchableListModel<Void, SystemTreeItemMod
         SystemTreeItemModel sessionsItem = new SystemTreeItemModel();
         sessionsItem.setType(SystemTreeItemType.Sessions);
         sessionsItem.setApplicationMode(ApplicationMode.AllModes);
-        sessionsItem.setTitle(ConstantsManager.getInstance().getConstants().guestInformationTitle());
+        sessionsItem.setTitle(ConstantsManager.getInstance().getConstants().sessionsTitle());
         systemItem.addChild(sessionsItem);
 
         if (!ApplicationModeHelper.getUiMode().equals(ApplicationMode.AllModes)) {

frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/UIConstants.java
@@ -778,6 +778,9 @@ public interface UIConstants extends com.google.gwt.i18n.client.Constants {
     @DefaultStringValue("Guest Information")
     String guestInformationTitle();
 
+    @DefaultStringValue("Sessions")
+    String sessionsTitle();
+
     @DefaultStringValue("User Sessions")
     String userSessionsTitle();
 