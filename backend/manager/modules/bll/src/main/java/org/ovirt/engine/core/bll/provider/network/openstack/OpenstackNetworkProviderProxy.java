cb9f5c859397a83a3e78214202daf79d734dede3
mirecki <mmirecki@redhat.com>
2016-03-29 17:17:47 +0200

    engine: create base class for network provider proxies: BaseNetworkProviderProxy
    This patch creates a base class for external network provider proxy.
    It moves the base functionality from OpenstackNetworkProviderProxy
    to BaseNetworkProviderProxy, letting OpenstackNetworkProviderProxy
    handle only the very openstack neutron related items.
    
    Change-Id: I7b0252b15323d1013611134a69f85ad215265771
    Bug-Url: https://bugzilla.redhat.com/1314375
    Signed-off-by: mirecki <mmirecki@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/network/openstack/OpenstackNetworkProviderProxy.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/network/openstack/OpenstackNetworkProviderProxy.java
@@ -1,360 +1,67 @@
 package org.ovirt.engine.core.bll.provider.network.openstack;
 
-import java.security.cert.Certificate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.ovirt.engine.core.bll.provider.ProviderValidator;
-import org.ovirt.engine.core.bll.provider.network.NetworkProviderProxy;
 import org.ovirt.engine.core.common.businessentities.OpenstackNetworkProviderProperties;
 import org.ovirt.engine.core.common.businessentities.Provider;
-import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.network.ExternalSubnet;
-import org.ovirt.engine.core.common.businessentities.network.ExternalSubnet.IpVersion;
 import org.ovirt.engine.core.common.businessentities.network.Network;
-import org.ovirt.engine.core.common.businessentities.network.ProviderNetwork;
 import org.ovirt.engine.core.common.businessentities.network.VmNic;
-import org.ovirt.engine.core.common.businessentities.network.VnicProfile;
-import org.ovirt.engine.core.common.errors.EngineError;
-import org.ovirt.engine.core.common.errors.EngineException;
-import org.ovirt.engine.core.utils.NetworkUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.woorea.openstack.base.client.HttpMethod;
-import com.woorea.openstack.base.client.OpenStackRequest;
-import com.woorea.openstack.base.client.OpenStackResponseException;
+
 import com.woorea.openstack.keystone.utils.KeystoneTokenProvider;
 import com.woorea.openstack.quantum.Quantum;
-import com.woorea.openstack.quantum.model.Networks;
 import com.woorea.openstack.quantum.model.Port;
-import com.woorea.openstack.quantum.model.Port.Binding;
 import com.woorea.openstack.quantum.model.Subnet;
-import com.woorea.openstack.quantum.model.Subnets;
 
-public class OpenstackNetworkProviderProxy implements NetworkProviderProxy {
-
-    private static final List<String> DEFAULT_SECURITY_GROUP = null;
-
-    private static final List<String> NO_SECURITY_GROUPS = Collections.emptyList();
-
-    private static final String SECURITY_GROUPS_PROPERTY = "SecurityGroups";
-
-    private static final String API_VERSION = "/v2.0";
-
-    private static final String DEVICE_OWNER = "oVirt";
-
-    private static final String FLAT_NETWORK = "flat";
-
-    private static final String VLAN_NETWORK = "vlan";
-
-    private Provider<OpenstackNetworkProviderProperties> provider;
-
-    private Quantum client;
-
-    private ProviderValidator providerValidator;
-
-    private static Logger log = LoggerFactory.getLogger(OpenstackNetworkProviderProxy.class);
+public class OpenstackNetworkProviderProxy extends BaseNetworkProviderProxy<OpenstackNetworkProviderProperties> {
 
     public OpenstackNetworkProviderProxy(Provider<OpenstackNetworkProviderProperties> provider) {
-        this.provider = provider;
-    }
-
-    private Quantum getClient() {
-        if (client == null) {
-            client = new Quantum(provider.getUrl() + API_VERSION);
-            if (provider.isRequiringAuthentication()) {
-                final String tenantName = provider.getAdditionalProperties().getTenantName();
-                final KeystoneTokenProvider keystoneTokenProvider =
-                        new KeystoneTokenProvider(provider.getAuthUrl(),
-                                provider.getUsername(),
-                                provider.getPassword());
-
-                client.setTokenProvider(keystoneTokenProvider.getProviderByTenant(tenantName));
-            }
-        }
-
-        return client;
+        super(provider);
     }
 
     @Override
-    public String add(Network network) {
-        com.woorea.openstack.quantum.model.Network networkForCreate = new com.woorea.openstack.quantum.model.Network();
-        networkForCreate.setAdminStateUp(true);
-        networkForCreate.setName(network.getName());
-        if (NetworkUtils.isLabeled(network)) {
-            networkForCreate.setProviderPhysicalNetwork(network.getLabel());
-            if (NetworkUtils.isVlan(network)) {
-                networkForCreate.setProviderNetworkType(VLAN_NETWORK);
-                networkForCreate.setProviderSegmentationId(network.getVlanId());
-            } else {
-                networkForCreate.setProviderNetworkType(FLAT_NETWORK);
-            }
-        }
+    protected void setClientTokenProvider(Quantum client) {
+        String tenantName = provider.getAdditionalProperties().getTenantName();
+        KeystoneTokenProvider keystoneTokenProvider =
+                new KeystoneTokenProvider(provider.getAuthUrl(),
+                        provider.getUsername(),
+                        provider.getPassword());
+        client.setTokenProvider(keystoneTokenProvider.getProviderByTenant(tenantName));
+    }
 
+    @Override
+    protected Map<String, String> createPortAllocationRuntimeProperties(Port port) {
+        Map<String, String> runtimeProperties = super.createPortAllocationRuntimeProperties(port);
+        runtimeProperties.put("plugin_type", provider.getAdditionalProperties().getPluginType());
+        return runtimeProperties;
+    }
+
+   @Override
+   protected Port createNewPortForAllocate(VmNic nic,
+            List<String> securityGroups, String hostId,
+            com.woorea.openstack.quantum.model.Network externalNetwork) {
+        Port portForCreate = super.createNewPortForAllocate(nic,
+                securityGroups, hostId, externalNetwork);
+        portForCreate.setTenantId(externalNetwork.getTenantId());
+        return portForCreate;
+    }
+
+    @Override
+    protected Subnet createNewSubnetEntity(ExternalSubnet subnet,
+            com.woorea.openstack.quantum.model.Network externalNetwork) {
+        Subnet subnetForCreate = super.createNewSubnetEntity(subnet, externalNetwork);
+        subnetForCreate.setTenantId(externalNetwork.getTenantId());
+        return subnetForCreate;
+    }
+
+    @Override
+    protected com.woorea.openstack.quantum.model.Network createNewNetworkEntity(Network network) {
+        com.woorea.openstack.quantum.model.Network networkForCreate =
+                super.createNewNetworkEntity(network);
         if (!provider.isRequiringAuthentication()) {
             networkForCreate.setTenantId(DEVICE_OWNER);
         }
-
-        try {
-            com.woorea.openstack.quantum.model.Network createdNetwork =
-                    getClient().networks().create(networkForCreate).execute();
-            return createdNetwork.getId();
-        } catch (RuntimeException e) {
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }
-    }
-
-    @Override
-    public void remove(String id) {
-        try {
-            getClient().networks().delete(id).execute();
-        } catch (RuntimeException e) {
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }
-    }
-
-    @Override
-    public List<Network> getAll() {
-        try {
-            Networks networks = getClient().networks().list().execute();
-            return map(networks.getList());
-        } catch (RuntimeException e) {
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }
-    }
-
-    @Override
-    public List<ExternalSubnet> getAllSubnets(ProviderNetwork network) {
-        List<ExternalSubnet> result = new ArrayList<>();
-        Subnets subnets = getClient().subnets().list().execute();
-        for (Subnet subnet : subnets.getList()) {
-            if (network.getExternalId().equals(subnet.getNetworkId())) {
-                result.add(map(subnet, network));
-            }
-        }
-
-        return result;
-    }
-
-    private ExternalSubnet map(Subnet subnet, ProviderNetwork network) {
-        ExternalSubnet s = new ExternalSubnet();
-        s.setId(subnet.getId());
-        s.setCidr(subnet.getCidr());
-        s.setIpVersion(Subnet.IpVersion.IPV6.equals(subnet.getIpversion())
-                ? IpVersion.IPV6
-                : IpVersion.IPV4);
-        s.setName(subnet.getName());
-        s.setExternalNetwork(network);
-        s.setGateway(subnet.getGw());
-        s.setDnsServers(subnet.getDnsNames());
-        return s;
-    }
-
-    @Override
-    public void addSubnet(ExternalSubnet subnet) {
-        com.woorea.openstack.quantum.model.Network externalNetwork = getExternalNetwork(subnet.getExternalNetwork());
-        Subnet subnetForCreate = new Subnet();
-        subnetForCreate.setCidr(subnet.getCidr());
-        subnetForCreate.setIpversion(subnet.getIpVersion() == IpVersion.IPV6
-                ? Subnet.IpVersion.IPV6 : Subnet.IpVersion.IPV4);
-        subnetForCreate.setName(subnet.getName());
-        subnetForCreate.setNetworkId(externalNetwork.getId());
-        subnetForCreate.setTenantId(externalNetwork.getTenantId());
-        subnetForCreate.setEnableDHCP(true);
-        subnetForCreate.setGw(subnet.getGateway());
-        subnetForCreate.setDnsNames(subnet.getDnsServers());
-
-        try {
-            getClient().subnets().create(subnetForCreate).execute();
-        } catch (RuntimeException e) {
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }
-    }
-
-    @Override
-    public void removeSubnet(String id) {
-        try {
-            getClient().subnets().delete(id).execute();
-        } catch (RuntimeException e) {
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }
-    }
-
-    @Override
-    public void testConnection() {
-        try {
-            getClient().execute(new OpenStackRequest<>(getClient(), HttpMethod.GET, "", null, ApiRootResponse.class));
-        }  catch (OpenStackResponseException e) {
-            log.error("{} (OpenStack response error code: {})", e.getMessage(), e.getStatus());
-            log.debug("Exception", e);
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }catch (RuntimeException e) {
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }
-    }
-
-    /**
-     * Currently, SSL is not supported by Openstack Network Providers.
-     */
-    @Override
-    public List<? extends Certificate> getCertificateChain() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void onAddition() {
-    }
-
-    @Override
-    public void onModification() {
-    }
-
-    @Override
-    public void onRemoval() {
-    }
-
-    private List<Network> map(List<com.woorea.openstack.quantum.model.Network> externalNetworks) {
-        List<Network> networks = new ArrayList<>(externalNetworks.size());
-
-        for (com.woorea.openstack.quantum.model.Network externalNetwork : externalNetworks) {
-            Network network = new Network();
-            network.setVmNetwork(true);
-            network.setProvidedBy(new ProviderNetwork(provider.getId(), externalNetwork.getId()));
-            network.setName(externalNetwork.getName());
-            networks.add(network);
-        }
-
-        return networks;
-    }
-
-    @Override
-    public Map<String, String> allocate(Network network, VnicProfile vnicProfile, VmNic nic, VDS host) {
-        try {
-            Port port = locatePort(nic);
-
-            List<String> securityGroups = getSecurityGroups(vnicProfile);
-            String hostId = NetworkUtils.getUniqueHostName(host);
-
-            if (port == null) {
-                com.woorea.openstack.quantum.model.Network externalNetwork =
-                        getExternalNetwork(network.getProvidedBy());
-                Port portForCreate = new Port();
-                portForCreate.setAdminStateUp(true);
-                portForCreate.setName(nic.getName());
-                portForCreate.setTenantId(externalNetwork.getTenantId());
-                portForCreate.setMacAddress(nic.getMacAddress());
-                portForCreate.setNetworkId(externalNetwork.getId());
-                portForCreate.setDeviceOwner(DEVICE_OWNER);
-                portForCreate.setDeviceId(nic.getId().toString());
-                portForCreate.setSecurityGroups(securityGroups);
-                portForCreate.setBinding(new Binding());
-                portForCreate.getBinding().setHostId(hostId);
-                port = getClient().ports().create(portForCreate).execute();
-            } else {
-                boolean securityGroupsChanged = securityGroupsChanged(port.getSecurityGroups(), securityGroups);
-                boolean hostChanged = hostChanged(port, hostId);
-
-                if (securityGroupsChanged || hostChanged) {
-                    Port portForUpdate = new PortForUpdate();
-                    portForUpdate.setId(port.getId());
-                    portForUpdate.setSecurityGroups(securityGroupsChanged ? securityGroups : port.getSecurityGroups());
-
-                    if (hostChanged) {
-                        portForUpdate.setBinding(new Binding());
-                        portForUpdate.getBinding().setHostId(hostId);
-                    }
-
-                    port = getClient().ports().update(portForUpdate).execute();
-                }
-            }
-
-            Map<String, String> runtimeProperties = new HashMap<>();
-            runtimeProperties.put("vnic_id", port.getId());
-            runtimeProperties.put("provider_type", provider.getType().name());
-            runtimeProperties.put("plugin_type", provider.getAdditionalProperties().getPluginType());
-            if (port.getSecurityGroups() != null && !port.getSecurityGroups().isEmpty()) {
-                runtimeProperties.put("security_groups", StringUtils.join(port.getSecurityGroups(), ','));
-            }
-
-            return runtimeProperties;
-        } catch (RuntimeException e) {
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }
-    }
-
-    private com.woorea.openstack.quantum.model.Network getExternalNetwork(ProviderNetwork providerNetwork) {
-        return getClient().networks().show(providerNetwork.getExternalId()).execute();
-    }
-
-    private boolean hostChanged(Port port, String hostId) {
-        return port.getBinding() == null || !StringUtils.equals(port.getBinding().getHostId(), hostId);
-    }
-
-    private boolean securityGroupsChanged(List<String> existingSecurityGroups, List<String> desiredSecurityGroups) {
-        existingSecurityGroups = existingSecurityGroups == null ? NO_SECURITY_GROUPS : existingSecurityGroups;
-        return (desiredSecurityGroups == DEFAULT_SECURITY_GROUP
-                && existingSecurityGroups.isEmpty())
-                || (desiredSecurityGroups != DEFAULT_SECURITY_GROUP
-                && !CollectionUtils.isEqualCollection(existingSecurityGroups, desiredSecurityGroups));
-    }
-
-    private List<String> getSecurityGroups(VnicProfile vnicProfile) {
-        Map<String, String> customProperties = vnicProfile.getCustomProperties();
-
-        if (customProperties.containsKey(SECURITY_GROUPS_PROPERTY)) {
-            String securityGroupsString = customProperties.get(SECURITY_GROUPS_PROPERTY);
-
-            if (StringUtils.isEmpty(securityGroupsString)) {
-                return NO_SECURITY_GROUPS;
-            }
-
-            return Arrays.asList(securityGroupsString.split(",\\w*"));
-        }
-
-        return DEFAULT_SECURITY_GROUP;
-    }
-
-    @Override
-    public void deallocate(VmNic nic) {
-        try {
-            Port port = locatePort(nic);
-
-            if (port != null) {
-                getClient().ports().delete(port.getId()).execute();
-            }
-        } catch (RuntimeException e) {
-            throw new EngineException(EngineError.PROVIDER_FAILURE, e);
-        }
-    }
-
-    private Port locatePort(VmNic nic) {
-        List<Port> ports = getClient().ports().list().execute().getList();
-        for (Port port : ports) {
-            if (DEVICE_OWNER.equals(port.getDeviceOwner()) && nic.getId().toString().equals(port.getDeviceId())) {
-                return port;
-            }
-        }
-
-        return null;
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    private static class ApiRootResponse {
-        // No implementation since we don't care what's inside the response, just that it succeeded.
-    }
-
-    @Override
-    public ProviderValidator getProviderValidator() {
-        if (providerValidator == null) {
-            providerValidator = new ProviderValidator(provider);
-        }
-        return providerValidator;
+        return networkForCreate;
     }
 }
