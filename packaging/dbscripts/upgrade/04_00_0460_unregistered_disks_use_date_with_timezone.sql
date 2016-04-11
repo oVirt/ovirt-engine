0e0e2a9631d259ae0e784d0faaba9b1bff0bf570
Maor Lipchuk <mlipchuk@redhat.com>
2016-04-11 15:17:07 +0300

    core: Update column's table with timestamp.
    Use timestamp instead of date in the unregistered_disks table to also
    save the hour, minute and second.
    
    Change-Id: I4b70e9a767017eea9e67a6c1f8d5793ab4fca1c2
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

---
+++ packaging/dbscripts/upgrade/04_00_0460_unregistered_disks_use_date_with_timezone.sql
@@ -0,0 +1,2 @@
+SELECT fn_db_change_column_type('unregistered_disks','creation_date','date','timestamp with time zone');
+SELECT fn_db_change_column_type('unregistered_disks','last_modified','date','timestamp with time zone');
