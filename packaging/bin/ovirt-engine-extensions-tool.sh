e5f6792a8e89bcc9ec082739d7546e2f66334de1
Ondra Machacek <omachace@redhat.com>
2016-04-13 13:14:01 +0200

    aaa: exttool: use JBoss JAAS configuration by default
    To be consistent with behaviour of aaa-ldap running in JBoss
    and extension-tool running outside of JBoss, we provide
    temporary configuration of JAAS to exttool, which reflect
    configuration of <security-domains> in JBoss.
    
    Change-Id: Idf816a065cc4e4b0a3dc5b378bacd4c2aa214dfb
    Bug-Url: https://bugzilla.redhat.com/1327041
    Signed-off-by: Ondra Machacek <omachace@redhat.com>
    (cherry picked from commit ab4a52b18ee3b75bfafaec6dc1f87f4182e9ec5b)

--- packaging/bin/ovirt-engine-extensions-tool.sh
+++ packaging/bin/ovirt-engine-extensions-tool.sh
@@ -13,6 +13,10 @@ exec "${JAVA_HOME}/bin/java" \
 	-Dorg.ovirt.engine.exttool.core.packageVersion="${PACKAGE_VERSION}" \
 	-Dorg.ovirt.engine.exttool.core.packageDisplayName="${PACKAGE_DISPLAY_NAME}" \
 	-Dorg.ovirt.engine.exttool.core.engineEtc="${ENGINE_ETC}" \
+	-Dorg.ovirt.engine.exttool.core.useTicketCache="${AAA_JAAS_USE_TICKET_CACHE}" \
+	-Dorg.ovirt.engine.exttool.core.ticketCacheFile="${AAA_JAAS_TICKET_CACHE_FILE}" \
+	-Dorg.ovirt.engine.exttool.core.useKeytab="${AAA_JAAS_USE_KEYTAB}" \
+	-Dorg.ovirt.engine.exttool.core.keytabFile="${AAA_JAAS_KEYTAB_FILE}" \
 	-jar "${JBOSS_HOME}/jboss-modules.jar" \
 	-dependencies org.ovirt.engine.core.extensions-tool \
 	-class org.ovirt.engine.exttool.core.ExtensionsToolExecutor \
