e0a1570de98360ccf7f77ab4acce61bfc099d44b
Maor Lipchuk <mlipchuk@redhat.com>
2016-04-18 19:48:37 +0300

    core: Add cinder call back for AddVmFromSnapshot.
    When clonning a VM from Cinder snapshot the VM and the snapshot stay in
    lock status since AddVmFromSnapshot has no call back method.
    Adding the call back method should fix this issue.
    
    Change-Id: Ic4a41089fbf035ddd4b914885db5dc2a39f1a25c
    Bug-Url: https://bugzilla.redhat.com/1316853
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmFromSnapshotCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmFromSnapshotCommand.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.context.CommandContext;
 import org.ovirt.engine.core.bll.snapshots.SnapshotsValidator;
 import org.ovirt.engine.core.bll.storage.disk.image.ImagesHandler;
+import org.ovirt.engine.core.bll.tasks.interfaces.CommandCallback;
 import org.ovirt.engine.core.bll.utils.PermissionSubject;
 import org.ovirt.engine.core.bll.validator.VmValidator;
 import org.ovirt.engine.core.common.VdcObjectType;
@@ -55,6 +56,15 @@ public class AddVmFromSnapshotCommand<T extends AddVmFromSnapshotParameters> ext
     protected AddVmFromSnapshotCommand(T params, CommandContext commandContext) {
         super(params, commandContext);
         sourceSnapshotId = params.getSourceSnapshotId();
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        VM vm = getVmDao().get(getVmIdFromSnapshot());
+        VmHandler.updateDisksFromDb(vm);
+        boolean isCinderDisksExist = !ImagesHandler.filterDisksBasedOnCinder(vm.getDiskList()).isEmpty();
+        getParameters().setUseCinderCommandCallback(isCinderDisksExist);
     }
 
     @Override
@@ -279,5 +289,10 @@ public class AddVmFromSnapshotCommand<T extends AddVmFromSnapshotParameters> ext
 
     public VmValidator createVmValidator(VM vm) {
         return new VmValidator(vm);
+    }
+
+    @Override
+    public CommandCallback getCallback() {
+        return getParameters().isUseCinderCommandCallback() ? new ConcurrentChildCommandsExecutionCallback() : null;
     }
 }
