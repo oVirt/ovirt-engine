oVirt/ovirt-engine

commit 7d5f94d4c79ebc93cdfe08a7709a6411dda8fce7

core: simplify permissions check on migrate vm
Simplify MigrateVmCommand#getPermissionCheckSubjects.

Change-Id: I7d737e96797f575365798673acd5bbb470957d81
Signed-off-by: Arik Hadas <ahadas@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/MigrateVmCommand.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.commons.lang.time.DurationFormatUtils;
 import org.ovirt.engine.core.bll.context.CommandContext;
@@ -545,12 +546,7 @@ public class MigrateVmCommand<T extends MigrateVmParameters> extends RunVmComman
     public List<PermissionSubject> getPermissionCheckSubjects() {
         List<PermissionSubject> permissionList = super.getPermissionCheckSubjects();
 
-        // this runs before validate so the getVm() can be null - instead of failing on NPE here we pass the parent permissions and let the validate to return proper error
-        if (getVm() == null) {
-            return permissionList;
-        }
-
-        if (getParameters().getTargetClusterId() != null && !getParameters().getTargetClusterId().equals(getVm().getClusterId())) {
+        if (getVm() != null && !Objects.equals(getParameters().getTargetClusterId(), getVm().getClusterId())) {
             // additional permissions needed since changing the cluster
             permissionList.addAll(VmHandler.getPermissionsNeededToChangeCluster(getParameters().getVmId(), getParameters().getTargetClusterId()));
         }

commit de26ddea3e501abd6f5d37a3b2eb3aeee72ee8e7

core: can not migrate vm using REST
The regression introduced by 7d5f94d4c79ebc93cdfe08a7709a6411dda8fce7

The issue is that if the getParameters().getTargetClusterId() is null it means
that the target cluster id is not set meaning the cluster did not change.
In this case the additional permission does not have to be added.

The problem was that if the getTargetClusterId was null the Objects.equals
returned false so the additional permission has been added in a way that you
need permission to create_vm on the cluster: null which did not pass.

Change-Id: Ia0d6201c3d095beebffb47a6fb6beab5e5684b6f
Bug-Url: https://bugzilla.redhat.com/1328011
Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/MigrateVmCommand.java
@@ -529,7 +529,9 @@ public class MigrateVmCommand<T extends MigrateVmParameters> extends RunVmComman
     public List<PermissionSubject> getPermissionCheckSubjects() {
         List<PermissionSubject> permissionList = super.getPermissionCheckSubjects();
 
-        if (getVm() != null && !Objects.equals(getParameters().getTargetClusterId(), getVm().getClusterId())) {
+        if (getParameters().getTargetClusterId() != null &&
+                getVm() != null &&
+                !Objects.equals(getParameters().getTargetClusterId(), getVm().getClusterId())) {
             // additional permissions needed since changing the cluster
             permissionList.addAll(VmHandler.getPermissionsNeededToChangeCluster(getParameters().getVmId(), getParameters().getTargetClusterId()));
         }