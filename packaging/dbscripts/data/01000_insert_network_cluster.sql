1a9ce3508c907f465901457db2ae23d6cd88de86
Yevgeny Zaspitsky <yzaspits@redhat.com>
2016-04-26 19:12:36 +0300

    engine: set ovirtmgmt net as display and migration
    Set ovirtmgmt network as display and migration
    in the initailly created default cluster.
    
    Change-Id: I278fec6e6ef0540b6487a70216f8b01006fa8b1e
    Bug-Url: https://bugzilla.redhat.com/1322515
    Signed-off-by: Yevgeny Zaspitsky <yzaspits@redhat.com>

--- packaging/dbscripts/data/01000_insert_network_cluster.sql
+++ packaging/dbscripts/data/01000_insert_network_cluster.sql
@@ -7,7 +7,7 @@
 -- Data for Name: network_cluster; Type: TABLE DATA; Schema: public; Owner: engine
 --
 
-INSERT INTO network_cluster (network_id, cluster_id, status, is_display, required, migration) VALUES ('00000000-0000-0000-0000-000000000009', '00000001-0001-0001-0001-0000000000d6', 1, false, true, false);
+INSERT INTO network_cluster (network_id, cluster_id, status, is_display, required, migration) VALUES ('00000000-0000-0000-0000-000000000009', '00000001-0001-0001-0001-0000000000d6', 1, true, true, true);
 
 
 --
