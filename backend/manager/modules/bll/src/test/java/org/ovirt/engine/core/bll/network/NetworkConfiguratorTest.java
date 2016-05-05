e2c4befb0a52d4a23898350300901b2a4ad442a3
Yevgeny Zaspitsky <yzaspits@redhat.com>
2016-05-05 18:33:09 +0300

    engine: fix coverity warnings
    The patch fixes the following Coverity warnings:
    * CID 1355030 - Dereference of an explicit null value
    * CID 1355031 - Dereference of an explicit null value
    
    Also it adds test coverage for getIpv[46]AddressOfNetwork methods.
    
    Change-Id: I2fa38b889cb97b41a9e34e646c39903273a1128e
    Signed-off-by: Yevgeny Zaspitsky <yzaspits@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/NetworkConfiguratorTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/NetworkConfiguratorTest.java
@@ -1,16 +1,17 @@
 package org.ovirt.engine.core.bll.network;
 
-import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import java.util.function.Function;
-
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.ovirt.engine.core.bll.InjectorRule;
+import org.ovirt.engine.core.bll.context.CommandContext;
 import org.ovirt.engine.core.bll.network.cluster.ManagementNetworkUtil;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
@@ -18,34 +19,65 @@ import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface
 @RunWith(MockitoJUnitRunner.class)
 public class NetworkConfiguratorTest {
 
+    private static final String NETWORK_NAME1 = "networkName";
+    private static final String NETWORK_NAME2 = "not" + NETWORK_NAME1;
+    private static final String IPV4_ADDRESS = "ipv4 address";
+    private static final String IPV6_ADDRESS = "ipv6 address";
+    private static final CommandContext COMMAND_CONTEXT = CommandContext.createContext("context");
+
     @Rule
     public InjectorRule injectorRule = new InjectorRule();
 
     @Mock
     private ManagementNetworkUtil managementNetworkUtil;
 
-    @Test
-    public void getIpv4AddressOfNetworkReturnsNullWhenThereIsNoNetworkOfGivenName() {
-        String networkName = "networkName";
-        testResolveHostNetworkAddress(e -> e.getIpv4AddressOfNetwork(networkName), networkName);
-    }
+    private VdsNetworkInterface nic = new VdsNetworkInterface();
 
-    @Test
-    public void getIpv6AddressOfNetwork() {
-        String networkName = "networkName";
-        testResolveHostNetworkAddress(e -> e.getIpv6AddressOfNetwork(networkName), networkName);
-    }
+    private NetworkConfigurator underTest;
 
-    private void testResolveHostNetworkAddress(Function<NetworkConfigurator, String> function, String networkName) {
+    @Before
+    public void setUp() {
         injectorRule.bind(ManagementNetworkUtil.class, managementNetworkUtil);
 
-        VdsNetworkInterface nic = new VdsNetworkInterface();
-        nic.setNetworkName(networkName);
+        nic.setNetworkName(NETWORK_NAME1);
 
         VDS vds = new VDS();
         vds.getInterfaces().add(nic);
 
-        assertThat(function.apply(new NetworkConfigurator(vds, null)), nullValue());
+        underTest = new NetworkConfigurator(vds, COMMAND_CONTEXT);
     }
 
+    @Test
+    public void getIpv4AddressOfNetwork() {
+        nic.setIpv4Address(IPV4_ADDRESS);
+        assertThat(underTest.getIpv4AddressOfNetwork(NETWORK_NAME1), is(IPV4_ADDRESS));
+    }
+
+    @Test
+    public void getIpv6AddressOfNetwork() {
+        nic.setIpv6Address(IPV6_ADDRESS);
+        assertThat(underTest.getIpv6AddressOfNetwork(NETWORK_NAME1), is(IPV6_ADDRESS));
+    }
+
+    @Test
+    public void getIpv4AddressOfNetworkReturnsNullWhenThereIsNoIpSet() {
+        assertThat(underTest.getIpv4AddressOfNetwork(NETWORK_NAME1), nullValue());
+    }
+
+    @Test
+    public void getIpv6AddressOfNetworkReturnsNullWhenThereIsNoIpSet() {
+        assertThat(underTest.getIpv6AddressOfNetwork(NETWORK_NAME1), nullValue());
+    }
+
+    @Test
+    public void getIpv4AddressOfNetworkReturnsNullWhenThereIsNoNetworkOfGivenName() {
+        nic.setIpv4Address(IPV4_ADDRESS);
+        assertThat(underTest.getIpv4AddressOfNetwork(NETWORK_NAME2), nullValue());
+    }
+
+    @Test
+    public void getIpv6AddressOfNetworkReturnsNullWhenThereIsNoNetworkOfGivenName() {
+        nic.setIpv6Address(IPV6_ADDRESS);
+        assertThat(underTest.getIpv6AddressOfNetwork(NETWORK_NAME2), nullValue());
+    }
 }
