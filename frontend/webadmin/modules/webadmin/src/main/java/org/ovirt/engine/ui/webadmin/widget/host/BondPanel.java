oVirt/ovirt-engine

commit eb86f8a219e5e99f71462d1478c1db5a6680f7db

webadmin: Adding missing aggregate link tooltip to setup networks panel
Bonds in mode 4 (link aggregation) have an aggregate assigned when working properly.
This mac is reported by vdsm. If this mac is missing, the condition must be reported
to the user.
This patch adds an alert icon to the host network interfaces subtab,
which notifies the user that such a contition has occured.

Change-Id: I73fc50bd59abf09f8372cf459126dfedb0b159e7
Bug-Url: https://bugzilla.redhat.com/1281666
Signed-off-by: mirecki <mmirecki@redhat.com>

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/host/BondPanel.java
@@ -1,13 +1,23 @@
 package org.ovirt.engine.ui.webadmin.widget.host;
 
+import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.VdsStatic;
+import org.ovirt.engine.core.common.businessentities.network.Bond;
+import org.ovirt.engine.core.common.businessentities.network.BondMode;
 import org.ovirt.engine.core.common.businessentities.network.InterfaceStatus;
+import org.ovirt.engine.core.common.queries.ConfigurationValues;
+import org.ovirt.engine.ui.common.widget.dialog.TooltippedIcon;
+import org.ovirt.engine.ui.uicommonweb.dataprovider.AsyncDataProvider;
 import org.ovirt.engine.ui.uicommonweb.models.hosts.HostInterfaceLineModel;
+import org.ovirt.engine.ui.webadmin.ApplicationConstants;
 import org.ovirt.engine.ui.webadmin.ApplicationResources;
+import org.ovirt.engine.ui.webadmin.ApplicationTemplates;
 import org.ovirt.engine.ui.webadmin.gin.AssetProvider;
 
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.dom.client.Style.BorderStyle;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
@@ -15,8 +25,10 @@ import com.google.gwt.user.client.ui.Label;
 public class BondPanel extends HorizontalPanel {
 
     private static final ApplicationResources resources = AssetProvider.getResources();
+    private static final ApplicationTemplates templates = AssetProvider.getTemplates();
+    private static final ApplicationConstants constants = AssetProvider.getConstants();
 
-    public BondPanel(HostInterfaceLineModel lineModel) {
+    public BondPanel(HostInterfaceLineModel lineModel, VDS vds) {
         super();
 
         clear();
@@ -29,6 +41,7 @@ public class BondPanel extends HorizontalPanel {
         if (lineModel.getIsBonded()) {
             // Bond icon
             Image bondIcon;
+            Bond bond = (Bond) lineModel.getInterface();
             if (InterfaceStatus.UP.equals(lineModel.getInterface().getStatistics().getStatus())) {
                 bondIcon = new Image(resources.splitUpImage());
             } else {
@@ -36,13 +49,40 @@ public class BondPanel extends HorizontalPanel {
             }
 
             add(bondIcon);
-            setCellWidth(bondIcon, "20%"); //$NON-NLS-1$
+
+            boolean isAdPartnerMacValid = isAdPartnerMacValid(bond, vds);
+            setCellWidth(bondIcon, isAdPartnerMacValid ? "20%" : "15%"); //$NON-NLS-1$ //$NON-NLS-2$
+            addAdPartnerMacIfNeeded(bond, vds, isAdPartnerMacValid);
 
             // Bond name
             add(new InterfaceLabel(lineModel.getInterface()));
         } else {
             add(new Label("")); //$NON-NLS-1$
         }
+    }
+
+    private void addAdPartnerMacIfNeeded(Bond bond, VDS vds, boolean isAdPartnerMacValid) {
+        if (!isAdPartnerMacValid) {
+            ImageResource noPartnerMacImage = resources.alertImage();
+            String noPartnerMacMessage = constants.bondInMode4HasNoPartnerMac();
+
+            TooltippedIcon bondInMode4HasNoPartnerMacIcon = new TooltippedIcon(
+                    templates.italicWordWrapMaxWidth(noPartnerMacMessage), noPartnerMacImage, noPartnerMacImage);
+            add(bondInMode4HasNoPartnerMacIcon);
+            setCellWidth(bondInMode4HasNoPartnerMacIcon, "5%"); //$NON-NLS-1$
+        }
+    }
+
+    private boolean isAdPartnerMacValid(Bond bond, VDS vds){
+
+        VdsStatic vdsStatic = vds.getStaticData();
+        boolean isAdPartnerMacEmpty = bond.getAdPartnerMac() == null || bond.getAdPartnerMac().isEmpty();
+        boolean isIfcUp = InterfaceStatus.UP.equals(bond.getStatistics().getStatus());
+        boolean isBond4 = BondMode.BOND4.equals(BondMode.parseBondMode(bond.getBondOptions()));
+        boolean isAdPartnerSupportedForCluster = (Boolean)AsyncDataProvider.getInstance().getConfigValuePreConverted(
+                ConfigurationValues.AdPartnerMacSupported, vds.getClusterCompatibilityVersion().getValue());
+
+        return !isAdPartnerMacEmpty || !isIfcUp || !isBond4 || !isAdPartnerSupportedForCluster;
     }
 
 }

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/network/BondMode.java
@@ -49,7 +49,7 @@ public enum BondMode {
         return isValidForVmNetwork;
     }
 
-    static BondMode parseBondMode(String bondOptions){
+    public static BondMode parseBondMode(String bondOptions){
         return bondOptions == null ? null : getBondMode(findMode(bondOptions));
     }
 

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/ConfigurationValues.java
@@ -94,7 +94,8 @@ public enum ConfigurationValues {
     UploadImageXhrTimeoutInSeconds,
     UploadImageXhrRetryIntervalInSeconds,
     UploadImageXhrMaxRetries,
-    MigrationPolicies;
+    MigrationPolicies,
+    AdPartnerMacSupported;
 
     public static enum ConfigAuthType {
         Admin,

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/ApplicationConstants.java
@@ -4825,4 +4825,7 @@ public interface ApplicationConstants extends CommonApplicationConstants {
 
     @DefaultStringValue("Custom Properties")
     String customPropertiesTabLabel();
+
+    @DefaultStringValue("Bond is in link aggregation mode (mode 4) but no partner mac has been reported for it.")
+    String bondInMode4HasNoPartnerMac();
 }

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/tab/host/SubTabHostInterfaceView.java
@@ -138,7 +138,7 @@ public class SubTabHostInterfaceView extends AbstractSubTabFormView<VDS, HostLis
     @Override
     public void setMainTabSelectedItem(VDS selectedItem) {
         // TODO(vszocs) possible performance optimization: don't create HostInterfaceForm upon each selection
-        hostInterfaceForm = new HostInterfaceForm(getDetailModel());
+        hostInterfaceForm = new HostInterfaceForm(getDetailModel(), selectedItem);
         contentPanel.remove(contentPanel.getWidgetCount() - 1);
         contentPanel.add(hostInterfaceForm);
     }

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/host/HostInterfaceForm.java
@@ -2,12 +2,14 @@ package org.ovirt.engine.ui.webadmin.widget.host;
 
 import java.util.List;
 
+import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.ui.uicommonweb.models.hosts.HostInterfaceLineModel;
 import org.ovirt.engine.ui.uicommonweb.models.hosts.HostInterfaceListModel;
 import org.ovirt.engine.ui.uicompat.Event;
 import org.ovirt.engine.ui.uicompat.EventArgs;
 import org.ovirt.engine.ui.uicompat.IEventListener;
 import org.ovirt.engine.ui.uicompat.PropertyChangedEventArgs;
+
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Widget;
@@ -17,7 +19,7 @@ public class HostInterfaceForm extends Composite {
     private final Grid grid;
 
     @SuppressWarnings("unchecked")
-    public HostInterfaceForm(final HostInterfaceListModel listModel) {
+    public HostInterfaceForm(final HostInterfaceListModel listModel, final VDS vds) {
         grid = new Grid(1, 4);
         grid.getColumnFormatter().setWidth(0, "230px"); //$NON-NLS-1$
         grid.getColumnFormatter().setWidth(1, "200px"); //$NON-NLS-1$
@@ -29,7 +31,7 @@ public class HostInterfaceForm extends Composite {
 
         List<HostInterfaceLineModel> interfaceLineModels = (List<HostInterfaceLineModel>) listModel.getItems();
         if (interfaceLineModels != null) {
-            showModels(interfaceLineModels);
+            showModels(interfaceLineModels, vds);
         }
 
         listModel.getItemsChangedEvent().addListener(new IEventListener<EventArgs>() {
@@ -37,7 +39,7 @@ public class HostInterfaceForm extends Composite {
             public void eventRaised(Event<? extends EventArgs> ev, Object sender, EventArgs args) {
                 HostInterfaceListModel model = (HostInterfaceListModel) sender;
                 List<HostInterfaceLineModel> interfaceLineModels = (List<HostInterfaceLineModel>) model.getItems();
-                showModels(interfaceLineModels);
+                showModels(interfaceLineModels, vds);
             }
         });
 
@@ -47,7 +49,7 @@ public class HostInterfaceForm extends Composite {
                 String propName = args.propertyName;
                 if ("isSelectionAvailable".equals(propName)) { //$NON-NLS-1$
                     if (listModel.getItems() != null) {
-                        showModels((List<HostInterfaceLineModel>) listModel.getItems());
+                        showModels((List<HostInterfaceLineModel>) listModel.getItems(), vds);
                     }
                 }
             }
@@ -62,8 +64,8 @@ public class HostInterfaceForm extends Composite {
         return panel;
     }
 
-    BondPanel createBondPanel(HostInterfaceLineModel lineModel) {
-        BondPanel panel = new BondPanel(lineModel);
+    BondPanel createBondPanel(HostInterfaceLineModel lineModel, VDS vds) {
+        BondPanel panel = new BondPanel(lineModel, vds);
         panel.setWidth("100%"); //$NON-NLS-1$
         panel.setHeight("100%"); //$NON-NLS-1$
         return panel;
@@ -85,14 +87,14 @@ public class HostInterfaceForm extends Composite {
         return panel;
     }
 
-    void showModels(List<HostInterfaceLineModel> interfaceLineModels) {
+    void showModels(List<HostInterfaceLineModel> interfaceLineModels, VDS vds) {
         this.setVisible(true);
         grid.resizeRows(interfaceLineModels.size());
         int row = 0;
 
         for (HostInterfaceLineModel lineModel : interfaceLineModels) {
             setGridWidget(row, 0, createInterfacePanel(lineModel));
-            setGridWidget(row, 1, createBondPanel(lineModel));
+            setGridWidget(row, 1, createBondPanel(lineModel, vds));
             setGridWidget(row, 2, createVLanPanel(lineModel));
             setGridWidget(row, 3, createStatisticsPanel(lineModel));
             row++;

commit 94292a07882e5a73c55ff00cb1fe9fe685e48f21

core: Fix findbugs DLS issue in BondPanel class
Commit eb86f8a219e5e99f71462d1478c1db5a6680f7db introduced a dead local
store in BondPanel, this patch removes this DLS

Change-Id: Iaed1beedaa9ca77a5304fadbb10229aa3404529e
Signed-off-by: Tal Nisan <tnisan@redhat.com>

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/host/BondPanel.java
@@ -1,7 +1,6 @@
 package org.ovirt.engine.ui.webadmin.widget.host;
 
 import org.ovirt.engine.core.common.businessentities.VDS;
-import org.ovirt.engine.core.common.businessentities.VdsStatic;
 import org.ovirt.engine.core.common.businessentities.network.Bond;
 import org.ovirt.engine.core.common.businessentities.network.BondMode;
 import org.ovirt.engine.core.common.businessentities.network.InterfaceStatus;
@@ -74,8 +73,6 @@ public class BondPanel extends HorizontalPanel {
     }
 
     private boolean isAdPartnerMacValid(Bond bond, VDS vds){
-
-        VdsStatic vdsStatic = vds.getStaticData();
         boolean isAdPartnerMacEmpty = bond.getAdPartnerMac() == null || bond.getAdPartnerMac().isEmpty();
         boolean isIfcUp = InterfaceStatus.UP.equals(bond.getStatistics().getStatus());
         boolean isBond4 = BondMode.BOND4.equals(BondMode.parseBondMode(bond.getBondOptions()));