e834f6e8a93b93155d4b368973e4c1393841b7e8
Tomas Jelinek <tjelinek@redhat.com>
2016-05-10 09:52:19 +0200

    tools: added migration policies to engine-config
    Added a possibility to configure the migration policies using engine config.
    
    The dependency to jackson is there to make sure the provided policy is valid
    and also to give an example policy.
    
    Change-Id: I2f84034348b0af963cc63dd1cb2470d9b45c28c8
    Bug-Url: https://bugzilla.redhat.com/1252426
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

---
+++ backend/manager/tools/src/main/java/org/ovirt/engine/core/config/entity/helper/MigrationPoliciesValueHelper.java
@@ -0,0 +1,52 @@
+package org.ovirt.engine.core.config.entity.helper;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.type.CollectionType;
+import org.ovirt.engine.core.common.migration.ConvergenceConfig;
+import org.ovirt.engine.core.common.migration.ConvergenceItem;
+import org.ovirt.engine.core.common.migration.ConvergenceItemWithStallingLimit;
+import org.ovirt.engine.core.common.migration.MigrationPolicy;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.config.entity.ConfigKey;
+import org.ovirt.engine.core.utils.JsonHelper;
+
+public class MigrationPoliciesValueHelper extends StringValueHelper {
+
+    @Override
+    public ValidationResult validate(ConfigKey key, String value) {
+        if (StringUtils.isEmpty(value)) {
+            return new ValidationResult(false, "The provided value can not be empty");
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        CollectionType type = mapper.getTypeFactory().constructCollectionType(List.class, MigrationPolicy.class);
+        try {
+            mapper.readValue(value, type);
+        } catch (IOException e) {
+            return new ValidationResult(false,
+                    String.format("The provided migration policy can not be parsed: '%s'. A valid format is: '%s'",
+                            e.getMessage(), getExamplePolicy()));
+        }
+
+        return new ValidationResult(true);
+    }
+
+    private String getExamplePolicy() {
+        ConvergenceConfig config = new ConvergenceConfig();
+        config.setInitialItems(Arrays.asList(new ConvergenceItem("setDowntime", 100)));
+        config.setConvergenceItems(Arrays.asList(new ConvergenceItemWithStallingLimit(1, "setDowntime", 200)));
+        config.setLastItems(Arrays.asList(new ConvergenceItem("abort")));
+        List<MigrationPolicy> policies = Arrays.asList(new MigrationPolicy(Guid.Empty, "name of the policy", "description", config));
+        try {
+            return JsonHelper.objectToJson(policies, false);
+        } catch (IOException e) {
+            // if this fails, than we really want to fail it with an exception
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ backend/manager/tools/src/test/java/org/ovirt/engine/core/config/entity/helper/MigrationPoliciesValueHelperTest.java
@@ -0,0 +1,30 @@
+package org.ovirt.engine.core.config.entity.helper;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MigrationPoliciesValueHelperTest {
+
+    private MigrationPoliciesValueHelper helper = new MigrationPoliciesValueHelper();
+
+    @Test
+    public void emptyIsNotValid() {
+        assertEquals(helper.validate(null, "").isOk(), false);
+    }
+
+    @Test
+    public void incorrectJsonNotValid() {
+        assertEquals(helper.validate(null, "this is not a valid json").isOk(), false);
+    }
+
+    @Test
+    public void notAnyValidJsonIsValid() {
+        assertEquals(helper.validate(null, "{}").isOk(), false);
+    }
+
+    @Test
+    public void listOfMigrationPoliciesIsValid() {
+        assertEquals(helper.validate(null, "[{\"id\":{\"uuid\":\"80554327-0569-496b-bdeb-fcbbf52b827b\"},\"maxMigrations\":2,\"autoConvergence\":true,\"migrationCompression\":true,\"enableGuestEvents\":true,\"name\":\"Safe but not may not converge\",\"description\":\"A safe policy which in typical situations lets the VM converge. The user of the VM should not notice any significant slowdown of the VM. If the VM is not converging for a longer time, the migration will be aborted.\",\"config\":{\"convergenceItems\":[{\"stallingLimit\":1,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"150\"]}},{\"stallingLimit\":2,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"200\"]}},{\"stallingLimit\":3,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"300\"]}},{\"stallingLimit\":4,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"400\"]}},{\"stallingLimit\":6,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"500\"]}}],\"initialItems\":[{\"action\":\"setDowntime\",\"params\":[\"100\"]}],\"lastItems\":[{\"action\":\"abort\",\"params\":[]}]}},{\"id\":{\"uuid\":\"80554327-0569-496b-bdeb-fcbbf52b827c\"},\"maxMigrations\":1,\"autoConvergence\":true,\"migrationCompression\":true,\"enableGuestEvents\":true,\"name\":\"Should converge but guest may notice a pause\",\"description\":\"A safe policy which makes also a highly loaded VM converge in most situations. On the other hand, the user may notice a slowdown. If the VM is still not converging, the migration is aborted.\",\"config\":{\"convergenceItems\":[{\"stallingLimit\":1,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"150\"]}},{\"stallingLimit\":2,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"200\"]}},{\"stallingLimit\":3,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"300\"]}},{\"stallingLimit\":4,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"400\"]}},{\"stallingLimit\":6,\"convergenceItem\":{\"action\":\"setDowntime\",\"params\":[\"500\"]}}],\"initialItems\":[{\"action\":\"setDowntime\",\"params\":[\"100\"]}],\"lastItems\":[{\"action\":\"setDowntime\",\"params\":[\"5000\"]},{\"action\":\"abort\",\"params\":[]}]}}]").isOk(), true);
+    }
+}
