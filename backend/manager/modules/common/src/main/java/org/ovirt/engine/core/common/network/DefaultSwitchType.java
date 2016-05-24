014ddeac67264df035f41f7c3a850d24b30caf26
Martin Mucha <mmucha@redhat.com>
2016-05-24 14:56:43 +0200

    core,webadmin: change switch type default value.
    currently the switch type default value has to be LEGACY, regardless
    of cluster version
    
    Bug-Url: https://bugzilla.redhat.com/1195208
    Change-Id: I9e30bf9b87ee5bb274f434e1ea3b451209f6998c
    Signed-off-by: Martin Mucha <mmucha@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/network/DefaultSwitchType.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/network/DefaultSwitchType.java
@@ -1,11 +1,9 @@
 package org.ovirt.engine.core.common.network;
 
-import org.ovirt.engine.core.common.FeatureSupported;
 import org.ovirt.engine.core.compat.Version;
 
 public class DefaultSwitchType {
     public static SwitchType getDefaultSwitchType(Version clusterCompatibilityVersion) {
-        boolean ovsSupported = FeatureSupported.ovsSupported(clusterCompatibilityVersion);
-        return ovsSupported ? SwitchType.OVS : SwitchType.LEGACY;
+        return SwitchType.LEGACY;
     }
 }
