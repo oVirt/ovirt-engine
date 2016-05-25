359707b486402a244d3aec7393f2f66bd6e1027d
Arik Hadas <ahadas@redhat.com>
2016-05-25 19:05:43 +0300

    core: cleanup in migrate command
    Minor cleanup in MigrateVDSCommand:
    1. Use VmDynamic instead of VM since all migration related data
    that needs to be updated resides in VmDynamic.
    2. Remove unused VmDao (VmDynamicDao was unused before)
    3. Styling issues
    
    Change-Id: Ic76b418f0d71c4a59a9af401110830c4566a282d
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/MigrateVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/MigrateVDSCommand.java
@@ -1,12 +1,12 @@
 package org.ovirt.engine.core.vdsbroker;
 
-import org.ovirt.engine.core.common.businessentities.VM;
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VMStatus;
+import org.ovirt.engine.core.common.businessentities.VmDynamic;
 import org.ovirt.engine.core.common.vdscommands.MigrateVDSCommandParameters;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
-import org.ovirt.engine.core.dao.VmDao;
 import org.ovirt.engine.core.dao.VmDynamicDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,36 +15,34 @@ public class MigrateVDSCommand<P extends MigrateVDSCommandParameters> extends Ma
 
     private static final Logger log = LoggerFactory.getLogger(MigrateVDSCommand.class);
 
+    @Inject
+    private VmDynamicDao vmDynamicDao;
+
     public MigrateVDSCommand(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeVmCommand() {
-        VDSReturnValue vdsReturnValue = resourceManager.runVdsCommand(VDSCommandType.MigrateBroker, getParameters());
-        VM vm = getVmDao().get(getParameters().getVmId());
+        VDSReturnValue vdsReturnValue = resourceManager.runVdsCommand(
+                VDSCommandType.MigrateBroker,
+                getParameters());
+        VmDynamic vmDynamic = vmDynamicDao.get(getParameters().getVmId());
 
         if (vdsReturnValue.getSucceeded()) {
             resourceManager.addAsyncRunningVm(getParameters().getVmId());
-            resourceManager.internalSetVmStatus(vm, VMStatus.MigratingFrom);
-            vm.setMigratingToVds(getParameters().getDstVdsId());
-            vmManager.update(vm.getDynamicData());
+            vmDynamic.setStatus(VMStatus.MigratingFrom);
+            vmDynamic.setMigratingToVds(getParameters().getDstVdsId());
+            vmManager.update(vmDynamic);
             getVDSReturnValue().setReturnValue(VMStatus.MigratingFrom);
         } else {
             log.error("Failed Vm migration");
             getVDSReturnValue().setSucceeded(false);
-            getVDSReturnValue().setReturnValue(vm.getStatus());
+            getVDSReturnValue().setReturnValue(vmDynamic.getStatus());
             getVDSReturnValue().setVdsError(vdsReturnValue.getVdsError());
             getVDSReturnValue().setExceptionString(vdsReturnValue.getExceptionString());
             getVDSReturnValue().setExceptionObject(vdsReturnValue.getExceptionObject());
         }
     }
 
-    private VmDynamicDao getVmDynamicDao() {
-        return DbFacade.getInstance().getVmDynamicDao();
-    }
-
-    private VmDao getVmDao() {
-        return DbFacade.getInstance().getVmDao();
-    }
 }
