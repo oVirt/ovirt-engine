caa6317d1102b4f5f0f8429cc21aa0097bfc023c
Allon Mureinik <amureini@redhat.com>
2016-05-26 15:48:59 +0300

    core: GetAllAttachableDisksForVmQuery branching
    Removed a useless if from
    GetAllAttachableDisksForVmQuery#executeQueryCommand. Since entering and
    not entering the if has the same effect, it's useless, and can safely be
    removed.
    
    This patch fixes Coverity issue 1355578.
    
    Change-Id: If6afa979b5fb4375bfa344ee8711b1a155e2d848
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/GetAllAttachableDisksForVmQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/GetAllAttachableDisksForVmQuery.java
@@ -1,10 +1,6 @@
 package org.ovirt.engine.core.bll.storage.disk;
 
-import java.util.List;
-
-import org.apache.commons.collections.CollectionUtils;
 import org.ovirt.engine.core.bll.QueriesCommandBase;
-import org.ovirt.engine.core.common.businessentities.storage.Disk;
 import org.ovirt.engine.core.common.queries.GetAllAttachableDisksForVmQueryParameters;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 
@@ -16,16 +12,11 @@ public class GetAllAttachableDisksForVmQuery<P extends GetAllAttachableDisksForV
 
     @Override
     protected void executeQueryCommand() {
-        List<Disk> diskList = DbFacade.getInstance()
+        setReturnValue(DbFacade.getInstance()
                 .getDiskDao()
                 .getAllAttachableDisksByPoolId(getParameters().getStoragePoolId(),
                         getParameters().getVmId(),
                         getUserID(),
-                        getParameters().isFiltered());
-        if (CollectionUtils.isEmpty(diskList)) {
-            setReturnValue(diskList);
-            return;
-        }
-        setReturnValue(diskList);
+                        getParameters().isFiltered()));
     }
 }
