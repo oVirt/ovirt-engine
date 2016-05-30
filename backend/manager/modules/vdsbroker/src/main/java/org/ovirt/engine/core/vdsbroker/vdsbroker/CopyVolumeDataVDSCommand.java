31316fa114acac59ad59cf5b1c0aeb0ac0c3dd4d
Liron Aravot <laravot@redhat.com>
2016-05-30 17:54:11 +0300

    vdsbroker: CopyVolumeDataVDSCommand - perform nothing
    as the verb is currently not implemented this patch comments
    out the actual call.
    
    Change-Id: I98f997867fa7553d17c620d6f5dcf0b2adcce8d7
    Signed-off-by: Liron Aravot <laravot@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/CopyVolumeDataVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/CopyVolumeDataVDSCommand.java
@@ -1,9 +1,10 @@
 package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
-import org.ovirt.engine.core.common.businessentities.LocationInfo;
-import org.ovirt.engine.core.common.utils.LocationInfoHelper;
+import org.ovirt.engine.core.common.errors.EngineError;
 import org.ovirt.engine.core.common.vdscommands.CopyVolumeDataVDSCommandParameters;
 
 public class CopyVolumeDataVDSCommand<P extends CopyVolumeDataVDSCommandParameters> extends VdsBrokerCommand<P> {
@@ -13,15 +14,21 @@ public class CopyVolumeDataVDSCommand<P extends CopyVolumeDataVDSCommandParamete
 
     @Override
     protected void executeVdsBrokerCommand() {
-        log.info("-- executeVdsBrokerCommand: calling 'copyVolumeData'");
+//        log.info("-- executeVdsBrokerCommand: calling 'copyVolumeData'");
+//
+//        status = getBroker().copyData(buildLocationInfo(getParameters().getSrcInfo()),
+//                buildLocationInfo(getParameters().getDstInfo()), getParameters().isCollapse());
+//
+//        proceedProxyReturnValue();
 
-        status = getBroker().copyData(buildLocationInfo(getParameters().getSrcInfo()),
-                buildLocationInfo(getParameters().getDstInfo()), getParameters().isCollapse());
-
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", EngineError.Done.getValue());
+        map.put("message", "done");
+        status = new StatusOnlyReturnForXmlRpc(Collections.<String, Object>singletonMap("status", map));
         proceedProxyReturnValue();
     }
-
-    private Map<?, ?> buildLocationInfo(LocationInfo info) {
-        return LocationInfoHelper.prepareLocationInfoForVdsCommand(info);
-    }
+//
+//    private Map<?, ?> buildLocationInfo(LocationInfo info) {
+//        return LocationInfoHelper.prepareLocationInfoForVdsCommand(info);
+//    }
 }
