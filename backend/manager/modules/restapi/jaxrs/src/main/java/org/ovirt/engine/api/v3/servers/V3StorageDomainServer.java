940c42cd3195d77bfe8bd99598cc9091abd2d922
Juan Hernandez <juan.hernandez@redhat.com>
2016-05-30 15:08:15 +0200

    restapi: Add support for V3 SD delete with body
    Version 3 of the API supported the use of a body in the operation that
    deletes a storage domain. This has been removed from version 4 of the
    API, as bodies are in general forbidden for delete operations. But we
    still need to support it for version 3 of the API, as that is how the
    "format", "destroy" and "host" parameters are handled. This patch
    modifies the backwards compatibility layer so that it accepts a body in
    this case, and transforms the parameters into query parameters.
    
    Change-Id: I37746b316f284854aab19674c0f9ab4d74cfbf9f
    Bug-Url: https://bugzilla.redhat.com/1340164
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/v3/servers/V3StorageDomainServer.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/v3/servers/V3StorageDomainServer.java
@@ -24,12 +24,16 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.ovirt.engine.api.model.Actionable;
 import org.ovirt.engine.api.resource.StorageDomainResource;
 import org.ovirt.engine.api.v3.V3Server;
 import org.ovirt.engine.api.v3.types.V3Action;
+import org.ovirt.engine.api.v3.types.V3Host;
 import org.ovirt.engine.api.v3.types.V3StorageDomain;
 
 @Produces({"application/xml", "application/json"})
@@ -63,6 +67,33 @@ public class V3StorageDomainServer extends V3Server<StorageDomainResource> {
     @Path("refreshluns")
     public Response refreshLuns(V3Action action) {
         return adaptAction(getDelegate()::refreshLuns, action);
+    }
+
+    @DELETE
+    @Consumes({"application/xml", "application/json"})
+    public Response remove(@Context UriInfo ui, V3StorageDomain storageDomain) {
+        // In version 3 of the API the "format", "destroy" and "host" parameters where part of the representation of the
+        // storage domain, but in version 4 of the API they have been replaced by parameters. This means that we need
+        // to check if they are present in the body of the request and convert them into request parameters.
+        if (storageDomain != null) {
+            MultivaluedMap<String, String> parameters = ui.getQueryParameters();
+            if (storageDomain.isSetFormat()) {
+                parameters.putSingle("format", storageDomain.isFormat().toString());
+            }
+            if (storageDomain.isSetDestroy()) {
+                parameters.putSingle("destroy", storageDomain.isFormat().toString());
+            }
+            V3Host host = storageDomain.getHost();
+            if (host != null) {
+                if (host.isSetId()) {
+                    parameters.putSingle("host", host.getId());
+                }
+                else if (host.isSetName()) {
+                    parameters.putSingle("host", host.getName());
+                }
+            }
+        }
+        return adaptRemove(getDelegate()::remove);
     }
 
     @DELETE
