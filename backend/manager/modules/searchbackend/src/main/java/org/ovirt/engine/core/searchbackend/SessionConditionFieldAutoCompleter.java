9761a97055dd815a35e89bbd2f17bb2f61103527
Ondra Machacek <omachace@redhat.com>
2016-05-31 16:12:03 +0200

    webadmin: sessions: make columns sortable
    This patch add missing support of those colums to be sortable:
    
    * sessionLastActiveColumn
    * sessionStartColumn
    * sourceIpColumn
    * sessionDbIdColumn
    
    Change-Id: Idc21a4e774053954d09fd0c604e7630b4b3a87ee
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1340451
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/SessionConditionFieldAutoCompleter.java
+++ backend/manager/modules/searchbackend/src/main/java/org/ovirt/engine/core/searchbackend/SessionConditionFieldAutoCompleter.java
@@ -7,12 +7,16 @@ public class SessionConditionFieldAutoCompleter extends BaseConditionFieldAutoCo
     public static final String USER_ID = "USER_ID";
     public static final String USER_NAME = "USER_NAME";
     public static final String AUTHZ_NAME = "AUTHZ_NAME";
+    public static final String SOURCE_IP = "SOURCE_IP";
+    public static final String SESSION_DB_ID = "ID";
 
     public SessionConditionFieldAutoCompleter() {
         // Building the basic verbs dict.
         verbs.add(USER_ID);
         verbs.add(USER_NAME);
         verbs.add(AUTHZ_NAME);
+        verbs.add(SOURCE_IP);
+        verbs.add(SESSION_DB_ID);
 
         // Building the autoCompletion dict.
         buildCompletions();
@@ -21,11 +25,15 @@ public class SessionConditionFieldAutoCompleter extends BaseConditionFieldAutoCo
         getTypeDictionary().put(USER_ID, UUID.class);
         getTypeDictionary().put(USER_NAME, String.class);
         getTypeDictionary().put(AUTHZ_NAME, String.class);
+        getTypeDictionary().put(SOURCE_IP, String.class);
+        getTypeDictionary().put(SESSION_DB_ID, Integer.class);
 
         // building the ColumnName dict.
         columnNameDict.put(USER_ID, USER_ID);
         columnNameDict.put(USER_NAME, USER_NAME);
         columnNameDict.put(AUTHZ_NAME, AUTHZ_NAME);
+        columnNameDict.put(SOURCE_IP, SOURCE_IP);
+        columnNameDict.put(SESSION_DB_ID, SESSION_DB_ID);
 
         // Building the validation dict.
         buildBasicValidationTable();
