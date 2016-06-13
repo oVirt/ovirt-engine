e304d507397608709b14e2eb020581d6f5ea2766
Arik Hadas <ahadas@redhat.com>
2016-06-14 01:13:07 +0300

    core: cleanup in parameters of add vm
    Remove unused field 'diskInfoList'
    
    Change-Id: Ieb33f29b8b99b26d7ca612ee96a96aa63357a01d
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddVmParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddVmParameters.java
@@ -1,16 +1,12 @@
 package org.ovirt.engine.core.common.action;
 
-import java.util.ArrayList;
-
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VmStatic;
-import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.compat.Guid;
 
 public class AddVmParameters extends VmManagementParametersBase {
     private static final long serialVersionUID = 8641610721114989096L;
 
-    private ArrayList<DiskImage> diskInfoList;
     private Guid diskOperatorAuthzPrincipalDbId;
     private Guid poolId;
 
@@ -19,7 +15,6 @@ public class AddVmParameters extends VmManagementParametersBase {
 
     public AddVmParameters(VmStatic vmStatic) {
         super(vmStatic);
-        diskInfoList = new ArrayList<>();
     }
 
     public AddVmParameters(VM vm) {
