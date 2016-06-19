50a51c11e42055125ed23f40355e4266c02d4c6b
Jenny Tokar <jtokar@redhat.com>
2016-06-19 13:23:29 +0300

    core: Allow user to create a vm from blank template on local storage
    When user creates a new vm from blank template on local storage the vm
    is pinned to the one host that exists on the storage.
    Since the blank template is not pinned to a specific host the
    authorization for that action fails (the dedicated host of the vm is
    compared to the dedicated host of the template and since only admin is
    allowed to change hosts and the template is not pinned an exception is
    thrown).
    The fix is to remove the comparison of hosts in case the vm is created
    from a default template.
    
    Change-Id: I5f30844e3f992e4a1454e0ddeab6ae4d13591997
    Bug-Url: https://bugzilla.redhat.com/1284472
    Signed-off-by: Jenny Tokar <jtokar@redhat.com>
    (cherry picked from commit 4522754)

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AddVmCommand.java
@@ -1412,16 +1412,20 @@ public class AddVmCommand<T extends AddVmParameters> extends VmManagementCommand
 
     protected void addPermissionSubjectForAdminLevelProperties(List<PermissionSubject> permissionList) {
         VmStatic vmFromParams = getParameters().getVmStaticData();
+        VmTemplate vmTemplate = getVmTemplate();
 
-        if (vmFromParams != null && getVmTemplate() != null) {
+        if (vmFromParams != null && vmTemplate != null) {
             // user needs specific permission to change custom properties
-            if (!Objects.equals(vmFromParams.getCustomProperties(), getVmTemplate().getCustomProperties())) {
+            if (!Objects.equals(vmFromParams.getCustomProperties(), vmTemplate.getCustomProperties())) {
                 permissionList.add(new PermissionSubject(getClusterId(),
                         VdcObjectType.Cluster, ActionGroup.CHANGE_VM_CUSTOM_PROPERTIES));
             }
-
+            //if the template is blank we ignore his pinned hosts
+            if(vmTemplate.isBlank()){
+                return;
+            }
             Set<Guid> dedicatedVmForVdsFromUser = new HashSet<>(vmFromParams.getDedicatedVmForVdsList());
-            Set<Guid> dedicatedVmForVdsFromTemplate = new HashSet<>(getVmTemplate().getDedicatedVmForVdsList());
+            Set<Guid> dedicatedVmForVdsFromTemplate = new HashSet<>(vmTemplate.getDedicatedVmForVdsList());
             // host-specific parameters can be changed by administration role only
             if (!dedicatedVmForVdsFromUser.equals(dedicatedVmForVdsFromTemplate)
                     || !StringUtils.isEmpty(vmFromParams.getCpuPinning())) {
