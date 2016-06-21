1500465386f3e6d391d8a6a33d232be423cf46c0
Juan Hernandez <juan.hernandez@redhat.com>
2016-06-21 15:27:56 +0200

    restapi: Fix adding of quota storage limits
    The quota limit resources use inheritance and generics in order to avoid
    implementing the "add" operation twice, once for cluster limits and
    another time for storage limits. Unfortunately the JAX-RS implementation
    that we use doesn't recognize these generic "add" operations, resulting
    in a 405 error message when they are used. To avoid that error this
    patch refactors the relevant resources so that they don't use
    inheritance and generics. The result is more verbose, but simpler, and
    works.
    
    Change-Id: I4ee930ecbf6bd6d477f3746eaa409082688cd5a4
    Bug-Url: https://bugzilla.redhat.com/1348559
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendQuotaStorageLimitsResource.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendQuotaStorageLimitsResource.java
@@ -1,19 +1,28 @@
 package org.ovirt.engine.api.restapi.resource;
 
+import javax.ws.rs.core.Response;
+
 import org.ovirt.engine.api.model.QuotaStorageLimit;
 import org.ovirt.engine.api.model.QuotaStorageLimits;
 import org.ovirt.engine.api.resource.QuotaStorageLimitResource;
 import org.ovirt.engine.api.resource.QuotaStorageLimitsResource;
+import org.ovirt.engine.core.common.action.QuotaCRUDParameters;
+import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.businessentities.Quota;
 import org.ovirt.engine.core.common.businessentities.QuotaStorage;
+import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
 
 public class BackendQuotaStorageLimitsResource
-        extends BackendQuotaLimitsResource<QuotaStorageLimits, QuotaStorageLimit>
+        extends AbstractBackendCollectionResource<QuotaStorageLimit, Quota>
         implements QuotaStorageLimitsResource {
 
+    private Guid quotaId;
+
     protected BackendQuotaStorageLimitsResource(Guid quotaId) {
-        super(quotaId, QuotaStorageLimit.class);
+        super(QuotaStorageLimit.class, Quota.class);
+        this.quotaId = quotaId;
     }
 
     @Override
@@ -21,32 +30,51 @@ public class BackendQuotaStorageLimitsResource
         Quota quota = getQuota();
         QuotaStorageLimits limits = new QuotaStorageLimits();
         if (quota.getGlobalQuotaStorage() != null) {
-            addLimit(quota.getGlobalQuotaStorage(), quotaId.toString(), limits, quota);
+            addLimit(quotaId.toString(), limits, quota);
         } else if (quota.getQuotaStorages() != null) {
             for (QuotaStorage quotaStorage : quota.getQuotaStorages()) {
-                addLimit(quotaStorage, quotaStorage.getStorageId().toString(), limits, quota);
+                addLimit(quotaStorage.getStorageId().toString(), limits, quota);
             }
         }
         return limits;
     }
 
-    private void addLimit(QuotaStorage quotaStorage, String id, QuotaStorageLimits limits, Quota quota) {
+    private void addLimit(String id, QuotaStorageLimits limits, Quota quota) {
         QuotaStorageLimit limit = new QuotaStorageLimit();
         limit.setId(id);
         limits.getQuotaStorageLimits().add(addLinks(map(quota, limit)));
     }
 
-    @Override
-    public QuotaStorageLimitResource getLimitResource(String id) {
-        return inject(new BackendQuotaStorageLimitResource(id, quotaId));
+    public Response add(QuotaStorageLimit incoming) {
+        Quota entity = getQuota();
+        QuotaCRUDParameters parameters = new QuotaCRUDParameters(map(incoming, entity));
+        performAction(VdcActionType.UpdateQuota, parameters);
+        entity = getQuota();
+        updateIncomingId(incoming, entity);
+        QuotaStorageLimit model = map(entity, incoming);
+        model = addLinks(doPopulate(model, entity));
+        return Response.ok(model).build();
     }
 
-    @Override
-    protected void updateIncomingId(QuotaStorageLimit incoming, Quota entity) {
+    private void updateIncomingId(QuotaStorageLimit incoming, Quota entity) {
         if (incoming.isSetStorageDomain() && incoming.getStorageDomain().isSetId()) {
             incoming.setId(incoming.getStorageDomain().getId());
         } else {
             incoming.setId(entity.getId().toString());
         }
+    }
+
+    private Quota getQuota() {
+        return getEntity(
+            Quota.class,
+            VdcQueryType.GetQuotaByQuotaId,
+            new IdQueryParameters(quotaId),
+            quotaId.toString()
+        );
+    }
+
+    @Override
+    public QuotaStorageLimitResource getLimitResource(String id) {
+        return inject(new BackendQuotaStorageLimitResource(id, quotaId));
     }
 }
