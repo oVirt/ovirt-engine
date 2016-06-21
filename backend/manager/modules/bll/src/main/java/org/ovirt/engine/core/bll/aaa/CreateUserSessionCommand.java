a5726e93673e61506ea2bd3991c99532759543cf
Ravi Nori <rnori@redhat.com>
2016-06-21 11:56:14 -0400

    aaa: My Groups in Add Permission to User dialog throws NPE
    My Groups in Add Permissions dialog should invoke SSO authz search with
    the user's external id. The SSO DirectorySearch should search all
    namespaces for the user.
    
    Change-Id: I7f21217df77db1a7ad9672ce200cfd0217ee0608
    Bug-Url: https://bugzilla.redhat.com/1346932
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/aaa/CreateUserSessionCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/aaa/CreateUserSessionCommand.java
@@ -57,6 +57,7 @@ public class CreateUserSessionCommand<T extends CreateUserSessionParameters> ext
         if (user == null) {
             user = new DbUser();
             user.setId(DEFAULT_USER_ID);
+            user.setExternalId(params.getPrincipalId());
             user.setDomain(authzName);
         }
         user.setEmail(params.getEmail());
