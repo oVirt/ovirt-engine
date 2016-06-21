4ac42ccb6976681df864ea620abc7bf32454af12
Juan Hernandez <juan.hernandez@redhat.com>
2016-06-21 15:27:56 +0200

    restapi: Fix adding of quota storage limits
    The quota limit resources use inheritance and generics in order to avoid
    implementing the "add" operation twice, once for cluster limits and
    another time for storage limits. Unfortunately the JAX-RS implementation
    that we use doesn't recognize these generic "add" operations, resulting
    in a 405 error message when they are used. To avoid that error this
    patch refactors the relevant resources so that they don't use
    inheritance and generics. The result is more verbose, but simpler, and
    works.
    
    Change-Id: I4ee930ecbf6bd6d477f3746eaa409082688cd5a4
    Bug-Url: https://bugzilla.redhat.com/1348559
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    (cherry picked from commit 1500465386f3e6d391d8a6a33d232be423cf46c0)

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendQuotaClusterLimitsResource.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendQuotaClusterLimitsResource.java
@@ -1,19 +1,28 @@
 package org.ovirt.engine.api.restapi.resource;
 
+import javax.ws.rs.core.Response;
+
 import org.ovirt.engine.api.model.QuotaClusterLimit;
 import org.ovirt.engine.api.model.QuotaClusterLimits;
 import org.ovirt.engine.api.resource.QuotaClusterLimitResource;
 import org.ovirt.engine.api.resource.QuotaClusterLimitsResource;
+import org.ovirt.engine.core.common.action.QuotaCRUDParameters;
+import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.businessentities.Quota;
 import org.ovirt.engine.core.common.businessentities.QuotaCluster;
+import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.common.queries.VdcQueryType;
 import org.ovirt.engine.core.compat.Guid;
 
 public class BackendQuotaClusterLimitsResource
-        extends BackendQuotaLimitsResource<QuotaClusterLimits, QuotaClusterLimit>
+        extends AbstractBackendCollectionResource<QuotaClusterLimit, Quota>
         implements QuotaClusterLimitsResource {
 
+    private Guid quotaId;
+
     protected BackendQuotaClusterLimitsResource(Guid quotaId) {
-        super(quotaId, QuotaClusterLimit.class);
+        super(QuotaClusterLimit.class, Quota.class);
+        this.quotaId = quotaId;
     }
 
     @Override
@@ -21,19 +30,31 @@ public class BackendQuotaClusterLimitsResource
         Quota quota = getQuota();
         QuotaClusterLimits limits = new QuotaClusterLimits();
         if (quota.getGlobalQuotaCluster() != null) {
-            addLimit(quota.getGlobalQuotaCluster(), quotaId.toString(), limits, quota);
+            addLimit(quotaId.toString(), limits, quota);
         } else if (quota.getQuotaClusters() != null) {
             for (QuotaCluster quotaCluster : quota.getQuotaClusters()) {
-                addLimit(quotaCluster, quotaCluster.getClusterId().toString(), limits, quota);
+                addLimit(quotaCluster.getClusterId().toString(), limits, quota);
             }
         }
         return limits;
     }
 
-    private void addLimit(QuotaCluster quotaCluster, String id, QuotaClusterLimits limits, Quota quota) {
+    private void addLimit(String id, QuotaClusterLimits limits, Quota quota) {
         QuotaClusterLimit limit = new QuotaClusterLimit();
         limit.setId(id);
         limits.getQuotaClusterLimits().add(addLinks(map(quota, limit)));
+    }
+
+    @Override
+    public Response add(QuotaClusterLimit incoming) {
+        Quota entity = getQuota();
+        QuotaCRUDParameters parameters = new QuotaCRUDParameters(map(incoming, entity));
+        performAction(VdcActionType.UpdateQuota, parameters);
+        entity = getQuota();
+        updateIncomingId(incoming, entity);
+        QuotaClusterLimit model = map(entity, incoming);
+        model = addLinks(doPopulate(model, entity));
+        return Response.ok(model).build();
     }
 
     @Override
@@ -41,12 +62,20 @@ public class BackendQuotaClusterLimitsResource
         return inject(new BackendQuotaClusterLimitResource(id, quotaId));
     }
 
-    @Override
-    protected void updateIncomingId(QuotaClusterLimit incoming, Quota entity) {
+    private void updateIncomingId(QuotaClusterLimit incoming, Quota entity) {
         if (incoming.isSetCluster() && incoming.getCluster().isSetId()) {
             incoming.setId(incoming.getCluster().getId());
         } else {
             incoming.setId(entity.getId().toString());
         }
+    }
+
+    private Quota getQuota() {
+        return getEntity(
+            Quota.class,
+            VdcQueryType.GetQuotaByQuotaId,
+            new IdQueryParameters(quotaId),
+            quotaId.toString()
+        );
     }
 }
