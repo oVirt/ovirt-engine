806dbe8086e36b4c5b2bd8861772e0a1e26e0d3f
Jakub Niedermertl <jniederm@redhat.com>
2016-07-07 14:37:32 +0200

    core: Cluster update triggers VM update only if necessary
    There was a problem when user tried to edit Cluster while there was a
    migrating VM in the Cluster. The root cause was collision of VM locks
    of MigrateVmCommand and UpdateVmCommand called by UpdateClusterCommand.
    
    This patch doesn't solve the problem entirely. It only lowers the
    probability its occurrence by triggering UpdateVmCommand only if cluster
    compatibility level was changed.
    
    Change-Id: Ieccac1f08847681d5f326b9c54a99364720cfdb0
    Bug-Url: https://bugzilla.redhat.com/1349526
    Signed-off-by: Jakub Niedermertl <jniederm@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateClusterCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateClusterCommand.java
@@ -191,14 +191,19 @@ public class UpdateClusterCommand<T extends ManagementNetworkOnClusterOperationP
     }
 
     private boolean updateVms() {
+        final boolean compatibilityVersionUnchanged = Objects.equals(
+                oldCluster.getCompatibilityVersion(),
+                getParameters().getCluster().getCompatibilityVersion());
+        if (compatibilityVersionUnchanged) {
+            return true;
+        }
+
         List<VM> vmList = getVmDao().getAllForCluster(getParameters().getCluster().getId());
 
         for (VM vm : vmList) {
             if (!vm.isExternalVm() && !vm.isHostedEngine()) {
                 VmManagementParametersBase updateParams = new VmManagementParametersBase(vm);
-                if (!Objects.equals(oldCluster.getCompatibilityVersion(), getParameters().getCluster().getCompatibilityVersion())) {
-                    updateParams.setClusterLevelChangeToVersion(getParameters().getCluster().getCompatibilityVersion());
-                }
+                updateParams.setClusterLevelChangeToVersion(getParameters().getCluster().getCompatibilityVersion());
 
                 VdcReturnValueBase result = runInternalAction(
                         VdcActionType.UpdateVm,
