b84c4bfa9e8076fc5d8a0ab749b971ba8ea0b9fa
Tal Nisan <tnisan@redhat.com>
2016-07-11 12:59:56 +0200

    restapi: Remove old template disks API and introduce template attachments
    The templates/{template_id}/disks is an old API and should be removed.
    All template disks operations operations in V4 should be done
    through templates/{template_id}/diskattachments which was added in this
    patch.
    
    Change-Id: I6b7428969b6692c536abc15d99a7bc3317ad88f8
    Bug-Url: https://bugzilla.redhat.com/1353175
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

---
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendTemplateDiskAttachmentResource.java
@@ -0,0 +1,47 @@
+package org.ovirt.engine.api.restapi.resource;
+
+import javax.ws.rs.core.Response;
+
+import org.ovirt.engine.api.model.DiskAttachment;
+import org.ovirt.engine.api.model.Template;
+import org.ovirt.engine.api.resource.TemplateDiskAttachmentResource;
+import org.ovirt.engine.core.common.businessentities.VmDeviceId;
+import org.ovirt.engine.core.common.queries.VdcQueryType;
+import org.ovirt.engine.core.common.queries.VmDeviceIdQueryParameters;
+import org.ovirt.engine.core.compat.Guid;
+
+public class BackendTemplateDiskAttachmentResource
+        extends AbstractBackendActionableResource<DiskAttachment, org.ovirt.engine.core.common.businessentities.storage.DiskVmElement>
+        implements TemplateDiskAttachmentResource {
+
+    private Guid templateId;
+    private String diskId;
+
+    protected BackendTemplateDiskAttachmentResource(Guid templateId, String diskId) {
+        super(diskId, DiskAttachment.class, org.ovirt.engine.core.common.businessentities.storage.DiskVmElement.class);
+        this.templateId = templateId;
+        this.diskId = diskId;
+    }
+
+    @Override
+    public DiskAttachment get() {
+        return performGet(VdcQueryType.GetDiskVmElementById, new VmDeviceIdQueryParameters(new VmDeviceId(Guid.createGuidFromString(diskId), templateId)));
+    }
+
+    @Override
+    public Response remove() {
+        return getTemplateDiskResource().remove();
+    }
+
+    private BackendTemplateDiskResource getTemplateDiskResource() {
+        return inject(new BackendTemplateDiskResource(diskId, templateId));
+    }
+
+    @Override
+    protected DiskAttachment addParents(DiskAttachment attachment) {
+        Template template = new Template();
+        template.setId(templateId.toString());
+        attachment.setTemplate(template);
+        return attachment;
+    }
+}
