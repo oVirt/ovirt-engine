f417e634003557e414a196538e47f67b2644760b
Allon Mureinik <amureini@redhat.com>
2016-07-13 12:54:05 +0300

    core: StorageDomainNumberOfVmDisksComparator streaming
    The StorageDomainNumberOfVmDisksComparator constructor converts a
    collections of DiskImages to a map from the storage domain id to the
    number of VMs located on it.
    
    This is much easier to do with Java 8's steam APIs instead of
    reinventing the wheel ourselves.
    
    As a side effect of this patch, the Map was converted from
    Map<Guid, Integer> to Map<Guid, Long> to conform to
    Collectors.counting()'s API. This may add some minor memory
    consumption overhead, as Longs are larger than Integers, but this is
    negligible as this Comparator is extremely short lived.
    
    Change-Id: Id8271fd39eb270090f25ebd9bd32351d61fa465b
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/memory/sdcomparators/StorageDomainNumberOfVmDisksComparator.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/memory/sdcomparators/StorageDomainNumberOfVmDisksComparator.java
@@ -2,8 +2,8 @@ package org.ovirt.engine.core.bll.memory.sdcomparators;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.ovirt.engine.core.common.businessentities.StorageDomain;
 import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
@@ -11,26 +11,18 @@ import org.ovirt.engine.core.compat.Guid;
 
 public class StorageDomainNumberOfVmDisksComparator implements Comparator<StorageDomain> {
 
-    private Map<Guid, Integer> numOfVmDisksInStorageDomains;
+    private Map<Guid, Long> numOfVmDisksInStorageDomains;
 
     public StorageDomainNumberOfVmDisksComparator(Collection<DiskImage> vmDisks) {
-        setUpNumOfVmDisksInStorageDomains(vmDisks);
+        numOfVmDisksInStorageDomains = vmDisks.stream().collect(Collectors.groupingBy(
+                vmDisk -> vmDisk.getStorageIds().get(0), Collectors.counting()
+        ));
     }
 
     @Override
     public int compare(StorageDomain storageDomain, StorageDomain storageDomain2) {
-        Integer numOfVmDisksInStorageDomain = numOfVmDisksInStorageDomains.getOrDefault(storageDomain.getId(), 0);
-        Integer numOfVmDisksInStorageDomain2 = numOfVmDisksInStorageDomains.getOrDefault(storageDomain2.getId(), 0);
+        Long numOfVmDisksInStorageDomain = numOfVmDisksInStorageDomains.getOrDefault(storageDomain.getId(), 0L);
+        Long numOfVmDisksInStorageDomain2 = numOfVmDisksInStorageDomains.getOrDefault(storageDomain2.getId(), 0L);
         return numOfVmDisksInStorageDomain.compareTo(numOfVmDisksInStorageDomain2);
-    }
-
-    private void setUpNumOfVmDisksInStorageDomains(Collection<DiskImage> vmDisks) {
-        numOfVmDisksInStorageDomains = new HashMap<>();
-
-        for (DiskImage vmDisk : vmDisks) {
-            Guid vmDiskStorageDomainId = vmDisk.getStorageIds().get(0);
-            numOfVmDisksInStorageDomains.put(vmDiskStorageDomainId,
-                    numOfVmDisksInStorageDomains.getOrDefault(vmDiskStorageDomainId, 0) + 1);
-        }
     }
 }
