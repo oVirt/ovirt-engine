e9d4baeee72cb429cff752227335f71de3392c77
Martin Betak <mbetak@redhat.com>
2016-07-13 13:33:59 +0200

    backend: Filter orphaned Host Devieces on refresh
    In some hotplug scenarios (especially those related to SCSI devices) it can
    happen that libvirt reports to VDSM an internally inconsistent list of devices,
    namely that the parent_device points to a device not in the list or is empty
    altogether.
    
    Those cases violate the DB constraint imposed on the host_device and cause the
    RefreshHostDevicesCommand transaction to fail. Since those cases are impossible
    to handle at lower layers and we need to maintain consistend view of the host
    devices for our main business logic, we now ignore those invalid devices in the
    hostdev refresh cycle.
    
    Change-Id: I520d7b6cc7ef099da7b6a67381a7cdc8d2ae0e64
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1315100
    Signed-off-by: Martin Betak <mbetak@redhat.com>

---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/hostdev/RefreshHostDevicesCommandTest.java
@@ -0,0 +1,73 @@
+package org.ovirt.engine.core.bll.hostdev;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.junit.Test;
+import org.ovirt.engine.core.common.businessentities.Entities;
+import org.ovirt.engine.core.common.businessentities.HostDevice;
+
+public class RefreshHostDevicesCommandTest {
+    @Test
+    public void testFilterOrphanedHostDevices() throws Exception {
+        Map<String, HostDevice> fetchedDevices = Entities.entitiesByName(Arrays.asList(
+            new HostDevice() {{
+                setDeviceName("computer");
+                setParentDeviceName("computer");
+            }},
+            new HostDevice() {{
+                setDeviceName("good");
+                setParentDeviceName("computer");
+            }},
+            new HostDevice() {{
+                setDeviceName("child_of_good");
+                setParentDeviceName("good");
+            }},
+            new HostDevice() {{
+                setDeviceName("orphan");
+                setParentDeviceName("non_existing");
+            }},
+            new HostDevice() {{
+                setDeviceName("child_of_orphan");
+                setParentDeviceName("orphan");
+            }},
+            new HostDevice() {{
+                setDeviceName("bad");
+                setParentDeviceName("");
+            }},
+            new HostDevice() {{
+                setDeviceName("child_of_bad");
+                setParentDeviceName("bad");
+            }},
+            new HostDevice() {{
+                setDeviceName("worse");
+                setParentDeviceName(null);
+            }},
+            new HostDevice() {{
+                setDeviceName("child_of_worse");
+                setParentDeviceName("worse");
+            }}
+        ));
+
+        Map<String, HostDevice> filteredDevices = RefreshHostDevicesCommand.filterOrphanedDevices(fetchedDevices);
+
+        Map<String, HostDevice> expectedDevices = Entities.entitiesByName(Arrays.asList(
+            new HostDevice() {{
+                setDeviceName("computer");
+                setParentDeviceName("computer");
+            }},
+            new HostDevice() {{
+                setDeviceName("good");
+                setParentDeviceName("computer");
+            }},
+            new HostDevice() {{
+                setDeviceName("child_of_good");
+                setParentDeviceName("good");
+            }}
+        ));
+
+        assertEquals(expectedDevices, filteredDevices);
+    }
+}
