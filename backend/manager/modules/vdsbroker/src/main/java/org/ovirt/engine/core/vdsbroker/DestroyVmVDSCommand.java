062ff365046657949b96eda4c41146c3c1b379c3
Arik Hadas <ahadas@redhat.com>
2016-07-14 18:22:52 +0300

    core: cleanup in destroy vm
    1. Refine DAOs instead of using DbFacade
    2. Inline the 'parameters' variable
    3. Remove getVmNumaNodeDao
    
    Change-Id: I27f0d30e9d1c897ff49e83ce3d16dfb9527314b4
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/DestroyVmVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/DestroyVmVDSCommand.java
@@ -2,14 +2,17 @@ package org.ovirt.engine.core.vdsbroker;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VMStatus;
 import org.ovirt.engine.core.common.businessentities.network.VmNetworkInterface;
 import org.ovirt.engine.core.common.vdscommands.DestroyVmVDSCommandParameters;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.VmDao;
 import org.ovirt.engine.core.dao.VmNumaNodeDao;
+import org.ovirt.engine.core.dao.network.VmNetworkInterfaceDao;
 import org.ovirt.engine.core.utils.transaction.TransactionSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,39 +21,48 @@ public class DestroyVmVDSCommand<P extends DestroyVmVDSCommandParameters> extend
 
     private static final Logger log = LoggerFactory.getLogger(DestroyVmVDSCommand.class);
 
+    @Inject
+    private VmDao vmDao;
+    @Inject
+    private VmNetworkInterfaceDao vmNetworkInterfaceDao;
+    @Inject
+    private VmNumaNodeDao vmNumaNodeDao;
+
     public DestroyVmVDSCommand(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeVmCommand() {
-        final DestroyVmVDSCommandParameters parameters = getParameters();
-        resourceManager.removeAsyncRunningVm(parameters.getVmId());
+        resourceManager.removeAsyncRunningVm(getParameters().getVmId());
 
-        final VM curVm = DbFacade.getInstance().getVmDao().get(parameters.getVmId());
-        curVm.setInterfaces(DbFacade.getInstance().getVmNetworkInterfaceDao().getAllForVm(curVm.getId()));
-        curVm.setvNumaNodeList(getVmNumaNodeDao().getAllVmNumaNodeByVmId(curVm.getId()));
+        final VM curVm = vmDao.get(getParameters().getVmId());
+        curVm.setInterfaces(vmNetworkInterfaceDao.getAllForVm(curVm.getId()));
+        curVm.setvNumaNodeList(vmNumaNodeDao.getAllVmNumaNodeByVmId(curVm.getId()));
 
-        VDSReturnValue vdsReturnValue = resourceManager.runVdsCommand(VDSCommandType.Destroy, parameters);
+        VDSReturnValue vdsReturnValue = resourceManager.runVdsCommand(
+                VDSCommandType.Destroy,
+                getParameters());
+
         if (vdsReturnValue.getSucceeded()) {
             if (curVm.getStatus() == VMStatus.Down) {
                 getVDSReturnValue().setReturnValue(VMStatus.Down);
             }
 
-            changeStatus(parameters, curVm);
+            changeStatus(getParameters(), curVm);
 
             TransactionSupport.executeInNewTransaction(() -> {
                 curVm.setStopReason(getParameters().getReason());
                 vmManager.update(curVm.getDynamicData());
                 vmManager.update(curVm.getStatisticsData());
                 update(curVm.getInterfaces());
-                getVmNumaNodeDao().massUpdateVmNumaNodeRuntimePinning(curVm.getvNumaNodeList());
+                vmNumaNodeDao.massUpdateVmNumaNodeRuntimePinning(curVm.getvNumaNodeList());
                 return null;
             });
             getVDSReturnValue().setReturnValue(curVm.getStatus());
         } else if (vdsReturnValue.getExceptionObject() != null) {
             log.error("Failed to destroy VM '{}' in VDS = '{}' , error = '{}'",
-                    parameters.getVmId(),
+                    getParameters().getVmId(),
                     getParameters().getVdsId(),
                     vdsReturnValue.getExceptionString());
             getVDSReturnValue().setSucceeded(false);
@@ -65,10 +77,6 @@ public class DestroyVmVDSCommand<P extends DestroyVmVDSCommandParameters> extend
         if (getParameters().getVdsId().equals(curVm.getRunOnVds())) {
             resourceManager.internalSetVmStatus(curVm, VMStatus.PoweringDown);
         }
-    }
-
-    protected VmNumaNodeDao getVmNumaNodeDao() {
-        return DbFacade.getInstance().getVmNumaNodeDao();
     }
 
     private void update(List<VmNetworkInterface> interfaces) {
