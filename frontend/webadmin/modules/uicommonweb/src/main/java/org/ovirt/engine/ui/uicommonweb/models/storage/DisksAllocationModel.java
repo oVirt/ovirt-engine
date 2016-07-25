f7724de9a638efdf15b7ada0f3df8ebbfd85f592
Daniel Erez <derez@redhat.com>
2016-07-25 16:20:10 +0300

    webadmin: rename getTemplateVolumeType method
    Renamed getTemplateVolumeType to a clearer one
    by its functionality: getVolumeType.
    
    Change-Id: I13912345e1c4180b5197e0c486315088e59115dc
    Bug-Url: https://bugzilla.redhat.com/1359736
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/storage/DisksAllocationModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/storage/DisksAllocationModel.java
@@ -324,7 +324,7 @@ public class DisksAllocationModel extends EntityModel {
             if (diskModel.getVolumeFormat().getIsAvailable()) {
                 VolumeFormat volumeFormat = diskModel.getVolumeFormat().getSelectedItem();
                 diskImage.setVolumeFormat(volumeFormat);
-                diskImage.setVolumeType(AsyncDataProvider.getInstance().getTemplateVolumeType(
+                diskImage.setVolumeType(AsyncDataProvider.getInstance().getVolumeType(
                         volumeFormat, storageDomain.getStorageType()));
             }
             else if (diskModel.getVolumeType().getIsAvailable()) {
