a3b6bf409a08a4522780636cd7cccaf5c32eaffa
Yevgeny Zaspitsky <yzaspits@redhat.com>
2016-08-03 17:49:24 +0300

    Revert "engine: Create default network QoS for ovirtmgmt upon DC creation"
    This reverts commit 7bbfdab92ac600994319aac2bc5ee35ec11f3dc9.
    
    Change-Id: I3aad475b1b075ffc3b182082d63304f48cbb56e6
    Bug-Url: https://bugzilla.redhat.com/1364145
    Signed-off-by: Yevgeny Zaspitsky <yzaspits@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/pool/AddEmptyStoragePoolCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/pool/AddEmptyStoragePoolCommand.java
@@ -13,14 +13,10 @@ import org.ovirt.engine.core.bll.utils.PermissionSubject;
 import org.ovirt.engine.core.bll.utils.VersionSupport;
 import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.VdcObjectType;
-import org.ovirt.engine.core.common.action.QosParametersBase;
 import org.ovirt.engine.core.common.action.StoragePoolManagementParameter;
-import org.ovirt.engine.core.common.action.VdcActionType;
-import org.ovirt.engine.core.common.action.VdcReturnValueBase;
 import org.ovirt.engine.core.common.businessentities.ActionGroup;
 import org.ovirt.engine.core.common.businessentities.StoragePool;
 import org.ovirt.engine.core.common.businessentities.StoragePoolStatus;
-import org.ovirt.engine.core.common.businessentities.network.HostNetworkQos;
 import org.ovirt.engine.core.common.businessentities.network.Network;
 import org.ovirt.engine.core.common.businessentities.network.VnicProfile;
 import org.ovirt.engine.core.common.errors.EngineMessage;
@@ -31,8 +27,6 @@ import org.ovirt.engine.core.dao.network.NetworkFilterDao;
 public class AddEmptyStoragePoolCommand<T extends StoragePoolManagementParameter> extends
         StoragePoolManagementCommandBase<T> {
 
-    private static final String DEFAULT_MGMT_NETWORK_QOS_NAME = "Default-Mgmt-Net-QoS";
-    private static final String DEFAULT_MGMT_NETWORK_QOS_DESC = "Default management network QoS";
     @Inject
     private ManagementNetworkUtil managementNetworkUtil;
 
@@ -79,45 +73,17 @@ public class AddEmptyStoragePoolCommand<T extends StoragePoolManagementParameter
     }
 
     private void addDefaultNetworks() {
-        final Guid dataCenterId = getStoragePool().getId();
-        Guid defaultQosId = createDefaultManagementNetworkQos(dataCenterId);
-
         Network net = new Network();
         net.setId(Guid.newGuid());
         net.setName(managementNetworkUtil.getDefaultManagementNetworkName());
         net.setDescription(AddClusterCommand.DefaultNetworkDescription);
-        net.setDataCenterId(dataCenterId);
+        net.setDataCenterId(getStoragePool().getId());
         net.setVmNetwork(true);
-        net.setQosId(defaultQosId);
-
         getNetworkDao().save(net);
-
         NetworkHelper.addPermissionsOnNetwork(getCurrentUser().getId(), net.getId());
         VnicProfile profile = NetworkHelper.createVnicProfile(net, networkFilterDao);
         getVnicProfileDao().save(profile);
         NetworkHelper.addPermissionsOnVnicProfile(getCurrentUser().getId(), profile.getId(), true);
-    }
-
-    private Guid createDefaultManagementNetworkQos(Guid dataCenterId) {
-        final HostNetworkQos hostNetworkQos = new HostNetworkQos();
-        hostNetworkQos.setStoragePoolId(dataCenterId);
-        hostNetworkQos.setName(DEFAULT_MGMT_NETWORK_QOS_NAME);
-        hostNetworkQos.setDescription(DEFAULT_MGMT_NETWORK_QOS_DESC);
-
-        hostNetworkQos.setOutAverageLinkshare(50);
-
-        final QosParametersBase<HostNetworkQos> hostNetworkQosParameters = new QosParametersBase<>();
-        hostNetworkQosParameters.setQos(hostNetworkQos);
-
-        final VdcReturnValueBase returnValue =
-                runInternalAction(VdcActionType.AddHostNetworkQos, hostNetworkQosParameters);
-        if (returnValue.getSucceeded()) {
-            return returnValue.getActionReturnValue();
-        } else {
-            propagateFailure(returnValue);
-            throw new RuntimeException(
-                    String.format("Failed to create default network QoS: %s", returnValue.getDescription()));
-        }
     }
 
     @Override
