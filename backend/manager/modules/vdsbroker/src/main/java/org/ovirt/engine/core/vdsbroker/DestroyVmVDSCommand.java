438ccd3dca3ffde20ed516a5a6c1b9d65246d091
Arik Hadas <ahadas@redhat.com>
2016-08-11 13:47:18 +0300

    core: use only vm dynamic data in destroy command
    There is no real need to fetch the whole VM object, only
    the dynamic part is required.
    
    Change-Id: Ic36e27be2dac0d5b5f69502c5ad28e645eafb66d
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/DestroyVmVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/DestroyVmVDSCommand.java
@@ -1,13 +1,16 @@
 package org.ovirt.engine.core.vdsbroker;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
-import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VMStatus;
+import org.ovirt.engine.core.common.businessentities.VmDynamic;
+import org.ovirt.engine.core.common.businessentities.VmNumaNode;
 import org.ovirt.engine.core.common.vdscommands.DestroyVmVDSCommandParameters;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
-import org.ovirt.engine.core.dao.VmDao;
+import org.ovirt.engine.core.dao.VmDynamicDao;
 import org.ovirt.engine.core.dao.VmNumaNodeDao;
 import org.ovirt.engine.core.utils.transaction.TransactionSupport;
 import org.slf4j.Logger;
@@ -18,7 +21,7 @@ public class DestroyVmVDSCommand<P extends DestroyVmVDSCommandParameters> extend
     private static final Logger log = LoggerFactory.getLogger(DestroyVmVDSCommand.class);
 
     @Inject
-    private VmDao vmDao;
+    private VmDynamicDao vmDynamicDao;
     @Inject
     private VmNumaNodeDao vmNumaNodeDao;
 
@@ -30,8 +33,8 @@ public class DestroyVmVDSCommand<P extends DestroyVmVDSCommandParameters> extend
     protected void executeVmCommand() {
         resourceManager.removeAsyncRunningVm(getParameters().getVmId());
 
-        final VM curVm = vmDao.get(getParameters().getVmId());
-        curVm.setvNumaNodeList(vmNumaNodeDao.getAllVmNumaNodeByVmId(curVm.getId()));
+        VmDynamic curVm = vmDynamicDao.get(getParameters().getVmId());
+        List<VmNumaNode> vmNumaNodes = vmNumaNodeDao.getAllVmNumaNodeByVmId(curVm.getId());
 
         VDSReturnValue vdsReturnValue = resourceManager.runVdsCommand(
                 VDSCommandType.Destroy,
@@ -44,11 +47,11 @@ public class DestroyVmVDSCommand<P extends DestroyVmVDSCommandParameters> extend
 
             changeStatus(getParameters(), curVm);
 
+            curVm.setStopReason(getParameters().getReason());
             TransactionSupport.executeInNewTransaction(() -> {
-                curVm.setStopReason(getParameters().getReason());
-                vmManager.update(curVm.getDynamicData());
-                curVm.getvNumaNodeList().forEach(node -> node.getVdsNumaNodeList().clear());
-                vmNumaNodeDao.massUpdateVmNumaNodeRuntimePinning(curVm.getvNumaNodeList());
+                vmManager.update(curVm);
+                vmNumaNodes.forEach(node -> node.getVdsNumaNodeList().clear());
+                vmNumaNodeDao.massUpdateVmNumaNodeRuntimePinning(vmNumaNodes);
                 return null;
             });
             getVDSReturnValue().setReturnValue(curVm.getStatus());
@@ -64,10 +67,10 @@ public class DestroyVmVDSCommand<P extends DestroyVmVDSCommandParameters> extend
         }
     }
 
-    private void changeStatus(DestroyVmVDSCommandParameters parameters, VM curVm) {
+    private void changeStatus(DestroyVmVDSCommandParameters parameters, VmDynamic curVm) {
         // do the state transition only if that VM is really running on SRC
         if (getParameters().getVdsId().equals(curVm.getRunOnVds())) {
-            resourceManager.internalSetVmStatus(curVm.getDynamicData(), VMStatus.PoweringDown);
+            resourceManager.internalSetVmStatus(curVm, VMStatus.PoweringDown);
         }
     }
 }
