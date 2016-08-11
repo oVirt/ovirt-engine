ce393479b92cc20577052fd602a5d4f17a38a624
Arik Hadas <ahadas@redhat.com>
2016-08-11 18:23:59 +0300

    core: cleanup managing vm
    1. Use the member resourceManager rather than the deprecated method
    ResourceManager#getInstance
    2. Inline the method 'updateVmDataChangedTime'
    
    Change-Id: I4834e0e6ce876850ec74edf22247a84adf4a60e5
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/ManagingVmCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/ManagingVmCommand.java
@@ -4,28 +4,21 @@ import org.ovirt.engine.core.common.vdscommands.VdsAndVmIDVDSParametersBase;
 
 public abstract class ManagingVmCommand<P extends VdsAndVmIDVDSParametersBase> extends VDSCommandBase<P> {
 
-    protected final VmManager vmManager;
+    protected VmManager vmManager;
 
     public ManagingVmCommand(P parameters) {
         super(parameters);
-        vmManager = ResourceManager.getInstance().getVmManager(parameters.getVmId());
     }
 
     protected void executeVDSCommand() {
+        vmManager = resourceManager.getVmManager(getParameters().getVmId());
         vmManager.lock();
         try {
             executeVmCommand();
-            updateVmDataChangedTime();
+            vmManager.updateVmDataChangedTime();
         } finally {
             vmManager.unlock();
         }
-    }
-
-    /**
-     * update the time the vm data was changed
-     */
-    protected void updateVmDataChangedTime() {
-        vmManager.updateVmDataChangedTime();
     }
 
     protected abstract void executeVmCommand();
