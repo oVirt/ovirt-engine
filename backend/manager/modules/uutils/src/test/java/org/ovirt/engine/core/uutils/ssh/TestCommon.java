63f2ea22be306788b88488daf316d1d0de51988b
Allon Mureinik <amureini@redhat.com>
2016-09-18 22:34:39 +0300

    core: TestCommon#main
    Remove TestCommon's main function - we have a perfectly good Maven
    environment set up to run JUnit tests. There's no reason to maintain a
    competing main too.
    
    Change-Id: I75c9279a3afd58349371284c0c546061efca6f06
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/uutils/src/test/java/org/ovirt/engine/core/uutils/ssh/TestCommon.java
+++ backend/manager/modules/uutils/src/test/java/org/ovirt/engine/core/uutils/ssh/TestCommon.java
@@ -4,18 +4,6 @@ package org.ovirt.engine.core.uutils.ssh;
  * Example of execution:
  *  $ mvn -Dssh-host=host1 -Dssh-test-port=22 -Dssh-test-user=root -Dssh-test-password=password -Dssh-test-p12=a.p12 -Dssh-test-p12-password=password -Dssh-test-large-file-size=50000000
  *
- * Standalone testing:
- *  $ mkdir t
- *  $ cp ~/.m2/repository/junit/junit/4.8.1/junit-4.8.1.jar t/
- *  $ cp ~/.m2/repository/org/apache/mina/mina-core/2.0.4/mina-core-2.0.4.jar t/
- *  $ cp ~/.m2/repository/org/apache/sshd/sshd-core/0.7.0/sshd-core-0.7.0.jar t/
- *  $ cp ~/.m2/repository/org/mockito/mockito-core/1.8.5/mockito-core-1.8.5.jar t/
- *  $ cp ~/.m2/repository/org/objenesis/objenesis/1.2/objenesis-1.2.jar t
- *  $ cp ~/.m2/repository/org/slf4j/slf4j-api/1.6.4/slf4j-api-1.6.4.jar t
- *  $ cp ~/.m2/repository/org/slf4j/slf4j-simple/1.6.4/slf4j-simple-1.6.4.jar t
- *  $ javac -d t -cp t/*:. ./backend/manager/modules/utils/src/{main,test}/java/org/ovirt/engine/core/utils/ssh/*.java
- *  $ ( cd t && java -cp *:. org.ovirt.engine.core.utils.ssh.TestCommon )
- *
  * Create PKCS#12:
  * $ openssl req -newkey rsa:2048 -x509 -days 365 -out a.crt -keyout a.key
  * $ openssl pkcs12 -export -inkey a.key -in a.crt -out a.p12
@@ -31,9 +19,6 @@ import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
 
-import org.junit.internal.TextListener;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -114,28 +99,5 @@ public class TestCommon {
             sshd.stop();
             sshd = null;
         }
-    }
-
-    /**
-     * main.
-     *
-     * @param args
-     *            arguments.
-     *
-     *            Use system properities to alternate behvaior.
-     */
-    public static void main(String[] args) throws Throwable {
-
-        initialize();
-
-        JUnitCore core = new JUnitCore();
-        core.addListener(new TextListener(System.out));
-        Result result = core.run(
-                ConstraintByteArrayOutputStreamTest.class,
-                BasicTest.class,
-                CommandTest.class,
-                TimeoutTest.class,
-                TransferTest.class);
-        System.exit(result.wasSuccessful() ? 0 : 1);
     }
 }
