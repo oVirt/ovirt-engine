986fc5023872d0d4644f98047d39e785bfe69dec
Allon Mureinik <amureini@redhat.com>
2016-09-19 22:39:57 +0300

    core: CreateGlusterVolumeCommandTest constants
    Make constants private static final as per Java's best practices.
    
    Change-Id: Ib8a414e63a04cb18ae7a3de7f5ce952461b3b333
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/gluster/CreateGlusterVolumeCommandTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/gluster/CreateGlusterVolumeCommandTest.java
@@ -52,9 +52,9 @@ public class CreateGlusterVolumeCommandTest extends BaseCommandTest {
 
     private static final String serverName = "myhost";
 
-    private final Guid clusterId = new Guid("c0dd8ca3-95dd-44ad-a88a-440a6e3d8106");
+    private static final Guid clusterId = new Guid("c0dd8ca3-95dd-44ad-a88a-440a6e3d8106");
 
-    private final Guid serverId = new Guid("d7f10a21-bbf2-4ffd-aab6-4da0b3b2ccec");
+    private static final Guid serverId = new Guid("d7f10a21-bbf2-4ffd-aab6-4da0b3b2ccec");
 
     private CreateGlusterVolumeCommand cmd;
 
