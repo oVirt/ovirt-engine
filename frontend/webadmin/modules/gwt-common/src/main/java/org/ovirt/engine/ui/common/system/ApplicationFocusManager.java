6407341c2bc0102bffad661a6fbbc4de5a7a388c
Vojtech Szocs <vszocs@redhat.com>
2016-09-23 17:19:42 +0200

    webadmin: Remove IE-specific code in ApplicationFocusManager
    IE9+ supports addEventListener() and deprecates attachEvent().
    
    IE11+ no longer supports attachEvent().
    
    Change-Id: I588dd8f46f8b0f583688068fcb1446feb254a309
    Bug-Url: https://bugzilla.redhat.com/1378935
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/system/ApplicationFocusManager.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/system/ApplicationFocusManager.java
@@ -1,7 +1,5 @@
 package org.ovirt.engine.ui.common.system;
 
-import org.ovirt.engine.ui.common.uicommon.ClientAgentType;
-import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HasHandlers;
@@ -15,20 +13,12 @@ import com.google.inject.Inject;
 public class ApplicationFocusManager implements HasHandlers {
 
     private final EventBus eventBus;
-    private final boolean isIE;
-
-    // IE tracks document.activeElement and fires onfocusin and onfocusout events as
-    // the activeElement changes. As the onfocusout events occur, we must track what
-    // the activeElement is. When an onfocusout event happens and the activeElement
-    // doesn't change, then the window has lost focus.
-    private JavaScriptObject activeElement;
 
     private boolean hasFocus = true;
 
     @Inject
-    public ApplicationFocusManager(EventBus eventBus, ClientAgentType clientAgentType) {
+    public ApplicationFocusManager(EventBus eventBus) {
         this.eventBus = eventBus;
-        this.isIE = clientAgentType.isIE();
         attachWindowFocusEvents();
     }
 
@@ -58,19 +48,8 @@ public class ApplicationFocusManager implements HasHandlers {
     private native void attachWindowFocusEvents() /*-{
         var context = this;
 
-        if (context.@org.ovirt.engine.ui.common.system.ApplicationFocusManager::isIE) {
-            $doc.attachEvent("onfocusin", onFocus);
-            $doc.attachEvent("onfocusout", function() {
-                if (context.@org.ovirt.engine.ui.common.system.ApplicationFocusManager::activeElement != $doc.activeElement) {
-                    context.@org.ovirt.engine.ui.common.system.ApplicationFocusManager::activeElement = $doc.activeElement;
-                } else {
-                    onBlur();
-                }
-            });
-        } else {
-            $wnd.addEventListener("focus", onFocus, false);
-            $wnd.addEventListener("blur", onBlur, false);
-        }
+        $wnd.addEventListener("focus", onFocus, false);
+        $wnd.addEventListener("blur", onBlur, false);
 
         function onFocus() {
             context.@org.ovirt.engine.ui.common.system.ApplicationFocusManager::onWindowFocus()();
