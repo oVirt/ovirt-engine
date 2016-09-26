60e04e16d40a736fedb3e3cca1496e0026670de0
Roy Golan <rgolan@redhat.com>
2016-09-26 09:18:53 +0300

    core: cleanup unnecessary modifiers in IOvfBuilder
    Change-Id: I02b494e3220e49704bab184f241a8acb63cacef5
    Bug-Url: https://bugzilla.redhat.com/1358383
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/IOvfBuilder.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/IOvfBuilder.java
@@ -2,16 +2,16 @@ package org.ovirt.engine.core.utils.ovf;
 
 public interface IOvfBuilder {
     // Namespace URIs:
-    public static final String OVF_URI = "http://schemas.dmtf.org/ovf/envelope/1/";
-    public static final String RASD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData";
-    public static final String VSSD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData";
-    public static final String XSI_URI = "http://www.w3.org/2001/XMLSchema-instance";
+    String OVF_URI = "http://schemas.dmtf.org/ovf/envelope/1/";
+    String RASD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData";
+    String VSSD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData";
+    String XSI_URI = "http://www.w3.org/2001/XMLSchema-instance";
 
     // Namespace prefixes:
-    public static final String OVF_PREFIX = "ovf";
-    public static final String RASD_PREFIX = "rasd";
-    public static final String VSSD_PREFIX = "vssd";
-    public static final String XSI_PREFIX = "xsi";
+    String OVF_PREFIX = "ovf";
+    String RASD_PREFIX = "rasd";
+    String VSSD_PREFIX = "vssd";
+    String XSI_PREFIX = "xsi";
 
     void buildReference();
 
