542e4a318584c8601159b4bd6d576f1bed6ffe90
Simone Tiraboschi <stirabos@redhat.com>
2016-09-29 14:31:22 +0200

    hosted-engine: add a DB cleaner utility
    Hosted-engine auto-import procedure is not going to work
    if the engine DB already contains  hosted-engine entities and,
    on the other side, they cannot be removed from the
    engine to avoid breaking a working environment.
    This was preventing the user from migration an hosted-engine env
    from one storage device to another via backup/restore.
    
    Change-Id: Id61ae0b05a75018ded532d7a0c38c15b4b885803
    Bug-Url: https://bugzilla.redhat.com/1240466
    Bug-Url: https://bugzilla.redhat.com/1235200
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

---
+++ packaging/setup/dbutils/hecleaner_sp.sql
@@ -0,0 +1,76 @@
+/************************************************************************************************
+ The following are helper SP for hecleaner utility and are not exposed to the application DAOs
+
+If you add a function here, drop it in hecleaner_sp_drop.sql
+************************************************************************************************/
+
+CREATE OR REPLACE FUNCTION DeleteHostedEngineStorageVM()
+  RETURNS void AS
+$procedure$
+DECLARE
+    v_HostedEngineVmName varchar;
+    v_vm_guid UUID;
+    v_vm_guid_check UUID;
+    v_storage_id UUID;
+    v_image_guid UUID;
+    v_disk_id UUID;
+BEGIN
+    SELECT option_value
+        INTO v_HostedEngineVmName
+        FROM getvdcoptionbyname('HostedEngineVmName', 'general') ;
+    BEGIN
+        SELECT vm_guid
+            INTO STRICT v_vm_guid
+            FROM getvmstaticbyname(v_HostedEngineVmName);
+        EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE EXCEPTION 'VM % not found', v_HostedEngineVmName;
+        WHEN TOO_MANY_ROWS THEN
+            RAISE EXCEPTION 'VM % not unique', v_HostedEngineVmName;
+    END;
+    BEGIN
+        SELECT storage_id, image_guid, disk_id
+            INTO STRICT v_storage_id, v_image_guid, v_disk_id
+            FROM getdisksvmguids(ARRAY[v_vm_guid]);
+        EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE EXCEPTION 'VM % disk not found', v_HostedEngineVmName;
+        WHEN TOO_MANY_ROWS THEN
+            RAISE EXCEPTION 'VM % disk not unique', v_HostedEngineVmName;
+    END;
+    BEGIN
+        SELECT vm_guid
+            INTO STRICT v_vm_guid_check
+            FROM getvmsbystoragedomainid(v_storage_id);
+        EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE WARNING 'VM % is not on the hosted-engine storage domain', v_HostedEngineVmName;
+        WHEN TOO_MANY_ROWS THEN
+            RAISE EXCEPTION 'The hosted-engine storage domain contains more than one vm.';
+        IF v_vm_guid <> v_vm_guid_check THEN
+            RAISE EXCEPTION 'The hosted-engine storage domain contains a vm that is not the hosted-engine one.';
+        END IF;
+    END;
+    PERFORM deletestorage_server_connections(id)
+        FROM (
+            SELECT id
+                FROM getstorageserverconnectionsfordomain(v_storage_id)
+        ) t;
+    PERFORM Force_Delete_storage_domain(v_storage_id);
+END;
+$procedure$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION DeleteHostedEngineHosts()
+  RETURNS void AS
+$procedure$
+BEGIN
+    PERFORM deletevds(vds_id)
+        FROM (
+            SELECT vds_id
+                FROM vds_statistics
+                WHERE ha_score IS NOT NULL AND ha_configured
+        ) t;
+END;
+$procedure$
+LANGUAGE plpgsql;
---
+++ packaging/setup/dbutils/hecleaner_sp_drop.sql
@@ -0,0 +1,5 @@
+/************************************************************************************************
+Drop stuff created by hecleaner_sp.sql
+************************************************************************************************/
+DROP FUNCTION IF EXISTS DeleteHostedEngineStorageVM();
+DROP FUNCTION IF EXISTS DeleteHostedEngineHosts();
