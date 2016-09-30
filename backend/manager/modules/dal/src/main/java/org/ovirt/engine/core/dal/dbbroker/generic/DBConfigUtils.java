4ada53057ade84523cff71b45ec056e9307bfc22
Allon Mureinik <amureini@redhat.com>
2016-10-01 00:37:24 +0300

    core: Move generic getValue to ConfigUtilsBase
    Moved the generic getValue implementation to ConfigBaseBase to create
    a better separation of layers - ConfigUtilsBase deals with all the
    logic regarding parsing values, handling defaults, etc, and
    DbConfigUtils adds only its DB specific bits - retrieving from the
    database, caching, etc.
    
    Change-Id: Ieada7dc63cdc394b5d3e9c031a324e0c986b0a2a
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dal/dbbroker/generic/DBConfigUtils.java
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dal/dbbroker/generic/DBConfigUtils.java
@@ -1,25 +1,15 @@
 package org.ovirt.engine.core.dal.dbbroker.generic;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.common.businessentities.VdcOption;
-import org.ovirt.engine.core.common.config.ConfigCommon;
 import org.ovirt.engine.core.common.config.ConfigValues;
-import org.ovirt.engine.core.common.config.OptionBehaviourAttribute;
 import org.ovirt.engine.core.common.config.Reloadable;
-import org.ovirt.engine.core.compat.Version;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 import org.ovirt.engine.core.dao.VdcOptionDao;
 import org.ovirt.engine.core.utils.ConfigUtilsBase;
-import org.ovirt.engine.core.utils.crypt.EngineEncryptionUtils;
-import org.ovirt.engine.core.utils.serialization.json.JsonObjectDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,113 +25,6 @@ public class DBConfigUtils extends ConfigUtilsBase {
         _vdcOptionCache.clear();
         List<VdcOption> list = db.getVdcOptionDao().getAll();
         list.forEach(DBConfigUtils::updateOption);
-    }
-
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    private static Object parseValue(String value, String name, Class<?> fieldType) {
-        if (value == null) {
-            return null;
-        }
-
-        try {
-            if (fieldType == Integer.class) {
-                return Integer.parseInt(value);
-            } else if (fieldType == Long.class) {
-                return Long.parseLong(value);
-            } else if (fieldType == Boolean.class) {
-                return Boolean.parseBoolean(value);
-            } else if (fieldType == Version.class) {
-                return new Version(value);
-            } else if (fieldType == Date.class) {
-                return new SimpleDateFormat("k:m:s").parse(value);
-            } else if (fieldType == Double.class) {
-                return Double.parseDouble(value);
-            } else if (Map.class.isAssignableFrom(fieldType)) {
-                /* This if statement is to make a difference between the old and the json-style
-                 * map support:
-                 * - if it is enclosed between brackets, then it is handled as json object and a map is returned
-                 * - otherwise it is handled as a string, the client code handles parsing it
-                 */
-                if(StringUtils.startsWith(value, "{") && StringUtils.endsWith(value, "}")) {
-                    return new JsonObjectDeserializer().deserialize(value, HashMap.class);
-                } else {
-                    return value;
-                }
-            } else if (fieldType.isEnum()) {
-                return Enum.valueOf((Class<Enum>)fieldType, value.toUpperCase());
-            } else {
-                return value;
-            }
-        } catch (Exception e2) {
-            log.error("Error parsing option '{}' value: {}", name, e2.getMessage());
-            log.debug("Exception", e2);
-            return null;
-        }
-    }
-
-    /**
-     * Returns the typed value of the given option. returns default value if option.option_value is null
-     */
-    private static Object getValue(VdcOption option) {
-        Object result = option.getOptionValue();
-        EnumValue enumValue = parseEnumValue(option.getOptionName());
-        if (enumValue != null) {
-            final OptionBehaviourAttribute optionBehaviour = enumValue.getOptionBehaviour();
-            final Class<?> fieldType = enumValue.getFieldType();
-            final String defaultValue = enumValue.getDefaultValue();
-            result = parseValue(option.getOptionValue(), option.getOptionName(), fieldType);
-
-            // if null use default from @DefaultValueAttribute
-            if (result == null) {
-                result = parseValue(defaultValue, option.getOptionName(), fieldType);
-            }
-
-            if (optionBehaviour != null) {
-                Map<String, Object> values = null;
-                switch (optionBehaviour.behaviour()) {
-                // split string by comma for List<string> constructor
-                case CommaSeparatedStringArray:
-                    result = Arrays.asList(((String) result).split("[,]", -1));
-                    break;
-                case Password:
-                    try {
-                        result = EngineEncryptionUtils.decrypt((String) result);
-                    } catch (Exception e) {
-                        log.error("Failed to decrypt value for property '{}', encrypted value will be used. Error: {} ",
-                                option.getOptionName(), e.getMessage());
-                        log.debug("Exception", e);
-                    }
-                    break;
-                case ValueDependent:
-                    // get the config that this value depends on
-                    VdcOption dependsOption = new VdcOption();
-                    dependsOption.setOptionName(optionBehaviour.dependentOn().toString());
-                    dependsOption.setVersion(ConfigCommon.defaultConfigurationVersion);
-                    String prefix = (String) getValue(dependsOption);
-                    // combine the prefix with the 'real value'
-                    if (prefix != null) {
-                        VdcOption real = new VdcOption();
-                        real.setOptionName(String.format("%1$s%2$s", prefix, optionBehaviour.realValue()));
-                        real.setVersion(ConfigCommon.defaultConfigurationVersion);
-                        result = getValue(real);
-                    }
-                    break;
-                case CommaSeparatedVersionArray:
-                    HashSet<Version> versions = new HashSet<>();
-                    for (String ver : result.toString().split("[,]", -1)) {
-                        try {
-                            versions.add(new Version(ver));
-                        } catch (Exception e) {
-                            log.error("Could not parse version '{}' for config value '{}'",
-                                    ver, option.getOptionName());
-                        }
-                    }
-                    result = versions;
-                    break;
-                }
-            }
-        }
-        return result;
     }
 
     /**
