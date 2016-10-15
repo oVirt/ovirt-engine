dd44d0de3ff51e3b56a0efe41725aa4da6823839
Allon Mureinik <amureini@redhat.com>
2016-10-15 03:22:33 +0300

    core: TagsDirectorTest unused imports
    Change-Id: Ifd431b59c7d61302d64aaf7e013141c39bd00695
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/TagsDirectorTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/TagsDirectorTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
