0b886c49043c66e171c0ab9b4f87e6a259d437c5
Allon Mureinik <amureini@redhat.com>
2016-10-15 05:14:01 +0300

    webadmin: Remove ConfigUtil commented out code
    Change-Id: I8062046bfb92f4b24d22b81ae0007922a090bf0e
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- frontend/webadmin/modules/gwt-extension/src/main/java/org/ovirt/engine/ui/uioverrides/org/ovirt/engine/core/common/config/ConfigUtil.java
+++ frontend/webadmin/modules/gwt-extension/src/main/java/org/ovirt/engine/ui/uioverrides/org/ovirt/engine/core/common/config/ConfigUtil.java
@@ -3,12 +3,6 @@ package org.ovirt.engine.core.common.config;
 //Only to ignore GWT errors
 public class ConfigUtil {
     public static String resolvePath(String baseDirectory, String path) {
-        /*
-        if (new File(path).isAbsolute()) {
-            return path;
-        } else {
-            return baseDirectory + File.separator + path;
-        }*/
         return null;
     }
 }
