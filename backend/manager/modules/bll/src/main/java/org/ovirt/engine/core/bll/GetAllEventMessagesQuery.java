b8e551f8579c69f8206c03f44b8de4fd725cdfa2
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllEventMessagesQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I17b9705caa645b0df23b149e8a51f712bd2d26f1
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllEventMessagesQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllEventMessagesQuery.java
@@ -2,18 +2,23 @@ package org.ovirt.engine.core.bll;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.AuditLog;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.dao.AuditLogDao;
 
 public class GetAllEventMessagesQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+    @Inject
+    private AuditLogDao auditLogDao;
+
     public GetAllEventMessagesQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        List<AuditLog> eventsList = getDbFacade()
-                .getAuditLogDao().getAll(getUserID(), getParameters().isFiltered());
+        List<AuditLog> eventsList = auditLogDao.getAll(getUserID(), getParameters().isFiltered());
         getQueryReturnValue().setReturnValue(eventsList);
     }
 }
