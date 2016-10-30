759d9c200383a7f2c9867bb1fa6dec5a20582bd7
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVdsCertificateSubjectByVmIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I62b71cc008cb09e7f2392d06569969e6f452cc42
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVdsCertificateSubjectByVmIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVdsCertificateSubjectByVmIdQuery.java
@@ -1,13 +1,23 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VdsStatic;
 import org.ovirt.engine.core.common.businessentities.VmDynamic;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.common.queries.VdcQueryReturnValue;
 import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.VdsStaticDao;
+import org.ovirt.engine.core.dao.VmDynamicDao;
 import org.ovirt.engine.core.utils.CertificateSubjectHelper;
 
 public class GetVdsCertificateSubjectByVmIdQuery <P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDynamicDao vmDynamicDao;
+
+    @Inject
+    private VdsStaticDao vdsStaticDao;
+
     public GetVdsCertificateSubjectByVmIdQuery(P parameters) {
         super(parameters);
     }
@@ -22,11 +32,11 @@ public class GetVdsCertificateSubjectByVmIdQuery <P extends IdQueryParameters> e
         // of the certificate to the value returned by the query:
         Guid vmId = getParameters().getId();
         if (vmId != null) {
-            VmDynamic vm = getDbFacade().getVmDynamicDao().get(vmId);
+            VmDynamic vm = vmDynamicDao.get(vmId);
             if (vm != null) {
                 Guid vdsId = vm.getRunOnVds();
                 if (vdsId != null) {
-                    VdsStatic vds = getDbFacade().getVdsStaticDao().get(vdsId);
+                    VdsStatic vds = vdsStaticDao.get(vdsId);
                     queryReturnValue.setSucceeded(true);
                     queryReturnValue.setReturnValue(CertificateSubjectHelper.getCertificateSubject(vds.getHostName()));
                 }
