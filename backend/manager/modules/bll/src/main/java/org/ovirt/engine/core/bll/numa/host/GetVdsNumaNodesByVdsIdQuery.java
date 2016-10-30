e4ddb8dd358e4d34ca3106d0d88f6a662b1c68b0
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVdsNumaNodesByVdsIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: If93ab969215bb4a1321e77e9ac9ac5edc17d2bb3
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/numa/host/GetVdsNumaNodesByVdsIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/numa/host/GetVdsNumaNodesByVdsIdQuery.java
@@ -2,11 +2,16 @@ package org.ovirt.engine.core.bll.numa.host;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.VdsNumaNode;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.VdsNumaNodeDao;
 
 public class GetVdsNumaNodesByVdsIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VdsNumaNodeDao vdsNumaNodeDao;
 
     public GetVdsNumaNodesByVdsIdQuery(P parameters) {
         super(parameters);
@@ -14,8 +19,7 @@ public class GetVdsNumaNodesByVdsIdQuery<P extends IdQueryParameters> extends Qu
 
     @Override
     protected void executeQueryCommand() {
-       List<VdsNumaNode> numaNodes = getDbFacade().getVdsNumaNodeDao()
-                .getAllVdsNumaNodeByVdsId(getParameters().getId());
+        List<VdsNumaNode> numaNodes = vdsNumaNodeDao.getAllVdsNumaNodeByVdsId(getParameters().getId());
         getQueryReturnValue().setReturnValue(numaNodes);
     }
 
