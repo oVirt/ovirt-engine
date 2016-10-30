8f7d4985b95b1c193b8794bc262953e800b6bcba
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetConsoleDevicesQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I2d7e7b0d5b412a1ad1737bc93f25fcab1ff237e6
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetConsoleDevicesQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetConsoleDevicesQuery.java
@@ -3,12 +3,16 @@ package org.ovirt.engine.core.bll;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VmDevice;
 import org.ovirt.engine.core.common.businessentities.VmDeviceGeneralType;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.dao.VmDeviceDao;
 
 public class GetConsoleDevicesQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDeviceDao vmDeviceDao;
 
     public GetConsoleDevicesQuery(P parameters) {
         super(parameters);
@@ -16,7 +20,7 @@ public class GetConsoleDevicesQuery<P extends IdQueryParameters> extends Queries
 
     @Override
     protected void executeQueryCommand() {
-        final List<VmDevice> consoleDevices = getVmDeviceDao().getVmDeviceByVmIdAndType(getParameters().getId(),
+        final List<VmDevice> consoleDevices = vmDeviceDao.getVmDeviceByVmIdAndType(getParameters().getId(),
                 VmDeviceGeneralType.CONSOLE);
 
         List<String> result = new ArrayList<>(consoleDevices.size());
@@ -26,9 +30,4 @@ public class GetConsoleDevicesQuery<P extends IdQueryParameters> extends Queries
 
         getQueryReturnValue().setReturnValue(result);
     }
-
-    protected VmDeviceDao getVmDeviceDao() {
-        return getDbFacade().getVmDeviceDao();
-    }
-
 }
