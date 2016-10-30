4ab09ddbc913ea9f76a6a8bdc242106a901306a0
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVdsByNameQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I6e0797d3d68fbf9e4c90b4ada84e3796a8493132
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVdsByNameQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVdsByNameQuery.java
@@ -1,16 +1,22 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.queries.NameQueryParameters;
+import org.ovirt.engine.core.dao.VdsDao;
 
 public class GetVdsByNameQuery<P extends NameQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VdsDao vdsDao;
+
     public GetVdsByNameQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        VDS vds = getDbFacade().getVdsDao().getByName(getParameters().getName());
+        VDS vds = vdsDao.getByName(getParameters().getName());
 
         getQueryReturnValue().setReturnValue(vds);
 
