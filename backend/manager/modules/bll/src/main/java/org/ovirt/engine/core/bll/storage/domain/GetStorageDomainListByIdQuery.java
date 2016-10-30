9db4e395eeb188897c135246482adffb0adb3214
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetStorageDomainListByIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I2f266546e3f962ee1a3896d1a83ad0a033719866
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/domain/GetStorageDomainListByIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/domain/GetStorageDomainListByIdQuery.java
@@ -3,21 +3,25 @@ package org.ovirt.engine.core.bll.storage.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.StorageDomain;
 import org.ovirt.engine.core.common.businessentities.StorageDomainSharedStatus;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.StorageDomainDao;
 
 public class GetStorageDomainListByIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private StorageDomainDao storageDomainDao;
+
     public GetStorageDomainListByIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        List<StorageDomain> result = DbFacade.getInstance().getStorageDomainDao().getAllForStorageDomain(
-                getParameters().getId());
+        List<StorageDomain> result = storageDomainDao.getAllForStorageDomain(getParameters().getId());
         ArrayList<StorageDomain> temp = new ArrayList<>(result);
         for (StorageDomain domain : temp) {
             if (domain.getStorageDomainSharedStatus() == StorageDomainSharedStatus.Unattached) {
