badc81eb8aa3dfcabdf4ff7d699dbb9114102a92
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetLibvirtSecretByIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I37ce03a16b80dd9b20e57220df41dd0e51ca27e6
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/cinder/GetLibvirtSecretByIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/cinder/GetLibvirtSecretByIdQuery.java
@@ -1,9 +1,14 @@
 package org.ovirt.engine.core.bll.storage.disk.cinder;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.LibvirtSecretDao;
 
 public class GetLibvirtSecretByIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private LibvirtSecretDao libvirtSecretDao;
 
     public GetLibvirtSecretByIdQuery(P parameters) {
         super(parameters);
@@ -11,6 +16,6 @@ public class GetLibvirtSecretByIdQuery<P extends IdQueryParameters> extends Quer
 
     @Override
     protected void executeQueryCommand() {
-        setReturnValue(getDbFacade().getLibvirtSecretDao().get(getParameters().getId()));
+        setReturnValue(libvirtSecretDao.get(getParameters().getId()));
     }
 }
