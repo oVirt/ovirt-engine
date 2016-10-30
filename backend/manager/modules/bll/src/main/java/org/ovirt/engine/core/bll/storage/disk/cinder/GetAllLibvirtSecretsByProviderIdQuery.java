b0f57a247daa449a6b74edeef5892cb6810df2c8
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllLibvirtSecretsByProviderIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I0f1e34014e8cd41d83093f1a5f63fabda6072eca
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/cinder/GetAllLibvirtSecretsByProviderIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/cinder/GetAllLibvirtSecretsByProviderIdQuery.java
@@ -1,9 +1,14 @@
 package org.ovirt.engine.core.bll.storage.disk.cinder;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.LibvirtSecretDao;
 
 public class GetAllLibvirtSecretsByProviderIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private LibvirtSecretDao libvirtSecretDao;
 
     public GetAllLibvirtSecretsByProviderIdQuery(P parameters) {
         super(parameters);
@@ -11,6 +16,6 @@ public class GetAllLibvirtSecretsByProviderIdQuery<P extends IdQueryParameters> 
 
     @Override
     protected void executeQueryCommand() {
-        setReturnValue(getDbFacade().getLibvirtSecretDao().getAllByProviderId(getParameters().getId()));
+        setReturnValue(libvirtSecretDao.getAllByProviderId(getParameters().getId()));
     }
 }
