a3169408e4415a47743c7be4f2caad3ac70e1389
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVmsByInstanceTypeIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I14741c6a40d5f9b4fcbc4dd4f8b4563cda6463bc
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmsByInstanceTypeIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmsByInstanceTypeIdQuery.java
@@ -1,19 +1,23 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.VmDao;
 
 /**
  * A query to retrieve all the VMs connected to a given instance type.
  */
 public class GetVmsByInstanceTypeIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDao vmDao;
+
     public GetVmsByInstanceTypeIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade()
-                .getVmDao()
-                .getVmsListByInstanceType(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(vmDao.getVmsListByInstanceType(getParameters().getId()));
     }
 }
