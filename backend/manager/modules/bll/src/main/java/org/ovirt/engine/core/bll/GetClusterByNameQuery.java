f39c970ccf5308d28c0a75341d13423ed90ffb32
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetClusterByNameQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ie217a93ffbe8fefd7f2ca653357dccff48df9d21
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetClusterByNameQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetClusterByNameQuery.java
@@ -1,15 +1,21 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.NameQueryParameters;
+import org.ovirt.engine.core.dao.ClusterDao;
 
 public class GetClusterByNameQuery<P extends NameQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private ClusterDao clusterDao;
+
     public GetClusterByNameQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getClusterDao().getByName(
+        getQueryReturnValue().setReturnValue(clusterDao.getByName(
                 getParameters().getName(), getUserID(), getParameters().isFiltered()));
     }
 }
