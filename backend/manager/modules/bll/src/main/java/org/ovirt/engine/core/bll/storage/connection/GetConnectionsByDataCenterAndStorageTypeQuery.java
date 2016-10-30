f7427ca840f378bffcc228c79ac24f801b2ab424
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetConnectionsByDataCenterAndStorageTypeQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ic3b5bb69a2036346432216a3e28152881e6c5c0d
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/connection/GetConnectionsByDataCenterAndStorageTypeQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/connection/GetConnectionsByDataCenterAndStorageTypeQuery.java
@@ -2,12 +2,17 @@ package org.ovirt.engine.core.bll.storage.connection;
 
 import java.util.EnumSet;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.StorageDomainStatus;
 import org.ovirt.engine.core.common.queries.GetConnectionsByDataCenterAndStorageTypeParameters;
+import org.ovirt.engine.core.dao.StorageServerConnectionDao;
 
 public class GetConnectionsByDataCenterAndStorageTypeQuery<P extends GetConnectionsByDataCenterAndStorageTypeParameters>
         extends QueriesCommandBase<P> {
+    @Inject
+    private StorageServerConnectionDao storageServerConnectionDao;
 
     public GetConnectionsByDataCenterAndStorageTypeQuery(P parameters) {
         super(parameters);
@@ -16,7 +21,7 @@ public class GetConnectionsByDataCenterAndStorageTypeQuery<P extends GetConnecti
     @Override
     protected void executeQueryCommand() {
         getQueryReturnValue().setReturnValue(
-                getDbFacade().getStorageServerConnectionDao()
+                storageServerConnectionDao
                         .getStorageConnectionsByStorageTypeAndStatus(getParameters().getId(),
                                 getParameters().getStorageType(),
                                 EnumSet.allOf(StorageDomainStatus.class)));
