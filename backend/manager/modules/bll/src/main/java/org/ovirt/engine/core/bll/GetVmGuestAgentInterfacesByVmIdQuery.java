2ae6610a1fea644b20355ca28ad5bde749a6ffc3
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVmGuestAgentInterfacesByVmIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I81597dd272ce2c43a672c988664648d36cb1f08a
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmGuestAgentInterfacesByVmIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmGuestAgentInterfacesByVmIdQuery.java
@@ -1,9 +1,15 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.VmGuestAgentInterfaceDao;
 
 public class GetVmGuestAgentInterfacesByVmIdQuery<P extends IdQueryParameters>
         extends QueriesCommandBase<P> {
+
+    @Inject
+    private VmGuestAgentInterfaceDao vmGuestAgentInterfaceDao;
 
     public GetVmGuestAgentInterfacesByVmIdQuery(P parameters) {
         super(parameters);
@@ -11,7 +17,7 @@ public class GetVmGuestAgentInterfacesByVmIdQuery<P extends IdQueryParameters>
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getVmGuestAgentInterfaceDao()
+        getQueryReturnValue().setReturnValue(vmGuestAgentInterfaceDao
                 .getAllForVm(getParameters().getId(), getUserID(), getParameters().isFiltered()));
     }
 }
--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetVmGuestAgentInterfacesByVmIdQueryTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetVmGuestAgentInterfacesByVmIdQueryTest.java
@@ -1,10 +1,10 @@
 package org.ovirt.engine.core.bll;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.mockito.Mock;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.VmGuestAgentInterfaceDao;
@@ -13,14 +13,14 @@ import org.ovirt.engine.core.dao.VmGuestAgentInterfaceDao;
 public class GetVmGuestAgentInterfacesByVmIdQueryTest
         extends AbstractUserQueryTest<IdQueryParameters, GetVmGuestAgentInterfacesByVmIdQuery<IdQueryParameters>> {
 
+    @Mock
+    private VmGuestAgentInterfaceDao vmGuestAgentInterfaceDao;
+
     @Test
     public void testExecuteQuery() {
         IdQueryParameters params = getQueryParameters();
         Guid vmId = Guid.newGuid();
         when(params.getId()).thenReturn(vmId);
-
-        VmGuestAgentInterfaceDao vmGuestAgentInterfaceDao = mock(VmGuestAgentInterfaceDao.class);
-        when(getQuery().getDbFacade().getVmGuestAgentInterfaceDao()).thenReturn(vmGuestAgentInterfaceDao);
 
         getQuery().executeQueryCommand();
         verify(vmGuestAgentInterfaceDao).getAllForVm(vmId,
