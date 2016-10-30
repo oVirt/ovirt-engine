ecc280d1631f272a6e0f22c95cf809b9a1031cae
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetSoundDevicesQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I2ffec56c5dcfa14506adad8eb4b805d7b1702135
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetSoundDevicesQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetSoundDevicesQuery.java
@@ -3,12 +3,16 @@ package org.ovirt.engine.core.bll;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VmDevice;
 import org.ovirt.engine.core.common.businessentities.VmDeviceGeneralType;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.dao.VmDeviceDao;
 
 public class GetSoundDevicesQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDeviceDao vmDeviceDao;
 
     public GetSoundDevicesQuery(P parameters) {
         super(parameters);
@@ -17,16 +21,12 @@ public class GetSoundDevicesQuery<P extends IdQueryParameters> extends QueriesCo
     @Override
     protected void executeQueryCommand() {
         List<VmDevice> soundDevList =
-                getVmDeviceDao().getVmDeviceByVmIdAndType(getParameters().getId(), VmDeviceGeneralType.SOUND);
+                vmDeviceDao.getVmDeviceByVmIdAndType(getParameters().getId(), VmDeviceGeneralType.SOUND);
 
         List<String> result = new ArrayList<>(soundDevList.size());
         for (VmDevice v : soundDevList) {
             result.add(v.getDevice());
         }
         getQueryReturnValue().setReturnValue(result);
-    }
-
-    protected VmDeviceDao getVmDeviceDao() {
-        return getDbFacade().getVmDeviceDao();
     }
 }
