3f017f7e2eafacb1a3a31f1f90f5a586843100f5
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetNetworkLabelsByNetworkIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ica72e351167b69b0d8a50a9167c22031a2ecd9af
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/dc/GetNetworkLabelsByNetworkIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/dc/GetNetworkLabelsByNetworkIdQuery.java
@@ -4,21 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.network.Network;
 import org.ovirt.engine.core.common.businessentities.network.pseudo.NetworkLabel;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.network.NetworkDao;
 import org.ovirt.engine.core.utils.NetworkUtils;
 
 public class GetNetworkLabelsByNetworkIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private NetworkDao networkDao;
+
     public GetNetworkLabelsByNetworkIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        Network network =
-                getDbFacade().getNetworkDao().get(getParameters().getId(), getUserID(), getParameters().isFiltered());
+        Network network = networkDao.get(getParameters().getId(), getUserID(), getParameters().isFiltered());
         getQueryReturnValue().setReturnValue(network == null || !NetworkUtils.isLabeled(network)
                 ? Collections.<NetworkLabel> emptyList()
                 : new ArrayList<>(Arrays.asList(new NetworkLabel(network.getLabel()))));
