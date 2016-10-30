813221497924f5343c754c15694b52aeeb67dc2a
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllClustersQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Icaa270f91214755da376a82a5e2a55a9ac5a53f9
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllClustersQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllClustersQuery.java
@@ -1,16 +1,20 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.dao.ClusterDao;
 
 public class GetAllClustersQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+    @Inject
+    private ClusterDao clusterDao;
+
     public GetAllClustersQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade()
-                .getClusterDao()
-                .getAll(getUserID(), getParameters().isFiltered()));
+        getQueryReturnValue().setReturnValue(clusterDao.getAll(getUserID(), getParameters().isFiltered()));
     }
 }
