889baed27cc9722a3292e1681101339a92e715df
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetStepsByJobIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I8c4425e3eb9c1e97021c12def2ab97756ddd3367
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetStepsByJobIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetStepsByJobIdQuery.java
@@ -1,8 +1,13 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.GetStepsByJobIdQueryParameters;
+import org.ovirt.engine.core.dao.StepDao;
 
 public class GetStepsByJobIdQuery <P extends GetStepsByJobIdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private StepDao stepDao;
 
     public GetStepsByJobIdQuery(P parameters) {
         super(parameters);
@@ -10,6 +15,6 @@ public class GetStepsByJobIdQuery <P extends GetStepsByJobIdQueryParameters> ext
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getStepDao().getStepsByJobId(getParameters().getJobId()));
+        getQueryReturnValue().setReturnValue(stepDao.getStepsByJobId(getParameters().getJobId()));
     }
 }
