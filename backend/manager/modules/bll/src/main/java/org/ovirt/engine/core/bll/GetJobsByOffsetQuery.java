6eca1579eb378246edd70e8e60f738dc93fd036e
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetJobsByOffsetQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I2b2afd0854d950735a7c3c12ae6c5b2058a5e1a0
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetJobsByOffsetQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetJobsByOffsetQuery.java
@@ -2,16 +2,20 @@ package org.ovirt.engine.core.bll;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.job.Job;
 import org.ovirt.engine.core.common.queries.GetJobsByOffsetQueryParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.JobDao;
 
 /**
  * Returns a list of Jobs by a given offset
  */
 public class GetJobsByOffsetQuery<P extends GetJobsByOffsetQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private JobDao jobDao;
 
     public GetJobsByOffsetQuery(P parameters) {
         super(parameters);
@@ -23,7 +27,7 @@ public class GetJobsByOffsetQuery<P extends GetJobsByOffsetQueryParameters> exte
         final int pageSize = Config.<Integer> getValue(ConfigValues.JobPageSize);
 
         int offset = Math.max(getParameters().getOffset(), 0);
-        List<Job> jobs = DbFacade.getInstance().getJobDao().getJobsByOffsetAndPageSize(offset * pageSize, pageSize);
+        List<Job> jobs = jobDao.getJobsByOffsetAndPageSize(offset * pageSize, pageSize);
         getQueryReturnValue().setReturnValue(jobs);
     }
 
