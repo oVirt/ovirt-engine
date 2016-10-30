77716d6180cb73c132a3f48ee1717d1c8f4c1b46
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetManagementInterfaceAddressByVmIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I05e8606d5d50a7b96f775a99ba4478b15f34a909
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetManagementInterfaceAddressByVmIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetManagementInterfaceAddressByVmIdQuery.java
@@ -1,12 +1,22 @@
 package org.ovirt.engine.core.bll.network.host;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.VmDao;
+import org.ovirt.engine.core.dao.network.InterfaceDao;
 
 public class GetManagementInterfaceAddressByVmIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+
+    @Inject
+    private VmDao vmDao;
+
+    @Inject
+    private InterfaceDao interfaceDao;
 
     public GetManagementInterfaceAddressByVmIdQuery(P parameters) {
         super(parameters);
@@ -17,14 +27,11 @@ public class GetManagementInterfaceAddressByVmIdQuery<P extends IdQueryParameter
         VdsNetworkInterface nic = null;
         Guid vmId = getParameters().getId();
         if (vmId != null) {
-            VM vm = getDbFacade().getVmDao().get(vmId);
+            VM vm = vmDao.get(vmId);
             if (vm != null) {
                 Guid vdsId = vm.getRunOnVds();
                 if (vdsId != null) {
-                    nic =
-                        getDbFacade().getInterfaceDao().getManagedInterfaceForVds(vdsId,
-                                getUserID(),
-                                getParameters().isFiltered());
+                    nic = interfaceDao.getManagedInterfaceForVds(vdsId, getUserID(), getParameters().isFiltered());
                 }
             }
         }
--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/host/GetManagementInterfaceAddressByVmIdQueryTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/host/GetManagementInterfaceAddressByVmIdQueryTest.java
@@ -1,10 +1,10 @@
 package org.ovirt.engine.core.bll.network.host;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.mockito.Mock;
 import org.ovirt.engine.core.bll.AbstractUserQueryTest;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.VM;
@@ -15,6 +15,12 @@ import org.ovirt.engine.core.dao.VmDao;
 import org.ovirt.engine.core.dao.network.InterfaceDao;
 
 public class GetManagementInterfaceAddressByVmIdQueryTest extends AbstractUserQueryTest<IdQueryParameters, GetManagementInterfaceAddressByVmIdQuery<IdQueryParameters>> {
+    @Mock
+    private VmDao vmDaoMock;
+
+    @Mock
+    private InterfaceDao interfaceDaoMock;
+
     @Test
     public void testExecuteQuery() {
         Guid vmID = Guid.newGuid();
@@ -30,12 +36,8 @@ public class GetManagementInterfaceAddressByVmIdQueryTest extends AbstractUserQu
         IdQueryParameters paramsMock = getQueryParameters();
         when(paramsMock.getId()).thenReturn(vmID);
 
-        InterfaceDao interfaceDaoMock = mock(InterfaceDao.class);
-        VmDao vmDaoMock = mock(VmDao.class);
-        when(getDbFacadeMockInstance().getVmDao()).thenReturn(vmDaoMock);
         when(vmDaoMock.get(vmID)).thenReturn(vm);
         when(interfaceDaoMock.getManagedInterfaceForVds(vdsID, getUser().getId(), getQueryParameters().isFiltered())).thenReturn(managementInterface);
-        when(getDbFacadeMockInstance().getInterfaceDao()).thenReturn(interfaceDaoMock);
 
         getQuery().executeQueryCommand();
 
