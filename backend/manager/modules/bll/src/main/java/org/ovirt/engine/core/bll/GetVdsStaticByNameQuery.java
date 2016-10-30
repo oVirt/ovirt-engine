d7d1e661aa6082481216f99a99321eadef8d4908
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVdsStaticByNameQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I1675a708d887b756206ca709d236139f80a1e874
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVdsStaticByNameQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVdsStaticByNameQuery.java
@@ -1,16 +1,22 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VdsStatic;
 import org.ovirt.engine.core.common.queries.NameQueryParameters;
+import org.ovirt.engine.core.dao.VdsStaticDao;
 
 public class GetVdsStaticByNameQuery<P extends NameQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VdsStaticDao vdsStaticDao;
+
     public GetVdsStaticByNameQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        VdsStatic vds = getDbFacade().getVdsStaticDao().getByVdsName(getParameters().getName());
+        VdsStatic vds = vdsStaticDao.getByVdsName(getParameters().getName());
 
         getQueryReturnValue().setReturnValue(vds);
 
