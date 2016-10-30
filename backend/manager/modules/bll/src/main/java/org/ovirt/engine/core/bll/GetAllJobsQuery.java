127b50e3149306688a8a3213a7b020003345eb21
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllJobsQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I097cff1d18b448ecfc8e99b639e29277ab16c8f8
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllJobsQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllJobsQuery.java
@@ -1,8 +1,13 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.dao.JobDao;
 
 public class GetAllJobsQuery <P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+    @Inject
+    private JobDao jobDao;
 
     public GetAllJobsQuery(P parameters) {
         super(parameters);
@@ -10,6 +15,6 @@ public class GetAllJobsQuery <P extends VdcQueryParametersBase> extends QueriesC
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getJobDao().getAll());
+        getQueryReturnValue().setReturnValue(jobDao.getAll());
     }
 }
