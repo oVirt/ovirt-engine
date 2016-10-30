d237afa523f2ddfb16c3a23f31656ec84a013b78
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetRngDeviceQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I8f5a7d98b4170394d9ed15237455cfb4595492a9
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetRngDeviceQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetRngDeviceQuery.java
@@ -3,14 +3,19 @@ package org.ovirt.engine.core.bll;
 import java.util.Collections;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.context.EngineContext;
 import org.ovirt.engine.core.common.businessentities.VmDevice;
 import org.ovirt.engine.core.common.businessentities.VmDeviceGeneralType;
 import org.ovirt.engine.core.common.businessentities.VmRngDevice;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.common.utils.VmDeviceType;
+import org.ovirt.engine.core.dao.VmDeviceDao;
 
 public class GetRngDeviceQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDeviceDao vmDeviceDao;
 
     public GetRngDeviceQuery(P parameters) {
         super(parameters);
@@ -22,7 +27,7 @@ public class GetRngDeviceQuery<P extends IdQueryParameters> extends QueriesComma
 
     @Override
     protected void executeQueryCommand() {
-        final List<VmDevice> vmDevices = getDbFacade().getVmDeviceDao().getVmDeviceByVmIdTypeAndDevice(
+        final List<VmDevice> vmDevices = vmDeviceDao.getVmDeviceByVmIdTypeAndDevice(
                 getParameters().getId(),
                 VmDeviceGeneralType.RNG,
                 VmDeviceType.VIRTIO.getName(),
