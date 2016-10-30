54e51663f04b27e2ed1ed1f316715db22798baaa
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllStoragePoolsQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ie0fd43bda0fa43c060f035e92f433cfdc9b961f2
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/pool/GetAllStoragePoolsQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/pool/GetAllStoragePoolsQuery.java
@@ -1,9 +1,14 @@
 package org.ovirt.engine.core.bll.storage.pool;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.dao.StoragePoolDao;
 
 public class GetAllStoragePoolsQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+    @Inject
+    private StoragePoolDao storagePoolDao;
 
     public GetAllStoragePoolsQuery(P parameters) {
         super(parameters);
@@ -11,7 +16,6 @@ public class GetAllStoragePoolsQuery<P extends VdcQueryParametersBase> extends Q
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getStoragePoolDao()
-                .getAll(getUserID(), getParameters().isFiltered()));
+        getQueryReturnValue().setReturnValue(storagePoolDao.getAll(getUserID(), getParameters().isFiltered()));
     }
 }
