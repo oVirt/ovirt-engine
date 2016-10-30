da64f3e6048c60bca7e5b562f67e10c26a132028
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetDiskProfileByIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I6017502d79fedf45ecbb271a2862a27f73047bc4
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/profiles/GetDiskProfileByIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/profiles/GetDiskProfileByIdQuery.java
@@ -1,9 +1,14 @@
 package org.ovirt.engine.core.bll.profiles;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.profiles.DiskProfileDao;
 
 public class GetDiskProfileByIdQuery extends QueriesCommandBase<IdQueryParameters> {
+    @Inject
+    private DiskProfileDao diskProfileDao;
 
     public GetDiskProfileByIdQuery(IdQueryParameters parameters) {
         super(parameters);
@@ -11,7 +16,7 @@ public class GetDiskProfileByIdQuery extends QueriesCommandBase<IdQueryParameter
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getDiskProfileDao().get(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(diskProfileDao.get(getParameters().getId()));
     }
 
 }
