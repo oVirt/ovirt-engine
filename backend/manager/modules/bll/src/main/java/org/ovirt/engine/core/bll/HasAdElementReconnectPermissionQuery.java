ac34a0f17387eb4c5dbdb7e4516ce588a2b7fcc6
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject HasAdElementReconnectPermissionQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I5351a4e0df9fd56988eb95cf550318b4f5d96d8c
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/HasAdElementReconnectPermissionQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/HasAdElementReconnectPermissionQuery.java
@@ -1,12 +1,18 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.common.businessentities.ActionGroup;
 import org.ovirt.engine.core.common.queries.HasAdElementReconnectPermissionParameters;
 import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.PermissionDao;
 
 public class HasAdElementReconnectPermissionQuery<P extends HasAdElementReconnectPermissionParameters>
     extends QueriesCommandBase<P> {
+
+    @Inject
+    private PermissionDao permissionDao;
 
     public HasAdElementReconnectPermissionQuery(P parameters) {
         super(parameters);
@@ -14,7 +20,7 @@ public class HasAdElementReconnectPermissionQuery<P extends HasAdElementReconnec
 
     @Override
     protected void executeQueryCommand() {
-        Guid perm = getDbFacade().getPermissionDao().getEntityPermissions(getParameters().getAdElementId(),
+        Guid perm = permissionDao.getEntityPermissions(getParameters().getAdElementId(),
                 ActionGroup.RECONNECT_TO_VM,
                 getParameters().getObjectId(),
                 VdcObjectType.VM);
