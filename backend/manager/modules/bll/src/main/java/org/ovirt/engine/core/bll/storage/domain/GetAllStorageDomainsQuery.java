3a38869b106fac28b2bf6362f8f72c2e567560b1
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllStorageDomainsQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I32aa4ba2a70302bbdaac9667cebbf7691ce1df64
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/domain/GetAllStorageDomainsQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/domain/GetAllStorageDomainsQuery.java
@@ -1,9 +1,14 @@
 package org.ovirt.engine.core.bll.storage.domain;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.dao.StorageDomainDao;
 
 public class GetAllStorageDomainsQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+    @Inject
+    private StorageDomainDao storageDomainDao;
 
     public GetAllStorageDomainsQuery(P parameters) {
         super(parameters);
@@ -11,7 +16,6 @@ public class GetAllStorageDomainsQuery<P extends VdcQueryParametersBase> extends
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getStorageDomainDao()
-                .getAll(getUserID(), getParameters().isFiltered()));
+        getQueryReturnValue().setReturnValue(storageDomainDao.getAll(getUserID(), getParameters().isFiltered()));
     }
 }
