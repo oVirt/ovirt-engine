96b6e620c7237ad2ba78258b766771384578f976
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVmTemplatesByBaseTemplateIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: If9e8778b9b4a7ae7108f9fa9256f0baac647f56a
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmTemplatesByBaseTemplateIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmTemplatesByBaseTemplateIdQuery.java
@@ -2,23 +2,28 @@ package org.ovirt.engine.core.bll;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.queries.GetVmTemplateParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.VmTemplateDao;
 
 /* Returns a list of all templates under required base template - including the base template */
 public class GetVmTemplatesByBaseTemplateIdQuery<P extends GetVmTemplateParameters>
         extends QueriesCommandBase<P> {
+
+    @Inject
+    private VmTemplateDao vmTemplateDao;
+
     public GetVmTemplatesByBaseTemplateIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        List<VmTemplate> templateList =
-                DbFacade.getInstance().getVmTemplateDao().getTemplateVersionsForBaseTemplate(getParameters().getId());
+        List<VmTemplate> templateList = vmTemplateDao.getTemplateVersionsForBaseTemplate(getParameters().getId());
         if (templateList != null) {
-            VmTemplate baseTemplate = DbFacade.getInstance().getVmTemplateDao().get(getParameters().getId(), getUserID(), getParameters().isFiltered());
+            VmTemplate baseTemplate = vmTemplateDao.get(getParameters().getId(), getUserID(), getParameters().isFiltered());
             if (baseTemplate != null) {
                 templateList.add(baseTemplate);
             }
