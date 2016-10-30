67efd95b5b69afd3d5faa5f25f804063e18eb442
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetTagsByUserGroupIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I059cb282e0003c7132755056b2b1caa0daf69c0a
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetTagsByUserGroupIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetTagsByUserGroupIdQuery.java
@@ -1,19 +1,22 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.GetTagsByUserGroupIdParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.TagDao;
 
 public class GetTagsByUserGroupIdQuery<P extends GetTagsByUserGroupIdParameters>
         extends QueriesCommandBase<P> {
+
+    @Inject
+    private TagDao tagDao;
+
     public GetTagsByUserGroupIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue()
-                .setReturnValue(DbFacade.getInstance()
-                                .getTagDao()
-                                .getAllForUserGroups(getParameters().getGroupId()));
+        getQueryReturnValue().setReturnValue(tagDao.getAllForUserGroups(getParameters().getGroupId()));
     }
 }
