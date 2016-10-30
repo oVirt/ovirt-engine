dfba5106185eeb5726fb73974625e95acab9fd44
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetNumberOfActiveVmsInClusterByClusterIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I64372bb57cdd4edadc345a0652f5b889739dcb62
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetNumberOfActiveVmsInClusterByClusterIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetNumberOfActiveVmsInClusterByClusterIdQuery.java
@@ -2,19 +2,24 @@ package org.ovirt.engine.core.bll;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VMStatus;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.VmDao;
 
 public class GetNumberOfActiveVmsInClusterByClusterIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDao vmDao;
+
     public GetNumberOfActiveVmsInClusterByClusterIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        List<VM> vms = DbFacade.getInstance().getVmDao().getAllForCluster(getParameters().getId());
+        List<VM> vms = vmDao.getAllForCluster(getParameters().getId());
 
         // Active VMs are VMs that aren't in Down status
         int activeVms = 0;
