ef28c74ba73a45e3d1e8c5b8335860cd81c32858
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVmTemplatesByStoragePoolIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I0ee69b22e697eae0fbfd2ee39b18e9cdb3a0ad36
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmTemplatesByStoragePoolIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmTemplatesByStoragePoolIdQuery.java
@@ -2,25 +2,30 @@ package org.ovirt.engine.core.bll;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.VmTemplateDao;
 
 public class GetVmTemplatesByStoragePoolIdQuery<P extends IdQueryParameters>
         extends QueriesCommandBase<P> {
+
+    @Inject
+    private VmTemplateDao vmTemplateDao;
+
     public GetVmTemplatesByStoragePoolIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        List<VmTemplate> templateList = DbFacade.getInstance().getVmTemplateDao().getAllForStoragePool(getParameters().getId());
+        List<VmTemplate> templateList = vmTemplateDao.getAllForStoragePool(getParameters().getId());
         // Load VmInit
         for (VmTemplate template : templateList) {
             VmHandler.updateVmInitFromDB(template, true);
         }
-        VmTemplate blank = DbFacade.getInstance().getVmTemplateDao()
-                .get(VmTemplateHandler.BLANK_VM_TEMPLATE_ID);
+        VmTemplate blank = vmTemplateDao.get(VmTemplateHandler.BLANK_VM_TEMPLATE_ID);
         if (!templateList.contains(blank)) {
             VmHandler.updateVmInitFromDB(blank, true);
             templateList.add(0, blank);
