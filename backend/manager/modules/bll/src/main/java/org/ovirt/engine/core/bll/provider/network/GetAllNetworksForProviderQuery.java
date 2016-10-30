34799430cac82b700794ba50db8e64f2e9d928e2
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllNetworksForProviderQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I71c1806bef8c7861155525fd9b6eec5c0c45d4ff
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/network/GetAllNetworksForProviderQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/network/GetAllNetworksForProviderQuery.java
@@ -1,9 +1,14 @@
 package org.ovirt.engine.core.bll.provider.network;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.network.NetworkViewDao;
 
 public class GetAllNetworksForProviderQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private NetworkViewDao networkViewDao;
 
     public GetAllNetworksForProviderQuery(P parameters) {
         super(parameters);
@@ -11,6 +16,6 @@ public class GetAllNetworksForProviderQuery<P extends IdQueryParameters> extends
 
     @Override
     protected void executeQueryCommand() {
-        setReturnValue(getDbFacade().getNetworkViewDao().getAllForProvider(getParameters().getId()));
+        setReturnValue(networkViewDao.getAllForProvider(getParameters().getId()));
     }
 }
