9f38586652a4f7b7347ab19d4c8cb9a310f2ed35
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetStorageServerConnectionExtensionsByHostIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ie6fcac49841052f7ae1ab77b581b228c08052d81
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/connection/GetStorageServerConnectionExtensionsByHostIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/connection/GetStorageServerConnectionExtensionsByHostIdQuery.java
@@ -1,15 +1,21 @@
 package org.ovirt.engine.core.bll.storage.connection;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.StorageServerConnectionExtensionDao;
 
 public class GetStorageServerConnectionExtensionsByHostIdQuery extends QueriesCommandBase<IdQueryParameters> {
+    @Inject
+    private StorageServerConnectionExtensionDao storageServerConnectionExtensionDao;
+
     public GetStorageServerConnectionExtensionsByHostIdQuery(IdQueryParameters parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getStorageServerConnectionExtensionDao().getByHostId(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(storageServerConnectionExtensionDao.getByHostId(getParameters().getId()));
     }
 }
