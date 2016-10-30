6f71de3b817092a62dbb9522ecaea5f6229b3a8b
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetStorageServerConnectionExtensionByIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Iee586c3255139dcb32ad78dc2f66ef4be3f484e6
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/connection/GetStorageServerConnectionExtensionByIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/connection/GetStorageServerConnectionExtensionByIdQuery.java
@@ -1,10 +1,16 @@
 package org.ovirt.engine.core.bll.storage.connection;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.bll.context.EngineContext;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.StorageServerConnectionExtensionDao;
 
 public class GetStorageServerConnectionExtensionByIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<IdQueryParameters> {
+    @Inject
+    private StorageServerConnectionExtensionDao storageServerConnectionExtensionDao;
+
     public GetStorageServerConnectionExtensionByIdQuery(P parameters) {
         super(parameters);
     }
@@ -15,6 +21,6 @@ public class GetStorageServerConnectionExtensionByIdQuery<P extends IdQueryParam
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getStorageServerConnectionExtensionDao().get(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(storageServerConnectionExtensionDao.get(getParameters().getId()));
     }
 }
