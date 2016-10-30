edb19fb9bb716ebf79549dc861b55d2d2d2a1c74
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllChildVlanInterfacesQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I12a2c4bc82161196671fbcf3dafd0207ed07f213
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetAllChildVlanInterfacesQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetAllChildVlanInterfacesQuery.java
@@ -3,11 +3,14 @@ package org.ovirt.engine.core.bll.network.host;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.bll.context.EngineContext;
 import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
 import org.ovirt.engine.core.common.queries.InterfaceAndIdQueryParameters;
 import org.ovirt.engine.core.common.utils.NetworkCommonUtils;
+import org.ovirt.engine.core.dao.network.InterfaceDao;
 import org.ovirt.engine.core.utils.NetworkUtils;
 
 /**
@@ -15,6 +18,9 @@ import org.ovirt.engine.core.utils.NetworkUtils;
  * return: eth2.4 eth2.5
  */
 public class GetAllChildVlanInterfacesQuery<P extends InterfaceAndIdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private InterfaceDao interfaceDao;
+
     public GetAllChildVlanInterfacesQuery(P parameters) {
         super(parameters);
     }
@@ -27,8 +33,7 @@ public class GetAllChildVlanInterfacesQuery<P extends InterfaceAndIdQueryParamet
     protected void executeQueryCommand() {
         ArrayList<VdsNetworkInterface> retVal = new ArrayList<>();
         if (!NetworkCommonUtils.isVlan(getParameters().getInterface())) {
-            List<VdsNetworkInterface> vdsInterfaces =
-                    getDbFacade().getInterfaceDao().getAllInterfacesForVds(getParameters().getId());
+            List<VdsNetworkInterface> vdsInterfaces = interfaceDao.getAllInterfacesForVds(getParameters().getId());
             for (int i = 0; i < vdsInterfaces.size(); i++) {
                 if (NetworkCommonUtils.isVlan(vdsInterfaces.get(i))) {
                     if (NetworkUtils.interfaceBasedOn(vdsInterfaces.get(i),
