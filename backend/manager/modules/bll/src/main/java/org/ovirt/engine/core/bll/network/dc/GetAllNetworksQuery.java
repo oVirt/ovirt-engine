2ae6e2dea636835694d7bc00619cedf461b5c879
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllNetworksQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I2ba4875765e67fb39726023fdee93b2d786a2875
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/dc/GetAllNetworksQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/dc/GetAllNetworksQuery.java
@@ -1,10 +1,16 @@
 package org.ovirt.engine.core.bll.network.dc;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.network.NetworkDao;
 
 public class GetAllNetworksQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private NetworkDao networkDao;
+
     public GetAllNetworksQuery(P parameters) {
         super(parameters);
     }
@@ -13,12 +19,10 @@ public class GetAllNetworksQuery<P extends IdQueryParameters> extends QueriesCom
     protected void executeQueryCommand() {
         if (getParameters().getId() == null
                 || getParameters().getId().equals(Guid.Empty)) {
-            getQueryReturnValue().setReturnValue(getDbFacade().getNetworkDao().getAll(getUserID(), getParameters().isFiltered()));
+            getQueryReturnValue().setReturnValue(networkDao.getAll(getUserID(), getParameters().isFiltered()));
         } else {
             getQueryReturnValue().setReturnValue(
-                    getDbFacade().getNetworkDao().getAllForDataCenter(getParameters().getId(),
-                            getUserID(),
-                            getParameters().isFiltered()));
+                    networkDao.getAllForDataCenter(getParameters().getId(), getUserID(), getParameters().isFiltered()));
         }
     }
 }
