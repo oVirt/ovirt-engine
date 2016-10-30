b6cd9733be76409cddec15a2ac96aa15871435f1
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAvailableStoragePoolVersionsQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I00682417d22f387d57e63eb4dce546587cb86261
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/pool/GetAvailableStoragePoolVersionsQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/pool/GetAvailableStoragePoolVersionsQuery.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.Cluster;
 import org.ovirt.engine.core.common.businessentities.StoragePool;
@@ -11,10 +13,17 @@ import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Version;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.ClusterDao;
+import org.ovirt.engine.core.dao.StoragePoolDao;
 
 public class GetAvailableStoragePoolVersionsQuery<P extends IdQueryParameters>
         extends QueriesCommandBase<P> {
+    @Inject
+    private StoragePoolDao storagePoolDao;
+
+    @Inject
+    private ClusterDao clusterDao;
+
     public GetAvailableStoragePoolVersionsQuery(P parameters) {
         super(parameters);
     }
@@ -23,11 +32,10 @@ public class GetAvailableStoragePoolVersionsQuery<P extends IdQueryParameters>
     protected void executeQueryCommand() {
         if (getParameters().getId() != null) {
             ArrayList<Version> result = new ArrayList<>();
-            StoragePool storagePool = DbFacade.getInstance().getStoragePoolDao().get(
-                    getParameters().getId());
+            StoragePool storagePool = storagePoolDao.get(getParameters().getId());
             if (storagePool != null) {
-                List<Cluster> clusters = DbFacade.getInstance().getClusterDao().getAllForStoragePool(
-                        storagePool.getId(), getUserID(), getParameters().isFiltered());
+                List<Cluster> clusters =
+                        clusterDao.getAllForStoragePool(storagePool.getId(), getUserID(), getParameters().isFiltered());
 
                 for (Version supportedVer : Config
                         .<HashSet<Version>> getValue(ConfigValues.SupportedClusterLevels)) {
