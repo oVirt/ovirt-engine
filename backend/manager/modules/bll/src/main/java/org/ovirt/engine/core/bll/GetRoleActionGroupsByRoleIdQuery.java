5dd1b66786c76427471bc6e6c06ce72d6cf11503
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetRoleActionGroupsByRoleIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ibd8e5f041240a19f1be2450cf9085c85c82e0618
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetRoleActionGroupsByRoleIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetRoleActionGroupsByRoleIdQuery.java
@@ -1,17 +1,22 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.ActionGroupDao;
 
 public class GetRoleActionGroupsByRoleIdQuery<P extends IdQueryParameters>
         extends QueriesCommandBase<P> {
+
+    @Inject
+    private ActionGroupDao actionGroupDao;
+
     public GetRoleActionGroupsByRoleIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(
-                getDbFacade().getActionGroupDao()
-                        .getAllForRole(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(actionGroupDao.getAllForRole(getParameters().getId()));
     }
 }
--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetRoleActionGroupsByRoleIdQueryTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetRoleActionGroupsByRoleIdQueryTest.java
@@ -1,13 +1,13 @@
 package org.ovirt.engine.core.bll;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.Mock;
 import org.ovirt.engine.core.common.businessentities.ActionGroup;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Guid;
@@ -15,6 +15,8 @@ import org.ovirt.engine.core.dao.ActionGroupDao;
 import org.ovirt.engine.core.utils.RandomUtils;
 
 public class GetRoleActionGroupsByRoleIdQueryTest extends AbstractUserQueryTest<IdQueryParameters, GetRoleActionGroupsByRoleIdQuery<IdQueryParameters>> {
+    @Mock
+    private ActionGroupDao actionGroupDaoMock;
 
     @Test
     public void testExecuteQueryCommand() {
@@ -27,9 +29,7 @@ public class GetRoleActionGroupsByRoleIdQueryTest extends AbstractUserQueryTest<
         List<ActionGroup> expected = Collections.singletonList(group);
 
         // Mock the Dao
-        ActionGroupDao actionGroupDaoMock = mock(ActionGroupDao.class);
         when(actionGroupDaoMock.getAllForRole(roleId)).thenReturn(expected);
-        when(getDbFacadeMockInstance().getActionGroupDao()).thenReturn(actionGroupDaoMock);
 
         // Execute the query and assert the result
         getQuery().executeQueryCommand();
