c0e5c7a9a82590efcdbfc43bf2412b9dadd100a7
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAuditLogByIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I9cbbc15bd4a33099b96d11280a433692001c4370
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAuditLogByIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAuditLogByIdQuery.java
@@ -1,8 +1,13 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.GetAuditLogByIdParameters;
+import org.ovirt.engine.core.dao.AuditLogDao;
 
 public class GetAuditLogByIdQuery <P extends GetAuditLogByIdParameters> extends QueriesCommandBase<P>{
+    @Inject
+    private AuditLogDao auditLogDao;
 
     public GetAuditLogByIdQuery(P parameters) {
         super(parameters);
@@ -11,8 +16,6 @@ public class GetAuditLogByIdQuery <P extends GetAuditLogByIdParameters> extends 
     /** Actually executes the query, and stores the result in {@link #getQueryReturnValue()} */
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(
-                getDbFacade().getAuditLogDao()
-                        .get(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(auditLogDao.get(getParameters().getId()));
     }
 }
