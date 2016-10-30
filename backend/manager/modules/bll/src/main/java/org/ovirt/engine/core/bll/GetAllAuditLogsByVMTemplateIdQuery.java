03943c0e47b206fa9a1aa8dbb4371c38c46da097
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllAuditLogsByVMTemplateIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Iaea07c1aa029844c08d87429d540fe0ca151b49b
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllAuditLogsByVMTemplateIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllAuditLogsByVMTemplateIdQuery.java
@@ -1,9 +1,14 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.AuditLogDao;
 
 /** A query to return all the Audit Logs according to a given VM Template ID */
 public class GetAllAuditLogsByVMTemplateIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private AuditLogDao auditLogDao;
 
     public GetAllAuditLogsByVMTemplateIdQuery(P parameters) {
         super(parameters);
@@ -13,9 +18,6 @@ public class GetAllAuditLogsByVMTemplateIdQuery<P extends IdQueryParameters> ext
     @Override
     protected void executeQueryCommand() {
         getQueryReturnValue().setReturnValue(
-                getDbFacade().getAuditLogDao().getAllByVMTemplateId(
-                        getParameters().getId(),
-                        getUserID(),
-                        getParameters().isFiltered()));
+                auditLogDao.getAllByVMTemplateId(getParameters().getId(), getUserID(), getParameters().isFiltered()));
     }
 }
--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetAllAuditLogsByVMTemplateIdQueryTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/GetAllAuditLogsByVMTemplateIdQueryTest.java
@@ -1,13 +1,13 @@
 package org.ovirt.engine.core.bll;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.Mock;
 import org.ovirt.engine.core.common.businessentities.AuditLog;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Guid;
@@ -16,6 +16,9 @@ import org.ovirt.engine.core.dao.AuditLogDao;
 /** A test case for the {@link GetAllAuditLogsByVMTemplateIdQuery} class. */
 public class GetAllAuditLogsByVMTemplateIdQueryTest
         extends AbstractUserQueryTest<IdQueryParameters, GetAllAuditLogsByVMTemplateIdQuery<? extends IdQueryParameters>> {
+
+    @Mock
+    private AuditLogDao auditLogDaoMock;
 
     @Test
     public void testExecuteQueryCommand() {
@@ -28,11 +31,9 @@ public class GetAllAuditLogsByVMTemplateIdQueryTest
         expectedResult.setVmTemplateId(vmTemplateId);
 
         // Mock the Daos
-        AuditLogDao auditLogDaoMock = mock(AuditLogDao.class);
         when(auditLogDaoMock.getAllByVMTemplateId(vmTemplateId,
                 getUser().getId(),
                 getQueryParameters().isFiltered())).thenReturn(Collections.singletonList(expectedResult));
-        when(getDbFacadeMockInstance().getAuditLogDao()).thenReturn(auditLogDaoMock);
 
         getQuery().executeQueryCommand();
 
