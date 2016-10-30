5ccccb1ba2e61051959e4197573e48bc4818405b
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllVnicProfilesQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ifec2427e35451ee00bc8f3011fd92cbb25e00665
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/GetAllVnicProfilesQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/GetAllVnicProfilesQuery.java
@@ -1,16 +1,21 @@
 package org.ovirt.engine.core.bll.network.vm;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.dao.network.VnicProfileViewDao;
 
 public class GetAllVnicProfilesQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+    @Inject
+    private VnicProfileViewDao vnicProfileViewDao;
+
     public GetAllVnicProfilesQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getVnicProfileViewDao().getAll(getUserID(),
-                getParameters().isFiltered()));
+        getQueryReturnValue().setReturnValue(vnicProfileViewDao.getAll(getUserID(), getParameters().isFiltered()));
     }
 }
