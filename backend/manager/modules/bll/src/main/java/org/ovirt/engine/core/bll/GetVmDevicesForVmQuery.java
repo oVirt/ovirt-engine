cc0050e76562ce66dcad330b178dac5f5a29f347
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVmDevicesForVmQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I732c8cff8229e3e7ba80640c51492e2eabbdde81
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmDevicesForVmQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmDevicesForVmQuery.java
@@ -1,17 +1,21 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.VmDeviceDao;
 
 public class GetVmDevicesForVmQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDeviceDao vmDeviceDao;
+
     public GetVmDevicesForVmQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        setReturnValue(getDbFacade().getVmDeviceDao().getVmDeviceByVmId(
-                getParameters().getId(),
-                getUserID(),
-                getParameters().isFiltered()));
+        setReturnValue(
+                vmDeviceDao.getVmDeviceByVmId(getParameters().getId(), getUserID(), getParameters().isFiltered()));
     }
 }
