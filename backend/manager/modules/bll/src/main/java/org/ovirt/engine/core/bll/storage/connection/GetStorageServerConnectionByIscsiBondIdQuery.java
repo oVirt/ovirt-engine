8ba21fffd5f4b247aa112b6d5d37acfb0627353c
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetStorageServerConnectionByIscsiBondIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I707f3bcf0bc3aea21cddf8c85b4287faf2b333a4
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/connection/GetStorageServerConnectionByIscsiBondIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/connection/GetStorageServerConnectionByIscsiBondIdQuery.java
@@ -3,11 +3,20 @@ package org.ovirt.engine.core.bll.storage.connection;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.StorageServerConnections;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.IscsiBondDao;
+import org.ovirt.engine.core.dao.StorageServerConnectionDao;
 
 public class GetStorageServerConnectionByIscsiBondIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private StorageServerConnectionDao storageServerConnectionDao;
+
+    @Inject
+    private IscsiBondDao iscsiBondDao;
 
     public GetStorageServerConnectionByIscsiBondIdQuery(P parameters) {
         super(parameters);
@@ -18,13 +27,13 @@ public class GetStorageServerConnectionByIscsiBondIdQuery<P extends IdQueryParam
         List<StorageServerConnections> conns = new ArrayList<>();
 
         for (String id : getConnectionsIds()) {
-            conns.add(getDbFacade().getStorageServerConnectionDao().get(id));
+            conns.add(storageServerConnectionDao.get(id));
         }
 
         getQueryReturnValue().setReturnValue(conns);
     }
 
     private List<String> getConnectionsIds() {
-        return getDbFacade().getIscsiBondDao().getStorageConnectionIdsByIscsiBondId(getParameters().getId());
+        return iscsiBondDao.getStorageConnectionIdsByIscsiBondId(getParameters().getId());
     }
 }
