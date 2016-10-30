052a236129769d309faa59e272cb89ba66dac288
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetQuotasConsumptionForCurrentUserQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I062a96c66c9d35dbf89bcc6f7e44107f4082574d
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetQuotasConsumptionForCurrentUserQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetQuotasConsumptionForCurrentUserQuery.java
@@ -7,11 +7,15 @@ import javax.inject.Inject;
 import org.ovirt.engine.core.bll.quota.QuotaManager;
 import org.ovirt.engine.core.common.businessentities.Quota;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.dao.QuotaDao;
 
 public class GetQuotasConsumptionForCurrentUserQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
 
     @Inject
     private QuotaManager quotaManager;
+
+    @Inject
+    private QuotaDao quotaDao;
 
     public GetQuotasConsumptionForCurrentUserQuery(P parameters) {
         super(parameters);
@@ -19,7 +23,7 @@ public class GetQuotasConsumptionForCurrentUserQuery<P extends VdcQueryParameter
 
     @Override
     protected void executeQueryCommand() {
-        List<Quota> quotaList = getDbFacade().getQuotaDao().getQuotaByAdElementId(getUser().getId(), null, true);
+        List<Quota> quotaList = quotaDao.getQuotaByAdElementId(getUser().getId(), null, true);
         getQueryReturnValue().setReturnValue(quotaManager.generatePerUserUsageReport(quotaList));
     }
 }
