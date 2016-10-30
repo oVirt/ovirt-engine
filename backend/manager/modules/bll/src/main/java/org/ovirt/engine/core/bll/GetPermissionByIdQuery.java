ca9d893a4d79c4c47b7de08c967b9e3410bed38b
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetPermissionByIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I28f4278436c97f3ab0912210ac18da64b6c84c3d
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetPermissionByIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetPermissionByIdQuery.java
@@ -1,17 +1,22 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.PermissionDao;
 
 public class GetPermissionByIdQuery<P extends IdQueryParameters>
         extends QueriesCommandBase<P> {
+
+    @Inject
+    private PermissionDao permissionDao;
+
     public GetPermissionByIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue()
-                .setReturnValue(DbFacade.getInstance().getPermissionDao().get(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(permissionDao.get(getParameters().getId()));
     }
 }
