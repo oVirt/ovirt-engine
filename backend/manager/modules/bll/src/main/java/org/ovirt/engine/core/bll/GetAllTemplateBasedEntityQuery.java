4e93eda1bd36f25363016ab7346af88a32dc5da7
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllTemplateBasedEntityQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Idd8b8d7884a9b0683fdb22c790ca9004316637cd
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllTemplateBasedEntityQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllTemplateBasedEntityQuery.java
@@ -2,11 +2,16 @@ package org.ovirt.engine.core.bll;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.VmEntityType;
 import org.ovirt.engine.core.common.businessentities.VmTemplate;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
+import org.ovirt.engine.core.dao.VmTemplateDao;
 
 public abstract class GetAllTemplateBasedEntityQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+    @Inject
+    private VmTemplateDao vmTemplateDao;
 
     private final VmEntityType entityType;
 
@@ -18,8 +23,7 @@ public abstract class GetAllTemplateBasedEntityQuery<P extends VdcQueryParameter
 
     @Override
     protected void executeQueryCommand() {
-        List<VmTemplate> retval =
-                getDbFacade().getVmTemplateDao().getAll(getUserID(), getParameters().isFiltered(), entityType);
+        List<VmTemplate> retval = vmTemplateDao.getAll(getUserID(), getParameters().isFiltered(), entityType);
         for (VmTemplate template : retval) {
             VmTemplateHandler.updateDisksFromDb(template);
         }
