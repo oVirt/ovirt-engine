12e07c6e5f5480a927a2f1e3da120a5768aef3f2
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllAttachableDisksForVmQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I293b349a40a60a5f6fd6bcdb51318def130d3317
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/GetAllAttachableDisksForVmQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/GetAllAttachableDisksForVmQuery.java
@@ -1,9 +1,14 @@
 package org.ovirt.engine.core.bll.storage.disk;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.GetAllAttachableDisksForVmQueryParameters;
+import org.ovirt.engine.core.dao.DiskDao;
 
 public class GetAllAttachableDisksForVmQuery<P extends GetAllAttachableDisksForVmQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private DiskDao diskDao;
 
     public GetAllAttachableDisksForVmQuery(P parameters) {
         super(parameters);
@@ -11,9 +16,7 @@ public class GetAllAttachableDisksForVmQuery<P extends GetAllAttachableDisksForV
 
     @Override
     protected void executeQueryCommand() {
-        setReturnValue(getDbFacade()
-                .getDiskDao()
-                .getAllAttachableDisksByPoolId(getParameters().getStoragePoolId(),
+        setReturnValue(diskDao.getAllAttachableDisksByPoolId(getParameters().getStoragePoolId(),
                         getParameters().getVmId(),
                         getUserID(),
                         getParameters().isFiltered()));
