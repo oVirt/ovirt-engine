bc1c4eddf9a6c9af583d5ae3f726f42d1a4dafec
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVmInterfacesByVmIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I415c96f818aca2b5168232849a75c1eb8144d965
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/GetVmInterfacesByVmIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/GetVmInterfacesByVmIdQuery.java
@@ -1,9 +1,15 @@
 package org.ovirt.engine.core.bll.network.vm;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.network.VmNetworkInterfaceDao;
 
 public class GetVmInterfacesByVmIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmNetworkInterfaceDao vmNetworkInterfaceDao;
+
     public GetVmInterfacesByVmIdQuery(P parameters) {
         super(parameters);
     }
@@ -11,7 +17,6 @@ public class GetVmInterfacesByVmIdQuery<P extends IdQueryParameters> extends Que
     @Override
     protected void executeQueryCommand() {
         getQueryReturnValue().setReturnValue(
-                getDbFacade().getVmNetworkInterfaceDao()
-                        .getAllForVm(getParameters().getId(), getUserID(), getParameters().isFiltered()));
+                vmNetworkInterfaceDao.getAllForVm(getParameters().getId(), getUserID(), getParameters().isFiltered()));
     }
 }
--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/vm/GetVmInterfacesByVmIdQueryTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/vm/GetVmInterfacesByVmIdQueryTest.java
@@ -1,27 +1,23 @@
 package org.ovirt.engine.core.bll.network.vm;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.mockito.Mock;
 import org.ovirt.engine.core.bll.AbstractUserQueryTest;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 import org.ovirt.engine.core.dao.network.VmNetworkInterfaceDao;
 
 /** A test case for {@link GetVmInterfacesByVmIdQuery} */
 public class GetVmInterfacesByVmIdQueryTest extends AbstractUserQueryTest<IdQueryParameters, GetVmInterfacesByVmIdQuery<IdQueryParameters>> {
+    @Mock
+    private VmNetworkInterfaceDao daoMock;
 
     /** A test that checked that all the parameters are passed properly to the Dao */
     @Test
     public void testExectueQuery() {
-        DbFacade dbFacadeMock = getDbFacadeMockInstance();
-
-        VmNetworkInterfaceDao daoMock = mock(VmNetworkInterfaceDao.class);
-        when(dbFacadeMock.getVmNetworkInterfaceDao()).thenReturn(daoMock);
-
         Guid guid = Guid.newGuid();
 
         IdQueryParameters params = getQueryParameters();
