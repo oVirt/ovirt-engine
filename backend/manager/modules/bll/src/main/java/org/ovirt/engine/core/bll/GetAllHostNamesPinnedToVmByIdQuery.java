9e77387dd7fb21413fce07c2d0f95826202b3554
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllHostNamesPinnedToVmByIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: If5d0fdf0470e2a2805b55c54c9239840fc524eb6
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllHostNamesPinnedToVmByIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllHostNamesPinnedToVmByIdQuery.java
@@ -1,8 +1,13 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.VdsStaticDao;
 
 public class GetAllHostNamesPinnedToVmByIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VdsStaticDao vdsStaticDao;
 
     public GetAllHostNamesPinnedToVmByIdQuery(P parameters) {
         super(parameters);
@@ -10,8 +15,7 @@ public class GetAllHostNamesPinnedToVmByIdQuery<P extends IdQueryParameters> ext
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getVdsStaticDao()
-                .getAllHostNamesPinnedToVm(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(vdsStaticDao.getAllHostNamesPinnedToVm(getParameters().getId()));
     }
 
 }
