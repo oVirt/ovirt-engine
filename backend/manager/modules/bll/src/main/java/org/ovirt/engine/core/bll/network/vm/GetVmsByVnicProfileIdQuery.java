e134431a8758067c252cb43fba15d7a774469116
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVmsByVnicProfileIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ie863c1ef0502291fd15ef9a7e3da42caeac93bbd
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/GetVmsByVnicProfileIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/vm/GetVmsByVnicProfileIdQuery.java
@@ -1,15 +1,21 @@
 package org.ovirt.engine.core.bll.network.vm;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.VmDao;
 
 public class GetVmsByVnicProfileIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDao vmDao;
+
     public GetVmsByVnicProfileIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getVmDao().getAllForVnicProfile(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(vmDao.getAllForVnicProfile(getParameters().getId()));
     }
 }
