cddf5e34c46ae1bd207c370f9853f016416ba645
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAvailableClusterVersionsByStoragePoolQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I40dc62cfd80392674421b92a6b37685e1cbdbc63
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAvailableClusterVersionsByStoragePoolQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAvailableClusterVersionsByStoragePoolQuery.java
@@ -3,15 +3,20 @@ package org.ovirt.engine.core.bll;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.StoragePool;
 import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Version;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.StoragePoolDao;
 
 public class GetAvailableClusterVersionsByStoragePoolQuery<P extends IdQueryParameters>
         extends QueriesCommandBase<P> {
+    @Inject
+    private StoragePoolDao storagePoolDao;
+
     public GetAvailableClusterVersionsByStoragePoolQuery(P parameters) {
         super(parameters);
     }
@@ -20,8 +25,7 @@ public class GetAvailableClusterVersionsByStoragePoolQuery<P extends IdQueryPara
     protected void executeQueryCommand() {
         if (getParameters().getId() != null) {
             ArrayList<Version> result = new ArrayList<>();
-            StoragePool storagePool = DbFacade.getInstance().getStoragePoolDao().get(
-                    getParameters().getId());
+            StoragePool storagePool = storagePoolDao.get(getParameters().getId());
             if (storagePool != null) {
                 // return all versions that >= to the storage pool version
                 for (Version supportedVer : Config
