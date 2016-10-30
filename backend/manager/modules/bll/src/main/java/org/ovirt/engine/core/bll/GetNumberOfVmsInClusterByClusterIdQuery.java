89b091ab8b005d8d346c638269d35fefcffe2c58
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetNumberOfVmsInClusterByClusterIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I596c3e4d9a9741f2215c068cf34ab252a41139a2
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetNumberOfVmsInClusterByClusterIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetNumberOfVmsInClusterByClusterIdQuery.java
@@ -1,15 +1,21 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.ClusterDao;
 
 public class GetNumberOfVmsInClusterByClusterIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private ClusterDao clusterDao;
+
     public GetNumberOfVmsInClusterByClusterIdQuery(P parameters) {
             super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        int numOfVms = getDbFacade().getClusterDao().getVmsCountByClusterId(getParameters().getId());
+        int numOfVms = clusterDao.getVmsCountByClusterId(getParameters().getId());
         getQueryReturnValue().setReturnValue(numOfVms);
     }
 }
