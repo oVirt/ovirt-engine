9de9f51d4f187e46841ca07c652ee38b76647eb2
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetNetworkLabelsByDataCenterIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Id829e624cf5226871e9b588851a9166ec8de2414
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/dc/GetNetworkLabelsByDataCenterIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/dc/GetNetworkLabelsByDataCenterIdQuery.java
@@ -2,18 +2,28 @@ package org.ovirt.engine.core.bll.network.dc;
 
 import java.util.Set;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.network.InterfaceDao;
+import org.ovirt.engine.core.dao.network.NetworkDao;
 
 public class GetNetworkLabelsByDataCenterIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private NetworkDao networkDao;
+
+    @Inject
+    private InterfaceDao interfaceDao;
+
     public GetNetworkLabelsByDataCenterIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        Set<String> labels = getDbFacade().getNetworkDao().getAllNetworkLabelsForDataCenter(getParameters().getId());
-        labels.addAll(getDbFacade().getInterfaceDao().getAllNetworkLabelsForDataCenter(getParameters().getId()));
+        Set<String> labels = networkDao.getAllNetworkLabelsForDataCenter(getParameters().getId());
+        labels.addAll(interfaceDao.getAllNetworkLabelsForDataCenter(getParameters().getId()));
         getQueryReturnValue().setReturnValue(labels);
     }
 }
