2d1317e9804eae2d4d13ba2d4feea418e211da22
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetGraphicsDevicesMultipleQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Id63703bead12eb2760add5f3976e7f655de2f9ae
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetGraphicsDevicesMultipleQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetGraphicsDevicesMultipleQuery.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.context.EngineContext;
 import org.ovirt.engine.core.common.businessentities.GraphicsDevice;
 import org.ovirt.engine.core.common.businessentities.VmDevice;
@@ -12,8 +14,11 @@ import org.ovirt.engine.core.common.businessentities.VmDeviceGeneralType;
 import org.ovirt.engine.core.common.queries.IdsQueryParameters;
 import org.ovirt.engine.core.common.utils.VmDeviceType;
 import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.VmDeviceDao;
 
 public class GetGraphicsDevicesMultipleQuery<P extends IdsQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private VmDeviceDao vmDeviceDao;
 
     public GetGraphicsDevicesMultipleQuery(P parameters) {
         super(parameters);
@@ -35,7 +40,7 @@ public class GetGraphicsDevicesMultipleQuery<P extends IdsQueryParameters> exten
 
     private void mapDevices(Map<Guid, List<GraphicsDevice>> result, VmDeviceType type, List<Guid> vmsIds) {
         log.debug("Retrieving graphics devices '{}' for '{}' vms", type.getName(), vmsIds.size());
-        List<VmDevice> devicesList = getDbFacade().getVmDeviceDao().getVmDeviceByTypeAndDevice(vmsIds,
+        List<VmDevice> devicesList = vmDeviceDao.getVmDeviceByTypeAndDevice(vmsIds,
                 VmDeviceGeneralType.GRAPHICS,
                 type.getName(),
                 getUserID(),
