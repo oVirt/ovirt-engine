0a9f0b306f659e69d36f9ef522112e49bd296b5d
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllNetworkQosByStoragePoolIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I04a748e749d66495df3468703909cba0092e35f1
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllNetworkQosByStoragePoolIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetAllNetworkQosByStoragePoolIdQuery.java
@@ -1,16 +1,22 @@
 package org.ovirt.engine.core.bll;
 
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.network.NetworkQoSDao;
 
 
 public class GetAllNetworkQosByStoragePoolIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private NetworkQoSDao networkQoSDao;
+
     public GetAllNetworkQosByStoragePoolIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getDbFacade().getNetworkQosDao().getAllForStoragePoolId(getParameters().getId()));
+        getQueryReturnValue().setReturnValue(networkQoSDao.getAllForStoragePoolId(getParameters().getId()));
     }
 }
