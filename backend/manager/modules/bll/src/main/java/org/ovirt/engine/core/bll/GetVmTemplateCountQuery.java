10890144efb331360ed27c8d79ac1b6786d90bda
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVmTemplateCountQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I5341e998885f2a551a4bb331d3c55bca91aa9843
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmTemplateCountQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetVmTemplateCountQuery.java
@@ -1,19 +1,20 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 import org.ovirt.engine.core.dao.VmTemplateDao;
 
 public class GetVmTemplateCountQuery<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
+    @Inject
+    private VmTemplateDao vmTemplateDao;
+
     public GetVmTemplateCountQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getVmTemplateDao().getCount());
-    }
-
-    private VmTemplateDao getVmTemplateDao() {
-        return getDbFacade().getVmTemplateDao();
+        getQueryReturnValue().setReturnValue(vmTemplateDao.getCount());
     }
 }
