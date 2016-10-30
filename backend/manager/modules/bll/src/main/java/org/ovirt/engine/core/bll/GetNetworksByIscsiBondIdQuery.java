d07a1c3c07237d49400418cec1ffebb288b6dd03
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetNetworksByIscsiBondIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I9cf393ef7d4b8086284781c9fa214cf3ebb0b831
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetNetworksByIscsiBondIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/GetNetworksByIscsiBondIdQuery.java
@@ -3,11 +3,20 @@ package org.ovirt.engine.core.bll;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.businessentities.network.Network;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.IscsiBondDao;
+import org.ovirt.engine.core.dao.network.NetworkDao;
 
 public class GetNetworksByIscsiBondIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private NetworkDao networkDao;
+
+    @Inject
+    private IscsiBondDao iscsiBondDao;
 
     public GetNetworksByIscsiBondIdQuery(P parameters) {
         super(parameters);
@@ -18,13 +27,13 @@ public class GetNetworksByIscsiBondIdQuery<P extends IdQueryParameters> extends 
         List<Network> networks = new ArrayList<>();
 
         for (Guid id : getNetworksIds()) {
-            networks.add(getDbFacade().getNetworkDao().get(id));
+            networks.add(networkDao.get(id));
         }
 
         getQueryReturnValue().setReturnValue(networks);
     }
 
     private List<Guid> getNetworksIds() {
-        return getDbFacade().getIscsiBondDao().getNetworkIdsByIscsiBondId(getParameters().getId());
+        return iscsiBondDao.getNetworkIdsByIscsiBondId(getParameters().getId());
     }
 }
