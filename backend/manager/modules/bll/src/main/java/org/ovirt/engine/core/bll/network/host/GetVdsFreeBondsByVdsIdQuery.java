1cb911ae8ef977fbefba0527e227c678ecd2b29a
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetVdsFreeBondsByVdsIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: I3ce5a5538b567a761a26b31d60a8c72dd1ffc188
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetVdsFreeBondsByVdsIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/host/GetVdsFreeBondsByVdsIdQuery.java
@@ -3,19 +3,24 @@ package org.ovirt.engine.core.bll.network.host;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.network.VdsNetworkInterface;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.network.InterfaceDao;
 
 public class GetVdsFreeBondsByVdsIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private InterfaceDao interfaceDao;
+
     public GetVdsFreeBondsByVdsIdQuery(P parameters) {
         super(parameters);
     }
 
     @Override
     protected void executeQueryCommand() {
-        final List<VdsNetworkInterface> list =
-                getDbFacade().getInterfaceDao().getAllInterfacesForVds(getParameters().getId());
+        final List<VdsNetworkInterface> list = interfaceDao.getAllInterfacesForVds(getParameters().getId());
 
         // we return only bonds that are not active (that have no interfaces
         // related to them)
