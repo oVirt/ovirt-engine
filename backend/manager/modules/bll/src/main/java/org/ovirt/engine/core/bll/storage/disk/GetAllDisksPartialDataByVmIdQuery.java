d5b8c13aa68d47e7e4e3db44093ad923c8e60045
Allon Mureinik <amureini@redhat.com>
2016-10-31 00:12:08 +0200

    core: @Inject GetAllDisksPartialDataByVmIdQuery Daos
    @Inject the DAOs instead of explicitly getting them from DbFacade.
    
    Change-Id: Ife3a1ba1d267858f786772348cf80b12f58a9d43
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/GetAllDisksPartialDataByVmIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/GetAllDisksPartialDataByVmIdQuery.java
@@ -2,11 +2,17 @@ package org.ovirt.engine.core.bll.storage.disk;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.businessentities.storage.Disk;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.dao.DiskDao;
 
 public class GetAllDisksPartialDataByVmIdQuery<P extends IdQueryParameters> extends QueriesCommandBase<P> {
+    @Inject
+    private DiskDao diskDao;
+
     public GetAllDisksPartialDataByVmIdQuery(P parameters) {
         super(parameters);
     }
@@ -14,7 +20,7 @@ public class GetAllDisksPartialDataByVmIdQuery<P extends IdQueryParameters> exte
     @Override
     protected void executeQueryCommand() {
         List<Disk> allDisks =
-                getDbFacade().getDiskDao().getAllForVmPartialData
+                diskDao.getAllForVmPartialData
                         (getParameters().getId(), false, getUserID(), getParameters().isFiltered());
         getQueryReturnValue().setReturnValue(allDisks);
     }
