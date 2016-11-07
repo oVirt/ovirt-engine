oVirt/ovirt-engine

commit f8da95ffeefa1f73432b5193e47dc521b0c70aca

aaa: desktopLogin sends empty password, impacts desktop SSO feature
The session password is not saved
on engine and is only saved on SSO
end. Engine should make a call to
retrieve the encryped password from
SSO and use its private key to decrypt
the password.

Change-Id: If3048210dcc71a6eb4a732b347e57398213b9f72
Bug-Url: https://bugzilla.redhat.com/1381606
Signed-off-by: Ravi Nori <rnori@redhat.com>
(cherry picked from commit 39b2133fbf683840af70028725774dbf2caf6d94)

backend/manager/modules/aaa/src/main/java/org/ovirt/engine/core/aaa/SsoUtils.java
@@ -1,17 +1,23 @@
 package org.ovirt.engine.core.aaa;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.api.extensions.ExtMap;
 import org.ovirt.engine.core.aaa.filters.FiltersHelper;
 import org.ovirt.engine.core.common.action.CreateUserSessionParameters;
 import org.ovirt.engine.core.common.action.VdcActionType;
 import org.ovirt.engine.core.common.action.VdcReturnValueBase;
 import org.ovirt.engine.core.common.constants.SessionConstants;
+import org.ovirt.engine.core.utils.crypt.EngineEncryptionUtils;
+import org.ovirt.engine.core.uutils.crypto.EnvelopeEncryptDecrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -69,6 +75,25 @@ public class SsoUtils {
             }
         }
         return engineSessionId;
+    }
+
+    public static String getPassword(String token) throws Exception {
+        if (StringUtils.isEmpty(token)) {
+            throw new RuntimeException("Sso access token is null.");
+        }
+        final Map<String, Object> response = SsoOAuthServiceUtils.getTokenInfo(
+                token,
+                "ovirt-ext=token:password-access");
+        FiltersHelper.isStatusOk(response);
+        final Map<String, Object> ovirt = (HashMap<String, Object>) response.get("ovirt");
+        String password = (String) ovirt.get("password");
+        // passwords can be empty so we check for null
+        if (password == null) {
+            throw new RuntimeException("Unable to retrieve user password for session.");
+        }
+        return new String(
+                EnvelopeEncryptDecrypt.decrypt(EngineEncryptionUtils.getPrivateKeyEntry(), password),
+                StandardCharsets.UTF_8);
     }
 
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VmLogonCommand.java
@@ -2,6 +2,7 @@ package org.ovirt.engine.core.bll;
 
 import javax.inject.Inject;
 
+import org.ovirt.engine.core.aaa.SsoUtils;
 import org.ovirt.engine.core.bll.aaa.SessionDataContainer;
 import org.ovirt.engine.core.bll.context.CommandContext;
 import org.ovirt.engine.core.common.action.VmOperationParameterBase;
@@ -9,8 +10,12 @@ import org.ovirt.engine.core.common.businessentities.aaa.DbUser;
 import org.ovirt.engine.core.common.errors.EngineMessage;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VmLogonVDSCommandParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VmLogonCommand<T extends VmOperationParameterBase> extends VmOperationCommandBase<T> {
+
+    private static final Logger log = LoggerFactory.getLogger(VmLogonCommand.class);
 
     public VmLogonCommand(T parameters, CommandContext cmdContext) {
         super(parameters, cmdContext);
@@ -40,18 +45,24 @@ public class VmLogonCommand<T extends VmOperationParameterBase> extends VmOperat
 
     @Override
     protected void perform() {
-        // Send the log on command to the virtual machine:
-        final DbUser currentUser = getCurrentUser();
-        final String password = sessionDataContainer.getPassword(getParameters().getSessionId());
-        final String domainController = currentUser != null ? currentUser.getDomain() : "";
-        final boolean sentToVM = runVdsCommand(
-                        VDSCommandType.VmLogon,
-                        new VmLogonVDSCommandParameters(
-                                getVdsId(),
-                                getVm().getId(),
-                                domainController,
-                                getUserName(),
-                                password)).getSucceeded();
-        setSucceeded(sentToVM);
+        try {
+            // Send the log on command to the virtual machine:
+            final DbUser currentUser = getCurrentUser();
+            final String password = SsoUtils.getPassword(
+                    sessionDataContainer.getSsoAccessToken(getParameters().getSessionId()));
+            final String domainController = currentUser != null ? currentUser.getDomain() : "";
+            final boolean sentToVM = runVdsCommand(
+                    VDSCommandType.VmLogon,
+                    new VmLogonVDSCommandParameters(
+                            getVdsId(),
+                            getVm().getId(),
+                            domainController,
+                            getUserName(),
+                            password)).getSucceeded();
+            setSucceeded(sentToVM);
+        } catch (Exception ex) {
+            log.error("Unable to execute VmLogon with message {}", ex.getMessage());
+            log.debug("Exception", ex);
+        }
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/aaa/IsPasswordDelegationPossibleQuery.java
@@ -2,6 +2,7 @@ package org.ovirt.engine.core.bll.aaa;
 
 import javax.inject.Inject;
 
+import org.ovirt.engine.core.aaa.SsoUtils;
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 
@@ -16,7 +17,14 @@ public class IsPasswordDelegationPossibleQuery<P extends VdcQueryParametersBase>
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(sessionDataContainer.getPassword(getParameters().getSessionId()) != null);
+        String password = null;
+        try {
+            password = SsoUtils.getPassword(sessionDataContainer.getSsoAccessToken(getParameters().getSessionId()));
+        } catch(Exception ex) {
+            log.error("Unable to execute IsPasswordDelegationPossibleQuery with message {}", ex.getMessage());
+            log.debug("Exception", ex);
+        }
+        getQueryReturnValue().setReturnValue(password != null);
         getQueryReturnValue().setSucceeded(true);
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/aaa/SessionDataContainer.java
@@ -51,7 +51,6 @@ public class SessionDataContainer {
 
     private static final String USER_PARAMETER_NAME = "user";
     private static final String SOURCE_IP = "source_ip";
-    private static final String PASSWORD_PARAMETER_NAME = "password";
     private static final String PROFILE_PARAMETER_NAME = "profile";
     private static final String HARD_LIMIT_PARAMETER_NAME = "hard_limit";
     private static final String SOFT_LIMIT_PARAMETER_NAME = "soft_limit";
@@ -290,27 +289,6 @@ public class SessionDataContainer {
     public DbUser getUser(String sessionId, boolean refresh) {
         return (DbUser) getData(sessionId, USER_PARAMETER_NAME, refresh);
     }
-
-    /**
-     * Sets the password of the user for the current session.
-     *
-     * @param user the password of the user
-     */
-    public void setPassword(String sessionId, String password) {
-        setData(sessionId, PASSWORD_PARAMETER_NAME, password);
-    }
-
-    /**
-     * Returns the password of the current user stored in the session.
-     *
-     * @return an array of characters containing the password or
-     *     <code>null</code> if the password is not available
-     */
-    public String getPassword(String sessionId) {
-        return (String) getData(sessionId, PASSWORD_PARAMETER_NAME, false);
-    }
-
-
 
     public void refresh(String sessionId) {
         refresh(getSessionInfo(sessionId));

commit b89c232ef22678885ec935d86f4fb22e347f26ba

engine: VmLogonCommand should propogate errors
VmLogonCommand should propogate errors
in running VmLogon vds command.

Fixing a regression caused by f8da95ffe

Change-Id: I23e5a22d16298027ffdd5814b26906cfdc9d60eb
Bug-Url: https://bugzilla.redhat.com/1391146
Signed-off-by: Ravi Nori <rnori@redhat.com>

backend/manager/modules/aaa/src/main/java/org/ovirt/engine/core/aaa/SsoUtils.java
@@ -77,7 +77,7 @@ public class SsoUtils {
         return engineSessionId;
     }
 
-    public static String getPassword(String token) throws Exception {
+    public static String getPassword(String token) {
         if (StringUtils.isEmpty(token)) {
             throw new RuntimeException("Sso access token is null.");
         }
@@ -87,13 +87,16 @@ public class SsoUtils {
         FiltersHelper.isStatusOk(response);
         final Map<String, Object> ovirt = (HashMap<String, Object>) response.get("ovirt");
         String password = (String) ovirt.get("password");
-        // passwords can be empty so we check for null
-        if (password == null) {
-            throw new RuntimeException("Unable to retrieve user password for session.");
+        try {
+            password = password == null ? null : new String(
+                    EnvelopeEncryptDecrypt.decrypt(EngineEncryptionUtils.getPrivateKeyEntry(), password),
+                    StandardCharsets.UTF_8);
+        } catch (Exception ex) {
+            log.error("Unable to decrypt user password for session {}.", ex.getMessage());
+            log.debug("Exception", ex);
+            password = null;
         }
-        return new String(
-                EnvelopeEncryptDecrypt.decrypt(EngineEncryptionUtils.getPrivateKeyEntry(), password),
-                StandardCharsets.UTF_8);
+        return password;
     }
 
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VmLogonCommand.java
@@ -45,24 +45,19 @@ public class VmLogonCommand<T extends VmOperationParameterBase> extends VmOperat
 
     @Override
     protected void perform() {
-        try {
-            // Send the log on command to the virtual machine:
-            final DbUser currentUser = getCurrentUser();
-            final String password = SsoUtils.getPassword(
-                    sessionDataContainer.getSsoAccessToken(getParameters().getSessionId()));
-            final String domainController = currentUser != null ? currentUser.getDomain() : "";
-            final boolean sentToVM = runVdsCommand(
-                    VDSCommandType.VmLogon,
-                    new VmLogonVDSCommandParameters(
-                            getVdsId(),
-                            getVm().getId(),
-                            domainController,
-                            getUserName(),
-                            password)).getSucceeded();
-            setSucceeded(sentToVM);
-        } catch (Exception ex) {
-            log.error("Unable to execute VmLogon with message {}", ex.getMessage());
-            log.debug("Exception", ex);
-        }
+        // Send the log on command to the virtual machine:
+        final DbUser currentUser = getCurrentUser();
+        final String password = SsoUtils.getPassword(
+                sessionDataContainer.getSsoAccessToken(getParameters().getSessionId()));
+        final String domainController = currentUser != null ? currentUser.getDomain() : "";
+        final boolean sentToVM = runVdsCommand(
+                VDSCommandType.VmLogon,
+                new VmLogonVDSCommandParameters(
+                        getVdsId(),
+                        getVm().getId(),
+                        domainController,
+                        getUserName(),
+                        password)).getSucceeded();
+        setSucceeded(sentToVM);
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/aaa/IsPasswordDelegationPossibleQuery.java
@@ -17,13 +17,7 @@ public class IsPasswordDelegationPossibleQuery<P extends VdcQueryParametersBase>
 
     @Override
     protected void executeQueryCommand() {
-        String password = null;
-        try {
-            password = SsoUtils.getPassword(sessionDataContainer.getSsoAccessToken(getParameters().getSessionId()));
-        } catch(Exception ex) {
-            log.error("Unable to execute IsPasswordDelegationPossibleQuery with message {}", ex.getMessage());
-            log.debug("Exception", ex);
-        }
+        String password = SsoUtils.getPassword(sessionDataContainer.getSsoAccessToken(getParameters().getSessionId()));
         getQueryReturnValue().setReturnValue(password != null);
         getQueryReturnValue().setSucceeded(true);
     }