279ec819795f1421e071a4689b9eed0bf1fec680
Liron Aravot <laravot@redhat.com>
2016-11-16 13:07:03 +0200

    core: CopyDataCommand - use entity polling
    This patch adds use of entity polling in the CopyData command by
    making the following changes:
    1. Pass the currnet volume generation when executing the copyData
    vdsm verb.
    2. Implement the EntityPollingCommand interface.
    3. Using VdsmImagePoller in order to poll the entity when needed.
    
    Change-Id: Ib4dbeb311120bba31361e5ae83963bec983febd1
    Signed-off-by: Liron Aravot <laravot@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/CopyDataCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/CopyDataCommand.java
@@ -3,14 +3,19 @@ package org.ovirt.engine.core.bll.storage.disk.image;
 import java.util.Collections;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.InternalCommandAttribute;
 import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
 import org.ovirt.engine.core.bll.context.CommandContext;
+import org.ovirt.engine.core.bll.storage.EntityPollingCommand;
 import org.ovirt.engine.core.bll.storage.StorageJobCommand;
 import org.ovirt.engine.core.bll.storage.utils.VdsCommandsHelper;
 import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.common.action.CopyDataCommandParameters;
+import org.ovirt.engine.core.common.businessentities.HostJobInfo.HostJobStatus;
 import org.ovirt.engine.core.common.businessentities.VdsmImageLocationInfo;
+import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.common.job.StepEnum;
 import org.ovirt.engine.core.common.job.StepSubjectEntity;
 import org.ovirt.engine.core.common.vdscommands.CopyVolumeDataVDSCommandParameters;
@@ -19,7 +24,10 @@ import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 @NonTransactiveCommandAttribute
 @InternalCommandAttribute
 public class CopyDataCommand<T extends CopyDataCommandParameters> extends
-        StorageJobCommand<T> {
+        StorageJobCommand<T> implements EntityPollingCommand {
+
+    @Inject
+    private VdsmImagePoller poller;
 
     public CopyDataCommand(T parameters, CommandContext cmdContext) {
         super(parameters, cmdContext);
@@ -27,6 +35,7 @@ public class CopyDataCommand<T extends CopyDataCommandParameters> extends
 
     @Override
     protected void executeCommand() {
+        completeGenerationInfo();
         VdsCommandsHelper.runVdsCommandWithFailover(VDSCommandType.CopyVolumeData,
                 new CopyVolumeDataVDSCommandParameters(getParameters().getStorageJobId(),
                         getParameters().getSrcInfo(),
@@ -34,6 +43,17 @@ public class CopyDataCommand<T extends CopyDataCommandParameters> extends
                         getParameters().isCollapse()),
                 getParameters().getStoragePoolId(), this);
         setSucceeded(true);
+    }
+
+    private void completeGenerationInfo() {
+        if (!isDstVdsmImage()) {
+            return;
+        }
+        VdsmImageLocationInfo info = (VdsmImageLocationInfo) getParameters().getDstInfo();
+        DiskImage image = ImagesHandler.getVolumeInfoFromVdsm(getParameters().getStoragePoolId(),
+                info.getStorageDomainId(), info.getImageGroupId(), info.getImageId());
+        info.setGeneration(image.getImage().getGeneration());
+        persistCommandIfNeeded();
     }
 
     @Override
@@ -55,5 +75,20 @@ public class CopyDataCommand<T extends CopyDataCommandParameters> extends
         }
 
         return super.getCommandStepSubjectEntities();
+    }
+
+    private boolean isDstVdsmImage() {
+        return getParameters().getDstInfo() instanceof VdsmImageLocationInfo;
+    }
+
+    @Override
+    public HostJobStatus poll() {
+        if (isDstVdsmImage()) {
+            VdsmImageLocationInfo info = (VdsmImageLocationInfo) getParameters().getDstInfo();
+            return poller.pollImage(getParameters().getStoragePoolId(), info.getStorageDomainId(),
+                    info.getImageGroupId(), info.getImageId(), info.getGeneration(), getCommandId(), getActionType());
+        }
+
+        return null;
     }
 }
