f125ebb5b4a1e3cec97d34715ff2a1d3ed8db656
Liron Aravot <laravot@redhat.com>
2016-11-16 13:01:01 +0200

    core: adding VdsmImagePoller
    This patch adds the VdsmImagePoller class. The poller is supposed to
    be used to determine the status of ongoing vdsm job on vdsm image when
    the job polling isn't possible.
    
    The poller uses the following logic:
    1. The volume lease isn't FREE - the operation is running
    2. The lease is free and the volume status is ILLEGAL - the operation
    has failed.
    3. The lease is free, the volume status is OK and the generation was
    incremented - the operation has succeded.
    
    Any other case means that the job status couldn't have been determined
    using entity polling.
    
    Change-Id: Ibf3f4d27ece12b2d855cdf3c1f5a563019aa94cc
    Signed-off-by: Liron Aravot <laravot@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/VdsmImagePoller.java
@@ -0,0 +1,56 @@
+package org.ovirt.engine.core.bll.storage.disk.image;
+
+import javax.inject.Singleton;
+
+import org.ovirt.engine.core.bll.storage.utils.VdsCommandsHelper;
+import org.ovirt.engine.core.common.action.VdcActionType;
+import org.ovirt.engine.core.common.businessentities.HostJobInfo.HostJobStatus;
+import org.ovirt.engine.core.common.businessentities.LeaseState;
+import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
+import org.ovirt.engine.core.common.businessentities.storage.Image;
+import org.ovirt.engine.core.common.businessentities.storage.ImageStatus;
+import org.ovirt.engine.core.common.vdscommands.GetVolumeInfoVDSCommandParameters;
+import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
+import org.ovirt.engine.core.compat.Guid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public final class VdsmImagePoller {
+    private static final Logger log = LoggerFactory.getLogger(VdsmImagePoller.class);
+
+    private VdsmImagePoller() {
+    }
+
+    protected HostJobStatus pollImage(Guid storagePoolId, Guid storageDomainId, Guid imageGroupId, Guid imageId,
+                                      int executionGeneration, Guid cmdId, VdcActionType actionType) {
+        Image imageInfo =
+                ((DiskImage) VdsCommandsHelper.runVdsCommandWithoutFailover(
+                        VDSCommandType.GetVolumeInfo,
+                        new GetVolumeInfoVDSCommandParameters(storagePoolId,
+                                storageDomainId,
+                                imageGroupId,
+                                imageId), storagePoolId, null).getReturnValue()).getImage();
+        if (imageInfo.getLeaseState() != LeaseState.FREE) {
+            log.info("Command {} id: '{}': the volume lease is not FREE - the job is running",
+                    actionType, cmdId);
+            return HostJobStatus.running;
+        }
+
+        if (imageInfo.getStatus() == ImageStatus.ILLEGAL) {
+            log.info("Command {} id: '{}': the volume is in ILLEGAL status - the job has failed",
+                    actionType, cmdId);
+            return HostJobStatus.failed;
+        }
+
+        if (imageInfo.getStatus() == ImageStatus.OK && imageInfo.getGeneration() == executionGeneration + 1) {
+            log.info("Command {} id: '{}': the volume is in OK status and the generation was incremented - the " +
+                            "job execution has completed successfully",
+                    actionType, cmdId);
+            return HostJobStatus.done;
+        }
+
+        log.info("Command {} id: '{}': couldn't determine the status of the job by entity polling", actionType, cmdId);
+        return null;
+    }
+}
