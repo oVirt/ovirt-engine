7128aa5c3ed66965374739324f2b078741e42d8a
Tal Nisan <tnisan@redhat.com>
2016-11-23 14:09:05 +0200

    core: Use injected DAOs in DiskHandler
    Change-Id: I59087ff8902e35b9f7c922eba0a17ba4f0791795
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/DiskHandler.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/DiskHandler.java
@@ -1,20 +1,27 @@
 package org.ovirt.engine.core.bll.storage.disk;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.ovirt.engine.core.common.businessentities.storage.Disk;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 import org.ovirt.engine.core.dao.DiskDao;
 import org.ovirt.engine.core.dao.DiskImageDao;
 
 @Singleton
 public class DiskHandler {
+
+    @Inject
+    private DiskDao diskDao;
+
+    @Inject
+    private DiskImageDao diskImageDao;
+
     /**
      * loads the disk info for the active snapshot, for luns the lun disk will be returned.
      */
     public Disk loadActiveDisk(Guid diskId) {
-        return getDiskDao().get(diskId);
+        return diskDao.get(diskId);
     }
 
     /**
@@ -22,14 +29,6 @@ public class DiskHandler {
      */
     public Disk loadDiskFromSnapshot(Guid diskId, Guid snapshotId) {
         return snapshotId == null ?  loadActiveDisk(diskId) :
-                getDiskImageDao().getDiskSnapshotForVmSnapshot(diskId, snapshotId);
-    }
-
-    private static DiskDao getDiskDao() {
-        return DbFacade.getInstance().getDiskDao();
-    }
-
-    private static DiskImageDao getDiskImageDao() {
-        return DbFacade.getInstance().getDiskImageDao();
+                diskImageDao.getDiskSnapshotForVmSnapshot(diskId, snapshotId);
     }
 }
