9c9d03a79277bebd7a17c19ce2e2eae460064881
Vojtech Szocs <vszocs@redhat.com>
2016-12-01 19:26:47 +0100

    webadmin: Remove IE9 hack in UI suggest box widget
    oVirt UI supports IE10+ so there's no need for IE9- hacks.
    
    Change-Id: I0c8f6596f55c4dfa42be674abceb374574a21cf8
    Bug-Url: https://bugzilla.redhat.com/1378935
    Related-To: https://bugzilla.redhat.com/1160774
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/BaseListModelSuggestBox.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/editor/BaseListModelSuggestBox.java
@@ -1,14 +1,12 @@
 package org.ovirt.engine.ui.common.widget.editor;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.ovirt.engine.ui.uicommonweb.HasCleanup;
 
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.dom.client.Style.Overflow;
-import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.event.dom.client.KeyUpHandler;
@@ -19,7 +17,6 @@ import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasConstrainedValue;
 import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
@@ -272,53 +269,6 @@ public abstract class BaseListModelSuggestBox<T> extends Composite implements
             scrollSelectedItemIntoView();
         }
 
-        @Override
-        protected void showSuggestions(SuggestBox suggestBox, Collection<? extends Suggestion> suggestions,
-                                       boolean isDisplayStringHTML, boolean isAutoSelectEnabled,
-                                       SuggestBox.SuggestionCallback callback) {
-            super.showSuggestions(suggestBox, suggestions, isDisplayStringHTML, isAutoSelectEnabled, callback);
-            fixIe9Scrollbar();
-        }
-
-        /**
-         * IE9 hack solving wrong scrollbar rendering.
-         * ie9 counts the width of block without scrollbar event if the scrollbar should be visible, then
-         * the scrollbar is show, content shifted to left and trimmed on left side.
-         * Resetting 'overflow-y' to 'hidden' and back to 'scroll' prevents the shift to left, extending the width
-         * adds space for scrollbar.
-         *
-         * <p>To be dropped together with IE9 support.</p>
-         *
-         * @see <a href="https://bugzilla.redhat.com/show_bug.cgi?id=1160774">Bug 1160774</a>
-         */
-        // TODO(vs) we don't support IE9 anymore, we should remove this code
-        private void fixIe9Scrollbar() {
-            boolean isIe9 = Window.Navigator.getUserAgent().contains("MSIE 9.0"); //$NON-NLS-1$
-            if (isIe9 && super.isSuggestionListShowing()) {
-                final Style suggestionMenuStyle = getSuggestionMenu().getElement().getStyle();
-                final Overflow originalOverflowY = valueOfOverflow(
-                        getComputedOverflowY(getSuggestionMenu().getElement()), Overflow.SCROLL);
-                suggestionMenuStyle.setOverflowY(Overflow.HIDDEN);
-                suggestionMenuStyle.setOverflowY(originalOverflowY);
-                final int scrollbarWidthEstimate = 32; // px
-                suggestionMenuStyle.setProperty("width", "");  //$NON-NLS-1$ $NON-NLS-2$
-                suggestionMenuStyle.setWidth(
-                        getSuggestionMenu().getElement().getClientWidth() + scrollbarWidthEstimate,
-                        Unit.PX);
-            }
-        }
-
-        private native String getComputedOverflowY(final Element element) /*-{
-            return $wnd.getComputedStyle(element).overflowY;
-        }-*/;
-
-        private Overflow valueOfOverflow(String value, Overflow defaultValue) {
-            for (Overflow overflow : Overflow.values()) {
-                if (overflow.getCssName().equals(value)) {
-                    return  overflow;
-                }
-            }
-            return defaultValue;
-        }
     }
+
 }
