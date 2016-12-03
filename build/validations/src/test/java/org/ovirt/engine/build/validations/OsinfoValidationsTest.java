a9480aeb613280bdc400cbc90e3628955dfc11f6
Greg Sheremeta <gshereme@redhat.com>
2016-12-02 19:50:14 -0500

    engine: spelling fix "Ovirt" -> "oVirt"
    Change-Id: I82fb5a11e5e9c20977f0cebb5958d476dc8b23f2
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

--- build/validations/src/test/java/org/ovirt/engine/build/validations/OsinfoValidationsTest.java
+++ build/validations/src/test/java/org/ovirt/engine/build/validations/OsinfoValidationsTest.java
@@ -10,7 +10,7 @@ public class OsinfoValidationsTest {
         try {
             OsinfoPropertiesParser.parse(System.getProperty("osinfo.properties"));
         } catch (Exception e) {
-            System.err.println("Ovirt-engine will fail to load with a broken osinfo properties file.");
+            System.err.println("oVirt-engine will fail to load with a broken osinfo properties file.");
             System.err.println("Please fix the properties file or osinfo.jj grammar under org.ovirt.engine.core.utils.osinfo.");
             throw e;
         }
