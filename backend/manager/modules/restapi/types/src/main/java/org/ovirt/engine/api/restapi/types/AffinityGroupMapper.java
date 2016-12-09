bc0263fd30717e995521b06e18bdf7a2a59008e7
Martin Sivak <msivak@redhat.com>
2016-12-09 15:34:51 +0100

    restapi: Update the mappers for AffinityGroup to fully utilize the new model
    Bug-Url: https://bugzilla.redhat.com/1392393
    Change-Id: I8b6ccee713f621f48969aad5307f72d9e7fab03b
    Signed-off-by: Martin Sivak <msivak@redhat.com>

--- backend/manager/modules/restapi/types/src/main/java/org/ovirt/engine/api/restapi/types/AffinityGroupMapper.java
+++ backend/manager/modules/restapi/types/src/main/java/org/ovirt/engine/api/restapi/types/AffinityGroupMapper.java
@@ -1,9 +1,18 @@
 package org.ovirt.engine.api.restapi.types;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.ovirt.engine.api.model.AffinityGroup;
+import org.ovirt.engine.api.model.AffinityRule;
 import org.ovirt.engine.api.model.Cluster;
+import org.ovirt.engine.api.model.Host;
+import org.ovirt.engine.api.model.Hosts;
+import org.ovirt.engine.api.model.Vm;
+import org.ovirt.engine.api.model.Vms;
 import org.ovirt.engine.api.restapi.utils.GuidUtils;
 import org.ovirt.engine.core.common.scheduling.EntityAffinityRule;
+import org.ovirt.engine.core.compat.Guid;
 
 public class AffinityGroupMapper {
 
@@ -14,11 +23,57 @@ public class AffinityGroupMapper {
         model.setId(entity.getId().toString());
         model.setName(entity.getName());
         model.setDescription(entity.getDescription());
-        model.setPositive(entity.getVmPolarityBooleanObject());
+
+        // These two fields are maintained to keep the backwards compatibility
+        // with version 4 of the API and will be removed in future.
+        model.setPositive(entity.isVmAffinityEnabled() ? entity.getVmPolarityBooleanObject() : null);
         model.setEnforcing(entity.isVmEnforcing());
+
+        AffinityRule hostsRule = model.getHostsRule();
+        if (hostsRule == null) {
+            hostsRule = new AffinityRule();
+            model.setHostsRule(hostsRule);
+        }
+        hostsRule.setEnabled(true);
+        hostsRule.setEnforcing(entity.isVdsEnforcing());
+        hostsRule.setPositive(entity.isVdsPositive());
+
+        AffinityRule vmsRule = model.getVmsRule();
+        if (vmsRule == null) {
+            vmsRule = new AffinityRule();
+            model.setVmsRule(vmsRule);
+        }
+        vmsRule.setEnabled(entity.isVmAffinityEnabled());
+        vmsRule.setEnforcing(entity.isVmEnforcing());
+        vmsRule.setPositive(entity.isVmPositive());
+
         Cluster cluster = new Cluster();
         cluster.setId(entity.getClusterId().toString());
         model.setCluster(cluster);
+
+        Hosts hosts = model.getHosts();
+        if (hosts == null) {
+            hosts = new Hosts();
+            model.setHosts(hosts);
+        }
+
+        entity.getVdsIds().stream().map(id -> {
+            Host host = new Host();
+            host.setId(id.toString());
+            return host;
+        }).forEach(model.getHosts().getHosts()::add);
+
+        Vms vms = model.getVms();
+        if (vms == null) {
+            vms = new Vms();
+            model.setVms(vms);
+        }
+
+        entity.getVmIds().stream().map(id -> {
+            Vm vm = new Vm();
+            vm.setId(id.toString());
+            return vm;
+        }).forEach(model.getVms().getVms()::add);
 
         return model;
     }
@@ -40,15 +95,75 @@ public class AffinityGroupMapper {
         if (model.isSetCluster() && model.getCluster().isSetId()) {
             entity.setClusterId(GuidUtils.asGuid(model.getCluster().getId()));
         }
-        if (model.isSetPositive()) {
-            if (model.isPositive()) {
-                entity.setVmAffinityRule(EntityAffinityRule.POSITIVE);
-            } else {
-                entity.setVmAffinityRule(EntityAffinityRule.NEGATIVE);
+
+        AffinityRule hostsRule = model.getHostsRule();
+        if (hostsRule != null) {
+            if (hostsRule.isSetEnforcing()) {
+                entity.setVdsEnforcing(hostsRule.isEnforcing());
+            }
+
+            if (hostsRule.isSetPositive()) {
+                entity.setVdsAffinityRule(hostsRule.isPositive()
+                        ? EntityAffinityRule.POSITIVE : EntityAffinityRule.NEGATIVE);
             }
         }
-        if (model.isSetEnforcing()) {
-            entity.setVmEnforcing(model.isEnforcing());
+
+        AffinityRule vmsRule = model.getVmsRule();
+        if (vmsRule != null) {
+            if (vmsRule.isSetEnabled()) {
+                if (!vmsRule.isEnabled()) {
+                    entity.setVmAffinityRule(EntityAffinityRule.DISABLED);
+                } else if (vmsRule.isSetPositive()) {
+                    entity.setVmAffinityRule(vmsRule.isPositive()
+                            ? EntityAffinityRule.POSITIVE : EntityAffinityRule.NEGATIVE);
+                }
+            }
+            if (vmsRule.isSetEnforcing()) {
+                entity.setVmEnforcing(vmsRule.isEnforcing());
+            }
+        } else {
+            if (model.isSetPositive()) {
+                entity.setVmAffinityRule(model.isPositive()
+                        ? EntityAffinityRule.POSITIVE : EntityAffinityRule.NEGATIVE);
+            } else {
+                entity.setVmAffinityRule(EntityAffinityRule.DISABLED);
+            }
+
+            if (model.isSetEnforcing()) {
+                entity.setVmEnforcing(model.isEnforcing());
+            }
+        }
+
+        if (model.isSetHosts()) {
+            List<Guid> hostIds = entity.getVdsIds();
+            if (hostIds == null) {
+                hostIds = new ArrayList<>();
+                entity.setVdsIds(hostIds);
+            }
+
+            // Replace the existing list with the provided one
+            hostIds.clear();
+            model.getHosts().getHosts().stream()
+                    .filter(Host::isSetId)
+                    .map(Host::getId)
+                    .map(Guid::createGuidFromString)
+                    .forEach(hostIds::add);
+        }
+
+        if (model.isSetVms()) {
+            List<Guid> vmIds = entity.getVmIds();
+            if (vmIds == null) {
+                vmIds = new ArrayList<>();
+                entity.setVmIds(vmIds);
+            }
+
+            // Replace the existing list with the provided one
+            vmIds.clear();
+            model.getVms().getVms().stream()
+                    .filter(Vm::isSetId)
+                    .map(Vm::getId)
+                    .map(Guid::createGuidFromString)
+                    .forEach(vmIds::add);
         }
 
         return entity;
--- backend/manager/modules/restapi/types/src/test/java/org/ovirt/engine/api/restapi/types/AffinityGroupMapperTest.java
+++ backend/manager/modules/restapi/types/src/test/java/org/ovirt/engine/api/restapi/types/AffinityGroupMapperTest.java
@@ -1,6 +1,14 @@
 package org.ovirt.engine.api.restapi.types;
 
+import org.junit.Test;
 import org.ovirt.engine.api.model.AffinityGroup;
+import org.ovirt.engine.api.model.AffinityRule;
+import org.ovirt.engine.api.model.Host;
+import org.ovirt.engine.api.model.Hosts;
+import org.ovirt.engine.api.model.Vm;
+import org.ovirt.engine.api.model.Vms;
+import org.ovirt.engine.core.common.scheduling.EntityAffinityRule;
+import org.ovirt.engine.core.compat.Guid;
 
 public class AffinityGroupMapperTest extends AbstractInvertibleMappingTest<AffinityGroup, org.ovirt.engine.core.common.scheduling.AffinityGroup, org.ovirt.engine.core.common.scheduling.AffinityGroup> {
 
@@ -8,6 +16,15 @@ public class AffinityGroupMapperTest extends AbstractInvertibleMappingTest<Affin
         super(AffinityGroup.class,
                 org.ovirt.engine.core.common.scheduling.AffinityGroup.class,
                 org.ovirt.engine.core.common.scheduling.AffinityGroup.class);
+    }
+
+    @Override
+    protected AffinityGroup postPopulate(AffinityGroup model) {
+        // Derandomize dependent and fixed values
+        model.setEnforcing(model.getVmsRule().isEnforcing());
+        model.setPositive(model.getVmsRule().isEnabled() ? model.getVmsRule().isPositive() : null);
+        model.getHostsRule().setEnabled(true);
+        return super.postPopulate(model);
     }
 
     @Override
@@ -19,6 +36,353 @@ public class AffinityGroupMapperTest extends AbstractInvertibleMappingTest<Affin
         assertEquals(model.getCluster().getId(), transform.getCluster().getId());
         assertEquals(model.isPositive(), transform.isPositive());
         assertEquals(model.isEnforcing(), transform.isEnforcing());
+        assertNotNull(transform.getHostsRule());
+        assertEquals(model.getHostsRule().isEnabled(), transform.getHostsRule().isEnabled());
+        assertEquals(model.getHostsRule().isEnforcing(), transform.getHostsRule().isEnforcing());
+        assertEquals(model.getHostsRule().isPositive(), transform.getHostsRule().isPositive());
+        assertNotNull(transform.getVmsRule());
+        assertEquals(model.getVmsRule().isEnabled(), transform.getVmsRule().isEnabled());
+        assertEquals(model.getVmsRule().isEnforcing(), transform.getVmsRule().isEnforcing());
+
+        // The positive field is coupled with the enabled field internally. Disabled
+        // group will always return false in the positive field.
+        if (transform.getVmsRule().isEnabled()) {
+            assertEquals(model.getVmsRule().isPositive(), transform.getVmsRule().isPositive());
+        } else {
+            assertEquals(false, transform.getVmsRule().isPositive());
+        }
     }
 
+    @Test
+    public void testVmsRule() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+        model.setEnforcing(true);
+        model.setPositive(false);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(EntityAffinityRule.NEGATIVE, entity.getVmAffinityRule());
+        assertEquals(true, entity.isVmEnforcing());
+        assertEquals(true, entity.isVmAffinityEnabled());
+    }
+
+    @Test
+    public void testVmsRuleNeg() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+        model.setEnforcing(false);
+        model.setPositive(true);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(EntityAffinityRule.POSITIVE, entity.getVmAffinityRule());
+        assertEquals(false, entity.isVmEnforcing());
+        assertEquals(true, entity.isVmAffinityEnabled());
+    }
+
+    @Test
+    public void testVmsRuleStructure() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityRule rule = new AffinityRule();
+        rule.setEnforcing(true);
+        rule.setPositive(false);
+        rule.setEnabled(true);
+
+        model.setVmsRule(rule);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(EntityAffinityRule.NEGATIVE, entity.getVmAffinityRule());
+        assertEquals(true, entity.isVmEnforcing());
+        assertEquals(true, entity.isVmAffinityEnabled());
+    }
+
+    @Test
+    public void testVmsRuleStructureNeg() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityRule rule = new AffinityRule();
+        rule.setEnforcing(false);
+        rule.setPositive(true);
+        rule.setEnabled(true);
+
+        model.setVmsRule(rule);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(EntityAffinityRule.POSITIVE, entity.getVmAffinityRule());
+        assertEquals(false, entity.isVmEnforcing());
+        assertEquals(true, entity.isVmAffinityEnabled());
+    }
+
+    @Test
+    public void testVmsRuleStructureDisabled() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityRule rule = new AffinityRule();
+        rule.setEnforcing(true);
+        rule.setPositive(false);
+        rule.setEnabled(false);
+
+        model.setVmsRule(rule);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(EntityAffinityRule.DISABLED, entity.getVmAffinityRule());
+        assertEquals(true, entity.isVmEnforcing());
+        assertEquals(false, entity.isVmAffinityEnabled());
+    }
+
+    @Test
+    public void testVmsRuleStructureWins() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+        model.setEnforcing(false);
+        model.setPositive(false);
+
+        AffinityRule rule = new AffinityRule();
+        rule.setEnforcing(true);
+        rule.setPositive(true);
+        rule.setEnabled(true);
+
+        model.setVmsRule(rule);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(EntityAffinityRule.POSITIVE, entity.getVmAffinityRule());
+        assertEquals(true, entity.isVmEnforcing());
+        assertEquals(true, entity.isVmAffinityEnabled());
+    }
+
+    @Test
+    public void testHostsRuleStructure() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityRule rule = new AffinityRule();
+        rule.setEnforcing(true);
+        rule.setPositive(false);
+
+        model.setHostsRule(rule);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(EntityAffinityRule.NEGATIVE, entity.getVdsAffinityRule());
+        assertEquals(true, entity.isVdsEnforcing());
+    }
+
+    @Test
+    public void testHostsRuleStructureNeg() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityRule rule = new AffinityRule();
+        rule.setEnforcing(false);
+        rule.setPositive(true);
+
+        model.setHostsRule(rule);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(EntityAffinityRule.POSITIVE, entity.getVdsAffinityRule());
+        assertEquals(false, entity.isVdsEnforcing());
+    }
+
+    @Test
+    public void testHostAffinityRestOutput() throws  Exception {
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+        entity.setId(Guid.Empty);
+        entity.setClusterId(Guid.Empty);
+        entity.setVdsEnforcing(false);
+        entity.setVdsAffinityRule(EntityAffinityRule.POSITIVE);
+
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityGroupMapper.map(entity, model);
+
+        assertNotNull(model.getHostsRule());
+        assertEquals(true, model.getHostsRule().isEnabled());
+        assertEquals(true, model.getHostsRule().isPositive());
+        assertEquals(false, model.getHostsRule().isEnforcing());
+    }
+
+    @Test
+    public void testHostAffinityNegRestOutput() throws  Exception {
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+        entity.setId(Guid.Empty);
+        entity.setClusterId(Guid.Empty);
+        entity.setVdsEnforcing(true);
+        entity.setVdsAffinityRule(EntityAffinityRule.NEGATIVE);
+
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityGroupMapper.map(entity, model);
+
+        assertNotNull(model.getHostsRule());
+        assertEquals(true, model.getHostsRule().isEnabled());
+        assertEquals(false, model.getHostsRule().isPositive());
+        assertEquals(true, model.getHostsRule().isEnforcing());
+    }
+
+    @Test
+    public void testVmAffinityRestOutput() throws  Exception {
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+        entity.setId(Guid.Empty);
+        entity.setClusterId(Guid.Empty);
+        entity.setVmEnforcing(false);
+        entity.setVmAffinityRule(EntityAffinityRule.POSITIVE);
+
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityGroupMapper.map(entity, model);
+
+        assertNotNull(model.getVmsRule());
+        assertEquals(true, model.getVmsRule().isEnabled());
+        assertEquals(true, model.getVmsRule().isPositive());
+        assertEquals(false, model.getVmsRule().isEnforcing());
+        assertEquals(true, model.isPositive());
+        assertEquals(false, model.isEnforcing());
+    }
+
+    @Test
+    public void testVmAffinityNegRestOutput() throws  Exception {
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+        entity.setId(Guid.Empty);
+        entity.setClusterId(Guid.Empty);
+        entity.setVmEnforcing(true);
+        entity.setVmAffinityRule(EntityAffinityRule.NEGATIVE);
+
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityGroupMapper.map(entity, model);
+
+        assertNotNull(model.getVmsRule());
+        assertEquals(true, model.getVmsRule().isEnabled());
+        assertEquals(false, model.getVmsRule().isPositive());
+        assertEquals(true, model.getVmsRule().isEnforcing());
+        assertEquals(false, model.isPositive());
+        assertEquals(true, model.isEnforcing());
+    }
+
+    @Test
+    public void testVmAffinityDisabledRestOutput() throws  Exception {
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+        entity.setId(Guid.Empty);
+        entity.setClusterId(Guid.Empty);
+        entity.setVmEnforcing(true);
+        entity.setVmAffinityRule(EntityAffinityRule.DISABLED);
+
+        AffinityGroup model = new AffinityGroup();
+
+        AffinityGroupMapper.map(entity, model);
+
+        assertNotNull(model.getVmsRule());
+        assertEquals(false, model.getVmsRule().isEnabled());
+        assertEquals(false, model.getVmsRule().isPositive());
+        assertEquals(true, model.getVmsRule().isEnforcing());
+        assertEquals(null, model.isPositive());
+        assertEquals(true, model.isEnforcing());
+    }
+
+    @Test
+    public void testVmIds() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        Vm vm = new Vm();
+        final Guid vmGuid = Guid.newGuid();
+        vm.setId(vmGuid.toString());
+        model.setVms(new Vms());
+        model.getVms().getVms().add(vm);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(vmGuid, entity.getVmIds().get(0));
+    }
+
+    @Test
+    public void testVmIdsReplacement() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        Vm vm = new Vm();
+        final Guid vmGuid = Guid.newGuid();
+        vm.setId(vmGuid.toString());
+        model.setVms(new Vms());
+        model.getVms().getVms().add(vm);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+        entity.getVdsIds().add(Guid.newGuid());
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(vmGuid, entity.getVmIds().get(0));
+        assertEquals(1, entity.getVmIds().size());
+    }
+
+    @Test
+    public void testHostIds() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        Host host = new Host();
+        final Guid hostGuid = Guid.newGuid();
+        host.setId(hostGuid.toString());
+        model.setHosts(new Hosts());
+        model.getHosts().getHosts().add(host);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(hostGuid, entity.getVdsIds().get(0));
+    }
+
+    @Test
+    public void testHostIdsReplacement() throws Exception {
+        AffinityGroup model = new AffinityGroup();
+
+        Host host = new Host();
+        final Guid hostGuid = Guid.newGuid();
+        host.setId(hostGuid.toString());
+        model.setHosts(new Hosts());
+        model.getHosts().getHosts().add(host);
+
+        org.ovirt.engine.core.common.scheduling.AffinityGroup entity =
+                new org.ovirt.engine.core.common.scheduling.AffinityGroup();
+        entity.getVdsIds().add(Guid.newGuid());
+
+        AffinityGroupMapper.map(model, entity);
+
+        assertEquals(1, entity.getVdsIds().size());
+        assertEquals(hostGuid, entity.getVdsIds().get(0));
+    }
 }
