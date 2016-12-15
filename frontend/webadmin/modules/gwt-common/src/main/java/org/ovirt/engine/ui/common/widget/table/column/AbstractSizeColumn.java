ebf683fac342b2fd3225602856c205b5addc85f1
Vojtech Szocs <vszocs@redhat.com>
2016-12-15 22:07:25 +0100

    webadmin,userportal: Fix NPE in AbstractSizeColumn
    The column's Comparator should not fail on NPE when
    getRawValue(T) returns null.
    
    Change-Id: Ibf61f5e244c1bfdb3ad3590e7cdd6c7ed2432203
    Bug-Url: https://bugzilla.redhat.com/1391013
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/AbstractSizeColumn.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/table/column/AbstractSizeColumn.java
@@ -15,7 +15,16 @@ public abstract class AbstractSizeColumn<T> extends AbstractRenderedTextColumn<T
         makeSortable(new Comparator<T>() {
             @Override
             public int compare(T object1, T object2) {
-                return getRawValue(object1).compareTo(getRawValue(object2));
+                Long val1 = getRawValue(object1);
+                Long val2 = getRawValue(object2);
+
+                if (val1 == null) {
+                    return (val2 == null) ? 0 : -1;
+                } else if (val2 == null) {
+                    return 1;
+                }
+
+                return val1.compareTo(val2);
             }
         });
     }
