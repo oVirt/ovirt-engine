a1a52a65d999780f531a6370d781cfffc1880e63
Sahina Bose <sabose@redhat.com>
2017-01-03 17:17:37 +0530

    webadmin: Fix tooltip for brick status
    Change-Id: Ia0c4b3b815b19f5bb3fa0768040d33d074482556
    Bug-Url: https://bugzilla.redhat.com/1296786
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/table/column/VolumeBrickStatusColumn.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/table/column/VolumeBrickStatusColumn.java
@@ -1,13 +1,17 @@
 package org.ovirt.engine.ui.webadmin.widget.table.column;
 
+import org.ovirt.engine.core.common.businessentities.gluster.GlusterBrickEntity;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeEntity;
 import org.ovirt.engine.ui.common.widget.table.column.AbstractColumn;
-import org.ovirt.engine.ui.uicompat.EnumTranslator;
+import org.ovirt.engine.ui.webadmin.ApplicationConstants;
+import org.ovirt.engine.ui.webadmin.gin.AssetProvider;
 import org.ovirt.engine.ui.webadmin.widget.table.cell.VolumeBrickStatusCell;
+
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 
 public class VolumeBrickStatusColumn extends AbstractColumn<GlusterVolumeEntity, GlusterVolumeEntity> {
+    private static final ApplicationConstants constants = AssetProvider.getConstants();
 
     public VolumeBrickStatusColumn() {
         super(new VolumeBrickStatusCell());
@@ -20,8 +24,31 @@ public class VolumeBrickStatusColumn extends AbstractColumn<GlusterVolumeEntity,
 
     @Override
     public SafeHtml getTooltip(GlusterVolumeEntity object) {
-        String status = EnumTranslator.getInstance().translate(object.getStatus());
-        return SafeHtmlUtils.fromString(status);
+        String SPACE = " "; //$NON-NLS-1$
+     // Nothing to render if no volume is provided:
+        if (object == null) {
+            return null;
+        }
+
+        int upBricks = 0;
+        int downBricks = 0;
+        for (GlusterBrickEntity brick : object.getBricks()) {
+            if (brick.isOnline()) {
+                upBricks++;
+            }
+            else {
+                downBricks++;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(upBricks)
+                .append(SPACE)
+                .append(constants.up())
+                .append(SPACE)
+                .append(downBricks)
+                .append(SPACE)
+                .append(constants.down());
+        return SafeHtmlUtils.fromString(sb.toString());
     }
 
 }
