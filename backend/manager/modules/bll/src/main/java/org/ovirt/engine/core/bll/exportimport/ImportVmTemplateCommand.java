31cde2fc567bd4cec0777c3cf77240cf0335b837
Jakub Niedermertl <jniederm@redhat.com>
2017-01-05 22:26:29 +0100

    core: Max memory validation in import commands
    VmBase#maxMemorySizeMb is validated in import commands since
    VMs/Templates may come from unreliable source and the value may be
    invalid/missing.
    
    Change-Id: I22fa4b0252e8bed8eb88e87a54903af25ce2e28c
    Bug-Url: https://bugzilla.redhat.com/1410606
    Signed-off-by: Jakub Niedermertl <jniederm@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/exportimport/ImportVmTemplateCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/exportimport/ImportVmTemplateCommand.java
@@ -243,6 +243,10 @@ public class ImportVmTemplateCommand extends MoveOrCopyTemplateCommand<ImportVmT
             return false;
         }
 
+        if (!validate(VmHandler.validateMaxMemorySize(getVmTemplate(), getEffectiveCompatibilityVersion()))) {
+            return false;
+        }
+
         return true;
     }
 
