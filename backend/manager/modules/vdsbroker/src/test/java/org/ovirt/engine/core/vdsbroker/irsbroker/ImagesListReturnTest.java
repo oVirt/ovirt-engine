1b20ce3aa4dd0a898f920e802c8cf2560f7a5e45
Benny Zlotnik <bzlotnik@redhat.com>
2017-01-08 15:07:00 +0200

    core: fix NumberFormatException when importing a removed VM
    When importing a VM while another is in the process of being
    removed a NumberFormatException will be thrown and the import
    will fail.
    
    This is caused by the removed VMs getting a "_remove_me_"
    prefix to its GUID when removed. Because of the prefix the
    method GetImagesListVDSCommand#executeIrsBrokerCommand fails
    when attempting to create an instance of the Guid object
    using the prefixed GUID of the removed VM.
    
    Fixed by filtering out the prefixed image GUIDs in
    ImagesListReturn constructor.
    
    Change-Id: I564fb9d0c08e6fabbf8ffd05cfdf93d5435a0e0b
    Bug-Url: https://bugzilla.redhat.com/1331335
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

---
+++ backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/irsbroker/ImagesListReturnTest.java
@@ -0,0 +1,26 @@
+package org.ovirt.engine.core.vdsbroker.irsbroker;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class ImagesListReturnTest {
+
+    private static final String IMAGES_LIST = "imageslist";
+
+    @Test
+    public void validateBadUUIDSFiltered() {
+        Map<String, Object> innerMap = new HashMap<>();
+        innerMap.put(IMAGES_LIST, new Object[]{"28e983b9-c2f6-4a50-a5d5-7a38089797c7",
+                                               "c3d75234-b360-4aed-8d78-413d7488bd9b",
+                                               "_remove_me_2f17489c-285e-496d-bd86-0d63d1be2efe"});
+        ImagesListReturn imagesListReturn = new ImagesListReturn(innerMap);
+        String[] expectedUUIDList = new String[]{"28e983b9-c2f6-4a50-a5d5-7a38089797c7",
+                                                 "c3d75234-b360-4aed-8d78-413d7488bd9b"};
+
+        assertArrayEquals(expectedUUIDList, imagesListReturn.getImageList());
+    }
+}
