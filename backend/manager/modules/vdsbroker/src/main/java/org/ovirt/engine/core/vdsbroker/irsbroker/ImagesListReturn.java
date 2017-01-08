3d62579bd6c8a9295902d450c1cad7096aa816f2
Benny Zlotnik <bzlotnik@redhat.com>
2017-01-08 15:07:00 +0200

    core: fix NumberFormatException when importing a removed VM
    When importing a VM while another is in the process of being
    removed a NumberFormatException will be thrown and the import
    will fail.
    
    This is caused by the removed VMs getting a "_remove_me_"
    prefix to its GUID when removed. Because of the prefix the
    method GetImagesListVDSCommand#executeIrsBrokerCommand fails
    when attempting to create an instance of the Guid object
    using the prefixed GUID of the removed VM.
    
    Fixed by filtering out the prefixed image GUIDs in
    ImagesListReturn constructor.
    
    Change-Id: I564fb9d0c08e6fabbf8ffd05cfdf93d5435a0e0b
    Bug-Url: https://bugzilla.redhat.com/1331335
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/ImagesListReturn.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/irsbroker/ImagesListReturn.java
@@ -1,23 +1,44 @@
 package org.ovirt.engine.core.vdsbroker.irsbroker;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class ImagesListReturn extends StatusReturn {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     private static final String IMAGES_LIST = "imageslist";
+
     private String[] imageList;
 
     public ImagesListReturn(Map<String, Object> innerMap) {
         super(innerMap);
         Object[] tempObj = (Object[]) innerMap.get(IMAGES_LIST);
+
         if (tempObj != null) {
-            imageList = new String[tempObj.length];
-            for (int i = 0; i < tempObj.length; i++) {
-                imageList[i] = (String) tempObj[i];
-            }
+            imageList = Arrays.stream(tempObj)
+                .map(String.class::cast)
+                .filter(this::isValidUUID)
+                .toArray(String[]::new);
         }
     }
 
     public String[] getImageList() {
         return imageList;
+    }
+
+    private boolean isValidUUID(String s) {
+        try {
+            UUID.fromString(s);
+        } catch (IllegalArgumentException e) {
+            log.warn("String '{}' is not a valid UUID", s);
+            return false;
+        }
+
+        return true;
     }
 }
