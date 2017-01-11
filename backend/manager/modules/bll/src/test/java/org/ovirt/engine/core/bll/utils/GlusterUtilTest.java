e4d2f6bfa30ea3e53cfac513f0e8d874c567f5e8
Allon Mureinik <amureini@redhat.com>
2017-01-11 17:22:17 +0200

    core: Remove GlusterUtilTest redundant mocking
    Remove redundant mocking that is not required by the test and does
    nothing but confuse the reader.
    
    Note that one the redundant mocks were removed, it became clear that
    EXPECTED_MAP was only used in a single method, so it was converted to a
    local variable.
    
    Change-Id: I7419ccb7afeaebee68ca3ffa69cfd212656ac907
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/utils/GlusterUtilTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/utils/GlusterUtilTest.java
@@ -31,7 +31,6 @@ public class GlusterUtilTest {
     private static final String WRONG_PASSWORD = "wrong_password";
     private static final String FINGER_PRINT1 = "31:e2:1b:7e:89:86:99:c3:f7:1e:57:35:fe:9b:5c:31";
     private static final String FINGER_PRINT2 = "31:e2:1b:7e:89:86:99:c3:f7:1e:57:35:fe:9b:5c:32";
-    private static final Map<String, String> EXPECTED_MAP = new HashMap<>();
     private static final String OUTPUT_XML =
             "<cliOutput><peerStatus><peer><uuid>85c42b0d-c2b7-424a-ae72-5174c25da40b</uuid><hostname>testserver1</hostname><connected>1</connected><state>3</state></peer>"
                     +
@@ -46,21 +45,20 @@ public class GlusterUtilTest {
 
     @Before
     public void setUp() throws Exception {
-        doReturn(client).when(glusterUtil).getSSHClient();
         doNothing().when(glusterUtil).connect(client, SERVER_NAME1, USER, WRONG_PASSWORD);
-        doReturn(FINGER_PRINT1).when(client).getHostFingerprint();
         doReturn(OUTPUT_XML).when(glusterUtil).executePeerStatusCommand(client);
         doThrow(AuthenticationException.class).when(glusterUtil).authenticate(client);
     }
 
     @Test
     public void testGetPeersWithFingerprint() throws AuthenticationException, IOException {
-        EXPECTED_MAP.put(SERVER_NAME1, FINGER_PRINT1);
-        EXPECTED_MAP.put(SERVER_NAME2, FINGER_PRINT2);
+        Map<String, String> expectedMap = new HashMap<>();
+        expectedMap.put(SERVER_NAME1, FINGER_PRINT1);
+        expectedMap.put(SERVER_NAME2, FINGER_PRINT2);
         doReturn(client).when(glusterUtil).getSSHClient();
         doNothing().when(glusterUtil).connect(client, SERVER_NAME1, USER, PASSWORD);
         doNothing().when(glusterUtil).authenticate(client);
-        doReturn(EXPECTED_MAP).when(glusterUtil).getFingerprints(anySetOf(String.class));
+        doReturn(expectedMap).when(glusterUtil).getFingerprints(anySetOf(String.class));
         Map<String, String> peers = glusterUtil.getPeers(SERVER_NAME1, USER, PASSWORD, FINGER_PRINT1);
         assertNotNull(peers);
         peers.containsKey(SERVER_NAME1);
@@ -71,12 +69,9 @@ public class GlusterUtilTest {
 
     @Test
     public void testGetPeers() throws AuthenticationException, IOException {
-        EXPECTED_MAP.put(SERVER_NAME1, FINGER_PRINT1);
-        EXPECTED_MAP.put(SERVER_NAME2, FINGER_PRINT2);
         doReturn(client).when(glusterUtil).getSSHClient();
         doNothing().when(glusterUtil).connect(client, SERVER_NAME1, USER, PASSWORD);
         doNothing().when(glusterUtil).authenticate(client);
-        doReturn(EXPECTED_MAP).when(glusterUtil).getFingerprints(anySetOf(String.class));
         Set<String> peers = glusterUtil.getPeers(SERVER_NAME1, USER, PASSWORD);
         assertNotNull(peers);
         assertTrue(peers.contains(SERVER_NAME1));
@@ -85,24 +80,18 @@ public class GlusterUtilTest {
 
     @Test(expected = AuthenticationException.class)
     public void testGetPeersWithWrongPassword() throws AuthenticationException, IOException {
-        EXPECTED_MAP.put(SERVER_NAME1, FINGER_PRINT1);
         doReturn(client).when(glusterUtil).getSSHClient();
-        doReturn(EXPECTED_MAP).when(glusterUtil).getFingerprints(anySetOf(String.class));
         glusterUtil.getPeers(SERVER_NAME1, USER, WRONG_PASSWORD);
     }
 
     @Test
     public void testHasPeersTrue() {
-        EXPECTED_MAP.put(SERVER_NAME1, FINGER_PRINT1);
-        doReturn(EXPECTED_MAP).when(glusterUtil).getFingerprints(anySetOf(String.class));
         assertNotNull(glusterUtil.getPeers(client));
         assertEquals(2, glusterUtil.getPeers(client).size());
     }
 
     @Test
     public void testHasPeersFalse() {
-        EXPECTED_MAP.put(SERVER_NAME1, FINGER_PRINT1);
-        doReturn(EXPECTED_MAP).when(glusterUtil).getFingerprints(anySetOf(String.class));
         doReturn(OUTPUT_XML_NO_PEERS).when(glusterUtil).executePeerStatusCommand(client);
         assertTrue(glusterUtil.getPeers(client).isEmpty());
     }
