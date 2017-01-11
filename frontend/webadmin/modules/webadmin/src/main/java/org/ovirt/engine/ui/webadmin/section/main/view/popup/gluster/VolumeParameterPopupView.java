649b7549ca8e6a07b888d4e16ffb43d046280e35
Ramesh Nachimuthu <rnachimu@redhat.com>
2017-01-11 18:02:04 +0530

    gluster: fix option listing issue in set options
    We were using a Combo box but it is not working
    as expected. Instead of Combo box, I moved this to
    ListModelTypeAheadChangeableListBoxEditor
    
    Change-Id: I09314f09096fb4be9cd81f63a456d1f1b0a45fdf
    Bug-Url: https://bugzilla.redhat.com/1408803
    Signed-off-by: Ramesh Nachimuthu <rnachimu@redhat.com>
    (cherry picked from commit f12f8509c639fb88836188630ad1071b5cf9a7b4)

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/gluster/VolumeParameterPopupView.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/gluster/VolumeParameterPopupView.java
@@ -1,20 +1,19 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.popup.gluster;
 
-import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeOptionInfo;
 import org.ovirt.engine.ui.common.editor.UiCommonEditorDriver;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.idhandler.WithElementId;
 import org.ovirt.engine.ui.common.view.popup.AbstractModelBoundPopupView;
-import org.ovirt.engine.ui.common.widget.ComboBox;
 import org.ovirt.engine.ui.common.widget.dialog.SimpleDialogPanel;
-import org.ovirt.engine.ui.common.widget.editor.ListModelListBoxEditor;
+import org.ovirt.engine.ui.common.widget.editor.ListModelTypeAheadChangeableListBoxEditor;
 import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextAreaLabelEditor;
 import org.ovirt.engine.ui.common.widget.editor.generic.StringEntityModelTextBoxEditor;
-import org.ovirt.engine.ui.common.widget.renderer.StringRenderer;
 import org.ovirt.engine.ui.uicommonweb.models.gluster.VolumeParameterModel;
 import org.ovirt.engine.ui.webadmin.ApplicationConstants;
+import org.ovirt.engine.ui.webadmin.ApplicationTemplates;
 import org.ovirt.engine.ui.webadmin.gin.AssetProvider;
 import org.ovirt.engine.ui.webadmin.section.main.presenter.popup.gluster.VolumeParameterPopupPresenterWidget;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -35,38 +34,26 @@ public class VolumeParameterPopupView extends AbstractModelBoundPopupView<Volume
         ViewIdHandler idHandler = GWT.create(ViewIdHandler.class);
     }
 
-    /*
-     * @UiField(provided = true)
-     *
-     * @Path(value = "keyList.selectedItem") ListModelListBoxEditor<Object> keyListEditor;
-     */
-
     @UiField(provided = true)
-    @WithElementId("keyComboBox")
-    ComboBox<GlusterVolumeOptionInfo> keyComboBox;
-
     @Path(value = "keyList.selectedItem")
     @WithElementId("keyListBox")
-    ListModelListBoxEditor<GlusterVolumeOptionInfo> keyListBoxEditor;
-
-    @Path(value = "selectedKey.entity")
-    @WithElementId("keyTextBox")
-    StringEntityModelTextBoxEditor keyTextBoxEditor;
+    public ListModelTypeAheadChangeableListBoxEditor keyListBoxEditor;
 
     @UiField
     @Path(value = "description.entity")
-    StringEntityModelTextAreaLabelEditor descriptionEditor;
+    public StringEntityModelTextAreaLabelEditor descriptionEditor;
 
     @UiField
     @Path(value = "value.entity")
-    StringEntityModelTextBoxEditor valueEditor;
+    public StringEntityModelTextBoxEditor valueEditor;
 
     @UiField
     @Ignore
-    Label messageLabel;
+    public Label messageLabel;
 
     private final Driver driver = GWT.create(Driver.class);
 
+    private static final ApplicationTemplates templates = AssetProvider.getTemplates();
     private static final ApplicationConstants constants = AssetProvider.getConstants();
 
     @Inject
@@ -86,20 +73,22 @@ public class VolumeParameterPopupView extends AbstractModelBoundPopupView<Volume
     }
 
     void initComboBox() {
-        keyListBoxEditor = new ListModelListBoxEditor<>(new StringRenderer<GlusterVolumeOptionInfo>() {
-            @Override
-            public String render(GlusterVolumeOptionInfo optionInfo) {
-                if (optionInfo != null) {
-                    return optionInfo.getKey();
-                }
-                return null;
-            }
-        });
-        keyTextBoxEditor = new StringEntityModelTextBoxEditor();
-
-        keyComboBox = new ComboBox<>(keyListBoxEditor, keyTextBoxEditor);
+        keyListBoxEditor = new ListModelTypeAheadChangeableListBoxEditor(
+                new ListModelTypeAheadChangeableListBoxEditor.NullSafeSuggestBoxRenderer() {
+                    @Override
+                    public String getDisplayStringNullSafe(String data) {
+                        return typeAheadNameTemplateNullSafe(data);
+                    }
+                });
     }
 
+    private String typeAheadNameTemplateNullSafe(String name) {
+        if (name != null && !name.trim().isEmpty()) {
+            return templates.typeAheadName(name).asString();
+        } else {
+            return templates.typeAheadEmptyContent().asString();
+        }
+    }
     @Override
     public void edit(final VolumeParameterModel object) {
         driver.edit(object);
