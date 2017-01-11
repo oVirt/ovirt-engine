a1c979d285add578629e0317af63d0477e1b2c22
Alexander Wels <awels@redhat.com>
2017-01-11 10:21:22 -0500

    webadmin: Update QoS popup layout
    - Updated QoS popups layout to be bootstrap based
    
    Change-Id: Ib8ce6672ed9a4b827358b8abeec8b37185e8af1f
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/networkQoS/NetworkQosWidget.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/networkQoS/NetworkQosWidget.java
@@ -1,5 +1,6 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.popup.networkQoS;
 
+import org.gwtbootstrap3.client.ui.Container;
 import org.ovirt.engine.ui.common.editor.UiCommonEditorDriver;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.idhandler.WithElementId;
@@ -11,13 +12,9 @@ import org.ovirt.engine.ui.uicommonweb.models.datacenters.BaseNetworkQosModel;
 import org.ovirt.engine.ui.uicompat.Event;
 import org.ovirt.engine.ui.uicompat.IEventListener;
 import org.ovirt.engine.ui.uicompat.PropertyChangedEventArgs;
-import org.ovirt.engine.ui.webadmin.ApplicationConstants;
-import org.ovirt.engine.ui.webadmin.gin.AssetProvider;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.FlowPanel;
 
 public class NetworkQosWidget extends AbstractModelBoundPopupWidget<BaseNetworkQosModel> {
 
@@ -26,7 +23,7 @@ public class NetworkQosWidget extends AbstractModelBoundPopupWidget<BaseNetworkQ
 
     private final Driver driver = GWT.create(Driver.class);
 
-    interface ViewUiBinder extends UiBinder<FlowPanel, NetworkQosWidget> {
+    interface ViewUiBinder extends UiBinder<Container, NetworkQosWidget> {
         ViewUiBinder uiBinder = GWT.create(ViewUiBinder.class);
     }
 
@@ -34,15 +31,8 @@ public class NetworkQosWidget extends AbstractModelBoundPopupWidget<BaseNetworkQ
         ViewIdHandler idHandler = GWT.create(ViewIdHandler.class);
     }
 
-    interface WidgetStyle extends CssResource {
-        String valueWidth();
-    }
-
     @UiField
-    WidgetStyle style;
-
-    @UiField
-    FlowPanel mainPanel;
+    Container mainContainer;
 
     @UiField(provided = true)
     @Path(value = "inbound.enabled.entity")
@@ -87,16 +77,12 @@ public class NetworkQosWidget extends AbstractModelBoundPopupWidget<BaseNetworkQ
     private BaseNetworkQosModel model;
     private final IEventListener<PropertyChangedEventArgs> availabilityListener;
 
-    private static final ApplicationConstants constants = AssetProvider.getConstants();
-
     public NetworkQosWidget() {
         inboundEnabled = new EntityModelCheckBoxEditor(Align.RIGHT);
         outboundEnabled = new EntityModelCheckBoxEditor(Align.RIGHT);
         initWidget(ViewUiBinder.uiBinder.createAndBindUi(this));
         ViewIdHandler.idHandler.generateAndSetIds(this);
 
-        setStyle();
-        localize();
         driver.initialize(this);
 
         availabilityListener = new IEventListener<PropertyChangedEventArgs>() {
@@ -110,28 +96,8 @@ public class NetworkQosWidget extends AbstractModelBoundPopupWidget<BaseNetworkQ
         };
     }
 
-    private void setStyle() {
-        inboundAverageEditor.setContentWidgetContainerStyleName(style.valueWidth());
-        inboundPeakEditor.setContentWidgetContainerStyleName(style.valueWidth());
-        inboundBurstEditor.setContentWidgetContainerStyleName(style.valueWidth());
-        outboundAverageEditor.setContentWidgetContainerStyleName(style.valueWidth());
-        outboundPeakEditor.setContentWidgetContainerStyleName(style.valueWidth());
-        outboundBurstEditor.setContentWidgetContainerStyleName(style.valueWidth());
-    }
-
-    private void localize() {
-        inboundEnabled.setLabel(constants.inboundLabelQoSPopup());
-        outboundEnabled.setLabel(constants.outboundLabelQoSPopup());
-        inboundAverageEditor.setTitle(constants.averageNetworkQoSPopup() + constants.inMegabitsNetworkQoSPopup());
-        inboundPeakEditor.setTitle(constants.peakNetworkQoSPopup() + constants.inMegabitsNetworkQoSPopup());
-        inboundBurstEditor.setTitle(constants.burstNetworkQoSPopup() + constants.inMegabytesNetworkQoSPopup());
-        outboundAverageEditor.setTitle(constants.averageNetworkQoSPopup() + constants.inMegabitsNetworkQoSPopup());
-        outboundPeakEditor.setTitle(constants.peakNetworkQoSPopup() + constants.inMegabitsNetworkQoSPopup());
-        outboundBurstEditor.setTitle(constants.burstNetworkQoSPopup() + constants.inMegabytesNetworkQoSPopup());
-    }
-
     private void toggleVisibility() {
-        mainPanel.setVisible(model.getIsAvailable());
+        mainContainer.setVisible(model.getIsAvailable());
     }
 
     @Override
--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/qos/CpuQosWidget.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/view/popup/qos/CpuQosWidget.java
@@ -1,24 +1,22 @@
 package org.ovirt.engine.ui.webadmin.section.main.view.popup.qos;
 
+import org.gwtbootstrap3.client.ui.Container;
 import org.ovirt.engine.core.common.businessentities.qos.CpuQos;
 import org.ovirt.engine.ui.common.editor.UiCommonEditorDriver;
 import org.ovirt.engine.ui.common.idhandler.ElementIdHandler;
 import org.ovirt.engine.ui.common.idhandler.WithElementId;
 import org.ovirt.engine.ui.common.widget.editor.generic.IntegerEntityModelTextBoxEditor;
 import org.ovirt.engine.ui.uicommonweb.models.datacenters.qos.CpuQosParametersModel;
-import org.ovirt.engine.ui.webadmin.ApplicationConstants;
-import org.ovirt.engine.ui.webadmin.gin.AssetProvider;
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.FlowPanel;
 
 public class CpuQosWidget extends QosWidget<CpuQos, CpuQosParametersModel> {
 
     interface Driver extends UiCommonEditorDriver<CpuQosParametersModel, CpuQosWidget> {
     }
 
-    interface ViewUiBinder extends UiBinder<FlowPanel, CpuQosWidget> {
+    interface ViewUiBinder extends UiBinder<Container, CpuQosWidget> {
         ViewUiBinder uiBinder = GWT.create(ViewUiBinder.class);
     }
 
@@ -31,13 +29,9 @@ public class CpuQosWidget extends QosWidget<CpuQos, CpuQosParametersModel> {
     @WithElementId
     IntegerEntityModelTextBoxEditor cpuLimitEditor;
 
-    private static final ApplicationConstants constants = AssetProvider.getConstants();
-
     public CpuQosWidget() {
         initWidget(ViewUiBinder.uiBinder.createAndBindUi(this));
         ViewIdHandler.idHandler.generateAndSetIds(this);
-
-        cpuLimitEditor.setLabel(constants.cpuQosCpuLimit());
 
         driver = GWT.create(Driver.class);
         driver.initialize(this);
