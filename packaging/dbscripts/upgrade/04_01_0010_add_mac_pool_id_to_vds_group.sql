cf51b305a9624f6a74a5f20c40480f73081efdb5
Martin Mucha <mmucha@redhat.com>
2017-01-12 14:00:43 +0100

    core: use default mac pool for clusters without DC
    There might exist clusters, which are not linked to any data center.
    For those first update statement does not work, leaving possible null
    values in cluster.mac_pool_id column, and because of that adding of
    constraint will fail.
    
    We have to select some mac pool to be used for such clusters, and
    having no information which should be used, we can use only default
    mac pool.
    
    Change-Id: If33a212d6c8f730bae8ebe46bc1d6359e95c0495
    Bug-Url: https://bugzilla.redhat.com/1410189
    Signed-off-by: Martin Mucha <mmucha@redhat.com>

--- packaging/dbscripts/upgrade/04_01_0010_add_mac_pool_id_to_vds_group.sql
+++ packaging/dbscripts/upgrade/04_01_0010_add_mac_pool_id_to_vds_group.sql
@@ -14,4 +14,17 @@ SET
         sp.id = c.storage_pool_id
     );
 
+UPDATE
+  cluster AS c
+SET
+  mac_pool_id = (
+  SELECT
+    mp.id
+  FROM
+    mac_pools mp
+  WHERE
+    mp.default_pool IS TRUE
+)
+WHERE c.mac_pool_id IS NULL;
+
 ALTER TABLE cluster ALTER COLUMN mac_pool_id SET NOT NULL;
