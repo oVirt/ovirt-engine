e16f8f023e9af3aa829d46b28611f4ff412da893
Allon Mureinik <amureini@redhat.com>
2017-01-13 05:47:32 +0200

    core: ScanStorageForUnregistedDisksCommandTest unused code
    generateQueryReturnValueForGetDiskImages was only ever called with
    succeeded=true, making this argument useless. Inlining it then revealed
    several code paths that were never used, so they could safely be
    removed.
    
    Change-Id: Ic411891e3c9c98af69c55dc33687529f8c19a5b4
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/storage/disk/image/ScanStorageForUnregisteredDisksCommandTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/storage/disk/image/ScanStorageForUnregisteredDisksCommandTest.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -19,19 +18,13 @@ import org.mockito.Spy;
 import org.ovirt.engine.core.bll.BaseCommandTest;
 import org.ovirt.engine.core.common.action.StorageDomainParametersBase;
 import org.ovirt.engine.core.common.businessentities.OvfEntityData;
-import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.common.queries.VdcQueryReturnValue;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.DiskImageDao;
-import org.ovirt.engine.core.dao.UnregisteredDisksDao;
 import org.ovirt.engine.core.dao.UnregisteredOVFDataDao;
 import org.ovirt.engine.core.utils.ovf.OvfReaderException;
 
 public class ScanStorageForUnregisteredDisksCommandTest extends BaseCommandTest {
-
-    @Mock
-    private UnregisteredDisksDao unregisteredDisksDaoMock;
-
     @Mock
     private UnregisteredOVFDataDao unregisteredOVFDataDaoMock;
 
@@ -44,30 +37,12 @@ public class ScanStorageForUnregisteredDisksCommandTest extends BaseCommandTest 
             new ScanStorageForUnregisteredDisksCommand<>
                     (new StorageDomainParametersBase(Guid.newGuid(), Guid.newGuid()), null);
 
-    private DiskImage diskImageForTest = new DiskImage();
     private Guid storageId = Guid.newGuid();
 
-    private VdcQueryReturnValue generateQueryReturnValueForGetDiskImages(boolean succeeded,
-            List<DiskImage> disksFromStorage,
-            String exceptionString) {
+    private VdcQueryReturnValue generateQueryReturnValueForGetDiskImages() {
         VdcQueryReturnValue vdcQueryReturnValue = new VdcQueryReturnValue();
-        vdcQueryReturnValue.setSucceeded(succeeded);
-        if (!succeeded) {
-            vdcQueryReturnValue.setReturnValue(disksFromStorage);
-        } else {
-            vdcQueryReturnValue.setExceptionString(exceptionString);
-        }
+        vdcQueryReturnValue.setSucceeded(true);
         return vdcQueryReturnValue;
-    }
-
-    private void createDiskForTest(DiskImage diskImage) {
-        diskImage.setDiskAlias("Disk Alias");
-        diskImage.setDiskDescription("Disk Description");
-        diskImage.setSizeInGigabytes(10L * 1024 * 1024 * 1024);
-        diskImage.setActualSize(10L * 1024 * 1024 * 1024);
-        diskImage.setId(Guid.newGuid());
-        diskImage.setImageId(Guid.newGuid());
-        diskImage.setStorageIds(new ArrayList<>(Collections.singletonList(storageId)));
     }
 
     @Test
@@ -78,11 +53,7 @@ public class ScanStorageForUnregisteredDisksCommandTest extends BaseCommandTest 
 
     @Before
     public void setUpCommandEntities() throws OvfReaderException {
-        List<DiskImage> disksFromStorage = new ArrayList<>();
-        createDiskForTest(diskImageForTest);
-        disksFromStorage.add(diskImageForTest);
-
-        VdcQueryReturnValue vdcRetVal = generateQueryReturnValueForGetDiskImages(true, disksFromStorage, "Exception");
+        VdcQueryReturnValue vdcRetVal = generateQueryReturnValueForGetDiskImages();
         doReturn(vdcRetVal).when(cmd).getUnregisteredDisksFromHost();
 
         List<OvfEntityData> allEntities = new ArrayList<>();
