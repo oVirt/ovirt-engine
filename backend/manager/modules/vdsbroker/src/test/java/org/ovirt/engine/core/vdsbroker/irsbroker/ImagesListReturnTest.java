4a21305a3966fc11775804c7c7fefefe5b99cb50
Benny Zlotnik <bzlotnik@redhat.com>
2017-01-20 12:48:16 +0200

    core: use singletonMap instead new HashMap
    Since there is only a single entry there is no point
    having an entire HashMap
    
    Change-Id: Ibb0fc4a8bf89eb24ecc0ede0192daec534f278f0
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/irsbroker/ImagesListReturnTest.java
+++ backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/irsbroker/ImagesListReturnTest.java
@@ -2,7 +2,7 @@ package org.ovirt.engine.core.vdsbroker.irsbroker;
 
 import static org.junit.Assert.assertArrayEquals;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 import org.junit.Test;
@@ -13,10 +13,11 @@ public class ImagesListReturnTest {
 
     @Test
     public void validateBadUUIDSFiltered() {
-        Map<String, Object> innerMap = new HashMap<>();
-        innerMap.put(IMAGES_LIST, new Object[]{"28e983b9-c2f6-4a50-a5d5-7a38089797c7",
-                                               "c3d75234-b360-4aed-8d78-413d7488bd9b",
-                                               "_remove_me_2f17489c-285e-496d-bd86-0d63d1be2efe"});
+        Map<String, Object> innerMap =
+                Collections.singletonMap(IMAGES_LIST,
+                    new Object[]{"28e983b9-c2f6-4a50-a5d5-7a38089797c7",
+                                 "c3d75234-b360-4aed-8d78-413d7488bd9b",
+                                 "_remove_me_2f17489c-285e-496d-bd86-0d63d1be2efe"});
         ImagesListReturn imagesListReturn = new ImagesListReturn(innerMap);
         String[] expectedUUIDList = new String[]{"28e983b9-c2f6-4a50-a5d5-7a38089797c7",
                                                  "c3d75234-b360-4aed-8d78-413d7488bd9b"};
