a7482152127eafc6217bbd196d87b297d9661833
Daniel Erez <derez@redhat.com>
2017-01-22 19:10:51 +0200

    core: add a test to getVmDeviceUnitMapForScsiDisks
    Added a test for getVmDeviceUnitMapForScsiDisks (VmInfoBuildUtilsTest).
    The test ensures that the SCSI address unit index of the boot disk is
    lower than the non-boot disk.
    
    Change-Id: Id87e1b7a06e6327c6033d41088e6712807a85138
    Bug-Url: https://bugzilla.redhat.com/??????
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/builder/vminfo/VmInfoBuildUtilsTest.java
+++ backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/builder/vminfo/VmInfoBuildUtilsTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 import static org.ovirt.engine.core.vdsbroker.vdsbroker.IoTuneUtils.MB_TO_BYTES;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,13 +18,21 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VmDevice;
+import org.ovirt.engine.core.common.businessentities.VmDeviceGeneralType;
+import org.ovirt.engine.core.common.businessentities.VmDeviceId;
 import org.ovirt.engine.core.common.businessentities.network.NetworkFilter;
 import org.ovirt.engine.core.common.businessentities.network.VmNic;
 import org.ovirt.engine.core.common.businessentities.network.VmNicFilterParameter;
 import org.ovirt.engine.core.common.businessentities.network.VnicProfile;
 import org.ovirt.engine.core.common.businessentities.qos.StorageQos;
+import org.ovirt.engine.core.common.businessentities.storage.Disk;
 import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
+import org.ovirt.engine.core.common.businessentities.storage.DiskInterface;
+import org.ovirt.engine.core.common.businessentities.storage.DiskVmElement;
+import org.ovirt.engine.core.common.businessentities.storage.LunDisk;
+import org.ovirt.engine.core.common.utils.VmDeviceType;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.VmDeviceDao;
 import org.ovirt.engine.core.dao.network.NetworkDao;
@@ -47,6 +56,9 @@ public class VmInfoBuildUtilsTest {
     private static final String NETWORK_FILTER_PARAMETER_0_VALUE = "10.0.0.1";
     private static final String NETWORK_FILTER_PARAMETER_1_NAME = "IP";
     private static final String NETWORK_FILTER_PARAMETER_1_VALUE = "10.0.0.2";
+    private static final Guid VM_ID = Guid.newGuid();
+    private static final Guid DISK_IMAGE_ID = Guid.newGuid();
+    private static final Guid LUN_DISK_ID = Guid.newGuid();
 
     @Mock
     private NetworkDao networkDao;
@@ -170,6 +182,75 @@ public class VmInfoBuildUtilsTest {
         assertEquals(NETWORK_FILTER_PARAMETER_0_VALUE, parametersList.get(0).get("value"));
         assertEquals(NETWORK_FILTER_PARAMETER_1_NAME, parametersList.get(1).get("name"));
         assertEquals(NETWORK_FILTER_PARAMETER_1_VALUE, parametersList.get(1).get("value"));
+    }
+
+    private Map<Guid, Disk> mockUnsortedDisksMap(VmDevice lunDiskVmDevice, VmDevice diskImageVmDevice) {
+        when(vmDeviceDao.get(lunDiskVmDevice.getId())).thenReturn(lunDiskVmDevice);
+        when(vmDeviceDao.get(diskImageVmDevice.getId())).thenReturn(diskImageVmDevice);
+
+        DiskVmElement nonBootDiskVmElement = new DiskVmElement(lunDiskVmDevice.getId());
+        nonBootDiskVmElement.setBoot(false);
+        nonBootDiskVmElement.setDiskInterface(DiskInterface.VirtIO_SCSI);
+
+        DiskVmElement bootDiskVmElement = new DiskVmElement(diskImageVmDevice.getId());
+        bootDiskVmElement.setBoot(true);
+        bootDiskVmElement.setDiskInterface(DiskInterface.VirtIO_SCSI);
+
+        LunDisk lunDisk = new LunDisk();
+        lunDisk.setId(LUN_DISK_ID);
+        lunDisk.setDiskVmElements(Collections.singleton(nonBootDiskVmElement));
+
+        DiskImage diskImage = new DiskImage();
+        diskImage.setId(DISK_IMAGE_ID);
+        diskImage.setDiskVmElements(Collections.singleton(bootDiskVmElement));
+
+        Map<Guid, Disk> map = new HashMap<>();
+        map.put(lunDisk.getId(), lunDisk);
+        map.put(diskImage.getId(), diskImage);
+
+        return map;
+    }
+
+    @Test
+    public void testGetVmDeviceUnitMapForScsiDisks() {
+        VmDevice lunDiskVmDevice = new VmDevice(new VmDeviceId(LUN_DISK_ID, VM_ID),
+                VmDeviceGeneralType.DISK,
+                VmDeviceType.DISK.getName(),
+                "",
+                1,
+                null,
+                true,
+                true,
+                null,
+                "",
+                null,
+                null,
+                null);
+
+        VmDevice diskImageVmDevice = new VmDevice(new VmDeviceId(DISK_IMAGE_ID, VM_ID),
+                VmDeviceGeneralType.DISK,
+                VmDeviceType.DISK.getName(),
+                "",
+                0,
+                null,
+                true,
+                true,
+                null,
+                "",
+                null,
+                null,
+                null);
+
+        VM vm = new VM();
+        vm.setId(VM_ID);
+        vm.setDiskMap(mockUnsortedDisksMap(lunDiskVmDevice, diskImageVmDevice));
+
+        Map<VmDevice, Integer> vmDeviceUnitMap =
+                underTest.getVmDeviceUnitMapForScsiDisks(vm, DiskInterface.VirtIO_SCSI, false);
+
+        // Ensures that the boot disk unit is lower
+        assertEquals(vmDeviceUnitMap.get(lunDiskVmDevice), (Integer) 1);
+        assertEquals(vmDeviceUnitMap.get(diskImageVmDevice), (Integer) 0);
     }
 
 }
