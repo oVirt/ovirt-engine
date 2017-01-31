d13d1daeab373087de2b4f6e0423b0e1579ec3f4
Yevgeny Zaspitsky <yzaspits@redhat.com>
2017-01-31 13:15:18 +0200

    engine: refactor HostConnectivityChecker
    Calculate the absolute timeout time and compare the current time to it
    instead of calculating number of polling attepts in beforehand.
    
    Change-Id: I15f186830d363f875a478dcfab21b763d301ccf4
    Bug-Url: https://bugzilla.redhat.com/1420239
    Signed-off-by: Yevgeny Zaspitsky <yzaspits@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/host/HostConnectivityChecker.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/host/HostConnectivityChecker.java
@@ -1,23 +1,23 @@
 package org.ovirt.engine.core.bll.host;
 
+import java.util.concurrent.TimeUnit;
+
 import org.ovirt.engine.core.bll.network.host.HostSetupNetworkPoller;
 import org.ovirt.engine.core.common.businessentities.VDS;
-import org.ovirt.engine.core.common.config.Config;
-import org.ovirt.engine.core.common.config.ConfigValues;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HostConnectivityChecker {
 
     private static Logger log = LoggerFactory.getLogger(HostConnectivityChecker.class);
-    private static final int VDSM_RESPONSIVENESS_PERIOD_IN_SECONDS = 120;
+    private static final long VDSM_RESPONSIVENESS_PERIOD_IN_SECONDS = 120;
+    private static final long VDSM_RESPONSIVENESS_PERIOD_IN_NANOS =
+            TimeUnit.SECONDS.toNanos(VDSM_RESPONSIVENESS_PERIOD_IN_SECONDS);
 
     public boolean check(final VDS host) {
-        final int checks =
-                VDSM_RESPONSIVENESS_PERIOD_IN_SECONDS
-                        / Config.<Integer> getValue(ConfigValues.SetupNetworksPollingTimeout);
         HostSetupNetworkPoller poller = new HostSetupNetworkPoller();
-        for (int i = 0; i < checks; i++) {
+        final long startTime = System.nanoTime();
+        while (System.nanoTime() - startTime < VDSM_RESPONSIVENESS_PERIOD_IN_NANOS) {
             if (poller.poll(host.getId())) {
                 log.info("Engine managed to communicate with VDSM agent on host '{}' with address '{}' ('{}')",
                         host.getName(),
