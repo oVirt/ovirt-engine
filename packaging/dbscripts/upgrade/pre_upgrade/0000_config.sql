9140dadf163010f9f035a1a346e593b982219986
Martin Perina <mperina@redhat.com>
2017-02-10 16:17:12 +0100

    core: Allow TLSv1.2 for external provider communication
    Allows to use TLSv1.2 for protocol negotiation for external provider
    communication.
    
    Change-Id: I96e22228e5780ad0e63b70fb3d7dcda31c53e41d
    Bug-Url: https://bugzilla.redhat.com/1421204
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
+++ packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -467,7 +467,7 @@ select fn_db_add_config_value('DelayResetPerVmInSeconds','0.5','general');
 --Handling Use Secure Connection with Hosts
 select fn_db_add_config_value('EncryptHostCommunication','true','general');
 select fn_db_add_config_value('VdsmSSLProtocol','TLS','general');
-select fn_db_add_config_value('ExternalCommunicationProtocol','TLSv1','general');
+select fn_db_add_config_value('ExternalCommunicationProtocol','TLSv1.2','general');
 select fn_db_add_config_value('VdsRequestQueueName','jms.topic.vdsm_requests','general');
 select fn_db_add_config_value('VdsResponseQueueName','jms.topic.vdsm_responses','general');
 select fn_db_add_config_value('IrsRequestQueueName','jms.topic.vdsm_irs_requests','general');
@@ -850,7 +850,8 @@ select fn_db_update_default_config_value('vdsRetries', '3', '0', 'general', fals
 select fn_db_update_default_config_value('VdsmSSLProtocol','SSLv3','TLSv1','general', false);
 select fn_db_update_default_config_value('VdsmSSLProtocol','TLSv1','TLS','general',false);
 select fn_db_update_default_config_value('VdsmSSLProtocol','TLS','TLSv1.2','general',false);
-select fn_db_update_default_config_value('ExternalCommunicationProtocol','SSLv3','TLSv1','general', false);
+select fn_db_update_default_config_value('ExternalCommunicationProtocol','SSLv3','TLSv1.2','general', false);
+select fn_db_update_default_config_value('ExternalCommunicationProtocol','TLSv1','TLSv1.2','general', false);
 
 -- Adding no-TSX Intel processors in 3.6
 select fn_db_update_default_config_value('ServerCPUList', '3:Intel Conroe Family:vmx,nx,model_Conroe:Conroe:x86_64; 4:Intel Penryn Family:vmx,nx,model_Penryn:Penryn:x86_64; 5:Intel Nehalem Family:vmx,nx,model_Nehalem:Nehalem:x86_64; 6:Intel Westmere Family:aes,vmx,nx,model_Westmere:Westmere:x86_64; 7:Intel SandyBridge Family:vmx,nx,model_SandyBridge:SandyBridge:x86_64; 8:Intel Haswell-noTSX Family:vmx,nx,model_Haswell-noTSX:Haswell-noTSX:x86_64; 9:Intel Haswell Family:vmx,nx,model_Haswell:Haswell:x86_64; 10:Intel Broadwell-noTSX Family:vmx,nx,model_Broadwell-noTSX:Broadwell-noTSX:x86_64; 11:Intel Broadwell Family:vmx,nx,model_Broadwell:Broadwell:x86_64; 2:AMD Opteron G1:svm,nx,model_Opteron_G1:Opteron_G1:x86_64; 3:AMD Opteron G2:svm,nx,model_Opteron_G2:Opteron_G2:x86_64; 4:AMD Opteron G3:svm,nx,model_Opteron_G3:Opteron_G3:x86_64; 5:AMD Opteron G4:svm,nx,model_Opteron_G4:Opteron_G4:x86_64; 6:AMD Opteron G5:svm,nx,model_Opteron_G5:Opteron_G5:x86_64; 3:IBM POWER 8:powernv,model_power8:power8:ppc64; 4:IBM POWER 8E:powernv,model_power8e:power8e:ppc64;', '3:Intel Conroe Family:vmx,nx,model_Conroe:Conroe:x86_64; 4:Intel Penryn Family:vmx,nx,model_Penryn:Penryn:x86_64; 5:Intel Nehalem Family:vmx,nx,model_Nehalem:Nehalem:x86_64; 6:Intel Westmere Family:aes,vmx,nx,model_Westmere:Westmere:x86_64; 7:Intel SandyBridge Family:vmx,nx,model_SandyBridge:SandyBridge:x86_64; 8:Intel Haswell-noTSX Family:vmx,nx,model_Haswell-noTSX:Haswell-noTSX:x86_64; 9:Intel Haswell Family:vmx,nx,model_Haswell:Haswell:x86_64; 10:Intel Broadwell-noTSX Family:vmx,nx,model_Broadwell-noTSX:Broadwell-noTSX:x86_64; 11:Intel Broadwell Family:vmx,nx,model_Broadwell:Broadwell:x86_64; 2:AMD Opteron G1:svm,nx,model_Opteron_G1:Opteron_G1:x86_64; 3:AMD Opteron G2:svm,nx,model_Opteron_G2:Opteron_G2:x86_64; 4:AMD Opteron G3:svm,nx,model_Opteron_G3:Opteron_G3:x86_64; 5:AMD Opteron G4:svm,nx,model_Opteron_G4:Opteron_G4:x86_64; 6:AMD Opteron G5:svm,nx,model_Opteron_G5:Opteron_G5:x86_64; 3:IBM POWER8:powernv,model_POWER8:POWER8:ppc64;', '3.6', FALSE);
