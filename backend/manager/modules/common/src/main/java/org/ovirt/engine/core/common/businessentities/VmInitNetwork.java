a98e4cf9931b5911419ebb6f7eaa325ce12bd5e4
Yevgeny Zaspitsky <yzaspits@redhat.com>
2017-02-13 18:18:29 +0200

    engine: add ipv6 properties to VmInitNetwork
    Change-Id: I313b0709fcc0037ffa4d564f0b516a47b3db218f
    Bug-Url: https://bugzilla.redhat.com/1330217
    Signed-off-by: Yevgeny Zaspitsky <yzaspits@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmInitNetwork.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmInitNetwork.java
@@ -3,20 +3,29 @@ package org.ovirt.engine.core.common.businessentities;
 import java.io.Serializable;
 
 import org.ovirt.engine.core.common.businessentities.network.Ipv4BootProtocol;
+import org.ovirt.engine.core.common.businessentities.network.Ipv6BootProtocol;
 import org.ovirt.engine.core.compat.Guid;
 
 public class VmInitNetwork implements Serializable, BusinessEntity<Guid> {
     private static final long serialVersionUID = 1257388375049806155L;
     private Boolean startOnBoot;
     private String name;
+
     private Ipv4BootProtocol bootProtocol;
     private String ip;
     private String netmask;
     private String gateway;
+
+    private Ipv6BootProtocol ipv6BootProtocol;
+    private String ipv6Address;
+    private Integer ipv6Prefix;
+    private String ipv6Gateway;
+
     private Guid id;
 
     public VmInitNetwork() {
         this.bootProtocol = Ipv4BootProtocol.NONE;
+        this.ipv6BootProtocol = Ipv6BootProtocol.NONE;
     }
 
     public Guid getId() {
@@ -62,5 +71,29 @@ public class VmInitNetwork implements Serializable, BusinessEntity<Guid> {
     }
     public void setGateway(String gateway) {
         this.gateway = gateway;
+    }
+    public Ipv6BootProtocol getIpv6BootProtocol() {
+        return ipv6BootProtocol;
+    }
+    public void setIpv6BootProtocol(Ipv6BootProtocol ipv6BootProtocol) {
+        this.ipv6BootProtocol = (ipv6BootProtocol == null) ? Ipv6BootProtocol.NONE : ipv6BootProtocol;
+    }
+    public String getIpv6Address() {
+        return ipv6Address;
+    }
+    public void setIpv6Address(String ipv6Address) {
+        this.ipv6Address = ipv6Address;
+    }
+    public Integer getIpv6Prefix() {
+        return ipv6Prefix;
+    }
+    public void setIpv6Prefix(Integer ipv6Prefix) {
+        this.ipv6Prefix = ipv6Prefix;
+    }
+    public String getIpv6Gateway() {
+        return ipv6Gateway;
+    }
+    public void setIpv6Gateway(String ipv6Gateway) {
+        this.ipv6Gateway = ipv6Gateway;
     }
 }
