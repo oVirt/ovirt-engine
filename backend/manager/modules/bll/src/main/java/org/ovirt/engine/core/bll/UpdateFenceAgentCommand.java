0ce5b2e5c89d4f38c049ef88e14f0fdc21e04da6
Aleksei Slaikovskii <aslaikov@redhat.com>
2017-02-13 13:52:45 +0100

    core: Validate fence agent type upon creation/update
    Check if fence agent type is valid for the host upon creation/update
    of fence agent. Checks are based on valid fence agent types per cluster
    level, which the host is part of.
    
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1420302
    
    Change-Id: Iecda6f02f96c6696ba01421857db05a32d8a3871
    Signed-off-by: Aleksei Slaikovskii <aslaikov@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateFenceAgentCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateFenceAgentCommand.java
@@ -1,13 +1,47 @@
 package org.ovirt.engine.core.bll;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.context.CommandContext;
 import org.ovirt.engine.core.common.action.FenceAgentCommandParameterBase;
+import org.ovirt.engine.core.common.businessentities.Cluster;
+import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.pm.FenceAgent;
+import org.ovirt.engine.core.common.errors.EngineMessage;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.ClusterDao;
+import org.ovirt.engine.core.dao.VdsDao;
+import org.ovirt.engine.core.utils.pm.FenceConfigHelper;
 
 public class UpdateFenceAgentCommand<T extends FenceAgentCommandParameterBase> extends FenceAgentCommandBase {
+    @Inject
+    private VdsDao vdsDao;
+
+    @Inject
+    private ClusterDao clusterDao;
 
     public UpdateFenceAgentCommand(FenceAgentCommandParameterBase parameters, CommandContext cmdContext) {
         super(parameters, cmdContext);
+    }
+
+    @Override
+    protected boolean validate() {
+        if (getParameters() != null
+                && getParameters().getAgent() != null
+                && getParameters().getAgent().getHostId() != null
+                && getParameters().getAgent().getType() != null) {
+            Guid vdsId = getParameters().getAgent().getHostId();
+            VDS vds = vdsDao.get(vdsId);
+            Guid vdsClusterId = vds.getClusterId();
+            Cluster cluster = clusterDao.get(vdsClusterId);
+            String clusterCompatibilityVersion = cluster.getCompatibilityVersion().toString();
+
+            if (!FenceConfigHelper.getValidFenceAgentTypes(clusterCompatibilityVersion)
+                    .contains(getParameters().getAgent().getType())) {
+                return failValidation(EngineMessage.ACTION_TYPE_FAILED_AGENT_NOT_SUPPORTED);
+            }
+        }
+        return super.validate();
     }
 
     @Override
