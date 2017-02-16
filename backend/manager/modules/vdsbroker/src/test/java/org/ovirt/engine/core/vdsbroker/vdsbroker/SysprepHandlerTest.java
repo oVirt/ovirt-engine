24e315d86d8592cb7837c3927251711d5d96b830
Tomas Jelinek <tjelinek@redhat.com>
2017-02-16 12:24:17 +0100

    core: remove the ProductKey section if product key is not provided
    The reason is that the sysprep operation fails if the product key is empty.
    
    Change-Id: I70c8bb82d28c20d338a0b04466a5e200fc558194
    Bug-Url: https://bugzilla.redhat.com/1419562
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>
    (cherry picked from commit 63262640829d5f3c242dfc15c0f788fe904eccd7)

--- backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/vdsbroker/SysprepHandlerTest.java
+++ backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/vdsbroker/SysprepHandlerTest.java
@@ -2,11 +2,58 @@ package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 public class SysprepHandlerTest {
+
+    private String sysprepFile = "<component name=\"Microsoft-Windows-Shell-Setup\" processorArchitecture=\"amd64\" publicKeyToken=\"31bf3856ad364e35\" language=\"neutral\" versionScope=\"nonSxS\" xmlns:wcm=\"http://schemas.microsoft.com/WMIConfig/2002/State\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+            "            <ComputerName><![CDATA[$ComputerName$]]></ComputerName>\n" +
+            "            <RegisteredOrganization><![CDATA[$OrgName$]]></RegisteredOrganization>\n" +
+            "            <RegisteredOwner>User</RegisteredOwner>\n" +
+            "            <ProductKey><![CDATA[$ProductKey$]]></ProductKey>\n" +
+            "        </component>\n" +
+            "<component name=\"Microsoft-Windows-Setup\" processorArchitecture=\"amd64\" publicKeyToken=\"31bf3856ad364e35\" language=\"neutral\" versionScope=\"nonSxS\" xmlns:wcm=\"http://schemas.microsoft.com/WMIConfig/2002/State\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+            "            <UserData>\n" +
+            "                <ProductKey>\n" +
+            "                    <Key><![CDATA[$ProductKey$]]></Key>\n" +
+            "                    <WillShowUI>Never</WillShowUI>\n" +
+            "                </ProductKey>\n" +
+            "                <AcceptEula>true</AcceptEula>\n" +
+            "                <Organization><![CDATA[$OrgName$]]></Organization>\n" +
+            "                <FullName>User</FullName>\n" +
+            "            </UserData>\n" +
+            "        </component>";
+
+    @Test
+    public void replaceProductKeyBothEmpty() {
+        String res = SysprepHandler.replaceProductKey("", "", false);
+        assertThat(res, is(equalTo("")));
+    }
+
+    @Test
+    public void replaceProductKeyDefinedKeyShouldBeReplaced() {
+        String res = SysprepHandler.replaceProductKey(sysprepFile, "someKey", false);
+        assertThat(res, containsString("<Key><![CDATA[someKey]]></Key>"));
+    }
+
+    @Test
+    public void replaceProductKeyEmptyKeyShouldRemoveTheWholeSection() {
+        String res = SysprepHandler.replaceProductKey(sysprepFile, "", false);
+        assertThat(res, not(containsString("<ProductKey>")));
+        assertThat(res, not(containsString("</ProductKey>")));
+    }
+
+    @Test
+    public void replaceProductKeyEmptyKeyShouldNotRemoveTheWholeSectionForCustomScript() {
+        String res = SysprepHandler.replaceProductKey(sysprepFile, "", true);
+        assertThat(res, containsString("<ProductKey>"));
+        assertThat(res, containsString("</ProductKey>"));
+    }
+
     @Test
     public void replaceEmptyBuilder() {
         runAndCheck("", "a", "b", "");
