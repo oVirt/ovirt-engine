37736b48215b39ce686453e7584f0ae862d3f87b
Allon Mureinik <amureini@redhat.com>
2017-03-06 15:31:41 +0200

    webadmin: SearchableListModelTest mocking
    Mock the SearchableListModel instead boiler-plate implementing it,
    which is hard to read and maintain.
    
    Change-Id: I7c63ee388a273368e030e85f94bac5405412b184
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/test/java/org/ovirt/engine/ui/uicommonweb/models/SearchableListModelTest.java
+++ frontend/webadmin/modules/uicommonweb/src/test/java/org/ovirt/engine/ui/uicommonweb/models/SearchableListModelTest.java
@@ -6,22 +6,22 @@ import java.util.Arrays;
 
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner.Silent;
 import org.ovirt.engine.ui.uicommonweb.junit.UiCommonSetup;
 
+@RunWith(Silent.class)
 public class SearchableListModelTest {
 
     @ClassRule
     public static UiCommonSetup setup = new UiCommonSetup();
 
+    @Spy
+    private SearchableListModel<Void, Integer> listModel;
+
     @Test
     public void testSelectionRestoredOnNewSetItems() {
-        SearchableListModel<Void, Integer> listModel = new SearchableListModel<Void, Integer>() {
-            @Override
-            protected String getListName() {
-                return "test"; //$NON-NLS-1$
-            }
-        };
-
         listModel.setItems(Arrays.asList(1, 2, 3));
         listModel.setSelectedItem(2);
 
