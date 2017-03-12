cee4281ee2faa71965fcd95b0c731f0497162907
Allon Mureinik <amureini@redhat.com>
2017-03-12 18:47:09 +0200

    core: Clean up SnapshotVmConfigurationHelperTest mocking
    Inject the mocks that are needed by the SnapshotVmConfigurationHelper
    under test and remove those that are not.
    
    Change-Id: Idc23142febd2b50ea545ca3a5f5308bf350c6ad5
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/snapshots/SnapshotVmConfigurationHelperTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/snapshots/SnapshotVmConfigurationHelperTest.java
@@ -1,18 +1,14 @@
 package org.ovirt.engine.core.bll.snapshots;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.ovirt.engine.core.bll.network.macpool.MacPoolPerCluster;
 import org.ovirt.engine.core.common.businessentities.Snapshot;
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.storage.Disk;
@@ -20,29 +16,23 @@ import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.common.businessentities.storage.ImageStatus;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.DiskImageDao;
-import org.ovirt.engine.core.dao.SnapshotDao;
-import org.ovirt.engine.core.dao.VmDao;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SnapshotVmConfigurationHelperTest {
 
     @Mock
-    private SnapshotDao snapshotDaoMock;
     private DiskImageDao diskImageDaoMock;
-    private VmDao vmDaoMock;
+
     private Guid existingSnapshotId = Guid.newGuid();
     private Guid existingVmId = Guid.newGuid();
     private Guid existingImageId = Guid.newGuid();
     private Guid existingImageGroupId = Guid.newGuid();
     private Snapshot existingSnapshot;
     private VM existingVm = null;
-    private SnapshotsManager snapshotsManager;
     private DiskImage existingDiskImage;
 
-    @Mock
-    private MacPoolPerCluster macPoolPerCluster;
-
     @InjectMocks
+    @Spy
     private SnapshotVmConfigurationHelper snapshotVmConfigurationHelper;
 
     private static final String EXISTING_VM_NAME = "Dummy configuration";
@@ -53,22 +43,6 @@ public class SnapshotVmConfigurationHelperTest {
         existingVm = createVm(existingVmId);
         existingSnapshot.setVmConfiguration(EXISTING_VM_NAME); // Dummy configuration
         existingDiskImage = createDiskImage(existingImageId, existingImageGroupId);
-        snapshotVmConfigurationHelper = spy(new SnapshotVmConfigurationHelper());
-        snapshotsManager = mock(SnapshotsManager.class);
-        when(snapshotVmConfigurationHelper.getSnapshotManager()).thenReturn(snapshotsManager);
-        setUpDaoMocks();
-    }
-
-    private void setUpDaoMocks() {
-        vmDaoMock = mock(VmDao.class);
-        doReturn(vmDaoMock).when(snapshotVmConfigurationHelper).getVmDao();
-
-        doReturn(snapshotDaoMock).when(snapshotVmConfigurationHelper).getSnapshotDao();
-
-        diskImageDaoMock = mock(DiskImageDao.class);
-        doReturn(diskImageDaoMock).when(snapshotVmConfigurationHelper).getDiskImageDao();
-
-        when(diskImageDaoMock.get(existingImageId)).thenReturn(existingDiskImage);
     }
 
     private VM createVm(Guid existingVmId) {
