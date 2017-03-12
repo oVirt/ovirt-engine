1a7c6947aaebb1829602cc19bd146c5dd113b113
Allon Mureinik <amureini@redhat.com>
2017-03-12 18:17:44 +0200

    core: CPUPolicyUnitTest redundant stubbing
    While this stubbing doesn't seem to break the official maven build,
    it is, in fact, redundant, and causes the test to fail when run
    independently in IntelliJ IDEA.
    
    This patch removes the redundant stubbing.
    
    Change-Id: I97b7aaa5d2a7229635edaa9f1d73bf1938f7b3e8
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/policyunits/CPUPolicyUnitTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/policyunits/CPUPolicyUnitTest.java
@@ -1,9 +1,7 @@
 package org.ovirt.engine.core.bll.scheduling.policyunits;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,14 +11,12 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.ovirt.engine.core.common.businessentities.Cluster;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.scheduling.PerHostMessages;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dao.ClusterDao;
 import org.ovirt.engine.core.utils.MockConfigRule;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -28,9 +24,6 @@ public class CPUPolicyUnitTest {
 
     @ClassRule
     public static MockConfigRule configRule = new MockConfigRule();
-
-    @Mock
-    private ClusterDao clusterDao;
 
     @InjectMocks
     private final CPUPolicyUnit cpuPolicyUnit = new CPUPolicyUnit(null, null);
@@ -62,7 +55,6 @@ public class CPUPolicyUnitTest {
 
         cluster = new Cluster();
         cluster.setId(Guid.newGuid());
-        when(clusterDao.get(any(Guid.class))).thenReturn(cluster);
     }
 
     /**
