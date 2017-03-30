b435afd9f1dd9981eb8faf15e5276130eb2a6c64
Ravi Nori <rnori@redhat.com>
2017-03-30 13:12:12 -0400

    aaa: Localize all user facing sso messages
    All message sent to browser from SSO
    needs to be localized.
    
    Change-Id: I84a56bdf76566da639763e6d22415f6752372f13
    Bug-Url: https://bugzilla.redhat.com/1435140
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- backend/manager/modules/enginesso/src/main/java/org/ovirt/engine/core/sso/servlets/InteractiveNextAuthServlet.java
+++ backend/manager/modules/enginesso/src/main/java/org/ovirt/engine/core/sso/servlets/InteractiveNextAuthServlet.java
@@ -1,8 +1,10 @@
 package org.ovirt.engine.core.sso.servlets;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Stack;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.ovirt.engine.core.sso.utils.InteractiveAuth;
 import org.ovirt.engine.core.sso.utils.OAuthException;
 import org.ovirt.engine.core.sso.utils.SsoConstants;
+import org.ovirt.engine.core.sso.utils.SsoContext;
 import org.ovirt.engine.core.sso.utils.SsoSession;
 import org.ovirt.engine.core.sso.utils.SsoUtils;
 
 public class InteractiveNextAuthServlet extends HttpServlet {
     private static final long serialVersionUID = 1188460579367588817L;
+
+    private SsoContext ssoContext;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        ssoContext = SsoUtils.getSsoContext(config.getServletContext());
+    }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
@@ -24,7 +34,11 @@ public class InteractiveNextAuthServlet extends HttpServlet {
 
         if (authStack == null || authStack.isEmpty()) {
             SsoUtils.redirectToErrorPage(request, response,
-                    new OAuthException(SsoConstants.ERR_CODE_UNAUTHORIZED_CLIENT, "Authentication required."));
+                    new OAuthException(
+                            SsoConstants.ERR_CODE_UNAUTHORIZED_CLIENT,
+                            ssoContext.getLocalizationUtils().localize(
+                                    SsoConstants.APP_ERROR_AUTHENTICATION_REQUIRED,
+                                    (Locale) request.getAttribute(SsoConstants.LOCALE))));
         } else {
             SsoUtils.getSsoSession(request).setStatus(SsoSession.Status.inprogress);
             response.sendRedirect(authStack.pop().getAuthUrl(request, response));
