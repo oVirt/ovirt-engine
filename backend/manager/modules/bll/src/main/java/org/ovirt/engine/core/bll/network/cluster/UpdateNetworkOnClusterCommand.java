f8e6065d01233b34bfc334fab9c08c544d8c2bfb
Yevgeny Zaspitsky <yzaspits@redhat.com>
2017-04-05 19:57:44 +0300

    engine: ignore NetworkCluster.status in the input
    Ignore status in the input of UpdateNetworkOnClusterCommand.
    
    Change-Id: I63c0c3468844fe07bc9947fe6c38f6632e478798
    Bug-Url: https://bugzilla.redhat.com/1438418
    Signed-off-by: Yevgeny Zaspitsky <yzaspits@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/cluster/UpdateNetworkOnClusterCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/network/cluster/UpdateNetworkOnClusterCommand.java
@@ -61,6 +61,8 @@ public class UpdateNetworkOnClusterCommand<T extends NetworkClusterParameters> e
             displayNetworkClusterHelper.warnOnActiveVm();
         }
 
+        preserveStatus();
+
         networkClusterDao.update(getNetworkCluster());
 
         final Network managementNetwork;
@@ -94,6 +96,13 @@ public class UpdateNetworkOnClusterCommand<T extends NetworkClusterParameters> e
 
         networkClusterHelper.setStatus(getClusterId(), getPersistedNetwork());
         setSucceeded(true);
+    }
+
+    /**
+     * Status is a calculated value, thus the value from the command input should be ignored.
+     */
+    private void preserveStatus() {
+        getNetworkCluster().setStatus(getOldNetworkCluster().getStatus());
     }
 
     @Override
