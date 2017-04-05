24209379f80c6a01737d8694fd4328bb705a1b47
Ala Hino <ahino@redhat.com>
2017-04-05 23:45:20 +0300

    cold-merge: Update step message to include the host name
    After the host running the merge snapshot command is chosen, update the
    step message to show the host name.
    
    Change-Id: Ie069edb8de1b54b6804f578bbce9169f5be97701
    Bug-Url: https://bugzilla.redhat.com/1420401
    Signed-off-by: Ala Hino <ahino@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/utils/VdsCommandsHelper.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/utils/VdsCommandsHelper.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,16 +19,24 @@ import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.errors.EngineError;
 import org.ovirt.engine.core.common.errors.EngineException;
+import org.ovirt.engine.core.common.job.Step;
+import org.ovirt.engine.core.common.job.StepEnum;
 import org.ovirt.engine.core.common.job.StepSubjectEntity;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
 import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
 import org.ovirt.engine.core.common.vdscommands.VdsIdVDSCommandParametersBase;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dal.job.ExecutionMessageDirector;
+import org.ovirt.engine.core.dao.StepDao;
 import org.ovirt.engine.core.dao.StepSubjectEntityDao;
 import org.ovirt.engine.core.dao.VdsDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VdsCommandsHelper {
+
+    private static final Logger log = LoggerFactory.getLogger(VdsCommandsHelper.class);
 
     private VdsCommandsHelper() {
     }
@@ -101,6 +110,8 @@ public class VdsCommandsHelper {
                 if (vdsForExecution != null) {
                     getStepSubjectEntityDao().saveAll(Collections.singletonList(
                             new StepSubjectEntity(stepId, VdcObjectType.EXECUTION_HOST, vdsForExecution)));
+
+                    updateStepMessage(cmd, vdsForExecution);
                 }
             }
 
@@ -132,5 +143,33 @@ public class VdsCommandsHelper {
 
     protected static StepSubjectEntityDao getStepSubjectEntityDao() {
         return DbFacade.getInstance().getStepSubjectEntityDao();
+    }
+
+    protected static StepDao getStepDao() {
+        return DbFacade.getInstance().getStepDao();
+    }
+
+    private static void updateStepMessage(CommandBase<?> cmd, Guid vdsForExecution) {
+        // As an HSM job can run on any host, we want to display the host running the job when it is
+        // chosen. To do so, we look for a corresponding command step with an "_ON_HOST" suffix that
+        // is supposed to contain a "vds" placeholder.
+        StepEnum stepEnum = null;
+        try {
+            stepEnum = StepEnum.valueOf(getStepWithHostname(cmd));
+        } catch (IllegalArgumentException e) {
+            // Ignore this exception and do nothing as no corresponding step_ON_HOST found.
+            log.debug("No StepEnum found for " + getStepWithHostname(cmd));
+            return;
+        }
+
+        Step step = cmd.getExecutionContext().getStep();
+        Map<String, String> jobProperties = cmd.getJobMessageProperties();
+        jobProperties.put(VdcObjectType.VDS.name().toLowerCase(), getVdsDao().get(vdsForExecution).getName());
+        step.setDescription(ExecutionMessageDirector.resolveStepMessage(stepEnum, jobProperties));
+        getStepDao().update(step);
+    }
+
+    private static String getStepWithHostname(CommandBase<?> cmd) {
+        return cmd.getCommandStep().toString() + "_ON_HOST";
     }
 }
