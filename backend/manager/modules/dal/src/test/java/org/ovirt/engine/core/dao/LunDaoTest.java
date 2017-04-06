b341e7822f0ac8a75d77c494c8e4444dbad0970b
Allon Mureinik <amureini@redhat.com>
2017-04-06 15:43:38 +0300

    core: LunDaoTest to extend BaseGenericDaoTestCase
    Reuse the tests from BaseGenericDaoTestCase instead of the copy-paste
    implementations that currently exist in the test.
    
    Change-Id: I4b25996019b6cc4372a86babd6effd72cffe825f
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/LunDaoTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/LunDaoTest.java
@@ -3,7 +3,6 @@ package org.ovirt.engine.core.dao;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -11,43 +10,38 @@ import java.util.List;
 import org.junit.Test;
 import org.ovirt.engine.core.common.businessentities.storage.LUNs;
 
-public class LunDaoTest extends BaseDaoTestCase {
-    private LunDao dao;
-    private LUNs existingLUN;
-    private LUNs newLUN;
+public class LunDaoTest extends BaseGenericDaoTestCase<String, LUNs, LunDao> {
+    @Override
+    protected String getExistingEntityId() {
+        return FixturesTool.LUN_ID1;
+    }
 
     @Override
-    public void setUp() throws Exception {
-        super.setUp();
+    protected LunDao prepareDao() {
+        return dbFacade.getLunDao();
+    }
 
-        dao = dbFacade.getLunDao();
-
-        existingLUN = dao.get(FixturesTool.LUN_ID1);
-
-        newLUN = new LUNs();
+    @Override
+    protected LUNs generateNewEntity() {
+        LUNs newLUN = new LUNs();
         newLUN.setLUNId("oicu812");
         newLUN.setVolumeGroupId("");
+        return newLUN;
     }
 
-    /**
-     * Ensures that the id must be valid.
-     */
-    @Test
-    public void testGetWithInvalidId() {
-        LUNs result = dao.get("farkle");
-
-        assertNull(result);
+    @Override
+    protected String generateNonExistingId() {
+        return "farkle";
     }
 
-    /**
-     * Ensures that retrieving by ID works as expected.
-     */
-    @Test
-    public void testGet() {
-        LUNs result = dao.get(existingLUN.getLUNId());
+    @Override
+    protected void updateExistingEntity() {
+        existingEntity.setSerial("killer");
+    }
 
-        assertNotNull(result);
-        assertEquals(existingLUN, result);
+    @Override
+    protected int getEntitiesTotalCount() {
+        return 8;
     }
 
     /**
@@ -88,51 +82,12 @@ public class LunDaoTest extends BaseDaoTestCase {
      */
     @Test
     public void testGetAllForVolumeGroup() {
-        List<LUNs> result = dao.getAllForVolumeGroup(existingLUN.getVolumeGroupId());
+        List<LUNs> result = dao.getAllForVolumeGroup(existingEntity.getVolumeGroupId());
 
         assertNotNull(result);
         assertFalse(result.isEmpty());
         for (LUNs lun : result) {
-            assertEquals(existingLUN.getVolumeGroupId(), lun.getVolumeGroupId());
+            assertEquals(existingEntity.getVolumeGroupId(), lun.getVolumeGroupId());
         }
     }
-
-    /**
-     * Ensures saving a LUN works as expected.
-     */
-    @Test
-    public void testSave() {
-        dao.save(newLUN);
-
-        LUNs result = dao.get(newLUN.getLUNId());
-
-        assertNotNull(result);
-        assertEquals(newLUN, result);
-    }
-
-    /**
-     * Ensures removing a LUN works as expected.
-     */
-    @Test
-    public void testRemove() {
-        dao.remove(existingLUN.getLUNId());
-
-        LUNs result = dao.get(existingLUN.getLUNId());
-
-        assertNull(result);
-    }
-
-    /**
-     * Ensures updating a LUN works as expected.
-     */
-    @Test
-    public void testUpdateLUNs() {
-        dao.update(existingLUN);
-
-        LUNs result = dao.get(existingLUN.getLUNId());
-
-        assertNotNull(result);
-        assertEquals(existingLUN, result);
-    }
-
 }
