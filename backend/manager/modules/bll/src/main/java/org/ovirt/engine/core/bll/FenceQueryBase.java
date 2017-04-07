4bbda4903153a45907161116eb8214f7fe8c8c19
Moti Asayag <masayag@redhat.com>
2017-04-07 15:01:10 +0300

    engine: Remove dead code
    Change-Id: I1a1b948dc15ee9a6385aacd4df304e5da8dd5cae
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/FenceQueryBase.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/FenceQueryBase.java
@@ -1,12 +1,8 @@
 package org.ovirt.engine.core.bll;
 
 import org.ovirt.engine.core.bll.context.EngineContext;
-import org.ovirt.engine.core.common.AuditLogType;
 import org.ovirt.engine.core.common.queries.VdcQueryParametersBase;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AlertDirector;
-import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogableBase;
-import org.ovirt.engine.core.di.Injector;
 
 public abstract class FenceQueryBase<P extends VdcQueryParametersBase> extends QueriesCommandBase<P> {
     protected FenceQueryBase(P parameters) {
@@ -35,27 +31,5 @@ public abstract class FenceQueryBase<P extends VdcQueryParametersBase> extends Q
 
     protected void setVdsName(String value) {
         privateVdsName = value;
-    }
-
-    /**
-     * Alerts the specified log type.
-     *
-     * @param logType
-     *            Type of the log.
-     * @param reason
-     *            The reason.
-     */
-    private void alert(AuditLogType logType, String reason) {
-        AuditLogableBase alert = Injector.injectMembers(new AuditLogableBase());
-        alert.setVdsId(getVdsId());
-        alert.addCustomValue("Reason", reason);
-        AlertDirector.alert(alert, logType, auditLogDirector);
-    }
-
-    /**
-     * Alerts if power management status failed.
-     */
-    protected void alertPowerManagementStatusFailed(String reason) {
-        alert(AuditLogType.VDS_ALERT_FENCE_TEST_FAILED, reason);
     }
 }
