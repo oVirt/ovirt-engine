85a4b06bf6f8c8717e26819e0684dff7f7f7b03b
Arik Hadas <ahadas@redhat.com>
2017-04-13 17:49:05 +0300

    core: add vm exit reason of type unknown
    When restoring a backup, the status and run_on_vds fields of the VM
    do not represent the actual information of the running VM. This may
    lead of unneeded restart of highly-available VMs that would cause a
    split-brain.
    
    Therefore, we decide to switch highly-available VMs to down when
    restoring a backup. But none of the existing exit reasons fit for
    this flow: Error would cause the VM to be restarted when the engine
    starts, and Normal doesn't reflect the situation properly since the
    VM was not shut-down properly.
    
    Thus, we add a new exit reason for this case, called Unknown. The
    whole purpose of this value, for now, is to prevent the automatic
    VM restart mechanism to skip VMs set with this value.
    
    Change-Id: I40710a943c60dbdbbdf9e8e9563afc121d1d632a
    Bug-Url: https://bugzilla.redhat.com/1441322
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmExitStatus.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmExitStatus.java
@@ -2,7 +2,10 @@ package org.ovirt.engine.core.common.businessentities;
 
 public enum VmExitStatus {
     Normal,
-    Error;
+    Error,
+    /** This status is set for highly available VMs (with no vm lease) after restoring a backup of
+     *  the database and is not being reported by VDSM. see https://bugzilla.redhat.com/1441322 */
+    Unknown;
 
     public int getValue() {
         return this.ordinal();
