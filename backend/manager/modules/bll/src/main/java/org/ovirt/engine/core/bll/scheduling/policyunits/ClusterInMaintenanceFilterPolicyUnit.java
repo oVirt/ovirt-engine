753331a0c61af31e8a855f18db0e75318b9fedcc
Martin Sivak <msivak@redhat.com>
2017-04-18 16:01:36 +0200

    scheduling: Add optional ClusterInMaintenance scheduling policy unit
    This follows a request to let the user define that no new VMs
    should be allowed to start unless those are marked as highly available.
    
    Balancing and migrations should not be affected in any way.
    
    An example cluster_maintenance cluster policy is also added by
    this change.
    
    Change-Id: I2b105b1dbcf36e0e905179df099d5d45c6827622
    Bug-Url: https://bugzilla.redhat.com/1438408
    Signed-off-by: Martin Sivak <msivak@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/policyunits/ClusterInMaintenanceFilterPolicyUnit.java
@@ -0,0 +1,49 @@
+package org.ovirt.engine.core.bll.scheduling.policyunits;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.ovirt.engine.core.bll.scheduling.PolicyUnitImpl;
+import org.ovirt.engine.core.bll.scheduling.SchedulingUnit;
+import org.ovirt.engine.core.bll.scheduling.pending.PendingResourceManager;
+import org.ovirt.engine.core.common.businessentities.Cluster;
+import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.VM;
+import org.ovirt.engine.core.common.errors.EngineMessage;
+import org.ovirt.engine.core.common.scheduling.PerHostMessages;
+import org.ovirt.engine.core.common.scheduling.PolicyUnit;
+import org.ovirt.engine.core.common.scheduling.PolicyUnitType;
+
+@SchedulingUnit(
+        guid = "0bd8fef9-bcb4-4250-9998-410f01608d9b",
+        name = "ClusterInMaintenance",
+        type = PolicyUnitType.FILTER,
+        description = "Prevents any VM start except for highly available VMs"
+)
+public class ClusterInMaintenanceFilterPolicyUnit extends PolicyUnitImpl {
+    public ClusterInMaintenanceFilterPolicyUnit(PolicyUnit policyUnit,
+            PendingResourceManager pendingResourceManager) {
+        super(policyUnit, pendingResourceManager);
+    }
+
+    @Override
+    public List<VDS> filter(Cluster cluster,
+            List<VDS> hosts,
+            VM vm,
+            Map<String, String> parameters,
+            PerHostMessages messages) {
+        // Highly available VMs are allowed to start
+        if (vm.isAutoStartup()) {
+            return hosts;
+        }
+
+        // Already running VMs are allowed to migrate
+        if (vm.getRunOnVds() != null) {
+            return hosts;
+        }
+
+        hosts.forEach(h -> messages.addMessage(h.getId(), EngineMessage.VAR__DETAIL__CLUSTER_IN_MAINTENANCE.name()));
+        return Collections.emptyList();
+    }
+}
---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/policyunits/ClusterInMaintenanceFilterPolicyUnitTest.java
@@ -0,0 +1,89 @@
+package org.ovirt.engine.core.bll.scheduling.policyunits;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.ovirt.engine.core.common.businessentities.VDS;
+import org.ovirt.engine.core.common.businessentities.VM;
+import org.ovirt.engine.core.common.scheduling.PerHostMessages;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.utils.MockConfigRule;
+
+public class ClusterInMaintenanceFilterPolicyUnitTest {
+    private VDS host;
+    private VM vm;
+
+    @ClassRule
+    public static MockConfigRule configRule = new MockConfigRule();
+
+    @Before
+    public void setUp() {
+        host = new VDS();
+        host.setId(Guid.newGuid());
+
+        vm = new VM();
+        vm.setId(Guid.newGuid());
+        vm.setAutoStartup(false);
+        vm.setRunOnVds(null);
+    }
+
+    @Test
+    public void testHaCanStart() throws Exception {
+        vm.setAutoStartup(true);
+
+        ClusterInMaintenanceFilterPolicyUnit unit = new ClusterInMaintenanceFilterPolicyUnit(null, null);
+        List<VDS> result = unit.filter(null, Collections.singletonList(host), vm, Collections.emptyMap(), new PerHostMessages());
+        assertThat(result)
+                .isNotNull()
+                .isNotEmpty()
+                .contains(host);
+    }
+
+    @Test
+    public void testHaCanMigrate() throws Exception {
+        vm.setRunOnVds(Guid.newGuid());
+        vm.setAutoStartup(true);
+
+        ClusterInMaintenanceFilterPolicyUnit unit = new ClusterInMaintenanceFilterPolicyUnit(null, null);
+        List<VDS> result = unit.filter(null,
+                Collections.singletonList(host), vm,
+                Collections.emptyMap(), new PerHostMessages());
+        assertThat(result)
+                .isNotNull()
+                .isNotEmpty()
+                .contains(host);
+    }
+
+    @Test
+    public void testNonHaCannotStart() throws Exception {
+        ClusterInMaintenanceFilterPolicyUnit unit = new ClusterInMaintenanceFilterPolicyUnit(null, null);
+        final PerHostMessages messages = new PerHostMessages();
+        List<VDS> result = unit.filter(null,
+                Collections.singletonList(host), vm,
+                Collections.emptyMap(), messages);
+        assertThat(result)
+                .isNotNull()
+                .isEmpty();
+        assertThat(messages.getMessages())
+                .isNotEmpty()
+                .containsKeys(host.getId());
+    }
+
+    @Test
+    public void testNonHaCanMigrate() throws Exception {
+        vm.setRunOnVds(Guid.newGuid());
+        ClusterInMaintenanceFilterPolicyUnit unit = new ClusterInMaintenanceFilterPolicyUnit(null, null);
+        List<VDS> result = unit.filter(null,
+                Collections.singletonList(host), vm,
+                Collections.emptyMap(), new PerHostMessages());
+        assertThat(result)
+                .isNotNull()
+                .isNotEmpty()
+                .contains(host);
+    }
+}
