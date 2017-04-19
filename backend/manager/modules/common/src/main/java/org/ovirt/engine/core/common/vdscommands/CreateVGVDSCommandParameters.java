04b6e260ae0ec709cfd2779f9de80d163dc4c9bd
Tal Nisan <tnisan@redhat.com>
2017-04-19 13:27:28 +0300

    core: Use interface in CreateVGVDSCommandParameters
    Change-Id: Ib8822177fe59d46b0bcc731a3d77b8881737eac6
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/vdscommands/CreateVGVDSCommandParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/vdscommands/CreateVGVDSCommandParameters.java
@@ -1,13 +1,12 @@
 package org.ovirt.engine.core.common.vdscommands;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.ovirt.engine.core.common.utils.ToStringBuilder;
 import org.ovirt.engine.core.compat.Guid;
 
 public class CreateVGVDSCommandParameters extends ValidateStorageDomainVDSCommandParameters {
-    public CreateVGVDSCommandParameters(Guid vdsId, Guid storageDomainId, ArrayList<String> deviceList, boolean force) {
+    public CreateVGVDSCommandParameters(Guid vdsId, Guid storageDomainId, List<String> deviceList, boolean force) {
         super(vdsId, storageDomainId);
         setDeviceList(deviceList);
         setForce(force);
