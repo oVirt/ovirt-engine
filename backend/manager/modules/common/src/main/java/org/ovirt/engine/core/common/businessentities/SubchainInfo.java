a85dd4d3c0322a5651393139820fdfab13f26ecd
Benny Zlotnik <bzlotnik@redhat.com>
2017-04-24 11:43:10 +0300

    core: Refactor SubchainInfo
    SubchainInfo will hold DiskImage instead of just the Guid
    and will have equals & hashCode, this was done to make this
    class usable in the new validation process of RemoveSnapshotCommand
    see commit: e981191a75537756dcc3e19f881a2b4314552aa5
    
    Change-Id: I759d838fea0cfb7b375568f2dd5937b8a245e106
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/SubchainInfo.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/SubchainInfo.java
@@ -1,24 +1,31 @@
 package org.ovirt.engine.core.common.businessentities;
 
+import java.util.Objects;
+
+import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.common.utils.ToStringBuilder;
 import org.ovirt.engine.core.compat.Guid;
 
 public class SubchainInfo {
 
     private Guid storageDomainId;
-    private Guid imageGroupId;
-    private Guid baseImageId;
-    private Guid topImageId;
     private Integer baseImageGeneration;
+    private DiskImage baseImage;
+    private DiskImage topImage;
 
     public SubchainInfo() {
     }
 
-    public SubchainInfo(Guid storageDomainId, Guid imageGroupId, Guid baseImageId, Guid topImageId) {
+    public SubchainInfo(DiskImage baseImage, DiskImage topImage) {
+        this.storageDomainId = baseImage.getStorageIds().get(0);
+        this.baseImage = baseImage;
+        this.topImage = topImage;
+    }
+
+    public SubchainInfo(Guid storageDomainId, DiskImage baseImage, DiskImage topImage) {
         this.storageDomainId = storageDomainId;
-        this.imageGroupId = imageGroupId;
-        this.baseImageId = baseImageId;
-        this.topImageId = topImageId;
+        this.baseImage = baseImage;
+        this.topImage = topImage;
     }
 
     public Guid getStorageDomainId() {
@@ -26,15 +33,15 @@ public class SubchainInfo {
     }
 
     public Guid getImageGroupId() {
-        return imageGroupId;
+        return baseImage.getId();
     }
 
     public Guid getBaseImageId() {
-        return baseImageId;
+        return getBaseImage().getImageId();
     }
 
     public Guid getTopImageId() {
-        return topImageId;
+        return getTopImage().getImageId();
     }
 
     public Integer getBaseImageGeneration() {
@@ -45,13 +52,51 @@ public class SubchainInfo {
         this.baseImageGeneration = generation;
     }
 
+    public DiskImage getBaseImage() {
+        return baseImage;
+    }
+
+    public void setBaseImage(DiskImage baseImage) {
+        this.baseImage = baseImage;
+    }
+
+    public DiskImage getTopImage() {
+        return topImage;
+    }
+
+    public void setTopImage(DiskImage topImage) {
+        this.topImage = topImage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SubchainInfo that = (SubchainInfo) o;
+        return Objects.equals(storageDomainId, that.storageDomainId) &&
+                Objects.equals(baseImageGeneration, that.baseImageGeneration) &&
+                Objects.equals(baseImage, that.baseImage) &&
+                Objects.equals(topImage, that.topImage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(storageDomainId,
+                baseImageGeneration,
+                baseImage,
+                topImage);
+    }
+
     @Override
     public String toString() {
         return ToStringBuilder.forInstance(this)
                 .append("storageDomainId", storageDomainId)
-                .append("imageGroupId", imageGroupId)
-                .append("baseImageId", baseImageId)
-                .append("topImageId", topImageId)
+                .append("baseImage", baseImage)
+                .append("topImage", topImage)
                 .append("baseImageGeneration", baseImageGeneration)
                 .build();
     }
