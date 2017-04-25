07bd1ccfc8c297ae3ab63b765a2fed6a97a91627
Arik Hadas <ahadas@redhat.com>
2017-04-25 12:35:22 +0300

    core: cleanup in SetVmStatus
    1. This command is called internally from just a few places so
    we can remove the validation of the parameters at the beginning
    of the execute method.
    
    2. This command is not used for setting the VM status to Unknown
    thus the code for this specific case is removed.
    
    3. This command is only used to set the VM status to Down or
    ImageLocked, in both statuses the VM is not running, thus its
    execute method can be simplified.
    
    Change-Id: I724544f8063e04a7a5405a8e5cc25abea87c3b42
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/SetVmStatusVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/SetVmStatusVDSCommand.java
@@ -2,17 +2,12 @@ package org.ovirt.engine.core.vdsbroker;
 
 import javax.inject.Inject;
 
-import org.ovirt.engine.core.common.businessentities.VMStatus;
 import org.ovirt.engine.core.common.businessentities.VmDynamic;
 import org.ovirt.engine.core.common.businessentities.VmStatistics;
 import org.ovirt.engine.core.common.vdscommands.SetVmStatusVDSCommandParameters;
 import org.ovirt.engine.core.dao.VmDynamicDao;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SetVmStatusVDSCommand<P extends SetVmStatusVDSCommandParameters> extends VDSCommandBase<P> {
-
-    private static final Logger log = LoggerFactory.getLogger(SetVmStatusVDSCommand.class);
 
     @Inject
     private VmDynamicDao vmDynamicDao;
@@ -23,22 +18,11 @@ public class SetVmStatusVDSCommand<P extends SetVmStatusVDSCommandParameters> ex
 
     @Override
     protected void executeVDSCommand() {
-        final VMStatus status = getParameters().getStatus();
-        if (status == null) {
-            log.warn("got request to change the status of VM '{}' to null,  ignoring", getParameters().getVmId());
-            return;
-        }
-
         VmDynamic vmDynamic = vmDynamicDao.get(getParameters().getVmId());
-        vmDynamic.setStatus(status);
-        if (status.isNotRunning()) {
-            resourceManager.removeAsyncRunningVm(getParameters().getVmId());
-            resourceManager.internalSetVmStatus(vmDynamic, status, getParameters().getExitStatus());
-            resourceManager.getVmManager(getParameters().getVmId()).update(new VmStatistics(getParameters().getVmId()));
-            // TODO: update network statistics
-        } else if (status == VMStatus.Unknown) {
-            resourceManager.removeAsyncRunningVm(getParameters().getVmId());
-        }
-        vmDynamicDao.update(vmDynamic);
+        resourceManager.removeAsyncRunningVm(getParameters().getVmId());
+        resourceManager.internalSetVmStatus(vmDynamic, getParameters().getStatus(), getParameters().getExitStatus());
+        resourceManager.getVmManager(getParameters().getVmId()).update(new VmStatistics(getParameters().getVmId()));
+        // TODO: update network statistics
+        resourceManager.getVmManager(getParameters().getVmId()).update(vmDynamic);
     }
 }
