22f0e5d03d54002103505927f16698904c96b676
emesika <emesika@redhat.com>
2017-04-25 15:50:15 +0300

    core: fix spec_params value in vm_device
    The squashing 03_06 patch replaced by mistake the following column
    values:
        initrd_url column in vm_static table in file 01100_insert_vm_static.sql
        spec_params column in vm_device table in file 01200_insert_vm_device.sql
        description column in policy_units table in file 00340_insert_policy_units.sql
    with UUID while in original it was empty.
    As a result an exception is thrown in engine when trying to create a map
    from this value for spec_params or the value is incorrect in the other
    cases.
    
    This patch fixes that issue.
    
    Change-Id: I281b8953401a9b8e71ee08031af9c9756fc72b32
    Signed-off-by: emesika <emesika@redhat.com>

--- packaging/dbscripts/data/00340_insert_policy_units.sql
+++ packaging/dbscripts/data/00340_insert_policy_units.sql
@@ -7,9 +7,9 @@
 --
 
 INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('12262ab6-9690-4bc3-a2b3-35573b172d54', 'PinToHost', true, NULL, 0, true, 'Filters out all hosts that VM is not pinned to');
-INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('6d636bf6-a35c-4f9d-b68d-0731f720cddc', 'CPU', true, NULL, 0, true, 'Filters out hosts with less CPUs than VM"58ca7b19-0071-00c0-01d6-000000000212"s CPUs');
+INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('6d636bf6-a35c-4f9d-b68d-0731f720cddc', 'CPU', true, NULL, 0, true, 'Filters out hosts with less CPUs than VMs CPUs');
 INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('c9ddbb34-0e1d-4061-a8d7-b0893fa80932', 'Memory', true, NULL, 0, true, 'Filters out hosts that have insufficient memory to run the VM');
-INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('72163d1c-9468-4480-99d9-0888664eb143', 'Network', true, NULL, 0, true, 'Filters out hosts that are missing networks required by VM NICs, or missing cluster"58ca7b19-0071-00c0-01d6-000000000212"s display network');
+INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('72163d1c-9468-4480-99d9-0888664eb143', 'Network', true, NULL, 0, true, 'Filters out hosts that are missing networks required by VM NICs, or missing clusters display network');
 INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('38440000-8cf0-14bd-c43e-10b96e4ef00a', 'None', true, NULL, 2, true, 'No load balancing operation');
 INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('38440000-8cf0-14bd-c43e-10b96e4ef00b', 'None', true, NULL, 1, true, 'Follows Even Distribution weight module');
 INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('438b052c-90ab-40e8-9be0-a22560202ea6', 'CPU-Level', true, NULL, 0, true, 'Runs VMs only on hosts with a proper CPU level');
@@ -35,7 +35,7 @@ INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, 
   "HighUtilization" : "^([5-9][0-9])$",
   "MaxFreeMemoryForOverUtilized": "^[1-9][0-9]*$",
   "MinFreeMemoryForUnderUtilized": "^[1-9][0-9]*$"
-}', 2, true, 'Load balancing VMs in cluster according to hosts CPU load, striving cluster"58ca7b19-0071-00c0-01d6-000000000212"s hosts CPU load to be under "58ca7b19-0071-00c0-01d6-000000000212"HighUtilization"58ca7b19-0071-00c0-01d6-000000000212"58ca7b19-00d8-0398-0138-00000000011d"');
+}', 2, true, 'Load balancing VMs in cluster according to hosts CPU load, striving clusters hosts CPU load to be under HighUtilization');
 INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('736999d0-1023-46a4-9a75-1316ed50e151', 'OptimalForPowerSaving', true, '{
   "CpuOverCommitDurationMinutes" : "^([1-9][0-9]*)$",
   "HighUtilization" : "^([5-9][0-9])$",
@@ -44,7 +44,7 @@ INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, 
   "EnableAutomaticHostPowerManagement": "^(true|false)$",
   "MaxFreeMemoryForOverUtilized": "^[1-9][0-9]*$",
   "MinFreeMemoryForUnderUtilized": "^[1-9][0-9]*$"
-}', 2, true, 'Load balancing VMs in cluster according to hosts CPU load, striving cluster"58ca7b19-0071-00c0-01d6-000000000212"s hosts CPU load to be over "58ca7b19-0071-00c0-01d6-000000000212"LowUtilization"58ca7b19-0071-00c0-01d6-000000000212" and under "58ca7b19-0071-00c0-01d6-000000000212"HighUtilization"58ca7b19-0071-00c0-01d6-000000000212""58ca7b19-00d8-0398-0138-00000000011d"');
+}', 2, true, 'Load balancing VMs in cluster according to hosts CPU load, striving clusters hosts CPU load to be over LowUtilization and under HighUtilization"');
 INSERT INTO policy_units (id, name, is_internal, custom_properties_regex, type, enabled, description) VALUES ('728a21f1-f97e-4d32-bc3e-b3cc49756abb', 'HostDevice', true, NULL, 0, true, 'Filters out hosts not supporting VM required host devices');
 
 
