61f971b8f1cc4baee537cebf4c965d51c2ca6cc7
Andrej Krejcir <akrejcir@redhat.com>
2017-05-04 17:08:22 +0200

    scheduler: Fix empty scores returned by external scheduler
    Fixes NPE if the external scheduler returns an empty score list.
    This can happen if an external policy times out.
    
    Change-Id: I0d2c4bbdb476b76b799a43a0a71f4de579452f1c
    Bug-Url: https://bugzilla.redhat.com/1444611
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/external/ScoringResult.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/external/ScoringResult.java
@@ -6,13 +6,9 @@ import java.util.List;
 import org.ovirt.engine.core.compat.Guid;
 
 public class ScoringResult extends SchedulerResult {
-    private List<WeightResultEntry> hostsWithScores;
+    private List<WeightResultEntry> hostsWithScores = new ArrayList<>();
 
     public void addHost(String policyUnit, Guid host, Integer score) {
-        if (hostsWithScores == null) {
-            hostsWithScores = new ArrayList<>();
-        }
-
         hostsWithScores.add(new WeightResultEntry(host, score, policyUnit));
     }
 
