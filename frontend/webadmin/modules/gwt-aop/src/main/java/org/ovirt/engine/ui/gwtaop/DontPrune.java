89deeb8d120094fd7bb9dc9425b209bbb5c395a9
Vojtech Szocs <vszocs@redhat.com>
2017-05-04 14:20:48 +0200

    frontend: Improve DontPrune aspect code
    - added some comments to explain the details
    - use Java 8 stream API to reduce code complexity
    
    Note: in aop.xml file, we're configuring AOP weaver to apply
    aspects not only to the code we want to advise (3rd party GWT
    class ControlFlowAnalyzer) but also to the aspect Java class
    itself (DontPrune). Without this, the build fails, so I guess
    this is needed simply to ensure the aspect's Java class gets
    properly loaded by AspectJ AOP weaver.
    
    Change-Id: I14e84d3193cfcc49582d3cf85e5140be8e364eb0
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

--- frontend/webadmin/modules/gwt-aop/src/main/java/org/ovirt/engine/ui/gwtaop/DontPrune.java
+++ frontend/webadmin/modules/gwt-aop/src/main/java/org/ovirt/engine/ui/gwtaop/DontPrune.java
@@ -1,23 +1,28 @@
 package org.ovirt.engine.ui.gwtaop;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
-import com.google.gwt.dev.jjs.ast.JDeclaredType;
 import com.google.gwt.dev.jjs.ast.JMethod;
 import com.google.gwt.dev.jjs.ast.JProgram;
 import com.google.gwt.dev.jjs.impl.ControlFlowAnalyzer;
 
+/**
+ * Advises GWT {@linkplain ControlFlowAnalyzer live code analyzer} to ensure that
+ * given Java classes, as defined by <code>{@value GWT_DONTPRUNE}</code> system
+ * property, are always treated as live code and never pruned from the GWT-generated
+ * JavaScript code.
+ */
 @Aspect
 public class DontPrune {
 
     /**
-     * The name of the system property that contains the regular expression that indicates
-     * which types should not be pruned.
+     * The name of the system property holding a regular expression that indicates
+     * which types (based on their {@linkplain Class#getName binary name}) should
+     * not be pruned.
      */
     private static final String GWT_DONTPRUNE = "gwt.dontPrune";
 
@@ -31,16 +36,17 @@ public class DontPrune {
     }
 
     /**
-     * Just after the invocation of the liveness analyzer, exclude all the fields and methods
-     * inside the classes whose name match the regular expression given in the system property.
+     * Right after constructing the liveness analyzer, make sure that all types
+     * matched by the regular expression are marked as if they were referenced
+     * by the GWT application code.
      */
     @After("createLivenessAnalyzer(analyzer, program)")
     public void afterCreateLivenessAnalyzer(ControlFlowAnalyzer analyzer, JProgram program) {
-        // Get the regular expression and warn the user if it is empty:
+        // Get the regular expression and warn the user if it's empty:
         String dontPruneRe = System.getProperty(GWT_DONTPRUNE);
         if (dontPruneRe == null) {
-            String error = "The system property \"" + GWT_DONTPRUNE
-                    + "\" that specifies the types not to be pruned wasn't set!";
+            String error = "The system property '" + GWT_DONTPRUNE
+                    + "' that specifies the types not to be pruned wasn't set!";
             System.err.println(error);
             throw new RuntimeException(error);
         }
@@ -48,22 +54,20 @@ public class DontPrune {
         // Compile the regular expression:
         Pattern dontPrunePattern = Pattern.compile(dontPruneRe);
 
-        // Scan all the types and make sure that the ones matching the regular expression
-        // are not pruned:
-        for (JDeclaredType type : program.getDeclaredTypes()) {
-            String typeName = type.getName();
-            Matcher matcher = dontPrunePattern.matcher(typeName);
+        // Scan all the types and make sure that the ones matching the given regular
+        // expression are not pruned:
+        program.getDeclaredTypes().stream()
+                .filter(type -> dontPrunePattern.matcher(type.getName()).matches())
+                .forEach(type -> {
+                    // Note: calling "analyzer.traverseFromInstantiationOf(type)"
+                    // may cause GWT compiler to crash on NPE; don't use that method:
+                    analyzer.traverseFromReferenceTo(type);
 
-            if (matcher.matches()) {
-                // Following line causes GWT compiler to crash on NPE with gwt-jackson:
-                // analyzer.traverseFromInstantiationOf(type);
-                analyzer.traverseFromReferenceTo(type);
-
-                for (JMethod method : type.getMethods()) {
-                    analyzer.traverseFrom(method);
-                }
-            }
-        }
+                    // Make sure that all methods of the given type are not pruned:
+                    for (JMethod method : type.getMethods()) {
+                        analyzer.traverseFrom(method);
+                    }
+                });
     }
 
 }
