a23dc7d7033699dad14e9510d1f124b3eeda86e8
Tal Nisan <tnisan@redhat.com>
2017-05-04 12:32:16 +0300

    core: Avoid injection in IsoDomainListSynchronizer
    Since all of the data to be logged is already present at the creating of
    the audit-log entity, there is no need to inject any beans to the
    AuditLogableBase and it can be replaced with a leaner and more efficient
    implementation of the AuditLogable.
    
    Change-Id: Ieba62c63f2c489ae1711f9237d69ef3d972a62f2
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/domain/IsoDomainListSynchronizer.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/domain/IsoDomainListSynchronizer.java
@@ -46,12 +46,12 @@ import org.ovirt.engine.core.common.vdscommands.VDSReturnValue;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.TransactionScopeOption;
 import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogDirector;
-import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogableBase;
+import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogable;
+import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogableImpl;
 import org.ovirt.engine.core.dao.RepoFileMetaDataDao;
 import org.ovirt.engine.core.dao.StorageDomainDao;
 import org.ovirt.engine.core.dao.StoragePoolIsoMapDao;
 import org.ovirt.engine.core.dao.provider.ProviderDao;
-import org.ovirt.engine.core.di.Injector;
 import org.ovirt.engine.core.utils.threadpool.ThreadPoolUtil;
 import org.ovirt.engine.core.utils.timer.OnTimerMethodAnnotation;
 import org.ovirt.engine.core.utils.timer.SchedulerUtilQuartzImpl;
@@ -242,9 +242,9 @@ public class IsoDomainListSynchronizer implements BackendService {
                                 Config.<Integer> getValue(ConfigValues.GlanceImageListSize), totalListSize);
 
                         if (repoImages.size() >= totalListSize) {
-                            AuditLogableBase logable = Injector.injectMembers(new AuditLogableBase());
-                            logable.addCustomValue("imageDomain", storageDomain.getName());
-                            logable.addCustomValue("imageListSize", String.valueOf(repoImages.size()));
+                            AuditLogable logable = new AuditLogableImpl();
+                            logable.addCustomValue("imageDomain", storageDomain.getName())
+                                    .addCustomValue("imageListSize", String.valueOf(repoImages.size()));
                             auditLogDirector.log(logable, AuditLogType.REFRESH_REPOSITORY_IMAGE_LIST_INCOMPLETE);
                         }
 
@@ -843,7 +843,7 @@ public class IsoDomainListSynchronizer implements BackendService {
      *            - List of Iso domain names, which encounter problem fetching from VDSM.
      */
     private void addToAuditLogErrorMessage(String problematicRepoFilesList) {
-        AuditLogableBase logable = Injector.injectMembers(new AuditLogableBase());
+        AuditLogable logable = new AuditLogableImpl();
 
         // Get translated error by error code ,if no translation found (should not happened) ,
         // will set the error code instead.
@@ -855,7 +855,7 @@ public class IsoDomainListSynchronizer implements BackendService {
      * Add audit log message when fetch encounter problems.
      */
     private void addToAuditLogSuccessMessage(String IsoDomain, String imageType) {
-        AuditLogableBase logable = Injector.injectMembers(new AuditLogableBase());
+        AuditLogable logable = new AuditLogableImpl();
         logable.addCustomValue("imageDomains", String.format("%s (%s file type)", IsoDomain, imageType));
         auditLogDirector.log(logable, AuditLogType.REFRESH_REPOSITORY_IMAGE_LIST_SUCCEEDED);
     }
