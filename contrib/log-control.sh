604bd60108911c4330489deb8e499e5e5d460f1e
Roy Golan <rgolan@redhat.com>
2017-05-10 11:05:32 +0300

    contrib: Introduce contrib folder
    Introduce a contrib folder to experiment and share useful tools and
    ideas. Best ones may/should be promoted to supported ones.
    
    The first one is a log control script to change logging level of various
    log categories (java packages) in real time.
    
    For example to log all db queries into the log:
    
    ```bash
        log-control.sh org.ovirt.engine.core.dal DEBUG
    ```
    
    Change-Id: I550f53b9bb38aeef0e88e0dbf7d1e5c98a2631d9
    Signed-off-by: Roy Golan <rgolan@redhat.com>

---
+++ contrib/log-control.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+function usage() {
+    echo "
+    USAGE
+        $0 [LOG_CATEGORY] [LOG_LEVEL]
+
+        LOG_CATEGORY
+            Logging category (or namespace)
+
+        LOG_LEVEL
+            The desired log level
+
+
+    EXAMPLE
+       Debug all ovirt related loggers - $0 org.ovirt DEBUG
+       Debug DB related stuff          - $0 org.ovirt.engine.core.dal DEBUG
+        "
+    exit 1
+}
+
+function jbossHome() {
+    if [[ -z "${JBOSS_HOME}" ]]; then
+        JBOSS_HOME=/usr/share/ovirt-engine-wildfly/
+    fi
+
+    if [[ ! -x ${JBOSS_HOME}/bin/jboss-cli.sh ]]; then
+        echo "jboss-cli.sh is missing. It should be reachable under "
+        echo "JBOSS_HOME/bin which is pointing to the installation "
+        echo "directory of the application server, typically "
+        echo "/usr/share/ovirt-engine-wildfly."
+        exit 1
+    fi
+}
+
+function validate() {
+    jbossHome && [[ $# -eq 2 ]]
+}
+
+validate || usage
+
+# Add the log category, ignore failures
+${JBOSS_HOME}/bin/jboss-cli.sh --controller=127.0.0.1:8706 --connect --user=admin@internal \
+   --commands="
+       /subsystem=logging/logger=$1:add,
+   "
+
+# Set the logging level on the specified category
+${JBOSS_HOME}/bin/jboss-cli.sh --controller=127.0.0.1:8706 --connect --user=admin@internal \
+   --commands="
+       /subsystem=logging/logger='$1':write-attribute(name=level,value=$2)
+   "
