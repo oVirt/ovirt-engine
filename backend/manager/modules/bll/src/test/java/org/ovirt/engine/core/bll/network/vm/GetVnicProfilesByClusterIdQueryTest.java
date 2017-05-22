215d030acffc72bd1f25b070d7e0b1a8024a60d8
Allon Mureinik <amureini@redhat.com>
2017-05-22 11:43:00 +0300

    core: Standardize GetVnicProfilesByClusterIdQueryTest
    Standardize GetVnicProfilesByClusterIdQueryTest to extend
    AbstractUserQueryTest, like any other test for any other query, and
    avoid the need to boilerplate extend it.
    
    Change-Id: I762a11edc7da42bd9aba90399cb9c1d630aa878e
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/vm/GetVnicProfilesByClusterIdQueryTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/vm/GetVnicProfilesByClusterIdQueryTest.java
@@ -10,60 +10,42 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.ovirt.engine.core.bll.AbstractUserQueryTest;
 import org.ovirt.engine.core.common.businessentities.network.VnicProfileView;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.network.VnicProfileViewDao;
 
 @RunWith(MockitoJUnitRunner.class)
-public class GetVnicProfilesByClusterIdQueryTest {
+public class GetVnicProfilesByClusterIdQueryTest extends
+        AbstractUserQueryTest<IdQueryParameters, GetVnicProfilesByClusterIdQuery<IdQueryParameters>> {
 
     private static final Guid clusterId = Guid.newGuid();
-    private static final Guid userId = Guid.newGuid();
 
     @Mock
     private VnicProfileViewDao mockVnicProfileViewDao;
 
-    @InjectMocks
-    private GetVnicProfilesByClusterIdQuery underTest =
-            new TestGetVnicProfilesByClusterIdQuery(createParams(clusterId), userId);
-
     private List<VnicProfileView> vnicProfileViews = new ArrayList<>();
 
     @Before
-    public void setUp() {
-        when(mockVnicProfileViewDao.getAllForCluster(clusterId, userId, true)).thenReturn(vnicProfileViews);
+    public void setUp() throws Exception {
+        super.setUp();
+        when(mockVnicProfileViewDao.getAllForCluster(clusterId, getUser().getId(), true)).thenReturn(vnicProfileViews);
     }
 
-    private IdQueryParameters createParams(Guid clusterId) {
-        final IdQueryParameters parameters = new IdQueryParameters(clusterId);
-        parameters.setFiltered(true);
-        return parameters;
+    @Override
+    protected void setUpMockQueryParameters() {
+        super.setUpMockQueryParameters();
+        when(getQueryParameters().getId()).thenReturn(clusterId);
     }
 
     @Test
     public void testExecuteQueryCommand() {
-        underTest.executeQueryCommand();
-        final List<VnicProfileView> actual = underTest.getQueryReturnValue().getReturnValue();
+        getQuery().executeQueryCommand();
+        final List<VnicProfileView> actual = getQuery().getQueryReturnValue().getReturnValue();
 
         assertThat(actual, Matchers.sameInstance(vnicProfileViews));
-    }
-
-    private static class TestGetVnicProfilesByClusterIdQuery extends GetVnicProfilesByClusterIdQuery<IdQueryParameters> {
-
-        private final Guid userId;
-
-        public TestGetVnicProfilesByClusterIdQuery(IdQueryParameters parameters, Guid userId) {
-            super(parameters);
-            this.userId = userId;
-        }
-
-        @Override
-        protected Guid getUserID() {
-            return userId;
-        }
     }
 }
