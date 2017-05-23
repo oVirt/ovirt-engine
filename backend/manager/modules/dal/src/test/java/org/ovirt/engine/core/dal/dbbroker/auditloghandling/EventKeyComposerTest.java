d3ff860633ed9cb36e3994ab58b14038789ba8c0
Moti Asayag <masayag@redhat.com>
2017-05-23 17:30:39 +0300

    engine: Run key composing test for AuditLogableImpl
    Change-Id: Ic2a84e1a638e1387970d27636f2a5cdd0dce7095
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dal/dbbroker/auditloghandling/EventKeyComposerTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dal/dbbroker/auditloghandling/EventKeyComposerTest.java
@@ -31,8 +31,18 @@ public class EventKeyComposerTest {
     }
 
     @Test
-    public void composeObjectId() throws Exception {
+    public void composeObjectIdWithAuditLogableBase() throws Exception {
         AuditLogableBase event = new AuditLogableBase();
+        createEventAndAssert(event);
+    }
+
+    @Test
+    public void composeObjectIdWithAuditLogableImpl() throws Exception {
+        AuditLogable event = new AuditLogableImpl();
+        createEventAndAssert(event);
+    }
+
+    private void createEventAndAssert(AuditLogable event) {
         event.setStorageDomainId(Guid.createGuidFromString("11111111-1111-1111-1111-111111111111"));
         event.setStoragePoolId(Guid.createGuidFromString("22222222-2222-2222-2222-222222222222"));
         event.setUserId(Guid.createGuidFromString("33333333-3333-3333-3333-333333333333"));
@@ -44,13 +54,13 @@ public class EventKeyComposerTest {
         String composedObjectId = EventKeyComposer.composeObjectId(event, AuditLogType.USER_RUN_VM);
 
         assertEquals("type=USER_RUN_VM," +
-                     "sd=11111111-1111-1111-1111-111111111111," +
-                     "dc=22222222-2222-2222-2222-222222222222," +
-                     "user=33333333-3333-3333-3333-333333333333," +
-                     "cluster=44444444-4444-4444-4444-444444444444," +
-                     "vds=55555555-5555-5555-5555-555555555555," +
-                     "vm=66666666-6666-6666-6666-666666666666," +
-                     "template=77777777-7777-7777-7777-777777777777," +
-                     "customId=123456", composedObjectId);
+                "sd=11111111-1111-1111-1111-111111111111," +
+                "dc=22222222-2222-2222-2222-222222222222," +
+                "user=33333333-3333-3333-3333-333333333333," +
+                "cluster=44444444-4444-4444-4444-444444444444," +
+                "vds=55555555-5555-5555-5555-555555555555," +
+                "vm=66666666-6666-6666-6666-666666666666," +
+                "template=77777777-7777-7777-7777-777777777777," +
+                "customId=123456", composedObjectId);
     }
 }
