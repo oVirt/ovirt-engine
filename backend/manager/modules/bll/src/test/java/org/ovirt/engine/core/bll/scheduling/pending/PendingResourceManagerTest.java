c16e66e7dbad7836bd59bafd4af01a998d20829b
Andrej Krejcir <akrejcir@redhat.com>
2017-05-25 13:40:13 +0200

    core: PendingResourceManager uses atomic compute methods on maps
    Access to resource sets inside maps is done only through
    atomic compute methods.
    
    Change-Id: I0f45916bef33b0f11d95137c3dc73062a78b8c3d
    Bug-Url: https://bugzilla.redhat.com/1430285
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/pending/PendingResourceManagerTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/scheduling/pending/PendingResourceManagerTest.java
@@ -1,7 +1,9 @@
 package org.ovirt.engine.core.bll.scheduling.pending;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
@@ -122,5 +124,77 @@ public class PendingResourceManagerTest {
 
         assertEquals(host1.getId(), PendingVM.getScheduledHost(manager, vm1));
         assertEquals(host2.getId(), PendingVM.getScheduledHost(manager, vm2));
+    }
+
+    @Test
+    public void testGetPendingHostResources() {
+        PendingResourceManager manager = new PendingResourceManager();
+
+        VDS host1 = new VDS();
+        host1.setId(Guid.newGuid());
+
+        VDS host2 = new VDS();
+        host2.setId(Guid.newGuid());
+
+        VM vm1 = new VM();
+        vm1.setId(Guid.newGuid());
+
+        VM vm2 = new VM();
+        vm2.setId(Guid.newGuid());
+
+        VM vm3 = new VM();
+        vm3.setId(Guid.newGuid());
+
+        manager.addPending(new PendingVM(host1, vm1));
+        manager.addPending(new PendingMemory(host1, vm1, 768));
+        manager.addPending(new PendingCpuCores(host1, vm1, 1));
+
+        manager.addPending(new PendingVM(host2, vm2));
+        manager.addPending(new PendingMemory(host2, vm2, 1024));
+        manager.addPending(new PendingCpuCores(host2, vm2, 10));
+
+        manager.addPending(new PendingVM(host1, vm3));
+        manager.addPending(new PendingMemory(host1, vm3, 1024));
+        manager.addPending(new PendingCpuCores(host1, vm3, 10));
+
+
+        List<PendingVM> vms = manager.pendingHostResources(host1.getId(), PendingVM.class);
+
+        assertThat(vms)
+                .hasSize(2)
+                .extracting(PendingResource::getVm)
+                .containsOnly(vm1.getId(), vm3.getId());
+    }
+
+    @Test
+    public void testGetPendingVmResources() {
+        PendingResourceManager manager = new PendingResourceManager();
+
+        VDS host1 = new VDS();
+        host1.setId(Guid.newGuid());
+
+        VDS host2 = new VDS();
+        host2.setId(Guid.newGuid());
+
+        VM vm1 = new VM();
+        vm1.setId(Guid.newGuid());
+
+        VM vm2 = new VM();
+        vm2.setId(Guid.newGuid());
+
+        manager.addPending(new PendingVM(host1, vm1));
+        manager.addPending(new PendingMemory(host1, vm1, 768));
+        manager.addPending(new PendingCpuCores(host1, vm1, 1));
+
+        manager.addPending(new PendingVM(host2, vm2));
+        manager.addPending(new PendingMemory(host2, vm2, 1024));
+        manager.addPending(new PendingCpuCores(host2, vm2, 10));
+
+        List<PendingMemory> memories = manager.pendingVmResources(vm1.getId(), PendingMemory.class);
+
+        assertThat(memories)
+                .hasSize(1)
+                .extracting(PendingMemory::getSizeInMb)
+                .containsOnly(768L);
     }
 }
