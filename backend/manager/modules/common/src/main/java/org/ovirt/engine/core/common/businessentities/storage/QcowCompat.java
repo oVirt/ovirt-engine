c8ec327b7597790d63e3e22dacf54c60948cec5f
Maor Lipchuk <mlipchuk@redhat.com>
2017-06-01 19:53:10 +0300

    core: Add QCOW2 header version for QcowCompat enum.
    Add mapping and field for QcowCompat enum to indicate the QCOW2 header
    version in a QCOW volume.
    
    Change-Id: Ie7a915175324407531a6a87325e30de11a764720
    Bug-Url: https://bugzilla.redhat.com/1456432
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/storage/QcowCompat.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/storage/QcowCompat.java
@@ -3,26 +3,30 @@ package org.ovirt.engine.core.common.businessentities.storage;
 import java.util.HashMap;
 
 public enum QcowCompat {
-    Undefined(0, "Undefined"),
-    QCOW2_V2(1, "0.10"),
-    QCOW2_V3(2, "1.1");
+    Undefined(0, "Undefined", 0),
+    QCOW2_V2(1, "0.10", 2),
+    QCOW2_V3(2, "1.1", 3);
 
     private int value;
     private String compatValue;
+    private int qcowHeaderVersion;
     private static final HashMap<String, QcowCompat> mappingByCompatValue = new HashMap<>();
     private static final HashMap<Integer, QcowCompat> mappingByValue = new HashMap<>();
+    private static final HashMap<Integer, QcowCompat> mappingByQcowHeaderVersion = new HashMap<>();
 
     static {
         QcowCompat[] enumValues = values();
         for (int i = 0; i < enumValues.length; i++) {
             mappingByCompatValue.put(enumValues[i].getCompatValue(), enumValues[i]);
             mappingByValue.put(enumValues[i].getValue(), enumValues[i]);
+            mappingByQcowHeaderVersion.put(enumValues[i].getQcowHeaderVersion(), enumValues[i]);
         }
     }
 
-    QcowCompat(int value, String compatValue) {
+    QcowCompat(int value, String compatValue, int qcowHeaderVersion) {
         this.value = value;
         this.compatValue = compatValue;
+        this.qcowHeaderVersion = qcowHeaderVersion;
     }
 
     public int getValue() {
@@ -33,11 +37,19 @@ public enum QcowCompat {
         return compatValue;
     }
 
+    public int getQcowHeaderVersion() {
+        return qcowHeaderVersion;
+    }
+
     public static QcowCompat forValue(int value) {
         return mappingByValue.get(value);
     }
 
     public static QcowCompat forCompatValue(String compatValue) {
         return mappingByCompatValue.get(compatValue);
+    }
+
+    public static QcowCompat forQcowHeaderVersion(int qcowHeaderVersion) {
+        return mappingByQcowHeaderVersion.get(qcowHeaderVersion);
     }
 }
