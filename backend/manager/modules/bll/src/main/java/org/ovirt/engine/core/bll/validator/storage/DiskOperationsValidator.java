4e2c5bfa581d8bf2f40d569b34ce6ec33fa9dbf1
Tal Nisan <tnisan@redhat.com>
2017-06-05 15:10:22 +0300

    core: Introducing DiskOperationsValidator
    Add DiskOperationsValidator which allows validating whether a command can
    be run on various disk content types instead of validating each content
    type individually
    
    Change-Id: If8e121d8ade58925a45c5832bfe4c93a210de157
    Bug-Url: https://bugzilla.redhat.com/1452988
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/validator/storage/DiskOperationsValidator.java
@@ -0,0 +1,45 @@
+package org.ovirt.engine.core.bll.validator.storage;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ovirt.engine.core.bll.ValidationResult;
+import org.ovirt.engine.core.common.action.VdcActionType;
+import org.ovirt.engine.core.common.businessentities.storage.Disk;
+import org.ovirt.engine.core.common.businessentities.storage.DiskContentType;
+import org.ovirt.engine.core.common.errors.EngineMessage;
+
+public class DiskOperationsValidator {
+
+    static final Map<VdcActionType, List<DiskContentType>> allowedCommandsOnTypes = new HashMap<>();
+    static {
+        allowedCommandsOnTypes.put(VdcActionType.RemoveDisk, Arrays.asList(DiskContentType.DATA, DiskContentType.OVF_STORE));
+        allowedCommandsOnTypes.put(VdcActionType.UpdateVmDisk, Arrays.asList(DiskContentType.DATA));
+        allowedCommandsOnTypes.put(VdcActionType.AttachDiskToVm, Arrays.asList(DiskContentType.DATA));
+        allowedCommandsOnTypes.put(VdcActionType.MoveOrCopyDisk, Arrays.asList(DiskContentType.DATA));
+    }
+
+    private Disk disk;
+
+    public DiskOperationsValidator(Disk disk) {
+        this.disk = disk;
+    }
+
+    public ValidationResult isOperationAllowedOnDisk(VdcActionType actionType) {
+        List<DiskContentType> allowedTypes = allowedCommandsOnTypes.get(actionType);
+        if (allowedTypes == null) {
+            throw new IllegalArgumentException(
+                    String.format("Cannot validate operation on disk for action of type %s as it is not an allowed command for the disk content type.", actionType));
+        }
+
+        if (!allowedTypes.contains(disk.getContentType())) {
+            return new ValidationResult(EngineMessage.ACTION_TYPE_FAILED_DISK_CONTENT_TYPE_NOT_SUPPORTED_FOR_OPERATION,
+                    String.format("$diskContentType %s", disk.getContentType()));
+        }
+
+        return ValidationResult.VALID;
+    }
+
+}
---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/validator/storage/DiskOperationsValidatorTest.java
@@ -0,0 +1,44 @@
+package org.ovirt.engine.core.bll.validator.storage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.ovirt.engine.core.bll.validator.ValidationResultMatchers.failsWith;
+import static org.ovirt.engine.core.bll.validator.ValidationResultMatchers.isValid;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.ovirt.engine.core.common.action.VdcActionType;
+import org.ovirt.engine.core.common.businessentities.storage.Disk;
+import org.ovirt.engine.core.common.businessentities.storage.DiskContentType;
+import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
+import org.ovirt.engine.core.common.errors.EngineMessage;
+
+public class DiskOperationsValidatorTest {
+
+    @Test
+    public void testAllowedOperations() {
+        Disk disk = new DiskImage();
+        for (Map.Entry<VdcActionType, List<DiskContentType>> entry : DiskOperationsValidator.allowedCommandsOnTypes.entrySet()) {
+            disk.setContentType(entry.getValue().get(0));
+            DiskOperationsValidator validator = new DiskOperationsValidator(disk);
+            assertThat(validator.isOperationAllowedOnDisk(entry.getKey()), isValid());
+        }
+    }
+
+    @Test
+    public void testDisallowedOperations() {
+        Disk disk = new DiskImage();
+        for (Map.Entry<VdcActionType, List<DiskContentType>> entry : DiskOperationsValidator.allowedCommandsOnTypes.entrySet()) {
+            EnumSet<DiskContentType> allowedTypes = EnumSet.copyOf(entry.getValue());
+            EnumSet<DiskContentType> disallowedTypes = EnumSet.complementOf(allowedTypes);
+
+            disk.setContentType(disallowedTypes.iterator().next());
+            DiskOperationsValidator validator = new DiskOperationsValidator(disk);
+            assertThat(validator.isOperationAllowedOnDisk(entry.getKey()),
+                    failsWith(EngineMessage.ACTION_TYPE_FAILED_DISK_CONTENT_TYPE_NOT_SUPPORTED_FOR_OPERATION,
+                            String.format("$diskContentType %s", disk.getContentType())));
+        }
+    }
+}
