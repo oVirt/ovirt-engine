5098c8a7c850c5414cdc55feff6c958f89426e18
Idan Shaby <ishaby@redhat.com>
2017-06-29 16:18:35 +0300

    backend: extract main logic of SyncAllStorageDomainsLuns
    This patch extracts the main logic of SyncAllStorageDomainsLuns to a new
    abstract class, AbstractSyncStorageDomainsLunsCommand, so that new
    classes can extend it and reuse the common code.
    
    Change-Id: I3af0e9a7c60461ae7e7a7cad607778ad0a1680f4
    Signed-off-by: Idan Shaby <ishaby@redhat.com>

---
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/pool/AbstractSyncStorageDomainsLunsCommand.java
@@ -0,0 +1,79 @@
+package org.ovirt.engine.core.bll.storage.pool;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.inject.Inject;
+
+import org.ovirt.engine.core.bll.context.CommandContext;
+import org.ovirt.engine.core.bll.storage.utils.VdsCommandsHelper;
+import org.ovirt.engine.core.common.AuditLogType;
+import org.ovirt.engine.core.common.action.ActionType;
+import org.ovirt.engine.core.common.action.SyncLunsInfoForBlockStorageDomainParameters;
+import org.ovirt.engine.core.common.action.SyncLunsParameters;
+import org.ovirt.engine.core.common.businessentities.StorageDomain;
+import org.ovirt.engine.core.common.businessentities.storage.LUNs;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dal.dbbroker.auditloghandling.AuditLogDirector;
+
+public abstract class AbstractSyncStorageDomainsLunsCommand<T extends SyncLunsParameters>
+        extends AbstractSyncLunsCommand<T> {
+
+    @Inject
+    private AuditLogDirector auditLogDirector;
+
+    public AbstractSyncStorageDomainsLunsCommand(T parameters, CommandContext cmdContext) {
+        super(parameters, cmdContext);
+    }
+
+    @Override
+    protected void executeCommand() {
+        List<Guid> unSyncedStorageDomains = syncStorageDomains();
+        if (!unSyncedStorageDomains.isEmpty()) {
+            addCustomValue("StorageDomainsIds",
+                    unSyncedStorageDomains.stream().map(Guid::toString).collect(Collectors.joining(", ")));
+            auditLogDirector.log(this, AuditLogType.STORAGE_DOMAINS_COULD_NOT_BE_SYNCED);
+        }
+
+        setSucceeded(true);
+    }
+
+    /**
+     * Synchronizes the storage domains and returns a list containing
+     * the IDs of the storage domains that have failed to sync.
+     */
+    protected List<Guid> syncStorageDomains() {
+        return getLunsGroupedByStorageDomainId()
+                .entrySet()
+                .stream()
+                .filter(sdLuns -> !runSyncLunsInfoForBlockStorageDomain(sdLuns.getKey(), sdLuns.getValue()))
+                .map(Map.Entry::getKey)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    protected Map<Guid, List<LUNs>> getLunsGroupedByStorageDomainId() {
+        Map<String, StorageDomain> storageDomainsByVirtualGroup = getStorageDomainsToSync()
+                .collect(Collectors.toMap(StorageDomain::getStorage, Function.identity()));
+        return getDeviceList()
+                .stream()
+                .filter(lun -> storageDomainsByVirtualGroup.containsKey(lun.getVolumeGroupId()))
+                .peek(lun -> lun.setStorageDomainId(storageDomainsByVirtualGroup.get(lun.getVolumeGroupId()).getId()))
+                .collect(Collectors.groupingBy(LUNs::getStorageDomainId));
+    }
+
+    protected boolean runSyncLunsInfoForBlockStorageDomain(Guid storageDomainId,
+            List<LUNs> storageDomainLuns) {
+        return runInternalAction(ActionType.SyncLunsInfoForBlockStorageDomain,
+                new SyncLunsInfoForBlockStorageDomainParameters(
+                        storageDomainId,
+                        VdsCommandsHelper.getHostForExecution(getParameters().getStoragePoolId()),
+                        storageDomainLuns))
+                .getSucceeded();
+    }
+
+    protected abstract Stream<StorageDomain> getStorageDomainsToSync();
+}
---
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/storage/pool/AbstractSyncStorageDomainsLunsCommandTest.java
@@ -0,0 +1,111 @@
+package org.ovirt.engine.core.bll.storage.pool;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.ovirt.engine.core.common.action.SyncLunsParameters;
+import org.ovirt.engine.core.common.businessentities.StorageDomain;
+import org.ovirt.engine.core.common.businessentities.StorageDomainStatus;
+import org.ovirt.engine.core.common.businessentities.storage.LUNs;
+import org.ovirt.engine.core.common.businessentities.storage.StorageType;
+import org.ovirt.engine.core.compat.Guid;
+
+public class AbstractSyncStorageDomainsLunsCommandTest {
+
+    private StorageDomain blockSd1;
+    private StorageDomain blockSd2;
+    private Guid blockSd1Id;
+    private Guid blockSd2Id;
+    private LUNs blockSd1Lun1;
+    private LUNs blockSd2Lun1;
+    private LUNs blockSd2Lun2;
+
+    private SyncLunsParameters parameters = new SyncLunsParameters();
+
+    @SuppressWarnings("unchecked")
+    @InjectMocks
+    private AbstractSyncStorageDomainsLunsCommand<SyncLunsParameters> command = mock(
+            AbstractSyncStorageDomainsLunsCommand.class,
+            withSettings().useConstructor(parameters, null).defaultAnswer(CALLS_REAL_METHODS));
+
+    @Before
+    public void setUp() {
+        blockSd1Id = Guid.newGuid();
+        String sd1Name = "vg1";
+        blockSd1 = new StorageDomain();
+        blockSd1.setId(blockSd1Id);
+        blockSd1.setStorageType(StorageType.ISCSI);
+        blockSd1.setStatus(StorageDomainStatus.Active);
+        blockSd1.setStorage(sd1Name);
+        blockSd1Lun1 = new LUNs();
+        blockSd1Lun1.setId("blockSd1Lun1");
+        blockSd1Lun1.setVolumeGroupId(sd1Name);
+        blockSd1Lun1.setStorageDomainId(blockSd1Id);
+
+        blockSd2Id = Guid.newGuid();
+        String sd2Name = "vg2";
+        blockSd2 = new StorageDomain();
+        blockSd2.setId(blockSd2Id);
+        blockSd2.setStorageType(StorageType.FCP);
+        blockSd2.setStatus(StorageDomainStatus.Active);
+        blockSd2.setStorage(sd2Name);
+        blockSd2Lun1 = new LUNs();
+        blockSd2Lun1.setId("blockSd2Lun1");
+        blockSd2Lun2 = new LUNs();
+        blockSd2Lun2.setId("blockSd2Lun2");
+        blockSd2Lun1.setVolumeGroupId(sd2Name);
+        blockSd2Lun1.setStorageDomainId(blockSd2Id);
+        blockSd2Lun2.setVolumeGroupId(sd2Name);
+        blockSd2Lun2.setStorageDomainId(blockSd2Id);
+    }
+
+    @Test
+    public void getLunsGroupedByStorageDomainId() {
+        command.getParameters().setDeviceList(Arrays.asList(
+                blockSd1Lun1, blockSd2Lun1, blockSd2Lun2, // Luns which are a part of storage domains.
+                new LUNs(), new LUNs())); // Luns which are not a part of storage domains.
+        doReturn(Stream.of(blockSd1, blockSd2)).when(command).getStorageDomainsToSync();
+        Map<Guid, List<LUNs>> lunsGroupedByStorageDomainId = command.getLunsGroupedByStorageDomainId();
+
+        assertEquals(2, lunsGroupedByStorageDomainId.values().size());
+        assertEquals(Collections.singletonList(blockSd1Lun1), lunsGroupedByStorageDomainId.get(blockSd1Id));
+        assertEquals(Arrays.asList(blockSd2Lun1, blockSd2Lun2), lunsGroupedByStorageDomainId.get(blockSd2Id));
+    }
+
+    @Test
+    public void syncStorageDomains() {
+        Map<Guid, List<LUNs>> lunsGroupedByStorageDomainId = new HashMap<>();
+        List<LUNs> sd1Luns = Collections.singletonList(blockSd1Lun1);
+        List<LUNs> sd2Luns = Arrays.asList(blockSd2Lun1, blockSd2Lun2);
+        lunsGroupedByStorageDomainId.put(blockSd1Id, sd1Luns);
+        lunsGroupedByStorageDomainId.put(blockSd2Id, sd2Luns);
+        doReturn(lunsGroupedByStorageDomainId).when(command).getLunsGroupedByStorageDomainId();
+
+        doReturn(true).when(command).runSyncLunsInfoForBlockStorageDomain(blockSd1Id, sd1Luns);
+        doReturn(true).when(command).runSyncLunsInfoForBlockStorageDomain(blockSd2Id, sd2Luns);
+        assertEquals("All luns should have been synchronized.", Collections.emptyList(), command.syncStorageDomains());
+
+        doReturn(false).when(command).runSyncLunsInfoForBlockStorageDomain(blockSd1Id, sd1Luns);
+        assertEquals("Storage domain with id " + blockSd1Id + " should have failed to get synchronized.",
+                Collections.singletonList(blockSd1Id), command.syncStorageDomains());
+
+        doReturn(false).when(command).runSyncLunsInfoForBlockStorageDomain(blockSd2Id, sd2Luns);
+        assertEquals("Both storage domains should have failed to get synchronized.",
+                Stream.of(blockSd1Id, blockSd2Id).sorted().collect(Collectors.toList()), command.syncStorageDomains());
+    }
+
+}
