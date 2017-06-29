1d84072b1853744217ec909404c9c1f1531c45a2
Martin Betak <mbetak@redhat.com>
2017-06-29 13:18:00 +0200

    core: Fix naming convention in SynchronizeNumberOfAsyncOperations
    Removed leading `_` from fields, also made them final.
    
    Change-Id: Ia77341276c2aaf16e07f06adf84fd2880716947a
    Signed-off-by: Martin Betak <mbetak@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/SynchronizeNumberOfAsyncOperations.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/SynchronizeNumberOfAsyncOperations.java
@@ -7,24 +7,24 @@ import java.util.concurrent.Callable;
 import org.ovirt.engine.core.utils.threadpool.ThreadPoolUtil;
 
 public final class SynchronizeNumberOfAsyncOperations {
-    private int _numberOfOperations;
-    private ISingleAsyncOperationFactory _factory;
+    private final int numberOfOperations;
+    private final ISingleAsyncOperationFactory factory;
 
     public SynchronizeNumberOfAsyncOperations(int numberOfOperations, ArrayList<?> parameters,
                                               ISingleAsyncOperationFactory factory) {
-        _numberOfOperations = numberOfOperations;
-        _factory = factory;
-        _factory.initialize(parameters);
+        this.numberOfOperations = numberOfOperations;
+        this.factory = factory;
+        this.factory.initialize(parameters);
     }
 
     public void execute() {
 
         List<AsyncOpThread> operations = new ArrayList<>();
-        for (int i = 0; i < _numberOfOperations; i++) {
+        for (int i = 0; i < numberOfOperations; i++) {
             operations.add(new AsyncOpThread(i));
         }
 
-        if (_numberOfOperations > 0) {
+        if (numberOfOperations > 0) {
             ThreadPoolUtil.invokeAll(operations);
         }
     }
@@ -39,7 +39,7 @@ public final class SynchronizeNumberOfAsyncOperations {
 
         @Override
         public Void call() {
-            ISingleAsyncOperation operation = _factory.createSingleAsyncOperation();
+            ISingleAsyncOperation operation = factory.createSingleAsyncOperation();
             operation.execute(currentEventId);
             return null;
         }
