a1ba045cb8cfbef6739038cab076f94a97f903b6
Tomas Jelinek <tjelinek@redhat.com>
2017-07-13 09:39:16 +0200

    core: added utility methods for huge pages
    It will mostly be used by the scheduler to know where to schedule this VM to.
    
    Added two methods:
    - isBackedByHudepages: returns true if the VM has a custom property defined and
      it is a valid integer
    - getHugePages: returns a map "huge page size" -> "required amount". It returns
      it in this format and not as a HugePage object to easy the processing by the
      scheduler.
    
    Implemented in a separate utility class because it can not be part of the
    VMBase due to it's dependency on CustomPropertiesUtil and it's transitive
    dependencies.
    
    Change-Id: I66fec5a5b367ddadcee8033b4a8932de5be23e57
    Bug-Url: https://bugzilla.redhat.com/??????
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/HugePageUtils.java
@@ -0,0 +1,57 @@
+package org.ovirt.engine.core.common.utils;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.inject.Singleton;
+
+import org.ovirt.engine.core.common.businessentities.VmBase;
+import org.ovirt.engine.core.common.utils.customprop.SimpleCustomPropertiesUtil;
+
+@Singleton
+public class HugePageUtils {
+
+    /**
+     * Returns true iff there is a defined "hugepages" custom property with an integer value
+     */
+    public boolean isBackedByHudepages(VmBase vm) {
+        if (vm.getCustomProperties() == null || vm.getCustomProperties().isEmpty()) {
+            return false;
+        }
+
+        String hugePage = getHugePageSize(vm);
+
+        if (hugePage == null) {
+            return false;
+        }
+
+        try {
+            Integer.parseInt(hugePage);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Returns a map of:
+     * huge page size -> required amount of such huge pages
+     */
+    public Map<Integer, Integer> getHugePages(VmBase vm) {
+        if (!isBackedByHudepages(vm)) {
+            return Collections.EMPTY_MAP;
+        }
+
+        int hugePageSize = Integer.parseInt(getHugePageSize(vm));
+
+        int fullPages = vm.getMemSizeMb() / hugePageSize;
+        int lastPage = Math.min(1, vm.getMemSizeMb() % hugePageSize);
+        return Collections.singletonMap(hugePageSize, fullPages + lastPage);
+    }
+
+    private String getHugePageSize(VmBase vm) {
+        SimpleCustomPropertiesUtil util = SimpleCustomPropertiesUtil.getInstance();
+        Map<String, String> customProperties = util.convertProperties(vm.getCustomProperties());
+        return customProperties.get("hugepages");
+    }
+}
---
+++ backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/utils/HugePageUtilsTest.java
@@ -0,0 +1,69 @@
+package org.ovirt.engine.core.common.utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Test;
+import org.ovirt.engine.core.common.businessentities.VmBase;
+
+
+public class HugePageUtilsTest {
+
+    @Test
+    public void isBackedByHudepagesEmptyCustomProperties() {
+        testIsBackedByHudepagesHugePage("", false);
+    }
+
+    @Test
+    public void isBackedByHudepagesCustomPropertySetButNotTheHugepage() {
+        testIsBackedByHudepagesHugePage("sap_agent=true", false);
+    }
+
+    @Test
+    public void isBackedByHudepagesHugePagePropertySet() {
+        testIsBackedByHudepagesHugePage("hugepages=1024", true);
+    }
+
+    @Test
+    public void isBackedByHudepagesHugePagePropertySetToEmpty() {
+        testIsBackedByHudepagesHugePage("hugepages=undefined", false);
+    }
+
+    @Test
+    public void getHugePagesNoHugePagesDefined() {
+        HugePageUtils utils = new HugePageUtils();
+        VmBase base = new VmBase();
+        base.setMemSizeMb(1025);
+
+        assertThat(utils.getHugePages(base).size(), is(0));
+    }
+
+    @Test
+    public void getHugeMemoryFitsIntoOneHugePage() {
+        HugePageUtils utils = new HugePageUtils();
+        VmBase base = new VmBase();
+        base.setCustomProperties("hugepages=1024");
+        base.setMemSizeMb(1024);
+
+        assertThat(utils.getHugePages(base).size(), is(1));
+        assertThat(utils.getHugePages(base).get(1024), is(1));
+    }
+
+    @Test
+    public void getHugePagesMemoryDoesNotFitIntoOne() {
+        HugePageUtils utils = new HugePageUtils();
+        VmBase base = new VmBase();
+        base.setCustomProperties("hugepages=1024");
+        base.setMemSizeMb(1025);
+
+        assertThat(utils.getHugePages(base).size(), is(1));
+        assertThat(utils.getHugePages(base).get(1024), is(2));
+    }
+
+    private void testIsBackedByHudepagesHugePage(String customProperties, boolean expected) {
+        HugePageUtils utils = new HugePageUtils();
+        VmBase base = new VmBase();
+        base.setCustomProperties(customProperties);
+        assertThat(utils.isBackedByHudepages(base), is(expected));
+    }
+}
