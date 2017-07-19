8d4a84525a23659987ec31e16059a400a33d5b91
Tomas Jelinek <tjelinek@redhat.com>
2017-07-19 09:16:45 +0200

    webadmin: fix NoTrimmingWhitespacesValidation
    The regex did not take into account the possibility that there will be only one
    non-whitespace character.
    
    Change-Id: Iaecb80a81419b234d3062325eef66c975a6cf7ca
    Bug-Url: https://bugzilla.redhat.com/1472143
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/validation/NoTrimmingWhitespacesValidation.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/validation/NoTrimmingWhitespacesValidation.java
@@ -5,7 +5,7 @@ import org.ovirt.engine.ui.uicompat.ConstantsManager;
 public class NoTrimmingWhitespacesValidation extends RegexValidation {
 
     public NoTrimmingWhitespacesValidation() {
-        setExpression("^$|^\\S.*\\S$"); //$NON-NLS-1$
+        setExpression("^$|^\\S.*\\S$|^\\S+$"); //$NON-NLS-1$
         setMessage(ConstantsManager.getInstance().getConstants().trimmingSpacesInField());
     }
 
