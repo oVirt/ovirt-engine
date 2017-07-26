2ade21422c3f912d028eb03c11aaa16012021eaa
Allon Mureinik <amureini@redhat.com>
2017-07-26 12:02:25 +0300

    core: StorageDomainStaticDaoTest to extend BaseGenericDaoTestCase
    Make StorageDomainStaticDaoTest extend BaseGenericDaoTestCase instead
    of reimplementing its logic.
    
    Change-Id: Iabdf10d6070e80ef823788158fcba61152f8f715
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/StorageDomainStaticDaoTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/StorageDomainStaticDaoTest.java
@@ -14,19 +14,12 @@ import org.ovirt.engine.core.common.businessentities.StorageDomainStatus;
 import org.ovirt.engine.core.common.businessentities.StorageFormatType;
 import org.ovirt.engine.core.compat.Guid;
 
-public class StorageDomainStaticDaoTest extends BaseDaoTestCase {
-    private StorageDomainStaticDao dao;
-    private StorageDomainStatic existingDomain;
-    private StorageDomainStatic newStaticDomain;
+public class StorageDomainStaticDaoTest
+        extends BaseGenericDaoTestCase<Guid, StorageDomainStatic, StorageDomainStaticDao> {
 
     @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
-        dao = dbFacade.getStorageDomainStaticDao();
-        existingDomain = dao.get(new Guid("bee623f3-9174-4ffd-aa30-4fb0dc0aa2f6"));
-
-        newStaticDomain = new StorageDomainStatic();
+    protected StorageDomainStatic generateNewEntity() {
+        StorageDomainStatic newStaticDomain = new StorageDomainStatic();
         newStaticDomain.setStorageName("NewStorageDomain");
         newStaticDomain.setStorage("fDMzhE-wx3s-zo3q-Qcxd-T0li-yoYU-QvVePl");
         newStaticDomain.setStorageFormat(StorageFormatType.V1);
@@ -37,38 +30,38 @@ public class StorageDomainStaticDaoTest extends BaseDaoTestCase {
         newStaticDomain.setFirstMetadataDevice(FixturesTool.LUN_ID2);
         newStaticDomain.setVgMetadataDevice(FixturesTool.LUN_ID2);
         newStaticDomain.setBackup(false);
+        return newStaticDomain;
     }
 
-    /**
-     * Ensures that null is returned when the id is invalid.
-     */
-    @Test
-    public void testGetWithInvalidId() {
-        StorageDomainStatic result = dao.get(Guid.newGuid());
-
-        assertNull(result);
+    @Override
+    protected void updateExistingEntity() {
+        existingEntity.setStorageName("UpdatedName");
+        existingEntity.setWipeAfterDelete(true);
+        existingEntity.setWarningLowSpaceIndicator(4);
+        existingEntity.setCriticalSpaceActionBlocker(8);
+        existingEntity.setFirstMetadataDevice(FixturesTool.LUN_ID1);
+        existingEntity.setVgMetadataDevice(FixturesTool.LUN_ID1);
+        existingEntity.setBackup(true);
     }
 
-    /**
-     * Ensures that retrieving the static domain works as expected.
-     */
-    @Test
-    public void testGet() {
-        StorageDomainStatic result = dao.get(existingDomain.getId());
-
-        assertNotNull(result);
-        assertEquals(existingDomain.getId(), result.getId());
+    @Override
+    protected Guid getExistingEntityId() {
+        return new Guid("bee623f3-9174-4ffd-aa30-4fb0dc0aa2f6");
     }
 
-    /**
-     * Ensures that get all is not implemented.
-     */
-    @Test
-    public void testGetAll() {
-        List<StorageDomainStatic> result = dao.getAll();
+    @Override
+    protected StorageDomainStaticDao prepareDao() {
+        return dbFacade.getStorageDomainStaticDao();
+    }
 
-        assertNotNull(result);
-        assertFalse(result.isEmpty());
+    @Override
+    protected Guid generateNonExistingId() {
+        return Guid.newGuid();
+    }
+
+    @Override
+    protected int getEntitiesTotalCount() {
+        return 12;
     }
 
     /**
@@ -86,11 +79,10 @@ public class StorageDomainStaticDaoTest extends BaseDaoTestCase {
      */
     @Test
     public void testGetByName() {
-        StorageDomainStatic result = dao.getByName(existingDomain
-                .getStorageName());
+        StorageDomainStatic result = dao.getByName(existingEntity.getStorageName());
 
         assertNotNull(result);
-        assertEquals(existingDomain.getId(), result.getId());
+        assertEquals(existingEntity.getId(), result.getId());
     }
 
     /**
@@ -143,48 +135,4 @@ public class StorageDomainStaticDaoTest extends BaseDaoTestCase {
             assertTrue(!Guid.Empty.equals(id));
         }
     }
-
-    /**
-     * Ensures that saving a domain works as expected.
-     */
-    @Test
-    public void testSave() {
-        dao.save(newStaticDomain);
-
-        StorageDomainStatic result = dao.get(newStaticDomain.getId());
-
-        assertNotNull(result);
-    }
-
-    /**
-     * Ensures that updating the static and dynamic portions works as expected.
-     */
-    @Test
-    public void testUpdate() {
-        existingDomain.setStorageName("UpdatedName");
-        existingDomain.setWipeAfterDelete(true);
-        existingDomain.setWarningLowSpaceIndicator(4);
-        existingDomain.setCriticalSpaceActionBlocker(8);
-        existingDomain.setFirstMetadataDevice(FixturesTool.LUN_ID1);
-        existingDomain.setVgMetadataDevice(FixturesTool.LUN_ID1);
-        existingDomain.setBackup(true);
-        dao.update(existingDomain);
-
-        StorageDomainStatic after = dao.get(existingDomain.getId());
-
-        assertEquals(after, existingDomain);
-    }
-
-    /**
-     * Ensures that removing a storage domain works as expected.
-     */
-    @Test
-    public void testRemove() {
-        dao.remove(existingDomain.getId());
-
-        StorageDomainStatic domainResult = dao.get(existingDomain.getId());
-
-        assertNull(domainResult);
-    }
-
 }
