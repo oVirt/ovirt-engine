65d19d9282d512efd9f3a972ed65fa2c58876a32
Allon Mureinik <amureini@redhat.com>
2017-07-27 13:56:04 +0300

    core: StorageServerConnectionExtensionDaoTest to extend BaseGenericDaoTestCase
    Make StorageServerConnectionExtensionDaoTest extend
    BaseGenericDaoTestCase instead of reimplementing its logic.
    
    Change-Id: I6e91f55864005cfed52fefc37c112b8cc2489cbf
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/StorageServerConnectionExtensionDaoTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/StorageServerConnectionExtensionDaoTest.java
@@ -1,79 +1,54 @@
 package org.ovirt.engine.core.dao;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
-import java.util.List;
-
 import org.junit.Rule;
-import org.junit.Test;
 import org.ovirt.engine.core.common.businessentities.storage.StorageServerConnectionExtension;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.utils.RandomUtils;
 import org.ovirt.engine.core.utils.RandomUtilsSeedingRule;
 
-public class StorageServerConnectionExtensionDaoTest extends BaseDaoTestCase {
+public class StorageServerConnectionExtensionDaoTest
+        extends BaseGenericDaoTestCase<Guid, StorageServerConnectionExtension, StorageServerConnectionExtensionDao> {
 
     @Rule
     public final RandomUtilsSeedingRule rusr = new RandomUtilsSeedingRule();
 
-    private StorageServerConnectionExtensionDao dao;
-
-    private static final Guid EXISTING_STORAGE_SERVER_CONNECTION_EXTENSION_ID = new Guid("9f0852ba-7c96-4974-9cb8-b214a6bf90d8");
-    private static final int NUM_OF_EXISTING_STORAGE_SERVER_CONNECTION_EXTENSIONS = 2;
-
-    @Override public void setUp() throws Exception {
-        super.setUp();
-        dao = dbFacade.getStorageServerConnectionExtensionDao();
-    }
-
-    @Test
-    public void testGetExisting() {
-        StorageServerConnectionExtension ssce = dao.get(EXISTING_STORAGE_SERVER_CONNECTION_EXTENSION_ID);
-        assertNotNull(ssce);
-        assertEquals(EXISTING_STORAGE_SERVER_CONNECTION_EXTENSION_ID, ssce.getId());
-    }
-
-    @Test
-    public void testGetNonExisting() {
-        StorageServerConnectionExtension ssce = dao.get(Guid.newGuid());
-        assertNull(ssce);
-    }
-
-    @Test
-    public void testUpdate() {
-        StorageServerConnectionExtension ssce = dao.get(EXISTING_STORAGE_SERVER_CONNECTION_EXTENSION_ID);
-        fillWithRandomData(ssce);
-        dao.update(ssce);
-
-        StorageServerConnectionExtension ssceFromDb = dao.get(EXISTING_STORAGE_SERVER_CONNECTION_EXTENSION_ID);
-        assertEquals(ssce, ssceFromDb);
-    }
-
-    @Test
-    public void testRemove() {
-        dao.remove(EXISTING_STORAGE_SERVER_CONNECTION_EXTENSION_ID);
-        StorageServerConnectionExtension ssce = dao.get(EXISTING_STORAGE_SERVER_CONNECTION_EXTENSION_ID);
-        assertNull(ssce);
-    }
-
-    @Test
-    public void testInsert() {
+    @Override
+    protected StorageServerConnectionExtension generateNewEntity() {
         Guid newId = Guid.newGuid();
         StorageServerConnectionExtension newssce = new StorageServerConnectionExtension();
         newssce.setId(newId);
         fillWithRandomData(newssce);
-        dao.save(newssce);
-
-        StorageServerConnectionExtension ssceFromDb = dao.get(newId);
-        assertEquals(newssce, ssceFromDb);
+        return newssce;
     }
 
-    @Test
-    public void testGetAll() {
-        List<StorageServerConnectionExtension> results = dao.getAll();
-        assertEquals(NUM_OF_EXISTING_STORAGE_SERVER_CONNECTION_EXTENSIONS, results.size());
+    @Override
+    protected void updateExistingEntity() {
+        fillWithRandomData(existingEntity);
+    }
+
+    @Override
+    protected Guid getExistingEntityId() {
+        return new Guid("9f0852ba-7c96-4974-9cb8-b214a6bf90d8");
+    }
+
+    @Override
+    protected StorageServerConnectionExtensionDao prepareDao() {
+        return dbFacade.getStorageServerConnectionExtensionDao();
+    }
+
+    @Override
+    protected Guid generateNonExistingId() {
+        return Guid.newGuid();
+    }
+
+    @Override
+    protected int getEntitiesTotalCount() {
+        return 2;
+    }
+
+    @Override public void setUp() throws Exception {
+        super.setUp();
+        dao = dbFacade.getStorageServerConnectionExtensionDao();
     }
 
     private void fillWithRandomData(StorageServerConnectionExtension ssce) {
