c48b0f7b9c90e354c6a963d92192784944570f72
Allon Mureinik <amureini@redhat.com>
2017-07-27 14:09:58 +0300

    core: StorageServerConnectionLunMapDaoTest to extend BaseGenericDaoTestCase
    Make StorageServerConnectionLunMapDaoTest extend
    BaseGenericDaoTestCase instead of reimplementing its logic.
    
    Change-Id: I01cb829beff56442f9a5f02c73b6429664452a5c
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/StorageServerConnectionLunMapDaoTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/StorageServerConnectionLunMapDaoTest.java
@@ -6,61 +6,64 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.ovirt.engine.core.common.businessentities.storage.LUNStorageServerConnectionMap;
 import org.ovirt.engine.core.common.businessentities.storage.LUNStorageServerConnectionMapId;
 
-public class StorageServerConnectionLunMapDaoTest extends BaseDaoTestCase {
-    private static final int NUM_LUN_MAPS = 6;
-
-    private StorageServerConnectionLunMapDao dao;
-    private LUNStorageServerConnectionMap existingLUNStorageMap;
-    private LUNStorageServerConnectionMap newLUNStorageMap;
+public class StorageServerConnectionLunMapDaoTest extends BaseGenericDaoTestCase
+        <LUNStorageServerConnectionMapId, LUNStorageServerConnectionMap, StorageServerConnectionLunMapDao> {
 
     @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
-        dao = dbFacade.getStorageServerConnectionLunMapDao();
-
-        existingLUNStorageMap =
-                dao.get(new LUNStorageServerConnectionMapId(FixturesTool.LUN_ID1, FixturesTool.STORAGE_CONNECTION_ID));
-        newLUNStorageMap = new LUNStorageServerConnectionMap(FixturesTool.LUN_ID_FOR_DISK, FixturesTool.STORAGE_CONNECTION_ID);
+    protected LUNStorageServerConnectionMap generateNewEntity() {
+        return new LUNStorageServerConnectionMap(FixturesTool.LUN_ID_FOR_DISK, FixturesTool.STORAGE_CONNECTION_ID);
     }
 
-    @Test
-    public void testGet() {
-        LUNStorageServerConnectionMap result = dao.get(existingLUNStorageMap.getId());
-
-        assertNotNull(result);
-        assertEquals(existingLUNStorageMap, result);
+    @Override
+    protected void updateExistingEntity() {
+        // Unused
     }
 
-    @Test
-    public void testSave() {
-        dao.save(newLUNStorageMap);
+    @Override
+    protected LUNStorageServerConnectionMapId getExistingEntityId() {
+        return new LUNStorageServerConnectionMapId(FixturesTool.LUN_ID1, FixturesTool.STORAGE_CONNECTION_ID);
+    }
 
-        LUNStorageServerConnectionMap result = dao.get(newLUNStorageMap.getId());
+    @Override
+    protected StorageServerConnectionLunMapDao prepareDao() {
+        return dbFacade.getStorageServerConnectionLunMapDao();
+    }
 
-        assertNotNull(result);
-        assertEquals(newLUNStorageMap, result);
+    @Override
+    protected LUNStorageServerConnectionMapId generateNonExistingId() {
+        return new LUNStorageServerConnectionMapId("not", "exists");
+    }
+
+    @Override
+    protected int getEntitiesTotalCount() {
+        return 6;
+    }
+
+    @Ignore
+    @Override
+    public void testUpdate() {
+        // Not supported
+    }
+
+    @Ignore
+    @Override
+    public void testRemove() {
+        // Note supported
     }
 
     @Test
     public void testGetAllByLunId() {
-        List<LUNStorageServerConnectionMap> result =
-                dao.getAll(existingLUNStorageMap.getId().lunId);
+        List<LUNStorageServerConnectionMap> result = dao.getAll(existingEntity.getId().lunId);
 
         assertNotNull(result);
         assertFalse(result.isEmpty());
         for (LUNStorageServerConnectionMap mapping : result) {
-            assertEquals(existingLUNStorageMap.getId().lunId, mapping.getId().lunId);
+            assertEquals(existingEntity.getId().lunId, mapping.getId().lunId);
         }
-    }
-
-    @Test
-    public void testGetAll() {
-        List<LUNStorageServerConnectionMap> result = dao.getAll();
-        assertEquals(NUM_LUN_MAPS, result.size());
     }
 }
