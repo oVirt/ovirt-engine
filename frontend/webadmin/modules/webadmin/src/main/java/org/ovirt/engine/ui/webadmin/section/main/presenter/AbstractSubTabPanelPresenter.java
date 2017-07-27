e678e284c8083198f66e8bcef4bd5ffbe1bf5ab9
Vojtech Szocs <vszocs@redhat.com>
2017-07-27 17:35:08 +0200

    webadmin: AbstractSubTabPanelPresenter code cleanup
    The getMainTabRequest method isn't defined through an interface
    and isn't overridden by subclasses. It's only called within the
    AbstractSubTabPanelPresenter class itself.
    
    Therefore, the condition part `getMainTabRequest() != null` will
    always be false, and the code below will never execute.
    
    Change-Id: I1f6211ac84d331f5d23b89be632cd934e0d60395
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/AbstractSubTabPanelPresenter.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/section/main/presenter/AbstractSubTabPanelPresenter.java
@@ -18,8 +18,6 @@ import com.gwtplatform.mvp.client.TabView;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.Proxy;
 import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
-import com.gwtplatform.mvp.client.proxy.TabContentProxyPlace;
-import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 
 /**
  * Base class for sub tab panel presenters.
@@ -102,27 +100,6 @@ public abstract class AbstractSubTabPanelPresenter<V extends AbstractSubTabPanel
 
     private void updateTabVisibility(TabData tabData, Model model) {
         getView().setTabVisible(tabData, model.getIsAvailable());
-    }
-
-    // TODO(vs) this currently has no effect and should be removed
-    protected PlaceRequest getMainTabRequest() {
-        return null;
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public void prepareFromRequest(PlaceRequest request) {
-        super.prepareFromRequest(request);
-
-        if (getProxy() instanceof TabContentProxyPlace && getMainTabRequest() != null) {
-            // Reveal presenter only when there is something selected in the main tab
-            if (selectedItems != null && selectedItems.hasSelection()) {
-                ((TabContentProxyPlace<AbstractSubTabPanelPresenter<V, P>>) getProxy()).manualReveal(this);
-            } else {
-                ((TabContentProxyPlace<AbstractSubTabPanelPresenter<V, P>>) getProxy()).manualRevealFailed();
-                placeManager.revealPlace(getMainTabRequest());
-            }
-        }
     }
 
 }
