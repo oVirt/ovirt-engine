5d004900bdecc1a41409515f6c2de70cbe0ea63c
Arik Hadas <ahadas@redhat.com>
2017-07-31 10:48:52 +0300

    core: add 'vdsm-' prefix for display networks
    VDSM used to add the 'vdsm-' to the name of the display network that
    is part of the graphics device definition. We follow the same rule.
    
    Change-Id: Ibae59d9dffaf9456b80c9a7f85dbb0f5c409eda2
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/builder/vminfo/LibvirtVmXmlBuilder.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/builder/vminfo/LibvirtVmXmlBuilder.java
@@ -1191,7 +1191,7 @@ public class LibvirtVmXmlBuilder {
             String displayIp = (String) device.getSpecParams().get("displayIp");
             if (displayIp == null) {
                 writer.writeAttributeString("type", "network");
-                writer.writeAttributeString("network", String.format("DISPLAY-NETWORK:%s", displayNetwork.getName()));
+                writer.writeAttributeString("network", String.format("vdsm-%s", displayNetwork.getName()));
             } else {
                 writer.writeAttributeString("type", "address");
                 writer.writeAttributeString("address", displayIp);
