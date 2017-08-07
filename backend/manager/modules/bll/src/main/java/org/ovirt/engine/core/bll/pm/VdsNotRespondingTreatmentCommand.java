e90e25543d56e78e271df23aac6f5c7d0b350dd5
emesika <emesika@redhat.com>
2017-08-07 17:11:57 +0300

    core: fixing NPE in fencing flow
    This patch fixed an NPE in fencing flow non-responding-treatment.
    Adding a check for NULL resolved the problem.
    
    Change-Id: I70b3e988cdc3392bea05bb58df2f480f73a4112c
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1477700
    Signed-off-by: emesika <emesika@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/pm/VdsNotRespondingTreatmentCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/pm/VdsNotRespondingTreatmentCommand.java
@@ -188,6 +188,7 @@ public class VdsNotRespondingTreatmentCommand<T extends FenceVdsActionParameters
                     getVdsName(), getVdsId());
         }
         if (restartVdsResult != null
+                && restartVdsResult.<RestartVdsResult>getActionReturnValue() != null
                 && restartVdsResult.<RestartVdsResult>getActionReturnValue().isSkippedDueToFencingPolicy()) {
             // fencing was skipped, fire an alert and suppress standard command logging
             AuditLogable alb = createAuditLogableForHost(getVds());
