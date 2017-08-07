dec232d77dade19509a85fcd038c0fde0b789bd3
Allon Mureinik <amureini@redhat.com>
2017-08-07 09:19:04 +0300

    core: StorageServerConnectionDaoTest to extend BaseGenericDaoTestCase
    Make StorageServerConnectionDaoTest extend BaseGenericDaoTestCase
    instead of reimplementing its logic.
    
    Besides cleaning up the code, this moves also increases coverage by
    running the base test case's tests and arguably improves the test's
    performance by consolidating test multiple test scenarios into single
    cases.
    
    Change-Id: If94b58c60ce256ce58d83958845c1a7ee6568260
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/StorageServerConnectionDaoTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/StorageServerConnectionDaoTest.java
@@ -3,7 +3,6 @@ package org.ovirt.engine.core.dao;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -27,46 +26,48 @@ import org.ovirt.engine.core.common.businessentities.storage.LUNStorageServerCon
 import org.ovirt.engine.core.common.businessentities.storage.StorageType;
 import org.ovirt.engine.core.compat.Guid;
 
-public class StorageServerConnectionDaoTest extends BaseDaoTestCase {
+public class StorageServerConnectionDaoTest
+        extends BaseGenericDaoTestCase<String, StorageServerConnections, StorageServerConnectionDao> {
+
     private static final int SERVER_CONNECTION_COUNT_FOR_SPECIFIC_STORAGE = 7;
     private static final String EXISTING_DOMAIN_STORAGE_NAME = "G95OWd-Wvck-vftu-pMq9-9SAC-NF3E-ulDPsQ";
 
-    private StorageServerConnectionDao dao;
-    private StorageServerConnections newServerConnection;
-    private StorageServerConnections existingConnection;
-
     @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
-        dao = dbFacade.getStorageServerConnectionDao();
-
-        existingConnection = dao.get("0cc146e8-e5ed-482c-8814-270bc48c297f");
-
-        newServerConnection = new StorageServerConnections();
+    protected StorageServerConnections generateNewEntity() {
+        StorageServerConnections newServerConnection = new StorageServerConnections();
         newServerConnection.setId("0cc146e8-e5ed-482c-8814-270bc48c2980");
         newServerConnection.setConnection(EXISTING_DOMAIN_STORAGE_NAME);
+        newServerConnection.setNfsVersion(NfsVersion.V4);
+        newServerConnection.setNfsRetrans((short) 5);
+        return newServerConnection;
     }
 
-    /**
-     * Ensures that null is returned when the id is invalid.
-     */
-    @Test
-    public void testGetServerConnectionWithInvalidId() {
-        StorageServerConnections result = dao.get("fakrel");
-
-        assertNull(result);
+    @Override
+    protected void updateExistingEntity() {
+        existingEntity.setIqn("1.2.3.4");
+        existingEntity.setConnection("/host2/lib/data");
+        existingEntity.setNfsRetrans((short) 3);
+        existingEntity.setNfsTimeo((short) 100);
     }
 
-    /**
-     * Ensures retrieving a connection by id works as expected.
-     */
-    @Test
-    public void testGetServerConnection() {
-        StorageServerConnections result = dao.get(existingConnection.getId());
+    @Override
+    protected String getExistingEntityId() {
+        return "0cc146e8-e5ed-482c-8814-270bc48c297f";
+    }
 
-        assertNotNull(result);
-        assertEquals(existingConnection, result);
+    @Override
+    protected StorageServerConnectionDao prepareDao() {
+        return dbFacade.getStorageServerConnectionDao();
+    }
+
+    @Override
+    protected String generateNonExistingId() {
+        return "farkle";
+    }
+
+    @Override
+    protected int getEntitiesTotalCount() {
+        return 11;
     }
 
     @Test
@@ -78,10 +79,10 @@ public class StorageServerConnectionDaoTest extends BaseDaoTestCase {
 
     @Test
     public void testGetForIqn() {
-        StorageServerConnections result = dao.getForIqn(existingConnection.getIqn());
+        StorageServerConnections result = dao.getForIqn(existingEntity.getIqn());
 
         assertNotNull(result);
-        assertEquals(existingConnection, result);
+        assertEquals(existingEntity, result);
     }
 
     @Test
@@ -243,10 +244,10 @@ public class StorageServerConnectionDaoTest extends BaseDaoTestCase {
 
     @Test
     public void getAllForForConnection() {
-        StorageServerConnections conn = dao.get(existingConnection.getId());
+        StorageServerConnections conn = dao.get(existingEntity.getId());
         conn.setId("copy");
         dao.save(conn);
-        assertGetAllForConnectionResult(Arrays.asList(existingConnection, conn), existingConnection);
+        assertGetAllForConnectionResult(Arrays.asList(existingEntity, conn), existingEntity);
     }
 
 
@@ -304,100 +305,15 @@ public class StorageServerConnectionDaoTest extends BaseDaoTestCase {
     }
 
     /**
-     * Ensures saving a connection works as expected.
-     */
-    @Test
-    public void testSaveServerConnection() {
-        StorageServerConnections conn = dao.get(newServerConnection.getId());
-        assertNull(conn);
-
-        dao.save(newServerConnection);
-
-        conn = dao.get(newServerConnection.getId());
-
-        assertEquals(newServerConnection, conn);
-    }
-
-    /**
-     * Ensures updating a connection works as expected.
-     */
-    @Test
-    public void testUpdateIscsiServerConnection() {
-        existingConnection.setIqn("1.2.3.4");
-
-        dao.update(existingConnection);
-
-        StorageServerConnections result = dao.get(existingConnection.getId());
-
-        assertEquals(existingConnection, result);
-    }
-
-    @Test
-    public void testUpdateNfsServerConnection() {
-        //create a new connection
-        StorageServerConnections newNFSServerConnection = new StorageServerConnections();
-        newNFSServerConnection.setId("0cb136e8-e5ed-472b-8914-260bc48c2987");
-        newNFSServerConnection.setStorageType(StorageType.NFS);
-        newNFSServerConnection.setConnection("host/lib/data");
-        newNFSServerConnection.setNfsVersion(NfsVersion.V4);
-        newNFSServerConnection.setNfsRetrans((short) 0);
-        dao.save(newNFSServerConnection);
-
-        //get it from db
-        StorageServerConnections newNFSServerConnectionFromDB = dao.get("0cb136e8-e5ed-472b-8914-260bc48c2987");
-
-        //update its properties and save back to db (update)
-        newNFSServerConnectionFromDB.setConnection("/host2/lib/data");
-        newNFSServerConnectionFromDB.setNfsRetrans((short) 3);
-        newNFSServerConnectionFromDB.setNfsTimeo((short)100);
-        dao.update(newNFSServerConnectionFromDB);
-
-        //get it again after the update
-        StorageServerConnections updatedNFSServerConnectionFromDB = dao.get("0cb136e8-e5ed-472b-8914-260bc48c2987");
-        assertEquals(updatedNFSServerConnectionFromDB.getConnection(), newNFSServerConnectionFromDB.getConnection());
-        assertEquals(updatedNFSServerConnectionFromDB.getId(), newNFSServerConnectionFromDB.getId());
-        assertEquals(updatedNFSServerConnectionFromDB.getNfsRetrans(), newNFSServerConnectionFromDB.getNfsRetrans());
-        assertEquals(updatedNFSServerConnectionFromDB.getNfsTimeo(), newNFSServerConnectionFromDB.getNfsTimeo());
-        assertNotSame(newNFSServerConnection.getConnection(), updatedNFSServerConnectionFromDB.getConnection());
-        //cleanup...
-        dao.remove("0cb136e8-e5ed-472b-8914-260bc48c2987");
-    }
-
-    /**
-     * Ensures removing a connection works as expected.
-     */
-    @Test
-    public void testRemoveServerConnection() {
-        dao.remove(existingConnection.getId());
-
-        StorageServerConnections result = dao.get(existingConnection.getId());
-
-        assertNull(result);
-    }
-
-    /**
      * Ensures NFS options work as expected.
      */
     @Test
     public void testNfsOptions() {
-        newServerConnection.setNfsVersion(NfsVersion.V4);
-        newServerConnection.setNfsRetrans((short)5);
-        dao.save(newServerConnection);
+        StorageServerConnections result = dao.get(FixturesTool.EXISTING_STORAGE_CONNECTION_NFS_AUTO_ID.toString());
 
-        StorageServerConnections result = dao.get(newServerConnection.getId());
-        assertEquals(NfsVersion.V4, result.getNfsVersion());
-        assertEquals(Short.valueOf((short) 5), result.getNfsRetrans());
-        assertNull(result.getNfsTimeo());
-
-        result = dao.get(FixturesTool.EXISTING_STORAGE_CONNECTION_NFS_AUTO_ID.toString());
         assertEquals(NfsVersion.AUTO, result.getNfsVersion());
         assertEquals(Short.valueOf((short) 7), result.getNfsRetrans());
         assertEquals(Short.valueOf((short) 42), result.getNfsTimeo());
-
-        result = dao.get(existingConnection.getId());
-        assertNull(result.getNfsVersion());
-        assertNull(result.getNfsRetrans());
-        assertNull(result.getNfsTimeo());
     }
 
     @Test
