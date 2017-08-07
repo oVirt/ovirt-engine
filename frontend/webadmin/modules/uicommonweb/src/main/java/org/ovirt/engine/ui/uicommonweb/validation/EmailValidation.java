9db180ad85ebc4147a53bb58fa0e26a2ecb30919
Tal Nisan <tnisan@redhat.com>
2017-08-07 14:10:16 +0300

    webadmin: Remove EmailAddress class
    The EmailAddress class sole purpose is to validate an email address when
    called by EmailAddressValidation, the logic was moved there and the class
    was deleted
    
    Change-Id: I8f6022412437dffe93e44480ef0c5bb111586a9e
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/validation/EmailValidation.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/validation/EmailValidation.java
@@ -1,21 +1,26 @@
 package org.ovirt.engine.ui.uicommonweb.validation;
 
+import org.ovirt.engine.core.compat.Regex;
+import org.ovirt.engine.core.compat.RegexOptions;
 import org.ovirt.engine.ui.uicompat.ConstantsManager;
-import org.ovirt.engine.ui.uicompat.MailAddress;
 
 @SuppressWarnings("unused")
 public class EmailValidation implements IValidation {
+
+    private static String ATOM = "[^\\x00-\\x1F^\\(^\\)^\\<^\\>^\\@^\\,^\\;^\\:^\\\\^\\\"^\\.^\\[^\\]^\\s]"; //$NON-NLS-1$
+    private static String DOMAIN = "(" + ATOM + "+(\\." + ATOM + "+)*"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    private static String IP_DOMAIN = "\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\]"; //$NON-NLS-1$
+
+    private static String pattern = "^" + ATOM + "+(\\." + ATOM + "+)*@" + DOMAIN + "|" + IP_DOMAIN + ")$"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+
     @Override
     public ValidationResult validate(Object value) {
-        ValidationResult result = new ValidationResult();
-
-        try {
-            new MailAddress((String) value);
-        } catch (RuntimeException e) {
+        if (!Regex.isMatch((String) value, pattern, RegexOptions.IgnoreCase)) {
+            ValidationResult result = new ValidationResult();
             result.setSuccess(false);
             result.getReasons().add(ConstantsManager.getInstance().getConstants().invalidEmailAddressInvalidReason());
+            return result;
         }
-
-        return result;
+        return ValidationResult.ok();
     }
 }
