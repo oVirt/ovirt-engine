455fb2850f94c5b213f72a1e5d43750743a4ec27
Tal Nisan <tnisan@redhat.com>
2017-08-07 13:25:32 +0300

    core: Remove redundant methods in VdcOperationManager
    Change-Id: Id65a867d04383dcf34e42016901b98b66f7b4618
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/communication/VdcOperationManager.java
+++ frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/communication/VdcOperationManager.java
@@ -105,24 +105,6 @@ public class VdcOperationManager {
     }
 
     /**
-     * Store a value on the back-end in the session.
-     * @param key The key.
-     * @param value The value.
-     */
-    public void storeInHttpSession(final String key, final String value) {
-        processor.storeInHttpSession(key, value);
-    }
-
-    /**
-     * Retrieve a stored value from the back-end session.
-     * @param key The key.
-     * @param callback The callback to call with the value.
-     */
-    public void retrieveFromHttpSession(final String key, final StorageCallback callback) {
-        processor.retrieveFromHttpSession(key, callback);
-    }
-
-    /**
      * Returns {@code true} if execution of given operation caused the Engine session to be refreshed.
      */
     boolean engineSessionRefreshed(VdcOperation<?, ?> operation) {
