3d59cfe506ef985473087922d61d8be68c2955f8
Leon Goldberg <lgoldber@redhat.com>
2017-08-07 17:01:14 +0300

    backend: Add updated size validation to network names.
    Maximum allowed network name size is now 256 characters;
    it is not completely unlimited. Re-add size validations
    for network names with the new appropriate size (256).
    
    Change-Id: Ic6e795d148cc2ac860c3932267ba88113af6695c
    Bug-Id: https://bugzilla.redhat.com/show_bug.cgi?id=1477161
    Signed-off-by: Leon Goldberg <lgoldber@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/network/Network.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/network/Network.java
@@ -26,6 +26,7 @@ public class Network implements Queryable, BusinessEntity<Guid>, Nameable, Comme
 
     private Guid id;
 
+    @Size(max = BusinessEntitiesDefinitions.NETWORK_NAME_SIZE)
     private String name;
 
     private String vdsmName;
