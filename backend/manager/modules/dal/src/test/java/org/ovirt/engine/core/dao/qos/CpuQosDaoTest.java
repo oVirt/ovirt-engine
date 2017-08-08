07a89a5c57490dbc78a5b3c0735d545d40822a51
Allon Mureinik <amureini@redhat.com>
2017-08-08 23:18:21 +0300

    core: CpuQosDaoTest to extend BaseGenericDaoTestCase
    Make CpuQosDaoTest extend BaseGenericDaoTestCase instead of
    reimplementing its logic.
    
    Change-Id: I4ae28c42ea32fbf8fbf9af65d89e99c82a2b2fcf
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/qos/CpuQosDaoTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/qos/CpuQosDaoTest.java
@@ -1,7 +1,6 @@
 package org.ovirt.engine.core.dao.qos;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
@@ -12,75 +11,47 @@ import java.util.Map;
 import org.junit.Test;
 import org.ovirt.engine.core.common.businessentities.qos.CpuQos;
 import org.ovirt.engine.core.compat.Guid;
-import org.ovirt.engine.core.dao.BaseDaoTestCase;
+import org.ovirt.engine.core.dao.BaseGenericDaoTestCase;
 import org.ovirt.engine.core.dao.FixturesTool;
 
-public class CpuQosDaoTest extends BaseDaoTestCase {
-
-    private CpuQosDao dao;
+public class CpuQosDaoTest extends BaseGenericDaoTestCase<Guid, CpuQos, CpuQosDao> {
 
     @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        dao = getDbFacade().getCpuQosDao();
-    }
-
-    /**
-     * Ensures that retrieving with an invalid ID returns null.
-     */
-    @Test
-    public void testGetWithInvalidId() {
-        assertNull(dao.get(Guid.newGuid()));
-    }
-
-    @Test
-    public void getCpuQos() {
-        CpuQos cpuQos = new CpuQos();
-        cpuQos.setId(FixturesTool.QOS_ID_4);
-        cpuQos.setName("qos_d");
-        cpuQos.setDescription("desc1");
-        cpuQos.setStoragePoolId(FixturesTool.STORAGE_POOL_NFS);
-        cpuQos.setCpuLimit(50);
-
-        CpuQos fetched = dao.get(FixturesTool.QOS_ID_4);
-        assertNotNull(fetched);
-        assertEquals(cpuQos, fetched);
-    }
-
-    @Test
-    public void updateCpuQos() {
-        CpuQos cpuQos = dao.get(FixturesTool.QOS_ID_5);
-        assertNotNull(cpuQos);
-        cpuQos.setName("newB");
-        cpuQos.setDescription("desc2");
-        cpuQos.setCpuLimit(30);
-        assertFalse(cpuQos.equals(dao.get(FixturesTool.QOS_ID_5)));
-        dao.update(cpuQos);
-        CpuQos fetched = dao.get(FixturesTool.QOS_ID_5);
-        assertNotNull(fetched);
-        assertEquals(cpuQos, fetched);
-    }
-
-    @Test
-    public void removeCpuQos() {
-        assertNotNull(dao.get(FixturesTool.QOS_ID_6));
-        dao.remove(FixturesTool.QOS_ID_6);
-        assertNull(dao.get(FixturesTool.QOS_ID_6));
-    }
-
-    @Test
-    public void saveCpuQos() {
+    protected CpuQos generateNewEntity() {
         CpuQos cpuQos = new CpuQos();
         cpuQos.setId(Guid.newGuid());
-        assertNull(dao.get(cpuQos.getId()));
         cpuQos.setName("qos_d");
         cpuQos.setDescription("desc3");
         cpuQos.setStoragePoolId(FixturesTool.STORAGE_POOL_MIXED_TYPES);
         cpuQos.setCpuLimit(40);
-        dao.save(cpuQos);
-        CpuQos fetched = dao.get(cpuQos.getId());
-        assertNotNull(fetched);
-        assertEquals(cpuQos, fetched);
+        return cpuQos;
+    }
+
+    @Override
+    protected void updateExistingEntity() {
+        existingEntity.setName("newB");
+        existingEntity.setDescription("desc2");
+        existingEntity.setCpuLimit(30);
+    }
+
+    @Override
+    protected Guid getExistingEntityId() {
+        return FixturesTool.QOS_ID_4;
+    }
+
+    @Override
+    protected CpuQosDao prepareDao() {
+        return dbFacade.getCpuQosDao();
+    }
+
+    @Override
+    protected Guid generateNonExistingId() {
+        return Guid.newGuid();
+    }
+
+    @Override
+    protected int getEntitiesTotalCount() {
+        return 3;
     }
 
     @Test
