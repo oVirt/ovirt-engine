a8031812f5d8439900c5dba4e1bdeaaced765b0c
Marcin Mirecki <mmirecki@redhat.com>
2017-08-10 16:48:17 +0200

    Pass plugin type for each external network allocate request.
    Plugin type must be passed to vdsm whenever
    a port is allocated. The plugin type can be used
    to determine which provider driver will be executed,
    and allow to have multiple drivers installed.
    Currently the drivers have no way of knowing
    which should be handling the nic.
    
    This will also fix the problem where the
    openstacknet driver can not be installed along
    any of the other external network providers.
    The openstack driver is checking for plugin
    type before it decides to run or not to run.
    With no plugin type, the openstacknet hooks
    were failing, even if the nic belonged to
    a different type of provider.
    
    Bug-Url: https://bugzilla.redhat.com/1478823
    
    Change-Id: I4f76d4f4e4e087bc88c9412705c168d4e1fa08c7
    Signed-off-by: Marcin Mirecki <mmirecki@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/network/openstack/OpenstackNetworkProviderProxy.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/network/openstack/OpenstackNetworkProviderProxy.java
@@ -1,25 +1,14 @@
 package org.ovirt.engine.core.bll.provider.network.openstack;
 
-import java.util.Map;
-
 import org.ovirt.engine.core.bll.provider.NetworkProviderValidator;
 import org.ovirt.engine.core.bll.provider.ProviderValidator;
 import org.ovirt.engine.core.common.businessentities.OpenstackNetworkProviderProperties;
 import org.ovirt.engine.core.common.businessentities.Provider;
 
-import com.woorea.openstack.quantum.model.Port;
-
 public class OpenstackNetworkProviderProxy extends BaseNetworkProviderProxy<OpenstackNetworkProviderProperties> {
 
     public OpenstackNetworkProviderProxy(Provider<OpenstackNetworkProviderProperties> provider) {
         super(provider);
-    }
-
-    @Override
-    protected Map<String, String> createPortAllocationRuntimeProperties(Port port) {
-        Map<String, String> runtimeProperties = super.createPortAllocationRuntimeProperties(port);
-        runtimeProperties.put("plugin_type", getProvider().getAdditionalProperties().getPluginType());
-        return runtimeProperties;
     }
 
     @Override
