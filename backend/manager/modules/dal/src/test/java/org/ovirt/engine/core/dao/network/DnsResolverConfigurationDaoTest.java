49d62d6d8d2e54b19a26556d5217e655cae70e40
Allon Mureinik <amureini@redhat.com>
2017-08-10 05:30:02 +0300

    core: DnsResolverConfigurationDaoTest to extend BaseGenericDaoTestCase
    Make DnsResolverConfigurationDaoTest extend BaseGenericDaoTestCase
    instead of reimplementing its logic.
    
    Change-Id: Ib755d5350c2641786216cabec08191b0ee90287d
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/network/DnsResolverConfigurationDaoTest.java
+++ backend/manager/modules/dal/src/test/java/org/ovirt/engine/core/dao/network/DnsResolverConfigurationDaoTest.java
@@ -1,41 +1,51 @@
 package org.ovirt.engine.core.dao.network;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 import org.ovirt.engine.core.common.businessentities.network.DnsResolverConfiguration;
 import org.ovirt.engine.core.common.businessentities.network.NameServer;
-import org.ovirt.engine.core.dao.BaseDaoTestCase;
+import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.BaseGenericDaoTestCase;
 import org.ovirt.engine.core.dao.FixturesTool;
 
-public class DnsResolverConfigurationDaoTest extends BaseDaoTestCase {
-
-    private DnsResolverConfigurationDao dao;
-    private DnsResolverConfiguration dnsResolverConfiguration;
+public class DnsResolverConfigurationDaoTest
+        extends BaseGenericDaoTestCase<Guid, DnsResolverConfiguration, DnsResolverConfigurationDao> {
 
     @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        dao = dbFacade.getDnsResolverConfigurationDao();
-        dnsResolverConfiguration = new DnsResolverConfiguration();
+    protected DnsResolverConfiguration generateNewEntity() {
+        DnsResolverConfiguration dnsResolverConfiguration = new DnsResolverConfiguration();
         dnsResolverConfiguration.setNameServers(new ArrayList<>(Arrays.asList(new NameServer("1.1.1.1"))));
+        return dnsResolverConfiguration;
     }
 
-    /**
-     * Ensures that the DnsResolverConfiguration is returned.
-     */
-    @Test
-    public void testGet() {
-        DnsResolverConfiguration result = dao.get(FixturesTool.EXISTING_DNS_RESOLVER_CONFIGURATION);
+    @Override
+    protected void updateExistingEntity() {
+        existingEntity.getNameServers().add(new NameServer("2.2.2.2"));
+    }
 
-        assertNotNull(result);
-        assertEquals(dnsResolverConfigurationFromFixtures(), result);
+    @Override
+    protected Guid getExistingEntityId() {
+        return FixturesTool.EXISTING_DNS_RESOLVER_CONFIGURATION;
+    }
+
+    @Override
+    protected DnsResolverConfigurationDao prepareDao() {
+        return dbFacade.getDnsResolverConfigurationDao();
+    }
+
+    @Override
+    protected Guid generateNonExistingId() {
+        return Guid.newGuid();
+    }
+
+    @Override
+    protected int getEntitiesTotalCount() {
+        return 2;
     }
 
     public DnsResolverConfiguration dnsResolverConfigurationFromFixtures() {
@@ -48,17 +58,6 @@ public class DnsResolverConfigurationDaoTest extends BaseDaoTestCase {
         ));
 
         return expected;
-    }
-
-    /**
-     * Ensures that a populated collection is returned.
-     */
-    @Test
-    public void testGetAll() {
-        List<DnsResolverConfiguration> result = dao.getAll();
-
-        assertNotNull(result);
-        assertEquals(2, result.size());
     }
 
     @Test
@@ -80,44 +79,5 @@ public class DnsResolverConfigurationDaoTest extends BaseDaoTestCase {
         assertNotNull(dao.get(FixturesTool.EXISTING_DNS_RESOLVER_CONFIGURATION));
         dao.removeByVdsDynamicId(FixturesTool.GLUSTER_SERVER_UUID3);
         assertNull(dao.get(FixturesTool.EXISTING_DNS_RESOLVER_CONFIGURATION));
-    }
-
-    /**
-     * Ensures that the save is working correctly
-     */
-    @Test
-    public void testSave() {
-        dao.save(dnsResolverConfiguration);
-        DnsResolverConfiguration result = dao.get(dnsResolverConfiguration.getId());
-
-        assertNotNull(result);
-        assertEquals(dnsResolverConfiguration, result);
-    }
-
-    /**
-     * Ensures that the update is working correctly
-     */
-    @Test
-    public void testUpdate() {
-        dao.save(dnsResolverConfiguration);
-
-        dnsResolverConfiguration.getNameServers().add(new NameServer("2.2.2.2"));
-
-        dao.update(dnsResolverConfiguration);
-        DnsResolverConfiguration result = dao.get(dnsResolverConfiguration.getId());
-        assertNotNull(result);
-        assertEquals(dnsResolverConfiguration, result);
-    }
-
-    /**
-     * Ensures that the remove is working correctly
-     */
-    @Test
-    public void testRemove() {
-        dao.save(dnsResolverConfiguration);
-        DnsResolverConfiguration result = dao.get(dnsResolverConfiguration.getId());
-        assertNotNull(result);
-        dao.remove(dnsResolverConfiguration.getId());
-        assertNull(dao.get(dnsResolverConfiguration.getId()));
     }
 }
