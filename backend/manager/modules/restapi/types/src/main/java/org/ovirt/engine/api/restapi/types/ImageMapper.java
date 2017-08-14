5e1e11ec6a560c0bc8d2be849b8b5ba6c6427f34
Shani Leviim <sleviim@redhat.com>
2017-08-14 16:50:33 +0300

    restapi: Display image size and type for glance images
    This patch displays image size and type for glance images when using
    RESTAPI. A patch for ovirt-engine-api-model was sent as well.
    
    Change-Id: I1c4277b854ea95c1e920cb43c1f97a238d094da0
    Bug-Url: https://bugzilla.redhat.com/1096497
    Signed-off-by: Shani Leviim <sleviim@redhat.com>

--- backend/manager/modules/restapi/types/src/main/java/org/ovirt/engine/api/restapi/types/ImageMapper.java
+++ backend/manager/modules/restapi/types/src/main/java/org/ovirt/engine/api/restapi/types/ImageMapper.java
@@ -1,14 +1,40 @@
 package org.ovirt.engine.api.restapi.types;
 
 import org.ovirt.engine.api.model.Image;
+import org.ovirt.engine.api.model.ImageFileType;
 import org.ovirt.engine.core.common.businessentities.storage.RepoImage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ImageMapper {
+    private static final Logger log = LoggerFactory.getLogger(ImageMapper.class);
+
     @Mapping(from = RepoImage.class, to = Image.class)
     public static Image map(RepoImage entity, Image template) {
         Image model = template != null ? template : new Image();
         model.setId(entity.getRepoImageId());
         model.setName(entity.getRepoImageName());
+        model.setSize(entity.getSize());
+        model.setType(mapImageType(entity.getFileType()));
+
         return model;
+    }
+
+    public static ImageFileType mapImageType(org.ovirt.engine.core.common.businessentities.storage.ImageFileType imageFileType) {
+        switch (imageFileType) {
+        case ISO:
+            return ImageFileType.ISO;
+        case Floppy:
+            return ImageFileType.FLOPPY;
+        case Disk:
+            return ImageFileType.DISK;
+        case Unknown:
+            // Explicitly map UNKNOWN to null, as it doesn't make
+            // sense in the API.
+            return null;
+        default:
+            log.warn("Don't know how to map image type '{}', will return null.", imageFileType);
+            return null;
+        }
     }
 }
