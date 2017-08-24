e2d79ea8e8de34ccd29894e2c7b4d7903b82286b
Phillip Bailey <phbailey@redhat.com>
2017-08-24 17:31:10 -0400

    webadmin: Create alert with icon widget
    This patch introduces an alert widget that allows for a Patterfly icon
    to be placed to the left of the alert text. The alert type can be
    configured using GWT-Bootstrap AlertTypes.
    
    Change-Id: I1c088862b5196ce085bad1110db26425b53351af
    Bug-Url: https://bugzilla.redhat.com/1231859
    Signed-off-by: Phillip Bailey <phbailey@redhat.com>

---
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/AlertWithIcon.java
@@ -0,0 +1,72 @@
+package org.ovirt.engine.ui.common.widget;
+
+
+import org.gwtbootstrap3.client.ui.Alert;
+import org.gwtbootstrap3.client.ui.constants.AlertType;
+import org.gwtbootstrap3.client.ui.html.Span;
+import org.ovirt.engine.ui.common.css.PatternflyConstants;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ * This widget is to be used to provide alerts to the user. The widget uses Patternfly
+ * icons, which are displayed to the left of the alert text. GWT-Bootstrap AlertTypes
+ * are used to control the color of the alert box and text.
+ */
+public class AlertWithIcon extends Composite {
+
+    interface WidgetUiBinder extends UiBinder<Alert, AlertWithIcon> {
+        WidgetUiBinder uiBinder = GWT.create(WidgetUiBinder.class);
+    }
+
+    @UiField
+    Alert alert;
+
+    @UiField
+    Span iconContainer;
+
+    @UiField
+    Label alertTextLabel;
+
+    public AlertWithIcon() {
+        initWidget(WidgetUiBinder.uiBinder.createAndBindUi(this));
+        setAlertType(AlertType.INFO);
+    }
+
+    public AlertWithIcon(AlertType alertType) {
+        initWidget(WidgetUiBinder.uiBinder.createAndBindUi(this));
+        setAlertType(alertType);
+    }
+
+    public void setAlertType(AlertType alertType) {
+        alert.setType(alertType);
+        iconContainer.addStyleName(PatternflyConstants.PFICON);
+        switch (alertType) {
+            case SUCCESS:
+                iconContainer.addStyleName(PatternflyConstants.PFICON_OK);
+                break;
+            case WARNING:
+                iconContainer.addStyleName(PatternflyConstants.PFICON_WARNING_TRIANGLE_O);
+                break;
+            case DANGER:
+                iconContainer.addStyleName(PatternflyConstants.PFICON_ERROR_CIRCLE_O);
+                break;
+            case INFO:
+            default:
+                iconContainer.addStyleName(PatternflyConstants.PFICON_INFO);
+                break;
+        }
+    }
+
+    public void setText(String alertText) {
+        alertTextLabel.setText(alertText);
+    }
+
+    public Alert asAlert() {
+        return alert;
+    }
+}
