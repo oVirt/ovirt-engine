402b885c348651d114ea2fa8479488b18fec213f
Allon Mureinik <amureini@redhat.com>
2017-08-25 10:37:51 +0300

    core: GuidUtils#getGuidListFromString return type
    Return the List interface instead of the concrete ArrayList type, as
    per Java's best practices.
    
    Change-Id: I38c68cd957aa2e7e3a8e5078c9e2f71abf5219bc
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/GuidUtils.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/GuidUtils.java
@@ -1,7 +1,7 @@
 package org.ovirt.engine.core.utils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -12,16 +12,16 @@ public class GuidUtils {
     private static final String SEPARATOR = ",";
 
     /**
-     * Gets a string containing multiple <code>Guid</code> values separated by a comma and returns an ArrayList of
+     * Gets a string containing multiple <code>Guid</code> values separated by a comma and returns a List of
      * <code>Guid</code>. If the String is null/empty returns an empty array.
      *
      * @param str
      *            - String which contains list of <code>Guid</code>.
-     * @return - Array of <code>Guid</code> type.
+     * @return - List of <code>Guid</code> type.
      */
-    public static ArrayList<Guid> getGuidListFromString(String str) {
+    public static List<Guid> getGuidListFromString(String str) {
         return Arrays.stream(StringUtils.split(Objects.toString(str, StringUtils.EMPTY), SEPARATOR))
                 .map(Guid::createGuidFromStringDefaultEmpty)
-                .collect(Collectors.toCollection(ArrayList<Guid>::new));
+                .collect(Collectors.toList());
     }
 }
