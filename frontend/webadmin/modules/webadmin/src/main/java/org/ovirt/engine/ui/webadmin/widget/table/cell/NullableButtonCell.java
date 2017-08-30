1e2b04cba33a5d532e1e2478d68f171181610d27
Alexander Wels <awels@redhat.com>
2017-08-30 13:14:18 -0400

    webadmin: ButtonCell button to bootstrap
    - Update the button style in NullableButtonCell to be
      bootstrap styles.
    
    Change-Id: I4d2c192fa8d47da2058fb6fe2ca6ffa1e2a3ef7a
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/table/cell/NullableButtonCell.java
+++ frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/widget/table/cell/NullableButtonCell.java
@@ -9,7 +9,7 @@ public class NullableButtonCell extends ButtonCell {
     @Override
     public void render(Context context, SafeHtml data, SafeHtmlBuilder sb) {
         if (data != null) {
-            sb.appendHtmlConstant("<button type=\"button\" style=\"height:24px;\" tabindex=\"-1\">"); //$NON-NLS-1$
+            sb.appendHtmlConstant("<button type=\"button\" class=\"btn btn-default\" tabindex=\"-1\">"); //$NON-NLS-1$
             sb.append(data);
             sb.appendHtmlConstant("</button>"); //$NON-NLS-1$
         }
