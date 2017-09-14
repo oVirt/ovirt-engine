e6bcf45cac65e218eb8f57344a27abf0029c5732
Allon Mureinik <amureini@redhat.com>
2017-09-14 14:22:59 +0300

    core: Clean up AddVmFromTemplateCommandTest
    This patch removes tests that test the inner workings of the
    VmHandler and not the AddVmFromTemplateCommand.
    As this is a UNIT test for the command, it should not test the
    implementation of external dependencies.
    
    The functionality "tested" are just simple pass-through methods, so
    there's no real value in recreating them elsewhere.
    
    Change-Id: I50d9227730ae32ddaff725677897b326b1158242
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/AddVmFromTemplateCommandTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/AddVmFromTemplateCommandTest.java
@@ -9,21 +9,16 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.ovirt.engine.core.utils.MockConfigRule.mockConfig;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner.Strict;
 import org.ovirt.engine.core.common.action.AddVmParameters;
-import org.ovirt.engine.core.common.businessentities.ArchitectureType;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.errors.EngineMessage;
 import org.ovirt.engine.core.common.osinfo.OsRepository;
@@ -103,59 +98,6 @@ public class AddVmFromTemplateCommandTest extends AddVmCommandTestBase<AddVmFrom
 
         ValidateTestUtils.runAndAssertValidateFailure
                 (cmd, EngineMessage.ACTION_TYPE_FAILED_DISK_SPACE_LOW_ON_STORAGE_DOMAIN);
-    }
-
-    @Test
-    public void canAddVmWithVirtioScsiControllerNotSupportedOs() {
-        when(cpuFlagsManagerHandler.getCpuId(any(), any())).thenReturn(CPU_ID);
-        when(osRepository.isCpuSupported(anyInt(), any(), any())).thenReturn(true);
-        doReturn(true).when(cmd).areParametersLegal(any());
-        doReturn(Collections.emptyList()).when(cmd).getVmInterfaces();
-        doReturn(Collections.emptyList()).when(cmd).getDiskVmElements();
-        mockStorageDomainDaoGetAllForStoragePool();
-        mockMaxPciSlots();
-        mockStorageDomainDaoGetAllForStoragePool();
-
-        cmd.getParameters().setVirtioScsiEnabled(true);
-        when(osRepository.isSoundDeviceEnabled(anyInt(), any())).thenReturn(true);
-        when(osRepository.getArchitectureFromOS(anyInt())).thenReturn(ArchitectureType.x86_64);
-        when(osRepository.getDiskInterfaces(anyInt(), any())).thenReturn(
-                new ArrayList<>(Collections.singletonList("VirtIO")));
-        mockGetAllSnapshots();
-
-        cmd.initEffectiveCompatibilityVersion();
-
-        Map<String, String> migrationMap = new HashMap<>();
-        migrationMap.put("undefined", "true");
-        migrationMap.put("x86", "true");
-        migrationMap.put("ppc", "true");
-        mcr.mockConfigValue(ConfigValues.IsMigrationSupported, cmd.getEffectiveCompatibilityVersion(), migrationMap);
-        mcr.mockConfigValue(ConfigValues.MaxNumOfVmCpus, cmd.getEffectiveCompatibilityVersion(), 16);
-        mcr.mockConfigValue(ConfigValues.MaxNumOfVmSockets, cmd.getEffectiveCompatibilityVersion(), 16);
-        mcr.mockConfigValue(ConfigValues.MaxNumOfCpuPerSocket, cmd.getEffectiveCompatibilityVersion(), 16);
-        mcr.mockConfigValue(ConfigValues.MaxNumOfThreadsPerCpu, cmd.getEffectiveCompatibilityVersion(), 8);
-
-        ValidateTestUtils.runAndAssertValidateFailure(cmd,
-                EngineMessage.ACTION_TYPE_FAILED_ILLEGAL_OS_TYPE_DOES_NOT_SUPPORT_VIRTIO_SCSI);
-    }
-
-    @Test
-    public void testUnsupportedCpus() {
-        when(cpuFlagsManagerHandler.getCpuId(any(), any())).thenReturn(CPU_ID);
-        doReturn(true).when(cmd).areParametersLegal(any());
-        doReturn(Collections.emptyList()).when(cmd).getVmInterfaces();
-        doReturn(Collections.emptyList()).when(cmd).getDiskVmElements();
-        vm.setVmOs(OsRepository.DEFAULT_X86_OS);
-
-        mockMaxPciSlots();
-        mockStorageDomainDaoGetAllForStoragePool();
-        mockGetAllSnapshots();
-        when(osRepository.getArchitectureFromOS(0)).thenReturn(ArchitectureType.x86_64);
-        doReturn(storagePool).when(cmd).getStoragePool();
-
-        when(osRepository.isCpuSupported(vm.getVmOsId(), cluster.getCompatibilityVersion(), CPU_ID)).thenReturn(false);
-
-        ValidateTestUtils.runAndAssertValidateFailure(cmd, EngineMessage.CPU_TYPE_UNSUPPORTED_FOR_THE_GUEST_OS);
     }
 
     private void mockMaxPciSlots() {
