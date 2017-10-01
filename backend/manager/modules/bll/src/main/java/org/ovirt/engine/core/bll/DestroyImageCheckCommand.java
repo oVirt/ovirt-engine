20608cfd2f168adf1805e01201a41f5b3205c4d4
Ala Hino <ahino@redhat.com>
2017-10-01 13:56:38 +0300

    Revert "core: Retry failed live merge commands"
    This reverts commit 733c52b204798af2c78a8adb32bd10b1d4c3b07f.
    
    The patch introduced random OST failure.
    
    Change-Id: Iae8e1ae2c76a3ac0a809a4b3ed136ce6c05331a6
    Bug-Url: https://bugzilla.redhat.com/1383301
    Bug-Url: https://bugzilla.redhat.com/1467928
    Signed-off-by: Ala Hino <ahino@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/DestroyImageCheckCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/DestroyImageCheckCommand.java
@@ -5,12 +5,7 @@ import static java.util.stream.Collectors.toList;
 import java.util.Collections;
 import java.util.List;
 
-import javax.enterprise.inject.Instance;
-import javax.enterprise.inject.Typed;
-import javax.inject.Inject;
-
 import org.ovirt.engine.core.bll.context.CommandContext;
-import org.ovirt.engine.core.bll.tasks.interfaces.CommandCallback;
 import org.ovirt.engine.core.bll.utils.PermissionSubject;
 import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.common.action.DestroyImageParameters;
@@ -19,17 +14,12 @@ import org.ovirt.engine.core.common.errors.EngineError;
 import org.ovirt.engine.core.common.errors.EngineException;
 import org.ovirt.engine.core.common.vdscommands.SPMGetVolumeInfoVDSCommandParameters;
 import org.ovirt.engine.core.common.vdscommands.VDSCommandType;
-import org.ovirt.engine.core.compat.CommandStatus;
 import org.ovirt.engine.core.compat.Guid;
 
 @InternalCommandAttribute
 @NonTransactiveCommandAttribute
 public class DestroyImageCheckCommand<T extends DestroyImageParameters>
         extends CommandBase<T> {
-
-    @Inject
-    @Typed(DestroyImageCheckCommandCallback.class)
-    private Instance<DestroyImageCheckCommandCallback> callbackProvider;
 
     public DestroyImageCheckCommand(T parameters, CommandContext cmdContext) {
         super(parameters, cmdContext);
@@ -44,7 +34,6 @@ public class DestroyImageCheckCommand<T extends DestroyImageParameters>
         if (failedGuids.isEmpty()) {
             log.info("Requested images were successfully removed");
             setSucceeded(true);
-            setCommandStatus(CommandStatus.SUCCEEDED);
             persistCommand(getParameters().getParentCommand());
         } else {
             log.error("The following images were not removed: {}", failedGuids);
@@ -72,21 +61,12 @@ public class DestroyImageCheckCommand<T extends DestroyImageParameters>
             }
             // We can't assume the volume is gone; return true so that Live Merge fails
             log.error("Failed to determine volume '{}' existence using GetVolumeInfo", volumeId, e);
-            setSucceeded(true);
-            setCommandStatus(CommandStatus.ACTIVE);
-            persistCommand(getParameters().getParentCommand());
         }
-
         return true;
     }
 
     @Override
     public List<PermissionSubject> getPermissionCheckSubjects() {
         return Collections.emptyList();
-    }
-
-    @Override
-    public CommandCallback getCallback() {
-        return callbackProvider.get();
     }
 }
