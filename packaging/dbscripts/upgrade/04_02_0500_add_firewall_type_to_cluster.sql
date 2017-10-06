269ae35df60b034b17d839dabe2344f476f85658
Martin Perina <mperina@redhat.com>
2017-10-06 21:33:06 +0200

    core: Enable firewalld on Default cluster for new installations
    We need to set firewall type iptables for all clusters with versions
    < 4.2 to maintain backward compatibility and at the same time for new
    installations we want to leave firewall type firewalld for default
    cluster. This patches fixes db upgrade script introduced in
    I65527ce4a383312fb6085a8045f07d4906161438 which enabled IPTables even
    on default cluster for new installations.
    
    Change-Id: I3ac688b393031085b0cbe9b9a8aca93f5859cf46
    Bug-Url: https://bugzilla.redhat.com/1462811
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- packaging/dbscripts/upgrade/04_02_0500_add_firewall_type_to_cluster.sql
+++ packaging/dbscripts/upgrade/04_02_0500_add_firewall_type_to_cluster.sql
@@ -1,2 +1,10 @@
 SELECT fn_db_add_column('cluster', 'firewall_type', 'INT NOT NULL DEFAULT 1');
-UPDATE cluster SET firewall_type = 0;
+
+--
+-- We need to set firewall type iptables for all clusters with version < 4.2
+-- to maintain backward compatibility and at the same time for new
+-- installations to leave firewall type firewalld for default cluster
+--
+UPDATE cluster
+  SET firewall_type = 0
+  WHERE compatibility_version IN ('3.6', '4.0', '4.1');
