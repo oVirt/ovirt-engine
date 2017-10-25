a2caf10dbb333d455b3ba8089f5c0bc6c7786a5a
Tal Nisan <tnisan@redhat.com>
2017-10-25 13:49:07 +0300

    core: Change StorageDomainStatus to map the enum to int value by ctor
    Change StorageDomainStatus to map the enum name to int value by
    initializing it in the constructor instead of using the ordinal value
    which can change when adding new enum values
    
    Change-Id: I781694da48da3306b17048b7b1fe19176dfaf55a
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/StorageDomainStatus.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/StorageDomainStatus.java
@@ -1,23 +1,36 @@
 package org.ovirt.engine.core.common.businessentities;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum StorageDomainStatus implements Identifiable{
-    Unknown,
-    Uninitialized,
-    Unattached,
-    Active,
-    Inactive,
-    Locked,
-    Maintenance,
-    PreparingForMaintenance,
-    Detaching,
-    Activating;
+    Unknown(0),
+    Uninitialized(1),
+    Unattached(2),
+    Active(3),
+    Inactive(4),
+    Locked(5),
+    Maintenance(6),
+    PreparingForMaintenance(7),
+    Detaching(8),
+    Activating(9);
+
+    private int value;
+    private static final Map<Integer, StorageDomainStatus> valueToStatus =
+            Stream.of(values()).collect(Collectors.toMap(StorageDomainStatus::getValue, Function.identity()));
+
+    StorageDomainStatus(int value) {
+        this.value = value;
+    }
 
     public int getValue() {
-        return this.ordinal();
+        return value;
     }
 
     public static StorageDomainStatus forValue(int value) {
-        return values()[value];
+        return valueToStatus.get(value);
     }
 
     public boolean isStorageDomainInProcess() {
