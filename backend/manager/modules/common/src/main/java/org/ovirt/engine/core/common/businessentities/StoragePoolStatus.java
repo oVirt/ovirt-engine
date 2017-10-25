0fb0194e052f09d46b29aa52a89241921597ab25
Tal Nisan <tnisan@redhat.com>
2017-10-25 14:26:01 +0300

    core: Change StoragePoolStatus to map the enum to int value by ctor
    Change StoragePoolStatus to map the enum name to int value by
    initializing it in the constructor instead of using the ordinal value
    which can change when adding new enum values
    
    Change-Id: Ia077c329204060ae6974d5a03d2cb3b24196c20e
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/StoragePoolStatus.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/StoragePoolStatus.java
@@ -1,19 +1,32 @@
 package org.ovirt.engine.core.common.businessentities;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum StoragePoolStatus implements Identifiable {
-    Uninitialized,
-    Up,
-    Maintenance,
-    NotOperational,
-    NonResponsive,
-    Contend;
+    Uninitialized(0),
+    Up(1),
+    Maintenance(2),
+    NotOperational(3),
+    NonResponsive(4),
+    Contend(5);
+
+    private int value;
+    private static final Map<Integer, StoragePoolStatus> valueToStatus =
+            Stream.of(values()).collect(Collectors.toMap(StoragePoolStatus::getValue, Function.identity()));
+
+    StoragePoolStatus(int value) {
+        this.value = value;
+    }
 
     @Override
     public int getValue() {
-        return this.ordinal();
+        return value;
     }
 
     public static StoragePoolStatus forValue(int value) {
-        return values()[value];
+        return valueToStatus.get(value);
     }
 }
