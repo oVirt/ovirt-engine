fb92157ad7d9f93460c13c7b20a8de9a212cda1e
Maor Lipchuk <mlipchuk@redhat.com>
2017-10-25 15:03:52 +0300

    cleanup: Set true flag
    Change-Id: I8ea125d48eec31d3e793b1f3cb4322d0b389cf50
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AttachStorageDomainToPoolParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AttachStorageDomainToPoolParameters.java
@@ -3,15 +3,13 @@ package org.ovirt.engine.core.common.action;
 import org.ovirt.engine.core.compat.Guid;
 
 public class AttachStorageDomainToPoolParameters extends StorageDomainPoolParametersBase {
-    private boolean activate;
+    private boolean activate = true;
 
     public AttachStorageDomainToPoolParameters() {
-        activate = true;
     }
 
     public AttachStorageDomainToPoolParameters(Guid storageDomainId, Guid storagePoolId) {
         super(storageDomainId, storagePoolId);
-        activate = true;
     }
 
     public boolean getActivate() {
