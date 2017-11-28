9331463fbac4d287266a915e3cb67963ac35c448
ahadas@redhat.com <ahadas@redhat.com>
2017-11-28 14:44:54 +0200

    core: fix ovf uri
    Should be:
    http://schemas.dmtf.org/ovf/envelope/1
    rather than:
    http://schemas.dmtf.org/ovf/envelope/1/
    
    VDSM relies on it while parsinig the OVF from OVA files.
    
    Change-Id: Ie0dd1bc827d3a41f5910e53b38be90393885bd1c
    Signed-off-by: ahadas@redhat.com <ahadas@redhat.com>

--- backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/IOvfBuilder.java
+++ backend/manager/modules/utils/src/main/java/org/ovirt/engine/core/utils/ovf/IOvfBuilder.java
@@ -2,7 +2,7 @@ package org.ovirt.engine.core.utils.ovf;
 
 public interface IOvfBuilder extends OvfProperties {
     // Namespace URIs:
-    String OVF_URI = "http://schemas.dmtf.org/ovf/envelope/1/";
+    String OVF_URI = "http://schemas.dmtf.org/ovf/envelope/1";
     String RASD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_ResourceAllocationSettingData";
     String VSSD_URI = "http://schemas.dmtf.org/wbem/wscim/1/cim-schema/2/CIM_VirtualSystemSettingData";
     String XSI_URI = "http://www.w3.org/2001/XMLSchema-instance";
