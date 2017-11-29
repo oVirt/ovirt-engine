fc9695532da733931193507b4ea56024bc9a029f
Sahina Bose <sabose@redhat.com>
2017-11-29 12:20:54 +0530

    core: Default gfapi access support to false
    Default gfapi access support to false for
    4.2 clusters until HA is available
    
    Bug-Url: https://bugzilla.redhat.com/1518558
    Change-Id: I52d80875eaf38e6d83e3865f2ae95477f0d0e855
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
+++ packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -145,7 +145,7 @@ select fn_db_add_config_value('GlusterUnSyncedEntriesHistoryLimit', '40', 'gener
 select fn_db_add_config_value('GlusterSelfHealMonitoringSupported', 'false', '3.6');
 select fn_db_add_config_value_for_versions_up_to('GlusterSelfHealMonitoringSupported', 'true', '4.2');
 select fn_db_add_config_value_for_versions_up_to('LibgfApiSupported', 'false', '4.1');
-select fn_db_add_config_value('LibgfApiSupported', 'true', '4.2');
+select fn_db_add_config_value('LibgfApiSupported', 'false', '4.2');
 -- Gluster Geo-replication --
 select fn_db_add_config_value('GlusterRefreshRateGeoRepDiscoveryInSecs', '3600', 'general');
 select fn_db_add_config_value('GlusterRefreshRateGeoRepStatusInSecs', '300', 'general');
