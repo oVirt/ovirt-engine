86474c4e4293bd709f931d65e7436d0cd19505de
Andrej Krejcir <akrejcir@redhat.com>
2017-12-07 12:59:54 +0100

    core: Quota: inline ChangeQuotaCommand into ChangeQuotaForDiskCommand
    The class ChangeQuotaCommand was used only as a base class for a single class.
    This patch merges the two classes together.
    
    Change-Id: I0ff926ed8f580c1113a7fe4f9e944adddbcec35b
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/quota/ChangeQuotaForDiskCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/quota/ChangeQuotaForDiskCommand.java
@@ -6,7 +6,11 @@ import java.util.Objects;
 
 import javax.inject.Inject;
 
+import org.ovirt.engine.core.bll.CommandBase;
 import org.ovirt.engine.core.bll.context.CommandContext;
+import org.ovirt.engine.core.bll.utils.PermissionSubject;
+import org.ovirt.engine.core.bll.validator.storage.StoragePoolValidator;
+import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.common.action.ChangeQuotaParameters;
 import org.ovirt.engine.core.common.businessentities.storage.Disk;
 import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
@@ -14,8 +18,9 @@ import org.ovirt.engine.core.common.errors.EngineMessage;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.DiskDao;
 import org.ovirt.engine.core.dao.ImageStorageDomainMapDao;
+import org.ovirt.engine.core.dao.QuotaDao;
 
-public class ChangeQuotaForDiskCommand extends ChangeQuotaCommand {
+public class ChangeQuotaForDiskCommand extends CommandBase<ChangeQuotaParameters> implements QuotaStorageDependent {
 
     @Inject
     private DiskDao diskDao;
@@ -23,10 +28,14 @@ public class ChangeQuotaForDiskCommand extends ChangeQuotaCommand {
     @Inject
     private ImageStorageDomainMapDao imageStorageDomainMapDao;
 
+    @Inject
+    private QuotaDao quotaDao;
+
     private DiskImage disk;
 
     public ChangeQuotaForDiskCommand(ChangeQuotaParameters parameters, CommandContext cmdContext) {
         super(parameters, cmdContext);
+        setStoragePoolId(getParameters().getStoragePoolId());
     }
 
     @Override
@@ -42,7 +51,21 @@ public class ChangeQuotaForDiskCommand extends ChangeQuotaCommand {
         }
         this.disk = (DiskImage) disk;
 
-        return super.validate();
+        // check if SP exist
+        StoragePoolValidator spValidator = new StoragePoolValidator(getStoragePool());
+        if (!validate(spValidator.exists())) {
+            return false;
+        }
+        // Check if quota exist:
+        if (getQuotaId() == null) {
+            addValidationMessage(EngineMessage.ACTION_TYPE_FAILED_QUOTA_IS_NOT_VALID);
+            return false;
+        }
+        if (quotaDao.getById(getQuotaId()) == null) {
+            addValidationMessage(EngineMessage.ACTION_TYPE_FAILED_QUOTA_NOT_EXIST);
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -80,4 +103,27 @@ public class ChangeQuotaForDiskCommand extends ChangeQuotaCommand {
         return disk.getSizeInGigabytes();
     }
 
+    @Override
+    public List<PermissionSubject> getPermissionCheckSubjects() {
+        List<PermissionSubject> permissionList = new ArrayList<>();
+        permissionList.add(new PermissionSubject(getParameters().getQuotaId(),
+                VdcObjectType.Quota,
+                getActionType().getActionGroup()));
+        return permissionList;
+    }
+
+    protected Guid getQuotaId() {
+        return getParameters().getQuotaId();
+    }
+
+    @Override
+    public void addQuotaPermissionSubject(List<PermissionSubject> quotaPermissionList) {
+        // no implementation here already checked in getPermissionCheckSubjects
+    }
+
+    @Override
+    protected void setActionMessageParameters() {
+        addValidationMessage(EngineMessage.VAR__ACTION__ASSIGN);
+        addValidationMessage(EngineMessage.VAR__TYPE__QUOTA);
+    }
 }
