dea02abd9cca6553c5042eed032dc3e80f021d39
Arik Hadas <ahadas@redhat.com>
2017-12-20 12:34:53 +0200

    core: do not use imageio in ova extraction
    Replace the use of imageio-common with a simpler version that
    is suitable for disk-to-disk copy.
    
    Change-Id: I357c142b5ab1e77f858e71adcabe4c94dea3a02a
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- packaging/playbooks/roles/ovirt-ova-extract/files/extract_ova.py
+++ packaging/playbooks/roles/ovirt-ova-extract/files/extract_ova.py
@@ -1,56 +1,101 @@
 #!/usr/bin/python
 
-import math
+import io
+import mmap
+import os
 import sys
-import tarfile
 
-from ovirt_imageio_common import directio
+from contextlib import closing
 
-BUF_SIZE = 8 * 1024 * 1024
+NUL = b"\0"
+BUF_SIZE = 8 * 1024**2
 TAR_BLOCK_SIZE = 512
 
 
-class SendAdapter(object):
-    def __init__(self, send):
-        self.iter = iter(send)
-
-    def read(self, size):
-        # we use the same buffer size in both Send and Receive
-        # so we will have 1 chunk at most
-        return self.iter.next()
-
-    def finish(self):
-        pass
-
-
-def extract_disk(ova_path, pos, disk_size, image_path):
-    send = directio.Send(ova_path, None, offset=pos, size=disk_size,
-                         buffersize=BUF_SIZE)
-    op = directio.Receive(image_path, SendAdapter(send), size=disk_size,
-                          buffersize=BUF_SIZE)
-    op.run()
+def extract_disk(ova_file, disk_size, image_path):
+    fd = os.open(image_path, os.O_RDWR | os.O_DIRECT)
+    buf = mmap.mmap(-1, BUF_SIZE)
+    with closing(buf), io.FileIO(fd, "r+", closefd=True) as image:
+        copied = 0
+        while copied < disk_size:
+            read = ova_file.readinto(buf)
+            read = min(read, disk_size - copied)
+            written = 0
+            while written < read:
+                wbuf = buffer(buf, written, read - written)
+                written += image.write(wbuf)
+            copied += written
 
 
-def extract_disks(ova_path, ova_entries, image_paths):
-    pos = 0
-    for ova_entry in ova_entries:
-        # skip the header
-        pos += TAR_BLOCK_SIZE
-        entry_size = ova_entry.size
-        for image_path in image_paths:
-            if ova_entry.name in image_path:
-                extract_disk(ova_path, pos, entry_size, image_path)
+def nts(s, encoding, errors):
+    """
+    Convert a null-terminated bytes object to a string.
+    Taken from tarfile.py.
+    """
+    p = s.find(b"\0")
+    if p != -1:
+        s = s[:p]
+    return s.decode(encoding, errors)
+
+
+def nti(s):
+    """
+    Convert a number field to a python number.
+    Taken from tarfile.py.
+    """
+    if s[0] in (0o200, 0o377):
+        n = 0
+        for i in range(len(s) - 1):
+            n <<= 8
+            n += s[i + 1]
+        if s[0] == 0o377:
+            n = -(256 ** (len(s) - 1) - n)
+    else:
+        try:
+            s = nts(s, "ascii", "strict")
+            n = int(s.strip() or "0", 8)
+        except ValueError:
+            print 'invalid header'
+            raise
+    return n
+
+
+def extract_disks(ova_path, image_paths):
+    fd = os.open(ova_path, os.O_RDONLY | os.O_DIRECT)
+    buf = mmap.mmap(-1, TAR_BLOCK_SIZE)
+    with io.FileIO(fd, "r", closefd=True) as ova_file, \
+            closing(buf):
+        # read the tar info of the ovf
+        ova_file.readinto(buf)
+        size = nti(buf[124:136])
+        jump = size
+        # the ovf is typically not aligned to blocks of 512 bytes
+        remainder = size % TAR_BLOCK_SIZE
+        if remainder:
+            jump += TAR_BLOCK_SIZE - remainder
+        ova_file.seek(jump, 1)
+
+        # extract the disks
+        while True:
+            # read next tar info
+            ova_file.readinto(buf)
+            info = buf.read(512)
+            # tar files end with NUL blocks
+            if info == NUL*512:
                 break
-        entry_num_of_blocks = math.ceil(entry_size * 1.0 / TAR_BLOCK_SIZE)
-        padded_entry_size = int(entry_num_of_blocks * TAR_BLOCK_SIZE)
-        pos += padded_entry_size
+            # preparation for the next iteration
+            buf.seek(0)
+            # extract the next disk to the corresponding image
+            name = nts(info[0:100], 'utf-8', 'surrogateescape')
+            size = nti(info[124:136])
+            for image_path in image_paths:
+                if name in image_path:
+                    extract_disk(ova_file, size, image_path)
+                    break
 
 
 if len(sys.argv) < 3:
     print ("Usage: extract_ova.py ova_path disks_paths")
     sys.exit(2)
 
-ova_path = sys.argv[1]
-with tarfile.open(ova_path) as ova_file:
-    ova_entries = ova_file.getmembers()
-extract_disks(ova_path, ova_entries, sys.argv[2].split('+'))
+extract_disks(sys.argv[1], sys.argv[2].split('+'))
