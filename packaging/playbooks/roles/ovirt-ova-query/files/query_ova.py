534df643c0e74e4bc38e02dbb0373c53f42c0d7c
Arik Hadas <ahadas@redhat.com>
2017-12-28 17:15:09 +0200

    ansible: support for an external ova in the form of a directory
    The OVF specification says that OVA can be a directory or a tar file.
    The recent switch to query OVF configuration within OVAs using Ansible
    broke this ability since the assumption was that the OVA is in the form
    of a tar file only. This patch enhances the new mechanism with support
    for OVA in the form of a directory. For now, importing OVA that is in
    the form of a directory is possible only for those that are generated
    by others (e.g., VMware).
    
    Change-Id: I038df3af9c11ccd56d763762f5c8e512d4410036
    Bug-Url: https://bugzilla.redhat.com/1529255
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- packaging/playbooks/roles/ovirt-ova-query/files/query_ova.py
+++ packaging/playbooks/roles/ovirt-ova-query/files/query_ova.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import sys
 import tarfile
 
@@ -7,10 +8,38 @@ if len(sys.argv) < 2:
     print ("Usage: query_ova.py ova_path")
     sys.exit(2)
 
-ova_path = sys.argv[1]
-with tarfile.open(ova_path) as ova_file:
-    for ova_entry in ova_file.getmembers():
-        if ova_entry.name.lower().endswith('ovf'):
-            ovf_file = ova_file.extractfile(ova_entry)
-            print (ovf_file.read())
+
+def is_ovf(filename):
+    return filename.lower().endswith('.ovf')
+
+
+def get_ovf_from_ova_file(ova_path):
+    with tarfile.open(ova_path) as ova_file:
+        for ova_entry in ova_file.getmembers():
+            if is_ovf(ova_entry.name):
+                ovf_file = ova_file.extractfile(ova_entry)
+                ovf = ovf_file.read()
+                break
+        else:
+            raise Exception('Failed to find OVF in file %s' % ova_path)
+    return ovf
+
+
+def get_ovf_from_ova_dir(ova_path):
+    for filename in os.listdir(ova_path):
+        if is_ovf(filename):
+            ovf_file = open(os.path.join(ova_path, filename))
+            ovf = ovf_file.read()
             break
+    else:
+        raise Exception('Failed to find OVF in dir %s' % ova_path)
+    return ovf
+
+
+ova_path = sys.argv[1]
+if os.path.isfile(ova_path):
+    ovf = get_ovf_from_ova_file(ova_path)
+else:
+    ovf = get_ovf_from_ova_dir(ova_path)
+
+print (ovf)
