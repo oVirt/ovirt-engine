4beb36d43c76932659ff3ec0da637281f5c0eb37
Daniel Erez <derez@redhat.com>
2018-01-02 13:00:42 +0200

    webadmin: DownloadImageHandler - set transfer size
    Set transfer size to getActualSizeInBytes instead of getSize.
    This is crucial, for instance, when downloading an image imported
    from glance, as the actual size is usually smaller.
    
    Change-Id: I427319e6a1ba198c9e4c2e561bf03e84ed62a1cc
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/storage/DownloadImageHandler.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/storage/DownloadImageHandler.java
@@ -44,7 +44,7 @@ public class DownloadImageHandler {
         String fileExtension = diskImage.getVolumeFormat() == VolumeFormat.COW ?
                 ".qcow2" : ".raw"; //$NON-NLS-1$ //$NON-NLS-2$
         parameters.setDownloadFilename(diskImage.getDiskAlias() + fileExtension); //$NON-NLS-1$
-        parameters.setTransferSize(diskImage.getSize());
+        parameters.setTransferSize(diskImage.getActualSizeInBytes());
         return parameters;
     }
 
