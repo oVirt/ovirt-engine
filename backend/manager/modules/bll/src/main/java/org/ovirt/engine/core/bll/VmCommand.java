oVirt/ovirt-engine

commit 607caa084852161c3332b2efe1364ab41b8c2d24

core: Extract immediate OVF update of HE VM to a method
Some commands should update the OVF of hosted engine VM
immediately and not wait until the OVF update interval passes.

This patch extracts the code to a method in VmCommand,
so any command can be easily flagged, that it should update the OVF.

Change-Id: I4b04d06beb40be26b292a7405a11b00d690ba58d
Bug-Url: https://bugzilla.redhat.com/1469538
Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VmCommand.java
@@ -15,6 +15,7 @@ import org.ovirt.engine.core.bll.network.macpool.MacPoolPerCluster;
 import org.ovirt.engine.core.bll.snapshots.SnapshotsManager;
 import org.ovirt.engine.core.bll.snapshots.SnapshotsValidator;
 import org.ovirt.engine.core.bll.storage.disk.image.ImagesHandler;
+import org.ovirt.engine.core.bll.storage.ovfstore.OvfDataUpdater;
 import org.ovirt.engine.core.bll.utils.PermissionSubject;
 import org.ovirt.engine.core.bll.utils.VmDeviceUtils;
 import org.ovirt.engine.core.bll.validator.LocalizedVmStatus;
@@ -57,6 +58,8 @@ import org.ovirt.engine.core.dao.StorageDomainDao;
 import org.ovirt.engine.core.dao.TagDao;
 import org.ovirt.engine.core.dao.VmStaticDao;
 import org.ovirt.engine.core.dao.network.VmNicDao;
+import org.ovirt.engine.core.utils.transaction.TransactionCompletionListener;
+import org.ovirt.engine.core.utils.transaction.TransactionSupport;
 import org.ovirt.engine.core.vdsbroker.ResourceManager;
 import org.ovirt.engine.core.vdsbroker.VmManager;
 import org.ovirt.engine.core.vdsbroker.builder.vminfo.VmInfoBuildUtils;
@@ -97,6 +100,8 @@ public abstract class VmCommand<T extends VmOperationParameterBase> extends Comm
     private DiskDao diskDao;
     @Inject
     private VmInfoBuildUtils vmInfoBuildUtils;
+    @Inject
+    private OvfDataUpdater ovfDataUpdater;
 
     @Inject
     protected ImagesHandler imagesHandler;
@@ -154,6 +159,10 @@ public abstract class VmCommand<T extends VmOperationParameterBase> extends Comm
             return;
         }
         executeVmCommand();
+
+        if (shouldUpdateHostedEngineOvf() && getVm().isHostedEngine() && getSucceeded()) {
+            updateHeOvf();
+        }
     }
 
     protected void executeVmCommand() {
@@ -531,5 +540,35 @@ public abstract class VmCommand<T extends VmOperationParameterBase> extends Comm
 
     public QuotaValidator createQuotaValidator(Guid quotaId) {
         return QuotaValidator.createInstance(quotaId, true);
+    }
+
+    /**
+     * Override if the command should immediately update the OVF of the hosted engine VM.
+     *
+     * @return false by default
+     */
+    protected boolean shouldUpdateHostedEngineOvf() {
+        return false;
+    }
+
+    private void updateHeOvf() {
+        // If there is no current transaction, trigger ovf update immediately
+        if (TransactionSupport.current() == null) {
+            ovfDataUpdater.triggerNow();
+            return;
+        }
+
+        // If there is a transaction, trigger update after it is committed.
+        registerRollbackHandler(new TransactionCompletionListener() {
+            @Override
+            public void onSuccess() {
+                ovfDataUpdater.triggerNow();
+            }
+
+            @Override
+            public void onRollback() {
+                // No notification is needed
+            }
+        });
     }
 }

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateVmCommand.java
@@ -24,7 +24,6 @@ import org.ovirt.engine.core.bll.quota.QuotaClusterConsumptionParameter;
 import org.ovirt.engine.core.bll.quota.QuotaConsumptionParameter;
 import org.ovirt.engine.core.bll.quota.QuotaSanityParameter;
 import org.ovirt.engine.core.bll.quota.QuotaVdsDependent;
-import org.ovirt.engine.core.bll.storage.ovfstore.OvfDataUpdater;
 import org.ovirt.engine.core.bll.utils.IconUtils;
 import org.ovirt.engine.core.bll.utils.PermissionSubject;
 import org.ovirt.engine.core.bll.validator.IconValidator;
@@ -110,7 +109,6 @@ import org.ovirt.engine.core.dao.network.NetworkDao;
 import org.ovirt.engine.core.dao.network.VmNicDao;
 import org.ovirt.engine.core.dao.provider.ProviderDao;
 import org.ovirt.engine.core.utils.ReplacementUtils;
-import org.ovirt.engine.core.utils.transaction.TransactionCompletionListener;
 import org.ovirt.engine.core.vdsbroker.ResourceManager;
 import org.ovirt.engine.core.vdsbroker.monitoring.VmDevicesMonitoring;
 
@@ -133,8 +131,6 @@ public class UpdateVmCommand<T extends VmManagementParametersBase> extends VmMan
     private ResourceManager resourceManager;
     @Inject
     private InClusterUpgradeValidator clusterUpgradeValidator;
-    @Inject
-    private OvfDataUpdater ovfDataUpdater;
     @Inject
     private VmNumaNodeDao vmNumaNodeDao;
     @Inject
@@ -244,21 +240,6 @@ public class UpdateVmCommand<T extends VmManagementParametersBase> extends VmMan
         newVmStatic.setCreationDate(oldVm.getStaticData().getCreationDate());
         newVmStatic.setQuotaId(getQuotaId());
         newVmStatic.setLeaseInfo(oldVm.getStaticData().getLeaseInfo());
-
-        // Trigger OVF update for hosted engine VM only
-        if (getVm().isHostedEngine()) {
-            registerRollbackHandler(new TransactionCompletionListener() {
-                @Override
-                public void onSuccess() {
-                    ovfDataUpdater.triggerNow();
-                }
-
-                @Override
-                public void onRollback() {
-                    // No notification is needed
-                }
-            });
-        }
 
         // save user selected value for hotplug before overriding with db values (when updating running vm)
         VM userVm = new VM();
@@ -1473,5 +1454,10 @@ public class UpdateVmCommand<T extends VmManagementParametersBase> extends VmMan
                 .map(Label::getId)
                 .collect(Collectors.toList());
         labelDao.updateLabelsForVm(getVmId(), labelIds);
+    }
+
+    @Override
+    protected boolean shouldUpdateHostedEngineOvf() {
+        return true;
     }
 }

commit b78fb0c23335815406f4e88987265f728ec2a3ea

core: NullPointerException while creating a new floating disk
While creating a new floating disk, which has no VM, the getVM() method
returns null.
Seems like it's a regression from 607caa0848

Change-Id: Ie2fe532e8f27ca19a550afd5683f1d407d003afe
Signed-off-by: Shani Leviim <sleviim@redhat.com>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/VmCommand.java
@@ -160,7 +160,7 @@ public abstract class VmCommand<T extends VmOperationParameterBase> extends Comm
         }
         executeVmCommand();
 
-        if (shouldUpdateHostedEngineOvf() && getVm().isHostedEngine() && getSucceeded()) {
+        if (shouldUpdateHostedEngineOvf() && getVm() != null && getVm().isHostedEngine() && getSucceeded()) {
             updateHeOvf();
         }
     }