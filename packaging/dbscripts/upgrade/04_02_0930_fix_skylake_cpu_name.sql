97cba399be891341d551b6f4772bc45282c02de0
Michal Skrivanek <michal.skrivanek@redhat.com>
2018-01-15 16:43:42 +0100

    core: rename Intel Skylake to Skylake Client
    "Skylake" has been changed to "Skylake Client".
    Also renumbered IBRS types and fixed Skylake number
    colliding with Broadwell.
    
    Change-Id: I1e213be5090a29b65d1a5156ff3495f13a6a94cf
    Related-To: https://bugzilla.redhat.com/1524824
    Related-To: https://bugzilla.redhat.com/1517286
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

---
+++ packaging/dbscripts/upgrade/04_02_0930_fix_skylake_cpu_name.sql
@@ -0,0 +1,13 @@
+
+UPDATE cluster SET cpu_name = CASE
+    WHEN cpu_name = 'Intel Nehalem Family-IBRS' THEN 'Intel Nehalem IBRS Family' -- fix wrong 3.6 name
+    WHEN cpu_name = 'Intel Nehalem-IBRS Family' THEN 'Intel Nehalem IBRS Family' -- align -IBRS
+    WHEN cpu_name = 'Intel Westmere-IBRS Family' THEN 'Intel Westmere IBRS Family'
+    WHEN cpu_name = 'Intel SandyBridge-IBRS Family' THEN 'Intel SandyBridge IBRS Family'
+    WHEN cpu_name = 'Intel Haswell-noTSX-IBRS Family' THEN 'Intel Haswell-noTSX IBRS Family'
+    WHEN cpu_name = 'Intel Haswell-IBRS Family' THEN 'Intel Haswell IBRS Family'
+    WHEN cpu_name = 'Intel Broadwell-noTSX-IBRS Family' THEN 'Intel Broadwell-noTSX IBRS Family'
+    WHEN cpu_name = 'Intel Broadwell-IBRS Family' THEN 'Intel Broadwell IBRS Family'
+    WHEN cpu_name = 'Intel Skylake Family' THEN 'Intel Skylake Client Family' -- rename Skylake
+    WHEN cpu_name = 'Intel Skylake-IBRS Family' THEN 'Intel Skylake Client IBRS Family'
+END;
