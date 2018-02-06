6f0e4c64b14b585af3e5a52605514f4e13a2650a
Ales Musil <amusil@redhat.com>
2018-02-06 09:16:47 +0100

    webadmin: Change profile behavior
    Change backend command to pull VM networks only. Change choice
    of the default profile value for the new VM nic.
    
    Change-Id: I76c15a84ff9f9af66b8d798fc986a841fca58ceb
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1539589
    Bug-Url: https://bugzilla.redhat.com/1530027
    (cherry picked from commit dbbe1544841631bc9bea1a4f56340e6f571fbc45)

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/EditProfileBehavior.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/EditProfileBehavior.java
@@ -1,34 +1,28 @@
 package org.ovirt.engine.ui.uicommonweb.models.vms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.ovirt.engine.core.common.businessentities.network.VmNetworkInterface;
 import org.ovirt.engine.core.common.businessentities.network.VnicProfileView;
-import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.ui.uicommonweb.models.ListModel;
 
 public class EditProfileBehavior extends ProfileBehavior {
 
     @Override
-    public void initSelectedProfile(ListModel profileList, VmNetworkInterface networkInterface) {
-        List<VnicProfileView> profiles = (List<VnicProfileView>) profileList.getItems();
-        profiles = profiles == null ? new ArrayList<VnicProfileView>() : profiles;
-
-        if (networkInterface.getVnicProfileId() == null) {
+    public void initSelectedProfile(ListModel<VnicProfileView> profileList, VmNetworkInterface networkInterface) {
+        if (networkInterface.getVnicProfileId() != null) {
+            Collection<VnicProfileView> profiles =
+                    Optional.ofNullable(profileList.getItems()).orElse(Collections.emptyList());
+            profileList.setSelectedItem(profiles.stream()
+                    .filter(profile -> Objects.equals(profile.getId(), networkInterface.getVnicProfileId()))
+                    .findAny()
+                    .orElse(VnicProfileView.EMPTY));
+        } else {
             profileList.setSelectedItem(VnicProfileView.EMPTY);
-            return;
         }
-
-        for (VnicProfileView vnicProfile : profiles) {
-            Guid profileId = vnicProfile == null ? null : vnicProfile.getId();
-            if (networkInterface.getVnicProfileId().equals(profileId)) {
-                profileList.setSelectedItem(vnicProfile);
-                return;
-            }
-        }
-
-        profileList.setSelectedItem(VnicProfileView.EMPTY);
     }
 
 }
--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/NewProfileBehavior.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/vms/NewProfileBehavior.java
@@ -1,26 +1,32 @@
 package org.ovirt.engine.ui.uicommonweb.models.vms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.ovirt.engine.core.common.businessentities.network.VmNetworkInterface;
 import org.ovirt.engine.core.common.businessentities.network.VnicProfileView;
+import org.ovirt.engine.core.compat.StringHelper;
 import org.ovirt.engine.ui.uicommonweb.models.ListModel;
 
 public class NewProfileBehavior extends ProfileBehavior {
 
     @Override
-    public void initSelectedProfile(ListModel profileList, VmNetworkInterface networkInterface) {
-        List<VnicProfileView> profiles = (List<VnicProfileView>) profileList.getItems();
-        profiles = profiles == null ? new ArrayList<VnicProfileView>() : profiles;
-        for (VnicProfileView profile : profiles) {
-            if (getManagementNetworkName() != null && profile != null &&
-                    getManagementNetworkName().equals(profile.getNetworkName())) {
-                profileList.setSelectedItem(profile);
-                return;
-            }
+    public void initSelectedProfile(ListModel<VnicProfileView> profileList, VmNetworkInterface networkInterface) {
+        Collection<VnicProfileView> profiles =
+                Optional.ofNullable(profileList.getItems()).orElse(Collections.emptyList());
+
+        Stream<VnicProfileView> profileStream = profiles.stream()
+                .filter(profile -> !Objects.equals(profile, VnicProfileView.EMPTY));
+
+        if (StringHelper.isNotNullOrEmpty(getManagementNetworkName())) {
+            profileStream = profileStream.filter(profile -> Objects.equals(profile.getNetworkName(),
+                    getManagementNetworkName()));
         }
-        profileList.setSelectedItem(profiles.size() > 0 ? profiles.get(0) : VnicProfileView.EMPTY);
+
+        profileList.setSelectedItem(profileStream.findFirst().orElse(VnicProfileView.EMPTY));
     }
 
 }
