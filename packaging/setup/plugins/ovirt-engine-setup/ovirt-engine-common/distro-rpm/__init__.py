05d45a0d2d2e24c9495982d04bac1f6621e02963
Yedidyah Bar David <didi@redhat.com>
2018-02-13 16:03:04 +0200

    packaging: setup: Configure yum versionlock plugin to follow obsoletes
    Bug is on rhvm, which was renamed from rhevm, not sure it affects oVirt.
    But still useful if we need to rename in the future something
    versionlocked.
    
    Change-Id: I27bd38ebb4f10501ea80d15f4f02c768c2c65347
    Bug-Url: https://bugzilla.redhat.com/1543526
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-common/distro-rpm/__init__.py
+++ packaging/setup/plugins/ovirt-engine-setup/ovirt-engine-common/distro-rpm/__init__.py
@@ -25,11 +25,13 @@ Includes code relevant for rpm-based distributions
 from otopi import util
 
 from . import packages
+from . import versionlock_config
 
 
 @util.export
 def createPlugins(context):
     packages.Plugin(context=context)
+    versionlock_config.Plugin(context=context)
 
 
 # vim: expandtab tabstop=4 shiftwidth=4
