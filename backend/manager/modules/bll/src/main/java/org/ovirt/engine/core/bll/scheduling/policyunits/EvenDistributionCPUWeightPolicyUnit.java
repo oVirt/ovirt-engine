0d7db5cb9bc37edc450050d02b2bf16419bdd885
Andrej Krejcir <akrejcir@redhat.com>
2018-02-14 16:04:38 +0100

    core: Remove base class EvenDistributionWeightPolicyUnit
    Change-Id: Ia247c6e7d1d783f1aa796d480d265628c72290ca
    Bug-Url: https://bugzilla.redhat.com/1541777
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/policyunits/EvenDistributionCPUWeightPolicyUnit.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/policyunits/EvenDistributionCPUWeightPolicyUnit.java
@@ -1,9 +1,15 @@
 package org.ovirt.engine.core.bll.scheduling.policyunits;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.ovirt.engine.core.bll.scheduling.PolicyUnitImpl;
 import org.ovirt.engine.core.bll.scheduling.SchedulingUnit;
 import org.ovirt.engine.core.bll.scheduling.SlaValidator;
 import org.ovirt.engine.core.bll.scheduling.pending.PendingCpuCores;
 import org.ovirt.engine.core.bll.scheduling.pending.PendingResourceManager;
+import org.ovirt.engine.core.common.businessentities.Cluster;
 import org.ovirt.engine.core.common.businessentities.VDS;
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VdsSpmStatus;
@@ -11,6 +17,8 @@ import org.ovirt.engine.core.common.config.Config;
 import org.ovirt.engine.core.common.config.ConfigValues;
 import org.ovirt.engine.core.common.scheduling.PolicyUnit;
 import org.ovirt.engine.core.common.scheduling.PolicyUnitType;
+import org.ovirt.engine.core.common.utils.Pair;
+import org.ovirt.engine.core.compat.Guid;
 
 @SchedulingUnit(
         guid = "7db4ab05-81ab-42e8-868a-aee2df483edb",
@@ -19,10 +27,21 @@ import org.ovirt.engine.core.common.scheduling.PolicyUnitType;
         description = "Gives hosts with lower CPU usage, lower weight (means that hosts with lower CPU usage are more"
                 + " likely to be selected)"
 )
-public class EvenDistributionCPUWeightPolicyUnit extends EvenDistributionWeightPolicyUnit {
+public class EvenDistributionCPUWeightPolicyUnit extends PolicyUnitImpl {
+
     public EvenDistributionCPUWeightPolicyUnit(PolicyUnit policyUnit,
             PendingResourceManager pendingResourceManager) {
         super(policyUnit, pendingResourceManager);
+    }
+
+    @Override
+    public List<Pair<Guid, Integer>> score(Cluster cluster, List<VDS> hosts, VM vm, Map<String, String> parameters) {
+        boolean countThreadsAsCores = cluster.getCountThreadsAsCores();
+        List<Pair<Guid, Integer>> scores = new ArrayList<>();
+        for (VDS vds : hosts) {
+            scores.add(new Pair<>(vds.getId(), calcHostScore(vds, vm, countThreadsAsCores)));
+        }
+        return scores;
     }
 
     /**
@@ -40,14 +59,18 @@ public class EvenDistributionCPUWeightPolicyUnit extends EvenDistributionWeightP
         return (hostCpu / vcpu) + (pendingVcpus + vm.getNumOfCpus() + spmCpu) / hostCores;
     }
 
-    protected int calcEvenDistributionScore(float maxMemoryOfVdsInCluster,
-            VDS vds,
-            VM vm,
-            boolean countThreadsAsCores) {
+    /**
+     * Calculate a single host weight score according to various parameters.
+     *
+     * @param vds                     host on which the score is calculated for
+     * @param vm                      virtual machine to be deployed at a selected host by score
+     * @param countThreadsAsCores     true - count threads as cores , false - otherwise
+     * @return weight score for a single host
+     */
+    protected int calcHostScore(VDS vds, VM vm, boolean countThreadsAsCores) {
         int score = MaxSchedulerWeight - 1;
         Integer effectiveCpuCores = SlaValidator.getEffectiveCpuCores(vds, countThreadsAsCores);
-        if (effectiveCpuCores != null
-                && vds.getUsageCpuPercent() != null) {
+        if (effectiveCpuCores != null && vds.getUsageCpuPercent() != null) {
             // round the result and adding one to avoid zero
             score = Math.min((int) Math.round(
                     calcDistributeMetric(vds, vm, effectiveCpuCores)) + 1, MaxSchedulerWeight);
