4ff7e1b82c52cebf7561bc36bd6d909c490da68e
Ales Musil <amusil@redhat.com>
2018-02-16 11:14:03 +0100

    webadmin: Fix cast error for incompatible provider types
    Only network providers are readonly aware. This check will prevent
    casting incompatible providers.
    
    Change-Id: Ibd6c414f53617ab0d7c9364f830bd26a1cf18cd9
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/providers/ProviderModel.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/providers/ProviderModel.java
@@ -275,9 +275,7 @@ public class ProviderModel extends Model {
             getNeutronAgentModel().setIsAvailable(isNetworkProvider);
 
             getReadOnly().setIsAvailable(isReadOnlyAware);
-            if (isReadOnlyAware){
-                setReadOnlyEntity();
-            }
+            setReadOnlyEntity();
 
             boolean isVmware = isTypeVmware();
             boolean isKvm = isTypeKVM();
@@ -357,9 +355,10 @@ public class ProviderModel extends Model {
     }
 
     private void setReadOnlyEntity() {
-        if (getIsUnmanaged().getEntity()) {
+        boolean isReadOnlyAware = getType().getSelectedItem().isReadOnlyAware();
+        if (isReadOnlyAware && getIsUnmanaged().getEntity()) {
             getReadOnly().setEntity(false);
-        } else {
+        } else if (isReadOnlyAware) {
             OpenstackNetworkProviderProperties properties =
                     (OpenstackNetworkProviderProperties) provider.getAdditionalProperties();
             getReadOnly().setEntity(properties != null ? properties.getReadOnly() : true);
