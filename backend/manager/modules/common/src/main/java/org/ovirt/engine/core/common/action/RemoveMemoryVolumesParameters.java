eef08f99ac58e52644ac4baad33675b6376e62c4
Tal Nisan <tnisan@redhat.com>
2018-03-05 17:00:11 +0200

    core: Remove redundant field in RemoveMemoryVolumesParameters
    Change-Id: I4b8bf474d42006a84ce6becc50ebfb05b395cd0b
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/RemoveMemoryVolumesParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/RemoveMemoryVolumesParameters.java
@@ -6,13 +6,6 @@ public class RemoveMemoryVolumesParameters extends ActionParametersBase {
     /** comma-separated string of UUIDs representing the memory volumes */
     private String memoryVolumes;
     private Guid vmId;
-    /** In the general case, we remove the memory volumes only if there is only
-     *  one snapshot in DB that uses it because we remove the memory before
-     *  removing the snapshots from the DB. But in some cases we first remove
-     *  the snapshot from the DB and only then remove its memory and in that
-     *  case we should remove the memory only if no other snapshot uses it */
-    private boolean removeOnlyIfNotUsedAtAll;
-
     private boolean forceRemove;
 
     public RemoveMemoryVolumesParameters(String memoryVolumes, Guid vmId) {
@@ -44,14 +37,6 @@ public class RemoveMemoryVolumesParameters extends ActionParametersBase {
 
     public void setVmId(Guid vmId) {
         this.vmId = vmId;
-    }
-
-    public boolean isRemoveOnlyIfNotUsedAtAll() {
-        return removeOnlyIfNotUsedAtAll;
-    }
-
-    public void setRemoveOnlyIfNotUsedAtAll(boolean removeOnlyIfNotUsedAtAll) {
-        this.removeOnlyIfNotUsedAtAll = removeOnlyIfNotUsedAtAll;
     }
 
     public boolean isForceRemove() {
