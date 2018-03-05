ac048eaf6fe6e59186eeb1d1a109a951c0584796
Allon Mureinik <amureini@redhat.com>
2018-03-05 17:15:46 +0200

    core: Remove UserQueryParametersBase
    UserQueryParametersBase has been mostly unused since commit 979e98.
    It's only remaining usage is in GetUserVmsByUserIdAndGroupsParameters
    that extends it, but does not use getUsedId it provides, making this
    extension pointless.
    
    This commit finishes the job started by commit 979e98 and changes
    GetUserVmsByUserIdAndGroupsParameters to extend QueryParametersBase.
    It then removes the now unused UserQueryParametersBase and its tests.
    
    Change-Id: I8d1fee80bb560e41f9fa4ef9d049aeeec23c1ee6
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetUserVmsByUserIdAndGroupsParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/queries/GetUserVmsByUserIdAndGroupsParameters.java
@@ -1,9 +1,9 @@
 package org.ovirt.engine.core.common.queries;
 
 
-public class GetUserVmsByUserIdAndGroupsParameters extends UserQueryParametersBase {
+public class GetUserVmsByUserIdAndGroupsParameters extends QueryParametersBase {
 
-    private static final long serialVersionUID = 98112763182708327L;
+    private static final long serialVersionUID = -7339942735100317616L;
 
     public GetUserVmsByUserIdAndGroupsParameters() {
     }
--- backend/manager/modules/common/src/test/java/org/ovirt/engine/core/common/queries/UserQueryParametersBaseTest.java
+++
@@ -1,5 +0,0 @@
-package org.ovirt.engine.core.common.queries;
-
-public class UserQueryParametersBaseTest extends AbstractUserQueryParametersBaseTestCase<UserQueryParametersBase> {
-    // Empty - all tests are in the super class.
-}
