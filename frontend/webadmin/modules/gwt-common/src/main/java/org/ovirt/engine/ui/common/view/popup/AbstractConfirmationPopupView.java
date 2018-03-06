4d5475945471e7ecb0fc1447d9e38c00b1697a1e
Alexander Wels <awels@redhat.com>
2018-03-06 10:51:16 -0500

    webadmin: Fix confirm message <br>
    - Fixed another <br> showing in confirmation message.
    
    Change-Id: Iae0c8344627161d558450b6e91011638bc5d729f
    Bug-Url: https://bugzilla.redhat.com/1537495
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/view/popup/AbstractConfirmationPopupView.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/view/popup/AbstractConfirmationPopupView.java
@@ -4,6 +4,7 @@ import org.ovirt.engine.ui.common.widget.AlertWithIcon;
 import org.ovirt.engine.ui.uicommonweb.models.ConfirmationModel;
 
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiField;
 
 public abstract class AbstractConfirmationPopupView extends AbstractModelBoundPopupView<ConfirmationModel> {
@@ -17,7 +18,9 @@ public abstract class AbstractConfirmationPopupView extends AbstractModelBoundPo
 
     @Override
     public void setMessage(String message) {
-        messagePanel.setText(message != null ? message.replace("\n", "<br>") : "");//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        String escapedMessage = SafeHtmlUtils.htmlEscape(message != null ? message : ""); //$NON-NLS-1$
+        escapedMessage = escapedMessage.replace("\n", "<br>"); //$NON-NLS-1$ //$NON-NLS-2$
+        messagePanel.setHtmlText(SafeHtmlUtils.fromTrustedString(escapedMessage));
     }
 
 }
