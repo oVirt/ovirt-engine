eee76aa456581c1d117822c5c2fc3ab8285132a5
Allon Mureinik <amureini@redhat.com>
2018-03-10 17:00:09 +0200

    core: @Inject RemoveVmPoolCommandCallback's DAO
    Use modern @Injections instead of using the deprecated
    Dbfacade.getInstance() method.
    
    Change-Id: I96cfe2fcb372c53491954f7351d6ea2128bd904e
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RemoveVmPoolCommandCallback.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/RemoveVmPoolCommandCallback.java
@@ -3,15 +3,18 @@ package org.ovirt.engine.core.bll;
 import java.util.List;
 
 import javax.enterprise.inject.Typed;
+import javax.inject.Inject;
 
 import org.ovirt.engine.core.common.action.VmPoolParametersBase;
 import org.ovirt.engine.core.common.businessentities.VmPool;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.backendcompat.CommandExecutionStatus;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
+import org.ovirt.engine.core.dao.VmPoolDao;
 
 @Typed(RemoveVmPoolCommandCallback.class)
 public class RemoveVmPoolCommandCallback extends ConcurrentChildCommandsExecutionCallback {
+    @Inject
+    private VmPoolDao vmPoolDao;
 
     @Override
     protected void childCommandsExecutionEnded(CommandBase<?> command,
@@ -25,7 +28,7 @@ public class RemoveVmPoolCommandCallback extends ConcurrentChildCommandsExecutio
         } else {
             RemoveVmPoolCommand<? extends VmPoolParametersBase> removeVmPoolCommand =
                     (RemoveVmPoolCommand<? extends VmPoolParametersBase>) command;
-            VmPool pool = DbFacade.getInstance().getVmPoolDao().get(removeVmPoolCommand.getVmPoolId());
+            VmPool pool = vmPoolDao.get(removeVmPoolCommand.getVmPoolId());
             if (pool == null || pool.getRunningVmsCount() == 0) {
                 setCommandEndStatus(command, false, status, childCmdIds);
             }
