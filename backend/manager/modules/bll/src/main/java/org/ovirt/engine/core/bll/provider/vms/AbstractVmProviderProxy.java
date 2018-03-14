bc34e14870658a18f6972d3bb36934efa6c4f56f
Allon Mureinik <amureini@redhat.com>
2018-03-14 17:00:28 +0200

    core: @Inject AbstractVmProviderProxy's DAO
    Use modern @Injections instead of using the deprecated
    Dbfacade.getInstance() method.
    
    Change-Id: I44da75e7e0b1214ad525e13bd6de1f6f337109b4
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/vms/AbstractVmProviderProxy.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/provider/vms/AbstractVmProviderProxy.java
@@ -19,7 +19,6 @@ import org.ovirt.engine.core.common.queries.QueryReturnValue;
 import org.ovirt.engine.core.common.queries.QueryType;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.compat.Version;
-import org.ovirt.engine.core.dal.dbbroker.DbFacade;
 import org.ovirt.engine.core.dao.StoragePoolDao;
 import org.ovirt.engine.core.dao.VdsDao;
 
@@ -28,6 +27,8 @@ public abstract class AbstractVmProviderProxy<P extends VmProviderProperties> im
 
     @Inject
     private StoragePoolDao storagePoolDao;
+    @Inject
+    private VdsDao vdsDao;
     @Inject
     private BackendInternal backend;
 
@@ -56,7 +57,7 @@ public abstract class AbstractVmProviderProxy<P extends VmProviderProperties> im
                 P properties = provider.getAdditionalProperties();
                 Guid proxyHostId = properties.getProxyHostId();
                 if (proxyHostId != null) {
-                    VDS proxyHost = getVdsDao().get(proxyHostId);
+                    VDS proxyHost = vdsDao.get(proxyHostId);
                     if (proxyHost == null) {
                         return new ValidationResult(EngineMessage.VDS_DOES_NOT_EXIST);
                     }
@@ -66,10 +67,6 @@ public abstract class AbstractVmProviderProxy<P extends VmProviderProperties> im
                     }
                 }
                 return ValidationResult.VALID;
-            }
-
-            private VdsDao getVdsDao() {
-                return DbFacade.getInstance().getVdsDao();
             }
         };
     }
