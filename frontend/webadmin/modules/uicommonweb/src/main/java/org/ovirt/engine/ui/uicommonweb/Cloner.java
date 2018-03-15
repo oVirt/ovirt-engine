5ff439203e8d487c30afddfa7cb220fdbc33ac31
Eyal Shenitzky <eshenitz@redhat.com>
2018-03-15 11:31:32 +0200

    webadmin: set leaseStorageDomainId while cloning a VM
    While cloning VM object the leaseStorageDomainId field
    is ignored, this patch add initialization for the missing field.
    
    Change-Id: I3e25fde6efd9a65436c0f609e88fe594abec85b2
    Bug-Url: https://bugzilla.redhat.com/1554875
    Signed-off-by: Eyal Shenitzky <eshenitz@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/Cloner.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/Cloner.java
@@ -524,7 +524,7 @@ public final class Cloner {
         obj.setSmallIconId(instance.getSmallIconId());
         obj.setLargeIconId(instance.getLargeIconId());
         obj.setProviderId(instance.getProviderId());
-
+        obj.setLeaseStorageDomainId(instance.getLeaseStorageDomainId());
         return obj;
     }
 
