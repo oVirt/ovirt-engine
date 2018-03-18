19e8a2719f1caf2207f95b9dba962bc6a8aac0ba
Arik Hadas <ahadas@redhat.com>
2018-03-18 17:13:40 +0200

    core: fix removal of vm-host device
    Since patch I6ba83b6 null values of iommu-group are
    not mapped to -1 as they are expected to.
    
    Change-Id: I8bc5a2d308efa26fb6dc1a56612a7c1032abb785
    Bug-Url: https://bugzilla.redhat.com/1557777
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/hostdev/RemoveVmHostDevicesCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/hostdev/RemoveVmHostDevicesCommand.java
@@ -43,7 +43,8 @@ public class RemoveVmHostDevicesCommand extends AbstractVmHostDevicesCommand<VmH
             if (deviceExists) {
                 VmHostDevice device = existingDevices.get(hostDevice.getDeviceName());
                 existingDevicesByIommuGroup
-                        .computeIfAbsent(hostDevice.getIommuGroup(), k -> new ArrayList<>()).add(device);
+                        .computeIfAbsent(getIommuGroupKey(hostDevice.getIommuGroup()), k -> new ArrayList<>())
+                        .add(device);
 
                 if (shouldRemoveDevice) {
                     // first just set the flag that this device is not required
