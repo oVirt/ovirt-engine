cf2babb5da32b9e26397352991e1b9ce54073ace
Allon Mureinik <amureini@redhat.com>
2018-04-11 18:01:35 +0300

    core: EntityDao to extend Dao
    Make EntityDao extend the Dao marker interface, just like any other
    Dao.
    
    Change-Id: Ib7324dd02d356ed284a79f390710408c528f48d8
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/EntityDao.java
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/EntityDao.java
@@ -3,6 +3,6 @@ package org.ovirt.engine.core.dao;
 import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.compat.Guid;
 
-public interface EntityDao {
+public interface EntityDao extends Dao {
     String getEntityNameByIdAndType(Guid objectId, VdcObjectType vdcObjectType);
 }
