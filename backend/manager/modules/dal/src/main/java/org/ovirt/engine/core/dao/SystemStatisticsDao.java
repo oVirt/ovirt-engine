c3a9350c84785940c3ab10d6796985200bdc42fc
Allon Mureinik <amureini@redhat.com>
2018-04-11 18:01:36 +0300

    core: SystemStatisticsDao to extend Dao
    Make SystemStatisticsDao extend the Dao marker interface, just like
    any other Dao.
    
    Change-Id: I7d614d1ba676757942d9211d55bfcf53b4f94f21
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/SystemStatisticsDao.java
+++ backend/manager/modules/dal/src/main/java/org/ovirt/engine/core/dao/SystemStatisticsDao.java
@@ -1,6 +1,6 @@
 package org.ovirt.engine.core.dao;
 
-public interface SystemStatisticsDao {
+public interface SystemStatisticsDao extends Dao {
     Integer getSystemStatisticsValue(String entity);
 
     Integer getSystemStatisticsValue(String entity, String status);
