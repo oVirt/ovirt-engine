652bea8bc8a2b8e1443be29d5c28a168f0921f0a
Daniel Erez <derez@redhat.com>
2018-04-22 17:46:18 +0300

    core: removes disk on transfer image failure
    Invoke RemoveDisk on tranfser image failure, instead of the
    current behaviour which removes the created disk only from db.
    
    Change-Id: I071bbecdcd52d13859ec5f74d30f08aafb5ed162
    Bug-Url: https://bugzilla.redhat.com/1565673
    Signed-off-by: Daniel Erez <derez@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/TransferDiskImageCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/TransferDiskImageCommand.java
@@ -35,6 +35,7 @@ import org.ovirt.engine.core.common.action.ActionReturnValue;
 import org.ovirt.engine.core.common.action.ActionType;
 import org.ovirt.engine.core.common.action.AddDiskParameters;
 import org.ovirt.engine.core.common.action.LockProperties;
+import org.ovirt.engine.core.common.action.RemoveDiskParameters;
 import org.ovirt.engine.core.common.action.TransferDiskImageParameters;
 import org.ovirt.engine.core.common.action.TransferImageStatusParameters;
 import org.ovirt.engine.core.common.businessentities.ActionGroup;
@@ -1117,8 +1118,8 @@ public class TransferDiskImageCommand<T extends TransferDiskImageParameters> ext
     @Override
     protected void endWithFailure() {
         if (getParameters().getTransferType() == TransferType.Upload) {
-            // Do rollback only for upload
-            super.endWithFailure();
+            // Do rollback only for upload - i.e. remove the disk added in 'createImage()'
+            runInternalAction(ActionType.RemoveDisk, new RemoveDiskParameters(getParameters().getImageGroupID()));
         }
         setSucceeded(true);
     }
