b4d25a40a96b5d862c4f7251b003147df3ebe631
Greg Sheremeta <gshereme@redhat.com>
2018-04-26 23:06:00 -0400

    webadmin: fix context sensitive help paths / open in new tab
    In 5f2e699, DocsServlet changed the backend URL to the
    in-product documentation.
    
    Update CSH frontend code to match.
    
    Open docs links in new tab, not in an evil popup window with scrollbars.
    
    Change-Id: Ia5914fabbe4e45067b5dcbd00e9cad7cee1017e0
    Bug-Url: https://bugzilla.redhat.com/1572445
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/utils/WebUtils.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/utils/WebUtils.java
@@ -1,45 +1,19 @@
 package org.ovirt.engine.ui.common.utils;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
 
 public class WebUtils {
-    /**
-     * constant for the 'scrollbars' window option.
-     */
-    public static final String OPTION_SCROLLBARS = "scrollbars"; //$NON-NLS-1$
 
     /**
-     * The default options to pass to Window.open when none are specified.
-     */
-    public static final String DEFAULT_OPTIONS = OPTION_SCROLLBARS;
-
-    /**
-     * Opens a link on another browser window.
+     * Opens a link on another browser tab.
      *
      * @param name
      *            The name of the new window
      * @param url
      *            The URL to open
      */
-    public static void openUrlInNewWindow(final String name, final String url) {
-        openUrlInNewWindow(name, url, DEFAULT_OPTIONS);
-    }
-
-    /**
-     * Opens a link on another browser window.
-     *
-     * @param name The name of the new window
-     * @param url The URL to open
-     * @param options The options to pass to the window, option are described here:<br/>
-     * {@link https://developer.mozilla.org/en-US/docs/DOM/window.open}
-     */
-    public static void openUrlInNewWindow(final String name, final String url, final String options) {
-        Window.open(url, name.replace(" ", "_"), options); //$NON-NLS-1$ //$NON-NLS-2$
-    }
-
-    public static void openRelativeUrlInNewWindow(String name, String relativeUrl) {
-        openUrlInNewWindow(name, GWT.getModuleBaseURL() + relativeUrl);
+    public static void openUrlInNewTab(final String url) {
+        Window.open(url, "_blank", ""); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
 }
