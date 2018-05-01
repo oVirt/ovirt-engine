b656f1e455713c87cc8323fc6b55bcd617004e19
Eyal Shenitzky <eshenitz@redhat.com>
2018-05-01 13:40:08 +0300

    core: use snapshot VM configuration when creating a template from snapshot
    Currently, when creating a VM from a snapshot, the active VM configuration
    was used when the command CreateAllTemplateDisksCommand  creates
    the template disks.
    
    The use of the active VM configuration caused a mismatch between the
    disks that should be created and an exception was thrown when the
    snapshot contains fewer disks than the active VM snapshot.
    
    This patch fixes this behavior by using the snapshot VM configuration
    instead of the active snapshot VM configuration
    
    Change-Id: Ia50f492e43658fdc537fc68f4eeda8d5a6ffe04a
    Bug-Url: https://bugzilla.redhat.com/1573421
    Signed-off-by: Eyal Shenitzky <eshenitz@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/CreateAllTemplateDisksFromSnapshotCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/CreateAllTemplateDisksFromSnapshotCommand.java
@@ -4,6 +4,7 @@ import static org.ovirt.engine.core.bll.storage.disk.image.DisksFilter.ONLY_ACTI
 import static org.ovirt.engine.core.bll.storage.disk.image.DisksFilter.ONLY_PLUGGED;
 import static org.ovirt.engine.core.bll.storage.disk.image.DisksFilter.ONLY_SNAPABLE;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.ovirt.engine.core.bll.DisableInPrepareMode;
@@ -11,14 +12,18 @@ import org.ovirt.engine.core.bll.InternalCommandAttribute;
 import org.ovirt.engine.core.bll.NonTransactiveCommandAttribute;
 import org.ovirt.engine.core.bll.context.CommandContext;
 import org.ovirt.engine.core.bll.storage.disk.image.DisksFilter;
-import org.ovirt.engine.core.common.action.CreateAllTemplateDisksParameters;
+import org.ovirt.engine.core.common.action.CreateAllTemplateDisksFromSnapshotParameters;
+import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
+import org.ovirt.engine.core.common.queries.IdQueryParameters;
+import org.ovirt.engine.core.common.queries.QueryReturnValue;
+import org.ovirt.engine.core.common.queries.QueryType;
 import org.ovirt.engine.core.compat.Guid;
 
 @DisableInPrepareMode
 @NonTransactiveCommandAttribute
 @InternalCommandAttribute
-public class CreateAllTemplateDisksFromSnapshotCommand<T extends CreateAllTemplateDisksParameters> extends CreateAllTemplateDisksCommand<T> {
+public class CreateAllTemplateDisksFromSnapshotCommand<T extends CreateAllTemplateDisksFromSnapshotParameters> extends CreateAllTemplateDisksCommand<T> {
 
     public CreateAllTemplateDisksFromSnapshotCommand(Guid commandId) {
         super(commandId);
@@ -30,11 +35,22 @@ public class CreateAllTemplateDisksFromSnapshotCommand<T extends CreateAllTempla
 
     @Override
     protected List<DiskImage> getVmDisksFromDb() {
-        vmHandler.updateDisksFromDb(getVm());
+        VM vmFromConfiguration = getVmFromConfiguration();
+        if (vmFromConfiguration == null) {
+            log.debug("No VM configuration found for snapshot id '{}'", getParameters().getSnapshotId());
+            return Collections.emptyList();
+        }
         List<DiskImage> disksFromDb =
-                DisksFilter.filterImageDisks(getVm().getDiskMap().values(), ONLY_SNAPABLE, ONLY_ACTIVE);
+                DisksFilter.filterImageDisks(vmFromConfiguration.getDiskMap().values(), ONLY_SNAPABLE, ONLY_ACTIVE);
         disksFromDb.addAll(DisksFilter.filterCinderDisks(getVm().getDiskMap().values(), ONLY_PLUGGED));
         return disksFromDb;
+    }
+
+    private VM getVmFromConfiguration() {
+        QueryReturnValue queryReturnValue = runInternalQuery(
+                QueryType.GetVmConfigurationBySnapshot,
+                new IdQueryParameters(getParameters().getSnapshotId()));
+        return queryReturnValue.getSucceeded() ? queryReturnValue.getReturnValue() : null;
     }
 
 }
