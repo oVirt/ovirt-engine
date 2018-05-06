e2802cf71550e123025b77f30b4e7d634657c220
Allon Mureinik <amureini@redhat.com>
2018-05-06 23:39:13 +0300

    engine: Remove unmaintained integration tests
    The integration tests aren't run in any CI, and are probably broken
    anyway by this stage.
    
    Removing them will remove the seemliness of them being maintained,
    and will prevent them from blocking the project from moving forward,
    e.g., to JUnit Jupiter.
    
    Change-Id: Ie43791ade3d5a376bc70e28ad7fbfa44e1feea58
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/arquillian/IntegrationTest.java
+++
@@ -1,8 +0,0 @@
-package org.ovirt.engine.arquillian;
-
-/**
- * Class to use in JUnit tests to indicate which tests are integration tests.
- * Use it in conjunction with {@link org.junit.experimental.categories.Category}.
- */
-public interface IntegrationTest {
-}
