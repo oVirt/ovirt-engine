0137a11d6d90ae020f982c15775970477281eabe
Allon Mureinik <amureini@redhat.com>
2018-05-07 17:56:37 +0300

    webadmin: SearchableListModelTest mocking
    @Spying with an annotation on a abstract class may be removed in
    later Mockito versions. Replacing it with an explicit call to
    Mockito.mock (with settings to use the real constructor and call real
    methods) will help future proof the code.
    
    Change-Id: Id6dcf2b2d3863e714f89aa1a3a7fb494c7bc67f1
    Signed-off-by: Allon Mureinik <amureini@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/test/java/org/ovirt/engine/ui/uicommonweb/models/SearchableListModelTest.java
+++ frontend/webadmin/modules/uicommonweb/src/test/java/org/ovirt/engine/ui/uicommonweb/models/SearchableListModelTest.java
@@ -1,27 +1,25 @@
 package org.ovirt.engine.ui.uicommonweb.models;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
 
 import java.util.Arrays;
 
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner.Silent;
+import org.mockito.Answers;
 import org.ovirt.engine.ui.uicommonweb.junit.UiCommonSetup;
 
-@RunWith(Silent.class)
 public class SearchableListModelTest {
 
     @ClassRule
     public static UiCommonSetup setup = new UiCommonSetup();
 
-    @Spy
-    private SearchableListModel<Void, Integer> listModel;
-
     @Test
     public void testSelectionRestoredOnNewSetItems() {
+        SearchableListModel<Void, Integer> listModel =
+                mock(SearchableListModel.class, withSettings().useConstructor().defaultAnswer(Answers.CALLS_REAL_METHODS));
         listModel.setItems(Arrays.asList(1, 2, 3));
         listModel.setSelectedItem(2);
 
