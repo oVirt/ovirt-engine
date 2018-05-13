5f5f1a1cba0253c260f07290c6283d6e1e2c04d8
Arik Hadas <ahadas@redhat.com>
2018-05-13 17:24:34 +0300

    core: hot unplug disk with alias-only xml
    Generate an XML for unplugging a disk that only includes
    the alias of the device. For the time being, VDSM uses
    the alias to find the complete XML of that device but
    eventually we hope that would be the API for unplugging
    a disk in libvirt.
    
    Change-Id: I1aa3e8c9ce141f882dc51fbf93648bea582f938b
    Bug-Url: https://bugzilla.redhat.com/1576729
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/HotUnPlugDiskVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/HotUnPlugDiskVDSCommand.java
@@ -1,8 +1,16 @@
 package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.common.vdscommands.HotPlugDiskVDSParameters;
+import org.ovirt.engine.core.utils.XmlUtils;
+import org.ovirt.engine.core.vdsbroker.builder.vminfo.LibvirtVmXmlBuilder;
+import org.ovirt.engine.core.vdsbroker.builder.vminfo.VmInfoBuildUtils;
 
 public class HotUnPlugDiskVDSCommand<P extends HotPlugDiskVDSParameters> extends HotPlugDiskVDSCommand<P> {
+
+    @Inject
+    private VmInfoBuildUtils vmInfoBuildUtils;
 
     public HotUnPlugDiskVDSCommand(P parameters) {
         super(parameters);
@@ -14,4 +22,19 @@ public class HotUnPlugDiskVDSCommand<P extends HotPlugDiskVDSParameters> extends
         proceedProxyReturnValue();
     }
 
+    @Override
+    protected String generateDomainXml() {
+        LibvirtVmXmlBuilder builder = new LibvirtVmXmlBuilder(
+                getParameters().getVm(),
+                getVds().getId(),
+                getParameters().getDisk(),
+                getParameters().getVmDevice(),
+                vmInfoBuildUtils);
+        String libvirtXml = builder.buildHotunplugDisk();
+        String prettyLibvirtXml = XmlUtils.prettify(libvirtXml);
+        if (prettyLibvirtXml != null) {
+            log.info("Disk hot-unplug: {}", prettyLibvirtXml);
+        }
+        return libvirtXml;
+    }
 }
