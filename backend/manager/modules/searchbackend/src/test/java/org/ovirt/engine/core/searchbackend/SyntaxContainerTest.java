1a214e152599ba6abd18b904f63286122f8e2f27
Roy Golan <rgolan@redhat.com>
2018-05-24 11:16:05 +0300

    searchbackend: Add unit test to cover tag usages in search
    This test will cover the tag usage behaviour, when analyzing a search
    text. The motivation is to later change the bahaviour so searches which
    do not need tags, will not join them in the saerch.
    
    Change-Id: Icba2419b975e40c7843b55ef33468fd43021aab3
    Bug-Url: https://bugzilla.redhat.com/1578357
    Signed-off-by: Roy Golan <rgolan@redhat.com>
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ backend/manager/modules/searchbackend/src/test/java/org/ovirt/engine/core/searchbackend/SyntaxContainerTest.java
@@ -0,0 +1,55 @@
+package org.ovirt.engine.core.searchbackend;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class SyntaxContainerTest {
+
+    private static final boolean USING_TAGS = true;
+    private static final boolean NOT_USING_TAGS = false;
+
+    private boolean expected;
+    private String input;
+
+    public SyntaxContainerTest(boolean expected, String input) {
+        this.expected = expected;
+        this.input = input;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> searchTexts() {
+        return Arrays.asList(new Object[][]{
+                {USING_TAGS, "Users: type=user"},
+                {USING_TAGS, "Users: type=group"},
+                {USING_TAGS, "Users: type=user and vm.id=12345678-1234-1234-1234-1234-123456789012"},
+                {USING_TAGS, "Users: type=user and tag = foo"},
+                {USING_TAGS, "Hosts: tag=foo"},
+                {NOT_USING_TAGS, "Hosts:"},
+                {NOT_USING_TAGS, "Vms:"},
+                {NOT_USING_TAGS, "Datacenters:"},
+                {NOT_USING_TAGS, "Clusters:"},
+        });
+    }
+
+    /**
+     * Make sure this searchText produces a search on the related tag table as well.
+     * For search on users the tag table is also used for user's VMs.
+     *
+     * On other cases, make sure the searchText DOES NOT use tag, i.e will not produce a join on the related
+     * tag table. Those searches, since they are code-generated, are sub-optimal and we wan't to avoid them
+     * when they are not needed. Specifically the general default searches for a default UI grids must
+     * be snappy and perform well.
+     */
+    @Test
+    public void searchUsingTags() {
+        assertEquals(expected, new SyntaxChecker(20).analyzeSyntaxState(input, false).isSearchUsingTags());
+    }
+
+}
