c375f39934789e3456789c4eae903995427a8d25
Roy Golan <rgolan@redhat.com>
2018-05-24 11:16:05 +0300

    searchbackend: Add unit test to cover tag usages in search
    This test will cover the tag usage behaviour, when analyzing a search
    text. The motivation is to later change the bahaviour so searches which
    do not need tags, will not join them in the saerch.
    
    Change-Id: Icba2419b975e40c7843b55ef33468fd43021aab3
    Bug-Url: https://bugzilla.redhat.com/1578357
    Signed-off-by: Roy Golan <rgolan@redhat.com>

---
+++ backend/manager/modules/searchbackend/src/test/java/org/ovirt/engine/core/searchbackend/SyntaxContainerTest.java
@@ -0,0 +1,48 @@
+package org.ovirt.engine.core.searchbackend;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SyntaxContainerTest {
+
+    /**
+     * Make sure this searchText produces a search on the related tag table as well
+     * For search on users the tag table is also used for user's VMs.
+     * @param searchText the search expression
+     */
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Users: type=user",
+            "Users: type=group",
+            "Users: type=user and vm.id=12345678-1234-1234-1234-1234-123456789012",
+            "Users: type=user and tag = foo",
+            "Hosts: tag=foo"
+    })
+    void searchUsingTags(String searchText) {
+        SyntaxChecker sc = new SyntaxChecker();
+        assertTrue(sc.analyzeSyntaxState(searchText, false).isSearchUsingTags());
+    }
+
+    /**
+     * Make sure this searchText DOES NOT use tag, i.e will not produce a join on the related
+     * tag table. Those searches, since they are code-generated, are sub-optimal and we wan't to avoid them
+     * when they are not needed. Specifically the general default searches for a default UI grids must
+     * be snappy and perform well.
+     * @param searchText the search expression
+     */
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Hosts:",
+            "Vms:",
+            "Datacenters:",
+            "Clusters:"
+    })
+    void searchNotUsingTags(String searchText) {
+        SyntaxChecker sc = new SyntaxChecker();
+        assertFalse(sc.analyzeSyntaxState(searchText, false).isSearchUsingTags());
+    }
+}
