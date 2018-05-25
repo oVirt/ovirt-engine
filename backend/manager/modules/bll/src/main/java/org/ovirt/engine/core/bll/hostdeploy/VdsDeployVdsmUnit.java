cbc2fe7e694a912426563d4988f722ad07bd156d
Martin Perina <mperina@redhat.com>
2018-05-25 15:50:59 +0200

    core: Enable only strong ciphers for 4.2 hosts
    Enables only strong ciphers when adding or reinstalling hosts in
    clusters with cluster level >= 4.2
    
    Change-Id: I5a6aa826b4f1ab1209b79a75ea84510b96e18a68
    Bug-Url: https://bugzilla.redhat.com/1582527
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/hostdeploy/VdsDeployVdsmUnit.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/hostdeploy/VdsDeployVdsmUnit.java
@@ -82,6 +82,18 @@ public class VdsDeployVdsmUnit implements VdsDeployUnit {
             return true;
         }},
         new Callable<Boolean>() { public Boolean call() throws Exception {
+            if (Version.v4_2.lessOrEquals(clusterVersion)) {
+                _deploy.getParser().cliEnvironmentSet(
+                        String.format(
+                                "%svars/ssl_ciphers",
+                                VdsmEnv.CONFIG_PREFIX
+                        ),
+                        "HIGH"
+                );
+            }
+            return true;
+        }},
+        new Callable<Boolean>() { public Boolean call() throws Exception {
             _deploy.getParser().cliEnvironmentSet(
                 String.format(
                     "%saddresses/management_port",
