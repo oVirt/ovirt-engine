1587fa2d933f9f4e73e8424d71d15514d8ea1b70
Asaf Rachmani <arachman@redhat.com>
2018-06-03 16:25:07 +0300

    packaging: dbscripts: upgrade: Verify that index does not exist
    Verify that index does not exist before creating it
    
    Change-Id: I5971763f7b5d016c65dc6cdaae8b5e6f106c2b4a
    https://bugzilla.redhat.com/1585483
    Signed-off-by: Asaf Rachmani <arachman@redhat.com>

--- packaging/dbscripts/upgrade/04_03_0070_add_missing_indexes.sql
+++ packaging/dbscripts/upgrade/04_03_0070_add_missing_indexes.sql
@@ -1,4 +1,4 @@
-CREATE INDEX CONCURRENTLY idx_network_provider_physical_network_id
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_network_provider_physical_network_id
     ON network (provider_physical_network_id);
-CREATE INDEX CONCURRENTLY idx_provider_binding_host_id_vds_id
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_provider_binding_host_id_vds_id
     ON provider_binding_host_id (vds_id);
