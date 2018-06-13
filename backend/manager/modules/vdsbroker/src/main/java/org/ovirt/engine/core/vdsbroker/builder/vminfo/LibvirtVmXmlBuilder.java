46e2c53da8e82e166e7d11b31b3f131daeecc2e4
Arik Hadas <ahadas@redhat.com>
2018-06-13 10:13:06 +0300

    core: set vgpu device in libvirt xml
    This patch changes LibvirtVmXmlBuilder to:
    1. Add the host device of vGPU, for example:
    
    <hostdev mode="subsystem" type="mdev" model="vfio-pci">
      <source>
        <address uuid="5c14b34a-6fd5-4486-8038-92b50960fcbc"/>
      </source>
    </hostdev>
    
    2. Add the value of mdev_type property to the metadata, for example:
    
    <ovirt-vm:device devtype="hostdev" uuid="5c14b34a-6fd5-4486-8038-92b50960fcbc">
      <ovirt-vm:mdevType>4</ovirt-vm:mdevType>
    </ovirt-vm:device>
    
    Change-Id: I6ac4f07dcf995d1966a02a016b5f6617a3713ca0
    Bug-Url: https://bugzilla.redhat.com/1581709
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/builder/vminfo/LibvirtVmXmlBuilder.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/builder/vminfo/LibvirtVmXmlBuilder.java
@@ -128,6 +128,7 @@ public class LibvirtVmXmlBuilder {
 
     private Map<String, Map<String, Object>> vnicMetadata;
     private Map<String, Map<String, String>> diskMetadata;
+    private Map<String, Map<String, String>> mdevMetadata;
     private Pair<String, VmPayload> payloadMetadata;
 
     private List<Pair<Guid, Guid>> volumeLeases = Collections.emptyList();
@@ -205,6 +206,7 @@ public class LibvirtVmXmlBuilder {
         cdRomIndex = -1;
         vnicMetadata = new HashMap<>();
         diskMetadata = new HashMap<>();
+        mdevMetadata = new HashMap<>();
         hypervEnabled = vmInfoBuildUtils.isHypervEnabled(vm.getVmOsId(), vm.getCompatibilityVersion());
         emulatedMachine = vm.getEmulatedMachine() != null ?
                 vm.getEmulatedMachine()
@@ -254,7 +256,7 @@ public class LibvirtVmXmlBuilder {
         // note that this must be called after writeDevices to get the serial console, if exists
         writeOs();
         writeMemoryBacking();
-        writeMetadata();
+        writeMetadata(); // must remain the last call!
         return writer.getStringXML();
     }
 
@@ -838,6 +840,7 @@ public class LibvirtVmXmlBuilder {
         writer.writeStartElement(OVIRT_VM_URI, "vm");
         writeMinGuaranteedMemoryMetadata();
         writeClusterVersionMetadata();
+        writeMDevMetadata();
         writeVmCustomMetadata();
         writeNetworkInterfaceMetadata();
         writeDiskMetadata();
@@ -845,6 +848,16 @@ public class LibvirtVmXmlBuilder {
         writePayloadMetadata();
         writeResumeBehaviorMetadata();
         writer.writeEndElement();
+    }
+
+    private void writeMDevMetadata() {
+        mdevMetadata.forEach((address, data) -> {
+            writer.writeStartElement(OVIRT_VM_URI, "device");
+            writer.writeAttributeString("devtype", "hostdev");
+            writer.writeAttributeString("uuid", address);
+            data.forEach((key, value) -> writer.writeElement(OVIRT_VM_URI, key, value.toString()));
+            writer.writeEndElement();
+        });
     }
 
     private void writeResumeBehaviorMetadata() {
@@ -1037,6 +1050,8 @@ public class LibvirtVmXmlBuilder {
         writeDisks(diskDevices);
         writeLeases();
 
+        writeVGpu();
+
         writer.writeEndElement();
     }
 
@@ -1106,6 +1121,26 @@ public class LibvirtVmXmlBuilder {
                 managedAndPluggedBootableDevices,
                 vm.getInterfaces(),
                 VmDeviceCommonUtils.extractDiskVmElements(vm));
+    }
+
+    private void writeVGpu() {
+        String mdevType = vmCustomProperties.remove("mdev_type");
+        if (mdevType != null) {
+            writer.writeStartElement("hostdev");
+            writer.writeAttributeString("mode", "subsystem");
+            writer.writeAttributeString("type", "mdev");
+            writer.writeAttributeString("model", "vfio-pci");
+
+            writer.writeStartElement("source");
+            String address = Guid.newGuid().toString();
+            writer.writeStartElement("address");
+            writer.writeAttributeString("uuid", address);
+            writer.writeEndElement();
+            writer.writeEndElement();
+
+            writer.writeEndElement();
+            mdevMetadata.put(address, Collections.singletonMap("mdevType", mdevType));
+        }
     }
 
     private void writeLeases() {
