1b20368aa22a314801e47771c276b431b8864a6b
Shmuel Melamud <smelamud@redhat.com>
2018-06-20 23:13:25 +0300

    core: Better descriptions for BIOS types
    Making descriptions of BIOS types more user-friendly. Removing unused
    description field from BiosType enum.
    
    Change-Id: Ieed0d0f5db18e3467796c6ce740e4f0eed143a01
    Bug-Url: https://bugzilla.redhat.com/1327846
    Signed-off-by: Shmuel Melamud <smelamud@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/BiosType.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/BiosType.java
@@ -7,22 +7,20 @@ import java.util.stream.Stream;
 
 public enum BiosType implements Identifiable {
 
-    I440FX_SEA_BIOS(0, ChipsetType.I440FX, "i440fx/BIOS"),
-    Q35_SEA_BIOS(1, ChipsetType.Q35, "q35/BIOS"),
-    Q35_OVMF(2, ChipsetType.Q35, "q35/UEFI"),
-    Q35_SECURE_BOOT(3, ChipsetType.Q35, "q35/SecureBoot");
+    I440FX_SEA_BIOS(0, ChipsetType.I440FX),
+    Q35_SEA_BIOS(1, ChipsetType.Q35),
+    Q35_OVMF(2, ChipsetType.Q35),
+    Q35_SECURE_BOOT(3, ChipsetType.Q35);
 
     private int value;
     private ChipsetType chipsetType;
-    private String description;
 
     private static final Map<Integer, BiosType> valueToBios =
             Stream.of(values()).collect(Collectors.toMap(BiosType::getValue, Function.identity()));
 
-    BiosType(int value, ChipsetType chipsetType, String description) {
+    BiosType(int value, ChipsetType chipsetType) {
         this.value = value;
         this.chipsetType = chipsetType;
-        this.description = description;
     }
 
     public int getValue() {
@@ -31,10 +29,6 @@ public enum BiosType implements Identifiable {
 
     public ChipsetType getChipsetType() {
         return chipsetType;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public static BiosType forValue(int value) {
