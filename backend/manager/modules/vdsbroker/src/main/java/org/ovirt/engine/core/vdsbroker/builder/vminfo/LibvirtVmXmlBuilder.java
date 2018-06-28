b273bf68789dcccaa2fd4850be3d683e59256447
Alona Kaplan <alkaplan@redhat.com>
2018-06-28 09:51:51 +0300

    engine: Specify non custom queues only if 'vmStatic.multiQueuesEnabled'
    Change-Id: I2c291764894318211ba1607bcdf9a20f728836e0
    Bug-Url:https://bugzilla.redhat.com/1574771
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/builder/vminfo/LibvirtVmXmlBuilder.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/builder/vminfo/LibvirtVmXmlBuilder.java
@@ -2137,7 +2137,8 @@ public class LibvirtVmXmlBuilder {
                 queues = vnicProfile.getCustomProperties().remove("queues");
             }
 
-            if (queues == null && vmInfoBuildUtils.isInterfaceQueuable(device, nic)) {
+            if (queues == null && vm.getStaticData().isMultiQueuesEnabled()
+                    && vmInfoBuildUtils.isInterfaceQueuable(device, nic)) {
                 queues = String.valueOf(vmInfoBuildUtils.getOptimalNumOfQueuesPerVnic(vm.getNumOfCpus()));
             }
 
