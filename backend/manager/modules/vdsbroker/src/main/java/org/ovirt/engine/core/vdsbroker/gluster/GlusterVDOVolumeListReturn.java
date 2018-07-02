fba6c330db6ed23fed7feeeb38a78ab4f60d7a36
Denis Chaplygin <dchaplyg@redhat.com>
2018-07-02 13:34:51 +0200

    gluster: Added support for physical/logical blocks usage
    To calculate VDO savings, logical/physical usage data needs
    to be retrieved from the VDSM.
    
    Change-Id: Iace9df3c7600a236fb4de276f9bad32bd325551e
    Bug-Url: https://bugzilla.redhat.com/1590967
    Signed-off-by: Denis Chaplygin <dchaplyg@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/gluster/GlusterVDOVolumeListReturn.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/gluster/GlusterVDOVolumeListReturn.java
@@ -13,6 +13,8 @@ public class GlusterVDOVolumeListReturn extends StatusReturn {
     private static final String VDO_VOLUME_DEVICE = "device";
     private static final String VDO_VOLUME_SIZE = "size";
     private static final String VDO_VOLUME_FREE = "free";
+    private static final String VDO_VOLUME_LOGICAL_BLOCKS = "logicalBytesUsed";
+    private static final String VDO_VOLUME_PHYSICAL_BLOCKS = "physicalBytesUsed";
 
     private final List<GlusterVDOVolume> vdoVolumes = new ArrayList<>();
 
@@ -28,6 +30,10 @@ public class GlusterVDOVolumeListReturn extends StatusReturn {
             volume.setDevice((String)volumeData.get(VDO_VOLUME_DEVICE));
             volume.setSize((Number)volumeData.get(VDO_VOLUME_SIZE));
             volume.setFree((Number)volumeData.get(VDO_VOLUME_FREE));
+
+            //This data may be missing, so some safe default values are needed.
+            volume.setLogicalBlocks((Number)volumeData.getOrDefault(VDO_VOLUME_LOGICAL_BLOCKS, 0));
+            volume.setPhysicalBlocks((Number)volumeData.getOrDefault(VDO_VOLUME_PHYSICAL_BLOCKS, 0));
 
             vdoVolumes.add(volume);
         }
