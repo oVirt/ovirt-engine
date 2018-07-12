a1a0b04d94ef66a77ef24f4befb2a9f75d372d20
Sahina Bose <sabose@redhat.com>
2018-07-12 20:45:29 +0530

    gluster: Added unittest for capacity calculation
    Change-Id: Iae3d01f8a334ba8aa91c9bf2f3cdeebfe63b94c8
    Signed-off-by: Sahina Bose <sabose@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1568893
    Signed-off-by: Denis Chaplygin <dchaplyg@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/gluster/GlusterThinDeviceServiceTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/gluster/GlusterThinDeviceServiceTest.java
@@ -3,6 +3,7 @@ package org.ovirt.engine.core.bll.gluster;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -26,6 +27,7 @@ import org.ovirt.engine.core.common.businessentities.gluster.GlusterLocalVolumeI
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeEntity;
 import org.ovirt.engine.core.common.businessentities.gluster.GlusterVolumeType;
 import org.ovirt.engine.core.common.interfaces.VDSBrokerFrontend;
+import org.ovirt.engine.core.common.utils.SizeConverter;
 import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.VdsDao;
 import org.ovirt.engine.core.dao.gluster.GlusterBrickDao;
@@ -52,28 +54,38 @@ public class GlusterThinDeviceServiceTest {
     private GlusterThinDeviceService thinDeviceService;
 
     GlusterBrickEntity brick;
-    BrickProperties brickProperties;
 
     @Before
     public void setUp() {
-        brickProperties = new BrickProperties();
-        brickProperties.setConfirmedTotalSize(100.500);
-        brickProperties.setConfirmedFreeSize(500.100);
-        brickProperties.setDevice("test");
+        brick = getBrick(brickId, "test", 100.500, 500.100);
+    }
+
+    private GlusterBrickEntity getBrick(Guid id, String device, Double confirmedTotalSize,
+            Double confirmedFreeSize) {
+        GlusterBrickEntity brick = new GlusterBrickEntity();
+        brick.setId(id);
+        BrickProperties brickProperties = new BrickProperties();
+        brickProperties.setConfirmedTotalSize(confirmedTotalSize);
+        brickProperties.setConfirmedFreeSize(confirmedFreeSize);
+        brickProperties.setDevice(device);
 
         BrickDetails brickDetails = new BrickDetails();
         brickDetails.setBrickProperties(brickProperties);
 
-        brick = new GlusterBrickEntity();
-        brick.setId(brickId);
         brick.setServerId(serverId);
         brick.setBrickDetails(brickDetails);
+        if (!brickId.equals(id)) {
+            doReturn(brick).when(brickDao).getById(id);
+        }
 
+        return brick;
     }
 
     @Test
     public void testSetConfirmedSizeNull() {
-        brickProperties = thinDeviceService.setConfirmedSize(Collections.emptyMap(), brick, brickProperties);
+
+        BrickProperties brickProperties =
+                thinDeviceService.setConfirmedSize(Collections.emptyMap(), brick, brick.getBrickProperties());
         assertThat(brickProperties.getConfirmedTotalSize(), closeTo(100.500, 0.001));
         assertThat(brickProperties.getConfirmedFreeSize(), closeTo(500.100, 0.001));
     }
@@ -87,7 +99,8 @@ public class GlusterThinDeviceServiceTest {
         Map<Guid, GlusterLocalVolumeInfo> volumeInfoMap = new HashMap<>();
         volumeInfoMap.put(serverId, volumeInfo);
 
-        brickProperties = thinDeviceService.setConfirmedSize(volumeInfoMap, brick, brickProperties);
+        BrickProperties brickProperties =
+                thinDeviceService.setConfirmedSize(volumeInfoMap, brick, brick.getBrickProperties());
         assertThat(brickProperties.getConfirmedFreeSize(), closeTo(8.5, 0.1));
         assertThat(brickProperties.getConfirmedTotalSize(), closeTo(34.3, 0.1));
     }
@@ -95,12 +108,54 @@ public class GlusterThinDeviceServiceTest {
     @Test
     public void testConfirmedVolumeCapacity() {
         GlusterVolumeEntity volumeEntity = new GlusterVolumeEntity();
-        volumeEntity.addBrick(brick);
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test2", 300.1, 500.1));
         volumeEntity.setVolumeType(GlusterVolumeType.DISTRIBUTE);
 
-        when(brickDao.getById(brickId)).thenReturn(brick);
+        long volumeConfirmedCapacity = thinDeviceService.calculateConfirmedVolumeCapacity(volumeEntity);
+        long value = (long) (500.1 * SizeConverter.BYTES_IN_MB);
+        assertEquals(value, volumeConfirmedCapacity);
+    }
+
+    @Test
+    public void testConfirmedVolumeCapacityForDist() {
+        GlusterVolumeEntity volumeEntity = new GlusterVolumeEntity();
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test2", 300.1, 500.1));
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test2", 300.1, 400.2));
+        volumeEntity.setVolumeType(GlusterVolumeType.DISTRIBUTE);
 
         long volumeConfirmedCapacity = thinDeviceService.calculateConfirmedVolumeCapacity(volumeEntity);
-        assertEquals(524392857, volumeConfirmedCapacity);
+        long expectedValue = (long) (500.1 * SizeConverter.BYTES_IN_MB) + (long) (400.2 * SizeConverter.BYTES_IN_MB);
+        assertEquals(expectedValue, volumeConfirmedCapacity);
+    }
+
+    @Test
+    public void testConfirmedVolumeCapacityForReplica() {
+        GlusterVolumeEntity volumeEntity = new GlusterVolumeEntity();
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test2", 100.1, 500.2));
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test2", 300.1, 400.2));
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test3", 300.1, 200.2));
+        volumeEntity.setVolumeType(GlusterVolumeType.REPLICATE);
+        volumeEntity.setReplicaCount(3);
+
+        long volumeConfirmedCapacity = thinDeviceService.calculateConfirmedVolumeCapacity(volumeEntity);
+        long expectedValue = (long) (200.2 * SizeConverter.BYTES_IN_MB);
+        assertEquals(expectedValue, volumeConfirmedCapacity);
+    }
+
+    @Test
+    public void testConfirmedVolumeCapacityForDistributedReplica() {
+        GlusterVolumeEntity volumeEntity = new GlusterVolumeEntity();
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test2", 300.1, 500.2));
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test2", 300.1, 400.2));
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test3", 300.1, 200.2));
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test4", 300.1, 600.2));
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test5", 300.1, 300.2));
+        volumeEntity.addBrick(getBrick(Guid.newGuid(), "test6", 300.1, 400.2));
+        volumeEntity.setVolumeType(GlusterVolumeType.DISTRIBUTED_REPLICATE);
+        volumeEntity.setReplicaCount(3);
+
+        long volumeConfirmedCapacity = thinDeviceService.calculateConfirmedVolumeCapacity(volumeEntity);
+        long expectedValue = (long) (200.2 * SizeConverter.BYTES_IN_MB + 300.2 * SizeConverter.BYTES_IN_MB);
+        assertEquals(expectedValue, volumeConfirmedCapacity);
     }
 }
