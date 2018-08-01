d4bf4d7af76cf8adbef34af9afa9d4227f6a715e
Alona Kaplan <alkaplan@redhat.com>
2018-08-01 11:07:21 +0300

    engine: Support LibvirtVmXml for UpdateVnic
    Change-Id: I4d624095a4c064f1509118b981a5bbfe206aae5e
    Bug-Url: https://bugzilla.redhat.com/1607704
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>
    (cherry picked from commit bd341b816ef012bc70546ce883e9261d2d401f48)

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/UpdateVmInterfaceVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/UpdateVmInterfaceVDSCommand.java
@@ -1,13 +1,17 @@
 package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.ovirt.engine.core.common.FeatureSupported;
 import org.ovirt.engine.core.common.businessentities.VmDevice;
 import org.ovirt.engine.core.common.businessentities.network.VmNic;
 import org.ovirt.engine.core.common.vdscommands.VmNicDeviceVDSParameters;
+import org.ovirt.engine.core.utils.XmlUtils;
+import org.ovirt.engine.core.vdsbroker.builder.vminfo.LibvirtVmXmlBuilder;
 import org.ovirt.engine.core.vdsbroker.builder.vminfo.VmInfoBuildUtils;
 
 public class UpdateVmInterfaceVDSCommand extends VdsBrokerCommand<VmNicDeviceVDSParameters> {
@@ -21,8 +25,19 @@ public class UpdateVmInterfaceVDSCommand extends VdsBrokerCommand<VmNicDeviceVDS
 
     @Override
     protected void executeVdsBrokerCommand() {
-        status = getBroker().vmUpdateDevice(getParameters().getVm().getId().toString(), initDeviceStructure());
+        status = getBroker().vmUpdateDevice(getParameters().getVm().getId().toString(), createParametersStruct());
         proceedProxyReturnValue();
+    }
+
+    protected Map<String, Object> createParametersStruct() {
+        if (FeatureSupported.isDomainXMLSupported(getParameters().getVm().getClusterCompatibilityVersion())) {
+            Map<String, Object> updateInfo = new HashMap<>();
+            updateInfo.put(VdsProperties.DeviceType, getParameters().getVmDevice().getType().getValue());
+            updateInfo.put(VdsProperties.engineXml, generateDomainXml());
+            return updateInfo;
+        } else {
+            return initDeviceStructure();
+        }
     }
 
     protected Map<String, Object> initDeviceStructure() {
@@ -39,4 +54,22 @@ public class UpdateVmInterfaceVDSCommand extends VdsBrokerCommand<VmNicDeviceVDS
         return deviceStruct;
     }
 
+    private String generateDomainXml() {
+        VmNic nic = getParameters().getNic();
+        VmDevice vmDevice = getParameters().getVmDevice();
+        LibvirtVmXmlBuilder builder = new LibvirtVmXmlBuilder(
+                getParameters().getVm(),
+                getVds().getId(),
+                nic,
+                vmDevice,
+                vmInfoBuildUtils,
+                nic.isPassthrough() ? Collections.singletonMap(nic.getId(), vmDevice.getHostDevice())
+                        : Collections.emptyMap());
+        String libvirtXml = builder.buildHotplugNic();
+        String prettyLibvirtXml = XmlUtils.prettify(libvirtXml);
+        if (prettyLibvirtXml != null) {
+            log.info("NIC update: {}", prettyLibvirtXml);
+        }
+        return libvirtXml;
+    }
 }
