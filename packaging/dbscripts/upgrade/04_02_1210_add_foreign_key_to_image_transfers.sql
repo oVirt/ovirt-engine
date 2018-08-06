87bf6e307934923ad5ac32aa9ca6f8bc16724643
Daniel Erez <derez@redhat.com>
2018-08-06 10:52:46 +0300

    core: clean stale image_transfer on upgrade
    To avoid a foreign key violation when adding a constraint
    on image_transfers, clear "zombie" records beforehand.
    I.e. image_transfers records without correlated command_entities
    records should be deleted (such stale records could reamin in
    DB if tasks were cleared during the upgrade process).
    
    Change-Id: I682a3e59fb53c14c489ac4c82fb2e80f481f358b
    Bug-Url: https://bugzilla.redhat.com/1609839
    Signed-off-by: Daniel Erez <derez@redhat.com>
    (cherry picked from commit d698626223f0e04ebea372e43e674f95db5ead2e)

--- packaging/dbscripts/upgrade/04_02_1210_add_foreign_key_to_image_transfers.sql
+++ packaging/dbscripts/upgrade/04_02_1210_add_foreign_key_to_image_transfers.sql
@@ -1,1 +1,8 @@
+-- Delete stale image_transfers records which don't have correlate records in command_entities
+-- (this is done to avoid a foreign key violation when adding the constraint).
+DELETE FROM image_transfers
+WHERE command_id NOT IN
+    (SELECT    command_entities.command_id
+     FROM      command_entities);
+
 ALTER TABLE ONLY image_transfers ADD CONSTRAINT fk_image_transfers_command_enitites FOREIGN KEY (command_id) REFERENCES command_entities(command_id) ON DELETE CASCADE;
