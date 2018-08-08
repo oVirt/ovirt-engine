cc6ac32293386176ca4101d075e81f0b268ca84e
Kaustav Majumder <kmajumde@redhat.com>
2018-08-08 16:31:55 +0530

    engine : Added check for brick status before geo-rep sync
    Validating if all bricks are available for a volume
    when syncing a geo-rep session.
    
    Change-Id: I6925a7f07bd2472e867b9b52f279d2f8351e88b0
    Bug-Url: https://bugzilla.redhat.com/1595140
    Signed-off-by: Kaustav Majumder <kmajumde@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/GlusterGeoRepSyncJob.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/gluster/GlusterGeoRepSyncJob.java
@@ -360,10 +360,16 @@ public class GlusterGeoRepSyncJob extends GlusterJob {
      * This method updates the status depending on health of individual nodes
      */
     private void updateGeoRepStatus(GlusterVolumeEntity volume, GlusterGeoRepSession session) {
-
         List<Set<GeoRepSessionStatus>> list = new ArrayList<>();
         // grouped node status
         int replicaCount = volume.getReplicaCount() == 0 ? 1 : volume.getReplicaCount();
+        if (replicaCount > volume.getBricks().size()) {
+            session.setStatus(GeoRepSessionStatus.UNKNOWN);
+            log.info("Not all bricks are available for this volume. replicaCount : {} , volumeBrickCount: {}",
+                    replicaCount,
+                    volume.getBricks().size());
+            return;
+        }
         for (int i = 0; i < volume.getBricks().size(); i = i + replicaCount) {
             Set<GeoRepSessionStatus> subVolumeStatusSet = new HashSet<>();
             int j = 0;
@@ -504,5 +510,4 @@ public class GlusterGeoRepSyncJob extends GlusterJob {
     private boolean supportsGlusterGeoRepFeature(Cluster cluster) {
         return cluster.supportsGlusterService();
     }
-
 }
