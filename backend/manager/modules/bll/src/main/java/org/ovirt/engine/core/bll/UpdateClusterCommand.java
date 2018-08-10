59e647aae31a2120da301a1fe5e2f5b3487a44a6
Andrej Krejcir <akrejcir@redhat.com>
2018-08-10 14:32:45 +0200

    core: Change order of code blocks in UpdateClusterCommand
    Change the order of lines in executeCommand(),
    so that statements that cannot fail are executed
    last. This will make compensation added in future patches
    simpler.
    
    Change-Id: I931cb269d20cd4e59c96f8d1ba91cfb797613258
    Bug-Url: https://bugzilla.redhat.com/1609319
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateClusterCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UpdateClusterCommand.java
@@ -224,10 +224,6 @@ public class UpdateClusterCommand<T extends ManagementNetworkOnClusterOperationP
 
         clusterDao.update(getParameters().getCluster());
         addOrUpdateAddtionalClusterFeatures();
-        if (!oldCluster.supportsGlusterService() && getCluster().supportsGlusterService()) {
-            //update gluster parameters on all hosts
-           updateGlusterHosts();
-        }
 
         if (isAddedToStoragePool) {
             for (VDS vds : allForCluster) {
@@ -245,24 +241,9 @@ public class UpdateClusterCommand<T extends ManagementNetworkOnClusterOperationP
             networkClusterDao.save(managementNetworkCluster);
         }
 
-        alertIfFencingDisabled();
-
-        boolean isKsmPolicyChanged = (getCluster().isKsmMergeAcrossNumaNodes() != oldCluster.isKsmMergeAcrossNumaNodes()) ||
-                (getCluster().isEnableKsm() != oldCluster.isEnableKsm());
-
-        if (isKsmPolicyChanged) {
-            momPolicyUpdatedEvent.fire(getCluster());
-        }
-
-        updateDefaultNetworkProvider();
-
         // Call UpdateVmCommand on all VMs in the cluster to update defaults (i.e. DisplayType)
         updateVms();
         updateTemplates();
-
-        if (getCluster().getFirewallType() != oldCluster.getFirewallType()) {
-            markHostsForReinstall();
-        }
 
         if (!failedUpgradeEntities.isEmpty()) {
             logFailedUpgrades();
@@ -271,6 +252,26 @@ public class UpdateClusterCommand<T extends ManagementNetworkOnClusterOperationP
             getReturnValue().setValid(false);
             setSucceeded(false);
             return;
+        }
+
+        updateDefaultNetworkProvider();
+
+        if (getCluster().getFirewallType() != oldCluster.getFirewallType()) {
+            markHostsForReinstall();
+        }
+
+        if (!oldCluster.supportsGlusterService() && getCluster().supportsGlusterService()) {
+            //update gluster parameters on all hosts
+            updateGlusterHosts();
+        }
+
+        alertIfFencingDisabled();
+
+        boolean isKsmPolicyChanged = (getCluster().isKsmMergeAcrossNumaNodes() != oldCluster.isKsmMergeAcrossNumaNodes()) ||
+                (getCluster().isEnableKsm() != oldCluster.isEnableKsm());
+
+        if (isKsmPolicyChanged) {
+            momPolicyUpdatedEvent.fire(getCluster());
         }
 
         if (!Objects.equals(oldCluster.getCompatibilityVersion(), getCluster().getCompatibilityVersion())) {
