6ff66c38715a923767fe91c83c6c5fd93160f654
Dominik Holler <dholler@redhat.com>
2018-09-25 17:40:13 +0200

    webadmin: Allow empty string in NoSpacesValidation
    Allowing the empty string enables the NoSpacesValidation to be
    used to check if a string contains spaces. To forbid the empty
    string, the NoSpacesValidation can be combined with the
    NotEmptyValidation.
    
    Change-Id: Ide57d3e9f11392d00993420ab709902c5abeb561
    Bug-Url: https://bugzilla.redhat.com/1478854
    Signed-off-by: Dominik Holler <dholler@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/validation/NoSpacesValidation.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/validation/NoSpacesValidation.java
@@ -5,7 +5,7 @@ import org.ovirt.engine.ui.uicompat.ConstantsManager;
 @SuppressWarnings("unused")
 public class NoSpacesValidation extends RegexValidation {
     public NoSpacesValidation() {
-        setExpression("^[^\\s]+$"); //$NON-NLS-1$
+        setExpression("^[^\\s]*$"); //$NON-NLS-1$
         setMessage(ConstantsManager.getInstance().getConstants().thisFieldCantConatainSpacesMsg());
     }
 }
