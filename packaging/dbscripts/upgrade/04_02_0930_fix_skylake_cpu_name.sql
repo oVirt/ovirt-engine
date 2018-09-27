5a627ff3e52dc677597c5e063df96bbf88028f11
Miguel Martin <mmartinv@redhat.com>
2018-09-27 12:52:06 +0200

    engine: rename 'Intel Haswell Family-IBRS' CPU type
    Rename 'Intel Haswell Family-IBRS' CPU type to the new name
    'Intel Haswell IBRS Family'
    
    Change-Id: I45901943af85b732fb7b62b34b9929817b7f0f69
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1633645
    Signed-off-by: Miguel Martin <mmartinv@redhat.com>

--- packaging/dbscripts/upgrade/04_02_0930_fix_skylake_cpu_name.sql
+++ packaging/dbscripts/upgrade/04_02_0930_fix_skylake_cpu_name.sql
@@ -1,9 +1,10 @@
 
 UPDATE cluster SET cpu_name = CASE
-    WHEN cpu_name = 'Intel Nehalem Family-IBRS' THEN 'Intel Nehalem IBRS Family' -- fix wrong 3.6 name
+    WHEN cpu_name = 'Intel Nehalem Family-IBRS' THEN 'Intel Nehalem IBRS Family' -- fix wrong 4.1.9 name
     WHEN cpu_name = 'Intel Nehalem-IBRS Family' THEN 'Intel Nehalem IBRS Family' -- align -IBRS
     WHEN cpu_name = 'Intel Westmere-IBRS Family' THEN 'Intel Westmere IBRS Family'
     WHEN cpu_name = 'Intel SandyBridge-IBRS Family' THEN 'Intel SandyBridge IBRS Family'
+    WHEN cpu_name = 'Intel Haswell Family-IBRS' THEN 'Intel Haswell IBRS Family' -- fix wrong 3.6 name
     WHEN cpu_name = 'Intel Haswell-noTSX-IBRS Family' THEN 'Intel Haswell-noTSX IBRS Family'
     WHEN cpu_name = 'Intel Haswell-IBRS Family' THEN 'Intel Haswell IBRS Family'
     WHEN cpu_name = 'Intel Broadwell-noTSX-IBRS Family' THEN 'Intel Broadwell-noTSX IBRS Family'
