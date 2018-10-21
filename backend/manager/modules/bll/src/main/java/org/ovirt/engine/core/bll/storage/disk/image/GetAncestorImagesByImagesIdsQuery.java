be7f10fc8ff7cb34546782d1bc39f231762670ae
Shani Leviim <sleviim@redhat.com>
2018-10-21 12:04:54 +0300

    core: Fix UI for moving a disk with a damaged ancestor
    Screenshot: https://imgur.com/a/LtKyEEv
    
    In case of trying to move a disk with a damaged ancestor (for example,
    the ancestor doesn't have an entry in the 'images' table),
    the query 'GetAncestorImagesByImagesIdsQuery' returns a null DiskImage
    as the image's ancestor, and causes to a UI error.
    
    Using Collectors.toMap() and receiving that null object as the Map's
    ValueMapper function result, causes to a NullPionterException.
    
    Change-Id: I299df85211e51afba3ffeb9ac8cc22de81f6a3ba
    Bug-Url: https://bugzilla.redhat.com/1635405
    Bug-Url: https://bugzilla.redhat.com/1641326
    Signed-off-by: Shani Leviim <sleviim@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/GetAncestorImagesByImagesIdsQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/GetAncestorImagesByImagesIdsQuery.java
@@ -1,13 +1,20 @@
 package org.ovirt.engine.core.bll.storage.disk.image;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.bll.context.EngineContext;
+import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.common.queries.IdsQueryParameters;
+import org.ovirt.engine.core.common.utils.Pair;
+import org.ovirt.engine.core.compat.Guid;
 import org.ovirt.engine.core.dao.DiskImageDao;
 
 public class GetAncestorImagesByImagesIdsQuery<P extends IdsQueryParameters>
@@ -22,8 +29,23 @@ public class GetAncestorImagesByImagesIdsQuery<P extends IdsQueryParameters>
 
     @Override
     protected void executeQueryCommand() {
-        getQueryReturnValue().setReturnValue(getParameters().getIds().stream()
-                .collect(Collectors.toMap(Function.identity(),
-                        id -> diskImageDao.getAncestor(id, getUserID(), getParameters().isFiltered()))));
+        Set<Guid> haveInvalidAncestor = new HashSet<>();
+        Function<Guid, DiskImage> getAncestor = (Guid imageId) -> {
+            DiskImage ancestor = diskImageDao.getAncestor(imageId, getUserID(), getParameters().isFiltered());
+            if (ancestor == null) {
+                haveInvalidAncestor.add(imageId);
+            }
+            return ancestor;
+        };
+
+        Map<Guid, DiskImage> haveValidAncestor = getParameters().getIds().stream()
+                .map(guid -> new Pair<>(guid, getAncestor.apply(guid)))
+                .filter(pair -> pair.getSecond() != null)
+                .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond));
+
+        if (!haveInvalidAncestor.isEmpty()) {
+            log.warn("Ancestor is missing for the following image id(s): {}", StringUtils.join(haveInvalidAncestor, ','));
+        }
+        getQueryReturnValue().setReturnValue(haveValidAncestor);
     }
 }
