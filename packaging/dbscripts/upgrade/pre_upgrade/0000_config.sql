e63b872537be1dad8ce16a5a2285977fa797ef6e
Steven Rosenberg <srosenbe@redhat.com>
2018-10-31 18:12:37 +0200

    engine: Remove Cluster Levels 3.6 and 4.0
    Dropping Support for Cluster Level support for versions 3.6 and 4.0.
    
    Removal of these cluster level versions only affect the configuration
    and setup checking. All of the code to support these versions still
    reside in the code.
    
    Changes to the VDSM shall be added via a different patch.
    
    Change-Id: Iee4f7c46138f79b224401cc8f0cba593c3d10bd1
    Bug-Url: https://bugzilla.redhat.com/1550634
    Signed-off-by: Steven Rosenberg <srosenbe@redhat.com>

--- packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
+++ packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -927,7 +927,7 @@ select fn_db_update_config_value('ClusterEmulatedMachines','pc-i440fx-rhel7.3.0,
 select fn_db_update_config_value('ClusterEmulatedMachines','pc-i440fx-rhel7.3.0,pc-i440fx-2.6,pseries-rhel7.5.0,s390-ccw-virtio-2.8','4.2');
 select fn_db_update_config_value('ClusterEmulatedMachines','pc-i440fx-rhel7.5.0,pc-i440fx-2.9,pseries-rhel7.5.0,s390-ccw-virtio-2.8','4.3');
 select fn_db_update_config_value('SpiceDriverNameInGuest','{"windows": "RHEV-Spice", "linux" : "xorg-x11-drv-qxl" }','general');
-select fn_db_update_config_value('SupportedClusterLevels','3.6,4.0,4.1,4.2,4.3','general');
+select fn_db_update_config_value('SupportedClusterLevels','4.1,4.2,4.3','general');
 select fn_db_update_config_value('SupportedVDSMVersions','4.17,4.18','general');
 select fn_db_update_config_value('VdcVersion','4.1.0.0','general');
 select fn_db_update_config_value('ProductRPMVersion','4.1.0.0','general');
