9f286ac100f07d735ad46ccbdf62248e769133db
Andrej Krejcir <akrejcir@redhat.com>
2018-10-31 14:49:39 +0100

    scheduler: HE policy fiters hosts with worse score than curent one
    During balancing, the HE VM should not be migrated to
    a host with worse score, because the ha-agent could
    migrate it back. Creating a migration cycle.
    
    If the hosted engine is in global maintenance,
    this check is skipped.
    
    Change-Id: I7c00cab775ba5180ecc860e9e7c8077fd8ef6591
    Bug-Url: https://bugzilla.redhat.com/1639650
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/policyunits/HostedEngineHAClusterFilterPolicyUnit.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/scheduling/policyunits/HostedEngineHAClusterFilterPolicyUnit.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import org.ovirt.engine.core.bll.scheduling.PolicyUnitImpl;
 import org.ovirt.engine.core.bll.scheduling.SchedulingUnit;
 import org.ovirt.engine.core.bll.scheduling.pending.PendingResourceManager;
@@ -14,6 +16,7 @@ import org.ovirt.engine.core.common.errors.EngineMessage;
 import org.ovirt.engine.core.common.scheduling.PerHostMessages;
 import org.ovirt.engine.core.common.scheduling.PolicyUnit;
 import org.ovirt.engine.core.common.scheduling.PolicyUnitType;
+import org.ovirt.engine.core.dao.VdsDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,6 +29,11 @@ import org.slf4j.LoggerFactory;
 public class HostedEngineHAClusterFilterPolicyUnit extends PolicyUnitImpl {
     private static final Logger log = LoggerFactory.getLogger(HostedEngineHAClusterFilterPolicyUnit.class);
 
+    private static final int MIGRATION_THRESHOLD_SCORE = 800;
+
+    @Inject
+    private VdsDao vdsDao;
+
     public HostedEngineHAClusterFilterPolicyUnit(PolicyUnit policyUnit,
             PendingResourceManager pendingResourceManager) {
         super(policyUnit, pendingResourceManager);
@@ -36,7 +44,7 @@ public class HostedEngineHAClusterFilterPolicyUnit extends PolicyUnitImpl {
 
         // The filter is relevant only for Hosted Engine VM
         if (vm.isHostedEngine()) {
-
+            int haScoreMin = getMinScore(vm);
             List<VDS> hostsToRunOn = new ArrayList<>();
             for (VDS host : hosts) {
                 if (!host.isHostedEngineConfigured()) {
@@ -46,21 +54,42 @@ public class HostedEngineHAClusterFilterPolicyUnit extends PolicyUnitImpl {
                 }
 
                 int haScore = host.getHighlyAvailableScore();
-                if (haScore > 0) {
-                    hostsToRunOn.add(host);
-                    log.debug("Host '{}' wasn't filtered out as it has a score of {}",
-                            host.getName(),
-                            haScore);
-                } else {
+                if (haScore <= 0) {
                     log.debug("Host '{}' was filtered out as it doesn't have a positive score (the score is {})",
                             host.getName(), haScore);
                     messages.addMessage(host.getId(), EngineMessage.VAR__DETAIL__HE_HOST_NOT_POSITIVE_SCORE.name());
+                    continue;
                 }
+
+                if (haScore <= haScoreMin) {
+                    log.debug("Host '{}' was filtered out as it has much lower score than the current host (the score is {}, must be more than {})",
+                            host.getName(), haScore, haScoreMin);
+                    messages.addMessage(host.getId(), EngineMessage.VAR__DETAIL__HE_HOST_LOW_SCORE.name());
+                    continue;
+                }
+
+                hostsToRunOn.add(host);
+                log.debug("Host '{}' wasn't filtered out as it has a score of {}",
+                        host.getName(),
+                        haScore);
             }
 
             return hostsToRunOn;
         } else {
             return hosts;
         }
+    }
+
+    private int getMinScore(VM vm) {
+        if (vm.getRunOnVds() == null) {
+            return 0;
+        }
+
+        VDS currentHost = vdsDao.get(vm.getRunOnVds());
+        if (currentHost.getHighlyAvailableGlobalMaintenance()) {
+            return 0;
+        }
+
+        return Math.max(currentHost.getHighlyAvailableScore() - MIGRATION_THRESHOLD_SCORE, 0);
     }
 }
--- frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
+++ frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
@@ -2639,6 +2639,8 @@ public interface AppErrors extends ConstantsWithLookup {
 
     String VAR__DETAIL__HE_HOST_NOT_POSITIVE_SCORE();
 
+    String VAR__DETAIL__HE_HOST_LOW_SCORE();
+
     String VAR__DETAIL__NOT_ENOUGH_HE_SPARES();
 
     String VAR__DETAIL__SAME_HOST();
