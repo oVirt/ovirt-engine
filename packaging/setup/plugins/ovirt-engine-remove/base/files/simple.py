7c5e277a810f6acf00fd6a237c16c69ff6cb4864
Yedidyah Bar David <didi@redhat.com>
2018-11-18 12:27:08 +0200

    packaging: setup: uninstall: Use common in remove
    Use the information exposed by the common uninstall code in
    engine-cleanup (aka ovirt-engine-remove).
    
    Change-Id: I4f7be8e80d27b32662c01c596a1deb8c3f35ca4a
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-remove/base/files/simple.py
+++ packaging/setup/plugins/ovirt-engine-remove/base/files/simple.py
@@ -19,10 +19,7 @@
 """Simple plugin."""
 
 
-import configparser
 import gettext
-import glob
-import hashlib
 import os
 
 from otopi import constants as otopicons
@@ -42,17 +39,6 @@ def _(m):
 @util.export
 class Plugin(plugin.PluginBase):
     """Simple plugin."""
-
-    def _digestFile(self, filename):
-        md5 = hashlib.new('md5')
-        # Read file in chunks of 10KB
-        with open(filename, 'rb') as f:
-            while True:
-                data = f.read(10240)
-                if not data:
-                    break
-                md5.update(data)
-        return md5.hexdigest()
 
     def _safeDelete(self, filename):
         try:
@@ -155,7 +141,6 @@ class Plugin(plugin.PluginBase):
 
     def __init__(self, context):
         super(Plugin, self).__init__(context=context)
-        self._infos = None
         self._files = {}
         self._toremove = None
         self._lines = {}
@@ -188,14 +173,6 @@ class Plugin(plugin.PluginBase):
             osetupcons.RemoveEnv.REMOVE_CHANGED,
             None
         )
-        self._infos = sorted(
-            glob.glob(
-                os.path.join(
-                    osetupcons.FileLocations.OVIRT_ENGINE_UNINSTALL_DIR,
-                    '*.conf',
-                )
-            )
-        )
 
     @plugin.event(
         stage=plugin.Stages.STAGE_CUSTOMIZATION,
@@ -208,146 +185,61 @@ class Plugin(plugin.PluginBase):
         interactive = self.environment[
             osetupcons.RemoveEnv.ASK_GROUPS
         ]
-        unremovable = {}
-        already_asked = []
-        for info in self._infos:
-            config = configparser.ConfigParser()
-            config.optionxform = str
-            config.read([info])
-            for section in config.sections():
-                if section.startswith(
-                    osetupcons.Const.FILE_GROUP_SECTION_PREFIX
-                ):
-                    group = section[
-                        len(osetupcons.Const.FILE_GROUP_SECTION_PREFIX):
-                    ]
-                    description = config.get(section, 'description')
-                    template = "%s.description" % group
-                    msg = gettext.dgettext(
-                        message=template,
-                        domain='ovirt-engine-setup'
+        for group, description in self.environment[
+            osetupcons.CoreEnv.UNINSTALL_GROUPS_DESCRIPTIONS
+        ].items():
+            add_group = self.environment[
+                osetupcons.RemoveEnv.REMOVE_ALL
+            ]
+            if not add_group:
+                if group in self.environment[
+                    osetupcons.RemoveEnv.REMOVE_SPEC_OPTION_GROUP_LIST
+                ]:
+                    add_group = True
+
+            if (
+                not add_group and
+                interactive
+            ):
+                if group not in self.environment[
+                    osetupcons.RemoveEnv.REMOVE_SPEC_OPTION_GROUP_LIST
+                ]:
+                    add_group = dialog.queryBoolean(
+                        dialog=self.dialog,
+                        name='OVESETUP_REMOVE_GROUP/' + group,
+                        note=_(
+                            'Do you want to remove {description}? '
+                            '(@VALUES@) [@DEFAULT@]: '
+                        ).format(
+                            description=description,
+                        ),
+                        prompt=True,
+                        true=_('Yes'),
+                        false=_('No'),
+                        default=False,
                     )
-                    if msg == template:
-                        msg = description
-                    self._descriptions[group] = msg
+            if add_group:
+                self.environment[
+                    osetupcons.RemoveEnv.
+                    REMOVE_GROUPS
+                ] += ',' + group
 
-                    add_group = self.environment[
-                        osetupcons.RemoveEnv.REMOVE_ALL
-                    ]
-                    if not add_group:
-                        if group in self.environment[
-                            osetupcons.RemoveEnv.REMOVE_SPEC_OPTION_GROUP_LIST
-                        ]:
-                            add_group = True
+        self._toremove = set([
+            name
+            for name, info in self.environment[
+                osetupcons.CoreEnv.UNINSTALL_FILES_INFO
+            ].items()
+            if 'unremovable' not in info['groups']
+        ])
 
-                    if (
-                        not add_group and
-                        interactive and
-                        group not in already_asked
-                    ):
-                        if group not in self.environment[
-                            osetupcons.RemoveEnv.REMOVE_SPEC_OPTION_GROUP_LIST
-                        ]:
-                            already_asked.append(group)
-                            add_group = dialog.queryBoolean(
-                                dialog=self.dialog,
-                                name='OVESETUP_REMOVE_GROUP/' + group,
-                                note=_(
-                                    'Do you want to remove {description}? '
-                                    '(@VALUES@) [@DEFAULT@]: '
-                                ).format(
-                                    description=msg,
-                                ),
-                                prompt=True,
-                                true=_('Yes'),
-                                false=_('No'),
-                                default=False,
-                            )
-                    if add_group:
-                        self.environment[
-                            osetupcons.RemoveEnv.
-                            REMOVE_GROUPS
-                        ] += ',' + group
+        changed = [
+            name
+            for name, info in self.environment[
+                osetupcons.CoreEnv.UNINSTALL_FILES_INFO
+            ].items()
+            if info['changed']
+        ]
 
-            def getFiles(section):
-                files = {}
-                for name, value in config.items(section):
-                    comps = name.split('.')
-                    if comps[0] == 'file':
-                        files.setdefault(comps[1], {})[comps[2]] = value
-
-                # python 2.6 doesn't support dict comprehensions.
-                # TODO: we may move to it when minimal python version
-                # available is 2.7+
-                return dict((f['name'], f['md5']) for f in files.values())
-
-            def getLines(section):
-                associated_lines = {}
-                aggregated_lines = {}
-                # line.{file_index:03}{line_index:03}.name
-                # line.{file_index:03}{line_index:03}.content.added
-                # line.{file_index:03}{line_index:03}.content.removed
-                for name, value in config.items(section):
-                    comps = name.split('.')
-                    if comps[0] == 'line':
-
-                        index = comps[1]        # '00001', '00002', etc
-                        line_type = comps[2]    # 'name' or 'content'
-
-                        if len(comps) == 3 and line_type == 'content':
-                            comps.append('added')
-
-                        if line_type == 'content':
-                            action = comps[3]   # 'added' or 'removed'
-
-                        associated_lines.setdefault(index, {})
-                        if line_type == 'name':
-                            associated_lines[index][line_type] = value
-                        elif line_type == 'content':
-                            associated_lines[index].setdefault(line_type, {})[
-                                action
-                            ] = value
-
-                for f in associated_lines.values():
-                    aggregated_lines.setdefault(
-                        f['name'], []
-                    ).append(f['content'])
-                self.logger.debug(
-                    'getLines: aggregated_lines = %s',
-                    aggregated_lines,
-                )
-                return aggregated_lines
-
-            for uninstall_group in [
-                x.strip()
-                for x in self.environment[
-                    osetupcons.RemoveEnv.REMOVE_GROUPS
-                ].split(',')
-                if x.strip()
-            ]:
-                uninstall_section = (
-                    osetupcons.Const.FILE_GROUP_SECTION_PREFIX +
-                    uninstall_group
-                )
-                if config.has_section(uninstall_section):
-                    # section could be missing in a conf file, for example if
-                    # PKI config was not done because already existing
-                    self._files.update(
-                        getFiles(uninstall_section)
-                    )
-                    self._lines.update(
-                        getLines(uninstall_section)
-                    )
-            if config.has_section('unremovable'):
-                unremovable.update(getFiles('unremovable'))
-
-        self._toremove = set(self._files.keys()) - set(unremovable.keys())
-        changed = []
-        for f in self._toremove:
-            if os.path.exists(f):
-                if self._digestFile(f) != self._files[f]:
-                    changed.append(f)
-        self.logger.debug('changed=%s', changed)
         if changed:
             if self.environment[osetupcons.RemoveEnv.REMOVE_CHANGED] is None:
                 self.environment[
@@ -372,11 +264,6 @@ class Plugin(plugin.PluginBase):
         if not self.environment[osetupcons.RemoveEnv.REMOVE_CHANGED]:
             self._toremove -= set(changed)
 
-        self._tomodifylines = self._lines.keys()
-        self.logger.debug('tomodifylines=%s', self._tomodifylines)
-        self.logger.debug('files=%s', self._files)
-        self.logger.debug('unremovable=%s', unremovable)
-        self.logger.debug('toremove=%s', self._toremove)
         self.environment[
             osetupcons.RemoveEnv.FILES_TO_REMOVE
         ] = self._toremove
@@ -396,11 +283,15 @@ class Plugin(plugin.PluginBase):
                 self._safeDelete(f)
 
         self.logger.info(_('Reverting changes to files'))
-        for f in self._tomodifylines:
+        for f, changes in self.environment[
+            osetupcons.CoreEnv.UNINSTALL_LINES_INFO
+        ].items():
             if os.path.exists(f):
-                self._revertChanges(f, self._lines[f])
+                self._revertChanges(f, changes)
 
-        for info in self._infos:
+        for info in self.environment[
+            osetupcons.CoreEnv.UNINSTALL_UNINSTALL_FILES
+        ]:
             self._safeDelete(info)
 
     @plugin.event(
@@ -413,7 +304,9 @@ class Plugin(plugin.PluginBase):
         ),
     )
     def _closeup(self):
-        all_groups = set(self._descriptions.keys())
+        all_groups = set(self.environment[
+            osetupcons.CoreEnv.UNINSTALL_GROUPS_DESCRIPTIONS
+        ].keys())
         uninstalled_groups = set([
             x.strip()
             for x in self.environment[
@@ -427,7 +320,9 @@ class Plugin(plugin.PluginBase):
                 text=_(
                     '{description} files not removed'
                 ).format(
-                    description=self._descriptions[group],
+                    description=self.environment[
+                        osetupcons.CoreEnv.UNINSTALL_GROUPS_DESCRIPTIONS
+                    ][group],
                 ),
             )
 
