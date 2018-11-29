ba31f2c86ba7f979269e596f663ffe6245748b7d
Benny Zlotnik <bzlotnik@redhat.com>
2018-11-29 13:44:07 +0200

    core: create a managed block storage disk using CinderlibExecutor
    Change-Id: I34647f090534990c8ebfed31ed3e764f70597840
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/managedblock/AddManagedBlockStorageDiskCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/managedblock/AddManagedBlockStorageDiskCommand.java
@@ -1,7 +1,10 @@
 package org.ovirt.engine.core.bll.storage.disk.managedblock;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -14,8 +17,26 @@ import org.ovirt.engine.core.bll.utils.PermissionSubject;
 import org.ovirt.engine.core.common.VdcObjectType;
 import org.ovirt.engine.core.common.action.AddDiskParameters;
 import org.ovirt.engine.core.common.businessentities.SubjectEntity;
+import org.ovirt.engine.core.common.businessentities.storage.DiskImageDynamic;
+import org.ovirt.engine.core.common.businessentities.storage.ImageStorageDomainMap;
+import org.ovirt.engine.core.common.businessentities.storage.ManagedBlockStorage;
+import org.ovirt.engine.core.common.businessentities.storage.ManagedBlockStorageDisk;
+import org.ovirt.engine.core.common.businessentities.storage.VolumeFormat;
+import org.ovirt.engine.core.common.businessentities.storage.VolumeType;
 import org.ovirt.engine.core.common.utils.Pair;
+import org.ovirt.engine.core.common.utils.SizeConverter;
+import org.ovirt.engine.core.common.utils.cinderlib.CinderlibCommandParameters;
+import org.ovirt.engine.core.common.utils.cinderlib.CinderlibExecutor;
+import org.ovirt.engine.core.common.utils.cinderlib.CinderlibExecutor.CinderlibCommand;
+import org.ovirt.engine.core.common.utils.cinderlib.CinderlibReturnValue;
 import org.ovirt.engine.core.compat.Guid;
+import org.ovirt.engine.core.dao.BaseDiskDao;
+import org.ovirt.engine.core.dao.CinderStorageDao;
+import org.ovirt.engine.core.dao.DiskImageDynamicDao;
+import org.ovirt.engine.core.dao.ImageDao;
+import org.ovirt.engine.core.dao.ImageStorageDomainMapDao;
+import org.ovirt.engine.core.utils.JsonHelper;
+import org.ovirt.engine.core.utils.transaction.TransactionSupport;
 import org.ovirt.engine.core.vdsbroker.ResourceManager;
 
 @InternalCommandAttribute
@@ -23,6 +44,24 @@ public class AddManagedBlockStorageDiskCommand<T extends AddDiskParameters> exte
 
     @Inject
     private ResourceManager resourceManager;
+
+    @Inject
+    private CinderStorageDao cinderStorageDao;
+
+    @Inject
+    private CinderlibExecutor cinderlibExecutor;
+
+    @Inject
+    private BaseDiskDao baseDiskDao;
+
+    @Inject
+    private ImageDao imageDao;
+
+    @Inject
+    private ImageStorageDomainMapDao imageStorageDomainMapDao;
+
+    @Inject
+    private DiskImageDynamicDao diskImageDynamicDao;
 
     public AddManagedBlockStorageDiskCommand(Guid commandId) {
         super(commandId);
@@ -34,7 +73,70 @@ public class AddManagedBlockStorageDiskCommand<T extends AddDiskParameters> exte
 
     @Override
     protected void executeCommand() {
+        ManagedBlockStorage managedBlockStorage = cinderStorageDao.get(getParameters().getStorageDomainId());
+        Guid volumeId = Guid.newGuid();
+        List<String> extraParams = new ArrayList<>();
+        extraParams.add(volumeId.toString());
+        Number sizeInGiB = SizeConverter.convert(getParameters().getDiskInfo().getSize(),
+                SizeConverter.SizeUnit.BYTES,
+                SizeConverter.SizeUnit.GiB);
+        extraParams.add(Long.toString(sizeInGiB.longValue()));
+        CinderlibReturnValue returnValue;
+
+        try {
+            CinderlibCommandParameters params =
+                    new CinderlibCommandParameters(JsonHelper.mapToJson(managedBlockStorage.getDriverOptions(),
+                            false),
+                    extraParams);
+            returnValue = cinderlibExecutor.runCommand(CinderlibCommand.CREATE_VOLUME, params);
+        } catch (Exception e) {
+            log.error("Failed executing volume creation", e);
+            return;
+        }
+
+        if (!returnValue.getSucceed()) {
+            return;
+        }
+
+        saveDisk(volumeId);
+        getReturnValue().setActionReturnValue(volumeId);
         setSucceeded(true);
+    }
+
+    private void saveDisk(Guid volumeId) {
+        ManagedBlockStorageDisk disk = createDisk();
+        disk.setImageId(volumeId);
+        disk.setId(volumeId);
+
+        TransactionSupport.executeInNewTransaction(() -> {
+            baseDiskDao.save(disk);
+            imageDao.save(disk.getImage());
+            imageStorageDomainMapDao.save(new ImageStorageDomainMap(disk.getImageId(),
+                    disk.getStorageIds().get(0), disk.getQuotaId(), disk.getDiskProfileId()));
+
+            DiskImageDynamic diskDynamic = new DiskImageDynamic();
+            diskDynamic.setId(disk.getImageId());
+            diskImageDynamicDao.save(diskDynamic);
+
+            return null;
+        });
+    }
+
+    private ManagedBlockStorageDisk createDisk() {
+        ManagedBlockStorageDisk disk = new ManagedBlockStorageDisk();
+        disk.setDiskAlias(getParameters().getDiskInfo().getDiskAlias());
+        disk.setSize(getParameters().getDiskInfo().getSize());
+        disk.setDiskDescription(getParameters().getDiskInfo().getDiskDescription());
+        disk.setShareable(getParameters().getDiskInfo().isShareable());
+        disk.setStorageIds(new ArrayList<>(Arrays.asList(getParameters().getStorageDomainId())));
+        disk.setVolumeType(VolumeType.Unassigned);
+        disk.setVolumeFormat(VolumeFormat.RAW);
+        disk.setCreationDate(new Date());
+        disk.setLastModified(new Date());
+        disk.setActive(true);
+        disk.setQuotaId(getParameters().getQuotaId());
+
+        return disk;
     }
 
     @Override
