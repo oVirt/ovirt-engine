357648e308fe7bdeab6ac6cdb8e9647a2e5c5644
Denis Chaplygin <dchaplyg@redhat.com>
2018-12-12 16:27:16 +0100

    storage: Added enum for a block size.
    Change-Id: I460acaa66ed96b39b059eb887976e57ed6e51e06
    Bug-Url: https://bugzilla.redhat.com/1592916
    Signed-off-by: Denis Chaplygin <dchaplyg@redhat.com>

---
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/StorageBlockSize.java
@@ -0,0 +1,29 @@
+package org.ovirt.engine.core.common.businessentities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum StorageBlockSize {
+    BLOCK_512(512),
+    BLOCK_4K(4096);
+
+    private final Integer value;
+    private static final Map<Integer, StorageBlockSize> mappings = new HashMap<>();
+    static {
+        for (StorageBlockSize storageBlockSize : values()) {
+            mappings.put(storageBlockSize.getValue(), storageBlockSize);
+        }
+    }
+
+    private StorageBlockSize(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public static StorageBlockSize forValue(Integer value) {
+        return mappings.get(value);
+    }
+}
