f78de682204e23ff08bc5fb241ef5e8d53946796
Yedidyah Bar David <didi@redhat.com>
2018-12-18 11:32:27 +0200

    packaging: setup: uninstall: Use common in rename
    Use the information exposed by the common uninstall code in
    ovirt-engine-rename.
    
    Change-Id: If427ad52ead8571c6acdc603eb4df61ba0093267
    Bug-Url: https://bugzilla.redhat.com/1660378
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- packaging/setup/plugins/ovirt-engine-rename/ovirt-engine/uninstall.py
+++ packaging/setup/plugins/ovirt-engine-rename/ovirt-engine/uninstall.py
@@ -19,10 +19,8 @@
 """Simple plugin."""
 
 
-import configparser
 import gettext
 import glob
-import hashlib
 import os
 
 from otopi import plugin
@@ -41,22 +39,8 @@ def _(m):
 class Plugin(plugin.PluginBase):
     """Simple plugin."""
 
-    def _digestFile(self, filename):
-        md5 = hashlib.new('md5')
-        # Read file in chunks of 10KB
-        with open(filename, 'rb') as f:
-            while True:
-                data = f.read(10240)
-                if not data:
-                    break
-                md5.update(data)
-        return md5.hexdigest()
-
     def __init__(self, context):
         super(Plugin, self).__init__(context=context)
-        self._infos = None
-        self._existing_conf_files = {}
-        self._descriptions = {}
 
     @plugin.event(
         stage=plugin.Stages.STAGE_INIT,
@@ -83,40 +67,17 @@ class Plugin(plugin.PluginBase):
         stage=plugin.Stages.STAGE_VALIDATION,
     )
     def _validation(self):
-        for info in self._infos:
-            config = configparser.ConfigParser()
-            config.optionxform = str
-            config.read([info])
-
-            def getFiles(section):
-                files = {}
-                for name, value in config.items(section):
-                    comps = name.split('.')
-                    if comps[0] == 'file':
-                        files.setdefault(comps[1], {})[comps[2]] = value
-
-                # python 2.6 doesn't support dict comprehensions.
-                # TODO: we may move to it when minimal python version
-                # available is 2.7+
-                return dict((f['name'], f['md5']) for f in files.values())
-
-            for section in config.sections():
-                self._existing_conf_files.update(
-                    getFiles(section)
-                )
-
         all_modified_files = self.environment[
             osetupcons.RenameEnv.FILES_TO_BE_MODIFIED
         ]
 
-        externally_modified_files = []
-
-        for f in all_modified_files:
-            if (
-                f in self._existing_conf_files and
-                self._digestFile(f) != self._existing_conf_files[f]
-            ):
-                externally_modified_files.append(f)
+        externally_modified_files = [
+            f
+            for f, info in self.environment[
+                osetupcons.CoreEnv.UNINSTALL_FILES_INFO
+            ].items()
+            if info['changed']
+        ]
 
         self.logger.info(_('The following files will be updated:'))
         self.dialog.note(
