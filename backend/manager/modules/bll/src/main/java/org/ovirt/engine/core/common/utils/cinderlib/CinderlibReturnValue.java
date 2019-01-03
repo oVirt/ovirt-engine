9b00ed62b02256c179f44dc8c15f53a6cd07efd0
Benny Zlotnik <bzlotnik@redhat.com>
2019-01-03 18:20:25 +0200

    core: add equals, hashCode and toString to CinderlibReturnValue
    Change-Id: Iff1b741f910c71bba729cd8787a85b5d3f762806
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/common/utils/cinderlib/CinderlibReturnValue.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/common/utils/cinderlib/CinderlibReturnValue.java
@@ -1,5 +1,9 @@
 package org.ovirt.engine.core.common.utils.cinderlib;
 
+import java.util.Objects;
+
+import org.ovirt.engine.core.common.utils.ToStringBuilder;
+
 public class CinderlibReturnValue {
     private int returnCode;
     private String output;
@@ -27,5 +31,31 @@ public class CinderlibReturnValue {
 
     public boolean getSucceed() {
         return returnCode == 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof CinderlibReturnValue)) {
+            return false;
+        }
+        CinderlibReturnValue that = (CinderlibReturnValue) o;
+        return returnCode == that.returnCode &&
+                Objects.equals(output, that.output);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(returnCode, output);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.forInstance(this)
+                .append("returnCode", returnCode)
+                .append("output", output)
+                .toString();
     }
 }
