715c820a14c3796e948fc56427f4418f06d49126
Ondra Machacek <omachace@redhat.com>
2019-01-18 13:12:01 +0100

    restapi: Fix NPE in FencingPolicyMapper
    When updating the fencing policy without setting the fencing
    enabled/disabled we failed with NPE. This patch fixes it.
    
    Change-Id: Ia114b7c2c3756c005f0b2e14056daea601a2cd7d
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- backend/manager/modules/restapi/types/src/main/java/org/ovirt/engine/api/restapi/types/FencingPolicyMapper.java
+++ backend/manager/modules/restapi/types/src/main/java/org/ovirt/engine/api/restapi/types/FencingPolicyMapper.java
@@ -8,7 +8,9 @@ public class FencingPolicyMapper {
     @Mapping(from = FencingPolicy.class, to = org.ovirt.engine.core.common.businessentities.FencingPolicy.class)
     public static org.ovirt.engine.core.common.businessentities.FencingPolicy map(FencingPolicy model, org.ovirt.engine.core.common.businessentities.FencingPolicy template) {
         org.ovirt.engine.core.common.businessentities.FencingPolicy entity = template != null ? template : new org.ovirt.engine.core.common.businessentities.FencingPolicy();
-        entity.setFencingEnabled(model.isEnabled());
+        if(model.isSetEnabled()) {
+            entity.setFencingEnabled(model.isEnabled());
+        }
         if (model.isSetSkipIfSdActive()) {
             entity.setSkipFencingIfSDActive(model.getSkipIfSdActive().isEnabled());
         }
