oVirt/ovirt-engine

commit b5e20deb1bb252c5a32f9ba47aacac32733c0129

webadmin: add port security enabled attribute
When a new network is created throught the UI, we need to
allow the user a way to indicate the default port security
value for all ports created in that network.

To do so, a new listbox as been added to the NetworkPopupView,
only editable in the 'add' view.

Bug-Url: https://bugzilla.redhat.com/1539829
Change-Id: I767d4171190ac0c090da1169fa0709a9368783b7
Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/datacenters/EditNetworkModel.java
@@ -15,6 +15,7 @@ import org.ovirt.engine.ui.uicommonweb.Linq;
 import org.ovirt.engine.ui.uicommonweb.dataprovider.AsyncDataProvider;
 import org.ovirt.engine.ui.uicommonweb.help.HelpTag;
 import org.ovirt.engine.ui.uicommonweb.models.SearchableListModel;
+import org.ovirt.engine.ui.uicommonweb.models.networks.PortSecuritySelectorValue;
 import org.ovirt.engine.ui.uicompat.ConstantsManager;
 
 public class EditNetworkModel extends NetworkModel {
@@ -60,11 +61,11 @@ public class EditNetworkModel extends NetworkModel {
     @Override
     public void syncWithBackend() {
         super.syncWithBackend();
-        checkAndMapCustomExternalNetwork();
+        checkAndMapNetwork();
     }
 
-    private void checkAndMapCustomExternalNetwork() {
-        if (getNetwork().isExternal() && !getNetwork().getProvidedBy().isSetPhysicalNetworkId()) {
+    private void checkAndMapNetwork() {
+        if (getNetwork().isExternal()) {
             startProgress();
             Frontend.getInstance()
                     .runQuery(QueryType.GetExternalNetworkById,
@@ -73,14 +74,19 @@ public class EditNetworkModel extends NetworkModel {
                                 Network network = result.getReturnValue();
                                 if (network != null) {
                                     getNetwork().setProvidedBy(network.getProvidedBy());
-                                    initExternalNetworkParameters();
+                                    initEnablePortSecurity();
+                                    if (!getNetwork().getProvidedBy().isSetPhysicalNetworkId()) {
+                                        initPhysnetAttachmentParameters();
+                                    }
                                 }
                                 stopProgress();
                             }));
+        } else {
+            getPortSecuritySelector().setSelectedItem(PortSecuritySelectorValue.UNDEFINED);
         }
     }
 
-    private void initExternalNetworkParameters() {
+    private void initPhysnetAttachmentParameters() {
         if (isConnectedToPhysicalNetwork()) {
             getConnectedToPhysicalNetwork().setEntity(true);
             getUsePhysicalNetworkFromCustom().setEntity(true);
@@ -104,6 +110,18 @@ public class EditNetworkModel extends NetworkModel {
     protected void initMtu() {
         getMtuSelector().setSelectedItem(getNetwork().isDefaultMtu() ? MtuSelector.defaultMtu : MtuSelector.customMtu);
         getMtu().setEntity(isCustomMtu() ? getNetwork().getMtu() : null);
+    }
+
+    @Override
+    protected void initEnablePortSecurity() {
+        if (getNetwork().getProvidedBy().isPortSecurityConfigured()) {
+            getPortSecuritySelector().setSelectedItem(
+                    getNetwork().getProvidedBy().getPortSecurityEnabled() ?
+                            PortSecuritySelectorValue.ENABLED : PortSecuritySelectorValue.DISABLED
+            );
+        } else {
+            getPortSecuritySelector().setSelectedItem(PortSecuritySelectorValue.UNDEFINED);
+        }
     }
 
     @Override
@@ -155,6 +173,7 @@ public class EditNetworkModel extends NetworkModel {
             getUsePhysicalNetworkFromDatacenter().setIsChangeable(false);
             getUsePhysicalNetworkFromCustom().setIsChangeable(false);
             getDatacenterPhysicalNetwork().setIsChangeable(false);
+            getPortSecuritySelector().setIsChangeable(false);
         }
     }
 

backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/network/ProviderNetwork.java
@@ -126,6 +126,10 @@ public class ProviderNetwork implements Serializable {
         this.portSecurityEnabled = portSecurityEnabled;
     }
 
+    public boolean isPortSecurityConfigured() {
+        return this.portSecurityEnabled != null;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/datacenters/NetworkModel.java
@@ -40,6 +40,7 @@ import org.ovirt.engine.ui.uicommonweb.models.TabName;
 import org.ovirt.engine.ui.uicommonweb.models.ValidationCompleteEvent;
 import org.ovirt.engine.ui.uicommonweb.models.datacenters.qos.NewHostNetworkQosModel;
 import org.ovirt.engine.ui.uicommonweb.models.dnsconfiguration.DnsConfigurationModel;
+import org.ovirt.engine.ui.uicommonweb.models.networks.PortSecuritySelectorValue;
 import org.ovirt.engine.ui.uicommonweb.models.profiles.NetworkProfilesModel;
 import org.ovirt.engine.ui.uicommonweb.models.profiles.NewVnicProfileModel;
 import org.ovirt.engine.ui.uicommonweb.models.profiles.VnicProfileModel;
@@ -86,6 +87,7 @@ public abstract class NetworkModel extends Model implements HasValidatedTabs {
     private EntityModel<Boolean> privateIsStpEnabled;
     private EntityModel<Boolean> privateHasVLanTag;
     private ListModel<MtuSelector> mtuSelector;
+    private ListModel<PortSecuritySelectorValue> portSecuritySelector;
     private EntityModel<Integer> mtu;
     private EntityModel<Boolean> privateIsVmNetwork;
     private ListModel<HostNetworkQos> qos;
@@ -153,6 +155,9 @@ public abstract class NetworkModel extends Model implements HasValidatedTabs {
         mtuSelector.getSelectedItemChangedEvent().addListener((ev, sender, args) -> updateMtuSelectorsChangeability());
 
         setMtu(new EntityModel<Integer>());
+
+        portSecuritySelector = new ListModel<>();
+        portSecuritySelector.setItems(Arrays.asList(PortSecuritySelectorValue.values()));
 
         EntityModel<Boolean> isVmNetwork = new EntityModel<>();
         isVmNetwork.setEntity(true);
@@ -357,6 +362,10 @@ public abstract class NetworkModel extends Model implements HasValidatedTabs {
 
     public ListModel<MtuSelector> getMtuSelector() {
         return mtuSelector;
+    }
+
+    public ListModel<PortSecuritySelectorValue> getPortSecuritySelector() {
+        return portSecuritySelector;
     }
 
     private void setMtuSelector(ListModel<MtuSelector> value) {
@@ -794,6 +803,8 @@ public abstract class NetworkModel extends Model implements HasValidatedTabs {
 
     protected abstract void initIsVm();
 
+    protected abstract void initEnablePortSecurity();
+
     protected abstract void selectExternalProvider();
 
     protected abstract void selectPhysicalDatacenterNetwork();
@@ -813,6 +824,7 @@ public abstract class NetworkModel extends Model implements HasValidatedTabs {
         getConnectedToPhysicalNetwork().setIsChangeable(externalNetwork);
         getUsePhysicalNetworkFromCustom().setIsChangeable(externalNetwork);
         getUsePhysicalNetworkFromDatacenter().setIsChangeable(externalNetwork);
+        getPortSecuritySelector().setIsChangeable(externalNetwork);
         updateDnsChangeabilityAndValue();
         updateMtuSelectorsChangeability();
         updateVlanChangeabilityAndValue();

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/datacenters/NewNetworkModel.java
@@ -20,6 +20,7 @@ import org.ovirt.engine.ui.uicommonweb.help.HelpTag;
 import org.ovirt.engine.ui.uicommonweb.models.ApplicationModeHelper;
 import org.ovirt.engine.ui.uicommonweb.models.ListModel;
 import org.ovirt.engine.ui.uicommonweb.models.SearchableListModel;
+import org.ovirt.engine.ui.uicommonweb.models.networks.PortSecuritySelectorValue;
 import org.ovirt.engine.ui.uicompat.ConstantsManager;
 import org.ovirt.engine.ui.uicompat.IFrontendActionAsyncCallback;
 
@@ -48,6 +49,7 @@ public class NewNetworkModel extends NetworkModel {
         getConnectedToPhysicalNetwork().setEntity(false);
 
         initMtu();
+        initEnablePortSecurity();
     }
 
     @Override
@@ -80,6 +82,11 @@ public class NewNetworkModel extends NetworkModel {
     protected void initMtu() {
         getMtuSelector().setSelectedItem(MtuSelector.defaultMtu);
         getMtu().setEntity(null);
+    }
+
+    @Override
+    protected void initEnablePortSecurity() {
+        getPortSecuritySelector().setSelectedItem(PortSecuritySelectorValue.ENABLED);
     }
 
     @Override
@@ -148,7 +155,7 @@ public class NewNetworkModel extends NetworkModel {
             ProviderNetwork providerNetwork = new ProviderNetwork();
             providerNetwork.setProviderId(externalProvider.getId());
             getNetwork().setProvidedBy(providerNetwork);
-
+            getNetwork().getProvidedBy().setPortSecurityEnabled(getPortSecuritySelector().getSelectedItem().getValue());
             if (hasDefinedSubnet()) {
                 getSubnetModel().flush();
                 parameters.setExternalSubnet(getSubnetModel().getSubnet());

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/networks/PortSecuritySelectorValue.java
@@ -1,0 +1,30 @@
+package org.ovirt.engine.ui.uicommonweb.models.networks;
+
+import org.ovirt.engine.ui.uicompat.ConstantsManager;
+
+public enum PortSecuritySelectorValue {
+    ENABLED(ConstantsManager.getInstance()
+            .getConstants().portSecurityEnabled(), true),
+    DISABLED(ConstantsManager.getInstance()
+            .getConstants().portSecurityDisabled(), false),
+    UNDEFINED(ConstantsManager.getInstance()
+            .getConstants().portSecurityUndefined(), null);
+
+    private String description;
+
+    private Boolean value;
+
+    private PortSecuritySelectorValue(String description, Boolean value) {
+        this.description = description;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+
+    public Boolean getValue() {
+        return value;
+    }
+}

frontend/webadmin/modules/uicompat/src/main/java/org/ovirt/engine/ui/uicompat/UIConstants.java
@@ -2152,4 +2152,10 @@ public interface UIConstants extends Constants {
     String separatedTitle();
 
     String vgpuPlacementCompatibilityInfo();
+
+    String portSecurityDisabled();
+
+    String portSecurityEnabled();
+
+    String portSecurityUndefined();
 }

frontend/webadmin/modules/uicompat/src/main/resources/org/ovirt/engine/ui/uicompat/UIConstants.properties
@@ -1090,3 +1090,6 @@ resetBrickMessage=Are you sure you want to reset the Brick?
 clusterSwitchChangeDisabled=Cluster switch type cannot be changed for an existing cluster.
 externalNetworkInfo=(External network)
 vgpuPlacementCompatibilityInfo=vGPU placement is available on cluster version 4.3 or newer
+portSecurityDisabled=Disabled
+portSecurityEnabled=Enabled
+portSecurityUndefined=Undefined

frontend/webadmin/modules/webadmin/src/main/java/org/ovirt/engine/ui/webadmin/ApplicationConstants.java
@@ -3490,4 +3490,6 @@ public interface ApplicationConstants extends CommonApplicationConstants {
     String vncEncryptionLabel();
 
     String vdsmName();
+
+    String portSecurityEnabledLabel();
 }

commit cd28f25cafba4136b01a9f1d8cd267a6fd6f2f04

webadmin: fix regression in physical networks
The physicalNetworkId is only populated on request. The
networks in the db have physicalNetworkId populated.
For this reason the physicalNetworkId has to be copied into
the new ProviderNetwork, which has no populated
physicalNetworkId.

The regression was introduced by
b5e20deb1bb252c5a32f9ba47aacac32733c0129

Change-Id: I3c9b3024eae35cbb868e1b79e2d654212553733f
Signed-off-by: Dominik Holler <dholler@redhat.com>

frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/datacenters/EditNetworkModel.java
@@ -73,6 +73,8 @@ public class EditNetworkModel extends NetworkModel {
                             new AsyncQuery<QueryReturnValue>(result -> {
                                 Network network = result.getReturnValue();
                                 if (network != null) {
+                                    network.getProvidedBy().setPhysicalNetworkId(
+                                            getNetwork().getProvidedBy().getPhysicalNetworkId());
                                     getNetwork().setProvidedBy(network.getProvidedBy());
                                     initEnablePortSecurity();
                                     if (!getNetwork().getProvidedBy().isSetPhysicalNetworkId()) {