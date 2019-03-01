e2e04e82351a6aea9c160b0da967de7ed39e5f24
Denis Chaplygin <dchaplyg@redhat.com>
2019-03-01 16:05:54 +0100

    storage: Made V5 latest and default storage domain
    Change-Id: I828bdc2399c3c807e41c6c24524253c0791ca1cf
    Bug-Url: https://bugzilla.redhat.com/1592916
    Signed-off-by: Denis Chaplygin <dchaplyg@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/VersionStorageFormatUtil.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/utils/VersionStorageFormatUtil.java
@@ -16,8 +16,8 @@ public class VersionStorageFormatUtil {
     static {
         versionToFormat.put(Version.v4_1, StorageFormatType.V4);
         versionToFormat.put(Version.v4_2, StorageFormatType.V4);
-        versionToFormat.put(Version.v4_3, StorageFormatType.V4);
-        versionToFormat.put(Version.v4_4, StorageFormatType.V4);
+        versionToFormat.put(Version.v4_3, StorageFormatType.V5);
+        versionToFormat.put(Version.v4_4, StorageFormatType.V5);
     };
 
     private static final Map<StorageFormatType, Version> earliestVersionSupported = new TreeMap<>();
