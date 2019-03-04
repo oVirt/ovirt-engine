472950ce242c0578c212967eabdb9ff817c69656
Yedidyah Bar David <didi@redhat.com>
2019-03-04 10:41:30 +0200

    automation: make generated-files before testing setup
    Change-Id: I3a62173a9d1c9a34080fd2fb468f709ac059d0ad
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/packaging-setup-tests.sh
+++ automation/packaging-setup-tests.sh
@@ -3,5 +3,6 @@
 trap popd 0
 pushd $(dirname "$(readlink -f "$0")")/..
 
+make generated-files
 export PYTHONPATH="packaging/pythonlib:packaging/setup:${PYTHONPATH}"
 python -m pytest packaging/setup
