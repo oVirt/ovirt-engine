098cde9798125b7b92290ae6e0386235bc3307b1
Tal Nisan <tnisan@redhat.com>
2019-04-02 18:13:19 +0300

    core: Allow attaching the Hosted Engine disk to VMs
    Change-Id: Ib898f6040d5b0501a7cef7d96f5e76d44bb3756a
    Signed-off-by: Tal Nisan <tnisan@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/validator/storage/DiskOperationsValidator.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/validator/storage/DiskOperationsValidator.java
@@ -18,7 +18,7 @@ public class DiskOperationsValidator {
         allowedCommandsOnTypes.put(ActionType.RemoveDisk, Arrays.asList(DiskContentType.DATA, DiskContentType.OVF_STORE,
                 DiskContentType.MEMORY_DUMP_VOLUME, DiskContentType.MEMORY_METADATA_VOLUME, DiskContentType.ISO));
         allowedCommandsOnTypes.put(ActionType.UpdateVmDisk, Arrays.asList(DiskContentType.DATA));
-        allowedCommandsOnTypes.put(ActionType.AttachDiskToVm, Arrays.asList(DiskContentType.DATA));
+        allowedCommandsOnTypes.put(ActionType.AttachDiskToVm, Arrays.asList(DiskContentType.DATA, DiskContentType.HOSTED_ENGINE));
         allowedCommandsOnTypes.put(ActionType.MoveOrCopyDisk, Arrays.asList(DiskContentType.DATA,
                 DiskContentType.MEMORY_DUMP_VOLUME, DiskContentType.MEMORY_METADATA_VOLUME, DiskContentType.ISO));
         allowedCommandsOnTypes.put(ActionType.LiveMigrateDisk, Arrays.asList(DiskContentType.DATA));
