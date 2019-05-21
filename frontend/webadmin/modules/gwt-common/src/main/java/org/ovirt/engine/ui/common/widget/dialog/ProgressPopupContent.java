0fe18efe095ecdfd6567e0233d8205bd6dc4ff89
Shani Leviim <sleviim@redhat.com>
2019-05-21 15:09:27 +0300

    webadmin: A notification while loading a large amount of LUNs
    Screenshot: https://imgur.com/a/Ju11GPi
    
    While trying to manage an iSCSI storage domain, and in case there is a
    large number of available LUNs, loading the popup may be slow.
    This patch adds notification while loading the popup.
    
    Change-Id: If864678886085377701fe089f6c6e0a2b34981ed
    Bug-Url: https://bugzilla.redhat.com/1475774
    Bug-Url: https://bugzilla.redhat.com/1259353
    Signed-off-by: Shani Leviim <sleviim@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/dialog/ProgressPopupContent.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/dialog/ProgressPopupContent.java
@@ -4,8 +4,10 @@ import org.ovirt.engine.ui.common.widget.IsProgressContentWidget;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
 
 public class ProgressPopupContent extends Composite implements IsProgressContentWidget {
 
@@ -13,12 +15,17 @@ public class ProgressPopupContent extends Composite implements IsProgressContent
         WidgetUiBinder uiBinder = GWT.create(WidgetUiBinder.class);
     }
 
+    @UiField
+    Label messageLabel;
+
     public ProgressPopupContent() {
         initWidget(WidgetUiBinder.uiBinder.createAndBindUi(this));
     }
 
     @Override
     public void setProgressMessage(String text) {
+        messageLabel.setText(text);
+        messageLabel.setVisible(true);
     }
 
 }
