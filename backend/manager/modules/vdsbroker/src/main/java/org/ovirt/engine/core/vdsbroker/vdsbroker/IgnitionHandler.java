ab62ed709e8b7c25233eccc29ead2486a0359c46
Roy Golan <rgolan@redhat.com>
2019-05-29 17:16:21 +0300

    core: Ignition supports hostname field
    Motivation
    In some cases we want to initialize the OS and set its hostname
    manually, be that by lack of DHCP or any other case. The initialization
    object VmInit already have a field for that, hostname, which is used by
    cloud-init
    
    Modification
    The ignition script will be enriched by a snippet to lay down
    /etc/hostname file with the content of the VmInit.hostname field
    
    Result
    The OS is ignited with the hostname requested by VmInit
    
    Change-Id: I497ff48ecd14540ce0283dc15d3e1ae791231c0a
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/IgnitionHandler.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/IgnitionHandler.java
@@ -1,8 +1,15 @@
 package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.Map;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+
+import org.apache.commons.lang.StringUtils;
 import org.ovirt.engine.core.common.businessentities.VmInit;
 
 /**
@@ -11,21 +18,67 @@ import org.ovirt.engine.core.common.businessentities.VmInit;
  * through the cloud-init config-2 disk. The ignition payload goes into the user_data as-is.
  * Ignition examples - https://coreos.com/ignition/docs/latest/examples.html
  *
- * From 4.4
+ * @since 4.4
  */
 public class IgnitionHandler {
 
     private final CloudInitHandler cloudInitHandler;
+    private final VmInit vmInit;
 
     /**
      * Ignition handler is simply a pass-through of the custom script as payload. The custom script
      * passed to vmInit should be a valid ignition config in json format
      */
     public IgnitionHandler(VmInit vmInit) {
-        this.cloudInitHandler = new CloudInitHandler(vmInit, () -> vmInit.getCustomScript());
+        this.vmInit = vmInit;
+        this.cloudInitHandler = new CloudInitHandler(vmInit, this::handle);
     }
 
     public Map<String, byte[]> getFileData() throws IOException {
         return cloudInitHandler.getFileData();
+    }
+
+    /**
+     * Enrich the ignition file with more data coming from vmInit
+     * @return the ignition script after handling, could  be remain untouched if nothing
+     * is there to be added (no other fields declared)
+     */
+    private String handle() {
+        String customScript = this.vmInit.getCustomScript();
+        customScript = handleHostname(customScript);
+        return customScript;
+    }
+
+    private String handleHostname(String customScript) {
+        if (StringUtils.isNotEmpty(vmInit.getHostname())) {
+            JsonReader reader = Json.createReader(new StringReader(customScript));
+            JsonObject ignitionJson = reader.readObject();
+            JsonObject hostnameIgnition = hostnameIgnitionSnippet();
+            JsonObjectBuilder builder = Json.createObjectBuilder();
+            ignitionJson.entrySet().forEach(e -> builder.add(e.getKey(), e.getValue()));
+            hostnameIgnition.entrySet().forEach(e -> builder.add(e.getKey(), e.getValue()));
+            customScript = builder.build().toString();
+        }
+        return customScript;
+    }
+
+    /**
+     * Creates a json snippet to add hostname file in ignition format
+     * @return
+     */
+    private JsonObject hostnameIgnitionSnippet() {
+        return Json.createObjectBuilder()
+                .add("storage", Json.createObjectBuilder()
+                    .add("files", Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                            .add("filesystem", "root")
+                            .add("path", "/etc/hostname")
+                            .add("mode", 420)
+                            .add("contents", Json.createObjectBuilder()
+                                .add("source", "data:," + vmInit.getHostname())
+                            )
+                        )
+                    )
+                ).build();
     }
 }
--- backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/vdsbroker/IgnitionPayloadTest.java
+++ backend/manager/modules/vdsbroker/src/test/java/org/ovirt/engine/core/vdsbroker/vdsbroker/IgnitionPayloadTest.java
@@ -5,10 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import javax.json.Json;
+import javax.json.JsonObject;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,23 +35,41 @@ public class IgnitionPayloadTest extends CloudInitHandlerTest {
         validIgnitionString = Files.lines(Paths.get("src/test/resources/ignition.ign")).collect(Collectors.joining());
     }
 
-     @Test
+    @Test
     public void testIgnitionCustomScriptHandling() {
-         vmInit.setCustomScript(validIgnitionString);
+        vmInit.setCustomScript(validIgnitionString);
 
-         Map<String, byte[]> payload =
-                 assertDoesNotThrow(() -> new IgnitionHandler(vmInit).getFileData());
+        Map<String, byte[]> payload =
+                assertDoesNotThrow(() -> new IgnitionHandler(vmInit).getFileData());
 
-         assertTrue(payload.containsKey("openstack/latest/user_data"));
+        assertTrue(payload.containsKey("openstack/latest/user_data"));
 
-         String userData =
-                 assertDoesNotThrow(() -> new String(payload.get("openstack/latest/user_data")));
+        String userData =
+                assertDoesNotThrow(() -> new String(payload.get("openstack/latest/user_data")));
 
-         Map<String, Object> ignitionJson =
-                 assertDoesNotThrow(() -> JsonHelper.jsonToMap(userData));
+        Map<String, Object> ignitionJson =
+                assertDoesNotThrow(() -> JsonHelper.jsonToMap(userData));
 
-         assertTrue(ignitionJson.containsKey("ignition"));
+        assertTrue(ignitionJson.containsKey("ignition"));
 
-         assertEquals(validIgnitionString, userData);
-     }
+        assertEquals(validIgnitionString, userData);
+    }
+
+
+    @Test
+    public void testIgnitionHostnameHandling() {
+        vmInit.setCustomScript(validIgnitionString);
+        vmInit.setHostname("master-0.our.org");
+
+        Map<String, byte[]> payload = assertDoesNotThrow(() -> new IgnitionHandler(vmInit).getFileData());
+
+        String userData =
+                assertDoesNotThrow(() -> new String(payload.get("openstack/latest/user_data")));
+
+        JsonObject jsonObject = Json.createReader(new StringReader(userData)).readObject();
+
+        assertTrue(jsonObject.containsKey("ignition"));
+        assertTrue(jsonObject.containsKey("storage"));
+        assertTrue(jsonObject.getJsonObject("storage").containsKey("files"));
+    }
 }
