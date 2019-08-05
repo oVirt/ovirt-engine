e26ea3bbbf02e15e2f956613922504cd2aba8134
Ondra Machacek <omachace@redhat.com>
2019-08-05 10:20:10 +0200

    restapi: Update to model 4.3.28
    New features:
    
    * Add services for labels in affinity groups
      https://bugzilla.redhat.com/1680499[1680499].
    
    Change-Id: I581a606ce154af0bd7f62f887932b61ad1fd6f58
    Bug-Url: https://bugzilla.redhat.com/1680499
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendAffinityGroupResource.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendAffinityGroupResource.java
@@ -3,7 +3,9 @@ package org.ovirt.engine.api.restapi.resource;
 import javax.ws.rs.core.Response;
 
 import org.ovirt.engine.api.model.AffinityGroup;
+import org.ovirt.engine.api.resource.AffinityGroupHostLabelsResource;
 import org.ovirt.engine.api.resource.AffinityGroupResource;
+import org.ovirt.engine.api.resource.AffinityGroupVmLabelsResource;
 import org.ovirt.engine.api.resource.AffinityGroupVmsResource;
 import org.ovirt.engine.core.common.action.ActionType;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
@@ -44,5 +46,17 @@ public class BackendAffinityGroupResource
         AffinityGroupCRUDParameters params = new AffinityGroupCRUDParameters();
         params.setAffinityGroupId(asGuid(id));
         return performAction(ActionType.RemoveAffinityGroup, params);
+    }
+
+    @Override
+    public AffinityGroupVmLabelsResource getVmLabelsResource() {
+        // TODO: implement
+        return null;
+    }
+
+    @Override
+    public AffinityGroupHostLabelsResource getHostLabelsResource() {
+        // TODO: implement
+        return null;
     }
 }
