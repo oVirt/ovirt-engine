bbf192fc05244e64ed884e75228e623c4d5dbaef
Eyal Shenitzky <eshenitz@redhat.com>
2019-11-03 11:03:43 +0200

    core: set IsDeferringFileVolumePreallocationSupported to 4.4
    Updates IsDeferringFileVolumePreallocationSupported value
    to support version 4.4.
    
    Change-Id: I98afa5b65c93a03cf3ea1099fe48d064dedcaa02
    Signed-off-by: Eyal Shenitzky <eshenitz@redhat.com>

--- packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
+++ packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -792,7 +792,7 @@ select fn_db_add_config_value('GuestNicNamesBlacklist','lo[0-9]*,Loopback.*','ge
 select fn_db_add_config_value_for_versions_up_to('HyperVSynicStimerSupported', 'false', '4.2');
 select fn_db_add_config_value_for_versions_up_to('HyperVSynicStimerSupported', 'true', '4.4');
 select fn_db_add_config_value_for_versions_up_to('IsDeferringFileVolumePreallocationSupported', 'false', '4.2');
-select fn_db_add_config_value_for_versions_up_to('IsDeferringFileVolumePreallocationSupported', 'true', '4.3');
+select fn_db_add_config_value_for_versions_up_to('IsDeferringFileVolumePreallocationSupported', 'true', '4.4');
 
 -- Managed block storage
 select fn_db_add_config_value('CinderlibCommandTimeoutInMinutes','2','general');
