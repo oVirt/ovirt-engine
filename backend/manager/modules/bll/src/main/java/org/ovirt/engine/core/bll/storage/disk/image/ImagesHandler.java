1ef90da23c3da627df59f5558853ed1ebef08222
Shani Leviim <sleviim@redhat.com>
2019-12-03 17:21:33 +0200

    core: Avoid wrong parsing of LUN ids
    When trying to delete a snapshot, and in case the VM has a LUN disk,
    its imageId is still should be ignored.
    Instead, the imageId parsed wrongly and uses "mapper" as its value,
    which causes an NPE.
    
    This patch avoids that scenario.
    
    Change-Id: I7070eac1ad65adc9bf8a634a7bedb43f9d4acb83
    Bug-Url: https://bugzilla.redhat.com/1750212
    Signed-off-by: Shani Leviim <sleviim@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/ImagesHandler.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/ImagesHandler.java
@@ -1046,6 +1046,7 @@ public class ImagesHandler {
         for (Object o : (Object[]) vm.get(VdsProperties.Devices)) {
             Map device = (Map<String, Object>) o;
             if (VmDeviceType.DISK.getName().equals(device.get(VdsProperties.Device))
+                    && !device.get(VdsProperties.ImageId).equals("mapper")
                     && activeDiskImage.getId().equals(Guid.createGuidFromString(
                     (String) device.get(VdsProperties.ImageId)))) {
                 Object[] volumeChain = (Object[]) device.get(VdsProperties.VolumeChain);
