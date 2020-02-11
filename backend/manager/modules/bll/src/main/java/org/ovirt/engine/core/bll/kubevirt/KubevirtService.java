af130b7db8ebb29359b1e302d5bdd65d98120f72
Moti Asayag <masayag@redhat.com>
2020-02-11 10:37:24 +0200

    kubevirt: Wrap kubevirt cluster registration with try-catch
    If more than a single cluster of kubevirt is added to ovirt-engine, we'd
    like to bypass an issue with its initialization (i.e. no connectivity)
    and move onwards to other kubevirt clusters initialization.
    
    Change-Id: I4886745003a637faccef66e4ac8cd23b9da08295
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/kubevirt/KubevirtService.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/kubevirt/KubevirtService.java
@@ -11,9 +11,13 @@ import org.ovirt.engine.core.common.businessentities.KubevirtProviderProperties;
 import org.ovirt.engine.core.common.businessentities.Provider;
 import org.ovirt.engine.core.common.businessentities.ProviderType;
 import org.ovirt.engine.core.dao.provider.ProviderDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class KubevirtService implements BackendService {
+    private static final Logger log = LoggerFactory.getLogger(KubevirtService.class);
+
     @Inject
     private ProviderDao providerDao;
 
@@ -23,6 +27,18 @@ public class KubevirtService implements BackendService {
     @PostConstruct
     public void init() {
         List<Provider<?>> providers = providerDao.getAllByTypes(ProviderType.KUBEVIRT);
-        providers.forEach(p -> kubevirtMonitoring.register((Provider<KubevirtProviderProperties>) p));
+        providers.forEach(p -> registerCluster((Provider<KubevirtProviderProperties>) p));
+    }
+
+    private void registerCluster(Provider<KubevirtProviderProperties> provider) {
+        try {
+            kubevirtMonitoring.register(provider);
+        } catch (Exception e) {
+            log.error("Failed to register KubeVirt cluster for provider {} with URL {}: {}",
+                    provider.getName(),
+                    provider.getUrl(),
+                    e.getMessage());
+            log.debug("Exception", e);
+        }
     }
 }
