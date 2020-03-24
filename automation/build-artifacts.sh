b49dafa2e4ed4a4025cc6afa36faba37b6150fed
Sandro Bonazzola <sbonazzo@redhat.com>
2020-03-24 11:43:50 +0100

    build: ovirt-engine-4.4.0_beta1
    Change-Id: Id0f47a9d1c4b3a3197f7f4ca1e707b2e36183675
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -38,6 +38,9 @@ else
 	export EXTRA_BUILD_FLAGS="-gs $MAVEN_SETTINGS"
 fi
 
+# this is beta build and we want full build:
+export EXTRA_BUILD_FLAGS="-gs $MAVEN_SETTINGS"
+
 export BUILD_JAVA_OPTS_GWT="$JVM_MEM_OPTS"
 
 # Set the location of the JDK that will be used for compilation:
@@ -96,9 +99,9 @@ BUILD_ALL_USER_AGENTS=1
 BUILD_LOCALES=1
 
 if [ -n "${MILESTONE}" ]; then
-	BUILD_UT=0
-	BUILD_ALL_USER_AGENTS=0
-	BUILD_LOCALES=0
+	BUILD_UT=1
+	BUILD_ALL_USER_AGENTS=1
+	BUILD_LOCALES=1
 fi
 
 rpmbuild \
