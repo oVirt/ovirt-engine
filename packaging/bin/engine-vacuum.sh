90a3bb940ebef07c567dfd4c80d925240bdafa3b
Eli Mesika <emesika@redhat.com>
2020-05-13 13:13:50 +0300

    db: cleaning orphan temp schemas
    This patch cleans orphan temp schemas left over after temp tables are
    used. vacuumdb attempts to work on those temp schemas and fails since
    those schemas are owned by the pg system admin (postgres)
    
    since engine-vacuum is running by super user account it is possible to
    remove those orphan schemas before vacuumdb is used.
    
    Change-Id: I380bcafbf23bce0240fd29bf727f0df7367b7853
    Bug-Url: https://bugzilla.redhat.com/1828931
    Signed-off-by: Eli Mesika <emesika@redhat.com>

--- packaging/bin/engine-vacuum.sh
+++ packaging/bin/engine-vacuum.sh
@@ -41,6 +41,32 @@ while getopts ":aAft:v" opt; do
     esac
 done
 
+cleanOrphanSchemas() {
+    echo "cleaning orphan schemas..."
+    tmpfile=$(mktemp /tmp/drop-orphan-system-schemas.XXXXXX.sql)
+    su - postgres -c "psql  -Atc \"select 'drop schema if exists ' || nspname || ' cascade;' \
+        from (select distinct nspname from pg_class join pg_namespace on (relnamespace=pg_namespace.oid) \
+        where pg_is_other_temp_schema(relnamespace)) as foo\" ${ENGINE_DB_DATABASE}" > $tmpfile
+    if [ -s $tmpfile ]; then
+        chmod 777 $tmpfile
+        su - postgres -c "psql -f ${tmpfile} ${ENGINE_DB_DATABASE}" >& /dev/null
+    fi
+}
+
+# Cleaning up orphans schemas left by using temporary tables
+# for details, see [1]
+# [1] https://community.synopsys.com/s/article/Postgresql-log-LOG-autovacuum-found-orphan-temp-table-pg-temp-xxx-loblist-in-database-cim
+
+[ -n "${FULL}" ] && CLEAN_ORPHAN_SCHEMAS=1
+
+if [ -n "${CLEAN_ORPHAN_SCHEMAS}" ]; then
+    if [ "${ENGINE_DB_HOST}" == "localhost" ]; then
+        cleanOrphanSchemas
+    else
+	REMOTE=1
+    fi
+fi
+
 # setups with 'trust' may have empty passwords
 [[ -n $ENGINE_DB_PASSWORD ]] && generatePgPass
 
@@ -55,3 +81,15 @@ ${TABLES+$TABLES} \
 -U $ENGINE_DB_USER \
 -d $ENGINE_DB_DATABASE \
 -w
+
+if [ "$?" -ne "0" ] && ${REMOTE}==1; then
+	echo "For remote vacuuming if you got errors like 'permission denied for schema pg_temp_XX' please do the following:
+	      1) log in into the remote database machine
+	      2) run
+	         psql  -U <db-admin-role> -Atc \"select 'drop schema if exists ' || nspname || ' cascade;'
+                 from (select distinct nspname from pg_class join pg_namespace on (relnamespace=pg_namespace.oid)
+                 where pg_is_other_temp_schema(relnamespace)) as foo\" ${ENGINE_DB_DATABASE} > <temporary file>
+              3) run
+	         psql  -U <db-admin-role> -f <temporary file>
+              4) try to run engine-vacuum again"
+fi
