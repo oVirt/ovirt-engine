324642831def4cb7738ddca60ef564b7fac9ce72
Ahmad Khiet <akhiet@redhat.com>
2020-05-19 11:32:44 +0300

    webadmin: show all disks when download denied
    Downloading multiple disks for a running VM
    is not allowed. At the current implementation
    a failure popup will show only one VM and
    one disk.
    
    This patch will list the VMs and the Disks
    that can't be downloaded including the reason,
    for example: plugged to a running VM.
    
    Screenshots: https://imgur.com/a/fczoMSs
    
    Change-Id: I2ec83197d2c5f8ade0634295114180b3d7981d5b
    Bug-Url: https://bugzilla.redhat.com/1662733
    Signed-off-by: Ahmad Khiet <akhiet@redhat.com>

--- frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/storage/DownloadImageManager.java
+++ frontend/webadmin/modules/uicommonweb/src/main/java/org/ovirt/engine/ui/uicommonweb/models/storage/DownloadImageManager.java
@@ -2,9 +2,14 @@ package org.ovirt.engine.ui.uicommonweb.models.storage;
 
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
+import org.ovirt.engine.core.common.action.ActionParametersBase;
+import org.ovirt.engine.core.common.action.ActionType;
 import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
+import org.ovirt.engine.ui.frontend.Frontend;
 import org.ovirt.engine.ui.uicommonweb.Linq;
+import org.ovirt.engine.ui.uicompat.IFrontendMultipleActionAsyncCallback;
 
 public class DownloadImageManager {
 
@@ -21,7 +26,21 @@ public class DownloadImageManager {
 
     public void startDownload(List<DiskImage> disks) {
         log.info("Start download for disks: " + Linq.getDiskAliases(disks)); //$NON-NLS-1$
-        disks.forEach(disk -> new DownloadImageHandler(disk).start());
+
+        List<ActionParametersBase> transferDiskImageParameters = disks.stream()
+                .map(DownloadImageHandler::createInitParams)
+                .collect(Collectors.toList());
+        Frontend.getInstance().runMultipleAction(ActionType.TransferDiskImage,
+                transferDiskImageParameters, callback(), true, true);
+    }
+
+    private IFrontendMultipleActionAsyncCallback callback() {
+        return result -> result.getReturnValue()
+                .forEach(actionReturnValue -> {
+                    if (actionReturnValue.getSucceeded()) {
+                        new DownloadImageHandler(actionReturnValue.getActionReturnValue()).start();
+                    }
+                });
     }
 }
 
