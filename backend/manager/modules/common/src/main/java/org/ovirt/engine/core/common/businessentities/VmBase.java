e6c2ecb9d7270e338c5a21a452191010f046e8d1
Michal Skrivanek <michal.skrivanek@redhat.com>
2020-06-26 09:50:44 +0200

    Allow to enable encrypted migrations for HE
    May not be the best idea for performance reasons, but it may make sense
    to enable encrypted migrations of HE VM in untrusted environments.
    
    Change-Id: I20c3bffc4d0b7096a3a508de7a3e49d8525dbf7c
    Bug-Url: https://bugzilla.redhat.com/1850909
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmBase.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/VmBase.java
@@ -532,7 +532,7 @@ public class VmBase implements Queryable, BusinessEntity<Guid>, Nameable, Commen
     private Boolean migrateCompressed;
 
     @CopyOnNewVersion
-    @EditableVmField
+    @EditableVmField(onHostedEngine = true)
     private Boolean migrateEncrypted;
 
     @CopyOnNewVersion
