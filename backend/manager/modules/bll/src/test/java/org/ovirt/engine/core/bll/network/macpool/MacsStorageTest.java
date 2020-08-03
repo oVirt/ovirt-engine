d1068161773d6aa328e7c71eca3299c0563b9b10
Eitan Raviv <eraviv@redhat.com>
2020-08-03 11:29:01 +0300

    core: mac storage test no macs left
    Test that MacStorage throws an exception if there are not enough macs
    available to allocate.
    
    Change-Id: I7109315082d8b30d0a58f0902e1c7c6b9e409b85
    Bug-Url: https://bugzilla.redhat.com/1760170
    Signed-off-by: Eitan Raviv <eraviv@redhat.com>

--- backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/macpool/MacsStorageTest.java
+++ backend/manager/modules/bll/src/test/java/org/ovirt/engine/core/bll/network/macpool/MacsStorageTest.java
@@ -2,17 +2,30 @@ package org.ovirt.engine.core.bll.network.macpool;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
+import org.apache.commons.lang.math.LongRange;
 import org.hamcrest.core.IsSame;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.ovirt.engine.core.common.errors.EngineError;
+import org.ovirt.engine.core.common.errors.EngineException;
 
 public class MacsStorageTest {
+
+    private static final int RANGE_FROM = 10;
+    private static final int RANGE_TO = 19;
+    private static final int NUMBER_OF_MACS = 12;
+    private static final boolean ALLOW_DUPLICATES = true;
 
     @Test
     public void rangesAltersInServingRequestsWhenLastRangeHasAvailableMac() {
@@ -86,5 +99,22 @@ public class MacsStorageTest {
             result.add(mock(Range.class));
         }
         return result;
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testNoMacsLeftToAllocate(boolean allowDuplicates) {
+        MacsStorage macsStorage = new MacsStorage(allowDuplicates);
+        Range range = new Range(new LongRange(RANGE_FROM, RANGE_TO));
+        macsStorage.addRange(range);
+        EngineException e = assertThrows(EngineException.class, () -> macsStorage.allocateAvailableMacs(NUMBER_OF_MACS));
+        assertTrue(e.getMessage().contains(EngineError.MAC_POOL_NO_MACS_LEFT.toString()));
+    }
+
+    static Stream<Object[]> testNoMacsLeftToAllocate() {
+        return Stream.of(
+                new Object[] { ALLOW_DUPLICATES },
+                new Object[] { !ALLOW_DUPLICATES }
+        );
     }
 }
