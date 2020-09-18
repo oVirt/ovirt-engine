475d88ecf7a9e48023454f7b24fe12e0afefe95f
Artur Socha <asocha@redhat.com>
2020-09-18 16:58:28 +0200

    engine: fixing score=0 after HE redeploy
    This patch provides a fix for an issue with he score being set to 0
    after host being re-installed with HE_DEPLOY option.
    
    Details:
    The scenario is as the following:
    1) put he host into maintenance
    2) reinstall with  HE_UNDEPLOY & 'put to active'
    3) when active put it back into maintenance
    4) reinstall with HE_DEPLOY & 'put to active'
    
    During activation (step 4) VDS in SetHaMaintenanceModeVDSCommand used
    to be loaded during post construct phase when
    'highlyAvailableIsConfigured' flag still being set to false
     because of steps (2,3).
    
    SetHaMaintenanceModeVDSCommand must be invoked on hosts with HA
    already configured. There is no need to check it inside the command as
    the 'invoker code' should already be aware of it and this is the idea behind
    the fix itself.
    
    Change-Id: I5fe821ff8c251ea375ebbac01d40c00d03bc921a
    Bug-Url: https://bugzilla.redhat.com/1859586
    Signed-off-by: Artur Socha <asocha@redhat.com>

--- backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/SetHaMaintenanceModeVDSCommand.java
+++ backend/manager/modules/vdsbroker/src/main/java/org/ovirt/engine/core/vdsbroker/vdsbroker/SetHaMaintenanceModeVDSCommand.java
@@ -1,38 +1,26 @@
 package org.ovirt.engine.core.vdsbroker.vdsbroker;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-
 import org.ovirt.engine.core.common.vdscommands.SetHaMaintenanceModeVDSCommandParameters;
-import org.ovirt.engine.core.dao.VdsDao;
 
 /**
  * Send variables that set Hosted Engine maintenance mode to VDSM
  */
 public class SetHaMaintenanceModeVDSCommand extends VdsBrokerCommand<SetHaMaintenanceModeVDSCommandParameters> {
-    @Inject
-    private VdsDao vdsDao;
 
     public SetHaMaintenanceModeVDSCommand(SetHaMaintenanceModeVDSCommandParameters parameters) {
         super(parameters);
     }
 
-    @PostConstruct
-    public void init() {
-        setVdsAndVdsStatic(vdsDao.get(getParameters().getVdsId()));
-    }
-
     @Override
     protected void executeVdsBrokerCommand() {
-        if (getVds().getHighlyAvailableIsConfigured()) {
-            try {
-                status = getBroker().setHaMaintenanceMode(getParameters().getMode().name(), getParameters().isEnabled());
-                proceedProxyReturnValue();
-            } catch (RuntimeException e) {
-                setVdsRuntimeErrorAndReport(e);
-                // prevent exception handler from rethrowing an exception
-                getVDSReturnValue().setExceptionString(null);
-            }
+        try {
+            status = getBroker().setHaMaintenanceMode(getParameters().getMode().name(),
+                    getParameters().isEnabled());
+            proceedProxyReturnValue();
+        } catch (RuntimeException e) {
+            setVdsRuntimeErrorAndReport(e);
+            // prevent exception handler from rethrowing an exception
+            getVDSReturnValue().setExceptionString(null);
         }
     }
 }
