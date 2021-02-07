d7c80d7734a4022f73655645d1aed4d7bffcd948
Dana Elfassy <delfassy@redhat.com>
2021-02-07 13:23:40 +0200

    engine: add fix to setting lifespan certificates
    Fixing a mistake in [1]
    Where the config value 'VdsCertificateValidityInDays' should've
    been added to 0000_config.sql and not as an upgrade script
    
    'Allow-db-upgrade-script-changes: Yes'
    
    [1] https://gerrit.ovirt.org/c/ovirt-engine/+/111628
    
    Change-Id: Ia6a02de4faf4afddcb8bb7220c4becb4c3f7da87
    Signed-off-by: Dana Elfassy <delfassy@redhat.com>

--- packaging/dbscripts/upgrade/04_04_0800_add_certificate_validity_in_days.sql
+++ packaging/dbscripts/upgrade/04_04_0800_add_certificate_validity_in_days.sql
@@ -1,1 +1,3 @@
-select fn_db_add_config_value('VdsCertificateValidityInDays','398','general');
+-- This was merged by mistake and replaced with sql code that does nothing
+--select fn_db_add_config_value('VdsCertificateValidityInDays','398','general');
+SELECT 1;
