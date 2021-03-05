9056037ec1b670b51e8ef469a468d2acdf91577e
Martin Perina <mperina@redhat.com>
2021-03-05 15:33:28 +0100

    core: Fix ClusterEmulatedMachines option content
    Fixes a difference between add and update parts for
    ClusterEmulatedMachines option for 4.5 cluster level:
    
    Add part contains CentOS/RHEL 8.3 machines:
    
     'pc-q35-rhel8.3.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.3.0,s390-ccw-virtio-2.12'
    
    Update part changes back to CentOS/RHEL 8.1 machines:
    
     'pc-q35-rhel8.1.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.1.0,s390-ccw-virtio-2.12'
    
    This patch fixes update part to really contain CentOS/RHEL 8.3 machines,
    which is a requirement for 4.5 cluster level.
    
    Change-Id: Ic04f07114147070b0a90648ef8b520b3cc36d44c
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
+++ packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -983,7 +983,8 @@ select fn_db_update_config_value('ClusterEmulatedMachines','pc-i440fx-rhel7.3.0,
 select fn_db_update_config_value('ClusterEmulatedMachines','pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel7.6.0-sxxm,s390-ccw-virtio-2.12','4.3');
 -- Since 4.4 every x86 cluster keeps two emulated machine types: for i440fx and q35 chipsets. Thus, host must support
 -- both chipsets and ClusterEmulatedMachines list also should contain values for both of them.
-select fn_db_update_config_value_for_versions_from_up_to('ClusterEmulatedMachines','pc-q35-rhel8.1.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.1.0,s390-ccw-virtio-2.12','4.4','4.5');
+select fn_db_update_config_value('ClusterEmulatedMachines','pc-q35-rhel8.1.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.1.0,s390-ccw-virtio-2.12','4.4');
+select fn_db_update_config_value('ClusterEmulatedMachines','pc-q35-rhel8.3.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.3.0,s390-ccw-virtio-2.12','4.5');
 select fn_db_update_config_value('SpiceDriverNameInGuest','{"windows": "RHEV-Spice", "linux" : "xorg-x11-drv-qxl" }','general');
 select fn_db_update_config_value('SupportedClusterLevels','4.2,4.3,4.4,4.5','general');
 select fn_db_update_config_value('SupportedVDSMVersions','4.20,4.30,4.40','general');
