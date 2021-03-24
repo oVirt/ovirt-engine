9f313384aea85295a197bbe652b023f1b8a1bfbe
Arik Hadas <ahadas@redhat.com>
2021-03-25 02:51:50 +1100

    Revert "core: block cpu hotplug on uefi"
    This reverts commit 5fe24b2e1950b859be0bf48ec09eccba7711014c.
    
    Should be fixed, no reason to keep blocking it.
    
    Change-Id: Ia480c9f808b83097b4562b4b2661eb71511d701a
    Bug-Url: https://bugzilla.redhat.com/1834250
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
+++ frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/AppErrors.java
@@ -2007,8 +2007,6 @@ public interface AppErrors extends ConstantsWithLookup {
 
     String HOT_PLUG_CPU_IS_NOT_SUPPORTED_FOR_GUEST_OS();
 
-    String HOT_PLUG_CPU_IS_NOT_SUPPORTED_FOR_UEFI();
-
     String HOT_UNPLUG_CPU_IS_NOT_SUPPORTED();
 
     String HOT_UNPLUG_CPU_IS_NOT_SUPPORTED_FOR_GUEST_OS();
