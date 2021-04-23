f3d7382cec6d78750590d63447f3feeb5aeeffb9
Hilda Stastna <hstastna@redhat.com>
2021-04-23 10:09:07 +0200

    webadmin: Display 5 sec option for refresh interval
    Display missing '5 sec' option which wasn't present anymore
    due to the change of the interval to 10 seconds for all grid tables
    to achieve decreasing load on the engine.
    
    Change-Id: I2cae7917576e1be5757b40ac66c28d5c66240d5c
    Bug-Url: https://bugzilla.redhat.com/1859921
    Signed-off-by: Hilda Stastna <hstastna@redhat.com>

--- frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/refresh/AbstractRefreshManager.java
+++ frontend/webadmin/modules/gwt-common/src/main/java/org/ovirt/engine/ui/common/widget/refresh/AbstractRefreshManager.java
@@ -3,8 +3,8 @@ package org.ovirt.engine.ui.common.widget.refresh;
 import static org.ovirt.engine.ui.common.system.StorageKeyUtils.GRID_REFRESH_RATE_KEY;
 
 import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.ovirt.engine.ui.common.system.ApplicationFocusChangeEvent;
 import org.ovirt.engine.ui.common.system.ClientStorage;
@@ -33,10 +33,11 @@ public abstract class AbstractRefreshManager<T extends RefreshPanel> implements 
 
     private static final Integer DEFAULT_REFRESH_RATE = GridTimer.DEFAULT_NORMAL_RATE;
     private static final Integer OUT_OF_FOCUS_REFRESH_RATE = Integer.valueOf(60000);
-    private static final Set<Integer> REFRESH_RATES = new LinkedHashSet<>();
+    private static final Set<Integer> REFRESH_RATES = new TreeSet<>();
 
     static {
         REFRESH_RATES.add(DEFAULT_REFRESH_RATE);
+        REFRESH_RATES.add(5000);
         REFRESH_RATES.add(10000);
         REFRESH_RATES.add(20000);
         REFRESH_RATES.add(30000);
