3938c5a6ef33dd6bf48ebc863c189a79bc2aef03
Arik Hadas <ahadas@redhat.com>
2021-04-27 19:01:56 +0300

    Revert "engine: add constraint to vm_static table"
    This reverts commit e1271f14fe1d6579f918281ea391d5280e783e29.
    
    Actually, this should be tested with Kubevirt VMs and import from
    external providers to make sure we don't fail when adding their
    configuration with this constraint.
    
    Change-Id: I84554f249079ccc7677afcb9520d8ba4c5ed35bb
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- packaging/dbscripts/upgrade/04_04_1120_add_bios_type_constraint_to_vm_static.sql
+++
@@ -1,3 +0,0 @@
-SELECT fn_db_create_constraint('vm_static',
-                               'vm_static_bios_type_not_null_if_cluster_set',
-                               'CHECK ((cluster_id IS NULL) = (bios_type IS NULL))');
