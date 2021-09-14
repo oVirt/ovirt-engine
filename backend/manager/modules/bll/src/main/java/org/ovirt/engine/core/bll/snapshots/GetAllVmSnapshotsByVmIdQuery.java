57010789883347f89c83fe9ab27186033bdf96d8
Arik Hadas <ahadas@redhat.com>
2021-09-14 16:29:41 +0300

    rest-api: skip next-run snapshots
    next-run snapshots are not 'real' snapshots, we just leveraged the
    snapshot entity to implement a next-run VM configuration, i.e.,
    configuration that should apply when the VM restarts. Therefore,
    this patch filters out next-run snapshots from the query
    GetAllVmSnapshotsByVmIdQuery.
    
    Change-Id: I40de7adff93bb598e1906ed4ba9deaba3f0f5e03
    Bug-Url: https://bugzilla.redhat.com/2003996
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/snapshots/GetAllVmSnapshotsByVmIdQuery.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/snapshots/GetAllVmSnapshotsByVmIdQuery.java
@@ -1,17 +1,17 @@
 package org.ovirt.engine.core.bll.snapshots;
 
-import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
 import org.ovirt.engine.core.bll.QueriesCommandBase;
 import org.ovirt.engine.core.bll.context.EngineContext;
-import org.ovirt.engine.core.common.businessentities.Snapshot;
+import org.ovirt.engine.core.common.businessentities.Snapshot.SnapshotType;
 import org.ovirt.engine.core.common.queries.IdQueryParameters;
 import org.ovirt.engine.core.dao.SnapshotDao;
 
 /**
- * Return a list of all the snapshots for the given VM id.<br>
+ * Return a list of all, but next-run, snapshots for the given VM id.<br>
  * The snapshots are sorted by their creation date.<br>
  * The snapshots don't contain their configuration, since this is parsed and returned as a VM object.
  */
@@ -26,8 +26,10 @@ public class GetAllVmSnapshotsByVmIdQuery<P extends IdQueryParameters>
 
     @Override
     protected void executeQueryCommand() {
-        List<Snapshot> snapshotsList =
-                snapshotDao.getAll(getParameters().getId(), getUserID(), getParameters().isFiltered());
-        getQueryReturnValue().setReturnValue(snapshotsList);
+        var snapshots = snapshotDao.getAll(getParameters().getId(), getUserID(), getParameters().isFiltered());
+        snapshots = snapshots.stream()
+                .filter(snapshot -> snapshot.getType() != SnapshotType.NEXT_RUN)
+                .collect(Collectors.toList());
+        getQueryReturnValue().setReturnValue(snapshots);
     }
 }
