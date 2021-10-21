00dff9469366615290c452cd537322b3ed2b6a15
Arik Hadas <ahadas@redhat.com>
2021-10-21 23:37:07 +0300

    core: change type of guest_mem_* fields
    They used to be of type biginteger and changed to integer by
    mistake as part of If9944576a3283d77ad6c46c14f366efdf536b816.
    This patch changes them back to biginteger.
    
    Change-Id: I9bafddc9e0ec3ac20ace65ae3db25d6ffd8328c3
    Bug-Url: https://bugzilla.redhat.com/2006602
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

---
+++ packaging/dbscripts/upgrade/04_05_0040_change_guest_mem_fields_type.sql
@@ -0,0 +1,2 @@
+SELECT fn_db_change_column_type('vm_statistics', 'guest_mem_buffered', 'INTEGER', 'BIGINT');
+SELECT fn_db_change_column_type('vm_statistics', 'guest_mem_cached', 'INTEGER', 'BIGINT');
