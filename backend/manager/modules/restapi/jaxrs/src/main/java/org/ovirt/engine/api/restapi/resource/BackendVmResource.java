846c8fe6ee6d2fda25c2a5f91a4de8b71ab8afd4
Saif Abu Saleh <sabusale@redhat.com>
2021-11-23 10:40:13 +0200

    restapi: introduce VM screenshot API call
    The API calls ScreenshotVm to get base64 encoded image
    then it decode it to byte array and return it
    
    Request example:
    
    URL: `/vms/1234/screenshot`
    
    body:
    	<action/>
    
    Response example:
    
    	P6
    	640 480
    	255
    	...
    
    Response is a byte array of the screenshot
    
    feature page: https://github.com/oVirt/ovirt-site/pull/2582
    
    Bug-Url: https://bugzilla.redhat.com/1964208
    Change-Id: I860a2443872bac9382c09f2a79120d628be96b22
    Signed-off-by: Saif Abu Saleh <sabusale@redhat.com>

--- backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendVmResource.java
+++ backend/manager/modules/restapi/jaxrs/src/main/java/org/ovirt/engine/api/restapi/resource/BackendVmResource.java
@@ -5,6 +5,7 @@
 
 package org.ovirt.engine.api.restapi.resource;
 
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -798,5 +799,14 @@ public class BackendVmResource
     @Override
     public AssignedAffinityLabelsResource getAffinityLabelsResource() {
         return inject(new BackendAssignedAffinityLabelsResource(id, VM::new));
+    }
+
+    @Override
+    public Response screenshot(Action action) {
+        String screenshot =
+                performAction(ActionType.ScreenshotVm, new VmOperationParameterBase(guid), String.class); // gets base64 encoded image
+
+        byte[] originalImage = Base64.getDecoder().decode(screenshot);
+        return Response.ok(originalImage).build();
     }
 }
