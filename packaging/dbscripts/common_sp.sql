3eec250211d3115ee2bc18f156a11019b8e0e29e
Martin Perina <mperina@redhat.com>
2021-11-26 10:50:03 +0100

    core: Add support for DC/cluster level 4.7
    This patch adds support for DC/cluster compatibility level 4.7. Hosts
    supporting 4.7 level need to be running on RHEL 8.6 (libvirt >= 7.10.0)
    or CentOS Stream/RHEL 9.
    This patch doesn't set 4.7 version for default datacenter and cluster
    for new installations, this will be solved by subsequent patch.
    
    Change-Id: I4d354fda0926784e7722a2a0cb30974db92eda63
    Bug-Url: https://bugzilla.redhat.com/2021545
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- packaging/dbscripts/common_sp.sql
+++ packaging/dbscripts/common_sp.sql
@@ -1291,7 +1291,7 @@ RETURNS VARCHAR []  AS $PROCEDURE$
 BEGIN
 
 RETURN
-ARRAY ['4.2', '4.3', '4.4', '4.5', '4.6'];
+ARRAY ['4.2', '4.3', '4.4', '4.5', '4.6', '4.7'];
 
 END;$PROCEDURE$
 LANGUAGE plpgsql;
