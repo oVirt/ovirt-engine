cc4e3476c181989fa35d16c713a5bf8e0d26b9f1
Martin Perina <mperina@redhat.com>
2022-01-31 20:57:44 +0100

    Build RPMs using GH actions
    Change-Id: I78ad897a886ece0de02fe24e4247f68b2ace795b
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ .automation/build-rpm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -xe
+
+source $(dirname "$(readlink -f "$0")")/build-srpm.sh
+
+# Install build dependencies
+dnf builddep -y rpmbuild/SRPMS/*src.rpm
+
+# Perform reasonable quick build with unit tests execution
+BUILD_UT=0
+BUILD_ALL_USER_AGENTS=0
+BUILD_LOCALES=0
+
+# Build binary package
+rpmbuild \
+    -D "_topmdir rpmbuild" \
+    -D "_rpmdir rpmbuild" \
+    ${RELEASE:+-D "release_suffix ${RELEASE}"} \
+    -D "ovirt_build_ut ${BUILD_UT}" \
+    -D "ovirt_build_all_user_agents ${BUILD_ALL_USER_AGENTS}" \
+    -D "ovirt_build_locales ${BUILD_LOCALES}" \
+    -D "ovirt_build_extra_flags ${EXTRA_BUILD_FLAGS}" \
+    --rebuild rpmbuild/SRPMS/*src.rpm
+
+# Move RPMs to exported artifacts
+[[ -d $ARTIFACTS_DIR ]] || mkdir -p $ARTIFACTS_DIR
+find rpmbuild -iname \*rpm | xargs mv -t $ARTIFACTS_DIR
---
+++ .automation/build-srpm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -xe
+
+# git hash of current commit should be passed as the 1st paraameter
+if [ "${GITHUB_SHA}" == "" ]; then
+  GIT_HASH=$(git rev-list HEAD | wc -l)
+else
+  GIT_HASH=$(git rev-parse --short $GITHUB_SHA)
+fi
+
+# Directory, where build artifacts will be stored, should be passed as the 1st parameter
+ARTIFACTS_DIR=${1:-exported-artifacts}
+
+# Prepare the version string (with support for SNAPSHOT versioning)
+VERSION=$(mvn help:evaluate  -q -DforceStdout -Dexpression=project.version)
+VERSION=${VERSION/-SNAPSHOT/-0.${GIT_HASH}.$(date +%04Y%02m%02d%02H%02M)}
+IFS='-' read -ra VERSION <<< "$VERSION"
+RELEASE=${VERSION[1]-1}
+MILESTONE=master
+
+# GH RPM builds will be used only for OST so Firefox and Chrome are enough
+# GWT build memory needs to be limited
+EXTRA_BUILD_FLAGS=""
+EXTRA_BUILD_FLAGS="${EXTRA_BUILD_FLAGS} -Dgwt.userAgent=gecko1_8,safari"
+EXTRA_BUILD_FLAGS="${EXTRA_BUILD_FLAGS} -Dgwt.compiler.localWorkers=1"
+EXTRA_BUILD_FLAGS="${EXTRA_BUILD_FLAGS} -Dgwt.jvmArgs='-Xms1G -Xmx3G'"
+
+export MAVEN_OPTS="-Xms1G -Xmx2G"
+
+# Set the location of the JDK that will be used for compilation:
+export JAVA_HOME="${JAVA_HOME:=/usr/lib/jvm/java-11}"
+
+[ -d ${ARTIFACTS_DIR} ] || mkdir -p ${ARTIFACTS_DIR}
+[ -d rpmbuild/SOURCES ] || mkdir -p rpmbuild/SOURCES
+
+make generated-files
+
+# Get the tarball
+make dist
+mv *.tar.gz rpmbuild/SOURCES
+
+# create the src.rpm
+rpmbuild \
+    -D "_topdir rpmbuild" \
+    -ts rpmbuild/SOURCES/*.gz
