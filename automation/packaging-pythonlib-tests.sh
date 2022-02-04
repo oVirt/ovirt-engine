308f7ee47cadcade0afb7afbe2ef04588c9dd9b5
Martin Perina <mperina@redhat.com>
2022-02-04 16:34:17 +0100

    Remove STDCI configuration
    1. Moves milestone-config.sh into .automation directory
    2. Removes STDCI configuration
    
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- automation/packaging-pythonlib-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-trap popd 0
-pushd $(dirname "$(readlink -f "$0")")/..
-
-export PYTHONPATH="packaging/pythonlib:${PYTHONPATH}"
-python3 -m pytest --verbose packaging/pythonlib
-
--- automation/spotbugs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn -v
-mvn clean install com.github.spotbugs:spotbugs-maven-plugin:spotbugs -U -DskipTests -s ${MAVEN_SETTINGS}
