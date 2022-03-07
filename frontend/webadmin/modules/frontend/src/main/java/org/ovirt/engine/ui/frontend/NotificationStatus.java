8aae9f6d6d2cc69b0a6bc7c0886655a1a72f0809
Radoslaw Szwajkowski <rszwajko@redhat.com>
2022-03-07 19:12:29 +0100

    webadmin: display toast notifications via Frontend
    Before, in order to display a toast notification, the component needed
    direct access to NotificationPresenterWidget.
    With this patch this restriction has been removed. The notification
    handler registers itself in the Frontend which allows public access.

---
+++ frontend/webadmin/modules/frontend/src/main/java/org/ovirt/engine/ui/frontend/NotificationStatus.java
@@ -0,0 +1,11 @@
+package org.ovirt.engine.ui.frontend;
+
+/**
+ * Version without any dependencies to be used across all modules.
+ */
+public enum NotificationStatus {
+    INFO,
+    SUCCESS,
+    WARNING,
+    DANGER;
+}
