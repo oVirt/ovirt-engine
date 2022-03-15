4aa4c2a16c607daa707c55bf6a19238ca30adc85
dangel101 <dangel101@gmail.com>
2022-03-15 15:53:33 +0200

    ansible runner for debug

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/common/utils/ansible/AnsibleRunnerHttpClient.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/common/utils/ansible/AnsibleRunnerHttpClient.java
@@ -19,6 +19,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.UUID;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
@@ -39,7 +40,6 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
-import org.ovirt.engine.core.common.businessentities.VDS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -61,6 +61,7 @@ public class AnsibleRunnerHttpClient {
     private ObjectMapper mapper;
     private HttpClient httpClient;
     private AnsibleRunnerLogger runnerLogger;
+    private UUID uuid;
 
     public AnsibleRunnerHttpClient() {
         this.httpClient = HttpClientBuilder.create().build();
@@ -70,6 +71,14 @@ public class AnsibleRunnerHttpClient {
                 .build()
                 .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
+
+    public void setUUID(UUID uuid ) {
+        this.uuid = uuid;
+    }
+//
+//    public UUID getUUID() {
+//        return this.uuid;
+//    }
 
     public void addHost(String hostname, int port) {
         if (existsHost(hostname)) {
@@ -108,42 +117,42 @@ public class AnsibleRunnerHttpClient {
         }
     }
 
-    public String runPlaybook(AnsibleCommandConfig command) {
-        if (command.hosts() != null) {
-            for (VDS host : command.hosts()) {
-                addHost(host.getHostName(), host.getSshPort());
-            }
-        }
-
-        URI uri = buildRunnerURI(
-            String.format("playbooks/%1$s", command.playbook()),
-            command.hostnames() == null ? null :
-                new BasicNameValuePair("limit", StringUtils.join(command.hostnames(), ",")),
-            new BasicNameValuePair("check", String.valueOf(command.isCheckMode()))
-        );
-
-        StringEntity entity = new StringEntity(formatCommandVariables(command.variables(), command.playAction()),
-                ContentType.APPLICATION_JSON);
-
-        HttpPost request = new HttpPost(uri);
-        request.setEntity(entity);
-
-        // We need to lock the call to execute the playbook, because all extravars/cmdline/settings of the playbook
-        // are written to the local disk and in multiple parallel execution of playbooks are executed it may happen
-        // that it execute the first playbook, store its data, and the second playbook is executed with cached data.
-        // The removal of cached data happens at the begging of the playbook execution in runner-service, so we must
-        // be sure to not execute that code in parallel.
-        synchronized (executeLock) {
-            HttpResponse response = execute(request);
-            JsonNode node = readResponse(response);
-            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_ACCEPTED) {
-                throw new AnsibleRunnerCallException(
-                        "Failed to execute call to start playbook. %1$s", node.asText()
-                );
-            }
-            return RunnerJsonNode.playUuid(node);
-        }
-    }
+//    public String runPlaybook(AnsibleCommandConfig command) {
+//        if (command.hosts() != null) {
+//            for (VDS host : command.hosts()) {
+//                addHost(host.getHostName(), host.getSshPort());
+//            }
+//        }
+//
+//        URI uri = buildRunnerURI(
+//            String.format("playbooks/%1$s", command.playbook()),
+//            command.hostnames() == null ? null :
+//                new BasicNameValuePair("limit", StringUtils.join(command.hostnames(), ",")),
+//            new BasicNameValuePair("check", String.valueOf(command.isCheckMode()))
+//        );
+//
+//        StringEntity entity = new StringEntity(formatCommandVariables(command.variables(), command.playAction()),
+//                ContentType.APPLICATION_JSON);
+//
+//        HttpPost request = new HttpPost(uri);
+//        request.setEntity(entity);
+//
+//        // We need to lock the call to execute the playbook, because all extravars/cmdline/settings of the playbook
+//        // are written to the local disk and in multiple parallel execution of playbooks are executed it may happen
+//        // that it execute the first playbook, store its data, and the second playbook is executed with cached data.
+//        // The removal of cached data happens at the begging of the playbook execution in runner-service, so we must
+//        // be sure to not execute that code in parallel.
+//        synchronized (executeLock) {
+//            HttpResponse response = execute(request);
+//            JsonNode node = readResponse(response);
+//            if (response.getStatusLine().getStatusCode() != HttpStatus.SC_ACCEPTED) {
+//                throw new AnsibleRunnerCallException(
+//                        "Failed to execute call to start playbook. %1$s", node.asText()
+//                );
+//            }
+//            return RunnerJsonNode.playUuid(node);
+//        }
+//    }
 
     protected String formatCommandVariables(Map<String, Object> variables, String playAction) {
         String result;
