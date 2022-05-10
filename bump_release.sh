4ade3f2ad32abda3fa9e59b06b01357b17867969
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-05-10 19:20:37 +0200

    build: post ovirt-engine-4.5.0.8
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

--- bump_release.sh
+++ bump_release.sh
@@ -6,7 +6,7 @@ if [ -z "${RPM_PACKAGER}" ] ; then
 fi
 
 # Set pom to build final release
-find . -name pom.xml -exec gsed -i "s:-SNAPSHOT::" {} +
+find . -name pom.xml -exec sed -i "s:-SNAPSHOT::" {} +
 
 # Get current ovirt-engine version
 VERSION="$(grep -E "<version"  pom.xml | head -n1 | awk -F '[<>]' '/version/{print $3}')"
@@ -16,7 +16,7 @@ CHANGELOG="* $(LC_ALL=C date "+%a %b %d %Y") ${RPM_PACKAGER} - ${VERSION}\n- Bum
 export CHANGELOG
 
 # Add changelog to the spec file
-gsed -i "/^%changelog/a ${CHANGELOG}" ovirt-engine.spec.in
+sed -i "/^%changelog/a ${CHANGELOG}" ovirt-engine.spec.in
 
 # Adjust copr build config for releasing
 patch -p0 --ignore-whitespace .copr/Makefile <<'__EOF__'
@@ -49,7 +49,7 @@ git checkout -- ovirt-engine.spec.in
 # Bump to next version
 NEXT_VERSION="$(echo ${VERSION} | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')"
 export NEXT_VERSION
-find . -name pom.xml -exec gsed -i "s:${VERSION}-SNAPSHOT:${NEXT_VERSION}-SNAPSHOT:" {} +
+find . -name pom.xml -exec sed -i "s:${VERSION}-SNAPSHOT:${NEXT_VERSION}-SNAPSHOT:" {} +
 
 # commit
 git add -u
