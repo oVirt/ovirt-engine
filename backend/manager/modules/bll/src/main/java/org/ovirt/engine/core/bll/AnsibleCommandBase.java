c3eef5fcd65bbed4c9fc8fdc90cd9b6b7c783d8a
Liran Rotenberg <lrotenbe@redhat.com>
2022-06-27 12:36:52 +0300

    ansible: set initial event number
    If the ansible playbook execution happened faster than the engine, we
    may get a successful status and the correct number of events. In that
    case we may skip the event processing in the ansible callback. Which may
    lead us to wrong data returned to the command itself.
    
    Change-Id: I432938a7fc241701eb260c1fbc460d1de7149092
    Signed-off-by: Liran Rotenberg <lrotenbe@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AnsibleCommandBase.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AnsibleCommandBase.java
@@ -64,7 +64,6 @@ public abstract class AnsibleCommandBase <T extends AnsibleCommandParameters> ex
         } else {
             getParameters().setPlayUuid(ansibleReturnValue.getPlayUuid());
             getParameters().setLogFile(ansibleReturnValue.getLogFile().toString());
-            getParameters().setLastEventId(ansibleReturnValue.getLastEventId());
             persistCommand(getParameters().getParentCommand(), true);
             setSucceeded(true);
         }
