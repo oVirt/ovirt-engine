03b718c51d2b41b4521dbf9c3e3f9cc5ee0f05ce
Martin Nečas <mnecas@redhat.com>
2022-06-28 12:25:50 +0200

    Ansible: add client and callback debug

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AnsibleCallback.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/AnsibleCallback.java
@@ -68,7 +68,8 @@ public class AnsibleCallback implements CommandCallback {
         // Process the events if the playbook is running:
         totalEvents = runnerClient.getTotalEvents(playUuid);
 
-        if (msg.equalsIgnoreCase("running") || totalEvents == 0 || msg.equalsIgnoreCase("successful")
+        log.debug("LastEventId: ", command.getParameters().getLastEventId(), "totalEvents: ", totalEvents, "playbookStatus: ", playbookStatus);
+        if (msg.equalsIgnoreCase("running") || msg.equalsIgnoreCase("successful")
                 && command.getParameters().getLastEventId() < totalEvents) {
             command.getParameters().setLastEventId(runnerClient.processEvents(
                     playUuid, command.getParameters().getLastEventId(), fn, msg, ret.getLogFile()));
