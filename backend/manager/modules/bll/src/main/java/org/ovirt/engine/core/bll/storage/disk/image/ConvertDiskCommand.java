24c3c61c5d9045de79a5934f74c0518f9423d35a
Benny Zlotnik <bzlotnik@redhat.com>
2022-07-20 11:59:06 +0300

    core: pass DiskBackup to CreateVolumeContainer
    Not passing DiskBackup from ConvertDiskCommand will result in the
    allocation policy being overriden and consequnetly fail the conversion.
    
    This patch sets DiskBackup and always passes it to ConvertDiskCommand.
    
    Bug-Url: https://bugzilla.redhat.com/2108012
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/ConvertDiskCommand.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/disk/image/ConvertDiskCommand.java
@@ -35,6 +35,7 @@ import org.ovirt.engine.core.common.asynctasks.EntityInfo;
 import org.ovirt.engine.core.common.businessentities.StorageDomain;
 import org.ovirt.engine.core.common.businessentities.VM;
 import org.ovirt.engine.core.common.businessentities.VdsmImageLocationInfo;
+import org.ovirt.engine.core.common.businessentities.storage.DiskBackup;
 import org.ovirt.engine.core.common.businessentities.storage.DiskImage;
 import org.ovirt.engine.core.common.businessentities.storage.ImageStatus;
 import org.ovirt.engine.core.common.businessentities.storage.VolumeFormat;
@@ -249,6 +250,7 @@ public class ConvertDiskCommand<T extends ConvertDiskCommandParameters> extends 
         parameters.setParentParameters(getParameters());
         parameters.setEndProcedure(ActionParametersBase.EndProcedure.COMMAND_MANAGED);
         parameters.setParentCommand(getActionType());
+        parameters.setBackup(getBackup(diskImage));
         parameters.setEntityInfo(new EntityInfo(VdcObjectType.Disk, diskImage.getId()));
         return parameters;
     }
@@ -344,6 +346,11 @@ public class ConvertDiskCommand<T extends ConvertDiskCommandParameters> extends 
     private VolumeFormat getVolumeFormat(DiskImage diskImage) {
         return getParameters().getVolumeFormat() == null ? diskImage.getVolumeFormat() :
                 getParameters().getVolumeFormat();
+    }
+
+    private DiskBackup getBackup(DiskImage diskImage) {
+        return getParameters().getBackup() == null ? diskImage.getBackup() :
+                getParameters().getBackup();
     }
 
     private VdsmImageLocationInfo getLocationInfo() {
