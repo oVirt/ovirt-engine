ac0f1ec21d7999c843948183bcbe3203926cc2a7
dangel101 <dangel101@gmail.com>
2022-08-16 14:41:48 +0300

    Replaced the usage of AppConfig & KeysConfig classes with storing the data in HashMaps
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=2089299

--- backend/manager/tools/src/main/java/org/ovirt/engine/core/config/db/ConfigDaoImpl.java
+++ backend/manager/tools/src/main/java/org/ovirt/engine/core/config/db/ConfigDaoImpl.java
@@ -8,12 +8,11 @@ import java.sql.SQLException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 import javax.xml.xpath.XPathExpressionException;
 
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.ConfigurationException;
 import org.ovirt.engine.core.config.entity.ConfigKey;
 import org.ovirt.engine.core.config.entity.ConfigKeyFactory;
 import org.ovirt.engine.core.utils.db.StandaloneDataSource;
@@ -31,25 +30,25 @@ public class ConfigDaoImpl implements ConfigDao {
     private String defaultColumn;
     private DataSource ds;
 
-    public ConfigDaoImpl(Configuration appConfig) throws ClassNotFoundException, SQLException, ConfigurationException,
-            ConnectException, XPathExpressionException {
-        valueColumn = appConfig.getString("configColumnValue");
-        configTable = appConfig.getString("configTable");
-        nameColumn = appConfig.getString("configColumnName");
-        versionColumn = appConfig.getString("configColumnVersion");
-        defaultColumn = appConfig.getString("configColumnDefault");
+    public ConfigDaoImpl(Map<String, String> appConfig) throws ClassNotFoundException, SQLException, ConnectException,
+            XPathExpressionException {
+        valueColumn = appConfig.get("configColumnValue");
+        configTable = appConfig.get("configTable");
+        nameColumn = appConfig.get("configColumnName");
+        versionColumn = appConfig.get("configColumnVersion");
+        defaultColumn = appConfig.get("configColumnDefault");
         selectSql =
                 MessageFormat.format("select {0} from {1} where {2}=? and {3} =?",
-                                 valueColumn,
-                                 configTable,
-                                 nameColumn,
-                                 versionColumn);
+                        valueColumn,
+                        configTable,
+                        nameColumn,
+                        versionColumn);
         updateSql =
                 MessageFormat.format("update {0} set {1}=? where {2}=? and {3}=?",
-                                 configTable,
-                                 valueColumn,
-                                 nameColumn,
-                                 versionColumn);
+                        configTable,
+                        valueColumn,
+                        nameColumn,
+                        versionColumn);
         selectKeysForNameSql =
                 MessageFormat.format("select * from {0} where {1}=? ",
                         configTable,
--- backend/manager/tools/src/test/java/org/ovirt/engine/core/config/EngineConfigPropertiesTest.java
+++ backend/manager/tools/src/test/java/org/ovirt/engine/core/config/EngineConfigPropertiesTest.java
@@ -4,12 +4,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.ovirt.engine.core.common.config.ConfigValues;
@@ -19,13 +21,19 @@ import org.ovirt.engine.core.common.config.ConfigValues;
  */
 public class EngineConfigPropertiesTest {
     private static final String PROPERTIES_PATH = System.getProperty("engine-config.properties.production.file");
-    private static PropertiesConfiguration pc;
+    private static Set<String> allProps;
 
     @BeforeAll
     public static void setUpClass() {
+        allProps = new HashSet<>();
         try {
-            pc = new PropertiesConfiguration(PROPERTIES_PATH);
-        } catch (ConfigurationException e) {
+            List<String> lines = Files.readAllLines(Path.of(PROPERTIES_PATH));
+            for (String line: lines) {
+                if (line.contains("=")) {
+                    allProps.add(line.split("=")[0].split("\\.")[0]);
+                }
+            }
+        } catch (IOException e) {
             fail(String.format("Failed to load configuration from the engine-config.properties.production.file " +
                     "environment variable set to %s.", PROPERTIES_PATH));
         }
@@ -34,11 +42,9 @@ public class EngineConfigPropertiesTest {
     @Test
     public void testKeysAreRealConfigValues() {
         Set<String> badKeys = new HashSet<>();
-        Iterator<String> keyIter = pc.getKeys();
+        Iterator<String> keyIter = allProps.iterator();
         while (keyIter.hasNext()) {
-            String key = keyIter.next();
-            String confValueName = key.split("\\.")[0];
-
+            String confValueName = keyIter.next();
             try {
                 ConfigValues.valueOf(confValueName);
             } catch (IllegalArgumentException e) {
