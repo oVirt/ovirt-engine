574f3ced80d6510b6937ca834311f787ad7d54ed
Brooklyn Dewolf <contact@brooklyn.gent>
2024-05-16 09:55:10 +0200

    Bump machine type for cluster level 4.8
    Hosts supporting level 4.8 need to be running on RHEL 9.4 or CentOS Stream/RHEL 9.
    
    Signed-off-by: Brooklyn Dewolf <contact@brooklyn.gent>

--- packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
+++ packaging/dbscripts/upgrade/pre_upgrade/0000_config.sql
@@ -87,6 +87,7 @@ select fn_db_add_config_value('ClusterEmulatedMachines','pc-q35-rhel8.1.0,pc-q35
 select fn_db_add_config_value('ClusterEmulatedMachines','pc-q35-rhel8.3.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.3.0,s390-ccw-virtio-2.12','4.5');
 select fn_db_add_config_value('ClusterEmulatedMachines','pc-q35-rhel8.4.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.4.0,s390-ccw-virtio-2.12','4.6');
 select fn_db_add_config_value('ClusterEmulatedMachines','pc-q35-rhel8.6.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.4.0,s390-ccw-virtio-2.12','4.7');
+select fn_db_add_config_value('ClusterEmulatedMachines','pc-q35-rhel9.4.0,pc-q35-4.1,pc-i440fx-rhel7.6.0,pc-i440fx-2.12,pseries-rhel8.4.0,s390-ccw-virtio-2.12','4.8');
 select fn_db_add_config_value('CpuOverCommitDurationMinutes','2','general');
 --Handling Data directory for ENGINE
 select fn_db_add_config_value('DataDir','/usr/share/engine','general');
