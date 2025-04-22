oVirt/ovirt-engine

commit 0455a66742488c8511c2a078c7a0bba695ac5cb4

core: LSM: fix locking on restart
This patch fixes an issue when the engine is restarted during LSM,
the command gets stuck.

The cause of this issue was that EngineLocks were passed to
LiveMigrateDiskCommand, and from there to CreateSnapshotCommand in
the executeCommand() stage. There the locks are released, and
reacquired later on before snapshot remove phase. If the engine was
restarted during CreateSnapshot phase - both the CreateSnapshot and
LiveMigrateDisk commands resumed, running reacquireLocks, and then
before the Snapshot Remove phase, LiveMigrateDiskCommand attempted
to reacquire locks again, getting stuck.

In this patch we override reacquireLocks method in LiveMigrateDisk
command, doing nothing, and then the locks on Disk and VM are
acquired separately before the snapshot remove phase

Bug-Url: https://bugzilla.redhat.com/2110186

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/lsm/LiveMigrateDiskCommand.java
@@ -83,6 +83,7 @@ import org.ovirt.engine.core.dao.ImageStorageDomainMapDao;
 import org.ovirt.engine.core.dao.StorageDomainDao;
 import org.ovirt.engine.core.dao.StorageDomainStaticDao;
 import org.ovirt.engine.core.dao.VmDao;
+import org.ovirt.engine.core.utils.lock.EngineLock;
 import org.ovirt.engine.core.utils.transaction.TransactionSupport;
 import org.ovirt.engine.core.vdsbroker.ResourceManager;
 import org.ovirt.engine.core.vdsbroker.builder.vminfo.VmInfoBuildUtils;
@@ -292,7 +293,8 @@ public class LiveMigrateDiskCommand<T extends LiveMigrateDiskParameters> extends
             // at the end of CreateSnapshotForVm command called from the executeCommand() method.
             // Here the lock is acquired again and will be released when this command (LiveMigrateDisk)
             // finishes.
-            if (!lockManager.acquireLock(getLock()).isAcquired()) {
+            EngineLock removeSnapshotLock = createEngineLockForSnapshotRemove();
+            if (!lockManager.acquireLock(removeSnapshotLock).isAcquired()) {
                 log.info("Failed to acquire VM lock, will retry on the next polling cycle");
                 return true;
             }
@@ -362,6 +364,17 @@ public class LiveMigrateDiskCommand<T extends LiveMigrateDiskParameters> extends
         imageDao.updateStatusOfImagesByImageGroupId(getParameters().getImageGroupID(), ImageStatus.OK);
     }
 
+    private void releaseSnapshotLock() {
+        EngineLock removeSnapshotLock = createEngineLockForSnapshotRemove();
+        lockManager.releaseLock(removeSnapshotLock);
+    }
+
+    private EngineLock createEngineLockForSnapshotRemove() {
+        return new EngineLock(
+                getExclusiveLocksForSnapshotRemove(),
+                getSharedLocksForSnapshotRemove());
+    }
+
     private boolean isConsiderSuccessful() {
         return getParameters().getLiveDiskMigrateStage() == LiveDiskMigrateStage.AUTO_GENERATED_SNAPSHOT_REMOVE_END;
     }
@@ -371,6 +384,7 @@ public class LiveMigrateDiskCommand<T extends LiveMigrateDiskParameters> extends
         super.endSuccessfully();
         updateImagesInfo();
         unlockDisk();
+        releaseSnapshotLock();
         setSucceeded(true);
     }
 
@@ -740,13 +754,21 @@ public class LiveMigrateDiskCommand<T extends LiveMigrateDiskParameters> extends
 
     @Override
     protected Map<String, Pair<String, String>> getExclusiveLocks() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    protected Map<String, Pair<String, String>> getSharedLocks() {
+        return Collections.emptyMap();
+    }
+
+    private Map<String, Pair<String, String>> getExclusiveLocksForSnapshotRemove() {
         return Collections.singletonMap(getParameters().getImageGroupID().toString(),
                 LockMessagesMatchUtil.makeLockingPair(LockingGroup.DISK,
                         getDiskIsBeingMigratedMessage(getDiskImageByDiskId(getParameters().getImageGroupID()))));
     }
 
-    @Override
-    protected Map<String, Pair<String, String>> getSharedLocks() {
+    private Map<String, Pair<String, String>> getSharedLocksForSnapshotRemove() {
         return Collections.singletonMap(getVmId().toString(),
                 LockMessagesMatchUtil.makeLockingPair(LockingGroup.VM, EngineMessage.ACTION_TYPE_FAILED_OBJECT_LOCKED));
     }

commit 3b514a536d07b68eca27114510eb8382a23233cc

Use correct locks while running live migration of multiple disks of one VM. The live disks migration may fail with an error "The VM is performing an operation on a Snapshot". This is caused by the https://github.com/oVirt/ovirt-engine/pull/670 (https://bugzilla.redhat.com/show_bug.cgi?id=2110186) fix. There may be two different scenarios of the LiveMigrateDiskCommand: 1. Generic scenario, when the live migration initiated via UI/API. In this case needed lock is created at the MoveDiskCommand.lockVmWithWait. And it should be used at the LiveMigrateDiskCommand.performNextOperation as it was before the "Bug 2110186" fix. 2. "ovirt-engine restarted during the live disks migration" scenario. In this case another lock (introduced by the "Bug 2110186" fix) should be used to cleanup interrupted commands.
Signed-off-by: Stepan Ermakov <sermakov@orionsoft.ru>

backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/storage/lsm/LiveMigrateDiskCommand.java
@@ -295,7 +295,7 @@ public class LiveMigrateDiskCommand<T extends LiveMigrateDiskParameters> extends
             // at the end of CreateSnapshotForVm command called from the executeCommand() method.
             // Here the lock is acquired again and will be released when this command (LiveMigrateDisk)
             // finishes.
-            EngineLock removeSnapshotLock = createEngineLockForSnapshotRemove();
+            EngineLock removeSnapshotLock = getEngineLockForSnapshotRemove();
             if (!lockManager.acquireLock(removeSnapshotLock).isAcquired()) {
                 log.info("Failed to acquire VM lock, will retry on the next polling cycle");
                 return true;
@@ -376,6 +376,17 @@ public class LiveMigrateDiskCommand<T extends LiveMigrateDiskParameters> extends
         return new EngineLock(
                 getExclusiveLocksForSnapshotRemove(),
                 getSharedLocksForSnapshotRemove());
+    }
+
+    private EngineLock getEngineLockForSnapshotRemove() {
+        EngineLock lock = getLock();
+        if (lock != null && !lock.getExclusiveLocks().isEmpty()) {
+            // In the generic scenario, use the lock created by the MigrateDiskCommand
+            return lock;
+        }
+        // This happens when the engine was restarted during active live disk migration. Use special lock to clean up
+        // temporary VM snapshot created by the migration
+        return createEngineLockForSnapshotRemove();
     }
 
     private boolean isConsiderSuccessful() {