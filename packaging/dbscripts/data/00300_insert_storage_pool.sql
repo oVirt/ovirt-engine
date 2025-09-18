51a78e99f6cb4f42151e6742c45b27f536327318
Jean-Louis Dupond <jean-louis@dupond.be>
2025-09-18 12:12:31 +0200

    core: Set default DC/cluster to 4.8 on new installs
    Sets default DC/cluster compatibility level to 4.8 on new installations.
    During upgrade the default DC/cluster level is unchanged and need to be
    updated manually.
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- packaging/dbscripts/data/00300_insert_storage_pool.sql
+++ packaging/dbscripts/data/00300_insert_storage_pool.sql
@@ -10,7 +10,7 @@
 -- Data for Name: storage_pool; Type: TABLE DATA; Schema: public; Owner: engine
 --
 
-INSERT INTO storage_pool (id, name, description, storage_pool_type, storage_pool_format_type, status, master_domain_version, spm_vds_id, compatibility_version, _create_date, _update_date, quota_enforcement_type, free_text_comment, is_local, mac_pool_id) VALUES ('58cfb470-02f3-03d7-0386-0000000003bc', 'Default', 'The default Data Center', 1, NULL, 0, 0, NULL, '4.7', '2019-11-04 10:42:40.790182+02', NULL, NULL, NULL, NULL, '58ca604b-017d-0374-0220-00000000014e');
+INSERT INTO storage_pool (id, name, description, storage_pool_type, storage_pool_format_type, status, master_domain_version, spm_vds_id, compatibility_version, _create_date, _update_date, quota_enforcement_type, free_text_comment, is_local, mac_pool_id) VALUES ('58cfb470-02f3-03d7-0386-0000000003bc', 'Default', 'The default Data Center', 1, NULL, 0, 0, NULL, '4.8', '2019-11-04 10:42:40.790182+02', NULL, NULL, NULL, NULL, '58ca604b-017d-0374-0220-00000000014e');
 
 
 --
